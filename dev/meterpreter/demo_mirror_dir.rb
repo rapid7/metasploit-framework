28195abe63fd523dba8ad76a3a1bbfee16dd4ff6
Matt Miller <mmiller@hick.org>
2005-04-18 03:56:29 +0000

    directory mirroring
    git-svn-id: file:///home/svn/incoming/trunk@2417 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ dev/meterpreter/demo_mirror_dir.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby -I. -I../../lib
+
+require 'DemoClient'
+
+host   = ARGV[1] || '127.0.0.1'
+port   = ARGV[2] || '12345'
+dir    = ARGV[3] || "%WINDIR%\inf"
+client = DemoClient.new(host, port).client
+
+begin
+	Dir.mkdir('/tmp/mirror_demo')
+rescue
+end
+
+client.fs.dir.download('/tmp/mirror_demo', '%WINDIR%\inf', true)
