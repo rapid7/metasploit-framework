be7b71783b85b6d4bc25b9aeec93b5d1b7957791
Matt Miller <mmiller@hick.org>
2005-04-18 04:10:38 +0000

    fixup
    git-svn-id: file:///home/svn/incoming/trunk@2419 4d416f70-5f16-0410-b530-b9f4589650da

--- dev/meterpreter/demo_mirror_dir.rb
+++ dev/meterpreter/demo_mirror_dir.rb
@@ -2,14 +2,15 @@
 
 require 'DemoClient'
 
-host   = ARGV[1] || '127.0.0.1'
-port   = ARGV[2] || '12345'
-dir    = ARGV[3] || "%WINDIR%\\inf"
-client = DemoClient.new(host, port).client
+host    = ARGV[0] || '127.0.0.1'
+port    = ARGV[1] || '12345'
+src_dir = ARGV[2] || "%WINDIR%\\inf"
+dst_dir = ARGV[3] || "/tmp/mirror_demo"
+client  = DemoClient.new(host, port).client
 
 begin
-	Dir.mkdir('/tmp/mirror_demo')
+	Dir.mkdir(dst_dir)
 rescue
 end
 
-client.fs.dir.download('/tmp/mirror_demo', dir, true)
+client.fs.dir.download(dst_dir, src_dir, true)
