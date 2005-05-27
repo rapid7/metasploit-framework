c783ce4d2994573a1d24fc5f02037a23af269416
Matt Miller <mmiller@hick.org>
2005-05-27 05:41:48 +0000

    cool
    git-svn-id: file:///home/svn/incoming/trunk@2530 4d416f70-5f16-0410-b530-b9f4589650da

--- user_interfaces/tc.rb
+++ user_interfaces/tc.rb
@@ -4,5 +4,5 @@ require 'test/unit/ui/console/testrunner'
 require 'Msf/Core.rb.ts'
 require 'Msf/Base.rb.ts'
 
-Test::Unit::UI::Console::TestRunner.run(Msf::TestSuite
+Test::Unit::UI::Console::TestRunner.run(Msf::TestSuite)
 Test::Unit::UI::Console::TestRunner.run(Msf::Base::TestSuite)
