675dbe37b0f1d420edab269e0ed9516e6359f306
Matt Miller <mmiller@hick.org>
2005-06-04 08:15:10 +0000

    starting to move shared classes into rex
    git-svn-id: file:///home/svn/incoming/trunk@2559 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ lib/rex/logging/log_sink.rb
@@ -0,0 +1,36 @@
+require 'Rex/Constants'
+
+module Rex
+module Logging
+
+###
+#
+# LogSink
+# -------
+#
+# This abstract interface is what must be implemented by any class
+# that would like to register as a log sink on a given LogDispatcher
+# instance, such as the Framework object.
+#
+###
+module LogSink
+
+	def cleanup
+	end
+
+	def log(sev, src, level, msg, from)
+		raise NotImplementedError
+	end
+
+protected
+
+	def get_current_timestamp
+		return Time.now.strftime("%m/%d/%Y %H:%M:%S")
+	end
+
+end
+
+end
+end
+
+require 'Rex/Logging/Sinks/Flatfile'
---
+++ lib/rex/logging/sinks/flatfile.rb
@@ -0,0 +1,50 @@
+module Rex
+module Logging
+module Sinks
+
+###
+#
+# Flatfile
+# --------
+#
+# This class implements the LogSink interface and backs it against a 
+# file on disk.
+#
+###
+class Flatfile
+
+	include Rex::Logging::LogSink
+
+	def initialize(file)
+		self.fd = File.new(file, "a")
+	end
+
+	def cleanup
+		fd.close
+	end
+
+	def log(sev, src, level, msg, from)
+		if (sev == LOG_RAW)
+			fd.write(msg)
+		else
+			code = 'i'
+
+			case sev
+				when LOG_DEBUG
+					code = 'd'
+				when LOG_ERROR
+					code = 'e'
+				when LOG_INFO
+					code = 'i'
+			end
+			fd.write("[#{get_current_timestamp}] [#{code}(#{level})] #{src}: #{msg}\n")
+		end
+	end
+
+protected
+
+	attr_accessor :fd
+
+end
+
+end end end
