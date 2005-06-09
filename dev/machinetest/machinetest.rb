6bb61417207263350ab8b9d0e1e303c8b390325f
Spoon M <spoonm@gmail.com>
2005-06-09 05:55:18 +0000

    added raw ability
    git-svn-id: file:///home/svn/incoming/trunk@2601 4d416f70-5f16-0410-b530-b9f4589650da

--- dev/machinetest/machinetest.rb
+++ dev/machinetest/machinetest.rb
@@ -6,4 +6,7 @@ module MachineTest
 	def MachineTest.test(str, all = false)
 		MachineTest::Internal.test(str + "\xcc", all)
 	end
+	def MachineTest.testraw(str, all = false)
+		MachineTest::Internal.test(str, all)
+	end
 end
