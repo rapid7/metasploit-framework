1bdddc4c31c81cfbf69848e0c10f754b5f335cb2
Spoon M <spoonm@gmail.com>
2005-07-09 21:01:16 +0000

    wee
    git-svn-id: file:///home/svn/incoming/trunk@2686 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ dev/adminy/movey_requires.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+
+while(<STDIN>) {
+	my $line = $_;
+
+	if($line =~ /require '(.*?)'/) {
+		my $required = $1;
+
+		my @pieces = split('/', $required);
+		map { $_ = old_to_new($_) } @pieces;
+		my $new = join('/', @pieces);
+
+		print "$required -> $new\n";
+	}
+}
+
+sub old_to_new {
+	my $name = shift;
+
+	$name =~ s/^([A-Z])/lc($1)/ge;
+	$name =~ s/([A-Z])/"_" . lc($1)/ge;
+
+	return($name);
+}
