26bc1e96a1a916e09b03761c50853b242ee22820
Matt Miller <mmiller@hick.org>
2005-11-15 15:29:56 +0000

    docs
    git-svn-id: file:///home/svn/incoming/trunk@3031 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/msf/base/simple/buffer.rb
+++ lib/msf/base/simple/buffer.rb
@@ -13,7 +13,8 @@ module Simple
 module Buffer
 
 	#
-	# Serializes a buffer to a provided format.
+	# Serializes a buffer to a provided format.  The formats supported are raw,
+	# ruby, perl, and c.
 	#
 	def self.transform(buf, fmt = "ruby")
 		case fmt
@@ -32,7 +33,8 @@ module Buffer
 	end
 
 	#
-	# Creates a comment using the supplied format.
+	# Creates a comment using the supplied format.  The formats supported are
+	# raw, ruby, perl, and c.
 	#
 	def self.comment(buf, fmt = "ruby")
 		case fmt
