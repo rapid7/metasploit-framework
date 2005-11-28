dcae2ac5a7a70b629fcba6e4b3c013998e4e961b
Matt Miller <mmiller@hick.org>
2005-11-28 14:26:33 +0000

    a handful of framework samples
    git-svn-id: file:///home/svn/incoming/trunk@3145 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ documentation/samples/framework/encode_file.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+#
+# This sample demonstrates how a file can be encoded using a framework
+# encoder.
+#
+
+$:.unshift(File.join(File.dirname(__FILE__), '..', '..', '..', 'lib'))
+
+require 'msf/base'
+
+if (ARGV.empty?)
+	puts "Usage: #{File.basename(__FILE__)} encoder_name file_name format"
+	exit
+end
+
+framework = Msf::Simple::Framework.create
+
+begin
+	# Create the encoder instance.
+	mod = framework.encoders.create(ARGV.shift)
+
+	puts(Msf::Simple::Buffer.transform(
+		mod.encode(IO.readlines(ARGV.shift).join), ARGV.shift || 'ruby'))
+rescue
+	puts "Error: #{$!}\n\n#{$@.join("\n")}"
+end
