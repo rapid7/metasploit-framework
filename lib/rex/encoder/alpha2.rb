daf5eebd288a7217550b840f12c5dbb8496addf3
Matt Miller <mmiller@hick.org>
2006-01-06 15:42:03 +0000

    added skylined copyright notice
    git-svn-id: file:///home/svn/incoming/trunk@3321 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/encoder/alpha2.rb
+++ lib/rex/encoder/alpha2.rb
@@ -1,6 +1,17 @@
 #!/usr/bin/env ruby
 
 #
+# ________________________________________________________________________________
+# 
+#     ,sSSs,,s,  ,sSSSs,  ALPHA 2: Zero-tolerance. (build 07)
+#    SS"  Y$P"  SY"  ,SY
+#   iS'   dY       ,sS"   Unicode-proof uppercase alphanumeric shellcode encoding.
+#   YS,  dSb    ,sY"      Copyright (C) 2003, 2004 by Berend-Jan Wever.
+#   `"YSS'"S' 'SSSSSSSP   <skylined@edup.tudelft.nl>
+# ________________________________________________________________________________
+#
+
+#
 # make sure the namespace is created
 #
 
