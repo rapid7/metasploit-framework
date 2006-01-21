2346b83ef413700a700bedafa30092f019814458
vlad902 <>
2006-01-21 02:44:01 +0000

    Slightly re-structured and bug fixed
    git-svn-id: file:///home/svn/incoming/trunk@3420 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/proto/sunrpc/client.rb
+++ lib/rex/proto/sunrpc/client.rb
@@ -17,12 +17,24 @@ class Client
 	CALL = 0
 	
 	MSG_ACCEPTED = 0
-	SUCCESS = 0
+
+	SUCCESS = 0		# RPC executed successfully
+	PROG_UMAVAIL = 1	# Remote hasn't exported program
+	PROG_MISMATCH = 2	# Remote can't support version #
+	PROC_UNAVAIL = 3	# Program can't support procedure
+	GARBAGE_ARGS = 4	# Procedure can't decode params
 	
-	attr_reader :rhost, :proto_call, :program, :version
+	attr_reader :rhost, :rport, :proto, :program, :version
 	attr_accessor :pport
 	
-	def initialize()
+	def initialize(rhost, rport, proto, program, version)
+		if proto.downcase !~ /^(tcp|udp)$/
+			raise ArgumentError, 'Protocol is not "tcp" or "udp"'
+		end
+
+		@rhost, @rport, @program, @version, @proto = \
+			rhost, rport, program, version, proto.downcase
+
 		@pport = nil
 		
 		@auth_type = AUTH_NULL
@@ -31,17 +43,12 @@ class Client
 		@call_sock = nil
 	end
 	
-	def create(rhost, rpcport, program, version, proto_create, proto_call)
-		if proto_create !~ /^(tcp|udp)$/ || proto_call !~ /^(tcp|udp)$/
-			raise ArgumentError, 'Protocol is not "tcp" or "udp"'
-		end
-
-		@rhost, @program, @version, @proto_call = rhost, program, version, proto_call.downcase
-
+# XXX: Add optional parameter to have proto be something else
+	def create()
 		proto_num = 0
-		if @proto_create.eql?('tcp')
+		if @proto.eql?('tcp')
 			proto_num = 6
-		elsif @proto_create.eql?('udp')
+		elsif @proto.eql?('udp')
 			proto_num = 17
 		end
 
@@ -50,15 +57,16 @@ class Client
 				@auth_type, [@auth_data, 400], AUTH_NULL, '',
 				@program, @version, proto_num, 0)
 		
-		sock = SunRPC.make_rpc(@proto_create, @rhost, rpcport)
-		SunRPC.send_rpc(sock, buf)
-		ret = SunRPC.recv_rpc(sock)
-		SunRPC.close_rpc(sock)
+		sock = Client.make_rpc(@proto, @rhost, @rport)
+		Client.send_rpc(sock, buf)
+		ret = Client.recv_rpc(sock)
+		Client.close_rpc(sock)
 
 		arr = XDR.decode!(ret, Integer, Integer, Integer, String, Integer,
 			Integer)
 		if arr[1] != MSG_ACCEPTED || arr[4] != SUCCESS || arr[5] == 0
-			raise 'SunRPC.create() failed'
+# Check PRO[CG]_*/GARBAGE_ARGS
+			raise 'create failed'
 		end
 
 		@pport = arr[5]
@@ -71,31 +79,33 @@ class Client
 			buffer
 		
 		if !@call_sock
-			@call_sock = SunRPC.make_rpc(@proto_call, @rhost, @pport)
+			@call_sock = Client.make_rpc(@proto, @rhost, @pport)
 		end
-		SunRPC.send_rpc(@call_sock, buf)
-		ret = SunRPC.recv_rpc(@call_sock)
+		Client.send_rpc(@call_sock, buf)
+		ret = Client.recv_rpc(@call_sock)
 		
 		arr = XDR.decode!(ret, Integer, Integer, Integer, String, Integer)
 		if arr[1] != MSG_ACCEPTED || arr[4] != SUCCESS
-			raise 'SunRPC.call() failed'
+			raise 'call failed'
 		end
 		
 		return ret
 	end
 	
-	def destroy()
-		SunRPC.close_rpc(@call_sock) if @call_sock
+	def destroy
+		Client.close_rpc(@call_sock) if @call_sock
+		@call_sock = nil
 	end
 	
 	
-	def authnull_create()
+	def authnull_create
 		@auth_type = AUTH_NULL
 		@auth_data = ''
 	end
 	
 	def authunix_create(host, uid, gid, groupz)
 		raise ArgumentError, 'Hostname length is too long' if host.length > 255
+# 10?
 		raise ArgumentError, 'Too many groups' if groupz.length > 10
 		
 		@auth_type = AUTH_UNIX
@@ -105,25 +115,25 @@ class Client
 	
 	
 # XXX: Dirty, integrate some sort of request system into create/call?
-	def SunRPC.portmap_req(host, port, rpc_vers, procedure, buffer)
+	def Client.portmap_req(host, port, rpc_vers, procedure, buffer)
 		buf = XDR.encode(CALL, 2, PMAP_PROG, rpc_vers, procedure,
 			AUTH_NULL, '', AUTH_NULL, '') + buffer
 		
-		sock = SunRPC.make_rpc('tcp', host, port)
-		SunRPC.send_rpc(sock, buf)
-		ret = SunRPC.recv_rpc(sock)
-		SunRPC.close_rpc(sock)
+		sock = Client.make_rpc('tcp', host, port)
+		Client.send_rpc(sock, buf)
+		ret = Client.recv_rpc(sock)
+		Client.close_rpc(sock)
 		
 		arr = XDR.decode!(ret, Integer, Integer, Integer, String, Integer)
 		if arr[1] != MSG_ACCEPTED || arr[4] != SUCCESS || arr[5] == 0
-			raise 'SunRPC: portmap_req failed'
+			raise 'portmap_req failed'
 		end
 		
 		return ret
 	end
 	
 # Msf::Config.data_directory
-#	def SunRPC.program2name(number)
+#	def Client.program2name(number)
 #		File.foreach('data/rpc_names') { |line|
 #			next if line.empty? || line =~ /^\s*#/
 #			
@@ -136,35 +146,35 @@ class Client
 #	end
 	
 	private
-	def SunRPC.make_rpc(proto, host, port)
-		Rex::Socket.create_tcp(
+	def Client.make_rpc(proto, host, port)
+		Rex::Socket.create(
 			'PeerHost'	=> host,
 			'PeerPort'	=> port,
 			'Proto'		=> proto)
 	end
 	
-	def SunRPC.send_rpc(sock, buf)
+	def Client.send_rpc(sock, buf)
 		buf = gen_xid() + buf
-		if sock.tcp?
+		if sock.type?.eql?('tcp')
 			buf = XDR.encode(0x80000000 | buf.length) + buf
 		end
 		sock.write(buf)
 	end
 	
-	def SunRPC.recv_rpc(sock)
+	def Client.recv_rpc(sock)
 		buf = sock.get_once(-1, 5)
 		buf.slice!(0..3)
-		if sock.tcp?
+		if sock.type?.eql?('tcp')
 			buf.slice!(0..3)
 		end
 		return buf
 	end
 	
-	def SunRPC.close_rpc(sock)
+	def Client.close_rpc(sock)
 		sock.close
 	end
 	
-	def SunRPC.gen_xid()
+	def Client.gen_xid
 		return XDR.encode(rand(0xffffffff) + 1)
 	end
 end
