57d50528f25741ef3c7d41bfefd0235c562d5064
bmc <>
2006-03-07 16:41:48 +0000

    * add
    git-svn-id: file:///home/svn/incoming/trunk@3556 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ lib/rex/test.rb
@@ -0,0 +1,34 @@
+require 'test/unit'
+require 'flexmock'
+
+# DEFAULTS
+module Rex
+class Test
+
+$_REX_TEST_NO_MOCK = nil
+$_REX_TEST_TIMEOUT = 30
+$_REX_TEST_SMB_HOST = '10.4.10.58'
+
+# overwrite test defaults with rex/test-config.rb
+def self.load()
+    file = File.join( ENV.fetch('HOME'), '.msf3', 'test')
+    begin
+        if File.stat(file + '.rb')
+            require file
+        end
+    rescue
+        # just ignore the errors
+    end
+
+end
+
+def self.cantmock()
+    if (!$_REX_TEST_NO_MOCK)
+        raise RuntimeError, "*** $_REX_TEST_NO_MOCK must not be set for this test ***", caller
+    end
+end
+
+Rex::Test.load()
+
+end
+end
