05bda2529a986a70f96c3510b2add9af158ededc
HD Moore <hd_moore@rapid7.com>
2006-03-09 17:32:53 +0000

    More aux, first hack on multi-target firefox exploit
    git-svn-id: file:///home/svn/incoming/trunk@3565 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ lib/msf/core/auxiliary/tcp.rb
@@ -0,0 +1,15 @@
+#module Msf
+#
+###
+#
+# This module provides methods for establish a connection to a remote host and
+# communicating with it.
+#
+###
+#module Auxiliary::Remote::Tcp
+#
+#	include Exploit::Remote::Tcp
+#
+#end
+#end
+
