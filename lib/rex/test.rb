ac2d76eb7125d4ce8dc07cbb37f0f0e7a33ef77f
bmc <>
2006-06-01 15:29:39 +0000

    add default smb username & password for testing in the test config
    git-svn-id: file:///home/svn/incoming/trunk@3651 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/test.rb
+++ lib/rex/test.rb
@@ -8,6 +8,8 @@ class Test
 $_REX_TEST_NO_MOCK = nil
 $_REX_TEST_TIMEOUT = 30
 $_REX_TEST_SMB_HOST = '10.4.10.58'
+$_REX_TEXT_SMB_USER = 'SMBTest'
+$_REX_TEXT_SMB_PASS = 'SMBTest'
 
 # overwrite test defaults with rex/test-config.rb
 def self.load()
