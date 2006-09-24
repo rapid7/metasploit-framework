874a8214dca39f944a5daff6f97e80686208e4bc
lmh <>
2006-09-24 04:09:06 +0000

    My last commit until I get some sleep. Will polish everything later today, and finish the interface (probably revert color scheme changes). Check my changes to the Payloads tab link for an example of AJAX request + update + visual effect on completion. Have fun.
    git-svn-id: file:///home/svn/framework3/trunk@3944 4d416f70-5f16-0410-b530-b9f4589650da

--- data/msfweb/app/controllers/payloads_controller.rb
+++ data/msfweb/app/controllers/payloads_controller.rb
@@ -1,5 +1,12 @@
 class PayloadsController < ApplicationController
-  layout 'msfweb'
+  layout 'msfweb', :except => [:list, :view, :generate]
+  
+  # shouldn have one but until we place a main controller, this suffices
+  # why? we need a controller that holds the layout, and all others are simply
+  # rendered and retrieved via AJAX requests updating the proper elements.
+  # problem: old browsers won't see a flippin' thing
+  def index
+  end
   
   def list
   end
