2e6ee3d41b5fc3e8118aba34766488889c2dfa88
lmh <>
2006-09-26 05:04:32 +0000

    Added missing windows. Fixed font for body elements.
    git-svn-id: file:///home/svn/framework3/trunk@3965 4d416f70-5f16-0410-b530-b9f4589650da

--- data/msfweb/public/javascripts/application.js
+++ data/msfweb/public/javascripts/application.js
@@ -8,39 +8,63 @@
  */
 
 function openAboutDialog() {
-
+    // ...
 }
 
 function openExploitWindow() {
-    var exploitList = create_window_ajax("/exploits/list", "exploits-list", "Available Exploits");
+    var exploitList = create_window_ajax("/exploits/list", "exploits-list", "Available Exploits", '700', '300');
     exploitList.setDestroyOnClose();
     exploitList.showCenter();
 }
 
 function openPayloadsWindow() {
-    var payloadList = create_window_ajax("/payloads/list", "payloads-list", "Available Payloads");
+    var payloadList = create_window_ajax("/payloads/list", "payloads-list", "Available Payloads", '500', '300');
     payloadList.setDestroyOnClose();
     payloadList.showCenter();
+}
+
+function openEncodersWindow() {
+    var encoderList = create_window_ajax("/encoders/list", "encoders-list", "Available Encoders", '300', '200');
+    encoderList.setDestroyOnClose();
+    encoderList.showCenter();
+}
+
+function openNopsWindow() {
+    var nopsList = create_window_ajax("/nops/list", "nops-list", "Available Nops", '300', '200');
+    nopsList.setDestroyOnClose();
+    nopsList.showCenter();
+}
+
+function openJobsWindow() {
+    var nopsList = create_window_ajax("/jobs/list", "jobs-list", "Current Jobs", '300', '200');
+    jobsList.setDestroyOnClose();
+    jobsList.showCenter();
+}
+
+function openSessionsWindow() {
+    var sessionsList = create_window_ajax("/sessions/list", "sessions-list", "Active Sessions", '300', '200');
+    sessionsList.setDestroyOnClose();
+    sessionsList.showCenter();
 }
 
 /*
  * Task and helper functions
  */
 
-function create_window_ajax(target_url, wid, wtitle) {
+function create_window_ajax(target_url, wid, wtitle, wwidth, wheight) {
     var new_mwindow = new Window(wid,
         { className: "metasploit",
           title: wtitle,
           top:70,
           left:100,
-          width:300,
-          height:200,
+          width:wwidth,
+          height:wheight,
           resizable: true,
           draggable: true,
           url: target_url,
           showEffectOptions:
             {
-                duration:3
+                duration:0.5
             }
           });
     return new_mwindow;
