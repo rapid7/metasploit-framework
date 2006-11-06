39a3bd4a63b57830d7120ad16226e80778901fef
HD Moore <hd_moore@rapid7.com>
2006-11-06 03:14:43 +0000

    Fixes the annoying Symbol to Fixnum conversion warnings
    git-svn-id: file:///home/svn/framework3/trunk@4108 4d416f70-5f16-0410-b530-b9f4589650da

--- external/ruby-pcapx/Pcap.c
+++ external/ruby-pcapx/Pcap.c
@@ -782,9 +782,9 @@ Init_pcap(void)
     /* define class PcapStat */
     cPcapStat = rb_funcall(rb_cStruct, rb_intern("new"), 4,
 			   Qnil,
-			   INT2NUM(rb_intern("recv")),
-			   INT2NUM(rb_intern("drop")),
-			   INT2NUM(rb_intern("ifdrop")));
+			   ID2SYM(rb_intern("recv")),
+			   ID2SYM(rb_intern("drop")),
+			   ID2SYM(rb_intern("ifdrop")));
     rb_define_const(mPcap, "Stat", cPcapStat);
 
     /* define exception classes */
