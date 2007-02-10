c60276c6575922233e36bbab3146bb10ca677610
HD Moore <hd_moore@rapid7.com>
2007-02-10 02:05:58 +0000

    Inherit from the real pipe class, including the new input_fd() method
    git-svn-id: file:///home/svn/framework3/trunk@4335 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/msf/ui/gtk2/bidirectional_pipe.rb
+++ lib/msf/ui/gtk2/bidirectional_pipe.rb
@@ -6,50 +6,11 @@ require 'msf/ui/gtk2/stream/output'
 require 'rex/ui/text/output/buffer'
 require 'rex/ui/text/input/buffer'
 
-class BidirectionalPipe < Rex::Ui::Text::Input
+class BidirectionalPipe < Rex:IO::BidirectionalPipe
 
 	def initialize(buffer)
 		@buffer = buffer
-		@subscribers_out = {}
-		@pipe_input = Rex::Ui::Text::Input::Buffer.new
-	end
-
-	def pipe_input
-		@pipe_input
-	end
-
-	def close
-		@pipe_input.close
-	end
-
-	def create_subscriber(id)
-		@subscribers_out[id] = Rex::Ui::Text::Output::Buffer.new
-	end
-
-	def remove_subscriber(id)
-		@subscribers_out.delete(id)
-	end
-
-	def write_input(buf)
-		@pipe_input.put(buf)
-	end
-
-	def read_subscriber(id)
-		output = @subscribers_out[id]
-
-		return '' if output.nil?
-
-		buf = output.buf
-
-		output.reset
-
-		buf
-	end
-
-	def print(msg)
-		@subscribers_out.each_pair { |id, buf|
-			buf.print(msg)
-		}
+		super()
 	end
 	
 	def print_error(msg)
@@ -64,9 +25,6 @@ class BidirectionalPipe < Rex::Ui::Text::Input
 	def print_good(msg)
 		@buffer.insert_at_cursor('[+] ' + msg)
 		print_line
-	end
-
-	def flush
 	end
 	
 	def print_status(msg)
