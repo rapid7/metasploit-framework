4983d73d710c9a8d23cde5ed7247314df63e940f
HD Moore <hd_moore@rapid7.com>
2007-02-19 04:55:52 +0000

    Changed rails version from 1.1.6 to 1.2.2
    git-svn-id: file:///home/svn/framework3/trunk@4444 4d416f70-5f16-0410-b530-b9f4589650da

--- data/msfweb/config/environment.rb
+++ data/msfweb/config/environment.rb
@@ -6,7 +6,7 @@
 ENV['RAILS_ENV'] = 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '1.1.6'
+RAILS_GEM_VERSION = '1.2.2'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
