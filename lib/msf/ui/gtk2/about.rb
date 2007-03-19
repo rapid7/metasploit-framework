79de2b7edbad65198c14c3b57c02036279b4c110
fab <>
2007-03-19 23:13:50 +0000

    arrf, correct the changeset #4538
    git-svn-id: file:///home/svn/framework3/trunk@4539 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/msf/ui/gtk2/about.rb
+++ lib/msf/ui/gtk2/about.rb
@@ -4,12 +4,18 @@ module Gtk2
 
 class MyAbout
     include Msf::Ui::Gtk2::MyControls
+    Gtk::AboutDialog.set_email_hook {|about, link|
+    	puts "Mail to #{link}"
+    }
+    Gtk::AboutDialog.set_url_hook {|about, link|
+	puts "Launch a browser to url #{link}"
+    }
     
     def initialize
 	@about = Gtk::AboutDialog.new
 	@about.set_name('MsfGUI')
 	@about.set_website('http://www.metasploit.org')
-	@about.set_authors(['Fabrice MOURRON fab<at>metasploit.com', 'Metasploit LLC'])
+	@about.set_authors(['Fabrice MOURRON <fab@metasploit.com>', 'Metasploit LLC'])
 	@about.set_license(File.read(File.join(Msf::Config.install_root, 'documentation', 'LICENSE')))
 	#@about.set_wrap_license('True')
 	@about.set_logo(driver.get_icon('msfwx.xpm'))
