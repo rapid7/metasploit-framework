1dd52c557d09fbd279c5bd3be16303f3ff79b791
fab <>
2007-03-21 22:32:36 +0000

    added treview for options dialog
    git-svn-id: file:///home/svn/framework3/trunk@4544 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/msf/ui/gtk2/options.rb
+++ lib/msf/ui/gtk2/options.rb
@@ -8,7 +8,7 @@ module Gtk2
 # notebook = Gtk::Notebook
 ##
 class SkeletonOption < Gtk::Dialog
-	def initialize(title, notebook)
+	def initialize(title, items)
 		super("", $gtk2driver.main, Gtk::Dialog::DESTROY_WITH_PARENT,
 			[ Gtk::Stock::OK, Gtk::Dialog::RESPONSE_NONE ],
 			[ Gtk::Stock::CLOSE, Gtk::Dialog::RESPONSE_NONE ])
@@ -19,18 +19,69 @@ class SkeletonOption < Gtk::Dialog
 		self.vbox.spacing = 12
 		self.title = title
 		
-		self.vbox.pack_start(notebook)
+		model = create_model(items)
+		treeview = Gtk::TreeView.new(model)
+		
+		hbox = Gtk::HBox.new(false, 10)
+		
+		# ScrolledWindow
+		sw = Gtk::ScrolledWindow.new
+		sw.set_policy(Gtk::POLICY_NEVER, Gtk::POLICY_AUTOMATIC)
+		hbox.pack_start(sw)
+		sw.add(treeview)
+		
+		renderer = Gtk::CellRendererText.new
+		column = Gtk::TreeViewColumn.new('Select an item', renderer, 'text' => 0)
+		column.pack_start(renderer, false)
+		treeview.append_column(column)
+		
+		self.vbox.pack_start(hbox)
+		
+		# Signal
+		selection = treeview.selection
+		selection.mode = Gtk::SELECTION_SINGLE
+		selection.signal_connect('changed') do |s|
+			selection_changed(s)
+		end
+	end
+	
+	def create_model(items)
+		store = Gtk::ListStore.new(String)
+		
+		items.each do |item|
+			iter = store.append
+			iter[0] = item
+		end
+		
+		return store
+	end
+	
+	def selection_changed(selection)
+		iter = selection.selected
+		puts iter[0]
 	end
 end
 
 class MsfOptions
 	class Preferences < Msf::Ui::Gtk2::SkeletonOption
-		def initialize()
-			@notebook = Gtk::Notebook.new
-			super("Preferences", @notebook)
+		def initialize
+			menu = ["Exploits", "Payloads"]
+			super("Preferences", menu)
 			
 			show_all and run
 			destroy
+		end
+		
+		#
+		# Describe the exploits widget
+		#
+		def w_exploits
+		end
+		
+		#
+		# Describe the payloads widget
+		#
+		def w_payloads
 		end
 	end
 end
