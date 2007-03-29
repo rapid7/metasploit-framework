9213b02356749f46e00a0ce9d4646601c4c9aa87
HD Moore <hd_moore@rapid7.com>
2007-03-29 13:52:31 +0000

    Patch froma A Sussex for allow extconf to build lorcon on debian
    git-svn-id: file:///home/svn/framework3/trunk@4586 4d416f70-5f16-0410-b530-b9f4589650da

--- external/ruby-lorcon/extconf.rb
+++ external/ruby-lorcon/extconf.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require 'mkmf'
 
-if have_library("orcon", "tx80211_txpacket")
+if have_library("orcon", "tx80211_txpacket", "tx80211.h")
 	create_makefile("Lorcon")
 else
 	puts "Error: the lorcon library was not found, please see the README"
