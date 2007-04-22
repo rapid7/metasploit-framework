54b6c3ef1afb94c512cde7d668931fc3623fabd4
fab <>
2007-04-22 13:39:32 +0000

    msf style for opcode status
    git-svn-id: file:///home/svn/framework3/trunk@4734 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/msf/ui/gtk2/opcode.rb
+++ lib/msf/ui/gtk2/opcode.rb
@@ -1,9 +1,7 @@
 module Msf
 module Ui
 module Gtk2
-	
-require 'rex'
-require 'rex/ui'
+
 require 'rex/exploitation/opcodedb'
 
 ##
@@ -15,12 +13,18 @@ require 'rex/exploitation/opcodedb'
 #
 class SkeletonOpcode < Gtk::Dialog
 	
-	attr_accessor :sw
+	include Msf::Ui::Gtk2::MyControls
 	
-	def initialize(title, items)
+	attr_accessor :comment, :stuff
+	
+	def initialize(title, comments)
 		super("", $gtk2driver.main, Gtk::Dialog::DESTROY_WITH_PARENT,
-			[ Gtk::Stock::OK, Gtk::Dialog::RESPONSE_NONE ],
+			#[ Gtk::Stock::OK, Gtk::Dialog::RESPONSE_NONE ],
 			[ Gtk::Stock::CLOSE, Gtk::Dialog::RESPONSE_NONE ])
+			
+		# Style
+		console_style = File.join(driver.resource_directory, 'style', 'opcode.rc')
+		Gtk::RC.parse(console_style)
 		
 		self.border_width = 6
 		self.resizable = true
@@ -30,30 +34,42 @@ class SkeletonOpcode < Gtk::Dialog
 		self.title = title
 		self.set_default_size(500, 400)
 		
-		@hbox = Gtk::HBox.new(false, 10)
-		self.vbox.pack_start(@hbox, true, true, 0)
+		@comment = Gtk::Label.new
+		@comment.set_alignment(0, 0)
+		@comment.set_markup("<b>#{comments}</b>")
+		self.vbox.pack_start(@comment, false, false, 0)
 		
-		# ScrolledWindow
-		@sw = Gtk::ScrolledWindow.new
-		@sw.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC)
-		@hbox.pack_start(@sw)
+		@stuff = Gtk::VBox.new(false, 10)
+		self.vbox.pack_start(@stuff, true, true, 0)
 	end
 end
 
+#
+# Gtk2 Interface for Metasploit Opcodes database
+#
 class MsfOpcode
+	
 	# Create the opcode client instance
 	$client = Rex::Exploitation::OpcodeDb::Client.new
 	
+	#
+	# Statistics Opcodes
+	#
 	class Stats < Msf::Ui::Gtk2::SkeletonOpcode
+		
 		def initialize
-			super("Statistics", nil)
+			comment = "Current database statistics: "
+			
+			# Call the parent
+			super("Statistics", comment)
+			
 			self.set_default_size(500, 230)
-
+			
 			stats = $client.statistics
 			
 			textview = Gtk::TextView.new
 			textbuffer = Gtk::TextBuffer.new
-			sw.add(textview)
+			stuff.pack_start(textview, true, true, 0)
 			
 			textbuffer.set_text(
 						"\n" +
@@ -76,6 +92,60 @@ class MsfOpcode
 		end
 	end
 	
+	#
+	# Locales Opcodes
+	#
+	class Locales < Msf::Ui::Gtk2::SkeletonOpcode
+		def initialize
+			super("Locales", nil)
+			self.set_default_size(500, 230)
+			
+			textview = Gtk::TextView.new
+			textbuffer = Gtk::TextBuffer.new
+			#sw.add(textview)
+			
+			locales = ""
+			$client.locales.each do |locale| 
+				locales << locale.name + "\n"
+			end
+			
+			textbuffer.set_text( locales )
+						
+			textview.set_buffer(textbuffer)
+			textview.set_editable(false)
+			textview.set_cursor_visible(false)
+			
+			show_all and run
+			destroy
+		end
+	end
+
+	#
+	# Locales Opcodes
+	#
+	class Locales2 < Msf::Ui::Gtk2::SkeletonOpcode
+		def initialize
+			super("Locales", nil)
+			self.set_default_size(500, 230)
+			
+			locales = ""
+			$client.locales.each do |locale| 
+				locales << locale.name + "\n"
+			end
+			
+			description = "Supported locales opcodes"
+			label = Gtk::Label.new(locales)
+			puts label.set_alignment(0, 0)
+			#sw.add(label)
+			
+			show_all and run
+			destroy
+		end
+	end
+
+	#
+	# Modules Opcodes
+	#
 	class Modules < Msf::Ui::Gtk2::SkeletonOpcode
 		def initialize
 			super("Modules", nil)
