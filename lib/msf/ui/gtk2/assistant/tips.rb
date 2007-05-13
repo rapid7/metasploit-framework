271f5e5fced4c4bb375402ad9da5cb6aaface156
fab <>
2007-05-13 09:10:26 +0000

    organisation
    git-svn-id: file:///home/svn/framework3/trunk@4907 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ lib/msf/ui/gtk2/assistant/tips.rb
@@ -0,0 +1,27 @@
+module Msf
+  module Ui
+    module Gtk2
+
+      #
+      # Subclass the TreeViewTooltips to add our get_tooltip function
+      #
+      class AssistantTips < Msf::Ui::Gtk2::TreeViewTooltips
+
+        def initialize(column)
+          super()
+          @column = column
+        end
+
+        def get_tooltip(view, column, path)
+          if (column == @column)
+            model = view.model
+            iter = model.get_iter(path)
+            return iter.get_value(3)
+          end
+        end
+      end
+      
+      
+    end
+  end
+end
