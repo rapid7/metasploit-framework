83a49959d89c91189c2b68ebe7eb5b8a35f00b96
fab <>
2007-05-19 20:23:24 +0000

    grapical meterpreter, browse inside selected folders ;-)
    git-svn-id: file:///home/svn/framework3/trunk@4939 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/msf/ui/gtk2/meterpreter/stdapi/fs.rb
+++ lib/msf/ui/gtk2/meterpreter/stdapi/fs.rb
@@ -17,13 +17,13 @@ module Msf
           def initialize(client)
             @client = client
             super("MsfBrowser on #{@client.tunnel_peer}")
-            @model = Gtk::ListStore.new(String, String, TrueClass, Gdk::Pixbuf)
+            @model_view = Gtk::ListStore.new(String, String, TrueClass, Gdk::Pixbuf)
             @parent = "/"
 
             @file_pixbuf = Gdk::Pixbuf.new(driver.get_image("msf_file.png"))
             @folder_pixbuf = Gdk::Pixbuf.new(driver.get_image("msf_folder.png"))
 
-            @model.set_default_sort_func do |a, b|
+            @model_view.set_default_sort_func do |a, b|
               if !a[COL_IS_DIR] and b[COL_IS_DIR]
                 1
               elsif a[COL_IS_DIR] and !b[COL_IS_DIR]
@@ -32,13 +32,13 @@ module Msf
                 a[COL_DISPLAY_NAME] <=> b[COL_DISPLAY_NAME]
               end
             end
-            @model.set_sort_column_id(Gtk::TreeSortable::DEFAULT_SORT_COLUMN_ID, Gtk::SORT_ASCENDING)
+            @model_view.set_sort_column_id(Gtk::TreeSortable::DEFAULT_SORT_COLUMN_ID, Gtk::SORT_ASCENDING)
 
             # Populate the ListStore
             cmd_ls
 
             # Define the size and border
-            set_default_size(650, 400)
+            set_default_size(800, 600)
             set_border_width(10)
 
             vbox = Gtk::VBox.new(false, 0)
@@ -50,15 +50,14 @@ module Msf
             sw.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC)
             vbox.pack_start(sw, true, true, 0)
 
-            iconview = Gtk::IconView.new(@model)
+            iconview = Gtk::IconView.new(@model_view)
             iconview.selection_mode = Gtk::SELECTION_MULTIPLE
             iconview.text_column = COL_DISPLAY_NAME
             iconview.pixbuf_column = COL_PIXBUF
             iconview.signal_connect("item_activated") do |iview, path|
-              iter = @model.get_iter(path)
+              iter = @model_view.get_iter(path)
               if iter[COL_DISPLAY_NAME]
-                @parent = iter[COL_PATH]
-                cmd_ls
+                cmd_ls(@parent + "\\" + iter[COL_DISPLAY_NAME])
               end
             end
             
@@ -67,9 +66,12 @@ module Msf
 
             show_all
           end
-
+          
+          #
+          # Lists file on the remote machine
+          #
           def cmd_ls(*args)
-            @model.clear
+            @model_view.clear
             path = args[0] || @client.fs.dir.getwd
 
             # Enumerate each item...
@@ -79,13 +81,12 @@ module Msf
               elsif p['StatBuf'].ftype[0,3] == "fil"
                 is_dir = false
               end
-              iter = @model.append
+              iter = @model_view.append
               iter[COL_DISPLAY_NAME] = p['FileName'] || 'unknown'
               iter[COL_PATH] = path
               iter[COL_IS_DIR] = is_dir
               iter[COL_PIXBUF] = is_dir ? @folder_pixbuf : @file_pixbuf
             end
-
           end # cmd_ls
 
         end # Fs
