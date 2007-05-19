688ea5b2b999d26d0029c7fa55d56298e781dc24
fab <>
2007-05-19 23:35:51 +0000

    grapical meterpreter (file browser)
    git-svn-id: file:///home/svn/framework3/trunk@4945 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/msf/ui/gtk2/meterpreter/stdapi/fs.rb
+++ lib/msf/ui/gtk2/meterpreter/stdapi/fs.rb
@@ -15,11 +15,21 @@ module Msf
           include Msf::Ui::Gtk2::MyControls
 
           def initialize(client)
+
+            # The session
             @client = client
+
+            # call the parent
             super("MsfBrowser on #{@client.tunnel_peer}")
+
+            # Define the models (navigation, view)
+            # TODO: model for navigation
             @model_view = Gtk::ListStore.new(String, String, TrueClass, Gdk::Pixbuf)
+
+            # Define the parent the parent
             @parent = "/"
 
+            # Define the icons for folders and files
             @file_pixbuf = Gdk::Pixbuf.new(driver.get_image("msf_file.png"))
             @folder_pixbuf = Gdk::Pixbuf.new(driver.get_image("msf_folder.png"))
 
@@ -34,21 +44,37 @@ module Msf
             end
             @model_view.set_sort_column_id(Gtk::TreeSortable::DEFAULT_SORT_COLUMN_ID, Gtk::SORT_ASCENDING)
 
-            # Populate the ListStore
-            cmd_ls
-
             # Define the size and border
             set_default_size(800, 600)
             set_border_width(10)
 
-            vbox = Gtk::VBox.new(false, 0)
-            add(vbox)
+            # Main hbox
+            hbox = Gtk::HBox.new(false, 0)
+            add(hbox)
+
+            # Left and right vbox
+            vbox_left = Gtk::VBox.new(false, 0)
+            vbox_right = Gtk::VBox.new(false, 0)
+            hbox.pack_start(vbox_left, false, false, 0)
+            hbox.pack_start(vbox_right, true, true, 0)
+
+            # Label, Entry and Signal for the path selection
+            hbox_path = Gtk::HBox.new(false, 0)
+            vbox_right.pack_start(hbox_path, false, true, 0)
+            label_path = Gtk::Label.new("Remote Path :")
+            hbox_path.pack_start(label_path, false, false, 0)
+            @entry_path = Gtk::Entry.new
+            @entry_path.set_text(@client.fs.dir.getwd)
+            hbox_path.pack_start(@entry_path, true, true, 0)
+            @entry_path.signal_connect('activate') do
+              cmd_ls(@entry_path.text)
+            end
 
             # Add the view in the scrolled window
             sw = Gtk::ScrolledWindow.new
             sw.shadow_type = Gtk::SHADOW_ETCHED_IN
             sw.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC)
-            vbox.pack_start(sw, true, true, 0)
+            vbox_right.pack_start(sw, true, true, 0)
 
             iconview = Gtk::IconView.new(@model_view)
             iconview.selection_mode = Gtk::SELECTION_MULTIPLE
@@ -60,38 +86,46 @@ module Msf
                 cmd_ls(@parent + "\\" + iter[COL_DISPLAY_NAME])
               end
             end
-            
+
+            # Populate the view
+            cmd_ls
+
             sw.add(iconview)
             iconview.grab_focus
 
             show_all
           end
-          
+
           #
           # Lists file on the remote machine
           #
           def cmd_ls(*args)
-            
-            # Just ignore the invalid UTF8
-            # Don't know why GLib.filename_to_utf8() don't work ;-(
-            ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
-            
-            @model_view.clear
-            path = args[0] || @client.fs.dir.getwd
-            @parent = path
+            begin
+              # Just ignore the invalid UTF8
+              # Don't know why GLib.filename_to_utf8() don't work ;-(
+              ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
 
-            # Enumerate each item...
-            @client.fs.dir.entries_with_info(path).sort { |a,b| a['FileName'] <=> b['FileName'] }.each do |p|
-              if p['StatBuf'].ftype[0,3] == "dir"
-                is_dir = true
-              elsif p['StatBuf'].ftype[0,3] == "fil"
-                is_dir = false
+              @model_view.clear
+              path = args[0] || @client.fs.dir.getwd
+              @parent = path
+              @entry_path.set_text(@parent)
+
+              # Enumerate each item...
+              @client.fs.dir.entries_with_info(path).sort { |a,b| a['FileName'] <=> b['FileName'] }.each do |p|
+                if p['StatBuf'].ftype[0,3] == "dir"
+                  is_dir = true
+                elsif p['StatBuf'].ftype[0,3] == "fil"
+                  is_dir = false
+                end
+                iter = @model_view.append
+                iter[COL_DISPLAY_NAME] = ic.iconv(p['FileName'] + ' ')[0..-2] || 'unknown'
+                iter[COL_PATH] = path
+                iter[COL_IS_DIR] = is_dir
+                iter[COL_PIXBUF] = is_dir ? @folder_pixbuf : @file_pixbuf
               end
-              iter = @model_view.append
-              iter[COL_DISPLAY_NAME] = ic.iconv(p['FileName'] + ' ')[0..-2] || 'unknown'
-              iter[COL_PATH] = path
-              iter[COL_IS_DIR] = is_dir
-              iter[COL_PIXBUF] = is_dir ? @folder_pixbuf : @file_pixbuf
+            rescue
+              MsfDialog::Warning.new(self, "No entries exist in #{path}")
+              cmd_ls
             end
           end # cmd_ls
 
