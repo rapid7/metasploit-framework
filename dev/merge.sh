04224a6138f06f17825fc0f9b63333dcef7ebeb5
Matt Miller <mmiller@hick.org>
2007-06-16 01:41:57 +0000

    todo for 3.1
    git-svn-id: file:///home/svn/framework3/trunk@4991 4d416f70-5f16-0410-b530-b9f4589650da

--- dev/merge.sh
+++ dev/merge.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export DEV=/msf3/
-export SBL=/projects/metasploit/framework3/tags/framework-3.0/
+export DEV=/home/mmiller/svn/framework3/trunk/
+export SBL=/home/mmiller/svn/framework3/tags/framework-3.0/
 export FIL=$1
 
 echo "[*] Gathering svn information..."
