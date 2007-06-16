1a57aa1af19cfadcd9b6547804eed35fddc522c4
HD Moore <hd_moore@rapid7.com>
2007-06-16 02:46:40 +0000

    Test!
    git-svn-id: file:///home/svn/framework3/trunk@4993 4d416f70-5f16-0410-b530-b9f4589650da

--- dev/merge.sh
+++ dev/merge.sh
@@ -6,12 +6,13 @@ export FIL=$1
 
 echo "[*] Gathering svn information..."
 TMP=`tempfile mergeXXXXXXX`
-svn log --limit 1 "${DEV}${FIL}" > $TMP
+svn log --limit 1  "${DEV}${FIL}" | egrep -v '^r[0-9]+|^--|^$' > $TMP
 
 echo "[*] Copying file ${FIL}..."
 cp ${DEV}${FIL} ${SBL}${FIL}
 
 echo "[*] Merging file ${FIL}..."
+svn add ${SBL}${FIL} >/dev/null 2>&1
 svn commit --non-interactive -F $TMP ${SBL}${FIL}
 
 rm -f $TMP
