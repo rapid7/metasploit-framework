73456e634248b3fd5d2cfe5be8bcf766c26006ff
HD Moore <hd_moore@rapid7.com>
2007-10-02 16:09:13 +0000

    Quick tool to dump module references
    git-svn-id: file:///home/svn/framework3/trunk@5133 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ tools/module_reference.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+#
+# This script lists each module by its licensing terms
+#
+
+msfbase = File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__
+$:.unshift(File.join(File.dirname(msfbase), '..', 'lib'))
+
+require 'rex'
+require 'msf/ui'
+require 'msf/base'
+
+Indent = '   ' 
+
+# Initialize the simplified framework instance.
+$framework = Msf::Simple::Framework.create
+
+
+tbl = Rex::Ui::Text::Table.new(
+	'Header'  => 'Module References',
+	'Indent'  => 0,
+	'Columns' => [ 'Module', 'Reference' ]
+)
+
+licenses = {}
+
+$framework.payloads.each_module { |name, mod|
+	x = mod.new
+	x.references.each do |r|
+		tbl << [ 'payload/' + name, r.ctx_id + '-' + r.ctx_val ]
+	end
+}
+
+$framework.exploits.each_module { |name, mod|
+	x = mod.new
+	x.references.each do |r|
+		tbl << [ 'exploit/' + name, r.ctx_id + '-' + r.ctx_val ]
+	end
+}
+
+$framework.nops.each_module { |name, mod|
+	x = mod.new
+	x.references.each do |r|
+		tbl << [ 'nop/' + name, r.ctx_id + '-' + r.ctx_val ]
+	end
+}
+$framework.encoders.each_module { |name, mod|
+	x = mod.new
+	x.references.each do |r|
+		tbl << [ 'encoder/' + name, r.ctx_id + '-' + r.ctx_val ]
+	end
+}
+$framework.auxiliary.each_module { |name, mod|
+	x = mod.new
+	x.references.each do |r|
+		tbl << [ 'auxiliary/' + name, r.ctx_id + '-' + r.ctx_val ]
+	end
+}
+
+puts tbl.to_s
