53f7b946acf6043f66c0ab34889c45b7254213db
HD Moore <hd_moore@rapid7.com>
2007-10-18 20:26:24 +0000

    Update to the context dumper from dustin
    git-svn-id: file:///home/svn/framework3/trunk@5156 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/pescan/analyze.rb
+++ lib/rex/pescan/analyze.rb
@@ -265,12 +265,26 @@ module Analyze
 				next if section.name == ".data"
 				next if section.name == ".reloc"
 				
-				data = section.read(0, section.size)
-				buff = [ 0x01, pe.rva_to_vma( section.base_rva ), data.length, data].pack("CNNA*")
+				for offset in 0...section.size
+					byte = section.read(offset, 1)[0]
+					if byte != 0
+						chunkbase = pe.rva_to_vma( section.base_rva) + offset
+						data = ''
+						while byte != 0
+							data << byte
+							offset += 1
+							byte = 0
+							byte = section.read(offset, 1)[0] if offset < section.size
+						end
+						buff = nil
+						buff = [ 0x01, chunkbase, data.length, data].pack("CNNA*") if data.length > 0
 				
-				fd.write(buff)
-				
+						fd.write(buff) if buff
+					end
+				end
+
 			end
+				
 			
 			fd.close
 		end
