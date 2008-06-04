14d8bc67fc9d970d781c6b9ee748163e81d01311
HD Moore <hd_moore@rapid7.com>
2008-06-04 22:56:25 +0000

    This patch solves a slowness problem introduced by the ruby thread scheduler and the readline extension. For every time slice that the readline thread receives, it does a 100ms select(stdin), which severely impacts the performance of all other running threads. This patch pushes readline into a forked subprocess. Not the most elegant, but it will do until we have a real client-server model.
    git-svn-id: file:///home/svn/framework3/trunk@5518 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/ui/text/input/readline.rb
+++ lib/rex/ui/text/input/readline.rb
@@ -16,21 +16,6 @@ begin
 	class Input::Readline < Rex::Ui::Text::Input
 		include ::Readline
 
-		@@rl_thread    = nil
-		@@rl_pipes     = nil
-		@@rl_prompt    = ''
-		@@rl_history   = true
-		
-		#
-		# Enable readline mode by setting rl_usestdio to false
-		# Address the following items before switching:
-		#
-		# XXX: Find a way to emulate ^Z when in readline mode
-		# XXX: Create histories/tab completion for shells
-		# XXX: Capture remote shell prompt and use for readline
-		#
-		@@rl_usestdio  = true
-		
 		#
 		# Initializes the readline-aware Input instance for text.
 		#
@@ -50,6 +35,16 @@ begin
 			::Readline.completion_proc = tab_complete_proc || @rl_saved_proc
 		end
 
+		def child_readline(wtr, prompt, history)
+			$0 = "<readline>"
+			line = ::Readline.readline(prompt, history)
+			line = "\n" if (line and line.strip.length == 0)
+			wtr.write(line || "exit\n")
+			wtr.flush
+			wtr.close
+			exit(0)
+		end
+		
 		#
 		# Whether or not the input medium supports readline.
 		#
@@ -58,95 +53,49 @@ begin
 		end
 		
 		#
-		# Start the readline thread
-		#
-		def readline_start
-			return if @@rl_usestdio
-			return if @@rl_thread
-			@@rl_pipes  = Rex::Compat.pipe		
-			@@rl_thread = ::Thread.new do
-				begin
-				while (line = ::Readline.readline(@@rl_prompt, @@rl_history))
-					@@rl_pipes[1].write(line+"\n")
-					@@rl_pipes[1].flush
-				end
-				rescue ::Exception => e
-					$stderr.puts "ERROR: readline thread: #{e.to_s} #{e.backtrace.to_s}"
-				end
-			end
-		end
-
-		#
-		# Stop the readline thread
-		#
-		def readline_stop			
-			# Stop the reader thread
-			if (@@rl_thread)
-				begin
-					@@rl_thread.kill
-				rescue ::Exception
-				end
-				@@rl_thread = nil
-			end
-			
-			# Close the pipes
-			if (@@rl_pipes)
-				begin
-					@rl_pipes[0].close
-					@rl_pipes[1].close
-				rescue ::Exception
-				end
-				@@rl_pipes = nil
-			end
-		end 
-		
-		#
-		# Status of the readline thread
-		#
-		def readline_status
-			@@rl_thread ? true : false
-		end
-		
-		#
 		# Calls sysread on the standard input handle.
 		#
 		def sysread(len = 1)
-			return $stdin.sysread(len) if @@rl_usestdio
-		
-			if (! readline_status)
-				$stderr.puts "ERROR: sysread() called outside of thread mode: " + caller(1).to_s
-				return ''
-			end
-			@@rl_pipes[0].sysread(len)
+			$stdin.sysread(len)
+
 		end
 		
 		#
-		# Fake gets using readline
+		# Read a line from stdin
 		#
 		def gets()
-			return $stdin.gets() if @@rl_usestdio
-		
-			if (! readline_status)
-				$stderr.puts "ERROR: gets() called outside of thread mode: " + caller(1).to_s
-				return ''
-			end
-
-			@@rl_pipes[0].gets
+			$stdin.gets()
 		end
 
 		#
-		# Prompt-based getline using readline.
+		# Prompt-based getline using readline. We run the actual Readline routine inside of
+		# a forked child process. This solves a ton of problems introduced by the Readline
+		# extension. Specifically, readline will use 100ms for each time slice that its thread
+		# receives, massively slowing down the entire framework.
 		#
 		def pgets
-
-			if (readline_status)
-				$stderr.puts "ERROR: pgets called inside of thread mode: " + caller(1).to_s
-				return ''
-			end
-		
+			
 			output.prompting
-			line = ::Readline.readline(prompt, true)
+
+			rdr,wtr = ::IO.pipe
+			pid = fork()
+
+			if(not pid)
+				child_readline(wtr, prompt, true)
+			end
+
+			line = nil
+			while(not line)
+				r = select([rdr], nil, nil, 0.01)
+				if(r)
+					line = rdr.sysread(16384)
+					break if not line
+				end
+			end
+
 			output.prompting(false)
+
+			::Process.waitpid(pid, 0)
 
 			if line
 				HISTORY.pop if (line.empty?)
@@ -161,14 +110,7 @@ begin
 		# Returns the output pipe handle
 		#
 		def fd
-			return $stdin if @@rl_usestdio
-		
-			if (! readline_status)
-				$stderr.puts "fd called outside of thread mode: " + caller(1).to_s
-				return ''
-			end
-					
-			@@rl_pipes[0]
+			$stdin
 		end
 		
 		#
