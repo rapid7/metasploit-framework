48667711e65509fc16ad9be5f17179b823f840de
Mario Ceballos <mc@metasploit.com>
2008-10-09 23:53:54 +0000

    added aux module alphastor_devicemanager.rb
    git-svn-id: file:///home/svn/framework3/trunk@5724 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/auxiliary/scanner/emc/alphastor_devicemanager.rb
@@ -0,0 +1,52 @@
+##
+# This file is part of the Metasploit Framework and may be subject to 
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/projects/Framework/
+##
+
+
+require 'msf/core'
+
+
+class Metasploit3 < Msf::Auxiliary
+        
+	include Msf::Exploit::Remote::Tcp
+	include Msf::Auxiliary::Scanner
+	
+	def initialize
+		super(
+			'Name'           => 'EMC AlphaStor Device Manager Service.',
+			'Version'        => '$Revision$',
+			'Description'    => 'This module querys the remote host for the EMC Alphastor Device Danagement service.',
+			'Author'         => 'MC',
+			'License'        => MSF_LICENSE
+		)
+		
+		register_options([Opt::RPORT(3000),], self.class)
+	end
+
+
+	def run_host(ip)
+
+		connect
+
+		pkt = "\x68" + Rex::Text.rand_text_alphanumeric(5) + "\x00" * 512
+		
+		sock.put(pkt)
+
+		sleep(0.25)
+
+		data = sock.get_once
+
+		if ( data and data =~ /rrobotd:rrobotd/ )
+				print_status("Host #{ip} is running the EMC AlphaStor Device Manager.")
+		else
+				print_error("Host #{ip} is not running the service...")
+		end 
+
+		disconnect
+
+	end
+end
+
