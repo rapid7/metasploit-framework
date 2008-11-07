7c03f1479270cf10ffa4fc640556c4fa393eaf14
HD Moore <hd_moore@rapid7.com>
2008-11-07 20:44:13 +0000

    Add a better test for the ruby short-named constant bug
    git-svn-id: file:///home/svn/framework3/trunk@5848 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/msf/sanity.rb
+++ lib/msf/sanity.rb
@@ -22,12 +22,49 @@ if (RUBY_VERSION =~ /^1\.[0-7]\./ or RUBY_VERSION =~ /^1\.8\.[0-1]$/)
 	exit(0)
 end
 
-if(RUBY_VERSION == "1.8.7")
+
+#
+# Check for the ugly 1.8.7 short-named constants bug
+#
+
+class ConstBugTestA
+	Const = 'A'
+	def test
+		Const == 'A'
+	end
+end
+
+ConstBugTestC = ConstBugTestA.dup
+
+class ConstBugTestB < ConstBugTestC
+	Const = 'B'
+end
+
+def ruby_187_const_bug
+	bugged = false
+
+	begin
+		ConstBugTestA.new.test()
+		ConstBugTestB.new.test()
+	rescue ::NameError
+		bugged = true
+	end
+	
+	bugged
+end
+
+if(ruby_187_const_bug())
 	$stderr.puts ""
 	$stderr.puts "***********************************************************************"
+	$stderr.puts "***                                                                   *"
 	$stderr.puts "*** This version of the Ruby interpreter has significant problems, we *"
 	$stderr.puts "*** strongly recommend that you switch to version 1.8.6 until these   *"
-	$stderr.puts "*** issues have been corrected.                                       *"
+	$stderr.puts "*** issues have been corrected. Alternatively, you can download,      *"
+	$stderr.puts "*** build, and install the latest Ruby snapshot from:                 *"
+	$stderr.puts "***  - http://www.ruby-lang.org/                                      *"
+	$stderr.puts "*** For more information, please see the following URL:               *"
+	$stderr.puts "***  - https://bugs.launchpad.net/bugs/282302                         *"
+	$stderr.puts "***                                                                   *"
 	$stderr.puts "***********************************************************************"
 	$stderr.puts ""
 end
