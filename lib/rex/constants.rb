73b02f1899e8d91318fb376269ca42ea444101ec
Ramon de C Valle <rcvalle@live.com>
2008-11-13 01:55:25 +0000

    Updated lib/rex/constants.rb
    git-svn-id: file:///home/svn/framework3/trunk@5898 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/constants.rb
+++ lib/rex/constants.rb
@@ -63,26 +63,30 @@ LEV_3     = 3
 #
 # Architecture constants
 #
-ARCH_ANY   = '_any_'
-ARCH_X86   = 'x86'
-ARCH_MIPS  = 'mips'
-ARCH_MIPSLE  = 'mipsle'
-ARCH_MIPSBE  = 'mipsbe'
-ARCH_POWER = 'power'
-ARCH_PPC   = 'ppc'
-ARCH_SPARC = 'sparc'
-ARCH_CMD   = 'cmd'
-ARCH_PHP   = 'php'
-ARCH_ARMLE = 'armle'
-ARCH_ARMBE = 'armbe'
-ARCH_TYPES =
+ARCH_ANY    = '_any_'
+ARCH_X86    = 'x86'
+ARCH_MIPS   = 'mips'
+ARCH_MIPSLE = 'mipsle'
+ARCH_MIPSBE = 'mipsbe'
+ARCH_PPC    = 'ppc'
+ARCH_PPC64  = 'ppc64'
+ARCH_CBEA   = 'cbea'
+ARCH_CBEA64 = 'cbea64'
+ARCH_SPARC  = 'sparc'
+ARCH_CMD    = 'cmd'
+ARCH_PHP    = 'php'
+ARCH_ARMLE  = 'armle'
+ARCH_ARMBE  = 'armbe'
+ARCH_TYPES  =
 	[
 		ARCH_X86,
 		ARCH_MIPS,
 		ARCH_MIPSLE,
 		ARCH_MIPSBE,
-		ARCH_POWER,
 		ARCH_PPC,
+		ARCH_PPC64,
+		ARCH_CBEA,
+		ARCH_CBEA64,
 		ARCH_SPARC,
 		ARCH_ARMLE,
 		ARCH_ARMBE,
