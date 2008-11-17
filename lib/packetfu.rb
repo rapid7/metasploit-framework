0188938b46fd293faf3cd1ceb0944f2fd85a41c4
HD Moore <hd_moore@rapid7.com>
2008-11-17 05:12:06 +0000

    Make packetfu work with 1.8.5
    git-svn-id: file:///home/svn/framework3/trunk@5937 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/packetfu.rb
+++ lib/packetfu.rb
@@ -1,5 +1,5 @@
-if VERSION < "1.8.6"
-	$stderr.puts "[*] WARNING: Ruby not at a minimum version of 1.8.6"
+if VERSION < "1.8.5"
+	$stderr.puts "[*] WARNING: Ruby not at a minimum version of 1.8.5"
 end
 
 require 'bindata'
