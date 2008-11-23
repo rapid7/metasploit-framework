110e7be0e98dfad44a8bbaa19485459b432361ff
et <>
2008-11-23 19:42:42 +0000

    Added wmap OPTIONS  module by Chris Gates
    git-svn-id: file:///home/svn/framework3/trunk@5977 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/auxiliary/scanner/http/options.rb
@@ -0,0 +1,60 @@
+##
+# This file is part of the Metasploit Framework and may be subject to 
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/projects/Framework/
+##
+
+
+require 'msf/core'
+
+
+class Metasploit3 < Msf::Auxiliary
+	
+	# Exploit mixins should be called first
+	include Msf::Exploit::Remote::HttpClient
+	include Msf::Auxiliary::WMAPScanServer
+	# Scanner mixin should be near last
+	include Msf::Auxiliary::Scanner
+
+	def initialize
+		super(
+			'Name'        => 'HTTP Options Detection',
+			'Version'     => '$Revision: $',
+			'Description' => 'Display available HTTP options for each system',
+			'Author'      => 'CG |at| carnal0wnage |dot| com',
+			'License'     => MSF_LICENSE
+		)
+		
+	end
+
+	# Fingerprint a single host
+	def run_host(ip)
+
+		self.target_port = datastore['RPORT']	
+
+		begin
+			res = send_request_raw({
+				'version'      => '1.0',
+				'uri'          => '*',					
+				'method'       => 'OPTIONS'
+			}, 10)
+
+			if (res and res.headers['Allow'])
+				print_status("#{ip} allows #{res.headers['Allow']} methods")
+
+				rep_id = wmap_base_report_id(
+						wmap_target_host,
+						wmap_target_port,
+						wmap_target_ssl
+				)
+				#not sure about this one, can you add columns in the db on the fly?
+				wmap_report(rep_id,'WEB_SERVER','OPTIONS',"#{res.headers['Allow']}",nil)
+			end
+			
+		rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout
+		rescue ::Timeout::Error, ::Errno::EPIPE
+		end
+	end
+end
+
