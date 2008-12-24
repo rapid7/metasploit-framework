b80f50b6cbc786d63a8e6aaf27b2c04aaca18859
HD Moore <hd_moore@rapid7.com>
2008-12-24 08:41:35 +0000

    Fixes #268. Updates the executable template and provides a bug fix for msfencode to force the encoded version
    git-svn-id: file:///home/svn/framework3/trunk@6043 4d416f70-5f16-0410-b530-b9f4589650da

--- data/templates/template.s
+++ data/templates/template.s
@@ -6,14 +6,22 @@ _code:
 .globl _WinMain@16
 	.def	_WinMain@16;	.scl	2;	.type	32;	.endef
 _WinMain@16:
+
 xorl %eax,%eax
-movl $_code,%ecx
 pushl $_ExitProcess@4
 pushl %fs:(%eax)
 movl %esp,%fs:(%eax)
-pushl %eax
+
 pushl $0x40
-pushl $8192
-pushl %ecx
-pushl %ecx
-jmp _VirtualProtect@16
+pushl $0x3000
+pushl $0x100000
+pushl $0
+call _VirtualAlloc@16
+
+movl %eax, %ebp
+movl %eax, %edi
+movl $_code,%esi
+mov $2048, %ecx
+rep movsd
+
+call %ebp
