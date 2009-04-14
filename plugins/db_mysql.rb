95419cce6f1a2d71cb3b32ae80f569199dc3eec8
James Lee <James_Lee@rapid7.com>
2009-04-14 03:54:18 +0000

    add stubs for deprecated db_* plugins
    git-svn-id: file:///home/svn/framework3/trunk@6480 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ plugins/db_mysql.rb
@@ -0,0 +1,18 @@
+
+module Msf
+class Plugin::DeprecatedStub < Msf::Plugin
+	def name
+		"Deprecated_plugin_stub"
+	end
+	def initialize(framework, opts)
+		super
+		print_error("")
+		print_error("The functionality previously provided by this plugin has been")
+		print_error("integrated into the core command set.  Use the new 'db_driver'")
+		print_error("command to use a database driver other than sqlite3 (which")
+		print_error("is now the default).  All of the old commands are the same.")
+		print_error("")
+	end
+end
+end
+
