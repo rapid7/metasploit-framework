9f692677591af2fb4964a60ab3a9239aa6a5fc98
HD Moore <hd_moore@rapid7.com>
2009-06-15 02:40:28 +0000

    Switch all warnings to stderr
    git-svn-id: file:///home/svn/framework3/trunk@6663 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/msf/sanity.rb
+++ lib/msf/sanity.rb
@@ -5,12 +5,12 @@
 
 # Check for the broken pack/unpack in OS X 10.4.x
 if ([1].pack('n') == "\x01\x00")
-	puts "*** This ruby build has a broken pack/unpack implementation! "
+	$stderr.puts "*** This ruby build has a broken pack/unpack implementation! "
 	
 	if (RUBY_PLATFORM =~ /darwin/)
-		puts "    Apple shipped a broken version of ruby with the 10.4.x   "
-		puts "    release. Please install ruby from source, or use one of  "
-		puts "    the free package managers to obtain a working ruby build."
+		$stderr.puts "    Apple shipped a broken version of ruby with the 10.4.x   "
+		$stderr.puts "    release. Please install ruby from source, or use one of  "
+		$stderr.puts "    the free package managers to obtain a working ruby build."
 	end
 	
 	exit(0)
@@ -18,19 +18,19 @@ end
 
 # Check for ruby 1.8.2 as the minimal supported version
 if (RUBY_VERSION =~ /^1\.[0-7]\./ or RUBY_VERSION =~ /^1\.8\.[0-1]$/)
-	puts "*** This version of ruby is not supported, please upgrade to 1.8.2+"
+	$stderr.puts "*** This version of ruby is not supported, please upgrade to 1.8.2+"
 	exit(0)
 end
 
 # Check for ruby 1.9.0 and throw a big nasty warning
 if (RUBY_VERSION =~ /^1\.9\.0/)
-	puts "*** Ruby 1.9.0 is not supported, please upgrade to Ruby 1.9.1 or newer."
+	$stderr.puts "*** Ruby 1.9.0 is not supported, please upgrade to Ruby 1.9.1 or newer."
 	exit(0)
 end
 
 if(RUBY_VERSION =~ /^1\.9\./)	
-	puts "*** Metasploit only has EXPERIMENTAL support for Ruby 1.9.1 and newer, things may break!"
-	puts "*** Please report bugs to msfdev[at]metasploit.com"
+	$stderr.puts "*** Metasploit only has EXPERIMENTAL support for Ruby 1.9.1 and newer, things may break!"
+	$stderr.puts "*** Please report bugs to msfdev[at]metasploit.com"
 
 	# Force binary encoding for Ruby versions that support it
 	if(Object.const_defined?('Encoding') and Encoding.respond_to?('default_external='))
@@ -42,8 +42,8 @@ if(RUBY_PLATFORM == 'java')
 	require 'socket'
 	s = Socket.new(::Socket::AF_INET, ::Socket::SOCK_STREAM, ::Socket::IPPROTO_TCP)
 	if(not s.respond_to?('bind'))
-		puts "*** JRuby will not be supported until the Socket API is complete"
-		puts "*** Information: http://jira.codehaus.org/browse/JRUBY-2739"
+		$stderr.puts "*** JRuby will not be supported until the Socket API is complete"
+		$stderr.puts "*** Information: http://jira.codehaus.org/browse/JRUBY-2739"
 		exit(0)
 	end
 end
