1a220d6dc54f450d8b3fc6a8af715305a3a7f8a0
Stephen Fewer <stephen_fewer@harmonysecurity.com>
2009-09-27 18:35:07 +0000

    add java payload jsp_shell_reverse_tcp.
    git-svn-id: file:///home/svn/framework3/trunk@7071 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/constants.rb
+++ lib/rex/constants.rb
@@ -80,6 +80,7 @@ ARCH_PHP    = 'php'
 ARCH_TTY    = 'tty'
 ARCH_ARMLE  = 'armle'
 ARCH_ARMBE  = 'armbe'
+ARCH_JAVA   = 'java'
 ARCH_TYPES  =
 	[
 		ARCH_X86,
@@ -96,7 +97,8 @@ ARCH_TYPES  =
 		ARCH_ARMBE,
 		ARCH_CMD,
 		ARCH_PHP,
-		ARCH_TTY
+		ARCH_TTY,
+		ARCH_JAVA
 	]
 
 ARCH_ALL = ARCH_TYPES
