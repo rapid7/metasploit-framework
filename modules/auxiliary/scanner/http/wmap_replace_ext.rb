bfd359f0d8fb21271068c90390b2400ca0f20625
et <>
2009-10-18 20:37:40 +0000

    Fixed #367. Adder error code detection and signatures
    git-svn-id: file:///home/svn/framework3/trunk@7191 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/auxiliary/scanner/http/wmap_replace_ext.rb
+++ modules/auxiliary/scanner/http/wmap_replace_ext.rb
@@ -37,12 +37,27 @@ class Metasploit3 < Msf::Auxiliary
 			[
 				OptString.new('PATH', [ true,  "The path/file to identify additional files", '/default.asp']),
 				OptString.new('EXT', [ false, "File extension to replace (blank for automatic replacement of extension)", '']), 
+			], self.class)
+				
+		register_advanced_options(
+			[
+				OptInt.new('ErrorCode', [ true,  "The expected http code for non existant files", 404]),
+				OptPath.new('HTTP404Sigs',   [ false, "Path of 404 signatures to use", 
+						File.join(Msf::Config.install_root, "data", "wmap", "wmap_404s.txt")
+					]
+				),
+				OptBool.new('NoDetailMessages', [ false, "Do not display detailed test messages", true ])
 			], self.class)	
+			
 						
 	end
 
 	def run_host(ip)
- 		
+	
+ 		conn = false
+		
+		dm = datastore['NoDetailMessages']
+		
 		extensions= [
 			'bak',
  			'txt',
@@ -59,6 +74,55 @@ class Metasploit3 < Msf::Auxiliary
   		
 
 		extensions.each { |testext|
+			
+			#
+			# Detect error code. This module is a special case as each extension
+			# usually is handled diferently by the server with different error codes 
+			# 
+			ecode = datastore['ErrorCode'].to_i		
+			begin
+				randchars = Rex::Text.rand_text_alpha(3).chomp
+				tpath = tpathnoext+randchars+testext
+				
+				res = send_request_cgi({
+					'uri'  		=>  tpath,
+					'method'   	=> 'GET',
+					'ctype'		=> 'text/html'
+				}, 20)
+
+				return if not res
+			
+				tcode = res.code.to_i 
+
+	
+				# Look for a string we can signature on as well
+				if(tcode >= 200 and tcode <= 299)
+			
+					File.open(datastore['HTTP404Sigs']).each do |str|
+						if(res.body.index(str))
+							emesg = str
+							break
+						end
+					end
+
+					if(not emesg)
+						print_status("Using first 256 bytes of the response as 404 string")
+						emesg = res.body[0,256]
+					else
+						print_status("Using custom 404 string of '#{emesg}'")
+					end
+				else
+					ecode = tcode
+					print_status("Using code '#{ecode}' as not found.")
+				end
+			
+			rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout
+				conn = false		
+			rescue ::Timeout::Error, ::Errno::EPIPE			
+			end
+			
+			#if not conn return
+		
 			begin
 				tpath = tpathnoext+testext
 					res = send_request_cgi({
@@ -66,23 +130,28 @@ class Metasploit3 < Msf::Auxiliary
 						'method'   	=> 'GET',
 						'ctype'		=> 'text/plain'
 				}, 20)
-
-				if (res and res.code >= 200 and res.code < 300) 
-					print_status("Found #{wmap_base_url}#{tpath}")
-				   
-					rep_id = wmap_base_report_id(
-						wmap_target_host,
-						wmap_target_port,
-						wmap_target_ssl
-					)
-								
-					vul_id = wmap_report(rep_id,'FILE','NAME',"#{tpath}","File #{tpath} found.")
-					wmap_report(vul_id,'FILE','RESP_CODE',"#{res.code}",nil)
+				
+				if(not res or ((res.code.to_i == ecode) or (emesg and res.body.index(emesg))))
+					if dm == false
+						print_status("NOT Found #{wmap_base_url}#{tpath}  #{res.code.to_i}") 
+						#blah
+					end
 				else
-					print_status("NOT Found #{wmap_base_url}#{tpath}") 
-					#blah
+					if res.code.to_i == 400  and ecode != 400    
+						print_error("Server returned an error code. #{wmap_base_url}#{tpath} #{res.code.to_i}") 
+					else
+						print_status("Found #{wmap_base_url}#{tpath}")
+				   
+						rep_id = wmap_base_report_id(
+							wmap_target_host,
+							wmap_target_port,
+							wmap_target_ssl
+						)
+								
+						vul_id = wmap_report(rep_id,'FILE','NAME',"#{tpath}","File #{tpath} found.")
+						wmap_report(vul_id,'FILE','RESP_CODE',"#{res.code}",nil)
+					end
 				end
-
 			rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout
 			rescue ::Timeout::Error, ::Errno::EPIPE			
 			end	
