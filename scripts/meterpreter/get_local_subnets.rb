cadb1c9337367128407f9c8f4b839e952576ff9e
James Lee <James_Lee@rapid7.com>
2009-10-25 21:03:42 +0000

    add -h and usage to get_local_subnets
    git-svn-id: file:///home/svn/framework3/trunk@7259 4d416f70-5f16-0410-b530-b9f4589650da

--- scripts/meterpreter/get_local_subnets.rb
+++ scripts/meterpreter/get_local_subnets.rb
@@ -4,6 +4,20 @@
 ## Provided by Nicob <nicob [at] nicob.net>
 ## Ripped from http://blog.metasploit.com/2006/10/meterpreter-scripts-and-msrt.html
 
+@@exec_opts = Rex::Parser::Arguments.new(
+	"-h" => [ false,"Help menu." ]
+)
+
+@@exec_opts.parse(args) { |opt, idx, val|
+	case opt
+	when "-h"
+		print_line("Get a list of local subnets based on the host's routes")
+		print_line("USAGE: run get_local_subnets")
+		puts @@exec_opts.usage
+		raise "Usage"
+	end
+}
+
 client.net.config.each_route { |route|
     # Remove multicast and loopback interfaces
     next if route.subnet =~ /^(224\.|127\.)/
