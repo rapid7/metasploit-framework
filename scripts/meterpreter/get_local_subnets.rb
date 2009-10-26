bf48e395321e7594d2a98d2cdc827d5b691ca09a
James Lee <James_Lee@rapid7.com>
2009-10-26 04:43:59 +0000

    get_local_subnets usage
    git-svn-id: file:///home/svn/framework3/trunk@7268 4d416f70-5f16-0410-b530-b9f4589650da

--- scripts/meterpreter/get_local_subnets.rb
+++ scripts/meterpreter/get_local_subnets.rb
@@ -1,20 +1,23 @@
 # $Id:$
 
-## Meterpreter script that display local subnets
-## Provided by Nicob <nicob [at] nicob.net>
-## Ripped from http://blog.metasploit.com/2006/10/meterpreter-scripts-and-msrt.html
+# Meterpreter script that display local subnets
+# Provided by Nicob <nicob [at] nicob.net>
+# Ripped from http://blog.metasploit.com/2006/10/meterpreter-scripts-and-msrt.html
 
 @@exec_opts = Rex::Parser::Arguments.new(
-	"-h" => [ false,"Help menu." ]
+	"-h" => [ false, "Help menu." ]
 )
+def usage
+	print_line("Get a list of local subnets based on the host's routes")
+	print_line("USAGE: run get_local_subnets")
+	print_line(@@exec_opts.usage)
+	raise Rex::Script::Completed
+end
 
 @@exec_opts.parse(args) { |opt, idx, val|
 	case opt
 	when "-h"
-		print_line("Get a list of local subnets based on the host's routes")
-		print_line("USAGE: run get_local_subnets")
-		print_line(@@exec_opts.usage)
-		raise Rex::Script::Completed
+		usage
 	end
 }
 
