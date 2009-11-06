c5ace6ef28435f504e65216756ece53683a1fa7a
HD Moore <hd_moore@rapid7.com>
2009-11-06 20:47:45 +0000

    Use the open_file API
    git-svn-id: file:///home/svn/framework3/trunk@7391 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/post/meterpreter/ui/console/command_dispatcher/espia.rb
+++ lib/rex/post/meterpreter/ui/console/command_dispatcher/espia.rb
@@ -33,59 +33,58 @@ class Console::CommandDispatcher::Espia
 			"screenshot" => "Attempt to grab screen shot from process's active desktop"
 		}
 	end
-	
+
 	def cmd_dev_image()
 		client.espia.espia_video_get_dev_image()
 		print_line("[*] Done.")
-		
+
 		return true
 	end
-	
+
 	def cmd_dev_audio(*args)
 		maxrec = 60
-		
+
 		if (args.length < 1)
 			print_line("Usage: dev_audio <rec_secs>\n")
 			print_line("Record mic audio\n")
 			return true
 		end
-		
+
 		secs = args[0].to_i
 		if secs  > 0 and secs <= maxrec
 			milsecs = secs*1000
 			print_line("[*] Recording #{milsecs} miliseconds.\n")
 			client.espia.espia_audio_get_dev_audio(milsecs)
 			print_line("[*] Done.")
-		else	
+		else
 			print_line("[-] Error: Recording time 0 to 60 secs \n")
-		end	
-		
+		end
+
 		return true
 	end
-	
+
 	def cmd_screenshot(*args)
 		if (args.length < 1 or args[0] == "-h")
 			print_line("Usage: screenshot <path.bmp> [view in browser: true|false]\n")
 			return true
 		end
-		
+
 		show = true
 		show = false if (args[1] and args[1] =~ /^(f|n|0)/i)
-		
+
 		path = args[0]
-		
+
 		data = client.espia.espia_image_get_dev_screen
-		
+
 		if(data)
 			::File.open(path, 'wb') do |fd|
 				fd.write(data)
 			end
 		end
-		
+		path = ::File.expand_path(path)
 		print_line("[*] Image saved to #{path}")
 		if(show)
-			print_line("Opening browser to image...")
-			::Rex::Compat.open_browser(path)
+			::Rex::Compat.open_file(path)
 		end
 		return true
 	end
@@ -103,3 +102,4 @@ end
 end
 end
 end
+
