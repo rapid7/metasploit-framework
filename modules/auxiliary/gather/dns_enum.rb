4c0ba49a65ab1b31cda556ef2b66ef29e097a75b
James Lee <James_Lee@rapid7.com>
2009-11-13 23:22:19 +0000

    fix syntax error
    git-svn-id: file:///home/svn/framework3/trunk@7500 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/auxiliary/gather/dns_enum.rb
+++ modules/auxiliary/gather/dns_enum.rb
@@ -21,7 +21,7 @@ class Metasploit3 < Msf::Auxiliary
 				},
 				'Author'		=> [ 'Carlos Perez <carlos_perez[at]darkoperator.com>' ],
 				'License'		=> MSF_LICENSE,
-				'Version'		=> '$Rev$'
+				'Version'		=> '$Rev$',
 			 	'References' 	=>
 					[
 						['CVE', '1999-0532'],
