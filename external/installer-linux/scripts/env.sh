8ba8945f53fb5475fd51051dd5d8a851cbd18f25
HD Moore <hd_moore@rapid7.com>
2009-11-28 16:38:52 +0000

    Fixes #579. Clean up the runtime environment for ruby, update the version
    git-svn-id: file:///home/svn/framework3/trunk@7633 4d416f70-5f16-0410-b530-b9f4589650da

--- external/installer-linux/scripts/env.sh
+++ external/installer-linux/scripts/env.sh
@@ -2,4 +2,10 @@
 export BASE=`dirname $0`
 export PATH=${BASE}/bin:$PATH
 export LD_LIBRARY_PATH=${BASE}/lib:$LD_LIBRARY_PATH
+unset GEM_PATH
+unset MY_RUBY_HOME
+unset RUBY_VERSION
+unset RUBY_OPTS
+
 $@
+
--- external/installer-linux/scripts/run.sh
+++ external/installer-linux/scripts/run.sh
@@ -17,9 +17,14 @@ export BASE
 
 export PATH=${BASE}/app:$PATH
 export LD_LIBRARY_PATH=${BASE}/lib:$LD_LIBRARY_PATH
+unset GEM_PATH
+unset MY_RUBY_HOME
+unset RUBY_VERSION
+unset RUBY_OPTS
 
 if [ -f "${BASE}/msf3/${NAME}" ]; then
 	exec ${BASE}/msf3/${NAME} $@
 fi
 
 exec ${NAME} $@
+
