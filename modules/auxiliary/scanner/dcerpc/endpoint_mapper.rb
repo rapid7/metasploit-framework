8c12722f9ba26d47f24eb2e948ada05b0cff1732
James Lee <James_Lee@rapid7.com>
2009-12-18 01:51:31 +0000

    add database reporting to endpoint_mapper. fixes #695
    git-svn-id: file:///home/svn/framework3/trunk@7913 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/auxiliary/scanner/dcerpc/endpoint_mapper.rb
+++ modules/auxiliary/scanner/dcerpc/endpoint_mapper.rb
@@ -18,6 +18,8 @@ class Metasploit3 < Msf::Auxiliary
 	# Exploit mixins should be called first
 	include Msf::Exploit::Remote::DCERPC
 	
+	include Msf::Auxiliary::Report
+
 	# Scanner mixin should be near last
 	include Msf::Auxiliary::Scanner
 	
@@ -47,6 +49,7 @@ class Metasploit3 < Msf::Auxiliary
 
 			ids = dcerpc_endpoint_list()
 			return if not ids
+			name = nil
 			ids.each do |id|
 				next if not id[:prot]
 				line = "#{id[:uuid]} v#{id[:vers]} "
@@ -56,7 +59,15 @@ class Metasploit3 < Msf::Auxiliary
 				line << "#{id[:host]} " if id[:host]
 				line << "[#{id[:note]}]" if id[:note]
 				print_status(line)							
+				if (id[:host] and id[:host][0,2] == "\\\\")
+					name = id[:host][2..-1]
+				end
+				if id[:prot].downcase == "tcp" or id[:prot].downcase == "udp"
+					report_service(:host => ip, :port => id[:port], :proto => id[:prot].downcase)
+				end
 			end
+			report_host(:host => ip, :name => name) if name
+
 			
 		rescue ::Interrupt
 			raise $!
