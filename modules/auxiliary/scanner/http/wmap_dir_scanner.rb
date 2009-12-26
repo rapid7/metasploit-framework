1cc9fa7dad8ab78d39b24e7feaf385017522977b
HD Moore <hd_moore@rapid7.com>
2009-12-26 05:53:29 +0000

    Print the site name for the 404 message
    git-svn-id: file:///home/svn/framework3/trunk@7980 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/auxiliary/scanner/http/wmap_dir_scanner.rb
+++ modules/auxiliary/scanner/http/wmap_dir_scanner.rb
@@ -3,7 +3,7 @@
 ##
 
 ##
-# This file is part of the Metasploit Framework and may be subject to 
+# This file is part of the Metasploit Framework and may be subject to
 # redistribution and commercial restrictions. Please see the Metasploit
 # Framework web site for more information on licensing and terms of use.
 # http://metasploit.com/framework/
@@ -22,58 +22,58 @@ class Metasploit3 < Msf::Auxiliary
 	include Msf::Auxiliary::Report
 
 	def initialize(info = {})
-		super(update_info(info,	
+		super(update_info(info,
 			'Name'   		=> 'HTTP Directory Scanner',
 			'Description'	=> %q{
-				This module identifies the existence of interesting directories 
-				in a given directory path.					
+				This module identifies the existence of interesting directories
+				in a given directory path.
 			},
 			'Author' 		=> [ 'et [at] metasploit.com' ],
 			'License'		=> BSD_LICENSE,
-			'Version'		=> '$Revision$'))   
-			
+			'Version'		=> '$Revision$'))
+
 		register_options(
 			[
 				OptString.new('PATH', [ true,  "The path  to identify files", '/']),
-				OptPath.new('DICTIONARY',   [ false, "Path of word dictionary to use", 
+				OptPath.new('DICTIONARY',   [ false, "Path of word dictionary to use",
 						File.join(Msf::Config.install_root, "data", "wmap", "wmap_dirs.txt")
 					]
 				)
-								
+
 			], self.class)
-			
+
 		register_advanced_options(
 			[
 				OptInt.new('ErrorCode', [ true, "Error code for non existent directory", 404]),
-				OptPath.new('HTTP404Sigs',   [ false, "Path of 404 signatures to use", 
+				OptPath.new('HTTP404Sigs',   [ false, "Path of 404 signatures to use",
 						File.join(Msf::Config.install_root, "data", "wmap", "wmap_404s.txt")
 					]
 				),
 				OptBool.new('NoDetailMessages', [ false, "Do not display detailed test messages", true ]),
 				OptInt.new('TestThreads', [ true, "Number of test threads", 25])
-				
-			], self.class)			
-						
+
+			], self.class)
+
 	end
 
 	def run_host(ip)
 		conn = true
 		ecode = nil
 		emesg = nil
-	
-		tpath = datastore['PATH'] 	
+
+		tpath = datastore['PATH']
 		if tpath[-1,1] != '/'
 			tpath += '/'
 		end
- 	
+
 		ecode = datastore['ErrorCode'].to_i
 		vhost = datastore['VHOST'] || wmap_target_host
 		prot  = datastore['SSL'] ? 'https' : 'http'
-		
-		 
+
+
 		#
 		# Detect error code
-		# 		
+		#
 		begin
 			randdir = Rex::Text.rand_text_alpha(5).chomp + '/'
 			res = send_request_cgi({
@@ -83,13 +83,13 @@ class Metasploit3 < Msf::Auxiliary
 			}, 20)
 
 			return if not res
-			
-			tcode = res.code.to_i 
 
-	
+			tcode = res.code.to_i
+
+
 			# Look for a string we can signature on as well
 			if(tcode >= 200 and tcode <= 299)
-			
+
 				File.open(datastore['HTTP404Sigs']).each do |str|
 					if(res.body.index(str))
 						emesg = str
@@ -98,28 +98,28 @@ class Metasploit3 < Msf::Auxiliary
 				end
 
 				if(not emesg)
-					print_status("Using first 256 bytes of the response as 404 string")
+					print_status("Using first 256 bytes of the response as 404 string for #{wmap_target_host}")
 					emesg = res.body[0,256]
 				else
-					print_status("Using custom 404 string of '#{emesg}'")
+					print_status("Using custom 404 string of '#{emesg}' for #{wmap_target_host}")
 				end
 			else
 				ecode = tcode
-				print_status("Using code '#{ecode}' as not found.")
+				print_status("Using code '#{ecode}' as not found for #{wmap_target_host}")
 			end
-			
+
 		rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout
-			conn = false		
-		rescue ::Timeout::Error, ::Errno::EPIPE			
+			conn = false
+		rescue ::Timeout::Error, ::Errno::EPIPE
 		end
 
 		return if not conn
-		
+
 		nt = datastore['TestThreads'].to_i
 		nt = 1 if nt == 0
-		
+
 		dm = datastore['NoDetailMessages']
-	
+
 		queue = []
 		File.open(datastore['DICTIONARY']).each_line do |testd|
 			queue << testd.strip + '/'
@@ -127,10 +127,10 @@ class Metasploit3 < Msf::Auxiliary
 
 		while(not queue.empty?)
 			t = []
-			1.upto(nt) do 
+			1.upto(nt) do
 				t << Thread.new(queue.shift) do |testf|
 					Thread.current.kill if not testf
-	
+
 					testfdir = testf
 					res = send_request_cgi({
 						'uri'  		=>  tpath+testfdir,
@@ -141,7 +141,7 @@ class Metasploit3 < Msf::Auxiliary
 
 					if(not res or ((res.code.to_i == ecode) or (emesg and res.body.index(emesg))))
 						if dm == false
-							print_status("NOT Found #{wmap_base_url}#{tpath}#{testfdir} #{res.code} (#{wmap_target_host})") 					
+							print_status("NOT Found #{wmap_base_url}#{tpath}#{testfdir} #{res.code} (#{wmap_target_host})")
 						end
 					else
 						report_note(
@@ -151,12 +151,12 @@ class Metasploit3 < Msf::Auxiliary
 							:type	=> 'DIRECTORY',
 							:data	=> "#{tpath}#{testfdir} Code: #{res.code}"
 						)
-					
+
 						print_status("Found #{wmap_base_url}#{tpath}#{testfdir} #{res.code} (#{wmap_target_host})")
 
 						if res.code.to_i == 401
 							print_status("#{wmap_base_url}#{tpath}#{testfdir} requires authentication: #{res.headers['WWW-Authenticate']}")
-							
+
 							report_note(
 								:host	=> ip,
 								:proto	=> 'HTTP',
@@ -165,11 +165,12 @@ class Metasploit3 < Msf::Auxiliary
 								:data	=> "#{tpath}#{testfdir} Auth: #{res.headers['WWW-Authenticate']}"
 							)
 						end
-					end			
-					
+					end
+
 				end
 			end
 			t.map{|x| x.join }
 		end
 	end
 end
+
