a0523407039434fc693c5b7329b4615d0fc862b6
Joshua Drake <github.jdrake@qoop.org>
2010-02-03 20:30:09 +0000

    a few fixes to sunrpc code
    this fixes a couple of errors handling error messages
    also, some whitespace/indenting adjustments
    
    
    
    git-svn-id: file:///home/svn/framework3/trunk@8365 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/proto/sunrpc/client.rb
+++ lib/rex/proto/sunrpc/client.rb
@@ -16,16 +16,16 @@ class RPCTimeout < ::Interrupt
 end
 
 # XXX: CPORT!
-class Client 
+class Client
 	AUTH_NULL = 0
 	AUTH_UNIX = 1
-	
+
 	PMAP_PROG = 100000
 	PMAP_VERS = 2
 	PMAP_GETPORT = 3
-	
+
 	CALL = 0
-	
+
 	attr_reader :rhost, :rport, :proto, :program, :version
 	attr_accessor :pport
 
@@ -40,13 +40,13 @@ class Client
 			rhost, rport, program, version, proto.downcase
 
 		@pport = nil
-		
+
 		@auth_type = AUTH_NULL
 		@auth_data = ''
-		
+
 		@call_sock = nil
 	end
-	
+
 # XXX: Add optional parameter to have proto be something else
 	def create()
 		proto_num = 0
@@ -60,7 +60,7 @@ class Client
 			Rex::Encoder::XDR.encode(CALL, 2, PMAP_PROG, PMAP_VERS, PMAP_GETPORT,
 				@auth_type, [@auth_data, 400], AUTH_NULL, '',
 				@program, @version, proto_num, 0)
-		
+
 		sock = make_rpc(@proto, @rhost, @rport)
 		send_rpc(sock, buf)
 		ret = recv_rpc(sock)
@@ -73,46 +73,46 @@ class Client
 		buf =
 			Rex::Encoder::XDR.encode(CALL, 2, @program, @version, procedure,
 				@auth_type, [@auth_data, 400], AUTH_NULL, '')+
-			buffer		
-		
+			buffer
+
 		if !@call_sock
 			@call_sock = make_rpc(@proto, @rhost, @pport)
 		end
 
 		send_rpc(@call_sock, buf)
-    recv_rpc(@call_sock, timeout)
+		recv_rpc(@call_sock, timeout)
 	end
-	
+
 	def destroy
 		close_rpc(@call_sock) if @call_sock
 		@call_sock = nil
 	end
-	
+
 	def authnull_create
 		@auth_type = AUTH_NULL
 		@auth_data = ''
 	end
-	
+
 	def authunix_create(host, uid, gid, groupz)
 		raise ::Rex::ArgumentError, 'Hostname length is too long' if host.length > 255
 # 10?
 		raise ::Rex::ArgumentError, 'Too many groups' if groupz.length > 10
-		
+
 		@auth_type = AUTH_UNIX
 		@auth_data =
 			Rex::Encoder::XDR.encode(0, host, uid, gid, groupz) # XXX: TIME! GROUPZ?!
 	end
-	
+
 # XXX: Dirty, integrate some sort of request system into create/call?
 	def portmap_req(host, port, rpc_vers, procedure, buffer)
 		buf = Rex::Encoder::XDR.encode(CALL, 2, PMAP_PROG, rpc_vers, procedure,
 			AUTH_NULL, '', AUTH_NULL, '') + buffer
-		
+
 		sock = make_rpc('tcp', host, port)
 		send_rpc(sock, buf)
 		ret = recv_rpc(sock)
 		close_rpc(sock)
-		
+
 		return ret
 	end
 
@@ -128,11 +128,11 @@ class Client
 		if !self.should_fragment
 			return Rex::Encoder::XDR.encode(0x80000000 | buf.length) + buf
 		end
-		
+
 		str = buf.dup
 
 		fragmented = ''
-		
+
 		while (str.size > 0)
 			frag = str.slice!(0, rand(3) + 1)
 			len = frag.size
@@ -153,7 +153,7 @@ class Client
 		end
 		sock.write(buf)
 	end
-	
+
 	def recv_rpc(sock, timeout=60)
 		buf = sock.get(timeout)
 		buf.slice!(0..3)
@@ -163,11 +163,11 @@ class Client
 		return buf if buf.length > 1
 		return nil
 	end
-	
+
 	def close_rpc(sock)
 		sock.close
 	end
-	
+
 	def gen_xid
 		return Rex::Encoder::XDR.encode(rand(0xffffffff) + 1)
 	end
