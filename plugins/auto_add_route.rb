4617eba2585b86b44a24b89963841f67c71f8a5b
James Lee <James_Lee@rapid7.com>
2010-02-15 15:10:09 +0000

    fix\ a\ bug\ in\ 1.8\ where\ instance methods of singletons aren't mirrored as class\ methods by calling the instance method
    git-svn-id: file:///home/svn/framework3/trunk@8503 4d416f70-5f16-0410-b530-b9f4589650da

--- plugins/auto_add_route.rb
+++ plugins/auto_add_route.rb
@@ -5,14 +5,15 @@ class Plugin::AutoAddRoute < Msf::Plugin
 	def on_session_open(session)
 		return if not session.type == 'meterpreter'
 		session.load_stdapi
+		sb = Rex::Socket::SwitchBoard.instance
 		session.net.config.each_route { |route|
 			# Remove multicast and loopback interfaces
 			next if route.subnet =~ /^(224\.|127\.)/
 			next if route.subnet == '0.0.0.0'
 			next if route.netmask == '255.255.255.255'
-			if not Rex::Socket::SwitchBoard.route_exists?(route.subnet, route.netmask)
+			if not sb.route_exists?(route.subnet, route.netmask)
 				print_status("AutoAddRoute: Routing new subnet #{route.subnet}/#{route.netmask} through session #{session.sid}")
-				Rex::Socket::SwitchBoard.add_route(route.subnet, route.netmask, session)
+				sb.add_route(route.subnet, route.netmask, session)
 			end
 		}
 	end
