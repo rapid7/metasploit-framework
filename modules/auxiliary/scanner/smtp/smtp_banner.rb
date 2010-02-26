2cbf64b85afd1b548a5745c3d049bbbfdc8e036c
HD Moore <hd_moore@rapid7.com>
2010-02-26 18:47:48 +0000

    Fix up the stored banner for SMTP
    git-svn-id: file:///home/svn/framework3/trunk@8661 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/auxiliary/scanner/smtp/smtp_banner.rb
+++ modules/auxiliary/scanner/smtp/smtp_banner.rb
@@ -1,5 +1,5 @@
 ##
-# This file is part of the Metasploit Framework and may be subject to 
+# This file is part of the Metasploit Framework and may be subject to
 # redistribution and commercial restrictions. Please see the Metasploit
 # Framework web site for more information on licensing and terms of use.
 # http://metasploit.com/framework/
@@ -26,33 +26,19 @@ class Metasploit3 < Msf::Auxiliary
 			'License'     => MSF_LICENSE
 		)
 		deregister_options('MAILFROM', 'MAILTO')
-
 	end
 
-	def run_host(target_host)
-
+	def run_host(ip)
 		begin
-		
-			res = connect(true)
-
-		if res 
-			report_note(
-				:host	=> target_host,
-				:proto	=> 'SMTP',
-				:port	=> rport,
-				:type	=> 'BANNER',
-				:data	=> banner.strip!
-			)
-
-			print_status("#{target_host}:#{rport} is running (#{banner})")
-
+			res = connect
+			banner_sanitized = banner.to_s.gsub(/[\x00-\x19\x7f-\xff]/) { |s| "\\x%02x" % s[0,1].unpack("C")[0] }
+			print_status("#{ip}:#{rport} SMTP #{banner_sanitized}")
+			report_service(:host => rhost, :port => rport, :name => "smtp", :info => banner)
+		rescue ::Rex::ConnectionError
+		rescue ::Exception => e
+			print_error("#{rhost}:#{rport} #{e} #{e.backtrace}")
 		end
-
-		disconnect
-		
-		rescue ::Interrupt
-			raise $!
-		rescue ::Rex::ConnectionError, ::IOError
-		end	
 	end
+
 end
+
