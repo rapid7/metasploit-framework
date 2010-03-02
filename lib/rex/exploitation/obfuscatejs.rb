15b13d247283ecad4c6a35d1af157e362272b242
James Lee <James_Lee@rapid7.com>
2010-03-02 14:36:09 +0000

    make string obfuscation produce correct code more often (should be 100% now, but no guarantees). stops doing hex-encoding (more hassle than it's worth) and adds random split/concats (jduck's idea)
    git-svn-id: file:///home/svn/framework3/trunk@8690 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/exploitation/obfuscatejs.rb
+++ lib/rex/exploitation/obfuscatejs.rb
@@ -1,3 +1,4 @@
+require 'rex/text'
 module Rex
 module Exploitation
 
@@ -8,10 +9,10 @@ class ObfuscateJS
 	attr_reader :opts
 
 	#
-	# Obfuscates a javascript string.  
+	# Obfuscates a javascript string.
 	#
 	# Options are 'Symbols', described below, and 'Strings', a boolean
-	# which specifies whether strings within the javascript should be 
+	# which specifies whether strings within the javascript should be
 	# mucked with (defaults to false).
 	#
 	# The 'Symbols' argument should have the following format:
@@ -39,9 +40,9 @@ class ObfuscateJS
 	#     }
 	# ENDJS
 	# js.obfuscate(
-	#     'Symbols' => { 
+	#     'Symbols' => {
 	#	       'Variables' => [ 'foo' ],
-	#	       'Methods'   => [ 'say_hi' ] 
+	#	       'Methods'   => [ 'say_hi' ]
 	#	  }
 	#     'Strings' => true
 	# )
@@ -54,7 +55,7 @@ class ObfuscateJS
 	# </code>
 	#
 	# String obfuscation tries to deal with escaped quotes within strings but
-	# won't catch things like 
+	# won't catch things like
 	#     "\\"
 	# so be careful.
 	#
@@ -94,7 +95,7 @@ class ObfuscateJS
 						end
 					}
 				elsif (opts['Symbols'][k])
-					@opts['Symbols'][k] = opts['Symbols'][k] 
+					@opts['Symbols'][k] = opts['Symbols'][k]
 				end
 			}
 		elsif opts['Symbols']
@@ -117,7 +118,7 @@ class ObfuscateJS
 	# Obfuscates the javascript string passed to the constructor
 	#
 	def obfuscate(opts = {})
-		return @js if (@done)
+		#return @js if (@done)
 		@done = true
 
 		# Remove our comments
@@ -125,7 +126,6 @@ class ObfuscateJS
 		
 		update_opts(opts)
 
-		#$stderr.puts @opts.inspect
 		if (@opts['Strings'])
 			obfuscate_strings()
 
@@ -133,16 +133,16 @@ class ObfuscateJS
 			# claims that space is irrelavent, newlines break things.  Instead,
 			# use only space (0x20) and tab (0x09).
 
-			@js.gsub!(/[\x09\x20]+/) { |s|
-				len = rand(50)+2
-				set = "\x09\x20"
-				buf = ''
-				while (buf.length < len)
-					buf << set[rand(set.length)].chr
-				end
-				
-				buf
-			}
+			#@js.gsub!(/[\x09\x20]+/) { |s|
+			#	len = rand(50)+2
+			#	set = "\x09\x20"
+			#	buf = ''
+			#	while (buf.length < len)
+			#		buf << set[rand(set.length)].chr
+			#	end
+			#	
+			#	buf
+			#}
 		end
 		# Globally replace symbols
 		replace_symbols(@opts['Symbols']) if @opts['Symbols']
@@ -214,34 +214,120 @@ protected
 	# There are a couple of caveats to using string obfuscation:
 	#   * it tries to deal with escaped quotes within strings but won't catch
 	#     things like: "\\"
-	#   * multiple calls to this method are very likely to result in incorrect
-	#     code.  DON'T CALL THIS METHOD MORE THAN ONCE
+	#   * depending on the random choices, this can easily balloon a short
+	#     string up to hundreds of kilobytes if called multiple times.
 	# so be careful.
 	#
 	def obfuscate_strings()
 		@js.gsub!(/".*?[^\\]"|'.*?[^\\]'/) { |str|
+			buf = ''
+			quote = str[0,1]
+			# Pull the quotes off either end
 			str = str[1, str.length-2]
-			case (rand(3))
+			case (rand(2))
+			# Disable hex encoding for now.  It's just too big a hassle.
+			#when 0
+			#	# This is where we can run into trouble with generating
+			#	# incorrect code.  If we hex encode a string twice, the second
+			#	# encoding will generate the first instead of the original
+			#	# string.
+			#	if str =~ /\\x/
+			#		# Always have to remove spaces from strings so the space
+			#		# randomization doesn't mess with them.
+			#		buf = quote + str.gsub(/ /, '\x20') + quote
+			#	else
+			#		buf = '"' + Rex::Text.to_hex(str) + '"'
+			#	end
 			when 0
-				buf = '"' + Rex::Text.to_hex(str) + '"'
-			when 1
-				buf = "unescape(\"" + Rex::Text.to_hex(str, "%") + "\")" 
-			when 2
-				buf = "String.fromCharCode(" 
-				str.each_byte { |c| 
-					if (0 == rand(2))
-						buf << " %i,"%(c)
-					else
-						buf << " 0x%0.2x,"%(c) 
+				#
+				# Escape sequences when naively encoded for unescape become a
+				# literal backslash instead of the intended meaning.  To avoid
+				# that problem, we scan the string for escapes and leave them
+				# unmolested.
+				#
+				buf << 'unescape("'
+				bytes = str.unpack("C*")
+				c = 0
+				while bytes[c]
+					if bytes[c].chr == "\\"
+						# XXX This is pretty slow.
+						esc_len = parse_escape(bytes, c)
+						buf << bytes[c, esc_len].map{|a| a.chr}.join
+						c += esc_len
+						next
 					end
-				}
-				buf = buf[0,buf.length-1] + " )" 
+					buf << "%%%0.2x"%(bytes[c])
+					# Break the string into smaller strings
+					if bytes[c+1] and rand(10) == 0
+						buf << '" + "'
+					end
+					c += 1
+				end
+				buf << '")'
+			when 1
+				buf = "String.fromCharCode( "
+				bytes = str.unpack("C*")
+				c = 0
+				while bytes[c]
+					if bytes[c].chr == "\\"
+						case bytes[c+1].chr
+						# For chars that contain their non-escaped selves, step
+						# past the backslash and let the rand() below decide
+						# how to represent the character.
+						when '"'; c += 1
+						when "'"; c += 1
+						when "\\"; c += 1
+						# For others, just take the hex representation out of
+						# laziness.
+						when "n"; buf << "0x0a"; c += 2; next
+						when "t"; buf << "0x09"; c += 2; next
+						# Lastly, if it's a hex, unicode, or octal escape,
+						# leave it, and anything after it, alone.  At some
+						# point we may want to parse up to the end of the
+						# escapes and encode subsequent non-escape characters.
+						# Since this is the lazy way to do it, spaces after an
+						# escape sequence will get away unmodified.  To prevent
+						# the space randomizer from hosing the string, convert
+						# spaces specifically.
+						else
+							buf = buf[0,buf.length-1] + " )"
+							buf << ' + ("' + bytes[c, bytes.length].map{|a| a==0x20 ? '\x20' : a.chr}.join + '" '
+							break
+						end
+					end
+					case (rand(3))
+					when 0
+						buf << " %i,"%(bytes[c])
+					when 1
+						buf << " 0%o,"%(bytes[c])
+					when 2
+						buf << " 0x%0.2x,"%(bytes[c])
+					end
+					c += 1
+				end
+				# Strip off the last comma
+				buf = buf[0,buf.length-1] + " )"
 			end
 			buf
 		}
 		@js
 	end
 
+	def parse_escape(bytes, offset)
+		esc_len = 0
+		if bytes[offset].chr == "\\"
+			case bytes[offset+1].chr
+			when "u"; esc_len = 6     # unicode \u1234
+			when "x"; esc_len = 4     # hex, \x41
+			when /[0-9]/              # octal, \123, \0
+				oct = bytes[offset+1, 4].map{|a|a.chr}.join
+				oct =~ /([0-9]+)/
+				esc_len = 1 + $1.length
+			else; esc_len = 2         # \" \n, etc.
+			end
+		end
+		esc_len
+	end
 end
 
 end
