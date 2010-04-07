4e239925880f481d8c886045de60185e63d292cf
Joshua Drake <github.jdrake@qoop.org>
2010-04-07 16:49:48 +0000

    move to more indirect method for defining syscall sets
    git-svn-id: file:///home/svn/framework3/trunk@9035 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/msf/core/payload/aix.rb
+++ lib/msf/core/payload/aix.rb
@@ -47,104 +47,128 @@ module Msf::Payload::Aix
 			raise RuntimeError, 'AIX version is not set!'
 		end
 
-		__CAL = 511
+		#
+		# NOTE:
+		# 
+		# To add a syscall set, add a aix_XXXX_syscalls hash as seen below,
+		# and add a line to the versions hash using that version.
+		#
 
-		# ATTENTION: Don't forget to update this string if you add a specific
-		# syscall set for a specific version of AIX.
-		supported = "6.1.4, 6.1.3, 6.1.2, 6.1.1, 6.1.0, 5.3.10, 5.3.9, 5.3.8, 5.3.7, 5.1"
+		aix_614_syscalls = {
+			:__NR_execve      => 7,
+			:__NR_getpeername => 211,
+			:__NR_accept      => 237,
+			:__NR_listen      => 240,
+			:__NR_bind        => 242,
+			:__NR_socket      => 243,
+			:__NR_connect     => 244,
+			:__NR_close       => 278,
+			:__NR_kfcntl      => 658
+		}
+		
+		aix_613_syscalls = {
+			:__NR_execve      => 7,
+			:__NR_getpeername => 205,
+			:__NR_accept      => 232,
+			:__NR_listen      => 235,
+			:__NR_bind        => 237,
+			:__NR_socket      => 238,
+			:__NR_connect     => 239,
+			:__NR_close       => 272,
+			:__NR_kfcntl      => 644
+		}
+		
+		aix_612_syscalls = {
+			:__NR_execve      => 7,
+			:__NR_getpeername => 205,
+			:__NR_accept      => 232,
+			:__NR_listen      => 235,
+			:__NR_bind        => 237,
+			:__NR_socket      => 238,
+			:__NR_connect     => 239,
+			:__NR_close       => 272,
+			:__NR_kfcntl      => 635
+		}
+		
+		aix_611_syscalls = {
+			:__NR_execve      => 7,
+			:__NR_getpeername => 202,
+			:__NR_accept      => 229,
+			:__NR_listen      => 232,
+			:__NR_bind        => 234,
+			:__NR_socket      => 235,
+			:__NR_connect     => 236,
+			:__NR_close       => 269,
+			:__NR_kfcntl      => 614
+		}
+		
+		aix_610_syscalls = {
+			:__NR_execve      => 6,
+			:__NR_getpeername => 203,
+			:__NR_accept      => 229,
+			:__NR_listen      => 232,
+			:__NR_bind        => 234,
+			:__NR_socket      => 235,
+			:__NR_connect     => 236,
+			:__NR_close       => 269,
+			:__NR_kfcntl      => 617
+		}
+		
+		aix_53x_syscalls = {
+			:__NR_execve      => 6,
+			:__NR_getpeername => 198,
+			:__NR_accept      => 214,
+			:__NR_listen      => 215,
+			:__NR_bind        => 216,
+			:__NR_socket      => 217,
+			:__NR_connect     => 218,
+			:__NR_close       => 245,
+			:__NR_kfcntl      => 493
+		}
+		
+		aix_51_syscalls = {
+			:__NR_execve      => 5,
+			:__NR_getpeername => 122,
+			:__NR_accept      => 138,
+			:__NR_listen      => 139,
+			:__NR_bind        => 140,
+			:__NR_socket      => 141,
+			:__NR_connect     => 142,
+			:__NR_close       => 160,
+			:__NR_kfcntl      => 322
+		}
 
-		case @aix
-		when '6.1.4'
-			__NR_execve      = 7
-			__NR_getpeername = 211
-			__NR_accept      = 237
-			__NR_listen      = 240
-			__NR_bind        = 242
-			__NR_socket      = 243
-			__NR_connect     = 244
-			__NR_close       = 278
-			__NR_kfcntl      = 658
+		versions = {
+			'6.1.4'  => aix_614_syscalls,
+			'6.1.3'  => aix_613_syscalls,
+			'6.1.2'  => aix_612_syscalls,
+			'6.1.1'  => aix_611_syscalls,
+			'6.1.0'  => aix_610_syscalls,
+			'5.3.10' => aix_53x_syscalls,
+			'5.3.9'  => aix_53x_syscalls,
+			'5.3.8'  => aix_53x_syscalls,
+			'5.3.7'  => aix_53x_syscalls,
+			'5.1'    => aix_51_syscalls
+		}
 
-		when '6.1.3'
-			__NR_execve      = 7
-			__NR_getpeername = 205
-			__NR_accept      = 232
-			__NR_listen      = 235
-			__NR_bind        = 237
-			__NR_socket      = 238
-			__NR_connect     = 239
-			__NR_close       = 272
-			__NR_kfcntl      = 644
-
-		when '6.1.2'
-			__NR_execve      = 7
-			__NR_getpeername = 205
-			__NR_accept      = 232
-			__NR_listen      = 235
-			__NR_bind        = 237
-			__NR_socket      = 238
-			__NR_connect     = 239
-			__NR_close       = 272
-			__NR_kfcntl      = 635
-
-		when '6.1.1'
-			__NR_execve      = 7
-			__NR_getpeername = 202
-			__NR_accept      = 229
-			__NR_listen      = 232
-			__NR_bind        = 234
-			__NR_socket      = 235
-			__NR_connect     = 236
-			__NR_close       = 269
-			__NR_kfcntl      = 614
-
-		when '6.1.0'
-			__NR_execve      = 6
-			__NR_getpeername = 203
-			__NR_accept      = 229
-			__NR_listen      = 232
-			__NR_bind        = 234
-			__NR_socket      = 235
-			__NR_connect     = 236
-			__NR_close       = 269
-			__NR_kfcntl      = 617
-
-		when '5.3.10', '5.3.9', '5.3.8', '5.3.7'
-			__NR_execve      = 6
-			__NR_getpeername = 198
-			__NR_accept      = 214
-			__NR_listen      = 215
-			__NR_bind        = 216
-			__NR_socket      = 217
-			__NR_connect     = 218
-			__NR_close       = 245
-			__NR_kfcntl      = 493
-
-		when '5.1'
-			__NR_execve      = 5
-			__NR_getpeername = 122
-			__NR_accept      = 138
-			__NR_listen      = 139
-			__NR_bind        = 140
-			__NR_socket      = 141
-			__NR_connect     = 142
-			__NR_close       = 160
-			__NR_kfcntl      = 322
-
+		if (not versions[@aix])
+			# Dynamically build the support version array :)
+			supported = versions.sort.reverse.map { |k,v| k.to_s }.join(', ')
+			raise RuntimeError, "Invalid AIX version: \"#{@aix}\".  Supported versions: #{supported}"
 		else
-			raise RuntimeError, "Invalid AIX version: #{@aix}.  Supported versions: #{supported}"
-
+			syscalls = versions[@aix]
 		end
 
-		__NC_execve      = -(__CAL - __NR_execve)
-		__NC_getpeername = -(__CAL - __NR_getpeername)
-		__NC_accept      = -(__CAL - __NR_accept)
-		__NC_listen      = -(__CAL - __NR_listen)
-		__NC_bind        = -(__CAL - __NR_bind)
-		__NC_socket      = -(__CAL - __NR_socket)
-		__NC_connect     = -(__CAL - __NR_connect)
-		__NC_close       = -(__CAL - __NR_close)
-		__NC_kfcntl      = -(__CAL - __NR_kfcntl)
+		__CAL = 511
+		__NC_execve      = -(__CAL - syscalls[:__NR_execve])
+		__NC_getpeername = -(__CAL - syscalls[:__NR_getpeername])
+		__NC_accept      = -(__CAL - syscalls[:__NR_accept])
+		__NC_listen      = -(__CAL - syscalls[:__NR_listen])
+		__NC_bind        = -(__CAL - syscalls[:__NR_bind])
+		__NC_socket      = -(__CAL - syscalls[:__NR_socket])
+		__NC_connect     = -(__CAL - syscalls[:__NR_connect])
+		__NC_close       = -(__CAL - syscalls[:__NR_close])
+		__NC_kfcntl      = -(__CAL - syscalls[:__NR_kfcntl])
 
 		cal = "\x38\x5d"
 		@cal_execve      = cal + [__NC_execve].pack('n')
