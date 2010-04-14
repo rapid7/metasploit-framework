321404e2feb835c691bde3cd61002ccf82860ef8
Joshua Drake <github.jdrake@qoop.org>
2010-04-14 07:40:04 +0000

    add payload/generic/tight_loop - x86 debug payload
    git-svn-id: file:///home/svn/framework3/trunk@9070 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/payloads/singles/generic/tight_loop.rb
@@ -0,0 +1,35 @@
+##
+# $Id$
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to 
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/framework/
+##
+
+require 'msf/core'
+require 'msf/core/payload/generic'
+
+module Metasploit3
+
+	include Msf::Payload::Single
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'          => 'Generic x86 Tight Loop',
+			'Version'       => '$Revision$',
+			'Description'   => 'Generate a tight loop in the target process',
+			'Author'        => 'jduck',
+			'Platform'	    => [ 'win', 'linux', 'bsd', 'solaris', 'bsdi', 'osx' ],
+			'License'       => MSF_LICENSE,
+			'Arch'		    => ARCH_X86,
+			'Payload'	    =>
+				{
+					'Payload' => "\xeb\xfe" # jump to self
+				}
+			))
+	end
+
+end
