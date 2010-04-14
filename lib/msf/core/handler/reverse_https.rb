914a90a71447f8b457ca157838d3e379381f14b3
HD Moore <hd_moore@rapid7.com>
2010-04-14 02:46:43 +0000

    Fix the print lins
    git-svn-id: file:///home/svn/framework3/trunk@9068 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/msf/core/handler/reverse_https.rb
+++ lib/msf/core/handler/reverse_https.rb
@@ -64,9 +64,9 @@ module ReverseHttps
 			},
 			'VirtualDirectory' => true)
 
-		dlog("Reverse HTTPS listener started on http://#{datastore['LHOST']}:#{datastore['LPORT']}/", 'core', LEV_2)
+		dlog("Reverse HTTPS listener started on https://#{datastore['LHOST']}:#{datastore['LPORT']}/", 'core', LEV_2)
 
-		print_status("HTTPS listener started on http://#{datastore['LHOST']}:#{datastore['LPORT']}/")
+		print_status("HTTPS listener started on https://#{datastore['LHOST']}:#{datastore['LPORT']}/")
 	end
 
 	#
