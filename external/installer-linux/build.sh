3a928fc9c2be2b9089ecb63e5239f8997395ee4a
James Lee <James_Lee@rapid7.com>
2010-05-17 21:14:10 +0000

    this is what i really meant.
    git-svn-id: file:///home/svn/framework3/trunk@9318 4d416f70-5f16-0410-b530-b9f4589650da

--- external/installer-linux/build.sh
+++ external/installer-linux/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 VERSION="3.4.0"
+BASE=`dirname $0`
 
 # Grab a fresh copy of Metasploit
 if [ -f "tmp/msf3/msfconsole" ]; then
@@ -13,11 +14,15 @@ fi
 
 NAME32="Metasploit Framework v${VERSION} Installer (32-bit)"
 PATH32="framework-${VERSION}-linux-i686.run"
+BINPATH32="${BASE}/bin/linux32.tar.bz2"
+ls -l ${BINPATH32}
 
 NAME64="Metasploit Framework v${VERSION} Installer (64-bit)"
 PATH64="framework-${VERSION}-linux-x86_64.run"
+BINPATH64="${BASE}/bin/linux64.tar.bz2"
+ls -l ${BINPATH64}
 
-if [ -f "${PATH32}" ]; then
+if [ -f "${BINPATH32}" ]; then
     rm -rf tmp32
     mkdir tmp32
     cp tmp/msf3.tar tmp32/
@@ -31,7 +36,7 @@ if [ -f "${PATH32}" ]; then
     rm -rf $TMP32
 fi
 
-if [ -f "${PATH64}" ]; then
+if [ -f "${BINPATH64}" ]; then
     rm -rf tmp64
     mkdir tmp64
     cp tmp/msf3.tar tmp64/
