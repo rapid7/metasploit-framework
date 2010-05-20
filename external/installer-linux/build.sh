a89db8351387d63c12bff94bb18ce4f3e3304f84
James Lee <James_Lee@rapid7.com>
2010-05-20 20:39:55 +0000

    use the BINPATH
    git-svn-id: file:///home/svn/framework3/trunk@9329 4d416f70-5f16-0410-b530-b9f4589650da

--- external/installer-linux/build.sh
+++ external/installer-linux/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION="3.4.0"
+VERSION="3.4.0-release"
 BASE=`dirname $0`
 
 # Grab a fresh copy of Metasploit
@@ -15,18 +15,16 @@ fi
 NAME32="Metasploit Framework v${VERSION} Installer (32-bit)"
 PATH32="framework-${VERSION}-linux-i686.run"
 BINPATH32="${BASE}/bin/linux32.tar.bz2"
-ls -l ${BINPATH32}
 
 NAME64="Metasploit Framework v${VERSION} Installer (64-bit)"
 PATH64="framework-${VERSION}-linux-x86_64.run"
 BINPATH64="${BASE}/bin/linux64.tar.bz2"
-ls -l ${BINPATH64}
 
 if [ -f "${BINPATH32}" ]; then
     rm -rf tmp32
     mkdir tmp32
     cp tmp/msf3.tar tmp32/
-    cp bin/linux32.tar.bz2 tmp32/metasploit.tar.bz2
+    cp ${BINPATH32} tmp32/metasploit.tar.bz2
     bunzip2 tmp32/metasploit.tar.bz2
     cp -a scripts/*.sh tmp32/
     cp -a scripts/msfupdate tmp32/
@@ -40,7 +38,7 @@ if [ -f "${BINPATH64}" ]; then
     rm -rf tmp64
     mkdir tmp64
     cp tmp/msf3.tar tmp64/
-    cp bin/linux64.tar.bz2 tmp64/metasploit.tar.bz2
+    cp ${BINPATH64} tmp64/metasploit.tar.bz2
     bunzip2 tmp64/metasploit.tar.bz2
     cp -a scripts/*.sh tmp64/
     cp -a scripts/msfupdate tmp64/
