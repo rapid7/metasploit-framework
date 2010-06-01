2bd20752d606c3fd47585d2bd50791b27a4e0fa2
Jonathan Cran <jcran@rapid7.com>
2010-06-01 19:28:03 +0000

    editor plugin -  see: http://blog.metasploit.com/2010/05/its-ruby-not-turtles-all-way-down.html
    git-svn-id: file:///home/svn/framework3/trunk@9389 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ plugins/editor.rb
@@ -0,0 +1,82 @@
+module Msf
+
+###
+# 
+# This plugin is a simple editor command, designed to make it easy to edit modules in the console.
+#
+###
+class Plugin::Sample < Msf::Plugin
+
+	###
+	#
+	# This class implements a single edit command.
+	#
+	###
+	class ConsoleCommandDispatcher
+		include Msf::Ui::Console::CommandDispatcher
+
+		#
+		# The dispatcher's name.
+		#
+		def name
+			"Editor"
+		end
+
+		#
+		# Returns the hash of commands supported by this dispatcher.
+		#
+		def commands
+			{
+				"edit" => "A handy editor commmand"
+			}
+		end
+
+		#
+		# This method handles the sample command.
+		#
+		def cmd_edit(*args)
+			print_line ("Launching editor...")
+			
+			e = Rex::Compat.getenv("EDITOR") || "vi"
+
+			if (not active_module) or (not (path = active_module.file_path))
+				$stderr.puts "no active module selected"
+				return nil
+			end
+		
+			system(e + " " + path)
+		end
+	end
+
+	#
+	def initialize(framework, opts)
+		super
+
+		# console dispatcher commands.
+		add_console_dispatcher(ConsoleCommandDispatcher)
+
+		print_status("Editor plugin loaded.")
+	end
+
+	#
+	def cleanup
+		# If we had previously registered a console dispatcher with the console,
+		# deregister it now.
+		remove_console_dispatcher('Editor') 
+	end
+
+	#
+	#
+	def name
+		"editor"
+	end
+
+	#
+	def desc
+		"Simple Editor Plugin"
+	end
+
+protected
+end
+
+end
