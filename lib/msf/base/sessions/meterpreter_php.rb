055ec7d768e1c82fc1e6261b2b9bb41a20f0e3e9
James Lee <James_Lee@rapid7.com>
2010-06-01 17:01:42 +0000

    session handler stub for php meterpreter
    git-svn-id: file:///home/svn/framework3/trunk@9387 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ lib/msf/base/sessions/meterpreter_php.rb
@@ -0,0 +1,24 @@
+require 'msf/base/sessions/meterpreter'
+
+module Msf
+module Sessions
+
+###
+#
+# This class creates a platform-specific meterpreter session type
+#
+###
+class Meterpreter_Php_Php < Msf::Sessions::Meterpreter
+	def supports_ssl?
+		false
+	end
+	def initialize(rstream, opts={})
+		super
+		self.platform      = 'php/php'
+		self.binary_suffix = 'php'
+	end
+end
+
+end
+end
+
