771deac36056449663f8ff5047f910a7fa6d6bf0
HD Moore <hd_moore@rapid7.com>
2010-07-01 19:57:03 +0000

    Adds domain bypass support from Rudy Ruiz
    git-svn-id: file:///home/svn/framework3/trunk@9644 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/auxiliary/server/fakedns.rb
+++ modules/auxiliary/server/fakedns.rb
@@ -45,6 +45,7 @@ class Metasploit3 < Msf::Auxiliary
 				OptAddress.new('SRVHOST',   [ true, "The local host to listen on.", '0.0.0.0' ]),
 				OptPort.new('SRVPORT',      [ true, "The local port to listen on.", 53 ]),
 				OptAddress.new('TARGETHOST', [ false, "The address that all names should resolve to", nil ]),
+				OptAddress.new('DOMAINBYPASS', [ true, "The list of domain names we want to fully resolve", 'www.google.com']),
 
 			], self.class)
 
@@ -79,73 +80,88 @@ class Metasploit3 < Msf::Auxiliary
 			@log_database = true
 		end
 
-        # MacOS X workaround
-        ::Socket.do_not_reverse_lookup = true
+		# MacOS X workaround
+		::Socket.do_not_reverse_lookup = true
 
 		print_status("DNS server initializing")
-        @sock = ::UDPSocket.new()
-        @sock.setsockopt(::Socket::SOL_SOCKET, ::Socket::SO_REUSEADDR, 1)
-        @sock.bind(datastore['SRVHOST'], @port)
-        @run = true
+		@sock = ::UDPSocket.new()
+		@sock.setsockopt(::Socket::SOL_SOCKET, ::Socket::SO_REUSEADDR, 1)
+		@sock.bind(datastore['SRVHOST'], @port)
+		@run = true
+		@domain_bypass_list = datastore['DOMAINBYPASS'].split
 
 		print_status("DNS server started")
 		begin
 
-        while @run
-            packet, addr = @sock.recvfrom(65535)
-            if (packet.length == 0)
-                break
-            end
-            request = Resolv::DNS::Message.decode(packet)
+		while @run
+			packet, addr = @sock.recvfrom(65535)
+			break if packet.length == 0
 
-            #
-            # XXX: Track request IDs by requesting IP address and port
-            #
-            # Windows XP SP1a: UDP source port constant,
-            #  sequential IDs since boot time
-            # Windows XP SP2: Randomized IDs
-            #
-            # Debian 3.1: Static source port (32906) until timeout,
-            #  randomized IDs
-            #
+			request = Resolv::DNS::Message.decode(packet)
+
+			#
+			# XXX: Track request IDs by requesting IP address and port
+			#
+			# Windows XP SP1a: UDP source port constant,
+			#  sequential IDs since boot time
+			# Windows XP SP2: Randomized IDs
+			#
+			# Debian 3.1: Static source port (32906) until timeout,
+			#  randomized IDs
+			#
 
 			lst = []
 
-            request.each_question {|name, typeclass|
-                tc_s = typeclass.to_s().gsub(/^Resolv::DNS::Resource::/, "")
+			request.each_question {|name, typeclass|
+				tc_s = typeclass.to_s().gsub(/^Resolv::DNS::Resource::/, "")
 
 				request.qr = 1
 				request.ra = 1
 
-                lst << "#{tc_s} #{name}"
+				lst << "#{tc_s} #{name}"
 				case tc_s
 				when 'IN::A'
 
-                    # Special fingerprinting name lookups:
-                    #
-                    # _isatap -> XP SP = 0
-                    # isatap.localdomain -> XP SP >= 1
-                    # teredo.ipv6.microsoft.com -> XP SP >= 2
-                    #
-                    # time.windows.com -> windows ???
-                    # wpad.localdomain -> windows ???
-                    #
-                    # <hostname> SOA -> windows XP self hostname lookup
-                    #
+					# Special fingerprinting name lookups:
+					#
+					# _isatap -> XP SP = 0
+					# isatap.localdomain -> XP SP >= 1
+					# teredo.ipv6.microsoft.com -> XP SP >= 2
+					#
+					# time.windows.com -> windows ???
+					# wpad.localdomain -> windows ???
+					#
+					# <hostname> SOA -> windows XP self hostname lookup
+					#
 
-                    answer = Resolv::DNS::Resource::IN::A.new( @targ || ::Rex::Socket.source_address(addr[3].to_s) )
-                    request.add_answer(name, 60, answer)
+					answer = Resolv::DNS::Resource::IN::A.new( @targ || ::Rex::Socket.source_address(addr[3].to_s) )
+
+					# Identify potential domain exceptions
+					@domain_bypass_list.each do |ex|
+						if (name.to_s <=> ex) == 0
+							# Resolve the exception domain
+							ip = Resolv::DNS.new().getaddress(name).to_s
+							answer = Resolv::DNS::Resource::IN::A.new( ip )
+							if (@log_console)
+							print_status("DNS bypass domain found: #{ex}")
+							print_status("DNS bypass domain #{ex} resolved #{ip}")
+							end
+						end
+					end
+
+
+					request.add_answer(name, 60, answer)
 
 				when 'IN::MX'
-                    mx = Resolv::DNS::Resource::IN::MX.new(10, Resolv::DNS::Name.create("mail.#{name}"))
-                    ns = Resolv::DNS::Resource::IN::NS.new(Resolv::DNS::Name.create("dns.#{name}"))
+					mx = Resolv::DNS::Resource::IN::MX.new(10, Resolv::DNS::Name.create("mail.#{name}"))
+					ns = Resolv::DNS::Resource::IN::NS.new(Resolv::DNS::Name.create("dns.#{name}"))
 					ar = Resolv::DNS::Resource::IN::A.new( @targ || ::Rex::Socket.source_address(addr[3].to_s) )
 					request.add_answer(name, 60, mx)
 					request.add_authority(name, 60, ns)
 					request.add_additional(Resolv::DNS::Name.create("mail.#{name}"), 60, ar)
 
 				when 'IN::NS'
-                    ns = Resolv::DNS::Resource::IN::NS.new(Resolv::DNS::Name.create("dns.#{name}"))
+					ns = Resolv::DNS::Resource::IN::NS.new(Resolv::DNS::Name.create("dns.#{name}"))
 					ar = Resolv::DNS::Resource::IN::A.new( @targ || ::Rex::Socket.source_address(addr[3].to_s) )
 					request.add_answer(name, 60, ns)
 					request.add_additional(name, 60, ar)
@@ -168,10 +184,10 @@ class Metasploit3 < Msf::Auxiliary
 				else
 					lst << "UNKNOWN #{tc_s}"
 				end
-            }
+			}
 
 			if(@log_console)
-				print_status("DNS #{addr[3]}:#{addr[1]} XID #{request.id} (#{lst.join(", ")})")
+				  print_status("DNS #{addr[3]}:#{addr[1]} XID #{request.id} (#{lst.join(", ")})")
 			end
 
 			if(@log_database)
@@ -182,8 +198,9 @@ class Metasploit3 < Msf::Auxiliary
 				) if lst.length > 0
 			end
 
-            @sock.send(request.encode(), 0, addr[3], addr[1])
-        end
+
+			@sock.send(request.encode(), 0, addr[3], addr[1])
+		end
 
 		rescue ::Exception => e
 			print_error("fakedns: #{e.class} #{e} #{e.backtrace}")
