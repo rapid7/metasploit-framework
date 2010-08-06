d8643568c8e1e32f8db533c43b8bf924399f731e
Joshua Drake <github.jdrake@qoop.org>
2010-08-06 01:09:05 +0000

    fixes #2342
    git-svn-id: file:///home/svn/framework3/trunk@9960 4d416f70-5f16-0410-b530-b9f4589650da

--- scripts/meterpreter/multicommand.rb
+++ scripts/meterpreter/multicommand.rb
@@ -67,7 +67,7 @@ end
 @@exec_opts.parse(args) { |opt, idx, val|
         case opt
 
-        when "-c"
+        when "-cl"
                 commands = val.split(",")
         when "-rc"
                 script = val
