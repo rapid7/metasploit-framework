b35cea94cd0d4094664f9c107956b57f1f64d194
James Lee <James_Lee@rapid7.com>
2010-08-21 07:27:26 +0000

    add source code for cve-2010-0840
    git-svn-id: file:///home/svn/framework3/trunk@10095 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ external/source/exploits/CVE-2010-0840/vuln/Test.java
@@ -0,0 +1,14 @@
+package vuln;
+
+/*
+ * This is just a placeholder class.  Link implements this so it can be
+ * replaced after compilation with Map.Entry since a normal compiler will not
+ * allow creating a class that implements an interface through inheritance.
+ *
+ * See Sami Koivu's original blog post for details.
+ */
+public interface Test {
+	
+	public Object getValue() throws Exception;
+
+}
