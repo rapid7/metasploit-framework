2b891db0e18dea700b4df8b96070fcc31cf6053d
Mike Smith <Mike_Smith@rapid7.com>
2010-09-17 16:12:27 +0000

    Use the fully-qualified name of the User class.  Also, use a bulk UPDATE instead of one UPDATE per record.
    git-svn-id: file:///home/svn/framework3/trunk@10348 4d416f70-5f16-0410-b530-b9f4589650da

--- data/sql/migrate/20100916151530_require_admin_flag.rb
+++ data/sql/migrate/20100916151530_require_admin_flag.rb
@@ -3,7 +3,7 @@ class RequireAdminFlag < ActiveRecord::Migration
 	# Make the admin flag required.
 	def self.up
 		# update any existing records
-		User.find_each { |u| u.admin = true if u.admin.nil? }
+		Msf::DBManager::User.update_all({:admin => true}, {:admin => nil})
 
 		change_column :users, :admin, :boolean, :null => false, :default => true
 	end
