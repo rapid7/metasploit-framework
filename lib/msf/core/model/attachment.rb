771fc82c8c4d83e3e8829e0db180b8120110a8f8
James Lee <James_Lee@rapid7.com>
2010-09-20 21:41:50 +0000

    add a unique cid to attachments
    git-svn-id: file:///home/svn/framework3/trunk@10399 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/msf/core/model/attachment.rb
+++ lib/msf/core/model/attachment.rb
@@ -3,6 +3,12 @@ class Msf::DBManager
 
 class Attachment < ActiveRecord::Base
 	has_and_belongs_to_many :email_template
+
+	# Generate a unique Content-ID
+	def cid
+		@cid ||= Rex::Text.to_hex(name + id.to_s, '')
+		@cid
+	end
 end
 
 end
