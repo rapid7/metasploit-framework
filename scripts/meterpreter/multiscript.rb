19d5b4cd6093248de8a8e6c755a597282d69f987
Joshua Drake <github.jdrake@qoop.org>
2010-10-08 18:35:43 +0000

    ignore comments/empty lines in rc
    git-svn-id: file:///home/svn/framework3/trunk@10604 4d416f70-5f16-0410-b530-b9f4589650da

--- scripts/meterpreter/multiscript.rb
+++ scripts/meterpreter/multiscript.rb
@@ -7,9 +7,9 @@ session = client
 # Setting Argument
 
 @@exec_opts = Rex::Parser::Arguments.new(
-        "-h" => [ false,"Help menu."                        ],
-        "-cl" => [ true,"Collection of scripts to execute. Each script command must be enclosed in double quotes and separated by a semicolon."],
-        "-rc" => [ true,"Text file with list of commands, one per line."]
+	"-h" => [ false,"Help menu."                        ],
+	"-cl" => [ true,"Collection of scripts to execute. Each script command must be enclosed in double quotes and separated by a semicolon."],
+	"-rc" => [ true,"Text file with list of commands, one per line."]
 )
 #Setting Argument variables
 commands = ""
@@ -21,17 +21,20 @@ help = 0
 def script_exec(session,scrptlst)
 	print_status("Running script List ...")
 	scrptlst.each_line do |scrpt|
+		next if scrpt.strip.length < 1
+		next if scrpt[0,1] == "#"
+
 		begin
 			script_components = scrpt.split
 			script = script_components.shift
 			script_args = script_components
 			print_status "\trunning script #{scrpt.chomp}"
 			session.execute_script(script, script_args)
-                rescue ::Exception => e
-                        print_error("Error: #{e.class} #{e}")
-                        print_error("Error in script: #{scrpt}")
-                end
-        end
+		rescue ::Exception => e
+			print_error("Error: #{e.class} #{e}")
+			print_error("Error in script: #{scrpt}")
+		end
+	end
 end
 
 def usage
@@ -50,7 +53,7 @@ end
 		if not ::File.exists?(script)
 			raise "Script List File does not exists!"
 		else
-			::File.open(script, "r").each_line do |line|
+			::File.open(script, "rb").each_line do |line|
 				commands << line
 			end
 		end
@@ -58,6 +61,7 @@ end
 		help = 1
 	end
 end
+
 
 if args.length == 0 or help == 1
 	usage
