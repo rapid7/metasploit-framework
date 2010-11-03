aa02905564d92f62ef1c52e7c8400eca5f9407db
Carlos Perez <carlos_perez@darkoperator.com>
2010-11-03 04:39:43 +0000

    Speeded up application list enumeration in winenum
    git-svn-id: file:///home/svn/framework3/trunk@10879 4d416f70-5f16-0410-b530-b9f4589650da

--- scripts/meterpreter/winenum.rb
+++ scripts/meterpreter/winenum.rb
@@ -145,16 +145,25 @@ nowin2kexe = [
 def findprogs()
 	print_status("Extracting software list from registry")
 	proglist = ""
-	a =[]
+	threadnum = 0
+	a = []
 	appkeys = ['HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall',
 		'HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall' ]
 	appkeys.each do |keyx86|
 		registry_enumkeys(keyx86).each do |k|
-			begin
-				dispnm = registry_getvaldata("#{keyx86}\\#{k}","DisplayName")
-				dispversion = registry_getvaldata("#{keyx86}\\#{k}","DisplayVersion")
-				proglist << "#{dispnm},#{dispversion}"
-			rescue
+			if threadnum < 10
+				a.push(::Thread.new {
+						begin
+							dispnm = registry_getvaldata("#{keyx86}\\#{k}","DisplayName")
+							dispversion = registry_getvaldata("#{keyx86}\\#{k}","DisplayVersion")
+							proglist << "#{dispnm},#{dispversion}"
+						rescue
+						end
+					})
+				threadnum += 1
+			else
+				sleep(0.05) and a.delete_if {|x| not x.alive?} while not a.empty?
+				threadnum = 0
 			end
 		end
 	end
