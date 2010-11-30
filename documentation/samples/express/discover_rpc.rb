9f73b7f28c1c066b0f356a1fbbb533574b3ee6a8
Jonathan Cran <jcran@rapid7.com>
2010-11-30 21:49:27 +0000

    added a nexpose rpc sample & update the discover sample for 3.5.0
    git-svn-id: file:///home/svn/framework3/trunk@11181 4d416f70-5f16-0410-b530-b9f4589650da

--- documentation/samples/express/discover_rpc.rb
+++ documentation/samples/express/discover_rpc.rb
@@ -1,16 +1,19 @@
 #!/usr/bin/env ruby
 
-require '/opt/metasploit-3.4.1/apps/pro/engine/lib/pro/client'
+
+## Note, you may need to change this, depending on the install path of your 
+## metasploit instance
+require '/opt/metasploit-3.5.0/apps/pro/engine/lib/pro/client'
 
 pro = Pro::Client.new() ## this will connect to the rpc service running on localhost:50505 
 
-pro.call('db.add_workspace', "hackmiami") ## create a workspace
-pro.call('db.set_workspace', "hackmiami") ## set that workspace
+pro.call('db.add_workspace', "default") ## create a workspace
+pro.call('db.set_workspace', "default") ## set that workspace
 
 conf = {
-        'workspace' => "hackmiami",
+        'workspace' => "default",
         'username'  => "rpc",
-	"ips" => ['10.0.0.0/24'],
+	"ips" => ['10.0.0.0/24'], 
         'DS_BLACKLIST_HOSTS' =>  "10.0.0.1 10.0.0.2",
         'DS_PORTSCAN_SPEED' =>  "3",
         'DS_PORTS_EXTRA' =>  "",
@@ -24,7 +27,8 @@ conf = {
         'DS_SMBDomain' => "",
         'DS_DRY_RUN' =>  "false",
         'DS_SINGLE_SCAN' => "false",
-        'DS_FAST_DETECT' => "false"
+        'DS_FAST_DETECT' => "false",
+    	'DS_CustomNmap' => "--reason"
 }
 
 puts "starting discover task"
