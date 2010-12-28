0cb85bfd2fa5bf0822d35f4efb55c647b7a5d6cf
James Lee <James_Lee@rapid7.com>
2010-12-28 21:09:42 +0000

    make gendocs work when run from a location other than the base installation directory, thanks mgee eegm, fixes #3391
    git-svn-id: file:///home/svn/framework3/trunk@11437 4d416f70-5f16-0410-b530-b9f4589650da

--- documentation/gendocs.sh
+++ documentation/gendocs.sh
@@ -1,11 +1,15 @@
 OPTS="-x .ut.rb -x .ts.rb -q"
-BASE="documentation/api"
+BASE="$(dirname "$0")"
+MSFDIR="${BASE}/.."
+DOCDIR="${BASE}/api"
+
+echo "Putting docs in ${DOCDIR}"
 
 echo "Generating rex..."
-rdoc $OPTS -t "Rex Documentation" -o $BASE/rex lib/rex
+rdoc $OPTS -t "Rex Documentation" -o ${DOCDIR}/rex ${MSFDIR}/lib/rex
 echo "Generating msfcore"
-rdoc $OPTS -t "Framework Core Documentation" -o $BASE/msfcore lib/msf/core
+rdoc $OPTS -t "Framework Core Documentation" -o ${DOCDIR}/msfcore ${MSFDIR}/lib/msf/core
 echo "Generating msfbase"
-rdoc $OPTS -t "Framework Base Documentation" -o $BASE/msfbase lib/msf/base
+rdoc $OPTS -t "Framework Base Documentation" -o ${DOCDIR}/msfbase ${MSFDIR}/lib/msf/base
 echo "Generating msfui"
-rdoc $OPTS -t "Framework UI Documentation" -o $BASE/msfui lib/msf/ui
+rdoc $OPTS -t "Framework UI Documentation" -o ${DOCDIR}/msfui ${MSFDIR}/lib/msf/ui
