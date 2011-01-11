18ee35149a70ebc7411a1e128b7e16be8d49fd3c
James Lee <James_Lee@rapid7.com>
2011-01-11 17:53:24 +0000

    move script mixin code into Post mixins and include them back in for backwards compatibility
    git-svn-id: file:///home/svn/framework3/trunk@11544 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ lib/msf/core/post/file.rb
@@ -0,0 +1,101 @@
+
+module Msf
+class Post
+
+module File
+
+	#
+	# Writes a given string to a file specified
+	#
+	def file_local_write(file2wrt, data2wrt)
+		if not ::File.exists?(file2wrt)
+			::FileUtils.touch(file2wrt)
+		end
+
+		output = ::File.open(file2wrt, "a")
+		data2wrt.each_line do |d|
+			output.puts(d)
+		end
+		output.close
+	end
+
+	#
+	# Returns a MD5 checksum of a given local file
+	#
+	def file_local_digestmd5(file2md5)
+		if not ::File.exists?(file2md5)
+			raise "File #{file2md5} does not exists!"
+		else
+			require 'digest/md5'
+			chksum = nil
+			chksum = Digest::MD5.hexdigest(::File.open(file2md5, "rb") { |f| f.read})
+			return chksum
+		end
+	end
+
+	#
+	# Returns a SHA1 checksum of a given local file
+	#
+	def file_local_digestsha1(file2sha1)
+		if not ::File.exists?(file2sha1)
+			raise "File #{file2sha1} does not exists!"
+		else
+			require 'digest/sha1'
+			chksum = nil
+			chksum = Digest::SHA1.hexdigest(::File.open(file2sha1, "rb") { |f| f.read})
+			return chksum
+		end
+	end
+
+	#
+	# Returns a SHA256 checksum of a given local file
+	#
+	def file_local_digestsha2(file2sha2)
+		if not ::File.exists?(file2sha2)
+			raise "File #{file2sha2} does not exists!"
+		else
+			require 'digest/sha2'
+			chksum = nil
+			chksum = Digest::SHA256.hexdigest(::File.open(file2sha2, "rb") { |f| f.read})
+			return chksum
+		end
+	end
+
+	#
+	# Platform-agnostic file read.  Returns contents of remote file +file_name+
+	# as a String.
+	#
+	def read_file(file_name)
+		return false if file_name.length > 0
+
+		data = nil
+		if session.type == "meterpreter"
+			data = read_file_meterpreter(file_name)
+		elsif session.respond_to? :shell_command_token
+			data = session.shell_command_token("cat '#{file_name}'")
+		end
+		data
+	end
+
+protected
+	#
+	# Meterpreter-specific file read.  Returns contents of remote file
+	# +file_name+ as a String
+	#
+	def read_file_meterpreter(file_name)
+		fd = session.fs.file.new(file_name, "rb")
+		data = ''
+		begin
+			until fd.eof?
+				data << fd.read
+			end
+		ensure
+			fd.close
+		end
+		data
+	end
+
+end
+
+end
+end
--- lib/msf/scripts/meterpreter/registry.rb
+++ lib/msf/scripts/meterpreter/registry.rb
@@ -1,102 +1,11 @@
+require 'msf/core/post/registry'
+
 module Msf
 module Scripts
 module Meterpreter
 module Common
 
-#Returns the data and type of a given registry key and value
-def registry_getvalinfo(key,valname)
-	value = {}
-	begin
-		root_key, base_key = client.sys.registry.splitkey(key)
-		open_key = client.sys.registry.open_key(root_key, base_key, KEY_READ)
-		v = open_key.query_value(valname)
-		value["Data"] = v.data
-		value["Type"] = v.type
-		open_key.close
-	end
-	return value
-end
-#Returns the data of a given registry key and value
-def registry_getvaldata(key,valname)
-	value = nil
-	begin
-		root_key, base_key = client.sys.registry.splitkey(key)
-		open_key = client.sys.registry.open_key(root_key, base_key, KEY_READ)
-		v = open_key.query_value(valname)
-		value = v.data
-		open_key.close
-	end
-	return value
-end
-#Sets the data for a given value and type of data on the target registry returns true if succesful
-def registry_setvaldata(key,valname,data,type)
-	begin
-		root_key, base_key = client.sys.registry.splitkey(key)
-		open_key = client.sys.registry.open_key(root_key, base_key, KEY_WRITE)
-		open_key.set_value(valname, client.sys.registry.type2str(type), data)
-		open_key.close
-		return true
-	end
-end
-#Deletes a registry value given the key and value name returns true if succesful
-def registry_deleteval(key,valname)
-	begin
-		root_key, base_key = client.sys.registry.splitkey(key)
-		open_key = client.sys.registry.open_key(root_key, base_key, KEY_WRITE)
-		open_key.delete_value(valname)
-		open_key.close
-		return true
-	end
-end
-#Enumerates the values of a given registry key returns array of value names
-def registry_enumvals(key)
-	values = []
-	begin
-		vals = {}
-		root_key, base_key = client.sys.registry.splitkey(key)
-		open_key = client.sys.registry.open_key(root_key, base_key, KEY_READ)
-		vals = open_key.enum_value
-		vals.each { |val|
-			values <<  val.name
-		}
-		open_key.close
-	end
-	return values
-end
-#Enumerates the subkeys of a given registry key returns array of subkeys
-def registry_enumkeys(key)
-	subkeys = []
-	begin
-		root_key, base_key = client.sys.registry.splitkey(key)
-		open_key = client.sys.registry.open_key(root_key, base_key, KEY_READ)
-		keys = open_key.enum_key
-		keys.each { |subkey|
-			subkeys << subkey
-		}
-		open_key.close
-	end
-	return subkeys
-end
-#Create a given registry key returns true if succesful
-def registry_createkey(key)
-	begin
-		root_key, base_key = client.sys.registry.splitkey(key)
-		open_key = client.sys.registry.create_key(root_key, base_key)
-		open_key.close
-		return true
-	end
-
-end
-#Delete a given registry key returns true if succesful
-def registry_delkey(key)
-	begin
-		root_key, base_key = client.sys.registry.splitkey(key)
-		open_key = client.sys.registry.delete_key(root_key, base_key)
-		open_key.close
-		return true
-	end
-
-end
+include ::Msf::Post::Registry
 
 end
 end
