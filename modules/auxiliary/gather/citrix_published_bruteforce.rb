362c409623cdd68e07cadcbdfaec3425dad45206
Joshua Drake <github.jdrake@qoop.org>
2011-01-20 00:22:21 +0000

    Fixes #3554, add missing variable to published_applications (from published_bruteforce), some clean ups
    git-svn-id: file:///home/svn/framework3/trunk@11606 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/auxiliary/gather/citrix_published_bruteforce.rb
+++ modules/auxiliary/gather/citrix_published_bruteforce.rb
@@ -24,7 +24,7 @@ class Metasploit3 < Msf::Auxiliary
 				This module attempts to brute force program names within the Citrix
 				Metaframe ICA server.
 			},
-			'Author' 	 => [ 'patrick' ],
+			'Author'         => [ 'patrick' ],
 			'Version'        => '$Revision$',
 			'References'     =>
 				[
@@ -34,10 +34,10 @@ class Metasploit3 < Msf::Auxiliary
 				]
 		))
 
-			register_options(
-				[
-					Opt::RPORT(1604),
-				], self.class)
+		register_options(
+			[
+				Opt::RPORT(1604),
+			], self.class)
 	end
 
 	def autofilter
@@ -49,96 +49,108 @@ class Metasploit3 < Msf::Auxiliary
 
 		print_status("Attempting to contact Citrix ICA service...")
 
-		client = Rex::Text.rand_text_alphanumeric(8) # Client NetBIOS hostname. This works fine >:)
-		client_connect = "\x1e\x00\x01\x30\x02\xfd\xa8\xe3\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" # Server hello packet
-		server_response = "\x30\x00\x02\x31\x02\xfd\xa8\xe3\x02\x00\x06\x44" # Server hello response
+		# Client NetBIOS hostname. This works fine >:)
+		client = Rex::Text.rand_text_alphanumeric(8)
+
+		# Server hello packet
+		client_connect =
+			"\x1e\x00\x01\x30\x02\xfd\xa8\xe3\x00\x00\x00\x00\x00\x00\x00\x00" +
+			"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+
+		# Server hello response
+		server_response =
+			"\x30\x00\x02\x31\x02\xfd\xa8\xe3\x02\x00\x06\x44"
 
 		applications = [
-					'TEST',
-					'NOTEPAD',
-					'ACROBAT READER',
-					'ACROBAR',
-					'EXPLORER',
-					'WORD',
-					'WORD2K',
-					'WORDXP',
-					'WORD2K3',
-					'WORD2K7',
-					'WORD 2000',
-					'WORD XP',
-					'WORD 2003',
-					'WORD 2007',
-					'WORD2000',
-					'WORD2003',
-					'WORD2007',
-					'EXCEL',
-					'EXCEL2K',
-					'EXCELXP',
-					'EXCEL2K3',
-					'EXCEL2K7',
-					'EXCEL 2000',
-					'EXCEL XP',
-					'EXCEL 2003',
-					'EXCEL 2007',
-					'EXCEL2000',
-					'EXCEL2003',
-					'EXCEL2007',
-					'ACCESS',
-					'ACCESS2K',
-					'ACCESSXP',
-					'ACCESS2K3',
-					'ACCESS2K7',
-					'ACCESS 2000',
-					'ACCESS XP',
-					'ACCESS 2003',
-					'ACCESS 2007',
-					'ACCESS2000',
-					'ACCESS2003',
-					'ACCESS2007',
-					'POWERPOINT',
-					'POWERPOINT2K',
-					'POWERPOINTXP',
-					'POWERPOINT2K3',
-					'POWERPOINT2K7',
-					'POWERPOINT 2000',
-					'POWERPOINT XP',
-					'POWERPOINT 2003',
-					'POWERPOINT 2007',
-					'POWERPOINT2000',
-					'POWERPOINT2003',
-					'POWERPOINT2007',
-					'OUTLOOK',
-					'OUTLOOKXP',
-					'OUTLOOK2K',
-					'OUTLOOK2K3',
-					'OUTLOOK2K7',
-					'OUTLOOK 2000',
-					'OUTLOOK XP',
-					'OUTLOOK 2003',
-					'OUTLOOK 2007',
-					'OUTLOOK2000',
-					'OUTLOOK2003',
-					'OUTLOOK2007',
-					'LOTUS',
-					'LOTUS NOTES',
-					'INTERNETEXPLORER',
-					'IE',
-					'IEXPLORER',
-					'FIREFOX',
-					'FIREFOX 3',
-					'NETSCAPE',
-					'NETSCAPE7',
-					'NETSCAPE6',
-					'MAIL',
-					'EMAIL',
-					'E-MAIL',
-					'INTERNET',
-					'CMD',
-					'COMMAND',
-				]
+			'TEST',
+			'NOTEPAD',
+			'ACROBAT READER',
+			'ACROBAR',
+			'EXPLORER',
+			'WORD',
+			'WORD2K',
+			'WORDXP',
+			'WORD2K3',
+			'WORD2K7',
+			'WORD 2000',
+			'WORD XP',
+			'WORD 2003',
+			'WORD 2007',
+			'WORD2000',
+			'WORD2003',
+			'WORD2007',
+			'EXCEL',
+			'EXCEL2K',
+			'EXCELXP',
+			'EXCEL2K3',
+			'EXCEL2K7',
+			'EXCEL 2000',
+			'EXCEL XP',
+			'EXCEL 2003',
+			'EXCEL 2007',
+			'EXCEL2000',
+			'EXCEL2003',
+			'EXCEL2007',
+			'ACCESS',
+			'ACCESS2K',
+			'ACCESSXP',
+			'ACCESS2K3',
+			'ACCESS2K7',
+			'ACCESS 2000',
+			'ACCESS XP',
+			'ACCESS 2003',
+			'ACCESS 2007',
+			'ACCESS2000',
+			'ACCESS2003',
+			'ACCESS2007',
+			'POWERPOINT',
+			'POWERPOINT2K',
+			'POWERPOINTXP',
+			'POWERPOINT2K3',
+			'POWERPOINT2K7',
+			'POWERPOINT 2000',
+			'POWERPOINT XP',
+			'POWERPOINT 2003',
+			'POWERPOINT 2007',
+			'POWERPOINT2000',
+			'POWERPOINT2003',
+			'POWERPOINT2007',
+			'OUTLOOK',
+			'OUTLOOKXP',
+			'OUTLOOK2K',
+			'OUTLOOK2K3',
+			'OUTLOOK2K7',
+			'OUTLOOK 2000',
+			'OUTLOOK XP',
+			'OUTLOOK 2003',
+			'OUTLOOK 2007',
+			'OUTLOOK2000',
+			'OUTLOOK2003',
+			'OUTLOOK2007',
+			'LOTUS',
+			'LOTUS NOTES',
+			'INTERNETEXPLORER',
+			'IE',
+			'IEXPLORER',
+			'FIREFOX',
+			'FIREFOX 3',
+			'NETSCAPE',
+			'NETSCAPE7',
+			'NETSCAPE6',
+			'MAIL',
+			'EMAIL',
+			'E-MAIL',
+			'INTERNET',
+			'CMD',
+			'COMMAND',
+		]
 
-		application_valid = "\x3e\x00\x02\x35\x02\xfd\xa8\xe3\x02\x00\x06\x44" # Citrix is publishing this application
-		application_invalid = "\x20\x00\x01\x3a\x02\xfd\xa8\xe3\x02\x00\x06\x44" # Application not found / published
+		# Citrix is publishing this application
+		application_valid =
+			"\x3e\x00\x02\x35\x02\xfd\xa8\xe3\x02\x00\x06\x44"
+		# Application not found / published
+		application_invalid =
+			"\x20\x00\x01\x3a\x02\xfd\xa8\xe3\x02\x00\x06\x44"
 
 		udp_sock.put(client_connect)
 		res = udp_sock.get(3)
@@ -149,10 +161,15 @@ class Metasploit3 < Msf::Auxiliary
 			applications.each do |application|
 
 				# Create the packet
-				packet = "#{(52 + application.length).chr}\x00\x02\x34\x02\xfd\xa8\xe3\x00\x00\x00\x00\x00\x00\x00\x00"
+				packet = [52 + application.length].pack('C')
+				packet << "\x00\x02\x34\x02\xfd\xa8\xe3\x00\x00\x00\x00\x00\x00\x00\x00"
 				packet << "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x26\x00\x02\x00"
-				packet << "#{(39 + application.length).chr}\x00\x00\x00\x00\x00" + application + "\x00\x01\x00\x04\x00"
-				packet << client + "\x00"
+				packet << [39 + application.length].pack('C')
+				packet << "\x00\x00\x00\x00\x00"
+				packet << application
+				packet << "\x00\x01\x00\x04\x00"
+				packet << client
+				packet << "\x00"
 
 				udp_sock.put(packet)
 				res = udp_sock.get(3)
