160865eee67231691210b8d5ef1f501238f71823
Jonathan Cran <jcran@rapid7.com>
2011-02-09 10:23:13 +0000

    minor changes
    git-svn-id: file:///home/svn/framework3/trunk@11733 4d416f70-5f16-0410-b530-b9f4589650da

--- test/functional/framework/msfconsole_spec.rb
+++ test/functional/framework/msfconsole_spec.rb
@@ -79,10 +79,11 @@ describe "Msfconsole" do
 		
 		## This needs to be here for the actual test cases (and helpers)
 		## Note that the actual setup happens pre-rspec (see below)
-		@working_directory = "msfconsole_spec_working"
-		@output_directory = "#{@working_directory}/output"
-		@input_directory = "#{@working_directory}/input"
-		@default_file = "#{@output_directory}/default"
+		@working_directory = File.dirname(__FILE__)
+		@temp_directory = "#{@working_directory}/msfconsole_spec_working"
+		@temp_output_directory = "#{@temp_directory}/output"
+		@temp_input_directory = "#{@temp_directory}/input"
+		@default_file = "#{@temp_output_directory}/default"
 	end
 
 	before :each do
@@ -94,7 +95,7 @@ describe "Msfconsole" do
 
 	after :all do
 		## CLEANUP THE WORKING DIRECTORY	
-		FileUtils.rm_rf(@working_directory)
+		#FileUtils.rm_rf(@temp_directory)
 	end
 
 	###
@@ -108,7 +109,7 @@ describe "Msfconsole" do
 			failure_strings = [] | generic_failure_strings
 			failure_exception_strings = [] | generic_failure_exception_strings
 
-			data = hlp_run_command_check_output("help","msftest/resource/help.rc")
+			data = hlp_run_command_check_output("help","#{@working_directory}/msftest/resource/help.rc")
 			
 			data.should contain_all_successes(success_strings)
 			data.should contain_no_failures_except(failure_strings, failure_exception_strings)
@@ -119,34 +120,31 @@ describe "Msfconsole" do
 	###
 
 	## PRE_TEST WORKING DIR SETUP`
-	@working_directory = "msfconsole_spec_working"
-	if File.directory? @working_directory
-		FileUtils.rm_rf(@working_directory)
+	@working_directory= File.dirname(__FILE__)
+
+	@temp_directory = "#{@working_directory}/msfconsole_spec_working"
+	if File.directory? @temp_directory
+		FileUtils.rm_rf(@temp_directory)
 	end
-	Dir.mkdir(@working_directory)
+
+	Dir.mkdir(@temp_directory)
+	@temp_input_directory = "#{@temp_directory}/input"
+	Dir.mkdir(@temp_input_directory)
+	@temp_output_directory = "#{@temp_directory}/output"
+	Dir.mkdir(@temp_output_directory)
 
 	## INPUT
-	@input_directory = "#{@working_directory}/input"
-	if File.directory? @input_directory
-		FileUtils.rm_rf(@input_directory)
-	end
-	Dir.mkdir(@input_directory)
-
-	@output_directory = "#{@working_directory}/output"
-	if File.directory? @output_directory
-		FileUtils.rm_rf(@output_directory)
-	end
-	Dir.mkdir(@output_directory)
 	## END PRE_TEST WORKING DIR SETUP
 
-	Dir.glob("msftest/*.msftest").each do |filename|
+	Dir.glob("#{@working_directory}/msftest/*.msftest").each do |filename|
 		
 		## Parse this test case
 		test_case = MsfTestCase.new(filename)
 		puts "Found #{test_case.name} in: #{filename}" 	
 
 		## Write the commands back to a temporary RC file
-		File.open("#{@input_directory}/#{test_case.name}.rc", 'w') { |f|  f.puts test_case.commands } 
+		puts "Writing #{@temp_input_directory}/#{test_case.name}.rc"
+		File.open("#{@temp_input_directory}/#{test_case.name}.rc", 'w') { |f|  f.puts test_case.commands } 
 		
 		## Create the rspec Test Case
 		it "should #{test_case.name}" do
@@ -157,15 +155,15 @@ describe "Msfconsole" do
 			failure_exception_strings = test_case.expected_failure_exceptions | generic_failure_exception_strings
 			
 			## run the commands
-			data = hlp_run_command_check_output( test_case.name, "#{@input_directory}/#{test_case.name}.rc")	
+			data = hlp_run_command_check_output( test_case.name, "#{@temp_input_directory}/#{test_case.name}.rc")	
 					
 			## check the output		
 			data.should contain_all_successes(success_strings)
 			data.should contain_no_failures_except(failure_strings, failure_exception_strings)
 			
 			## Clean up 
-			#File.delete("#{@input_directory}/#{test_case.name}.rc")
-			#File.delete("#{@output_directory}/#{test_case.name}")
+			#File.delete("#{@temp_input_directory}/#{test_case.name}.rc")
+			#File.delete("#{@temp_output_directory}/#{test_case.name}")
 		end
 	end
 
@@ -192,11 +190,14 @@ describe "Msfconsole" do
 
 	def hlp_run_command_check_output(name,rc_file, database_file=false)
 
-		temp_output_file  = "#{@output_directory}/#{name}"
+
+		temp_output_file  = "#{@temp_output_directory}/#{name}"
+		#puts "Setting output to: #{@temp_output_directory}/#{name}"
+
 		if database_file
-			msfconsole_string = "../../../msfconsole -o #{temp_output_file} -r #{rc_file} -y #{database_file}"
+			msfconsole_string = "#{@working_directory}/../../../msfconsole -o #{temp_output_file} -r #{rc_file} -y #{database_file}"
 		else
-			msfconsole_string = "../../../msfconsole -o #{temp_output_file} -r #{rc_file}"
+			msfconsole_string = "#{@working_directory}/../../../msfconsole -o #{temp_output_file} -r #{rc_file}"
 		end
 
 		puts "\n\nName: #{name}"
