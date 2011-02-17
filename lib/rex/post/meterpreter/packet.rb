d6b8ef4c1607de3f640905ea7d1140843b9bbfb1
James Lee <James_Lee@rapid7.com>
2011-02-17 20:04:46 +0000

    don't show meta type for packets (since they don't have one)
    git-svn-id: file:///home/svn/framework3/trunk@11768 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/post/meterpreter/packet.rb
+++ lib/rex/post/meterpreter/packet.rb
@@ -184,7 +184,7 @@ class Tlv
 		if val.length > 50
 			val = val[0,50] + ' ..."'
 		end
-		"#<#{self.class} type=#{stype} meta-type=#{meta} #{self.class.to_s =~ /Packet/ ? "tlvs=#{@tlvs.inspect}" : "value=#{val}"} >"
+		"#<#{self.class} type=#{stype} #{self.class.to_s =~ /Packet/ ? "tlvs=#{@tlvs.inspect}" : "meta-type=#{meta} value=#{val}"} >"
 	end
 
 	##
