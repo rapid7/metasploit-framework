15c6e90e2e4029f0b19f8a10242c1b45bee2cc4f
James Lee <James_Lee@rapid7.com>
2011-02-22 02:02:04 +0000

    fix some whitespace and be more angry when something is broken
    git-svn-id: file:///home/svn/framework3/trunk@11789 4d416f70-5f16-0410-b530-b9f4589650da

--- test/modules/post/test/registry.rb
+++ test/modules/post/test/registry.rb
@@ -29,7 +29,6 @@ class Metasploit3 < Msf::Post
 			))
 		register_options(
 		[
-				OptBool.new("VERBOSE" , [ true, "Enumerate currently configured shares", true]),
 				OptString.new("KEY" , [true, "Registry key to test", "HKLM\\Software\\Microsoft\\Active Setup"]),
 				OptString.new("VALUE" , [true, "Registry value to test", "DisableRepair"]),
 			], self.class)
@@ -37,32 +36,31 @@ class Metasploit3 < Msf::Post
 	end
 
 	def run
-		$blab = false
-		#$blab = true if datastore["VERBOSE"]
 		print_status("Running against session #{datastore["SESSION"]}")
 
 		print_status "testing get_val_info for key:#{datastore['KEY']}, val:#{datastore['VALUE']}"
 		results = registry_getvalinfo(datastore['KEY'], datastore['VALUE'])
-		print_status ("results: #{results.class} #{results.inspect}")
+		print_status("results: #{results.class} #{results.inspect}")
 		print_status "testing get_val_data for key:#{datastore['KEY']}, val:#{datastore['VALUE']}"
 		results = registry_getvaldata(datastore['KEY'], datastore['VALUE'])
-		print_status ("results: #{results.class} #{results.inspect}")
+		print_status("results: #{results.class} #{results.inspect}")
 
 		print_status "testing create_key for key:#{datastore['KEY']}\\test"
 		results = registry_createkey("#{datastore['KEY']}\\test")
-		print_status ("results: #{results.class} #{results.inspect}")
+		print_status("results: #{results.class} #{results.inspect}")
 
 		print_status "testing set_val_data for key:#{datastore['KEY']}\\test, val:test, data:test, type:REG_SZ"
 		results = registry_setvaldata("#{datastore['KEY']}\\test", "test", "test", "REG_SZ")
-		print_status ("results: #{results.class} #{results.inspect}")
+		print_status("results: #{results.class} #{results.inspect}")
 
 		print_status "getting newly created val_info for key:#{datastore['KEY']}\\test, val:test"
 		results = registry_getvalinfo("#{datastore['KEY']}\\test", "test")
-		print_status ("results: #{results.class} #{results.inspect}")
+		print_status("results: #{results.class} #{results.inspect}")
 
 		print_status "testing del_val_data for key:#{datastore['KEY']}\\test, val:test"
 		results = registry_deleteval("#{datastore['KEY']}\\test", "test")
-		print_status ("results: #{results.class} #{results.inspect}")
+		print_errror("registry_deleteval reported failure") unless results
+		print_status("results: #{results.class} #{results.inspect}")
 
 		print_status "testing del_key"
 		results = registry_deletekey("#{datastore['KEY']}\\test")
@@ -70,7 +68,11 @@ class Metasploit3 < Msf::Post
 
 		print_status "getting deleted val_info for key:#{datastore['KEY']}\\test, val:test, this should return nils"
 		results = registry_getvalinfo("#{datastore['KEY']}\\test", "test")
-		print_status ("results: #{results.class} #{results.inspect}")
+		if (results.nil?)
+			print_status ("Delete worked correctly")
+		else
+			print_error ("Deleted key is still there!")
+		end
 
 		print_status "testing enum_keys"
 		results = registry_enumkeys(datastore['KEY'])
