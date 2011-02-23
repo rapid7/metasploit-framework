91c4e5418530973061fb5d89f17f06ee6d417415
James Lee <James_Lee@rapid7.com>
2011-02-23 22:13:53 +0000

    prefer sdoc when it's installed.  much prettier than rdoc
    git-svn-id: file:///home/svn/framework3/trunk@11813 4d416f70-5f16-0410-b530-b9f4589650da

--- documentation/gendocs.sh
+++ documentation/gendocs.sh
@@ -2,14 +2,20 @@ OPTS="-x .ut.rb -x .ts.rb -q"
 BASE="$(dirname "$0")"
 MSFDIR="${BASE}/.."
 DOCDIR="${BASE}/api"
+doc=$(which sdoc)
 
+if [ -z $doc ]; then
+    doc=$(which rdoc)
+fi
+
+echo "Using ${doc} for doc generation"
 echo "Putting docs in ${DOCDIR}"
 
 echo "Generating rex..."
-rdoc $OPTS -t "Rex Documentation" -o ${DOCDIR}/rex ${MSFDIR}/lib/rex
+$doc $OPTS -t "Rex Documentation" -o ${DOCDIR}/rex ${MSFDIR}/lib/rex
 echo "Generating msfcore"
-rdoc $OPTS -t "Framework Core Documentation" -o ${DOCDIR}/msfcore ${MSFDIR}/lib/msf/core
+$doc $OPTS -t "Framework Core Documentation" -o ${DOCDIR}/msfcore ${MSFDIR}/lib/msf/core
 echo "Generating msfbase"
-rdoc $OPTS -t "Framework Base Documentation" -o ${DOCDIR}/msfbase ${MSFDIR}/lib/msf/base
+$doc $OPTS -t "Framework Base Documentation" -o ${DOCDIR}/msfbase ${MSFDIR}/lib/msf/base
 echo "Generating msfui"
-rdoc $OPTS -t "Framework UI Documentation" -o ${DOCDIR}/msfui ${MSFDIR}/lib/msf/ui
+$doc $OPTS -t "Framework UI Documentation" -o ${DOCDIR}/msfui ${MSFDIR}/lib/msf/ui
