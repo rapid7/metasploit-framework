c775f07a82566cbf895057f1301eb534ad93f3d6
James Lee <James_Lee@rapid7.com>
2011-03-04 16:43:20 +0000

    this test works, re-enable.  fix a 1.9 compat issue.
    git-svn-id: file:///home/svn/framework3/trunk@11877 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/socket/ssl_tcp_server.rb.ut.rb
+++ lib/rex/socket/ssl_tcp_server.rb.ut.rb
@@ -5,17 +5,23 @@ $:.unshift(File.join(File.dirname(__FILE__), '..', '..'))
 require 'test/unit'
 require 'rex/socket/ssl_tcp_server'
 require 'rex/socket/ssl_tcp'
+require 'rex/text'
 
 class Rex::Socket::SslTcpServer::UnitTest < Test::Unit::TestCase
 
 	# XXX.  The client data is sent & decrypted just fine.  The server data is not.  the client thread just spins.  BAH.
+	#
+	# As of 2011-03-04, works fine on 1.8.6-p399, 1.8.7-p330, 1.9.1-p378
+	#
 	def test_tcp_server
-		return;
+		#return;
 
 		serv_port = 65433
 		c = nil
 
 		threads = []
+
+		# Server thread
 		threads << Thread.new() {
 			serv = Rex::Socket.create_tcp_server('LocalPort' => serv_port, 'SSL' => true)
 			assert_kind_of(Rex::Socket::SslTcpServer, serv, "type => ssl")
@@ -24,12 +30,16 @@ class Rex::Socket::SslTcpServer::UnitTest < Test::Unit::TestCase
 			s = serv.accept
 			assert_equal("client_data\n", s.get_once(), "s: get_once")
 			assert_equal(3, s.write("Yo\n"), "s: put Yo")
-			assert(s.methods.include?('<<'))
-			assert(s.methods.include?('>>'))
-			assert(s.methods.include?('has_read_data?'))
+			# Make sure methods are Strings for 1.9 compat (which returns
+			# symbols)
+			meths = s.methods.map {|m| m.to_s}
+			assert(meths.include?("<<"), "Has <<")
+			assert(meths.include?(">>"), "Has >>")
+			assert(meths.include?("has_read_data?"), "Has has_read_data?")
 			serv.close
 		}
 
+		# Client thread
 		threads << Thread.new() {
 			sleep(2)
 			assert_nothing_raised {
