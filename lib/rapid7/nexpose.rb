7043e25ae999dd6852847869af2fa429d12d6e74
HD Moore <hd_moore@rapid7.com>
2011-03-21 19:00:35 +0000

    Clarify the license and the usability
    git-svn-id: file:///home/svn/framework3/trunk@12061 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rapid7/nexpose.rb
+++ lib/rapid7/nexpose.rb
@@ -1,6 +1,37 @@
 #
 # The NeXpose API
 #
+=begin
+
+Copyright (C) 2009-2011, Rapid7 LLC
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+	  this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright notice,
+	  this list of conditions and the following disclaimer in the documentation
+	  and/or other materials provided with the distribution.
+
+    * Neither the name of Rapid7 LLC nor the names of its contributors
+	  may be used to endorse or promote products derived from this software
+	  without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=end
 
 #
 # WARNING! This code makes an SSL connection to the NeXpose server, but does NOT
@@ -11,9 +42,9 @@
 #
 
 #
-# WARNING! This code is still rough and will go through a number of major changes
-#          before the final release. Please do not build any tools using this API
-#          unless you are prepared to rewrite them in the near future.
+# WARNING! This code is still rough and going through substantive changes. While
+#          you can build tools using this library today, keep in mind that method
+#          names and parameters may change in the future.
 #
 
 require 'date'
