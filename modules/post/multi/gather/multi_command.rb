cffe667c71ef135ebcb20e67ce8483ec4b6cf249
Carlos Perez <carlos_perez@darkoperator.com>
2011-03-22 22:59:01 +0000

    Multi command script migrated to post module, supports *nix system and both shell and meterpreter. Will run commands in a given resource file.
    git-svn-id: file:///home/svn/framework3/trunk@12073 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/post/multi/gather/multi_command.rb
@@ -0,0 +1,70 @@
+##
+# $Id$
+##
+
+##
+# ## This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/framework/
+##
+
+require 'msf/core'
+require 'rex'
+require 'msf/core/post/common'
+
+class Metasploit3 < Msf::Post
+
+	include Msf::Post::Common
+
+	def initialize(info={})
+		super( update_info( info,
+				'Name'          => 'Run Shell Command Resource File',
+				'Description'   => %q{ This module will read shell commands from a resource file and
+					execute the commands in the speciffied Meterpreter or shell session.},
+				'License'       => MSF_LICENSE,
+				'Author'        => [ 'Carlos Perez <carlos_perez[at]darkoperator.com>'],
+				'Version'       => '$Revision$',
+				'Platform'      => [ 'windows', 'linux', 'bsd', 'unix', 'osx' ],
+				'SessionTypes'  => [ 'meterpreter','shell' ]
+			))
+		register_options(
+			[
+				OptBool.new('VERBOSE', [ false, 'Print the output of each command.', false]),
+				OptString.new('RESOURCE', [true, 'Full path to resource file to read commands from.', nil])
+
+			], self.class)
+	end
+
+	# Run Method for when run command is issued
+	def run
+		session_type = session.type
+		print_status("Running module against #{sysinfo['Computer']}")
+		if not ::File.exists?(datastore['RESOURCE'])
+                        raise "Resource File does not exists!"
+                else
+                        ::File.open(datastore['RESOURCE'], "r").each_line do |cmd|
+                                next if cmd.strip.length < 1
+				next if cmd[0,1] == "#"
+				begin
+					tmpout = "\n"
+					tmpout << "*****************************************\n"
+					tmpout << "      Output of #{cmd}\n"
+					tmpout << "*****************************************\n"
+					print_status "Running command #{cmd.chomp}"
+					if session_type =~ /meterpreter/
+						tmpout << cmd_exec(cmd.chomp)
+					elsif session_type =~ /shell/
+						session.shell_command_token(cmd.chomp).chomp
+					end
+					print_status tmpout if datastore['VERBOSE']
+					command_log = store_loot("host.command", "text/plain", session,@data_out ,
+						"#{cmd.gsub(/\.|\/|\s/,"_")}.txt", "Command Output \'#{cmd.chomp}\'")
+					print_status("Command output saved to: #{command_log}")
+				rescue ::Exception => e
+					print_status("Error Running Command #{cmd.chomp}: #{e.class} #{e}")
+				end
+                        end
+                end
+	end
+end
