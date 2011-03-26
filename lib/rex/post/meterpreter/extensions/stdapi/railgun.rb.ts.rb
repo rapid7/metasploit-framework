d18f14de4e4f5adc53f73eb3b0166a837cc5501d
Jonathan Cran <jcran@rapid7.com>
2011-03-26 19:49:08 +0000

    add a test suite for railgun
    git-svn-id: file:///home/svn/framework3/trunk@12152 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ lib/rex/post/meterpreter/extensions/stdapi/railgun.rb.ts.rb
@@ -0,0 +1,6 @@
+$:.unshift(File.join(File.dirname(__FILE__)))
+require 'railgun/api_constants.rb.ut'
+require 'railgun/buffer_item.rb.ut'
+require 'railgun/dll_function.rb.ut'
+require 'railgun/dll_helper.rb.ut'
+require 'railgun/win_const_manager.rb.ut'
