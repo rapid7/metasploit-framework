cf3fbdad7194561d1cc543f28601b01edfdc0f50
James Lee <James_Lee@rapid7.com>
2011-03-26 00:45:52 +0000

    unneeded require in comments
    git-svn-id: file:///home/svn/framework3/trunk@12142 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/post/meterpreter/extensions/stdapi/railgun/dll.rb
+++ lib/rex/post/meterpreter/extensions/stdapi/railgun/dll.rb
@@ -20,7 +20,7 @@
 # LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 # ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.require 'rex/post/meterpreter/extensions/stdapi/railgun/dll_function'
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 require 'rex/post/meterpreter/extensions/stdapi/railgun/dll_helper'
 require 'rex/post/meterpreter/extensions/stdapi/railgun/dll_function'
