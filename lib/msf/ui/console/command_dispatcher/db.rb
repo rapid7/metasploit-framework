537e312c1353593d6194a14ffb73f587ba4bff36
James Lee <James_Lee@rapid7.com>
2011-04-22 18:31:55 +0000

    set RHOSTS directly when we have a short list
    git-svn-id: file:///home/svn/framework3/trunk@12404 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/msf/ui/console/command_dispatcher/db.rb
+++ lib/msf/ui/console/command_dispatcher/db.rb
@@ -1779,21 +1779,28 @@ class Db
 				print_status "The list is empty, cowardly refusing to set RHOSTS"
 				return
 			end
-			rhosts_file = Rex::Quickfile.new("db_services-rhosts-")
-
-			# create the output file and assign it to the RHOSTS variable					
-			rhosts_file.write(rhosts.join("\n")+"\n")
 			if active_module
 				mydatastore = active_module.datastore
 			else
 				# if there is no module in use set the list to the global variable
 				mydatastore = self.framework.datastore
 			end
-			mydatastore['RHOSTS'] = 'file:'+rhosts_file.path
+
+			if rhosts.length > 5
+				# Lots of hosts makes 'show options' wrap which is difficult to
+				# read, store to a temp file
+				rhosts_file = Rex::Quickfile.new("msf-db-rhosts-")
+				mydatastore['RHOSTS'] = 'file:'+rhosts_file.path
+				# create the output file and assign it to the RHOSTS variable					
+				rhosts_file.write(rhosts.join("\n")+"\n")
+				rhosts_file.close
+			else
+				# For short lists, just set it directly
+				mydatastore['RHOSTS'] = rhosts.join(" ")
+			end
 
 			print_line "RHOSTS => #{mydatastore['RHOSTS']}"
 			print_line 
-			rhosts_file.close
 		end
 
 
