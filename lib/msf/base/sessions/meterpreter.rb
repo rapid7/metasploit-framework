rapid7/metasploit-framework

commit 93b0c5beb31d586b802ea51a2b5fa147ad3876f5

Check for the database first
git-svn-id: file:///home/svn/framework3/trunk@12523 4d416f70-5f16-0410-b530-b9f4589650da

lib/msf/base/sessions/meterpreter.rb
@@ -279,6 +279,7 @@ class Meterpreter < Rex::Post::Meterpreter::Client
 	# Also reports a session_fingerprint note for host os normalization.
 	#
 	def load_session_info()
+		return if not (framework.db and framework.db.active)	
 		begin
 			::Timeout.timeout(60) do
 				username  = self.sys.config.getuid

commit 1f7285987454dcf5cdb13169e2f2a8278f2469d0

set the session info when there is no database.  fixes regression introduced by r12523
git-svn-id: file:///home/svn/framework3/trunk@12690 4d416f70-5f16-0410-b530-b9f4589650da

lib/msf/base/sessions/meterpreter.rb
@@ -279,11 +279,23 @@ class Meterpreter < Rex::Post::Meterpreter::Client
 	# Also reports a session_fingerprint note for host os normalization.
 	#
 	def load_session_info()
-		return if not (framework.db and framework.db.active)	
 		begin
 			::Timeout.timeout(60) do
 				username  = self.sys.config.getuid
 				sysinfo   = self.sys.config.sysinfo
+
+				safe_info = "#{username} @ #{sysinfo['Computer']}"
+				safe_info.force_encoding("ASCII-8BIT") if safe_info.respond_to?(:force_encoding)
+				# Should probably be using Rex::Text.ascii_safe_hex but leave
+				# this as is for now since "\xNN" is arguably uglier than "_"
+				# showing up in various places in the UI.
+				safe_info.gsub!(/[\x00-\x08\x0b\x0c\x0e-\x19\x7f-\xff]+/n,"_")
+				self.info = safe_info
+
+				# The rest of this requires a database, so bail if it's not
+				# there
+				return if not (framework.db and framework.db.active)	
+
 				framework.db.report_note({
 					:type => "host.os.session_fingerprint",
 					:host => self,
@@ -294,13 +306,6 @@ class Meterpreter < Rex::Post::Meterpreter::Client
 						:arch => sysinfo["Architecture"],
 					}
 				})
-				safe_info = "#{username} @ #{sysinfo['Computer']}"
-				safe_info.force_encoding("ASCII-8BIT") if safe_info.respond_to?(:force_encoding)
-				# Should probably be using Rex::Text.ascii_safe_hex but leave
-				# this as is for now since "\xNN" is arguably uglier than "_"
-				# showing up in various places in the UI.
-				safe_info.gsub!(/[\x00-\x08\x0b\x0c\x0e-\x19\x7f-\xff]+/n,"_")
-				self.info = safe_info
 				if self.db_record
 					self.db_record.desc = safe_info
 					self.db_record.save!