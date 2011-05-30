3e0f3639efed17d72f344dd03278a9f1fe678401
HD Moore <hd_moore@rapid7.com>
2011-05-30 03:44:59 +0000

    This adds a quick windows/loadlibrary payload for folks who have a need for such things. The library path can be a UNC location and works fine over WebDAV...
    git-svn-id: file:///home/svn/framework3/trunk@12765 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ modules/payloads/singles/windows/loadlibrary.rb
@@ -0,0 +1,24 @@
+##
+# $Id$
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/framework/
+##
+
+require 'msf/core'
+require 'msf/core/payload/windows/loadlibrary'
+
+###
+#
+# Executes a command on the target machine
+#
+###
+module Metasploit3
+
+	include Msf::Payload::Windows::LoadLibrary
+
+end
