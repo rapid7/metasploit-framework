a0292f77221be79b3f1892f4617aaa3223772ba0
Carlos Perez <carlos_perez@darkoperator.com>
2011-06-03 12:26:26 +0000

    added name to authors list
    git-svn-id: file:///home/svn/framework3/trunk@12841 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/msf/core/module/author.rb
+++ lib/msf/core/module/author.rb
@@ -39,7 +39,8 @@ class Msf::Module::Author
 			'jcran'		=> 'jcran' + 0x40.chr + 'metasploit.com',
 			'sinn3r'	=> 'sinn3r' + 0x40.chr + 'metasploit.com',
 			'bannedit'	=> 'bannedit' + 0x40.chr + 'metasploit.com',
-			'amaloteaux'	=> 'alex_maloteaux' + 0x40.chr + 'metasploit.com'
+			'amaloteaux'	=> 'alex_maloteaux' + 0x40.chr + 'metasploit.com',
+			'Carlos Perez'  => 'carlos_perez' + 0x40.chr + 'darkoperator.com'
 		}
 
 	#
