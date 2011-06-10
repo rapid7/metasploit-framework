2263e51cd247001c2a5ef029c1d7bdc80cbbea36
Carlos Perez <carlos_perez@darkoperator.com>
2011-06-10 03:07:54 +0000

    Initial Solaris mixin
    git-svn-id: file:///home/svn/framework3/trunk@12895 4d416f70-5f16-0410-b530-b9f4589650da

---
+++ lib/msf/core/post/solaris/priv.rb
@@ -0,0 +1,21 @@
+require 'msf/core/post/common'
+
+module Msf
+class Post
+
+module Priv
+	include ::Msf::Post::Common
+
+		# Returns true if running as root, false if not.
+		def is_root?
+			root_priv = false
+			user_id = cmd_exec("/bin/id -u")
+			if user_id.to_i == 0
+				root_priv = true
+			end
+			return root_priv
+		end
+
+end # Priv
+end # Post
+end # Msf
