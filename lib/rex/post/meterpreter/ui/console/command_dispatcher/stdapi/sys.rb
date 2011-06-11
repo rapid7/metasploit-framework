c3307c80be94061f3109e5eca875a2d8da183be1
HD Moore <hd_moore@rapid7.com>
2011-06-11 23:01:11 +0000

    Fixes #1733 by merging in Danilo Nascimento's patch. Thanks!
    git-svn-id: file:///home/svn/framework3/trunk@12919 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/post/meterpreter/ui/console/command_dispatcher/stdapi/sys.rb
+++ lib/rex/post/meterpreter/ui/console/command_dispatcher/stdapi/sys.rb
@@ -40,7 +40,8 @@ class Console::CommandDispatcher::Stdapi::Sys
 		"-h" => [ true,  "Help menu."                                              ],
 		"-k" => [ true,  "The registry key path (E.g. HKLM\\Software\\Foo)."       ],
 		"-t" => [ true,  "The registry value type (E.g. REG_SZ)."                  ],
-		"-v" => [ true,  "The registry value name (E.g. Stuff)."                   ])
+		"-v" => [ true,  "The registry value name (E.g. Stuff)."                   ],
+		"-w" => [ true,  "Set KEY_WOW64 flag, valid values [32|64]."               ])
 
 	#
 	# List of supported commands.
@@ -271,10 +272,11 @@ class Console::CommandDispatcher::Stdapi::Sys
 		end
 
 		# Initiailze vars
-		key   = nil
-		value = nil
-		data  = nil
-		type  = nil
+		key     = nil
+		value   = nil
+		data    = nil
+		type    = nil
+    wowflag = 0x0000
 
 		@@reg_opts.parse(args) { |opt, idx, val|
 			case opt
@@ -290,7 +292,7 @@ class Console::CommandDispatcher::Stdapi::Sys
 						"    queryclass Queries the class of the supplied key [-k <key>]\n" +
 						"    setval     Set a registry value [-k <key> -v <val> -d <data>]\n" +
 						"    deleteval  Delete the supplied registry value [-k <key> -v <val>]\n" +
-						"    queryval   Queries the data contents of a value [-k <key> -v <val>]\n\n")
+						"    queryval   Queries the data contents of a value [-k <key> -v <val>]\n\n") 
 					return false
 				when "-k"
 					key   = val
@@ -300,6 +302,12 @@ class Console::CommandDispatcher::Stdapi::Sys
 					type  = val
 				when "-d"
 					data  = val
+				when "-w"
+					if val == '64'
+            wowflag = KEY_WOW64_64KEY
+          elsif val == '32'
+            wowflag = KEY_WOW64_32KEY
+          end
 			end
 		}
 
@@ -316,7 +324,7 @@ class Console::CommandDispatcher::Stdapi::Sys
 			# Rock it
 			case cmd
 				when "enumkey"
-					open_key = client.sys.registry.open_key(root_key, base_key)
+					open_key = client.sys.registry.open_key(root_key, base_key, KEY_READ + wowflag)
 
 					print_line(
 						"Enumerating: #{key}\n")
@@ -349,7 +357,7 @@ class Console::CommandDispatcher::Stdapi::Sys
 					end
 
 				when "createkey"
-					open_key = client.sys.registry.create_key(root_key, base_key)
+					open_key = client.sys.registry.create_key(root_key, base_key, KEY_READ + wowflag)
 
 					print_line("Successfully created key: #{key}")
 
@@ -366,7 +374,7 @@ class Console::CommandDispatcher::Stdapi::Sys
 
 					type = "REG_SZ" if (type == nil)
 
-					open_key = client.sys.registry.open_key(root_key, base_key, KEY_WRITE)
+					open_key = client.sys.registry.open_key(root_key, base_key, KEY_WRITE + wowflag)
 
 					open_key.set_value(value, client.sys.registry.type2str(type), data)
 
@@ -378,7 +386,7 @@ class Console::CommandDispatcher::Stdapi::Sys
 						return false
 					end
 
-					open_key = client.sys.registry.open_key(root_key, base_key, KEY_WRITE)
+					open_key = client.sys.registry.open_key(root_key, base_key, KEY_WRITE + wowflag)
 
 					open_key.delete_value(value)
 
@@ -390,7 +398,7 @@ class Console::CommandDispatcher::Stdapi::Sys
 						return false
 					end
 
-					open_key = client.sys.registry.open_key(root_key, base_key, KEY_READ)
+					open_key = client.sys.registry.open_key(root_key, base_key, KEY_READ + wowflag)
 
 					v = open_key.query_value(value)
 
@@ -401,7 +409,7 @@ class Console::CommandDispatcher::Stdapi::Sys
 						"Data: #{v.data}\n")
 
 				when "queryclass"
-					open_key = client.sys.registry.open_key(root_key, base_key, KEY_READ)
+					open_key = client.sys.registry.open_key(root_key, base_key, KEY_READ + wowflag)
 
 					data = open_key.query_class
 
