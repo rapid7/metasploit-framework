d68e855bb641f73ac403d4376d4a0b66d84d3da9
James Lee <James_Lee@rapid7.com>
2011-07-01 23:46:54 +0000

    don't try to chomp nil when the command failed (happens when a session dies)
    git-svn-id: file:///home/svn/framework3/trunk@13085 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/msf/core/post/common.rb
+++ lib/msf/core/post/common.rb
@@ -5,20 +5,23 @@ module Common
 
 	# Execute given command as hidden and channelize, output of command given as a multiline string.
 	# For certain versions of Meterpreter options can not be included in the cmd var
-	def cmd_exec(cmd,opts = nil,time_out = 15)
+	def cmd_exec(cmd, opts=nil, time_out=15)
 		case session.type
 		when /meterpreter/
 			session.response_timeout = time_out
 			cmd = session.sys.process.execute(cmd, opts, {'Hidden' => true, 'Channelized' => true})
 			o = ""
-			while(d = cmd.channel.read)
-			o << d
+			while (d = cmd.channel.read)
 				break if d == ""
+				o << d
 			end
 			cmd.channel.close
+			cmd.close
 		when /shell/
-			o = session.shell_command_token("#{cmd} #{opts}",time_out).chomp
+			o = session.shell_command_token("#{cmd} #{opts}", time_out)
+			o.chomp! if o
 		end
+		return "" if o.nil?
 		return o
 	end
 
