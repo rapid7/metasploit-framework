dd84169187d4467986984697a70711c8721921b5
James Lee <James_Lee@rapid7.com>
2011-07-24 23:50:55 +0000

    tell the linker to make DT_HASH sections instead of DT_GNU_HASH which msflinker can't handle
    git-svn-id: file:///home/svn/framework3/trunk@13332 4d416f70-5f16-0410-b530-b9f4589650da

--- external/source/meterpreter/source/bionic/libc/out/x86/make.sh
+++ external/source/meterpreter/source/bionic/libc/out/x86/make.sh
@@ -11,7 +11,7 @@ for i in $BAD_FILES ; do
 	rm flood/$i
 done
 
-gcc -nostdinc -nostdlib -shared -o libbionic.so flood/*.o -lgcc
+gcc -Wl,--hash-style=sysv -nostdinc -nostdlib -shared -o libbionic.so flood/*.o -lgcc
 
 [ ! -f libc.so ] && ln -s ${PWD}/libbionic.so libc.so
 
