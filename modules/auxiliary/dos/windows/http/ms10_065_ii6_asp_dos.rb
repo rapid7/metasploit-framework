5fdf482a8b4288a366bdeffa30d6e35315f8c116
Wei Chen <Wei_Chen@rapid7.com>
2011-07-24 19:50:56 +0000

    Add MSB reference
    git-svn-id: file:///home/svn/framework3/trunk@13331 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/auxiliary/dos/windows/http/ms10_065_ii6_asp_dos.rb
+++ modules/auxiliary/dos/windows/http/ms10_065_ii6_asp_dos.rb
@@ -37,6 +37,7 @@ class Metasploit3 < Msf::Auxiliary
 				[
 					[ 'CVE', '2010-1899' ],
 					[ 'OSVDB', '67978'],
+					[ 'MSB', 'MS10-065'],
 					[ 'URL', 'http://www.exploit-db.com/exploits/15167/' ]
 				],
 			'DisclosureDate' => 'Sep 14 2010'))
