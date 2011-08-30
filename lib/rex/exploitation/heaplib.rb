aa5ad30805dec0ec9b2f35614a52c54d3ccdaecd
David Rude <bannedit0@gmail.com>
2011-08-30 22:08:51 +0000

    Alters heaplib so it no longer obfuscates using the old method
    git-svn-id: file:///home/svn/framework3/trunk@13665 4d416f70-5f16-0410-b530-b9f4589650da

--- lib/rex/exploitation/heaplib.rb
+++ lib/rex/exploitation/heaplib.rb
@@ -16,43 +16,6 @@ class HeapLib
 	JavascriptFile = File.join(File.dirname(__FILE__), "heaplib.js.b64")
 
 	#
-	# The list of symbols found in the file.  This is used to dynamically
-	# replace contents.
-	#
-	SymbolNames  = 
-		{
-			"Methods" =>
-				[
-					"vtable",
-					"lookasideAddr",
-					"lookaside",
-					"freeList",
-					"gc",
-					"flushOleaut32",
-					"freeOleaut32",
-					"allocOleaut32",
-					"free",
-					"alloc",
-					"addr",
-					"hex",
-					"round",
-					"paddingStr",
-					"padding",
-					"debugBreak",
-					"debugHeap",
-					"debug",
-				],
-			"Classes" =>
-				[	
-					{ 'Namespace' => "heapLib", 'Class' => "ie" }
-				],
-			"Namespaces" =>
-				[
-					"heapLib"
-				]
-		}
-
-	#
 	# Initializes the heap library javascript
 	#
 	def initialize(custom_js = '')
@@ -72,22 +35,18 @@ protected
 	# Loads the raw javascript from the source file and strips out comments
 	#
 	def load_js(custom_js)
-		
+
 		# Grab the complete javascript
 		File.open(JavascriptFile) { |f|
 			@js = f.read
 		}
-		
+
 		# Decode the text
 		@js = Rex::Text.decode_base64(@js)
-		
+
 		# Append the real code
 		@js += "\n" + custom_js
-	
-		# Obfuscate the javascript
-		@js = ObfuscateJS.obfuscate(@js, 'Symbols' => SymbolNames)
 	end
-
 end
 
 end
