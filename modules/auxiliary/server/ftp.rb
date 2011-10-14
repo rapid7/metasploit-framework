3c73c3c2f80077e42238aa113fea3846f22d10fe
HD Moore <hd_moore@rapid7.com>
2011-10-14 23:07:09 +0000

    Pile of small bug fixes for the FTP server and mixin
    git-svn-id: file:///home/svn/framework3/trunk@13924 4d416f70-5f16-0410-b530-b9f4589650da

--- modules/auxiliary/server/ftp.rb
+++ modules/auxiliary/server/ftp.rb
@@ -111,16 +111,20 @@ class Metasploit3 < Msf::Auxiliary
 			return
 		end
 
-		pwd = datastore['FTPROOT']
+		pwd = ::File.join(datastore['FTPROOT'], @state[c][:cwd])
 		buf = ''
-		Dir.new(pwd).entries.each do |ent|
-			path = ::File.join(datastore['FTPROOT'], ent)
-			if(::File.directory?(path))
-				buf << "d--x--x--x   1 1            512 Jun 1  2001 #{ent}\r\n"
+		
+		begin
+			Dir.new(pwd).entries.each do |ent|
+				path = ::File.join(datastore['FTPROOT'], ent)
+				if(::File.directory?(path))
+					buf << "drwxr-xr-x   2 0      0       512 Jan  1  2000 #{ent}\r\n"
+				end
+				if(::File.file?(path))
+					buf << "-rw-r--r--   1 0      0       #{::File.size(path)} Jan  1  2000 #{ent}\r\n"
+				end
 			end
-			if(::File.file?(path))
-				buf << "rwsx--r--r   1 1            512 Jun 1  2001 #{ent}\r\n"
-			end
+		rescue ::Exception
 		end
 
 		c.put("150 Opening ASCII mode data connection for /bin/ls\r\n")
@@ -136,7 +140,7 @@ class Metasploit3 < Msf::Auxiliary
 			return
 		end
 
-		path = ::File.join(datastore['FTPROOT'], arg.gsub("../", '').gsub("..\\", ''))
+		path = ::File.join(datastore['FTPROOT'], Rex::FileUtils.clean_path(arg))
 		if(not ::File.exists?(path))
 			c.put "550 File does not exist\r\n"
 			return
@@ -145,4 +149,32 @@ class Metasploit3 < Msf::Auxiliary
 		c.put("213 #{::File.size(path)}\r\n")
 	end
 
+
+	def on_client_command_cwd(c,arg)
+
+		if(not @state[c][:auth])
+			c.put "500 Access denied\r\n"
+			return
+		end
+		
+		upath = ::File.expand_path(datastore['FTPROOT'])
+		npath = ::File.expand_path(::File.join(datastore['FTPROOT'], @state[c][:cwd], arg))
+		bpath = npath[upath.length, npath.length - upath.length]
+
+		# Check for traversal above the root directory
+		if not (npath[0, upath.length] == upath or bpath == '')
+			bpath = '/'
+		end
+
+		npath = ::File.expand_path(::File.join(datastore['FTPROOT'], bpath))
+		if not (::File.exists?(npath) and ::File.directory?(npath))
+			c.put "550 Directory does not exist\r\n"
+			return
+		end
+
+		bpath = '/' if bpath == ''
+		@state[c][:cwd] = bpath
+
+		c.put "250 CWD command successful.\r\n"
+	end
 end
