3b629340b5b8bcb17071826735790c3656df7465
Konrads Smelkovs <konrads.smelkovs@kpmg.co.uk>
2012-02-09 22:59:16 +0000

    first succesful compile of sample

---
+++ external/source/meterpreter/source/extensions/sample/sample.c
@@ -0,0 +1,73 @@
+/*
+ * This server feature extension provides:
+ *
+ *
+ */
+#include "sample.h"
+
+#include "../../ReflectiveDLLInjection/DelayLoadMetSrv.h"
+// include the Reflectiveloader() function, we end up linking back to the metsrv.dll's Init function
+// but this doesnt matter as we wont ever call DLL_METASPLOIT_ATTACH as that is only used by the 
+// second stage reflective dll inject payload and not the metsrv itself when it loads extensions.
+#include "../../ReflectiveDLLInjection/ReflectiveLoader.c"
+
+// this sets the delay load hook function, see DelayLoadMetSrv.h
+EnableDelayLoadMetSrv();
+
+DWORD request_sample_ping(Remote *remote, Packet *packet)
+{
+	LPCSTR message="Just a ping from me to you";
+	DWORD dwResult    = ERROR_SUCCESS;
+	// Initialise response packet
+	Packet * response = NULL;
+	response = packet_create_response( packet );
+	packet_add_tlv_string(packet,TLV_TYPE_SAMPLE_PING,message);
+	packet_transmit_response( dwResult, remote, response );
+	return dwResult;
+}
+
+Command customCommands[] =
+{
+	{ "sample",
+	  { request_sample_ping,                                    { 0 }, 0 },
+	  { EMPTY_DISPATCH_HANDLER                                      },
+	},
+
+	// Terminator
+	{ NULL,
+	  { EMPTY_DISPATCH_HANDLER                      },
+	  { EMPTY_DISPATCH_HANDLER                      },
+	},
+};
+
+/*
+ * Initialize the server extension
+ */
+DWORD __declspec(dllexport) InitServerExtension(Remote *remote)
+{
+	DWORD index;
+
+	hMetSrv = remote->hMetSrv;
+
+	for (index = 0;
+	     customCommands[index].method;
+	     index++)
+		command_register(&customCommands[index]);
+
+	return ERROR_SUCCESS;
+}
+
+/*
+ * Deinitialize the server extension
+ */
+DWORD __declspec(dllexport) DeinitServerExtension(Remote *remote)
+{
+	DWORD index;
+
+	for (index = 0;
+	     customCommands[index].method;
+	     index++)
+		command_deregister(&customCommands[index]);
+
+	return ERROR_SUCCESS;
+}
