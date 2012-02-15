e67e9ab34ff2330f5ebc4337aea237d9a8b2869c
David Maloney <David_Maloney@rapid7.com>
2012-02-14 20:52:45 -0600

    Adds a power off vm aux module

---
+++ modules/auxiliary/admin/vmware/poweroff_vm.rb
@@ -0,0 +1,70 @@
+##
+# $Id$
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/framework/
+##
+
+
+require 'msf/core'
+require 'msf/core/exploit/vim_soap'
+
+
+class Metasploit3 < Msf::Auxiliary
+
+	include Msf::Exploit::Remote::HttpClient
+	include Msf::Auxiliary::Report
+	include Msf::Exploit::Remote::VIMSoap
+
+	def initialize
+		super(
+			'Name'           => 'VMWare Power Off Virtual Machine',
+			'Description'    => %Q{
+							This module will log into the Web API of VMWare and try to power off
+							a specified Virtual Machine.},
+			'Author'         => ['TheLightCosine <thelightcosine[at]metasploit.com>'],
+			'License'        => MSF_LICENSE
+		)
+
+		register_options(
+			[
+				Opt::RPORT(443),
+				OptString.new('USERNAME', [ true, "The username to Authenticate with.", 'root' ]),
+				OptString.new('PASSWORD', [ true, "The password to Authenticate with.", 'password' ]),
+				OptString.new('VM', [true, "The VM to try to Power Off"])
+			], self.class)
+	end
+
+	def run
+
+		if vim_do_login(datastore['USERNAME'], datastore['PASSWORD']) == :success
+			vm_ref = vim_find_vm_by_name(datastore['VM'])
+			if vm_ref 
+				return_state = vim_powerOFF_vm(vm_ref)
+				case return_state 
+				when 'success'
+					print_good "VM Powered Off Successfully"
+				when 'alreadyOFF'
+					print_status "The Server says that VM #{datastore['VM']} is already off."
+				else
+					print_error "The server returned an unexpected status #{return_state}"
+				end
+			else
+				print_error "Could not locate VM #{datastore['VM']}"
+			end
+		else
+			print_error "Login Failure on #{ip}"
+			return
+		end
+	end
+
+
+
+
+
+end
+
