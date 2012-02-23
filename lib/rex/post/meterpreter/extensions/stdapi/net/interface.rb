38eea5d3f9125ad4d556df737b72b6b3d226a59d
MM <gaspmat@gmail.com>
2012-02-23 23:42:26 +0100

    posix meterpreter - IPv6 support for route and ipconfig using netlink sockets

--- lib/rex/post/meterpreter/extensions/stdapi/net/interface.rb
+++ lib/rex/post/meterpreter/extensions/stdapi/net/interface.rb
@@ -27,11 +27,16 @@ class Interface
 	# Returns a logical interface and initializes it to the supplied
 	# parameters.
 	#
-	def initialize(ip, netmask, mac_addr, mac_name)
+	def initialize(index, ip, netmask, mac_addr, mac_name, ip6=nil, netmask6=nil, mtu=0, flags=nil)
+		self.index    = index
 		self.ip       = IPAddr.ntop(ip)
 		self.netmask  = IPAddr.ntop(netmask)
 		self.mac_addr = mac_addr
 		self.mac_name = mac_name
+		self.ip6      = (ip6!=nil) ? IPAddr.new_ntoh(ip6).to_s : ""
+		self.netmask6 = (netmask6!=nil) ? IPAddr.new_ntoh(netmask6).to_s : ""
+		self.mtu      = mtu
+		self.flags    = flags
 	end
 
 	#
@@ -41,16 +46,39 @@ class Interface
 		macocts = []
 		mac_addr.each_byte { |o| macocts << o }
 		macocts += [0] * (6 - macocts.size) if macocts.size < 6
+		# no ip6 received or NULL addr
+		if ip6 != "" and ip6 != "::"
+			ipv6_conf  = sprintf("IPv6 Address : %s\n" +
+							 	 "Netmask      : %s\n", 
+								  ip6, netmask6)
+		else
+			ipv6_conf = ""
+		end
+		if flags != nil
+			flags_str =  sprintf("Flags        : %s\n", flags)
+		else	
+			flags_str = ""
+		end
 		return sprintf(
-				"#{mac_name}\n" +
-				"Hardware MAC: %02x:%02x:%02x:%02x:%02x:%02x\n" +
-				"IP Address  : %s\n" +
-				"Netmask     : %s\n" +
+				"Interface %2d\n" +
+				"============\n"+
+				"Name         : %s\n" +
+				"Hardware MAC : %02x:%02x:%02x:%02x:%02x:%02x\n" +
+				"MTU          : %d\n" +
+				"%s" +
+				"IP Address   : %s\n" +
+				"Netmask      : %s\n" +
+				"%s" + 
 				"\n",
+				index, mac_name,
 				macocts[0], macocts[1], macocts[2], macocts[3],
-				macocts[4], macocts[5], ip, netmask)
+				macocts[4], macocts[5], mtu, flags_str, ip, netmask, ipv6_conf)
 	end
 
+	#
+	# The indedx of the interface.
+	#
+	attr_accessor :index
 	#
 	# The IP address bound to the interface.
 	#
@@ -67,7 +95,22 @@ class Interface
 	# The name of the interface.
 	#
 	attr_accessor :mac_name
-
+	#
+	# The IPv6 address bound to the interface.
+	#
+	attr_accessor :ip6
+	#
+	# The subnet mask associated with the IPv6 interface.
+	#
+	attr_accessor :netmask6
+	#
+	# The MTU associated with the interface.
+	#
+	attr_accessor :mtu
+	#
+	# The flags associated with the interface.
+	#
+	attr_accessor :flags
 end
 
 end; end; end; end; end; end
