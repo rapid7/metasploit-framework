e9df9d6c2c617e387087b358143a4f6d3133a509
sinn3r <msfsinn3r@gmail.com>
2012-02-29 16:24:18 -0600

    Increase default depth

--- modules/auxiliary/scanner/http/http_traversal.rb
+++ modules/auxiliary/scanner/http/http_traversal.rb
@@ -52,7 +52,7 @@ class Metasploit3 < Msf::Auxiliary
 				OptEnum.new('METHOD',    [true, 'HTTP Request Method', 'GET', ['GET', 'POST', 'HEAD', 'PUT']]),
 				OptString.new('PATH',    [true, 'Vulnerable path. Ex: /foo/index.php?pg=', '/']),
 				OptString.new('DATA',    [false,'HTTP body data', '']),
-				OptInt.new('DEPTH',      [true, 'Traversal depth', 3]),
+				OptInt.new('DEPTH',      [true, 'Traversal depth', 5]),
 				OptRegexp.new('PATTERN', [true, 'Regexp pattern to determine directory traversal', '^HTTP/1.1 200 OK']),
 				OptPath.new(
 					'FILELIST',
