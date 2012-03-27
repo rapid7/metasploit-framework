9b905c53b4d46beb86da8168a1c2c5b2da340f6d
James Lee <egypt@metasploit.com>
2012-03-27 11:02:42 -0600

    Abstract out copy-pasted methods
    Need to do the same thing for OSX, but it's a different implementation.

--- lib/msf/core/post/linux/system.rb
+++ lib/msf/core/post/linux/system.rb
@@ -8,6 +8,8 @@ module System
 	include ::Msf::Post::Common
 		include ::Msf::Post::File
 
+		include ::Msf::Post::Unix
+
 		# Returns a Hash containing Distribution Name, Version and Kernel Information
 		def get_sysinfo
 			system_data = {}
@@ -109,41 +111,6 @@ module System
 				system_data[:kernel] = kernel_version
 			end
 			return system_data
-		end
-
-		# Returns an array of hashes each representing a user
-		# Keys are name, uid, gid, info, dir and shell
-		def get_users
-			users = []
-			cmd_out = cmd_exec("cat /etc/passwd").split("\n")
-			cmd_out.each do |l|
-				entry = {}
-				user_field = l.split(":")
-				entry[:name] = user_field[0]
-				entry[:uid] = user_field[2]
-				entry[:gid] = user_field[3]
-				entry[:info] = user_field[4]
-				entry[:dir] = user_field[5]
-				entry[:shell] = user_field[6]
-				users << entry
-			end
-			return users
-		end
-
-		# Returns an array of hashes each hash representing a user group
-		# Keys are name, gid and users
-		def get_groups
-			groups = []
-			cmd_out = cmd_exec("cat /etc/group").split("\n")
-			cmd_out.each do |l|
-				entry = {}
-				user_field = l.split(":")
-				entry[:name] = user_field[0]
-				entry[:gid] = user_field[2]
-				entry[:users] = user_field[3]
-				groups << entry
-			end
-			return groups
 		end
 
 
