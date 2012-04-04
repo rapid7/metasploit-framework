21e47bc2431deb23b99db68e44205dad3e044d20
RageLtMan <rageltman [at] sempervictus>
2012-04-04 13:14:17 -0400

    Pass datastore['Proxies'] to Anemone init options
    Anemone is not part of lib/msf so it doesnt take the datastore
    options implicitly. Try crawling through a proxy right now with
    outbound access to the target blocked, then try it with the
    pull request.

--- lib/msf/core/auxiliary/crawler.rb
+++ lib/msf/core/auxiliary/crawler.rb
@@ -263,6 +263,7 @@ module Auxiliary::HttpCrawler
 		opts[:framework]           = framework
 		opts[:module]              = self
 		opts[:timeout]             = get_connection_timeout
+		opts[:proxies]		= datastore['Proxies']
 
 		if (t[:headers] and t[:headers].length > 0)
 			opts[:inject_headers] = t[:headers]
