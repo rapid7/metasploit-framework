3116f66d4dd8641054b199add492ed94bb907b4b
James Lee <egypt@metasploit.com>
2012-04-16 23:22:41 -0600

    MDM update
    [Fixes #6649]

--- lib/gemcache/ruby/1.9.1/gems/metasploit_data_models-0.0.2.43DEV/lib/metasploit_data_models/active_record_models/note.rb
+++ lib/gemcache/ruby/1.9.1/gems/metasploit_data_models-0.0.2.43DEV/lib/metasploit_data_models/active_record_models/note.rb
@@ -1,12 +1,13 @@
+require 'metasploit_data_models/note_serializer'
 module MetasploitDataModels::ActiveRecordModels::Note
   def self.included(base)
     base.class_eval{
-      notes = base.arel_table      
+      notes = base.arel_table
 
       belongs_to :workspace, :class_name => "Mdm::Workspace"
       belongs_to :host, :class_name => "Mdm::Host"
       belongs_to :service, :class_name => "Mdm::Service"
-      serialize :data, ::MetasploitDataModels::Base64Serializer.new
+      serialize :data, ::MetasploitDataModels::NoteSerializer.new
 
       scope :flagged, where('critical = true AND seen = false')
       scope :visible, where(notes[:ntype].not_in(['web.form', 'web.url', 'web.vuln']))
