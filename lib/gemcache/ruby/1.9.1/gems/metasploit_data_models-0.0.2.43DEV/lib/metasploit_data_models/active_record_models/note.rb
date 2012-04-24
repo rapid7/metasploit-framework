0e4593ce5b457d5e0c9a0bc7ece2dfa55b706e5c
HD Moore <hd_moore@rapid7.com>
2012-04-23 22:20:41 -0500

    Purge the NoteSerializer and switch to Base64, have Base64 use the old format as well

--- lib/gemcache/ruby/1.9.1/gems/metasploit_data_models-0.0.2.43DEV/lib/metasploit_data_models/active_record_models/note.rb
+++ lib/gemcache/ruby/1.9.1/gems/metasploit_data_models-0.0.2.43DEV/lib/metasploit_data_models/active_record_models/note.rb
@@ -1,4 +1,3 @@
-require 'metasploit_data_models/note_serializer'
 module MetasploitDataModels::ActiveRecordModels::Note
   def self.included(base)
     base.class_eval{
@@ -7,7 +6,7 @@ module MetasploitDataModels::ActiveRecordModels::Note
       belongs_to :workspace, :class_name => "Mdm::Workspace"
       belongs_to :host, :class_name => "Mdm::Host"
       belongs_to :service, :class_name => "Mdm::Service"
-      serialize :data, ::MetasploitDataModels::NoteSerializer.new
+      serialize :data, ::MetasploitDataModels::Base64Serializer.new
 
       scope :flagged, where('critical = true AND seen = false')
       scope :visible, where(notes[:ntype].not_in(['web.form', 'web.url', 'web.vuln']))
