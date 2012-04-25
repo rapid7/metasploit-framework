03117ffa9527eb8ac6d8efeecbe27e5207e76fba
HD Moore <hd_moore@rapid7.com>
2012-04-25 11:24:28 -0500

    Add a version scanner for RMI

---
+++ modules/auxiliary/scanner/misc/java_rmi_server_version.rb
@@ -0,0 +1,68 @@
+##
+# $Id$
+##
+
+##
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# web site for more information on licensing and terms of use.
+#   http://metasploit.com/
+##
+
+require 'msf/core'
+
+class Metasploit3 < Msf::Auxiliary
+
+	include Msf::Exploit::Remote::Tcp
+	include Msf::Auxiliary::Scanner
+	include Msf::Auxiliary::Report
+
+	def initialize
+		super(
+			'Name'        => 'Java RMI Server Endpoint Scanner',
+			'Version'     => '$Revision$',
+			'Description' => 'Detect RMI endpoints',
+			'Author'      => 'hdm',
+			'License'     => MSF_LICENSE,
+			'References'     =>
+				[
+					# RMI protocol specification
+					[ 'URL', 'http://download.oracle.com/javase/1.3/docs/guide/rmi/spec/rmi-protocol.html'],
+				],
+			'DisclosureDate' => 'Oct 15 2011',			
+		)
+
+		register_options(
+			[
+				Opt::RPORT(1099),
+			], self.class)
+	end
+
+	def run_host(target_host)
+
+		begin
+			connect
+			sock.put("\x4a\x52\x4d\x49\0\x02\x4b")
+			res = sock.get_once
+
+			if res and res =~ /^\x4e..([^\x00]+)\x00\x00/
+				info = $1
+				print_good("#{rhost}:#{rport} RMI Endpoint: #{info}")
+				report_service(:host => rhost, :port => rport, :name => "rmi", :info => info)
+				report_vuln(
+					:host          => rhost,
+					:name          => self.fullname,
+					:info          => "Identified RMI Endpoint: #{info}",
+					:refs          => self.references
+				)				
+			end
+			
+		rescue ::Interrupt
+			raise $!
+		rescue ::Rex::ConnectionError, ::IOError
+		ensure
+			disconnect
+		end
+
+	end
+end
