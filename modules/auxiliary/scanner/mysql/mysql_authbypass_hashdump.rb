21ea5396489d57f883d974bd6c0a20e1fa93bd64
HD Moore <hd_moore@rapid7.com>
2012-06-12 00:05:51 -0500

    Permissions

--- modules/auxiliary/scanner/mysql/mysql_authbypass_hashdump.rb
+++ modules/auxiliary/scanner/mysql/mysql_authbypass_hashdump.rb
@@ -40,6 +40,9 @@ class Metasploit3 < Msf::Auxiliary
 		)
 
 		deregister_options('PASSWORD')
+		register_options( [ 
+			OptString.new('USERNAME', [ true, 'The username to authenticate as', "root" ]) 
+		], self.class )
 	end
 
 
