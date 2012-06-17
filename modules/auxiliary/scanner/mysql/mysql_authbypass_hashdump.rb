74cbca5809fe53d57a6f4339bef36ac43c5d42d1
Thomas Grainger <tagrain@gmail.com>
2012-06-17 13:19:53 +0200

    Print out successful mysql connection URI

--- modules/auxiliary/scanner/mysql/mysql_authbypass_hashdump.rb
+++ modules/auxiliary/scanner/mysql/mysql_authbypass_hashdump.rb
@@ -130,7 +130,7 @@ class Metasploit3 < Msf::Auxiliary
 							:socket         => s,
 							:db             => nil
 							})
-						print_status "#{rhost}:#{rport} Successfully bypassed authentication after #{count} attempts"
+						print_status "mysql://#{username}:#{password}@#{rhost}:#{rport} Successfully bypassed authentication after #{count} attempts"
 						results << x
 					rescue RbMysql::AccessDeniedError
 					rescue Exception => e
