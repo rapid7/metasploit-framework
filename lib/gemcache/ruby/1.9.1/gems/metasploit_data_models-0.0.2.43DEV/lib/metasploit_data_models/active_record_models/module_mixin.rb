37531d44863a0bb98ca3707baec0d3e6cba7e982
HD Moore <hd_moore@rapid7.com>
2012-06-17 02:01:23 -0500

    Fix typo in table name

--- lib/gemcache/ruby/1.9.1/gems/metasploit_data_models-0.0.2.43DEV/lib/metasploit_data_models/active_record_models/module_mixin.rb
+++ lib/gemcache/ruby/1.9.1/gems/metasploit_data_models-0.0.2.43DEV/lib/metasploit_data_models/active_record_models/module_mixin.rb
@@ -1,7 +1,7 @@
 module MetasploitDataModels::ActiveRecordModels::ModuleMixin
   def self.included(base)
     base.class_eval{
-      base.table_name = "module_mixin"
+      base.table_name = "module_mixins"
       belongs_to :module_detail
       validate :name, :presence => true
     }
