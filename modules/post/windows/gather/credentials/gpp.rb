15a020dbdaa741beca9663f679198840e8bccc5f
Meatballs1 <campbell_ab@hotmail.com>
2012-06-22 11:36:27 +0100

    Clear EOL chars

--- modules/post/windows/gather/credentials/gpp.rb
+++ modules/post/windows/gather/credentials/gpp.rb
@@ -122,7 +122,7 @@ class Metasploit3 < Msf::Post
 					end
 				rescue Rex::Post::Meterpreter::RequestError => e
 					print_error "Received error code #{e.code} when reading #{tpath}"
-				end				
+				end
 			end
 		rescue Rex::Post::Meterpreter::RequestError => e
 			print_error "Received error code #{e.code} when reading #{path}"
@@ -195,7 +195,7 @@ class Metasploit3 < Msf::Post
 		padding = "=" * (4 - (encrypted_data.length % 4))
 		epassword = "#{encrypted_data}#{padding}"
 		decoded = Rex::Text.decode_base64(epassword)
-		
+
 		key = "\x4e\x99\x06\xe8\xfc\xb6\x6c\xc9\xfa\xf4\x93\x10\x62\x0f\xfe\xe8\xf4\x96\xe8\x06\xcc\x05\x79\x90\x20\x9b\x09\xa4\x33\xb6\x6c\x1b"
 		aes = OpenSSL::Cipher::Cipher.new("AES-256-CBC")
 		aes.decrypt
