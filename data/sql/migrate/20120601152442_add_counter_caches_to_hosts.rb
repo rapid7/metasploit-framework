fce5ff00ca02ecd5141769569ca298a3c20efcd6
HD Moore <hd_moore@rapid7.com>
2012-06-25 10:52:00 -0700

    Revert "Remove column reset - triggers issue with existing columns"
    This reverts commit f479fae6aa4b888321547ef54787f6577b670051.

--- data/sql/migrate/20120601152442_add_counter_caches_to_hosts.rb
+++ data/sql/migrate/20120601152442_add_counter_caches_to_hosts.rb
@@ -4,6 +4,12 @@ class AddCounterCachesToHosts < ActiveRecord::Migration
     add_column :hosts, :note_count, :integer, :default => 0
     add_column :hosts, :vuln_count, :integer, :default => 0
     add_column :hosts, :service_count, :integer, :default => 0
+  
+    Mdm::Host.all.each do |h|
+      Mdm::Host.reset_counters h.id, :notes
+      Mdm::Host.reset_counters h.id, :vulns
+      Mdm::Host.reset_counters h.id, :services
+    end
   end
 
   def self.down
