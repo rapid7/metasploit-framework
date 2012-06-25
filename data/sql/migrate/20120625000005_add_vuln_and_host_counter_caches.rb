f479fae6aa4b888321547ef54787f6577b670051
HD Moore <hd_moore@rapid7.com>
2012-06-25 10:20:48 -0700

    Remove column reset - triggers issue with existing columns

--- data/sql/migrate/20120625000005_add_vuln_and_host_counter_caches.rb
+++ data/sql/migrate/20120625000005_add_vuln_and_host_counter_caches.rb
@@ -3,17 +3,8 @@ class AddVulnAndHostCounterCaches < ActiveRecord::Migration
   def self.up
     add_column :hosts, :host_detail_count, :integer, :default => 0
   
-    Mdm::Host.all.each do |h|
-      Mdm::Host.reset_counters h.id, :host_detail_count
-    end
-
     add_column :vulns, :vuln_detail_count, :integer, :default => 0
     add_column :vulns, :vuln_attempt_count, :integer, :default => 0
-  
-    Mdm::Vuln.all.each do |h|
-      Mdm::Vuln.reset_counters h.id, :vuln_detail_count
-      Mdm::Vuln.reset_counters h.id, :vuln_attempt_count
-    end
 
   end
 
