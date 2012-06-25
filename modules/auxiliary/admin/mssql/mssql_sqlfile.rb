adf4b0ef870308e61013a2ecc57e838bad20c2a8
j0hnf <jf@tinternet.org.uk>
2012-06-25 18:35:23 +0200

    Exception handling code added, also stripped out the printing of the SQL file on read.

--- modules/auxiliary/admin/mssql/mssql_sqlfile.rb
+++ modules/auxiliary/admin/mssql/mssql_sqlfile.rb
@@ -9,47 +9,48 @@
 #   http://metasploit.com/
 ##
 
-require 'msf/core' 
+require 'msf/core'
 
 class Metasploit3 < Msf::Auxiliary
 
-	include Msf::Exploit::Remote::MSSQL
+        include Msf::Exploit::Remote::MSSQL
 
-	def initialize(info = {})
-		super(update_info(info,
-			'Name'           => 'MSSQL - Execute SQL from file',
-			'Description'    => %q{
-					This module will allow for multiple SQL queries contained within a specified 
-					file to be executed against a MSSQL instance given the appropiate credentials.
-			},
-			'Author'         => [ 'j0hn__f : <jf [at] tinternet dot org dot uk>' ],
-			'License'        => MSF_LICENSE,
-			'Version'        => '$Revision: 1 $'
-		))
+        def initialize(info = {})
+                super(update_info(info,
+                        'Name'           => 'MSSQL - Execute SQL from file',
+                        'Description'    => %q{
+                                        This module will allow for multiple SQL queries contained within a specified 
+                                        file to be executed against a MSSQL instance given the appropiate credentials.
+                        },
+                        'Author'         => [ 'j0hn__f : <jf[at]tinternet.org.uk>' ],
+                        'License'        => MSF_LICENSE,
+                        'Version'        => '$Revision: 1 $'
+                ))
 
-		register_options(
-			[
-				OptString.new('SQL_FILE', [ true, "file containing multiple SQL queries execute (one per line)"]),
-				OptString.new('QUERY_PREFIX', [ false, "string to append each line of the file",""]),
-				OptString.new('QUERY_SUFFIX', [ false, "string to prepend each line of the file",""])
-			], self.class)
-	end
+                register_options(
+                        [
+                                OptString.new('SQL_FILE', [ true, "file containing multiple SQL queries execute (one per line)"]),
+                                OptString.new('QUERY_PREFIX', [ false, "string to append each line of the file",""]),
+                                OptString.new('QUERY_SUFFIX', [ false, "string to prepend each line of the file",""])
+                        ], self.class)
+        end
 
 
-	def run
+        def run
 
-		print_status "> loaded the following SQL:"
-		queries = File.readlines(datastore['SQL_FILE'])
-		print_status queries.to_s
+                queries = File.readlines(datastore['SQL_FILE'])
 
-		prefix = datastore['QUERY_PREFIX']
-		suffix = datastore['QUERY_SUFFIX']
+                prefix = datastore['QUERY_PREFIX']
+                suffix = datastore['QUERY_SUFFIX']
 
-		queries.each do |sql_query|
-			mssql_query(prefix+sql_query.chomp+suffix,true) if mssql_login_datastore
-		end
-		disconnect
-	end
+                begin
+                        queries.each do |sql_query|
+                                mssql_query(prefix+sql_query.chomp+suffix,true) if mssql_login_datastore
+                        end
+                rescue Rex::ConnectionRefused, Rex::ConnectionTimeout
+                        print_error "Error connecting to server: #{$!}"
+                ensure
+                        disconnect
+                end
+        end
 end
-
-
