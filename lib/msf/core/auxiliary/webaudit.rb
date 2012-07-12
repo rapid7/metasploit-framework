61e8b4d8ff05187e99bc6cbecd3379e5dbe0cd88
Tasos Laskos <Tasos_Laskos@rapid7.com>
2012-07-12 22:22:22 +0300

    * converted spaces to tabs * added a formal WebTarget object to replace the Hash based one * general code cleanup

--- lib/msf/core/auxiliary/webaudit.rb
+++ lib/msf/core/auxiliary/webaudit.rb
@@ -13,307 +13,321 @@ module Msf
 ###
 
 module Auxiliary::WebAudit
-    include Msf::Auxiliary::Report
+	include Msf::Auxiliary::Report
 
-    attr_reader :target
-    attr_reader :parent
+	attr_reader :target
+	attr_reader :parent
 
-    def initialize(info = {})
-        super
-    end
+	def initialize(info = {})
+		super
+	end
 
-    #
-    # Called directly before 'run'
-    #
-    def setup( parent, target )
-        @parent = parent
-        @target = target
-    end
+	#
+	# Called directly before 'run'
+	#
+	def setup( parent, target )
+		@parent = parent
+		@target = target
+	end
 
-    def token
-        "xssmsfpro"
-    end
+	def token
+		"xssmsfpro"
+	end
 
-    def signature
-    end
+	def signature
+	end
 
-    def run
-        target[:auditable][:methods].each do |method|
-            audit_form_methods( method, target[:auditable][:params] )
-        end
-    end
+	def run
+		return if !signature.kind_of? ::Regexp
 
-    def audit_form_methods( method, params )
-        generate_parameters( method, params ).each do |form, param, pname|
-            resp = submit_request( method, param )
-            next if not resp
+		target.auditable[:methods].each do |method|
+			audit_form_methods( method, target.auditable[:params] )
+		end
+	end
 
-            if signature.kind_of? ::Regexp
-                m = resp.body.match( signature )
-                next if not (m and m.length > 1)
+	def audit_form_methods( method, params )
+		return if !signature.kind_of? ::Regexp
 
-                1.upto(m.length-1) do |mi|
-                    next if not m[mi]
-                    proof = m[mi].gsub(/[\r\n]/, ' ')
-                    process_vulnerability( method, form, param, pname, proof )
-                    break
-                end
+		generate_parameters( method, params ).each do |form, param, pname|
+			response = submit_request( method, param )
 
-            end
-        end
-    end
+			if proof = find_proof( response )
+				process_vulnerability( method, form, param, pname, proof )
+			end
+		end
+	end
 
-    def submit_request( method, params )
-        parent.request_count += 1
-        parent.update_audit_task_info
+	def find_proof( response )
+		m = response.body.match( signature )
+		return if !m || m.size < 1
 
-        retries = 0
-        begin
-            conn = http
+		1.upto( m.length - 1 ) { |i| return m[i].gsub( /[\r\n]/, ' ' ) if m[i] }
+		nil
+	end
 
-            # Configure the headers
-            headers = {
-                'User-Agent' => parent.datastore['UserAgent'] || "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
-                'Accept'     => "*/*",
-                'Host'       => target[:vhost]
-            }
+	def submit_request( method, params )
+		parent.increment_request_counter
 
-            if parent.datastore['HTTPCookie']
-                headers['Cookie'] = parent.datastore['HTTPCookie']
-            end
+		retries = 0
+		begin
+			conn = http
 
-            if parent.datastore['BasicAuthUser']
-                auth = [ parent.datastore['BasicAuthUser'].to_s + ":" + parent.datastore['BasicAuthPass'] ].pack("m*").gsub(/\s+/, '')
-                headers["Authorization"] ="Basic #{auth}\r\n"
-            end
+			# Configure the headers
+			headers = {
+				'User-Agent' => parent.datastore['UserAgent'] || 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+				'Accept'	 => '*/*',
+				'Host'	     => target.vhost
+			}
 
-            parent.datastore['HttpAdditionalHeaders'].to_s.split("\x01").each do |hdr|
-                next if not (hdr and hdr.strip.length > 0)
-                k,v = hdr.split(":", 2)
-                next if not v
-                headers[k.strip] = v.strip
-            end
+			if parent.datastore['HTTPCookie']
+				headers['Cookie'] = parent.datastore['HTTPCookie']
+			end
 
-            req  = nil
-            buff = ''
-            case method.upcase
-                when 'GET'
-                    buff = params.map{|param| Rex::Text.uri_encode(param[0].to_s) + "=" + Rex::Text.uri_encode(param[1].to_s)}.join("&")
-                    req = ::Net::HTTP::Get.new(target[:path] + "?" + buff, headers)
-                when 'POST'
-                    req = ::Net::HTTP::Post.new(target[:path], headers)
-                    req['Content-Type'] = "application/x-www-form-urlencoded"
-                    buff = params.map{|param| Rex::Text.uri_encode(param[0].to_s) + "=" + Rex::Text.uri_encode(param[1].to_s)}.join("&")
-                    req.body = buff
-                when 'PATH'
-                    buff = params[0][1]
-                    uri = target[:path].to_s.dup
-                    uri << "/" if uri[-1,1] != "/"
-                    uri << buff
-                    print_status("URI: #{uri}")
-                    req = ::Net::HTTP::Get.new(uri, headers)
-                else
-                    print_error("Unsupported method #{method}")
-                    return nil
-            end
+			if parent.datastore['BasicAuthUser']
+				auth = [ parent.datastore['BasicAuthUser'].to_s + ':' +
+					         parent.datastore['BasicAuthPass'] ].pack( 'm*' ).gsub( /\s+/, '' )
 
-            resp = conn.request(req)
+				headers['Authorization'] = "Basic #{auth}\r\n"
+			end
 
-            if resp
-                case resp.code.to_i
-                    when 200,404,301,302,303
-                        # print_status("    #{resp.code} - #{method.upcase} #{t[:path]} #{buff}")
-                    when 500,503,401,403
-                        print_good("    #{resp.code} - #{method.upcase} #{target[:path]} #{buff}")
-                    else
-                        print_error("    #{resp.code} - #{method.upcase} #{target[:path]} #{buff}")
-                end
-            end
+			parent.datastore['HttpAdditionalHeaders'].to_s.split( "\x01" ).each do |hdr|
+				next if !( hdr && hdr.strip.size > 0 )
 
-            return resp
+				k, v = hdr.split( ':', 2 )
+				next if !v
 
-                # Some CGI servers just spew errors without headers, we need to process these anyways
-        rescue ::Net::HTTPBadResponse, ::Net::HTTPHeaderSyntaxError => e
-            print_status("Error processing response for #{target.to_url} #{e.class} #{e} ")
-            return
-        rescue ::Exception => e
-            retries += 1
-            retry if retries < 3
+				headers[k.strip] = v.strip
+			end
 
-            print_error("Maximum retry count for #{target.to_url} reached (#{e})")
-            return
-        end
-    end
+			req  = nil
+			buff = ''
+			case method.upcase
 
-    #
-    # This method generates parameters for form testing. The
-    # returned array is in the form of:
-    # [ [regex-signature,
-    #   [ [param1, param_value_1],
-    #	  [param2, param_value_2] ..
-    #   ], ... ],
-    #   [regex-signature, ...
-    # ]
-    #
-    def generate_parameters( method, params )
-        return generate_parameters_path_info( params ) if method == 'PATH'
+				when 'GET'
+					buff = params.map do |param|
+						Rex::Text.uri_encode( param[0].to_s ) + '=' +
+							Rex::Text.uri_encode( param[1].to_s )
+					end.join( '&' )
 
-        values = []
-        fcnt = 0
+					req = ::Net::HTTP::Get.new( "#{target.path}?#{buff}", headers )
 
-        # Walk through all discovered form instances for this method
-        self.target[:forms].each do |form|
-            fcnt += 1
-            break if fcnt > 5
+				when 'POST'
+					req = ::Net::HTTP::Post.new( target.path, headers)
+					req['Content-Type'] = 'application/x-www-form-urlencoded'
 
-            # Skip methods that don't apply to this pass
-            next if form.method.upcase != method
+					buff = params.map do |param|
+						Rex::Text.uri_encode( param[0].to_s) + '=' +
+							Rex::Text.uri_encode( param[1].to_s )
+					end.join( '&' )
 
-            # Initialize the list of options from this form
-            options = form.params.map{|x| x[0] } + ["unknown#{self.token}"]
-            options.delete(nil)
-            options.delete("")
+					req.body = buff
 
-            # Create the defaults for this specific form
-            defaults = {}
-            form.params.each do |opt|
-                next if not opt[0]
-                defaults[opt[0]] = opt[1]
-            end
+				when 'PATH'
+					buff = params[0][1]
 
-            # Create a baseline to clone for value permutation
-            baseline = []
-            options.each do |oname|
-                baseline << [oname, defaults[oname].to_s]
-            end
+					uri = target.path.to_s.dup
+					uri << "/" if uri[-1,1] != '/'
+					uri << buff
 
-            # Starting with this specific form and option set, walk through all known options for each item
-            options.each_index do |idx|
-                ovalues = [ baseline[idx][1] ]
-                if params[options[idx]] and params[options[idx]][:values] and params[options[idx]][:values].keys.length > 0
-                    ovalues = params[options[idx]][:values].keys
-                end
+					print_status "URI: #{uri}"
+					req = ::Net::HTTP::Get.new( uri, headers )
 
-                # This replaces the previous step, limiting the test to smaller range of values
-                ovalues = [ defaults[baseline[idx][0]].to_s ]
-                ovalues.delete(nil)
-                ovalues.uniq!
+				else
+					print_error "Unsupported method #{method}"
+					return nil
+			end
 
-                ovalues.each do |ovalue|
-                    seeds_for( ovalue ).each do |input|
-                        input = input
-                        tparams = []
+			if resp = conn.request( req )
+				case resp.code.to_i
+					when 200,404,301,302,303
+						# print_status("	#{resp.code} - #{method.upcase} #{t[:path]} #{buff}")
+					when 500,503,401,403
+						print_good  "    #{resp.code} - #{method.upcase} #{target.path} #{buff}"
+					else
+						print_error "    #{resp.code} - #{method.upcase} #{target.path} #{buff}"
+				end
+			end
 
-                        baseline.each_index do |bdx|
-                            tparams << [ baseline[bdx][0], (idx == bdx) ? input : baseline[bdx][1] ]
-                        end
+			resp
+		# Some CGI servers just spew errors without headers, we need to process these anyways
+		rescue ::Net::HTTPBadResponse, ::Net::HTTPHeaderSyntaxError => e
+			print_status "Error processing response for #{target.to_url} #{e.class} #{e} "
+			return
+		rescue ::Exception => e
+			retries += 1
+			retry if retries < 3
 
-                        # Insert a fake value for this signature in order to catch false positives
-                        # XXX: Need some way to skip related values when this fires
-                        # values << [sig, baseline, -1]
+			print_error "Maximum retry count for #{target.to_url} reached (#{e})"
+			return
+		end
+	end
 
-                        # Insert the real value
-                        values << [form, tparams, baseline[idx][0]]
-                    end
+	#
+	# This method generates parameters for form testing. The
+	# returned array is in the form of:
+	# [ [regex-signature,
+	#   [ [param1, param_value_1],
+	#	  [param2, param_value_2] ..
+	#   ], ... ],
+	#   [regex-signature, ...
+	# ]
+	#
+	def generate_parameters( method, params )
+		return generate_parameters_path_info( params ) if method == 'PATH'
 
-                    # De-duplicate values (especially the false positive tests)
-                    values.uniq!
-                end
-            end
-        end
+		values = []
+		fcnt = 0
 
-        values
-    end
+		# Walk through all discovered form instances for this method
+		target.forms.each do |form|
+			fcnt += 1
+			break if fcnt > 5
 
-    #
-    # This method generates malicious path_info values for form testing. The
-    # returned array is in the form of: [ [regex-signature, "param-string"], ... ]
-    #
-    def generate_parameters_path_info( params )
-        values  = []
-        sigs    = []
+			# Skip methods that don't apply to this pass
+			next if form.method.upcase != method
 
-        options = params[:path_info][:values].keys + ['Unknown']
+			# Initialize the list of options from this form
+			options = form.params.map{|x| x[0] } + ["unknown#{token}"]
+			options.delete( nil )
+			options.delete( '' )
 
-        form = target[:forms].first
+			# Create the defaults for this specific form
+			defaults = {}
+			form.params.each do |opt|
+				next if not opt[0]
+				defaults[opt[0]] = opt[1]
+			end
 
-        options.each do |input|
-            seeds_for( input ).each do |input|
-                sig, input = input
-                values << [form, sig, [['PATH', input]], :path]
-            end
-        end
+			# Create a baseline to clone for value permutation
+			baseline = []
+			options.each { |oname| baseline << [ oname, defaults[oname].to_s ] }
 
-        values.uniq
-    end
+			# Starting with this specific form and option set, walk through all known options for each item
+			options.each_index do |idx|
+				ovalues = [ baseline[idx][1] ]
 
-    def http
-        host = target[:host]
-        port = target[:port]
-        proxy_host = proxy_port = proxy_user = proxy_pass = nil
+				if params[options[idx]] && params[options[idx]][:values] &&
+					params[options[idx]][:values].keys.length > 0
+					ovalues = params[options[idx]][:values].keys
+				end
 
-        http = ::Net::HTTP.new(host, port, proxy_host, proxy_port, proxy_user, proxy_pass)
-        if target[:ssl]
-            http.use_ssl = true
-            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        end
+				# This replaces the previous step, limiting the test to smaller range of values
+				ovalues = [ defaults[baseline[idx][0]].to_s ]
+				ovalues.delete( nil )
+				ovalues.uniq!
 
-        http
-    end
+				ovalues.each do |ovalue|
+					seeds_for( ovalue ).each do |input|
+						input = input
+						tparams = []
 
-    def calculate_confidence( vuln )
-        100
-    end
+						baseline.each_index do |bdx|
+							tparams << [ baseline[bdx][0], (idx == bdx) ? input : baseline[bdx][1] ]
+						end
 
-    def process_vulnerability( method, form, param, pname, proof )
-        mode  = details[:category].to_sym
-        vhash = [target.to_url, mode, pname].map{|x| x.to_s}.join("|")
+						# Insert a fake value for this signature in order to catch false positives
+						# XXX: Need some way to skip related values when this fires
+						# values << [sig, baseline, -1]
 
-        parent.vulns[mode] ||= {}
-        return parent.vulns[mode][vhash] if parent.vulns[mode][vhash]
+						# Insert the real value
+						values << [ form, tparams, baseline[idx][0] ]
+					end
 
-        parent.vulns[mode][vhash] = {
-            :target => target,
-            :method => method,
-            :params => param,
-            :mode   => mode,
-            :pname  => pname,
-            :proof  => proof,
-            :form   => form,
-            :risk     => details[:risk],
-            :name     => details[:name],
-            :blame    => details[:blame],
-            :category => details[:category],
-            :description => details[:description]
-        }
+					# De-duplicate values (especially the false positive tests)
+					values.uniq!
+				end
+			end
+		end
 
-        confidence = calculate_confidence( parent.vulns[mode][vhash] )
+		values
+	end
 
-        parent.vulns[mode][vhash].merge!( confidence: confidence )
+	#
+	# This method generates malicious path_info values for form testing. The
+	# returned array is in the form of: [ [regex-signature, "param-string"], ... ]
+	#
+	def generate_parameters_path_info( params )
+		values  = []
+		sigs	= []
 
-        info = {
-            :web_site => form.web_site,
-            :path     => form.path,
-            :query    => form.query,
-            :method   => method,
-            :params   => param,
-            :pname    => pname,
-            :proof    => proof,
-            :risk     => details[:risk],
-            :name     => details[:name],
-            :blame    => details[:blame],
-            :category => details[:category],
-            :description => details[:description],
-            :confidence  => confidence
-        }
+		options = params[:path_info][:values].keys + %w(Unknown)
 
-        report_web_vuln(info)
+		options.each do |input|
+			seeds_for( input ).each do |input|
+				sig, input = input
+				values << [ target.forms.first, sig, [ ['PATH', input] ], :path ]
+			end
+		end
 
-        print_good("    VULNERABLE(#{mode.to_s.upcase}) URL(#{target.to_url}) PARAMETER(#{pname}) VALUES(#{param.inspect})")
-        print_good("         PROOF( #{proof} )")
+		values.uniq
+	end
 
-        return
-    end
+	def http
+		proxy_host = proxy_port = proxy_user = proxy_pass = nil
+
+		http = ::Net::HTTP.new( target.host, target.port, proxy_host,
+		                        proxy_port, proxy_user, proxy_pass )
+		if target.ssl
+			http.use_ssl     = true
+			http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+		end
+
+		http
+	end
+
+	def calculate_confidence( vuln )
+		100
+	end
+
+	def process_vulnerability( method, form, param, pname, proof )
+		mode  = details[:category].to_sym
+		vhash = [target.to_url, mode, pname].map{|x| x.to_s}.join("|")
+
+		parent.vulns[mode] ||= {}
+		return parent.vulns[mode][vhash] if parent.vulns[mode][vhash]
+
+		parent.vulns[mode][vhash] = {
+			:target      => target,
+			:method      => method,
+			:params      => param,
+			:mode        => mode,
+			:pname       => pname,
+			:proof       => proof,
+			:form        => form,
+			:risk	     => details[:risk],
+			:name	     => details[:name],
+			:blame	     => details[:blame],
+			:category    => details[:category],
+			:description => details[:description]
+		}
+
+		confidence = calculate_confidence( parent.vulns[mode][vhash] )
+
+		parent.vulns[mode][vhash].merge!( confidence: confidence )
+
+		info = {
+			:web_site    => form.web_site,
+			:path	     => form.path,
+			:query	     => form.query,
+			:method      => method,
+			:params      => param,
+			:pname	     => pname,
+			:proof	     => proof,
+			:risk	     => details[:risk],
+			:name	     => details[:name],
+			:blame	     => details[:blame],
+			:category    => details[:category],
+			:description => details[:description],
+			:confidence  => confidence
+		}
+
+		report_web_vuln( info )
+
+		print_good "	VULNERABLE(#{mode.to_s.upcase}) URL(#{target.to_url}) PARAMETER(#{pname}) VALUES(#{param.inspect})"
+		print_good "		 PROOF( #{proof} )"
+
+		return
+	end
 
 end
 end
