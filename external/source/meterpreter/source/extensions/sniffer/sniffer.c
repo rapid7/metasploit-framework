5907f18a91089829b644907720a3c2ad63bbcfc5
m m <gaspmat@gmail.com>
2012-07-15 23:52:11 +0200

    netstat and arp commands in win32/posix meterpreter

--- external/source/meterpreter/source/extensions/sniffer/sniffer.c
+++ external/source/meterpreter/source/extensions/sniffer/sniffer.c
@@ -1098,7 +1098,6 @@ DWORD __declspec(dllexport) InitServerExtension(Remote *remote)
 {
 	DWORD index;
 
-	hMetSrv = remote->hMetSrv;
 
 	dprintf("[SERVER] Registering command handlers...");
 	for (index = 0; customCommands[index].method; index++) {
@@ -1112,6 +1111,7 @@ DWORD __declspec(dllexport) InitServerExtension(Remote *remote)
 	memset(open_captures, 0, sizeof(open_captures));
 
 #ifdef _WIN32
+	hMetSrv = remote->hMetSrv;
 	// initialize structures for the packet sniffer sdk
 	hMgr = NULL;
 	hErr = 0;
