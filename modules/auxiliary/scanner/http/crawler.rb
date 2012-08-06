70e591bcd3f8603c01806d356473b814f00860a8
Tasos Laskos <Tasos_Laskos@rapid7.com>
2012-08-06 21:04:43 +0300

    * added #form_from_url * forms are now extracted from the seed URI

--- modules/auxiliary/scanner/http/crawler.rb
+++ modules/auxiliary/scanner/http/crawler.rb
@@ -35,6 +35,16 @@ class Metasploit3 < Msf::Auxiliary
 	end
 =end
 
+	def run
+		super
+
+		if form = form_from_url( @current_site, datastore['URI'] )
+			print_status((" " * 24) + "FORM: #{form[:method]} #{form[:path]}")
+			report_web_form( form )
+			self.form_count += 1
+		end
+	end
+
 	#
 	# The main callback from the crawler, redefines crawler_process_page() as
 	# defined by Msf::Auxiliary::HttpCrawler
@@ -111,49 +121,10 @@ class Metasploit3 < Msf::Auxiliary
 		# Apache multiview directories
 		return if page.url.query =~ /^C=[A-Z];O=/ # Apache
 
-		# Scrub out the jsessionid appends
-		page.url.path = page.url.path.sub(/;jsessionid=[a-zA-Z0-9]+/, '')
-
-		#
-		# Continue processing forms
-		#
 		forms = []
-		form_template = { :web_site => t[:site] }
-		form  = {}.merge(form_template)
-
-		# This page has a query parameter we can test with GET parameters
-		# ex: /test.php?a=b&c=d
-		if page.url.query and not page.url.query.empty?
-			form[:method] = 'GET'
-			form[:path]   = page.url.path
-			vars = page.url.query.split('&').map{|x| x.split("=", 2) }
-			form[:params] = vars
+		if form = form_from_url( t[:site], page.url )
+			forms << form
 		end
-
-		# This is a REST-ish application with numeric parameters
-		# ex: /customers/343
-		if not form[:path] and page.url.path.to_s =~ /(.*)\/(\d+)$/
-			path_base = $1
-			path_info = $2
-			form[:method] = 'PATH'
-			form[:path]   = path_base
-			form[:params] = [['PATH', path_info]]
-			form[:query]  = page.url.query.to_s
-		end
-
-		# This is an application that uses PATH_INFO for parameters:
-		# ex:  /index.php/Main_Page/Article01
-		if not form[:path] and page.url.path.to_s =~ /(.*\/[a-z0-9A-Z]{3,256}\.[a-z0-9A-Z]{2,8})(\/.*)/
-			path_base = $1
-			path_info = $2
-			form[:method] = 'PATH'
-			form[:path]   = path_base
-			form[:params] = [['PATH', path_info]]
-			form[:query]  = page.url.query.to_s
-		end
-
-		# Done processing URI-based forms
-		forms << form
 
 		if page.doc
 			page.doc.css("form").each do |f|
@@ -226,4 +197,52 @@ class Metasploit3 < Msf::Auxiliary
 			self.form_count += 1
 		end
 	end
+
+	def form_from_url( website, url )
+		url = URI( url.to_s ) if !url.is_a?( URI )
+
+		# Scrub out the jsessionid appends
+		url.path = url.path.sub(/;jsessionid=[a-zA-Z0-9]+/, '')
+
+		#
+		# Continue processing forms
+		#
+		forms = []
+		form_template = { :web_site => website }
+		form  = {}.merge(form_template)
+
+		# This page has a query parameter we can test with GET parameters
+		# ex: /test.php?a=b&c=d
+		if url.query and not url.query.empty?
+			form[:method] = 'GET'
+			form[:path]   = url.path
+			vars = url.query.split('&').map{|x| x.split("=", 2) }
+			form[:params] = vars
+		end
+
+		# This is a REST-ish application with numeric parameters
+		# ex: /customers/343
+		if not form[:path] and url.path.to_s =~ /(.*)\/(\d+)$/
+			path_base = $1
+			path_info = $2
+			form[:method] = 'PATH'
+			form[:path]   = path_base
+			form[:params] = [['PATH', path_info]]
+			form[:query]  = url.query.to_s
+		end
+
+		# This is an application that uses PATH_INFO for parameters:
+		# ex:  /index.php/Main_Page/Article01
+		if not form[:path] and url.path.to_s =~ /(.*\/[a-z0-9A-Z]{3,256}\.[a-z0-9A-Z]{2,8})(\/.*)/
+			path_base = $1
+			path_info = $2
+			form[:method] = 'PATH'
+			form[:path]   = path_base
+			form[:params] = [['PATH', path_info]]
+			form[:query]  = url.query.to_s
+		end
+
+		form[:method] ? form : nil
+	end
+
 end
