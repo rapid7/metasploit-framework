cd5ea1ab8040ad21f9826bc5535ad3170d58daf5
Daniel Miller <bonsaiviking@gmail.com>
2012-08-10 13:34:09 -0500

    Fix handling of PAYLOAD_TYPE in persistence
    post/windows/manage/persistence incorrectly checked the STARTUP option
    to set the payload, which meant it was always the default (reverse_tcp).
    Changed to check PAYLOAD_TYPE instead, as intended.

--- modules/post/windows/manage/persistence.rb
+++ modules/post/windows/manage/persistence.rb
@@ -101,7 +101,7 @@ class Metasploit3 < Msf::Post
 			end
 
 			# Set the proper payload
-			case datastore['STARTUP']
+			case datastore['PAYLOAD_TYPE']
 			when /TCP/i
 				payload = "windows/meterpreter/reverse_tcp"
 			when /HTTP/i
