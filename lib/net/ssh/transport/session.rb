33c74c97e2cb5cbddb06e4735b582b2b8d10b043
RageLtMan <rageltman [at] sempervictus>
2012-08-12 16:23:22 -0400

    Add Opt::Proxies and opthash[:proxies] to ssh mods

--- lib/net/ssh/transport/session.rb
+++ lib/net/ssh/transport/session.rb
@@ -74,7 +74,7 @@ module Net; module SSH; module Transport
           Rex::Socket::Tcp.create(
           	'PeerHost' => @host,
           	'PeerPort' => @port,
-		'Proxies' => options[:proxies],
+            'Proxies' => options[:proxies],
           	'Context'  => {
           	  'Msf'          => options[:msframework],
           	  'MsfExploit'   => options[:msfmodule]
