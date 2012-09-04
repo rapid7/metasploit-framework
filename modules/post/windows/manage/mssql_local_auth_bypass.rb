ba0de5acd92e414ba47dfb791b98de35d071c576
Tod Beardsley <todb@metasploit.com>
2012-09-04 15:38:42 -0500

    Retitled for consistency and accuracy

--- modules/post/windows/manage/mssql_local_auth_bypass.rb
+++ modules/post/windows/manage/mssql_local_auth_bypass.rb
@@ -9,7 +9,7 @@ class Metasploit3 < Msf::Post
 
 	def initialize(info={})
 		super( update_info( info,
-				'Name'          => 'SQL Server - Local Authorization Bypass',
+				'Name'          => 'Windows Manage Local Microsoft SQL Server Authorization Bypass',
 				'Description'   => %q{ When this module is executed via an existing
 				meterpreter session it can be used to add a sysadmin to local
 				SQL Server instances.  It first attempts to gain LocalSystem privileges
