56d5c1375545f7e34247b061b460d79d819e9de6
kernelsmith <GitHub@kernelsmith>
2012-09-20 12:23:48 -0500

    adds -s <string> Execute the specified string as console commands to msfconsole
    for convenience when you don't need/want a full resource file, you just
    want to run something quick
    example usage:
    # say you have a saved config ready to go on load
    ./msfconsole -s 'exploit -j'
    # you can run multiple commands too
    ./msfconsole -s 'set ConsoleLogging true\nshow options'

--- lib/msf/ui/console/driver.rb
+++ lib/msf/ui/console/driver.rb
@@ -249,6 +249,13 @@ class Driver < Msf::Ui::Driver
 			# If the opt is nil here, we load ~/.msf3/msfconsole.rc
 			load_resource(opts['Resource'])
 		end
+
+		# Process any additional startup commands
+		if opts['SCommands'] and opts['SCommands'].kind_of? Array
+			opts['SCommands'].each { |c|
+				run_single(c)
+			}
+		end
 	end
 
 	#
