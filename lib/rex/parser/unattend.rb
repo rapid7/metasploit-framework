62d1a8b1f8c39015e990f981bd29d45d1f0638b2
Meatballs <eat_meatballs@hotmail.co.uk>
2012-10-10 22:25:53 +0100

    Reporting added, refactored unattend parser and enum_unattend

--- lib/rex/parser/unattend.rb
+++ lib/rex/parser/unattend.rb
@@ -6,7 +6,7 @@ module Parser
 class Unattend
 
 	def self.parse(xml)
-                       tables = []
+                       results = []
                        unattend = xml.elements['unattend']
                        return if unattend.nil?
 
@@ -14,13 +14,12 @@ class Unattend
                                 next if settings.class != REXML::Element
                                 settings.get_elements('component').each do |c|
                                         next if c.class != REXML::Element
-                                        tables << extract_useraccounts(c.elements['UserAccounts'])
-                                        tables << extract_autologon(c.elements['AutoLogon'])
-                                        tables << extract_deployment(c.elements['WindowsDeploymentServices'])
+                                        results << extract_useraccounts(c.elements['UserAccounts'])
+                                        results << extract_autologon(c.elements['AutoLogon'])
+                                        results << extract_deployment(c.elements['WindowsDeploymentServices'])
                                 end
                         end
-		
-		return tables
+		return results
 	end
 
         #
@@ -40,15 +39,7 @@ class Unattend
                         password = password.gsub(/#{Rex::Text.to_unicode('Password')}$/, '')
                 end
 
-                table = Rex::Ui::Text::Table.new({
-                        'Header' => 'WindowsDeploymentServices',
-                        'Indent' => 1,
-                        'Columns' => ['Domain', 'Username', 'Password']
-                })
-
-                table << [domain, username, password]
-                
-		return [table]
+		return {'type' => 'wds', 'domain' => domain, 'username' => username, 'password' => password }
         end
 
         #
@@ -67,16 +58,8 @@ class Unattend
                         password = password.gsub(/#{Rex::Text.to_unicode('Password')}$/, '')
                 end
 
-                table = Rex::Ui::Text::Table.new({
-                        'Header' => 'AutoLogon',
-                        'Indent' => 1,
-                        'Columns' => ['Domain', 'Username', 'Password']
-                })
-
-                table << [domain, username, password]
-
-                return [table]
-        end
+	        return {'type' => 'auto', 'domain' => domain, 'username' => username, 'password' => password }
+	end
 
         #
         # Extract sensitive data from UserAccounts
@@ -84,7 +67,7 @@ class Unattend
         def self.extract_useraccounts(user_accounts)
                 return[] if user_accounts.nil?
 
-                cred_tables = []
+                results = []
                 account_types = ['AdministratorPassword', 'DomainAccounts', 'LocalAccounts']
                 account_types.each do |t|
                         element = user_accounts.elements[t]
@@ -95,12 +78,6 @@ class Unattend
                         # Extract the password from AdministratorPasswords
                         #
                         when account_types[0]
-                                table = Rex::Ui::Text::Table.new({
-                                        'Header'  => 'AdministratorPasswords',
-                                        'Indent'  => 1,
-                                        'Columns' => ['Username', 'Password']
-                                })
-
                                 password = element.elements['Value'].get_text.value rescue ''
                                 plaintext = element.elements['PlainText'].get_text.value rescue 'true'
 
@@ -110,8 +87,7 @@ class Unattend
                                 end
 
                                 if not password.empty?
-                                        table << ['Administrator', password]
-                                        cred_tables << table
+                                        results << {'type' => 'admin', 'username' => 'Administrator', 'password' => password}
                                 end
 
                         #
@@ -119,30 +95,16 @@ class Unattend
                         # According to MSDN, unattend.xml doesn't seem to store passwords for domain accounts
                         #
                         when account_types[1]  #DomainAccounts
-                                table = Rex::Ui::Text::Table.new({
-                                        'Header'  => 'DomainAccounts',
-                                        'Indent'  => 1,
-                                        'Columns' => ['Username', 'Group']
-                                })
                                 element.elements.each do |account_list|
                                         name = account_list.elements['DomainAccount/Name'].get_text.value rescue ''
                                         group = account_list.elements['DomainAccount/Group'].get_text.value rescue 'true'
 
-                                        table << [name, group]
+                                        results << {'type' => 'domain', 'username' => name, 'group' => group}
                                 end
-
-                                cred_tables << table if not table.rows.empty?
-
                         #
                         # Extract the username/password from LocalAccounts
                         #
                         when account_types[2]  #LocalAccounts
-                                table = Rex::Ui::Text::Table.new({
-                                        'Header'  => 'LocalAccounts',
-                                        'Indent'  => 1,
-                                        'Columns' => ['Username', 'Password']
-                                })
-
                                 element.elements.each do |local|
                                         password = local.elements['Password/Value'].get_text.value rescue ''
                                         plaintext = local.elements['Password/PlainText'].get_text.value rescue 'true'
@@ -153,14 +115,12 @@ class Unattend
                                         end
 
                                         username = local.elements['Name'].get_text.value rescue ''
-                                        table << [username, password]
+                                        results << {'type' => 'local', 'username' => username, 'password' => password}
                                 end
-
-                                cred_tables << table if not table.rows.empty?
                         end
                 end
 
-                return cred_tables
+                return results
         end
 
 end
