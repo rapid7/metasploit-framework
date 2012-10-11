c896e8578e66000ac51b5147dfa7ba27b5fe897d
Tod Beardsley <todb@metasploit.com>
2012-10-11 11:50:08 -0500

    Starting again now with OO

--- msfswitch.rb
+++ msfswitch.rb
@@ -1,61 +1,53 @@
 #!/usr/bin/env ruby
 
+# The goal here is to switch out the legacy www.metasploit.com
+# source and switch over to Github with minimum fuss and minimum
+# magic.
+#
+# This is potentially destructive. Most people shouldn't care too
+# much though.
+#
+# Here's what happens.
+# We check out the new subversion repo to a local subdir
+# We catch a list of everything that's not checked in.
+# We move that stuff down to the subdir
+# We delete everything except the new checkout and this file.
+# We move everything in the subdir up one level, except this file.
+# Celebrate!
+
+
 require 'find'
 require 'fileutils'
 
-new_subdir = "ghnew-1234"
+class SvnConfig
 
-msfbase = '/tmp/svn-tests/metasploit-svn/trunk' + File::SEPARATOR
-new_checkout = File.join(msfbase, new_subdir, File::SEPARATOR)
-i = 0
+	SEP = File::SEPARATOR
 
-new_svn_dirs = []
-old_svn_dirs = []
-puts "Finding .svn dirs"
-Find.find(msfbase) do |path|
-	next unless File.directory? path
-	next unless path =~ /\.svn$/
-	if path =~ /ghnew-1234/
-		fullpath = File.expand_path(path)
-		new_svn_dirs << fullpath
-		next
+	def initialize(i=nil)
+		@i = i
+		new_checkout
 	end
-	fullpath = File.expand_path(path)
-	old_svn_dirs << fullpath
-end
 
-puts "Old: #{old_svn_dirs.size} New: #{new_svn_dirs.size}"
-
-puts "Matching up source and dest..."
-
-new_svn_dirs.each do |new_path|
-	old_svn_dirs.each_with_index do |old_path,i|
-		if old_path == new_path.gsub(/#{new_subdir + File::SEPARATOR}/,"")
-			FileUtils.rm_rf(old_path)
-			FileUtils.cp_r(new_path, old_path)
-			puts "Copied #{new_path}"
-			old_svn_dirs.delete_at i
-			break
+	def github_checkout_subdir
+		if @i.kind_of? Integer
+			"msf-github-#{@i}"
+		else
+			"msf-github-#{rand(2**16)}"
 		end
 	end
+
+	def msfbase
+		base = __FILE__
+		while File.symlink?(base)
+			base = File.expand_path(File.readlink(base), File.dirname(base))
+		end
+		File.dirname(base)
+	end
+
+	def new_checkout
+		@new_checkout ||= File.expand_path(
+			File.join(msfbase, github_checkout_subdir, SEP)
+		)
+	end
+
 end
-
-puts "Deleting the remaining empty dirs"
-
-old_svn_dirs.each do |old_path|
-	FileUtils.rm_rf old_path.gsub(/.svn$/,"")
-end
-
-puts "Removing the temp checkout"
-
-FileUtils.rm_rf new_checkout
-
-puts "SVN reverting against GitHub"
-
-puts "Base is #{msfbase}"
-
-res = system("svn", "revert", "--recursive", msfbase )
-puts "Results: #{res.inspect}"
-res = system("svn", "update", "--recursive", msfbase )
-puts "Results: #{res.inspect}"
-
