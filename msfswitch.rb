980654b35db7a461e5da0a247b1caabc04566370
Tod Beardsley <todb@metasploit.com>
2012-10-13 17:30:10 -0500

    Finalizing msfswitch.rb

--- msfswitch.rb
+++ msfswitch.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/env ruby
 
+# TODO: Allow the user to specify the msfbase, might be useful
+# TODO: More or less verbosity? Some steps are silent, others are verbose.
+# TODO: Need to test on basically everything, since this is a one-way trip
+# TODO: Consider an Are You Sure confirmation.
+
 start_time = Time.now.utc
 
 msfbase = __FILE__
@@ -9,14 +14,15 @@ end
 
 $:.unshift(File.expand_path(File.join(File.dirname(msfbase), 'lib')))
 
-require 'msf/util/switch'
 require 'msf/util/svn'
-unless Msf::Util::SVN.root =~ /\.metasploit\.com/
+unless Msf::Util::SVN.root =~ /^http.+metasploit\.com/
 	$stdout.puts "[-] This is not an anonymous SVN checkout, aborting."
 	exit 1
 end
 
-@svn_switcher = Msf::Util::SvnSwitch.new(1234)
+require 'msf/util/switch'
+
+@svn_switcher = Msf::Util::SvnSwitch.new
 
 $stdout.puts "[*]"
 $stdout.puts "[*] Switching Metasploit Framework to the official GitHub SVN repo."
@@ -44,10 +50,10 @@ $stdout.puts "[*] Cleaning up"
 @svn_switcher.system :cleanup_current_cmd
 @svn_switcher.system :revert_gemfile_current_cmd
 @svn_switcher.cleanup_empty_dirs { |f| $stdout.puts "[*]  Removing empty dir #{f}" }
-@svn_switcher.system :info_cmd
-# $stdout.puts "[*] Deleting the temporary Git checkout."
-# @svn_switcher.delete_new_svn_checkout
+$stdout.puts "[*] Deleting the temporary Git checkout."
+@svn_switcher.delete_new_svn_checkout
 $stdout.puts "[+] Conversion complete!"
+@svn_switcher.status_current_cmd
 
 end_time = Time.now.utc - start_time
 $stdout.puts "[*] Time elapsed: %0.2fm" % (end_time / 60.0)
