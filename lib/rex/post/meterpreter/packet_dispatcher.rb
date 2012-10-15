45a60b6bdda60d93101c602b7c924a97fa6bb4f5
James Lee <egypt@metasploit.com>
2012-10-15 17:53:15 -0500

    Clarify why we need to sleep.
    [Closes #911][FixRM #7344]

--- lib/rex/post/meterpreter/packet_dispatcher.rb
+++ lib/rex/post/meterpreter/packet_dispatcher.rb
@@ -367,7 +367,13 @@ module PacketDispatcher
 					end
 				end
 
-				# detect a CPU starvation condition and force a sleep to mitigate it
+				# If the backlog and incomplete arrays are the same, it means
+				# dispatch_inbound_packet wasn't able to handle any of the
+				# packets. When that's the case, we can get into a situation
+				# where @pqueue is not empty and, since nothing else bounds this
+				# loop, we spin CPU trying to handle packets that can't be
+				# handled. Sleep here to treat that situation as though the
+				# queue is empty.
 				if (backlog.length > 0 && backlog.length == incomplete.length)
 					::IO.select(nil, nil, nil, 0.10)
 				end
