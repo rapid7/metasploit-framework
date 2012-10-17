1f55e0253517f6c60864dbd8afd4c9e2e2e098b6
jvazquez-r7 <juan.vazquez@metasploit.com>
2012-10-17 22:21:28 +0200

    minor cleanup

--- modules/auxiliary/admin/mssql/mssql_ntlm_stealer_sqli.rb
+++ modules/auxiliary/admin/mssql/mssql_ntlm_stealer_sqli.rb
@@ -2,31 +2,31 @@ require 'msf/core'
 
 class Metasploit3 < Msf::Auxiliary
 
-	include Rex::Text
 	include Msf::Exploit::Remote::MSSQL_SQLI
 
 	def initialize(info = {})
 		super(update_info(info,
 			'Name'           => 'Microsoft SQL Server NTLM Stealer - SQLi',
 			'Description'    => %q{
-				This module can be used to help capture or relay the LM/NTLM
-				credentials of the account running the remote SQL Server service.
-				The module will use the SQL injection from GET_PATH to connect to the
-				target SQL Server instance and execute the native "xp_dirtree" or
-				stored procedure.   The stored procedures will then
-				force the service account to authenticate to the system defined in
-				the SMBProxy option. In order for the attack to be successful, the
-				SMB capture or relay module must be running on the system defined
-				as the SMBProxy.  The database account used to connect to the
-				database should only require the "PUBLIC" role to execute.
-				Successful execution of this attack usually results in local
-				administrative access to the Windows system.  Specifically, this
-				works great for relaying credentials between two SQL Servers using
-				a shared service account to get shells.  However, if the relay fails,
-				then the LM hash can be reversed using the Halflm rainbow tables and
-				john the ripper.
+				This module can be used to help capture or relay the LM/NTLM credentials of the
+				account running the remote SQL Server service. The module will use the SQL
+				injection from GET_PATH to connect to the target SQL Server instance and execute
+				the native "xp_dirtree" or stored procedure.   The stored procedures will then
+				force the service account to authenticate to the system defined in the SMBProxy
+				option. In order for the attack to be successful, the SMB capture or relay module
+				must be running on the system defined as the SMBProxy. The database account used to
+				connect to the database should only require the "PUBLIC" role to execute.
+				Successful execution of this attack usually results in local administrative access
+				to the Windows system.  Specifically, this works great for relaying credentials
+				between two SQL Servers using a shared service account to get shells.  However, if
+				the relay fails, then the LM hash can be reversed using the Halflm rainbow tables
+				and john the ripper.
 			},
-			'Author'         => [ 'nullbind <scott.sutherland[at]netspi.com>', 'Antti <antti.rantasaari[at]netspi.com>' ],
+			'Author'         =>
+				[
+					'nullbind <scott.sutherland[at]netspi.com>',
+					'Antti <antti.rantasaari[at]netspi.com>'
+				],
 			'License'        => MSF_LICENSE,
 			'Targets'        =>
 				[
