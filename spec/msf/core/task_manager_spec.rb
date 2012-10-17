e6ec7281f6a48b1a5d6db5e5f132711a4b76a6ce
James Lee <egypt@metasploit.com>
2012-10-17 14:50:25 -0500

    Make old specs work with new rake task

--- spec/msf/core/task_manager_spec.rb
+++ spec/msf/core/task_manager_spec.rb
@@ -1,102 +1,97 @@
-$:.push("../../lib")
 
-require 'testbase'
 require 'msf/core/task_manager'
 
 describe Msf::TaskManager do
-	it "should have attributes" do
-		tm = Msf::TaskManager.new($msf)
-		tm.should respond_to("processing")
-		tm.should respond_to("queue")
-		tm.should respond_to("thread")
-		tm.should respond_to("framework")
-		tm.should respond_to("processing=")
-		tm.should respond_to("queue=")
-		tm.should respond_to("thread=")
-		tm.should respond_to("framework=")
-	end
 
-	it "should initialize with an empty queue" do
-		tm = Msf::TaskManager.new($msf)
-		tm.queue.length.should == 0
-		tm.backlog.should == 0
-		tm.backlog.should == tm.queue.length
-	end
+  let(:framework) do
+    Msf::Framework.new
+  end
 
-	it "should add items to the queue and process them" do
-		tm = Msf::TaskManager.new($msf)
-		tm.queue_proc(Proc.new{ })
-		tm.backlog.should == 1
-		tm.queue_proc(Proc.new{ })
-		tm.backlog.should == 2
-		tm.start
-		sleep(0.5)
-		tm.backlog.should == 0
-	end
+  let(:tm) do
+    Msf::TaskManager.new(framework)
+  end
 
-	it "should add items to the queue and flush them" do
-		tm = Msf::TaskManager.new($msf)
-		tm.queue_proc(Proc.new{ })
-		tm.backlog.should == 1
-		tm.queue_proc(Proc.new{ })
-		tm.backlog.should == 2
-		tm.flush
-		tm.backlog.should == 0
-	end
+  it "should have attributes" do
+    tm.should respond_to("processing")
+    tm.should respond_to("queue")
+    tm.should respond_to("thread")
+    tm.should respond_to("framework")
+    tm.should respond_to("processing=")
+    tm.should respond_to("queue=")
+    tm.should respond_to("thread=")
+    tm.should respond_to("framework=")
+  end
 
-	it "should start and stop" do
-		tm = Msf::TaskManager.new($msf)
-		tm.queue_proc(Proc.new{ })
-		tm.backlog.should == 1
-		tm.start
-		sleep(0.5)
-		tm.backlog.should == 0
-		tm.stop
-		tm.queue_proc(Proc.new{ })
-		tm.backlog.should == 1
-		sleep(0.5)
-		tm.queue_proc(Proc.new{ })
-		tm.backlog.should == 2
-		tm.start
-		sleep(0.5)
-		tm.backlog.should == 0
-	end
+  it "should initialize with an empty queue" do
+    tm.queue.length.should == 0
+    tm.backlog.should == 0
+    tm.backlog.should == tm.queue.length
+  end
 
-	it "should handle task timeouts" do
-		tm = Msf::TaskManager.new($msf)
-		t  = Msf::TaskManager::Task.new(Proc.new { sleep(30) })
-		t.timeout = 0.1
+  it "should add items to the queue and process them" do
+    tm.queue_proc(Proc.new{ })
+    tm.backlog.should == 1
+    t = Msf::TaskManager::Task.new(Proc.new { })
+    tm.queue_task(t)
+    tm.backlog.should == 2
+    tm.start
+    t.wait
+    tm.backlog.should == 0
+  end
 
-		tm.start
-		tm.queue_task(t)
-		sleep(0.5)
+  it "should add items to the queue and flush them" do
+    tm.queue_proc(Proc.new{ })
+    tm.backlog.should == 1
+    tm.queue_proc(Proc.new{ })
+    tm.backlog.should == 2
+    tm.flush
+    tm.backlog.should == 0
+  end
 
-		t.status.should == :timeout
-		t.duration.should <= 1.0
-	end
+  it "should start and stop" do
+    t = Msf::TaskManager::Task.new(Proc.new { })
+    tm.queue_task(t)
+    tm.backlog.should == 1
+    tm.start
+    t.wait
+    tm.backlog.should == 0
+    tm.stop
+    1.upto 100 do |cnt|
+      tm.queue_proc(Proc.new{ })
+      tm.backlog.should == cnt
+    end
+    t = Msf::TaskManager::Task.new(Proc.new { })
+    tm.queue_task(t)
+    tm.start
+    t.wait
+    tm.backlog.should == 0
+  end
 
-	it "should handle task exceptions" do
-		tm = Msf::TaskManager.new($msf)
-		t  = Msf::TaskManager::Task.new(Proc.new { asdf1234() })
+  it "should handle task timeouts" do
+    t = Msf::TaskManager::Task.new(Proc.new { sleep(30) })
+    t.timeout = 0.1
 
-		tm.start
-		tm.queue_task(t)
-		sleep(0.5)
+    tm.start
+    tm.queue_task(t)
+    t.wait
 
-		t.status.should == :dropped
-		t.exception.class.should == ::NoMethodError
-	end
+    t.status.should == :timeout
+    t.duration.should <= 1.0
+  end
 
-	it "should handle a bad proc return" do
-		tm = Msf::TaskManager.new($msf)
-		t  = Msf::TaskManager::Task.new(Proc.new { return 12345 })
+  it "should handle task exceptions" do
+    t = Msf::TaskManager::Task.new(Proc.new { asdf1234() })
+    tm.start
+    tm.queue_task(t)
+    t.wait
+    t.status.should == :dropped
+    t.exception.class.should == ::NoMethodError
 
-		tm.start
-		tm.queue_task(t)
-		sleep(0.5)
-
-		t.status.should == :done
-		t.exception.should == nil
-	end
+    t = Msf::TaskManager::Task.new(Proc.new { return 12345 })
+    tm.queue_task(t)
+    t.wait
+    t.status.should == :dropped
+    t.exception.should be_a ::LocalJumpError
+  end
 end
 
