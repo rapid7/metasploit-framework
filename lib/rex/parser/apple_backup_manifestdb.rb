b7a4cbb6a8d1a66e1869d8049c7c16826c94bbcd
satishb3 <satishb3@hotmail.com>
2012-10-21 18:03:44 +0530

    updated apple_ios_backup to support iOS 5 & iOS 6 backups

--- lib/rex/parser/apple_backup_manifestdb.rb
+++ lib/rex/parser/apple_backup_manifestdb.rb
@@ -1,92 +1,65 @@
-# -*- coding: binary -*-
 #
 # This is a Ruby port of the Python manifest parsing code posted to:
 # 	http://stackoverflow.com/questions/3085153/how-to-parse-the-manifest-mbdb-file-in-an-ios-4-0-itunes-backup/3130860#3130860
 #
+# The script is updated to support iOS 5 & iOS 6 backups : by Satish B (satishb3) - www.securitylearn.net
+# Reference: http://code.google.com/p/iphone-dataprotection/source/browse/python_scripts/backups/backup4.py
+#
 
+require 'digest/sha1'
 module Rex
 module Parser
 class AppleBackupManifestDB
 
 	attr_accessor :entry_offsets
 	attr_accessor :entries
-	attr_accessor :mbdb, :mbdx
-	attr_accessor :mbdb_data, :mbdx_data
-	attr_accessor :mbdb_offset, :mbdx_offset
+	attr_accessor :mbdb
+	attr_accessor :mbdb_data
+	attr_accessor :mbdb_offset
+	
+	def initialize(mbdb_data)	
 
-	def initialize(mbdb_data, mbdx_data)
 		self.entries = {}
 		self.entry_offsets = {}
 		self.mbdb_data = mbdb_data
-		self.mbdx_data = mbdx_data
 		parse_mbdb
-		parse_mbdx
+		
 	end
-
-	def self.from_files(mbdb_file, mbdx_file)
-		mbdb_data = ""
-		::File.open(mbdb_file, "rb") {|fd| mbdb_data = fd.read(fd.stat.size) }
-		mbdx_data = ""
-		::File.open(mbdx_file, "rb") {|fd| mbdx_data = fd.read(fd.stat.size) }
-
-		self.new(mbdb_data, mbdx_data)
-	end
-
 	def parse_mbdb
-		raise ArgumentError, "Not valid MBDB data" if self.mbdb_data[0,4] != "mbdb"
-		self.mbdb_offset = 4
-		self.mbdb_offset = self.mbdb_offset + 2 # Maps to \x05 \x00 (unknown)
+		raise ArgumentError, "Not valid MBDB data" if self.mbdb_data[0,6] != "mbdb\x05\x00"	
+		self.mbdb_offset = 6
 
 		while self.mbdb_offset < self.mbdb_data.length
-			info = {}
-			info[:start_offset] = self.mbdb_offset
-			info[:domain]       = mbdb_read_string
-			info[:filename]     = mbdb_read_string
-			info[:linktarget]   = mbdb_read_string
-			info[:datahash]     = mbdb_read_string
-			info[:unknown1]     = mbdb_read_string
-			info[:mode]         = mbdb_read_int(2)
-			info[:unknown2]     = mbdb_read_int(4)
-			info[:unknown3]     = mbdb_read_int(4)
-			info[:uid]          = mbdb_read_int(4)
-			info[:gid]          = mbdb_read_int(4)
-			info[:mtime]        = Time.at(mbdb_read_int(4))
-			info[:atime]        = Time.at(mbdb_read_int(4))
-			info[:ctime]        = Time.at(mbdb_read_int(4))
-			info[:length]       = mbdb_read_int(8)
-			info[:flag]         = mbdb_read_int(1)
-			property_count      = mbdb_read_int(1)
-			info[:properties]   = {}
+			info 			 = {}
+			info[:domain]      	 = mbdb_read_string
+			info[:filename]    	 = mbdb_read_string		
+			info[:linktarget]  	 = mbdb_read_string			
+			info[:datahash]    	 = mbdb_read_string
+			info[:encryptionkey]     = mbdb_read_string
+			info[:mode]         	 = mbdb_read_int(2)
+			info[:inodenumber]       = mbdb_read_int(8)			
+			info[:uid]        	 = mbdb_read_int(4)				
+			info[:gid]          	 = mbdb_read_int(4)				
+			info[:mtime]        	 = Time.at(mbdb_read_int(4))
+			info[:atime]        	 = Time.at(mbdb_read_int(4))
+			info[:ctime]       	 = Time.at(mbdb_read_int(4))
+			info[:length]       	 = mbdb_read_int(8)	
+			info[:protectionClass]   = mbdb_read_int(1)
+			property_count      	 = mbdb_read_int(1)
+			info[:properties]   	 = {}
 			1.upto(property_count) do |i|
 				k = mbdb_read_string
 				v = mbdb_read_string
 				info[:properties][k] = v
 			end
-			self.entry_offsets[ info[:start_offset] ] = info
+
+			filepath=info[:domain]+'-'+info[:filename]
+			info[:fname]=Digest::SHA1.hexdigest filepath
+			self.entry_offsets[ info[:fname] ] = info
 		end
+
 		self.mbdb_data = ""
-	end
-
-	def parse_mbdx
-		raise ArgumentError, "Not a valid MBDX file" if self.mbdx_data[0,4] != "mbdx"
-
-		self.mbdx_offset = 4
-		self.mbdx_offset = self.mbdx_offset + 2 # Maps to \x02 \x00 (unknown)
-
-		file_count = mbdx_read_int(4)
-
-		while self.mbdx_offset < self.mbdx_data.length
-			file_id = self.mbdx_data[self.mbdx_offset, 20].unpack("C*").map{|c| "%02x" % c}.join
-			self.mbdx_offset += 20
-			entry_offset = mbdx_read_int(4) + 6
-			mode = mbdx_read_int(2)
-			entry = entry_offsets[ entry_offset ]
-			# May be corrupted if there is no matching entry, but what to do about it?
-			next if not entry
-			self.entries[file_id] = entry.merge({:mbdx_mode => mode, :file_id => file_id})
-		end
-		self.mbdx_data = ""
-	end
+	end   	
 
 	def mbdb_read_string
 		raise RuntimeError, "Corrupted MBDB file" if self.mbdb_offset > self.mbdb_data.length
@@ -97,7 +70,7 @@ class AppleBackupManifestDB
 		self.mbdb_offset += len
 		return val
 	end
-
+	
 	def mbdb_read_int(size)
 		val = 0
 		size.downto(1) do |i|
@@ -106,27 +79,7 @@ class AppleBackupManifestDB
 		end
 		val
 	end
-
-	def mbdx_read_string
-		raise RuntimeError, "Corrupted MBDX file" if self.mbdx_offset > self.mbdx_data.length
-		len = self.mbdx_data[self.mbdx_offset, 2].unpack("n")[0]
-		self.mbdx_offset += 2
-		return '' if len == 65535
-		val = self.mbdx_data[self.mbdx_offset, len]
-		self.mbdx_offset += len
-		return val
-	end
-
-	def mbdx_read_int(size)
-		val = 0
-		size.downto(1) do |i|
-			val = (val << 8) + self.mbdx_data[self.mbdx_offset, 1].unpack("C")[0]
-			self.mbdx_offset += 1
-		end
- 		val
-	end
-end
-
-
 end
 end
+end
+
