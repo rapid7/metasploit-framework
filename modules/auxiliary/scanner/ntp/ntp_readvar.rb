a2fd377326bf15b4abed582245c44ced86bef4da
jvazquez-r7 <juan.vazquez@metasploit.com>
2012-10-31 09:20:00 +0100

    module cleanup

--- modules/auxiliary/scanner/ntp/ntp_readvar.rb
+++ modules/auxiliary/scanner/ntp/ntp_readvar.rb
@@ -17,16 +17,16 @@ class Metasploit3 < Msf::Auxiliary
 		super(update_info(info,
 			'Name'           => 'NTP Clock Variables Disclosure',
 			'Description'    => %q{
-				This module reads the system internal NTP variables. These variables contain potentially sensitive
-				information, such as the NTP software version, operating system version, peers, and more..
+					This module reads the system internal NTP variables. These variables contain
+				potentially sensitive information, such as the NTP software version, operating
+				system version, peers, and more.
 			},
-			'Author'         => 'Ewerson Guimaraes(Crash) <crash[at]dclabs.com.br>',
+			'Author'         => [ 'Ewerson Guimaraes(Crash) <crash[at]dclabs.com.br>' ],
 			'License'        => MSF_LICENSE,
-			'Version'        => '',
 			'References'     =>
 				[
-						['URL','http://www.rapid7.com/vulndb/lookup/ntp-clock-variables-disclosure' ],
-			]
+					[ 'URL','http://www.rapid7.com/vulndb/lookup/ntp-clock-variables-disclosure' ],
+				]
 			)
 		)
 		register_options(
@@ -45,17 +45,21 @@ class Metasploit3 < Msf::Auxiliary
 		print_status("Sending command")
 		udp_sock.put(readvar)
 		reply = udp_sock.recvfrom(65535, 0.1)
-		p_reply =( reply[0].split(","))
+		if not reply or reply[0].empty?
+			print_error("#{rhost}:#{rport} - Couldn't read NTP variables")
+			return
+		end
+		p_reply = reply[0].split(",")
 		arr_count = 0
-				while ( arr_count < p_reply.size)
-						if arr_count == 0
-							print_good (p_reply[arr_count].slice(12,p_reply[arr_count].size)) #12 is the adjustment of packet garbage
-							arr_count =  arr_count + 1
-						else
-							print_good (p_reply[arr_count].strip)
-							arr_count =  arr_count + 1
-						end
-				end
+		while ( arr_count < p_reply.size)
+			if arr_count == 0
+				print_good("#{rhost}:#{rport} - #{p_reply[arr_count].slice(12,p_reply[arr_count].size)}") #12 is the adjustment of packet garbage
+				arr_count =  arr_count + 1
+			else
+				print_good("#{rhost}:#{rport} - #{p_reply[arr_count].strip}")
+				arr_count =  arr_count + 1
+			end
+		end
 		disconnect_udp
 
 	end
