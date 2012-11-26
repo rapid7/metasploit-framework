32ea36916cb2b987b147309153f861092d46c532
sinn3r <msfsinn3r@gmail.com>
2012-11-26 16:28:16 -0600

    Cosmetic changes

--- modules/auxiliary/scanner/msf/msf_rpc_login.rb
+++ modules/auxiliary/scanner/msf/msf_rpc_login.rb
@@ -1,8 +1,4 @@
 ##
-# msf_rpc_login.rb
-##
-
-##
 # This file is part of the Metasploit Framework and may be subject to
 # redistribution and commercial restrictions. Please see the Metasploit
 # web site for more information on licensing and terms of use.
@@ -20,8 +16,8 @@ class Metasploit3 < Msf::Auxiliary
 
 	def initialize
 		super(
-			'Name' 		=> 'Metasploit RPC interface Login Utility',
-			'Description'	=> %q{
+			'Name'          => 'Metasploit RPC interface Login Utility',
+			'Description'   => %q{
 				This module simply attempts to login to a
 				Metasploit RPC interface using a specific
 				user/pass.
@@ -44,12 +40,12 @@ class Metasploit3 < Msf::Auxiliary
 	def run_host(ip)
 		begin
 			@rpc = Msf::RPC::Client.new(
-			:host => datastore['rhost'],
-			:port => datastore['rport'],
-			:ssl  => datastore['SSL']
+				:host => datastore['rhost'],
+				:port => datastore['rport'],
+				:ssl  => datastore['SSL']
 			)
 		rescue => e
-			vprint_error("#{msg} #{datastore['SSL']} Cannot create RPC client : #{e}")
+			vprint_error("#{datastore['SSL'].to_s} Cannot create RPC client : #{e.to_s}")
 			return
 		end
 
@@ -59,11 +55,11 @@ class Metasploit3 < Msf::Auxiliary
 	end
 
 	def do_login(user='msf', pass='msf')
-		vprint_status("#{msg} - Trying username:'#{user}' with password:'#{pass}'")
+		vprint_status("Trying username:'#{user}' with password:'#{pass}'")
 		begin
 			res = @rpc.login(user, pass)
 			if res
-				print_good("#{msg} SUCCESSFUL LOGIN. '#{user}' : '#{pass}'")
+				print_good("SUCCESSFUL LOGIN. '#{user}' : '#{pass}'")
 
 				report_hash = {
 					:host   => datastore['RHOST'],
@@ -79,14 +75,9 @@ class Metasploit3 < Msf::Auxiliary
 				return :next_user
 			end
 		rescue  => e
-			# vprint_status("#{msg} #{datastore['SSL']} - Bad login #{e}")
-			vprint_status("#{msg} #{datastore['SSL']} - Bad login")
+			vprint_status("#{datastore['SSL'].to_s} - Bad login")
 			@rpc.close
 			return :skip_pass
 		end
-	end
-
-	def msg
-		"#{datastore['RHOST']}:#{datastore['RPORT']} Metasploit RPC -"
 	end
 end
