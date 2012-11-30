2cbc15a22b966774faf43218cee51fb5ed4bd38e
James Lee <egypt@metasploit.com>
2012-11-29 18:10:29 -0600

    Add #import_option and #from_file

--- spec/lib/msf/core/data_store_spec.rb
+++ spec/lib/msf/core/data_store_spec.rb
@@ -18,9 +18,24 @@ shared_examples "datastore" do
 		subject["FoO"].should == "bar"
 		subject["foO"].should == "bar"
 	end
+	context "#to_h" do
+		it "should return a Hash with correct values" do
+			subject.to_h.should == { "foo" => "bar", "fizz" => "buzz" }
+		end
+	end
 end
 
 describe Msf::DataStore do
+
+	describe "#import_option" do
+		subject do
+			s = described_class.new
+			s.import_option("foo", "bar")
+			s.import_option("fizz", "buzz")
+			s
+		end
+		it_behaves_like "datastore"
+	end
 
 	describe "#import_options_from_hash" do
 		subject do
@@ -41,5 +56,23 @@ describe Msf::DataStore do
 		end
 		it_behaves_like "datastore"
 	end
+
+	describe "#from_file" do
+		subject do
+			ini_instance = double
+			ini_instance.stub(:group?).and_return(true)
+			ini_instance.stub(:[]).and_return( { "foo" => "bar", "fizz" => "buzz" } )
+
+			ini = stub_const("Rex::Parser::Ini", Class.new)
+			ini.stub(:from_file).and_return(ini_instance)
+
+			s = described_class.new
+			s.from_file("path")
+			s
+		end
+
+		it_behaves_like "datastore"
+	end
+
 
 end
