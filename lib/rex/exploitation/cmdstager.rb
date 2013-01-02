3c039327c0b31c807690ad200896f429195d6754
Spencer McIntyre <zeroSteiner@gmail.com>
2013-01-02 13:41:57 -0500

    include the new mixin

--- lib/rex/exploitation/cmdstager.rb
+++ lib/rex/exploitation/cmdstager.rb
@@ -1,6 +1,6 @@
 # -*- coding: binary -*-
 ##
-# $Id$
+# $Id: cmdstager.rb 15548 2012-06-29 06:08:20Z rapid7 $
 ##
 
 require 'rex/exploitation/cmdstager/base'
@@ -8,3 +8,4 @@ require 'rex/exploitation/cmdstager/vbs'
 require 'rex/exploitation/cmdstager/debug_write'
 require 'rex/exploitation/cmdstager/debug_asm'
 require 'rex/exploitation/cmdstager/tftp'
+require 'rex/exploitation/cmdstager/bourne'
