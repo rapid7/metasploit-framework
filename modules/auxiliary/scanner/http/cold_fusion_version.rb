99ed8289c8f70b88b4f9e43260330fadf0b63693
lmercer <lmercer@mit.edu>
2013-01-08 22:40:13 -0500

    Enhancements to coldfusion_locale_traversal and cold_fusion_version Redmine Feature #6822

--- modules/auxiliary/scanner/http/cold_fusion_version.rb
+++ modules/auxiliary/scanner/http/cold_fusion_version.rb
@@ -41,10 +41,10 @@ class Metasploit3 < Msf::Auxiliary
 			end
 		end
 
-		len = (response.body.length > 2500) ?  2500 : response.body.length
 		return nil if response.body.length < 100
 
 		title = "Not Found"
+		response.body.gsub!(/[\r\n]/, '')
 		if(response.body =~ /<title.*\/?>(.+)<\/title\/?>/i)
 			title = $1
 			title.gsub!(/\s/, '')
@@ -56,6 +56,8 @@ class Metasploit3 < Msf::Auxiliary
 		if(response.body =~ />\s*Version:\s*(.*)<\/strong\><br\s\//)
 			v = $1
 			out = (v =~ /^6/) ? "Adobe ColdFusion MX6 #{v}" : "Adobe ColdFusion MX7 #{v}"
+		elsif(response.body =~ /<meta name=\"Author\" content=\"Copyright 1995-2012 Adobe/ and response.body =~ /Administrator requires a browser that supports frames/ )
+			out = "Adobe ColdFusion MX7"
 		elsif(response.body =~ /<meta name=\"Author\" content=\"Copyright \(c\) 1995-2006 Adobe/)
 			out = "Adobe ColdFusion 8"
 		elsif(response.body =~ /<meta name=\"Author\" content=\"Copyright \(c\) 1995-2010 Adobe/ or
@@ -82,7 +84,7 @@ class Metasploit3 < Msf::Auxiliary
 		res = send_request_cgi({
 				'uri' => url,
 				'method' => 'GET',
-				}, 5)
+				}, 10)
 
 		return if not res or not res.body or not res.code
 		res.body.gsub!(/[\r|\n]/, ' ')
