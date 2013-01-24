fd8afd2545cdf04427899504bc0eb68c499cae6c
Brandon Turner <brandon_turner@rapid7.com>
2013-01-23 21:48:53 -0600

    Update MDM gem in gemcache

--- lib/gemcache/ruby/1.9.1/gems/metasploit_data_models-0.4.0/lib/metasploit_data_models/version.rb
+++ lib/gemcache/ruby/1.9.1/gems/metasploit_data_models-0.4.0/lib/metasploit_data_models/version.rb
@@ -3,5 +3,5 @@ module MetasploitDataModels
   # is considered unstable because the database migrations are still in metasploit-framework and certain models may not
   # be shared between metasploit-framework and pro, so models may be removed in the future.  Because of the unstable API
   # the version should remain below 1.0.0
-  VERSION = '0.3.0'
+  VERSION = '0.4.0'
 end
