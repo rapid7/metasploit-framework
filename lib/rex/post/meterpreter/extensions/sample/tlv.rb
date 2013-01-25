623c29227275d9cedab534d2dff469f126fa88ba
Konrads Smelkovs <konrads.smelkovs@kpmg.co.uk>
2013-01-25 00:03:27 +0000

    The ruby side of ext_sample

---
+++ lib/rex/post/meterpreter/extensions/sample/tlv.rb
@@ -0,0 +1,12 @@
+module Rex
+module Post
+module Meterpreter
+module Extensions
+module Sample
+TLV_TYPE_SAMPLE_PONG = TLV_META_TYPE_STRING | (TLV_EXTENSIONS + 671)
+TLV_TYPE_SAMPLE_PING = TLV_META_TYPE_STRING | (TLV_EXTENSIONS + 672)
+end
+end
+end
+end
+end
