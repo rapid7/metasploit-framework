10e017ae73b522bba44d56bed2b5b5a43d681ef6
David Maloney <DMaloney@rapid7.com>
2013-02-07 14:56:26 -0600

    finish up the SSLScan::Result class
    finishes up result class for SSLScan , compelte with tests

--- lib/rex/sslscan/result.rb
+++ lib/rex/sslscan/result.rb
@@ -4,18 +4,17 @@ require 'rex/socket'
 module Rex::SSLScan
 class Result
 
-	attr_accessor :sslv2
-	attr_accessor :sslv3
-	attr_accessor :tlsv1
-
+	attr_reader :sslv2
+	attr_reader :sslv3
+	attr_reader :tlsv1
 	attr_reader :supported_versions
 
 	def initialize()
 		@cert = nil
-		@sslv2 = {}
-		@sslv3 = {}
-		@tlsv1 = {}
-		@supported_versions = [:sslv2, :sslv3, :tlsv1]
+		@sslv2 = {:accepted => [], :rejected => []}
+		@sslv3 = {:accepted => [], :rejected => []}
+		@tlsv1 = {:accepted => [], :rejected => []}
+		@supported_versions = [:SSLv2, :SSLv3, :TLSv1]
 	end
 
 	def cert
@@ -30,7 +29,44 @@ class Result
 	end
 
 	def add_cipher(version, cipher, key_length, status)
-		
+		unless @supported_versions.include? version
+			raise ArgumentError, "Must be a supported SSL Version"
+		end
+		unless OpenSSL::SSL::SSLContext.new(version).ciphers.flatten.include? cipher
+			raise ArgumentError, "Must be a valid SSL Cipher for #{version}!"
+		end
+		unless key_length.kind_of? Fixnum
+			raise ArgumentError, "Must supply a valid key length"
+		end
+		unless [:accepted, :rejected].include? status
+			raise ArgumentError, "status Must be either :accepted or :rejected"
+		end
+
+		cipher_details = {:cipher => cipher, :key_length => key_length}
+		case version
+		when :SSLv2
+			@sslv2[status] << cipher_details
+		when :SSLv3
+			@sslv3[status] << cipher_details
+		when :TLSv1
+			@tlsv1[status] << cipher_details
+		end
+	end
+
+	def accepted
+		{
+			:SSLv2 => @sslv2[:accepted],
+			:SSLv3 => @sslv3[:accepted],
+			:TLSv1 => @tlsv1[:accepted]
+		}
+	end
+
+	def rejected
+		{
+			:SSLv2 => @sslv2[:rejected],
+			:SSLv3 => @sslv3[:rejected],
+			:TLSv1 => @tlsv1[:rejected]
+		}
 	end
 
 end
--- spec/lib/rex/sslscan/result_spec.rb
+++ spec/lib/rex/sslscan/result_spec.rb
@@ -4,37 +4,34 @@ describe Rex::SSLScan::Result do
 	
 	subject{Rex::SSLScan::Result.new}
 
-	it "should respond to cert" do
-		subject.should respond_to :cert
-	end
-
-	it "should respond to sslv2" do
-		subject.should respond_to :sslv2
-	end
-
-	it "should respond to sslv3" do
-		subject.should respond_to :sslv3
-	end
-
-	it "should respond to tlsv1" do
-		subject.should respond_to :tlsv1
-	end
+      it { should respond_to :cert }
+      it { should respond_to :sslv2 }
+      it { should respond_to :sslv3 }
+      it { should respond_to :tlsv1 }
 
 	context "with no values set" do
 		it "should return nil for the cert" do
 			subject.cert.should == nil
 		end
 
-		it "should return an empty hash for sslv2" do
-			subject.sslv2.should == {}
+		it "should return an empty structure for sslv2" do
+			subject.sslv2.should == {:accepted => [], :rejected => []}
 		end
 
-		it "should return an empty hash for sslv3" do
-			subject.sslv3.should == {}
+		it "should return an empty structure for sslv3" do
+			subject.sslv3.should == {:accepted => [], :rejected => []}
 		end
 
-		it "should return an empty hash for tlsv1" do
-			subject.tlsv1.should == {}
+		it "should return an empty structure for tlsv1" do
+			subject.tlsv1.should == {:accepted => [], :rejected => []}
+		end
+
+		it "should return an empty structure for #accepted" do
+			subject.accepted.should == {:SSLv2=>[], :SSLv3=>[], :TLSv1=>[]}
+		end
+
+		it "should return an emtpy structure for #rejected" do
+			subject.rejected.should == {:SSLv2=>[], :SSLv3=>[], :TLSv1=>[]}
 		end
 	end
 
@@ -55,6 +52,92 @@ describe Rex::SSLScan::Result do
 		end
 	end
 
+	context "adding a cipher result" do
+		context "should raise an exception if" do
+			it "given an invalid SSL version" do
+				expect{subject.add_cipher(:ssl3, 'AES256-SHA', 256, :accepted )}.to raise_error
+			end
+				
+			it "given SSL version as a string" do
+				expect{subject.add_cipher('sslv3', 'AES256-SHA', 256, :accepted )}.to raise_error
+			end
 
+			it "given an invalid SSL cipher" do
+				expect{subject.add_cipher(:SSLv3, 'FOO256-SHA', 256, :accepted )}.to raise_error
+			end
+
+			it "given an unsupported cipher for the version" do
+				expect{subject.add_cipher(:SSLv3, 'DES-CBC3-MD5', 256, :accepted )}.to raise_error
+			end
+
+			it "given a non-number for key length" do
+				expect{subject.add_cipher(:SSLv3, 'AES256-SHA', "256", :accepted )}.to raise_error
+			end
+
+			it "given a decimal key length" do
+				expect{subject.add_cipher(:SSLv3, 'AES256-SHA', 25.6, :accepted )}.to raise_error
+			end
+
+			it "given an invalid status" do
+				expect{subject.add_cipher(:SSLv3, 'AES256-SHA', 256, :good )}.to raise_error
+			end
+
+			it "given status as a string" do
+				expect{subject.add_cipher(:SSLv3, 'AES256-SHA', 256, "accepted" )}.to raise_error
+			end
+		end
+		context "that was accepted" do
+			it "should add an SSLv2 cipher result to the SSLv2 Accepted array" do
+				subject.add_cipher(:SSLv2, "DES-CBC3-MD5", 168, :accepted)
+				subject.sslv2[:accepted].should include({:cipher=>"DES-CBC3-MD5", :key_length=>168})
+				subject.accepted[:SSLv2].should include({:cipher=>"DES-CBC3-MD5", :key_length=>168})
+			end
+
+			it "should add an SSLv3 cipher result to the SSLv3 Accepted array" do
+				subject.add_cipher(:SSLv3, "AES256-SHA", 256, :accepted)
+				subject.sslv3[:accepted].should include({:cipher=>"AES256-SHA", :key_length=>256})
+				subject.accepted[:SSLv3].should include({:cipher=>"AES256-SHA", :key_length=>256})
+			end
+
+			it "should add an TLSv1 cipher result to the TLSv1 Accepted array" do
+				subject.add_cipher(:TLSv1, "AES256-SHA", 256, :accepted)
+				subject.tlsv1[:accepted].should include({:cipher=>"AES256-SHA", :key_length=>256})
+				subject.accepted[:TLSv1].should include({:cipher=>"AES256-SHA", :key_length=>256})
+			end
+
+			it "should successfully add multiple entries in a row" do
+				subject.add_cipher(:SSLv3, "AES128-SHA", 128, :accepted)
+				subject.add_cipher(:SSLv3, "AES256-SHA", 256, :accepted)
+				subject.sslv3[:accepted].should include({:cipher=>"AES256-SHA", :key_length=>256})
+				subject.sslv3[:accepted].should include({:cipher=>"AES128-SHA", :key_length=>128})
+			end
+		end
+		context "that was rejected" do
+			it "should add an SSLv2 cipher result to the SSLv2 Rejected array" do
+				subject.add_cipher(:SSLv2, "DES-CBC3-MD5", 168, :rejected)
+				subject.sslv2[:rejected].should include({:cipher=>"DES-CBC3-MD5", :key_length=>168})
+				subject.rejected[:SSLv2].should include({:cipher=>"DES-CBC3-MD5", :key_length=>168})
+			end
+
+			it "should add an SSLv3 cipher result to the SSLv3 Rejected array" do
+				subject.add_cipher(:SSLv3, "AES256-SHA", 256, :rejected)
+				subject.sslv3[:rejected].should include({:cipher=>"AES256-SHA", :key_length=>256})
+				subject.rejected[:SSLv3].should include({:cipher=>"AES256-SHA", :key_length=>256})
+			end
+
+			it "should add an TLSv1 cipher result to the TLSv1 Rejected array" do
+				subject.add_cipher(:TLSv1, "AES256-SHA", 256, :rejected)
+				subject.tlsv1[:rejected].should include({:cipher=>"AES256-SHA", :key_length=>256})
+				subject.rejected[:TLSv1].should include({:cipher=>"AES256-SHA", :key_length=>256})
+			end
+
+			it "should successfully add multiple entries in a row" do
+				subject.add_cipher(:SSLv3, "AES128-SHA", 128, :rejected)
+				subject.add_cipher(:SSLv3, "AES256-SHA", 256, :rejected)
+				subject.sslv3[:rejected].should include({:cipher=>"AES256-SHA", :key_length=>256})
+				subject.sslv3[:rejected].should include({:cipher=>"AES128-SHA", :key_length=>128})
+			end
+		end
+	end
 
 end
