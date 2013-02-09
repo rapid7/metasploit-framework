38d0a244fd2e77979ec35dd7cfb1c6faf5284f66
David Maloney <DMaloney@rapid7.com>
2013-02-09 00:03:58 -0600

    Beginings of the actual scanner
    configuration and configuration validation in place with tests.

--- lib/rex/sslscan/scanner.rb
+++ lib/rex/sslscan/scanner.rb
@@ -1,6 +1,42 @@
+require 'rex/socket'
 
 module Rex::SSLScan
+
 class Scanner
+
+	attr_accessor :host
+	attr_accessor :port
+	attr_accessor :timeout
+
+	def initialize(host,port,timeout=20)
+		@host       = host
+		@port       = port
+		@timeout = timeout
+		raise StandardError, "The supplied configuration is invalid" unless valid?
+	end
+
+	def valid?
+		begin
+			@host = Rex::Socket.getaddress(@host, true)
+		rescue
+			return false
+		end
+		return false unless @port.kind_of? Fixnum
+		return false unless @port >= 0 and @port <= 65535
+		return false unless @timeout.kind_of? Fixnum
+		return true
+	end
+
+	def scan
+		raise StandardError, "The supplied configuration is invalid" unless valid?
+		scan_result = Rex::SSLScan::Result.new
+
+
+	end
+
+	def test_cipher(ssl_version, cipher)
+
+	end
 
 end
 end
--- spec/lib/rex/sslscan/scanner_spec.rb
+++ spec/lib/rex/sslscan/scanner_spec.rb
@@ -2,4 +2,32 @@ require 'rex/sslscan/scanner'
 
 describe Rex::SSLScan::Scanner do
 
+	subject{Rex::SSLScan::Scanner.new("127.0.0.1", 65535)}
+
+	it { should respond_to :host }
+	it { should respond_to :port }
+	it { should respond_to :timeout }
+	it { should respond_to :valid? }
+
+	context "when validating the scanner config" do
+		it "should return true when given a valid config" do
+			subject.valid?.should == true
+		end
+
+		it "should return false if given an invalid host" do
+			subject.host = nil
+			subject.valid?.should == false
+		end
+
+		it "should return false if given an invalid port" do
+			subject.port = nil
+			subject.valid?.should == false
+		end
+
+		it "should return false if given an invalid timeout" do
+			subject.timeout = nil
+			subject.valid?.should == false
+		end
+	end
+
 end
