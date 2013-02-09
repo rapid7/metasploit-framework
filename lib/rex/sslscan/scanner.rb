c25d4b48634ca30495f64bfd091584aa4d728c51
David Maloney <DMaloney@rapid7.com>
2013-02-09 01:07:56 -0600

    Test Cipher method underway
    Trying to get a clever test plan under way to actually test the network
    side of this. Not quite working yet

--- lib/rex/sslscan/scanner.rb
+++ lib/rex/sslscan/scanner.rb
@@ -7,12 +7,15 @@ class Scanner
 	attr_accessor :host
 	attr_accessor :port
 	attr_accessor :timeout
+	
+	attr_reader :supported_versions
 
-	def initialize(host,port,timeout=20)
+	def initialize(host,port = 443,timeout=20)
 		@host       = host
 		@port       = port
 		@timeout = timeout
-		raise StandardError, "The supplied configuration is invalid" unless valid?
+		@supported_versions = [:SSLv2, :SSLv3, :TLSv1]
+		raise StandardError, "The scanner configuration is invalid" unless valid?
 	end
 
 	def valid?
@@ -28,14 +31,38 @@ class Scanner
 	end
 
 	def scan
-		raise StandardError, "The supplied configuration is invalid" unless valid?
 		scan_result = Rex::SSLScan::Result.new
-
-
 	end
 
 	def test_cipher(ssl_version, cipher)
+		validate_params(ssl_version,cipher)
 
+		begin
+			scan_client = Rex::Socket::Tcp.create(
+				'PeerHost'     => @host,
+				'PeerPort'      => @port,
+				'SSL'               => true,
+				'SSLVersion'  => ssl_version,
+				'SSLCipher'   => cipher,
+				'Timeout'      => @timeout
+			)
+		rescue ::Exception => e 
+			return :rejected
+		end
+		return :accepted
+	end
+
+
+	protected
+
+	def validate_params(ssl_version, cipher)
+		raise StandardError, "The scanner configuration is invalid" unless valid?
+		unless @supported_versions.include? ssl_version
+			raise StandardError, "SSL Version must be one of: #{@supported_versions.to_s}"
+		end
+		unless OpenSSL::SSL::SSLContext.new(ssl_version).ciphers.flatten.include? cipher
+			raise ArgumentError, "Must be a valid SSL Cipher for #{version}!"
+		end
 	end
 
 end
--- spec/lib/rex/sslscan/scanner_spec.rb
+++ spec/lib/rex/sslscan/scanner_spec.rb
@@ -1,4 +1,7 @@
 require 'rex/sslscan/scanner'
+require 'rex/thread_factory'
+require 'rex/text'
+require 'rex/compat'
 
 describe Rex::SSLScan::Scanner do
 
@@ -28,6 +31,70 @@ describe Rex::SSLScan::Scanner do
 			subject.timeout = nil
 			subject.valid?.should == false
 		end
+	end
+
+	context "when testing a single cipher" do
+		context "an exception should be raised if" do
+			it "has an invalid scanner configuration" do
+				subject.host =nil
+				expect{ subject.test_cipher(:SSLv2, "AES128-SHA")}.to raise_error
+			end
+
+			it "is given an invalid SSL version" do
+				expect{ subject.test_cipher(:SSLv5, "AES128-SHA")}.to raise_error
+			end
+
+			it "is given an invalid cipher" do
+				expect{ subject.test_cipher(:SSLv2, "FOO128-SHA")}.to raise_error
+			end
+
+			it "is given an invalid cipher for the SSL Version" do
+				expect{ subject.test_cipher(:SSLv3, 'DES-CBC3-MD5')}.to raise_error
+			end
+		end
+
+		# context ":rejected should be returned if" do
+		# 	it "scans a non-SSL server" do
+		# 		server = Rex::Socket::TcpServer.create(
+		# 			'LocalHost' => '127.0.01',
+		# 			'LocalPort' => 65535,
+		# 			'SSL'		=> false,
+		# 		)
+		# 		server.start
+		# 		subject.test_cipher(:SSLv2, "DES-CBC3-MD5").should == :rejected
+		# 		server.stop
+		# 		server.close
+		# 	end
+
+		# 	it "scans a server that doesn't support the supplied SSL version" do
+		# 		server = Rex::Socket::TcpServer.create(
+		# 			'LocalHost' => '127.0.01',
+		# 			'LocalPort' => 65535,
+		# 			'SSL'		=> true,
+		# 			'SSLVersion' => :SSLv3
+		# 		)
+		# 		server.start
+		# 		subject.test_cipher(:SSLv2, "DES-CBC3-MD5").should == :rejected
+		# 		server.stop
+		# 		server.close
+		# 	end
+		# end
+
+		# context ":accepted should be returned if" do
+		# 	it "scans a server that accepts the given cipher" do
+		# 		server = Rex::Socket::TcpServer.create(
+		# 			'LocalHost' => '127.0.01',
+		# 			'LocalPort' => 65535,
+		# 			'SSL'		=> true,
+		# 			'SSLVersion' => :SSLv3,
+		# 			'SSLCipher'  => 'AES256-SHA'
+		# 		)
+		# 		server.start
+		# 		subject.test_cipher(:SSLv3, "AES256-SHA").should == :accepted
+		# 		server.stop
+		# 		server.close
+		# 	end
+		# end
 	end
 
 end
