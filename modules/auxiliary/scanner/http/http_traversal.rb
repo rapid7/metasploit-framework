21607182504e924e5cef685e8810aacdd6bc20c3
James Lee <egypt@metasploit.com>
2013-03-07 17:53:19 -0600

    Fix file header comment
    [See #1555]

--- modules/auxiliary/scanner/http/http_traversal.rb
+++ modules/auxiliary/scanner/http/http_traversal.rb
@@ -1,3 +1,10 @@
+##
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# web site for more information on licensing and terms of use.
+#   http://metasploit.com/
+##
+
 #
 # Thanks to:
 # ipax, neriberto, flambaz, bperry, egypt, and sinn3r for help
