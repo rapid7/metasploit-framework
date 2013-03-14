2604fad164129378fb0084ad8c54a21d6bd6ef51
Luke Imhoff <luke_imhoff@rapid7.com>
2013-03-14 15:46:18 -0500

    Allow use of rake db tasks
    [#46224565]
    
    The following rake tasks are added and work similar to how they work in
    rails apps:
    * db:create
    * db:drop
    * db:migrate
    * db:migrate:status
    * db:rollback
    * db:schema:dump
    * db:schema:load
    * db:seed (but no db seeds defined at this time)
    * db:setup
    * db:version
    
    The hidden task db:test:prepare is also available, which means `rake
    spec` can depend on it so that the test database is dropped and
    recreated from the development database when running specs (Although
    there are yet to be database tests, this branch is in preparation for
    that work that will be split between multiple developers.)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'bundler'
-Bundler.setup(:default, :test)
+Bundler.require(:default, :test)
 
 # add project lib directory to load path
 spec_pathname = Pathname.new(__FILE__).dirname
@@ -12,6 +12,7 @@ $LOAD_PATH.unshift(lib_pathname.to_s)
 # code.  It is after the rubygems and bundler only because Bundler.setup supplies the LOAD_PATH to simplecov.
 require 'simplecov'
 
+# now that simplecov is loaded, load everything else
 require 'rspec/core'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
