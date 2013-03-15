5f47694fac2ccc3b59097a6181233c3be8012458
Joshua Abraham <jabra@spl0it.org>
2013-03-15 10:18:44 -0400

    Linux post module to remove a file on the remote system

---
+++ modules/post/linux/manage/remove_file.rb
@@ -0,0 +1,46 @@
+##
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+#   http://metasploit.com/framework/
+##
+
+require 'msf/core'
+require 'rex'
+require 'msf/core/post/common'
+require 'msf/core/post/file'
+require 'msf/core/post/linux/system'
+require 'msf/core/post/linux/priv'
+
+class Metasploit3 < Msf::Post
+
+	include Msf::Post::Common
+	include Msf::Post::File
+	include Msf::Post::Linux::System
+
+
+	def initialize(info={})
+		super( update_info( info,
+				'Name'          => 'Linux Removes File or Directory',
+				'Description'   => %q{
+					This module removes a file or directory. This is recursive.
+				},
+				'License'       => MSF_LICENSE,
+				'Author'        =>
+					[
+						'Joshua D. Abraham <jabra[at]praetorian.com>',
+					],
+				'Platform'      => [ 'linux' ],
+				'SessionTypes'  => [ 'shell' ]
+			))
+			register_options(
+			[
+				OptString.new('REMOTEFILE', [true, 'Remote file to delete.']),
+			], self.class)
+
+	end
+
+	def run
+		vcmd_exec("rm -Rfv #{datastore['REMOTEFILE']}")
+	end
+end
