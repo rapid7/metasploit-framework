352f4f34d78423b8b4d21fa218222ba2f352c208
Joshua Abraham <jabra@spl0it.org>
2013-03-15 12:27:49 -0400

    added a check for root privs

--- modules/post/linux/manage/delete_user.rb
+++ modules/post/linux/manage/delete_user.rb
@@ -9,21 +9,22 @@ require 'msf/core'
 require 'rex'
 require 'msf/core/post/common'
 require 'msf/core/post/file'
-require 'msf/core/post/linux/system'
+require 'msf/core/post/unix'
 require 'msf/core/post/linux/priv'
+require 'msf/core/post/linux/system'
 
 class Metasploit3 < Msf::Post
 
 	include Msf::Post::Common
 	include Msf::Post::File
+	include Msf::Post::Linux::Priv
 	include Msf::Post::Linux::System
-
 
 	def initialize(info={})
 		super( update_info( info,
 				'Name'          => 'Linux Delete User',
 				'Description'   => %q{
-					This module deletes a user.
+					This module deletes a user. Requires: root permissions.
 				},
 				'License'       => MSF_LICENSE,
 				'Author'        =>
@@ -41,6 +42,11 @@ class Metasploit3 < Msf::Post
 	end
 
 	def run
-		vcmd_exec("deluser #{datastore['USER']}")
+		if is_root?
+			vcmd_exec("deluser #{datastore['USER']}")
+		else
+			print_error("This module require root permissions")
+			return
+		end		
 	end
 end
