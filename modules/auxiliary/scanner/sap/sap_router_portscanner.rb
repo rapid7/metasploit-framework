e969cbb0bb1a16067f7026b8e3a0d307537d1fec
Bruno Morisson <morisson@genhex.org>
2013-06-22 23:09:59 +0100

    added INSTANCES option, and support for it on PORTS

--- modules/auxiliary/scanner/sap/sap_router_portscanner.rb
+++ modules/auxiliary/scanner/sap/sap_router_portscanner.rb
@@ -15,55 +15,94 @@ class Metasploit3 < Msf::Auxiliary
 
 	def initialize
 		super(
-			'Name' => 'SAPRouter Port Scanner',
-			'Description' => %q{
+		  'Name' => 'SAPRouter Port Scanner',
+		  'Description' => %q{
 					This module allows for mapping ACLs and identify open/closed ports accessible
 				on hosts through a saprouter.
 			},
-			'Author' => [
-				'Bruno Morisson <bm[at]integrity.pt>', # metasploit module
-				'nmonkee' # saprouter packet building code from sapcat.rb
+		  'Author' => [
+			'Bruno Morisson <bm[at]integrity.pt>', # metasploit module
+			'nmonkee' # saprouter packet building code from sapcat.rb, and default ports + info
+		  ],
+		  'References' =>
+			[
+			  # General
+			  ['URL', 'http://help.sap.com/saphelp_nw70/helpdata/EN/4f/992dfe446d11d189700000e8322d00/frameset.htm'],
+			  ['URL', 'http://help.sap.com/saphelp_dimp50/helpdata/En/f8/bb960899d743378ccb8372215bb767/content.htm'],
+			  ['URL', 'http://labs.mwrinfosecurity.com/blog/2012/09/13/sap-smashing-internet-windows/'],
+			  ['URL', 'http://conference.hitb.org/hitbsecconf2010ams/materials/D2T2%20-%20Mariano%20Nunez%20Di%20Croce%20-%20SAProuter%20.pdf'],
+			  ['URL', 'http://scn.sap.com/docs/DOC-17124'] # SAP default ports
 			],
-			'References' =>
-				[
-					# General
-					['URL', 'http://help.sap.com/saphelp_nw70/helpdata/EN/4f/992dfe446d11d189700000e8322d00/frameset.htm'],
-					['URL', 'http://help.sap.com/saphelp_dimp50/helpdata/En/f8/bb960899d743378ccb8372215bb767/content.htm'],
-					['URL', 'http://labs.mwrinfosecurity.com/blog/2012/09/13/sap-smashing-internet-windows/'],
-					['URL', 'http://conference.hitb.org/hitbsecconf2010ams/materials/D2T2%20-%20Mariano%20Nunez%20Di%20Croce%20-%20SAProuter%20.pdf'],
-					['URL', 'http://scn.sap.com/docs/DOC-17124'] # SAP default ports
-				],
-			'License' => MSF_LICENSE
+		  'License' => MSF_LICENSE
 		)
 
 		register_options(
-			[
-				OptAddress.new('SAPROUTER_HOST', [true, 'SAPRouter address', '']),
-				OptPort.new('SAPROUTER_PORT', [true, 'SAPRouter TCP port', '3299']),
-				OptEnum.new('MODE', [true, 'Connection Mode: SAP_PROTO or TCP ', 'SAP_PROTO', ['SAP_PROTO', 'TCP']]),
-				OptString.new('PORTS', [true, 'Ports to scan (e.g. 22-25,80,110-900)', '3200-3299']),
-				OptInt.new('CONCURRENCY', [true, 'The number of concurrent ports to check per host', 10]),
-			], self.class)
+		  [
+			OptAddress.new('SAPROUTER_HOST', [true, 'SAPRouter address', '']),
+			OptPort.new('SAPROUTER_PORT', [true, 'SAPRouter TCP port', '3299']),
+			OptEnum.new('MODE', [true, 'Connection Mode: SAP_PROTO or TCP ', 'SAP_PROTO', ['SAP_PROTO', 'TCP']]),
+			OptString.new('INSTANCES', [false, 'SAP instance numbers to scan (NN)', '00']),
+			OptString.new('PORTS', [true, 'Ports to scan (e.g. 3200-3299,5NN13)', '32NN,33NN,48NN,80NN,36NN,81NN,5NN00-5NN19,21212,21213,59975,59976,4238-4241,3299,3298,515,7200,7210,7269,7270,7575,39NN,3909,4NN00,8200,8210,8220,8230,4363,4444,4445,9999,3NN01-3NN08,3NN11,3NN17,20003-20007,31596,31597,31602,31601,31604,2000-2002,8355,8357,8351-8353,8366,1090,1095,20201,1099,1089,443NN,444NN']),
+			OptInt.new('CONCURRENCY', [true, 'The number of concurrent ports to check per host', 10]),
+		  ], self.class)
 
 		deregister_options('RPORT')
 
 	end
 
+	def sap_instance_to_list(instance)
+		instances = []
+
+		return if !instance
+
+		# Build ports array from port specification
+		instance.split(/,/).each do |item|
+			start, stop = item.split(/-/).map { |p| p.to_i }
+
+			start ||= 0
+			stop ||= item.match(/-/) ? 99 : start
+
+			start, stop = stop, start if stop < start
+
+			start.upto(stop) { |p| instances << p }
+		end
+
+		# Sort, and remove dups and invalid instances
+		instances.sort.uniq.delete_if { |p| p < 0 or p > 99 }
+
+	end
+
+	def build_sap_ports
+		sap_ports = []
+
+		sap_instances = sap_instance_to_list(datastore['INSTANCES'])
+		ports = datastore['PORTS']
+
+		# if we have INSTANCES, let's fill in the NN on PORTS
+		if sap_instances and ports.include? 'NN'
+			sap_instances.each { |i| sap_ports << (ports.gsub('NN', "%02d" % i)).to_s }
+			ports = Rex::Socket.portspec_crack(sap_ports.join(','))
+		else
+			ports = Rex::Socket.portspec_crack(ports)
+		end
+
+	end
+
 	def build_ni_packet(routes)
 
-		mode = {'SAP_PROTO'=>0,'TCP'=>1}[datastore['MODE']]
+		mode = {'SAP_PROTO' => 0, 'TCP' => 1}[datastore['MODE']]
 
 		route_data=''
 		ni_packet = [
-			'NI_ROUTE',
-			0,
-			2,
-			39,
-			2,
-			mode,
-			0,
-			0,
-			1
+		  'NI_ROUTE',
+		  0,
+		  2,
+		  39,
+		  2,
+		  mode,
+		  0,
+		  0,
+		  1
 		].pack("A8c8")
 
 		first = false
@@ -84,7 +123,7 @@ class Metasploit3 < Msf::Auxiliary
 
 	def parse_response_packet(response, ip, port)
 
-		#vprint_error("#{ip}:#{port} - response packet: #{response}")
+		vprint_error("#{ip}:#{port} - response packet: #{response}")
 
 		case response
 		when /NI_RTERR/
@@ -115,10 +154,16 @@ class Metasploit3 < Msf::Auxiliary
 
 	def run_host(ip)
 
-		ports = Rex::Socket.portspec_crack(datastore['PORTS'])
-
 		sap_host = datastore['SAPROUTER_HOST']
 		sap_port = datastore['SAPROUTER_PORT']
+
+		# if port definition has NN then we require INSTANCES
+		if datastore['PORTS'].include? 'NN' and datastore['INSTANCES'].nil?
+			print_error('Error: No instances specified')
+			return
+		end
+
+		ports = build_sap_ports()
 
 		if ports.empty?
 			print_error('Error: No valid ports specified')
@@ -130,47 +175,47 @@ class Metasploit3 < Msf::Auxiliary
 		r = []
 
 		begin
-		ports.each do |port|
+			ports.each do |port|
 
-			if thread.length >= datastore['CONCURRENCY']
-				# Assume the first thread will be among the earliest to finish
-				thread.first.join
-			end
-			thread << framework.threads.spawn("Module(#{self.refname})-#{ip}:#{port}", false) do
+				if thread.length >= datastore['CONCURRENCY']
+					# Assume the first thread will be among the earliest to finish
+					thread.first.join
+				end
+				thread << framework.threads.spawn("Module(#{self.refname})-#{ip}:#{port}", false) do
 
-				begin
-					# create ni_packet to send to saprouter
-					routes = {sap_host => sap_port, ip => port}
-					ni_packet = build_ni_packet(routes)
+					begin
+						# create ni_packet to send to saprouter
+						routes = {sap_host => sap_port, ip => port}
+						ni_packet = build_ni_packet(routes)
 
-					s = connect(false,
-						{
-						'RPORT' => sap_port,
-						'RHOST' => sap_host
-						}
-					)
+						s = connect(false,
+						            {
+						              'RPORT' => sap_port,
+						              'RHOST' => sap_host
+						            }
+						)
 
-					s.write(ni_packet, ni_packet.length)
-					response = s.get()
+						s.write(ni_packet, ni_packet.length)
+						response = s.get()
 
-					res = parse_response_packet(response, ip, port)
-					if res
-						r << res
+						res = parse_response_packet(response, ip, port)
+						if res
+							r << res
+						end
+
+					rescue ::Rex::ConnectionRefused
+						print_error("#{ip}:#{port} - Unable to connect to SAPRouter #{sap_host}:#{sap_port} - Connection Refused")
+
+					rescue ::Rex::ConnectionError, ::IOError, ::Timeout::Error
+					rescue ::Rex::Post::Meterpreter::RequestError
+					rescue ::Interrupt
+						raise $!
+					ensure
+						disconnect(s) rescue nil
 					end
-
-				rescue ::Rex::ConnectionRefused
-					print_error("#{ip}:#{port} - Unable to connect to SAPRouter #{sap_host}:#{sap_port} - Connection Refused")
-
-				rescue ::Rex::ConnectionError, ::IOError, ::Timeout::Error
-				rescue ::Rex::Post::Meterpreter::RequestError
-				rescue ::Interrupt
-					raise $!
-				ensure
-					disconnect(s) rescue nil
 				end
 			end
-		end
-		thread.each { |x| x.join }
+			thread.each { |x| x.join }
 
 		rescue ::Timeout::Error
 		ensure
