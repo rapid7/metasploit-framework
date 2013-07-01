1f108c24e95298a798ddd39f5ee4a31fc289fb48
James Lee <egypt@metasploit.com>
2013-07-01 16:22:39 -0500

    Fix requrires for enable_support_account

--- modules/post/windows/manage/enable_support_account.rb
+++ modules/post/windows/manage/enable_support_account.rb
@@ -1,6 +1,10 @@
 require 'msf/core'
 require 'rex'
 
+require 'msf/core/post/windows/registry'
+require 'msf/core/post/windows/priv'
+require 'msf/core/post/common'
+
 class Metasploit3 < Msf::Post
 
 	include Msf::Post::Windows::Registry
