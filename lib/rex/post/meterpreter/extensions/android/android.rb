e4a4c4e954d30c99eb64617791530c8d71727501
AnwarMohamed <anwarelmakrahy@gmail.com>
2013-07-30 07:25:26 +0200

    adding android common sub tree

--- lib/rex/post/meterpreter/extensions/android/android.rb
+++ lib/rex/post/meterpreter/extensions/android/android.rb
@@ -1,109 +1,150 @@
 #!/usr/bin/env ruby
 # -*- coding: binary -*-
 
-require 'rex/post/meterpreter/extensions/android/tlv'
-require 'rex/post/meterpreter/packet'
-require 'rex/post/meterpreter/client'
-require 'rex/post/meterpreter/channels/pools/stream_pool'
+require 'rex/post/meterpreter/object_aliases'
+require 'rex/post/meterpreter/extension'
+require 'rex/post/meterpreter/extensions/stdapi/constants'
+require 'rex/post/meterpreter/extensions/stdapi/tlv'
+require 'rex/post/meterpreter/extensions/stdapi/fs/dir'
+require 'rex/post/meterpreter/extensions/stdapi/fs/file'
+require 'rex/post/meterpreter/extensions/stdapi/fs/file_stat'
+require 'rex/post/meterpreter/extensions/stdapi/net/config'
+require 'rex/post/meterpreter/extensions/stdapi/net/socket'
+require 'rex/post/meterpreter/extensions/stdapi/sys/config'
+require 'rex/post/meterpreter/extensions/stdapi/sys/process'
+require 'rex/post/meterpreter/extensions/stdapi/sys/registry'
+require 'rex/post/meterpreter/extensions/stdapi/sys/event_log'
+require 'rex/post/meterpreter/extensions/stdapi/sys/power'
+require 'rex/post/meterpreter/extensions/stdapi/railgun/railgun'
+require 'rex/post/meterpreter/extensions/stdapi/ui'
+require 'rex/post/meterpreter/extensions/stdapi/webcam/webcam'
 
 module Rex
 module Post
 module Meterpreter
 module Extensions
-module Android
+module Stdapi
 
-class Android < Extension
+###
+#
+# Standard ruby interface to remote entities for meterpreter.  It provides
+# basic access to files, network, system, and other properties of the remote
+# machine that are fairly universal.
+#
+###
+class Stdapi < Extension
 
+	#
+	# Initializes an instance of the standard API extension.
+	#
 	def initialize(client)
-		super(client, 'android')
+		super(client, 'stdapi')
 
+		# Alias the following things on the client object so that they
+		# can be directly referenced
 		client.register_extension_aliases(
 			[
 				{
-					'name' => 'android',
-					'ext'  => self
+					'name' => 'fs',
+					'ext'  => ObjectAliases.new(
+						{
+							'dir'      => self.dir,
+							'file'     => self.file,
+							'filestat' => self.filestat
+						})
 				},
+				{
+					'name' => 'sys',
+					'ext'  => ObjectAliases.new(
+						{
+							'config'   => Sys::Config.new(client),
+							'process'  => self.process,
+							'registry' => self.registry,
+							'eventlog' => self.eventlog,
+							'power'    => self.power
+						})
+				},
+				{
+					'name' => 'net',
+					'ext'  => ObjectAliases.new(
+						{
+							'config'   => Rex::Post::Meterpreter::Extensions::Stdapi::Net::Config.new(client),
+							'socket'   => Rex::Post::Meterpreter::Extensions::Stdapi::Net::Socket.new(client)
+						})
+				},
+				{
+					'name' => 'railgun',
+					'ext'  => Rex::Post::Meterpreter::Extensions::Stdapi::Railgun::Railgun.new(client)
+				},
+				{
+					'name' => 'webcam',
+					'ext'  => Rex::Post::Meterpreter::Extensions::Stdapi::Webcam::Webcam.new(client)
+				},
+				{
+					'name' => 'ui',
+					'ext'  => UI.new(client)
+				}
+
 			])
 	end
 
-	def dump_sms
-		sms = Array.new
-		request = Packet.create_request('dump_sms')
-		response = client.send_request(request)
-
-		response.each( TLV_TYPE_SMS_GROUP ) { |p|
-
-			sms <<
-			{
-				'type' => client.unicode_filter_encode(p.get_tlv(TLV_TYPE_SMS_TYPE).value),
-				'address' => client.unicode_filter_encode(p.get_tlv(TLV_TYPE_SMS_ADDRESS).value),
-				'body' => client.unicode_filter_encode(p.get_tlv(TLV_TYPE_SMS_BODY).value).squish,
-				'status' => client.unicode_filter_encode(p.get_tlv(TLV_TYPE_SMS_STATUS).value),
-				'date' => client.unicode_filter_encode(p.get_tlv(TLV_TYPE_SMS_DATE).value)
-			}
-
-		}
-		return sms
+	#
+	# Sets the client instance on a duplicated copy of the supplied class.
+	#
+	def brand(klass)
+		klass = klass.dup
+		klass.client = self.client
+		return klass
 	end
 
-	def dump_contacts
-		contacts = Array.new
-		request = Packet.create_request('dump_contacts')
-		response = client.send_request(request)
-
-		response.each( TLV_TYPE_CONTACT_GROUP ) { |p|
-
-			contacts <<
-			{
-				'name' => client.unicode_filter_encode(p.get_tlv(TLV_TYPE_CONTACT_NAME).value),
-				'email' => client.unicode_filter_encode(p.get_tlv_values(TLV_TYPE_CONTACT_EMAIL)),
-				'number' => client.unicode_filter_encode(p.get_tlv_values(TLV_TYPE_CONTACT_NUMBER))
-			}
-
-		}
-		return contacts
+	#
+	# Returns a copy of the Dir class.
+	#
+	def dir
+		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Fs::Dir)
 	end
 
-	def geolocate
-
-		loc = Array.new
-		request = Packet.create_request('geolocate')
-		response = client.send_request(request)
-
-		loc <<
-		{
-			'lat' => "#{client.unicode_filter_encode(response.get_tlv(TLV_TYPE_GEO_LAT).value)}",
-			'long' => "#{client.unicode_filter_encode(response.get_tlv(TLV_TYPE_GEO_LONG).value)}"
-		}
-
-		return loc
+	#
+	# Returns a copy of the File class.
+	#
+	def file
+		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Fs::File)
 	end
 
-	def dump_calllog
-		log = Array.new
-		request = Packet.create_request('dump_calllog')
-		response = client.send_request(request)
-
-		response.each(TLV_TYPE_CALLLOG_GROUP) { |p|
-
-			log <<
-			{
-				'name' => client.unicode_filter_encode(p.get_tlv(TLV_TYPE_CALLLOG_NAME).value),
-				'number' => client.unicode_filter_encode(p.get_tlv(TLV_TYPE_CALLLOG_NUMBER).value),
-				'date' => client.unicode_filter_encode(p.get_tlv(TLV_TYPE_CALLLOG_DATE).value),
-				'duration' => client.unicode_filter_encode(p.get_tlv(TLV_TYPE_CALLLOG_DURATION).value),
-				'type' => client.unicode_filter_encode(p.get_tlv(TLV_TYPE_CALLLOG_TYPE).value)
-			}
-
-		}
-		return log
+	#
+	# Returns a copy of the FileStat class.
+	#
+	def filestat
+		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Fs::FileStat)
 	end
 
+	#
+	# Returns a copy of the Process class.
+	#
+	def process
+		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Sys::Process)
+	end
+
+	#
+	# Returns a copy of the Registry class.
+	#
+	def registry
+		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Sys::Registry)
+	end
+
+	#
+	# Returns a copy of the EventLog class.
+	#
+	def eventlog
+		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Sys::EventLog)
+	end
+
+	#
+	# Returns a copy of the Power class.
+	#
+	def power
+		brand(Rex::Post::Meterpreter::Extensions::Stdapi::Sys::Power)
+	end
 end
 
-end; 
-end; 
-end; 
-end; 
-end; 
-
+end; end; end; end; end
