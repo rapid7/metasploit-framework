f307aa70d3178a22b07e8fba895c75815a3c639d
Tod Beardsley <tod_beardsley@rapid7.com>
2013-08-07 12:31:56 -0500

    Add some old hard-tabs for sanity

--- test/modules/auxiliary/test/space-check.rb
+++ test/modules/auxiliary/test/space-check.rb
@@ -9,11 +9,11 @@ require 'msf/core'
 
 class Metasploit3 < Msf::Auxiliary
 
-  include Msf::Auxiliary::Report
-  include Msf::Exploit::Remote::HttpClient
+	include Msf::Auxiliary::Report
+	include Msf::Exploit::Remote::HttpClient
 
   def initialize(info = {})
-    super(update_info(info,
+		super(update_info(info,
 	    'Name'           => "Check Test",
 	  	'Description'    => %q{
 		   This module ensures that 'check' actually functions for Auxiilary modules.
