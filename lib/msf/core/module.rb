4706f8b54c100140c0a2ce4ba9a1b933377fd71b
HD Moore <hd_moore@rapid7.com>
2013-08-15 13:30:47 -0500

    Add fail_with() stub and move Failure from Exploit

--- lib/msf/core/module.rb
+++ lib/msf/core/module.rb
@@ -734,6 +734,85 @@ class Module
 		false
 	end
 
+	#
+	# Support fail_with for all module types, allow specific classes to override
+	#
+	def fail_with(reason, msg=nil)
+		raise RuntimeError, "#{reason.to_s}: #{msg}"
+	end
+
+	#
+	# Constants indicating the reason for an unsuccessful module attempt
+	#
+	module Failure
+		
+		#
+		# No confidence in success or failure
+		#
+		None            = 'none'
+
+		#
+		# No confidence in success or failure
+		#
+		Unknown         = 'unknown'
+
+		#
+		# The network service was unreachable (connection refused, etc)
+		#
+		Unreachable     = 'unreachable'
+
+		#
+		# The exploit settings were incorrect
+		#
+		BadConfig       = 'bad-config'
+
+		#
+		# The network service disconnected us mid-attempt
+		#
+		Disconnected    = 'disconnected'
+
+		#
+		# The application endpoint or specific service was not found
+		#
+		NotFound        = 'not-found'
+
+		#
+		# The application replied in an unexpected fashion
+		#
+		UnexpectedReply = 'unexpected-reply'
+
+		#
+		# The exploit triggered some form of timeout
+		#
+		TimeoutExpired  = 'timeout-expired'
+
+		#
+		# The exploit was interrupted by the user
+		#
+		UserInterrupt   = 'user-interrupt'
+
+		#
+		# The application replied indication we do not have access
+		#
+		NoAccess        = 'no-access'
+
+		#
+		# The target is not compatible with this exploit or settings
+		#
+		NoTarget        = 'no-target'
+
+		#
+		# The application response indicated it was not vulnerable
+		#
+		NotVulnerable   = 'not-vulnerable'
+
+		#
+		# The payload was delivered but no session was opened (AV, network, etc)
+		#
+		PayloadFailed   = 'payload-failed'
+	end	
+
+
 	##
 	#
 	# Just some handy quick checks
