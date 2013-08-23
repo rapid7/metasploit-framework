e9eb6b24273f0628c570fc4d252d83cfbe5e8fc3
Christian Mehlmauer <firefart@gmail.com>
2013-08-23 22:29:31 +0200

    simplification

--- lib/msf/http/wordpress/base.rb
+++ lib/msf/http/wordpress/base.rb
@@ -4,24 +4,24 @@ module Msf::HTTP::Wordpress::Base
 
 	# Checks if the site is online and running wordpress
 	#
-	# @return [Boolean] Returns true if the site is online and running wordpress
+	# @return [Rex::Proto::Http::Response] Returns the HTTP response if the site is online and running wordpress, nil otherwise
 	def wordpress_and_online?
 		begin
 			res = send_request_cgi({
 					'method' => 'GET',
 					'uri' => normalize_uri(target_uri.path)
-			}, 20)
-			return true if res and
+			})
+			return res if res and
 					res.code == 200 and
 					(
 						res.body =~ /["'][^"']*\/wp-content\/[^"']*["']/i or
 						res.body =~ /<link rel=["']wlwmanifest["'].*href=["'].*\/wp-includes\/wlwmanifest\.xml["'] \/>/i or
 						res.body =~ /<link rel=["']pingback["'].*href=["'].*\/xmlrpc\.php["'] \/>/i
 					)
-			return false
+			return nil
 		rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout
 			print_error("Error connecting to #{target_uri}")
-			return false
+			return nil
 		end
 	end
 end
