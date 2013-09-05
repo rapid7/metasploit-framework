def3902058cc8dbbbefdc5336e9ba0063bbaf67b
Tab Assassin <tabassassin@metasploit.com>
2013-09-05 16:15:07 -0500

    Retab changes for PR #1604

--- lib/rex/post/meterpreter/ui/console/command_dispatcher/stdapi/net.rb
+++ lib/rex/post/meterpreter/ui/console/command_dispatcher/stdapi/net.rb
@@ -14,306 +14,306 @@ module Ui
 ###
 class Console::CommandDispatcher::Stdapi::Net
 
-	Klass = Console::CommandDispatcher::Stdapi::Net
+  Klass = Console::CommandDispatcher::Stdapi::Net
 
-	include Console::CommandDispatcher
+  include Console::CommandDispatcher
 
-	#
-	# This module is used to extend the meterpreter session
-	# so that local port forwards can be tracked and cleaned
-	# up when the meterpreter session goes away
-	#
-	module PortForwardTracker
-		def cleanup
-			super
+  #
+  # This module is used to extend the meterpreter session
+  # so that local port forwards can be tracked and cleaned
+  # up when the meterpreter session goes away
+  #
+  module PortForwardTracker
+    def cleanup
+      super
 
-			if pfservice
-				pfservice.deref
-			end
-		end
+      if pfservice
+        pfservice.deref
+      end
+    end
 
-		attr_accessor :pfservice
-	end
-
-	#
-	# Options for the route command.
-	#
-	@@route_opts = Rex::Parser::Arguments.new(
-		"-h" => [ false, "Help banner." ])
-
-	#
-	# Options for the portfwd command.
-	#
-	@@portfwd_opts = Rex::Parser::Arguments.new(
-		"-h" => [ false, "Help banner." ],
-		"-l" => [ true,  "The local port to listen on." ],
-		"-r" => [ true,  "The remote host to connect to." ],
-		"-p" => [ true,  "The remote port to connect to." ],
-		"-L" => [ true,  "The local host to listen on (optional)." ])
-
-	#
-	# Options for the netstat command
-	#
-	@@netstat_opts = Rex::Parser::Arguments.new(
-		"-h" => [ false, "Help banner." ],
-		"-S" => [ true, "Row search string to be used as a regex"  ])
-
-	#
-	# List of supported commands.
-	#
-	def commands
-		all = {
-			"ipconfig" => "Display interfaces",
-			"ifconfig" => "Display interfaces",
-			"route"    => "View and modify the routing table",
-			"portfwd"  => "Forward a local port to a remote service",
-			"arp"      => "Display the host ARP cache",
-			"netstat"  => "Display the network connections",
-		}
-		reqs = {
-			"ipconfig" => [ "stdapi_net_config_get_interfaces" ],
-			"ifconfig" => [ "stdapi_net_config_get_interfaces" ],
-			"route"    => [
-				# Also uses these, but we don't want to be unable to list them
-				# just because we can't alter them.
-				#"stdapi_net_config_add_route",
-				#"stdapi_net_config_remove_route",
-				"stdapi_net_config_get_routes"
-			],
-			# Only creates tcp channels, which is something whose availability
-			# we can't check directly at the moment.
-			"portfwd"  => [ ],
-			"arp"      => [ "stdapi_net_config_get_arp_table" ],
-			"netstat"  => [ "stdapi_net_config_get_netstat" ],
-		}
-
-		all.delete_if do |cmd, desc|
-			del = false
-			reqs[cmd].each do |req|
-				next if client.commands.include? req
-				del = true
-				break
-			end
-
-			del
-		end
-
-		all
-	end
-
-	#
-	# Name for this dispatcher.
-	#
-	def name
-		"Stdapi: Networking"
-	end
-
-  def cmd_netstat_help
-	  print_line "Usage: netstat [options]"
-	  print_line
-	  print_line "Lists curent network connections"
-	  print_line @@netstat_opts.usage
+    attr_accessor :pfservice
   end
 
-	#
-	# Displays network connections of the remote machine.
-	#
-	def cmd_netstat(*args)
-		connection_table = client.net.config.netstat
+  #
+  # Options for the route command.
+  #
+  @@route_opts = Rex::Parser::Arguments.new(
+    "-h" => [ false, "Help banner." ])
+
+  #
+  # Options for the portfwd command.
+  #
+  @@portfwd_opts = Rex::Parser::Arguments.new(
+    "-h" => [ false, "Help banner." ],
+    "-l" => [ true,  "The local port to listen on." ],
+    "-r" => [ true,  "The remote host to connect to." ],
+    "-p" => [ true,  "The remote port to connect to." ],
+    "-L" => [ true,  "The local host to listen on (optional)." ])
+
+  #
+  # Options for the netstat command
+  #
+  @@netstat_opts = Rex::Parser::Arguments.new(
+    "-h" => [ false, "Help banner." ],
+    "-S" => [ true, "Row search string to be used as a regex"  ])
+
+  #
+  # List of supported commands.
+  #
+  def commands
+    all = {
+      "ipconfig" => "Display interfaces",
+      "ifconfig" => "Display interfaces",
+      "route"    => "View and modify the routing table",
+      "portfwd"  => "Forward a local port to a remote service",
+      "arp"      => "Display the host ARP cache",
+      "netstat"  => "Display the network connections",
+    }
+    reqs = {
+      "ipconfig" => [ "stdapi_net_config_get_interfaces" ],
+      "ifconfig" => [ "stdapi_net_config_get_interfaces" ],
+      "route"    => [
+        # Also uses these, but we don't want to be unable to list them
+        # just because we can't alter them.
+        #"stdapi_net_config_add_route",
+        #"stdapi_net_config_remove_route",
+        "stdapi_net_config_get_routes"
+      ],
+      # Only creates tcp channels, which is something whose availability
+      # we can't check directly at the moment.
+      "portfwd"  => [ ],
+      "arp"      => [ "stdapi_net_config_get_arp_table" ],
+      "netstat"  => [ "stdapi_net_config_get_netstat" ],
+    }
+
+    all.delete_if do |cmd, desc|
+      del = false
+      reqs[cmd].each do |req|
+        next if client.commands.include? req
+        del = true
+        break
+      end
+
+      del
+    end
+
+    all
+  end
+
+  #
+  # Name for this dispatcher.
+  #
+  def name
+    "Stdapi: Networking"
+  end
+
+  def cmd_netstat_help
+    print_line "Usage: netstat [options]"
+    print_line
+    print_line "Lists curent network connections"
+    print_line @@netstat_opts.usage
+  end
+
+  #
+  # Displays network connections of the remote machine.
+  #
+  def cmd_netstat(*args)
+    connection_table = client.net.config.netstat
     search_term = nil
     @@netstat_opts.parse(args) { |opt, idx, val|
     	case opt
       when '-S'
-	      search_term = val
-	      if search_term.nil?
+        search_term = val
+        if search_term.nil?
           print_error("Enter a search term")
           return true
-	      end
+        end
       when "-h"
         cmd_netstat_help
-	      return 0
+        return 0
       end
     }
 
     tbl = Rex::Ui::Text::Table.new(
 
-		'Header'  => "Connection list",
-		'Indent'  => 4,
-		'Columns' =>
-			[
-				"Proto",
-				"Local address",
-				"Remote address",
-				"State",
-				"User",
-				"Inode",
-				"PID/Program name"
-			],
-		'SearchTerm' => search_term)
+    'Header'  => "Connection list",
+    'Indent'  => 4,
+    'Columns' =>
+      [
+        "Proto",
+        "Local address",
+        "Remote address",
+        "State",
+        "User",
+        "Inode",
+        "PID/Program name"
+      ],
+    'SearchTerm' => search_term)
 
-		connection_table.each { |connection|
-			tbl << [ connection.protocol, connection.local_addr_str, connection.remote_addr_str,
-				connection.state, connection.uid, connection.inode, connection.pid_name]
-		}
+    connection_table.each { |connection|
+      tbl << [ connection.protocol, connection.local_addr_str, connection.remote_addr_str,
+        connection.state, connection.uid, connection.inode, connection.pid_name]
+    }
 
-		if tbl.rows.length > 0
-			print("\n" + tbl.to_s + "\n")
-		else
-			print_line("Connection list is empty.")
-		end
-	end
+    if tbl.rows.length > 0
+      print("\n" + tbl.to_s + "\n")
+    else
+      print_line("Connection list is empty.")
+    end
+  end
 
-	#
-	# Displays ARP cache of the remote machine.
-	#
-	def cmd_arp(*args)
-		arp_table = client.net.config.arp_table
-		tbl = Rex::Ui::Text::Table.new(
-		'Header'  => "ARP cache",
-		'Indent'  => 4,
-		'Columns' =>
-			[
-				"IP address",
-				"MAC address",
-				"Interface"
-			])
+  #
+  # Displays ARP cache of the remote machine.
+  #
+  def cmd_arp(*args)
+    arp_table = client.net.config.arp_table
+    tbl = Rex::Ui::Text::Table.new(
+    'Header'  => "ARP cache",
+    'Indent'  => 4,
+    'Columns' =>
+      [
+        "IP address",
+        "MAC address",
+        "Interface"
+      ])
 
-		arp_table.each { |arp|
-			tbl << [ arp.ip_addr, arp.mac_addr, arp.interface ]
-		}
+    arp_table.each { |arp|
+      tbl << [ arp.ip_addr, arp.mac_addr, arp.interface ]
+    }
 
-		if tbl.rows.length > 0
-			print("\n" + tbl.to_s + "\n")
-		else
-			print_line("ARP cache is empty.")
-		end
-	end
+    if tbl.rows.length > 0
+      print("\n" + tbl.to_s + "\n")
+    else
+      print_line("ARP cache is empty.")
+    end
+  end
 
 
-	#
-	# Displays interfaces on the remote machine.
-	#
-	def cmd_ipconfig(*args)
-		ifaces = client.net.config.interfaces
+  #
+  # Displays interfaces on the remote machine.
+  #
+  def cmd_ipconfig(*args)
+    ifaces = client.net.config.interfaces
 
-		if (ifaces.length == 0)
-			print_line("No interfaces were found.")
-		else
-			ifaces.sort{|a,b| a.index <=> b.index}.each do |iface|
-				print("\n" + iface.pretty + "\n")
-			end
-		end
-	end
+    if (ifaces.length == 0)
+      print_line("No interfaces were found.")
+    else
+      ifaces.sort{|a,b| a.index <=> b.index}.each do |iface|
+        print("\n" + iface.pretty + "\n")
+      end
+    end
+  end
 
-	alias :cmd_ifconfig :cmd_ipconfig
+  alias :cmd_ifconfig :cmd_ipconfig
 
-	#
-	# Displays or modifies the routing table on the remote machine.
-	#
-	def cmd_route(*args)
-		# Default to list
-		if (args.length == 0)
-			args.unshift("list")
-		end
+  #
+  # Displays or modifies the routing table on the remote machine.
+  #
+  def cmd_route(*args)
+    # Default to list
+    if (args.length == 0)
+      args.unshift("list")
+    end
 
-		# Check to see if they specified -h
-		@@route_opts.parse(args) { |opt, idx, val|
-			case opt
-				when "-h"
-					print(
-						"Usage: route [-h] command [args]\n\n" +
-						"Display or modify the routing table on the remote machine.\n\n" +
-						"Supported commands:\n\n" +
-						"   add    [subnet] [netmask] [gateway]\n" +
-						"   delete [subnet] [netmask] [gateway]\n" +
-						"   list\n\n")
-					return true
-			end
-		}
+    # Check to see if they specified -h
+    @@route_opts.parse(args) { |opt, idx, val|
+      case opt
+        when "-h"
+          print(
+            "Usage: route [-h] command [args]\n\n" +
+            "Display or modify the routing table on the remote machine.\n\n" +
+            "Supported commands:\n\n" +
+            "   add    [subnet] [netmask] [gateway]\n" +
+            "   delete [subnet] [netmask] [gateway]\n" +
+            "   list\n\n")
+          return true
+      end
+    }
 
-		cmd = args.shift
+    cmd = args.shift
 
-		# Process the commands
-		case cmd
-			when "list"
-				routes = client.net.config.routes
+    # Process the commands
+    case cmd
+      when "list"
+        routes = client.net.config.routes
 
-				# IPv4
-				tbl = Rex::Ui::Text::Table.new(
-					'Header'  => "IPv4 network routes",
-					'Indent'  => 4,
-					'Columns' =>
-						[
-							"Subnet",
-							"Netmask",
-							"Gateway",
-							"Metric",
-							"Interface"
-						])
+        # IPv4
+        tbl = Rex::Ui::Text::Table.new(
+          'Header'  => "IPv4 network routes",
+          'Indent'  => 4,
+          'Columns' =>
+            [
+              "Subnet",
+              "Netmask",
+              "Gateway",
+              "Metric",
+              "Interface"
+            ])
 
-				routes.select {|route|
-					Rex::Socket.is_ipv4?(route.netmask)
-				}.each { |route|
-					tbl << [ route.subnet, route.netmask, route.gateway, route.metric, route.interface ]
-				}
+        routes.select {|route|
+          Rex::Socket.is_ipv4?(route.netmask)
+        }.each { |route|
+          tbl << [ route.subnet, route.netmask, route.gateway, route.metric, route.interface ]
+        }
 
-				if tbl.rows.length > 0
-					print("\n" + tbl.to_s + "\n")
-				else
-					print_line("No IPv4 routes were found.")
-				end
+        if tbl.rows.length > 0
+          print("\n" + tbl.to_s + "\n")
+        else
+          print_line("No IPv4 routes were found.")
+        end
 
-				# IPv6
-				tbl = Rex::Ui::Text::Table.new(
-					'Header'  => "IPv6 network routes",
-					'Indent'  => 4,
-					'Columns' =>
-						[
-							"Subnet",
-							"Netmask",
-							"Gateway",
-							"Metric",
-							"Interface"
-						])
+        # IPv6
+        tbl = Rex::Ui::Text::Table.new(
+          'Header'  => "IPv6 network routes",
+          'Indent'  => 4,
+          'Columns' =>
+            [
+              "Subnet",
+              "Netmask",
+              "Gateway",
+              "Metric",
+              "Interface"
+            ])
 
-				routes.select {|route|
-					Rex::Socket.is_ipv6?(route.netmask)
-				}.each { |route|
-					tbl << [ route.subnet, route.netmask, route.gateway, route.metric, route.interface ]
-				}
+        routes.select {|route|
+          Rex::Socket.is_ipv6?(route.netmask)
+        }.each { |route|
+          tbl << [ route.subnet, route.netmask, route.gateway, route.metric, route.interface ]
+        }
 
-				if tbl.rows.length > 0
-					print("\n" + tbl.to_s + "\n")
-				else
-					print_line("No IPv6 routes were found.")
-				end
+        if tbl.rows.length > 0
+          print("\n" + tbl.to_s + "\n")
+        else
+          print_line("No IPv6 routes were found.")
+        end
 
-			when "add"
-													# Satisfy check to see that formatting is correct
-																unless Rex::Socket::RangeWalker.new(args[0]).length == 1
-																				print_error "Invalid IP Address"
-																				return false
-																end
+      when "add"
+                          # Satisfy check to see that formatting is correct
+                                unless Rex::Socket::RangeWalker.new(args[0]).length == 1
+                                        print_error "Invalid IP Address"
+                                        return false
+                                end
 
-																unless Rex::Socket::RangeWalker.new(args[1]).length == 1
-																				print_error "Invalid Subnet mask"
-																				return false
-																end
+                                unless Rex::Socket::RangeWalker.new(args[1]).length == 1
+                                        print_error "Invalid Subnet mask"
+                                        return false
+                                end
 
         print_line("Creating route #{args[0]}/#{args[1]} -> #{args[2]}")
 
-				client.net.config.add_route(*args)
-			when "delete"
-							# Satisfy check to see that formatting is correct
-																unless Rex::Socket::RangeWalker.new(args[0]).length == 1
-																				print_error "Invalid IP Address"
-																				return false
-																end
+        client.net.config.add_route(*args)
+      when "delete"
+              # Satisfy check to see that formatting is correct
+                                unless Rex::Socket::RangeWalker.new(args[0]).length == 1
+                                        print_error "Invalid IP Address"
+                                        return false
+                                end
 
-																unless Rex::Socket::RangeWalker.new(args[1]).length == 1
-																				print_error "Invalid Subnet mask"
-																				return false
-																end
+                                unless Rex::Socket::RangeWalker.new(args[1]).length == 1
+                                        print_error "Invalid Subnet mask"
+                                        return false
+                                end
 
         print_line("Deleting route #{args[0]}/#{args[1]} -> #{args[2]}")
 
