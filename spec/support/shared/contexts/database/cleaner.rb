3efc496f7829c0a341b102289adc56695aff63aa
Luke Imhoff <luke_imhoff@rapid7.com>
2013-09-21 17:15:47 -0500

    Re-entrant database cleaner
    [#56005352]
    
    Keep track if a connection had to be established by
    with_established_connection and only remove the connection if one was
    established in this call so that nested calls do not remove the
    connection of their ancestors.

--- spec/support/shared/contexts/database/cleaner.rb
+++ spec/support/shared/contexts/database/cleaner.rb
@@ -6,40 +6,47 @@ shared_context 'database cleaner' do |options={}|
 
   scope = options.fetch(:after, :each)
 
-	def with_established_connection
-		begin
-			ActiveRecord::Base.connection_pool.with_connection do
-				yield
-			end
-		rescue ActiveRecord::ConnectionNotEstablished
-			# if there isn't a connection established, then established one and try
-			# again
-			ActiveRecord::Base.configurations = Metasploit::Framework::Database.configurations
-			spec = ActiveRecord::Base.configurations[Metasploit::Framework.env]
-			ActiveRecord::Base.establish_connection(spec)
+  def with_established_connection
+    remove_connection = false
 
-			retry
-		ensure
-			# remove the established connection so that it doesn't leak into another
-			# example
-			ActiveRecord::Base.remove_connection
-		end
-	end
+    begin
+      ActiveRecord::Base.connection_pool.with_connection do
+        yield
+      end
+    rescue ActiveRecord::ConnectionNotEstablished
+      # if there isn't a connection established, then established one and try
+      # again
+      ActiveRecord::Base.configurations = Metasploit::Framework::Database.configurations
+      spec = ActiveRecord::Base.configurations[Metasploit::Framework.env]
+      ActiveRecord::Base.establish_connection(spec)
+      # allow with_established_connection to be reentrant so that it can be called in nested blocks: only the outer most
+      # block that actually creates the connection will remove it.
+      remove_connection = true
 
-	# clean before all in case last test run was interrupted before
-	# after(:each) could clean up
-	before(:all) do
-		with_established_connection do
-			DatabaseCleaner.clean_with(:truncation)
-		end
-	end
+      retry
+    ensure
+      # remove the established connection so that it doesn't leak into another
+      # example
+      if remove_connection
+        ActiveRecord::Base.remove_connection
+      end
+    end
+  end
 
-	# Clean up after each test
-	after(scope) do
-		with_established_connection do
-			# Testing using both :truncation and :deletion; :truncation took long
-			# for testing.
-			DatabaseCleaner.clean_with(:deletion)
-		end
-	end
+  # clean before all in case last test run was interrupted before
+  # after(:each) could clean up
+  before(:all) do
+    with_established_connection do
+      DatabaseCleaner.clean_with(:truncation)
+    end
+  end
+
+  # Clean up after each test
+  after(scope) do
+    with_established_connection do
+      # Testing using both :truncation and :deletion; :truncation took long
+      # for testing.
+      DatabaseCleaner.clean_with(:deletion)
+    end
+  end
 end
