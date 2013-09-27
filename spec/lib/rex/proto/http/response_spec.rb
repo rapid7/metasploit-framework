5e95df13704ba378294ee1da51d95e490f412dc9
Tod Beardsley <tod_beardsley@rapid7.com>
2013-09-27 10:02:22 -0500

    Convert local variables to HEREDOC methods

--- spec/lib/rex/proto/http/response_spec.rb
+++ spec/lib/rex/proto/http/response_spec.rb
@@ -2,104 +2,119 @@ require 'rex/proto/http/response'
 
 describe Rex::Proto::Http::Response do
 
-  get_cookies_test_no_cookies = '
-HTTP/1.1 200 OK
-Date: Fri, 26 Apr 2013 12:43:12 GMT
-Server: Apache/2.2.22 (Ubuntu)
-X-Powered-By: PHP/5.4.6-1ubuntu1.2
-Expires: Thu, 19 Nov 1981 08:52:00 GMT
-Cache-Control: private, max-age=10800, pre-check=10800
-Last-Modified: Fri, 26 Apr 2013 12:01:52 GMT
-Vary: Accept-Encoding
-Content-Length: 63951
-Keep-Alive: timeout=5, max=100
-Connection: Keep-Alive
-Content-Type: text/html
+  def get_cookies_test_no_cookies
+    <<-HEREDOC.gsub(/^ {6}/, '')
+      HTTP/1.1 200 OK
+      Date: Fri, 26 Apr 2013 12:43:12 GMT
+      Server: Apache/2.2.22 (Ubuntu)
+      X-Powered-By: PHP/5.4.6-1ubuntu1.2
+      Expires: Thu, 19 Nov 1981 08:52:00 GMT
+      Cache-Control: private, max-age=10800, pre-check=10800
+      Last-Modified: Fri, 26 Apr 2013 12:01:52 GMT
+      Vary: Accept-Encoding
+      Content-Length: 63951
+      Keep-Alive: timeout=5, max=100
+      Connection: Keep-Alive
+      Content-Type: text/html
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">'
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">'
+    HEREDOC
+  end
 
-  get_cookies_test_five_cookies = '
-HTTP/1.1 200 OK
-Date: Fri, 26 Apr 2013 08:44:54 GMT
-Server: Apache/2.2.22 (Ubuntu)
-X-Powered-By: PHP/5.4.6-1ubuntu1.2
-Set-Cookie: phpMyAdmin=gpjif0gtpqbvfion91ddtrq8p8vgjtue; path=/phpmyadmin/; HttpOnly
-Expires: Thu, 19 Nov 1981 08:52:00 GMT
-Cache-Control: private, max-age=10800, pre-check=10800
-Last-Modified: Sun, 12 Aug 2012 13:38:18 GMT
-Set-Cookie: pma_lang=en; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
-Set-Cookie: pma_collation_connection=utf8_general_ci; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
-Set-Cookie: pma_mcrypt_iv=mF1NmTE64IY%3D; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
-Set-Cookie: phpMyAdmin=fmilioji5cn4m8bo5vjrrr6q9cada954; path=/phpmyadmin/; HttpOnly
-Vary: Accept-Encoding
-Content-Length: 7356
-Keep-Alive: timeout=5, max=100
-Connection: Keep-Alive
-Content-Type: text/html; charset=utf-8
+  def get_cookies_test_five_cookies
+    <<-HEREDOC.gsub(/^ {6}/, '')
+      HTTP/1.1 200 OK
+      Date: Fri, 26 Apr 2013 08:44:54 GMT
+      Server: Apache/2.2.22 (Ubuntu)
+      X-Powered-By: PHP/5.4.6-1ubuntu1.2
+      Set-Cookie: phpMyAdmin=gpjif0gtpqbvfion91ddtrq8p8vgjtue; path=/phpmyadmin/; HttpOnly
+      Expires: Thu, 19 Nov 1981 08:52:00 GMT
+      Cache-Control: private, max-age=10800, pre-check=10800
+      Last-Modified: Sun, 12 Aug 2012 13:38:18 GMT
+      Set-Cookie: pma_lang=en; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
+      Set-Cookie: pma_collation_connection=utf8_general_ci; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
+      Set-Cookie: pma_mcrypt_iv=mF1NmTE64IY%3D; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
+      Set-Cookie: phpMyAdmin=fmilioji5cn4m8bo5vjrrr6q9cada954; path=/phpmyadmin/; HttpOnly
+      Vary: Accept-Encoding
+      Content-Length: 7356
+      Keep-Alive: timeout=5, max=100
+      Connection: Keep-Alive
+      Content-Type: text/html; charset=utf-8
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    HEREDOC
+  end
 
-  get_cookies_test_five_ordered_cookies = '
-HTTP/1.1 200 OK
-Date: Fri, 26 Apr 2013 08:44:54 GMT
-Server: Apache/2.2.22 (Ubuntu)
-X-Powered-By: PHP/5.4.6-1ubuntu1.2
-Expires: Thu, 19 Nov 1981 08:52:00 GMT
-Cache-Control: private, max-age=10800, pre-check=10800
-Last-Modified: Sun, 12 Aug 2012 13:38:18 GMT
-Set-Cookie: pma_lang=en; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
-Set-Cookie: pma_collation_connection=utf8_general_ci; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
-Set-Cookie: pma_mcrypt_iv=mF1NmTE64IY%3D; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
-Set-Cookie: phpMyAdmin=fmilioji5cn4m8bo5vjrrr6q9cada954; path=/phpmyadmin/; HttpOnly
-Set-Cookie: superC00kie!=stupidcookie; Path=/parp/; domain=.foo.com; HttpOnly; Expires=Wed, 13-Jan-2012 22:23:01 GMT; Secure
-Vary: Accept-Encoding
-Content-Length: 7356
-Keep-Alive: timeout=5, max=100
-Connection: Keep-Alive
-Content-Type: text/html; charset=utf-8
+  def get_cookies_test_five_ordered_cookies
+    <<-HEREDOC.gsub(/^ {6}/, '')
+      HTTP/1.1 200 OK
+      Date: Fri, 26 Apr 2013 08:44:54 GMT
+      Server: Apache/2.2.22 (Ubuntu)
+      X-Powered-By: PHP/5.4.6-1ubuntu1.2
+      Expires: Thu, 19 Nov 1981 08:52:00 GMT
+      Cache-Control: private, max-age=10800, pre-check=10800
+      Last-Modified: Sun, 12 Aug 2012 13:38:18 GMT
+      Set-Cookie: pma_lang=en; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
+      Set-Cookie: pma_collation_connection=utf8_general_ci; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
+      Set-Cookie: pma_mcrypt_iv=mF1NmTE64IY%3D; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
+      Set-Cookie: phpMyAdmin=fmilioji5cn4m8bo5vjrrr6q9cada954; path=/phpmyadmin/; HttpOnly
+      Set-Cookie: superC00kie!=stupidcookie; Path=/parp/; domain=.foo.com; HttpOnly; Expires=Wed, 13-Jan-2012 22:23:01 GMT; Secure
+      Vary: Accept-Encoding
+      Content-Length: 7356
+      Keep-Alive: timeout=5, max=100
+      Connection: Keep-Alive
+      Content-Type: text/html; charset=utf-8
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    HEREDOC
+  end
 
-  get_cookies_test_with_empty_cookie ='
-HTTP/1.1 200 OK
-Date: Fri, 26 Apr 2013 08:44:54 GMT
-Server: Apache/2.2.22 (Ubuntu)
-X-Powered-By: PHP/5.4.6-1ubuntu1.2
-Set-Cookie: phpMyAdmin=gpjif0gtpqbvfion91ddtrq8p8vgjtue; path=/phpmyadmin/; HttpOnly
-Expires: Thu, 19 Nov 1981 08:52:00 GMT
-Cache-Control: private, max-age=10800, pre-check=10800
-Last-Modified: Sun, 12 Aug 2012 13:38:18 GMT
-Set-Cookie: pma_lang=en; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
-Set-Cookie: pma_collation_connection=utf8_general_ci; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
-Set-Cookie: pma_mcrypt_iv=mF1NmTE64IY%3D; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
-Set-Cookie: phpMyAdmin=; path=/phpmyadmin/; HttpOnly
-Vary: Accept-Encoding
-Content-Length: 7356
-Keep-Alive: timeout=5, max=100
-Connection: Keep-Alive
-Content-Type: text/html; charset=utf-8
+  def get_cookies_test_with_empty_cookie
+    <<-HEREDOC.gsub(/^ {6}/, '')
+      HTTP/1.1 200 OK
+      Date: Fri, 26 Apr 2013 08:44:54 GMT
+      Server: Apache/2.2.22 (Ubuntu)
+      X-Powered-By: PHP/5.4.6-1ubuntu1.2
+      Set-Cookie: phpMyAdmin=gpjif0gtpqbvfion91ddtrq8p8vgjtue; path=/phpmyadmin/; HttpOnly
+      Expires: Thu, 19 Nov 1981 08:52:00 GMT
+      Cache-Control: private, max-age=10800, pre-check=10800
+      Last-Modified: Sun, 12 Aug 2012 13:38:18 GMT
+      Set-Cookie: pma_lang=en; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
+      Set-Cookie: pma_collation_connection=utf8_general_ci; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
+      Set-Cookie: pma_mcrypt_iv=mF1NmTE64IY%3D; expires=Sun, 26-May-2013 08:44:54 GMT; path=/phpmyadmin/; httponly
+      Set-Cookie: phpMyAdmin=; path=/phpmyadmin/; HttpOnly
+      Vary: Accept-Encoding
+      Content-Length: 7356
+      Keep-Alive: timeout=5, max=100
+      Connection: Keep-Alive
+      Content-Type: text/html; charset=utf-8
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    HEREDOC
+  end
 
-  get_cookies_test_one_set_cookie_header ='
-HTTP/1.1 200 OK
-Date: Wed, 25 Sep 2013 20:29:23 GMT
-Server: Apache/2.2.22 (Ubuntu)
-X-Powered-By: PHP/5.4.9-4ubuntu2.2
-Expires: Wed, 11 Jan 1984 05:00:00 GMT
-Last-Modified: Wed, 25 Sep 2013 20:29:23 GMT
-Cache-Control: no-cache, must-revalidate, max-age=0
-Pragma: no-cache
-Set-Cookie: wordpressuser_a97c5267613d6de70e821ff82dd1ab94=admin; path=/wordpress-2.0/, wordpresspass_a97c5267613d6de70e821ff82dd1ab94=c3284d0f94606de1fd2af172aba15bf3; path=/wordpress-2.0/
-Vary: Accept-Encoding
-Content-Length: 0
-Content-Type: text/html; charset=UTF-8
+  def get_cookies_test_one_set_cookie_header
+    <<-HEREDOC.gsub(/^ {6}/, '')
+      HTTP/1.1 200 OK
+      Date: Wed, 25 Sep 2013 20:29:23 GMT
+      Server: Apache/2.2.22 (Ubuntu)
+      X-Powered-By: PHP/5.4.9-4ubuntu2.2
+      Expires: Wed, 11 Jan 1984 05:00:00 GMT
+      Last-Modified: Wed, 25 Sep 2013 20:29:23 GMT
+      Cache-Control: no-cache, must-revalidate, max-age=0
+      Pragma: no-cache
+      Set-Cookie: wordpressuser_a97c5267613d6de70e821ff82dd1ab94=admin; path=/wordpress-2.0/, wordpresspass_a97c5267613d6de70e821ff82dd1ab94=c3284d0f94606de1fd2af172aba15bf3; path=/wordpress-2.0/
+      Vary: Accept-Encoding
+      Content-Length: 0
+      Content-Type: text/html; charset=UTF-8
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    HEREDOC
+end
 
   it 'get_cookies returns empty string for no Set-Cookies' do
     resp = described_class.new()
