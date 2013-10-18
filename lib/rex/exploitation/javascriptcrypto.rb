b0d614bc6af3c6f0b5587c57c00de4407a2d4494
sinn3r <wei_chen@rapid7.com>
2013-10-18 01:47:27 -0500

    Cleaning up requires

--- lib/rex/exploitation/javascriptcrypto.rb
+++ lib/rex/exploitation/javascriptcrypto.rb
@@ -10,10 +10,20 @@ module Exploitation
 #
 # Provides cryptographic functions in JavaScript
 #
-class JavascriptCrypto < JSObfu
+class JavascriptCrypto
 
   def self.base64
-  	::File.read(::File.join(::File.dirname(__FILE__), "../", "../", "../", "data", "js", "crypto", "base64.js"))
+    js = ::File.read(::File.join(::File.dirname(__FILE__), "../", "../", "../", "data", "js", "crypto", "base64.js"))
+
+    opts = {
+      'Symbols' => {
+        'Variables' => %w{ Base64 encoding result _keyStr encoded_data utftext input_idx
+          input output chr chr1 chr2 chr3 enc1 enc2 enc3 enc4 },
+        'Methods'   => %w{ _utf8_encode _utf8_decode encode decode }
+      }
+    }
+
+    ::Rex::Exploitation::ObfuscateJS.new(js, opts).to_s
   end
 
 end
