afcce8a511f74ae9e027b15fcbe95cdc9ee4c44d
sinn3r <wei_chen@rapid7.com>
2013-10-22 01:11:11 -0500

    Merge osdetect and addonsdetect

--- lib/rex/exploitation/js.rb
+++ lib/rex/exploitation/js.rb
@@ -1,7 +1,6 @@
 # -*- coding: binary -*-
 
-require 'rex/exploitation/js/addonsdetect'
 require 'rex/exploitation/js/memory'
 require 'rex/exploitation/js/network'
-require 'rex/exploitation/js/osdetect'
 require 'rex/exploitation/js/utils'
+require 'rex/exploitation/js/detect'
