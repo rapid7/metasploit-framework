f46cdb89704b5dc4cec7617b590f209b2a4a5464
jvazquez-r7 <juan.vazquez@metasploit.com>
2013-10-22 09:27:37 -0500

    Add the correct plate

--- modules/auxiliary/scanner/http/jenkins_vulnscan.rb
+++ modules/auxiliary/scanner/http/jenkins_vulnscan.rb
@@ -1,4 +1,9 @@
 ##
+# This module requires Metasploit: http//metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+##
 # Some of this code was taken from the "jboss_vulnscan" module by: Tyler Krpata
 ##
 
