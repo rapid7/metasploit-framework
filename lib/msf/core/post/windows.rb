a476595ddb4eb800772d93ed4c3a21237b4fa38c
b00stfr3ak <goduke44@gmail.com>
2013-10-25 14:42:22 -0700

    Added require to post/windows

--- lib/msf/core/post/windows.rb
+++ lib/msf/core/post/windows.rb
@@ -9,6 +9,7 @@ module Msf::Post::Windows
   require 'msf/core/post/windows/process'
   require 'msf/core/post/windows/railgun'
   require 'msf/core/post/windows/registry'
+  require 'msf/core/post/windows/runas'
   require 'msf/core/post/windows/services'
   require 'msf/core/post/windows/shadowcopy'
   require 'msf/core/post/windows/user_profiles'
