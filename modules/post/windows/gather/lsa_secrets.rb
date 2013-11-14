5b96ad595ff3f8e0335d2dd44476a581c8e88f1b
James Lee <egypt@metasploit.com>
2013-11-13 19:05:16 -0600

    Skip reg values with no secretes
    Also update header comment to match new standard

--- modules/post/windows/gather/lsa_secrets.rb
+++ modules/post/windows/gather/lsa_secrets.rb
@@ -1,8 +1,6 @@
 ##
-# This file is part of the Metasploit Framework and may be subject to
-# redistribution and commercial restrictions. Please see the Metasploit
-# web site for more information on licensing and terms of use.
-#   http://metasploit.com/
+# This module requires Metasploit: http//metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
 ##
 
 require 'msf/core'
@@ -53,6 +51,8 @@ class Metasploit3 < Msf::Post
 
         val_key = root_regkey + "\\" + secret_regkey + "\\" + mkeys
         encrypted_secret = registry_getvaldata(val_key, "")
+
+        next unless encrypted_secret
 
         if lsa_vista_style?
           # Magic happens here
