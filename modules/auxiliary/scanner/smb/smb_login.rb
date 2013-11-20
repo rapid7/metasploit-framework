542bff47b137dbcfcf4fa37b6a7ab37b0c3443da
Tod Beardsley <tod_beardsley@rapid7.com>
2013-11-20 09:14:11 -0600

    Remove duplicate line for Meatballs authorship
    Spotted by sho_luv

--- modules/auxiliary/scanner/smb/smb_login.rb
+++ modules/auxiliary/scanner/smb/smb_login.rb
@@ -32,7 +32,6 @@ class Metasploit3 < Msf::Auxiliary
       'Author'         =>
         [
           'tebo <tebo [at] attackresearch [dot] com>', # Original
-          'Ben Campbell <eat_meatballs [at] hotmail.co.uk>' # Refactoring
           'Ben Campbell <eat_meatballs [at] hotmail.co.uk>', # Refactoring
           'Brandon McCann "zeknox" <bmccann [at] accuvant.com>'
         ],
