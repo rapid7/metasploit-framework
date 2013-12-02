8d6a5345822d43ed43621ff87b9160071ebd0972
jvazquez-r7 <juan_vazquez@rapid7.com>
2013-12-02 08:54:37 -0600

    Change title

--- modules/auxiliary/scanner/http/oracle_ilom_login.rb
+++ modules/auxiliary/scanner/http/oracle_ilom_login.rb
@@ -14,7 +14,7 @@ class Metasploit3 < Msf::Auxiliary
 
   def initialize(info={})
     super(update_info(info,
-      'Name'           => 'Oracle ILO Manager Login Utility',
+      'Name'           => 'Oracle ILO Manager Login Brute Force Utility',
       'Description'    => %{
         This module scans for Oracle Integrated Lights Out Manager login portal, and performs login brute force
         to identify valid credentials.
