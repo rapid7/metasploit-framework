040a629f344081fa285716e92f82fcb17da711b1
joev <joev@metasploit.com>
2013-12-01 20:17:43 -0600

    Kill meterpreter support.
    * Meterpreter seems to fall over on the cmd escaping, and dies if you
    try to pass it an array of args (python/java meterpreter on various versions
    of osx).

--- modules/post/osx/manage/mount_share.rb
+++ modules/post/osx/manage/mount_share.rb
@@ -32,7 +32,7 @@ class Metasploit3 < Msf::Post
             'joev'
           ],
         'Platform'      => [ 'osx' ],
-        'SessionTypes'  => [ 'shell', 'meterpreter' ],
+        'SessionTypes'  => [ 'shell' ],
         'Actions'       => [
           [ 'LIST',    { 'Description' => 'Show a list of stored network share credentials' } ],
           [ 'MOUNT',   { 'Description' => 'Mount a network shared volume using stored credentials' } ],
