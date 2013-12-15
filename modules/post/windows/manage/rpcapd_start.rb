6d5aefd6207696ffaafbc4a38c2c9cc6bf1626c7
Meatballs <eat_meatballs@hotmail.co.uk>
2013-12-15 18:09:57 +0000

    rpcapd_start service_info fixup

--- modules/post/windows/manage/rpcapd_start.rb
+++ modules/post/windows/manage/rpcapd_start.rb
@@ -41,10 +41,10 @@ class Metasploit3 < Msf::Post
       serv = service_info("rpcapd")
       print_status("Checking if machine #{sysinfo['Computer']} has rpcapd service")
 
-      if serv['Name'] !~ /remote/i
+      if serv[:display_name] !~ /remote/i
         print_error("This machine doesn't seem to have the rpcapd service")
       else
-        print_status("Rpcap service found: #{serv['Name']}")
+        print_status("Rpcap service found: #{serv[:display_name]}")
         reg=registry_getvaldata("HKLM\\SYSTEM\\CurrentControlSet\\Services\\rpcapd","Start")
         prog=expand_path("%ProgramFiles%") << "\\winpcap\\rpcapd.exe"
         if reg != 2
