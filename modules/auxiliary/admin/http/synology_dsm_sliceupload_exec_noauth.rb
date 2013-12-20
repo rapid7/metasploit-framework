15f6a62f90cf4c3cd5d3ab74b97c4b6be2abf825
Markus Wulftange <markus.wulftange@gmail.com>
2013-12-20 15:10:10 +0100

    Msf::Exploit::Remote::HttpClient already provides 'peer'

--- modules/auxiliary/admin/http/synology_dsm_sliceupload_exec_noauth.rb
+++ modules/auxiliary/admin/http/synology_dsm_sliceupload_exec_noauth.rb
@@ -48,10 +48,6 @@ class Metasploit3 < Msf::Auxiliary
       ], self.class)
   end
 
-  def peer
-    "#{rhost}:#{rport}"
-  end
-
   def check
     print_status("#{peer} - Trying to detect installed version")
 
