f99a5b8b47d40764dcfb07d1a856d74d5e19fddb
Meatballs <eat_meatballs@hotmail.co.uk>
2013-12-20 13:18:01 +0000

    Update for extapi

--- modules/post/windows/gather/enum_ad_user_comments.rb
+++ modules/post/windows/gather/enum_ad_user_comments.rb
@@ -5,7 +5,6 @@
 
 require 'rex'
 require 'msf/core'
-require 'msf/core/auxiliary/report'
 
 class Metasploit3 < Msf::Post
 
@@ -32,69 +31,46 @@ class Metasploit3 < Msf::Post
     register_options([
       OptInt.new('MAX_SEARCH', [true, 'Maximum values to retrieve, 0 for all.', 50]),
       OptBool.new('STORE_LOOT', [true, 'Store file in loot.', false]),
-      OptString.new('ATTRIBS', [true, 'Attributes to retrieve.', 'sAMAccountName,userAccountControl,description']),
+      OptString.new('FIELDS', [true, 'Fields to retrieve.', 'sAMAccountName,userAccountControl,description']),
       OptString.new('FILTER', [true, 'Search filter.', '(&(&(&(&(objectCategory=person)(objectClass=user)(description=*pass*)))))']),
     ], self.class)
   end
 
   def run
-    print_status("Connecting to default LDAP server")
-    session_handle = bind_default_ldap_server(datastore['MAX_SEARCH'])
-
-    return false unless session_handle
-
-    print_status("Querying default naming context")
-
-    query_result = query_ldap(session_handle, "", 0, "(objectClass=user)", ["defaultNamingContext"])
-    first_entry_attributes = query_result[0]['attributes']
-    # Value from First Attribute of First Entry
-    defaultNamingContext = first_entry_attributes[0]['values']
-
-    print_status("Default Naming Context #{defaultNamingContext}")
-
-    attributes = datastore['ATTRIBS'].gsub(/\s+/,"").split(',')
-
+    fields = datastore['FIELDS'].gsub(/\s+/,"").split(',')
     search_filter = datastore['FILTER']
+    max_search = datastore['MAX_SEARCH']
+    q = query(search_filter, max_search, fields)
 
-    print_status("Querying #{search_filter} - Please wait...")
-    results = query_ldap(session_handle, defaultNamingContext, 2, search_filter, attributes)
-
-    print_status("Unbinding from LDAP service.")
-    wldap32.ldap_unbind(session_handle)
-
-    if results.nil? or results.empty?
+    if q.nil? or q[:results].empty?
       return
     end
 
     # Results table holds raw string data
     results_table = Rex::Ui::Text::Table.new(
-        'Header'     => "#{defaultNamingContext} Domain Users",
+        'Header'     => "Domain Users",
         'Indent'     => 1,
         'SortIndex'  => -1,
-        'Columns'    => attributes
+        'Columns'    => fields
       )
 
-    # Hostnames holds DNS Names to Resolve
-    hostnames = []
-    # Reports are collections for easy database insertion
-    reports = []
-    results.each do |result|
+    q[:results].each do |result|
       row = []
 
       report = {}
-      result['attributes'].each do |attr|
-        if attr['values'].nil?
+      result.each do |field|
+        if field.nil?
           row << ""
         else
-          row << attr['values']
+          row << field
         end
       end
 
-      reports << report
       results_table << row
     end
 
     print_line results_table.to_s
+
     if datastore['STORE_LOOT']
       stored_path = store_loot('ad.users', 'text/plain', session, results_table.to_csv)
       print_status("Results saved to: #{stored_path}")
