b07dfc4f44b8a49437bc15c309a455ded293f60f
Timothy Swartz <totaldowner@gmail.com>
2013-12-24 17:43:14 -0800

    Added YARD tags to msf/core/logging.rb

--- lib/msf/base/logging.rb
+++ lib/msf/base/logging.rb
@@ -4,19 +4,18 @@ require 'msf/base'
 
 module Msf
 
-###
-#
 # This module provides an initialization interface for logging.
-#
-###
 class Logging
 
+  #Is logging initialized
+  #@private
   @@initialized = false
+  #Is session logging enabled
+  #@private
   @@session_logging = false
 
-  #
   # Initialize logging.
-  #
+  # @return [void]
   def self.init
     if (! @@initialized)
       @@initialized = true
@@ -35,9 +34,12 @@ class Logging
     end
   end
 
-  #
-  # Enables a log source.
-  #
+  # Enables a log source of name src. Creates the .log file in the
+  # configured directory if logging is not already enabled for this
+  # source.
+  # @param [String] src log source name.
+  # @param [Integer] level logging level.
+  # @return [void]
   def self.enable_log_source(src, level = 0)
     if (log_source_registered?(src) == false)
       f = Rex::Logging::Sinks::Flatfile.new(
@@ -47,30 +49,29 @@ class Logging
     end
   end
 
-  #
   # Stops logging for a given log source.
-  #
+  # @param [String] src the log source to disable.
+  # @return [Boolean] true if successful. false if not.
   def self.disable_log_source(src)
     deregister_log_source(src)
   end
 
-  #
   # Sets whether or not session logging is to be enabled.
-  #
+  # @param [Boolean] tf true if enabling. false if disabling.
+  # @return [void]
   def self.enable_session_logging(tf)
     @@session_logging = tf
   end
 
-  #
   # Returns whether or not session logging is enabled.
-  #
+  # @return [Boolean] true if enabled. false if disabled.
   def self.session_logging_enabled?
     @@session_logging || false
   end
 
-  #
   # Starts logging for a given session.
-  #
+  # @param [Msf::Core::Session] session the session to start logging on.
+  # @return [void]
   def self.start_session_log(session)
     if (log_source_registered?(session.log_source) == false)
       f = Rex::Logging::Sinks::Flatfile.new(
@@ -82,9 +83,9 @@ class Logging
     end
   end
 
-  #
   # Stops logging for a given session.
-  #
+  # @param [Msf::Core::Session] session the session to stop logging.
+  # @return [Boolean] true if sucessful. false if not.
   def self.stop_session_log(session)
     rlog("\n[*] Logging stopped: #{Time.now}\n\n", session.log_source)
 
