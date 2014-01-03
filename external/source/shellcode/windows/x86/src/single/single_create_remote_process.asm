25d48b73009aca99a6665c10cec07d3d621b3de2
Florian Gaultier <florian@scrt.ch>
2014-01-03 12:16:37 +0100

    Add create_remote_process block, now used in exe_service generation

--- external/source/shellcode/windows/x86/src/single/single_create_remote_process.asm
+++ external/source/shellcode/windows/x86/src/single/single_create_remote_process.asm
@@ -3,7 +3,7 @@
 ; Compatible: Windows 7, 2008, Vista, 2003, XP, 2000, NT4
 ; Version: 1.0 (28 July 2009)
 ; Size: 189 bytes + strlen(libpath) + 1
-; Build: >build.py single_service_stuff
+; Build: >build.py single_create_remote_process
 ;-----------------------------------------------------------------------------;
 
 [BITS 32]
@@ -14,5 +14,4 @@
 %include "./src/block/block_api.asm"
 start:                   ;
   pop ebp                ; pop off the address of 'api_call' for calling later.
-%include "./src/block/block_service.asm"
 %include "./src/block/block_create_remote_process.asm"
