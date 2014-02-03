cfebdae9d8cb6ca64f9bd9559882bd6cafae208c
parzamendi-r7 <peter_arzamendi@rapid7.com>
2014-02-03 12:47:24 -0600

    Catch and deal with Rex::Proto::SMB::Exceptions::NoReply error from ensure simple.connect

--- modules/auxiliary/scanner/smb/smb_login.rb
+++ modules/auxiliary/scanner/smb/smb_login.rb
@@ -138,7 +138,11 @@ class Metasploit3 < Msf::Auxiliary
       rescue
         status_code = :not_admin
       ensure
-        simple.connect("\\\\#{datastore['RHOST']}\\IPC$")
+        begin
+          simple.connect("\\\\#{datastore['RHOST']}\\IPC$")
+        rescue ::Rex::Proto::SMB::Exceptions::NoReply
+         nil
+        end
       end
     end
 
