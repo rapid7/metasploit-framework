2ee1764ceb23c406ac19dd9edc5e4e057f04e5e0
sinn3r <wei_chen@rapid7.com>
2014-02-03 01:05:43 -0600

    Add method rhost, rport, and peer for post modules
    [SeeRM #8761]

--- lib/msf/core/post/common.rb
+++ lib/msf/core/post/common.rb
@@ -2,6 +2,28 @@
 
 module Msf::Post::Common
 
+  def rhost
+    case session.type
+    when 'meterpreter'
+      session.sock.peerhost
+    when 'shell'
+      session.session_host
+    end
+  end
+
+  def rport
+    case session.type
+    when 'meterpreter'
+      session.sock.peerport
+    when 'shell'
+      session.session_port
+    end
+  end
+
+  def peer
+    "#{rhost}:#{rport}"
+  end
+
   #
   # Checks if the remote system has a process with ID +pid+
   #
