ce2de8f3bfc3a802e6ccdf205de8cc15abe951b4
sinn3r <wei_chen@rapid7.com>
2014-02-12 15:08:20 -0600

    Different way to write this

--- modules/post/windows/gather/enum_ad_user_comments.rb
+++ modules/post/windows/gather/enum_ad_user_comments.rb
@@ -46,15 +46,10 @@ class Metasploit3 < Msf::Post
       if q.nil? or q[:results].empty?
         return
       end
-    rescue ::Exception => e
-      if e.kind_of?(RuntimeError) or e.kind_of?(::Rex::Post::Meterpreter::RequestError)
-        # Can't bind or in a network w/ limited accounts
-        print_error(e.message)
-        return
-      else
-        # Unexpected, raise it
-        raise $1
-      end
+    rescue ::RuntimeError, ::Rex::Post::Meterpreter::RequestError => e
+      # Can't bind or in a network w/ limited accounts
+      print_error(e.message)
+      return
     end
 
     # Results table holds raw string data
