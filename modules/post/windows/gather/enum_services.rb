2af503df11fa4888cbcc349f476e2d3e8c80e194
James Lee <egypt@metasploit.com>
2014-02-18 16:25:55 -0600

    Add Post::Windows::Services#each_service
    Replaces Post::Windows::Services#service_list, which gathered all the
    services' info from the remote system before returning or printing
    anything.
    
    Deprecates post/windows/escalate/net_runtime_modify in favor of a new
    shiny local exploit version, exploit/windows/local/net_runtime_modify

--- modules/post/windows/gather/enum_services.rb
+++ modules/post/windows/gather/enum_services.rb
@@ -15,12 +15,14 @@ class Metasploit3 < Msf::Post
     super(update_info(info,
       'Name'                 => "Windows Gather Service Info Enumeration",
       'Description'          => %q{
-        This module will query the system for services and display name and configuration
-        info for each returned service. It allows you to optionally search the credentials, path, or start
-        type for a string and only return the results that match. These query operations
-        are cumulative and if no query strings are specified, it just returns all services.
-        NOTE: If the script hangs, windows firewall is most likely on and you did not
-        migrate to a safe process (explorer.exe for example).
+        This module will query the system for services and display name and
+        configuration info for each returned service. It allows you to
+        optionally search the credentials, path, or start type for a string
+        and only return the results that match. These query operations are
+        cumulative and if no query strings are specified, it just returns all
+        services.  NOTE: If the script hangs, windows firewall is most likely
+        on and you did not migrate to a safe process (explorer.exe for
+        example).
         },
       'License'              => MSF_LICENSE,
       'Platform'             => ['win'],
@@ -39,53 +41,55 @@ class Metasploit3 < Msf::Post
   def run
 
     # set vars
-    qcred = datastore["CRED"] || nil
-    qpath = datastore["PATH"] || nil
+    qcred = datastore["CRED"]
+    qpath = datastore["PATH"]
+
+    if qcred
+      qcred = qcred.downcase
+      print_status("Credential Filter: " + qcred)
+    end
+
+    if qpath
+      qpath = qpath.downcase
+      print_status("Executable Path Filter: " + qpath)
+    end
+
     if datastore["TYPE"] == "All"
       qtype = nil
     else
-      qtype = datastore["TYPE"]
-    end
-    if qcred
-      print_status("Credential Filter: " + qcred)
-    end
-    if qpath
-      print_status("Executable Path Filter: " + qpath)
-    end
-    if qtype
+      qtype = datastore["TYPE"].downcase
       print_status("Start Type Filter: " + qtype)
     end
 
-    print_status("Listing Service Info for matching services:")
-    service_list.each do |sname|
+    print_status("#{session.session_host} - Listing Service Info for matching services:")
+    each_service do |sname|
       srv_conf = {}
-      isgood = true
-      #make sure we got a service name
+
+      # make sure we got a service name
       if sname
         begin
           srv_conf = service_info(sname)
-          #filter service based on filters passed, the are cumulative
-          if qcred and ! srv_conf['Credentials'].downcase.include? qcred.downcase
-            isgood = false
-          end
-          if qpath and ! srv_conf['Command'].downcase.include? qpath.downcase
-            isgood = false
-          end
-          # There may not be a 'Startup', need to check nil
-          if qtype and ! (srv_conf['Startup'] || '').downcase.include? qtype.downcase
-            isgood = false
-          end
-
-          #if we are still good return the info
-          if isgood
-            vprint_status("\tName: #{sname}")
-            vprint_good("\t\tStartup: #{srv_conf['Startup']}")
-            vprint_good("\t\tCommand: #{srv_conf['Command']}")
-            vprint_good("\t\tCredentials: #{srv_conf['Credentials']}")
-          end
-        rescue
-          print_error("An error occured enumerating service: #{sname}")
+        rescue => e
+          print_error("Error enumerating service '#{sname}': #{e}")
+          next
         end
+
+        # filter service based on filters passed, they are cumulative
+        if qcred && !srv_conf['Credentials'].downcase.include?(qcred)
+          next
+        end
+        if qpath && !srv_conf['Command'].downcase.include?(qpath)
+          next
+        end
+        # There may not be a 'Startup', need to check nil
+        if qtype && !(srv_conf['Startup'] || '').downcase.include?(qtype)
+          next
+        end
+
+        print_status("\tName: #{sname}")
+        print_good("\t\tStartup: #{srv_conf['Startup']}")
+        print_good("\t\tCommand: #{srv_conf['Command']}")
+        print_good("\t\tCredentials: #{srv_conf['Credentials']}")
       else
         print_error("Problem enumerating services")
       end
