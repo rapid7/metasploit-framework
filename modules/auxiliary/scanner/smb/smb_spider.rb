ceb4c9e4d77abdcedd7e25824ac6c219a73d8b97
Alton Johnson <alton.jx@gmail.com>
2014-02-20 15:45:13 -0600

    Updated reference in smb_spider.rb

--- modules/auxiliary/scanner/smb/smb_spider.rb
+++ modules/auxiliary/scanner/smb/smb_spider.rb
@@ -26,7 +26,7 @@ class Metasploit3 < Msf::Auxiliary
         ],
       'License'     => MSF_LICENSE,
       'References'   => [
-         ['URL','https://github.com/altjx/ipwn/blob/master/smb_spider.md']
+         ['URL','http://altonjx.wordpress.com/2014/02/20/finding-the-juicy-files-during-post-exploitation/']
       ]
     ))
 
@@ -38,7 +38,7 @@ class Metasploit3 < Msf::Auxiliary
     OptString.new('RootDir', [false, 'Root directory within share to spider','/']),
     OptPath.new('ShareFile', [false, 'Import list of \\\\IP\\Share formatted lines']),
     OptString.new('Verbose', [true, 'Display verbose information', true]),
-    OptString.new('LogResults', [false, 'Outputs spider results to smbspider/ip_share.txt.', false])
+    OptString.new('LogResults', [false, 'Outputs spider results to smbspider/ip_share.txt', false])
     ], self.class)
 
   end
