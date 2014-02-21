fcfb48fda150ddf524d7421d53500768c0782274
kn0 <Trenton.Ivey@gmail.com>
2014-02-21 13:37:31 -0600

    Added support for Gemfile.local

--- lib/msfenv.rb
+++ lib/msfenv.rb
@@ -2,7 +2,13 @@
 # Use bundler to load dependencies
 #
 
-ENV['BUNDLE_GEMFILE'] ||= ::File.expand_path(::File.join(::File.dirname(__FILE__), "..", "Gemfile"))
+gemfile_base = ::File.expand_path(::File.join(::File.dirname(__FILE__), ".."))
+if File.readable?(::File.join(gemfile_base,"Gemfile.local"))
+  ENV['BUNDLE_GEMFILE'] ||= ::File.join(gemfile_base, "Gemfile.local")
+else
+  ENV['BUNDLE_GEMFILE'] ||= ::File.join(gemfile_base, "Gemfile")
+end
+
 begin
   require 'bundler/setup'
 rescue ::LoadError
