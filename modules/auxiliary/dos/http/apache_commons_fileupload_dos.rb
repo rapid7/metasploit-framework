de6be50d64b026643ed236f3f8651e2c846aea08
Tod Beardsley <tod_beardsley@rapid7.com>
2014-03-03 14:12:22 -0600

    Minor cleanup and finger-wagging about a for loop

--- modules/auxiliary/dos/http/apache_commons_fileupload_dos.rb
+++ modules/auxiliary/dos/http/apache_commons_fileupload_dos.rb
@@ -57,6 +57,9 @@ class Metasploit4 < Msf::Auxiliary
       }
     }
 
+    # XXX: There is rarely, if ever, a need for a 'for' loop in Ruby
+    #      This should be rewritten with 1.upto() or Enumerable#each or
+    #      something
     for x in 1..datastore['RLIMIT']
       print_status("Sending request #{x} to #{peer}")
       begin
