e8226f9d40eeba2b8a6bf721c9c4c7a24a0fa672
Joe Vennix <joev@metasploit.com>
2014-03-02 19:47:24 -0600

    Use a keyed cookie. Moves AJAX call to a form post.

--- data/js/network/ajax_post.js
+++ data/js/network/ajax_post.js
@@ -8,3 +8,27 @@ function postInfo(path, data) {
   xmlHttp.open('POST', path, false);
   xmlHttp.send(data);
 }
+
+function postForm(path, data) {
+  var set = function(obj, attr, val) {
+    if (obj.setAttribute) { obj.setAttribute(attr, val); }
+    else { obj[attr] = val; }
+  }
+
+  var form = document.createElement('form');
+  set(form, 'method', 'POST');
+  set(form, 'action', path);
+
+  var input;
+  for (var i in data) {
+    input = document.createElement('input')
+    set(input, 'type', 'hidden');
+    set(input, 'name', i);
+    set(input, 'value', data[i]);
+    form.appendChild(input);
+  }
+
+  form.style.display = 'none';
+  document.body.appendChild(form);
+  form.submit();
+}
