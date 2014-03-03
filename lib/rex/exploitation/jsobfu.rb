ae9ce962c043b41978526cd999463cd94c369089
Joe Vennix <joev@metasploit.com>
2014-03-03 14:59:46 -0600

    Add future reserved words.
    Gotta stay ahead of the game.

--- lib/rex/exploitation/jsobfu.rb
+++ lib/rex/exploitation/jsobfu.rb
@@ -52,7 +52,8 @@ class JSObfu
   RESERVED_KEYWORDS = %w(
     break case catch continue debugger default delete do else finally
     for function if in instanceof new return switch this throw try
-    typeof var void while with
+    typeof var void while with class enum export extends import super
+    implements interface let package private protected public static yield
   )
 
   #
