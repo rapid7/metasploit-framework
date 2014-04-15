3c9507c30c24ce5668ac26c82827843c38ee0f59
David Maloney <DMaloney@rapid7.com>
2014-04-15 13:23:24 -0500

    Adds invalid exception class
    adds an invalid exception class to the
    LoginScanner namespace.
    
    MSP-9683

---
+++ lib/msf/core/auxiliary/login_scanner/invalid.rb
@@ -0,0 +1,17 @@
+module Msf
+  module LoginScanner
+
+    class Invalid < StandardError
+
+      attr_reader :model
+
+      def initialize(model)
+        @model = model
+
+        errors = @model.errors.full_messages.join(', ')
+        super(errors)
+      end
+    end
+
+  end
+end
---
+++ lib/msf/core/auxiliary/login_scanner/ssh.rb
@@ -0,0 +1,2 @@
+class SSH
+end
