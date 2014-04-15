60c879c8246c395b74d373b613d7d086fd070b85
David Maloney <DMaloney@rapid7.com>
2014-04-15 17:25:55 -0500

    specs for port validation
    created specs for port validation
    
    MSP-9683

--- spec/lib/metasploit/framework/login_scanner/ssh_spec.rb
+++ spec/lib/metasploit/framework/login_scanner/ssh_spec.rb
@@ -1,10 +1,48 @@
 require 'spec_helper'
-require 'msf/core/auxiliary/login_scanner/ssh'
+require 'metasploit/framework/login_scanner/ssh'
 
-describe Msf::Auxiliary::LoginScanner::SSH do
+describe Metasploit::Framework::LoginScanner::SSH do
 
-  it 'should do something' do
+  subject(:ssh_scanner) {
+    described_class.new
+  }
 
-    true.should == false
+  it { should respond_to :port }
+  it { should respond_to :host }
+  it { should respond_to :cred_pairs }
+
+  context 'validations' do
+    context 'port' do
+
+      it 'is not valid for a non-number' do
+        ssh_scanner.port = "a"
+        expect(ssh_scanner).to_not be_valid
+        expect(ssh_scanner.errors[:port]).to include "is not a number"
+      end
+
+      it 'is not valid for a floating point' do
+        ssh_scanner.port = 5.76
+        expect(ssh_scanner).to_not be_valid
+        expect(ssh_scanner.errors[:port]).to include "must be an integer"
+      end
+
+      it 'is not valid for a negative number' do
+        ssh_scanner.port = -8
+        expect(ssh_scanner).to_not be_valid
+        expect(ssh_scanner.errors[:port]).to include "must be greater than or equal to 1"
+      end
+
+      it 'is not valid for 0' do
+        ssh_scanner.port = 0
+        expect(ssh_scanner).to_not be_valid
+        expect(ssh_scanner.errors[:port]).to include "must be greater than or equal to 1"
+      end
+
+      it 'is not valid for a number greater than 65535' do
+        ssh_scanner.port = rand(1000) + 65535
+        expect(ssh_scanner).to_not be_valid
+        expect(ssh_scanner.errors[:port]).to include "must be less than or equal to 65535"
+      end
+    end
   end
 end
