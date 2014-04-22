8f6567967db18edbd13584b165c4b058c0c38ff8
Wiesław Kielas <wieslaw.kielas@bluemedia.pl>
2014-04-22 17:36:06 +0200

    Heartbleed PostgreSQL TLS support improvements

--- modules/auxiliary/scanner/ssl/openssl_heartbleed.rb
+++ modules/auxiliary/scanner/ssl/openssl_heartbleed.rb
@@ -225,9 +225,14 @@ class Metasploit3 < Msf::Auxiliary
   end
 
   def tls_postgres
+    # postgresql TLS - works with all modern pgsql versions - 8.0 - 9.3
     # http://www.postgresql.org/docs/9.3/static/protocol-message-formats.html
     sock.get_once
-    sock.put("\x00\x00\x00\x08\x04\xD2\x16\x2F")
+    # the postgres SSLRequest packet is a int32(8) followed by a int16(1234), 
+    # int16(5679) in network format
+    psql_sslrequest = [8].pack('N')
+    psql_sslrequest << [1234, 5679].pack('n*')
+    sock.put(psql_sslrequest)
     res = sock.get_once
     unless res && res =~ /S/
       return nil
