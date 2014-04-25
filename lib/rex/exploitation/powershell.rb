206184007f73d2613594e75d72ab62b0b9d37dd3
Meatballs <eat_meatballs@hotmail.co.uk>
2014-04-25 15:16:15 +0100

    Move methods and rename file so it is run by rspec

--- lib/rex/exploitation/powershell.rb
+++ lib/rex/exploitation/powershell.rb
@@ -8,6 +8,53 @@ module Exploitation
 
 module Powershell
 
+  #
+  # Reads script into a PowershellScript
+  #
+  # @param script_path [String] Path to the Script File
+  #
+  # @return [PowershellScript] PowerShellScript object
+  def read_script(script_path)
+    return PowershellScript.new(script_path)
+  end
+
+  #
+  # Insert substitutions into the powershell script
+  # If script is a path to a file then read the file
+  # otherwise treat it as the contents of a file
+  #
+  # @param script [String] Script file or path to script
+  # @param subs [Array] Substitutions to insert
+  #
+  # @return [String] Modified script file
+  def make_subs(script, subs)
+    if ::File.file?(script)
+      script = ::File.read(script)
+    end
+
+    subs.each do |set|
+      script.gsub!(set[0],set[1])
+    end
+
+    script
+  end
+
+  #
+  # Return an array of substitutions for use in make_subs
+  #
+  # @param subs [String] A ; seperated list of substitutions
+  #
+  # @return [Array] An array of substitutions
+  def process_subs(subs)
+    return [] if subs.nil? or subs.empty?
+    new_subs = []
+    subs.split(';').each do |set|
+      new_subs << set.split(',', 2)
+    end
+
+    new_subs
+  end
+
   module Output
 
     #
