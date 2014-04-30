b617be3ddac3cd9556de4f09f2f9111c2be5fa4a
James Lee <egypt@metasploit.com>
2014-04-30 13:30:42 -0500

    Move doc to the right place

--- lib/metasploit/framework/login_scanner.rb
+++ lib/metasploit/framework/login_scanner.rb
@@ -2,12 +2,11 @@ require 'metasploit/framework/login_scanner/credential'
 require 'metasploit/framework/login_scanner/invalid'
 require 'metasploit/framework/login_scanner/result'
 
-# This module provides the namespace for all LoginScanner classes.
-# LoginScanners are the classes that provide functionality for testing
-# authentication against various different protocols and mechanisms.
-
 module Metasploit
   module Framework
+    # This module provides the namespace for all LoginScanner classes.
+    # LoginScanners are the classes that provide functionality for testing
+    # authentication against various different protocols and mechanisms.
     module LoginScanner
     end
   end
