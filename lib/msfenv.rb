cea7b6cd771c56d7cc4c214201f7d91cbae903f7
Luke Imhoff <luke_imhoff@rapid7.com>
2014-05-12 15:37:59 -0500

    Revert to production as default environment
    MSP-9606
    
    When switching to Rails.env to integrate better with railties for
    Rails::Engines, I forgot that rails would default to development instead
    of production.

--- lib/msfenv.rb
+++ lib/msfenv.rb
@@ -2,6 +2,10 @@
 # Use bundler to load dependencies
 #
 
+# Override the normal rails default, so that msfconsole will come up in production mode instead of development mode
+# unless the `--environment` flag is passed.
+ENV['RAILS_ENV'] ||= 'production'
+
 require 'pathname'
 root = Pathname.new(__FILE__).expand_path.parent.parent
 config = root.join('config')
