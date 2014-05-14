bb6201d66d229e0b964cd264820eb4ebf8288cf8
nstarke <starke.nicholas@gmail.com>
2014-05-14 15:51:42 +0000

    Fixing nil bug and making format constant
    The date format has been moved into a constant variable.
    Certain modules do not have a disclosure_date.  For example,
    ‘checkvm’.  This necessitated checking disclosure_date for nil
    before attempting a format conversion.  Also, there was an additional
    location in core.rb that needed the formatting / nil check added.  Specs
    were also updated appropriately.

--- lib/msf/ui/console/command_dispatcher/core.rb
+++ lib/msf/ui/console/command_dispatcher/core.rb
@@ -97,6 +97,8 @@ class Core
   # mode.
   DefangedProhibitedDataStoreElements = [ "MsfModulePaths" ]
 
+  # Constant for disclosure date formatting in search functions
+  DISCLOSURE_DATE_FORMAT = "%Y-%m-%d"
   # Returns the list of commands supported by this command dispatcher
   def commands
     {
@@ -1488,7 +1490,7 @@ class Core
         next if not o
 
         if not o.search_filter(match)
-          tbl << [ o.fullname, o.disclosure_date.strftime("%Y-%m-%d"), o.rank_to_s, o.name ]
+          tbl << [ o.fullname, o.disclosure_date.nil? ? "" : o.disclosure_date.strftime(DISCLOSURE_DATE_FORMAT), o.rank_to_s, o.name ]
         end
       end
     end
@@ -1503,7 +1505,7 @@ class Core
   def search_modules_sql(search_string)
     tbl = generate_module_table("Matching Modules")
     framework.db.search_modules(search_string).each do |o|
-      tbl << [ o.fullname, o.disclosure_date.strftime("%Y-%m-%d"), RankingName[o.rank].to_s, o.name ]
+      tbl << [ o.fullname, o.disclosure_date.nil? ? "" : o.disclosure_date.strftime(DISCLOSURE_DATE_FORMAT), RankingName[o.rank].to_s, o.name ]
     end
     print_line(tbl.to_s)
   end
@@ -3239,7 +3241,7 @@ class Core
             end
           end
           if (opts == nil or show == true)
-            tbl << [ refname, o.disclosure_date||"", o.rank_to_s, o.name ]
+            tbl << [ refname, o.disclosure_date.nil? ? "" : o.disclosure_date.strftime(DISCLOSURE_DATE_FORMAT), o.rank_to_s, o.name ]
           end
         end
       end
