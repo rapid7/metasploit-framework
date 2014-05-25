33ba1341474fed4cb31bbe0dc348c979dc3784d7
jvazquez-r7 <juan_vazquez@rapid7.com>
2014-05-25 18:52:01 -0500

    Clean msftidy warnings and metadata

--- modules/auxiliary/dos/wireshark/capwap.rb
+++ modules/auxiliary/dos/wireshark/capwap.rb
@@ -12,27 +12,28 @@ class Metasploit3 < Msf::Auxiliary
 
   def initialize(info = {})
     super(update_info(info,
-      'Name'           => 'Wireshark CAPWAP dissector DoS',
+      'Name'           => 'Wireshark CAPWAP Dissector DoS',
       'Description'    => %q{
-          	This module inject malicious packet udp to crash wireshark. The crash is when we send 
-		a incomplete packet and trigger capwap dissector. 
+        This module inject a malicious udp packet to crash Wireshark 1.8.0 to 1.8.7 and 1.6.0
+        to 1.6.15. The vulnerability exists in the capwap dissector which fails to handle an
+        incomplete packet.
       },
       'License'        => MSF_LICENSE,
       'Author'         =>
         [
-          'j0sm1',  # Auxiliary msf module
-          'Laurent Butti' # Discovery vulnerability -> "Reported: 2013-05-28 23:38 UTC by Laurent Butti"
+          'Laurent Butti', # Discovery vulnerability
+          'j0sm1'  # Auxiliary msf module
         ],
       'References'     =>
         [
-          [ 'CVE', '2013-4074'],
+          ['CVE', '2013-4074'],
+          ['OSVDB', '94091'],
+          ['BID', '60500']
         ],
       'DisclosureDate' => 'Apr 28 2014'))
-      
 
     # Protocol capwap needs port 5247 to trigger the dissector in wireshark
     register_options([ Opt::RPORT(5247) ], self.class)
-
   end
 
   def run
@@ -41,7 +42,7 @@ class Metasploit3 < Msf::Auxiliary
 
     # We send a packet incomplete to crash dissector
     print_status("#{rhost}:#{rport} - Trying to crash wireshark capwap dissector ...")
-    # With 0x90 in this location we set to 1 the flags F and M. The others flags are sets to 0, then 
+    # With 0x90 in this location we set to 1 the flags F and M. The others flags are sets to 0, then
     # the dissector crash
     # You can see more information here: https://www.rfc-editor.org/rfc/rfc5415.txt
     # F = 1 ; L = 0 ; W = 0 ; M = 1 ; K = 0 ; Flags = 000
