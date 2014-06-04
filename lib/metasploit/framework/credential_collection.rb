b1ff6b95b5254cf679f13e6329ddba1f4730eaf1
James Lee <egypt@metasploit.com>
2014-06-04 14:44:53 -0500

    Better docs

--- lib/metasploit/framework/credential_collection.rb
+++ lib/metasploit/framework/credential_collection.rb
@@ -2,21 +2,41 @@ require 'metasploit/framework/credential'
 
 class Metasploit::Framework::CredentialCollection
 
+  # @!attribute blank_passwords
+  #   Whether each username should be tried with a blank password
+  #   @return [Boolean]
   attr_accessor :blank_passwords
+  # @!attribute pass_file
+  #   Path to a file containing passwords, one per line
+  #   @return [String]
   attr_accessor :pass_file
+  # @!attribute password
+  #   @return [String]
   attr_accessor :password
+  # @!attribute user_as_pass
+  #   Whether each username should be tried as a password for that user
+  #   @return [Boolean]
   attr_accessor :user_as_pass
+  # @!attribute user_file
+  #   Path to a file containing usernames, one per line
+  #   @return [String]
   attr_accessor :user_file
+  # @!attribute username
+  #   @return [String]
   attr_accessor :username
+  # @!attribute user_file
+  #   Path to a file containing usernames and passwords seperated by a space,
+  #   one pair per line
+  #   @return [String]
   attr_accessor :userpass_file
 
-  # @option opts [Boolean] :blank_passwords Whether each username should be tried with a blank password
-  # @option opts [String] :pass_file Path to a file containing passwords, one per line
-  # @option opts [String] :password
-  # @option opts [Boolean] :user_as_pass Whether each username should be tried as a password for that user
-  # @option opts [String] :user_file Path to a file containing usernames, one per line
-  # @option opts [String] :username
-  # @option opts [String] :userpass_file Path to a file containing usernames and passwords seperated by a space, one pair per line
+  # @option opts [Boolean] :blank_passwords See {#blank_passwords}
+  # @option opts [String] :pass_file See {#pass_file}
+  # @option opts [String] :password See {#password}
+  # @option opts [Boolean] :user_as_pass See {#user_as_pass}
+  # @option opts [String] :user_file See {#user_file}
+  # @option opts [String] :username See {#username}
+  # @option opts [String] :userpass_file See {#userpass_file}
   def initialize(opts = {})
     opts.each do |attribute, value|
       public_send("#{attribute}=", value)
@@ -42,6 +62,7 @@ class Metasploit::Framework::CredentialCollection
       end
       if pass_fd
         pass_fd.each_line do |pass_from_file|
+          pass_from_file.chomp!
           yield Metasploit::Framework::Credential.new(public: username, private: pass_from_file)
         end
         pass_fd.seek(0)
@@ -62,6 +83,7 @@ class Metasploit::Framework::CredentialCollection
           end
           if pass_fd
             pass_fd.each_line do |pass_from_file|
+              pass_from_file.chomp!
               yield Metasploit::Framework::Credential.new(public: user_from_file, private: pass_from_file)
             end
             pass_fd.seek(0)
@@ -69,6 +91,8 @@ class Metasploit::Framework::CredentialCollection
         end
       end
     end
+  ensure
+    pass_fd.close if pass_fd && !pass_fd.closed?
   end
 
 end
