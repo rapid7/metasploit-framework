466576d03f9f14219de267a8357aac402baec58d
David Maloney <DMaloney@rapid7.com>
2014-06-14 16:16:30 -0500

    jtr wordlist validations started
    start adding validations and exceptions for the
    JtR Wordlist class.

--- lib/metasploit/framework/jtr/wordlist.rb
+++ lib/metasploit/framework/jtr/wordlist.rb
@@ -6,15 +6,77 @@ module Metasploit
       class Wordlist
         include ActiveModel::Validations
 
+        # @!attribute appenders
+        #   @return [Array] an array of strings to append to each word
         attr_accessor :appenders
+
+        # @!attribute custom_wordlist
+        #   @return [String] the path to a custom wordlist file to include
         attr_accessor :custom_wordlist
+
+        # @!attribute mutate
+        #   @return [TrueClass] if you want each word mutated as it is added
+        #   @return [FalseClass] if you do not want each word mutated
         attr_accessor :mutate
+
+        # @!attribute prependers
+        #   @return [Array] an array of strings to prepend to each word
         attr_accessor :prependers
+
+        # @!attribute use_common_root
+        #   @return [TrueClass] if you want to use the common root words wordlist
+        #   @return [FalseClass] if you do not want to use the common root words wordlist
         attr_accessor :use_common_root
+
+        # @!attribute use_creds
+        #   @return [TrueClass] if you want to seed the wordlist with existing credential data from the database
+        #   @return [FalseClass] if you do not want to seed the wordlist with existing credential data from the database
         attr_accessor :use_creds
+
+        # @!attribute use_db_info
+        #   @return [TrueClass] if you want to seed the wordlist with looted database names and schemas
+        #   @return [FalseClass] if you do not want to seed the wordlist with looted database names and schemas
         attr_accessor :use_db_info
+
+        # @!attribute use_default_wordlist
+        #   @return [TrueClass] if you want to use the default wordlist
+        #   @return [FalseClass] if you do not want to use the default wordlist
         attr_accessor :use_default_wordlist
+
+        # @!attribute use_hostnames
+        #   @return [TrueClass] if you want to seed the wordlist with existing hostnames from the database
+        #   @return [FalseClass] if you do not want to seed the wordlist with existing hostnames from the database
         attr_accessor :use_hostnames
+
+        validates :mutate,
+                  inclusion: { in: [true, false] }
+
+        validates :use_common_root,
+                  inclusion: { in: [true, false] }
+
+        validates :use_creds,
+                  inclusion: { in: [true, false] }
+
+        validates :use_db_info,
+                  inclusion: { in: [true, false] }
+
+        validates :use_default_wordlist,
+                  inclusion: { in: [true, false] }
+
+        validates :use_hostnames,
+                  inclusion: { in: [true, false] }
+
+
+        # Raise an exception if the attributes are not valid.
+        #
+        # @raise [Invalid] if the attributes are not valid on this scanner
+        # @return [void]
+        def valid!
+          unless valid?
+            raise Metasploit::Framework::JtR::InvalidWordlist.new(self)
+          end
+          nil
+        end
 
       end
 
