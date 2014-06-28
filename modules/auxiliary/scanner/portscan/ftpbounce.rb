6d0d8a911d6bcda67149ba080db0b75df86b0f71
HD Moore <hd_moore@rapid7.com>
2014-06-28 15:22:16 -0500

    Fix incorrect use of sock.get() that could lead to indefinite hang

--- modules/auxiliary/scanner/portscan/ftpbounce.rb
+++ modules/auxiliary/scanner/portscan/ftpbounce.rb
@@ -59,7 +59,7 @@ class Metasploit3 < Msf::Auxiliary
       # on the response codes.  We need to do this between every
       # port scan attempt unfortunately.
       while true
-        r = self.sock.get(0.25)
+        r = sock.get_once(-1, 0.25)
         break if not r or r.empty?
       end
 
