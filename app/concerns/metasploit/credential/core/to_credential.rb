38419dae833a9b66b57e7a6c25835aa88750cbf5
David Maloney <DMaloney@rapid7.com>
2014-07-07 18:05:04 -0500

    fix to_credential on core
    the Metasploit::Credential::Core to_credential
    method now seats private_type and realm_key correctly

--- app/concerns/metasploit/credential/core/to_credential.rb
+++ app/concerns/metasploit/credential/core/to_credential.rb
@@ -1,12 +1,19 @@
 # Adds associations to `Metasploit::Credential::Core` which are inverses of association on models under
 # {BruteForce::Reuse}.
+require 'metasploit/framework/credential'
+
 module Metasploit::Credential::Core::ToCredential
   extend ActiveSupport::Concern
   
   included do
     
     def to_credential
-      Metasploit::Framework::Credential.new(public: public.try(:username), private: private.try(:data), realm: realm.try(:value) )
+      if private.present?
+        private_type = private.type.demodulize.underscore.to_sym
+      else
+        private_type = nil
+      end
+      Metasploit::Framework::Credential.new(public: public.try(:username), private: private.try(:data), private_type: private_type, realm: realm.try(:value), realm_key: realm.try(:key) )
     end
     
   end
