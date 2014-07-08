a3641729781ceaa5598352b277cd94fa0eb17bd3
jvazquez-r7 <juan_vazquez@rapid7.com>
2014-07-08 16:25:50 -0500

    Add meterpreter as session type

--- modules/post/linux/gather/enum_protections.rb
+++ modules/post/linux/gather/enum_protections.rb
@@ -29,7 +29,7 @@ class Metasploit3 < Msf::Post
           'ohdae <bindshell[at]live.com>'
         ],
       'Platform'      => ['linux'],
-      'SessionTypes'  => ['shell']
+      'SessionTypes'  => ['shell', 'meterpreter']
     ))
   end
 
