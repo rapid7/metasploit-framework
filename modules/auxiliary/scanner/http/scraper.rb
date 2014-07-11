8b302cd47290866d8e03a1d9dbf36b4e58676ee7
nodeofgithub <nodeofserver@gmail.com>
2014-07-11 20:31:46 +0200

    Add report_note to scraper.rb
    Just a suggestion. I always personally modify this. I use it to scrape titles often, and i prefer it to be saved in notes rather than wmap results, because i find it easier to search and automatically add results to rhosts.

--- modules/auxiliary/scanner/http/scraper.rb
+++ modules/auxiliary/scanner/http/scraper.rb
@@ -60,13 +60,13 @@ class Metasploit3 < Msf::Auxiliary
       result.each do |u|
         print_status("[#{target_host}] #{tpath} [#{u}]")
         
-	report_note(
-	  :host    => target_host,
-	  :port    => rport,
-	  :proto   => 'tcp',
+        report_note(
+          :host    => target_host,
+          :port    => rport,
+          :proto   => 'tcp',
           :type    => "http.#{rport.to_s}.scraper",
           :data    => u
-      	)
+        )
 
         report_web_vuln(
           :host	=> target_host,
