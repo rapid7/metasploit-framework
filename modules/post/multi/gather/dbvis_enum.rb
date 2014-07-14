0ef0f6aae1000452ca5110f8cab37d84adb7d86a
David Bloom <softage.informatique@gmail.com>
2014-07-14 10:54:43 +0200

    Update dbvis_enum.rb

--- modules/post/multi/gather/dbvis_enum.rb
+++ modules/post/multi/gather/dbvis_enum.rb
@@ -19,7 +19,7 @@ class Metasploit3 < Msf::Post
         'Description'   => %q{
           DbVisualizer stores the user database configuration in dbvis.xml.
 	  This module retrieves the connections settings from this file.
-          | Author: David Bloom |
+ 
         },
         'License'       => MSF_LICENSE,
         'Author'        => [ 'David Bloom <@philophobia78>' ],
