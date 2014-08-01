73ca8c0f6d39f6f9eeb51356161e6aed553a6dd1
jvazquez-r7 <juan_vazquez@rapid7.com>
2014-08-01 14:28:26 -0500

    Work on jboss refactoring

--- lib/msf/http/jboss/base.rb
+++ lib/msf/http/jboss/base.rb
@@ -1,40 +1,27 @@
 # -*- coding: binary -*-
 module Msf::HTTP::JBoss::Base
-  def call_uri_mtimes(uri, num_attempts = 5, verb = nil, data = nil)
-    verb = datastore['VERB'] if verb.nil? 
+  def deploy(opts = {}, num_attempts = 5)
+    uri = opts['uri']
+
+    if uri.blank?
+      return nil
+    end
 
     # JBoss might need some time for the deployment. Try 5 times at most and
     # wait 5 seconds inbetween tries
     num_attempts.times do |attempt|
-
-      if (verb == "POST")
-        res = send_request_cgi(
-          {
-            'uri'    => uri,
-            'method' => verb,
-            'data'   => data
-          }, 5)
-      else
-
-        uri += "?#{data}" unless data.nil?
-        res = send_request_cgi(
-          {
-            'uri'    => uri,
-            'method' => verb
-          }, 30)
-      end
-
+      res = send_request_cgi(opts, 5)
       msg = nil
-      if (!res)
+      if res.nil?
         msg = "Execution failed on #{uri} [No Response]"
-      elsif (res.code < 200 or res.code >= 300)
+      elsif res.code < 200 || res.code >= 300
         msg = "http request failed to #{uri} [#{res.code}]"
-      elsif (res.code == 200)
+      elsif res.code == 200
         vprint_status("Successfully called '#{uri}'")
         return res
       end
 
-      if (attempt < num_attempts - 1)
+      if attempt < num_attempts - 1
         msg << ", retrying in 5 seconds..."
         vprint_status(msg)
         Rex.sleep(5)
@@ -44,4 +31,9 @@ module Msf::HTTP::JBoss::Base
       end
     end
   end
+
+  def http_verb
+    datastore['VERB']
+  end
+
 end
