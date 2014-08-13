0d045d007acb87f7519b6de52d55042b66bbb7a8
darkbushido <lance.sanchez@gmail.com>
2014-08-13 17:37:40 -0500

    moving the module file
    adding some comments on things we should not see when running
    msfconsole

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,6 +1,9 @@
 Before('@msfconsole') do
   step 'I run `msfconsole` interactively'
   step 'I wait for stdout to contain "Free Metasploit Pro trial: http://r-7.co/trymsp"'
+  # we should not see the following
+  # -- --=[ 0 exploits - 0 auxiliary - 0 post               ]
+  # -- --=[ 0 payloads - 0 encoders - 0 nops                ]
 end
 
 Before('@target') do
