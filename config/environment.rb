92fc67cabaeae70f0b1bb2d1f79ef3a0117da8b9
Tod Beardsley <tod_beardsley@rapid7.com>
2014-08-18 12:02:39 -0500

    Dumb hack to fix the no database error

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,10 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-Metasploit::Framework::Application.initialize!
+begin
+  Metasploit::Framework::Application.initialize!
+rescue Exception, Errno::ENOENT => e
+  $stderr.puts "[!] WARNING, the Metasploit Framework Application threw an exception:"
+  $stderr.puts "[!] #{e.inspect}"
+  $stderr.puts "[!] Try running with '-n' (no database)"
+end
