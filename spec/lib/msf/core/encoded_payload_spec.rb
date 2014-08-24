6313b29b7a0ab9a40c6a33c82a240d29b3767079
Joe Vennix <joe_vennix@rapid7.com>
2014-08-24 02:22:15 -0500

    Add #arch method to Msf::EncodedPayload.
    This allows exploits with few one automatic target to support many
    different architectures.

---
+++ spec/lib/msf/core/encoded_payload_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'msf/core/encoded_payload'
+
+describe Msf::EncodedPayload do
+  PAYLOAD_FRAMEWORK = Msf::Simple::Framework.create(
+    :module_types => [::Msf::MODULE_PAYLOAD, ::Msf::MODULE_ENCODER, ::Msf::MODULE_NOP],
+    'DisableDatabase' => true,
+    'DisableLogging' => true
+  )
+
+  let(:framework) { PAYLOAD_FRAMEWORK }
+  let(:payload) { 'linux/x86/shell_reverse_tcp' }
+  let(:pinst) { framework.payloads.create(payload) }
+
+  subject(:encoded_payload) do
+    described_class.new(framework, pinst, {})
+  end
+
+  describe '#arch' do
+    context 'when payload is linux/x86 reverse tcp' do
+      let(:payload) { 'linux/x86/shell_reverse_tcp' }
+
+      it 'returns ["X86"]' do
+        expect(encoded_payload.arch).to eq [ARCH_X86]
+      end
+    end
+
+    context 'when payload is linux/x64 reverse tcp' do
+      let(:payload) { 'linux/x64/shell_reverse_tcp' }
+
+      it 'returns ["X86_64"]' do
+        expect(encoded_payload.arch).to eq [ARCH_X86_64]
+      end
+    end
+  end
+end
