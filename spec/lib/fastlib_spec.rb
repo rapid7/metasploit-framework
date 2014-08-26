042b8a3672b2867188c01ee60ec4307c09a56209
jvazquez-r7 <juan_vazquez@rapid7.com>
2014-08-26 15:17:00 -0500

    Switch from pending to skip in specs

--- spec/lib/fastlib_spec.rb
+++ spec/lib/fastlib_spec.rb
@@ -91,7 +91,7 @@ describe FastLib do
             FastLib.cache[@destination_path][:fastlib_flags].should == flags
           end
 
-          pending "Fix https://www.pivotaltracker.com/story/show/38730815" do
+          skip "Fix https://www.pivotaltracker.com/story/show/38730815" do
             it 'should include header' do
               described_class.dump(@destination_path, flag_string, base_path, *unarchived_paths)
               header = FastLib.cache[@destination_path][:fastlib_header]
@@ -198,7 +198,7 @@ describe FastLib do
         end
       end
 
-      context 'with cached dump', :pending => "Fix https://www.pivotaltracker.com/story/show/38730815" do
+      context 'with cached dump', :skip => "Fix https://www.pivotaltracker.com/story/show/38730815" do
         it 'should have dump cached' do
           FastLib.cache[@destination_path].should_not be_nil
         end
