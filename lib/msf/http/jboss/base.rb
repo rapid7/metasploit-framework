af9f3b83a7ba9c77c53dfc87e71ccc04222e90f7
us3r777 <us3r777@n0b0.so>
2014-08-27 22:54:40 +0200

    Refactoring jboss module to work with the Mixin
    Removed datastore USERNAME and PASSWORD which are provided by
    Msf::Exploit::Remote::HttpClient. Removed datastore PATH and VERB which
    are provided by the mixin (lib/msf/http/jboss). Moved target detection
    to the mixin.

--- lib/msf/http/jboss/base.rb
+++ lib/msf/http/jboss/base.rb
@@ -46,4 +46,73 @@ module Msf::HTTP::JBoss::Base
     datastore['VERB']
   end
 
+
+  def auto_target
+    if http_verb == 'HEAD' then
+      print_status("Sorry, automatic target detection doesn't work with HEAD requests")
+    else
+      print_status("Attempting to automatically select a target...")
+      res = query_serverinfo
+      if not (plat = detect_platform(res))
+        fail_with(Failure::NoTarget, 'Unable to detect platform!')
+      end
+
+      if not (arch = detect_architecture(res))
+        fail_with(Failure::NoTarget, 'Unable to detect architecture!')
+      end
+
+      # see if we have a match
+      targets.each { |t| return t if (t['Platform'] == plat) and (t['Arch'] == arch) }
+    end
+
+    # no matching target found, use Java as fallback
+    java_targets = targets.select {|t| t.name =~ /^Java/ }
+    return java_targets[0]
+  end
+
+  def query_serverinfo
+    path = normalize_uri(target_uri.path.to_s, '/HtmlAdaptor?action=inspectMBean&name=jboss.system:type=ServerInfo')
+    res = send_request_raw(
+      {
+        'uri'    => path,
+        'method' => http_verb
+      })
+
+    unless res && res.code == 200
+      print_error("Failed: Error requesting #{path}")
+      return nil
+    end
+
+    res
+  end
+
+  # Try to autodetect the target platform
+  def detect_platform(res)
+    if res && res.body =~ /<td.*?OSName.*?(Linux|FreeBSD|Windows).*?<\/td>/m
+      os = $1
+      if (os =~ /Linux/i)
+        return 'linux'
+      elsif (os =~ /FreeBSD/i)
+        return 'linux'
+      elsif (os =~ /Windows/i)
+        return 'win'
+      end
+    end
+
+    nil
+  end
+
+  # Try to autodetect the target architecture
+  def detect_architecture(res)
+    if res && res.body =~ /<td.*?OSArch.*?(x86|i386|i686|x86_64|amd64).*?<\/td>/m
+      arch = $1
+      if (arch =~ /(x86|i386|i686)/i)
+        return ARCH_X86
+      elsif (arch =~ /(x86_64|amd64)/i)
+        return ARCH_X86
+      end
+    end
+
+    nil
+  end
 end
