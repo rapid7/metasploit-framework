a1823b6c1e41c4d4db386d64fbd9932cf3f6204e
jvazquez-r7 <juan_vazquez@rapid7.com>
2014-09-02 18:17:14 -0500

    Add more specs for Rex::Arch::X86

--- spec/lib/rex/arch/x86_spec.rb
+++ spec/lib/rex/arch/x86_spec.rb
@@ -1,7 +1,7 @@
 # -*- coding:binary -*-
 require 'spec_helper'
 
-require 'rex/exploitation/encryptjs'
+require 'rex/arch/x86'
 
 describe Rex::Arch::X86 do
 
@@ -24,7 +24,7 @@ describe Rex::Arch::X86 do
     context "when invalid argument" do
       let(:register) { "non_existent" }
       it "raises an error" do
-        expect { subject.to raise_error(NameError) }
+        expect { subject }.to raise_error(NameError)
       end
     end
   end
@@ -98,7 +98,7 @@ describe Rex::Arch::X86 do
       let(:badchars) { "sac" }
 
       it "raises an error" do
-        expect { subject.to raise_error(Rex::RuntimeError) }
+        expect { subject }.to raise_error(Rex::RuntimeError)
       end
     end
 
@@ -132,7 +132,7 @@ describe Rex::Arch::X86 do
     context "when invalid register" do
       let(:reg) { "non_existent"}
       it "raises an error" do
-        expect{ subject.to raise_error(NameError) }
+        expect { subject }.to raise_error(NameError)
       end
     end
   end
@@ -175,7 +175,7 @@ describe Rex::Arch::X86 do
       context "num argument is other string" do
         let(:num) { "20" }
         it "raises error" do
-          expect { subject.to raise_error(TypeError) }
+          expect { subject }.to raise_error(TypeError)
         end
       end
 
@@ -222,7 +222,7 @@ describe Rex::Arch::X86 do
       context "num argument is other string" do
         let(:num) { "20" }
         it "raises error" do
-          expect { subject.to raise_error(TypeError) }
+          expect { subject }.to raise_error(TypeError)
         end
       end
 
@@ -296,7 +296,7 @@ describe Rex::Arch::X86 do
       context "starting in another way" do
         let(:offset) { "20" }
         it "raises error" do
-          expect { subject.to raise_error(TypeError) }
+          expect { subject }.to raise_error(TypeError)
         end
       end
     end
@@ -344,7 +344,7 @@ describe Rex::Arch::X86 do
       context "starting in another way" do
         let(:addr) { "20" }
         it "raises error" do
-          expect { subject.to raise_error(TypeError) }
+          expect { subject }.to raise_error(TypeError)
         end
       end
     end
@@ -357,7 +357,7 @@ describe Rex::Arch::X86 do
       let(:dword) { "\xff"}
 
       it "raises error" do
-        expect { subject.to raise_error(NoMethodError) }
+        expect { subject }.to raise_error(NoMethodError)
       end
     end
 
@@ -386,8 +386,8 @@ describe Rex::Arch::X86 do
         let(:dword) { "\xff\xff\xff\xff" }
         let(:amount) { 1 }
 
-        it "raises error" do
-          expect { subject.to raise_error(NoMethodError) }
+        it "truncates" do
+          is_expected.to eq("\x00\x00\x00\x00")
         end
       end
     end
@@ -515,7 +515,7 @@ describe Rex::Arch::X86 do
       context "starting in another way" do
         let(:addr) { "20" }
         it "raises error" do
-          expect { subject.to raise_error(TypeError) }
+          expect { subject }.to raise_error(TypeError)
         end
       end
     end
@@ -563,7 +563,7 @@ describe Rex::Arch::X86 do
       context "starting in another way" do
         let(:addr) { "20" }
         it "raises error" do
-          expect { subject.to raise_error(TypeError) }
+          expect { subject }.to raise_error(TypeError)
         end
       end
     end
@@ -580,9 +580,432 @@ describe Rex::Arch::X86 do
     context "when reg id isn't valid" do
       let(:num) { 29 }
       it "raises an error" do
-        expect { subject.to raise_error(ArgumentError) }
+        expect { subject }.to raise_error(ArgumentError)
       end
     end
   end
+
+  describe ".encode_effective" do
+    subject { described_class.encode_effective(shift, reg) }
+
+    let(:shift) { 0 }
+
+    let(:reg) { Rex::Arch::X86::ECX }
+
+    it "encodes the effective value for a register" do
+      is_expected.to eq(0xc0 | (shift << 3) | reg)
+    end
+  end
+
+  describe ".encode_modrm" do
+    subject { described_class.encode_modrm(dst, src) }
+
+    context "when dst is an invalid register" do
+      let(:dst) { 31337 }
+      let(:src) { Rex::Arch::X86::ECX }
+
+      it { expect { subject }.to raise_error(ArgumentError) }
+    end
+
+    context "when src is an invalid register" do
+      let(:dst) { Rex::Arch::X86::ECX }
+      let(:src) { 31337 }
+
+      it { expect { subject }.to raise_error(ArgumentError) }
+    end
+
+    context "when dst and src are valid registers" do
+      let(:dst) { Rex::Arch::X86::ECX }
+      let(:src) { Rex::Arch::X86::EAX }
+
+      it "generates the mod r/m character" do
+        is_expected.to eq((0xc8).chr)
+      end
+    end
+  end
+
+  describe ".push_byte" do
+    subject { described_class.push_byte(byte) }
+
+    context "when byte is out of range" do
+      let(:byte) { 0x100 }
+      it { expect { subject }.to raise_error(::ArgumentError) }
+    end
+
+    context "when byte is in range" do
+      let(:byte) { 127 }
+      it "generates correct instruction" do
+        is_expected.to eq("\x6a\x7f")
+      end
+    end
+  end
+
+  describe ".push_word" do
+    subject { described_class.push_word(val) }
+
+    context "when val is a word" do
+      let(:val) { 0x4142 }
+      it "generates push instruction" do
+        is_expected.to eq("\x66\x68\x42\x41")
+      end
+    end
+
+    context "when val is bigger than word" do
+      let(:val) { 0x41424344 }
+      it "generates push instruction with val truncated" do
+        is_expected.to eq("\x66\x68\x44\x43")
+      end
+    end
+  end
+
+  describe ".push_dword" do
+    subject { described_class.push_dword(val) }
+
+    context "when val is a dword" do
+      let(:val) { 0x41424344 }
+      it "generates push instruction" do
+        is_expected.to eq("\x68\x44\x43\x42\x41")
+      end
+    end
+
+    context "when val is bigger than dword" do
+      let(:val) { 0x100000000 }
+      it "generates push instruction with val truncated" do
+        is_expected.to eq("\x68\x00\x00\x00\x00")
+      end
+    end
+  end
+
+  describe ".pop_dword" do
+    subject { described_class.pop_dword(reg) }
+
+    context "when reg is invalid" do
+      let(:reg) { 31337 }
+      it "raises an error" do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "reg is valid" do
+      let(:reg) { Rex::Arch::X86::ECX }
+      it "generates pop instruction" do
+        is_expected.to eq("\x59")
+      end
+    end
+  end
+
+  describe ".mov_byte" do
+    subject { described_class.mov_byte(reg, val) }
+
+    context "when reg is invalid" do
+      let(:reg) { 31337 }
+      let(:val) { 3 }
+      it "raises an error" do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "when val is out of range" do
+      let(:reg) { Rex::Arch::X86::ECX }
+      let(:val) { 31337 }
+      it "raises an error" do
+        expect { subject }.to raise_error(RangeError)
+      end
+    end
+
+    context "when reg and val are valid" do
+      let(:reg) { Rex::Arch::X86::ECX }
+      let(:val) { 3 }
+      it "generates a mov instruction" do
+        is_expected.to eq("\xb1\x03")
+      end
+    end
+  end
+
+  describe ".mov_word" do
+    subject { described_class.mov_word(reg, val) }
+
+    context "when reg is invalid" do
+      let(:reg) { 31337 }
+      let(:val) { 3 }
+      it "raises an error" do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "when val is out of range" do
+      let(:reg) { Rex::Arch::X86::ECX }
+      let(:val) { 0x41424344 }
+      it "raises an error" do
+        expect { subject }.to raise_error(RangeError)
+      end
+    end
+
+    context "when reg and val are valid" do
+      let(:reg) { Rex::Arch::X86::ECX }
+      let(:val) { 0x4142 }
+      it "generates a mov instruction" do
+        is_expected.to eq("\x66\xb9\x42\x41")
+      end
+    end
+  end
+
+
+  describe ".mov_dword" do
+    subject { described_class.mov_dword(reg, val) }
+
+    context "when reg is invalid" do
+      let(:reg) { 31337 }
+      let(:val) { 3 }
+      it "raises an error" do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "when reg and val are valid" do
+      let(:reg) { Rex::Arch::X86::ECX }
+      let(:val) { 0x41424344 }
+      it "generates a mov instruction" do
+        is_expected.to eq("\xb9\x44\x43\x42\x41")
+      end
+    end
+
+    context "when val is out of range" do
+      let(:reg) { Rex::Arch::X86::ECX }
+      let(:val) { 0x100000000 }
+      it "truncates value" do
+        is_expected.to eq("\xb9\x00\x00\x00\x00")
+      end
+    end
+  end
+
+  describe ".set" do
+    subject { described_class.set(reg, val, badchars) }
+
+    context "when reg is invalid" do
+      let(:reg) { 31337 }
+      let(:val) { 100 }
+      let(:badchars) { '' }
+      it "raises an error" do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "when val is 0" do
+      let(:reg) { Rex::Arch::X86::ECX }
+      let(:val) { 0 }
+
+      context "when no badchars" do
+        let(:badchars) { '' }
+        it "uses xor/sub instructions" do
+          expect(subject.length).to eq(2)
+        end
+      end
+
+      context "when xor/sub opcodes are badchars" do
+        let(:badchars) { "\x29\x2b\x31\x33" }
+
+        it "uses push byte/pop instructions" do
+          expect(subject.length).to eq(3)
+        end
+      end
+
+      context "when xor/sub/push byte opcodes are badchars" do
+        let(:badchars) { "\x29\x2b\x31\x33\x6a" }
+
+        it "uses mov dword instruction" do
+          expect(subject.length).to eq(5)
+        end
+      end
+
+      context "when xor/sub/push byte/mov dword opcodes are badchars" do
+        let(:badchars) { "\x29\x2b\x31\x33\x6a\xb9" }
+
+        it "uses push dword / pop instructions" do
+          expect(subject.length).to eq(6)
+        end
+      end
+
+      context "when xor/sub/push byte/mov dword opcodes/push dword are badchars" do
+        let(:badchars) { "\x29\x2b\x31\x33\x6a\xb9\x68" }
+
+        it "uses clear / mov word instructions" do
+          expect { subject.length }.to raise_error(RuntimeError)
+        end
+      end
+    end
+
+    context "when val isn't 0" do
+      let(:reg) { Rex::Arch::X86::ECX }
+      let(:val) { 75 }
+
+      context "when no badchars" do
+        let(:badchars) { '' }
+        it "uses push byte/pop instructions" do
+          expect(subject.length).to eq(3)
+        end
+      end
+
+      context "when push byte opcodes are badchars" do
+        let(:badchars) { "\x6a" }
+
+        it "uses clear/mov byte instruction" do
+          expect(subject.length).to eq(4)
+        end
+      end
+
+      context "when push byte/mov byte opcodes are badchars" do
+        let(:badchars) { "\x6a\xb1" }
+
+        it "uses mov dword instruction" do
+          expect(subject.length).to eq(5)
+        end
+      end
+
+      context "when push byte/mov byte/mov dword opcodes are badchars" do
+        let(:badchars) { "\x6a\xb1\xb9" }
+
+        it "it uses push dword/pop dst instructions" do
+          expect(subject.length).to eq(6)
+        end
+      end
+
+      context "when push byte/mov byte/mov dword/push dword opcodes are badchars" do
+        let(:badchars) { "\x6a\xb1\xb9\x68" }
+
+        it "raises an error" do
+          expect { subject.length }.to raise_error(RuntimeError)
+        end
+      end
+    end
+  end
+
+  describe ".sub" do
+    subject { described_class.sub(val, reg) }
+
+    context "when reg is valid" do
+      let(:reg) { Rex::Arch::X86::ECX }
+
+      context "when val is one byte" do
+        let(:val) { 0x08 }
+        it { is_expected.to include("\x83") }
+      end
+
+      context "when val is bigger than one byte" do
+        let(:val) { 0x4142 }
+        it { is_expected.to include("\x81") }
+      end
+
+      context "when there are too many badchars" do
+        subject(:with_badchars) { described_class.sub(val, reg, badchars) }
+        let(:val) { 0x08 }
+        let(:reg) { Rex::Arch::X86::ECX }
+        let(:badchars) { "\x81\x83" }
+        it { expect(with_badchars).to be_nil }
+      end
+    end
+
+    context "when reg is invalid" do
+      let(:reg) { 31337 }
+      let(:val) { 0x7 }
+      it { expect {subject}.to raise_error }
+    end
+
+  end
+
+  describe ".add" do
+    subject { described_class.add(val, reg) }
+
+    context "when reg is valid" do
+      let(:reg) { Rex::Arch::X86::ECX }
+
+      context "when val is one byte" do
+        let(:val) { 0x08 }
+        it { is_expected.to include("\x83") }
+      end
+
+      context "when val is bigger than one byte" do
+        let(:val) { 0x4142 }
+        it { is_expected.to include("\x81") }
+      end
+
+      context "when there are too many badchars" do
+        subject(:with_badchars) { described_class.add(val, reg, badchars) }
+        let(:val) { 0x08 }
+        let(:reg) { Rex::Arch::X86::ECX }
+        let(:badchars) { "\x81\x83" }
+        it { expect(with_badchars).to be_nil }
+      end
+    end
+
+    context "when reg is invalid" do
+      let(:reg) { 31337 }
+      let(:val) { 0x7 }
+      it "raises an error" do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe ".adjust_reg" do
+    subject { described_class.adjust_reg(reg, adjustment) }
+
+    context "when reg is invalid" do
+      let(:reg) { 31337 }
+      let(:adjustment) { 0x8 }
+
+      it "raises an error" do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+
+    context  "when adjustment is > 0" do
+      let(:reg) { Rex::Arch::X86::ECX }
+      let(:adjustment) { 0x8 }
+
+      it { is_expected.to include("\x81") }
+      it { expect(subject.length).to eq(8) }
+    end
+
+    context "when adjusmtent is <= 0" do
+      let(:reg) { Rex::Arch::X86::ECX }
+      let(:adjustment) { 0 }
+
+      it { is_expected.to include("\x81") }
+      it { expect(subject.length).to eq(6) }
+    end
+  end
+
+  describe ".geteip_fpu" do
+    subject { described_class.geteip_fpu(badchars) }
+
+    context "when no badchars" do
+      let(:badchars) { '' }
+
+      it "returns an Array" do
+        is_expected.to be_an Array
+      end
+
+      it "returns the stub as first element" do
+        expect(subject[0]).to be_an String
+      end
+
+      it "returns a register as second element" do
+        expect(subject[1]).to be_an String
+      end
+
+      it "returns a register as third element" do
+        expect(subject[2]).to be_an Fixnum
+      end
+    end
+
+    context "when too many badchars" do
+      let(:badchars) { (0x00..0xff).to_a.pack("C*") }
+
+      it { is_expected.to be_nil }
+    end
+  end
+
+
 
 end
