84e4db90354a79f3bee0a11690e66746a9db3a8e
James Lee <egypt@metasploit.com>
2014-09-10 19:47:24 -0500

    Don't raise in the middle
    MSP-11343
    
    This means we don't bomb out with an unhandled exception, instead
    continuing attempting logins against the host even though it will never
    succeed. Next up: verify state before running scan!()

--- lib/metasploit/framework/login_scanner/glassfish.rb
+++ lib/metasploit/framework/login_scanner/glassfish.rb
@@ -5,10 +5,6 @@ module Metasploit
   module Framework
     module LoginScanner
 
-      # I don't want to raise RuntimeError to be able to abort login
-      class GlassfishError < StandardError
-      end
-
       # The Glassfish HTTP LoginScanner class provides methods to do login routines
       # for Glassfish 2, 3 and 4.
       class Glassfish < HTTP
@@ -23,7 +19,6 @@ module Metasploit
         # @!attribute jsession
         #   @return [String] Cookie session
         attr_accessor :jsession
-
 
         # Sends a HTTP request with Rex
         #
@@ -126,9 +121,9 @@ module Metasploit
               return {:status => Metasploit::Model::Login::Status::SUCCESSFUL, :proof => res.body}
             end
           elsif res && is_secure_admin_disabled?(res)
-            return {:status => Metasploit::Model::Login::Status::SUCCESSFUL, :proof => res.body}
+            return {:status => Metasploit::Model::Login::Status::DENIED_ACCESS, :proof => res.body}
           elsif res && res.code == 400
-            raise GlassfishError, "400: Bad HTTP request from try_login"
+            return {:status => Metasploit::Model::Login::Status::UNABLE_TO_CONNECT, :proof => res.body}
           end
 
           {:status => Metasploit::Model::Login::Status::INCORRECT, :proof => res.body}
@@ -146,12 +141,10 @@ module Metasploit
             case self.version
             when /^[29]\.x$/
               status = try_glassfish_2(credential)
-              result_opts.merge!(status: status[:status], proof:status[:proof])
+              result_opts.merge!(status)
             when /^[34]\./
               status = try_glassfish_3(credential)
-              result_opts.merge!(status: status[:status], proof:status[:proof])
-           else
-              raise GlassfishError, "Glassfish version '#{self.version}' not supported"
+              result_opts.merge!(status)
             end
           rescue ::EOFError, Rex::ConnectionError, ::Timeout::Error
             result_opts.merge!(status: Metasploit::Model::Login::Status::UNABLE_TO_CONNECT)
@@ -159,6 +152,29 @@ module Metasploit
 
           Result.new(result_opts)
         end
+
+        #
+        # Extract the target's glassfish version from the HTTP Server header
+        # (ex: Sun Java System Application Server 9.x)
+        #
+        # @param banner [String] `Server` header from a Glassfish service response
+        # @return [String] version string, e.g. '2.x'
+        # @return [nil] If the banner did not match any of the expected values
+        def extract_version(banner)
+          # Set version.  Some GlassFish servers return banner "GlassFish v3".
+          if banner =~ /(GlassFish Server|Open Source Edition)[[:blank:]]*(\d\.\d)/
+            @version = $2
+          elsif banner =~ /GlassFish v(\d)/
+            @version = $1
+          elsif banner =~ /Sun GlassFish Enterprise Server v2/
+            @version = '2.x'
+          elsif banner =~ /Sun Java System Application Server 9/
+            @version = '9.x'
+          end
+
+          return @version
+        end
+
 
       end
     end
