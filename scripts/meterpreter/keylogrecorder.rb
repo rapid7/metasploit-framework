5e076e0375be5ca3d73b30039e26321c4fead489
sinn3r <wei_chen@rapid7.com>
2014-09-16 16:41:14 -0500

    Text about not accepting changes to meterpreter scripts

--- scripts/meterpreter/keylogrecorder.rb
+++ scripts/meterpreter/keylogrecorder.rb
@@ -1,3 +1,11 @@
+##
+# WARNING: Metasploit no longer maintains or accepts meterpreter scripts.
+# If you'd like to imporve this script, please try to port it as a post
+# module instead. Thank you.
+##
+
+
+
 # Author: Carlos Perez at carlos_perez[at]darkoperator.com
 # Updates by Shellster
 #-------------------------------------------------------------------------------
