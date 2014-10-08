2206a863879925f2b35150d7747895a65c9ad613
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-08 14:01:58 -0500

    Extract Msf::DatabaseEvent
    MSP-11124
    
    Extract `Msf::DatabaseEvent` from `lib/msf/core/db.rb` into a more
    conventional `lib/msf/core/database_event.rb`.

--- lib/msf/core/db.rb
+++ lib/msf/core/db.rb
@@ -56,66 +56,12 @@ require 'rex/parser/retina_xml'
 #
 
 require 'metasploit/framework/require'
+require 'msf/core/database_event'
 require 'msf/core/db_manager/import_msf_xml'
 require 'msf/core/host_state'
 require 'msf/core/service_state'
 
 module Msf
-
-###
-#
-# Events that can occur in the host/service database.
-#
-###
-module DatabaseEvent
-
-  #
-  # Called when an existing host's state changes
-  #
-  def on_db_host_state(host, ostate)
-  end
-
-  #
-  # Called when an existing service's state changes
-  #
-  def on_db_service_state(host, port, ostate)
-  end
-
-  #
-  # Called when a new host is added to the database.  The host parameter is
-  # of type Host.
-  #
-  def on_db_host(host)
-  end
-
-  #
-  # Called when a new client is added to the database.  The client
-  # parameter is of type Client.
-  #
-  def on_db_client(client)
-  end
-
-  #
-  # Called when a new service is added to the database.  The service
-  # parameter is of type Service.
-  #
-  def on_db_service(service)
-  end
-
-  #
-  # Called when an applicable vulnerability is found for a service.  The vuln
-  # parameter is of type Vuln.
-  #
-  def on_db_vuln(vuln)
-  end
-
-  #
-  # Called when a new reference is created.
-  #
-  def on_db_ref(ref)
-  end
-
-end
 
 class DBImportError < RuntimeError
 end
