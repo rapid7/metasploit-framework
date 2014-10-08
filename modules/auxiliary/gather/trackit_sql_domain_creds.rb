9cb0ad1ac22f68706f439b34ebdb20535518fd5b
Pedro Ribeiro <pedrib@gmail.com>
2014-10-08 01:18:17 +0100

    Change the reporting address to the real value

--- modules/auxiliary/gather/trackit_sql_domain_creds.rb
+++ modules/auxiliary/gather/trackit_sql_domain_creds.rb
@@ -288,14 +288,8 @@ class Metasploit3 < Msf::Auxiliary
 
     if loot[schema_owner] and loot[database_pw] and loot[database_type] and loot[database_server_name]
       service_data = {
-        # database_server_name is the name of the host that is running the database.
-        # Due to limitations of the Credential API we cannot report a hostname but have to report a numeric IP.
-        # What this means in practice is that we might be reporting an incorrect address, as the database might
-        # be running in a different host than the one running the Track-It! application. However in most cases
-        # rhost == database_server_name as Track-It! is quite a lightweight app.
-        #
-        # Also we can't get the database port number from packet_reply, so set it to 0.
-        address: rhost,
+        # We can't get the database port number from packet_reply, so set it to 0.
+        address: loot[database_server_name].split('\\')[0],
         port: 0,
         service_name: loot[database_type],
         protocol: 'tcp',
