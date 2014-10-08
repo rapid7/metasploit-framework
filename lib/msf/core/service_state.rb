6b3d70ce00edb747093bdf739240ab4690da04e8
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-08 13:52:42 -0500

    Fix code style in Msf::ServiceState
    MSP-11124

--- lib/msf/core/service_state.rb
+++ lib/msf/core/service_state.rb
@@ -1,11 +1,7 @@
-###
-#
 # The states that a service can be in.
-#
-###
 module Msf::ServiceState
-  Open      = "open"
   Closed    = "closed"
   Filtered  = "filtered"
+  Open      = "open"
   Unknown   = "unknown"
 end
