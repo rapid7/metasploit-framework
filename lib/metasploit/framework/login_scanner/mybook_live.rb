70b13819d96b32a5c07fdb1f3913e5ddefae84a4
nstarke <starke.nicholas@gmail.com>
2014-10-12 23:13:49 +0000

    Adding Login Scanner for MyBook Live
    This is a LoginScanner auxiliary module for Western
    Digital MyBook Live NAS devices as well as the spec
    for testing.

---
+++ lib/metasploit/framework/login_scanner/mybook_live.rb
@@ -0,0 +1,59 @@
+require 'metasploit/framework/login_scanner/http'
+
+module Metasploit
+  module Framework
+    module LoginScanner
+
+      # Western Digital MyBook Live login scanner
+      class MyBookLive < HTTP
+
+        # Inherit LIKELY_PORTS,LIKELY_SERVICE_NAMES, and REALM_KEY from HTTP
+        CAN_GET_SESSION = true
+        DEFAULT_PORT    = 80
+        PRIVATE_TYPES   = [ :password ]
+
+        # (see Base#set_sane_defaults)
+        def set_sane_defaults
+          self.uri = "/UI/login" if self.uri.nil?
+          self.method = "POST" if self.method.nil?
+
+          super
+        end
+
+        def attempt_login(credential)
+          result_opts = {
+              credential: credential,
+              host: host,
+              port: port,
+              protocol: 'tcp'
+          }
+          if ssl
+            result_opts[:service_name] = 'https'
+          else
+            result_opts[:service_name] = 'http'
+          end
+          begin
+            body = "data%5BLogin%5D%5Bowner_name%5D=admin&data%5BLogin%5D%5Bowner_passwd%5D=#{Rex::Text.uri_encode(credential.private)}"
+            cli = Rex::Proto::Http::Client.new(host, port, {}, ssl, ssl_version)
+            cli.connect
+            req = cli.request_cgi({
+              'method' => 'POST',
+              'uri' => '/UI/login',
+              'data' => body
+            })
+            res = cli.send_recv(req)
+            print res
+            if res && res.code == 302 && res.headers['location'] && res.headers['location'].include?('UI')
+              result_opts.merge!(status: Metasploit::Model::Login::Status::SUCCESSFUL, proof: res.headers)
+            else
+              result_opts.merge!(status: Metasploit::Model::Login::Status::INCORRECT, proof: res)
+            end
+          rescue ::EOFError, Errno::ETIMEDOUT, Rex::ConnectionError, ::Timeout::Error
+            result_opts.merge!(status: Metasploit::Model::Login::Status::UNABLE_TO_CONNECT)
+          end
+          Result.new(result_opts)
+        end
+      end
+    end
+  end
+end
