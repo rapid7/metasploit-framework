e0f76a7517529a16ff36b5574e6dcc74ba18a1b2
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-13 09:28:48 -0500

    Extract Msf::DBManager::Task
    MSP-11124
    
    Extract methods related to `Mdm::Task`s.

--- lib/msf/core/db_manager.rb
+++ lib/msf/core/db_manager.rb
@@ -88,6 +88,7 @@ class DBManager
   autoload :Report, 'msf/core/db_manager/report'
   autoload :Service, 'msf/core/db_manager/service'
   autoload :Sink, 'msf/core/db_manager/sink'
+  autoload :Task, 'msf/core/db_manager/task'
   autoload :Vuln, 'msf/core/db_manager/vuln'
   autoload :WMAP, 'msf/core/db_manager/wmap'
   autoload :Workspace, 'msf/core/db_manager/workspace'
@@ -109,6 +110,7 @@ class DBManager
   include Msf::DBManager::Report
   include Msf::DBManager::Service
   include Msf::DBManager::Sink
+  include Msf::DBManager::Task
   include Msf::DBManager::Vuln
   include Msf::DBManager::WMAP
   include Msf::DBManager::Workspace
@@ -1038,63 +1040,6 @@ class DBManager
     Mdm::Ref.find_by_name(name)
   }
   end
-
-  #
-  # Find or create a task matching this type/data
-  #
-  def find_or_create_task(opts)
-    report_task(opts)
-  end
-
-  def report_task(opts)
-    return if not active
-  ::ActiveRecord::Base.connection_pool.with_connection {
-    wspace = opts.delete(:workspace) || workspace
-    path = opts.delete(:path) || (raise RuntimeError, "A task :path is required")
-
-    ret = {}
-
-    user      = opts.delete(:user)
-    desc      = opts.delete(:desc)
-    error     = opts.delete(:error)
-    info      = opts.delete(:info)
-    mod       = opts.delete(:mod)
-    options   = opts.delete(:options)
-    prog      = opts.delete(:prog)
-    result    = opts.delete(:result)
-    completed_at = opts.delete(:completed_at)
-    task      = wspace.tasks.new
-
-    task.created_by = user
-    task.description = desc
-    task.error = error if error
-    task.info = info
-    task.module = mod
-    task.options = options
-    task.path = path
-    task.progress = prog
-    task.result = result if result
-    msf_import_timestamps(opts,task)
-    # Having blank completed_ats, while accurate, will cause unstoppable tasks.
-    if completed_at.nil? || completed_at.empty?
-      task.completed_at = opts[:updated_at]
-    else
-      task.completed_at = completed_at
-    end
-    task.save!
-    ret[:task] = task
-  }
-  end
-
-  #
-  # This methods returns a list of all tasks in the database
-  #
-  def tasks(wspace=workspace)
-  ::ActiveRecord::Base.connection_pool.with_connection {
-    wspace.tasks
-  }
-  end
-
 
   # Creates a ReportArtifact based on passed parameters.
   # @param opts [Hash] of ReportArtifact attributes
