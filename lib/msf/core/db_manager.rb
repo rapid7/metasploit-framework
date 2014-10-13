43c99096362058cf9bbfd2cdd1a3eead618b48f1
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-13 09:54:38 -0500

    Extract Msf::DBManager::VulnDetail
    MSP-11124
    
    Extract methods related to `Mdm::VulnDetail`s.

--- lib/msf/core/db_manager.rb
+++ lib/msf/core/db_manager.rb
@@ -91,6 +91,7 @@ class DBManager
   autoload :Sink, 'msf/core/db_manager/sink'
   autoload :Task, 'msf/core/db_manager/task'
   autoload :Vuln, 'msf/core/db_manager/vuln'
+  autoload :VulnDetail, 'msf/core/db_manager/vuln_detail'
   autoload :WMAP, 'msf/core/db_manager/wmap'
   autoload :Workspace, 'msf/core/db_manager/workspace'
 
@@ -114,6 +115,7 @@ class DBManager
   include Msf::DBManager::Sink
   include Msf::DBManager::Task
   include Msf::DBManager::Vuln
+  include Msf::DBManager::VulnDetail
   include Msf::DBManager::WMAP
   include Msf::DBManager::Workspace
 
@@ -769,36 +771,6 @@ class DBManager
   def get_ref(name)
   ::ActiveRecord::Base.connection_pool.with_connection {
     ::Mdm::Ref.find_by_name(name)
-  }
-  end
-
-  #
-  # Populate the vuln_details table with additional
-  # information, matched by a specific criteria
-  #
-  def report_vuln_details(vuln, details)
-  ::ActiveRecord::Base.connection_pool.with_connection {
-    detail = ::Mdm::VulnDetail.where(( details.delete(:key) || {} ).merge(:vuln_id => vuln.id)).first
-    if detail
-      details.each_pair do |k,v|
-        detail[k] = v
-      end
-      detail.save! if detail.changed?
-      detail
-    else
-      detail = ::Mdm::VulnDetail.create(details.merge(:vuln_id => vuln.id))
-    end
-  }
-  end
-
-  #
-  # Update vuln_details records en-masse based on specific criteria
-  # Note that this *can* update data across workspaces
-  #
-  def update_vuln_details(details)
-  ::ActiveRecord::Base.connection_pool.with_connection {
-    criteria = details.delete(:key) || {}
-    ::Mdm::VulnDetail.update(key, details)
   }
   end
 
