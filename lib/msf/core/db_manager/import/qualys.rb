11bcac8a4ea783880da1b70f87c54a64a4db0036
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-14 15:19:55 -0500

    Extract Msf::DBManager::Import::Qualys::Scan
    MSP-11124

--- lib/msf/core/db_manager/import/qualys.rb
+++ lib/msf/core/db_manager/import/qualys.rb
@@ -1,5 +1,7 @@
 module Msf::DBManager::Import::Qualys
   autoload :Asset, 'msf/core/db_manager/import/qualys/asset'
+  autoload :Scan, 'msf/core/db_manager/import/qualys/scan'
 
   include Msf::DBManager::Import::Qualys::Asset
+  include Msf::DBManager::Import::Qualys::Scan
 end
