a0494b2eeb9bb210c8218922b85adae6f411e90d
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-15 11:27:23 -0500

    Extract Msf::DBManager::Import::Nessus::XML
    MSP-11124

--- lib/msf/core/db_manager/import.rb
+++ lib/msf/core/db_manager/import.rb
@@ -413,27 +413,6 @@ module Msf::DBManager::Import
     end
   end
 
-  #
-  # Import Nessus XML v1 and v2 output
-  #
-  # Old versions of openvas exported this as well
-  #
-  def import_nessus_xml_file(args={})
-    filename = args[:filename]
-    wspace = args[:wspace] || workspace
-
-    data = ""
-    ::File.open(filename, 'rb') do |f|
-      data = f.read(f.stat.size)
-    end
-
-    if data.index("NessusClientData_v2")
-      import_nessus_xml_v2(args.merge(:data => data))
-    else
-      import_nessus_xml(args.merge(:data => data))
-    end
-  end
-
   def import_nessus_xml_v2(args={}, &block)
     data = args[:data]
     wspace = args[:wspace] || workspace
--- lib/msf/core/db_manager/import/nessus.rb
+++ lib/msf/core/db_manager/import/nessus.rb
@@ -1,5 +1,7 @@
 module Msf::DBManager::Import::Nessus
   autoload :NBE, 'msf/core/db_manager/import/nessus/nbe'
+  autoload :XML, 'msf/core/db_manager/import/nessus/xml'
 
   include Msf::DBManager::Import::Nessus::NBE
+  include Msf::DBManager::Import::Nessus::XML
 end
