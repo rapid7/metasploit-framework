92a6c792009c52d18f6910f1c73aeb247eff0f59
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-15 13:24:40 -0500

    Extract Msf::DBManager::Import::Retina shared examples
    MSP-11124

--- spec/support/shared/examples/msf/db_manager/import.rb
+++ spec/support/shared/examples/msf/db_manager/import.rb
@@ -5,8 +5,6 @@ shared_examples_for 'Msf::DBManager::Import' do
   it { is_expected.to respond_to :import_file }
   it { is_expected.to respond_to :import_filetype_detect }
   it { is_expected.to respond_to :import_report }
-  it { is_expected.to respond_to :import_retina_xml }
-  it { is_expected.to respond_to :import_retina_xml_file }
   it { is_expected.to respond_to :import_spiceworks_csv }
   it { is_expected.to respond_to :import_wapiti_xml }
   it { is_expected.to respond_to :import_wapiti_xml_file }
@@ -39,4 +37,5 @@ shared_examples_for 'Msf::DBManager::Import' do
   it_should_behave_like 'Msf::DBManager::Import::OpenVAS'
   it_should_behave_like 'Msf::DBManager::Import::Outpost24'
   it_should_behave_like 'Msf::DBManager::Import::Qualys'
+  it_should_behave_like 'Msf::DBManager::Import::Retina'
 end
