370daaed5e90fb59abf92d8df09438ace1e9d42e
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-15 15:52:26 -0500

    Extract Msf::Module::Failure
    MSP-11126
    
    Move `Msf::Module::Failure` to a file of its own.

--- lib/msf/core/module.rb
+++ lib/msf/core/module.rb
@@ -12,6 +12,7 @@ module Msf
 #
 ###
 class Module
+  autoload :Failure, 'msf/core/module/failure'
 
   # Modules can subscribe to a user-interface, and as such they include the
   # UI subscriber module.  This provides methods like print, print_line, etc.
@@ -730,78 +731,6 @@ class Module
   def fail_with(reason, msg=nil)
     raise RuntimeError, "#{reason.to_s}: #{msg}"
   end
-
-  #
-  # Constants indicating the reason for an unsuccessful module attempt
-  #
-  module Failure
-
-    #
-    # No confidence in success or failure
-    #
-    None            = 'none'
-
-    #
-    # No confidence in success or failure
-    #
-    Unknown         = 'unknown'
-
-    #
-    # The network service was unreachable (connection refused, etc)
-    #
-    Unreachable     = 'unreachable'
-
-    #
-    # The exploit settings were incorrect
-    #
-    BadConfig       = 'bad-config'
-
-    #
-    # The network service disconnected us mid-attempt
-    #
-    Disconnected    = 'disconnected'
-
-    #
-    # The application endpoint or specific service was not found
-    #
-    NotFound        = 'not-found'
-
-    #
-    # The application replied in an unexpected fashion
-    #
-    UnexpectedReply = 'unexpected-reply'
-
-    #
-    # The exploit triggered some form of timeout
-    #
-    TimeoutExpired  = 'timeout-expired'
-
-    #
-    # The exploit was interrupted by the user
-    #
-    UserInterrupt   = 'user-interrupt'
-
-    #
-    # The application replied indication we do not have access
-    #
-    NoAccess        = 'no-access'
-
-    #
-    # The target is not compatible with this exploit or settings
-    #
-    NoTarget        = 'no-target'
-
-    #
-    # The application response indicated it was not vulnerable
-    #
-    NotVulnerable   = 'not-vulnerable'
-
-    #
-    # The payload was delivered but no session was opened (AV, network, etc)
-    #
-    PayloadFailed   = 'payload-failed'
-  end
-
 
   ##
   #
