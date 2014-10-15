46a2c47dfe66912dddc2e3286a0bbbd6dd538cd8
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-15 10:56:08 -0500

    Extract Msf::DBManager::Import::MetasploitFramework::Zip
    MSP-11124

--- lib/msf/core/db_manager/import/metasploit_framework.rb
+++ lib/msf/core/db_manager/import/metasploit_framework.rb
@@ -1,5 +1,7 @@
 module Msf::DBManager::Import::MetasploitFramework
   autoload :XML, 'msf/core/db_manager/import/metasploit_framework/xml'
+  autoload :Zip, 'msf/core/db_manager/import/metasploit_framework/zip'
 
   include Msf::DBManager::Import::MetasploitFramework::XML
+  include Msf::DBManager::Import::MetasploitFramework::Zip
 end
