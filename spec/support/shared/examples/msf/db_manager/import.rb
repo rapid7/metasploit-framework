3bce8e418d116af82975f695baab4f90f50fed1d
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-15 14:03:55 -0500

    unserialize_object -> Msf::DBManager::Import::MetasploitFramework shared examples
    MSP-11124

--- spec/support/shared/examples/msf/db_manager/import.rb
+++ spec/support/shared/examples/msf/db_manager/import.rb
@@ -7,7 +7,6 @@ shared_examples_for 'Msf::DBManager::Import' do
   it { is_expected.to respond_to :msf_import_timestamps }
   it { is_expected.to respond_to :report_import_note }
   it { is_expected.to respond_to :rexmlify }
-  it { is_expected.to respond_to :unserialize_object }
   it { is_expected.to respond_to :validate_import_file }
 
   it_should_behave_like 'Msf::DBManager::Import::Acunetix'
--- spec/support/shared/examples/msf/db_manager/import/metasploit_framework.rb
+++ spec/support/shared/examples/msf/db_manager/import/metasploit_framework.rb
@@ -1,5 +1,6 @@
 shared_examples_for 'Msf::DBManager::Import::MetasploitFramework' do
   it { is_expected.to respond_to :nils_for_nulls }
+  it { is_expected.to respond_to :unserialize_object }
 
   it_should_behave_like 'Msf::DBManager::Import::MetasploitFramework::Credential'
   it_should_behave_like 'Msf::DBManager::Import::MetasploitFramework::XML'
