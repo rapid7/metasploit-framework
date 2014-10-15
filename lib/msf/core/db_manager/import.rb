1f7ad1cac92e7dc9a372644a423192565067222a
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-15 14:03:18 -0500

    unserialize_object -> Msf::DBManager::Import::MetasploitFramework
    MSP-11124

--- lib/msf/core/db_manager/import.rb
+++ lib/msf/core/db_manager/import.rb
@@ -436,37 +436,6 @@ module Msf::DBManager::Import
     end
   end
 
-  def unserialize_object(xml_elem, allow_yaml = false)
-    return nil unless xml_elem
-    string = xml_elem.text.to_s.strip
-    return string unless string.is_a?(String)
-    return nil if (string.empty? || string.nil?)
-
-    begin
-      # Validate that it is properly formed base64 first
-      if string.gsub(/\s+/, '') =~ /^([a-z0-9A-Z\+\/=]+)$/
-        Marshal.load($1.unpack("m")[0])
-      else
-        if allow_yaml
-          begin
-            YAML.load(string)
-          rescue
-            dlog("Badly formatted YAML: '#{string}'")
-            string
-          end
-        else
-          string
-        end
-      end
-    rescue ::Exception => e
-      if allow_yaml
-        YAML.load(string) rescue string
-      else
-        string
-      end
-    end
-  end
-
   # Boils down the validate_import_file to a boolean
   def validate_import_file(data)
     begin
--- lib/msf/core/db_manager/import/metasploit_framework.rb
+++ lib/msf/core/db_manager/import/metasploit_framework.rb
@@ -11,4 +11,35 @@ module Msf::DBManager::Import::MetasploitFramework
   def nils_for_nulls(str)
     str == "NULL" ? nil : str
   end
+
+  def unserialize_object(xml_elem, allow_yaml = false)
+    return nil unless xml_elem
+    string = xml_elem.text.to_s.strip
+    return string unless string.is_a?(String)
+    return nil if (string.empty? || string.nil?)
+
+    begin
+      # Validate that it is properly formed base64 first
+      if string.gsub(/\s+/, '') =~ /^([a-z0-9A-Z\+\/=]+)$/
+        Marshal.load($1.unpack("m")[0])
+      else
+        if allow_yaml
+          begin
+            YAML.load(string)
+          rescue
+            dlog("Badly formatted YAML: '#{string}'")
+            string
+          end
+        else
+          string
+        end
+      end
+    rescue ::Exception => e
+      if allow_yaml
+        YAML.load(string) rescue string
+      else
+        string
+      end
+    end
+  end
 end
