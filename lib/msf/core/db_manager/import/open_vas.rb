eff95221dad320a89c276fd18505fe8564c7317e
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-15 13:14:20 -0500

    Order methods
    MSP-11124

--- lib/msf/core/db_manager/import/open_vas.rb
+++ lib/msf/core/db_manager/import/open_vas.rb
@@ -1,16 +1,6 @@
 require 'rex/parser/openvas_nokogiri'
 
 module Msf::DBManager::Import::OpenVAS
-  #
-  # Of course they had to change the nessus format.
-  #
-  def import_openvas_xml(args={}, &block)
-    filename = args[:filename]
-    wspace = args[:wspace] || workspace
-
-    raise DBImportError.new("No OpenVAS XML support. Please submit a patch to msfdev[at]metasploit.com")
-  end
-
   def import_openvas_new_xml(args={}, &block)
     if block
       doc = Rex::Parser::OpenVASDocument.new(args,framework.db) {|type, data| yield type,data }
@@ -30,5 +20,15 @@ module Msf::DBManager::Import::OpenVAS
       data = f.read(f.stat.size)
     end
     import_wapiti_xml(args.merge(:data => data))
+  end
+
+  #
+  # Of course they had to change the nessus format.
+  #
+  def import_openvas_xml(args={}, &block)
+    filename = args[:filename]
+    wspace = args[:wspace] || workspace
+
+    raise DBImportError.new("No OpenVAS XML support. Please submit a patch to msfdev[at]metasploit.com")
   end
 end
