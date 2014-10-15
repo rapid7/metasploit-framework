66b1dcc42d4db2dbec4c3a76fb4f35a4ef99b1ba
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-15 09:38:44 -0500

    Extract Msf::DBManager::Import::FusionVM shared examples
    MSP-11124

--- spec/support/shared/examples/msf/db_manager/import.rb
+++ spec/support/shared/examples/msf/db_manager/import.rb
@@ -4,7 +4,6 @@ shared_examples_for 'Msf::DBManager::Import' do
   it { is_expected.to respond_to :import }
   it { is_expected.to respond_to :import_file }
   it { is_expected.to respond_to :import_filetype_detect }
-  it { is_expected.to respond_to :import_fusionvm_xml }
   it { is_expected.to respond_to :import_ip_list }
   it { is_expected.to respond_to :import_ip_list_file }
   it { is_expected.to respond_to :import_libpcap }
@@ -67,6 +66,7 @@ shared_examples_for 'Msf::DBManager::Import' do
   it_should_behave_like 'Msf::DBManager::Import::Burp'
   it_should_behave_like 'Msf::DBManager::Import::CI'
   it_should_behave_like 'Msf::DBManager::Import::Foundstone'
+  it_should_behave_like 'Msf::DBManager::Import::FusionVM'
   it_should_behave_like 'Msf::DBManager::Import::IP360'
   it_should_behave_like 'Msf::DBManager::Import::MsfXml'
   it_should_behave_like 'Msf::DBManager::Import::Qualys'
