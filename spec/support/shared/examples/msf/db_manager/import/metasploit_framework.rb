4c9b29de954356fadefccf5eaa1b6dfc86316ec6
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-15 11:15:13 -0500

    Extract Msf::DBManager::Import::MetasploitFramework::Credential shared examples
    MSP-11124

--- spec/support/shared/examples/msf/db_manager/import/metasploit_framework.rb
+++ spec/support/shared/examples/msf/db_manager/import/metasploit_framework.rb
@@ -1,4 +1,5 @@
 shared_examples_for 'Msf::DBManager::Import::MetasploitFramework' do
+  it_should_behave_like 'Msf::DBManager::Import::MetasploitFramework::Credential'
   it_should_behave_like 'Msf::DBManager::Import::MetasploitFramework::XML'
   it_should_behave_like 'Msf::DBManager::Import::MetasploitFramework::Zip'
 end
