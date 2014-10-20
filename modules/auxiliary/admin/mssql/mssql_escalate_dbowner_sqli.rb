acc590b59ce20125f4d66767f917f2ae77bbb922
jvazquez-r7 <juan_vazquez@rapid7.com>
2014-10-20 16:22:10 -0500

    Modify metadata

--- modules/auxiliary/admin/mssql/mssql_escalate_dbowner_sqli.rb
+++ modules/auxiliary/admin/mssql/mssql_escalate_dbowner_sqli.rb
@@ -15,27 +15,20 @@ class Metasploit3 < Msf::Auxiliary
     super(update_info(info,
       'Name'           => 'Microsoft SQL Server - SQLi Escalate Db_Owner',
       'Description'    => %q{
-        This module can be used to escalate privileges to sysadmin if the user has
-        the db_owner role in a trustworthy database owned by a sysadmin user.  Once
-        the user has the sysadmin role the mssql_payload_sqli module can be used to obtain
-        a shell on the system.
+        This module can be used to escalate SQL Server user privileges to sysadmin through a web
+        SQL Injection. In order to escalate, the database user must to have the db_owner role in
+        a trustworthy database owned by a sysadmin user. Once the database user has the sysadmin
+        role, the mssql_payload_sqli module can be used to obtain a shell on the system.
 
-        Syntax for injection URLs:
-
-        Error: /account.asp?id=1+and+1=[SQLi];--
+        The syntax for injection URLs is: /testing.asp?id=1+and+1=[SQLi];--
       },
-      'Author'         =>
-        [
-          'nullbind <scott.sutherland[at]netspi.com>'
-        ],
       'Author'         => [ 'nullbind <scott.sutherland[at]netspi.com>'],
       'License'        => MSF_LICENSE,
-      'References'     => [[ 'URL','http://technet.microsoft.com/en-us/library/ms188676(v=sql.105).aspx']]
+      'References'     => [['URL','http://technet.microsoft.com/en-us/library/ms188676(v=sql.105).aspx']]
     ))
   end
 
   def run
-
     # Get the database user name
     print_status("#{peer} - Grabbing the database user name from ...")
     db_user = get_username
