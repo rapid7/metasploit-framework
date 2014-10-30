8b4767449fa9c9b736f14de730ab0eb84d5bfb9a
Luke Imhoff <luke_imhoff@rapid7.com>
2014-10-30 14:56:47 -0500

    Fix constant leaks in Msf::EncodedPayload spec
    MSP-11130
    
    Switch to using 'Msf::Simple::Framework' shared context and explicit
    module loading instead of loading all moduels to just test with one.

--- spec/lib/msf/core/encoded_payload_spec.rb
+++ spec/lib/msf/core/encoded_payload_spec.rb
@@ -2,18 +2,54 @@ require 'spec_helper'
 require 'msf/core/encoded_payload'
 
 describe Msf::EncodedPayload do
-  PAYLOAD_FRAMEWORK = Msf::Simple::Framework.create(
-    :module_types => [::Msf::MODULE_PAYLOAD, ::Msf::MODULE_ENCODER, ::Msf::MODULE_NOP],
-    'DisableDatabase' => true,
-    'DisableLogging' => true
-  )
+  include_context 'Msf::Simple::Framework'
+  include_context 'Metasploit::Framework::Spec::Constants cleaner'
 
-  let(:framework) { PAYLOAD_FRAMEWORK }
-  let(:payload) { 'linux/x86/shell_reverse_tcp' }
-  let(:pinst) { framework.payloads.create(payload) }
+  let(:ancestor_reference_names) {
+    %w{singles/linux/x86/shell_reverse_tcp}
+  }
+
+  let(:loader) {
+    loader = framework.modules.send(:loaders).find { |loader|
+      loader.loadable?(modules_path)
+    }
+
+    # Override load_error so that rspec will print it instead of going to framework log
+    def loader.load_error(module_path, error)
+      raise error
+    end
+
+    loader
+  }
+
+  let(:modules_path) {
+    Rails.application.paths['modules'].expanded.first
+  }
+
+  let(:module_type) {
+    'payload'
+  }
+
+  let(:reference_name) {
+    'linux/x86/shell_reverse_tcp'
+  }
+
+  let(:module_set) {
+    framework.modules.module_set(module_type)
+  }
+
+  let(:payload) {
+    ancestor_reference_names.each do |ancestor_reference_name|
+      loaded = loader.load_module(modules_path, module_type, ancestor_reference_name)
+
+      expect(loaded).to eq(true), "#{ancestor_reference_name} failed to load from #{modules_path}"
+    end
+
+    module_set.create(reference_name)
+  }
 
   subject(:encoded_payload) do
-    described_class.new(framework, pinst, {})
+    described_class.new(framework, payload, {})
   end
 
   it 'is an Msf::EncodedPayload' do
@@ -28,7 +64,7 @@ describe Msf::EncodedPayload do
       before { described_class.any_instance.stub(:generate) }
 
       it 'returns an Msf::EncodedPayload instance' do
-        expect(described_class.create(pinst)).to be_a(described_class)
+        expect(described_class.create(payload)).to be_a(described_class)
       end
 
     end
@@ -37,7 +73,13 @@ describe Msf::EncodedPayload do
 
   describe '#arch' do
     context 'when payload is linux/x86 reverse tcp' do
-      let(:payload) { 'linux/x86/shell_reverse_tcp' }
+      let(:ancestor_reference_names) {
+        %w{singles/linux/x86/shell_reverse_tcp}
+      }
+
+      let(:reference_name) {
+        'linux/x86/shell_reverse_tcp'
+      }
 
       it 'returns ["X86"]' do
         expect(encoded_payload.arch).to eq [ARCH_X86]
@@ -45,7 +87,13 @@ describe Msf::EncodedPayload do
     end
 
     context 'when payload is linux/x64 reverse tcp' do
-      let(:payload) { 'linux/x64/shell_reverse_tcp' }
+      let(:ancestor_reference_names) {
+        %w{singles/linux/x64/shell_reverse_tcp}
+      }
+
+      let(:reference_name) {
+        'linux/x64/shell_reverse_tcp'
+      }
 
       it 'returns ["X86_64"]' do
         expect(encoded_payload.arch).to eq [ARCH_X86_64]
