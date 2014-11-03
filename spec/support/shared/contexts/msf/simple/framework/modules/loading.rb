d97bc52d8a77836eed52de12966747de34d8900d
Luke Imhoff <luke_imhoff@rapid7.com>
2014-11-03 09:47:29 -0600

    Complete documentation for 'Msf::Simple::Framework::Modules loading'
    MSP-11130

--- spec/support/shared/contexts/msf/simple/framework/modules/loading.rb
+++ spec/support/shared/contexts/msf/simple/framework/modules/loading.rb
@@ -1,3 +1,34 @@
+# Loads, creates, and cleans up modules.
+#
+# @example Load and create encoder
+#   include_context 'Msf::Simple::Framework#modules loading'
+#
+#   let(:encoder) {
+#     load_and_create_module(
+#       ancestor_reference_names: %w{
+#         x86/shikata_ga_nai
+#       },
+#       modules_path: modules_path,
+#       module_type: 'encoder',
+#       reference_name: 'x86/shikata_ga_nai'
+#     )
+#   }
+#
+# @example Load and create staged payload
+#   include_context 'Msf::Simple::Framework#modules loading'
+#
+#   let(:staged_payload) {
+#     load_and_create_module(
+#       ancestor_reference_names: %w{
+#         stagers/android/reverse_https
+#         stages/android/meterpreter
+#       },
+#       modules_path: modules_path,
+#       module_type: 'payload',
+#       reference_name: 'android/meterpreter/reverse_tcp'
+#     )
+#   }
+#
 shared_context 'Msf::Simple::Framework#modules loading' do
   include_context 'Metasploit::Framework::Spec::Constants cleaner'
   include_context 'Msf::Simple::Framework'
@@ -6,7 +37,10 @@ shared_context 'Msf::Simple::Framework#modules loading' do
   # Methods
   #
 
+  # The module loader that can load module ancestors from `modules_path`
+  #
   # @param modules_path [String] path to `modules` directory from which to load ancestor reference names.
+  # @return [Msf::Modules::Loader::Base]
   def loader_for_modules_path(modules_path)
     loader = framework.modules.send(:loaders).find { |loader|
       loader.loadable?(modules_path)
@@ -20,6 +54,9 @@ shared_context 'Msf::Simple::Framework#modules loading' do
     loader
   end
 
+  # Expects to load `:ancestor_reference_name` of `:module_type` from `:modules_path`.
+  #
+  # @raise expectation failure if `:ancestor_reference_name` cannot be loaded
   def expect_to_load_module_ancestor(options={})
     options.assert_valid_keys(:ancestor_reference_name, :modules_path, :module_type)
 
@@ -33,6 +70,15 @@ shared_context 'Msf::Simple::Framework#modules loading' do
     expect(loaded).to eq(true), "#{ancestor_reference_name} failed to load from #{modules_path}"
   end
 
+  # Expects to laod `:ancestor_reference_names` of `:module_type` from `:modules_path`
+  #
+  # @param options [Hash{Symbol => Array<String>, String}]
+  # @option options [Array<String>] :ancestor_reference_names the reference names of the module ancestors of
+  #   `:module_type` to load from `:modules_path`.
+  # @option options [String] :modules_path The path from which to load `:ancestor_reference_names`.
+  # @option options [Stirng] :module_type The type of `:ancestor_reference_names` to derive their full paths under
+  #   `:modules_path`.
+  # @raise expectation failure if any `:ancestor_reference_names` cannot be loaded.
   def expect_to_load_module_ancestors(options={})
     options.assert_valid_keys(:ancestor_reference_names, :modules_path, :module_type)
 
@@ -57,7 +103,12 @@ shared_context 'Msf::Simple::Framework#modules loading' do
   # @option options [String] :modules_path path to the `modules` directory from which to load
   #   `:ancestor_reference_names`.
   # @option options [String] :module_type the type of module
+  # @option options [String] :modules_path the 'modules' directory from which to load `:ancestor_reference_names`.
   # @return [Msf::Module]
+  # @raise [KeyError] unless :ancestor_reference_names is given.
+  # @raise [KeyError] unless :modules_path is given.
+  # @raise [KeyError] unless :module_type is given.
+  # @raise [KeyError] unless :reference_name is given.
   def load_and_create_module(options={})
     options.assert_valid_keys(:ancestor_reference_names, :modules_path, :module_type, :reference_name)
 
@@ -80,6 +131,10 @@ shared_context 'Msf::Simple::Framework#modules loading' do
     module_instance
   end
 
+  # The module set for `module_type`.
+  #
+  # @param module_type [String] the module type creatable by the module set.
+  # @return [Msf::ModuleSet]
   def module_set_for_type(module_type)
     framework.modules.module_set(module_type)
   end
@@ -88,6 +143,9 @@ shared_context 'Msf::Simple::Framework#modules loading' do
   # lets
   #
 
+  # The default modules path for this `Rails.application`.
+  #
+  # @return [String]
   let(:modules_path) {
     Rails.application.paths['modules'].expanded.first
   }
