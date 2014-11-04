cc4e553c1119452dbebf97add8c615dba6ae462f
Luke Imhoff <luke_imhoff@rapid7.com>
2014-11-04 09:50:16 -0600

    Remove Metasploit::Framework::Spec::Constants::Each.configured!
    MSP-11130
    
    No longer needed because all before suite leaks have been fixed.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -54,5 +54,4 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
 end
 
-Metasploit::Framework::Spec::Constants::Each.configure!
 Metasploit::Framework::Spec::Constants::Suite.configure!
