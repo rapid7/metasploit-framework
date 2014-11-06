8416985c9d0569b2834780f392e292653447f8a7
Luke Imhoff <luke_imhoff@rapid7.com>
2014-11-06 14:05:35 -0600

    Give Threads UUIDs for spec run so caller can be correlated
    Have 'metasploit/framework/spec/threads/suite/logger' generate a UUID
    for each Thread.  This UUID is printed on the "BEGIN Thread.new caller"
    line and is assigned as a thread-local variable,
    'metasploit/framework/spec/threads/logger/uuid'.  In `after(:suite)`,
    the log can be parsed to map the caller back to each UUID and then only
    the UUID of the still existing threads is used to look up the caller and
    print their stacktraces.  This means only leaked threads callers will be
    printed.

--- lib/metasploit/framework/spec/threads/logger.rb
+++ lib/metasploit/framework/spec/threads/logger.rb
@@ -1,10 +1,22 @@
+#
+# Standard Library
+#
+
+require 'securerandom'
+
+#
+# Project
+#
+
 require 'metasploit/framework/spec/threads/suite'
 
 original_thread_new = Thread.method(:new)
 
 # Patches `Thread.new` so that if logs `caller` so thread leaks can be traced
 Thread.define_singleton_method(:new) { |*args, &block|
-  lines = ['BEGIN Thread.new caller']
+  uuid = SecureRandom.uuid
+  # tag caller with uuid so that only leaked threads caller needs to be printed
+  lines = ["BEGIN Thread.new caller (#{uuid})"]
 
   caller.each do |frame|
     lines << "  #{frame}"
@@ -19,5 +31,11 @@ Thread.define_singleton_method(:new) { |*args, &block|
     f.puts lines.join("\n")
   }
 
-  original_thread_new.call(*args, &block)
+  options = {original_args: args, uuid: uuid}
+
+  original_thread_new.call(options) {
+    # record uuid for thread-leak detection can used uuid to correlate log with this thread.
+    Thread.current[Metasploit::Framework::Spec::Threads::Suite::UUID_THREAD_LOCAL_VARIABLE] = options.fetch(:uuid)
+    block.call(*options.fetch(:original_args))
+  }
 }
