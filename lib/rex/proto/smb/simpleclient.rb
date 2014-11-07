e54442af36a8b7fc067ee5630e23e9a4e146ae2b
sinn3r <wei_chen@rapid7.com>
2014-11-07 02:45:22 -0600

    Fix #4089 - undefined method `downcase' for nil:NilClass

--- lib/rex/proto/smb/simpleclient.rb
+++ lib/rex/proto/smb/simpleclient.rb
@@ -66,6 +66,10 @@ attr_accessor :socket, :client, :direct, :shares, :last_share
 
       self.client.spnopt = spnopt
 
+      # In case the user unsets the password option, we make sure this is
+      # always a string
+      pass ||= ''
+
       ok = self.client.session_setup(user, pass, domain)
     rescue ::Interrupt
       raise $!
