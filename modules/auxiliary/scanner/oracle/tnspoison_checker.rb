d242bc220b7db145a6c5bd46a0a6fd0c459eda52
Tod Beardsley <tod_beardsley@rapid7.com>
2014-11-12 13:25:10 -0600

    Minor fixups and disclosure date for TNS module

--- modules/auxiliary/scanner/oracle/tnspoison_checker.rb
+++ modules/auxiliary/scanner/oracle/tnspoison_checker.rb
@@ -16,13 +16,16 @@ class Metasploit3 < Msf::Auxiliary
       'Name'           => 'Oracle TNS Listener Checker',
       'Description'    => %q{
         This module checks the server for vulnerabilities like TNS Poison.
-        Module sends to server a packet with command to register new TNS Listener and check response.
+        Module sends a server a packet with command to register new TNS Listener and checks
+        for a response indicating an error. If the registration is errored, the target is not
+        vulnearble. Otherwise, the target is vulnerable to malicious registrations.
       },
       'Author'         => ['ir0njaw (Nikita Kelesis) <nikita.elkey[at]gmail.com>'], # of Digital Security [http://dsec.ru]
       'References'     =>
         [
           [ 'URL', 'http://seclists.org/fulldisclosure/2012/Apr/204' ],
         ],
+      'DisclosureDate' => 'Apr 18 2012',
       'License'        => MSF_LICENSE))
 
     register_options(
@@ -30,7 +33,7 @@ class Metasploit3 < Msf::Auxiliary
         Opt::RPORT(1521)
       ], self.class)
 
-    deregister_options('RHOST')
+    deregister_options('RHOST') # Provided by the TNS mixin, but not needed in a scanner module
   end
 
   def run_host(ip)
@@ -41,6 +44,7 @@ class Metasploit3 < Msf::Auxiliary
       packet = sock.read(100)
       find_packet = packet.include? "(ERROR_STACK=(ERROR="
       find_packet == true ? print_error("#{ip}:#{rport} is not vulnerable ") : print_good("#{ip}:#{rport} is vulnerable")
+      #TODO: Module should report_vuln if this finding is solid.
       rescue ::Rex::ConnectionError, ::Errno::EPIPE
       print_error("#{ip}:#{rport} unable to connect to the server")
     end
