cca82f4b3647fb4989a5f31a525cb4c3b3473ee5
Luke Imhoff <luke_imhoff@rapid7.com>
2014-11-12 13:41:30 -0600

    Failing spec for Msf::Framework.new threads
    MSP-11605
    
    `Msf::Framework.new` creates 8 threads (change from 1 thread to 9 threads), but
    it shouldn't create any threads.

--- spec/lib/msf/core/framework_spec.rb
+++ spec/lib/msf/core/framework_spec.rb
@@ -6,6 +6,18 @@ require 'msf/core/framework'
 describe Msf::Framework do
   include_context 'Msf::Framework#threads cleaner'
 
+  context '#initialize' do
+    subject(:framework) {
+      described_class.new
+    }
+
+    it 'creates no threads' do
+      expect {
+        framework
+      }.not_to change { Thread.list.count }
+    end
+  end
+
   describe "#version" do
     CURRENT_VERSION = "4.10.1-dev"
 
