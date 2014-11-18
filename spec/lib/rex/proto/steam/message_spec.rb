6330c428d7cbe6c4015de3050f05311fc6ae53ee
Jon Hart <jon_hart@rapid7.com>
2014-11-18 11:06:32 -0800

    Address some of @jvazquez-r7's spec feedback

--- spec/lib/rex/proto/steam/message_spec.rb
+++ spec/lib/rex/proto/steam/message_spec.rb
@@ -3,30 +3,30 @@ require 'spec_helper'
 require 'rex/proto/steam/message'
 
 describe Rex::Proto::Steam do
-  subject do
+  subject(:steam) do
     mod = Module.new
     mod.extend described_class
     mod
   end
 
   describe '#encode_message' do
-    it 'should properly encode messages' do
-      message = subject.encode_message('T', 'Test')
+    it 'properly encodes messages' do
+      message = steam.encode_message('T', 'Test')
       expect(message).to eq("\xFF\xFF\xFF\xFF\x54Test")
     end
   end
 
   describe '#decode_message' do
-    it 'should not decode overly short messages' do
-      expect(subject.decode_message('foo')).to eq(nil)
+    it 'does not decode overly short messages' do
+      expect(steam.decode_message('foo')).to eq(nil)
     end
 
-    it 'should not decode unknown messages' do
-      expect(subject.decode_message("\xFF\xFF\xFF\x01blahblahblah")).to eq(nil)
+    it 'does not decode unknown messages' do
+      expect(steam.decode_message("\xFF\xFF\xFF\x01blahblahblah")).to eq(nil)
     end
 
-    it 'should properly decode valid messages' do
-      header, type, message = subject.decode_message("\xFF\xFF\xFF\xFF\x54Test")
+    it 'properly decodes valid messages' do
+      header, type, message = steam.decode_message("\xFF\xFF\xFF\xFF\x54Test")
       expect(header).to eq(Rex::Proto::Steam::UNFRAGMENTED_HEADER)
       expect(type).to eq(0x54)
       expect(message).to eq('Test')
@@ -34,14 +34,14 @@ describe Rex::Proto::Steam do
   end
 
   describe '#a2s_info_decode' do
-    it 'should extract a2s_info fields properly' do
+    it 'extracts a2s_info fields properly' do
       expected_info = {
         version: 17, name: "-=THE BATTLEGROUNDS *HARDCORE*=-", map: "aoc_battleground",
         folder: "ageofchivalry", game_name: "Age of Chivalry", game_id: 17510,
         players: "22/32", bots: 0, game_version: "1.0.0.6", type: "Dedicated",
         environment: "Linux", visibility: "public", VAC: "secured"
       }
-      actual_info = subject.a2s_info_decode(IO.read(File.join(File.dirname(__FILE__), 'steam_info.bin')))
+      actual_info = steam.a2s_info_decode(IO.read(File.join(File.dirname(__FILE__), 'steam_info.bin')))
       expect(actual_info).to eq(expected_info)
     end
   end
