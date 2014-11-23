57419bb0fc2cbc798f3310b23f909d1ff19f2a03
sinn3r <wei_chen@rapid7.com>
2014-11-22 23:09:15 -0600

    Fix #4253 - Print access level for snmp_login
    Fix #4253 - module should print the access level

--- modules/auxiliary/scanner/snmp/snmp_login.rb
+++ modules/auxiliary/scanner/snmp/snmp_login.rb
@@ -74,7 +74,7 @@ class Metasploit3 < Msf::Auxiliary
           credential_data[:core] = credential_core
           create_credential_login(credential_data)
 
-          print_good "#{ip}:#{rport} - LOGIN SUCCESSFUL: #{result.credential}"
+          print_good "#{ip}:#{rport} - LOGIN SUCCESSFUL: #{result.credential} (Access level: #{result.access_level})"
         else
           invalidate_login(credential_data)
           vprint_error "#{ip}:#{rport} - LOGIN FAILED: #{result.credential} (#{result.status}: #{result.proof})"
