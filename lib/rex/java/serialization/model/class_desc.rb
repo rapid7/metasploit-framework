b9023e8fcc8a7c4e9ec3f6e33884985dfec9fef0
jvazquez-r7 <juan_vazquez@rapid7.com>
2014-12-03 00:38:27 -0600

    Split ClassDescription into ClassDesc and NewClassDesc

---
+++ lib/rex/java/serialization/model/class_desc.rb
@@ -0,0 +1,65 @@
+module Rex
+  module Java
+    module Serialization
+      module Model
+        # This class provides a Java classDesc representation
+        class ClassDesc < Element
+
+          include Rex::Java::Serialization
+
+          attr_accessor :description
+
+          def initialize
+            self.description = nil
+          end
+
+          # Unserializes a Java::Serialization::Model::ClassDesc
+          #
+          # @param io [IO] the io to read from
+          # @return [self] if deserialization succeeds
+          # @raise [RuntimeError] if deserialization doesn't succeed
+          def decode(io)
+            super_opcode = io.read(1)
+            raise ::RuntimeError, 'Failed to unserialize ClassDesc' if super_opcode.nil?
+            super_opcode = super_opcode.unpack('C')[0]
+
+            case super_opcode
+            when TC_NULL
+              self.description = NullReference.new
+            when TC_CLASSDESC
+              self.description = NewClassDesc.decode(io)
+            when TC_PROXYCLASSDESC
+              #TODO: Support TC_PROXYCLASSDESC
+              raise ::RuntimeError, 'ClassDesc unserialize failed due to unsupported class'
+            else
+              raise ::RuntimeError, 'ClassDesc unserialize failed'
+            end
+
+            self
+          end
+
+          # Serializes the Java::Serialization::Model::ClassDesc
+          #
+          # @return [String] if serialization succeeds
+          # @raise [RuntimeError] if serialization doesn't succeed
+          def encode
+            encoded = ''
+
+            case description
+            when NewClassDesc
+              encoded << [TC_CLASSDESC].pack('C')
+              encoded << description.encode
+            when NullReference
+              encoded << [TC_NULL].pack('C')
+            else
+              #TODO: support other superclass types
+              raise RuntimeError, 'Failed to serialize ClassDesc'
+            end
+
+            encoded
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/rex/java/serialization/model/new_class_desc.rb
@@ -0,0 +1,124 @@
+module Rex
+  module Java
+    module Serialization
+      module Model
+        # This class provides a newClassDesc representation
+        class NewClassDesc < Element
+
+          include Rex::Java::Serialization
+
+          # @!attribute class_name
+          #   @return [Java::Serialization::Model::Utf] The name of the class
+          attr_accessor :class_name
+          # @!attribute name
+          #   @return [Integer] The java class serial version
+          attr_accessor :serial_version
+          # @!attribute flags
+          #   @return [Integer] The java class flags
+          attr_accessor :flags
+          # @!attribute fields
+          #   @return [Array] The java class fields
+          attr_accessor :fields
+          # @!attribute fields
+          #   @return [Java::Serialization::Model::Annotation] The java class annotations
+          attr_accessor :class_annotation
+          # @!attribute super_class
+          #   @return [Java::Serialization::Model::ClassDesc] The java class superclass description
+          attr_accessor :super_class
+
+          def initialize
+            self.class_name = nil
+            self.serial_version = 0
+            self.flags = 0
+            self.fields = []
+            self.class_annotation = nil
+            self.super_class = nil
+          end
+
+          # Unserializes a Java::Serialization::Model::ClassDescription
+          #
+          # @param io [IO] the io to read from
+          # @return [self] if deserialization succeeds
+          # @raise [RuntimeError] if deserialization doesn't succeed
+          def decode(io)
+            self.class_name = Utf.decode(io)
+            self.serial_version = decode_serial_version(io)
+            self.flags = decode_flags(io)
+            fields_length = decode_fields_length(io)
+
+            fields_length.times do
+              field = Field.decode(io)
+              self.fields << field
+            end
+
+            self.class_annotation = Annotation.decode(io)
+            self.super_class = ClassDesc.decode(io)
+
+            self
+          end
+
+          # Serializes the Java::Serialization::Model::ClassDescription
+          #
+          # @return [String] if serialization succeeds
+          # @raise [RuntimeError] if serialization doesn't succeed
+          def encode
+            encoded = ''
+            encoded << class_name.encode
+            encoded << [serial_version].pack('Q>')
+            encoded << [flags].pack('C')
+            encoded << [fields.length].pack('n')
+            fields.each do |field|
+              encoded << field.encode
+            end
+            encoded << class_annotation.encode
+            encoded << super_class.encode
+
+            encoded
+          end
+
+          private
+
+          # Unserializes a class serial version
+          #
+          # @param io [IO] the io to read from
+          # @return [Integer] if deserialization is possible
+          # @raise [RuntimeError] if deserialization isn't possible
+          def decode_serial_version(io)
+            raw_serial = io.read(8)
+            if raw_serial.nil? || raw_serial.length != 8
+              raise ::RuntimeError, 'Failed to unserialize ClassDescription'
+            end
+
+            raw_serial.unpack('Q>')[0]
+          end
+
+          # Unserializes a class flags
+          #
+          # @param io [IO] the io to read from
+          # @return [Integer] if deserialization is possible
+          # @raise [RuntimeError] if deserialization isn't possible
+          def decode_flags(io)
+            raw_flags = io.read(1)
+            raise ::RuntimeError, 'Failed to unserialize ClassDescription' if raw_flags.nil?
+
+            raw_flags.unpack('C')[0]
+          end
+
+          # Unserializes a class fields length
+          #
+          # @param io [IO] the io to read from
+          # @return [Integer] if deserialization is possible
+          # @raise [RuntimeError] if deserialization isn't possible
+          def decode_fields_length(io)
+            fields_length = io.read(2)
+            if fields_length.nil? || fields_length.length != 2
+              raise ::RuntimeError, 'Failed to unserialize ClassDescription'
+            end
+
+            fields_length.unpack('n')[0]
+          end
+        end
+      end
+    end
+  end
+end
