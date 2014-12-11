b533f7402478106046c6b3b71048c9da3dc957d8
Tod Beardsley <tod_beardsley@rapid7.com>
2014-12-11 11:06:32 -0600

    Add a bruteforce_speed option to all LoginScanners

--- modules/auxiliary/scanner/http/tomcat_mgr_login.rb
+++ modules/auxiliary/scanner/http/tomcat_mgr_login.rb
@@ -111,6 +111,7 @@ class Metasploit3 < Msf::Auxiliary
         proxies: datastore['PROXIES'],
         cred_details: cred_collection,
         stop_on_success: datastore['STOP_ON_SUCCESS'],
+        bruteforce_speed: datastore['BRUTEFORCE_SPEED'],
         connection_timeout: 10,
         user_agent: datastore['UserAgent'],
         vhost: datastore['VHOST']
--- modules/auxiliary/scanner/snmp/snmp_login.rb
+++ modules/auxiliary/scanner/snmp/snmp_login.rb
@@ -60,6 +60,7 @@ class Metasploit3 < Msf::Auxiliary
           port: rport,
           cred_details: collection,
           stop_on_success: datastore['STOP_ON_SUCCESS'],
+          bruteforce_speed: datastore['BRUTEFORCE_SPEED'],
           connection_timeout: 2
       )
 
