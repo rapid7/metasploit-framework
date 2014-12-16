1721641138b99050d95e5bce38efb5a412d2e3d9
jvazquez-r7 <juan_vazquez@rapid7.com>
2014-12-16 09:32:47 -0600

    Add support for PAC-LOGON-INFO

--- lib/rex/proto/kerberos/pac.rb
+++ lib/rex/proto/kerberos/pac.rb
@@ -4,7 +4,12 @@ module Rex
   module Proto
     module Kerberos
       module Pac
-
+        SE_GROUP_MANDATORY = 1
+        SE_GROUP_ENABLED_BY_DEFAULT = 2
+        SE_GROUP_ENABLED = 4
+        SE_GROUP_ALL = SE_GROUP_MANDATORY | SE_GROUP_ENABLED_BY_DEFAULT | SE_GROUP_ENABLED
+        USER_NORMAL_ACCOUNT = 0x00000010
+        USER_DONT_EXPIRE_PASSWORD = 0x00000200
       end
     end
   end
@@ -14,3 +19,4 @@ require 'rex/proto/kerberos/pac/element'
 require 'rex/proto/kerberos/pac/priv_svr_checksum'
 require 'rex/proto/kerberos/pac/server_checksum'
 require 'rex/proto/kerberos/pac/client_info'
+require 'rex/proto/kerberos/pac/logon_info'
