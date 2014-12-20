ca75b4b74a41ae6de2f7a6c9f0cdf1d7232449db
jvazquez-r7 <juan_vazquez@rapid7.com>
2014-12-20 01:36:54 -0600

    Add documentation for Msf::Client::Kerberos::Pac

--- lib/msf/kerberos/client/pac.rb
+++ lib/msf/kerberos/client/pac.rb
@@ -21,6 +21,17 @@ module Msf
           pa_pac_request
         end
 
+        # Builds a kerberos PACTYPE structure
+        #
+        # @param opts [Hash{Symbol => <String, Fixnum, Array, Time>}]
+        # @option opts [String] :client_name
+        # @option opts [Fixnum] :user_id the user SID Ex: 1000
+        # @option opts [Fixnum] :group_id Ex: 513 for 'Domain Users'
+        # @option opts [Array<Fixnum>] :group_ids
+        # @option opts [String] :realm
+        # @option opts [String] :domain_id the domain SID Ex: S-1-5-21-1755879683-3641577184-3486455962
+        # @option opts [Time] :logon_time
+        # @return [Rex::Proto::Kerberos::Pac::Type]
         def build_pac(opts)
           user_name = opts[:client_name] || ''
           user_id = opts[:user_id] || 1000
@@ -28,56 +39,59 @@ module Msf
           group_ids = opts[:group_ids] || [513]
           domain_name = opts[:realm] || ''
           domain_id = opts[:domain_id] || ''
-          logon_time = opts[:logon_time]
-          if logon_time.nil?
-            raise ::RuntimeError, 'logon_time not set on build pac'
-          end
+          logon_time = opts[:logon_time] || Time.now
+
           checksum_type = opts[:checksum_type] || Rex::Proto::Kerberos::Crypto::RsaMd5::RSA_MD5
 
           logon_info = Rex::Proto::Kerberos::Pac::LogonInfo.new(
-              logon_time: logon_time,
-              effective_name: user_name,
-              user_id: user_id,
-              primary_group_id: primary_group_id,
-              group_ids: group_ids,
-              logon_domain_name: domain_name,
-              logon_domain_id: domain_id,
+            logon_time: logon_time,
+            effective_name: user_name,
+            user_id: user_id,
+            primary_group_id: primary_group_id,
+            group_ids: group_ids,
+            logon_domain_name: domain_name,
+            logon_domain_id: domain_id,
           )
 
           client_info = Rex::Proto::Kerberos::Pac::ClientInfo.new(
-              client_id: logon_time,
-              name: user_name
+            client_id: logon_time,
+            name: user_name
           )
 
           server_checksum = Rex::Proto::Kerberos::Pac::ServerChecksum.new(
-              checksum: checksum_type
+            checksum: checksum_type
           )
 
           priv_srv_checksum = Rex::Proto::Kerberos::Pac::PrivSvrChecksum.new(
-              checksum: checksum_type
+            checksum: checksum_type
           )
 
           pac_type = Rex::Proto::Kerberos::Pac::Type.new(
-              buffers: [
-                           logon_info,
-                           client_info,
-                           server_checksum,
-                           priv_srv_checksum
-                       ],
-              checksum: checksum_type
+            buffers: [
+              logon_info,
+              client_info,
+              server_checksum,
+              priv_srv_checksum
+            ],
+            checksum: checksum_type
           )
 
           pac_type
         end
 
+        # Builds an kerberos AuthorizationData structure containing a PACTYPE
+        #
+        # @param opts [Hash{Symbol => String}]
+        # @option opts [String] :pac
+        # @return [Rex::Proto::Kerberos::Model::AuthorizationData]
         def build_pac_authorization_data(opts)
           pac = opts[:pac] || ''
 
           pac_auth_data = Rex::Proto::Kerberos::Model::AuthorizationData.new(
-              elements: [{:type => Rex::Proto::Kerberos::Pac::AD_WIN2K_PAC, :data => pac}]
+            elements: [{:type => Rex::Proto::Kerberos::Pac::AD_WIN2K_PAC, :data => pac}]
           )
           authorization_data = Rex::Proto::Kerberos::Model::AuthorizationData.new(
-              elements: [{:type => Rex::Proto::Kerberos::Model::AD_IF_RELEVANT, :data => pac_auth_data.encode}]
+            elements: [{:type => Rex::Proto::Kerberos::Model::AD_IF_RELEVANT, :data => pac_auth_data.encode}]
           )
 
           authorization_data
