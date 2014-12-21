e4455800e1f20fcf044ca10d04fa4695ad8f40ff
jvazquez-r7 <juan_vazquez@rapid7.com>
2014-12-21 16:46:26 -0600

    Add specs for Msf::Kerberos::Client::Base

---
+++ spec/lib/msf/kerberos/client/cache_credential_spec.rb
@@ -0,0 +1,44 @@
+# -*- coding:binary -*-
+require 'spec_helper'
+
+require 'rex/proto/kerberos'
+require 'msf/kerberos/client'
+
+describe Msf::Kerberos::Client::CacheCredential do
+  subject do
+    mod = ::Msf::Exploit.new
+    mod.extend ::Msf::Kerberos::Client
+    mod.send(:initialize)
+    mod
+  end
+
+  let(:valid_subkey) { 'AAAABBBBCCCCDDDD' }
+  let(:invalid_subkey) { '1234567890123456' }
+
+  describe "#create_cache_credential" do
+    context "when no opts" do
+      it "create a default Rex::Proto::Kerberos::CredentialCache::Credential" do
+
+      end
+    end
+
+    context "when opts" do
+      it "creates a Rex::Proto::Kerberos::CredentialCache::Credential according to options" do
+
+      end
+    end
+  end
+
+  describe "#create_cache" do
+  end
+
+  describe "#create_cache" do
+  end
+
+  describe "#create_cache" do
+  end
+  describe "#create_cache" do
+  end
+
+end
+
