fee033d6df833a207d61afe80fa87d6d00283443
jvazquez-r7 <juan_vazquez@rapid7.com>
2014-12-23 19:30:23 -0600

    Use Rex::Text.md5_raw

--- lib/rex/proto/kerberos/crypto/rsa_md5.rb
+++ lib/rex/proto/kerberos/crypto/rsa_md5.rb
@@ -6,10 +6,7 @@ module Rex
       module Crypto
         module RsaMd5
           def checksum_rsa_md5(data)
-            md5 = OpenSSL::Digest::MD5.new
-            md5 << data
-
-            md5.digest
+            Rex::Text.md5_raw(data)
           end
         end
       end
