294cd80a085abe63b1fdccada688f68d79e5c1ea
rastating <rob@rastating.com>
2015-01-07 18:32:52 +0000

    Update documentation for wordpress_login

--- lib/msf/http/wordpress/login.rb
+++ lib/msf/http/wordpress/login.rb
@@ -5,6 +5,7 @@ module Msf::HTTP::Wordpress::Login
   #
   # @param user [String] Username
   # @param pass [String] Password
+  # @param timeout [Integer] The maximum number of seconds to wait before the request times out
   # @return [String,nil] the session cookies as a single string on successful login, nil otherwise
   def wordpress_login(user, pass, timeout = 20)
     redirect = "#{target_uri}#{Rex::Text.rand_text_alpha(8)}"
