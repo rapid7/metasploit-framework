e9e6c3276976c9598a480d9f7f992c5de6f43d5f
jvazquez-r7 <juan_vazquez@rapid7.com>
2015-01-08 14:13:06 -0600

    Move build* calls to Streams

--- lib/msf/rmi/client.rb
+++ lib/msf/rmi/client.rb
@@ -66,44 +66,6 @@ module Msf
 
         return_data.return_value
       end
-
-      # Builds a RMI header stream
-      #
-      # @param opts [Hash{Symbol => <String, Fixnum>}]
-      # @option opts [String] :signature
-      # @option opts [Fixnum] :version
-      # @option opts [Fixnum] :protocol
-      # @return [Rex::Proto::Rmi::Model::OutputHeader]
-      def build_header(opts = {})
-        signature = opts[:signature] || Rex::Proto::Rmi::Model::SIGNATURE
-        version = opts[:version] || 2
-        protocol = opts[:protocol] || Rex::Proto::Rmi::Model::STREAM_PROTOCOL
-
-        header = Rex::Proto::Rmi::Model::OutputHeader.new(
-          signature: signature,
-          version: version,
-          protocol: protocol)
-
-        header
-      end
-
-      # Builds a RMI call stream
-      #
-      # @param opts [Hash{Symbol => <Fixnum, Rex::Java::Serialization::Model::Stream>}]
-      # @option opts [Fixnum] :message_id
-      # @option opts [Rex::Java::Serialization::Model::Stream] :call_data
-      # @return [Rex::Proto::Rmi::Model::Call]
-      def build_call(opts = {})
-        message_id = opts[:message_id] || Rex::Proto::Rmi::Model::CALL_MESSAGE
-        call_data = opts[:call_data] || Rex::Java::Serialization::Model::Stream.new
-
-        call = Rex::Proto::Rmi::Model::Call.new(
-          message_id: message_id,
-          call_data: call_data
-        )
-
-        call
-      end
     end
   end
 end
--- lib/msf/rmi/client/streams.rb
+++ lib/msf/rmi/client/streams.rb
@@ -7,7 +7,52 @@ module Msf
     module Client
       module Streams
 
-        def build_gc_call(jar_url)
+        # Builds a RMI header stream
+        #
+        # @param opts [Hash{Symbol => <String, Fixnum>}]
+        # @option opts [String] :signature
+        # @option opts [Fixnum] :version
+        # @option opts [Fixnum] :protocol
+        # @return [Rex::Proto::Rmi::Model::OutputHeader]
+        def build_header(opts = {})
+          signature = opts[:signature] || Rex::Proto::Rmi::Model::SIGNATURE
+          version = opts[:version] || 2
+          protocol = opts[:protocol] || Rex::Proto::Rmi::Model::STREAM_PROTOCOL
+
+          header = Rex::Proto::Rmi::Model::OutputHeader.new(
+              signature: signature,
+              version: version,
+              protocol: protocol)
+
+          header
+        end
+
+        # Builds a RMI call stream
+        #
+        # @param opts [Hash{Symbol => <Fixnum, Rex::Java::Serialization::Model::Stream>}]
+        # @option opts [Fixnum] :message_id
+        # @option opts [Rex::Java::Serialization::Model::Stream] :call_data
+        # @return [Rex::Proto::Rmi::Model::Call]
+        def build_call(opts = {})
+          message_id = opts[:message_id] || Rex::Proto::Rmi::Model::CALL_MESSAGE
+          call_data = opts[:call_data] || Rex::Java::Serialization::Model::Stream.new
+
+          call = Rex::Proto::Rmi::Model::Call.new(
+              message_id: message_id,
+              call_data: call_data
+          )
+
+          call
+        end
+
+        # Builds a call data (serializated) stream) as used by Michael Schierl (@mihi42)
+        # to achieve arbitrary code execution through the RMI garbage collector loading
+        # arbitrary classes
+        #
+        # @param jar_url [String] the (URL) location pointing to the jar containing the
+        #                         metasploit.RMILoader.
+        # @return [Rex::Java::Serialization::Model::Stream]
+        def build_gc_call_data(jar_url)
           stream = Rex::Java::Serialization::Model::Stream.new
 
           block_data = Rex::Java::Serialization::Model::BlockData.new
