8a0bb15939fe36dc0b5f42e78f23c4c14d9bddb0
Fatih Ozavci <fatih.ozavci@viproy.com>
2015-01-13 15:15:29 +1100

    RPORTS support for NEGOTIATE

--- modules/auxiliary/voip/viproy_sip_negotiate.rb
+++ modules/auxiliary/voip/viproy_sip_negotiate.rb
@@ -26,13 +26,12 @@ class Metasploit3 < Msf::Auxiliary
       OptString.new('TO',   [ true, "The destination username to probe at each host", "100"]),
       OptString.new('FROM',   [ true, "The source username to probe at each host", "100"]),
       OptString.new('PROTO',   [ true, "Protocol for SIP service (UDP|TCP|TLS)", "UDP"]),
-      Opt::RPORT(5060),
+      OptString.new('RPORTS', [true, 'Port Range (5060-5065)', "5060"]),
     ], self.class)
 
     register_advanced_options(
     [
       Opt::CHOST,
-      Opt::CPORT(5065),
       OptString.new('USERAGENT',   [ false, "SIP user agent" ]),
       OptString.new('REALM',   [ false, "The login realm to probe at each host", nil]),
       OptString.new('MACADDRESS',   [ false, "MAC Address for Vendor", "000000000000"]),
@@ -42,28 +41,31 @@ class Metasploit3 < Msf::Auxiliary
   end
 
   def run_host(dest_addr)
-    sockinfo={}
-    # Protocol parameters
-    sockinfo["proto"] = datastore['PROTO'].downcase
-    sockinfo["vendor"] = datastore['VENDOR'].downcase
-    sockinfo["macaddress"] = datastore['MACADDRESS']
+    rports = Rex::Socket.portspec_crack(datastore['RPORTS'])
+    rports.each { |rport|
+      sockinfo={}
+      # Protocol parameters
+      sockinfo["proto"] = datastore['PROTO'].downcase
+      sockinfo["vendor"] = datastore['VENDOR'].downcase
+      sockinfo["macaddress"] = datastore['MACADDRESS']
 
-    # Socket parameters
-    sockinfo["listen_addr"] = datastore['CHOST']
-    sockinfo["listen_port"] = datastore['CPORT']
-    sockinfo["dest_addr"] =datastore['RHOST']
-    sockinfo["dest_port"] = datastore['RPORT']
+      # Socket parameters
+      sockinfo["listen_addr"] = datastore['CHOST']
+      sockinfo["listen_port"] = datastore['CPORT']
+      sockinfo["dest_addr"] =datastore['RHOST']
+      sockinfo["dest_port"] = rport
 
-    sipsocket_start(sockinfo)
-    sipsocket_connect
+      sipsocket_start(sockinfo)
+      sipsocket_connect
 
-    results = send_negotiate(
-      'realm'		  => datastore['REALM'],
-      'from'    	=> datastore['FROM'],
-      'to'    	  => datastore['TO']
-    )
+      results = send_negotiate(
+        'realm'		  => datastore['REALM'],
+        'from'    	=> datastore['FROM'],
+        'to'    	  => datastore['TO']
+      )
 
-    printresults(results)
-    sipsocket_stop
+      printresults(results)
+      sipsocket_stop
+    }
   end
 end
