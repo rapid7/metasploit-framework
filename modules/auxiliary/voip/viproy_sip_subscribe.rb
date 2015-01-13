1a96e244359886c59082713611b7824bd5384675
Fatih Ozavci <fatih.ozavci@viproy.com>
2015-01-13 15:12:14 +1100

    THREAD support for SUBSCRIBE

--- modules/auxiliary/voip/viproy_sip_subscribe.rb
+++ modules/auxiliary/voip/viproy_sip_subscribe.rb
@@ -21,7 +21,7 @@ class Metasploit3 < Msf::Auxiliary
       'License'     => MSF_LICENSE
     )
 
-    deregister_options('RHOSTS','USER_AS_PASS','THREADS','USERPASS_FILE','PASS_FILE','PASSWORD','BLANK_PASSWORDS', 'BRUTEFORCE_SPEED','STOP_ON_SUCCESS' )
+    deregister_options('USER_AS_PASS', 'USERPASS_FILE','PASS_FILE','PASSWORD','BLANK_PASSWORDS', 'BRUTEFORCE_SPEED','STOP_ON_SUCCESS' )
 
     register_options(
         [
@@ -32,15 +32,12 @@ class Metasploit3 < Msf::Auxiliary
             OptString.new('FROMNAME',   [ false, "Custom Name for Message Spoofing", nil]),
             OptString.new('PROTO',   [ true, "Protocol for SIP service (UDP|TCP|TLS)", "UDP"]),
             OptBool.new('LOGIN', [false, 'Login Before Sending Message', false]),
-            Opt::RHOST,
-            Opt::RPORT(5060),
-
+            OptString.new('RPORTS', [true, 'Port Range (5060-5065)', "5060"]),
         ], self.class)
 
     register_advanced_options(
         [
             Opt::CHOST,
-            Opt::CPORT(5065),
             OptString.new('USERAGENT',   [ false, "SIP user agent" ]),
             OptString.new('SUBSCRIBETYPE',   [ false, "Subscribe message type (presence,message)", 'message']),
             OptString.new('REALM',   [ false, "The login realm to probe at each host", nil]),
@@ -57,89 +54,92 @@ class Metasploit3 < Msf::Auxiliary
         ], self.class)
   end
 
-  def run
-    # Login Parameters
-    login = datastore['LOGIN']
-    user = datastore['USERNAME']
-    password = datastore['PASSWORD']
-    realm = datastore['REALM']
+  def run_host(dest_addr)
+    rports = Rex::Socket.portspec_crack(datastore['RPORTS'])
+    rports.each { |rport|
+      # Login Parameters
+      login = datastore['LOGIN']
+      user = datastore['USERNAME']
+      password = datastore['PASSWORD']
+      realm = datastore['REALM']
 
-    sockinfo={}
-    # Protocol parameters
-    sockinfo["proto"] = datastore['PROTO'].downcase
-    sockinfo["vendor"] = datastore['VENDOR'].downcase
-    sockinfo["macaddress"] = datastore['MACADDRESS']
+      sockinfo={}
+      # Protocol parameters
+      sockinfo["proto"] = datastore['PROTO'].downcase
+      sockinfo["vendor"] = datastore['VENDOR'].downcase
+      sockinfo["macaddress"] = datastore['MACADDRESS']
 
-    # Socket parameters
-    sockinfo["listen_addr"] = datastore['CHOST']
-    sockinfo["listen_port"] = datastore['CPORT']
-    sockinfo["dest_addr"] =datastore['RHOST']
-    sockinfo["dest_port"] = datastore['RPORT']
+      # Socket parameters
+      sockinfo["listen_addr"] = datastore['CHOST']
+      sockinfo["listen_port"] = datastore['CPORT']
+      sockinfo["dest_addr"] = dest_addr
+      sockinfo["dest_port"] = rport
 
-    # Dumb fuzzing for FROM, FROMNAME and TO fields
-    if datastore['FROM'] =~ /FUZZ/
-      from=Rex::Text.pattern_create(datastore['FROM'].split(" ")[1].to_i)
-      fromname=nil
-    else
-      from = datastore['FROM']
-      if datastore['FROMNAME'] =~ /FUZZ/
-        fromname=Rex::Text.pattern_create(datastore['FROMNAME'].split(" ")[1].to_i)
+      # Dumb fuzzing for FROM, FROMNAME and TO fields
+      if datastore['FROM'] =~ /FUZZ/
+        from=Rex::Text.pattern_create(datastore['FROM'].split(" ")[1].to_i)
+        fromname=nil
       else
-        fromname = datastore['FROMNAME'] || datastore['FROM']
+        from = datastore['FROM']
+        if datastore['FROMNAME'] =~ /FUZZ/
+          fromname=Rex::Text.pattern_create(datastore['FROMNAME'].split(" ")[1].to_i)
+        else
+          fromname = datastore['FROMNAME'] || datastore['FROM']
+        end
       end
-    end
-    if datastore['TO'] =~ /FUZZ/
-      from=Rex::Text.pattern_create(datastore['TO'].split(" ")[1].to_i)
-    else
-      to = datastore['TO']
-    end
+      if datastore['TO'] =~ /FUZZ/
+        from=Rex::Text.pattern_create(datastore['TO'].split(" ")[1].to_i)
+      else
+        to = datastore['TO']
+      end
 
 
-    #Building Custom Headers
-    customheader = ""
-    customheader << datastore['CUSTOMHEADER']+"\r\n" if datastore['CUSTOMHEADER'] != nil
-    customheader << "P-Asserted-Identity: "+datastore['P-Asserted-Identity']+"\r\n" if datastore['P-Asserted-Identity'] != nil
-    customheader << "P-Charging-Vector: "+datastore['P-Charging-Vector']+"\r\n" if datastore['P-Charging-Vector'] != nil
-    customheader << "Record-Route: "+datastore['Record-Route']+"\r\n" if datastore['Record-Route'] != nil
-    customheader << "Route: "+datastore['Route']+"\r\n" if datastore['Route'] != nil
+      #Building Custom Headers
+      customheader = ""
+      customheader << datastore['CUSTOMHEADER']+"\r\n" if datastore['CUSTOMHEADER'] != nil
+      customheader << "P-Asserted-Identity: "+datastore['P-Asserted-Identity']+"\r\n" if datastore['P-Asserted-Identity'] != nil
+      customheader << "P-Charging-Vector: "+datastore['P-Charging-Vector']+"\r\n" if datastore['P-Charging-Vector'] != nil
+      customheader << "Record-Route: "+datastore['Record-Route']+"\r\n" if datastore['Record-Route'] != nil
+      customheader << "Route: "+datastore['Route']+"\r\n" if datastore['Route'] != nil
 
-    sipsocket_start(sockinfo)
-    sipsocket_connect
+      sipsocket_start(sockinfo)
+      sipsocket_connect
 
-    to.to_s
-    if datastore['TO_as_FROM']
-      from=to
-      fromname=nil
-    end
+      to.to_s
+      if datastore['TO_as_FROM']
+        from=to
+        fromname=nil
+      end
 
-    results = send_subscribe(
-        'login' 	      => login,
-        'loginmethod'  	=> datastore['LOGINMETHOD'],
-        'subscribetype'	=> datastore['SUBSCRIBETYPE'],
-        'user'  	      => user,
-        'password'	    => password,
-        'realm' 	      => realm,
-        'from'  	      => from,
-        'fromname'  	  => fromname,
-        'to'  		      => to,
-        'customheader'	=> customheader,
-    )
+      results = send_subscribe(
+          'login' 	      => login,
+          'loginmethod'  	=> datastore['LOGINMETHOD'],
+          'subscribetype'	=> datastore['SUBSCRIBETYPE'],
+          'user'  	      => user,
+          'password'	    => password,
+          'realm' 	      => realm,
+          'from'  	      => from,
+          'fromname'  	  => fromname,
+          'to'  		      => to,
+          'customheader'	=> customheader,
+      )
 
-    printresults(results) if datastore['DEBUG'] == true
+      printresults(results) if datastore['DEBUG'] == true
 
-    rdata = results["rdata"]
+      rdata = results["rdata"]
 
-    if rdata != nil and rdata['resp'] =~ /^18|^20|^48/ and resutls["rawdata"].to_s =~ /#{results["callopts"]["tag"]}/
-      print_good("Message: #{from} ==> #{to} Subscribe Sent (Server Response: #{rdata['resp_msg'].split(" ")[1,5].join(" ")})")
-    else
-      vprint_status("Message: #{from} ==> #{to} Subscription Failed (Server Response: #{rdata['resp_msg'].split(" ")[1,5].join(" ")})") if rdata != nil
-    end
+      if rdata != nil and rdata['resp'] =~ /^18|^20|^48/ and resutls["rawdata"].to_s =~ /#{results["callopts"]["tag"]}/
+        print_good("Message: #{from} ==> #{to} Subscribe Sent (Server Response: #{rdata['resp_msg'].split(" ")[1,5].join(" ")})")
+      else
+        vprint_status("Message: #{from} ==> #{to} Subscription Failed (Server Response: #{rdata['resp_msg'].split(" ")[1,5].join(" ")})") if rdata != nil
+      end
 
-    if customheader != ""
-      vprint_status("Custom Headers")
-      vprint_status(customheader)
-    end
+      if customheader != ""
+        vprint_status("Custom Headers")
+        vprint_status(customheader)
+      end
 
-    sipsocket_stop
+      sipsocket_stop
+    }
   end
 end
