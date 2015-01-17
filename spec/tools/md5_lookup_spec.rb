84030d273b56181a55ab31582cc6927082093e72
sinn3r <wei_chen@rapid7.com>
2015-01-16 22:06:49 -0600

    rspec progress

--- spec/tools/md5_lookup_spec.rb
+++ spec/tools/md5_lookup_spec.rb
@@ -47,7 +47,7 @@ describe Md5LookupUtility do
 
   let(:options) do
     {
-      :databases => ['all'],
+      :databases => [db_source],
       :outfile   => output_file,
       :input     => input_file
     }
@@ -57,7 +57,7 @@ describe Md5LookupUtility do
     Md5LookupUtility::OptsConsole.stub(:parse).with(any_args).and_return(options)
   end
 
-  subject(:md5_lookup_object) do
+  subject do
     Md5LookupUtility::Md5Lookup.new
   end
 
@@ -69,7 +69,7 @@ describe Md5LookupUtility do
   end
 
   def set_send_request_cgi(body)
-    allow(md5_lookup_object).to receive(:send_request_cgi) do |opts|
+    allow(subject).to receive(:send_request_cgi) do |opts|
       set_expected_response(body)
     end
   end
@@ -82,7 +82,7 @@ describe Md5LookupUtility do
 
     describe '.new' do
       it 'returns a Md5LookupUtility::Md5Lookup instance' do
-        expect(md5_lookup_object).to be_a(Md5LookupUtility::Md5Lookup)
+        expect(subject).to be_a(Md5LookupUtility::Md5Lookup)
       end
     end
 
@@ -91,14 +91,14 @@ describe Md5LookupUtility do
       context 'when a hash is found' do
         it 'returns the cracked result' do
           set_send_request_cgi(good_json_response)
-          expect(md5_lookup_object.lookup(input_data, db_source)).to eq(good_result)
+          expect(subject.lookup(input_data, db_source)).to eq(good_result)
         end
       end
 
       context 'when a hash is not found' do
         it 'returns an empty result' do
           set_send_request_cgi(bad_json_response)
-          expect(md5_lookup_object.lookup(input_data, db_source)).to eq(empty_result)
+          expect(subject.lookup(input_data, db_source)).to eq(empty_result)
         end
       end
     end
@@ -107,24 +107,34 @@ describe Md5LookupUtility do
       context 'when JSON contains the found result' do
         it 'returns the cracked result' do
           res = set_expected_response(good_json_response)
-          expect(md5_lookup_object.send(:get_json_result, res)).to eq(good_result)
+          expect(subject.send(:get_json_result, res)).to eq(good_result)
         end
       end
 
       context 'when there is no JSON data' do
         it 'returns an empty result' do
           res = set_expected_response('')
-          expect(md5_lookup_object.send(:get_json_result, res)).to eq(empty_result)
+          expect(subject.send(:get_json_result, res)).to eq(empty_result)
         end
       end
     end
 
   end
 
+
   describe Md5LookupUtility::Driver do
+
+    let(:expected_result) {
+      {
+        :hash         => input_data,
+        :cracked_hash => good_result,
+        :credit       => db_source
+      }
+    }
 
     before(:each) do
       allow(File).to receive(:open).with(input_file, 'rb').and_yield(StringIO.new(input_data))
+      allow(File).to receive(:new).with(output_file, 'wb').and_return(StringIO.new)
     end
 
     subject do
@@ -137,22 +147,63 @@ describe Md5LookupUtility do
       end
     end
 
+    describe '#run' do
+      context 'when a hash is found' do
+
+        def get_stdout(&block)
+          out = $stdout
+          $stdout = fake = StringIO.new
+          begin
+            yield
+          ensure
+            $stdout = out
+          end
+          fake.string
+        end
+
+        it 'prints a \'found\' message' do
+          allow(subject).to receive(:get_hash_results).and_yield(expected_result)
+          output = get_stdout { subject.run }
+          expect (output).should include('Found:')
+        end
+      end
+    end
+
     describe '#save_result' do
+      context 'when a result is given' do
+        it 'writes the result to file' do
+          subject.send(:save_result, expected_result)
+          expect(subject.instance_variable_get(:@output_handle).string).to include(good_result)
+        end
+      end
     end
 
     describe '#get_hash_results' do
       context 'when a hash is found' do
         it 'yields a result' do
-          set_send_request_cgi(good_json_response)
-          expect{ |b| subject.send(:get_hash_results, input_file, [db_source], &b) }.to yield_control
+          search_engine = Md5LookupUtility::Md5Lookup.new
+          allow(search_engine).to receive(:lookup).and_return(good_result)
+          allow(Md5LookupUtility::Md5Lookup).to receive(:new).and_return(search_engine)
+
+          expect{ |b| subject.send(:get_hash_results, input_file, [db_source], &b) }.to yield_with_args(expected_result)
         end
       end
     end
 
     describe '#extract_hashes' do
-      context 'when an input file is supplied' do
+      context 'when a MD5 file is supplied' do
         it 'yields the MD5 hash' do
           expect{ |b| subject.send(:extract_hashes, input_file, &b) }.to yield_with_args(input_data)
+        end
+      end
+
+      context 'when an empty file is supplied' do
+        before do
+          allow(File).to receive(:open).with(input_file, 'rb').and_yield(StringIO.new(''))
+        end
+
+        it 'yields nothing' do
+          expect{ |b| subject.send(:extract_hashes, input_file, &b) }.not_to yield_control
         end
       end
     end
@@ -171,6 +222,27 @@ describe Md5LookupUtility do
       end
     end
 
+  end
+
+
+  describe Md5LookupUtility::OptsConsole do
+    let(:argv) {}
+
+    describe '.parse' do
+
+    end
+
+    describe '.get_parsed_options' do
+    end
+
+    describe '.extract_db_names' do
+    end
+
+    describe '.get_database_symbols' do
+    end
+
+    describe '.get_database_names' do
+    end
   end
 
 end
