4311226840199876fe2c8c0b04208852e07f268f
jvazquez-r7 <juan_vazquez@rapid7.com>
2015-01-20 11:26:52 -0600

    Add documentation for Rex::Java::Serialization::Builder

--- lib/rex/java/serialization/builder.rb
+++ lib/rex/java/serialization/builder.rb
@@ -3,7 +3,18 @@
 module Rex
   module Java
     module Serialization
+      # This class provides a builder to help in the construction of
+      # Java serialized contents.
       class Builder
+
+        # Creates a Rex::Java::Serialization::Model::NewArray
+        #
+        # @param opts [Hash{Symbol => <Rex::Java::Serialization::Model::NewClassDesc, String, Array>}]
+        # @option opts [Rex::Java::Serialization::Model::NewClassDesc] :description
+        # @option opts [String] :values_type
+        # @option opts [Array] :values
+        # @return [Rex::Java::Serialization::Model::NewArray]
+        # @see #new_class
         def new_array(opts = {})
           class_desc = opts[:description] || new_class(opts)
           type = opts[:values_type] || ''
@@ -18,6 +29,13 @@ module Rex
           array
         end
 
+        # Creates a Rex::Java::Serialization::Model::NewObject
+        #
+        # @param opts [Hash{Symbol => <Rex::Java::Serialization::Model::NewClassDesc, Array>}]
+        # @option opts [Rex::Java::Serialization::Model::NewClassDesc] :description
+        # @option opts [Array] :data
+        # @return [Rex::Java::Serialization::Model::NewObject]
+        # @see #new_class
         def new_object(opts = {})
           class_desc = opts[:description] || new_class(opts)
           data = opts[:data] || []
@@ -30,6 +48,16 @@ module Rex
           object
         end
 
+        # Creates a Rex::Java::Serialization::Model::NewClassDesc
+        #
+        # @param opts [Hash{Symbol => <Rex::Java::Serialization::Model::NewClassDesc, Array>}]
+        # @option opts [String] :name
+        # @option opts [Fixnum] :serial
+        # @option opts [Fixnum] :flags
+        # @option opts [Array] :fields
+        # @option opts [Array] :annotations
+        # @option opts [Rex::Java::Serialization::Model::Element] :super_class
+        # @return [Rex::Java::Serialization::Model::NewClassDesc]
         def new_class(opts = {})
           class_name = opts[:name] || ''
           serial_version = opts[:serial] || 0
