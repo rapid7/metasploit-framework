dc6a365a13e5af442d250d2ca923f127a4359f5a
Christian Catalan <ccatalan@rapid7.com>
2015-02-13 13:18:36 -0600

    Fix finder query in Msf::DBManager::Vuln
    MSP-12152
    
    * This is part of updating finder queries to be Rails 4 compatibile
    * In #find_vuln_by_details, pass in conditons hash crit rather than symbol :crit

--- lib/msf/core/db_manager/vuln.rb
+++ lib/msf/core/db_manager/vuln.rb
@@ -31,7 +31,7 @@ module Msf::DBManager::Vuln
     vuln = nil
 
     if service
-      vuln = service.vulns.includes(:vuln_details).where(:crit).first
+      vuln = service.vulns.includes(:vuln_details).where(crit).first
     end
 
     # Return if we matched based on service
@@ -39,7 +39,7 @@ module Msf::DBManager::Vuln
 
     # Prevent matches against other services
     crit["vulns.service_id"] = nil if service
-    vuln = host.vulns.includes(:vuln_details).where(:crit).first
+    vuln = host.vulns.includes(:vuln_details).where(crit).first
 
     return vuln
   end
