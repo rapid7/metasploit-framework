a427e417a33d82cfca3301b60beea30c98504060
Bazin Danil <danil.bazin@gmail.com>
2015-02-26 21:23:09 +0100

    -consomation +consumption

--- lib/rex/parser/fs/ntfs.rb
+++ lib/rex/parser/fs/ntfs.rb
@@ -181,7 +181,7 @@ module Rex
 
             data = ''
             while data.length < size_wanted
-              # use an arbitrary block size to avoid target memory consomation
+              # use an arbitrary block size to avoid target memory consumption
               data << @file_handler.read([size_wanted - data.length, 2**26].min)
             end
             attribut << data
