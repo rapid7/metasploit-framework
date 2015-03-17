f95b783193784c13564aaa01916695945758f6a6
sinn3r <wei_chen@rapid7.com>
2015-03-17 11:33:49 -0500

    I don't need these eitehr

--- modules/auxiliary/scanner/http/symantec_web_gateway_login.rb
+++ modules/auxiliary/scanner/http/symantec_web_gateway_login.rb
@@ -48,8 +48,6 @@ class Metasploit3 < Msf::Auxiliary
       configure_http_login_scanner(
         host: ip,
         port: datastore['RPORT'],
-        ssl: datastore['SSL'],
-        ssl_version: datastore['SSLVersion'],
         cred_details:       @cred_collection,
         stop_on_success:    datastore['STOP_ON_SUCCESS'],
         bruteforce_speed:   datastore['BRUTEFORCE_SPEED'],
