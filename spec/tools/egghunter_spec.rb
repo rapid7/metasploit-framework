470464874f8b227fcf222cb389d37875326a588c
sinn3r <wei_chen@rapid7.com>
2015-03-20 16:53:42 -0500

    The important test cases I want to cover

--- spec/tools/egghunter_spec.rb
+++ spec/tools/egghunter_spec.rb
@@ -4,11 +4,32 @@ require 'rex/proto/http/response'
 require 'stringio'
 
 describe Egghunter do
+
   subject do
     Egghunter::Driver.new
   end
 
   describe '#run' do
-  end
 
+    context 'when the platform is windows' do
+      it 'returns a windows egghunter' do
+      end
+    end
+
+    context 'when the platform is linux' do
+      it 'returns a linux egghunter' do
+      end
+    end
+
+    context 'when the output format is java' do
+      it 'returns java format egghunter' do
+      end
+    end
+
+    context 'when the egg is WOOT' do
+      it 'includes W00TW00T in the egghunter' do
+      end
+    end
+
+  end
 end
