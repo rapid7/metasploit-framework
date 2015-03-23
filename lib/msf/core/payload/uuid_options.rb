6852475be013cebdb737e84235d17e02de81ff42
HD Moore <hd_moore@rapid7.com>
2015-03-23 14:35:33 -0500

    Placeholder for UUID options

---
+++ lib/msf/core/payload/uuid_options.rb
@@ -0,0 +1,15 @@
+# -*- coding => binary -*-
+
+require 'msf/core/payload/uuid'
+
+#
+# This module provides datastore option definitions and helper
+# methods for payload modules that support UUIDs
+#
+module Msf::Payload::UUID::Options
+
+
+
+
+end
+
