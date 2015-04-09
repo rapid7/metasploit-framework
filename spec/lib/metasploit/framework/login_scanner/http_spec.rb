717120b8c54621f1ff19768bb30f7ec05eaf60da
sinn3r <wei_chen@rapid7.com>
2015-04-09 00:34:09 -0500

    Add #get_hidden_inputs for Metasploit::Framework::LoginScanner::HTTP

--- spec/lib/metasploit/framework/login_scanner/http_spec.rb
+++ spec/lib/metasploit/framework/login_scanner/http_spec.rb
@@ -30,4 +30,51 @@ describe Metasploit::Framework::LoginScanner::HTTP do
     end
   end
 
+  describe '#get_hidden_inputs' do
+    let(:response) do
+      res = Rex::Proto::Http::Response.new(200, 'OK')
+      res.body = %Q|
+      <html>
+      <head>
+      <body>
+      <form action="test.php">
+        <input name="input_1" type="hidden" value="some_value_1" />
+      </form>
+      <form>
+        <input name="input_1" type="hidden" value="some_value_1" />
+        <INPUT name="input_2" type="hidden" value="" />
+      </form>
+      </body>
+      </head>
+      </htm>
+      |
+      res
+    end
+
+
+    context 'when an HTML page contains two forms containing hidden inputs' do
+      it 'returns an array' do
+        expect(subject.get_hidden_inputs(response)).to be_kind_of(Array)
+      end
+
+      it 'returns hashes in the array' do
+        subject.get_hidden_inputs(response).each do |form|
+          expect(form).to be_kind_of(Hash)
+        end
+      end
+
+      it 'returns \'some_value_1\' in the input_1 hidden input from the first element' do
+        expect(subject.get_hidden_inputs(response)[0]['input_1']).to eq('some_value_1')
+      end
+
+      it 'returns two hidden inputs in the second element' do
+        expect(subject.get_hidden_inputs(response)[1].length).to eq(2)
+      end
+
+      it 'returns an empty string for the input_2 hidden input from the second element' do
+        expect(subject.get_hidden_inputs(response)[1]['input_2']).to be_empty
+      end
+    end
+  end
+
 end
