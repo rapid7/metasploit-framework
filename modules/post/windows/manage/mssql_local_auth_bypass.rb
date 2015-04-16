352e1706245eef919aeb7642a7c0962416d3235a
Christian Mehlmauer <firefart@gmail.com>
2015-04-16 22:04:11 +0200

    more failure reasons

--- modules/post/windows/manage/mssql_local_auth_bypass.rb
+++ modules/post/windows/manage/mssql_local_auth_bypass.rb
@@ -120,9 +120,9 @@ class Metasploit3 < Msf::Post
       print_good("Successfully added login \"#{dbuser}\" with password \"#{dbpass}\"")
       return true
     when /already exists/i
-      fail_with(Exploit::Failure::BadConfig, "Unable to add login #{dbuser}, user already exists")
+      fail_with(Failure::BadConfig, "Unable to add login #{dbuser}, user already exists")
     when /password validation failed/i
-      fail_with(Exploit::Failure::BadConfig, "Unable to add login #{dbuser}, password does not meet complexity requirements")
+      fail_with(Failure::BadConfig, "Unable to add login #{dbuser}, password does not meet complexity requirements")
     else
       print_error("Unable to add login #{dbuser}")
       print_error("Database Error:\n #{add_login_result}")
