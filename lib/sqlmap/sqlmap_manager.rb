4aed12f56120c757bc12017473a1a019ac1a3579
root <void-in@users.noreply.github.com>
2015-04-24 15:40:35 +0500

    Include if condition in parse_response as per Meatballs1 suggestion

--- lib/sqlmap/sqlmap_manager.rb
+++ lib/sqlmap/sqlmap_manager.rb
@@ -8,73 +8,53 @@ module Sqlmap
 
     def new_task
       res = @session.get('/task/new')
-      if res && res.body 
-        parse_response(res.body)
-      end
+      parse_response(res)
     end
 
     def delete_task(task_id)
       res = @session.get('/task/' + task_id + '/delete')
-      if res && res.body
-        parse_response(res.body)
-      end
-
+      parse_response(res)
     end
 
     def set_option(task_id, key, value)
       post = { key => value }
       res = @session.post('/option/' + task_id + '/set', nil, post.to_json, {'ctype' => 'application/json'})
-      if res && res.body
-        parse_response(res.body)
-      end
-
+      parse_response(res)
     end
 
     def get_options(task_id)
       res = @session.get('/option/' + task_id + '/list')
-      if res && res.body
-        parse_response(res.body)
-      end
-
+      parse_response(res)
     end
 
     def start_task(task_id, options = {})
       res = @session.post('/scan/' + task_id + '/start' , nil, options.to_json, {'ctype' => 'application/json'})
-      if res && res.body
-        parse_response(res.body)
-      end
+      parse_response(res)
 
     end
 
     def get_task_status(task_id)
       res = @session.get('/scan/' + task_id + '/status')
-      if res && res.body
-        parse_response(res.body)
-      end
-
+      parse_response(res)
     end
 
     def get_task_log(task_id)
       res = @session.get('/scan/' + task_id + '/log')
-      if res && res.body
-        parse_response(res.body)
-      end
-
+      parse_response(res)
     end
 
     def get_task_data(task_id)
       res = @session.get('/scan/' + task_id + '/data')
-      if res && res.body
-        parse_response(res.body)
-      end
-
+      parse_response(res)
     end
 
     private
     def parse_response(res)
-      begin
-        res = JSON.parse(res)
-      rescue JSON::ParserError
+      if res && res.body
+        begin
+          res = JSON.parse(res.body)
+        rescue JSON::ParserError
+        end
       end
 
       res
