1f66840533da90b06f2dc93d9aa1a45b52e0e282
David Maloney <DMaloney@rapid7.com>
2015-04-29 12:53:54 -0500

    add YARD docs to NTDS Account
    added yard around the attrs for the NTDS::Account
    class
    
    MSP-12357

--- lib/metasploit/framework/ntds/account.rb
+++ lib/metasploit/framework/ntds/account.rb
@@ -5,26 +5,73 @@ module Metasploit
       # priv extension.
       class Account
 
-        attr_accessor :name
+        # @!attribute description
+        #   @return [String] The AD Account Description
         attr_accessor :description
-        attr_accessor :rid
+        # @!attribute disabled
+        #   @return [TrueClass] If the AD account is disabled
+        #   @return [FalseClass] If the AD account is not disabled
         attr_accessor :disabled
-        attr_accessor :locked
-        attr_accessor :no_pass
-        attr_accessor :no_expire
+        # @!attribute expired
+        #   @return [TrueClass] If the AD account password is expired
+        #   @return [FalseClass] If the AD account password is not expired
         attr_accessor :expired
-        attr_accessor :logon_count
-        attr_accessor :nt_history_count
-        attr_accessor :lm_history_count
+        # @!attribute expiry_date
+        #   @return [String] Human Readable Date for the account's password expiration
         attr_accessor :expiry_date
-        attr_accessor :logon_date
-        attr_accessor :logon_time
-        attr_accessor :pass_date
-        attr_accessor :pass_time
+        # @!attribute lm_hash
+        #   @return [String] The LM Hash of the current password
         attr_accessor :lm_hash
-        attr_accessor :nt_hash
+        # @!attribute lm_history
+        #   @return [Array<String>] The LM hashes for previous passwords, up to 24
         attr_accessor :lm_history
+        # @!attribute lm_history_count
+        #   @return [Fixnum] The count of historical LM hashes
+        attr_accessor :lm_history_count
+        # @!attribute locked
+        #   @return [TrueClass] If the AD account is locked
+        #   @return [FalseClass] If the AD account is not locked
+        attr_accessor :locked
+        # @!attribute logon_count
+        #   @return [Fixnum] The number of times this account has logged in
+        attr_accessor :logon_count
+        # @!attribute logon_date
+        #   @return [String] Human Readable Date for the last time the account logged in
+        attr_accessor :logon_date
+        # @!attribute logon_time
+        #   @return [String] Human Readable Time for the last time the account logged in
+        attr_accessor :logon_time
+        # @!attribute name
+        #   @return [String] The samAccountName of the account
+        attr_accessor :name
+        # @!attribute no_expire
+        #   @return [TrueClass] If the AD account password does not expire
+        #   @return [FalseClass] If the AD account password does expire
+        attr_accessor :no_expire
+        # @!attribute no_pass
+        #   @return [TrueClass] If the AD account does not require a password
+        #   @return [FalseClass] If the AD account does require a password
+        attr_accessor :no_pass
+        # @!attribute nt_hash
+        #   @return [String] The NT Hash of the current password
+        attr_accessor :nt_hash
+        # @!attribute nt_history
+        #   @return [Array<String>] The NT hashes for previous passwords, up to 24
         attr_accessor :nt_history
+        # @!attribute nt_history_count
+        #   @return [Fixnum] The count of historical NT hashes
+        attr_accessor :nt_history_count
+        # @!attribute pass_date
+        #   @return [String] Human Readable Date for the last password change
+        attr_accessor :pass_date
+        # @!attribute pass_time
+        #   @return [String] Human Readable Time for the last password change
+        attr_accessor :pass_time
+        # @!attribute rid
+        #   @return [Fixnum] The Relative ID of the account
+        attr_accessor :rid
+        # @!attribute sid
+        #   @return [String] Byte String for the Account's SID
         attr_accessor :sid
 
 
