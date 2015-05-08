dc98d340076655f7f37674807e0e8a50645d9236
benpturner <benpturner@yahoo.com>
2015-05-08 20:11:09 +0100

    No spaces

--- modules/post/windows/gather/powershell/enum_domain_users.rb
+++ modules/post/windows/gather/powershell/enum_domain_users.rb
@@ -27,7 +27,7 @@ class Metasploit3 < Msf::Post
     print_good("Running the post module: #{name} on: " + session.shell_command('$env:COMPUTERNAME').gsub!(/(\r\n)/, ''))
     # Find all domain users
     pscommand = '$searcher = new-object System.DirectoryServices.DirectorySearcher ; $searcher.filter = "(&(objectClass=user)(sAMAccountName=*))"; $colResults = $searcher.findall()'
-    print(session.shell_command(pscommand))
+    session.shell_command(pscommand)
     pscommand = 'foreach ($objResult in $colResults) {$objItem = $objResult.Properties; $objItem.samaccountname } ; "`n"'
     print(session.shell_command(pscommand))
   end
