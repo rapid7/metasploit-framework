e9e3d9c1e4f07d19c26ddc4ccb30c6a2029c24e4
OJ <oj@buffered.io>
2015-05-13 15:37:09 +1000

    Update payloads gem, and updated payload sizes

--- modules/payloads/singles/windows/meterpreter_bind_tcp.rb
+++ modules/payloads/singles/windows/meterpreter_bind_tcp.rb
@@ -13,7 +13,7 @@ require 'rex/payloads/meterpreter/config'
 
 module Metasploit4
 
-  CachedSize = 883870
+  CachedSize = 883358
 
   include Msf::Payload::TransportConfig
   include Msf::Payload::Windows
--- modules/payloads/singles/windows/x64/meterpreter_reverse_http.rb
+++ modules/payloads/singles/windows/x64/meterpreter_reverse_http.rb
@@ -13,7 +13,7 @@ require 'rex/payloads/meterpreter/config'
 
 module Metasploit4
 
-  CachedSize = 1103030
+  CachedSize = 1103542
 
   include Msf::Payload::TransportConfig
   include Msf::Payload::Windows
