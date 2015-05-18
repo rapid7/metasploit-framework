b0a8c7712747c7158f0b52470008f7d53944e0da
HD Moore <hd_moore@rapid7.com>
2015-05-18 15:33:01 -0500

    Switch RuntimeError -> EncodingError

--- modules/encoders/cmd/printf_php_mq.rb
+++ modules/encoders/cmd/printf_php_mq.rb
@@ -50,7 +50,7 @@ class Metasploit3 < Msf::Encoder
       (state.badchars.include?("|")) or
       # We must have at least ONE of these two..
       (state.badchars.include?("x") and state.badchars.include?("0"))
-      raise RuntimeError
+      raise EncodingError
     end
 
     # Now we build a string of the original payload with bad characters
