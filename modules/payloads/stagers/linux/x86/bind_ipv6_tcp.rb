923c4274d3236c74c63837df0c6f8ccc58e4a591
OJ <oj@buffered.io>
2015-05-18 21:52:33 +1000

    Formatting fixes

--- modules/payloads/stagers/linux/x86/bind_ipv6_tcp.rb
+++ modules/payloads/stagers/linux/x86/bind_ipv6_tcp.rb
@@ -1,9 +1,7 @@
-
 ##
 # This module requires Metasploit: http://metasploit.com/download
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
-
 
 require 'msf/core'
 require 'msf/core/handler/bind_tcp'
@@ -22,16 +20,16 @@ module Metasploit4
 
   def initialize(info = {})
     super(merge_info(info,
-      'Name'          => 'Bind IPv6 TCP Stager (Linux x86)',
-      'Description'   => 'Listen for an IPv6 connection (Linux x86)',
-      'Author'        => [ 'kris katterjohn', 'egypt' ],
-      'License'       => MSF_LICENSE,
-      'Platform'      => 'linux',
-      'Arch'          => ARCH_X86,
-      'Handler'       => Msf::Handler::BindTcp,
-      'Convention'    => 'sockedi',
-      'Stager'        => { 'RequiresMidstager' => true }
-      ))
+      'Name'        => 'Bind IPv6 TCP Stager (Linux x86)',
+      'Description' => 'Listen for an IPv6 connection (Linux x86)',
+      'Author'      => [ 'kris katterjohn', 'egypt' ],
+      'License'     => MSF_LICENSE,
+      'Platform'    => 'linux',
+      'Arch'        => ARCH_X86,
+      'Handler'     => Msf::Handler::BindTcp,
+      'Convention'  => 'sockedi',
+      'Stager'      => { 'RequiresMidstager' => true }
+    ))
   end
 
   def use_ipv6
