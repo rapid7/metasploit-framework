27e12754fee7698031ce6840a09e648787e7fd16
RageLtMan <rageltman [at] sempervictus>
2015-05-20 18:09:28 -0400

    Import Powershell libraries and sample post module
    Sync critical functionality from Rex and Msf namespaces dealing
    with encoding and processing of powershell script for exploit
    or post namespaces.
    
    Import Post module. Primarily adds a psh_exec method which will be
    replaced in the next PR with @benpturner's work integrated into
    the Post module namespace.
    
    Provide a sample metasploit windows post module to show the
    execution pipeline - entire subs process can be removed and the
    module reduced to a psh_exec(datastore['SCRIPT']).
    
    This commit is designed to provide sync between the SVIT fork and
    upstream. Pending commits to be based on this work will provide
    access to .NET compiler in the Post namespace to be used for
    dynamic persistent payload creation on target and the import of
    @benpturner's work.

---
+++ modules/post/windows/manage/exec_powershell.rb
@@ -0,0 +1,62 @@
+
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/framework/
+##
+
+require 'msf/core'
+require 'rex'
+require 'msf/core/post/windows/powershell'
+
+class Metasploit3 < Msf::Post
+	include Msf::Post::Windows::Powershell
+
+	def initialize(info={})
+		super(update_info(info,
+			'Name'                 => "Windows Powershell Execution Post Module",
+			'Description'          => %q{
+				This module will execute a powershell script in a meterpreter session.
+				The user may also enter text substitutions to be made in memory before execution.
+				Setting VERBOSE to true will output both the script prior to execution and the results.
+			},
+			'License'              => MSF_LICENSE,
+			'Platform'             => ['windows'],
+			'SessionTypes'         => ['meterpreter'],
+			'Author'               => [
+				'Nicholas Nam (nick[at]executionflow.org)', # original meterpreter script
+				'RageLtMan' # post module and libs
+				]
+		))
+
+		register_options(
+			[
+				OptString.new( 'SCRIPT',  [true, 'Path to the PS script or command string to execute' ]),
+			], self.class)
+
+		register_advanced_options(
+			[
+				OptString.new('SUBSTITUTIONS', [false, 'Script subs in gsub format - original,sub;original,sub' ]),
+			], self.class)
+
+	end
+
+
+
+	def run
+
+		# Make sure we meet the requirements before running the script, note no need to return
+		# unless error
+		raise "Powershell not available" if ! have_powershell?
+
+                # Preprocess the Powershell::Script object with substitions from Exploit::Powershell
+                script = make_subs(read_script(datstore['SCRIPT']),process_subs(datstore['SUBSTITUTIONS']))
+
+                # Execute in session
+		print_status psh_exec(script)
+		print_good('Finished!')
+	end
+
+
+
+end
