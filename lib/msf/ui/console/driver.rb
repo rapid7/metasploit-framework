583fccdbc8243ebf82db6abf1874d63ad66931b4
wchen-r7 <wei_chen@rapid7.com>
2015-05-27 18:28:08 -0500

    Resolve #5404, Check payload compatibility when using set payload
    Resolve #5404. This patch will check payload compatibility when
    you are using set payload in msfconsole.

--- lib/msf/ui/console/driver.rb
+++ lib/msf/ui/console/driver.rb
@@ -571,6 +571,8 @@ class Driver < Msf::Ui::Driver
 
         if (framework and framework.payloads.valid?(val) == false)
           return false
+        elsif active_module.type == 'exploit' && !is_payload_compatible?(active_module, val)
+          return false
         elsif (active_module)
           active_module.datastore.clear_non_user_defined
         elsif (framework)
@@ -587,6 +589,15 @@ class Driver < Msf::Ui::Driver
       when "promptchar"
         update_prompt(framework.datastore['Prompt'], val, true)
     end
+  end
+
+
+  def is_payload_compatible?(m, payload_name)
+    m.compatible_payloads.each do |k|
+      return true if k[0] == payload_name
+    end
+
+    false
   end
 
   #
