dede8a9eacbb89c79dce51908a4ca6644dce3a95
Jon Hart <jon_hart@rapid7.com>
2015-06-15 15:20:11 -0700

    Support multi-line FTP banners

--- modules/auxiliary/scanner/ftp/ftp_version.rb
+++ modules/auxiliary/scanner/ftp/ftp_version.rb
@@ -26,18 +26,15 @@ class Metasploit3 < Msf::Auxiliary
 
   def run_host(_target_host)
     begin
-      if connect(true, false) && banner
-        if /^220 (?<recog_banner>.*)$/ =~ banner
-          recog_banner.strip!
-          sanitized_banner = Rex::Text.to_hex_ascii(recog_banner)
-          unless info = Recog::Nizer.match('ftp.banner', recog_banner)
-            info = sanitized_banner
-            print_warning("#{peer} -- no Recog match: #{sanitized_banner}")
-          end
-          print_status("#{peer} FTP Banner: '#{sanitized_banner}'")
-          report_service(host: rhost, port: rport, name: 'ftp', info: info.to_s)
-        else
+      if connect(true, false) && banner && banner =~ /^220[ -]/
+        recog_banner = banner.gsub(/^220[ -](.*)\r\n/) { "#{Regexp.last_match(1)}\r\n" }
+        sanitized_banner = Rex::Text.to_hex_ascii(recog_banner)
+        unless info = Recog::Nizer.match('ftp.banner', recog_banner)
+          info = sanitized_banner
+          print_warning("#{peer} -- no Recog match: #{sanitized_banner}")
         end
+        print_status("#{peer} FTP Banner: '#{sanitized_banner}'")
+        report_service(host: rhost, port: rport, name: 'ftp', info: info.to_s)
       end
     rescue ::Interrupt
       raise $ERROR_INFO
