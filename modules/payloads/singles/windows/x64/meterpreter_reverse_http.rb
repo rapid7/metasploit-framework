226137896e187565ddb4d120cf482ec6d3a8ff91
Brent Cook <bcook@rapid7.com>
2015-07-10 22:30:20 -0500

    updated cached payload sizes

--- modules/payloads/singles/windows/x64/meterpreter_reverse_http.rb
+++ modules/payloads/singles/windows/x64/meterpreter_reverse_http.rb
@@ -13,7 +13,7 @@ require 'rex/payloads/meterpreter/config'
 
 module Metasploit4
 
-  CachedSize = 1105478
+  CachedSize = 1107014
 
   include Msf::Payload::TransportConfig
   include Msf::Payload::Windows
