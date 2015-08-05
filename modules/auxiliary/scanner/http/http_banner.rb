7997265482cdf704e9086b11291f5bf0e0a8adef
Manuel Mancera <sinkmanu@gmail.com>
2015-08-05 23:16:14 +0200

    Add http_banner module

--- modules/auxiliary/scanner/http/http_banner.rb
+++ modules/auxiliary/scanner/http/http_banner.rb
@@ -13,8 +13,7 @@ class Metasploit3 < Msf::Auxiliary
   def initialize(info={})
     super(update_info(info,
       'Name'        => 'HTTP Banner Detection',
-      'Description' => %q{ This module shows HTTP Banner returned by webserver. If you have loaded a database 
-plugin and connected to a database this module will record the info so you can list all banners. },
+      'Description' => %q{ This module shows HTTP Banner returned by webserver. If you have loaded a database plugin and connected to a database this module will record the info so you can list all banners. },
       'Author'      =>  'Manuel Mancera (sinkmanu)',
       'References'  =>
       [
@@ -24,40 +23,40 @@ plugin and connected to a database this module will record the info so you can l
     ))
 
     register_options([
-	  OptString.new('METHOD', [ true, 'HTTP Method to use', 'GET']),
-	  OptString.new('PROTOCOL', [ false, 'Protocol to use', 'HTTP']),
-      	  OptString.new('TARGETURI', [ true, 'The URI to use', '/']),
-	  OptInt.new('TIMEOUT', [ false, "The timeout waiting for the server response", 10])
+      OptString.new('METHOD', [ true, 'HTTP Method to use', 'GET']),
+      OptString.new('PROTOCOL', [ false, 'Protocol to use', 'HTTP']),
+      OptString.new('TARGETURI', [ true, 'The URI to use', '/']),
+      OptInt.new('TIMEOUT', [ false, "The timeout waiting for the server response", 10])
     ])
   end
 
   def timeout
-  	datastore['TIMEOUT']
+    datastore['TIMEOUT']
   end
 
   def run_host(ip)
-	print_status "Trying #{peer}"
-	uri = normalize_uri(target_uri.path)
-	method = datastore['METHOD']
-	proto = datastore['PROTOCOL']
+    print_status "Trying #{peer}"
+    uri = normalize_uri(target_uri.path)
+    method = datastore['METHOD']
+    proto = datastore['PROTOCOL']
 
-	res = send_request_raw({
-    	'method'  => method,
-      	'uri'     => uri,
-	'proto'   => proto
+    res = send_request_raw({
+      'method'  => method,
+      'uri'     => uri,
+      'proto'   => proto
     }, timeout)
 
     if res
-		banner = res.headers['Server']
-		if !banner.nil? 
-			print_good "Found: #{banner}"
-			report_service(
-    			:host => ip,
-    			:port => rport,
-        		:name => "http",
-          		:info => "#{banner}"
-        	)
-		end
-	end
+      banner = res.headers['Server']
+      if !banner.nil? 
+        print_good "Found: #{banner}"
+        report_service(
+          :host => ip,
+          :port => rport,
+          :name => "http",
+          :info => "#{banner}"
+        )
+      end
+    end
   end
 end
