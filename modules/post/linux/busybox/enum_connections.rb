dfdb4fe044b7ff686690ced8b9e8f45149e7e5dc
jvazquez-r7 <juan_vazquez@rapid7.com>
2015-08-28 09:28:12 -0500

    Review enum_connections

--- modules/post/linux/busybox/enum_connections.rb
+++ modules/post/linux/busybox/enum_connections.rb
@@ -10,48 +10,53 @@ class Metasploit3 < Msf::Post
   include Msf::Post::File
   include Msf::Post::Linux::Busybox
 
+  FILES = [
+    '/proc/net/nf_conntrack',
+    '/proc/net/ip_conntrack',
+    '/proc/net/tcp',
+    '/proc/net/udp',
+    '/proc/net/arp',
+    '/proc/fcache/*'
+  ]
+
   def initialize
     super(
       'Name'         => 'BusyBox Enumerate Connections',
-      'Description'  => 'This module will be applied on a session connected
-                         to a BusyBox sh shell. The script will enumerate
-                         the connections established by the hosts connected
-                         to the router or device executing BusyBox.',
+      'Description'  => %q{
+        This module will be applied on a session connected to a BusyBox sh shell. It will
+        enumerate the connections established with the router or device executing BusyBox.
+      },
       'Author'       => 'Javier Vicente Vallejo',
       'License'      => MSF_LICENSE,
-      'References'   =>
-        [
-          [ 'URL', 'http://vallejo.cc']
-        ],
-      'Platform'      => ['linux'],
-       'SessionTypes'  => ['shell']
+      'Platform'     => ['linux'],
+      'SessionTypes' => ['shell']
     )
   end
 
   def run
     found = false
-    conns_files =[
-      "/proc/net/nf_conntrack", "/proc/net/ip_conntrack", "/proc/net/tcp", "/proc/net/udp", "/proc/net/arp", "/proc/fcache/*"
-    ]
-    vprint_status("Searching for files that store information about network connections.")
-    conns_files.each do |conns_file|
-      if file_exists(conns_file)
+    print_status('Searching for files that store information about network connections')
+    FILES.each do |f|
+      if file_exists(f)
         found = true
-        print_good("Connections File found: #{conns_file}.")
-        begin
-          str_file=read_file(conns_file)
-          vprint_line(str_file)
-          #Store file
-          p = store_loot("Connections", "text/plain", session, str_file, conns_file, "BusyBox Device Network Established Connections")
-          print_good("Connections saved to #{p}.")
-        rescue EOFError
-          # If there's nothing in the file, we hit EOFError
-          print_error("Nothing read from file #{conns_file}, file may be empty.")
-        end
+        print_good("Connections File found: #{f}.")
+        read_connection_file(f)
       end
     end
-    if found == false
-      print_error("Nothing read from connection files, files may be empty.")
+
+    print_error('Any file with connections found') unless found
+  end
+
+  def read_connection_file(file)
+    begin
+      str_file=read_file(file)
+      vprint_line(str_file)
+      #Store file
+      p = store_loot('busybox.enum.connections', 'text/plain', session, str_file, file, 'BusyBox Device Network Established Connections')
+      print_good("Connections saved to #{p}.")
+    rescue EOFError
+      # If there's nothing in the file, we hit EOFError
+      print_error("Nothing read from file #{file}, file may be empty.")
     end
   end
 
