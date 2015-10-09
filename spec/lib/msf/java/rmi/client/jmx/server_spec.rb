3cd9dc4fde06e43e6e7b489cbcb31a2f7eb94b3d
jvazquez-r7 <juan_vazquez@rapid7.com>
2015-10-09 12:17:44 -0500

    Fix JMX Server specs

--- spec/lib/msf/java/rmi/client/jmx/server_spec.rb
+++ spec/lib/msf/java/rmi/client/jmx/server_spec.rb
@@ -51,6 +51,10 @@ describe Msf::Java::Rmi::Client::Jmx::Server do
         allow_any_instance_of(::StringIO).to receive(:get_once) do |io, length, timeout|
           io.read
         end
+
+        allow_any_instance_of(::StringIO).to receive(:has_read_data?) do |io|
+          false
+        end
       end
 
       it "returns the reference information" do
