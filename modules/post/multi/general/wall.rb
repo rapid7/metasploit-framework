a328675f77544bbc406749046b79fcdeda2d12f8
Jon Hart <jon_hart@rapid7.com>
2015-11-11 11:54:46 -0800

    Add simulated cowsay support to wall

--- modules/post/multi/general/wall.rb
+++ modules/post/multi/general/wall.rb
@@ -16,7 +16,9 @@ class Metasploit3 < Msf::Post
           to send messages to users on the target system.
         },
         'License'       => MSF_LICENSE,
-        'Author'        => [ 'Jon Hart <jon_hart[at]rapid7.com>' ],
+        'Author'        => [
+          'Jon Hart <jon_hart[at]rapid7.com>' # original metasploit module
+        ],
         # TODO: is there a way to do this on Windows?
         'Platform'      => %w(linux osx unix),
         'SessionTypes'  => %w(shell meterpreter)
@@ -24,10 +26,41 @@ class Metasploit3 < Msf::Post
     )
     register_options(
       [
-        OptString.new('MESSAGE', [true, 'The message to send']),
+        OptString.new('MESSAGE', [false, 'The message to send', '']),
         OptString.new('USERS', [false, 'List of users to write(1) to, separated by commas. ' \
-                      ' wall(1)s to all users by default'])
+                      ' wall(1)s to all users by default']),
+        OptBool.new('COWSAY', [true, 'Display MESSAGE in a ~cowsay way', false])
       ], self.class)
+  end
+
+  def cowsay(text)
+    # cowsay(1) chunks a message up into 39-byte chunks and wraps it in '| ' and ' |'
+    # Rex::Text.wordwrap(text, 0, 39, ' |', '| ') almost does this, but won't
+    # split a word that has > 39 characters in it which results in oddly formed
+    # text in the cowsay banner, so just do it by hand
+    text_lines = text.scan(/.{1,34}/)
+    max_length = text_lines.map(&:size).sort.last
+    cloud_parts = []
+    cloud_parts << " #{'_' * (max_length + 2)} "
+    if text_lines.size == 1
+      cloud_parts << "< #{text} >"
+    else
+      cloud_parts << "/ #{text_lines.first.ljust(max_length, ' ')} \\"
+      if text_lines.size > 2
+        text_lines[1, text_lines.length - 2].each do |line|
+          cloud_parts << "| #{line.ljust(max_length, ' ')} |"
+        end
+      end
+      cloud_parts << "\\ #{text_lines.last.ljust(max_length, ' ')} /"
+    end
+    cloud_parts << " #{'-' * (max_length + 2)} "
+    cloud_parts << <<EOS
+       \\   ,__,
+        \\  (oo)____
+           (__)    )\\
+              ||--|| *
+EOS
+    cloud_parts.join("\n")
   end
 
   def users
@@ -35,14 +68,24 @@ class Metasploit3 < Msf::Post
   end
 
   def message
-    datastore['MESSAGE'] ? datastore['MESSAGE'] : "Hello metasploit session #{session.id}, the time is #{Time.now}"
+    if datastore['MESSAGE'].blank?
+      text = "Hello from a metasploit session at #{Time.now}"
+    else
+      text = datastore['MESSAGE']
+    end
+
+    datastore['COWSAY'] ? cowsay(text) : text
   end
 
   def run
     if users
+      # this requires that the target user has write turned on
       users.map { |user| cmd_exec("echo '#{message}' | write #{user}") }
     else
-      cmd_exec("echo '#{message}' | wall")
+      # this will send the messages to all users, regardless of whether or
+      # not they have write turned on.  If the session is root, the -n will disable
+      # the annoying banner
+      cmd_exec("echo '#{message}' | wall -n")
     end
   end
 end
