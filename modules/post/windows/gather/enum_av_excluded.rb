81ee01a93e435d06d583bdc49ef33ae9b9eaab16
Jon Hart <jon_hart@rapid7.com>
2015-12-04 11:42:03 -0800

    Simplify exclusion extraction and printing

--- modules/post/windows/gather/enum_av_excluded.rb
+++ modules/post/windows/gather/enum_av_excluded.rb
@@ -27,20 +27,23 @@ class Metasploit3 < Msf::Post
 
     register_options(
       [
-        OptBool.new('DEFENDER', [true, 'Enumerate exclusions for Microsoft Defener', true]),
+        OptBool.new('DEFENDER', [true, 'Enumerate exclusions for Microsoft Defender', true]),
         OptBool.new('ESSENTIALS', [true, 'Enumerate exclusions for Microsoft Security Essentials/Antimalware', true]),
         OptBool.new('SEP', [true, 'Enumerate exclusions for Symantec Endpoint Protection (SEP)', true])
       ]
     )
   end
 
+  DEFENDER = 'Windows Defender'
   DEFENDER_BASE_KEY = 'HKLM\\SOFTWARE\\Microsoft\\Windows Defender'
+  ESSENTIALS = 'Microsoft Security Essentials / Antimalware'
   ESSENTIALS_BASE_KEY = 'HKLM\\SOFTWARE\\Microsoft\\Microsoft Antimalware'
+  SEP = 'Symantec Endpoint Protection (SEP)'
   SEP_BASE_KEY = 'HKLM\\SOFTWARE\\Symantec\\Symantec Endpoint Protection'
 
   def av_installed?(base_key, product)
     if registry_key_exist?(base_key)
-      print_status("Found #{product}")
+      print_good("Found #{product}")
       true
     else
       false
@@ -48,77 +51,69 @@ class Metasploit3 < Msf::Post
   end
 
   def excluded_sep
-    print_status "Excluded Locations:"
-    keyadm = "HKLM\\SOFTWARE\\Symantec\\Symantec Endpoint Protection\\AV\\Exclusions\\ScanningEngines\\Directory\\Admin"
-    if (found_keysadm = registry_enumkeys(keyadm))
-      found_keysadm.each do |vals|
-        full = keyadm + "\\" + vals
-        values = registry_getvaldata(full, "DirectoryName")
-        print_good "#{values}"
-      end
-    else
-      print_error "No Admin Locations Found"
-    end
+    base_exclusion_key = "#{SEP_BASE_KEY}\\Exclusions\\ScanningEngines\\Directory"
+    admin_exclusion_key = "#{base_exclusion_key}\\Admin"
+    admin_exclusion_key = "#{base_exclusion_key}\\Client"
 
-    keycli = "HKLM\\SOFTWARE\\Symantec\\Symantec Endpoint Protection\\AV\\Exclusions\\ScanningEngines\\Directory\\Client"
-    if (found_keyscli = registry_enumkeys(keycli))
-      found_keyscli.each do |vals|
-        full = keycli + "\\" + vals
-        values = registry_getvaldata(full, "DirectoryName")
-        print_good "#{values}"
+    paths = []
+    if (admin_exclusion_keys = registry_enumkeys(admin_exclusion_key))
+      admin_exclusion_keys.map do |key|
+        paths << registry_getvaldata("#{admin_exclusion_key}\\#{key}", 'DirectoryName') + ' (admin)'
       end
-    else
-      print_error "No Client Locations Found"
     end
-  end
-
-  def excluded_mssec
-    print_status "Excluded Locations:"
-    keyms = "HKLM\\SOFTWARE\\Microsoft\\Microsoft Antimalware\\Exclusions\\Paths\\"
-    if (found = registry_enumvals(keyms))
-      found.each do |num|
-        print_good "#{num}"
+    if (client_exclusion_keys = registry_enumkeys(client_exclusion_key))
+      client_exclusion_keys.map do |key|
+        paths << registry_getvaldata("#{client_exclusion_key}\\#{key}", 'DirectoryName') + ' (client)'
       end
-    else
-      print_error "No Excluded Locations Found"
     end
+    print_exclusions_table(SEP, paths)
   end
 
   def excluded_defender
-    print_status "Excluded Locations:"
-    keyms = "HKLM\\SOFTWARE\\Microsoft\\Windows Defender\\Exclusions\\Paths\\"
-    if (found = registry_enumvals(keyms))
-      found.each do |num|
-        print_good "#{num}"
-      end
-    else
-      print_error "No Excluded Locations Found"
+    print_exclusions_table(DEFENDER, registry_enumvals("#{DEFENDER_BASE_KEY}\\Exclusions\\Paths"))
+  end
+
+  def excluded_mssec
+    print_exclusions_table(ESSENTIALS, registry_enumvals("#{ESSENTIALS_BASE_KEY}\\Exclusions\\Paths"))
+  end
+
+  def print_exclusions_table(product, exclusions)
+    unless exclusions && !exclusions.empty?
+      print_status("No exclusions for #{product}")
+      return
     end
+    table = Rex::Ui::Text::Table.new(
+      'Header'    => "#{product} excluded paths",
+      'Indent'    => 1,
+      'Columns'   => %w(path)
+    )
+    exclusions.map { |exclusion| table << [exclusion] }
+    print_line(table.to_s)
   end
 
   def setup
+    if sysinfo['Architecture'] =~ /WOW64/
+      fail_with(Failure::BadConfig, 'You are running this module from a 32-bit process on a 64-bit machine. ' \
+                'Migrate to a 64-bit process and try again')
+    end
     unless datastore['DEFENDER'] || datastore['ESSENTIALS'] || datastore['SEP']
       fail_with(Failure::BadConfig, 'Must set one or more of DEFENDER, ESSENTIALS or SEP to true')
     end
   end
 
   def run
-    if sysinfo['Architecture'] =~ /WOW64/
-      print_error "You are running this module from a 32-bit process on a 64-bit machine. Migrate to a 64-bit process and try again"
-      return
-    end
-
     print_status("Enumerating Excluded Paths for AV on #{sysinfo['Computer']}")
+
     found = false
-    if datastore['DEFENDER'] && av_installed?(DEFENDER_BASE_KEY, 'Microsoft Defender')
+    if datastore['DEFENDER'] && av_installed?(DEFENDER_BASE_KEY, DEFENDER)
       found = true
       excluded_defender
     end
-    if datastore['ESSENTIALS'] && av_installed?(ESSENTIALS_BASE_KEY, 'Microsoft Security Essentials / Antimalware')
+    if datastore['ESSENTIALS'] && av_installed?(ESSENTIALS_BASE_KEY, ESSENTIALS)
       found = true
       excluded_mssec
     end
-    if datastore['SEP'] && av_installed?(SEP_BASE_KEY, 'Symantec Endpoint Protection')
+    if datastore['SEP'] && av_installed?(SEP_BASE_KEY, SEP)
       found = true
       excluded_sep
     end
