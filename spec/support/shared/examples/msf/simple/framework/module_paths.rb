a96445b30229aefaf06aa7f7e5a5d7e21e12c765
Greg Mikeska <greg_mikeska@rapid7.com>
2015-12-09 14:31:43 -0600

    switch out expect with a proper mock by allow

--- spec/support/shared/examples/msf/simple/framework/module_paths.rb
+++ spec/support/shared/examples/msf/simple/framework/module_paths.rb
@@ -25,7 +25,7 @@ shared_examples_for 'Msf::Simple::Framework::ModulePaths' do
       # to init_module_paths doesn't get captured.
       framework
 
-      expect(Msf::Config).to receive(:user_module_directory).and_return(user_module_directory)
+      allow(Msf::Config).to receive(:user_module_directory).and_return(user_module_directory)
     end
 
     it 'should refresh module cache from database' do
