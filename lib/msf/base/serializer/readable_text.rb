7e708e31597bda61a87abdd174c92d4905677fc1
Rory McNamara <rmcnamara@gdssecurity.com>
2015-12-14 16:40:27 +0000

    sessions LURI display

--- lib/msf/base/serializer/readable_text.rb
+++ lib/msf/base/serializer/readable_text.rb
@@ -530,6 +530,7 @@ class ReadableText
     columns << 'Id'
     columns << 'Type'
     columns << 'Checkin?' if show_extended
+    columns << 'Local URI' if show_extended
     columns << 'Information'
     columns << 'Connection'
 
@@ -555,6 +556,12 @@ class ReadableText
       if show_extended
         if session.respond_to?(:last_checkin) && session.last_checkin
           row << "#{(Time.now.to_i - session.last_checkin.to_i)}s ago"
+        else
+          row << '?'
+        end
+
+        if !session.exploit_datastore['LURI'].empty?
+          row << " (#{session.exploit_datastore['LURI']})"
         else
           row << '?'
         end
@@ -597,6 +604,7 @@ class ReadableText
       sess_type    = session.type.to_s
       sess_uuid    = session.payload_uuid.to_s
       sess_puid    = session.payload_uuid.respond_to?(:puid_hex) ? session.payload_uuid.puid_hex : nil
+      sess_luri    = session.exploit_datastore['LURI'] || ""
 
       sess_checkin = "<none>"
       sess_machine_id = session.machine_id.to_s
@@ -626,6 +634,9 @@ class ReadableText
       out << "   MachineID: #{sess_machine_id}\n"
       out << "     CheckIn: #{sess_checkin}\n"
       out << "  Registered: #{sess_registration}\n"
+      if !sess_luri.empty?
+        out << "        LURI: #{sess_luri}\n"
+      end
 
 
 
