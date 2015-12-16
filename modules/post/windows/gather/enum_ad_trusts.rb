47e484408f0f13558f17df298512c0733ee4bcd6
Stuart Morgan <stuart.morgan@mwrinfosecurity.com>
2015-12-16 22:31:54 +0000

    rubocop

--- modules/post/windows/gather/enum_ad_trusts.rb
+++ modules/post/windows/gather/enum_ad_trusts.rb
@@ -13,17 +13,17 @@ class Metasploit3 < Msf::Post
 
   def initialize(info = {})
     super(update_info(info,
-      'Name'         => 'Enumerate Active Directory Trusts From Current Domain',
-      'Description'  => %q{
-        This module will enumerate AD trusts from the current domain, including decoding
-        of the remote SIDs. This could be particularly useful when creating golden tickets
-        with a SID history, or just to immediately map the available trusts.
-      },
-      'License'      => MSF_LICENSE,
-      'Platform'     => ['win'],
-      'SessionTypes' => ['meterpreter'],
-      'Author'       => ['Stuart Morgan <stuart.morgan[at]mwrinfosecurity.com>'],
-    ))
+                      'Name'         => 'Enumerate Active Directory Trusts From Current Domain',
+                      'Description'  => %q(
+                        This module will enumerate AD trusts from the current domain, including decoding
+                        of the remote SIDs. This could be particularly useful when creating golden tickets
+                        with a SID history, or just to immediately map the available trusts.
+                                            ),
+                      'License'      => MSF_LICENSE,
+                      'Platform'     => ['win'],
+                      'SessionTypes' => ['meterpreter'],
+                      'Author'       => ['Stuart Morgan <stuart.morgan[at]mwrinfosecurity.com>']
+                     ))
 
     register_options([
       OptInt.new('MAX_SEARCH', [true, 'Maximum number of items.', '500'])
@@ -31,8 +31,8 @@ class Metasploit3 < Msf::Post
   end
 
   def run
-    ldap_fields = ['flatname','cn','securityIdentifier','trustAttributes','trustDirection','trustType','whenCreated','whenChanged','distinguishedName']
-    ldap_names = ['Name','Domain','SID','Attributes','Direction','Trust Type','Created','Changed','DN']
+    ldap_fields = ['flatname', 'cn', 'securityIdentifier', 'trustAttributes', 'trustDirection', 'trustType', 'whenCreated', 'whenChanged', 'distinguishedName']
+    ldap_names = ['Name', 'Domain', 'SID', 'Attributes', 'Direction', 'Trust Type', 'Created', 'Changed', 'DN']
     search_filter = '(objectClass=trustedDomain)'
     max_search = datastore['MAX_SEARCH']
 
@@ -52,7 +52,7 @@ class Metasploit3 < Msf::Post
 
     # Draw the results table with the
     results_table = Rex::Ui::Text::Table.new(
-      'Header'     => "#{num.to_s} Domain Trust" + (num==1)?"":"s",
+      'Header'     => "#{num} Domain Trust" + (num == 1) ? "" : "s",
       'Indent'     => 1,
       'SortIndex'  => -1,
       'Columns'    => ldap_names
@@ -62,22 +62,22 @@ class Metasploit3 < Msf::Post
       row = []
 
       # Go through each of the fields translating each one if necessary
-      result.each_with_index do |field,index|
+      result.each_with_index do |field, index|
         if field.nil?
-            field_value = ''
-            next
+          field_value = ''
+          next
         end
 
-        if index==3 #trustAttributes
-            field_value = translate_trust_attributes(field[:value])
-        elsif index==4 #trustDirection
-            field_value = translate_trust_direction(field[:value])
-        elsif index==5 #trustType
-            field_value = translate_trust_type(field[:value])
-        elsif index==2 #SID
-            field_value = sid_hex_to_string(field[:value])
+        if index == 3 # trustAttributes
+          field_value = translate_trust_attributes(field[:value])
+        elsif index == 4 # trustDirection
+          field_value = translate_trust_direction(field[:value])
+        elsif index == 5 # trustType
+          field_value = translate_trust_type(field[:value])
+        elsif index == 2 # SID
+          field_value = sid_hex_to_string(field[:value])
         else # Just add the raw data
-            field_value = field[:value].to_s
+          field_value = field[:value].to_s
         end
 
         row << field_value
@@ -89,9 +89,7 @@ class Metasploit3 < Msf::Post
 
     # Draw the whole table
     print_line results_table.to_s
-
   end
-
 
   # Translate the trustAttributes parameter
   # https://msdn.microsoft.com/en-us/library/cc223779.aspx
@@ -108,7 +106,7 @@ class Metasploit3 < Msf::Post
     result << 'No TGT Delegation' if val & 0x00000200
     result << 'PIM Trust' if val & 0x00000400
     return '' unless result.nil?
-    return result.join(',')
+    result.join(',')
   end
 
   # Translate the trustDirection parameter
@@ -119,7 +117,7 @@ class Metasploit3 < Msf::Post
     result = 'Inbound' if val == 0x00000001
     result = 'Outbound' if val == 0x00000002
     result = 'Bidirectional' if val == 0x00000003
-    return result
+    result
   end
 
   # Translate the trustType parameter
@@ -130,7 +128,7 @@ class Metasploit3 < Msf::Post
     result = 'Uplevel (AD)' if val == 0x00000002
     result = 'MIT (Not Windows)' if val == 0x00000003
     result = 'DCE (Historic)' if val == 0x00000004
-    return result
+    result
   end
 
   # Convert the SID from Hex to printable string.
@@ -140,21 +138,21 @@ class Metasploit3 < Msf::Post
   #  Bytes [3-9]: Identifier Authority - convert to hex as the second number group.
   #  The rest: A variable length list of unsigned 32bit integers, the number of which is defined in byte 2.
   #  i.e. S-[1]-[3-9]-[10+] < the number of '10+' groups is defined by [2]
-  def sid_hex_to_string(sidhex)
-  sid = []
-  sid << data[0].to_s
-  rid = ''
-  (6).downto(1) do |i|
-    rid += byte2hex(data[i,1][0])
-  end
-  sid << rid.to_i.to_s
-  sid += data.unpack("bbbbbbbbV*")[8..-1]
-  "S-" + sid.join('-')
- end
- def byte2hex(b)
-  ret = '%x' % (b.to_i & 0xff)
-  ret = '0' + ret if ret.length < 2
-  ret
+  def sid_hex_to_string(_sidhex)
+    sid = []
+    sid << data[0].to_s
+    rid = ''
+    (6).downto(1) do |i|
+      rid += byte2hex(data[i, 1][0])
+    end
+    sid << rid.to_i.to_s
+    sid += data.unpack("bbbbbbbbV*")[8..-1]
+    "S-" + sid.join('-')
  end
 
+  def byte2hex(b)
+    ret = '%x' % (b.to_i & 0xff)
+    ret = '0' + ret if ret.length < 2
+    ret
+  end
 end
