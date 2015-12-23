cf62218139d34f8a6182424e70ff08a4e215d75b
RageLtMan <rageltman [at] sempervictus>
2015-12-23 01:04:20 -0500

    Update payload sizes

--- modules/payloads/stagers/windows/bind_tcp_rc4.rb
+++ modules/payloads/stagers/windows/bind_tcp_rc4.rb
@@ -12,7 +12,7 @@ require 'msf/core/payload/windows/bind_tcp_rc4'
 
 module MetasploitModule
 
-  CachedSize = 402
+  CachedSize = 404
 
   include Msf::Payload::Stager
   include Msf::Payload::Windows::BindTcpRc4
