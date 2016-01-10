b7e3d97935edf3cadd97da8bec2324649c5a26b3
Christopher Moore <chrisdcmoore@live.com>
2016-01-10 17:12:41 +0000

    remove extra spacing

--- lib/net/ssh/transport/kex.rb
+++ lib/net/ssh/transport/kex.rb
@@ -8,8 +8,8 @@ module Net::SSH::Transport
     # Maps the supported key-exchange algorithms as named by the SSH protocol
     # to their corresponding implementors.
     MAP = {
-      'diffie-hellman-group-exchange-sha1'   => DiffieHellmanGroupExchangeSHA1,
-      'diffie-hellman-group1-sha1'           => DiffieHellmanGroup1SHA1,
+      'diffie-hellman-group-exchange-sha1' => DiffieHellmanGroupExchangeSHA1,
+      'diffie-hellman-group1-sha1'         => DiffieHellmanGroup1SHA1,
     }
     if defined?(DiffieHellmanGroupExchangeSHA256)
       MAP['diffie-hellman-group-exchange-sha256'] = DiffieHellmanGroupExchangeSHA256
