4653c271672cd54c76d6db9e187ec466393447c2
Josh Hale <jhale85446@gmail.com>
2016-02-09 21:24:40 -0600

    Fix minor grammar error in description

--- modules/post/windows/manage/priv_migrate.rb
+++ modules/post/windows/manage/priv_migrate.rb
@@ -20,7 +20,7 @@ class Metasploit3 < Msf::Post
          It will do everything it can to migrate, including spawing a new User level process.
          For sessions with Admin rights: It will try to migrate into a System level process in the following
          order: ANAME (if specified), services.exe, winlogon.exe, wininit.exe, lsm.exe, and lsass.exe.
-         If al these fail, it will fall back to User level migration. For sessions with User level rights:
+         If all these fail, it will fall back to User level migration. For sessions with User level rights:
          It will try to migrate to a user level process, if that fails it will attempt to spawn the process
          then migrate to it. It will attempt the User level processes in the following order:
          NAME (if specified), explorer.exe, then notepad.exe.},
