15f6992aec1329ac9c76101ab717ae3b7145ec18
nixawk <hap.ddup@gmail.com>
2016-02-18 00:03:28 +0800

    add yahoo_search_domain(domain) / yahoo_search_ip(ip)

--- modules/auxiliary/gather/yahoo_subdomains_collector.rb
+++ modules/auxiliary/gather/yahoo_subdomains_collector.rb
@@ -21,7 +21,8 @@ class Metasploit4 < Msf::Auxiliary
 
     register_options(
       [
-        OptString.new('DOMAIN', [ true, "The domain name to locate subdomains for"])
+        OptString.new('TARGET', [ true, "The target to locate subdomains for, ex: rapid7.com, 8.8.8.8"]),
+        OptBool.new('IP_SEARCH', [ false, "Enable ip of subdomains to locate subdomains", true])
       ], self.class)
 
     deregister_options('RHOST', 'RPORT', 'VHOST', 'SSL', 'Proxies')
@@ -35,13 +36,22 @@ class Metasploit4 < Msf::Auxiliary
     80
   end
 
-  def yahoo_search(domain)
-    print_status("Searching Yahoo for subdomains from #{domain}")
-    domains = {}
+  def valid?(ip, domain)
+    begin
+      ips = Rex::Socket.getaddresses(domain)
+      true if ips.include?(ip)
+    rescue SocketError
+    ensure
+      false
+    end
+  end
 
+  def yahoo_search(dork)
+    print_status("Searching Yahoo for subdomains from #{dork}")
+    results = []
     searches = ["1", "101", "201", "301", "401", "501"]
     searches.each do |num|
-      resp = send_request_cgi(
+      resp = send_request_cgi!(
         'rhost' => rhost_yahoo,
         'rport' => rport_yahoo,
         'vhost' => rhost_yahoo,
@@ -49,7 +59,7 @@ class Metasploit4 < Msf::Auxiliary
         'uri' => '/search',
         'vars_get' => {
           'pz' => 100,
-          'p' => "domain:#{domain}",
+          'p' => dork,
           'b' => num
         })
 
@@ -57,29 +67,69 @@ class Metasploit4 < Msf::Auxiliary
       html = resp.get_html_document
       matches = html.search('span[@class=" fz-15px fw-m fc-12th wr-bw lh-15"]')
       matches.each do |match|
-        subdomain = match.text.split('/')[0]
-        subdomain = subdomain.split(':')[0]
-        subdomain.downcase!
-
-        next if domains.include?(subdomain)
-        next unless subdomain.include?(domain)
-        ips = Rex::Socket.getaddresses(subdomain)
-        ips.each do |ip|
-          report_host(host: ip, name: subdomain)
-        end
-        domains[subdomain] = ips
-        print_good("#{domain} subdomain: #{subdomain} - #{ips.join(',')}")
+        result = match.text
+        result = result.split('/')[0]
+        result = result.split(':')[0]
+        next unless result
+        result.to_s.downcase!
+        results << result
       end
     end
+    results
+  end
+
+  def yahoo_search_domain(domain)
+    domains = {}
+    dork = "domain:#{domain}"
+    results = yahoo_search(dork)
+    results.each do |subdomain|
+      next if domains.include?(subdomain)
+      next unless subdomain.include?(domain)
+      ips = Rex::Socket.getaddresses(subdomain)
+      ips.each do |ip|
+        report_host(host: ip, name: subdomain)
+        print_good("#{dork} subdomain: #{subdomain} - #{ip}")
+        yahoo_search_ip(ip) if datastore['IP_SEARCH']
+      end
+      domains[subdomain] = ips
+    end
+
+    return unless domains
     report_note(
       host: domain,
-      type: 'Yahoo Search Engine Subdomains Collector',
+      type: 'Yahoo Search Subdomains',
+      update: :unique_data,
+      data: domains)
+    domains
+  end
+
+  def yahoo_search_ip(ip)
+    dork = "ip:#{ip}"
+    domains = {}
+
+    results = yahoo_search(dork)
+    results.each do |subdomain|
+      next if domains.include?(subdomain)
+      next unless valid?(ip, subdomain)
+      report_host(host: ip, name: subdomain)
+      print_good("#{dork} subdomain: #{subdomain}")
+      domains[subdomain] = ip
+    end
+    return unless domains
+    report_note(
+      host: ip,
+      type: 'Yahoo Search Subdomains',
       update: :unique_data,
       data: domains)
     domains
   end
 
   def run
-    yahoo_search(datastore['domain'])
+    target = datastore['TARGET']
+    if Rex::Socket.is_ipv4?(target)
+      yahoo_search_ip(target)
+    else
+      yahoo_search_domain(target)
+    end
   end
 end
