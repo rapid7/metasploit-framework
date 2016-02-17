091cdccccfefde6d4f231f7597debfd4a81474dc
mo khan <mo@mokhan.ca>
2016-02-16 21:46:44 -0700

    add database.yml and install missing dependencies.

--- scripts/shell/provision.rb
+++ scripts/shell/provision.rb
@@ -2,34 +2,42 @@ execute "apt-get update -y"
 execute "apt-get upgrade -y"
 
 package [
+  "autoconf",
+  "bison",
   "build-essential",
   "curl",
   "git-core",
+  "libapr1",
+  "libaprutil1",
   "libcurl4-openssl-dev",
   "libffi-dev",
+  "libgmp3-dev",
+  "libpcap-dev",
+  "libpq-dev",
   "libreadline-dev",
+  "libreadline6-dev",
   "libsqlite3-dev",
   "libssl-dev",
+  "libsvn1",
+  "libtool",
+  "libxml2",
   "libxml2-dev",
+  "libxslt-dev",
   "libxslt1-dev",
   "libyaml-dev",
+  "locate",
+  "ncurses-dev",
+  "openssl",
+  "postgresql",
+  "postgresql-contrib",
   "python-software-properties",
   "sqlite3",
+  "vim",
+  "wget",
+  "xsel",
+  "zlib1g",
   "zlib1g-dev",
 ]
-
-bash "install postgres" do
-  user "root"
-  not_if { ::File.exist?("/etc/apt/sources.list.d/pgdg.list") }
-  code <<-SCRIPT
-    echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
-    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc \
-      | apt-key add -
-    apt-get update -y
-    apt-get install -y postgresql-9.4 libpq-dev \
-      postgresql-contrib-9.4 postgresql-client-common
-  SCRIPT
-end
 
 sql = "SELECT 1 FROM pg_roles WHERE rolname='vagrant'"
 create_user = "createuser -s -e -w vagrant"
@@ -41,6 +49,35 @@ sql = "SELECT 1 FROM pg_roles WHERE rolname='vagrant'"
 execute "createdb" do
   user "vagrant"
   not_if { "psql postgres -tAc \"#{sql}\" | grep -q 1" }
+end
+
+file "/vagrant/.msf4/database.yml" do
+  content <<-EOH
+# Development Database
+development: &pgsql
+  adapter: postgresql
+  database: msf_dev_db
+  username: vagrant
+  host: localhost
+  port: 5432
+  pool: 5
+  timeout: 5
+
+# Production database -- same as dev
+production: &production
+  <<: *pgsql
+
+# Test database -- not the same, since it gets dropped all the time
+test:
+  <<: *pgsql
+  database: msf_test_db
+EOH
+end
+
+["postgresql"].each do |service_name|
+  service service_name do
+    action [:enable, :start]
+  end
 end
 
 git "/usr/local/rbenv" do
@@ -77,10 +114,4 @@ bash "install_bundler" do
 source /etc/profile.d/rbenv.sh
 gem install bundler --no-ri --no-rdoc
   EOH
-end
-
-["postgresql"].each do |service_name|
-  service service_name do
-    action [:enable, :start]
-  end
 end
