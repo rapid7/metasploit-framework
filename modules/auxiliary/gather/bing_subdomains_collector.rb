f21884a82453fefd02f2feccdd0134e373cad12e
nixawk <hap.ddup@gmail.com>
2016-02-17 18:12:25 +0800

    add bing_search_domain / bing_search_ip

--- modules/auxiliary/gather/bing_subdomains_collector.rb
+++ modules/auxiliary/gather/bing_subdomains_collector.rb
@@ -21,24 +21,41 @@ class Metasploit4 < Msf::Auxiliary
 
     register_options(
       [
-        OptString.new('DOMAIN', [ true, "The domain name to locate subdomains for"])
+        OptString.new('TARGET', [ true, "The target to locate subdomains for, ex: rapid7.com, 8.8.8.8"]),
+        OptBool.new('IP_SEARCH', [ false, "Enable ip of subdomains to locate subdomains", true])
       ], self.class)
 
     deregister_options('RHOST', 'RPORT', 'VHOST', 'SSL', 'Proxies')
   end
 
   def rhost_bing
-    'www.bing.com'
+    'global.bing.com'
   end
 
   def rport_bing
     80
   end
 
-  def bing_search(domain)
-    print_status("Searching Bing for subdomains from #{domain}")
-    domains = {}
+  def valid?(ip, domain)
+    begin
+      ips = Rex::Socket.getaddresses(domain)
+      true if ips.include?(ip)
+    rescue SocketError
+    ensure
+      false
+    end
+  end
 
+  def uri2domain(uri)
+    begin
+      URI(uri).host
+    rescue URI::InvalidURIError
+      nil
+    end
+  end
+
+  def bing_search(dork)
+    results = []
     searches = ['1', '51', '101', '151', '201', '251', '301', '351', '401', '451']
     searches.each do |num|
       resp = send_request_cgi!(
@@ -48,27 +65,42 @@ class Metasploit4 < Msf::Auxiliary
         'method' => 'GET',
         'uri' => '/search',
         'vars_get' => {
-          'first' => num,
-          'q' => "domain:#{domain}"
+          'FROM'     => 'HPCNEN',
+          'setmkt'   => 'en-us',
+          'setlang'  => 'en-us',
+          'first'    => num,
+          'q'        => dork
         })
 
       next unless resp && resp.code == 200
       html = resp.get_html_document
       matches = html.search('cite')
       matches.each do |match|
-        subdomain = match.text.split('/')[0]
-        subdomain = subdomain.split(':')[0]
-        subdomain.downcase!
-
-        next if domains.include?(subdomain)
-        next unless subdomain.include?(domain)
-        ips = Rex::Socket.getaddresses(subdomain)
-        ips.each do |ip|
-          report_host(host: ip, name: subdomain)
-        end
-        domains[subdomain] = ips
-        print_good("#{domain} subdomain: #{subdomain} - #{ips.join(',')}")
+        result = uri2domain(match.text)
+        next unless result
+        result.to_s.downcase!
+        results << result
       end
+    end
+    results
+  end
+
+  def bing_search_domain(domain)
+    print_status("Searching Bing for subdomains from #{domain}")
+    dork = "domain:#{domain}"
+    domains = {}
+
+    results = bing_search(dork)
+    results.each do |subdomain|
+      next if domains.include?(subdomain)
+      next unless subdomain.include?(domain)
+      ips = Rex::Socket.getaddresses(subdomain)
+      ips.each do |ip|
+        report_host(host: ip, name: subdomain)
+        print_good("#{dork} subdomain: #{subdomain} - #{ip}")
+        bing_search_ip(ip) if datastore['IP_SEARCH']
+      end
+      domains[subdomain] = ips
     end
 
     return unless domains
@@ -80,7 +112,34 @@ class Metasploit4 < Msf::Auxiliary
     domains
   end
 
+  def bing_search_ip(ip)
+    print_status("Searching Bing for subdomains from #{ip}")
+    dork = "ip:#{ip}"
+    domains = {}
+
+    results = bing_search(dork)
+    results.each do |subdomain|
+      next if domains.include?(subdomain)
+      next unless valid?(ip, subdomain)
+      report_host(host: ip, name: subdomain)
+      print_good("#{dork} suddomain: #{subdomain}")
+      domains[subdomain] = ip
+    end
+    return unless domains
+    report_note(
+      host: ip,
+      type: 'Bing Search Subdomains',
+      update: :unique_data,
+      data: domains)
+    domains
+  end
+
   def run
-    bing_search(datastore['domain'])
+    target = datastore['TARGET']
+    if Rex::Socket.is_ipv4?(target)
+      bing_search_ip(target)
+    else
+      bing_search_domain(target)
+    end
   end
 end
