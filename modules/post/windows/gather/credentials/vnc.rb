bfd204ac502015450583cdb749b64b0d4b6ffd59
William Vu <William_Vu@rapid7.com>
2016-02-19 15:00:56 -0600

    Fix some cosmetic issues

--- modules/post/windows/gather/credentials/vnc.rb
+++ modules/post/windows/gather/credentials/vnc.rb
@@ -111,13 +111,13 @@ class Metasploit3 < Msf::Post
     #check uninstall key
     begin
       root_key, base_key = session.sys.registry.splitkey("HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Ultravnc2_is1")
-      open_key = session.sys.registry.open_key(root_key,base_key,KEY_READ)
+      open_key = session.sys.registry.open_key(root_key, base_key, KEY_READ)
       vnclocation = open_key.query_value("InstallLocation").data
       locations << {:name => 'UltraVNC',
-          :check_file => vnclocation + "\\ultravnc.ini",
-          :pass_variable => 'passwd=',
-          :viewonly_variable => 'passwd2=',
-          :port_variable => 'PortNumber='}
+        :check_file => vnclocation + "\\ultravnc.ini",
+        :pass_variable => 'passwd=',
+        :viewonly_variable => 'passwd2=',
+        :port_variable => 'PortNumber='}
     rescue Rex::Post::Meterpreter::RequestError => e
       vprint_error(e.message)
     end
