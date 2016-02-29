d955c6a8f6b5643767fea5f961b2eb4b2076453d
Brent Cook <bcook@rapid7.com>
2016-02-29 14:06:49 -0600

    style fixes

--- modules/post/multi/manage/set_wallpaper.rb
+++ modules/post/multi/manage/set_wallpaper.rb
@@ -6,22 +6,24 @@
 require 'msf/core'
 
 class Metasploit3 < Msf::Post
-
   include Msf::Post::File
   include Msf::Post::Windows::Registry
 
-  def initialize(info={})
-    super( update_info( info,
-      'Name'          => 'Multi Manage Set Wallpaper',
-      'Description'   => %q{
-        This module will set the desktop wallpaper background on the specified session.
-        The method of setting the wallpaper depends on the platform type.
-      },
-      'License'       => MSF_LICENSE,
-      'Author'        => [ 'timwr'],
-      'Platform'      => [ 'win', 'osx', 'linux', 'android' ],
-      'SessionTypes'  => [ 'meterpreter' ]
-    ))
+  def initialize(info = {})
+    super(
+      update_info(
+        info,
+        'Name'          => 'Multi Manage Set Wallpaper',
+        'Description'   => %q(
+          This module will set the desktop wallpaper background on the specified session.
+          The method of setting the wallpaper depends on the platform type.
+        ),
+        'License'       => MSF_LICENSE,
+        'Author'        => [ 'timwr'],
+        'Platform'      => [ 'win', 'osx', 'linux', 'android' ],
+        'SessionTypes'  => [ 'meterpreter' ]
+      )
+    )
 
     register_options(
       [
@@ -29,22 +31,21 @@ class Metasploit3 < Msf::Post
       ], self.class)
   end
 
-  def upload_wallpaper(tempdir)
-    wallpaper_file = datastore["WALLPAPER_FILE"]
-    remote_file = "#{tempdir}#{File.basename(wallpaper_file)}"
+  def upload_wallpaper(tempdir, file)
+    remote_file = "#{tempdir}#{File.basename(file)}"
     print_status("#{peer} - Uploading to #{remote_file}")
-    localfile = File.open(wallpaper_file, "rb") {|fd| fd.read(fd.stat.size) }
-    write_file(remote_file, localfile)
+
+    write_file(remote_file, File.binread(file))
     print_status("#{peer} - Uploaded to #{remote_file}")
     remote_file
   end
 
   #
-  # The OSX version uses an apple script to do this
+  # The OS X version uses an AppleScript to do this
   #
-  def osx_set_wallpaper
-    remote_file = upload_wallpaper("/tmp/")
-    script =  %Q|osascript -e 'tell application "Finder" to set desktop picture to POSIX file "#{remote_file}"' |
+  def osx_set_wallpaper(file)
+    remote_file = upload_wallpaper("/tmp/", file)
+    script = %(osascript -e 'tell application "Finder" to set desktop picture to POSIX file "#{remote_file}"')
     begin
       cmd_exec(script)
     rescue EOFError
@@ -54,42 +55,38 @@ class Metasploit3 < Msf::Post
   end
 
   #
-  # The Windows version uses the SystemParametersInfo
+  # The Windows version uses the SystemParametersInfo call
   #
-  def win_set_wallpaper(id)
-    remote_file = upload_wallpaper("%TEMP%\\")
-    client.railgun.user32.SystemParametersInfoA(0x0014,nil,remote_file,0x2)
-    true
+  def win_set_wallpaper(file)
+    remote_file = upload_wallpaper("%TEMP%\\", file)
+    client.railgun.user32.SystemParametersInfoA(0x0014, nil, remote_file, 0x2) != 0
   end
 
   #
   # The Android version uses the set_wallpaper command
   #
-  def android_set_wallpaper(id)
-    wallpaper_file = datastore["WALLPAPER_FILE"]
-    local_file = File.open(wallpaper_file, "rb") {|fd| fd.read(fd.stat.size) }
-    client.android.set_wallpaper(local_file)
+  def android_set_wallpaper(file)
+    client.android.set_wallpaper(File.binread(file))
     true
   end
 
-  def set_wallpaper(id)
-    case session.platform
+  def os_set_wallpaper(file)
+    case platform
     when /osx/
-      osx_set_wallpaper(id)
+      osx_set_wallpaper(file)
     when /win/
-      win_set_wallpaper(id)
+      win_set_wallpaper(file)
     when /android/
-      android_set_wallpaper(id)
+      android_set_wallpaper(file)
     end
   end
 
   def run
     file = datastore['WALLPAPER_FILE']
-    if set_wallpaper(file)
+    if os_set_wallpaper(file)
       print_good("#{peer} - The wallpaper has been set")
     else
       print_error("#{peer} - Unable to set the wallpaper")
     end
   end
-
 end
