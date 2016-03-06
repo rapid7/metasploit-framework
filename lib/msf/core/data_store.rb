694f7f0a657e0951ef5cc2f17f9ed35ef88513d4
Brent Cook <bcook@rapid7.com>
2016-03-05 23:09:14 -0600

    stop turning all default options into strings
    we need to adjust vprint* functions, since they now fallthrough to the
    'framework.datastore' checks because the false case actually triggers.

--- lib/msf/core/data_store.rb
+++ lib/msf/core/data_store.rb
@@ -66,14 +66,9 @@ class DataStore < Hash
   #
   def import_options(options, imported_by = nil, overwrite = false)
     options.each_option { |name, opt|
-      # If there's already a value defined for this option, then skip it
-      # and don't import it.
-      next if self.has_key?(name) and overwrite == false
-
-      # If the option has a default value, import it, but only if the
-      # datastore doesn't already have a value set for it.
-      if ((opt.default != nil) and (overwrite or self[name] == nil))
-        import_option(name, opt.default.to_s, true, imported_by)
+      # Skip options without a default or if is already a value defined
+      if !opt.default.nil? && (!self.has_key?(name) || overwrite)
+        import_option(name, opt.default, true, imported_by)
       end
     }
   end
