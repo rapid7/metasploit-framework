80e0bbeb68ae7510c3437f8013396296089fda71
OJ <oj@buffered.io>
2016-03-14 20:23:26 +1000

    Add the interactive shell prompt with sessions

--- lib/rex/post/meterpreter/ui/console/command_dispatcher/powershell.rb
+++ lib/rex/post/meterpreter/ui/console/command_dispatcher/powershell.rb
@@ -29,16 +29,52 @@ class Console::CommandDispatcher::Powershell
   #
   def commands
     {
-      'powershell_execute'            => 'Execute a Powershell command string',
+      'powershell_shell'    => 'Create an interactive Powershell prompt',
+      'powershell_execute'  => 'Execute a Powershell command string'
     }
   end
 
+  @@powershell_shell_opts = Rex::Parser::Arguments.new(
+    '-s' => [true, 'Specify the id/name of the Powershell session to interact with.'],
+    '-h' => [false, 'Help banner']
+  )
+
+  def powershell_shell_usage
+    print_line('Usage: powershell_shell [-s session-id]')
+    print_line
+    print_line('Creates an interactive Powershell prompt.')
+    print_line(@@powershell_shell_opts.usage)
+  end
+
+  #
+  # Create an interactive powershell prompts
+  #
+  def cmd_powershell_shell(*args)
+    if args.include?('-h')
+      powershell_shell_usage
+      return false
+    end
+
+    opts = {}
+
+    @@powershell_shell_opts.parse(args) { |opt, idx, val|
+      case opt
+      when '-s'
+        opts[:session_id] = val
+      end
+    }
+
+    channel = client.powershell.shell(opts)
+    shell.interact_with_channel(channel)
+  end
+
   @@powershell_execute_opts = Rex::Parser::Arguments.new(
+    '-s' => [true, 'Specify the id/name of the Powershell session to run the command in.'],
     '-h' => [false, 'Help banner']
   )
 
   def powershell_execute_usage
-    print_line('Usage: powershell_execute <powershell code>')
+    print_line('Usage: powershell_execute <powershell code> [-s session-id]')
     print_line
     print_line('Runs the given Powershell string on the target.')
     print_line(@@powershell_execute_opts.usage)
@@ -53,16 +89,18 @@ class Console::CommandDispatcher::Powershell
       return false
     end
 
-    code = args.shift
-
-    @@powershell_execute_opts.parse(args) { |opt, idx, val|
-      #case opt
-      #when '-r'
-      #  result_var = val
-      #end
+    opts = {
+      code: args.shift
     }
 
-    result = client.powershell.execute_string(code)
+    @@powershell_execute_opts.parse(args) { |opt, idx, val|
+      case opt
+      when '-s'
+        opts[:session_id] = val
+      end
+    }
+
+    result = client.powershell.execute_string(opts)
     print_good("Command execution completed:\n#{result}")
   end
 
