98355c397c4a569cde36d7a1cf4c78e6a588a56a
James Lee <egypt@metasploit.com>
2016-03-23 15:07:00 -0500

    Clean up some variable names

--- lib/rex/post/meterpreter/channels/socket_abstraction.rb
+++ lib/rex/post/meterpreter/channels/socket_abstraction.rb
@@ -38,17 +38,17 @@ module SocketAbstraction
         csock = csock.channel.client.sock
         hops += 1
       end
-      tmp,caddr,cport = csock.getsockname
-      tmp,raddr,rport = csock.getpeername
-      maddr,mport = [ channel.params.localhost, channel.params.localport ]
-      [ tmp, "#{caddr}#{(hops > 0) ? "-_#{hops}_" : ""}-#{raddr}", "#{mport}" ]
+      _address_family,caddr,_cport = csock.getsockname
+      address_family,raddr,_rport = csock.getpeername
+      _maddr,mport = [ channel.params.localhost, channel.params.localport ]
+      [ address_family, "#{caddr}#{(hops > 0) ? "-_#{hops}_" : ""}-#{raddr}", "#{mport}" ]
     end
 
     def getpeername
       return super if not channel
-      tmp,caddr,cport = channel.client.sock.getpeername
+      address_family,_caddr,_cport = channel.client.sock.getpeername
       maddr,mport = [ channel.params.peerhost, channel.params.peerport ]
-      [ tmp, "#{maddr}", "#{mport}" ]
+      [ address_family, "#{maddr}", "#{mport}" ]
     end
 
     %i{localhost localport peerhost peerport}.map do |meth|
