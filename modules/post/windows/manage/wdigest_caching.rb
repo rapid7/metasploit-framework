397d5580be0e44ea5bb1ecd8c0979ab1a7d7c862
Meatballs <eat_meatballs@hotmail.co.uk>
2016-03-30 15:44:37 +0100

    Use MetasploitModule convention

--- modules/post/windows/manage/wdigest_caching.rb
+++ modules/post/windows/manage/wdigest_caching.rb
@@ -2,7 +2,7 @@ require 'msf/core'
 require 'rex'
 require 'msf/core/auxiliary/report'
 
-class Metasploit3 < Msf::Post
+class MetasploitModule < Msf::Post
   include Msf::Post::Windows::Registry
 
   WDIGEST_REG_LOCATION = 'HKLM\\SYSTEM\\CurrentControlSet\\Control\\SecurityProviders\\WDigest'
