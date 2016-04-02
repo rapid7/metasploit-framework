992df12fa7ff2314961d2f37dafda602abc30174
RageLtMan <rageltman [at] sempervictus>
2016-04-02 00:22:26 -0400

    Address ActiveRecord deprecation warning
    AR will start to complain about eager loading in command_dispatcher
    /db.rb:519 because it references hosts as string without explicitly
    stating that the table is being referenced.
    
    Add a call .references in the AR call chain after the where clause
    to silence this abysmal warning.

--- lib/msf/ui/console/command_dispatcher/db.rb
+++ lib/msf/ui/console/command_dispatcher/db.rb
@@ -463,7 +463,7 @@ class Db
         if search_term
           next unless (
             host.attribute_names.any? { |a| host[a.intern].to_s.match(search_term) } ||
-            !Mdm::Tag.includes(:hosts).where("hosts.workspace_id = ? and hosts.address = ? and tags.name = ?", framework.db.workspace.id, host.address, search_term.source).order("tags.id DESC").empty?
+            !Mdm::Tag.includes(:hosts).where("hosts.workspace_id = ? and hosts.address = ? and tags.name = ?", framework.db.workspace.id, host.address, search_term.source).references(:hosts).order("tags.id DESC").empty?
           )
         end
 
