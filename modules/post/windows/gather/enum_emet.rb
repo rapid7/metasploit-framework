16ff74e293aad42f72849ebef3df773a065242bb
join-us <hap.ddup@gmail.com>
2016-04-22 10:53:03 +0800

    syntax check / code reduce

--- modules/post/windows/gather/enum_emet.rb
+++ modules/post/windows/gather/enum_emet.rb
@@ -8,39 +8,28 @@ require 'rex'
 require 'msf/core/post/common'
 
 class MetasploitModule < Msf::Post
-
   include Msf::Post::Windows::Registry
   include Msf::Post::Common
 
-  def initialize(info={})
-    super( update_info( info,
-        'Name'          => 'Windows Gather EMET Protected Paths',
-        'Description'   => %q{ This module will enumerate the EMET protected paths on the target host.},
-        'License'       => MSF_LICENSE,
-        'Author'        => [ 'vysec <vincent.yiu[at]mwrinfosecurity.com>'],
-        'Platform'      => [ 'win' ],
-        'SessionTypes'  => [ 'meterpreter' ]
-      ))
-
+  def initialize(info = {})
+    super(update_info(info,
+      'Name'          => 'Windows Gather EMET Protected Paths',
+      'Description'   => %q( This module will enumerate the EMET protected paths on the target host.),
+      'License'       => MSF_LICENSE,
+      'Author'        => [ 'vysec <vincent.yiu[at]mwrinfosecurity.com>' ],
+      'Platform'      => [ 'win' ],
+      'SessionTypes'  => [ 'meterpreter' ]
+    ))
   end
 
- def run
-    if sysinfo['Architecture'] =~ /x64/
-        reg_vals = registry_enumvals('HKLM\\SOFTWARE\\Microsoft\\EMET\\AppSettings',REGISTRY_VIEW_64_BIT)
+  def run
+    reg_view = sysinfo['Architecture'] =~ /x64/ ? REGISTRY_VIEW_64_BIT : REGISTRY_VIEW_32_BIT
+    reg_vals = registry_enumvals('HKLM\\SOFTWARE\\Microsoft\\EMET\\AppSettings', reg_view)
+    if reg_vals.nil?
+      print_status('Failed to enumerate EMET Protected.')
     else
-      reg_vals = registry_enumvals('HKLM\\SOFTWARE\\Microsoft\\EMET\\AppSettings',REGISTRY_VIEW_32_BIT)
-    end 
-    
-    t = ""
-
-    reg_vals.each do |x|
-        t << "#{x}\r\n"
+      path = store_loot('host.emet_paths', 'text/plain', session, reg_vals.join("\r\n"), 'emet_paths.txt', 'EMET Paths')
+      print_good("Results stored in: #{path}")
     end
-
-    puts t
-
-    p = store_loot("host.emet_paths", "text/plain", session, t, "emet_paths.txt", "EMET Paths")
-    print_status("Results stored in: #{p}")
   end
-
 end
