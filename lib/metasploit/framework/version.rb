7490ab1c78f353009589bb2e1a3f94443318fa1f
Jenkins <jenkins@rapid7.com>
2016-05-03 17:09:07 -0700

    Bump version of framework to 4.11.25

--- lib/metasploit/framework/version.rb
+++ lib/metasploit/framework/version.rb
@@ -30,7 +30,7 @@ module Metasploit
         end
       end
 
-      VERSION = "4.11.24"
+      VERSION = "4.11.25"
       MAJOR, MINOR, PATCH = VERSION.split('.').map { |x| x.to_i }
       PRERELEASE = 'dev'
       HASH = get_hash
