a69432abe51b4205c3d7075083d6a11399dfeaf5
Brent Cook <bcook@rapid7.com>
2016-05-12 12:42:04 -0500

    update module class and move to recon from manage

--- modules/post/multi/recon/multiport_egress_traffic.rb
+++ modules/post/multi/recon/multiport_egress_traffic.rb
@@ -8,7 +8,7 @@
 require 'msf/core'
 require 'rex'
 
-class Metasploit3 < Msf::Post
+class MetasploitModule < Msf::Post
   def initialize(info = {})
     super(update_info(info,
                       'Name'          => 'Generate TCP/UDP Outbound Traffic On Multiple Ports',
