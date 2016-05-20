a71e853c2a7bad60ceed050afd4fc27cb76afb65
root <nodgesoft@gmail.com>
2016-05-20 02:32:37 +0000

    Fixed cache size for python/shell_reverse_tcp_ssl

--- modules/payloads/singles/python/shell_reverse_tcp_ssl.rb
+++ modules/payloads/singles/python/shell_reverse_tcp_ssl.rb
@@ -10,7 +10,7 @@ require 'msf/base/sessions/command_shell_options'
 
 module MetasploitModule
 
-  CachedSize = 557
+  CachedSize = 561
 
   include Msf::Payload::Single
   include Msf::Sessions::CommandShellOptions
