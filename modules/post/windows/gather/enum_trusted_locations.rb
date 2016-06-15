8a68e86a0a6d0a95623fb2ae44d97da7dc5054cb
Vincent Yiu <vysec@users.noreply.github.com>
2016-06-15 13:42:38 +0100

    Update enum_trusted_locations.rb
    Changed some colours

--- modules/post/windows/gather/enum_trusted_locations.rb
+++ modules/post/windows/gather/enum_trusted_locations.rb
@@ -37,7 +37,7 @@ class MetasploitModule < Msf::Post
       print_status('Failed to enumerate Office.')
     else
       print_status('')
-      print_status('Found Office:')
+      print_status('Found Office.')
       #find version to use
       reg_keys.each do |path|
         if not /[0-9][0-9].0/.match(path).nil?
@@ -55,7 +55,8 @@ class MetasploitModule < Msf::Post
                   val2 = path2
                   reg_keys3 = registry_enumkeys("HKCU\\SOFTWARE\\Microsoft\\Office\\#{val1}\\#{val2}\\Security\\Trusted Locations", reg_view)
                   if not reg_keys3.nil?
-                     print_status("Found trusted locations in #{val2}")
+                  	 print_status('')
+                     print_good("Found trusted locations in #{val2}")
                      #find version to use
                      reg_keys3.each do |path3|
                         val3 = path3
