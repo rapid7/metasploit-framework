1cb01ee876390ca72c1a6c465f00119437ff1f09
Brent Cook <bcook@rapid7.com>
2016-08-10 14:46:48 -0500

    remove architecture fidling from platform string for now

--- lib/msf/base/sessions/meterpreter.rb
+++ lib/msf/base/sessions/meterpreter.rb
@@ -326,9 +326,7 @@ class Meterpreter < Rex::Post::Meterpreter::Client
     username = self.sys.config.getuid
     sysinfo  = self.sys.config.sysinfo
 
-    self.platform =
-      self.sys.config.sysinfo["Architecture"].downcase + '/' +
-      self.platform.split('/')[0] +'/' +
+    self.platform = self.platform.split('/')[0] + '/' +
       case self.sys.config.sysinfo['OS']
       when /windows/i
         Msf::Module::Platform::Windows
