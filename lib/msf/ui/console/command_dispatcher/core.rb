1b31e0a63e3ed5355bf4c7f187ca2ec83dd183a3
Brent Cook <bcook@rapid7.com>
2016-09-20 14:27:59 -0500

    remove osvdb links

--- lib/msf/ui/console/command_dispatcher/core.rb
+++ lib/msf/ui/console/command_dispatcher/core.rb
@@ -1615,7 +1615,6 @@ class Core
       'cve'      => 'Modules with a matching CVE ID',
       'edb'      => 'Modules with a matching Exploit-DB ID',
       'name'     => 'Modules with a matching descriptive name',
-      'osvdb'    => 'Modules with a matching OSVDB ID',
       'platform' => 'Modules affecting this platform',
       'ref'      => 'Modules with a matching ref',
       'type'     => 'Modules of a specific type (exploit, auxiliary, or post)',
