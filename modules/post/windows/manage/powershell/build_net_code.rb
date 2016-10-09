5284db6b58410e4aba3a67f2f8fedca0744f6a4a
Brent Cook <busterb@gmail.com>
2016-10-08 20:17:29 -0500

    module cleanup

--- modules/post/windows/manage/powershell/build_net_code.rb
+++ modules/post/windows/manage/powershell/build_net_code.rb
@@ -1,4 +1,3 @@
-
 ##
 # This module requires Metasploit: http://metasploit.com/download
 # Current source: https://github.com/rapid7/metasploit-framework
@@ -14,57 +13,54 @@ class MetasploitModule < Msf::Post
   include Msf::Post::Windows::Powershell
   include Msf::Exploit::Powershell::DotNet
 
-  def initialize(info={})
-    super(update_info(info,
-      'Name'                 => "Powershell .NET Compiler",
-      'Description'          => %q{
-        This module will build a .NET source file using powershell. The compiler builds
-        the executable or library in memory and produces a binary. After compilation the
-        PowerShell session can also sign the executable if provided a path the
-        a .pfx formatted certificate. Compiler options and a list of assemblies
-        required can be configured in the datastore.
-      },
-      'License'              => MSF_LICENSE,
-      'Author'               => 'RageLtMan <rageltman[at]sempervictus>',
-      'Platform'      => [ 'windows' ],
-      'SessionTypes'  => [ 'meterpreter' ],
-      'Targets' => [ [ 'Universal', {} ] ],
-      'DefaultTarget' => 0,
-      'DisclosureDate' => 'Aug 14 2012'
-    ))
+  def initialize(info = {})
+    super(
+      update_info(
+        info,
+        'Name'           => "Powershell .NET Compiler",
+        'Description'    => %q(
+          This module will build a .NET source file using powershell. The compiler builds
+          the executable or library in memory and produces a binary. After compilation the
+          PowerShell session can also sign the executable if provided a path the
+          a .pfx formatted certificate. Compiler options and a list of assemblies
+          required can be configured in the datastore.
+        ),
+        'License'        => MSF_LICENSE,
+        'Author'         => 'RageLtMan <rageltman[at]sempervictus>',
+        'Platform'       => [ 'windows' ],
+        'SessionTypes'   => [ 'meterpreter' ],
+        'Targets'        => [ [ 'Universal', {} ] ],
+        'DefaultTarget'  => 0,
+        'DisclosureDate' => 'Aug 14 2012'
+      )
+    )
 
     register_options(
       [
         OptPath.new('SOURCE_FILE', [true, 'Path to source code']),
-        OptBool.new('RUN_BINARY', [false, 'Execute the genrated binary', false]),
-        OptString.new('ASSEMBLIES', [
-          false,
-          'Any assemblies outside the defaults',
-          "mscorlib.dll, System.dll, System.Xml.dll, System.Data.dll"
-        ]),
+        OptBool.new('RUN_BINARY', [false, 'Execute the generated binary', false]),
+        OptString.new('ASSEMBLIES', [false, 'Any assemblies outside the defaults',
+            "mscorlib.dll, System.dll, System.Xml.dll, System.Data.dll" ]),
         OptString.new('OUTPUT_TARGET', [false, 'Name and path of the generated binary, default random, omit extension' ]),
         OptString.new('COMPILER_OPTS', [false, 'Options to pass to compiler', '/optimize']),
-        OptString.new('CODE_PROVIDER', [true, 'Code provider to use', 'Microsoft.CSharp.CSharpCodeProvider']),
-
+        OptString.new('CODE_PROVIDER', [true, 'Code provider to use', 'Microsoft.CSharp.CSharpCodeProvider'])
       ], self.class)
     register_advanced_options(
       [
-        OptString.new('NET_CLR_VER', [false, 'Minimum NET CLR version required to compile', '4.0']),
+        OptString.new('NET_CLR_VER', [false, 'Minimum NET CLR version required to compile', '4.0'])
       ], self.class)
-
   end
 
   def run
-
     # Make sure we meet the requirements before running the script
     if !(session.type == "meterpreter" || have_powershell?)
-      print_error("Incompatible Environment")
+      print_error "Incompatible Environment"
       return 0
     end
 
     # Havent figured this one out yet, but we need a PID owned by a user, can't steal tokens either
     if client.sys.config.getuid == 'NT AUTHORITY\SYSTEM'
-      print_error("Cannot run as system")
+      print_error "Cannot run as system"
       return 0
     end
 
@@ -73,7 +69,7 @@ class MetasploitModule < Msf::Post
     env_suffix = Rex::Text.rand_text_alpha(8)
     net_com_opts = {}
     net_com_opts[:target] = datastore['OUTPUT_TARGET'] ||
-      session.fs.file.expand_path('%TEMP%') + "\\#{ Rex::Text.rand_text_alpha(rand(8)+8) }.exe"
+      session.fs.file.expand_path('%TEMP%') + "\\#{Rex::Text.rand_text_alpha(rand(8) + 8)}.exe"
     net_com_opts[:com_opts] = datastore['COMPILER_OPTS']
     net_com_opts[:provider] = datastore['CODE_PROVIDER']
     net_com_opts[:assemblies] = datastore['ASSEMBLIES']
@@ -84,40 +80,40 @@ class MetasploitModule < Msf::Post
       net_com_opts[:harness] = ::File.read(datastore['SOURCE_FILE'])
       script = dot_net_compiler(net_com_opts)
       if datastore['Powershell::Post::dry_run']
-        print_good("Compiler code:\n#{script}")
+        print_good "Compiler code:\n#{script}"
         return
       end
     rescue => e
-      print_error(e)
+      print_error e
       return
     end
 
-    vprint_good("Writing to #{net_com_opts[:target]}")
+    vprint_good "Writing to #{net_com_opts[:target]}"
 
     # Execute the powershell script
-    print_status('Building remote code.')
+    print_status 'Building remote code.'
     cmd_out, running_pids, open_channels = execute_script(script, true)
     get_ps_output(cmd_out, eof)
-    vprint_good( "Cleaning up #{running_pids.join(', ')}" )
+    vprint_good "Cleaning up #{running_pids.join(', ')}"
 
     clean_up(nil, eof, running_pids, open_channels, env_suffix, false)
 
     # Check for result
     begin
-      size = session.fs.file.stat(net_com_opts[:target].gsub('\\','\\\\')).size
-      print_good("File #{net_com_opts[:target].gsub('\\','\\\\')} found, #{size}kb")
+      size = session.fs.file.stat(net_com_opts[:target].gsub('\\', '\\\\')).size
+      print_good "File #{net_com_opts[:target].gsub('\\', '\\\\')} found, #{size}kb"
     rescue
-      print_error("File #{net_com_opts[:target].gsub('\\','\\\\')} not found, NET CLR version #{datastore['NET_CLR_VER']} possibly not available")
+      print_error "File #{net_com_opts[:target].gsub('\\', '\\\\')} not found," \
+                  " NET CLR version #{datastore['NET_CLR_VER']} possibly not available"
       return
     end
 
     # Run the result
     if datastore['RUN_BINARY']
-      session.sys.process.execute(net_com_opts[:target].gsub('\\','\\\\'), nil, {'Hidden' => true, 'Channelized' => true})
+      session.sys.process.execute(net_com_opts[:target].gsub('\\', '\\\\'),
+                                  nil, { 'Hidden' => true, 'Channelized' => true })
     end
 
-    print_good('Finished!')
+    print_good 'Finished!'
   end
-
-
 end
