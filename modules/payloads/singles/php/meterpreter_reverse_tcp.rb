0a3acf57d1b3e7e6316fefc421aa3305f8192091
Brent Cook <bcook@rapid7.com>
2016-11-20 19:47:17 -0600

    update payload sizes

--- modules/payloads/singles/php/meterpreter_reverse_tcp.rb
+++ modules/payloads/singles/php/meterpreter_reverse_tcp.rb
@@ -12,7 +12,7 @@ require 'msf/base/sessions/meterpreter_options'
 
 module MetasploitModule
 
-  CachedSize = 26803
+  CachedSize = 27144
 
   include Msf::Payload::Single
   include Msf::Payload::Php::ReverseTcp
