90ec367a99da5956280b913ac22d7a22ca2211b6
wchen-r7 <wei_chen@rapid7.com>
2016-12-01 14:52:51 -0600

    Add method to save creds to database

--- modules/auxiliary/admin/http/wp_symposium_sql_injection.rb
+++ modules/auxiliary/admin/http/wp_symposium_sql_injection.rb
@@ -65,6 +65,32 @@ class MetasploitModule < Msf::Auxiliary
     end
   end
 
+  def report_cred(opts)
+    service_data = {
+      address: opts[:ip],
+      port: opts[:port],
+      service_name: opts[:service_name],
+      protocol: 'tcp',
+      workspace_id: myworkspace_id
+    }
+
+    credential_data = {
+      origin_type: :service,
+      module_fullname: fullname,
+      username: opts[:user],
+      private_data: opts[:password],
+      private_type: :nonreplayable_hash,
+    }.merge(service_data)
+
+    login_data = {
+      core: create_credential(credential_data),
+      status: Metasploit::Model::Login::Status::UNTRIED,
+      proof: opts[:proof]
+    }.merge(service_data)
+
+    create_credential_login(login_data)
+  end
+
   def run
     vprint_status("#{peer} - Attempting to connect...")
     vprint_status("#{peer} - Trying to retrieve the first user id...")
@@ -103,6 +129,14 @@ class MetasploitModule < Msf::Auxiliary
         user_email = values[2]
 
         print_good("#{peer} - #{sprintf("%-15s %-34s %s", user_login, user_pass, user_email)}")
+        report_cred(
+          ip: rhost,
+          port: datastore['RPORT'],
+          service_name: datastore['SSL'] ? 'https' : 'http',
+          user: user_login,
+          password: user_pass,
+          proof: user_email
+        )
 
         credentials << "#{user_login},#{user_pass},#{user_email}\n"
       end
