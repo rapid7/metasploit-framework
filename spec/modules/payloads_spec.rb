62a9a31222f7565532171954e809f66d4049cad4
Adam Cammack <adam_cammack@rapid7.com>
2016-12-09 18:28:29 -0600

    Add stageless mettle for Linux/x64

--- spec/modules/payloads_spec.rb
+++ spec/modules/payloads_spec.rb
@@ -4394,6 +4394,16 @@ RSpec.describe 'modules/payloads', :content do
                           reference_name: 'linux/x64/mettle/reverse_tcp'
   end
 
+  context 'linux/x64/mettle_reverse_tcp' do
+    it_should_behave_like 'payload cached size is consistent',
+                          ancestor_reference_names: [
+                            'singles/linux/x64/mettle_reverse_tcp'
+                          ],
+                          dynamic_size: false,
+                          modules_pathname: modules_pathname,
+                          reference_name: 'linux/x64/mettle_reverse_tcp'
+  end
+
   context 'linux/x86/mettle/bind_ipv6_tcp' do
     it_should_behave_like 'payload cached size is consistent',
                           ancestor_reference_names: [
