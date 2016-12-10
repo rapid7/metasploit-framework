ee7d5fc0c9cd69ab154f7aa1bee0d0f72a175a2d
Adam Cammack <adam_cammack@rapid7.com>
2016-12-09 18:25:57 -0600

    Add stageless mettle for Linux/ppc

--- spec/modules/payloads_spec.rb
+++ spec/modules/payloads_spec.rb
@@ -4352,6 +4352,16 @@ RSpec.describe 'modules/payloads', :content do
                           reference_name: 'linux/mipsle/mettle_reverse_tcp'
   end
 
+  context 'linux/ppc/mettle_reverse_tcp' do
+    it_should_behave_like 'payload cached size is consistent',
+                          ancestor_reference_names: [
+                            'singles/linux/ppc/mettle_reverse_tcp'
+                          ],
+                          dynamic_size: false,
+                          modules_pathname: modules_pathname,
+                          reference_name: 'linux/ppc/mettle_reverse_tcp'
+  end
+
   context 'linux/x64/mettle/bind_tcp' do
     it_should_behave_like 'payload cached size is consistent',
                           ancestor_reference_names: [
