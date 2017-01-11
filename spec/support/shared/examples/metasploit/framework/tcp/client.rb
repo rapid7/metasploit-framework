c97dba39f2b65cc64a346f0bbf947a19c957e406
wchen-r7 <wei_chen@rapid7.com>
2017-01-11 11:48:52 -0600

    creds should mock these methods too

--- spec/support/shared/examples/metasploit/framework/tcp/client.rb
+++ spec/support/shared/examples/metasploit/framework/tcp/client.rb
@@ -11,7 +11,9 @@ RSpec.shared_examples_for 'Metasploit::Framework::Tcp::Client' do
     allow(creds).to receive(:username)
     allow(creds).to receive(:user_file)
     allow(creds).to receive(:userpass_file)
-    allow(creds).to receive(:additional_publics).and_return([])
+    allow(creds).to receive(:prepend_creds).and_return([])
+    allow(creds).to receive(:additional_privates).and_return([])
+    allow(creds).to receive(:additional_publics).and_return(['user'])
     login_scanner.cred_details = creds
   end
 
