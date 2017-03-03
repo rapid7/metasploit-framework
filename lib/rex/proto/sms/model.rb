c61f8ded783215ddb1d8b1a1643ca2a635e9f861
wchen-r7 <wei_chen@rapid7.com>
2017-03-03 11:09:04 -0600

    Comment out Sprint
    It looks like the Sprint gateways won't accept our email for
    some reason, so we can't use it.

--- lib/rex/proto/sms/model.rb
+++ lib/rex/proto/sms/model.rb
@@ -10,7 +10,11 @@ module Rex
           :att    => 'txt.att.net',              # AT&T Wireless
           :boost  => 'sms.myboostmobile.com',    # Boost Mobile
           :cricket => 'sms.mycricket.com',       # Cricket Wireless
-          :sprint  => 'messaging.sprintpcs.com', # Sprint
+          # Sprint is commented out, because the gateways don't seem to work.
+          # Gateways tried for Sprint:
+          # messaging.sprintpcs.com
+          # pm.sprint.com
+          #:sprint  => 'messaging.sprintpcs.com', # Sprint
           :tmobile => 'tmomail.net',             # T-Mobile
           :verizon => 'vtext.com',               # Verizon
           :virgin  => 'vmobl.com'                # Virgin Mobile
