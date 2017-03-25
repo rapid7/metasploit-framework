dd7cf39678eb3a0e44c99b29b0f75cb3dfe1eaf8
juushya <KarnGaneshen@gmail.com>
2017-03-25 12:31:08 +0530

    updated references

--- modules/auxiliary/scanner/http/epmp1000_dump_hashes.rb
+++ modules/auxiliary/scanner/http/epmp1000_dump_hashes.rb
@@ -19,7 +19,7 @@ class MetasploitModule < Msf::Auxiliary
       },
       'References' =>
         [
-          ['URL', 'http://ipositivesecurity.blogspot.in/2015/11/cambium-epmp-1000-multiple.html'],
+          ['URL', 'http://ipositivesecurity.com/2015/11/28/cambium-epmp-1000-multiple-vulnerabilities/'],
           ['URL', 'https://support.cambiumnetworks.com/file/476262a0256fdd8be0e595e51f5112e0f9700f83']
         ],
       'Author' =>
