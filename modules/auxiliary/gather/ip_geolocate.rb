ef35dfcc9b8686805eb54c8e26429697a71abb9b
Carter <cbawsome77@gmail.com>
2017-04-06 13:10:20 -0400

    Fix review stuff

--- modules/auxiliary/gather/ip_geolocate.rb
+++ modules/auxiliary/gather/ip_geolocate.rb
@@ -3,20 +3,16 @@
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
 
-require 'msf/core'
 require 'json'
 
 class MetasploitModule < Msf::Auxiliary
 
-  include Rex::Proto::Http
-  include Msf::Exploit::Remote::HttpClient
-  # HttpClient is only included because of normalize_uri
 
   def initialize(info = {})
     super(update_info(info,
       'Name'           => 'IP Based Geolocation',
       'Description'    => %q{
-        This module uses a GeoIP API to locate the location
+        This module uses a GeoIP API to estimate the location
         of a given IP address. Even though an external API is being
         used, a API key is not needed for the module to work properly.
         The shown longitude and latitude values are estimations.
@@ -28,7 +24,7 @@ class MetasploitModule < Msf::Auxiliary
 
     register_options(
       [
-        OptString.new("RHOSTS", [true, "A comma separated list of addresses to scan", nil])
+        OptString.new('TARGETS', [true, "A comma separated list of addresses to scan", nil])
       ])
 
     deregister_options( 'RHOST', 'SSL', 'RPORT', 'VHOST', 'Proxies' )
@@ -73,8 +69,8 @@ class MetasploitModule < Msf::Auxiliary
       # Because we're requesting an external site, we aren't actually requesting `rhost`
       # We're requesting the API and passing `rhost` in the URI.
       # This is why Rex::Proto::Http is being used and not send_request_cgi from HttpClient
-      uri = normalize_uri('json', "#{host}")
-      cli = Client.new('freegeoip.net', 80, {}, false)  # The API uses only HTTP unfortunately
+      uri = Msf::Exploit::Remote::HttpClient.normalize_uri('json', "#{host}")
+      cli = Rex::Proto::Http::Client.new('freegeoip.net', 80, {}, false)  # The API uses only HTTP unfortunately
       cli.connect
       req = cli.request_cgi({ 'uri' => uri })
       res = cli.send_recv(req)
@@ -92,11 +88,11 @@ class MetasploitModule < Msf::Auxiliary
       parsed = JSON.parse(res.body)  # Make it fun to use (hash)
       print_line
       print_status("Data for #{host}")
-      print_line("  Country: #{parsed['country_name']} (#{parsed['country_code']})")  # Print the country and country country code
-      print_line("  City: #{parsed['region_name']} (#{parsed['region_code']})")       # Print the city and city code
-      print_line("  Zip Code: #{parsed['zip_code']}")                                 # Print the zip code
-      print_line("  Latitude: #{parsed['latitude']} (estimation)")                    # Print the latitude
-      print_line("  Longitude: #{parsed['longitude']} (estimation)")                  # Print the longitude
+      print_line("  Country: #{parsed['country_name']} (#{parsed['country_code']})")
+      print_line("  City: #{parsed['region_name']} (#{parsed['region_code']})")
+      print_line("  Zip Code: #{parsed['zip_code']}")
+      print_line("  Latitude: #{parsed['latitude']} (estimation)")
+      print_line("  Longitude: #{parsed['longitude']} (estimation)")
       print_line("  Google Maps URL: #{gmap_url(parsed['latitude'], parsed['longitude'])}")
     end
   end
