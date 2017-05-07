c05e7b3b585763b4947bd2501e9aab716d0d17f0
Pearce Barry <pearce_barry@rapid7.com>
2017-05-07 11:55:20 -0500

    Minor corrections and a tweak to appease msftidy.

--- modules/post/hardware/automotive/canprobe.rb
+++ modules/post/hardware/automotive/canprobe.rb
@@ -9,8 +9,8 @@ class MetasploitModule < Msf::Post
     super( update_info( info,
         'Name'          => 'Module to Probe Different Data Points in a CAN Packet',
         'Description'   => %q{
-                               Scans between two CAN IDs and puts data at eacy byte position. It will
-                               Either write a set byte value (Default 0xFF) or iterate through all possible values
+                               Scans between two CAN IDs and writes data at each byte position. It will
+                               either write a set byte value (Default 0xFF) or iterate through all possible values
                                of that byte position (takes much longer). Does not check for responses and is
                                basically a simple blind fuzzer.
         },
