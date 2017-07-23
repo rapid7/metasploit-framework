399557124f8c4d18da7767349138e1f27f0b0281
Brent Cook <bcook@rapid7.com>
2017-07-23 05:28:32 -0700

    update payload cached sizes

--- modules/payloads/singles/python/meterpreter_reverse_tcp.rb
+++ modules/payloads/singles/python/meterpreter_reverse_tcp.rb
@@ -11,7 +11,7 @@ require 'msf/base/sessions/meterpreter_python'
 
 module MetasploitModule
 
-  CachedSize = 54058
+  CachedSize = 54278
 
   include Msf::Payload::Single
   include Msf::Payload::Python
