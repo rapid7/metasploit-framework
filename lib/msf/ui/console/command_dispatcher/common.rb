92a255ff1aec69edc181ce312b26800f94ec1bab
g0tmi1k <have.you.g0tmi1k@gmail.com>
2017-09-07 12:02:12 +0100

    Add msg on 'options' about DisablePayloadHandler when its enabled

--- lib/msf/ui/console/command_dispatcher/common.rb
+++ lib/msf/ui/console/command_dispatcher/common.rb
@@ -119,6 +119,7 @@ module Common
       if (p)
         p_opt = Serializer::ReadableText.dump_options(p, '   ')
         print("\nPayload options (#{mod.datastore['PAYLOAD']}):\n\n#{p_opt}\n") if (p_opt and p_opt.length > 0)
+        print("   **DisablePayloadHandler: True   (RHOST and RPORT settings will be ignored!)**\n\n") if mod.datastore['DisablePayloadHandler']
       end
     end
 
