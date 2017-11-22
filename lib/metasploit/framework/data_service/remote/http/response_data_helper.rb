770f092e5d747c7313f01422744f5ea9581580a5
James Barnett <James_Barnett@rapid7.com>
2017-11-22 12:34:24 -0600

    Use more accurate variable name

--- lib/metasploit/framework/data_service/remote/http/response_data_helper.rb
+++ lib/metasploit/framework/data_service/remote/http/response_data_helper.rb
@@ -1,4 +1,5 @@
 require 'ostruct'
+require 'digest'
 
 #
 # HTTP response helper class
@@ -27,21 +28,21 @@ module ResponseDataHelper
   # Saves the file in the location specified in the parameter.
   #
   # @param base64_file [String] The Base64 encoded file.
-  # @param save_dir [String] The location to store the file. This should include the file's name.
+  # @param save_path [String] The location to store the file. This should include the file's name.
   # @return [String] The location where the file was successfully stored.
-  def process_file(base64_file, save_dir)
+  def process_file(base64_file, save_path)
     decoded_file = Base64.urlsafe_decode64(base64_file)
     begin
       # If we are running the data service on the same box this will ensure we only write
       # the file if it is somehow not there already.
-      unless File.exists?(save_dir) && File.read(save_dir) == decoded_file
-        File.open(save_dir, 'w+') { |file| file.write(decoded_file) }
+      unless File.exists?(save_path) && File.read(save_path) == decoded_file
+        File.open(save_path, 'w+') { |file| file.write(decoded_file) }
       end
     rescue Exception => e
       puts "There was an error writing the file: #{e}"
       e.backtrace.each { |line| puts "#{line}\n"}
     end
-    save_dir
+    save_path
   end
 
   #
