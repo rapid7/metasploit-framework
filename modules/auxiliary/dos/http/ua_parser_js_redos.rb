2d23054a1f104fd72478acb06f33ce4edc09dc4e
Nicholas Starke <nstarke@sonatype.com>
2017-12-11 14:11:40 -0600

    Changes as per comments
    A few things were changed as per the PR comments:
    1) The module title was reworded
    2) The module description was multi-lined
    3) Negative logic was rewritten to use 'unless'
    4) Strings which did not require interpolation were rewritten
    5) Documentation markdown was added.

--- modules/auxiliary/dos/http/ua_parser_js_redos.rb
+++ modules/auxiliary/dos/http/ua_parser_js_redos.rb
@@ -9,12 +9,17 @@ class MetasploitModule < Msf::Auxiliary
 
   def initialize
     super(
-      'Name'           => 'ua-parser-js npm module - Regular Expression Denial of Service',
+      'Name'           => 'ua-parser-js npm module ReDoS',
       'Description'    => %q{
-        This module exploits a Regular Expression Denial of Service vulnerability in the npm module "ua-parser-js". Server-side applications that use "ua-parser-js" for parsing the browser user-agent string will be vulnerable if they call the "getOS" or "getResult" functions. This vulnerability was fixed as of version 0.7.16.
+        This module exploits a Regular Expression Denial of Service vulnerability
+        in the npm module "ua-parser-js". Server-side applications that use
+        "ua-parser-js" for parsing the browser user-agent string will be vulnerable
+        if they call the "getOS" or "getResult" functions. This vulnerability was
+        fixed as of version 0.7.16.
       },
       'References'     =>
         [
+          ['URL', 'https://github.com/faisalman/ua-parser-js/commit/25e143ee7caba78c6405a57d1d06b19c1e8e2f79'],
           ['CWE', '400'],
         ],
       'Author'         =>
@@ -31,7 +36,7 @@ class MetasploitModule < Msf::Auxiliary
   end
 
   def run
-    if !test_service
+    unless test_service
       fail_with(Failure::Unreachable, "#{peer} - Could not communicate with service.")
     else
       trigger_redos
@@ -66,7 +71,7 @@ class MetasploitModule < Msf::Auxiliary
 
   def test_service_unresponsive
     begin
-      print_status("Testing for service unresponsiveness.")
+      print_status('Testing for service unresponsiveness.')
 
       res = send_request_cgi({
         'uri' => '/' + Rex::Text.rand_text_alpha(8),
@@ -74,20 +79,20 @@ class MetasploitModule < Msf::Auxiliary
       })
 
       if res.nil?
-        print_good("Service not responding.")
+        print_good('Service not responding.')
       else
-        print_error("Service responded with a valid HTTP Response; ReDoS attack failed.")
+        print_error('Service responded with a valid HTTP Response; ReDoS attack failed.')
       end
     rescue ::Rex::ConnectionRefused
-      print_error("An unknown error occurred.")
+      print_error('An unknown error occurred.')
     rescue ::Timeout::Error
-      print_good("HTTP request timed out, most likely the ReDoS attack was successful.")
+      print_good('HTTP request timed out, most likely the ReDoS attack was successful.')
     end
   end
 
   def test_service
     begin
-      print_status("Testing Service to make sure it is working.")
+      print_status('Testing Service to make sure it is working.')
 
       res = send_request_cgi({
         'uri' => '/' + Rex::Text.rand_text_alpha(8),
@@ -95,7 +100,7 @@ class MetasploitModule < Msf::Auxiliary
       })
 
       if !res.nil? && (res.code == 200 || res.code == 404)
-        print_status("Test request successful, attempting to send payload")
+        print_status('Test request successful, attempting to send payload')
         return true
       else
         return false
