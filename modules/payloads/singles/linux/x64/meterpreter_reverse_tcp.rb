2a94a4417adb7a4e55b57514e38b7d6e7cf38270
Brent Cook <busterb@gmail.com>
2017-12-18 10:01:10 -0600

    bump payloads

--- modules/payloads/singles/linux/x64/meterpreter_reverse_tcp.rb
+++ modules/payloads/singles/linux/x64/meterpreter_reverse_tcp.rb
@@ -10,7 +10,7 @@ require 'msf/base/sessions/meterpreter_x64_linux'
 
 module MetasploitModule
 
-  CachedSize = 746944
+  CachedSize = 905984
 
   include Msf::Payload::Single
   include Msf::Sessions::MeterpreterOptions
