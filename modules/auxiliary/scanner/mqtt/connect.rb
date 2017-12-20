ed5f177fcd70248c607a4df17f526dec39fe5826
Jon Hart <jon_hart@rapid7.com>
2017-12-20 14:20:08 -0800

    syntax

--- modules/auxiliary/scanner/mqtt/connect.rb
+++ modules/auxiliary/scanner/mqtt/connect.rb
@@ -3,59 +3,94 @@
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
 
+require 'metasploit/framework/credential_collection'
+require 'metasploit/framework/login_scanner/mqtt'
+
 class MetasploitModule < Msf::Auxiliary
   include Msf::Exploit::Remote::Tcp
   include Msf::Auxiliary::Scanner
   include Msf::Auxiliary::MQTT
   include Msf::Auxiliary::Report
-
-  HANDLED_EXCEPTIONS = [
-    Rex::AddressInUse, Rex::HostUnreachable, Rex::ConnectionTimeout, Rex::ConnectionRefused,
-    ::Errno::ETIMEDOUT, ::Timeout::Error, ::EOFError
-  ]
+  include Msf::Auxiliary::AuthBrute
 
   def initialize
     super(
-      'Name'        => 'Connect to and discover MQTT endpoints',
+      'Name'        => 'MQTT Authentication Scanner',
       'Description' => %q(
-        This module attempts to establish a connection with MQTT endpoints.
+        This module attempts to authenticate to MQTT.
       ),
-      'Author'      => [
-        'Jon Hart <jon_hart[at]rapid7.com>' # original metasploit module
-      ],
-      'References'  =>
+      'Author'      =>
+        [
+          'Jon Hart <jon_hart[at]rapid7.com>'
+        ],
+      'References'     =>
         [
           ['URL', 'http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/os/mqtt-v3.1.1-os.html#_Table_3.1_-']
         ],
-      'License'     => MSF_LICENSE
+      'License'     => MSF_LICENSE,
+      'DefaultOptions' =>
+        {
+          'BLANK_PASSWORDS' => true,
+          'USERNAME' => '',
+          'PASSWORD' => ''
+        }
     )
   end
 
   def run_host(ip)
-    begin
-      connect
-      client = mqtt_client
-      if mqtt_connect?(client)
-        print_good("Connected to MQTT")
-        # TODO: eventually we should subscribe to $SYS/# in order to get useful metadata:
-        # $  mosquitto_sub -t '$SYS/#' -v
-        # $SYS/broker/version mosquitto version 1.4.14
-        # $SYS/broker/timestamp Mon, 10 Jul 2017 23:48:43 +0100
-        report_service(
-          host: ip,
-          port: rport,
-          proto: 'tcp',
-          name: 'MQTT'
-        )
+    vprint_status("#{ip}:#{rport} - Starting MQTT login sweep")
+
+    cred_collection = Metasploit::Framework::CredentialCollection.new(
+      blank_passwords: datastore['BLANK_PASSWORDS'],
+      pass_file: datastore['PASS_FILE'],
+      password: datastore['PASSWORD'],
+      user_file: datastore['USER_FILE'],
+      userpass_file: datastore['USERPASS_FILE'],
+      username: datastore['USERNAME'],
+      user_as_pass: datastore['USER_AS_PASS']
+    )
+
+    cred_collection = prepend_db_passwords(cred_collection)
+
+    scanner = Metasploit::Framework::LoginScanner::MQTT.new(
+      host: ip,
+      port: rport,
+      read_timeout: datastore['READ_TIMEOUT'],
+      client_id: client_id(),
+      proxies: datastore['PROXIES'],
+      cred_details: cred_collection,
+      stop_on_success: datastore['STOP_ON_SUCCESS'],
+      bruteforce_speed: datastore['BRUTEFORCE_SPEED'],
+      connection_timeout: datastore['ConnectTimeout'],
+      max_send_size: datastore['TCP::max_send_size'],
+      send_delay: datastore['TCP::send_delay'],
+      framework: framework,
+      framework_module: self,
+      ssl: datastore['SSL'],
+      ssl_version: datastore['SSLVersion'],
+      ssl_verify_mode: datastore['SSLVerifyMode'],
+      ssl_cipher: datastore['SSLCipher'],
+      local_port: datastore['CPORT'],
+      local_host: datastore['CHOST']
+    )
+
+    scanner.scan! do |result|
+      credential_data = result.to_h
+      credential_data.merge!(
+        module_fullname: fullname,
+        workspace_id: myworkspace_id
+      )
+      password = result.credential.private
+      username = result.credential.public
+      if result.success?
+        credential_core = create_credential(credential_data)
+        credential_data[:core] = credential_core
+        create_credential_login(credential_data)
+        print_good("#{ip}:#{rport} - MQTT Login Successful: #{username}/#{password}")
       else
-        vprint_error("Failed to connect to MQTT")
+        invalidate_login(credential_data)
+        vprint_error("#{ip}:#{rport} - MQTT LOGIN FAILED: #{username}/#{password} (#{result.status}: #{result.proof})")
       end
-    rescue *HANDLED_EXCEPTIONS => e
-      vprint_error("error while connecting and negotiating: #{e}")
-      return
-    ensure
-      mqtt_disconnect(client)
-      disconnect
     end
   end
 end
