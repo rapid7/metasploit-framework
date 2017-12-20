f15309bc48d39671cb1c60fc892332164a0be79a
Jon Hart <jon_hart@rapid7.com>
2017-12-20 12:28:02 -0800

    Add basic framework for interacting with MQTT

---
+++ lib/msf/core/auxiliary/mqtt.rb
@@ -0,0 +1,69 @@
+# -*- coding: binary -*-
+
+require 'msf/core/exploit'
+require 'rex/proto/mqtt'
+
+module Msf
+  module Auxiliary::MQTT
+      include Exploit::Remote::Tcp
+
+      def initialize(info = {})
+        super
+
+        register_options(
+          [
+            Opt::RPORT(Rex::Proto::MQTT::DEFAULT_PORT),
+            OptString.new('USERNAME', [false, 'The user to authenticate as']),
+            OptString.new('PASSWORD', [false, 'The password to authenticate with'])
+          ]
+        )
+
+        register_advanced_options(
+          [
+            OptString.new('CLIENT_ID', [false, 'The client ID to send if necessary for bypassing clientid_prefixes']),
+            OptInt.new('READ_TIMEOUT', [true, 'Seconds to wait while reading MQTT responses', 5])
+          ]
+        )
+
+        register_autofilter_ports([Rex::Proto::MQTT::DEFAULT_PORT])
+      end
+
+      def setup
+        fail_with(Failure::BadConfig, 'READ_TIMEOUT must be > 0') if read_timeout <= 0
+        client_id_arg = datastore['CLIENT_ID']
+        if client_id_arg
+          fail_with(Failure::BadConfig, 'CLIENT_ID must be a non-empty string') if client_id_arg.blank?
+        end
+      end
+
+      def read_timeout
+        datastore['READ_TIMEOUT']
+      end
+
+      def client_id
+        datastore['CLIENT_ID'] || Rex::Text.rand_text_alpha(1 + rand(10))
+      end
+
+      def mqtt_client
+        client_opts = {
+          client_id: client_id().to_s,
+          username: datastore['USERNAME'],
+          password: datastore['PASSWORD'],
+          read_timeout: read_timeout
+        }
+        Rex::Proto::MQTT::Client.new(sock, client_opts)
+      end
+
+      def mqtt_connect(client)
+        client.connect
+      end
+
+      def mqtt_connect?(client)
+        mqtt_connect(client).return_code == 0
+      end
+
+      def mqtt_disconnect(client)
+        client.disconnect
+      end
+    end
+end
