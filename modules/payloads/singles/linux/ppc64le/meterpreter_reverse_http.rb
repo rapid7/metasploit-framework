55c345418d6233de0fc8c59ec66beb31b1a54d18
Brent Cook <busterb@gmail.com>
2018-01-22 09:37:07 -0600

    Land #9438, address cmd_exec inconsistencies

--- modules/payloads/singles/linux/ppc64le/meterpreter_reverse_http.rb
+++ modules/payloads/singles/linux/ppc64le/meterpreter_reverse_http.rb
@@ -10,7 +10,7 @@ require 'msf/base/sessions/meterpreter_ppc64le_linux'
 
 module MetasploitModule
 
-  CachedSize = 1014024
+  CachedSize = 1014352
 
   include Msf::Payload::Single
   include Msf::Sessions::MeterpreterOptions
