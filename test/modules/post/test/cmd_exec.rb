bfec2e8293a4364b875e1c35b10fcf852c6380e2
Tim W <timrlw@gmail.com>
2018-02-22 17:05:18 +0800

    add more cmd_exec tests

--- test/modules/post/test/cmd_exec.rb
+++ test/modules/post/test/cmd_exec.rb
@@ -1,4 +1,3 @@
-require 'msf/core'
 require 'rex'
 
 lib = File.join(Msf::Config.install_root, "test", "lib")
@@ -21,7 +20,7 @@ class MetasploitModule < Msf::Post
   end
 
   def test_cmd_exec
-    print_status("Starting cmd_exec tests")
+    vprint_status("Starting cmd_exec tests")
 
     it "should return the result of echo" do
       test_string = Rex::Text.rand_text_alpha(4)
@@ -39,9 +38,35 @@ class MetasploitModule < Msf::Post
       test_string = Rex::Text.rand_text_alpha(4)
       test_string2 = Rex::Text.rand_text_alpha(4)
       output = cmd_exec("echo #{test_string}; sleep 1; echo #{test_string2}")
-      output == "#{test_string}\n#{test_string2}"
+      output.delete("\r") == "#{test_string}\n#{test_string2}"
     end
 
-    print_status("Finished cmd_exec tests")
+    it "should return the result of echo 10 times" do
+      10.times do
+        test_string = Rex::Text.rand_text_alpha(4)
+        output = cmd_exec("echo #{test_string}")
+        return false unless output == test_string
+      end
+      true
+    end
+
+    vprint_status("Finished cmd_exec tests")
   end
+
+  def test_cmd_exec_quotes
+    vprint_status("Starting cmd_exec quote tests")
+
+    it "should return the result of echo with single quotes" do
+      test_string = Rex::Text.rand_text_alpha(4)
+      output = cmd_exec("echo '#{test_string}'")
+      output == test_string
+    end
+
+    it "should return the result of echo with double quotes" do
+      test_string = Rex::Text.rand_text_alpha(4)
+      output = cmd_exec("echo \"#{test_string}\"")
+      output == test_string
+    end
+  end
+
 end
