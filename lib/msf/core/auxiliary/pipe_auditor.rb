be77cb2a2b013c40c799ee259cba973a0b1de7b9
Auxilus <raghavsphadke@gmail.com>
2018-02-24 11:04:41 +0530

    Add pipe_auditor

---
+++ lib/msf/core/auxiliary/pipe_auditor.rb
@@ -0,0 +1,56 @@
+module Msf
+
+module Auxiliary::PIPEAudit
+  include Msf::Exploit::Remote::SMB::Client
+  include Msf::Exploit::Remote::SMB::Client::Authenticated
+  include Msf::Auxiliary::Scanner
+
+  def initialize(info = {})
+    super
+	register_options([
+			OptString.new('RPORT', [true, 'The Target port', 445])
+		], Msf::Auxiliary::PIPEAudit)
+  end
+
+  def connect_to_pipe
+		  accessible_pipes||=[]
+		  target_pipes = [
+                'netlogon',
+                'lsarpc',
+                'samr',
+                'browser',
+                'atsvc',
+                'DAV RPC SERVICE',
+                'epmapper',
+                'eventlog',
+                'InitShutdown',
+                'keysvc',
+                'lsass',
+                'LSM_API_service',
+                'ntsvcs',
+                'plugplay',
+                'protected_storage',
+                'router',
+                'SapiServerPipeS-1-5-5-0-70123',
+                'scerpc',
+                'srvsvc',
+                'tapsrv',
+                'trkwks',
+                'W32TIME_ALT',
+                'wkssvc',
+                'PIPE_EVENTROOT\CIMV2SCM EVENT PROVIDER',
+                'db2remotecmd'
+        ]
+		
+		target_pipes.each do |pipe|
+		     begin
+				pipe_name = "#{pipe}"
+				pipe_handle = self.simple.create_pipe(pipe_name, 'o')
+				accessible_pipes << pipe_name
+		     end
+      end
+		return accessible_pipes[0], pipe_handle
+  end
+end
+end
+
