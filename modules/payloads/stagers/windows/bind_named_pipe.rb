35b66d0e60d60870c36d280f8e558e84ea5b4cbd
UserExistsError <UserExistsError@gmail.com>
2018-02-27 19:24:51 -0700

    added payload tests

--- modules/payloads/stagers/windows/bind_named_pipe.rb
+++ modules/payloads/stagers/windows/bind_named_pipe.rb
@@ -8,7 +8,7 @@ require 'msf/core/payload/windows/bind_named_pipe'
 
 module MetasploitModule
 
-  CachedSize = 0
+  CachedSize = 336
 
   include Msf::Payload::Stager
   include Msf::Payload::Windows::BindNamedPipe
