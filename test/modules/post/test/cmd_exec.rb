6c177b7c3b8931d3e9b42928b96b930ae54a009a
bwatters-r7 <bwatters@rapid7.com>
2018-03-05 18:51:34 -0600

    Add windows checks

--- test/modules/post/test/cmd_exec.rb
+++ test/modules/post/test/cmd_exec.rb
@@ -24,7 +24,11 @@ class MetasploitModule < Msf::Post
 
     it "should return the result of echo" do
       test_string = Rex::Text.rand_text_alpha(4)
-      output = cmd_exec("echo #{test_string}")
+      if session.platform.eql?'windows'
+        output = cmd_exec('cmd.exe', "/c echo #{test_string}")
+      else
+        output = cmd_exec("echo #{test_string}")
+      end
       output == test_string
     end
 
@@ -37,14 +41,22 @@ class MetasploitModule < Msf::Post
     it "should return the full response after sleeping" do
       test_string = Rex::Text.rand_text_alpha(4)
       test_string2 = Rex::Text.rand_text_alpha(4)
-      output = cmd_exec("echo #{test_string}; sleep 1; echo #{test_string2}")
+      if session.platform.eql?'windows'
+        output = cmd_exec('cmd.exe', "/c echo #{test_string} & timeout 1 > null & echo #{test_string2}")
+      else
+        output = cmd_exec("echo #{test_string}; sleep 1; echo #{test_string2}")
+      end
       output.delete("\r") == "#{test_string}\n#{test_string2}"
     end
 
     it "should return the result of echo 10 times" do
       10.times do
         test_string = Rex::Text.rand_text_alpha(4)
-        output = cmd_exec("echo #{test_string}")
+	    if session.platform.eql?'windows'
+          output = cmd_exec("cmd.exe", "/c echo #{test_string}")
+        else
+          output = cmd_exec("echo #{test_string}")
+        end
         return false unless output == test_string
       end
       true
@@ -58,14 +70,24 @@ class MetasploitModule < Msf::Post
 
     it "should return the result of echo with single quotes" do
       test_string = Rex::Text.rand_text_alpha(4)
-      output = cmd_exec("echo '#{test_string}'")
-      output == test_string
+      if session.platform.eql?'windows'
+        output = cmd_exec("cmd.exe", "/c echo '#{test_string}'")
+        output == "'" + test_string + "'"
+      else
+        output = cmd_exec("echo '#{test_string}'")
+        output == test_string
+      end
     end
 
     it "should return the result of echo with double quotes" do
       test_string = Rex::Text.rand_text_alpha(4)
-      output = cmd_exec("echo \"#{test_string}\"")
-      output == test_string
+      if session.platform.eql?'windows'
+        output = cmd_exec("cmd.exe", "/c echo \"#{test_string}\"")
+        output == "\"" + test_string + "\""
+      else
+        output = cmd_exec("echo \"#{test_string}\"")
+        output == test_string
+      end
     end
   end
 
