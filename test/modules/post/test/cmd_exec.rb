b13a54ba2b560cd5c5c3da5975f2ff99e7378f6d
bwatters-r7 <bwatters@rapid7.com>
2018-03-06 10:10:53 -0600

    clean up some code and skip non-windows tests

--- test/modules/post/test/cmd_exec.rb
+++ test/modules/post/test/cmd_exec.rb
@@ -32,21 +32,24 @@ class MetasploitModule < Msf::Post
       output == test_string
     end
 
-    it "should return the result after sleeping" do
-      test_string = Rex::Text.rand_text_alpha(4)
-      output = cmd_exec("sleep 1; echo #{test_string}")
-      output == test_string
-    end
-
-    it "should return the full response after sleeping" do
-      test_string = Rex::Text.rand_text_alpha(4)
-      test_string2 = Rex::Text.rand_text_alpha(4)
-      if session.platform.eql?'windows'
-        output = cmd_exec('cmd.exe', "/c echo #{test_string} & timeout 1 > null & echo #{test_string2}")
-      else
-        output = cmd_exec("echo #{test_string}; sleep 1; echo #{test_string2}")
-      end
-      output.delete("\r") == "#{test_string}\n#{test_string2}"
+    # trying to do a sleep in windows without trashing stdout is hard
+    unless session.platform.eql?'windows'
+	  it "should return the result after sleeping" do
+	    test_string = Rex::Text.rand_text_alpha(4)
+	    output = cmd_exec("sleep 1; echo #{test_string}")
+	    output == test_string
+	  end
+	  
+	  it "should return the full response after sleeping" do
+	    test_string = Rex::Text.rand_text_alpha(4)
+	    test_string2 = Rex::Text.rand_text_alpha(4)
+	    if session.platform.eql?'windows'
+	      output = cmd_exec('cmd.exe', "/c echo #{test_string} & timeout 1 > null & echo #{test_string2}")
+	    else
+	      output = cmd_exec("echo #{test_string}; sleep 1; echo #{test_string2}")
+	    end
+	    output.delete("\r") == "#{test_string}\n#{test_string2}"
+	  end
     end
 
     it "should return the result of echo 10 times" do
