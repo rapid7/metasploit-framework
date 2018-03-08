db6c7549a61af8a8ed0cc7f6d5cffe60f9eab3f6
Mateus Lino <0daycode@protonmail.com>
2018-03-07 23:22:07 -0300

    Update gmail_checker.rb

--- modules/auxiliary/gather/gmail_checker.rb
+++ modules/auxiliary/gather/gmail_checker.rb
@@ -12,35 +12,30 @@ class MetasploitModule < Msf::Auxiliary
     super(update_info(info,
       'Name' => 'Check e-mail valid by gmail',
       'Description' => %q{
-      This module check valid e-mail by gmail
-	},
+      This module check valid e-mail by gmail},
       'Author' => [ 'Mateus Lino <dctoralves@protonmail.ch>' ],
       'License' => MSF_LICENSE))
 
-    register_options(
-      [
+    register_options([
         OptString.new('CHEK_GMAIL', [true, 'check emails valid by gmail']),
      ])
-
-
-  end
+end
 
   def check_gmail(targetdom)
-    print_status(". . . CHECKING . . . ")
-	clnt = URI("https://mail.google.com/mail/gxlu?email=#{targetdom}@gmail.com")
-	resp = Net::HTTP.get_response(clnt)
-	if (resp['Set-Cookie']) 	
-	print_status("E-mail valid")
-	else
-	print_status("E-mail invalid")
-	end
- end
-  
-
-  def run
-    print_status("Checking email")
- 	target = datastore['CHEK_GMAIL']
-    check_gmail(target) if datastore['CHEK_GMAIL']
-   
+  print_status(". . . CHECKING . . . ")
+  clnt = URI("https://mail.google.com/mail/gxlu?email=#{targetdom}@gmail.com")
+  resp = Net::HTTP.get_response(clnt)
+  if resp['Set-Cookie']  || resp.get_cookies
+  print_status("E-mail valid")
+  else
+  print_status("E-mail invalid")
   end
+ end
+def run
+    print_status("Checking email")
+    target = datastore['CHEK_GMAIL']
+    check_gmail(target) if datastore['CHEK_GMAIL']
+   end
 end
+
+
