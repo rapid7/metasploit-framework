4c536a1819b6d56b59255d93da2fdae106d285ad
Matthew Kienow <matthew_kienow@rapid7.com>
2018-03-30 19:23:41 -0400

    Add optional parameter to skip columns

--- lib/msf/util/db_manager.rb
+++ lib/msf/util/db_manager.rb
@@ -5,13 +5,19 @@ module Msf
       #
       # @param model - An ActiveRecord model object
       # @param search - A string regex search
+      # @param column_name_skip_list - An array of strings containing column names to skip
       # @return Arel::Nodes::Or object that represents a search of all of the model's columns
-      def self.create_all_column_search_conditions(model, search)
+      def self.create_all_column_search_conditions(model, search, column_name_skip_list=nil)
         search = "(?mi)#{search}"
-        condition_set = model.columns.map do |column|
+        # remove skip columns
+        columns = model.columns.reject { |column|
+          column_name_skip_list && column_name_skip_list.include?(column.name)
+        }
+
+        condition_set = columns.map { |column|
           Arel::Nodes::Regexp.new(Arel::Nodes::NamedFunction.new("CAST", [model.arel_table[column.name].as("TEXT")]),
                                   Arel::Nodes.build_quoted(search))
-        end
+        }
         condition_set.reduce { |conditions, condition| conditions.or(condition).expr }
       end
     end
