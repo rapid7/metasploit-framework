7b88ea54038724fabb3835f492842d2533a377f1
Jon Hart <jon_hart@rapid7.com>
2018-04-06 08:28:53 -0700

    Add other linux kernel post helpers

--- lib/msf/core/post/linux/kernel.rb
+++ lib/msf/core/post/linux/kernel.rb
@@ -8,14 +8,50 @@ module Kernel
   include ::Msf::Post::Common
 
   #
+  # Returns uname output
+  #
+  # @return [String]
+  #
+  def uname(opts='-a')
+    cmd_exec("uname #{opts}").to_s
+  rescue
+    raise "Failed to run uname #{opts}"
+  end
+
+  #
+  # Returns the kernel release
+  #
+  # @return [String]
+  #
+  def kernel_release
+    uname('-r')
+  end
+
+  #
   # Returns the kernel version
   #
   # @return [String]
   #
   def kernel_version
-    cmd_exec('uname -r').to_s
-  rescue
-    raise 'Could not determine kernel version'
+    uname('-v')
+  end
+
+  #
+  # Returns the kernel name
+  #
+  # @return [String]
+  #
+  def kernel_name
+    uname('-s')
+  end
+
+  #
+  # Returns the kernel hardware
+  #
+  # @return [String]
+  #
+  def kernel_hardware
+    uname('-m')
   end
 
   #
