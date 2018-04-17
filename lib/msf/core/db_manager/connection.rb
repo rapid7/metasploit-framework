77558cd2d72f101720c1a80326f64e5e9b5782bf
James Barnett <James_Barnett@rapid7.com>
2018-04-17 14:28:28 -0500

    Remove unused code

--- lib/msf/core/db_manager/connection.rb
+++ lib/msf/core/db_manager/connection.rb
@@ -16,9 +16,6 @@ module Msf::DBManager::Connection
     begin
       # Migrate the database, if needed
       migrate
-
-      # Set the default workspace
-      #framework.db.workspace = framework.db.default_workspace
     rescue ::Exception => exception
       self.error = exception
       elog("DB.connect threw an exception: #{exception}")
