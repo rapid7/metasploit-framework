7afefe07a605515967053335a90f60a8432ef369
Brent Cook <busterb@gmail.com>
2018-04-24 06:32:54 -0500

    aliases was not being copied, dup it

--- lib/msf/core/data_store.rb
+++ lib/msf/core/data_store.rb
@@ -372,7 +372,7 @@ class ModuleDataStore < DataStore
     self.keys.each do |k|
       clone.import_option(k, self[k].kind_of?(String) ? self[k].dup : self[k], @imported[k], @imported_by[k])
     end
-    clone.aliases = self.aliases
+    clone.aliases = self.aliases.dup
     clone
   end
 end
