8fd7448e48f6929229fc8a03f1cf3de55218d3ca
Brent Cook <busterb@gmail.com>
2018-04-27 14:18:54 -0500

    bump payloads, ipv6 channel fixes

--- modules/payloads/singles/python/meterpreter_bind_tcp.rb
+++ modules/payloads/singles/python/meterpreter_bind_tcp.rb
@@ -11,7 +11,7 @@ require 'msf/base/sessions/meterpreter_python'
 
 module MetasploitModule
 
-  CachedSize = 71178
+  CachedSize = 71634
 
   include Msf::Payload::Single
   include Msf::Payload::Python
