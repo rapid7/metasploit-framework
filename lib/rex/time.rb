fcc38b75bff0f135bd3c0a76688845f553e13bbd
Brent Cook <busterb@gmail.com>
2018-04-30 18:40:07 -0500

    style updates

--- lib/rex/time.rb
+++ lib/rex/time.rb
@@ -1,63 +1,59 @@
 # -*- coding: binary -*-
 module Rex
-
-###
-#
-# Extended time related functions.
-#
-###
-module ExtTime
-
+  ###
   #
-  # Convert seconds to a string that is broken down into years, days, hours,
-  # minutes, and second.
+  # Extended time related functions.
   #
-  def self.sec_to_s(seconds)
-    parts = [ 31536000, 86400, 3600, 60, 1 ].map { |d|
-      if ((c = seconds / d) > 0)
-        seconds -= c.truncate * d
-        c.truncate
-      else
-        0
+  ###
+  module ExtTime
+    #
+    # Convert seconds to a string that is broken down into years, days, hours,
+    # minutes, and second.
+    #
+    def self.sec_to_s(seconds)
+      parts = [31536000, 86400, 3600, 60, 1].map do |d|
+        if (c = seconds / d) > 0
+          seconds -= c.truncate * d
+          c.truncate
+        else
+          0
+        end
       end
-    }.reverse
 
-    str = ''
+      str = ''
 
-    [ "sec", "min", "hour", "day", "year" ].each_with_index { |name, idx|
-      next if (!parts[idx] or parts[idx] == 0)
+      ['year', 'day', 'hour', 'min', 'sec'].each_with_index do |name, idx|
+        next if !parts[idx] || parts[idx].zero?
 
-      str = "#{parts[idx]} #{name + ((parts[idx] != 1) ? 's' :'')} " + str
-    }
-
-    str.empty? ? "0 secs" : str.strip
-  end
-
-  #
-  # Converts a string in the form n years g days x hours y mins z secs.
-  #
-  def self.str_to_sec(str)
-    fields = str.split
-    secs   = 0
-
-    fields.each_with_index do |f, idx|
-      case f
-      when 'year', 'years'
-        secs += 31536000 * fields[idx - 1].to_i
-      when 'day', 'days'
-        secs += 86400 * fields[idx - 1].to_i
-      when 'hour', 'hours'
-        secs += 3600 * fields[idx - 1].to_i
-      when 'min', 'mins'
-        secs += 60 * fields[idx - 1].to_i
-      when 'sec', 'secs'
-        secs += 1 * fields[idx - 1].to_i
+        str << "#{parts[idx]} #{name + ((parts[idx] != 1) ? 's' : '')} "
       end
+
+      str.empty? ? "0 secs" : str.strip
     end
 
-    secs
+    #
+    # Converts a string in the form n years g days x hours y mins z secs.
+    #
+    def self.str_to_sec(str)
+      fields = str.split
+      secs   = 0
+
+      fields.each_with_index do |f, idx|
+        case f
+        when 'year', 'years'
+          secs += 31536000 * fields[idx - 1].to_i
+        when 'day', 'days'
+          secs += 86400 * fields[idx - 1].to_i
+        when 'hour', 'hours'
+          secs += 3600 * fields[idx - 1].to_i
+        when 'min', 'mins'
+          secs += 60 * fields[idx - 1].to_i
+        when 'sec', 'secs'
+          secs += 1 * fields[idx - 1].to_i
+        end
+      end
+
+      secs
+    end
   end
-
-end
-
 end
