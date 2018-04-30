a14892774f2756ea4419f045c8b655089627f463
Brent Cook <busterb@gmail.com>
2018-04-30 15:14:12 -0500

    Land #9942, IPv6 channel fixes for Python and Linux/macOS Meterpreters

--- modules/payloads/singles/linux/zarch/meterpreter_reverse_http.rb
+++ modules/payloads/singles/linux/zarch/meterpreter_reverse_http.rb
@@ -10,7 +10,7 @@ require 'msf/base/sessions/meterpreter_zarch_linux'
 
 module MetasploitModule
 
-  CachedSize = 1071704
+  CachedSize = 1071752
 
   include Msf::Payload::Single
   include Msf::Sessions::MeterpreterOptions
