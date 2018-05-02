a7ec7b52b76d3f6c655b77f8005d599e0ca65636
christopher lee <christopher_lee@rapid7.com>
2018-05-02 14:59:02 -0500

    Fix error on data server due to unexpected report_exploit call

--- lib/metasploit/framework/data_service/proxy/host_data_proxy.rb
+++ lib/metasploit/framework/data_service/proxy/host_data_proxy.rb
@@ -25,7 +25,7 @@ module HostDataProxy
     begin
       data_service = self.get_data_service()
       data_service.get_host(opts)
-    rescue Exception => e
+    rescue e
       self.log_error(e, "Problem retrieving host")
     end
   end
