94de5a4e42cfb1dc0fdfc1233707f71b6d9a628b
christopher lee <christopher_lee@rapid7.com>
2018-05-03 14:20:32 -0500

    Add skip message, add event test

--- spec/support/shared/examples/msf/db_manager/host_tag.rb
+++ spec/support/shared/examples/msf/db_manager/host_tag.rb
@@ -1,7 +1,7 @@
 RSpec.shared_examples_for 'Msf::DBManager::HostTag' do
 
   if ENV['REMOTE_DB']
-    before {skip("")}
+    before {skip("Awaiting host tag port")}
   end
 
   it { is_expected.to respond_to :report_host_tag }
