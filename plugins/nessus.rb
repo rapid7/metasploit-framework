d0941e847b900f9a816efcca75c5b94e6b29a2e4
Auxilus <raghavsphadke@gmail.com>
2018-05-21 09:06:00 +0530

    return PLUGIN_DESCRIPTION instead of actual string

--- plugins/nessus.rb
+++ plugins/nessus.rb
@@ -12,9 +12,9 @@ module Msf
     def name
       PLUGIN_NAME
     end
-
+      PLUGIN_DESCRIPTION
     def desc
-      "Nessus Bridge for Metasploit"
+      
     end
 
     class ConsoleCommandDispatcher
