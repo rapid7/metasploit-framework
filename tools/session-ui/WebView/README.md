41196730c8b4a10dc9083fd5fc394b2298749713
Rahul Dhawan <rahuldhawan291@gmail.com>
2018-05-27 19:45:58 -0400

    Added a Template of Meterpreter WebView. Other Components will be added to this template soon

---
+++ tools/session-ui/WebView/README.md
@@ -0,0 +1,56 @@
+# CoolAdmnin Bootstrap 4.1 Admin Dashboard Template
+**CoolAdmnin** is a responsive Bootstrap 4.1 admin template. It provides you with a collection of ready to use code snippets and utilities, custom pages, loads of charts, 4 different dashboard variations, a collection of applications and some useful widgets. Preview of this awesome admin template available here: (soon)
+
+# Preview
+
+### Screenshot
+
+![CoolAdmin admin dashboard template preview](https://colorlib.com/wp/wp-content/uploads/sites/2/Dashboard-v2.jpg)
+
+### Demo Site: [Sooon]
+
+### TOC
+- [Built With](#built-with)
+- [Changelog](#changelog)
+- [Authors](#authors)
+- [License](#license)
+
+### Built With
+
+- [Accordion.JS](https://github.com/awps/Accordion.JS)
+- [animsition](http://blivesta.github.io/animsition)
+- [bootstrap-progressbar](https://github.com/minddust/bootstrap-progressbar)
+- [Bootstrap](http://getbootstrap.com/)
+- [Chart.js](http://www.chartjs.org/)
+- [Counter-Up](https://github.com/bfintal/Counter-Up)
+- [CSS Hamburgers](https://github.com/jonsuh/hamburgers)
+- [Fontawesome](http://fontawesome.io/)
+- [jquery-circle-progress](http://kottenator.github.io/jquery-circle-progress/)
+- [jQuery](https://jquery.com/)
+- [JQVMap](https://jqvmap.com/)
+- [Lightbox2](https://github.com/lokesh/lightbox2)
+- [Material Design Icons](https://github.com/Templarian/MaterialDesign-Webfont)
+- [perfect-scrollbar](https://github.com/utatti/perfect-scrollbar)
+- [ProgressBar.js](https://github.com/kimmobrunfeldt/progressbar.js)
+- [Select2](https://github.com/select2/select2)
+- [Slick](http://kenwheeler.github.io/slick/)
+- [Sweetalwer](https://github.com/sweetalert2/sweetalert2)
+- [The Final Countdown for jQuery](http://hilios.github.io/jQuery.countdown/)
+- [WOW](https://github.com/matthieua/WOW)
+
+### Changelog
+#### V 1.0.0
+Initial Release
+### Authors
+[Colorlib](https://colorlib.com)
+
+### More info
+- [Boostrap dashboards](https://colorlib.com/wp/free-bootstrap-admin-dashboard-templates/)
+- [Angular dashboards](https://colorlib.com/wp/angularjs-admin-templates/)
+- [Free Admin Dashboards](https://colorlib.com/wp/free-html5-admin-dashboard-templates/)
+- [Website Templates](https://colorlib.com/wp/templates/)
+- [Free WordPress Themes](https://colorlib.com/wp/free-wordpress-themes/)
+
+### License
+
+CoolAdmin is licensed under The MIT License (MIT). Which means that you can use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the final products. But you always need to state that Colorlib is the original author of this template.
---
+++ tools/session-ui/WebView/resources/slick/slick-theme.css
@@ -0,0 +1,204 @@
+@charset 'UTF-8';
+/* Slider */
+.slick-loading .slick-list
+{
+    background: #fff url('./ajax-loader.gif') center center no-repeat;
+}
+
+/* Icons */
+@font-face
+{
+    font-family: 'slick';
+    font-weight: normal;
+    font-style: normal;
+
+    src: url('./fonts/slick.eot');
+    src: url('./fonts/slick.eot?#iefix') format('embedded-opentype'), url('./fonts/slick.woff') format('woff'), url('./fonts/slick.ttf') format('truetype'), url('./fonts/slick.svg#slick') format('svg');
+}
+/* Arrows */
+.slick-prev,
+.slick-next
+{
+    font-size: 0;
+    line-height: 0;
+
+    position: absolute;
+    top: 50%;
+
+    display: block;
+
+    width: 20px;
+    height: 20px;
+    padding: 0;
+    -webkit-transform: translate(0, -50%);
+    -ms-transform: translate(0, -50%);
+    transform: translate(0, -50%);
+
+    cursor: pointer;
+
+    color: transparent;
+    border: none;
+    outline: none;
+    background: transparent;
+}
+.slick-prev:hover,
+.slick-prev:focus,
+.slick-next:hover,
+.slick-next:focus
+{
+    color: transparent;
+    outline: none;
+    background: transparent;
+}
+.slick-prev:hover:before,
+.slick-prev:focus:before,
+.slick-next:hover:before,
+.slick-next:focus:before
+{
+    opacity: 1;
+}
+.slick-prev.slick-disabled:before,
+.slick-next.slick-disabled:before
+{
+    opacity: .25;
+}
+
+.slick-prev:before,
+.slick-next:before
+{
+    font-family: 'slick';
+    font-size: 20px;
+    line-height: 1;
+
+    opacity: .75;
+    color: white;
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+.slick-prev
+{
+    left: -25px;
+}
+[dir='rtl'] .slick-prev
+{
+    right: -25px;
+    left: auto;
+}
+.slick-prev:before
+{
+    content: '←';
+}
+[dir='rtl'] .slick-prev:before
+{
+    content: '→';
+}
+
+.slick-next
+{
+    right: -25px;
+}
+[dir='rtl'] .slick-next
+{
+    right: auto;
+    left: -25px;
+}
+.slick-next:before
+{
+    content: '→';
+}
+[dir='rtl'] .slick-next:before
+{
+    content: '←';
+}
+
+/* Dots */
+.slick-dotted.slick-slider
+{
+    margin-bottom: 30px;
+}
+
+.slick-dots
+{
+    position: absolute;
+    bottom: -25px;
+
+    display: block;
+
+    width: 100%;
+    padding: 0;
+    margin: 0;
+
+    list-style: none;
+
+    text-align: center;
+}
+.slick-dots li
+{
+    position: relative;
+
+    display: inline-block;
+
+    width: 20px;
+    height: 20px;
+    margin: 0 5px;
+    padding: 0;
+
+    cursor: pointer;
+}
+.slick-dots li button
+{
+    font-size: 0;
+    line-height: 0;
+
+    display: block;
+
+    width: 20px;
+    height: 20px;
+    padding: 5px;
+
+    cursor: pointer;
+
+    color: transparent;
+    border: 0;
+    outline: none;
+    background: transparent;
+}
+.slick-dots li button:hover,
+.slick-dots li button:focus
+{
+    outline: none;
+}
+.slick-dots li button:hover:before,
+.slick-dots li button:focus:before
+{
+    opacity: 1;
+}
+.slick-dots li button:before
+{
+    font-family: 'slick';
+    font-size: 6px;
+    line-height: 20px;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 20px;
+    height: 20px;
+
+    content: '•';
+    text-align: center;
+
+    opacity: .25;
+    color: black;
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+.slick-dots li.slick-active button:before
+{
+    opacity: .75;
+    color: black;
+}
