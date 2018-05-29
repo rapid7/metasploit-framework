3948a0a62e189c05fd048afe5b32ff4cf434bbf8
christopher lee <christopher_lee@rapid7.com>
2018-05-29 10:50:33 -0500

    Use JSON instead of PSTORE for module metadata

--- lib/msf/core/modules/metadata/cache.rb
+++ lib/msf/core/modules/metadata/cache.rb
@@ -35,10 +35,10 @@ class Cache
   #  Returns the module data cache, but first ensures all the metadata is loaded
   #
   def get_metadata
-    @mutex.synchronize {
+   @mutex.synchronize {
       wait_for_load
       @module_metadata_cache.values
-    }
+   }
   end
 
   #
@@ -146,6 +146,7 @@ class Cache
   end
 
   def initialize
+    super
     @mutex = Mutex.new
     @module_metadata_cache = {}
     @store_loaded = false
