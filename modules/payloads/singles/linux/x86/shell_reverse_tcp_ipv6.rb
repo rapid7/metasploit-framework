267d451ba239e0800d6d07e9893ebfe011e2e5a0
Matteo Malvica <matteo@malvica.com>
2018-05-31 16:26:08 +0200

    Update shell_reverse_tcp_ipv6.rb

--- modules/payloads/singles/linux/x86/shell_reverse_tcp_ipv6.rb
+++ modules/payloads/singles/linux/x86/shell_reverse_tcp_ipv6.rb
@@ -34,7 +34,7 @@ def generate_stage(opts={})
       tcp_port = temp.unpack('H*')
       order = (order = [3, 2, 1, 0])
       my_ipv6 = IPAddr.new(datastore['LHOST']).hton.scan(/..../)
-      first = (my_ipv5[0].unpack('H*')).to_s.scan(/../)
+      first = (my_ipv6[0].unpack('H*')).to_s.scan(/../)
       first.pop
       first.shift
       first = (order.map{|x| first[x]}).join('')
