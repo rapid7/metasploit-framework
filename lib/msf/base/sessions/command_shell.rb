28f5e8e5adec5c7df95284cf5d210fee33147f0c
WangYihang <wangyihanger@gmail.com>
2018-06-05 11:46:31 +0800

    Implementation of background meta command (Not prefect)

--- lib/msf/base/sessions/command_shell.rb
+++ lib/msf/base/sessions/command_shell.rb
@@ -65,11 +65,73 @@ class CommandShell
     "Command shell"
   end
 
+
+  def commands
+    c = {
+        # 'help'            => 'Help menu',
+        'background'   => 'Backgrounds the current shell session',
+        # 'close'        => 'Closes a channel',
+        # 'channel'      => 'Displays information or control active channels',
+        # 'exit'         => 'Terminate the meterpreter session',
+        # 'help'         => 'Help menu',
+        # 'irb'          => 'Drop into irb scripting mode',
+        # 'use'          => 'Deprecated alias for "load"',
+        # 'load'         => 'Load one or more meterpreter extensions',
+        # 'machine_id'   => 'Get the MSF ID of the machine attached to the session',
+        # 'guid'         => 'Get the session GUID',
+        # 'quit'         => 'Terminate the meterpreter session',
+        # 'resource'     => 'Run the commands stored in a file',
+        # 'uuid'         => 'Get the UUID for the current session',
+        # 'read'         => 'Reads data from a channel',
+        # 'run'          => 'Executes a meterpreter script or Post module',
+        # 'bgrun'        => 'Executes a meterpreter script as a background thread',
+        # 'bgkill'       => 'Kills a background meterpreter script',
+        # 'get_timeouts' => 'Get the current session timeout values',
+        # 'set_timeouts' => 'Set the current session timeout values',
+        # 'sessions'     => 'Quickly switch to another session',
+        # 'bglist'       => 'Lists running background scripts',
+        # 'write'        => 'Writes data to a channel',
+        # 'enable_unicode_encoding'  => 'Enables encoding of unicode strings',
+        # 'disable_unicode_encoding' => 'Disables encoding of unicode strings'
+    }
+
+  end
+
+  #
+  # Parse a line into an array of arguments.
+  #
+  def parse_line(line)
+    line.split(' ')
+  end
+
+
   #
   # Explicitly runs a command.
   #
   def run_cmd(cmd)
-    shell_command(cmd)
+    arguments = parse_line(cmd)
+    method    = arguments.shift
+    built_in_command_found = false
+
+    # Built-in command
+    if self.commands.has_key?(method)
+      return self.run_command(method, arguments)
+    end
+
+    # User input is not a built-in command, write to socket directly
+    shell_write(cmd)
+  end
+
+
+  def cmd_background()
+    if (prompt_yesno("Background session #{name}?") == true)
+      self.interacting = false
+    end
+  end
+
+  def run_command(method, arguments)
+    # Dynamic function call
+    self.send('cmd_' + method, *arguments)
   end
 
   #
@@ -324,7 +386,8 @@ protected
       sd = Rex::ThreadSafe.select([ _local_fd ], nil, [_local_fd], 5.0)
 
       # Write input to the ring's input mechanism
-      shell_write(user_input.gets) if sd
+      # shell_write(user_input.gets) if sd
+      run_cmd(user_input.gets) if sd
     end
 
     ensure
