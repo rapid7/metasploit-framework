66c2c3a78bb13e3353ed1c3ef96a8691b81dd065
WangYihang <wangyihanger@gmail.com>
2018-06-06 13:14:38 +0800

    [-] Remove useless commented lines

--- lib/msf/base/sessions/command_shell.rb
+++ lib/msf/base/sessions/command_shell.rb
@@ -66,35 +66,14 @@ class CommandShell
   end
 
 
+  #
+  # List of supported commands.
+  #
   def commands
     c = {
-        # 'help'            => 'Help menu',
         'background'   => 'Backgrounds the current shell session',
-        # 'close'        => 'Closes a channel',
-        # 'channel'      => 'Displays information or control active channels',
-        # 'exit'         => 'Terminate the meterpreter session',
-        # 'help'         => 'Help menu',
-        # 'irb'          => 'Drop into irb scripting mode',
-        # 'use'          => 'Deprecated alias for "load"',
-        # 'load'         => 'Load one or more meterpreter extensions',
-        # 'machine_id'   => 'Get the MSF ID of the machine attached to the session',
-        # 'guid'         => 'Get the session GUID',
-        # 'quit'         => 'Terminate the meterpreter session',
-        # 'resource'     => 'Run the commands stored in a file',
-        # 'uuid'         => 'Get the UUID for the current session',
-        # 'read'         => 'Reads data from a channel',
-        # 'run'          => 'Executes a meterpreter script or Post module',
-        # 'bgrun'        => 'Executes a meterpreter script as a background thread',
-        # 'bgkill'       => 'Kills a background meterpreter script',
-        # 'get_timeouts' => 'Get the current session timeout values',
-        # 'set_timeouts' => 'Set the current session timeout values',
         'sessions'     => 'Quickly switch to another session',
-        # 'bglist'       => 'Lists running background scripts',
-        # 'write'        => 'Writes data to a channel',
-        # 'enable_unicode_encoding'  => 'Enables encoding of unicode strings',
-        # 'disable_unicode_encoding' => 'Disables encoding of unicode strings'
     }
-
   end
 
   #
@@ -103,7 +82,6 @@ class CommandShell
   def parse_line(line)
     line.split(' ')
   end
-
 
   #
   # Explicitly runs a command.
@@ -121,7 +99,6 @@ class CommandShell
     # User input is not a built-in command, write to socket directly
     shell_write(cmd)
   end
-
 
   def cmd_background_help()
     print_line "Usage: background"
@@ -144,7 +121,6 @@ class CommandShell
     print_line
   end
 
-
   def cmd_sessions(*args)
     if args.length.zero? || args[0].to_i.zero?
       # No args
@@ -161,7 +137,9 @@ class CommandShell
     end
   end
 
-
+  #
+  # Run built-in command
+  #
   def run_command(method, arguments)
     # Dynamic function call
     self.send('cmd_' + method, *arguments)
