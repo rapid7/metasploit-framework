17471b62922f1653d82c5cdeceffa31859073fa4
Rahul Dhawan <rahuldhawan291@gmail.com>
2018-06-12 03:57:29 +0530

    Fixed javascript issues with dynamic page loading

--- tools/session-ui/public/js/load.js
+++ tools/session-ui/public/js/load.js
@@ -1,228 +1,162 @@
-function postModule()
-{
-    var xhr=new XMLHttpRequest();
-    var url="http://127.0.0.1:3000/post";
-    xhr.open("GET",url,true);
+
+function postModule() {
+    var xhr = new XMLHttpRequest();
+    var url = "http://127.0.0.1:3000/post";
+    xhr.open("GET", url);
     xhr.send();
 
-    xhr.onreadystatechange = function(){
+    xhr.onload = function () {
         var postJson = xhr.response;
-        //var postData = JSON.parse(postJson);
-        if(xhr.readyState===4 && xhr.status===200){
-            var arr= Object.keys(postJson);
-            for(var i=0;i<arr.length;i++)
-            {
+        var postData = JSON.parse(postJson);
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var arr = Object.keys(postData);
+            for (var i = 0; i < arr.length; i++) {
 
-                var text=document.createElement("li");
-                var myList = document.createElement('ul');
+                var text = document.createElement("li");
+                var myList = document.createElement("ul");
+                myList.setAttribute("class","list-unstyled navbar__sub-list js-sub-list");
+                text.setAttribute("class","active has-sub");
+                var anchor= document.createElement("a");
+                anchor.setAttribute("class","js-arrow");
+                anchor.setAttribute("href","#");
 
-                text.textContent=arr[i];
 
-                var post_mod =postJson[arr[i]]; 		// content inside windows,llinux
-                var postmod_arr=Object.keys(post_mod);
+                anchor.textContent = arr[i];
 
-                if(postmod_arr[i]!==0){
-                    for(var j=0;j< postmod_arr.length;j++){
-                        var subList=document.createElement('li');
-                        subList.textContent=postmod_arr[j];
-                        myList.appendChild(subList);
+                var post_mod = postData[arr[i]]; 		// content inside windows,linux
+                var postmod_key = Object.keys(post_mod);
+
+                if (postmod_key[i] != 0) {
+                    for (var j = 0; j < postmod_key.length; j++) {
+                        var subList = document.createElement("li");
+                        subList.setAttribute("class","active has-sub");
+                        var anchor1=document.createElement("a");
+                        anchor1.setAttribute("class","js-arrow");
+                        anchor1.setAttribute("href","#");
+                        subList.appendChild(anchor1);
+                        anchor1.textContent = postmod_key[j];
+                        var value= post_mod[postmod_key[j]];
+                        if(value!=0){
+                            for(var k=0;k<value.length;k++){
+                                var valueSubList=document.createElement("ul");
+                                valueSubList.setAttribute("class","list-unstyled navbar__sub-list js-sub-list");
+                                var valueList=document.createElement("li");
+                                valueList.setAttribute("class","active has-sub");
+                                var anchor2=document.createElement('a');
+                                anchor2.setAttribute("class","js_arrow");
+                                anchor2.setAttribute("href","#");
+                                anchor2.textContent = value[k];
+                                valueList.appendChild(anchor2);
+                                valueSubList.appendChild(valueList);
+
+                                subList.appendChild(valueSubList);
+                                myList.appendChild(subList);
+                            }
+                        }
                     }
                 }
-
+                text.appendChild(anchor);
                 text.appendChild(myList);
                 document.getElementById("post").appendChild(text);
             }
 
-        } else{
-            alert("Unable to Parse the request");
         }
     }
+
+
 }
 
-/*
-
-    var post = document.getElementById("post");
-    var exten = document.getElementById("exten");
-    var exten_url = "127.0.0.1:3000/exten"
-    var post_url = "127.0.0.1:3000/post";
-
-    var xhr1 = new XMLHttpRequest();
+function ExtensionCommand() {
     var xhr2 = new XMLHttpRequest();
-
-    xhr1.open("GET", post_url, true);
-    xhr1.send();
-
-    xhr2.open("GET", exten_url, true);
+    var url = "http://127.0.0.1:3000/exten";
+    xhr2.open("GET", url);
     xhr2.send();
 
-
-    xhr1.onload = function () {
-
-            var postJson = xhr1.response;
-            //var postData = JSON.parse(postJson);
-            postModule(postJson);
-
-    };
-
     xhr2.onload = function () {
+        var extenJson = xhr2.response;
+        var extenData = JSON.parse(extenJson);
+        if (xhr2.readyState === 4 && xhr2.status === 200) {
+            var val = Object.values(extenData);
+            for (var i = 0; i < val.length; i++) {
 
-            var extenJson = xhr2.response;
-            //var extenData = JSON.parse(extenJson);
-            extensionCommand(extenJson);
-
-    };
-
-    function postModule(postJsonObj) {
-        var arr= Object.keys(postJsonObj);
-        for(var i=0;i<arr.length;i++)
-        {
-
-            var text=document.createElement("li");
-            var myList = document.createElement('ul');
-
-            text.textContent=arr[i];
-
-            var post_mod =postJsonObj[arr[i]]; 		// content inside windows,llinux
-            var postmod_arr=Object.keys(post_mod);
-
-            if(postmod_arr[i]!==0){
-                for(var j=0;j< postmod_arr.length;j++){
-                    var subList=document.createElement('li');
-                    subList.textContent=postmod_arr[j];
-                    myList.appendChild(subList);
-                }
+                var list = document.createElement("li");
+                list.setAttribute("class", "active");
+                var ancr = document.createElement("a");
+                ancr.setAttribute("class", "js-arrow");
+                ancr.setAttribute("href", "#");
+                ancr.textContent = val[i];
+                list.appendChild(ancr);
+                document.getElementById("exten").appendChild(list);
             }
 
-            text.appendChild(myList);
-            document.getElementById("post").appendChild(text);
         }
+        else
+            alert("Unable To load Extension Command");
     }
 
-    function extensionCommand(extenJsonObj) {
-        var exdata = extenJsonObj.length;
-        for (var i = 0; i, exdata; i++) {
-            var extList = document.createElement("li");
-            extList.textContent = extenJsonObj[i];
-            exten.appendChild(extList);
-        }
-    }
-*/
-
-
-
-
-/*
-var post=document.getElementById("post");
-var exten=document.getElementById("exten");
-
-var post_url="127.0.0.1:3000/post";
-var exten_url="127.0.0.1:3000/exten";
-var xhr= new XMLHttpRequest();
-xhr.open("GET",post_url,true);
-xhr.open("GET",exten_url,true);
-
-xhr.send();
-
-
-
-
-
- function loadDoc(url,cFunction){
-    xhr.onload= function(){
-      if(this.readyState==4 && this.status==200){
-          var JsonData=xhr.response;
-          var JsonParsed=JSON.parse(JsonData);
-          postModule(JsonParsed);
-     //     cFunction(this);
-          xhr.open("GET",post_url,true);
-          xhr.send();
-      }
-    };
-
-  xhr.responseType="text";
-  xhr.open("GET",url,true);
-  xhr.send();
 }
 
-   function postModule(xhr){
-     var postText=xhr.response;
-     var postJson=JSON.parse(postText);
-
-     var android=postJson[android];
-     var android_list=document.createElement("li");
-     for(var i=0;i<postJson;i++)
-     {
-
-     }
-    var post_list= document.createElement("li");
-    var post_text= document.createTextNode('<a id="runpost" href="#">' + this.responseText + '</a>');
-    post_list.appendChild(post_text);
-  }
-
-  function extension(xhr){
-   var exten_list= document.createElement("li");
-    var exten_text= document.createTextNode('<a id="runexten" href="#">' + this.responseText + '</a>');
-    exten_list.appendChild(exten_text);
-  }
 
 
-*/
 
-  /*----------------- Implementation of web socket for Browser shell.-------------
 
-  var wsuri = "ws://127.0.0.1:3000/soc";
+
+
+/*----------------- Implementation of web socket for Browser shell.-------------
+
+var wsuri = "ws://127.0.0.1:3000/soc";
 var output;
 
 function init()
 {
-  output=document.getElementById("add");
-  testWebSocket();
+output=document.getElementById("add");
+testWebSocket();
 }
 
 function testWebSocket()
 {
-  websocket=new WebSocket(wsuri);
-  websocket.onopen=function(evt){ onOpen(evt) };
-  websocket.onclose=function(evt){ onClose(evt) };
-  websocket.onmessage=function(evt){ onMessage(evt) };
-  websocket.onerror=function(evt) { onError(evt) };
+websocket=new WebSocket(wsuri);
+websocket.onopen=function(evt){ onOpen(evt) };
+websocket.onclose=function(evt){ onClose(evt) };
+websocket.onmessage=function(evt){ onMessage(evt) };
+websocket.onerror=function(evt) { onError(evt) };
 
 }
 
 function onOpen(evt)
 {
-  writeToScreen("CONNECTED !");
-  doSend("WebSocket rocks");
+writeToScreen("CONNECTED !");
+doSend("WebSocket rocks");
 }
 
 function onClose(evt)
 {
-  writeToScreen("DISCONNECTED");
+writeToScreen("DISCONNECTED");
 }
 
 function onMessage(evt)
 {
-  writeToScreen('<span style="color: blue;"> Response : ' + evt.data + '</span>');
-  websocket.close();
+writeToScreen('<span style="color: blue;"> Response : ' + evt.data + '</span>');
+websocket.close();
 }
 
 function onError(evt)
 {
-  writeToScreen('<span style="color:red;">ERROR: </span>' + evt.data)
+writeToScreen('<span style="color:red;">ERROR: </span>' + evt.data)
 }
 
 function doSend(message)
 {
-  writeToScreen("SENT: " + message);
-  websocket.send(message);
+writeToScreen("SENT: " + message);
+websocket.send(message);
 }
 
 function writeToScreen(message)
 {
-  var pre=document.createElement("p");
-  pre.style.wordWrap = 'break-word';
-  pre.innerHTML=message;
-  output.appendChild(pre);
+var pre=document.createElement("p");
+pre.style.wordWrap = 'break-word';
+pre.innerHTML=message;
+output.appendChild(pre);
 }
 
 window.addEventListener("load",init,false)
