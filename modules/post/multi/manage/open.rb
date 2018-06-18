8342751b05e74e4cbb3e0945370c53c3f59f69d9
Eliott Teissonniere <10683430+DeveloppSoft@users.noreply.github.com>
2018-06-18 06:54:13 +0000

    Move xdg_open to multi module

---
+++ modules/post/multi/manage/open.rb
@@ -0,0 +1,73 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+class MetasploitModule < Msf::Post
+  include Msf::Post::File
+
+  def initialize(info={})
+    super( update_info( info,
+      'Name'          => 'Open a file or URL on the target computer',
+      'Description'   => %q{
+        This module will open any file or URL specified with the URI format on the
+        target computer via the embedded commands such as 'open' or 'xdg-open'.
+      },
+      'License'       => MSF_LICENSE,
+      'Author'        => [ 'Eliott Teissonniere'],
+      'Platform'      => [ 'osx', 'linux' ],
+      'SessionTypes'  => [ 'shell', 'meterpreter' ]
+    ))
+
+    register_options(
+      [
+        OptString.new('URI', [true, 'URI path to open'])
+      ])
+  end
+
+  #
+  # The OSX version simply uses 'open'
+  #
+  def osx_open(uri)
+    begin
+      cmd_exec("open #{uri}")
+    rescue EOFError
+      return false
+    end
+
+    true
+  end
+
+  #
+  # The Linux version relies on 'xdg-open'
+  #
+  def linux_open(uri)
+    begin
+      cmd_exec("xdg-open #{uri}")
+    rescue EOFError
+      return false
+    end
+
+    true
+  end
+
+  def open(uri)
+    case session.platform
+    when 'osx'
+      return osx_open(uri)
+    when 'linux'
+      return linux_open(uri)
+    end
+  end
+
+  def run
+    uri = datastore['URI']
+
+    print_status("Opening #{uri}")
+    if open(uri)
+      print_good('Success')
+    else
+      print_error('Command failed')
+    end
+  end
+end
