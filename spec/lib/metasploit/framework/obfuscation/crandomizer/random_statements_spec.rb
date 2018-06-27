ede98ff8db4ae94430d6ae159c5728d67af745f9
Wei Chen <wei_chen@rapid7.com>
2018-06-27 17:10:35 -0500

    Update random_statements_spec

--- spec/lib/metasploit/framework/obfuscation/crandomizer/random_statements_spec.rb
+++ spec/lib/metasploit/framework/obfuscation/crandomizer/random_statements_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Metasploit::Framework::Obfuscation::CRandomizer::RandomStatements
 
   subject(:random_statements) do
     parser = Metasploit::Framework::Obfuscation::CRandomizer::Utility.parse(c_source_code)
-    fake_function_size = rand(0..3)
+    fake_function_size = rand(1..3)
     fake_function_collection = Metasploit::Framework::Obfuscation::CRandomizer::CodeFactory::FakeFunctionCollection.new(fake_function_size)
     described_class.new(parser, fake_function_collection)
   end
@@ -36,6 +36,38 @@ RSpec.describe Metasploit::Framework::Obfuscation::CRandomizer::RandomStatements
   describe '#get' do
     it 'returns an array' do
       expect(subject.get.class).to eq(Array)
+    end
+  end
+
+  describe '#make_func_arg_str' do
+    it 'returns the argument string' do
+      fake_function = subject.fake_function_collection.sample
+      fake_function_name = fake_function.var.name
+      fake_function_args = fake_function.var.type.args
+      s = subject.send(:make_func_arg_str, fake_function_args)
+      expect(s).to match(/(.+)/)
+    end
+  end
+
+  describe '#make_func_declare_arg_str' do
+    it 'returns the function declaration argument string' do
+      fake_function = subject.fake_function_collection.sample
+      fake_function_name = fake_function.var.name
+      fake_function_args = fake_function.var.type.args
+      s = subject.send(:make_func_declare_arg_str, fake_function_args)
+      expect(s).to match(/(.+)/)
+    end
+  end
+
+  describe '#get_random_statements' do
+    it 'returns an array' do
+      expect(subject.send(:get_random_statements).class).to eq(Array)
+    end
+  end
+
+  describe '#get_random_function_call' do
+    it 'returns a function call' do
+      expect(subject.send(:get_random_function_call).class).to eq(Array)
     end
   end
 end
