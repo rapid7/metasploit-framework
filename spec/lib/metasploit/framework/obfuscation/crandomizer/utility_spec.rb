752192bfad8d9f90d81692952ec4ed6b4b5ff1f4
Wei Chen <wei_chen@rapid7.com>
2018-06-27 00:13:56 -0500

    Starting rspec

---
+++ spec/lib/metasploit/framework/obfuscation/crandomizer/utility_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require 'metasploit/framework/obfuscation/crandomizer/utility'
+
+RSpec.describe Metasploit::Framework::Obfuscation::CRandomizer::Utility do
+
+  describe '#self.rand_int' do
+  end
+
+  describe '#self.rand_string' do
+  end
+
+  describe '#self.parse' do
+  end
+
+end
