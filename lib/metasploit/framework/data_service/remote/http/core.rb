ed34cd88982831bd5c51a10ac53bfdf47925c02e
Matthew Kienow <matthew_kienow@rapid7.com>
2018-07-03 17:59:57 -0400

    Simplify request headers assignment

--- lib/metasploit/framework/data_service/remote/http/core.rb
+++ lib/metasploit/framework/data_service/remote/http/core.rb
@@ -26,11 +26,13 @@ class RemoteHTTPDataService
   def initialize(endpoint, opts = {})
     validate_endpoint(endpoint)
     @endpoint = URI.parse(endpoint)
-    @opts = opts
-    @https_opts = @opts.delete(:https_opts)
-    @api_token = @opts.delete(:api_token)
-    build_client_pool(5)
+    @https_opts = opts[:https_opts]
+    @api_token = opts[:api_token]
+
+    @headers = {}
     set_header('Authorization', "Bearer #{@api_token}") unless @api_token.nil?
+
+    build_client_pool(5)
   end
 
   def connection_established?
@@ -150,13 +152,13 @@ class RemoteHTTPDataService
       client = @client_pool.pop
       case request_type
         when GET_REQUEST
-          request = Net::HTTP::Get.new(uri.request_uri)
+          request = Net::HTTP::Get.new(uri.request_uri, initheader=@headers)
         when POST_REQUEST
-          request = Net::HTTP::Post.new(uri.request_uri)
+          request = Net::HTTP::Post.new(uri.request_uri, initheader=@headers)
         when DELETE_REQUEST
-          request = Net::HTTP::Delete.new(uri.request_uri)
+          request = Net::HTTP::Delete.new(uri.request_uri, initheader=@headers)
         when PUT_REQUEST
-          request = Net::HTTP::Put.new(uri.request_uri)
+          request = Net::HTTP::Put.new(uri.request_uri, initheader=@headers)
         else
           raise Exception, 'A request_type must be specified'
       end
@@ -182,8 +184,6 @@ class RemoteHTTPDataService
   end
 
   def set_header(key, value)
-    @headers = {} if @headers.nil?
-
     @headers[key] = value
   end
 
@@ -257,12 +257,6 @@ class RemoteHTTPDataService
       end
       json_body = data_hash.to_json
       request.body = json_body
-    end
-
-    if !@headers.nil? && !@headers.empty?
-      @headers.each do |key, value|
-        request[key] = value
-      end
     end
 
     request
