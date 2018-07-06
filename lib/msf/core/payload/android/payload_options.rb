76da853cbdd0023c26fdc45bab38dc6764e28eec
Tim W <timrlw@gmail.com>
2018-07-06 18:10:05 +0800

    set AndroidWakelock=true by default

--- lib/msf/core/payload/android/payload_options.rb
+++ lib/msf/core/payload/android/payload_options.rb
@@ -8,9 +8,9 @@ module Msf::Payload::Android::PayloadOptions
     super(info)
     register_advanced_options(
       [
-        Msf::OptBool.new('AndroidMeterpreterDebug', [ false, "Run the payload in debug mode, with logging enabled" ]), 
-        Msf::OptBool.new('AndroidWakelock', [ false, "Acquire a wakelock before starting the payload" ]), 
-        Msf::OptBool.new('AndroidHideAppIcon', [ false, "Hide the application icon automatically after launch" ]), 
+        Msf::OptBool.new('AndroidMeterpreterDebug', [ false, "Run the payload in debug mode, with logging enabled" ]),
+        Msf::OptBool.new('AndroidWakelock', [ false, "Acquire a wakelock before starting the payload", true ]),
+        Msf::OptBool.new('AndroidHideAppIcon', [ false, "Hide the application icon automatically after launch" ]),
       ]
     )
   end
