b8ae4f5d127ba0003445d7cbc71e9a6325c8e66f
James Barnett <James_Barnett@rapid7.com>
2018-07-11 17:42:55 -0500

    Update rspec tests for creds and add stubs

---
+++ lib/metasploit/framework/data_service/stubs/login_data_service.rb
@@ -0,0 +1,14 @@
+module LoginDataService
+
+  def logins(opts)
+    raise 'LoginDataService#logins is not implemented'
+  end
+
+  def create_credential_login(opts)
+    raise 'LoginDataService#create_credential_login is not implemented'
+  end
+
+  def update_login(hosts)
+    raise 'LoginDataService#update_login is not implemented'
+  end
+end
