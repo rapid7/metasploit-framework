71e25f14942cbb34f00f101970e8d43e09189422
Auxilus <raghavsphadke@gmail.com>
2018-07-14 16:32:38 +0530

    show error if server.pid == pid
    This PR adds changes to catch error when `server.pid` and `pid` are the same

--- lib/rex/post/meterpreter/ui/console/command_dispatcher/core.rb
+++ lib/rex/post/meterpreter/ui/console/command_dispatcher/core.rb
@@ -1113,7 +1113,11 @@ class Console::CommandDispatcher::Core
         print_status("#{existing_relays.length} TCP relay(s) removed.")
       end
     end
-
+    
+    if server.pid == pid
+      print_error("Process already running at PID #{pid}")
+      return
+    end
     server ? print_status("Migrating from #{server.pid} to #{pid}...") : print_status("Migrating to #{pid}")
 
     # Do this thang.
