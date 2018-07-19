462655dea11655d9d51401b88c0d9402f13416c7
Erin Bleiweiss <erin_bleiweiss@rapid7.com>
2018-07-19 14:10:04 -0500

    update response example documentation

--- documentation/api/v1/module_api_doc.rb
+++ documentation/api/v1/module_api_doc.rb
@@ -43,65 +43,65 @@ module ModuleApiDoc
       'post',
       ''
   ]
-  APP_ENUM = [
-      'client',
-      'server',
-      ''
-  ]
-  FIELDS_ENUM = [
-      'name',
-      'full_name',
-      'disclosure_date',
-      'rank',
-      'type',
-      'description',
-      'author',
-      'references',
-      'is_server',
-      'is_client',
-      'platform',
-      'arch',
-      'rport',
-      'mod_time',
-      'ref_name',
-      'path',
-      'is_install_path',
-      'targets',
-      ''
-  ]
 
-  APP_EXAMPLE = 'server'
-  AUTHOR_EXAMPLE = 'wvu'
-  BID_EXAMPLE = 'BID-36075'
-  CVE_EXAMPLE = 'CVE-2017'
-  EDB_EXAMPLE = 'EDB-24453'
-  NAME_EXAMPLE = 'eternalblue'
-  PATH_EXAMPLE = 'eternalblue'
-  PLATFORM_EXAMPLE = 'android'
-  PORT_EXAMPLE = '80'
-  REF_EXAMPLE = 'CVE-2017'
-  TEXT_EXAMPLE = 'eternalblue'
+  NAME_EXAMPLE = 'MS17-010 EternalBlue SMB Remote Windows Kernel Pool Corruption'
+  FULL_NAME_EXAMPLE = 'exploit/windows/smb/ms17_010_eternalblue'
+  DISCLOSURE_DATE_EXAMPLE = '2017-03-14T00:00:00.000-05:00'
+  RANK_EXAMPLE = '200'
   TYPE_EXAMPLE = 'exploit'
-  FIELDS_EXAMPLE = 'full_name,type,platform,references'
-
-
+  DESCRIPTION_EXAMPLE = 'This module is a port of the Equation Group ETERNALBLUE exploit, part of\n        the FuzzBunch toolkit released by Shadow Brokers.\n\n        There is a buffer overflow memmove operation in Srv!SrvOs2FeaToNt. The size\n        is calculated in Srv!SrvOs2FeaListSizeToNt, with mathematical error where a\n        DWORD is subtracted into a WORD. The kernel pool is groomed so that overflow\n        is well laid-out to overwrite an SMBv1 buffer. Actual RIP hijack is later\n        completed in srvnet!SrvNetWskReceiveComplete.\n\n        This exploit, like the original may not trigger 100% of the time, and should be\n        run continuously until triggered. It seems like the pool will get hot streaks\n        and need a cool down period before the shells rain in again.\n\n        The module will attempt to use Anonymous login, by default, to authenticate to perform the\n        exploit. If the user supplies credentials in the SMBUser, SMBPass, and SMBDomain options it will use\n        those instead.\n\n        On some systems, this module may cause system instability and crashes, such as a BSOD or\n        a reboot. This may be more likely with some payloads.'
+  AUTHOR_EXAMPLE = [
+      'Sean Dillon <sean.dillon@risksense.com>',
+      'Dylan Davis <dylan.davis@risksense.com>',
+      'Equation Group',
+      'Shadow Brokers',
+      'thelightcosine'
+  ]
+  REFERENCES_EXAMPLE = [
+      'AKA-ETERNALBLUE',
+      'MSB-MS17-010',
+      'CVE-2017-0143',
+      'CVE-2017-0144',
+      'CVE-2017-0145',
+      'CVE-2017-0146',
+      'CVE-2017-0147',
+      'CVE-2017-0148',
+      'URL-https://github.com/RiskSense-Ops/MS17-010'
+  ]
+  IS_SERVER_EXAMPLE = true
+  IS_CLIENT_EXAMPLE = false
+  PLATFORM_EXAMPLE = 'Windows'
+  ARCH_EXAMPLE = ''
+  RPORT_EXAMPLE = '445'
+  MOD_TIME_EXAMPLE = '2018-07-10T17:39:42.000-05:00'
+  REF_NAME_EXAMPLE = 'windows/smb/ms17_010_eternalblue'
+  PATH_EXAMPLE = '/modules/exploits/windows/smb/ms17_010_eternalblue.rb'
+  IS_INSTALL_PATH_EXAMPLE = true
+  TARGETS_EXAMPLE = [
+      'Windows 7 and Server 2008 R2 (x64) All Service Packs'
+  ]
 
 
   # Swagger documentation for Module model
   swagger_schema :Module do
-    property :app, type: :string, description: APP_DESC, example: APP_EXAMPLE, enum: APP_ENUM
-    property :author, type: :string, description: AUTHOR_DESC, example: AUTHOR_EXAMPLE
-    property :bid, type: :string, description: BID_DESC, example: BID_EXAMPLE
-    property :cve, type: :string, description: CVE_DESC, example: CVE_EXAMPLE
-    property :edb, type: :string, description: EDB_DESC, example: EDB_EXAMPLE
     property :name, type: :string, description: NAME_DESC, example: NAME_EXAMPLE
-    property :path, type: :string, description: PATH_DESC, example: PATH_EXAMPLE
-    property :platform, type: :string, description: PLATFORM_DESC, example: PLATFORM_EXAMPLE
-    property :port, type: :string, description: PORT_DESC, example: PORT_EXAMPLE
-    property :ref, type: :string, description: REF_DESC, example: REF_EXAMPLE
-    property :text, type: :string, description: TEXT_DESC, example: TEXT_EXAMPLE
+    property :full_name, type: :string, description: FULL_NAME_DESC, example: FULL_NAME_EXAMPLE
+    property :disclosure_date, type: :string, description: DISCLOSURE_DATE_EXAMPLE, example: DISCLOSURE_DATE_EXAMPLE
+    property :rank, type: :integer, description: RANK_DESC, example: RANK_EXAMPLE
     property :type, type: :string, description: TYPE_DESC, example: TYPE_EXAMPLE, enum: TYPE_ENUM
-    property :fields, type: :string, description: FIELDS_DESC, example: FIELDS_EXAMPLE, enum: FIELDS_ENUM
+    property :description, type: :string, description: DESCRIPTION_DESC, example: DESCRIPTION_EXAMPLE
+    property :author, description: AUTHOR_DESC, example: AUTHOR_EXAMPLE, type: :array do items type: :string end
+    property :references, description: REFERENCE_DESC, example: REFERENCES_EXAMPLE, type: :array do items type: :string end
+    property :is_client, type: :boolean, description: IS_CLIENT_DESC, example: IS_CLIENT_EXAMPLE
+    property :is_server, type: :boolean, description: IS_SERVER_DESC, example: IS_SERVER_EXAMPLE
+    property :platform, type: :string, description: PLATFORM_DESC, example: PLATFORM_EXAMPLE
+    property :arch, type: :string, description: ARCH_DESC, example: ARCH_EXAMPLE
+    property :rport, type: :string, description: PORT_DESC, example: RPORT_EXAMPLE
+    property :mod_time, type: :string, description: MOD_TIME_DESC, example: MOD_TIME_EXAMPLE
+    property :ref_name, type: :string, description: REF_NAME_DESC, example: REF_NAME_EXAMPLE
+    property :path, type: :string, description: PATH_DESC, example: PATH_EXAMPLE
+    property :is_install_path, type: :boolean, description: IS_INSTALL_PATH_DESC, example: IS_INSTALL_PATH_EXAMPLE
+    property :targets, description: TARGET_DESC, example: TARGETS_EXAMPLE, type: :array do items type: :string end
   end
 
 
