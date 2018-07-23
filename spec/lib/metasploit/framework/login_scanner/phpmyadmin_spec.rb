1473bbd49268de44618b42a3d53a4737f89cbac7
Shelby Pace <shelby_pace@rapid7.com>
2018-07-22 23:37:36 -0500

    created test file for login scanner

---
+++ spec/lib/metasploit/framework/login_scanner/phpmyadmin_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'metasploit/framework/login_scanner/phpmyadmin'
+
+RSpec.describe Metasploit::Framework::LoginScanner::PhpMyAdmin do
+  describe '#check_setup' do
+
+  end
+
+  describe '#get_session_info' do
+
+  end
+
+  describe '#do_login' do
+
+  end
+
+end
