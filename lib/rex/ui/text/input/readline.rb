1949cade784a98c48a4b87cbc0e5482ca502759a
William Vu <William_Vu@rapid7.com>
2018-08-07 17:16:36 -0500

    Land #10430, history clearing and bug fixes

--- lib/rex/ui/text/input/readline.rb
+++ lib/rex/ui/text/input/readline.rb
@@ -176,7 +176,15 @@ begin
 
         line.try(:dup)
       else
-        ::Readline.readline(reset_sequence + prompt, true)
+        # The line that's read is immediately added to history
+        line = ::Readline.readline(reset_sequence + prompt, true)
+
+        # Don't add duplicate lines to history
+        if ::Readline::HISTORY.length > 1 && line == ::Readline::HISTORY[-2]
+          ::Readline::HISTORY.pop
+        end
+
+        line
       end
     end
 
