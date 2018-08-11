7e4a666e1b433916cef5c8fa650510226955e918
Wang Yihang <yihangwang@tencent.com>
2018-08-12 04:54:08 +0800

    [+] Modify script arguments, change LOG_FILE to /dev/null

--- lib/msf/base/sessions/command_shell.rb
+++ lib/msf/base/sessions/command_shell.rb
@@ -243,7 +243,10 @@ class CommandShell
     script_path = binary_exists("script")
     if script_path != nil
       print_status("Using `script` to pop up an interactive shell")
-      shell_command(script_path)
+      # Payload: script /dev/null
+      # Using /dev/null to make sure there is no log file on the target machine
+      # Prevent being detected by the admin or antivirus softwares
+      shell_command("#{script_path} /dev/null")
       return
     end
 
