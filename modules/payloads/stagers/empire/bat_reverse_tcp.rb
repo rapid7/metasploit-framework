d0a2e2098cb1c6e1d6efe340a5bcf85df95782f4
ManasMahapatra <Chirag301212@gmail.com>
2018-08-13 17:53:35 +0530

    Update bat_reverse_tcp.rb

--- modules/payloads/stagers/empire/bat_reverse_tcp.rb
+++ modules/payloads/stagers/empire/bat_reverse_tcp.rb
@@ -21,7 +21,7 @@ module MetasploitModule
     super(merge_info(info,
     'Name'       => 'Empire Stager Module',
     'Description'=> 'Creates a Batch File Payload for Empire',
-    'Author'     => ['author_name'],
+    'Author'     => ['zed009'],
     'License'    => MSF_LICENSE,
     'Platform'   => ['empire'],
     'Arch'       => ARCH_CMD,
