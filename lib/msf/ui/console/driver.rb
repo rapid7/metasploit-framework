ce1fe7fe7735cdd75d039af90a5d1f21218968c8
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2018-08-16 15:37:10 -0500

    Land #10203, Add command for persistent job handler when msf restart

--- lib/msf/ui/console/driver.rb
+++ lib/msf/ui/console/driver.rb
@@ -220,6 +220,22 @@ class Driver < Msf::Ui::Driver
       }
     end
 
+    # Process persistent job handler
+    begin
+      restore_handlers = JSON.parse(File.read(Msf::Config.persist_file))
+    rescue Errno::ENOENT, JSON::ParserError
+      restore_handlers = nil
+    end
+
+    unless restore_handlers.nil?
+      print_status("Starting persistent handler(s)...")
+
+      restore_handlers.each do |handler_opts|
+        handler = framework.modules.create(handler_opts['mod_name'])
+        handler.exploit_simple(handler_opts['mod_options'])
+      end
+    end
+
     # Process any additional startup commands
     if opts['XCommands'] and opts['XCommands'].kind_of? Array
       opts['XCommands'].each { |c|
