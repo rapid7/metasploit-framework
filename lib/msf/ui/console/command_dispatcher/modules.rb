0e594266e9495e4128b43065dde3f3ced1797669
Auxilus <raghavsphadke@gmail.com>
2018-08-20 23:32:34 +0530

    show help when no argument is provided to `show`

--- lib/msf/ui/console/command_dispatcher/modules.rb
+++ lib/msf/ui/console/command_dispatcher/modules.rb
@@ -480,6 +480,12 @@ module Msf
           # no type is provided.
           #
           def cmd_show(*args)
+            if args.empty?
+              print_error("Argument required\n")
+              cmd_show_help
+              return
+            end
+            
             mod = self.active_module
 
             args << "all" if (args.length == 0)
