4ea6477eeb72dc02b6d9d51dc11a148a5ab3b40b
RAMELLA Sébastien <sebastien.ramella@pirates.re>
2018-08-20 15:44:09 +0400

    fix. some errors and syntax corrections

--- modules/auxiliary/gather/behind_cloudflare.rb
+++ modules/auxiliary/gather/behind_cloudflare.rb
@@ -8,7 +8,7 @@ class MetasploitModule < Msf::Auxiliary
   def initialize(info = {})
     super(update_info(info,
       'Name'           => 'Behind CloudFlare',
-      'Version' => '$Release: 1.0.1',
+      'Version'        => '$Release: 1.0.2',
       'Description'    => %q{
         This module can be useful if you need to test
         the security of your server and your website
@@ -18,7 +18,7 @@ class MetasploitModule < Msf::Auxiliary
         'mekhalleh',
         'RAMELLA Sébastien <sebastien.ramella[at]Pirates.RE>'
       ],
-      'References' => [
+      'References'     => [
         ['URL', 'http://www.crimeflare.com/cfs.html'],
         ['URL', 'https://github.com/HatBashBR/HatCloud']
       ],
@@ -50,60 +50,60 @@ class MetasploitModule < Msf::Auxiliary
 
     rescue ::Rex::ConnectionError, Errno::ECONNREFUSED, Errno::ETIMEDOUT
       print_error('HTTP connection failed to Crimflare website.')
-      return(false)
+      return false
     end
 
     html  = response.get_html_document
     unless /No working nameservers are registered/.match(html).nil? then
       print_bad('No working nameservers are registered! :(')
-      return(false)
+      return false
     end
 
-    arIps = Array.new
+    arIps = []
     rows  = html.search('li')
     rows.each_with_index do | row, index |
       date = /(\d*\-\d*\-\d*)/.match(row)
       arIps.push(/(\d*\.\d*\.\d*\.\d*)/.match(row))
-      print_status("  * #{date} | #{arIps[index]}")
+      print_status(" * #{date} | #{arIps[index]}")
     end
 
-    if arIps.empty? then
+    if arIps.empty?
       print_bad('No previous lookups founds.')
-      return(false)
+      return false
     end
 
-    return(arIps)
+    return arIps
   end
 
   def do_check_bypass(fingerprint, host, ip, uri, proxies)
 
     # Check for "misconfigured" web server on TCP/80.
-    if do_check_tcp_port(ip, 80, proxies) then
+    if do_check_tcp_port(ip, 80, proxies)
       response = do_simple_get_request_raw(ip, 80, false, host, uri, proxies)
 
-      if response != false then
+      if response != false
         html = response.get_html_document
-        if fingerprint.to_s.eql? html.at('head').to_s then
+        if fingerprint.to_s.eql? html.at('head').to_s
           print_good("A direct-connect IP address was found: #{ip}")
-          return(true)
+          return true
         end
       end
     end
 
     # Check for "misconfigured" web server on TCP/443.
-    if do_check_tcp_port(ip, 443, proxies) then
+    if do_check_tcp_port(ip, 443, proxies)
       response = do_simple_get_request_raw(ip, 443, true, host, uri, proxies)
 
-      if response != false then
+      if response != false
         html = response.get_html_document
-        if fingerprint.to_s.eql? html.at('head').to_s then
+        if fingerprint.to_s.eql? html.at('head').to_s
           print_good("A direct-connect IP address was found: #{ip}")
-          return(true)
+          return true
         end
       end
     end
 
-    return(false)
+    return false
   end
 
   def do_check_tcp_port(ip, port, proxies)
@@ -114,10 +114,10 @@ class MetasploitModule < Msf::Auxiliary
         'Proxies'  => proxies
       )
     rescue ::Rex::ConnectionRefused, Rex::ConnectionError
-      return(false)
+      return false
     end
     sock.close
-    return(true)
+    return true
   end
 
   ## TODO: Improve on-demand response (send_recv? Timeout).
@@ -126,7 +126,7 @@ class MetasploitModule < Msf::Auxiliary
       http    = Rex::Proto::Http::Client.new(host, port, {}, ssl, nil, proxies)
       http.connect
 
-      unless host_header.eql? nil then
+      unless host_header.eql? nil
         http.set_config({ 'vhost' => host_header })
       end
 
@@ -139,10 +139,10 @@ class MetasploitModule < Msf::Auxiliary
 
     rescue ::Rex::ConnectionError, Errno::ECONNREFUSED, Errno::ETIMEDOUT
       print_error('HTTP Connection Failed')
-      return(false)
+      return false
     end
 
-    return(response)
+    return response
   end
 
   ## TODO: Improve the efficiency by mixing the sources (dnsenum, censys, shodan, ...).
@@ -153,7 +153,7 @@ class MetasploitModule < Msf::Auxiliary
     ip_list     = do_crimflare_request(datastore['HOSTNAME'], datastore['PROXIES'])
     print_status()
 
-    unless ip_list.eql? false then
+    unless ip_list.eql? false
       print_status('Bypass Cloudflare is in progress...')
 
       # Initial HTTP request to the server (for <head> comparison).
@@ -185,7 +185,7 @@ class MetasploitModule < Msf::Auxiliary
       end
     end
 
-    if ret_val.eql? false then
+    if ret_val.eql? false
       print_bad('No direct-connect IP address found :-(')
     end
   end
