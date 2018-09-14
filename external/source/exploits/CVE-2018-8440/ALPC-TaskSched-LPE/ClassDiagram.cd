f38e6f45ceb3fb93bddb09b0e46456fe9050de43
bwatters-r7 <bwatters@rapid7.com>
2018-09-14 17:47:53 -0500

    Redo dllinjection

---
+++ external/source/exploits/CVE-2018-8440/ALPC-TaskSched-LPE/ClassDiagram.cd
@@ -0,0 +1,2 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<ClassDiagram /> 
---
+++ external/source/exploits/CVE-2018-8440/dll/src/stdafx.h
@@ -0,0 +1,15 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+
+
+#define RPC_USE_NATIVE_WCHAR
+
+#include <stdio.h>
+#include <tchar.h>
+#include <Windows.h>
+#include <memory>
