858b67f94319808405faf1979f341005481beed0
William Vu <William_Vu@rapid7.com>
2018-09-20 17:51:50 -0500

    Land #10681, payload cached sizes fix

--- modules/payloads/singles/linux/zarch/meterpreter_reverse_https.rb
+++ modules/payloads/singles/linux/zarch/meterpreter_reverse_https.rb
@@ -10,7 +10,7 @@ require 'msf/base/sessions/meterpreter_zarch_linux'
 
 module MetasploitModule
 
-  CachedSize = 1080824
+  CachedSize = 1080856
 
   include Msf::Payload::Single
   include Msf::Sessions::MeterpreterOptions
