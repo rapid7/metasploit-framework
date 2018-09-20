e32abe9d455353e66ddf29de1d62655d7d5c2ca0
bwatters-r7 <bwatters@rapid7.com>
2018-09-20 17:26:15 -0500

    Update Payload cached sizes

--- modules/payloads/singles/apple_ios/aarch64/meterpreter_reverse_https.rb
+++ modules/payloads/singles/apple_ios/aarch64/meterpreter_reverse_https.rb
@@ -10,7 +10,7 @@ require 'msf/base/sessions/meterpreter_aarch64_apple_ios'
 
 module MetasploitModule
 
-  CachedSize = 694472
+  CachedSize = 694520
 
   include Msf::Payload::Single
   include Msf::Sessions::MeterpreterOptions
