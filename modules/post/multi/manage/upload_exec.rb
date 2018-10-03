e0a664c0cd0a9c1d20ec3e60aeee9579ccee3226
William Vu <William_Vu@rapid7.com>
2018-10-02 21:33:01 -0500

    Improve prints, use FileDropper, and bump TIMEOUT

--- modules/post/multi/manage/upload_exec.rb
+++ modules/post/multi/manage/upload_exec.rb
@@ -5,6 +5,7 @@
 
 class MetasploitModule < Msf::Post
   include Msf::Post::File
+  include Msf::Exploit::FileDropper
 
   def initialize(info = {})
     super(update_info(info,
@@ -20,12 +21,14 @@ class MetasploitModule < Msf::Post
       OptPath.new('LPATH',   [true, 'Local file path to upload and execute']),
       OptString.new('RPATH', [false, 'Remote file path on target (default is basename of LPATH)']),
       OptString.new('ARGS',  [false, 'Command-line arguments to pass to the uploaded file']),
-      OptInt.new('TIMEOUT',  [true, 'Timeout for command execution', 15])
+      OptInt.new('TIMEOUT',  [true, 'Timeout for command execution', 60])
     ])
   end
 
   def run
+    print_status("Uploading #{lpath} to #{rpath}")
     upload_file(rpath, lpath)
+    register_file_for_cleanup(rpath)
 
     if session.platform == 'windows'
       # Don't use cmd.exe /c start so we can fetch output
@@ -38,15 +41,14 @@ class MetasploitModule < Msf::Post
       cmd = rpath.start_with?('/') ? rpath : "./#{rpath}"
     end
 
+    print_status("Executing command: #{cmd}")
     output = cmd_exec(cmd, args, timeout)
 
     if output.blank?
-      vprint_status('Command returned no output')
+      print_status('Command returned no output')
     else
       print_line(output)
     end
-
-    rm_f(rpath)
   end
 
   def lpath
