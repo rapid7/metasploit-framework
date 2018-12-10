36a2db2b466c9626e199fd7b0fe006b59a0fa1f8
Brent Cook <busterb@gmail.com>
2018-12-10 10:20:51 -0600

    Land #11082, Update show plugins to show all available plugins as well

--- lib/msf/ui/console/command_dispatcher/modules.rb
+++ lib/msf/ui/console/command_dispatcher/modules.rb
@@ -1096,7 +1096,7 @@ module Msf
           def show_plugins # :nodoc:
             tbl = Table.new(
               Table::Style::Default,
-              'Header'  => 'Plugins',
+              'Header'  => 'Loaded Plugins',
               'Prefix'  => "\n",
               'Postfix' => "\n",
               'Columns' => [ 'Name', 'Description' ]
@@ -1106,6 +1106,9 @@ module Msf
               tbl << [ plugin.name, plugin.desc ]
             }
 
+            # create an instance of core to call the list_plugins
+            core = Msf::Ui::Console::CommandDispatcher::Core.new(driver)
+            core.list_plugins
             print(tbl.to_s)
           end
 
