a94e52ca3116829b8406047ce389e79a6fd4f3a9
Francesco Soncina <phra@users.noreply.github.com>
2018-12-10 22:42:31 +0100

    fixes: updates x64/xor_dynamic for #11100

--- modules/encoders/x64/xor_dynamic.rb
+++ modules/encoders/x64/xor_dynamic.rb
@@ -14,6 +14,14 @@ class MetasploitModule < Msf::Encoder::XorDynamic
       'License'          => MSF_LICENSE
       )
   end
+  
+  # Indicate that this module can preserve some registers
+  # ...which is currently not true. This is a temp fix
+  # until the full preserve_registers functionality is
+  # implemented.
+  def can_preserve_registers?
+    true
+  end
 
   def stub
     "\xeb\x27" +             #        jmp    _call
