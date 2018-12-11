904f3428489cae6a3996e6090e23f198439c6365
Stephen Haywood <stephen@averagesecurityguy.info>
2018-12-10 20:38:04 -0500

    Option to not create shell on login.

--- modules/auxiliary/scanner/ssh/ssh_login.rb
+++ modules/auxiliary/scanner/ssh/ssh_login.rb
@@ -35,7 +35,8 @@ class MetasploitModule < Msf::Auxiliary
 
     register_options(
       [
-        Opt::RPORT(22)
+        Opt::RPORT(22),
+        OptBool.new('NO_SHELL', [ false, 'Do not create a shell on sucessful login.', false]),
       ], self.class
     )
 
@@ -128,7 +129,7 @@ class MetasploitModule < Msf::Auxiliary
         credential_core = create_credential(credential_data)
         credential_data[:core] = credential_core
         create_credential_login(credential_data)
-        session_setup(result, scanner)
+        session_setup(result, scanner) unless datastore['NO_SHELL']
         :next_user
       when Metasploit::Model::Login::Status::UNABLE_TO_CONNECT
         vprint_brute :level => :verror, :ip => ip, :msg => "Could not connect: #{result.proof}"
