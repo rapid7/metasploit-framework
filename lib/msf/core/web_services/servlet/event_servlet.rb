b5bc65c3bd4e8a40972f2ca7100f303e5b37a89f
Matthew Kienow <matthew_kienow@rapid7.com>
2018-12-21 22:18:10 -0500

    Add GET handler to query events

--- lib/msf/core/web_services/servlet/event_servlet.rb
+++ lib/msf/core/web_services/servlet/event_servlet.rb
@@ -4,13 +4,32 @@ module EventServlet
     '/api/v1/events'
   end
 
+  def self.api_path_with_id
+    "#{EventServlet.api_path}/?:id?"
+  end
+
   def self.registered(app)
+    app.get EventServlet.api_path_with_id, &get_event
     app.post EventServlet.api_path, &report_event
   end
 
   #######
   private
   #######
+
+  def self.get_event
+    lambda {
+      warden.authenticate!
+      begin
+        sanitized_params = sanitize_params(params, env['rack.request.query_hash'])
+        data = get_db.events(sanitized_params)
+        data = data.first if is_single_object?(data, sanitized_params)
+        set_json_data_response(response: data)
+      rescue => e
+        print_error_and_create_response(error: e, message: 'There was an error getting events:', code: 500)
+      end
+    }
+  end
 
   def self.report_event
     lambda {
