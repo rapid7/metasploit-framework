6c9a5b3fead5b2dc3e4df60e250a0cf51e329fef
bwatters <bwatters@rapid7.com>
2019-01-28 15:53:19 -0600

    Update Cache Sizes

--- modules/payloads/singles/osx/x64/meterpreter_reverse_tcp.rb
+++ modules/payloads/singles/osx/x64/meterpreter_reverse_tcp.rb
@@ -10,7 +10,7 @@ require 'msf/base/sessions/meterpreter_x64_osx'
 
 module MetasploitModule
 
-  CachedSize = 842712
+  CachedSize = 748256
 
   include Msf::Payload::Single
   include Msf::Sessions::MeterpreterOptions
