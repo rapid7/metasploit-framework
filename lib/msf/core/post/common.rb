62560f958119d843f4f65325386878f52804820b
William Vu <William_Vu@rapid7.com>
2019-01-31 22:07:30 -0600

    Add rudimentary Windows support to command_exists?

--- lib/msf/core/post/common.rb
+++ lib/msf/core/post/common.rb
@@ -240,7 +240,13 @@ module Msf::Post::Common
   # @return [Boolean]
   #
   def command_exists?(cmd)
-    cmd_exec("command -v #{cmd} && echo true").to_s.include? 'true'
+    if session.platform == 'windows'
+      # https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/where_1
+      # https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/if
+      cmd_exec("cmd /c where /q #{cmd} & if not errorlevel 1 echo true").to_s.include? 'true'
+    else
+      cmd_exec("command -v #{cmd} && echo true").to_s.include? 'true'
+    end
   rescue
     raise "Unable to check if command `#{cmd}' exists"
   end
