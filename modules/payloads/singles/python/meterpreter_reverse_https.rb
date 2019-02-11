2820da5e1b43a2b3214122b6f1d838f3a36edfbd
bwatters <bwatters@rapid7.com>
2019-02-11 15:41:04 -0600

    Update Cache Sizes

--- modules/payloads/singles/python/meterpreter_reverse_https.rb
+++ modules/payloads/singles/python/meterpreter_reverse_https.rb
@@ -11,7 +11,7 @@ require 'msf/base/sessions/meterpreter_python'
 
 module MetasploitModule
 
-  CachedSize = 71918
+  CachedSize = 71898
 
   include Msf::Payload::Single
   include Msf::Payload::Python
