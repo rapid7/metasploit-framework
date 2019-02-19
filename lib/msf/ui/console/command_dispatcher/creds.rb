e25abebd805a6d1dd457cd9c6e18253ec1432703
h00die <michaeltcyr@gmail.com>
2019-02-18 20:30:08 -0500

    private can be nil

--- lib/msf/ui/console/command_dispatcher/creds.rb
+++ lib/msf/ui/console/command_dispatcher/creds.rb
@@ -505,7 +505,11 @@ class Creds
           private_val = core.private ? core.private.to_s : ""
           realm_val = core.realm ? core.realm.value : ""
           human_val = core.private ? core.private.class.model_name.human : ""
-          jtr_val = core.private.jtr_format ? core.private.jtr_format : ""
+          if human_val == ""
+            jtr_val = "" #11433, private can be nil
+          else
+            jtr_val = core.private.jtr_format ? core.private.jtr_format : ""
+          end
 
           row += [
             public_val,
