5a73a57e87c3bc66d4c3b15c48ecf27a2864f0c5
Aaron Soto <aaron_soto@rapid7.com>
2019-03-19 14:36:40 -0500

    WIP: REST API for async-callbacks, added UUID search

--- lib/msf/core/db_manager/async_callback.rb
+++ lib/msf/core/db_manager/async_callback.rb
@@ -15,8 +15,10 @@ module Msf::DBManager::AsyncCallback
 
   def async_callbacks(opts)
     ::ActiveRecord::Base.connection_pool.with_connection do
-      if opts[:id] && !opts[:id].to_s.empty?
-        return Array.wrap(Mdm::AsyncCallback.find(opts[:id]))
+      if opts[:uuid] && !opts[:uuid].to_s.empty?
+        #return Array.wrap(Mdm::AsyncCallback.find_by :uuid => opts[:uuid] )
+        #return Array.wrap( Mdm::AsyncCallback.where ( uuid: opts[:uuid] ))
+        return Mdm::AsyncCallback.where( 'uuid' => opts[:uuid] )
       end
 
       wspace = Msf::Util::DBManager.process_opts_workspace(opts, framework)
