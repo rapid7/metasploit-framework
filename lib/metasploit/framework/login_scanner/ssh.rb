6aca047dbbb665e1226fb1b8b03a77a325bcbca2
h00die <michaeltcyr@gmail.com>
2019-03-22 21:51:41 -0400

    unifi ssh cleanup

--- lib/metasploit/framework/login_scanner/ssh.rb
+++ lib/metasploit/framework/login_scanner/ssh.rb
@@ -124,10 +124,10 @@ module Metasploit
                   # Ubiquiti Unifi USG
                   proof << ssh_socket.exec!("cat /etc/version\n").to_s
                 end
-                temp_proof << ssh_socket.exec!("grep unifi.version /tmp/system.cfg\n").to_s
-                if (temp_proof =~ /unifi.version/)
+                temp_proof = ssh_socket.exec!("grep unifi.version /tmp/system.cfg\n").to_s
+                if (temp_proof =~ /unifi\.version/)
                   proof << temp_proof
-                  # Ubiquiti Unifi device (non-USG), possibly a switch.  Tested on US-24
+                  # Ubiquiti Unifi device (non-USG), possibly a switch.  Tested on US-24, UAP-nanoHD
                   # The /tmp/*.cfg files don't give us device info, however the info command does
                   # we dont call it originally since it doesnt say unifi/ubiquiti in it and info
                   # is a linux command as well
