e7974e495514621229cf475cdf35c887db375ba8
Brent Cook <busterb@gmail.com>
2019-04-11 09:43:17 -0500

    bump mettle and other gems

--- modules/payloads/singles/apple_ios/armle/meterpreter_reverse_https.rb
+++ modules/payloads/singles/apple_ios/armle/meterpreter_reverse_https.rb
@@ -10,7 +10,7 @@ require 'msf/base/sessions/meterpreter_armle_apple_ios'
 
 module MetasploitModule
 
-  CachedSize = 639132
+  CachedSize = 639620
 
   include Msf::Payload::Single
   include Msf::Sessions::MeterpreterOptions
