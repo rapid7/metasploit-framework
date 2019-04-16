e233ee38ea0fe3edc6837f5b02e1fd043bbd5904
bwatters-r7 <bwatters@rapid7.com>
2019-04-16 13:57:11 -0500

    Let's try it as a single this time....

---
+++ lib/msf/base/sessions/pingback_options.rb
@@ -0,0 +1,51 @@
+# -*- coding: binary -*-
+
+##
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# https://metasploit.com/framework/
+##
+
+
+module Msf
+module Sessions
+module PingbackOptions
+
+  def initialize(info = {})
+    super(info)
+
+    register_advanced_options(
+      [
+        OptBool.new('CreateSession', [false, 'Create a new session for every successful login', true]),
+        OptString.new('InitialAutoRunScript', "An initial script to run on session creation (before AutoRunScript)"),
+        OptString.new('AutoRunScript', "A script to run automatically on session creation."),
+        OptString.new('CommandShellCleanupCommand', "A command to run before the session is closed")
+      ]
+    )
+  end
+
+  def on_session(session)
+    super
+
+    # Configure input/output to match the payload
+    if self.platform and self.platform.kind_of? Msf::Module::PlatformList
+      session.platform = self.platform.platforms.first.realname.downcase
+    end
+    if self.platform and self.platform.kind_of? Msf::Module::Platform
+      session.platform = self.platform.realname.downcase
+    end
+
+    if self.arch
+      if self.arch.kind_of?(Array)
+        session.arch = self.arch.join('')
+      else
+        session.arch = self.arch
+      end
+    end
+
+  end
+
+end
+end
+end
