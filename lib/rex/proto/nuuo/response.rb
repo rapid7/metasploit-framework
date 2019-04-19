e85147a5f2b4facb53dcbe976cec8311885fed3c
Jacob Robles <jrobles@rapid7.com>
2019-04-19 14:23:53 -0500

    Add nuuo response
    Parse server responses

---
+++ lib/rex/proto/nuuo/response.rb
@@ -0,0 +1,96 @@
+# -*- coding:binary -*-
+
+module Rex
+module Proto
+module Nuuo
+class Response
+
+  module ParseCode
+    Completed = 1
+    Partial   = 2
+    Error     = 3
+  end
+
+  module ParseState
+    ProcessingHeader  = 1
+    ProcessingBody    = 2
+    Completed         = 3
+  end
+
+  attr_accessor :headers
+  attr_accessor :body
+  attr_accessor :bufq
+  attr_accessor :state
+
+  def initialize(buf=nil)
+    self.state = ParseState::ProcessingHeader
+    self.headers = {}
+    self.body = ''
+    self.bufq = ''
+    parse(buf) if buf
+  end
+
+  # returns state of parsing
+  def parse(buf)
+    self.bufq << buf
+
+    if self.state == ParseState::ProcessingHeader
+      parse_header
+    end
+
+    if self.state == ParseState::ProcessingBody
+      if self.body_bytes_left == 0
+        self.state = ParseState::Completed
+      else
+        parse_body
+      end
+    end
+
+    (self.state == ParseState::Completed) ? ParseCode::Completed : ParseCode::Partial
+  end
+
+  protected
+  attr_accessor :body_bytes_left
+
+  def parse_header
+    head,body = self.bufq.split("\r\n\r\n", 2)
+    return nil unless body
+
+    get_headers(head)
+    self.bufq = body || ''
+    self.body_bytes_left = 0
+
+    if self.headers['Content-Length']
+      self.body_bytes_left = self.headers['Content-Length'].to_i
+    end
+
+    self.state = ParseState::ProcessingBody
+  end
+
+  def parse_body
+    return if self.bufq.length == 0
+    if self.body_bytes_left >= 0
+      part = self.bufq.slice!(0, self.body_bytes_left)
+      self.body << part
+      self.body_bytes_left -= part.length
+    else
+      self.body_bytes_left = 0
+    end
+
+    if self.body_bytes_left == 0
+      self.state = ParseState::Completed
+    end
+  end
+
+  def get_headers(head)
+    head.each_line.with_index do |l, i|
+      next if i == 0
+      k,v = l.split(':', 2)
+      self.headers[k] = v.strip
+    end
+  end
+
+end
+end
+end
+end
