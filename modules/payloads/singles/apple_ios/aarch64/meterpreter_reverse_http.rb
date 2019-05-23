05a769eaaf5960554f1d2975afade7e91a28a356
bwatters-r7 <bwatters@rapid7.com>
2019-05-23 07:56:10 -0500

    Land #11863, Bump mettle 0.5.13
    Merge branch 'land-11863' into upstream-master

--- modules/payloads/singles/apple_ios/aarch64/meterpreter_reverse_http.rb
+++ modules/payloads/singles/apple_ios/aarch64/meterpreter_reverse_http.rb
@@ -10,7 +10,7 @@ require 'msf/base/sessions/meterpreter_aarch64_apple_ios'
 
 module MetasploitModule
 
-  CachedSize = 796376
+  CachedSize = 796364
 
   include Msf::Payload::Single
   include Msf::Sessions::MeterpreterOptions
