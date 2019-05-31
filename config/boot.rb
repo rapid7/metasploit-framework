d9d0639a91d36946b6491cd3e013988d5fcb7b25
Brent Cook <busterb@gmail.com>
2019-05-31 12:23:44 -0500

    move BigDecimal patch earlier in boot process
    This makes msfvenom also quiet about the impending deprecation.

--- config/boot.rb
+++ config/boot.rb
@@ -22,6 +22,18 @@ unless ENV['BUNDLE_GEMFILE']
   end
 end
 
+# Remove bigdecimal warning - start
+# https://github.com/ruby/bigdecimal/pull/115
+# https://github.com/rapid7/metasploit-framework/pull/11184#issuecomment-461971266
+# TODO: remove when upgrading from rails 4.x
+require 'bigdecimal'
+
+def BigDecimal.new(*args, **kwargs)
+  return BigDecimal(*args) if kwargs.empty?
+  BigDecimal(*args, **kwargs)
+end
+# Remove bigdecimal warning - end
+
 begin
   require 'bundler/setup'
 rescue LoadError => e
