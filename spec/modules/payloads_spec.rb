f11a205b41a95e6f8d71d920ab02fc80fc682dfd
Brent Cook <busterb@gmail.com>
2019-06-12 17:39:56 -0500

    remove patchupdllinject

--- spec/modules/payloads_spec.rb
+++ spec/modules/payloads_spec.rb
@@ -3466,17 +3466,6 @@ RSpec.describe 'modules/payloads', :content do
                           reference_name: 'windows/patchupdllinject/reverse_tcp_rc4_dns'
   end
 
-  context 'windows/patchupdllinject/reverse_udp' do
-    it_should_behave_like 'payload cached size is consistent',
-                          ancestor_reference_names: [
-                              'stagers/windows/reverse_udp',
-                              'stages/windows/patchupdllinject'
-                          ],
-                          dynamic_size: false,
-                          modules_pathname: modules_pathname,
-                          reference_name: 'windows/patchupdllinject/reverse_udp'
-  end
-
   context 'windows/patchupmeterpreter/bind_ipv6_tcp' do
     it_should_behave_like 'payload cached size is consistent',
                           ancestor_reference_names: [
