66c3c6a94bc35e41d1a6a354e742d4be99f777d2
Jacob Robles <jrobles@rapid7.com>
2019-06-18 14:08:47 -0500

    Remove unused mixin, update save loot logic
    Capture mixin was not used. Loot was being
    saved when a zip file wasn't found. Updated
    file path so the module is under analyze.

--- modules/auxiliary/analyze/modbus_zip.rb
+++ modules/auxiliary/analyze/modbus_zip.rb
@@ -2,33 +2,33 @@
 ## This module requires Metasploit: https://metasploit.com/download
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
-require 'packetfu'
+
 class MetasploitModule < Msf::Auxiliary
-  include Msf::Exploit::Capture
   include Msf::Auxiliary::Report
 
   def initialize(info = {})
     super(
-      'Name'           => "Extract zip from Modbus communication",
+      'Name'           => 'Extract zip from Modbus communication',
       'Description'    => %q{
         This module is able to extract a zip file sent through Modbus from a pcap.
         Tested with Schneider TM221CE16R
       },
-        'Author' => [
-          'José Diogo Monteiro <jdlopes[at]student.dei.uc.pt>',
-          'Luis Rosa <lmrosa[at]dei.uc.pt)>',
-        ],
+      'Author' => [
+        'José Diogo Monteiro <jdlopes[at]student.dei.uc.pt>',
+        'Luis Rosa <lmrosa[at]dei.uc.pt)>'
+      ],
       'License' => MSF_LICENSE
-      )
-    register_options([Opt::RPORT(502),
-                      OptEnum.new("MODE", [true, 'Extract zip from upload/download capture', "UPLOAD",
-                                           [
-                                             "UPLOAD",
-                                             "DOWNLOAD"
-                                           ]]),
-                      OptString.new('FILENAME', [ true, "The zip file", '' ]),])
-  end
+    )
 
+    register_options [
+      Opt::RPORT(502),
+      OptEnum.new('MODE', [true, 'Extract zip from upload/download capture', 'UPLOAD',
+                  ['UPLOAD','DOWNLOAD']]),
+      OptString.new('PCAPFILE', [ true, 'Pcap to read', '' ]),
+      OptString.new('FILENAME', [ false, 'Zip file output name'])
+    ]
+
+  end
 
   FIRST_BYTE_UPLOAD = 12
   FIRST_BYTE_DOWNLOAD = 16
@@ -42,6 +42,7 @@ class MetasploitModule < Msf::Auxiliary
       zip_packet += 1
       return zip_packet, data
     end
+
     # ZIP end signature (central directory record)
     h = packet.payload.scan(/.*\x50\x4B\x05\x06................../)
     if h.size.nonzero?
@@ -50,6 +51,7 @@ class MetasploitModule < Msf::Auxiliary
       zip_packet += 1
       return zip_packet, data
     end
+
     # ZIP data
     if zip_packet == 1
       unless packet.payload[first_byte..-1].nil?
@@ -62,20 +64,26 @@ class MetasploitModule < Msf::Auxiliary
   def run
     packets = PacketFu::PcapFile.read_packets datastore['PCAPFILE']
     zip_packet = 0
-    data = ""
+    data = ''
     packets.each_with_index do |packet, i|
-      if datastore["MODE"] == "UPLOAD"
+      if datastore['MODE'] == 'UPLOAD'
         if  packet.respond_to?(:tcp_src) and packet.tcp_src == datastore['RPORT']
           zip_packet, data = extract_zip(packet, zip_packet, FIRST_BYTE_UPLOAD, data, i)
         end
-      elsif datastore["MODE"] == "DOWNLOAD"
+      elsif datastore['MODE'] == 'DOWNLOAD'
         if  packet.respond_to?(:tcp_dst) and packet.tcp_dst == datastore['RPORT']
           zip_packet, data = extract_zip(packet, zip_packet, FIRST_BYTE_DOWNLOAD, data, i)
         end
       end
       break if zip_packet == 2
     end
-    path = store_loot(datastore['FILENAME'], 'application/zip', datastore['RHOSTS'], data, datastore['FILENAME'], "File from pcap")
-    print_status "Done! #{path}"
+
+    filename = datastore['FILENAME'] || 'project.zip'
+    unless data.empty?
+      path = store_loot(filename, 'application/zip', datastore['RHOSTS'], data, filename, 'modbus.zip')
+      print_good "Zip file saved in loot: #{path}"
+    else
+      print_status "Zip file not found in #{datastore['PCAPFILE']}"
+    end
   end
 end
