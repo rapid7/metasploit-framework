f2ed8235167e324877b62f7a576ecc93b2b79342
bwatters-r7 <bwatters@rapid7.com>
2019-07-17 13:23:20 -0500

    First stab at filtering payloads that require cleanup

--- lib/msf/core/payload/pingback.rb
+++ lib/msf/core/payload/pingback.rb
@@ -3,6 +3,7 @@
 require 'msf/core'
 require 'msf/core/module/platform'
 require 'rex/text'
+require 'pry'
 
 #
 # This class provides methods for calculating, extracting, and parsing
@@ -27,9 +28,14 @@ class Msf::Payload::Pingback
   #
 
   def initialize(opts=nil)
+    super
+    self.can_cleanup = true
     self.uuid ||= SecureRandom.uuid()
     opts[:pingback_uuid] = self.uuid
+    self.cleans_up = false
+    binding.pry
   end
 
   attr_accessor :uuid
+  attr_accessor :can_cleanup
 end
