79e17d0dda02b057d81ac90b4c6452607009515c
bwatters-r7 <bwatters@rapid7.com>
2019-07-29 21:38:20 -0500

    Remove unsupported options

--- lib/msf/core/payload/pingback/options.rb
+++ lib/msf/core/payload/pingback/options.rb
@@ -13,9 +13,7 @@ module Msf::Payload::Pingback::Options
     register_advanced_options(
       [
         Msf::OptInt.new('PingbackRetries', [true, "How many additional successful pingbacks", 0]),
-        Msf::OptInt.new('PingbackSleep', [true, "Time (in seconds) to sleep between pingbacks", 30]),
-        Msf::OptString.new('PingbackUUID', [ false, 'A pingback UUID to use']),
-        Msf::OptBool.new('PingbackUUIDDatabase', [ true, 'Save the pingback UUID to the database', false]),
+        Msf::OptInt.new('PingbackSleep', [true, "Time (in seconds) to sleep between pingbacks", 30])
       ], self.class)
   end
 
