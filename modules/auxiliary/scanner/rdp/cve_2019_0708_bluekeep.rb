178339db70d4ce97f2e1261b8f2f5600226d4f14
William Vu <William_Vu@rapid7.com>
2019-08-01 15:03:14 -0500

    Rescue EOFError in quick check for patch
    Thank you to @cnotin for noticing this case.

--- modules/auxiliary/scanner/rdp/cve_2019_0708_bluekeep.rb
+++ modules/auxiliary/scanner/rdp/cve_2019_0708_bluekeep.rb
@@ -207,7 +207,11 @@ class MetasploitModule < Msf::Auxiliary
       rdp_send(x64_packet)
 
       # Quick check for the Ultimatum PDU
-      res = sock.get_once(-1, 1)
+      begin
+        res = sock.get_once(-1, 1)
+      rescue EOFError
+        # we don't care
+      end
       return Exploit::CheckCode::Vulnerable if res && res.include?(["0300000902f0802180"].pack("H*"))
 
       # Slow check for Ultimatum PDU. If it doesn't respond in a timely
