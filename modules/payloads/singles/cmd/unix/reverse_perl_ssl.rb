b85b799d4f6750d62e92231819388b1848066223
Green-m <greenm.xxoo@gmail.com>
2019-10-13 19:09:07 +0800

    Update CachedSize of payload.

--- modules/payloads/singles/cmd/unix/reverse_perl_ssl.rb
+++ modules/payloads/singles/cmd/unix/reverse_perl_ssl.rb
@@ -9,7 +9,7 @@ require 'msf/base/sessions/command_shell_options'
 
 module MetasploitModule
 
-  CachedSize = 144
+  CachedSize = 173
 
   include Msf::Payload::Single
   include Msf::Sessions::CommandShellOptions
