6321946a82b2c58331bea7143dc6ba9e80d02f61
William Vu <William_Vu@rapid7.com>
2019-10-14 11:31:45 -0500

    Start work on scanner

--- modules/auxiliary/scanner/vxworks/urgent11_check.rb
+++ modules/auxiliary/scanner/vxworks/urgent11_check.rb
@@ -3,60 +3,80 @@
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
 
-require 'packetfu'
-
 class MetasploitModule < Msf::Auxiliary
-  def initialize
-    super(
-      'Name'        => 'Urgent/11 Scanner, based on detection tool by Armis',
-      'Description' => %q{
-        This module detects VXWorks and the IPNet IP stack, along with CVE-2019-12258-vulnerable devices.
+
+  include Msf::Auxiliary::Scanner
+  include Msf::Exploit::Capture
+
+  def initialize(info = {})
+    super(update_info(info,
+      'Name'           => 'URGENT/11 Scanner, Based on Detection Tool by Armis',
+      'Description'    => %q{
+        This module detects VxWorks and the IPnet IP stack, along with devices
+        vulnerable to CVE-2019-12258.
       },
-      'References'  =>
-        [
-          [ 'URL', 'https://armis.com/urgent11' ],
-          [ 'URL', 'https://github.com/ArmisSecurity/urgent11-detector' ],
-          [ 'CVE', '2019-12258' ]
-        ],
-      'Author'      => [
-        'Ben Seri', # Upstream tool
-        'Brent Cook' # Metasploit module
-        ],
-      'License'     => MSF_LICENSE
-      )
+      'Author'         => [
+        'Ben Seri',   # Upstream tool
+        'Brent Cook', # Metasploit module
+        'wvu'         # Metasploit module
+      ],
+      'References'     => [
+        ['CVE', '2019-12258'],
+        ['URL', 'https://armis.com/urgent11'],
+        ['URL', 'https://github.com/ArmisSecurity/urgent11-detector']
+      ],
+      'DisclosureDate' => '2019-08-09', # NVD published date
+      'License'        => MSF_LICENSE
+    ))
 
-    register_options(
-      [
-        OptString.new('INTERFACE', [ true, 'Set an interface', 'eth0' ]),
-        OptInt.new('ANSWERTIME', [ true, 'Seconds to wait for answers, set longer on slower networks', 2 ]),
-        Opt::RPORT(80)
-      ]
-    )
+    deregister_options('PCAPFILE', 'FILTER')
   end
 
-  def bin_to_hex(s)
-    s.each_byte.map { |b| b.to_s(16).rjust(2, '0') }.join
+  def filter(ip)
+    "src host #{ip} and dst host #{Rex::Socket.source_address(ip)}"
   end
 
-  def receive(iface, answertime)
-    capture = PacketFu::Capture.new(iface: iface, start: true, filter: 'ether proto 0x0800')
-    sleep answertime
-    capture.save
-    capture.array.each do |packet|
-      data = bin_to_hex(packet).downcase
-      mac = data[0..1] + ':' + data[2..3] + ':' + data[4..5] + ':' + data[6..7] + ':' + data[8..9] + ':' + data[10..11]
-      print_good("Parsing packet from #{mac}")
-    end
+  def run_host(ip)
+    open_pcap
+    capture.setfilter(filter(ip))
+
+    tcp_malformed_options_detection(ip)
+    tcp_dos_detection(ip)
+    icmp_code_detection(ip)
+    icmp_timestamp_detection(ip)
+  rescue RuntimeError => e
+    fail_with(Failure::BadConfig, e.message)
+  ensure
+    close_pcap
   end
 
-  def run
-    iface = datastore['INTERFACE']
-    answertime = datastore['ANSWERTIME']
-
-    tcp_pkt = PacketFu::TCPPacket.new()
-    print_status("Sending packet out to #{iface}")
-    tcp_pkt.to_w(iface)
-
-    receive(iface, answertime)
+  def tcp_malformed_options_detection(ip)
   end
+
+  def tcp_dos_detection(ip)
+  end
+
+  def icmp_code_detection(ip)
+    p = PacketFu::ICMPPacket.new
+
+    # IP
+    p.ip_saddr = Rex::Socket.source_address(ip)
+    p.ip_daddr = ip
+
+    # ICMP
+    p.icmp_type = 8    # Echo request
+    p.icmp_code = 0x41 # Randomize?
+    p.recalc
+
+    vprint_status(p.inspect)
+    inject(p.to_s)
+
+    r = inject_reply(:icmp)
+
+    require 'pry'; binding.pry
+  end
+
+  def icmp_timestamp_detection(ip)
+  end
+
 end
