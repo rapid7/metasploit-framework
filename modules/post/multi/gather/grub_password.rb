bd76e1f2cba80d3b758c65f5adda259c76434df2
Brent Cook <busterb@gmail.com>
2019-10-29 03:42:01 -0500

    initial tidy pass w/rubocop

--- modules/post/multi/gather/grub_password.rb
+++ modules/post/multi/gather/grub_password.rb
@@ -10,9 +10,9 @@ class MetasploitModule < Msf::Post
   def initialize(info = {})
     super(update_info(info,
       'Name'         => 'Gather GRUB Password',
-      'Description'  => %q{
+      'Description'  => %q(
         This module gathers GRUB passwords from GRUB bootloader config files.
-      },
+      ),
       'License'      => MSF_LICENSE,
       'Author'       =>
         [
@@ -26,11 +26,14 @@ class MetasploitModule < Msf::Post
 
   def parse_passwd_from_file(file)
     return unless readable?(file)
+
     print_status("Reading #{file}")
 
+    idx = 0
     contents = read_file(file)
     contents.each_line do |line|
       next unless line.start_with?('password')
+
       pass_line = line.strip.split(' ')
       unless pass_line.length == 3
         print_status("Unknown Grub password convention. Printing line")
@@ -38,7 +41,6 @@ class MetasploitModule < Msf::Post
         next
       end
 
-      idx = 0
       convention = pass_line[0]
       case convention
       when 'password_pbkdf2'
@@ -81,10 +83,10 @@ class MetasploitModule < Msf::Post
       parse_passwd_from_file(target)
     end
 
-    unless @creds_hash && @pass_hash
-      print_status("No passwords found in GRUB config files")
-    else
+    if @creds_hash && @pass_hash
       print_good("Found credentials")
+    else
+      print_status("No passwords found in GRUB config files")
     end
 
     cred_table = Rex::Text::Table.new(
@@ -94,8 +96,7 @@ class MetasploitModule < Msf::Post
     )
 
     @creds_hash.each do |user, pass|
-      credential_data =
-      {
+      credential_data = {
         origin_type:          :session,
         post_reference_name:  self.refname,
         private_type:         :nonreplayable_hash,
@@ -109,9 +110,8 @@ class MetasploitModule < Msf::Post
       create_credential(credential_data)
     end
 
-    @pass_hash.each do |index, pass|
-      credential_data =
-      {
+    @pass_hash.each do |_index, pass|
+      credential_data = {
         origin_type:          :session,
         post_reference_name:  self.refname,
         private_type:         :nonreplayable_hash,
