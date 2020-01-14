58a3f88907af0e03b2aa3dd66e0c4379a6bbcd15
L <l-codes@qq.com>
2020-01-14 17:34:47 +0800

    update CacheSize

--- modules/payloads/singles/cmd/windows/bind_lua.rb
+++ modules/payloads/singles/cmd/windows/bind_lua.rb
@@ -9,7 +9,7 @@ require 'msf/base/sessions/command_shell_options'
 
 module MetasploitModule
 
-  CachedSize = 223
+  CachedSize = 218
 
   include Msf::Payload::Single
   include Msf::Sessions::CommandShellOptions
