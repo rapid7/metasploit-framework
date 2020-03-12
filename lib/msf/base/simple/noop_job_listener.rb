754b42f9f2840aa062c88fe036c413badaa583f9
dwelch-r7 <dean_welch@rapid7.com>
2020-03-12 11:47:07 +0000

    run rubocop -a on new files

--- lib/msf/base/simple/noop_job_listener.rb
+++ lib/msf/base/simple/noop_job_listener.rb
@@ -4,41 +4,11 @@ class NoopJobListener
 
   include Singleton
 
-  def waiting(id)
-  end
+  def waiting(id); end
 
-  def start(id)
-  end
+  def start(id); end
 
-  def completed(id, result, mod)
-  end
+  def completed(id, result, mod); end
 
-  def failed(id, error, mod)
-  end
-
-  def running?(id)
-  end
-
-  def waiting?(id)
-  end
-
-  def finished?(id)
-  end
-
-  def result(id)
-  end
-
-  def delete(id)
-  end
-
-  def results_size
-  end
-
-  def waiting_size
-  end
-
-  def running_size
-  end
-
-  alias :ack :delete
+  def failed(id, error, mod); end
 end
