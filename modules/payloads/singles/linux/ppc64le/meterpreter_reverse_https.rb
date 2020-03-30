def95c41ce8007b3463e47e650ed4b381cd2edc3
Tim W <timrlw@gmail.com>
2020-03-30 15:30:03 +0800

    update payload cached sizes

--- modules/payloads/singles/linux/ppc64le/meterpreter_reverse_https.rb
+++ modules/payloads/singles/linux/ppc64le/meterpreter_reverse_https.rb
@@ -10,7 +10,7 @@ require 'msf/base/sessions/meterpreter_ppc64le_linux'
 
 module MetasploitModule
 
-  CachedSize = 1236760
+  CachedSize = 1169304
 
   include Msf::Payload::Single
   include Msf::Sessions::MeterpreterOptions
--- modules/payloads/singles/linux/x64/meterpreter_reverse_https.rb
+++ modules/payloads/singles/linux/x64/meterpreter_reverse_https.rb
@@ -10,7 +10,7 @@ require 'msf/base/sessions/meterpreter_x64_linux'
 
 module MetasploitModule
 
-  CachedSize = 1046632
+  CachedSize = 1036536
 
   include Msf::Payload::Single
   include Msf::Sessions::MeterpreterOptions
