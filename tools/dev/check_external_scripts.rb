b18a2fd463e707c5eba91b402a77a1e3d7f79be8
Brent Cook <busterb@gmail.com>
2020-04-02 21:32:10 -0500

    Support inline transforms on tools, make some code snazzier

--- tools/dev/check_external_scripts.rb
+++ tools/dev/check_external_scripts.rb
@@ -83,36 +83,44 @@ end
 
 scripts = []
 scripts << {
-    'Name' => 'Sharphound (Bloodhound)',
-    'address' => 'https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Ingestors/SharpHound.ps1',
-    'msf_file' => '/data/post/powershell/SharpHound.ps1',
-  }
+  name: 'Sharphound (Bloodhound)',
+  addr: 'https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Ingestors/SharpHound.ps1',
+  dest: '/data/post/powershell/SharpHound.ps1',
+  subs: [
+    ["\t", "    "], # tabs to spaces
+    [/\s+$/, '']    # trailing whitespace
+  ]
+}
 
 path = File.expand_path('../../', File.dirname(__FILE__))
 
 scripts.each do |script|
-  puts "Downloading: #{script['Name']}"
+  puts "Downloading: #{script[:name]}"
   begin
-    content = open(script['address']).read
-    new_hash = Digest::SHA1.hexdigest content
-    f = open(path + script['msf_file'], 'rb')
-    old_hash = Digest::SHA1.hexdigest f.read
-    f.close()
+    old_content = File.binread(path + script[:dest])
+    old_hash = Digest::SHA1.hexdigest old_content
     info "Old Hash: #{old_hash}"
+
+    new_content = open(script[:addr]).read
+    if script.key?(:subs) then
+      script[:subs].each do |sub|
+        new_content.gsub!(sub[0], sub[1])
+      end
+    end
+    new_hash = Digest::SHA1.hexdigest new_content
     info "New Hash: #{new_hash}"
+
     unless old_hash == new_hash
       warn "  New version identified!"
       if options[:update] == true
-        warn "    Updating MSF copy of #{script['msf_file']}"
-        f = open(path + script['msf_file'], 'wb')
-        f.write(content)
-        f.close()
+        warn "    Updating MSF copy of #{script[:dest]}"
+        File.binwrite(path + script[:dest], new_content)
       end
     end
   rescue OpenURI::HTTPError
-    error "Unable to download, check URL: #{script['address']}"
+    error "Unable to download, check URL: #{script[:addr]}"
   rescue Errno::ENOENT
-    error "msf_file not found, check path: #{path + script['msf_file']}"
+    error "Destination not found, check path: #{path + script[:dest]}"
   end
 end
 
