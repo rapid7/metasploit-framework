f174b7154959db9476c9317f5ec1322329fe35d1
Mehmet İnce <mehmet@mehmetince.net>
2020-04-21 16:06:36 +0300

    Recalculate payload cache sizes and update payload format

--- modules/payloads/stagers/python/bind_tcp.rb
+++ modules/payloads/stagers/python/bind_tcp.rb
@@ -11,7 +11,7 @@ require 'msf/base/sessions/command_shell_options'
 
 module MetasploitModule
 
-  CachedSize = 386
+  CachedSize = 369
 
   include Msf::Payload::Stager
   include Msf::Payload::Python
