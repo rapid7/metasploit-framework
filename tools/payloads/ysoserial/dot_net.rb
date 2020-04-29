197365b61031bb175f93c5a310a4c473f3cb77cb
Spencer McIntyre <zeroSteiner@gmail.com>
2020-04-29 17:50:07 -0400

    Finish up the .NET deserialization tool

--- tools/payloads/ysoserial/dot_net.rb
+++ tools/payloads/ysoserial/dot_net.rb
@@ -16,31 +16,57 @@ require 'msf/base'
 require 'rex'
 require 'optparse'
 
+DND = Msf::Util::DotNetDeserialization
 BANNER = %Q{
 Usage: #{__FILE__} [options]
 
-Example: #{__FILE__} -f BinaryFormatter
-}
+Generate a .NET deserialization payload that will execute an operating system
+command using the specified gadget chain and formatter.
+
+Available formatters:
+#{DND::Formatters::NAMES.map { |n| "    * #{n}\n"}.join}
+Available gadget chains:
+#{DND::GadgetChains::NAMES.map { |n| "    * #{n}\n"}.join}
+Example: #{__FILE__} -c "net user msf msf /ADD" -f BinaryFormatter -g TextFormattingRunProperties
+}.strip
+
+def puts_transform_formats
+  $stdout.puts 'Available transform formats:'
+  $stdout.puts Msf::Simple::Buffer.transform_formats.map { |n| "    * #{n}\n"}.join
+end
 
 module YSoSerialDotNet
   class OptsConsole
     def self.parse(args)
-      options = {}
+      options = {
+          formatter:     DND::DEFAULT_FORMATTER,
+          gadget_chain:  DND::DEFAULT_GADGET_CHAIN,
+          output_format: 'raw'
+      }
       parser = OptionParser.new do |opt|
         opt.banner = BANNER
         opt.separator ''
         opt.separator 'Specific options:'
 
-        opt.on('-c', '--command <String>', "The command to run") do |v|
+        opt.on('-c', '--command   <String>', 'The command to run') do |v|
           options[:command] = v
         end
 
-        opt.on('-f', '--formatter <String>', "The formatter to use") do |v|
+        opt.on('-f', '--formatter <String>', "The formatter to use (default: #{DND::DEFAULT_FORMATTER})") do |v|
           options[:formatter] = v.to_sym
         end
 
-        opt.on('-g', '--gadget <String>', "The gadget to use") do |v|
+        opt.on('-g', '--gadget    <String>', "The gadget chain to use (default: #{DND::DEFAULT_GADGET_CHAIN})") do |v|
           options[:gadget_chain] = v.to_sym
+        end
+
+        opt.on('-o', '--output    <String>', 'The output format to use (default: raw, see: --list-output-formats)') do |v|
+          options[:output_format] = v.downcase
+        end
+
+        opt.on_tail('--list-output-formats', 'List available output formats, for use with --output') do |v|
+          puts_transform_formats
+          exit
         end
 
         opt.on_tail('-h', '--help', 'Show this message') do
@@ -55,7 +81,12 @@ module YSoSerialDotNet
         raise OptionParser::MissingArgument, 'No options set, try -h for usage'
       elsif options[:command].blank?
         raise OptionParser::MissingArgument, '-c is required'
-        # todo: validate formatter and gadget_chain
+      elsif !DND::Formatters::NAMES.include?(options[:formatter])
+        raise OptionParser::InvalidArgument, "#{options[:formatter]} is not a valid formatter"
+      elsif !DND::GadgetChains::NAMES.include?(options[:gadget_chain])
+        raise OptionParser::InvalidArgument, "#{options[:gadget_chain]} is not a valid gadget chain"
+      elsif !Msf::Simple::Buffer.transform_formats.include?(options[:output_format])
+        raise OptionParser::InvalidArgument, "#{options[:output_format]} is not a valid output format"
       end
 
       options
@@ -73,12 +104,14 @@ module YSoSerialDotNet
     end
 
     def run
-      serialized = Msf::Util::DotNetDeserialization.generate(
+      serialized = DND.generate(
         @opts[:command],
         gadget_chain: @opts[:gadget_chain],
         formatter: @opts[:formatter]
       )
-      $stdout.puts serialized
+
+      transformed = ::Msf::Simple::Buffer.transform(serialized, @opts[:output_format])
+      $stdout.puts transformed
     end
 
   end
