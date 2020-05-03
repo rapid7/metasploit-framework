9501ec0b1b18df6d3c8b4fe00c620810fc94db1a
gwillcox-r7 <gwillcox@rapid7.com>
2020-05-02 21:48:31 -0500

    RuboCop alpanum.rb code

--- lib/msf/core/encoder/alphanum.rb
+++ lib/msf/core/encoder/alphanum.rb
@@ -1,29 +1,28 @@
 # -*- coding: binary -*-
+
 require 'msf/core'
 
 module Msf
+  ###
+  #
+  # This class provides common options for certain alphanumeric encoders.
+  #
+  ###
+  class Encoder::Alphanum < Msf::Encoder
 
-###
-#
-# This class provides common options for certain alphanumeric encoders.
-#
-###
-class Encoder::Alphanum < Msf::Encoder
+    def initialize(info)
+      super(info)
 
-  def initialize(info)
-    super(info)
+      off = 0
 
-    off = 0
+      register_options(
+        [
+          OptString.new('BufferRegister', [ false, 'The register that points to the encoded payload' ]),
+          OptInt.new('BufferOffset', [ false, 'The offset to the buffer from the start of the register', off ]),
+          OptBool.new('AllowWin32SEH', [ true, 'Use SEH to determine the address of the stub (Windows only)', false ])
+        ], Msf::Encoder::Alphanum
+      )
+    end
 
-    register_options(
-      [
-        OptString.new('BufferRegister', [ false, 'The register that points to the encoded payload' ]),
-        OptInt.new('BufferOffset', [ false, 'The offset to the buffer from the start of the register', off ]),
-        OptBool.new('AllowWin32SEH', [ true, 'Use SEH to determine the address of the stub (Windows only)', false ])
-      ], Msf::Encoder::Alphanum)
   end
-
 end
-
-end
-
