30a23c470b3b8d3b973adc95b7a570ffd970a4de
OJ <oj@buffered.io>
2020-05-05 13:27:13 +1000

    Update payload cache sizes

--- modules/payloads/singles/windows/x64/meterpreter_bind_named_pipe.rb
+++ modules/payloads/singles/windows/x64/meterpreter_bind_named_pipe.rb
@@ -12,7 +12,7 @@ require 'rex/payloads/meterpreter/config'
 
 module MetasploitModule
 
-  CachedSize = 201283
+  CachedSize = 201286
 
   include Msf::Payload::TransportConfig
   include Msf::Payload::Windows
