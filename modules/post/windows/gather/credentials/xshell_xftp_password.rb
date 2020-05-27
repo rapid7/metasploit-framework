18d1290bb898f668a4ee107cb8709c628e0fcec0
cn-kali-team <kali-team@qq.com>
2020-05-27 20:09:31 +0800

    Modify description

--- modules/post/windows/gather/credentials/xshell_xftp_password.rb
+++ modules/post/windows/gather/credentials/xshell_xftp_password.rb
@@ -162,7 +162,7 @@ class MetasploitModule < Msf::Post
       'Password'
     ]
     tbl = Rex::Text::Table.new(
-      'Header' => "UserName: #{path}",
+      'Header' => "Path: #{path}",
       'Columns' => columns
     )
     xsh.each do |item|
