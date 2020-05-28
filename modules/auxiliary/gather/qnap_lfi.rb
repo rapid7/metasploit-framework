0496e2d00d93970e59615a8ed1ea945338b46f91
Niboucha Redouane <rniboucha@yahoo.fr>
2020-05-28 22:09:11 +0200

    update References

--- modules/auxiliary/gather/qnap_lfi.rb
+++ modules/auxiliary/gather/qnap_lfi.rb
@@ -31,6 +31,10 @@ This module has been tested with QNAP QTS 4.3.3
         'References' =>
           [
             [ 'CVE', '2019-7192'],
+            [ 'CVE', '2019-7193'],
+            [ 'CVE', '2019-7194'],
+            [ 'CVE', '2019-7195'],
+            [ 'EDB', '48531' ],
             [ 'URL', 'https://medium.com/bugbountywriteup/qnap-pre-auth-root-rce-affecting-450k-devices-on-the-internet-d55488d28a05' ],
             [ 'URL', 'https://github.com/Imanfeng/QNAP-NAS-RCE' ]
           ],
