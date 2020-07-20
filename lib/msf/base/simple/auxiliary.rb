f70043bf4ed6f17a267edd69402584f6d9ac81ed
Brent Cook <busterb@gmail.com>
2020-07-20 11:09:46 -0500

    check if a module has a check method first
    Currently, if you run 'check' on a module that does not have a check
    method, it will first complain that you have not set the 'RHOSTS'
    option, whether it's an exploit module or a scanner. Then, once you set
    RHOSTS (or whatever else it needs), it will then say 'Psych! I didn't
    have a method in the first place!'.
    
    This switches that logic around so that it first alerts you that the
    module doesn't have support in the first place. It also similarizes more
    logic between aux and exploit for some future convergence possibility.

--- lib/msf/base/simple/auxiliary.rb
+++ lib/msf/base/simple/auxiliary.rb
@@ -116,10 +116,11 @@ module Auxiliary
       mod.init_ui(opts['LocalInput'], opts['LocalOutput'])
     end
 
+    return Msf::Exploit::CheckCode::Unsupported unless mod.has_check?
+
     # Validate the option container state so that options will
     # be normalized
     mod.validate
-
 
     run_uuid = Rex::Text.rand_text_alphanumeric(24)
     job_listener.waiting run_uuid
@@ -131,7 +132,7 @@ module Auxiliary
         ctx,
         Proc.new do |ctx_|
           self.job_run_proc(ctx_) do |m|
-            m.has_check? ? m.check : Msf::Exploit::CheckCode::Unsupported
+            m.check
           end
         end,
         Proc.new { |ctx_| self.job_cleanup_proc(ctx_) }
@@ -141,7 +142,7 @@ module Auxiliary
     else
       # Run check if it exists
       result = self.job_run_proc(ctx) do |m|
-        m.has_check? ? m.check : Msf::Exploit::CheckCode::Unsupported
+        m.check
       end
       self.job_cleanup_proc(ctx)
 
