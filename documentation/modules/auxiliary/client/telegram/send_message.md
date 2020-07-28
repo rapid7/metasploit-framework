e89bae5b511435c52038eca50c4cc255bde09415
gwillcox-r7 <gwillcox@rapid7.com>
2020-07-28 16:14:29 -0500

    Apply RuboCop module changes. Also update documentation file to remove some extra small sections that are not needed, and also to move the documentation from the module to the proper documentation file.

--- documentation/modules/auxiliary/client/telegram/send_message.md
+++ documentation/modules/auxiliary/client/telegram/send_message.md
@@ -1,41 +1,28 @@
-The ```auxiliary/client/telegram/send_message``` module allows you to send a telegram message to given chat ID with given telegram bot token. This module also can be used as a notifier for established sessions with using the `AutoRunScript` handler option.
+The ```auxiliary/client/telegram/send_message``` module allows you to send a Telegram message to given chat ID with given telegram bot token. This module also can be used as a notifier for established sessions with using the `AutoRunScript` handler option.
 
 ## Module Options
 
 **BOT TOKEN**
 
-Each telegram bot is given a unique authentication token when it is created. The token looks something like `123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11`, You can learn about obtaining tokens and generating new ones in [this document](https://core.telegram.org/bots#6-botfather).
-
-```
-set BOT_TOKEN 123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11
-```
+Each Telegram bot is given a unique authentication token when it is created. The token looks like `123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11`. You can generate
+a new token by messaging @botfather via `https://t.me/botfather` and sending the message `/newbot` to him, which should prompt him to ask a series of questions
+that will allow you to generate your bot. Once you have completed this, you should get a message saying `Use this token to access the HTTP API:` followed by
+the value of the bot's token. Use this value for `BOT_TOKEN`. If you have any issues, refer to [this document](https://core.telegram.org/bots#6-botfather).
 
 **CHAT ID**
 
-Unique identifier for a chat. Chat ID can be obtained using `getUpdates` method on bot API.
-`https://api.telegram.org/bot<token>/getUpdates`
-
-```
-set CHAT_ID 123456789
-```
+Unique identifier for a chat. To get the `CHAT_ID` value, send a message to the bot username that you created
+earlier. Then browse to `https://api.telegram.org/bot<BOT_TOKEN VALUE>/getUpdates`
+and look for a line like `"chat":"id":1344308063`. That ID is what you will
+want to use the value of `CHAT_ID`; in this case it would be `1344308063`.
 
 **MSG**
 
 The message content.
 
-```
-set MSG "New meterpreter session has opened !"
-```
-
 **FORMATTING**
 
 The Bot API supports basic formatting for messages. You can use bold, italic, underlined and strikethrough text, as well as inline links and pre-formatted code in your bots' messages. Telegram clients will render them accordingly. You can use either markdown-style or HTML-style formatting.
-
-```
-set FORMATTING Markdown
-```
-
-And you're good to go.
 
 ## Demonstration
 
--- modules/auxiliary/client/telegram/send_message.rb
+++ modules/auxiliary/client/telegram/send_message.rb
@@ -6,58 +6,51 @@
 require 'uri'
 
 class MetasploitModule < Msf::Auxiliary
-    def initialize
-        super(
-        'Name' => 'Telegram Message Client',
-        'Description' => %q{
-            This module will send a Telegram message to given chat ID with the 
-            given bot token. To get the value for BOT_TOKEN, got to https://t.me/botfather 
-            and send the message '/newbot' to him, then follow the prompts and respond with 
-            the bot name and its user name. You should then get a congratulations message 
-            with the bot's API key, which you will need for this module.
+  def initialize
+    super(
+    'Name' => 'Telegram Message Client',
+    'Description' => %q(
+            This module will send a Telegram message to given chat ID with the
+            given bot token. Please refer to the module documentation for info
+            on how to retrieve the bot token and corresponding chat ID values.
+        ),
+    'Author' => 'Ege Balcı <egebalci[at]pm.me>', # Aka @egeblc of https://pentest.blog
+    'License' => MSF_LICENSE,
+    )
 
-            To get the CHAT_ID value, send a message to the bot username that you created 
-            earlier. Then browse to https://api.telegram.org/bot<BOT_TOKEN VALUE>/getUpdates
-            and look for a line like "chat":"id":1344308063. That ID is what you will 
-            want to use the value of CHAT_ID; in this case it would be 1344308063.
-        },
-        'Author' => 'Ege Balcı <egebalci[at]pm.me>', # Aka @egeblc of https://pentest.blog
-        'License' => MSF_LICENSE,
-        )
+    register_options(
+      [
+        OptString.new('BOT_TOKEN', [true, 'Telegram BOT token', '']),
+        OptInt.new('CHAT_ID', [true, 'Chat ID for the BOT', '']),
+        OptString.new('MSG', [true, 'Message content', 'New session opened!']),
+        OptEnum.new('FORMATTING', [true, 'Message formating option (Markdown|MarkdownV2|HTML)', 'Markdown', [ 'Markdown', 'MarkdownV2', 'HTML']]),
+      ], self.class
+    )
+  end
 
-        register_options(
-            [
-                OptString.new('BOT_TOKEN', [true, 'Telegram BOT token', '']),
-                OptInt.new('CHAT_ID', [true, 'Chat ID for the BOT', '']),
-                OptString.new('MSG', [true, 'Message content', 'New session opened!']),
-                OptEnum.new('FORMATTING', [true, 'Message formating option (Markdown|MarkdownV2|HTML)', 'Markdown', [ 'Markdown', 'MarkdownV2', 'HTML']]),
-            ], self.class
-        )
+  def message
+    datastore['MSG']
+  end
+
+  def formatting
+    datastore['FORMATTING']
+  end
+
+  def bot_token
+    datastore['BOT_TOKEN']
+  end
+
+  def run
+    uri = URI("https://api.telegram.org/bot#{bot_token}/sendMessage")
+    params = { chat_id: datastore['CHAT_ID'], parse_mode: formatting, text: message }
+    uri.query = URI.encode_www_form(params)
+    res = Net::HTTP.get_response(uri)
+
+    if res.is_a?(Net::HTTPSuccess)
+      print_good('Message sent!')
+    else
+      print_error('Unable to send the message!')
+      print_error("API Status: #{res.code}")
     end
-
-    def message
-        datastore['MSG']
-    end
-
-    def formatting
-        datastore['FORMATTING']
-    end
-
-    def bot_token
-        datastore['BOT_TOKEN']
-    end
-
-    def run
-        uri = URI("https://api.telegram.org/bot#{bot_token}/sendMessage")
-        params = { :chat_id => datastore['CHAT_ID'], :parse_mode => formatting, :text => message }
-        uri.query = URI.encode_www_form(params)
-        res = Net::HTTP.get_response(uri)
-
-        if res.is_a?(Net::HTTPSuccess)
-            print_good('Message sent!')
-        else
-            print_error('Unable to send the message!')
-            print_error("API Status: #{res.code}")
-        end
-    end
+  end
 end
