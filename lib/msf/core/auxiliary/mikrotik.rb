97a700fafcb4e80041a9283557bc4117c50a9b08
h00die <michaeltcyr@gmail.com>
2020-08-09 09:41:40 -0400

    fix #13962

--- lib/msf/core/auxiliary/mikrotik.rb
+++ lib/msf/core/auxiliary/mikrotik.rb
@@ -78,17 +78,19 @@ module Msf
     end
 
     def mikrotik_swos_config_eater(thost, tport, config)
-      credential_data = {
-        address: thost,
-        port: tport,
-        protocol: 'tcp',
-        workspace_id: myworkspace.id,
-        origin_type: :service,
-        private_type: :password,
-        service_name: '',
-        module_fullname: fullname,
-        status: Metasploit::Model::Login::Status::UNTRIED
-      }
+      if framework.db.active
+        credential_data = {
+          address: thost,
+          port: tport,
+          protocol: 'tcp',
+          workspace_id: myworkspace.id,
+          origin_type: :service,
+          private_type: :password,
+          service_name: '',
+          module_fullname: fullname,
+          status: Metasploit::Model::Login::Status::UNTRIED
+        }
+      end
 
       # Default SNMP to UDP
       if tport == 161
@@ -128,13 +130,15 @@ module Msf
       if /,\.pwd\.b:{pwd:'(?<password>[a-f\d]*)'}/ =~ config
         password = Array(password).pack('H*')
         print_good("#{thost}:#{tport} Admin login password: #{password}")
-        cred = credential_data.dup
-        cred[:port] = 80
-        cred[:protocol] = 'tcp'
-        cred[:service_name] = 'www'
-        cred[:username] = 'admin' # hardcoded
-        cred[:private_data] = password.to_s
-        create_credential_and_login(cred)
+        if framework.db.active
+          cred = credential_data.dup
+          cred[:port] = 80
+          cred[:protocol] = 'tcp'
+          cred[:service_name] = 'www'
+          cred[:username] = 'admin' # hardcoded
+          cred[:private_data] = password.to_s
+          create_credential_and_login(cred)
+        end
       end
 
       # ,snmp.b:{en:0x01,com:'7075626c6963',ci:'636f6e74616374696e666f',loc:'6c6f636174696f6e'}
@@ -144,12 +148,14 @@ module Msf
         contact = Array(contact).pack('H*')
         location = Array(location).pack('H*')
         print_good("#{thost}:#{tport} SNMP Community: #{community}, contact: #{contact}, location: #{location}")
-        cred = credential_data.dup
-        cred[:port] = 161
-        cred[:protocol] = 'udp'
-        cred[:service_name] = 'snmp'
-        cred[:private_data] = community.to_s
-        create_credential_and_login(cred)
+        if framework.db.active
+          cred = credential_data.dup
+          cred[:port] = 161
+          cred[:protocol] = 'udp'
+          cred[:service_name] = 'snmp'
+          cred[:private_data] = community.to_s
+          create_credential_and_login(cred)
+        end
       end
 
       # ,nm:['506f727431','506f727432','506f727433','506f727434','506f727435','506f727436','506f727437','506f727438','506f727439','506f72743130','506f72743131','506f72743132','506f72743133','506f72743134','506f72743135','506f72743136','506f72743137','506f72743138','506f72743139','506f72743230','506f72743231','506f72743232','506f72743233','75706c696e6b','53465031','53465032']}
@@ -167,17 +173,19 @@ module Msf
     end
 
     def mikrotik_routeros_config_eater(thost, tport, config)
-      credential_data = {
-        address: thost,
-        port: tport,
-        protocol: 'tcp',
-        workspace_id: myworkspace.id,
-        origin_type: :service,
-        private_type: :password,
-        service_name: '',
-        module_fullname: fullname,
-        status: Metasploit::Model::Login::Status::UNTRIED
-      }
+      if framework.db.active
+        credential_data = {
+          address: thost,
+          port: tport,
+          protocol: 'tcp',
+          workspace_id: myworkspace.id,
+          origin_type: :service,
+          private_type: :password,
+          service_name: '',
+          module_fullname: fullname,
+          status: Metasploit::Model::Login::Status::UNTRIED
+        }
+      end
 
       # Default SNMP to UDP
       if tport == 161
@@ -222,6 +230,8 @@ module Msf
 
             value = values_to_hash(value)
             print_good("#{thost}:#{tport} #{value['disabled'] ? 'disabled' : ''} Open VPN Client to #{value['connect-to']} on mac #{value['mac-address']} named #{value['name']} with username #{value['user']} and password #{value['password']}")
+            next unless framework.db.active
+
             cred = credential_data.dup
             cred[:port] = 1194
             cred[:service_name] = 'openvpn'
@@ -241,6 +251,8 @@ module Msf
 
             value = values_to_hash(value)
             print_good("#{thost}:#{tport} #{value['disabled'] ? '' : 'disabled'} PPPoE Client on #{value['interface']} named #{value['name']} and service name #{value['service-name']} with username #{value['user']} and password #{value['password']}")
+            next unless framework.db.active
+
             cred = credential_data.dup
             cred[:username] = value['user']
             cred[:service_name] = 'pppoe'
@@ -259,6 +271,8 @@ module Msf
 
             value = values_to_hash(value)
             print_good("#{thost}:#{tport} #{value['disabled'] ? '' : 'disabled'} L2TP Client to #{value['connect-to']} named #{value['name']} with username #{value['user']} and password #{value['password']}")
+            next unless framework.db.active
+
             cred = credential_data.dup
             cred[:port] = 1701
             cred[:service_name] = 'l2tp'
@@ -277,6 +291,8 @@ module Msf
 
             value = values_to_hash(value)
             print_good("#{thost}:#{tport} #{value['disabled'] ? '' : 'disabled'} PPTP Client to #{value['connect-to']} named #{value['name']} with username #{value['user']} and password #{value['password']}")
+            next unless framework.db.active
+
             cred = credential_data.dup
             cred[:service_name] = 'pptp'
             cred[:port] = 1723
@@ -299,6 +315,8 @@ module Msf
             else
               print_good("#{thost}:#{tport} SNMP community #{value['name']} with password #{value['authentication-password']} and #{value['write-access'] ? 'write access' : 'read only'}")
             end
+
+            next unless framework.db.active
 
             cred = credential_data.dup
             if value['write-access'] == 'yes'
@@ -323,6 +341,8 @@ module Msf
 
             value = values_to_hash(value)
             print_good("#{thost}:#{tport} #{value['disabled'] ? 'disabled' : ''} PPP tunnel bridging named #{value['name']} with profile name #{value['profile']} and password #{value['password']}")
+            next unless framework.db.active
+
             cred = credential_data.dup
             cred[:username] = ''
             cred[:private_data] = value['password']
@@ -340,6 +360,8 @@ module Msf
 
             value = values_to_hash(value)
             print_good("#{thost}:#{tport} #{value['disabled'] == 'yes' ? 'disabled' : ''} SMB Username #{value['name']} and password #{value['password']}#{' with RO only access' if value['read-only'] == 'yes' || !value['read-only']}")
+            next unless framework.db.active
+
             cred = credential_data.dup
             if value['read-only'] == 'yes' || !value['read-only']
               cred[:access_level] = 'RO'
@@ -360,6 +382,8 @@ module Msf
 
             value = values_to_hash(value)
             print_good("#{thost}:#{tport} SMTP Username #{value['user']} and password #{value['password']} for #{value['address']}:#{value['port'] || '25'}")
+            next unless framework.db.active
+
             cred = credential_data.dup
             cred[:service_name] = 'smtp'
             cred[:port] = value['port'] ? value['port'].to_i : 25
@@ -391,7 +415,11 @@ module Msf
               next
             end
 
-            cred = credential_data.dup
+            if framework.db.active
+              cred = credential_data.dup
+            else
+              cred = {}
+            end
 
             # The following section is a little complicated due to the way mikrotik exports values
             # in compact/terse/default mode, it will skip printing default values, so we have to check
@@ -399,25 +427,33 @@ module Msf
             # In verbose mode, they will print but be empty
             if value['wpa-pre-shared-key'] && !value['wpa-pre-shared-key'].empty?
               output << " with WPA password #{value['wpa-pre-shared-key']}"
-              cred[:private_data] = value['wpa-pre-shared-key']
-              create_credential_and_login(cred)
+              if framework.db.active
+                cred[:private_data] = value['wpa-pre-shared-key']
+                create_credential_and_login(cred)
+              end
             elsif value['wpa2-pre-shared-key'] && !value['wpa2-pre-shared-key'].empty?
               output << " with WPA2 password #{value['wpa2-pre-shared-key']}"
-              cred[:private_data] = value['wpa2-pre-shared-key']
-              create_credential_and_login(cred)
+              if framework.db.active
+                cred[:private_data] = value['wpa2-pre-shared-key']
+                create_credential_and_login(cred)
+              end
             elsif value['authentication-types'] == 'wpa2-eap'
               output << " with WPA2-EAP username #{value['mschapv2-username']} password #{value['mschapv2-password']}"
-              cred[:username] = value['mschapv2-username']
-              cred[:private_data] = value['mschapv2-password']
-              create_credential_and_login(cred)
+              if framework.db.active
+                cred[:username] = value['mschapv2-username']
+                cred[:private_data] = value['mschapv2-password']
+                create_credential_and_login(cred)
+              end
             elsif value['static-key-0'] || value['static-key-1'] || value['static-key-2'] || value['static-key-3']
               (0..3).each do |i|
                 key = "static-key-#{i}"
                 next unless value[key]
 
                 output << " with WEP password #{value[key]}"
-                cred[:private_data] = value[key]
-                create_credential_and_login(cred) # run for each key we find
+                if framework.db.active
+                  cred[:private_data] = value[key]
+                  create_credential_and_login(cred) # run for each key we find
+                end
               end
             end
 
