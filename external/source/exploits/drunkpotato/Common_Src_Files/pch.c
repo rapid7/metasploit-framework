3336040f2df5f029fd261be27530c3e61a508a1a
C4ssandre <cassandre@mailden.com>
2020-08-25 14:27:41 +0200

    Adding a new privilege escalation exploit for windows.
    New files and folders:
    
    - metasploit-framework/modules/exploits/windows/local/bits_ntlm_token_impersonation.rb
    
    - metasploit-framework/data/exploits/drunkpotato/
    
    - metasploit-framework/external/source/exploits/drunkpotato/

---
+++ external/source/exploits/drunkpotato/Common_Src_Files/pch.c
@@ -0,0 +1,5 @@
+// pch.cpp : fichier source correspondant à l'en-tête précompilé
+
+#include "pch.h"
+
+// Quand vous utilisez des en-têtes précompilés, ce fichier source est nécessaire pour la réussite de la compilation.
