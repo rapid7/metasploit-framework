3e10ec5fc91153daf0a93188021cd7ccedc6eca1
Grant Willcox <gwillcox@rapid7.com>
2020-09-16 12:49:36 -0500

    Redo the checks using the command_exists? command that @bcoles recommended

--- modules/post/multi/gather/enum_software_versions.rb
+++ modules/post/multi/gather/enum_software_versions.rb
@@ -32,6 +32,10 @@ class MetasploitModule < Msf::Post
   def run
     case session.platform
     when 'windows'
+      if command_exists?('wmic') == false
+        print_error("The 'wmic' command doesn't exist on this host!") # wmic is technically marked as depreciated so this command could very well be removed in future releases.
+        return nil
+      end
       listing = cmd_exec('wmic product get Name, Description, Version, InstallDate', nil, 6000)
       unless listing.include?('Description')
         print_error('Was unable to get a listing of installed products...')
@@ -42,6 +46,10 @@ class MetasploitModule < Msf::Post
     when 'linux'
       # All of the following options were taken from https://distrowatch.com/dwres.php?resource=package-management
       cmd = %w{ 'hostnamectl' }
+      if command_exists?('hostnamectl') == false
+        print_error("The 'hostnamectl' command doesn't exist on the host, so we can't enumerate what OS this Linux host is running!")
+        return nil
+      end
       operating_system = cmd_exec("#{cmd[0]}")
       case operating_system
       when /(?:[uU]buntu|[dD]ebian|[eE]lementary|[mM]int|MX|[zZ]orin|[kK]ali)/
@@ -60,42 +68,35 @@ class MetasploitModule < Msf::Post
         cmd = %w{ 'pkg info' }
       end
 
-      if (listing = cmd_exec(cmd)) =~ /not found/
+      if (command_exists?("#{cmd[0]}")) == false
         print_error("The command #{cmd[0]} was not found on the target.")
         return nil
       else
+        listing = cmd_exec(cmd.join(' '))
         store_linux_loot(listing)
       end
-    when 'bsd'
-      listing = cmd_exec('pkg info')
-      if listing =~ /not found/
+    when 'bsd','solaris'
+      if command_exists?('pkg') == false
         print_error("The command 'pkg' does not exist on the host")
         return nil
       end
-      file = store_loot('host.bsd.software.versions', 'text/plain', session, listing, 'installed_software.txt', 'Installed Software and Versions')
+      listing = cmd_exec('pkg info')
+      file = store_loot('host.bsd.solaris.software.versions', 'text/plain', session, listing, 'installed_software.txt', 'Installed Software and Versions')
       print_good("Stored information about the installed products to the loot file at #{file}")
     when 'osx'
-      listing = cmd_exec('system_profiler SPApplicationsDataType')
-      if listing =~ /not found/
+      if command_exists?('system_profiler') == false
         print_error("The command 'system_profiler' does not exist on the host")
         return nil
       end
+      listing = cmd_exec('system_profiler SPApplicationsDataType')
       file = store_loot('host.osx.software.versions', 'text/plain', session, listing, 'installed_software.txt', 'Installed Software and Versions')
       print_good("Stored information about the installed products to the loot file at #{file}")
-    when 'solaris'
-      listing = cmd_exec('pkg info')
-      if listing =~ /not found/
-        print_error("The command 'pkg' does not exist on the host")
-        return nil
-      end
-      file = store_loot('host.solaris.software.versions', 'text/plain', session, listing, 'installed_software.txt', 'Installed Software and Versions')
-      print_good("Stored information about the installed products to the loot file at #{file}")
     when 'android'
-      listing = cmd_exec('pm list packages -f')
-      if listing =~ /not found/
+      if command_exists?('pm') == false
         print_error("The command 'pm' does not exist on the host")
         return nil
       end
+      listing = cmd_exec('pm list packages -f')
       file = store_loot('host.android.software.versions', 'text/plain', session, listing, 'installed_software.txt', 'Installed Software and Versions')
       print_good("Stored information about the installed products to the loot file at #{file}")
     end
