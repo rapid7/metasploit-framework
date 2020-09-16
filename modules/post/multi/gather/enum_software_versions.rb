bc9986f72ca2f3c3eab9a90e7f013d55e258ea17
Grant Willcox <gwillcox@rapid7.com>
2020-09-16 15:14:52 -0500

    Apply RuboCop edits and also edit the %w[] entries so they didn't include the stray single quotes in the resulting string

--- modules/post/multi/gather/enum_software_versions.rb
+++ modules/post/multi/gather/enum_software_versions.rb
@@ -36,7 +36,7 @@ class MetasploitModule < Msf::Post
         print_error("The 'wmic' command doesn't exist on this host!") # wmic is technically marked as depreciated so this command could very well be removed in future releases.
         return
       end
-      listing = cmd_exec('wmic product get Name, Description, Version, InstallDate',, 6000).to_s
+      listing = cmd_exec('wmic product get Name, Description, Version, InstallDate', nil, 6000).to_s
       unless listing.include?('Description')
         print_error('Was unable to get a listing of installed products...')
         return
@@ -45,52 +45,52 @@ class MetasploitModule < Msf::Post
       print_good("Stored information about the installed products to the loot file at #{file}")
     when 'linux'
       # All of the following options were taken from https://distrowatch.com/dwres.php?resource=package-management
-      cmd = %w{ 'hostnamectl' }
+      cmd = %w[hostnamectl]
       if command_exists?('hostnamectl') == false
         print_error("The 'hostnamectl' command doesn't exist on the host, so we can't enumerate what OS this Linux host is running!")
         return
       end
-      operating_system = cmd_exec("#{cmd[0]}").to_s
+      operating_system = cmd_exec(cmd[0]).to_s
       if operating_system.empty?
-        print_error("No results were returned when trying to determine the OS. An error likely occured.")
+        print_error('No results were returned when trying to determine the OS. An error likely occured.')
         return
       end
       case operating_system
       when /(?:[uU]buntu|[dD]ebian|[eE]lementary|[mM]int|MX|[zZ]orin|[kK]ali)/
-        cmd = %w{ 'apt list --installed' }
+        cmd = %w[apt list --installed]
       when /(?: [aA]rch |[mM]anjaro)/
-        cmd = %w{ 'pacman -Q' }
+        cmd = %w[pacman -Q]
       when /opensuse/i
-        cmd = %w{ 'zypper search -is' }
+        cmd = %w[zypper search -is]
       when /(?:fedora|centos|red hat enterprise linux)/i
-        cmd = %w{ 'rpm -qa' }
+        cmd = %w[rpm -qa]
       when /alpine/i
-        cmd = %w{ 'apk info' }
+        cmd = %w[apk info]
       when /gentoo/i
-        cmd = %w{ 'qlist -i' }
+        cmd = %w[qlist -i]
       when /freebsd/i
-        cmd = %w{ 'pkg info' }
+        cmd = %w[pkg info]
       end
 
-      if (command_exists?("#{cmd[0]}")) == false
+      if command_exists?((cmd[0]).to_s) == false
         print_error("The command #{cmd[0]} was not found on the target.")
         return
       else
         listing = cmd_exec(cmd.join(' ')).to_s
         if listing.empty?
-            print_error("No results were returned when trying to get software installed on the Linux host. An error likely occured.")
-            return
+          print_error('No results were returned when trying to get software installed on the Linux host. An error likely occured.')
+          return
         end
         store_linux_loot(listing)
       end
-    when 'bsd','solaris'
+    when 'bsd', 'solaris'
       if command_exists?('pkg') == false
         print_error("The command 'pkg' does not exist on the host")
         return
       end
       listing = cmd_exec('pkg info').to_s
       if listing.empty?
-        print_error("No results were returned when trying to get software installed on the BSD/Solaris host. An error likely occured.")
+        print_error('No results were returned when trying to get software installed on the BSD/Solaris host. An error likely occured.')
         return
       end
       file = store_loot('host.bsd.solaris.software.versions', 'text/plain', session, listing, 'installed_software.txt', 'Installed Software and Versions')
@@ -102,7 +102,7 @@ class MetasploitModule < Msf::Post
       end
       listing = cmd_exec('system_profiler SPApplicationsDataType').to_s
       if listing.empty?
-        print_error("No results were returned when trying to get software installed on the OSX host. An error likely occured.")
+        print_error('No results were returned when trying to get software installed on the OSX host. An error likely occured.')
         return
       end
       file = store_loot('host.osx.software.versions', 'text/plain', session, listing, 'installed_software.txt', 'Installed Software and Versions')
@@ -114,7 +114,7 @@ class MetasploitModule < Msf::Post
       end
       listing = cmd_exec('pm list packages -f').to_s
       if listing.empty?
-        print_error("No results were returned when trying to get software installed on the Linux host. An error likely occured.")
+        print_error('No results were returned when trying to get software installed on the Linux host. An error likely occured.')
         return
       end
       file = store_loot('host.android.software.versions', 'text/plain', session, listing, 'installed_software.txt', 'Installed Software and Versions')
