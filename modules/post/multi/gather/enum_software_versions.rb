1795edfa29d30c492bdc99070ccd32d77a93b628
Grant Willcox <gwillcox@rapid7.com>
2020-09-16 11:40:37 -0500

    Fix up enum_software_versions.rb and its code based on initial feedback

--- modules/post/multi/gather/enum_software_versions.rb
+++ modules/post/multi/gather/enum_software_versions.rb
@@ -9,7 +9,7 @@ class MetasploitModule < Msf::Post
     super(
       update_info(
         info,
-        'Name' => 'Multiplatform Installed Software Version',
+        'Name' => 'Multiplatform Installed Software Version Enumerator',
         'Description' => %q{
           This module, when run against a compromised machine, will gather details on all installed software,
           including their versions and if available, when they were installed, and will save it into a loot file for later use.
@@ -18,7 +18,6 @@ class MetasploitModule < Msf::Post
         'License' => MSF_LICENSE,
         'Author' => [ 'gwillcox-r7' ],
         'Platform' => %w[win linux osx bsd solaris android],
-        # 'Platform'      => %w{ android osx win linux bsd solaris },
         'SessionTypes' => [ 'meterpreter', 'shell' ]
       )
     )
@@ -34,7 +33,7 @@ class MetasploitModule < Msf::Post
     case session.platform
     when 'windows'
       listing = cmd_exec('wmic product get Name, Description, Version, InstallDate', nil, 6000)
-      unless listing =~ /Description/
+      unless listing.include?('Description')
         print_error('Was unable to get a listing of installed products...')
         return nil
       end
@@ -42,66 +41,29 @@ class MetasploitModule < Msf::Post
       print_good("Stored information about the installed products to the loot file at #{file}")
     when 'linux'
       # All of the following options were taken from https://distrowatch.com/dwres.php?resource=package-management
-      operating_system = cmd_exec('hostnamectl')
-      if operating_system =~ /not found/
-        print_error("Can't determine the host's OS, as the 'hostnamectl' command was not found")
-        return nil
-      elsif operating_system =~ /ubuntu/i || operating_system =~ /debian/i || operating_system =~ /elementary/i || operating_system =~ /mint/i || operating_system =~ /MX/ || operating_system =~ /zorin/i || operating_system =~ /kali/i
-        listing = cmd_exec('apt list --installed')
-        if listing =~ /not found/
-          print_error("The command 'apt' does not exist on the host")
-          return nil
-        end
-        unless listing =~ /listing\.\./i
-          print_error('Was unable to dump the versions of software installed!')
-          return nil
-        end
-        store_linux_loot(listing)
-      elsif operating_system =~ / [aA]rch / || operating_system =~ /manjaro/i
-        listing = cmd_exec('pacman -Q')
-        if listing =~ /not found/
-          print_error("The command 'pacman' does not exist on the host")
-          return nil
-        end
-        store_linux_loot(listing)
+      cmd = %w{ 'hostnamectl' }
+      operating_system = cmd_exec("#{cmd[0]}")
+      if operating_system =~ /(?:[uU]buntu|[dD]ebian|[eE]lementary|[mM]int|MX|[zZ]orin|[kK]ali)/
+        cmd = %w{ 'apt list --installed' }
+      elsif operating_system =~ /(?: [aA]rch |[mM]anjaro)/
+        cmd = %w{ 'pacman -Q' }
       elsif operating_system =~ /opensuse/i
-        listing = cmd_exec('zypper search -is')
-        if listing =~ /not found/
-          print_error("The command 'zypper' does not exist on the host")
-          return nil
-        end
-        store_linux_loot(listing)
-      elsif operating_system =~ /fedora/i || operating_system =~ /centos/i
-        listing = cmd_exec('rpm -qa')
-        if listing =~ /not found/
-          print_error("The command 'rpm' does not exist on the host")
-          return nil
-        end
-        store_linux_loot(listing)
+        cmd = %w{ 'zypper search -is' }
+      elsif operating_system =~ /(?:fedora|centos|red hat enterprise linux)/i
+        cmd = %w{ 'rpm -qa' }
       elsif operating_system =~ /alpine/i
-        listing = cmd_exec('apk info')
-        if listing =~ /not found/
-          print_error("The command 'apk' does not exist on the host")
-          return nil
-        end
-        store_linux_loot(listing)
+        cmd = %w{ 'apk info' }
       elsif operating_system =~ /gentoo/i
-        listing = cmd_exec('qlist -i')
-        if listing =~ /not found/
-          print_error("The command 'qlist' does not exist on the host")
-          return nil
-        end
-        store_linux_loot(listing)
+        cmd = %w{ 'qlist -i' }
       elsif operating_system =~ /freebsd/i
-        listing = cmd_exec('pkg info')
-        if listing =~ /not found/
-          print_error("The command 'pkg' does not exist on the host")
-          return nil
-        end
-        store_linux_loot(listing)
-      else
-        print_error("The target's operating system is not supported at this time.")
+        cmd = %w{ 'pkg info' }
+      end
+      
+      if (listing = cmd_exec(cmd)) =~ /not found/
+        print_error("The command #{cmd[0]} was not found on the target.")
         return nil
+      else
+        store_linux_loot(listing)
       end
     when 'bsd'
       listing = cmd_exec('pkg info')
@@ -109,7 +71,8 @@ class MetasploitModule < Msf::Post
         print_error("The command 'pkg' does not exist on the host")
         return nil
       end
-      store_linux_loot(listing)
+      file = store_loot('host.bsd.software.versions', 'text/plain', session, listing, 'installed_software.txt', 'Installed Software and Versions')
+      print_good("Stored information about the installed products to the loot file at #{file}")
     when 'osx'
       listing = cmd_exec('system_profiler SPApplicationsDataType')
       if listing =~ /not found/
@@ -129,7 +92,7 @@ class MetasploitModule < Msf::Post
     when 'android'
       listing = cmd_exec('pm list packages -f')
       if listing =~ /not found/
-        print_error("The command 'pkg' does not exist on the host")
+        print_error("The command 'pm' does not exist on the host")
         return nil
       end
       file = store_loot('host.android.software.versions', 'text/plain', session, listing, 'installed_software.txt', 'Installed Software and Versions')
