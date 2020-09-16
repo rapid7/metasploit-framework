798e5cce6173b86f29465be87a8604da2c5d0cc2
Grant Willcox <gwillcox@rapid7.com>
2020-09-16 15:06:11 -0500

    Add in casting the results of cmd_exec calls to strings using .to_s as @bcoles recommended and also check for potential cases where no results are returned. This should make the code more defensive and help protect against potential failure cases

--- modules/post/multi/gather/enum_software_versions.rb
+++ modules/post/multi/gather/enum_software_versions.rb
@@ -34,12 +34,12 @@ class MetasploitModule < Msf::Post
     when 'windows'
       if command_exists?('wmic') == false
         print_error("The 'wmic' command doesn't exist on this host!") # wmic is technically marked as depreciated so this command could very well be removed in future releases.
-        return nil
+        return
       end
-      listing = cmd_exec('wmic product get Name, Description, Version, InstallDate', nil, 6000)
+      listing = cmd_exec('wmic product get Name, Description, Version, InstallDate',, 6000).to_s
       unless listing.include?('Description')
         print_error('Was unable to get a listing of installed products...')
-        return nil
+        return
       end
       file = store_loot('host.windows.software.versions', 'text/plain', session, listing, 'installed_software.txt', 'Installed Software and Versions')
       print_good("Stored information about the installed products to the loot file at #{file}")
@@ -48,9 +48,13 @@ class MetasploitModule < Msf::Post
       cmd = %w{ 'hostnamectl' }
       if command_exists?('hostnamectl') == false
         print_error("The 'hostnamectl' command doesn't exist on the host, so we can't enumerate what OS this Linux host is running!")
-        return nil
+        return
       end
-      operating_system = cmd_exec("#{cmd[0]}")
+      operating_system = cmd_exec("#{cmd[0]}").to_s
+      if operating_system.empty?
+        print_error("No results were returned when trying to determine the OS. An error likely occured.")
+        return
+      end
       case operating_system
       when /(?:[uU]buntu|[dD]ebian|[eE]lementary|[mM]int|MX|[zZ]orin|[kK]ali)/
         cmd = %w{ 'apt list --installed' }
@@ -70,33 +74,49 @@ class MetasploitModule < Msf::Post
 
       if (command_exists?("#{cmd[0]}")) == false
         print_error("The command #{cmd[0]} was not found on the target.")
-        return nil
+        return
       else
-        listing = cmd_exec(cmd.join(' '))
+        listing = cmd_exec(cmd.join(' ')).to_s
+        if listing.empty?
+            print_error("No results were returned when trying to get software installed on the Linux host. An error likely occured.")
+            return
+        end
         store_linux_loot(listing)
       end
     when 'bsd','solaris'
       if command_exists?('pkg') == false
         print_error("The command 'pkg' does not exist on the host")
-        return nil
+        return
       end
-      listing = cmd_exec('pkg info')
+      listing = cmd_exec('pkg info').to_s
+      if listing.empty?
+        print_error("No results were returned when trying to get software installed on the BSD/Solaris host. An error likely occured.")
+        return
+      end
       file = store_loot('host.bsd.solaris.software.versions', 'text/plain', session, listing, 'installed_software.txt', 'Installed Software and Versions')
       print_good("Stored information about the installed products to the loot file at #{file}")
     when 'osx'
       if command_exists?('system_profiler') == false
         print_error("The command 'system_profiler' does not exist on the host")
-        return nil
+        return
       end
-      listing = cmd_exec('system_profiler SPApplicationsDataType')
+      listing = cmd_exec('system_profiler SPApplicationsDataType').to_s
+      if listing.empty?
+        print_error("No results were returned when trying to get software installed on the OSX host. An error likely occured.")
+        return
+      end
       file = store_loot('host.osx.software.versions', 'text/plain', session, listing, 'installed_software.txt', 'Installed Software and Versions')
       print_good("Stored information about the installed products to the loot file at #{file}")
     when 'android'
       if command_exists?('pm') == false
         print_error("The command 'pm' does not exist on the host")
-        return nil
+        return
       end
-      listing = cmd_exec('pm list packages -f')
+      listing = cmd_exec('pm list packages -f').to_s
+      if listing.empty?
+        print_error("No results were returned when trying to get software installed on the Linux host. An error likely occured.")
+        return
+      end
       file = store_loot('host.android.software.versions', 'text/plain', session, listing, 'installed_software.txt', 'Installed Software and Versions')
       print_good("Stored information about the installed products to the loot file at #{file}")
     end
