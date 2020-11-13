49fac19692717bcd5d49503fc311a85e50e66e6b
Jeffrey Martin <Jeffrey_Martin@rapid7.com>
2020-11-13 17:06:14 -0600

    guard persist on handler existence
    Short term fix to only persist jobs with handlers. More work should
    be done to improve job persistence to allow more jobs types to persist.

--- lib/msf/base/serializer/readable_text.rb
+++ lib/msf/base/serializer/readable_text.rb
@@ -1018,8 +1018,9 @@ class ReadableText
         end
 
         persist_list.each do |e|
-          if framework.jobs[job_id.to_s].ctx[1]
-             row[7] = 'true' if e['mod_options']['Options'] == framework.jobs[job_id.to_s].ctx[1].datastore
+          handler_ctx = framework.jobs[job_id.to_s].ctx[1]
+          if handler_ctx && handler_ctx.respond_to?(:datastore)
+             row[7] = 'true' if e['mod_options']['Options'] == handler_ctx.datastore
           end
         end
 
