3586644b62a722254d00dacbccbf7ee136d95d71
Spencer McIntyre <zeroSteiner@gmail.com>
2020-11-16 15:58:59 -0500

    Increase the payload space to 4096 within the DLL template

--- lib/msf/util/exe.rb
+++ lib/msf/util/exe.rb
@@ -545,7 +545,7 @@ require 'msf/core/exe/segment_appender'
       end
       pe[136, 4] = [rand(0x100000000)].pack('V') unless opts[:sub_method]
     when :dll
-      max_length = 2048
+      max_length = 4096
     when :exe_sub
       max_length = 4096
     end
