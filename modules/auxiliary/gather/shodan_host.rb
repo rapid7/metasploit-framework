12277d3020f8e413b4396f140fe18435e1d6fbf0
Grant Willcox <gwillcox@rapid7.com>
2020-12-21 15:26:48 -0600

    Apply RuboCop changes to the exploit module and also make final adjustments to the exploit code to handle some edge cases and fix review comments

--- modules/auxiliary/gather/shodan_host.rb
+++ modules/auxiliary/gather/shodan_host.rb
@@ -6,29 +6,31 @@ class MetasploitModule < Msf::Auxiliary
   include Msf::Exploit::Remote::HttpClient
   include Msf::Auxiliary::Report
   def initialize(info = {})
-    super(update_info(info,
-      'Name' => 'Shodan Host Port',
-      'Description' => %q{
-        This module uses the shodan API to return all port information found on a given host IP.
-      },
-      'Author' =>
-        [ 'natto97' ],
-      'License' => MSF_LICENSE,
-      'References' =>
-        [
-          [ 'URL', 'https://honeyscore.shodan.io/']
-        ]
+    super(
+      update_info(
+        info,
+        'Name' => 'Shodan Host Port',
+        'Description' => %q{
+          This module uses the shodan API to return all port information found on a given host IP.
+        },
+        'Author' => [ 'natto97' ],
+        'License' => MSF_LICENSE,
+        'References' =>
+          [
+            [ 'URL', 'https://honeyscore.shodan.io/']
+          ]
       )
     )
     register_options(
       [
         OptString.new('SHODAN_APIKEY', [true, 'The SHODAN API key'])
-      ])
-    deregister_options("SSL")
-    deregister_options("RPORT")
-    deregister_options("VHOST")
-    deregister_options("Proxies")
+      ]
+    )
+    deregister_options('SSL')
+    deregister_options('RPORT')
+    deregister_options('VHOST')
   end
+
   def run
     # check our API key is somewhat sane
     unless /^[a-z\d]{32}$/i.match?(datastore['SHODAN_APIKEY'])
@@ -37,29 +39,34 @@ class MetasploitModule < Msf::Auxiliary
     key = datastore['SHODAN_APIKEY']
     # Check the length of the key (should be 32 chars)
     if key.length != 32
-      print_error('Invalid API key (Not long enough)')
-      return
+      fail_with(Failure::BadConfig, 'Invalid API key (Not long enough)')
     end
     cli = Rex::Proto::Http::Client.new('api.shodan.io', 443, {}, true)
     cli.connect
     req = cli.request_cgi({
-      'uri'    => "/shodan/host/#{rhost}?key=#{key}&minify=true",
-      'method' => 'GET',
-      })
+      'uri' => "/shodan/host/#{rhost}?key=#{key}&minify=true",
+      'method' => 'GET'
+    })
     res = cli.send_recv(req)
     cli.close
     if res.nil?
-      fail_with(Failure::Unknown, 'Unable to connect to shodan')
+      fail_with(Failure::Unreachable, 'Unable to connect to shodan')
+    end
+    if res.body =~ /No information available for that IP/
+      print_error('The target IP address has not been scanned by Shodan!')
+      return
     end
     if res.code != 200
-      print_error('Shodan did not respond in an expected way. Check your api key')
+      fail_with(Failure::UnexpectedReply, 'Shodan did not respond in an expected way. Check your api key')
     end
     json = res.get_json_document
-    if json["ports"] != nil
-      json["ports"].each do |post|
+    if !json.nil? && !json['ports'].nil? && !json['ports'].empty?
+      json['ports'].each do |post|
         print_good("#{rhost}:#{post}")
-        report_service(:host => rhost, :port => post, :name => "shodan")
+        report_service(host: rhost, port: post, name: 'shodan')
       end
+    else
+      print_error("Shodan did not return any open ports for #{rhost}!")
     end
   end
 end
