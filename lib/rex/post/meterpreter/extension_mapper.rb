2bc8ff8db7b55e83c77245d36436db459549f6c1
Spencer McIntyre <zeroSteiner@gmail.com>
2021-01-15 13:04:38 -0500

    Consistently return nil when an id or name fails to resolve

--- lib/rex/post/meterpreter/extension_mapper.rb
+++ lib/rex/post/meterpreter/extension_mapper.rb
@@ -16,18 +16,24 @@ class ExtensionMapper
   end
 
   def self.get_extension_id(name)
-    k = self.get_extension_klass(name)
+    begin
+      k = self.get_extension_klass(name)
+    rescue RuntimeError
+      return nil
+    end
+
     k.extension_id
   end
 
   def self.get_extension_name(id)
-    self.get_extension_names.each do |name|
+    self.get_extension_names.find do |name|
       begin
         klass = self.get_extension_klass(name)
       rescue RuntimeError
         next
       end
-      return name if klass.extension_id == id
+
+      klass.extension_id == id
     end
   end
 
