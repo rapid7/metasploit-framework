f21e3c238633ce78ba3f9f15c0ebbc950bea61e6
RAMELLA Sébastien <sebastien.ramella@pirates.re>
2021-02-01 09:55:56 +0400

    fix. typo.

--- modules/auxiliary/scanner/http/fortios_vpnssl_traversal_leak.rb
+++ modules/auxiliary/scanner/http/fortios_vpnssl_traversal_leak.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 ##
 # This module requires Metasploit: https://metasploit.com/download
 # Current source: https://github.com/rapid7/metasploit-framework
@@ -37,7 +35,7 @@ class MetasploitModule < Msf::Auxiliary
       ],
       'License' => MSF_LICENSE,
       'DefaultOptions' => {
-        'RPORT' => 443,
+        'RPORT' => 10443,
         'SSL' => true
       }
     ))
