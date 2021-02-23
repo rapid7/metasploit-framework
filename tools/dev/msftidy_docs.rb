4dfd62fd470baa707d2c221cf26bcbcf982538a0
XenoAmess <xenoamess@gmail.com>
2021-02-24 00:18:39 +0800

    Update tools/dev/msftidy_docs.rb
    Co-authored-by: Jeffrey Martin <jeffrey_martin@rapid7.com>

--- tools/dev/msftidy_docs.rb
+++ tools/dev/msftidy_docs.rb
@@ -102,7 +102,7 @@ class MsftidyDoc
   def has_module
     module_filepath = @full_filepath.sub('documentation/','').sub('/exploit/', '/exploits/')
     found = false
-    ['.rb', '.py', '.go', '.jar', '.java'].each do |ext|
+    ['.rb', '.py', '.go', '.java'].each do |ext|
       if File.file? module_filepath.sub(/.md$/, ext)
         found = true
         break
