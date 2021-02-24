a6461a0cfee249a8a20e2b245fbdeb9208801685
dwelch-r7 <dean_welch@rapid7.com>
2021-02-24 12:49:26 +0000

    rubocop

--- lib/rex/version.rb
+++ lib/rex/version.rb
@@ -1,8 +1,7 @@
-
 class Rex::Version < Gem::Version
 
   def initialize(version)
-    if version == nil
+    if version.nil?
       # Rubygems 4 is deprecating `nil` as a valid version number
       # Currently it is the equivalent of a `0` so we set that here to keep the same functionality
       version = 0
