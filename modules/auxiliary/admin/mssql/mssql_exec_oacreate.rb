c570c9f30e51c27e042addbb42aa2a82c8b2b4f4
Paul Werther <pw@evait.de>
2021-02-26 16:16:23 +0100

    add documentation and linting (rubocop)

--- modules/auxiliary/admin/mssql/mssql_exec_oacreate.rb
+++ modules/auxiliary/admin/mssql/mssql_exec_oacreate.rb
@@ -7,23 +7,26 @@ class MetasploitModule < Msf::Auxiliary
   include Msf::Exploit::Remote::MSSQL
 
   def initialize(info = {})
-    super(update_info(info,
-      'Name'           => 'Microsoft SQL Server sp_oacreate Command Execution',
-      'Description'    => %q{
-        This module will execute a Windows command on a MSSQL/MSDE instance
-        via the sp_oacreate procedure (ole) instead of the xp_cmdshell.
-        A valid username and password is required to use this module.
-      },
-      'Author'         => [ 'arcc <pw[at]evait.de>' ],
-      'License'        => MSF_LICENSE,
-      'References'     =>
-        [
-          [ 'URL', 'https://docs.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sp-oacreate-transact-sql'],
-        ]
-    ))
+    super(
+      update_info(
+        info,
+        'Name' => 'Microsoft SQL Server sp_oacreate Command Execution',
+        'Description' => %q{
+          This module will execute a Windows command on a MSSQL/MSDE instance
+          via the sp_oacreate procedure (ole) instead of the xp_cmdshell.
+          A valid username and password is required to use this module.
+        },
+        'Author' => [ 'arcc <pw[at]evait.de>' ],
+        'License' => MSF_LICENSE,
+        'References' =>
+          [
+            [ 'URL', 'https://docs.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sp-oacreate-transact-sql'],
+          ]
+      )
+    )
 
-    register_options( [
-      OptString.new('CMD', [ false, 'Command to execute',  'cmd.exe /c echo OWNED > C:\\owned.exe']),
+    register_options([
+      OptString.new('CMD', [ false, 'Command to execute', 'cmd.exe /c echo OWNED > C:\\owned.exe']),
     ])
   end
 
