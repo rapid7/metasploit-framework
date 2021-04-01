278c56652e648c13fac88d8a0bdca3c66d3742b6
dwelch-r7 <dean_welch@rapid7.com>
2021-04-01 14:17:28 +0100

    Update RbMysql to the most recent code from this gem https://github.com/tmtm/ruby-mysql

--- lib/rbmysql/constants.rb
+++ lib/rbmysql/constants.rb
@@ -1,4 +1,4 @@
-# -*- coding: binary -*-
+# coding: ascii-8bit
 # Copyright (C) 2003-2008 TOMITA Masahiro
 # mailto:tommy@tmtm.org
 
@@ -33,26 +33,38 @@ class RbMysql
   COM_STMT_RESET          = 26
   COM_SET_OPTION          = 27
   COM_STMT_FETCH          = 28
+  COM_DAEMON              = 29
+  COM_BINLOG_DUMP_GTID    = 30
+  COM_RESET_CONNECTION    = 31
 
   # Client flag
-  CLIENT_LONG_PASSWORD     = 1         # new more secure passwords
-  CLIENT_FOUND_ROWS        = 1 << 1    # Found instead of affected rows
-  CLIENT_LONG_FLAG         = 1 << 2    # Get all column flags
-  CLIENT_CONNECT_WITH_DB   = 1 << 3    # One can specify db on connect
-  CLIENT_NO_SCHEMA         = 1 << 4    # Don't allow database.table.column
-  CLIENT_COMPRESS          = 1 << 5    # Can use compression protocol
-  CLIENT_ODBC              = 1 << 6    # Odbc client
-  CLIENT_LOCAL_FILES       = 1 << 7    # Can use LOAD DATA LOCAL
-  CLIENT_IGNORE_SPACE      = 1 << 8    # Ignore spaces before '('
-  CLIENT_PROTOCOL_41       = 1 << 9    # New 4.1 protocol
-  CLIENT_INTERACTIVE       = 1 << 10   # This is an interactive client
-  CLIENT_SSL               = 1 << 11   # Switch to SSL after handshake
-  CLIENT_IGNORE_SIGPIPE    = 1 << 12   # IGNORE sigpipes
-  CLIENT_TRANSACTIONS      = 1 << 13   # Client knows about transactions
-  CLIENT_RESERVED          = 1 << 14   # Old flag for 4.1 protocol
-  CLIENT_SECURE_CONNECTION = 1 << 15   # New 4.1 authentication
-  CLIENT_MULTI_STATEMENTS  = 1 << 16   # Enable/disable multi-stmt support
-  CLIENT_MULTI_RESULTS     = 1 << 17   # Enable/disable multi-results
+  CLIENT_LONG_PASSWORD                  = 1         # new more secure passwords
+  CLIENT_FOUND_ROWS                     = 1 << 1    # Found instead of affected rows
+  CLIENT_LONG_FLAG                      = 1 << 2    # Get all column flags
+  CLIENT_CONNECT_WITH_DB                = 1 << 3    # One can specify db on connect
+  CLIENT_NO_SCHEMA                      = 1 << 4    # Don't allow database.table.column
+  CLIENT_COMPRESS                       = 1 << 5    # Can use compression protocol
+  CLIENT_ODBC                           = 1 << 6    # Odbc client
+  CLIENT_LOCAL_FILES                    = 1 << 7    # Can use LOAD DATA LOCAL
+  CLIENT_IGNORE_SPACE                   = 1 << 8    # Ignore spaces before '('
+  CLIENT_PROTOCOL_41                    = 1 << 9    # New 4.1 protocol
+  CLIENT_INTERACTIVE                    = 1 << 10   # This is an interactive client
+  CLIENT_SSL                            = 1 << 11   # Switch to SSL after handshake
+  CLIENT_IGNORE_SIGPIPE                 = 1 << 12   # IGNORE sigpipes
+  CLIENT_TRANSACTIONS                   = 1 << 13   # Client knows about transactions
+  CLIENT_RESERVED                       = 1 << 14   # Old flag for 4.1 protocol
+  CLIENT_SECURE_CONNECTION              = 1 << 15   # New 4.1 authentication
+  CLIENT_MULTI_STATEMENTS               = 1 << 16   # Enable/disable multi-stmt support
+  CLIENT_MULTI_RESULTS                  = 1 << 17   # Enable/disable multi-results
+  CLIENT_PS_MULTI_RESULTS               = 1 << 18   # Multi-results in PS-protocol
+  CLIENT_PLUGIN_AUTH                    = 1 << 19   # Client supports plugin authentication
+  CLIENT_CONNECT_ATTRS                  = 1 << 20   # Client supports connection attribute
+  CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA = 1 << 21   # Enable authentication response packet to be larger than 255 bytes.
+  CLIENT_CAN_HANDLE_EXPIRED_PASSWORDS   = 1 << 22   # Don't close the connection for a connection with expired password.
+  CLIENT_SESSION_TRACK                  = 1 << 23   # Capable of handling server state change information. Its a hint to the server to include the state change information in Ok packet.
+  CLIENT_DEPRECATE_EOF                  = 1 << 24   # Client no longer needs EOF packet
+  CLIENT_SSL_VERIFY_SERVER_CERT         = 1 << 30
+  CLIENT_REMEMBER_OPTIONS               = 1 << 31
 
   # Connection Option
   OPT_CONNECT_TIMEOUT         = 0
@@ -92,18 +104,35 @@ class RbMysql
   SERVER_STATUS_LAST_ROW_SENT        = 1 << 7
   SERVER_STATUS_DB_DROPPED           = 1 << 8
   SERVER_STATUS_NO_BACKSLASH_ESCAPES = 1 << 9
+  SERVER_STATUS_METADATA_CHANGED     = 1 << 10
+  SERVER_QUERY_WAS_SLOW              = 1 << 11
+  SERVER_PS_OUT_PARAMS               = 1 << 12
+  SERVER_STATUS_IN_TRANS_READONLY    = 1 << 13
+  SERVER_SESSION_STATE_CHANGED       = 1 << 14
 
   # Refresh parameter
-  REFRESH_GRANT     = 1
-  REFRESH_LOG       = 1 << 1
-  REFRESH_TABLES    = 1 << 2
-  REFRESH_HOSTS     = 1 << 3
-  REFRESH_STATUS    = 1 << 4
-  REFRESH_THREADS   = 1 << 5
-  REFRESH_SLAVE     = 1 << 6
-  REFRESH_MASTER    = 1 << 7
-  REFRESH_READ_LOCK = 1 << 14
-  REFRESH_FAST      = 1 << 15
+  REFRESH_GRANT            = 1
+  REFRESH_LOG              = 1 << 1
+  REFRESH_TABLES           = 1 << 2
+  REFRESH_HOSTS            = 1 << 3
+  REFRESH_STATUS           = 1 << 4
+  REFRESH_THREADS          = 1 << 5
+  REFRESH_SLAVE            = 1 << 6
+  REFRESH_MASTER           = 1 << 7
+  REFRESH_ERROR_LOG        = 1 << 8
+  REFRESH_ENGINE_LOG       = 1 << 9
+  REFRESH_BINARY_LOG       = 1 << 10
+  REFRESH_RELAY_LOG        = 1 << 11
+  REFRESH_GENERAL_LOG      = 1 << 12
+  REFRESH_SLOW_LOG         = 1 << 13
+  REFRESH_READ_LOCK        = 1 << 14
+  REFRESH_FAST             = 1 << 15
+  REFRESH_QUERY_CACHE      = 1 << 16
+  REFRESH_QUERY_CACHE_FREE = 1 << 17
+  REFRESH_DES_KEY_FILE     = 1 << 18
+  REFRESH_USER_RESOURCES   = 1 << 19
+  REFRESH_FOR_EXPORT       = 1 << 20
+  REFRESH_OPTIMIZER_COSTS  = 1 << 21
 
   class Field
     # Field type
@@ -124,6 +153,10 @@ class RbMysql
     TYPE_NEWDATE     = 14
     TYPE_VARCHAR     = 15
     TYPE_BIT         = 16
+    TYPE_TIMESTAMP2  = 17
+    TYPE_DATETIME2   = 18
+    TYPE_TIME2       = 19
+    TYPE_JSON        = 245
     TYPE_NEWDECIMAL  = 246
     TYPE_ENUM        = 247
     TYPE_SET         = 248
@@ -138,29 +171,32 @@ class RbMysql
     TYPE_INTERVAL    = TYPE_ENUM
 
     # Flag
-    NOT_NULL_FLAG       = 1
-    PRI_KEY_FLAG        = 2
-    UNIQUE_KEY_FLAG     = 4
-    MULTIPLE_KEY_FLAG   = 8
-    BLOB_FLAG           = 16
-    UNSIGNED_FLAG       = 32
-    ZEROFILL_FLAG       = 64
-    BINARY_FLAG         = 128
-    ENUM_FLAG           = 256
-    AUTO_INCREMENT_FLAG = 512
-    TIMESTAMP_FLAG      = 1024
-    SET_FLAG            = 2048
-    NUM_FLAG            = 32768
-    PART_KEY_FLAG       = 16384
-    GROUP_FLAG          = 32768
-    UNIQUE_FLAG         = 65536
-    BINCMP_FLAG         = 131072
+    NOT_NULL_FLAG         = 1
+    PRI_KEY_FLAG          = 2
+    UNIQUE_KEY_FLAG       = 4
+    MULTIPLE_KEY_FLAG     = 8
+    BLOB_FLAG             = 16
+    UNSIGNED_FLAG         = 32
+    ZEROFILL_FLAG         = 64
+    BINARY_FLAG           = 128
+    ENUM_FLAG             = 256
+    AUTO_INCREMENT_FLAG   = 512
+    TIMESTAMP_FLAG        = 1024
+    SET_FLAG              = 2048
+    NO_DEFAULT_VALUE_FLAG = 4096
+    ON_UPDATE_NOW_FLAG    = 8192
+    NUM_FLAG              = 32768
+    PART_KEY_FLAG         = 16384
+    GROUP_FLAG            = 32768
+    UNIQUE_FLAG           = 65536
+    BINCMP_FLAG           = 131072
   end
 
-  class Statement
+  class Stmt
     # Cursor type
-    CURSOR_TYPE_NO_CURSOR = 0
-    CURSOR_TYPE_READ_ONLY = 1
+    CURSOR_TYPE_NO_CURSOR  = 0
+    CURSOR_TYPE_READ_ONLY  = 1
+    CURSOR_TYPE_FOR_UPDATE = 2
+    CURSOR_TYPE_SCROLLABLE = 4
   end
 end
-
