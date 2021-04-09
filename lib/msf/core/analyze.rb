14a3d480448600a9fc24ca6aa089f632aa34c35c
Adam Cammack <adam_cammack@rapid7.com>
2021-04-09 03:53:52 -0500

    Fix grouping of transitive vulns and add spec
    Properly merge the contents of the grouping sets when combining sets of
    vulns. All permutations of simple double-transitive sets are now tested.

--- lib/msf/core/analyze.rb
+++ lib/msf/core/analyze.rb
@@ -5,23 +5,32 @@ class Msf::Analyze
   end
 
   def host(eval_host)
-    suggested_modules = []
-
-    mrefs, _mports, _mservs = Msf::Modules::Metadata::Cache.instance.all_exploit_maps
-
     unless eval_host.vulns
       return {}
     end
 
     # Group related vulns
-    vulns = eval_host.vulns.map do |vuln|
+    vuln_families = group_vulns(eval_host.vulns)
+
+    # finds all modules that have references matching those found on host vulns with service data
+    suggested_modules = suggest_modules_for_vulns(eval_host, vuln_families)
+
+    {results: suggested_modules}
+  end
+
+  private
+
+  def group_vulns(vulns)
+    return [] if vulns.empty?
+
+    vulns = vulns.map do |vuln|
       [vuln, Set.new(vuln.refs.map {|r| r.name.upcase})]
     end
     grouped_vulns = Hash.new
 
     vulns.each_index do |ii|
       vuln, refs = vulns[ii]
-      grouped_vulns[vuln] ||= [Array.new, Set.new]
+      grouped_vulns[vuln] ||= [Set.new, Set.new]
       grouped_vulns[vuln][0] << vuln
       grouped_vulns[vuln][1].merge(refs)
 
@@ -29,17 +38,34 @@ class Msf::Analyze
         # TODO: measure if sorting the refs ahead of time and doing a O(n + m)
         # walk here is faster
         if candidate_refs.intersect? refs
-          grouped_vulns[candidate_match] = grouped_vulns[vuln]
+          if grouped_vulns[candidate_match]
+            # For merging two different transitive sets that overlap, we need
+            # to merge the individual grouping elements and then use those
+            # cells inside both the grouping arrays so that all the
+            # already-visited vulns are rolled into the big new set
+            grouped_vulns[candidate_match][0] = grouped_vulns[candidate_match][0].merge(grouped_vulns[vuln][0])
+            grouped_vulns[candidate_match][1] = grouped_vulns[candidate_match][1].merge(grouped_vulns[vuln][1])
+            grouped_vulns[vuln][0] = grouped_vulns[candidate_match][0]
+            grouped_vulns[vuln][1] = grouped_vulns[candidate_match][1]
+          else
+            # Whoever was initialized first has the canonical set
+            grouped_vulns[candidate_match] = grouped_vulns[vuln]
+          end
         end
       end
     end
 
     vuln_families = grouped_vulns.values
-    vuln_families = vuln_families.uniq! || vuln_families
-    # finds all modules that have references matching those found on host vulns with service data
+    vuln_families.uniq! || vuln_families
+  end
+
+  def suggest_modules_for_vulns(eval_host, vuln_families)
+    mrefs, _mports, _mservs = Msf::Modules::Metadata::Cache.instance.all_exploit_maps
+    suggested_modules = []
+
     evaluated_module_targets = Set.new
     to_evaluate_with_defaults = Array.new
-    vuln_families&.each do |vulns, refs|
+    vuln_families.each do |vulns, refs|
       found_modules = mrefs.values_at(*refs).compact.reduce(:+)
       next unless found_modules
 
@@ -79,6 +105,6 @@ class Msf::Analyze
       evaluated_module_targets << [fnd_mod, port]
     end
 
-    {results: suggested_modules}
+    suggested_modules
   end
 end
