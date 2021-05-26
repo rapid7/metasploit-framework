22f25d7a7fa9ec64f959d4486d74a9b7552d237c
Hakyac <84333864+Hakyac@users.noreply.github.com>
2021-05-26 15:34:54 +0200

    Update modules/auxiliary/gather/http/cve_2021_27850_apache_tapestry_hmac_key.rb
    Co-authored-by: Shelby Pace <40177151+space-r7@users.noreply.github.com>

--- modules/auxiliary/gather/http/cve_2021_27850_apache_tapestry_hmac_key.rb
+++ modules/auxiliary/gather/http/cve_2021_27850_apache_tapestry_hmac_key.rb
@@ -30,7 +30,7 @@ class MetasploitModule < Msf::Auxiliary
 
     register_options([
       Opt::RPORT(8080),
-      OptString.new('TARGETED_CLASS', [false, 'Name of the targeted java class', 'AppModule.class'])
+      OptString.new('TARGETED_CLASS', [true, 'Name of the targeted java class', 'AppModule.class'])
     ])
   end
 
