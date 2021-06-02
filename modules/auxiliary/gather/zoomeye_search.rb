9de8ad8108597d66c9af5626217cc358142bc9a6
Owein <owein@localhost.localdomain>
2021-06-02 15:51:03 +0200

    with web search and possible lists of results: do not use newline to list it but pass the list as is to the table

--- modules/auxiliary/gather/zoomeye_search.rb
+++ modules/auxiliary/gather/zoomeye_search.rb
@@ -6,44 +6,49 @@
 class MetasploitModule < Msf::Auxiliary
   include Msf::Auxiliary::Report
 
-  def initialize(info={})
-    super(update_info(info,
-      'Name'        => 'ZoomEye Search',
-      'Description' => %q{
-        The module use the ZoomEye API to search ZoomEye. ZoomEye is a search
-        engine for cyberspace that lets the user find specific network
-        components(ip, services, etc.).
-        Mind to enclose the whole request with quotes and limit the span of filters:
-        `set zoomeye_dork 'country:"france"+some+query'`
+  def initialize(info = {})
+    super(
+      update_info(
+        info,
+        'Name' => 'ZoomEye Search',
+        'Description' => %q{
+          The module use the ZoomEye API to search ZoomEye. ZoomEye is a search
+          engine for cyberspace that lets the user find specific network
+          components(ip, services, etc.).
+          Mind to enclose the whole request with quotes and limit the span of filters:
+          `set zoomeye_dork 'country:"france"+some+query'`
 
-        Setting facets will output a simple report on the overall search. It's values are:
-        Host search: app, device, service, os, port, country, city
-        Web search: webapp, component, framework, frontend, server, waf, os, country, city
+          Setting facets will output a simple report on the overall search. It's values are:
+          Host search: app, device, service, os, port, country, city
+          Web search: webapp, component, framework, frontend, server, waf, os, country, city
 
-        Possible filters values are:
-        Host search: app, ver, device, os, service, ip, cidr, hostname, port, city, country, asn
-        Web search: app, header, keywords, desc, title, ip, site, city, country
-      },
-      'Author'      => [ 'Nixawk, modified by Yvain' ],
-      'References'  => [
-        ['URL', 'https://github.com/zoomeye/SDK'],
-        ['URL', 'https://www.zoomeye.org/api/doc'],
-        ['URL', 'https://www.zoomeye.org/help/manual']
-      ],
-      'License'     => MSF_LICENSE
-      ))
-      register_options(
-        [
-          OptString.new('USERNAME', [true, 'The ZoomEye username']),
-          OptString.new('PASSWORD', [true, 'The ZoomEye password']),
-          OptString.new('ZOOMEYE_DORK', [true, 'The ZoomEye dork']),
-          OptString.new('FACETS', [false, 'A comma-separated list of properties to get summary information on query', nil]),
-          OptEnum.new('RESOURCE', [true, 'ZoomEye Resource Type', 'host', ['host', 'web']]),
-          OptInt.new('MAXPAGE', [true, 'Max amount of pages to collect', 1]),
-          OptString.new('OUTFILE', [false, 'A filename to store the list of IPs']),
-          OptBool.new('DATABASE', [false, 'Add search results to the database', false])
-       ])
+          Possible filters values are:
+          Host search: app, ver, device, os, service, ip, cidr, hostname, port, city, country, asn
+          Web search: app, header, keywords, desc, title, ip, site, city, country
+        },
+        'Author' => [ 'Nixawk', 'Yvain' ],
+        'References' => [
+          ['URL', 'https://github.com/zoomeye/SDK'],
+          ['URL', 'https://www.zoomeye.org/api/doc'],
+          ['URL', 'https://www.zoomeye.org/help/manual']
+        ],
+        'License' => MSF_LICENSE
+      )
+    )
+    register_options(
+      [
+        OptString.new('USERNAME', [true, 'The ZoomEye username']),
+        OptString.new('PASSWORD', [true, 'The ZoomEye password']),
+        OptString.new('ZOOMEYE_DORK', [true, 'The ZoomEye dork']),
+        OptString.new('FACETS', [false, 'A comma-separated list of properties to get summary information on query', nil]),
+        OptEnum.new('RESOURCE', [true, 'ZoomEye Resource Type', 'host', ['host', 'web']]),
+        OptInt.new('MAXPAGE', [true, 'Max amount of pages to collect', 1]),
+        OptString.new('OUTFILE', [false, 'A filename to store the list of IPs']),
+        OptBool.new('DATABASE', [false, 'Add search results to the database', false])
+      ]
+    )
   end
+
   # save output to file
   def save_output(data)
     ::File.open(datastore['OUTFILE'], 'wb') do |f|
@@ -55,11 +60,11 @@ class MetasploitModule < Msf::Auxiliary
   # Check to see if api.zoomeye.org resolves properly
   def zoomeye_resolvable?
     begin
-      Rex::Socket.resolv_to_dotted("api.zoomeye.org")
+      Rex::Socket.resolv_to_dotted('api.zoomeye.org')
     rescue RuntimeError, SocketError
       return false
     end
-    return true
+    true
   end
 
   def login(username, password)
@@ -69,11 +74,11 @@ class MetasploitModule < Msf::Auxiliary
     @cli = Rex::Proto::Http::Client.new('api.zoomeye.org', 443, {}, true)
     @cli.connect
 
-    data = {'username' => username, 'password' => password}
+    data = { 'username' => username, 'password' => password }
     req = @cli.request_cgi({
-      'uri'    => '/user/login',
+      'uri' => '/user/login',
       'method' => 'POST',
-      'data'   => data.to_json
+      'data' => data.to_json
     })
 
     res = @cli.send_recv(req)
@@ -85,26 +90,25 @@ class MetasploitModule < Msf::Auxiliary
 
     records = ActiveSupport::JSON.decode(res.body)
     access_token = records['access_token'] if records && records.key?('access_token')
-    return access_token
+    access_token
   end
 
   def dork_search(resource, dork, page, facets)
     begin
       req = @cli.request_cgi({
-        'uri'      => "/#{resource}/search",
-        'method'   => 'GET',
-        'headers'  => { 'Authorization' => "JWT #{@zoomeye_token}" },
+        'uri' => "/#{resource}/search",
+        'method' => 'GET',
+        'headers' => { 'Authorization' => "JWT #{@zoomeye_token}" },
         'vars_get' => {
-          'query'  => dork,
-          'page'   => page,
-          'facets'  => facets
+          'query' => dork,
+          'page' => page,
+          'facets' => facets
         }
       })
 
       res = @cli.send_recv(req)
-
     rescue ::Rex::ConnectionError, Errno::ECONNREFUSED, Errno::ETIMEDOUT
-      print_error("HTTP Connection Failed")
+      print_error('HTTP Connection Failed')
     end
 
     unless res
@@ -117,7 +121,7 @@ class MetasploitModule < Msf::Auxiliary
     if res.body =~ /Invalid Token, /
       fail_with(Failure::BadConfig, '401 Unauthorized. Your ZOOMEYE_APIKEY is invalid')
     end
-    return ActiveSupport::JSON.decode(res.body)
+    res.get_json_document
   end
 
   def match_records?(records)
@@ -131,24 +135,25 @@ class MetasploitModule < Msf::Auxiliary
     facets = datastore['FACETS']
     # check to ensure api.zoomeye.org is resolvable
     unless zoomeye_resolvable?
-      print_error("Unable to resolve api.zoomeye.org")
+      print_error('Unable to resolve api.zoomeye.org')
       return
     end
 
     @zoomeye_token = login(datastore['USERNAME'], datastore['PASSWORD'])
     if @zoomeye_token.blank?
-      print_error("Unable to login api.zoomeye.org")
+      print_error('Unable to login api.zoomeye.org')
       return
     else
-      print_status("Logged in to zoomeye")
+      print_status('Logged in to zoomeye')
     end
 
+    first_page = 0
     results = []
-    results[0] = dork_search(resource, dork, 1, facets)
+    results[first_page] = dork_search(resource, dork, 1, facets)
 
-    if results[0]['total'].nil? || results[0]['total'] == 0
-      msg = "No results."
-      if results[0]['error'].to_s.length > 0
+    if results[first_page]['total'].nil? || results[first_page]['total'] == 0
+      msg = 'No results.'
+      if !results[first_page]['error'].to_s.empty?
         msg << " Error: #{results[0]['error']}"
       end
       print_error(msg)
@@ -156,52 +161,57 @@ class MetasploitModule < Msf::Auxiliary
     end
 
     # Determine page count based on total results
-    if results[0]['total'] % 20 == 0
-      tpages = results[0]['total'] / 20
+    if results[first_page]['total'] % 20 == 0
+      tpages = results[first_page]['total'] / 20
     else
-      tpages = results[0]['total'] / 20 + 1
+      tpages = results[first_page]['total'] / 20 + 1
     end
     maxpage = tpages if datastore['MAXPAGE'] > tpages
 
-    print_status("Total: #{results[0]['total']} on #{tpages} " +
-      "pages. Showing: #{maxpage} page(s)")
-
-    # If search results greater than 20, loop & get all results
-    if results[0]['total'] > 20
-      print_status('Collecting data, please wait...')
-      page = 1
-      while page < maxpage
-        page_result = dork_search(resource, dork, page+1, facets)
-        if page_result['matches'].nil?
-          next
+    if facets
+      facets_tbl = Rex::Text::Table.new(
+        'Header' => 'Facets',
+        'Indent' => 1,
+        'Columns' => ['Facet', 'Name', 'Count']
+      )
+      print_status("Total: #{results[first_page]['total']} on #{tpages} " \
+        'pages. Showing facets')
+      facet = results[first_page]['facets']
+      facet.each do |name, list|
+        list.each do |f|
+          facets_tbl << [name.to_s, (f['name']).to_s, (f['count']).to_s]
         end
-        results[page] = page_result
-        page += 1
       end
-    end
-
-    tbl1 = Rex::Text::Table.new(
-      'Header'  => 'Search Results',
-      'Indent'  => 1,
-      'Columns' => ['IP:Port', 'City', 'Country', 'Hostname', 'OS', 'Service:Version', 'Info']
-    )
-    tbl2 = Rex::Text::Table.new(
-      'Header'  => 'Search Results',
-      'Indent'  => 1,
-      'Columns' => ['IP', 'Site', 'City', 'Country', 'DB:Version', 'WebApp:Version']
-    )
-    # scroll max pages from ZoomEye
-    results.each do |page|
-      if facets
-        fcets = page['facets']
-        fcets.each do |fac|
-          print_line("#{fac[0]}")
-          fac[1].each do |f|
-            print_line("#{f['name']} count=#{f['count']}")
+      print_line(facets_tbl.to_s)
+    else
+      print_status("Total: #{results[first_page]['total']} on #{tpages} " \
+        "pages. Showing: #{maxpage} page(s)")
+      # If search results greater than 20, loop & get all results
+      if results[first_page]['total'] > 20
+        print_status('Collecting data, please wait...')
+        page = 1
+        while page < maxpage
+          page_result = dork_search(resource, dork, page + 1, facets)
+          if page_result['matches'].nil?
+            next
           end
+          results[page] = page_result
+          page += 1
         end
-      else
-        page['matches'].each do |match|
+      end
+      tbl1 = Rex::Text::Table.new(
+        'Header' => 'Host search',
+        'Indent' => 1,
+        'Columns' => ['IP:Port', 'City', 'Country', 'Hostname', 'OS', 'Service:Version', 'Info']
+      )
+      tbl2 = Rex::Text::Table.new(
+        'Header' => 'Web search',
+        'Indent' => 1,
+        'Columns' => ['IP', 'Site', 'City', 'Country', 'DB:Version', 'WebApp:Version']
+      )
+      # scroll max pages from ZoomEye
+      results.each do |result|
+        result['matches'].each do |match|
           city = match['geoinfo']['city']['names']['en']
           country = match['geoinfo']['country']['names']['en']
           if resource.include?('host')
@@ -213,53 +223,41 @@ class MetasploitModule < Msf::Auxiliary
             name = match['portinfo']['name']
             version = match['portinfo']['version']
             info = match['portinfo']['extrainfo']
-            report_host(:host     => ip,
-                        :name     => hostname,
-                        :os_name  => os,
-                        :comments => 'Added from Zoomeye'
-                        ) if datastore['DATABASE']
-            report_service(:host => ip,
-                           :port => port,
-                           :proto => name,
-                           :name => "#{service}:#{version}",
-                           :info => info
-                           ) if datastore['DATABASE']
+            if datastore['DATABASE']
+              report_host(host: ip,
+                          name: hostname,
+                          os_name: os,
+                          comments: 'Added from Zoomeye')
+            end
+            if datastore['DATABASE']
+              report_service(host: ip,
+                             port: port,
+                             proto: name,
+                             name: "#{service}:#{version}",
+                             info: info)
+            end
             tbl1 << ["#{ip}:#{port}", city, country, hostname, os, "#{service}:#{version}", info]
           else
             ips = match['ip']
-            ipList = ''
-            ips.each do |ip|
-              ipList << "#{ip}\n"
-            end
             site = match['site']
             database = match['db']
-            dbInfo = ''
-            database.each do |db|
-              dbInfo << "#{db['name']}:"
-              dbInfo << "#{db['version']}\n"
-            end
+            db_info = database.map { |db| "#{db['name']}:#{db['version']}" }
             webapp = match['webapp']
-            waInfo = ''
-            webapp.each do |wa|
-              waInfo << "#{wa['name']}:"
-              waInfo << "#{wa['version']}\n"
+            wa_info = webapp.map { |wa| "#{wa['name']}:#{wa['version']}" }
+            if datastore['DATABASE']
+              report_host(host: ip,
+                          name: site,
+                          comments: 'Added from Zoomeye')
             end
-            report_host(:host     => ip,
-                        :name     => site,
-                        :comments => 'Added from Zoomeye'
-                        ) if datastore['DATABASE']
-            tbl2 << [ipList, site, city, country, dbInfo, waInfo]
+            tbl2 << [ips, site, city, country, db_info, wa_info]
           end
         end
       end
-      if facets
-        return
-      end
       if resource.include?('host')
-        print_line("#{tbl1}")
+        print_line(tbl1.to_s)
         save_output(tbl1) if datastore['OUTFILE']
       else
-        print_line("#{tbl2}")
+        print_line(tbl2.to_s)
         save_output(tbl2) if datastore['OUTFILE']
       end
     end
