c975527006503d1caa10f9cb2549c0bdfcd7739c
William Vu <William_Vu@rapid7.com>
2021-06-11 17:57:11 -0500

    rubocop -a

--- modules/auxiliary/scanner/http/http_header.rb
+++ modules/auxiliary/scanner/http/http_header.rb
@@ -7,26 +7,31 @@ class MetasploitModule < Msf::Auxiliary
   include Msf::Exploit::Remote::HttpClient
   include Msf::Auxiliary::Scanner
 
-  def initialize(info={})
-    super(update_info(info,
-      'Name'        => 'HTTP Header Detection',
-      'Description' => %q{ This module shows HTTP Headers returned by the scanned systems. },
-      'Author'      =>
-      [
-        'Christian Mehlmauer',
-        'rick2600'
-      ],
-      'References'  =>
-      [
-        ['URL', 'http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html'],
-        ['URL', 'http://en.wikipedia.org/wiki/List_of_HTTP_header_fields']
-      ],
-      'License'     => MSF_LICENSE
-    ))
+  def initialize(info = {})
+    super(
+      update_info(
+        info,
+        'Name' => 'HTTP Header Detection',
+        'Description' => %q{ This module shows HTTP Headers returned by the scanned systems. },
+        'Author' =>
+        [
+          'Christian Mehlmauer',
+          'rick2600'
+        ],
+        'References' =>
+        [
+          ['URL', 'http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html'],
+          ['URL', 'http://en.wikipedia.org/wiki/List_of_HTTP_header_fields']
+        ],
+        'License' => MSF_LICENSE
+      )
+    )
 
     register_options([
-      OptString.new('IGN_HEADER', [ true, 'List of headers to ignore, separated by comma',
-        'Vary,Date,Content-Length,Connection,Etag,Expires,Pragma,Accept-Ranges']),
+      OptString.new('IGN_HEADER', [
+        true, 'List of headers to ignore, separated by comma',
+        'Vary,Date,Content-Length,Connection,Etag,Expires,Pragma,Accept-Ranges'
+      ]),
       OptEnum.new('HTTP_METHOD', [ true, 'HTTP Method to use, HEAD or GET', 'HEAD', ['GET', 'HEAD'] ]),
       OptString.new('TARGETURI', [ true, 'The URI to use', '/'])
     ])
@@ -39,8 +44,8 @@ class MetasploitModule < Msf::Auxiliary
     method = datastore['HTTP_METHOD']
     vprint_status("#{peer}: requesting #{uri} via #{method}")
     res = send_request_raw({
-      'method'  => method,
-      'uri'     => uri
+      'method' => method,
+      'uri' => uri
     })
 
     unless res
@@ -55,31 +60,30 @@ class MetasploitModule < Msf::Auxiliary
     end
 
     # Header Names are case insensitve so convert them to upcase
-    headers_uppercase = headers.inject({}) do |hash, keys|
+    headers_uppercase = headers.each_with_object({}) do |keys, hash|
       hash[keys[0].upcase] = keys[1]
-      hash
     end
 
     ignored_headers.each do |h|
-      if headers_uppercase.has_key?(h.upcase)
+      if headers_uppercase.key?(h.upcase)
         vprint_status("#{peer}: deleted header #{h}")
         headers_uppercase.delete(h.upcase)
       end
     end
     headers_uppercase.to_a.compact.sort
 
-    counter = 0;
+    counter = 0
     headers_uppercase.each do |h|
       header_string = "#{h[0]}: #{h[1]}"
       print_good "#{peer}: #{header_string}"
 
       report_note(
-        :type => "http.header.#{rport}.#{counter}",
-        :data => header_string,
-        :host => ip,
-        :port => rport
+        type: "http.header.#{rport}.#{counter}",
+        data: header_string,
+        host: ip,
+        port: rport
       )
-      counter = counter + 1
+      counter += 1
     end
     if counter == 0
       print_warning "#{peer}: all detected headers are defined in IGN_HEADER and were ignored "
