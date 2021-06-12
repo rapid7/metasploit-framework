d58eb309e61cb929e5f2ee23e26b82f68190494d
h00die <michaeltcyr@gmail.com>
2021-06-12 15:07:03 -0400

    update module location

--- modules/auxiliary/scanner/http/emby_version_ssrf.rb
+++ modules/auxiliary/scanner/http/emby_version_ssrf.rb
@@ -50,7 +50,7 @@ class MetasploitModule < Msf::Auxiliary
     print_good("[Media Server] URI: http://#{peer}#{datastore['TARGETURI']}")
     print_good("[Media Server] Version: #{result['Version']}")
     print_good("[Media Server] Internal IP: #{result['LocalAddress']}") if ((result['LocalAddress']).to_s) != ''
-    print_good('*** Vulnerable to SSRF module auxiliary/scanner/emby_ssrf_scanner! ***') if Rex::Version.new((result['Version']).to_s) < Rex::Version.new('4.5.0')
+    print_good('*** Vulnerable to SSRF module auxiliary/scanner/http/emby_ssrf_scanner! ***') if Rex::Version.new((result['Version']).to_s) < Rex::Version.new('4.5.0')
     report_service(
       host: rhost,
       port: rport,
