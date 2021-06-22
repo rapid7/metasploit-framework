fc509644e74d4c4e530c8592bcf29c555fda6755
Stephen Bradshaw <stephen.bradshaw@avast.com>
2021-06-22 11:19:01 +1000

    Fix bug 15218 authentication issue in ssh_login_pubkey

--- modules/auxiliary/scanner/ssh/ssh_login_pubkey.rb
+++ modules/auxiliary/scanner/ssh/ssh_login_pubkey.rb
@@ -175,7 +175,7 @@ class MetasploitModule < Msf::Auxiliary
           create_credential_login(credential_data)
           tmp_key = result.credential.private
           ssh_key = SSHKey.new tmp_key
-          session_setup(result, scanner, ssh_key.fingerprint, credential_core.private_id) if datastore['CreateSession']
+          session_setup(result, scanner, ssh_key.fingerprint, result.credential.private) if datastore['CreateSession']
           if datastore['GatherProof'] && scanner.get_platform(result.proof) == 'unknown'
             msg = "While a session may have opened, it may be bugged.  If you experience issues with it, re-run this module with"
             msg << " 'set gatherproof false'.  Also consider submitting an issue at github.com/rapid7/metasploit-framework with"
