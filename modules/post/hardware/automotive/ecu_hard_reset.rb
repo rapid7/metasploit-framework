a38f27d15743b78b63534723ee6652173a90907c
Jay Turla <shipcodez@gmail.com>
2021-09-24 22:50:44 +0800

    Create ecu_hard_reset.rb

---
+++ modules/post/hardware/automotive/ecu_hard_reset.rb
@@ -0,0 +1,31 @@
++##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+class MetasploitModule < Msf::Post
+
+  def initialize(info={})
+    super( update_info( info,
+      'Name'          => 'ECU Hard Reset',
+      'Description'   => %q{ This module performs hard reset in the ECU Reset Service Identifier (0x11) where the tester can request this to re-initialize the core hardware components of the system.},
+      'License'       => MSF_LICENSE,
+      'Author'        => ['Jay Turla'],
+      'Platform'      => ['hardware'],
+      'SessionTypes'  => ['hwbridge']
+    ))
+    register_options([
+      OptString.new('ARBID', [false, "CAN ID to perform ECU Hard Reset", "0x7DF"]),
+      OptString.new('CANBUS', [false, "CAN Bus to perform scan on, defaults to connected bus", nil])
+    ])
+  end
+
+  def run
+    unless client.automotive
+      print_error("The hwbridge requires a functional automotive extention")
+      return
+    end
+    print_status("Performing ECU Hard Reset...")
+    client.automotive.cansend(datastore['CANBUS'], datastore['ARBID'], "0211010000000000")
+  end
+end
