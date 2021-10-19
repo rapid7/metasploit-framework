d42e119b8f29c484b756d1daa7846f567975eaf8
Matthew Dunn <matthew.eric.dunn@gmail.com>
2021-10-19 12:14:01 -0400

    Minor updates

--- modules/auxiliary/scanner/http/iis_internal_ip.rb
+++ modules/auxiliary/scanner/http/iis_internal_ip.rb
@@ -9,8 +9,6 @@ class MetasploitModule < Msf::Auxiliary
 
   def initialize
     super(
-      update_info(
-        info,
         'Name' => 'Microsoft IIS HTTP Internal IP Disclosure',
         'Description' => %q{
           Collect any leaked internal IPs by requesting commonly redirected locations from IIS.
@@ -28,7 +26,6 @@ class MetasploitModule < Msf::Auxiliary
           ['URL', 'https://support.microsoft.com/en-us/help/967342/fix-the-internal-ip-address-of-an-iis-7-0-server-is-revealed-if-an-htt'], # iis 7+
           ['URL', 'https://techcommunity.microsoft.com/t5/iis-support-blog/iis-web-servers-running-in-windows-azure-may-reveal-their/ba-p/826500']
         ]
-      )
     )
   end
 
@@ -58,12 +55,12 @@ class MetasploitModule < Msf::Auxiliary
           result = res.headers['Location'].scan(intipregex).uniq.flatten
 
           if !result.empty?
-            print_good("Result for #{target_host} found Internal IP: #{result.first}")
+            print_good("Result for #{target_host}#{uri} with method #{method}. Found Internal IP: #{result.first}")
           end
         elsif res.code == 405
           result = res.body.scan(intipregex).uniq.flatten
           if !result.empty?
-            print_good("Result for #{target_host} found Internal IP: #{result.first}")
+            print_good("Result for #{target_host}#{uri} with method #{method}. Found Internal IP: #{result.first}")
           end
         end
 
