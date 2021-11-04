3155f21d775cec2830b578c08873b96d75929b7e
Metasploit <metasploit@rapid7.com>
2021-11-04 12:05:03 -0500

    Bump version of framework to 6.1.14

--- lib/metasploit/framework/version.rb
+++ lib/metasploit/framework/version.rb
@@ -30,7 +30,7 @@ module Metasploit
         end
       end
 
-      VERSION = "6.1.13"
+      VERSION = "6.1.14"
       MAJOR, MINOR, PATCH = VERSION.split('.').map { |x| x.to_i }
       PRERELEASE = 'dev'
       HASH = get_hash
