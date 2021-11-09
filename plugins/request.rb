839722720af585b5b1c3f6d047bc63b8319ccdea
Ashley Donaldson <smashery@gmail.com>
2021-11-09 18:20:41 +1100

    Updated the default metasploit user agent string to be more modern
    Selected from a set of currently-modern browser UA strings

--- plugins/request.rb
+++ plugins/request.rb
@@ -137,7 +137,7 @@ class Plugin::Requests < Msf::Plugin
         :print_body      => true,
         :print_headers   => false,
         :ssl_version     => 'Auto',
-        :user_agent      => Rex::Proto::Http::Client::DefaultUserAgent,
+        :user_agent      => Rex::Proto::Http::ClientRequest.DefaultUserAgent,
         :version         => '1.1'
       }
 
