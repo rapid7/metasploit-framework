99226f1a5c8fd32456e7b1f34454efcc5f747f62
space-r7 <shelby_pace@rapid7.com>
2022-02-15 15:51:22 -0600

    add definitions for winspool and spoolss libs

---
+++ lib/rex/post/meterpreter/extensions/stdapi/railgun/def/windows/def_winspool.rb
@@ -0,0 +1,62 @@
+# -*- coding: binary -*-
+module Rex
+module Post
+module Meterpreter
+module Extensions
+module Stdapi
+module Railgun
+module Def
+
+class Def_windows_winspool
+
+  def self.create_library(constant_manager, library_path = 'winspool.drv')
+    dll = Library.new(library_path, constant_manager)
+
+    dll.add_function('AddPrinterA', 'HANDLE',[
+      ["PCHAR","pName","in"],
+      ["DWORD","Level","in"],
+      ["PBLOB","pPrinter","in"]
+    ])
+
+    dll.add_function('AddPrinterW', 'HANDLE',[
+      ["PWCHAR","pName","in"],
+      ["DWORD","Level","in"],
+      ["PBLOB","pPrinter","in"]
+    ])
+
+    dll.add_function('SetPrinterDataExA', 'DWORD',[
+      ["HANDLE","hPrinter","in"],
+      ["PCHAR","pKeyName","in"],
+      ["PCHAR","pValueName","in"],
+      ["DWORD","Type","in"],
+      ["PBLOB","pData","in"],
+      ["DWORD","cbData","in"]
+    ])
+
+    dll.add_function('SetPrinterDataExW', 'DWORD',[
+      ["HANDLE","hPrinter","in"],
+      ["PWCHAR","pKeyName","in"],
+      ["PWCHAR","pValueName","in"],
+      ["DWORD","Type","in"],
+      ["PBLOB","pData","in"],
+      ["DWORD","cbData","in"]
+    ])
+
+    dll.add_function('OpenPrinterA','BOOL',[
+      ["PCHAR","pPrinterName","in"],
+      ["PDWORD","phPrinter","out"],
+      ["PBLOB","pDefault","in"]
+    ])
+
+    dll.add_function('OpenPrinterW','BOOL',[
+      ["PWCHAR","pPrinterName","in"],
+      ["PDWORD","phPrinter","out"],
+      ["PBLOB","pDefault","in"]
+    ])
+
+    return dll
+  end
+
+end
+
+end; end; end; end; end; end; end
