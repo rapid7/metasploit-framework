cebdebb1881465519d88ce0ee596b8984f2f049e
alanfoster <alan@alanfoster.me>
2022-03-23 02:28:47 +0000

    Fix kubernetes websocket crash when identifying peer host

--- lib/rex/proto/http/web_socket.rb
+++ lib/rex/proto/http/web_socket.rb
@@ -56,7 +56,7 @@ module Rex::Proto::Http::WebSocket
 
         # beware of: https://github.com/rapid7/rex-socket/issues/32
         _, localhost, localport = websocket.getlocalname
-        _, peerhost, peerport = Socket.from_sockaddr(websocket.getpeername)
+        _, peerhost, peerport = Rex::Socket.from_sockaddr(websocket.getpeername)
         @params = Rex::Socket::Parameters.from_hash({
           'LocalHost' => localhost,
           'LocalPort' => localport,
