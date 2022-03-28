e3c3c8e338c3eeb4281239d6085b16fcd0d98561
Brendan Coles <bcoles@gmail.com>
2022-03-28 10:04:34 +0000

    Msf::Post::Windows::FileInfo: Support powershell and shell sessions

--- lib/msf/core/post/windows/file_info.rb
+++ lib/msf/core/post/windows/file_info.rb
@@ -1,10 +1,6 @@
 # -*- coding: binary -*-
-module Msf
-class Post
-module Windows
 
-module FileInfo
-
+module Msf::Post::Windows::FileInfo
   def initialize(info = {})
     super(
       update_info(
@@ -35,44 +31,64 @@ module FileInfo
   # @return [String] Returns the file version information of the file.
 
   def file_version(filepath)
-    file_version_info_size = client.railgun.version.GetFileVersionInfoSizeA(
-      filepath,
-      nil
-    )['return']
+    return unless file_exist?(filepath)
 
-    buffer = session.railgun.kernel32.VirtualAlloc(
-      nil,
-      file_version_info_size,
-      MEM_COMMIT|MEM_RESERVE,
-      PAGE_READWRITE
-    )['return']
+    if session.type == 'meterpreter'
+      file_version_info_size = client.railgun.version.GetFileVersionInfoSizeA(
+        filepath,
+        nil
+      )['return']
 
-    client.railgun.version.GetFileVersionInfoA(
-      filepath,
-      0,
-      file_version_info_size,
-      buffer
-    )
+      buffer = session.railgun.kernel32.VirtualAlloc(
+        nil,
+        file_version_info_size,
+        MEM_COMMIT | MEM_RESERVE,
+        PAGE_READWRITE
+      )['return']
 
-    result = client.railgun.version.VerQueryValueA(buffer, "\\", 4, 4)
-    ffi = client.railgun.memread(result['lplpBuffer'], result['puLen'])
+      client.railgun.version.GetFileVersionInfoA(
+        filepath,
+        0,
+        file_version_info_size,
+        buffer
+      )
 
-    vs_fixed_file_info = ffi.unpack('V13')
+      result = client.railgun.version.VerQueryValueA(buffer, '\\', 4, 4)
+      ffi = client.railgun.memread(result['lplpBuffer'], result['puLen'])
 
-    unless vs_fixed_file_info and (vs_fixed_file_info.length == 13)	and
-(vs_fixed_file_info[0] = 0xfeef04bd)
-      return nil
+      vs_fixed_file_info = ffi.unpack('V13')
+
+      unless vs_fixed_file_info && (vs_fixed_file_info.length == 13) &&
+             (vs_fixed_file_info[0] = 0xfeef04bd)
+        return nil
+      end
+
+      major = hiword(vs_fixed_file_info[2])
+      minor = loword(vs_fixed_file_info[2])
+      build = hiword(vs_fixed_file_info[3])
+      revision = loword(vs_fixed_file_info[3])
+      branch = revision.to_s[0..1].to_i
+    elsif session.type == 'powershell'
+      result = cmd_exec("([System.Diagnostics.FileVersionInfo]::GetVersionInfo(\"#{filepath}\") | Select-Object FileMajorPart,FileMinorPart,FileBuildPart,FilePrivatePart) -join ''")
+      return unless result
+
+      major = result.scan(/FileMajorPart=(\d+)/).flatten.first.to_i
+      minor = result.scan(/FileMinorPart=(\d+)/).flatten.first.to_i
+      build = result.scan(/FileBuildPart=(\d+)/).flatten.first.to_i
+      revision = result.scan(/FilePrivatePart=(\d+)/).flatten.first.to_i
+      branch = revision.to_s[0..1].to_i
+    else
+      result = cmd_exec("wmic datafile where name=\"#{filepath.gsub('\\', '\\\\\\')}\" get Version /value")
+      return unless result
+      return unless result.to_s.include?('Version=')
+
+      major = result.scan(/Version=(\d+).\d+.\d+.\d+/).flatten.first.to_i
+      minor = result.scan(/Version=\d+.(\d+).\d+.\d+/).flatten.first.to_i
+      build = result.scan(/Version=\d+.\d+.(\d+).\d+/).flatten.first.to_i
+      revision = result.scan(/Version=\d+.\d+.\d+.(\d+)/).flatten.first.to_i
+      branch = revision.to_s[0..1].to_i
     end
-
-    major = hiword(vs_fixed_file_info[2])
-    minor = loword(vs_fixed_file_info[2])
-    build = hiword(vs_fixed_file_info[3])
-    revision = loword(vs_fixed_file_info[3])
-    branch = revision.to_s[0..1].to_i
 
     return major, minor, build, revision, branch
   end
-end # FileInfo
-end # Windows
-end # Post
-end # Msf
+end
