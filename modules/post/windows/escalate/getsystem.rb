35e535415a96912a29751a487bc5b11d0086e560
Christophe De La Fuente <christophe_delafuente@rapid7.com>
2022-06-14 15:31:33 +0200

    getsytem module: use ACTION instead of TECHNIQUE datastore option

--- modules/post/windows/escalate/getsystem.rb
+++ modules/post/windows/escalate/getsystem.rb
@@ -4,11 +4,13 @@
 ##
 
 require 'metasm'
+require 'rex/post/meterpreter/ui/console/command_dispatcher/priv'
 
 class MetasploitModule < Msf::Post
   include Msf::Post::Windows::Priv
 
   def initialize(info = {})
+    techniques = Rex::Post::Meterpreter::Ui::Console::CommandDispatcher::Priv::Elevate::ELEVATE_TECHNIQUE_DESCRIPTION
     super(
       update_info(
         info,
@@ -28,13 +30,21 @@ class MetasploitModule < Msf::Post
               priv_elevate_getsystem
             ]
           }
+        },
+        'Actions' => techniques.map.with_index {|t,i| [i.to_s, { 'Description' => t }]},
+        'DefaultAction' => '0',
+        'Notes' => {
+          'AKA' => [
+            'Named Pipe Impersonation',
+            'Token Duplication',
+            'RPCSS',
+            'PrintSpooler',
+            'EFSRPC',
+            'EfsPotato'
+          ]
         }
       )
     )
-
-    register_options([
-      OptInt.new('TECHNIQUE', [false, "Specify a particular technique to use (1-6), otherwise try them all", 0])
-    ])
   end
 
   def unsupported
@@ -43,7 +53,7 @@ class MetasploitModule < Msf::Post
   end
 
   def run
-    technique = datastore['TECHNIQUE'].to_i
+    technique = action.name.to_i
 
     unsupported if client.platform != 'windows' || (client.arch != ARCH_X64 && client.arch != ARCH_X86)
 
