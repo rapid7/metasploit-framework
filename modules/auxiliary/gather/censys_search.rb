066d01b7b2450a3654d37a7612e44f90b6d45312
Christophe De La Fuente <christophe_delafuente@rapid7.com>
2022-07-04 17:19:16 +0200

    Rework censys_search module to use Censys Search API v2

--- modules/auxiliary/gather/censys_search.rb
+++ modules/auxiliary/gather/censys_search.rb
@@ -3,179 +3,141 @@
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
 
-
-
 class MetasploitModule < Msf::Auxiliary
   include Msf::Auxiliary::Report
 
-  def initialize(info={})
-    super(update_info(info,
-      'Name' => 'Censys Search',
-      'Description' => %q{
-        The module use the Censys REST API to access the same data
-        accessible through web interface. The search endpoint allows searches
-        against the current data in the IPv4, Top Million Websites, and
-        Certificates indexes using the same search syntax as the primary site.
-      },
-      'Author' => [ 'Nixawk' ],
-      'References' => [
-        ['URL', 'https://censys.io/api']
-      ],
-      'License' => MSF_LICENSE
-    ))
+  CENSYS_SEARCH_API = 'search.censys.io'.freeze
+
+  def initialize(info = {})
+    super(
+      update_info(
+        info,
+        'Name' => 'Censys Search',
+        'Description' => %q{
+          The module uses the Censys REST API to access the same data accessible
+          through the web interface. The search endpoint allows queries using
+          the Censys Search Language against the Hosts dataset. Setting the
+          CERTIFICATES option will also retrieve the certificate details for each
+          relevant service by querying the Certificates dataset.
+        },
+        'Author' => [
+          'Nixawk', # original Metasploit module
+          'e2002e', # rework to use the API v2
+          'Christophe De La Fuente' # rework to use the API v2
+        ],
+        'References' => [
+          ['URL', 'https://search.censys.io']
+        ],
+        'License' => MSF_LICENSE,
+        'Notes' => {
+          'Stability' => [CRASH_SAFE],
+          'SideEffects' => [],
+          'Reliability' => []
+        }
+      )
+    )
 
     register_options([
       OptString.new('CENSYS_UID', [true, 'The Censys API UID']),
       OptString.new('CENSYS_SECRET', [true, 'The Censys API SECRET']),
-      OptString.new('DORK', [true, 'The Censys Search Dork']),
+      OptString.new('QUERY', [true, 'The Censys search query']),
       OptBool.new('CERTIFICATES', [false, 'Query infos about certificates', false])
     ])
   end
 
-  def basic_auth_header(username, password)
-    auth_str = username.to_s + ":" + password.to_s
-    auth_str = "Basic " + Rex::Text.encode_base64(auth_str)
+  def basic_auth_header
+    auth_str = datastore['CENSYS_UID'].to_s + ':' + datastore['CENSYS_SECRET'].to_s
+    'Basic ' + Rex::Text.encode_base64(auth_str)
   end
 
   def search(keyword)
-    # search_type should be one of ipv4, websites, certificates
-
     begin
-      # "80.http.get.headers.server: Apache"
-      payload = {
-        'query' => keyword
-      }
-      @cli = Rex::Proto::Http::Client.new('search.censys.io', 443, {}, true)
+      @cli = Rex::Proto::Http::Client.new(CENSYS_SEARCH_API, 443, {}, true)
       @cli.connect
 
       response = @cli.request_cgi(
         'method' => 'GET',
-         'uri' => "/api/v2/hosts/search?q=#{keyword}",
-         'headers' => { 'Authorization' => basic_auth_header(@uid, @secret) },
+        'uri' => "/api/v2/hosts/search?q=#{keyword}",
+        'headers' => { 'Authorization' => basic_auth_header }
+      )
+      res = @cli.send_recv(response)
+    rescue ::Rex::ConnectionError, Errno::ECONNREFUSED, Errno::ETIMEDOUT => e
+      fail_with(Failure::Unreachable, "#search: HTTP Connection Failed: #{e}")
+    end
+    fail_with(Failure::Unreachable, '#search: HTTP Connection Failed') unless res
+
+    records = ActiveSupport::JSON.decode(res.body)
+    if records['code'] == 200
+      parse_record(records['result'])
+    else
+      fail_with(Failure::UnexpectedReply, "Error returned by '/api/v2/hosts/search': code=#{records['code']}, status=#{records['status']}, error=#{records['error']}")
+    end
+  end
+
+  def get_certificate_details(cert_fingerprint)
+    return if cert_fingerprint.nil?
+
+    begin
+      response = @cli.request_cgi(
+        'method' => 'GET',
+        'uri' => "/api/v1/view/certificates/#{cert_fingerprint}",
+        'headers' => { 'Authorization' => basic_auth_header }
       )
       res = @cli.send_recv(response)
     rescue ::Rex::ConnectionError, Errno::ECONNREFUSED, Errno::ETIMEDOUT
-      print_error("HTTP Connection Failed")
-    end
-
-    unless res
-      print_error('server_response_error')
+      print_error('#get_certificate_details - HTTP Connection Failed')
       return
     end
+    return unless res
 
-    records = ActiveSupport::JSON.decode(res.body)
-    parse_ipv4(records['result'])
+    cert_details = ActiveSupport::JSON.decode(res.body)
+    subject = cert_details.dig('parsed', 'subject_dn')
+    return unless subject
+
+    issuer = cert_details.dig('parsed', 'issuer_dn')
+    cert_details = subject
+    cert_details << " (Issuer: #{issuer})" if issuer
+    cert_details
   end
 
-  def valid_domain?(domain)
-    domain =~ /^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$/
-  end
-
-  def domain2ip(domain)
-    ips = []
-    begin
-      ips = Rex::Socket.getaddresses(domain)
-    rescue SocketError
-    end
-    ips
-  end
-
-  def parse_certificate(certificate)
-    if certificate.nil?
-      return "NO_CERT_DATA"
-    end
-    ips = []
-    # parsed.fingerprint_sha256
-    # parsed.subject_dn
-    # parsed.issuer_dn
-    subject_dn = certificate['parsed']['subject_dn']
-    return unless subject_dn.include?('CN=')
-
-    host = subject_dn.split('CN=')[1]
-    if Rex::Socket.is_ipv4?(host)
-      ips << host
-    elsif valid_domain?(host) # Fake DNS server
-      ips |= domain2ip(host)
-    end
-
-    result = []
-    ips.each do |ip|
-      result.append("#{ip} - #{subject_dn}")
-    end
-    return result
-  end
-
-  def parse_ipv4(records)
-    if records.nil?
+  def parse_record(records)
+    unless records&.dig('hits')&.any?
+      print_error('The query did not return any records')
       return
     end
-    records['hits'].each do |ipv4|
-      ip = ipv4['ip']
-      services = ipv4['services']
+    records['hits'].each do |hit|
+      ip = hit['ip']
+      services = hit['services']
       ports = []
-      port_count = 0
+      certs = []
       services.each do |service|
         port = service['port']
         name = service['service_name']
-        ports.append("#{port}/#{name}")
-        port_count += 1
-        if @certificates == true
-          certificate = service['certificate']
-          if certificate
-            begin
-              response = @cli.request_cgi(
-                'method' => 'GET',
-                'uri' => "/api/v1/view/certificates/#{certificate}",
-                'headers' => { 'Authorization' => basic_auth_header(@uid, @secret) },
-              )
-              res = @cli.send_recv(response)
-            rescue ::Rex::ConnectionError, Errno::ECONNREFUSED, Errno::ETIMEDOUT
-              print_error("HTTP Connection Failed")
-            end
-            unless res
-              print_error('server_response_error')
-              return
-            end
-            cert = ActiveSupport::JSON.decode(res.body)
-            ports.append(parse_certificate(cert))
+        ports << "#{port}/#{name}"
+        cert_details = nil
+        if datastore['CERTIFICATES'] && service['certificate']
+          cert_details = get_certificate_details(service['certificate'])
+          if cert_details
+            certs << "Certificate for #{port}/#{name}: #{cert_details}"
           else
-            ports.append("NO_CERT_DATA") #Need to input data for organization
+            vprint_error("Unable to get certificate details for #{port}/#{name}")
           end
+        end
+        if cert_details
+          report_service(host: ip, port: port, name: name, info: cert_details)
         else
-          ports.append("NO_CERT_DATA") #Need to input data for organization
-        end
-        report_service(:host => ip, :port => port, :name => name)
-      end
-
-      if ports != nil
-        i = 0
-        print_good("#{ip}")
-        ports.each do |port|
-          if i % 2 == 0 && i / 2 < port_count
-            print "#{port} "
-          end
-          i += 1
-        end
-        print "\n"
-        if @certificate == true
-          ports.each do |port|
-            if i % 2 == 1 && i / 2 < port_count
-              if !port.include? "NO_CERT_DATA"
-                port.each do |cert|
-                  print_good("#{ports[i-1]} - #{cert}")
-                end
-              end
-            end
-            i += 1
-          end
+          report_service(host: ip, port: port, name: name)
         end
       end
+      print_good("#{ip} - #{ports.join(',')}")
+      certs.each { |cert| print_status(cert) }
     end
   end
-  # Check to see if www.censys.io resolves properly
+
+  # Check to see if Censys Search API host resolves properly
   def censys_resolvable?
     begin
-      Rex::Socket.resolv_to_dotted("www.censys.io")
+      Rex::Socket.resolv_to_dotted(CENSYS_SEARCH_API)
     rescue RuntimeError, SocketError
       return false
     end
@@ -183,16 +145,10 @@ class MetasploitModule < Msf::Auxiliary
   end
 
   def run
-    # check to ensure www.censys.io is resolvable
     unless censys_resolvable?
-      print_error("Unable to resolve www.censys.io")
-      return
+      fail_with(Failure::Unreachable, "Unable to resolve #{CENSYS_SEARCH_API}")
     end
 
-    @uid = datastore['CENSYS_UID']
-    @secret = datastore['CENSYS_SECRET']
-    @dork = datastore['DORK']
-    @certificates = datastore['CERTIFICATES']
-    search(@dork)
+    search(datastore['QUERY'])
   end
 end
