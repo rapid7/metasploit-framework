07f5b2e1088b2fd329541fe8ff8c9d4775a00bd5
skylerknecht <skyler.knecht@outlook.com>
2022-09-09 12:03:46 -0400

    Upgraded the tab complete

--- lib/rex/post/meterpreter/ui/console/command_dispatcher/bofloader.rb
+++ lib/rex/post/meterpreter/ui/console/command_dispatcher/bofloader.rb
@@ -28,11 +28,8 @@ class Console::CommandDispatcher::Bofloader
 
   @@bof_cmd_opts = Rex::Parser::Arguments.new(
     ['-h', '--help']          => [ false, "Help Banner" ],
-    ['-a', '--arguments']     => [ true,  "List of command-line arguments to pass to the BOF" ],
-    ['-b', '--bof-file']      => [ true,  "Local path to Beacon Object File" ],
     ['-e', '--entry']         => [ true,  "The entry point (default: #{DEFAULT_ENTRY})." ],
     ['-f', '--format-string'] => [ true,  "bof_pack compatible format-string. Choose combination of: b, i, s, z, Z" ],
-    ['-j', '--json-file']     => [ true,  "Local path to JSON arguments file" ],
   )
 
   # TODO: Properly parse arguments (positional and named switches)
@@ -42,73 +39,65 @@ class Console::CommandDispatcher::Bofloader
   #
   def commands
     {
-      'bof_cmd'                => 'Execute an arbitrary BOF file',
+      'execute_bof'                => 'Execute an arbitrary BOF file',
     }
   end
 
-  def cmd_bof_cmd_help
-    print_line('Usage:   bof_exec </path/to/bof_file.o> [fstring] [bof_arguments ...]')
-    print_line("Example: bof_exec /root/dir.x64.o Zs C:\\ 0")
+  def cmd_execute_bof_help
+    print_line('Usage:   execute_bof </path/to/bof_file.o> [arguments [arguments]] --format-string [format-string]')
+    print_line("Example: execute_bof /root/dir.x64.o C:\\ 0 --format-string Zs")
     print_line(@@bof_cmd_opts.usage)
   end
 
   # Tab complete the first argument as a file on the local filesystem
-  def cmd_bof_cmd_tabs(str, words)
+  def cmd_execute_bof_tabs(str, words)
+    return tab_complete_filenames(str, words) if words.length == 1
     fmt = {
-      '-b'              => [ :file ],
-      '--bof-file'      => [ :file ],
       '-a'              => [ true ],
       '--arguments'     => [ true ],
       '-e'              => [ true ],
       '--entry'         => [ true ],
       '-f'              => [ true ],
       '--format-string' => [ true ],
-      '-j'              => [ :file ],
-      '--json-file'     => [ :file ]
     }
     tab_complete_generic(fmt, str, words)
   end
 
-  def cmd_bof_cmd(*args)
+  def cmd_execute_bof(*args)
     if args.length == 0 || args.include?('-h') || args.include?('--help')
       cmd_bof_cmd_help
       return false
     end
 
-    filename = nil
-    entry = DEFAULT_ENTRY
-    bof_args_format = nil
     bof_args = nil
+    bof_args_format = nil
+    bof_filename = args[0]
+    bof_json_filename = nil
+    entry = DEFAULT_ENTRY
 
     @@bof_cmd_opts.parse(args) { |opt, idx, val|
       case opt
-      when '-a', '--arguments'
-        bof_args = val
-      when '-b', '--bof-file'
-        filename = val
-      when '-e', '--entry'
-        bof_args = val
       when '-f', '--format-string'
         bof_args_format = val
+      when '-e', '--entry'
+        entry = val
+      when '-j', '--json-file'
+        bof_json_filename = val
       end
     }
 
-    unless filename
-      print_error("The -b / --bof-file argument is required")
+    unless ::File.file?(bof_filename) && ::File.readable?(bof_filename)
+      print_error("Unreadable file: #{bof_filename}")
       return
     end
 
-    unless ::File.file?(filename) && ::File.readable?(filename)
-      print_error("Unreadable file: #{filename}")
-      return
+    if bof_args_format
+      bof_args = args[1..bof_args_format.length]
+    else
+      print_status('No argument format specified executing bof with no arguments.')
     end
 
-    if !!bof_args ^ !!bof_args_format
-      print_error('-a / --arguments and -f / --format-string must be used together')
-      return
-    end
-
-    bof_data = ::File.binread(filename)
+    bof_data = ::File.binread(bof_filename)
     parsed = Metasm::COFF.decode_header(bof_data[0...20])
     bof_arch = { # map of metasm to metasploit architectures
       'AMD64' => ARCH_X64,
