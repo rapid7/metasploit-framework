f7c3c505443f26509abee0fa6aac7cdcd5a38f59
Spencer McIntyre <Spencer_McIntyre@rapid7.com>
2022-09-09 15:17:31 -0400

    Apply rubocop changes

--- lib/rex/post/meterpreter/extensions/bofloader/bofloader.rb
+++ lib/rex/post/meterpreter/extensions/bofloader/bofloader.rb
@@ -6,154 +6,156 @@ require 'rexml/document'
 require 'set'
 
 module Rex
-module Post
-module Meterpreter
-module Extensions
-module Bofloader
+  module Post
+    module Meterpreter
+      module Extensions
+        module Bofloader
+          ###
+          #
+          # Bofloader extension - Executes a beacon object file in
+          # the current meterpreter session.
+          #
+          # Kevin Haubris (@kev169)
+          # Kevin Clark (@GuhnooPlusLinux)
+          # TrustedSec (@TrustedSec)
+          #
+          ###
 
-###
-#
-# Bofloader extension - Executes a beacon object file in
-# the current meterpreter session.
-#
-# Kevin Haubris (@kev169)
-# Kevin Clark (@GuhnooPlusLinux)
-# TrustedSec (@TrustedSec)
-#
-###
+          class BofPack
+            # Code referenced from: https://github.com/trustedsec/COFFLoader/blob/main/beacon_generate.py
+            # Emulates the native Cobalt Strike bof_pack() function.
+            # Documented here: https://hstechdocs.helpsystems.com/manuals/cobaltstrike/current/userguide/content/topics_aggressor-scripts/as-resources_functions.htm#bof_pack
+            #
+            # Type      Description                             Unpack With (C)
+            # --------|---------------------------------------|------------------------------
+            # b       | binary data                           | BeaconDataExtract
+            # i       | 4-byte integer                        | BeaconDataInt
+            # s       | 2-byte short integer                  | BeaconDataShort
+            # z       | zero-terminated+encoded string        | BeaconDataExtract
+            # Z       | zero-terminated wide-char string      | (wchar_t *)BeaconDataExtract
 
-class BofPack
-  # Code referenced from: https://github.com/trustedsec/COFFLoader/blob/main/beacon_generate.py
-  # Emulates the native Cobalt Strike bof_pack() function.
-  # Documented here: https://hstechdocs.helpsystems.com/manuals/cobaltstrike/current/userguide/content/topics_aggressor-scripts/as-resources_functions.htm#bof_pack
-  #
-  # Type      Description                             Unpack With (C)
-  # --------|---------------------------------------|------------------------------
-  # b       | binary data                           | BeaconDataExtract
-  # i       | 4-byte integer                        | BeaconDataInt
-  # s       | 2-byte short integer                  | BeaconDataShort
-  # z       | zero-terminated+encoded string        | BeaconDataExtract
-  # Z       | zero-terminated wide-char string      | (wchar_t *)BeaconDataExtract
+            def initialize
+              @buffer = ''
+              @size = 0
+            end
 
-  def initialize()
-    @buffer = ''
-    @size = 0
-  end
+            def addshort(short)
+              @buffer << [short.to_i].pack('<s')
+              @size += 2
+            end
 
-  def addshort(short)
-    @buffer << [short.to_i].pack("<s")
-    @size += 2
-  end
+            def addint(dint)
+              @buffer << [dint.to_i].pack('<I')
+              @size += 4
+            end
 
-  def addint(dint)
-    @buffer << [dint.to_i].pack("<I")
-    @size += 4
-  end
+            def addstr(s)
+              s = s.encode('utf-8').bytes
+              s << 0x00 # Null terminated strings...
+              s_length = s.length
+              s = [s_length] + s
+              buf = s.pack("<Ic#{s_length}")
+              @size += buf.length
+              @buffer << buf
+            end
 
-  def addstr(s)
-    s = s.encode("utf-8").bytes
-    s << 0x00 # Null terminated strings...
-    s_length = s.length
-    s = [s_length] + s
-    buf = s.pack("<Ic#{s_length}")
-    @size += buf.length
-    @buffer << buf
-  end
+            def addWstr(s)
+              s = s.encode('utf-16le').bytes
+              s << 0x00 << 0x00 # Null terminated wide string
+              s_length = s.length
+              s = [s_length] + s
+              buf = s.pack("<Ic#{s_length}")
+              @size += buf.length
+              @buffer << buf
+            end
 
-  def addWstr(s)
-    s = s.encode("utf-16le").bytes
-    s << 0x00 << 0x00 # Null terminated wide string
-    s_length = s.length
-    s = [s_length] + s
-    buf = s.pack("<Ic#{s_length}")
-    @size += buf.length
-    @buffer << buf
-  end
+            def addbinary(b)
+              # Add binary data to the buffer
+              if b.class != 'Array'
+                b = b.bytes
+              end
+              b << 0x00 # Null terminated binary data
+              b_length = b.length
+              b = [b_length] + b
+              buf = b.pack("<Ic#{b_length}")
+              @size += buf.length
+              @buffer << buf
+            end
 
-  def addbinary(b)
-    # Add binary data to the buffer
-    if b.class != "Array"
-      b = b.bytes
-    end
-    b << 0x00 # Null terminated binary data
-    b_length = b.length
-    b = [b_length] + b
-    buf = b.pack("<Ic#{b_length}")
-    @size += buf.length
-    @buffer << buf
-  end
+            def finalize_buffer
+              output = [@size].pack('<I') + @buffer
+              initialize # Reset the class' buffer for another round
+              return output
+            end
 
-  def finalize_buffer()
-    output = [@size].pack("<I") + @buffer
-    initialize() # Reset the class' buffer for another round
-    return output
-  end
+            def bof_pack(fstring, args)
+              # Wrapper function to pack an entire bof command line into a buffer
+              if fstring.nil? or args.nil?
+                return finalize_buffer
+              end
 
-  def bof_pack(fstring, args)
-    # Wrapper function to pack an entire bof command line into a buffer
-    if fstring.nil? or args.nil?
-      return finalize_buffer()
-    end
+              fstring.each_char.each_with_index do |c, i|
+                if c == 'b'
+                  addbinary(args[i])
+                elsif c == 'i'
+                  addint(args[i])
+                elsif c == 's'
+                  addshort(args[i])
+                elsif c == 'z'
+                  addstr(args[i])
+                elsif c == 'Z'
+                  addWstr(args[i])
+                else
+                  raise "Invalid character in format string: #{c}. Must be one of \"b, i, s, z, Z\""
+                end
+              end
 
-    fstring.each_char.each_with_index do |c,i|
-      if c == "b"
-        addbinary(args[i])
-      elsif c == "i"
-        addint(args[i])
-      elsif c == "s"
-        addshort(args[i])
-      elsif c == "z"
-        addstr(args[i])
-      elsif c == "Z"
-        addWstr(args[i])
-      else
-        raise "Invalid character in format string: #{c}. Must be one of \"b, i, s, z, Z\""
+              # return the packed bof_string
+              finalize_buffer
+            end
+          end
+
+          class Bofloader < Extension
+
+            def self.extension_id
+              EXTENSION_ID_BOFLOADER
+            end
+
+            # Typical extension initialization routine.
+            #
+            # @param client (see Extension#initialize)
+            def initialize(client)
+              super(client, 'bofloader')
+
+              client.register_extension_aliases(
+                [
+                  {
+                    'name' => 'bofloader',
+                    'ext' => self
+                  },
+                ]
+              )
+            end
+
+            def execute(bof_data, args_format: nil, args: nil, entry: 'go')
+              request = Packet.create_request(COMMAND_ID_BOFLOADER_EXECUTE)
+
+              # Pack up beacon object file data and arguments into one single binary blob
+              # Hardcode the entrypoint to "go" (CobaltStrike approved)
+              bof = BofPack.new
+              packed_args = bof.bof_pack(args_format, args)
+
+              # Send the meterpreter TLV packet and get the output back
+              request.add_tlv(TLV_TYPE_BOFLOADER_EXECUTE_BUFFER, bof_data)
+              request.add_tlv(TLV_TYPE_BOFLOADER_EXECUTE_BUFFER_ENTRY, entry)
+              request.add_tlv(TLV_TYPE_BOFLOADER_EXECUTE_ARGUMENTS, packed_args)
+              response = client.send_request(request)
+              return response.get_tlv_value(TLV_TYPE_BOFLOADER_EXECUTE_RESULT)
+            end
+
+          end
+        end
       end
     end
-
-    # return the packed bof_string
-    finalize_buffer
   end
 end
-
-class Bofloader < Extension
-
-  def self.extension_id
-    EXTENSION_ID_BOFLOADER
-  end
-
-  # Typical extension initialization routine.
-  #
-  # @param client (see Extension#initialize)
-  def initialize(client)
-    super(client, 'bofloader')
-
-    client.register_extension_aliases(
-      [
-        {
-          'name' => 'bofloader',
-          'ext'  => self
-        },
-      ])
-
-  end
-
-  def execute(bof_data, args_format: nil, args: nil, entry: 'go')
-    request = Packet.create_request(COMMAND_ID_BOFLOADER_EXECUTE)
-
-    # Pack up beacon object file data and arguments into one single binary blob
-    # Hardcode the entrypoint to "go" (CobaltStrike approved)
-    bof = BofPack.new
-    packed_args = bof.bof_pack(args_format, args)
-
-    # Send the meterpreter TLV packet and get the output back
-    request.add_tlv(TLV_TYPE_BOFLOADER_EXECUTE_BUFFER, bof_data)
-    request.add_tlv(TLV_TYPE_BOFLOADER_EXECUTE_BUFFER_ENTRY, entry)
-    request.add_tlv(TLV_TYPE_BOFLOADER_EXECUTE_ARGUMENTS, packed_args)
-    response = client.send_request(request)
-    return response.get_tlv_value(TLV_TYPE_BOFLOADER_EXECUTE_RESULT)
-  end
-
-end
-
-end; end; end; end; end
--- lib/rex/post/meterpreter/ui/console/command_dispatcher/bofloader.rb
+++ lib/rex/post/meterpreter/ui/console/command_dispatcher/bofloader.rb
@@ -1,173 +1,172 @@
 # -*- coding: binary -*-
+
 require 'rex/post/meterpreter'
 
 module Rex
-module Post
-module Meterpreter
-module Ui
+  module Post
+    module Meterpreter
+      module Ui
+        ###
+        #
+        # Bofloader extension - load and execute bof files
+        #
+        ###
+        class Console::CommandDispatcher::Bofloader
 
-###
-#
-# Bofloader extension - load and execute bof files
-#
-###
-class Console::CommandDispatcher::Bofloader
+          Klass = Console::CommandDispatcher::Bofloader
 
-  Klass = Console::CommandDispatcher::Bofloader
+          include Console::CommandDispatcher
 
-  include Console::CommandDispatcher
+          #
+          # Name for this dispatcher
+          #
+          def name
+            'Beacon Object File Loader'
+          end
 
-  #
-  # Name for this dispatcher
-  #
-  def name
-    'Beacon Object File Loader'
-  end
+          DEFAULT_ENTRY = 'go'
 
-  DEFAULT_ENTRY = 'go'
+          @@execute_bof_opts = Rex::Parser::Arguments.new(
+            ['-h', '--help'] => [ false, 'Help Banner' ],
+            ['-e', '--entry'] => [ true, "The entry point (default: #{DEFAULT_ENTRY})." ],
+            ['-f', '--format-string'] => [ true, 'bof_pack compatible format-string. Choose combination of: b, i, s, z, Z' ]
+          )
 
-  @@execute_bof_opts = Rex::Parser::Arguments.new(
-    ['-h', '--help']          => [ false, "Help Banner" ],
-    ['-e', '--entry']         => [ true,  "The entry point (default: #{DEFAULT_ENTRY})." ],
-    ['-f', '--format-string'] => [ true,  "bof_pack compatible format-string. Choose combination of: b, i, s, z, Z" ],
-  )
+          # TODO: Properly parse arguments (positional and named switches)
 
-  # TODO: Properly parse arguments (positional and named switches)
+          #
+          # List of supported commands.
+          #
+          def commands
+            {
+              'execute_bof' => 'Execute an arbitrary BOF file'
+            }
+          end
 
-  #
-  # List of supported commands.
-  #
-  def commands
-    {
-      'execute_bof'                => 'Execute an arbitrary BOF file',
-    }
-  end
+          def cmd_execute_bof_help
+            print_line('Usage:   execute_bof </path/to/bof_file.o> [arguments [arguments]] --format-string [format-string]')
+            print_line('Example: execute_bof /root/dir.x64.o C:\\ 0 --format-string Zs')
+            print_line(@@bof_cmd_opts.usage)
+          end
 
-  def cmd_execute_bof_help
-    print_line('Usage:   execute_bof </path/to/bof_file.o> [arguments [arguments]] --format-string [format-string]')
-    print_line("Example: execute_bof /root/dir.x64.o C:\\ 0 --format-string Zs")
-    print_line(@@bof_cmd_opts.usage)
-  end
+          # Tab complete the first argument as a file on the local filesystem
+          def cmd_execute_bof_tabs(str, words)
+            return tab_complete_filenames(str, words) if words.length == 1
 
-  # Tab complete the first argument as a file on the local filesystem
-  def cmd_execute_bof_tabs(str, words)
-    return tab_complete_filenames(str, words) if words.length == 1
-    fmt = {
-      '-e'              => [ true ],
-      '--entry'         => [ true ],
-      '-f'              => [ true ],
-      '--format-string' => [ true ],
-    }
-    tab_complete_generic(fmt, str, words)
-  end
+            fmt = {
+              '-e' => [ true ],
+              '--entry' => [ true ],
+              '-f' => [ true ],
+              '--format-string' => [ true ]
+            }
+            tab_complete_generic(fmt, str, words)
+          end
 
-  def cmd_execute_bof(*args)
-    if args.length == 0 || args.include?('-h') || args.include?('--help')
-      cmd_bof_cmd_help
-      return false
-    end
+          def cmd_execute_bof(*args)
+            if args.length == 0 || args.include?('-h') || args.include?('--help')
+              cmd_bof_cmd_help
+              return false
+            end
 
-    bof_args = nil
-    bof_args_format = nil
-    bof_cmdline = []
-    entry = DEFAULT_ENTRY
+            bof_args = nil
+            bof_args_format = nil
+            bof_cmdline = []
+            entry = DEFAULT_ENTRY
 
-    @@execute_bof_opts.parse(args) { |opt, idx, val|
-      case opt
-      when '-f', '--format-string'
-        bof_args_format = val
-      when '-e', '--entry'
-        entry = val
-      when nil
-        bof_cmdline << val
+            @@execute_bof_opts.parse(args) do |opt, _idx, val|
+              case opt
+              when '-f', '--format-string'
+                bof_args_format = val
+              when '-e', '--entry'
+                entry = val
+              when nil
+                bof_cmdline << val
+              end
+            end
+
+            bof_filename = bof_cmdline[0]
+
+            unless ::File.file?(bof_filename) && ::File.readable?(bof_filename)
+              print_error("Unreadable file: #{bof_filename}")
+              return
+            end
+
+            if bof_args_format
+              if bof_args_format.length != bof_cmdline.length - 1
+                print_error("Format string length must be the same as argument length: fstring:#{bof_args_format.length}, args:#{bof_cmdline.length - 1}")
+                return
+              end
+              bof_args = bof_cmdline[1..]
+            elsif bof_cmdline.length > 1
+              print_error('Arguments detected and no format string specified.')
+              return
+            else
+              print_status('No argument format specified executing bof with no arguments.')
+            end
+
+            bof_data = ::File.binread(bof_filename)
+
+            # loading all data will hang on invalid files like DLLs, so only parse the 20-byte header at first
+            parsed = Metasm::COFF.decode_header(bof_data[0...20])
+            bof_arch = { # map of metasm to metasploit architectures
+              'AMD64' => ARCH_X64,
+              'I386' => ARCH_X86
+            }.fetch(parsed.header.machine, nil)
+
+            unless bof_arch
+              print_error('Unable to determine the file architecture.')
+              return
+            end
+            unless bof_arch == client.arch
+              print_error("The file architecture is incompatible with the current session (file: #{bof_arch} session: #{client.arch})")
+              return
+            end
+
+            parsed = Metasm::COFF.decode(bof_data)
+            unless (executable_symbols = get_executable_symbols(parsed)).include?(entry)
+              print_error("The specified entry point was not found: #{entry}")
+              print_error("Available symbols: #{executable_symbols.join(', ')}")
+              return
+            end
+
+            output = client.bofloader.execute(bof_data, args_format: bof_args_format, args: bof_args, entry: entry)
+            if output.nil?
+              print_status('No output returned from bof')
+            else
+              print_line(output)
+            end
+          end
+
+          private
+
+          def get_executable_symbols(coff)
+            executable_symbols = []
+            coff.symbols.each do |sym|
+              next unless sym
+              next unless sym.sec_nr.is_a? Integer
+
+              section = coff.sections[sym.sec_nr - 1]
+              next unless section
+
+              next if section.name == sym.name
+              next unless section.characteristics.include?('MEM_EXECUTE')
+              next unless section.characteristics.include?('CONTAINS_CODE')
+
+              # see: https://github.com/trustedsec/COFFLoader/blob/24da168356bd20438a4e66ef3261c5012344d362/COFFLoader.c#L182-L189
+              if client.arch == ARCH_X64
+                executable_symbols << sym.name
+              else
+                next unless sym.name.start_with?('_')
+
+                executable_symbols << sym.name[1..]
+              end
+            end
+
+            executable_symbols
+          end
+
+        end
       end
-    }
-
-    bof_filename = bof_cmdline[0]
-
-    unless ::File.file?(bof_filename) && ::File.readable?(bof_filename)
-      print_error("Unreadable file: #{bof_filename}")
-      return
     end
-
-    if bof_args_format
-      if bof_args_format.length != bof_cmdline.length - 1
-        print_error("Format string length must be the same as argument length: fstring:#{bof_args_format.length}, args:#{bof_cmdline.length - 1}")
-        return
-      end
-      bof_args = bof_cmdline[1..]
-    elsif bof_cmdline.length > 1
-      print_error('Arguments detected and no format string specified.')
-      return
-    else
-      print_status('No argument format specified executing bof with no arguments.')
-    end
-
-    bof_data = ::File.binread(bof_filename)
-
-    # loading all data will hang on invalid files like DLLs, so only parse the 20-byte header at first
-    parsed = Metasm::COFF.decode_header(bof_data[0...20])
-    bof_arch = { # map of metasm to metasploit architectures
-      'AMD64' => ARCH_X64,
-      'I386'  => ARCH_X86
-    }.fetch(parsed.header.machine, nil)
-
-    unless bof_arch
-      print_error('Unable to determine the file architecture.')
-      return
-    end
-    unless bof_arch == client.arch
-      print_error("The file architecture is incompatible with the current session (file: #{bof_arch} session: #{client.arch})")
-      return
-    end
-
-    parsed = Metasm::COFF.decode(bof_data)
-    unless (executable_symbols = get_executable_symbols(parsed)).include?(entry)
-      print_error("The specified entry point was not found: #{entry}")
-      print_error("Available symbols: #{executable_symbols.join(', ')}")
-      return
-    end
-
-    output = client.bofloader.execute(bof_data, args_format: bof_args_format, args: bof_args, entry: entry)
-    if output.nil?
-      print_status("No output returned from bof")
-    else
-      print_line(output)
-    end
-
   end
-
-  private
-
-  def get_executable_symbols(coff)
-    executable_symbols = []
-    coff.symbols.each do |sym|
-      next unless sym
-      next unless sym.sec_nr.is_a? Integer
-
-      section = coff.sections[sym.sec_nr - 1]
-      next unless section
-
-      next if section.name == sym.name
-      next unless section.characteristics.include?('MEM_EXECUTE')
-      next unless section.characteristics.include?('CONTAINS_CODE')
-
-      # see: https://github.com/trustedsec/COFFLoader/blob/24da168356bd20438a4e66ef3261c5012344d362/COFFLoader.c#L182-L189
-      unless client.arch == ARCH_X64
-        next unless sym.name.start_with?('_')
-
-        executable_symbols << sym.name[1..]
-      else
-        executable_symbols << sym.name
-      end
-    end
-
-    executable_symbols
-  end
-
-end
-
-end
-end
-end
 end
