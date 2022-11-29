236b28f945fe737df2bca9fdf97aaa05ae2dd90e
dru1d-foofus <tyler.booth@cdw.com>
2022-11-29 01:41:32 -0500

    I had a lot to fix after linting the module.

--- modules/post/windows/manage/ducky.rb
+++ modules/post/windows/manage/ducky.rb
@@ -11,12 +11,20 @@ class MetasploitModule < Msf::Post
         info,
         'Name' => 'Windows Ducky Script Parser',
         'Description' => %q{
-          This module can be used to execute supplied Ducky Scripts.
+          This *incomplete* module can be used to execute supplied Ducky Scripts.
         },
         'License' => MSF_LICENSE,
         'Author' => [
-          '@dru1d-foofus <tyler.booth[at]cdw.com>',
+          'dru1d <tyler.booth[at]cdw.com>',
         ],
+        'References' => [
+          'URL', 'https://docs.hak5.org/hak5-usb-rubber-ducky/duckyscript-tm-quick-reference',
+        ],
+        'Notes' => {
+          'Stability' => [CRASH_SAFE],
+          'Reliability' => [],
+          'SideEffects' => [CONFIG_CHANGES, ARTIFACTS_ON_DISK]
+        },
         'Platform' => [ 'win' ],
         'SessionTypes' => [ 'meterpreter', ],
         'Compat' => {
@@ -31,43 +39,43 @@ class MetasploitModule < Msf::Post
     )
     register_options(
       [
-        OptString.new('FILENAME', [true, "The Ducky Script you want to parse"]),
-        ], self.class)
+        OptString.new('FILENAME', [true, 'The Ducky Script you want to parse']),
+        OptInt.new('SLEEP', [false, 'Sleep time between commands.', 3])
+      ]
+    )
   end
 
-  def duckyParse(line)
+  def ducky_parse(line)
     # Define common key codes for initial testing; this can be expanded later
-    key_codes = {"8" => "backspace", "9" => "tab", "13" => "enter", "91" => "windows" }
-    actions = {"0" => "press", "1" => "down", "2" => "up"}
-    lineArray = line.split(' ', 2)
-    if lineArray[0] == "STRING"
-      session.ui.keyboard_send(lineArray[1])
+    key_codes = { '8' => 'backspace', '9' => 'tab', '13' => 'enter', '91' => 'windows' }
+    actions = { '0' => 'press', '1' => 'down', '2' => 'up' }
+    line_array = line.split(' ', 2)
+    if line_array[0] == 'STRING'
+      session.ui.keyboard_send(line_array[1])
     end
-    if lineArray[0] == "STRINGLN"
-      session.ui.keyboard_send(lineArray[1])
-      session.ui.keyevent_send(key_codes.key("enter").to_i, actions.key("press").to_i)
+    if line_array[0] == 'STRINGLN'
+      session.ui.keyboard_send(line_array[1])
+      session.ui.keyevent_send(key_codes.key('enter').to_i, actions.key('press').to_i)
     end
-    if lineArray[0] == "GUI"
-      session.ui.keyevent_send(key_codes.key("windows").to_i, actions.key("down").to_i)
-      session.ui.keyevent_send(82, actions.key("press").to_i)
-      session.ui.keyevent_send(key_codes.key("windows").to_i, actions.key("up").to_i)
+    if line_array[0] == 'GUI'
+      session.ui.keyevent_send(key_codes.key('windows').to_i, actions.key('down').to_i)
+      session.ui.keyevent_send(82, actions.key('press').to_i)
+      session.ui.keyevent_send(key_codes.key('windows').to_i, actions.key('up').to_i)
     end
   end
-
 
   def run
-    return 0 if session.type != "meterpreter"
+    return 0 if session.type != 'meterpreter'
 
     if datastore['FILENAME'].blank?
-      print_error("A file needs to be provided!")
+      print_error('A file needs to be provided!')
       return 0
     end
-    print_good("Readining file #{datastore['FILENAME']}")
+    print_good("Reading file #{datastore['FILENAME']}")
     File.readlines(datastore['FILENAME']).each do |line|
       print("Line: #{line}")
-      sleep(3)
-      duckyParse(line)
+      sleep(datastore['SLEEP'])
+      ducky_parse(line)
     end
   end
-
 end
