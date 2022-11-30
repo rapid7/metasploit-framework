3a93fcb4044206984f7cf3512d1c2497107862d5
Ashley Donaldson <smashery@gmail.com>
2022-11-30 11:32:23 +1100

    Rubocop'd

--- modules/auxiliary/scanner/winrm/winrm_wql.rb
+++ modules/auxiliary/scanner/winrm/winrm_wql.rb
@@ -3,41 +3,39 @@
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
 
-
 class MetasploitModule < Msf::Auxiliary
   include Msf::Exploit::Remote::WinRM
   include Msf::Auxiliary::Report
-
 
   include Msf::Auxiliary::Scanner
 
   def initialize
     super(
-      'Name'           => 'WinRM WQL Query Runner',
-      'Description'    => %q{
+      'Name' => 'WinRM WQL Query Runner',
+      'Description' => %q{
         This module runs WQL queries against remote WinRM Services.
         Authentication is required. Currently only works with NTLM auth.
         Please note in order to use this module, the 'AllowUnencrypted'
         winrm option must be set.
       },
-      'Author'         => [ 'thelightcosine' ],
-      'License'        => MSF_LICENSE
+      'Author' => [ 'thelightcosine' ],
+      'License' => MSF_LICENSE
     )
 
     register_options(
       [
-        OptString.new('WQL', [ true, "The WQL query to run", "Select Name,Status from Win32_Service" ]),
+        OptString.new('WQL', [ true, 'The WQL query to run', 'Select Name,Status from Win32_Service' ]),
         OptString.new('NAMESPACE', [true, 'The WMI namespace to use for queries', 'root/cimv2'])
-      ])
+      ]
+    )
   end
-
 
   def run_host(ip)
     connection = create_winrm_connection
     wql_result = connection.run_wql(datastore['WQL'], "#{wmi_namespace}/*")
     result = parse_wql_hash(wql_result)
     print_good result.to_s
-    path = store_loot("winrm.wql_results", "text/csv", ip, result.to_csv, "winrm_wql_results.csv", "WinRM WQL Query Results")
+    path = store_loot('winrm.wql_results', 'text/csv', ip, result.to_csv, 'winrm_wql_results.csv', 'WinRM WQL Query Results')
     print_good "Results saved to #{path}"
   end
 end
