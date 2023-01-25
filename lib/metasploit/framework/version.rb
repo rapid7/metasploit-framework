ea96629925fc4e7f3be507e842f25f3b51c7801c
adfoster-r7 <alandavid_foster@rapid7.com>
2023-01-25 18:36:48 +0000

    Bump Metasploit version to 6.3.0

--- lib/metasploit/framework/version.rb
+++ lib/metasploit/framework/version.rb
@@ -30,7 +30,7 @@ module Metasploit
         end
       end
 
-      VERSION = "6.2.37"
+      VERSION = "6.3.0"
       MAJOR, MINOR, PATCH = VERSION.split('.').map { |x| x.to_i }
       PRERELEASE = 'dev'
       HASH = get_hash
