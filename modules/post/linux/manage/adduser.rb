6e6910519aa13ca152c85ffc844ef155018b4eff
RadioLogic <ncottrellweb@gmail.com>
2023-07-17 13:59:30 -0400

    Added error if custom command doesnt exist

--- modules/post/linux/manage/adduser.rb
+++ modules/post/linux/manage/adduser.rb
@@ -51,6 +51,7 @@ class MetasploitModule < Msf::Post
   def run
     fail_with(Failure::NoAccess, 'Session isnt running as root') unless is_root?
     fail_with(Failure::NotVulnerable, 'Cannot find a means to add a new user') unless datastore['UseraddBinary'] == 'MANUAL' || (datastore['UseraddBinary'] && command_exists?(datastore['UseraddBinary'])) || command_exists?('useradd') || command_exists?('adduser')
+    fail_with(Failure::NotFound, "Cannot find command on path given: #{datastore['UseraddBinary']}") if datastore['UseraddBinary'] && !(datastore['UseraddBinary'] == 'MANUAL' || command_exists?(datastore['UseraddBinary']))
     fail_with(Failure::NotVulnerable, 'Cannot add user to sudo as sudoers doesnt exist') unless datastore['SudoMethod'] != 'SUDO_FILE' || file_exist?('/etc/sudoers')
     fail_with(Failure::NotFound, 'Shell specified does not exist on system') unless command_exists?(datastore['SHELL'])
     fail_with(Failure::BadConfig, "Username [#{datastore['USERNAME']}] is not a legal unix username.") unless datastore['USERNAME'] =~ /^[a-z][a-z0-9_-]{0,31}$/
