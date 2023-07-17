c63810101c171a9a5434ed0a67b4700ba3600235
RadioLogic <ncottrellweb@gmail.com>
2023-07-17 12:47:34 -0400

    Added error for illegal usernames

--- modules/post/linux/manage/adduser.rb
+++ modules/post/linux/manage/adduser.rb
@@ -53,6 +53,7 @@ class MetasploitModule < Msf::Post
     fail_with(Failure::NotVulnerable, 'Cannot find a means to add a new user') unless datastore['UseraddBinary'] == 'MANUAL' || (datastore['UseraddBinary'] && command_exists?(datastore['UseraddBinary'])) || command_exists?('useradd') || command_exists?('adduser')
     fail_with(Failure::NotVulnerable, 'Cannot add user to sudo as sudoers doesnt exist') unless datastore['SudoMethod'] != 'SUDO_FILE' || file_exist?('/etc/sudoers')
     fail_with(Failure::NotFound, 'Shell specified does not exist on system') unless command_exists?(datastore['SHELL'])
+    fail_with(Failure::BadConfig, "Username [#{datastore['USERNAME']}] is not a legal unix username.") unless datastore['USERNAME'] =~ /^[a-z][a-z0-9_-]{0,31}$/
 
     # Encrypting password ahead of time
     passwd = UnixCrypt::MD5.build(datastore['PASSWORD'])
