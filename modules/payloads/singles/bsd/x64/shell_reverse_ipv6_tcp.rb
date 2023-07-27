f3adc3f79fbfa9fbeab13664f652fa88963ebfa1
adfoster-r7 <alandavid_foster@rapid7.com>
2023-07-27 16:02:37 +0100

    Fix invalid references in modules

--- modules/payloads/singles/bsd/x64/shell_reverse_ipv6_tcp.rb
+++ modules/payloads/singles/bsd/x64/shell_reverse_ipv6_tcp.rb
@@ -17,7 +17,7 @@ module MetasploitModule
       'Name'          => 'BSD x64 Command Shell, Reverse TCP Inline (IPv6)',
       'Description'   => 'Connect back to attacker and spawn a command shell over IPv6',
       'Author'        => 'Balazs Bucsay @xoreipeip <balazs.bucsay[-at-]rycon[-dot-]hu>',
-      'References'    => ['URL', 'https://github.com/earthquake/shellcodes/blob/master/x86_64_bsd_ipv6_reverse_tcp.asm.c'],
+      'References'    => [['URL', 'https://github.com/earthquake/shellcodes/blob/master/x86_64_bsd_ipv6_reverse_tcp.asm.c']],
       'License'       => MSF_LICENSE,
       'Platform'      => 'bsd',
       'Arch'          => ARCH_X64,
