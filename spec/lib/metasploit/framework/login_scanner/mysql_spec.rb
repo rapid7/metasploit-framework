e6d1a20a05a75c23da820ddc61e421b50bb20bbe
Rory McKinley <rorymckinley@gmail.com>
2023-08-11 08:01:11 +0200

    Use `ruby-mysql` for MySQL login scanner

--- spec/lib/metasploit/framework/login_scanner/mysql_spec.rb
+++ spec/lib/metasploit/framework/login_scanner/mysql_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe Metasploit::Framework::LoginScanner::MySQL do
 
     context 'when the attempt is successful' do
       it 'returns a result object with a status of Metasploit::Model::Login::Status::SUCCESSFUL' do
-        expect(::RbMysql).to receive(:connect).and_return "fake mysql handle"
+        expect(::Mysql).to receive(:connect).and_return "fake mysql handle"
         expect(login_scanner.attempt_login(pub_pri).status).to eq Metasploit::Model::Login::Status::SUCCESSFUL
       end
     end
@@ -45,61 +45,61 @@ RSpec.describe Metasploit::Framework::LoginScanner::MySQL do
     context 'when the attempt is unsuccessful' do
       context 'due to connection refused' do
         it 'returns a result with a status of Metasploit::Model::Login::Status::UNABLE_TO_CONNECT' do
-          expect(::RbMysql).to receive(:connect).and_raise Errno::ECONNREFUSED
+          expect(::Mysql).to receive(:connect).and_raise Errno::ECONNREFUSED
           expect(login_scanner.attempt_login(pub_pub).status).to eq Metasploit::Model::Login::Status::UNABLE_TO_CONNECT
         end
 
         it 'returns a result with the proof containing an appropriate error message' do
-          expect(::RbMysql).to receive(:connect).and_raise Errno::ECONNREFUSED
+          expect(::Mysql).to receive(:connect).and_raise Errno::ECONNREFUSED
           expect(login_scanner.attempt_login(pub_pub).proof).to be_a(Errno::ECONNREFUSED)
         end
       end
 
       context 'due to connection timeout' do
         it 'returns a result with a status of Metasploit::Model::Login::Status::UNABLE_TO_CONNECT' do
-          expect(::RbMysql).to receive(:connect).and_raise RbMysql::ClientError, "Client Error"
+          expect(::Mysql).to receive(:connect).and_raise Mysql::ClientError, "Client Error"
           expect(login_scanner.attempt_login(pub_pub).status).to eq Metasploit::Model::Login::Status::UNABLE_TO_CONNECT
         end
 
         it 'returns a result with the proof containing an appropriate error message' do
-          expect(::RbMysql).to receive(:connect).and_raise RbMysql::ClientError, "Client Error"
-          expect(login_scanner.attempt_login(pub_pub).proof).to be_a(RbMysql::ClientError)
+          expect(::Mysql).to receive(:connect).and_raise Mysql::ClientError, "Client Error"
+          expect(login_scanner.attempt_login(pub_pub).proof).to be_a(Mysql::ClientError)
         end
       end
 
       context 'due to operation timeout' do
         it 'returns a result with a status of Metasploit::Model::Login::Status::UNABLE_TO_CONNECT' do
-          expect(::RbMysql).to receive(:connect).and_raise Errno::ETIMEDOUT
+          expect(::Mysql).to receive(:connect).and_raise Errno::ETIMEDOUT
           expect(login_scanner.attempt_login(pub_pub).status).to eq Metasploit::Model::Login::Status::UNABLE_TO_CONNECT
         end
 
         it 'returns a result with the proof containing an appropriate error message' do
-          expect(::RbMysql).to receive(:connect).and_raise Errno::ETIMEDOUT
+          expect(::Mysql).to receive(:connect).and_raise Errno::ETIMEDOUT
           expect(login_scanner.attempt_login(pub_pub).proof).to be_a(Errno::ETIMEDOUT)
         end
       end
 
       context 'due to not being allowed to connect from this host' do
         it 'returns a result with a status of Metasploit::Model::Login::Status::UNABLE_TO_CONNECT' do
-          expect(::RbMysql).to receive(:connect).and_raise RbMysql::HostNotPrivileged, "Host not privileged"
+          expect(::Mysql).to receive(:connect).and_raise Mysql::HostNotPrivileged, "Host not privileged"
           expect(login_scanner.attempt_login(pub_pub).status).to eq Metasploit::Model::Login::Status::UNABLE_TO_CONNECT
         end
 
         it 'returns a result with the proof containing an appropriate error message' do
-          expect(::RbMysql).to receive(:connect).and_raise RbMysql::HostNotPrivileged, "Host not privileged"
-          expect(login_scanner.attempt_login(pub_pub).proof).to be_a(RbMysql::HostNotPrivileged)
+          expect(::Mysql).to receive(:connect).and_raise Mysql::HostNotPrivileged, "Host not privileged"
+          expect(login_scanner.attempt_login(pub_pub).proof).to be_a(Mysql::HostNotPrivileged)
         end
       end
 
       context 'due to access denied' do
         it 'returns a result with a status of Metasploit::Model::Login::Status::INCORRECT' do
-          expect(::RbMysql).to receive(:connect).and_raise RbMysql::AccessDeniedError, "Access Denied"
+          expect(::Mysql).to receive(:connect).and_raise Mysql::AccessDeniedError, "Access Denied"
           expect(login_scanner.attempt_login(pub_pub).status).to eq Metasploit::Model::Login::Status::INCORRECT
         end
 
         it 'returns a result with the proof containing an appropriate error message' do
-          expect(::RbMysql).to receive(:connect).and_raise RbMysql::AccessDeniedError, "Access Denied"
-          expect(login_scanner.attempt_login(pub_pub).proof).to be_a(RbMysql::AccessDeniedError)
+          expect(::Mysql).to receive(:connect).and_raise Mysql::AccessDeniedError, "Access Denied"
+          expect(login_scanner.attempt_login(pub_pub).proof).to be_a(Mysql::AccessDeniedError)
         end
       end
     end
