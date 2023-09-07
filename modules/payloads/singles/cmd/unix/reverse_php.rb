1f53d34f70bb4644fdb007633a599e76a0c22385
jvoisin <julien.voisin@dustri.org>
2023-09-07 15:39:07 +0200

    Run rubocop on the module

--- modules/payloads/singles/cmd/unix/reverse_php.rb
+++ modules/payloads/singles/cmd/unix/reverse_php.rb
@@ -3,32 +3,32 @@
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
 
-
 module MetasploitModule
-
   CachedSize = 238
 
   include Msf::Payload::Single
   include Msf::Sessions::CommandShellOptions
 
   def initialize(info = {})
-    super(merge_info(info,
-     'Name'          => 'Unix Command Shell, Reverse TCP (via php)',
-     'Description'   => 'Creates an interactive shell via php',
-     'Author'        => 'Julien (jvoisin) Voisin',
-     'License'       => BSD_LICENSE,
-     'Platform'      => 'unix',
-     'Arch'          => ARCH_CMD,
-     'Handler'       => Msf::Handler::ReverseTcp,
-     'Session'       => Msf::Sessions::CommandShell,
-     'PayloadType'   => 'cmd',
-     'RequiredCmd'   => 'php',
-     'Payload'       =>
-       {
-         'Offsets' => { },
-         'Payload' => ''
-       }
-    ))
+    super(
+      merge_info(
+        info,
+        'Name' => 'Unix Command Shell, Reverse TCP (via php)',
+        'Description' => 'Creates an interactive shell via php',
+        'Author' => 'Julien (jvoisin) Voisin',
+        'License' => BSD_LICENSE,
+        'Platform' => 'unix',
+        'Arch' => ARCH_CMD,
+        'Handler' => Msf::Handler::ReverseTcp,
+        'Session' => Msf::Sessions::CommandShell,
+        'PayloadType' => 'cmd',
+        'RequiredCmd' => 'php',
+        'Payload' => {
+          'Offsets' => {},
+          'Payload' => ''
+        }
+      )
+    )
     register_advanced_options(
       [
         OptString.new('PHPPath', [true, 'The path to the PHP executable', 'php'])
