91ce4c3d9d30d0ff939149c9fbe501a24814a3c5
joev <105156480+j0ev@users.noreply.github.com>
2023-09-20 14:29:47 -0600

    Add new spec for Msf::Simple::Payload.

---
+++ spec/lib/msf/simple/payload_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+RSpec.describe Msf::Simple::Payload do
+  include_context 'Msf::Simple::Framework#modules loading'
+
+  let(:generator_format) { 'raw' }
+  let(:generator_opts) {
+    {
+        'BadChars' => '',
+        'Encoder' => '',
+        'Options' => { 'LHOST' => '1.1.1.1', 'LPORT' => '8443' } ,
+        'Format' => generator_format,
+        'NoComment' => false,
+        'NopSledSize' => 0,
+        'MaxSize' => 0,
+        'Iterations' => 1,
+        'ForceEncode' => false
+    }
+  }
+
+  let!(:payload_module) {
+    load_and_create_module(
+        ancestor_reference_names: %w{
+          stagers/windows/reverse_tcp
+          stages/windows/meterpreter
+        },
+        module_type: 'payload',
+        reference_name: 'windows/meterpreter/reverse_tcp'
+    )
+  }
+
+  subject(:payload) {
+    described_class.generate_simple(payload_module, generator_opts)
+  }
+
+  it { expect { payload }.not_to raise_error }
+
+  context 'when given any valid transform format' do
+    let(:generator_format) { 
+      'octal' # TODO change to ::Msf::Simple::Buffer.transform_formats.sample
+    }
+
+    it { expect { payload }.not_to raise_error }
+  end
+end
