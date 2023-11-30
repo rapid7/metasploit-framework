7c125ab4a227e3b72a5f7daafafb97ec38cfc3f4
Zach Goldman <zach_goldman@rapid7.com>
2023-11-30 09:30:31 -0600

    remove include mssql base from client

--- lib/metasploit/framework/mssql/client.rb
+++ lib/metasploit/framework/mssql/client.rb
@@ -1,5 +1,4 @@
 require 'metasploit/framework/mssql/tdssslproxy'
-require 'metasploit/framework/mssql/base'
 
 module Metasploit
   module Framework
@@ -7,7 +6,6 @@ module Metasploit
 
       module Client
         extend ActiveSupport::Concern
-        include Metasploit::Framework::MSSQL::Base
 
         attr_accessor :sock
         attr_accessor :max_send_size
@@ -28,8 +26,32 @@ module Metasploit
                         only_integer:             true,
                         greater_than_or_equal_to: 0
                     }
-
         end
+        # Encryption
+        ENCRYPT_OFF     = 0x00 #Encryption is available but off.
+        ENCRYPT_ON      = 0x01 #Encryption is available and on.
+        ENCRYPT_NOT_SUP = 0x02 #Encryption is not available.
+        ENCRYPT_REQ     = 0x03 #Encryption is required.
+
+        # Packet Type
+        TYPE_SQL_BATCH                   = 1  # (Client) SQL command
+        TYPE_PRE_TDS7_LOGIN              = 2  # (Client) Pre-login with version < 7 (unused)
+        TYPE_RPC                         = 3  # (Client) RPC
+        TYPE_TABLE_RESPONSE              = 4  # (Server)  Pre-Login Response ,Login Response, Row Data, Return Status, Return Parameters,
+        # Request Completion, Error and Info Messages, Attention Acknowledgement
+        TYPE_ATTENTION_SIGNAL            = 6  # (Client) Attention
+        TYPE_BULK_LOAD                   = 7  # (Client) SQL Command with binary data
+        TYPE_TRANSACTION_MANAGER_REQUEST = 14 # (Client) Transaction request manager
+        TYPE_TDS7_LOGIN                  = 16 # (Client) Login
+        TYPE_SSPI_MESSAGE                = 17 # (Client) Login
+        TYPE_PRE_LOGIN_MESSAGE           = 18 # (Client) pre-login with version > 7
+
+        # Status
+        STATUS_NORMAL                  = 0x00
+        STATUS_END_OF_MESSAGE          = 0x01
+        STATUS_IGNORE_EVENT            = 0x02
+        STATUS_RESETCONNECTION         = 0x08 # TDS 7.1+
+        STATUS_RESETCONNECTIONSKIPTRAN = 0x10 # TDS 7.3+
 
         #
         # This method connects to the server over TCP and attempts
@@ -523,6 +545,132 @@ module Metasploit
 
         def mssql_ssl_send_recv(req, tdsproxy, timeout=15, check_status=true)
           tdsproxy.send_recv(req)
+        end
+
+        def mssql_send_recv(req, timeout=15, check_status = true)
+          sock.put(req)
+      
+          # Read the 8 byte header to get the length and status
+          # Read the length to get the data
+          # If the status is 0, read another header and more data
+      
+          done = false
+          resp = ""
+      
+          while(not done)
+            head = sock.get_once(8, timeout)
+            if !(head && head.length == 8)
+              return false
+            end
+      
+            # Is this the last buffer?
+            if head[1, 1] == "\x01" || !check_status
+              done = true
+            end
+      
+            # Grab this block's length
+            rlen = head[2, 2].unpack('n')[0] - 8
+      
+            while(rlen > 0)
+              buff = sock.get_once(rlen, timeout)
+              return if not buff
+              resp << buff
+              rlen -= buff.length
+            end
+          end
+      
+          resp
+        end
+
+        def mssql_tds_encrypt(pass)
+          # Convert to unicode, swap 4 bits both ways, xor with 0xa5
+          Rex::Text.to_unicode(pass).unpack('C*').map {|c| (((c & 0x0f) << 4) + ((c & 0xf0) >> 4)) ^ 0xa5 }.pack("C*")
+        end
+
+        def mssql_parse_reply(data, info)
+          info[:errors] = []
+          return if not data
+          until data.empty?
+            token = data.slice!(0, 1).unpack('C')[0]
+            case token
+            when 0x81
+              mssql_parse_tds_reply(data, info)
+            when 0xd1
+              mssql_parse_tds_row(data, info)
+            when 0xe3
+              mssql_parse_env(data, info)
+            when 0x79
+              mssql_parse_ret(data, info)
+            when 0xfd, 0xfe, 0xff
+              mssql_parse_done(data, info)
+            when 0xad
+              mssql_parse_login_ack(data, info)
+            when 0xab
+              mssql_parse_info(data, info)
+            when 0xaa
+              mssql_parse_error(data, info)
+            when nil
+              break
+            else
+              info[:errors] << "unsupported token: #{token}"
+            end
+          end
+          info
+        end
+
+        def mssql_parse_error(data, info)
+          len  = data.slice!(0, 2).unpack('v')[0]
+          buff = data.slice!(0, len)
+      
+          errno, state, sev, elen = buff.slice!(0, 8).unpack('VCCv')
+          emsg = buff.slice!(0, elen * 2)
+          emsg.gsub!("\x00", '')
+      
+          info[:errors] << "SQL Server Error ##{errno} (State:#{state} Severity:#{sev}): #{emsg}"
+          info
+        end
+
+        def mssql_parse_done(data, info)
+          status, cmd, rows = data.slice!(0, 8).unpack('vvV')
+          info[:done] = { :status => status, :cmd => cmd, :rows => rows }
+          info
+        end
+
+        def mssql_parse_env(data, info)
+          len  = data.slice!(0, 2).unpack('v')[0]
+          buff = data.slice!(0, len)
+          type = buff.slice!(0, 1).unpack('C')[0]
+      
+          nval = ''
+          nlen = buff.slice!(0, 1).unpack('C')[0] || 0
+          nval = buff.slice!(0, nlen * 2).gsub("\x00", '') if nlen > 0
+      
+          oval = ''
+          olen = buff.slice!(0, 1).unpack('C')[0] || 0
+          oval = buff.slice!(0, olen * 2).gsub("\x00", '') if olen > 0
+      
+          info[:envs] ||= []
+          info[:envs] << { :type => type, :old => oval, :new => nval }
+          info
+        end
+
+        def mssql_parse_info(data, info)
+          len  = data.slice!(0, 2).unpack('v')[0]
+          buff = data.slice!(0, len)
+      
+          errno, state, sev, elen = buff.slice!(0, 8).unpack('VCCv')
+          emsg = buff.slice!(0, elen * 2)
+          emsg.gsub!("\x00", '')
+      
+          info[:infos] ||= []
+          info[:infos] << "SQL Server Info ##{errno} (State:#{state} Severity:#{sev}): #{emsg}"
+          info
+        end
+
+        def mssql_parse_login_ack(data, info)
+          len = data.slice!(0, 2).unpack('v')[0]
+          _buff = data.slice!(0, len)
+          info[:login_ack] = true
         end
 
         def connect(global = true, opts={})
