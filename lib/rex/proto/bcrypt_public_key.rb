8800a74b2756d54aff375926d7d2cbc68daba39a
Ashley Donaldson <smashery@gmail.com>
2024-03-28 14:31:50 +1100

    Wrap credential struct with nicer API

---
+++ lib/rex/proto/bcrypt_public_key.rb
@@ -0,0 +1,22 @@
+# -*- coding: binary -*-
+
+require 'bindata'
+
+module Rex::Proto
+  class BcryptPublicKey < BinData::Record
+    endian :little
+
+    uint32              :magic
+    uint32              :key_length, :value => lambda { exponent.length + modulus.length + prime1.length + prime2.length  }
+    uint32              :exponent_length, :value => lambda { exponent.length }
+    uint32              :modulus_length, :value => lambda { modulus.length }
+    uint32              :prime1_length, :value => lambda { prime1.length }
+    uint32              :prime2_length, :value => lambda { prime2.length }
+
+    string              :exponent, :read_length => :exponent_length
+    string              :modulus, :read_length => :modulus_length
+    string              :prime1, :read_length => :prime1_length
+    string              :prime2, :read_length => :prime2_length
+  end
+end
+
--- lib/rex/proto/ms_adts/key_credential.rb
+++ lib/rex/proto/ms_adts/key_credential.rb
@@ -8,37 +8,44 @@ module Rex::Proto::MsAdts
     KEY_USAGE_FIDO = 0x07
     KEY_USAGE_FIDO = 0x08
 
-    def init(key_material, key_usage, last_logon_time, creation_time)
-      self.key_material = key_material
+    def initialize
+      self.key_source = 0
+      self.device_id = SecureRandom.bytes(16)
+      self.custom_key_information = "\x01\x00" # Version and flags
+    end
+
+    def set_key(public_key, key_usage)
+      self.public_key = public_key
       self.key_usage = key_usage
 
       calculate_raw_key_material
+    end
 
-      self.key_approximate_last_logon_time_stamp
+    def set_times(last_logon_time, creation_time)
+      self.key_approximate_last_logon_time_stamp = last_logon_time
+      self.key_approximate_last_logon_time_stamp_raw = RubySMB::Field::FileTime.new(self.key_approximate_last_logon_time_stamp).to_binary_s
       self.key_creation_time = creation_time
-
-      self.key_source = 0
-      self.device_id = SecureRandom.uuid
-      self.custom_key_information = "\x01\x00" # Version and flags
-      sha256 = OpenSSL::Digest.new('SHA256')
-      self.key_id = sha256.digest(self.raw_key_material.to_der)
+      self.key_creation_time_raw = RubySMB::Field::FileTime.new(self.key_creation_time).to_binary_s
     end
 
     # Creates a KeyCredentialStruct, including calculating the value for key_hash
     def to_struct
       result = KeyCredentialStruct.new
+      result.version = 0x200
       add_entry(result, 3, self.raw_key_material)
       add_entry(result, 4, [self.key_usage].pack('C'))
       add_entry(result, 5, [self.key_source].pack('C'))
       add_entry(result, 6, self.device_id)
       add_entry(result, 7, self.custom_key_information)
-      add_entry(result, 8, RubySMB::Field::FileTime.new(self.key_approximate_last_logon_time_stamp).to_binary_s)
-      add_entry(result, 9, RubySMB::Field::FileTime.new(self.key_creation_time).to_binary_s)
+      add_entry(result, 8, self.key_approximate_last_logon_time_stamp_raw)
+      add_entry(result, 9, self.key_creation_time_raw)
 
-      calculate_key_hash
+      calculate_key_hash(result)
 
-      add_entry(result, 1, self.key_id, insert_at_end: false)
       add_entry(result, 2, self.key_hash, insert_at_end: false)
+      add_entry(result, 1, self.key_id, insert_at_end: false)
+
+      result
     end
 
 
@@ -52,10 +59,12 @@ module Rex::Proto::MsAdts
       obj.key_source = get_entry(cred_struct, 5).unpack('C')[0]
       obj.device_id = get_entry(cred_struct, 6)
       obj.custom_key_information = get_entry(cred_struct, 7)
-      ft = get_entry(cred_struct, 8).unpack('Q')[0]
-      obj.key_approximate_last_logon_time_stamp = RubySMB::Field::FileTime.new(ft).to_time
-      ft = get_entry(cred_struct, 9).unpack('Q')[0]
-      obj.key_creation_time = RubySMB::Field::FileTime.new(ft).to_time
+      ft = get_entry(cred_struct, 8)
+      obj.key_approximate_last_logon_time_stamp_raw = ft
+      obj.key_approximate_last_logon_time_stamp = RubySMB::Field::FileTime.new(ft.unpack('Q')[0]).to_time
+      ft = get_entry(cred_struct, 9)
+      obj.key_creation_time_raw = ft
+      obj.key_creation_time = RubySMB::Field::FileTime.new(ft.unpack('Q')[0]).to_time
 
       construct_cert_from_raw_material(obj)
 
@@ -65,12 +74,14 @@ module Rex::Proto::MsAdts
     # Properties
     attr_accessor :key_id # SHA256 hash of KeyMaterial
     attr_accessor :key_hash # SHA256 hash of all entries after this entry
-    attr_accessor :key_material # Key material of the credential
-    attr_accessor :raw_key_material # Key material of the credential
+    attr_accessor :public_key # The public_key applied to the account
+    attr_accessor :raw_key_material # Key material of the credential, in bytes
     attr_accessor :key_usage # Enumeration
     attr_accessor :key_source # Always KEY_SOURCE_AD (0)
     attr_accessor :device_id # Identifier for this credential
     attr_accessor :custom_key_information # Two bytes is fine: Version and Flags
+    attr_accessor :key_approximate_last_logon_time_stamp_raw # Raw bytes for approximate time this key was last used
+    attr_accessor :key_creation_time_raw # Raw bytes for approximate time this key was created
     attr_accessor :key_approximate_last_logon_time_stamp # Approximate time this key was last used
     attr_accessor :key_creation_time # Approximate time this key was created
 
@@ -90,29 +101,63 @@ module Rex::Proto::MsAdts
       entry.data = data
       entry.struct_length = data.length
       if insert_at_end
-        struct.credential_entries.append(entry)
+        struct.credential_entries.insert(struct.credential_entries.length, entry)
       else # Insert at start
         struct.credential_entries.insert(0, entry)
       end
     end
 
+    def self.int_to_bytes(num)
+      str = num.to_s(16).rjust(2, '0')
+
+      [str].pack('H*')
+    end
+
+    def self.bytes_to_int(num)
+      num.unpack('H*')[0].to_i(16)
+    end
+
     # Sets self.key_hash based on the credential_entries value in the provided parameter
-    # @param struct [KeyCredentialStruct] Its entries value should have only those required to calculate the key_hash value (no key_id or key_hash)
+    # @param struct [KeyCredentialStruct] Its credential_entries value should have only those required to calculate the key_hash value (no key_id or key_hash)
     def calculate_key_hash(struct)
         sha256 = OpenSSL::Digest.new('SHA256')
-        self.key_hash = sha256.digest(struct.entries.to_binary_s)
+        self.key_hash = sha256.digest(struct.credential_entries.to_binary_s)
     end
 
     # Sets self.raw_key_material, based on the key material, and the key usage
     def calculate_raw_key_material
       case self.key_usage
-      when 
+      when KEY_USAGE_NGC
+        result = BcryptPublicKey.new
+        result.magic = 0x31415352
+        n = int_to_bytes(self.public_key.n)
+        e = int_to_bytes(self.public_key.e)
+        result.exponent = e
+        result.modulus = n
+        result.prime1 = ''
+        result.prime2 = ''
+
+        self.raw_key_material = result.to_binary_s
       end
+      sha256 = OpenSSL::Digest.new('SHA256')
+      self.key_id = sha256.digest(self.raw_key_material.to_der)
     end
 
     def self.construct_cert_from_raw_material(obj)
       case obj.key_usage
-      when 1
+      when KEY_USAGE_NGC
+        result = Rex::Proto::BcryptPublicKey.read(obj.raw_key_material)
+        key = OpenSSL::PKey::RSA.new
+        exponent = OpenSSL::BN.new(bytes_to_int(result.exponent))
+        modulus = OpenSSL::BN.new(bytes_to_int(result.modulus))
+        if key.respond_to?(:set_key)
+          # Ruby 2.4+
+          key.set_key(modulus, exponent, nil)
+        else
+          key.e = exponent
+          key.n = modulus
+        end
+        obj.public_key = key
       end
     end
   end
