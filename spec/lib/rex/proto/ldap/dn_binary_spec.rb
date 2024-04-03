816d834f8390fb5e0524d57f651ae76ecd6a37d1
Ashley Donaldson <smashery@gmail.com>
2024-04-03 16:47:47 +1100

    Add dn-binary unit tests

---
+++ spec/lib/rex/proto/ldap/dn_binary_spec.rb
@@ -0,0 +1,36 @@
+require 'securerandom'
+
+RSpec.describe Rex::Proto::LDAP::DnBinary do
+  let(:dn) do
+    'CN=User,CN=Users,DC=msf,DC=local'
+  end
+
+  let(:data) do
+    'abc123'
+  end
+
+  let(:sample) do
+    described_class.new(dn, data)
+  end
+
+  it 'encodes to the expected value' do
+    expect(sample.encode).to eq('B:12:616263313233:CN=User,CN=Users,DC=msf,DC=local')
+  end
+
+  it 'encodes an empty value' do
+    initial = described_class.new(dn, '')
+    encoded = initial.encode
+    expect(encoded).to eq('B:0::CN=User,CN=Users,DC=msf,DC=local')
+    decoded = described_class.decode(encoded)
+    expect(decoded.data).to eq('')
+  end
+
+  it 'reversibly decodes a random value' do
+    data = SecureRandom.bytes((SecureRandom.rand * 100).to_i + 1)
+    initial = described_class.new(dn, data)
+    encoded = initial.encode
+    decoded = described_class.decode(encoded)
+    expect(decoded.dn).to eq(initial.dn)
+    expect(decoded.data).to eq(initial.data)
+  end
+end
