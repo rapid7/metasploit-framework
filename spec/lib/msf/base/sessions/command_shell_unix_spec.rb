e9f86c48653fd15112783e5e6ca583f69db08599
Ashley Donaldson <smashery@gmail.com>
2024-04-10 16:18:52 +1000

    Reworked unix create_process, as it was buggy

---
+++ spec/lib/msf/base/sessions/command_shell_unix_spec.rb
@@ -0,0 +1,44 @@
+RSpec.describe Msf::Sessions::CommandShellUnix do
+  describe 'to_cmd processing' do
+    it 'should not do anything for simple args' do
+      expect(described_class.to_cmd('./test', [])).to eq('./test')
+      expect(described_class.to_cmd('sh', [])).to eq('sh')
+      expect(described_class.to_cmd('./test', ['basic','args'])).to eq('./test basic args')
+    end
+
+    it 'should escape spaces' do
+      expect(described_class.to_cmd('/home/user/some folder/some program', [])).to eq('/home/user/some\\ folder/some\\ program')
+      expect(described_class.to_cmd('./test', ['with space'])).to eq('./test with\\ space')
+    end
+
+    it 'should escape logical operators' do
+      expect(described_class.to_cmd('./test', ['&&', 'echo', 'words'])).to eq('./test \\&\\& echo words')
+      expect(described_class.to_cmd('./test', ['||', 'echo', 'words'])).to eq('./test \\|\\| echo words')
+      expect(described_class.to_cmd('./test', ['&echo', 'words'])).to eq('./test \\&echo words')
+      expect(described_class.to_cmd('./test', ['run&echo', 'words'])).to eq('./test run\\&echo words')
+    end
+
+    it 'should quote if single quotes are present' do
+      expect(described_class.to_cmd('./test', ["it's"])).to eq("./test \"it's\"")
+      expect(described_class.to_cmd('./test', ["it's a param"])).to eq("./test \"it's a param\"")
+    end
+
+    it 'should escape redirectors' do
+      expect(described_class.to_cmd('./test', ['>', 'out.txt'])).to eq('./test \\> out.txt')
+      expect(described_class.to_cmd('./test', ['<', 'in.txt'])).to eq('./test \\< in.txt')
+    end
+
+    it 'should not expand env vars' do
+      expect(described_class.to_cmd('./test', ['$PATH'])).to eq("./test \\$PATH")
+      # Still escape even when quoted:
+      expect(described_class.to_cmd('./test', ["it's $PATH"])).to eq("./test \"it's \\$PATH\"")
+      expect(described_class.to_cmd('./test', ["\"$PATH\""])).to eq("./test \\\"\\$PATH\\\"")
+      expect(described_class.to_cmd('./test', ["it's \"$PATH\""])).to eq("./test \"it's \\\"\\$PATH\\\"\"")
+    end
+
+    it 'should not quote and escape every character' do
+      expect(described_class.to_cmd('/home/user/some folder/some program', [])).to eq('/home/user/some\\ folder/some\\ program')
+      expect(described_class.to_cmd('./test', ['with space'])).to eq('./test with\\ space')
+    end
+  end
+end
