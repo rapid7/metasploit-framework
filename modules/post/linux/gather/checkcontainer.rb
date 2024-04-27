b51d1b9017c3194972ecd0474ad9840ac83bd16a
RadioLogic <ncottrellweb@gmail.com>
2024-04-26 21:32:20 -0400

    Made shared function with checkcontainer

--- modules/post/linux/gather/checkcontainer.rb
+++ modules/post/linux/gather/checkcontainer.rb
@@ -4,7 +4,7 @@
 ##
 
 class MetasploitModule < Msf::Post
-  include Msf::Post::File
+  include Msf::Post::Linux::System
 
   def initialize(info = {})
     super(
@@ -18,72 +18,20 @@ class MetasploitModule < Msf::Post
         },
         'License' => MSF_LICENSE,
         'Author' => [ 'James Otten <jamesotten1[at]gmail.com>'],
-        'Platform' => [ 'linux' ],
-        'SessionTypes' => [ 'shell', 'meterpreter' ]
+        'Platform' => %w[linux unix],
+        'SessionTypes' => %w[shell meterpreter],
+        'Notes' => {
+          'Stability' => [ CRASH_SAFE ],
+          'Reliability' => [ REPEATABLE_SESSION ],
+          'SideEffects' => []
+        }
       )
     )
   end
 
   # Run Method for when run command is issued
   def run
-    container = nil
-
-    # Check for .dockerenv file
-    if container.nil? && file?('/.dockerenv')
-      container = 'Docker'
-    end
-
-    # Check for .dockerinit file
-    if container.nil? && file?('/.dockerinit')
-      container = 'Docker'
-    end
-
-    # Check for /.containerenv file
-    if container.nil? && file?('/run/.containerenv')
-      container = 'Podman'
-    end
-
-    # Check for /dev/lxd/sock file
-    if container.nil? && directory?('/dev/lxc')
-      container = 'LXC'
-    end
-
-    # Check for WSL, as suggested in https://github.com/Microsoft/WSL/issues/423#issuecomment-221627364
-    if container.nil? && file?('/proc/sys/kernel/osrelease')
-      osrelease = read_file('/proc/sys/kernel/osrelease')
-      if osrelease
-        case osrelease.tr("\n", ' ')
-        when /WSL|Microsoft/i
-          container = 'WSL'
-        end
-      end
-    end
-
-    # Check cgroup on PID 1
-    if container.nil?
-      cgroup = read_file('/proc/1/cgroup')
-      if cgroup
-        case cgroup.tr("\n", ' ')
-        when /docker/i
-          container = 'Docker'
-        when /lxc/i
-          container = 'LXC'
-        end
-      end
-    end
-
-    # Check for the "container" environment variable
-    if container.nil?
-      container_variable = get_env('container')
-      case container_variable
-      when 'lxc'
-        container = 'LXC'
-      when 'systemd-nspawn'
-        container = 'systemd nspawn'
-      when 'podman'
-        container = 'podman'
-      end
-    end
+    container = get_container_type
 
     if container
       print_good("This appears to be a '#{container}' container")
