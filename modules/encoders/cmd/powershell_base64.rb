111d329609a4131ff898ff3f95de5002c7cdd5c5
Spencer McIntyre <zeroSteiner@gmail.com>
2024-06-13 15:46:02 -0400

    Fix the powershell_base64 encoder

--- modules/encoders/cmd/powershell_base64.rb
+++ modules/encoders/cmd/powershell_base64.rb
@@ -22,7 +22,6 @@ class MetasploitModule < Msf::Encoder
   # Encodes the payload
   #
   def encode_block(state, buf)
-
     # Skip encoding for empty badchars
     if state.badchars.length == 0
       return buf
@@ -45,18 +44,7 @@ class MetasploitModule < Msf::Encoder
   end
 
   def encode_buf(buf)
-    # From https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_quoting_rules?view=powershell-7.3
-    # To include a single quotation mark in a single-quoted string, use a second consecutive single quote. For example:
-    # 'don''t' would be the string "don't" but using single quotes.
-    #
-    # Note that we can't use double quotes here as double quote strings in PowerShell are classed as expandable strings
-    # and we don't want expansion here, as this might cause any potential elements starting with $ to be interpreted
-    # as a variable within the string to be replaced by that variable's value.
-    #
-    # The use of quotes also ensures that we get around the issue with cmd.exe understanding & as a symbol for
-    # "also execute this command", whereas in PowerShell it is a reserved character, so not quoting the string
-    # will result in the & being interpreted by PowerShell and the command failing on an interpretation error in PowerShell itself.
-    base64 = Rex::Text.encode_base64(Rex::Text.to_unicode("cmd.exe /c 'start #{Msf::Post::Windows.escape_powershell_literal(buf)} '"))
+    base64 = Rex::Text.encode_base64(Rex::Text.to_unicode("cmd.exe /c '#{Msf::Post::Windows.escape_powershell_literal(buf)}'"))
     cmd = "powershell -w hidden -nop -e #{base64}"
   end
 end
