daa63e931b9a4af2d664f67aea9a1072fe78361e
Arne De Herdt <arne_deherdt@rapid7.com>
2024-11-12 11:32:47 +0100

    testing a hack to see if it solves the problem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -128,6 +128,12 @@ RSpec.configure do |config|
     expectations.syntax = :expect
   end
 
+  # DEBUG: Hack to see if we can force the connection to reconnect
+  # to the database before each test, if this somehow affects the behavior.
+  before(:each) do
+    ::ActiveRecord::Base.connection.reconnect! unless ::ActiveRecord::Base.connected?
+  end
+
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
