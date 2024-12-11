4752df3ab5d7a8f2ca24892445f07bbf0b9cb377
h00die <michaeltcyr@gmail.com>
2024-12-11 17:52:07 -0500

    more specs and progress

--- spec/lib/msf/core/post/linux/process_spec.rb
+++ spec/lib/msf/core/post/linux/process_spec.rb
@@ -12,11 +12,13 @@ RSpec.describe Msf::Post::Linux::Process do
     let(:length) { 64 }
     let(:pid) { 1234 }
     let(:memory_content) { 'memory content' }
+    let(:mock_session) { double('Session', send: nil) }
 
     it 'reads memory from the specified base address and length' do
       expect(subject).to receive(:session)
       expect(subject).to receive(:open).with(pid, PROCESS_READ).and_return(1)
       expect(memory).to receive(:read).with(base_address, length).and_return(memory_content)
+      expect(mock_session).to receive(:type).and_return('meterpreter')
 
       result = subject.mem_read(base_address, length, pid: pid)
       expect(result).to eq(memory_content)
@@ -26,6 +28,7 @@ RSpec.describe Msf::Post::Linux::Process do
       expect(subject).to receive(:session)
       expect(subject).to receive(:open).with(0, PROCESS_READ).and_return(1)
       expect(memory).to receive(:read).with(base_address, length).and_return(memory_content)
+      expect(mock_session).to receive(:type).and_return('meterpreter')
 
       result = subject.mem_read(base_address, length)
       expect(result).to eq(memory_content)
