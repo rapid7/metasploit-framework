aca6613a3e414b7c9c2be3c4ac44d68d7aa37765
dledda-r7 <diego_ledda@rapid7.com>
2024-12-20 09:44:39 -0500

    feat: add PrependSetreuid for linux/aarch64

--- lib/msf/core/payload/linux/aarch64/prepends.rb
+++ lib/msf/core/payload/linux/aarch64/prepends.rb
@@ -17,8 +17,13 @@ module Msf
         # setuid(0)
         'PrependSetuid' =>  "\xe0\x03\x1f\xaa" +  # mov   x0, xzr
                             "\x48\x12\x80\xd2" +  # mov   x8, #0x92
-                            "\x01\x00\x00\xd4"    # svc   0x0
+                            "\x01\x00\x00\xd4",   # svc   0x0
 
+        # setreuid(0, 0)
+        'PrependSetreuid' =>  "\xe0\x03\x1f\xaa" +  # mov        x0, xzr
+                              "\xe1\x03\x1f\xaa" +  # mov        x1, xzr
+                              "\x28\x12\x80\xd2" +  # mov        x8, #0x91
+                              "\x01\x00\x00\xd4"    # svc        0x0
       }
     end
 
