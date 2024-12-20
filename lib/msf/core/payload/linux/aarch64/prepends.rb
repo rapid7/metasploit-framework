647972b7c8f50de2d3e36bc6d5f447117ab1131f
dledda-r7 <diego_ledda@rapid7.com>
2024-12-20 08:13:09 -0500

    feat: add PrependSetuid for linux/aarch64

---
+++ lib/msf/core/payload/linux/aarch64/prepends.rb
@@ -0,0 +1,29 @@
+module Msf
+  module Payload::Linux::Aarch64::Prepends
+    include Msf::Payload::Linux::Prepends
+
+    def prepends_order 
+      %w[PrependSetuid]
+    end
+
+    def appends_order 
+      %w[]
+    end
+
+    def prepends_map
+      {
+        # 'PrependFork' =>  "",
+
+        # setuid(0)
+        'PrependSetuid' =>  "\xe0\x03\x1f\xaa" +  # mov   x0, xzr
+                            "\x48\x12\x80\xd2" +  # mov   x8, #0x92
+                            "\x01\x00\x00\xd4"    # svc   0x0
+
+      }
+    end
+
+    def appends_map
+      {}
+    end
+  end
+end
