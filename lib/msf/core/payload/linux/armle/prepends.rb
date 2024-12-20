30e13c9040abaacf587e6fdcfea01ecd5df6c2c9
dledda-r7 <diego_ledda@rapid7.com>
2024-12-20 08:13:52 -0500

    fix: fix mismatch between prepend name and stub in linux/armle

--- lib/msf/core/payload/linux/armle/prepends.rb
+++ lib/msf/core/payload/linux/armle/prepends.rb
@@ -14,17 +14,18 @@ module Msf
       {
         # 'PrependFork' =>  "",
 
-        # setresuid(0, 0, 0)                  
-        'PrependSetresuid' => "\x00\x00\x20\xe0"    + #    eor r0, r0, r0                    #
+        # 
+        # setuid(0)                  
+        'PrependSetuid' =>    "\x00\x00\x20\xe0"    + #    eor r0, r0, r0                    #
                               "\x17\x70\xa0\xe3"    + #    mov r7, #23                       #
                               "\x00\x00\x00\xef",     #    svc                               #
 
-        # setuid(0)
-        'PrependSetuid' =>  "\x00\x00\x20\xe0"    + #    eor r0, r0, r0                    #
-                            "\x01\x10\x21\xe0"    + #    eor r1, r1, r1                    #
-                            "\x02\x20\x22\xe0"    + #    eor r2, r2, r2                    #
-                            "\xa4\x70\xa0\xe3"    + #    mov r7, #0xa4                     #
-                            "\x00\x00\x00\xef"      #    svc                               #
+        # setresuid(0, 0, 0)
+        'PrependSetresuid' =>   "\x00\x00\x20\xe0"    + #    eor r0, r0, r0                    #
+                                "\x01\x10\x21\xe0"    + #    eor r1, r1, r1                    #
+                                "\x02\x20\x22\xe0"    + #    eor r2, r2, r2                    #
+                                "\xa4\x70\xa0\xe3"    + #    mov r7, #0xa4                     #
+                                "\x00\x00\x00\xef"      #    svc                               #
       }
     end
 
