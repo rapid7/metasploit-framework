0c2b25e0999333e09526975b48af65514830c322
Martin Sutovsky <martin_sutovsky@rapid7.com>
2025-01-27 14:12:44 +0100

    PPC64 shellcode added, adding PPC initial work

---
+++ data/templates/src/elf/exe/elf_ppc_template.s
@@ -0,0 +1,35 @@
+BITS 32
+org     0x8000
+ehdr:                            ; Elf32_Ehdr
+  db    0x7F, "ELF", 1, 2, 1, 0  ;   e_ident
+  db    0, 0, 0, 0,  0, 0, 0, 0  ;
+  dw    0x0200                   ;   e_type       = ET_EXEC for an executable
+  dw    0x1400                   ;   e_machine    = AARCH64
+  dd    0x10000000                        ;   e_version
+  dd    0x00008054      ;   e_entry
+  dd    0x34000000       ;   e_phoff
+  dd    0                        ;   e_shoff
+  dd    0                        ;   e_flags
+  dw    0x3400                   ;   e_ehsize
+  dw    0x2000                   ;   e_phentsize
+  dw    0x0100                   ;   e_phnum
+  dw    0                        ;   e_shentsize
+  dw    0                        ;   e_shnum
+  dw    0                        ;   e_shstrndx
+
+ehdrsize equ  $ - ehdr
+
+phdr:                            ; Elf32_Phdr
+  dd    0x01000000               ;   p_type       = PT_LOAD
+  dd    0                        ;   p_offset
+  dd    0x00800000       ;   p_vaddr
+  dd    0x00800000                       ;   p_paddr
+  dd    0xdeadbeef       ;   p_filesz
+  dd    0xdeadbeef       ;   p_memsz
+  dd    0x07000000               ;   p_flags      = rwx
+  dd    0x00010000       ;   p_align
+
+phdrsize equ  $ - phdr
+
+_start:
+
