6f2ff5110e3c519e7c247edbecef8bb0dbbdd93f
TheBigStonk <64230005+TheBigStonk@users.noreply.github.com>
2025-01-31 11:20:22 +1300

    Update modules/auxiliary/gather/argus_dvr_4_lfi_cve_2018_15745.rb
    awesome cutting this one out then :)
    
    Co-authored-by: jheysel-r7 <Jack_Heysel@rapid7.com>

--- modules/auxiliary/gather/argus_dvr_4_lfi_cve_2018_15745.rb
+++ modules/auxiliary/gather/argus_dvr_4_lfi_cve_2018_15745.rb
@@ -44,7 +44,6 @@ def initialize(info = {})
 
     register_options(
         [
-          Opt::RHOST(),
           OptString.new('TARGET_FILE', [true, 'The file to retrieve', 'Windows/system.ini'])
         ]
       )
