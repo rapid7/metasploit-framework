39b7aa88faee3e8734b592b7abddeb03b9b50152
cgranleese-r7 <Christopher_Granleese@rapid7.com>
2025-02-14 10:56:54 +0000

    Adds skips for functionality not yet implemented

--- test/modules/post/test/resolve_host.rb
+++ test/modules/post/test/resolve_host.rb
@@ -26,42 +26,63 @@ class MetasploitModule < Msf::Post
     vprint_status('Starting resolve_host tests')
 
     it 'should return a Hash' do
-      hostname = 'localhost'
+      hostname = 'google.com'
       family = AF_INET
+      does_not_support_local_resolution = ['php']
 
-      resolved_host = resolve_host(hostname, family)
-      resolved_host.is_a?(Hash)
+      if does_not_support_local_resolution.include?(session.arch) && session.platform.eql?('windows') && hostname == 'localhost'
+        vprint_status("test skipped for #{session.arch} - local DNS resolution not available")
+      else
+        resolved_host = resolve_host(hostname, family)
+        resolved_host.is_a?(Hash)
+      end
     end
 
     it 'should return a valid IPV4 host' do
-      hostname = 'localhost'
+      hostname = 'google.com'
       family = AF_INET
+      does_not_support_local_resolution = ['php']
 
-      resolved_host = resolve_host(hostname, family)
-      if resolved_host[:ips].empty?
-        false
+      if does_not_support_local_resolution.include?(session.arch) && session.platform.eql?('windows') && hostname == 'localhost'
+        vprint_status("test skipped for #{session.arch} - local DNS resolution not available")
       else
-        matches = resolved_host[:ips].map do |ip|
-          !!(ip =~ Resolv::IPv4::Regex)
-        end
+        resolved_host = resolve_host(hostname, family)
+        if resolved_host[:ips].empty?
+          false
+        else
+          matches = resolved_host[:ips].map do |ip|
+            !!(ip =~ Resolv::IPv4::Regex)
+          end
 
-        matches.all?(true)
+          matches.all?(true)
+        end
       end
     end
 
     it 'should return a valid IPV6 host' do
-      hostname = 'localhost'
+      hostname = 'google.com'
       family = AF_INET6
+      does_not_support_ipv6_resolution = %w[python java x64 x86]
+      does_not_support_local_resolution = ['php']
 
-      resolved_host = resolve_host(hostname, family)
-      if resolved_host[:ips].empty?
-        false
+      print_status(session.arch)
+      print_status(session.platform)
+
+      if does_not_support_ipv6_resolution.include?(session.arch) && session.platform.eql?('windows')
+        vprint_status("test skipped for #{session.arch} - IPV6 DNS resolution not available")
+      elsif does_not_support_local_resolution.include?(session.arch) && session.platform.eql?('windows') && hostname == 'localhost'
+        vprint_status("test skipped for #{session.arch} - local DNS resolution not available")
       else
-        matches = resolved_host[:ips].map do |ip|
-          !!(ip =~ Resolv::IPv6::Regex)
-        end
+        resolved_host = resolve_host(hostname, family)
+        if resolved_host[:ips].empty?
+          false
+        else
+          matches = resolved_host[:ips].map do |ip|
+            !!(ip =~ Resolv::IPv6::Regex)
+          end
 
-        matches.all?(true)
+          matches.all?(true)
+        end
       end
     end
 
