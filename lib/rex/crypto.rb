b853168a895ab6263ac1f34376844e46fe2969c6
Spencer McIntyre <zeroSteiner@gmail.com>
2025-02-21 16:56:22 -0500

    Make common byte to int conversion functions

--- lib/rex/crypto.rb
+++ lib/rex/crypto.rb
@@ -27,4 +27,27 @@ module Rex::Crypto
   def self.rc4(key, value)
     Rc4.rc4(key, value)
   end
+
+  # Returns an integer represented as a byte array. Useful for certain key-related operations.
+  #
+  # @param bytes [String] The bytes to convert
+  # @return [Integer] The converted value.
+  def self.bytes_to_int(bytes)
+    bytes.each_byte.reduce(0) { |acc, byte| (acc << 8) | byte }
+  end
+
+  # Returns a byte array represented as a big-endian integer. Useful for certain key-related operations.
+  #
+  # @param bytes [String] The bytes to convert
+  # @return [Integer] The converted value.
+  def self.int_to_bytes(num)
+    bytes = []
+
+    while num > 0
+      bytes.unshift(num & 0xff)
+      num >>= 8
+    end
+
+    bytes.pack("C*")
+  end
 end
