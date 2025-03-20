d4f66ad7d222fd9d283b878b1635fe0dd3c3a601
dledda-r7 <diego_ledda@rapid7.com>
2025-03-20 06:33:58 -0400

    fix: removing duplicate dispatchers stdapi_fs and stdapi_net

--- lib/rex/post/meterpreter/ui/console/command_dispatcher/stdapi_net.rb
+++ lib/rex/post/meterpreter/ui/console/command_dispatcher/stdapi_net.rb
@@ -38,6 +38,17 @@ class Console::CommandDispatcher::Stdapi_Net
     Dispatchers.each { |d|
       shell.enstack_dispatcher(d)
     }
+    str_dispatchers = []
+    uniq_dispatchers = []
+    idx = 0
+    while idx < shell.dispatcher_stack.length
+      unless str_dispatchers.include?(shell.dispatcher_stack[idx].class.to_s)
+        str_dispatchers.push(shell.dispatcher_stack[idx].class.to_s)
+        uniq_dispatchers.push(shell.dispatcher_stack[idx])
+      end
+      idx += 1
+    end
+    shell.dispatcher_stack = uniq_dispatchers
   end
 
   #
