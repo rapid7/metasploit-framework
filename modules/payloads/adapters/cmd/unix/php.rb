028a7b3df21c27713525503751449807a5386f79
Martin Sutovsky <martin_sutovsky@rapid7.com>
2025-03-21 15:54:59 +0100

    More reliable full command path detection, using already existing base64 encoder

--- modules/payloads/adapters/cmd/unix/php.rb
+++ modules/payloads/adapters/cmd/unix/php.rb
@@ -20,12 +20,18 @@ module MetasploitModule
         'AdaptedPlatform' => 'php'
       )
     )
+    register_advanced_options(
+      [
+        OptString.new('Base64Decoder', [true, 'The binary to use for base64 decodin    g', 'base64-short', %w[base64-short] ])
+      ]
+    )
   end
 
   def compatible?(mod)
     if mod.type == Msf::MODULE_PAYLOAD && (mod.class.const_defined?(:CachedSize) && mod.class::CachedSize != :dynamic) && (mod.class::CachedSize >= 120_000) # echo does not have an unlimited amount of space
       return false
     end
+    return false if !datastore['Base64Decoder']
 
     super
   end
@@ -33,6 +39,6 @@ module MetasploitModule
   def generate(_opts = {})
     payload = super
 
-    "echo '#{Base64.strict_encode64(payload)}'|base64 -d|exec $(which php)"
+    "echo '#{payload}'|base64 -d|exec $(command -v php)"
   end
 end
