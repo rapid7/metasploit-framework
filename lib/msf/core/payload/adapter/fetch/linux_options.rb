7ab1e0dc1050732e9d94fbb90c9a69753602c0e1
Martin Sutovsky <martin_sutovsky@rapid7.com>
2025-03-28 15:07:37 +0100

    Fix sh option

--- lib/msf/core/payload/adapter/fetch/linux_options.rb
+++ lib/msf/core/payload/adapter/fetch/linux_options.rb
@@ -8,6 +8,7 @@ module Msf::Payload::Adapter::Fetch::LinuxOptions
         Msf::OptString.new('FETCH_FILENAME', [ false, 'Name to use on remote system when storing payload; cannot contain spaces or slashes', Rex::Text.rand_text_alpha(rand(8..12))], regex: %r{^[^\s/\\]*$}, conditions: ['FETCH_FILELESS', '==', 'none']),
         Msf::OptBool.new('FETCH_PIPE', [true, 'Host both the binary payload and the command so it can be piped directly to the shell.', false], conditions: ['FETCH_COMMAND', 'in', %w[CURL GET WGET]]),
         Msf::OptString.new('FETCH_WRITABLE_DIR', [ true, 'Remote writable dir to store payload; cannot contain spaces', './'], regex: /^\S*$/, conditions: ['FETCH_FILELESS', '==', 'none'])
+
       ]
     )
   end
