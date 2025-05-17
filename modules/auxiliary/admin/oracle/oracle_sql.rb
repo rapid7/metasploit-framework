15f2ac0e4e5467fec9a257a1f589c5d5f742b712
bcoles <bcoles@gmail.com>
2025-05-18 00:23:38 +1000

    modules/auxiliary/admin/oracle: Resolve RuboCop violations

--- modules/auxiliary/admin/oracle/oracle_sql.rb
+++ modules/auxiliary/admin/oracle/oracle_sql.rb
@@ -7,29 +7,38 @@ class MetasploitModule < Msf::Auxiliary
   include Msf::Exploit::ORACLE
 
   def initialize(info = {})
-    super(update_info(info,
-      'Name'           => 'Oracle SQL Generic Query',
-      'Description'    => %q{
+    super(
+      update_info(
+        info,
+        'Name' => 'Oracle SQL Generic Query',
+        'Description' => %q{
           This module allows for simple SQL statements to be executed
           against an Oracle instance given the appropriate credentials
           and sid.
-      },
-      'Author'         => [ 'MC' ],
-      'License'        => MSF_LICENSE,
-      'References'     =>
-        [
+        },
+        'Author' => [ 'MC' ],
+        'License' => MSF_LICENSE,
+        'References' => [
           [ 'URL', 'http://web.archive.org/web/20110322124810/http://www.metasploit.com:80/users/mc/' ],
         ],
-      'DisclosureDate' => '2007-12-07'))
+        'DisclosureDate' => '2007-12-07',
+        'Notes' => {
+          'Stability' => [CRASH_SAFE],
+          'SideEffects' => [IOC_IN_LOGS],
+          'Reliability' => []
+        }
+      )
+    )
 
-      register_options(
-        [
-          OptString.new('SQL', [ false, 'The SQL to execute.',  'select * from v$version']),
-        ])
+    register_options(
+      [
+        OptString.new('SQL', [false, 'The SQL to execute.', 'select * from v$version']),
+      ]
+    )
   end
 
   def run
-    return if not check_dependencies
+    return if !check_dependencies
 
     query = datastore['SQL']
 
@@ -42,7 +51,7 @@ class MetasploitModule < Msf::Auxiliary
           print_status(line)
         end
       end
-    rescue => e
+    rescue StandardError
       return
     end
   end
