53ce08ef6e90c8d97cfed0927e33668f8c77cb00
dledda-r7 <diego_ledda@rapid7.com>
2025-06-15 09:55:09 -0400

    feat: add stdapi_webcam extension

---
+++ lib/rex/post/meterpreter/extensions/stdapi_webcam/stdapi_webcam.rb
@@ -0,0 +1,131 @@
+# -*- coding: binary -*-
+
+require 'rex/post/meterpreter/object_aliases'
+require 'rex/post/meterpreter/extension'
+require 'rex/post/meterpreter/extensions/stdapi/constants'
+require 'rex/post/meterpreter/extensions/stdapi/tlv'
+require 'rex/post/meterpreter/extensions/stdapi/command_ids'
+require 'rex/post/meterpreter/extensions/stdapi/mic/mic'
+require 'rex/post/meterpreter/extensions/stdapi/audio_output/audio_output'
+require 'rex/post/meterpreter/extensions/stdapi/webcam/webcam'
+require 'rex/post/meterpreter/extensions/stdapi/sys/config'
+require 'rex/post/meterpreter/extensions/stdapi/sys/process'
+require 'rex/post/meterpreter/extensions/stdapi/sys/registry'
+require 'rex/post/meterpreter/extensions/stdapi/sys/event_log'
+require 'rex/post/meterpreter/extensions/stdapi/sys/power'
+
+module Rex
+  module Post
+    module Meterpreter
+      module Extensions
+        module Stdapi_Webcam
+          module AudioOutput
+            include Rex::Post::Meterpreter::Extensions::Stdapi::AudioOutput
+          end
+
+          module Mic
+            include Rex::Post::Meterpreter::Extensions::Stdapi::Mic
+          end
+
+          module Webcam
+            include Rex::Post::Meterpreter::Extensions::Stdapi::Webcam
+          end
+
+          module Sys
+            include Rex::Post::Meterpreter::Extensions::Stdapi::Sys
+          end
+          include Rex::Post::Meterpreter::Extensions::Stdapi
+
+          ###
+          #
+          # Standard ruby interface to remote entities for meterpreter.  It provides
+          # basic access to files, network, system, and other properties of the remote
+          # machine that are fairly universal.
+          #
+          ###
+          class Stdapi_Webcam < Extension
+
+            def self.extension_id
+              Rex::Post::Meterpreter::Extensions::Stdapi::EXTENSION_ID_STDAPI
+            end
+
+            #
+            # Initializes an instance of the standard API extension.
+            #
+            def initialize(client)
+              super(client, 'stdapi_webcam')
+
+              # Alias the following things on the client object so that they
+              # can be directly referenced
+              client.register_extension_aliases(
+                [
+                  {
+                    'name' => 'audio_output',
+                    'ext' => Rex::Post::Meterpreter::Extensions::Stdapi_Webcam::AudioOutput::AudioOutput.new(client)
+                  },
+                  {
+                    'name' => 'mic',
+                    'ext' => Rex::Post::Meterpreter::Extensions::Stdapi_Webcam::Mic::Mic.new(client)
+                  },
+                  {
+                    'name' => 'sys',
+                    'ext' => ObjectAliases.new(
+                      {
+                        'config' => Rex::Post::Meterpreter::Extensions::Stdapi_Webcam::Sys::Config.new(client),
+                        'process' => process,
+                        'registry' => registry,
+                        'eventlog' => eventlog,
+                        'power' => power
+                      }
+                    )
+                  },
+                  {
+                    'name' => 'webcam',
+                    'ext' => Rex::Post::Meterpreter::Extensions::Stdapi_Webcam::Webcam::Webcam.new(client)
+                  },
+                ]
+              )
+            end
+
+            #
+            # Sets the client instance on a duplicated copy of the supplied class.
+            #
+            def brand(klass)
+              klass = klass.dup
+              klass.client = client
+              return klass
+            end
+
+            #
+            # Returns a copy of the Process class.
+            #
+            def process
+              brand(Rex::Post::Meterpreter::Extensions::Stdapi_Webcam::Sys::Process)
+            end
+
+            #
+            # Returns a copy of the Registry class.
+            #
+            def registry
+              brand(Rex::Post::Meterpreter::Extensions::Stdapi_Webcam::Sys::Registry)
+            end
+
+            #
+            # Returns a copy of the EventLog class.
+            #
+            def eventlog
+              brand(Rex::Post::Meterpreter::Extensions::Stdapi_Webcam::Sys::EventLog)
+            end
+
+            #
+            # Returns a copy of the Power class.
+            #
+            def power
+              brand(Rex::Post::Meterpreter::Extensions::Stdapi_Webcam::Sys::Power)
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/rex/post/meterpreter/ui/console/command_dispatcher/stdapi_webcam.rb
@@ -0,0 +1,71 @@
+# -*- coding: binary -*-
+
+require 'rex/post/meterpreter'
+
+module Rex
+  module Post
+    module Meterpreter
+      module Ui
+        ###
+        #
+        # Standard API extension.
+        #
+        ###
+        class Console::CommandDispatcher::Stdapi_Webcam
+          require 'rex/post/meterpreter/ui/console/command_dispatcher/stdapi'
+          require 'rex/post/meterpreter/ui/console/command_dispatcher/stdapi/webcam'
+
+          Klass = Console::CommandDispatcher::Stdapi_Webcam
+
+          Dispatchers =
+            [
+              Console::CommandDispatcher::Stdapi::Webcam,
+            ]
+
+          include Console::CommandDispatcher
+
+          def self.has_command?(name)
+            Dispatchers.any? { |klass| klass.has_command?(name) }
+          end
+
+          #
+          # Initializes an instance of the stdapi command interaction.
+          #
+          def initialize(shell)
+            super
+
+            Dispatchers.each do |d|
+              shell.enstack_dispatcher(d)
+            end
+            str_dispatchers = []
+            uniq_dispatchers = []
+            idx = 0
+            while idx < shell.dispatcher_stack.length
+              unless str_dispatchers.include?(shell.dispatcher_stack[idx].class.to_s)
+                str_dispatchers.push(shell.dispatcher_stack[idx].class.to_s)
+                uniq_dispatchers.push(shell.dispatcher_stack[idx])
+              end
+              idx += 1
+            end
+            shell.dispatcher_stack = uniq_dispatchers
+          end
+
+          #
+          # List of supported commands.
+          #
+          def commands
+            {}
+          end
+
+          #
+          # Name for this dispatcher
+          #
+          def name
+            'Standard Webcam extension'
+          end
+
+        end
+      end
+    end
+  end
+end
