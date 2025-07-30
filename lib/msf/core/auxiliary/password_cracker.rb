96ba71b47f84878ddc7a07fda082cd9568c9684e
Martin Sutovsky <martin_sutovsky@rapid7.com>
2025-07-30 07:11:35 +0200

    Removes incorrect comment

--- lib/msf/core/auxiliary/password_cracker.rb
+++ lib/msf/core/auxiliary/password_cracker.rb
@@ -89,8 +89,6 @@ module Msf
         fail_with(Msf::Module::Failure::BadConfig, e.message)
       end
 
-      # redefine cracker is action is auto
-
       # throw this to a local variable since it causes a shell out to pull the version
       cracker_version = cracker.cracker_version
       if cracker.cracker == 'john' && (cracker_version.nil? || !cracker_version.include?('jumbo'))
