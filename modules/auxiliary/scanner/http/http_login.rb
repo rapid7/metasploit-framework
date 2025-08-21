16e4d48a73349047198f30001f69e296c6d0bfe5
rodolphopivetta <rodolpho.pivetta@gmail.com>
2025-08-21 00:19:48 -0400

    Add ssl param to new HTTP scanner on http_login

--- modules/auxiliary/scanner/http/http_login.rb
+++ modules/auxiliary/scanner/http/http_login.rb
@@ -171,6 +171,7 @@ class MetasploitModule < Msf::Auxiliary
         cred_details: cred_collection,
         stop_on_success: datastore['STOP_ON_SUCCESS'],
         bruteforce_speed: datastore['BRUTEFORCE_SPEED'],
+        ssl: datastore['SSL'],
         http_success_codes: success_codes,
         connection_timeout: 5
       )
