2896b232d1542fa4072b4701e4a79487567c2db4
Metasploit <metasploit@rapid7.com>
2025-08-28 06:09:25 -0500

    Bump version of framework to 6.4.86

--- lib/metasploit/framework/version.rb
+++ lib/metasploit/framework/version.rb
@@ -32,7 +32,7 @@ module Metasploit
         end
       end
 
-      VERSION = "6.4.85"
+      VERSION = "6.4.86"
       MAJOR, MINOR, PATCH = VERSION.split('.').map { |x| x.to_i }
       PRERELEASE = 'dev'
       HASH = get_hash
