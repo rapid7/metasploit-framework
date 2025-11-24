10a267d673898ab66f74da7a984368cb21b1afdf
Umasreemandla <umasreemandla@gmail.com>
2025-11-24 18:29:04 +0530

    Add C program to calculate the sum of two numbers

---
+++ sum.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    sum = a + b;
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
