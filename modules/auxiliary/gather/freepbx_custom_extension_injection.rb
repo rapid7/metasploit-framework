147fd901b0d54d7c0faad4db24a187ef3851ebb5
Martin Sutovsky <martin_sutovsky@rapid7.com>
2026-01-06 14:57:45 +0100

    Adds descriptions

--- modules/auxiliary/gather/freepbx_custom_extension_injection.rb
+++ modules/auxiliary/gather/freepbx_custom_extension_injection.rb
@@ -12,7 +12,9 @@ class MetasploitModule < Msf::Auxiliary
       update_info(
         info,
         'Name' => 'FreePBX Custom Extension SQL Injection',
-        'Description' => 'TODO',
+        'Description' => %q{
+          FreePBX versions prior to 16.0.44 and 17.0.23 are vulnerable to multiple CVEs, specifically CVE-2025-66039 and CVE-2025-61675, in the context of this module. The former represents an authentication bypass: when FreePBX uses Webserver Authorization Mode (an option the admin can enable), it allows an attacker to authenticate as any user. The latter CVE describes multiple SQL injections; this module exploits the SQL injection in the custom extension component. The module chains these vulnerabilities into an unauthenticated SQL injection attack that creates a new fake user and effectively grants an attacker access to the administration.
+        },
         'Author' => [
           'Noah King', # research
           'msutovsky-r7', # module
