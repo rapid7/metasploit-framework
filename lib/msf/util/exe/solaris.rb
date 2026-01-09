186050785088cd3d7672d6cd6b9f80cf95ed27f6
dledda-r7 <diego_ledda@rapid7.com>
2026-01-09 07:21:27 -0500

    feat: utils/exe.rb break-up solaris executable handling

--- lib/msf/util/exe/solaris.rb
+++ lib/msf/util/exe/solaris.rb
@@ -1,6 +1,9 @@
 module Msf::Util::EXE::Solaris
   def to_executable(framework, arch, code, opts = {}, fmt = 'elf')
-    return EXE::Solaris::X86.to_executable(framework, code, opts, fmt) if arch =~ /x86|i386/i
+    solaris = Object.new.extend(Msf::Util::EXE::Common)
+    # solaris.extend(Msf::Util::EXE::Solaris::Common) Solaris has no common module yet
+    solaris.extend(Msf::Util::EXE::Solaris::X86) if arch =~ /x86|i386/i
+    return solaris.to_executable(framework, code, opts, fmt) if solaris.respond_to?(:to_executable)
     nil
   end
 end
