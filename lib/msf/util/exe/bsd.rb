033a2358f82fad8ad6ab3eb48dd84143f41ab15b
dledda-r7 <diego_ledda@rapid7.com>
2026-01-09 07:20:39 -0500

    feat: utils/exe.rb refactor BSD executable handling

--- lib/msf/util/exe/bsd.rb
+++ lib/msf/util/exe/bsd.rb
@@ -1,7 +1,11 @@
 module Msf::Util::EXE::Bsd
   def to_executable(framework, arch, code, opts = {}, fmt = 'elf')
-    return EXE::Bsd::X86.to_executable(framework, code, opts, fmt) if arch =~ /x86|i386/i
-    return EXE::Bsd::X64.to_executable(framework, code, opts, fmt) if arch =~ /x64|amd64/i
+    bsd = Object.new.extend(Msf::Util::EXE::Common)
+    # bsd.extend(Msf::Util::EXE::Bsd::Common) BSD has no common module yet
+    bsd.extend(Msf::Util::EXE::Bsd::X86) if arch =~ /x86|i386/i
+    bsd.extend(Msf::Util::EXE::Bsd::X64) if arch =~ /x64|amd64/i
+    return bsd.to_executable(framework, code, opts, fmt) if bsd.respond_to?(:to_executable)
+
     nil
   end
 end
