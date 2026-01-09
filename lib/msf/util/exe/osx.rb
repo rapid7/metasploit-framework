0802ef9adc81716b45695006c3cb62f256264cef
dledda-r7 <diego_ledda@rapid7.com>
2026-01-09 07:21:15 -0500

    feat: utils/exe.rb refactor OSX executable handling

--- lib/msf/util/exe/osx.rb
+++ lib/msf/util/exe/osx.rb
@@ -1,9 +1,13 @@
 module Msf::Util::EXE::OSX
   def to_executable(framework, arch, code, opts = {}, fmt = 'macho')
-    return Msf::Util::EXE::OSX::Armle::to_executable(framework, code, opts, fmt) if arch =~ /armle|armv7/i
-    return Msf::Util::EXE::OSX::Aarch64::to_executable(framework, code, opts, fmt) if arch =~ /aarch64|arm64/i
-    return Msf::Util::EXE::OSX::X86::to_executable(framework, code, opts, fmt) if arch =~ /x86|i386/i
-    return Msf::Util::EXE::OSX::X64::to_executable(framework, code, opts, fmt) if arch =~ /x64|amd64/i
+    osx = Object.new.extend(Msf::Util::EXE::Common)
+    osx.extend(Msf::Util::EXE::OSX::Common)
+    osx.extend(Msf::Util::EXE::OSX::X86) if arch =~ /x86|i386/i
+    osx.extend(Msf::Util::EXE::OSX::X64) if arch =~ /x64|amd64/i
+    osx.extend(Msf::Util::EXE::OSX::Armle) if arch =~ /armle|armv7/i
+    osx.extend(Msf::Util::EXE::OSX::Aarch64) if arch =~ /aarch64|arm64/i
+    return osx.to_executable(framework, code, opts, fmt) if osx.respond_to?(:to_executable)
+
     nil
   end
 
