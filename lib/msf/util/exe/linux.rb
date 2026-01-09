6598e151b71493469a5c5060d67584171c705022
dledda-r7 <diego_ledda@rapid7.com>
2026-01-09 07:20:59 -0500

    feat: utils/exe.rb refactor Linux executable handling

--- lib/msf/util/exe/linux.rb
+++ lib/msf/util/exe/linux.rb
@@ -1,9 +1,13 @@
 module Msf::Util::EXE::Linux
   def to_executable(framework, arch, code, opts = {}, fmt = 'elf')
-    return EXE::Linux::X86.to_executable(framework, code, opts, fmt) if arch =~ /x86|i386/i
-    return EXE::Linux::X64.to_executable(framework, code, opts, fmt) if arch =~ /x64|amd64/i
-    return EXE::Linux::Aarch64.to_executable(framework, code, opts, fmt) if arch =~ /aarch64|arm64/i
-    return EXE::Linux::Armle.to_executable(framework, code, opts, fmt) if arch =~ /armle|armv7l/i
+    linux = Object.new.extend(Msf::Util::EXE::Common)
+    linux.extend(Msf::Util::EXE::Linux::Common)
+    linux.extend(Msf::Util::EXE::Linux::Common)
+    linux.extend(Msf::Util::EXE::Linux::X86) if arch =~ /x86|i386/i
+    linux.extend(Msf::Util::EXE::Linux::X64) if arch =~ /x64|x86_64|amd64/i
+    linux.extend(Msf::Util::EXE::Linux::Aarch64) if arch =~ /aarch64|arm64/i
+    linux.extend(Msf::Util::EXE::Linux::Armle) if arch =~ /armle|armv7l/i
+    return linux.to_executable(framework, code, opts, fmt) if linux.respond_to?(:to_executable)
     nil
   end
 end
