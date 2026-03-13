d5c6b0333a709b4fd58fb40e4e89670c317fc5bd
Ramesh Adhikari <adhikari.resume@gmail.com>
2026-03-13 15:23:39 +0530

    Fix VERBOSE flag ignored when set before module load
    When 'set VERBOSE false' is called before 'use <module>', the value is
    stored in the framework datastore as a raw string 'false' without going
    through OptBool#normalize. Ruby treats non-empty strings as truthy, so
    the verbose condition evaluated 'false' (string) as true.
    
    Fix by using .to_s =~ TRUE_REGEX for both module and framework datastore
    lookups, consistent with how OptBool normalizes boolean values.

--- lib/msf/core/module/ui/line/verbose.rb
+++ lib/msf/core/module/ui/line/verbose.rb
@@ -1,6 +1,7 @@
 module Msf::Module::UI::Line::Verbose
+  TRUE_REGEX = /^(y|yes|t|1|true)$/i
   # Verbose version of #print_line
-  def vprint_line(msg='')
-    print_line(msg) if datastore['VERBOSE'] || framework.datastore['VERBOSE']
+  def vprint_line(msg = '')
+    print_line(msg) if datastore['VERBOSE'].to_s =~ TRUE_REGEX || (!framework.nil? && framework.datastore['VERBOSE'].to_s =~ TRUE_REGEX)
   end
 end
--- lib/msf/core/module/ui/message/verbose.rb
+++ lib/msf/core/module/ui/message/verbose.rb
@@ -1,23 +1,25 @@
 module Msf::Module::UI::Message::Verbose
+  TRUE_REGEX = /^(y|yes|t|1|true)$/i
   # Verbose version of #print_error
-  def vprint_error(msg='')
-    print_error(msg) if datastore['VERBOSE'] || (!framework.nil? && framework.datastore['VERBOSE'])
+  def vprint_error(msg = '')
+    print_error(msg) if datastore['VERBOSE'].to_s =~ TRUE_REGEX || (!framework.nil? && framework.datastore['VERBOSE'].to_s =~ TRUE_REGEX)
   end
 
-  alias_method :vprint_bad, :vprint_error
+  alias vprint_bad vprint_error
 
   # Verbose version of #print_good
-  def vprint_good(msg='')
+  def vprint_good(msg = '')
     print_good(msg) if datastore['VERBOSE'] || (!framework.nil? && framework.datastore['VERBOSE'])
   end
+  alias vprint_bad vprint_error
 
   # Verbose version of #print_status
-  def vprint_status(msg='')
-    print_status(msg) if datastore['VERBOSE'] || (!framework.nil? && framework.datastore['VERBOSE'])
+  def vprint_status(msg = '')
+    print_status(msg) if datastore['VERBOSE'].to_s =~ TRUE_REGEX || (!framework.nil? && framework.datastore['VERBOSE'].to_s =~ TRUE_REGEX)
   end
 
   # Verbose version of #print_warning
-  def vprint_warning(msg='')
-    print_warning(msg) if datastore['VERBOSE'] || (!framework.nil? && framework.datastore['VERBOSE'])
+  def vprint_warning(msg = '')
+    print_warning(msg) if datastore['VERBOSE'].to_s =~ TRUE_REGEX || (!framework.nil? && framework.datastore['VERBOSE'].to_s =~ TRUE_REGEX)
   end
 end
