21f1fbacebd15b8ee332d416160f86d0079a43bb
vognik <vognik@tuta.io>
2026-04-04 11:18:03 -0700

    Add CVE-2026-29053

---
+++ data/exploits/CVE-2026-29053/theme/assets/js/index.js
@@ -0,0 +1,13 @@
+// JavaScript files are compiled and minified during the build process to the assets/built folder. See available scripts in the package.json file.
+
+// Import CSS
+import "../css/index.css";
+
+// Import JS
+import menuOpen from "./menuOpen";
+import infiniteScroll from "./infiniteScroll";
+
+
+// Call the menu and infinite scroll functions
+menuOpen();
+infiniteScroll();
