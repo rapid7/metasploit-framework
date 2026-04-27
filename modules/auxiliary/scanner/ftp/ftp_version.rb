317d3b5722da171895a83ca00fae2ac53ec78dd7
g0t mi1k <have.you.g0tmi1k@gmail.com>
2026-04-27 13:26:33 +0100

    ftp_version: Add notes & references

--- modules/auxiliary/scanner/ftp/ftp_version.rb
+++ modules/auxiliary/scanner/ftp/ftp_version.rb
@@ -11,8 +11,19 @@ class MetasploitModule < Msf::Auxiliary
     super(
       'Name' => 'FTP Version Scanner',
       'Description' => 'Detect FTP Version.',
-      'Author' => 'hdm',
-      'License' => MSF_LICENSE
+      'Author' => [
+        'hdm',
+        'g0tmi1k' # @g0tmi1k - additional features
+      ],
+      'License' => MSF_LICENSE,
+      'References' => [
+        ['URL', 'https://www.ietf.org/rfc/rfc959']
+      ],
+      'Notes' => {
+        'Stability' => [CRASH_SAFE],
+        'Reliability' => [],
+        'SideEffects' => [IOC_IN_LOGS]
+      }
     )
 
     register_options(
