7c66020441b71f76c0ef50e512e4c8bb823fa9e0
g0t mi1k <have.you.g0tmi1k@gmail.com>
2026-04-29 15:22:16 +0100

    ssh_version: Update name and description

--- modules/auxiliary/scanner/ssh/ssh_version.rb
+++ modules/auxiliary/scanner/ssh/ssh_version.rb
@@ -12,8 +12,12 @@ class MetasploitModule < Msf::Auxiliary
 
   def initialize
     super(
-      'Name' => 'SSH Version Scanner',
-      'Description' => 'Detect SSH Version, and the server encryption',
+      'Name' => 'SSH Version and Algorithm Scanner',
+      'Description' => %q{
+         This module detects the SSH version, cryptographic algorithm support,
+         as well as the host key fingerprint. It will flag deprecated/weak ciphers,
+         HMAC algorithms, key exchange methods, and host key types.
+      },
       'References' => [
         ['URL', 'https://en.wikipedia.org/wiki/SecureShell'], # general info
         ['URL', 'https://datatracker.ietf.org/doc/html/rfc8732#name-deprecated-algorithms'], # deprecation of kex gss-sha1 stuff
@@ -24,7 +28,8 @@ class MetasploitModule < Msf::Auxiliary
       ],
       'Author' => [
         'Daniel van Eeden <metasploit[at]myname.nl>', # original author
-        'h00die' # algorithms enhancements
+        'h00die', # algorithms enhancements
+        'g0tmi1k' # @g0tmi1k - additional features
       ],
       'License' => MSF_LICENSE
     )
