1ee5b6eb353e91b581940ec71ee6895eaa16e34a
g0t mi1k <have.you.g0tmi1k@gmail.com>
2026-05-05 09:06:27 +0100

    Strip out same address in msg

--- lib/msf/core/module/ui/message.rb
+++ lib/msf/core/module/ui/message.rb
@@ -6,14 +6,14 @@ module Msf::Module::UI::Message
 
   def print_error(msg='', prefix: nil)
     msg_prefix = prefix.nil? ? print_prefix : prefix
-    super(msg_prefix + msg)
+    super(msg_prefix + strip_peer_prefix(msg_prefix, msg))
   end
 
   alias_method :print_bad, :print_error
 
   def print_good(msg='', prefix: nil)
     msg_prefix = prefix.nil? ? print_prefix : prefix
-    super(msg_prefix + msg)
+    super(msg_prefix + strip_peer_prefix(msg_prefix, msg))
   end
 
   def print_prefix
@@ -38,11 +38,27 @@ module Msf::Module::UI::Message
 
   def print_status(msg='', prefix: nil)
     msg_prefix = prefix.nil? ? print_prefix : prefix
-    super(msg_prefix + msg)
+    super(msg_prefix + strip_peer_prefix(msg_prefix, msg))
   end
 
   def print_warning(msg='', prefix: nil)
     msg_prefix = prefix.nil? ? print_prefix : prefix
-    super(msg_prefix + msg)
+    super(msg_prefix + strip_peer_prefix(msg_prefix, msg))
+  end
+
+  private
+
+  # When print_prefix already contains a peer address (e.g. injected by Msf::Exploit::Remote::Tcp),
+  # strip that same address from the start of msg to avoid it appearing twice in the output
+  def strip_peer_prefix(msg_prefix, msg)
+    # IPv4 address (e.g. 127.0.0.1:yy)
+    if (m = msg_prefix.match(/(\d+\.\d+\.\d+\.\d+:\d+)/))
+      msg.to_s.sub(/\A#{Regexp.escape(m[1])}\s*-\s*/, '')
+    # IPv6 address (e.g. [::1]:yy    ::1:yy)
+    elsif (m = msg_prefix.match(/\[([\da-fA-F:]+)\]:(\d+)/))
+      msg.to_s.sub(/\A(?:\[#{Regexp.escape(m[1])}\]|#{Regexp.escape(m[1])}):#{m[2]}\s*-\s*/, '')
+    else
+      msg
+    end
   end
 end
