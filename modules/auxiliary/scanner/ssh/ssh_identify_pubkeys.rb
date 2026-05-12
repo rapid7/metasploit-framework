bb13b2d6a843422ade012e41074ace44a55adb0d
g0t mi1k <have.you.g0tmi1k@gmail.com>
2026-05-12 10:04:00 +0100

    ssh_identify_pubkeys: Support more key types

--- modules/auxiliary/scanner/ssh/ssh_identify_pubkeys.rb
+++ modules/auxiliary/scanner/ssh/ssh_identify_pubkeys.rb
@@ -8,6 +8,7 @@
 require 'net/ssh'
 require 'sshkey'
 require 'net/ssh/pubkey_verifier'
+require 'tempfile'
 
 class MetasploitModule < Msf::Auxiliary
   include Msf::Auxiliary::Scanner
@@ -65,8 +66,6 @@ class MetasploitModule < Msf::Auxiliary
       'PASSWORD', 'PASS_FILE', 'BLANK_PASSWORDS', 'USER_AS_PASS', 'USERPASS_FILE', 'DB_ALL_PASS', 'DB_ALL_CREDS'
     )
 
-    @good_credentials = {}
-    @good_key = ''
     @strip_passwords = true
   end
 
@@ -86,39 +85,46 @@ class MetasploitModule < Msf::Auxiliary
     datastore['RHOST']
   end
 
-  def read_keyfile(file)
-    if file == :keyfile_b64
-      keyfile = datastore['SSH_KEYFILE_B64'].unpack('m*').first
-    elsif file.is_a? Array
-      keyfile = ''
-      file.each do |dir_entry|
-        next unless ::File.readable? dir_entry
-
-        keyfile << ::File.open(dir_entry, 'rb') { |f| f.read(f.stat.size) }
-      end
-    else
-      keyfile = ::File.open(file, 'rb') { |f| f.read(f.stat.size) }
-    end
+  def extract_raw_keys(content)
     keys = []
     this_key = []
     in_key = false
-    keyfile.split("\n").each do |line|
-      if /(?<key>ssh-(?:dss|rsa)\s+.*)/ =~ line
+    content.split("\n").each do |line|
+      if /(?<key>(?:ssh-|ecdsa-)\S+\s+\S+.*)/ =~ line
         keys << key
         next
       end
-      in_key = true if (line =~ /^-----BEGIN [RD]SA (PRIVATE|PUBLIC) KEY-----/)
+      in_key = true if line =~ /^-----BEGIN (?:[RD]SA|EC|OPENSSH) (?:PRIVATE|PUBLIC) KEY-----/
       this_key << line if in_key
-      next unless (line =~ /^-----END [RD]SA (PRIVATE|PUBLIC) KEY-----/)
+      next unless line =~ /^-----END (?:[RD]SA|EC|OPENSSH) (?:PRIVATE|PUBLIC) KEY-----/
 
       in_key = false
       keys << (this_key.join("\n") + "\n")
       this_key = []
     end
+    keys
+  end
+
+  def read_keyfile(file)
+    if file == :keyfile_b64
+      content = datastore['SSH_KEYFILE_B64'].unpack('m*').first
+      keys = validate_keys(extract_raw_keys(content))
+    elsif file.is_a? Array
+      keys = []
+      file.each do |dir_entry|
+        next unless ::File.readable? dir_entry
+
+        content = ::File.open(dir_entry, 'rb') { |f| f.read(f.stat.size) }
+        keys.concat(validate_keys(extract_raw_keys(content)))
+      end
+    else
+      content = ::File.open(file, 'rb') { |f| f.read(f.stat.size) }
+      keys = validate_keys(extract_raw_keys(content))
+    end
     if keys.empty?
       print_error "#{ip}:#{rport} SSH - No valid keys found"
     end
-    return validate_keys(keys)
+    keys
   end
 
   # Validates that the key isn't total garbage, and converts PEM formatted
@@ -126,9 +132,24 @@ class MetasploitModule < Msf::Auxiliary
   def validate_keys(keys)
     keepers = []
     keys.each do |key|
-      if key =~ /ssh-(dss|rsa)/
-        # A public key has been provided
+      if key =~ /(?:ssh-|ecdsa-)\S+/
         keepers << { public: key, private: '' }
+      elsif key =~ /-----BEGIN (?:OPENSSH|EC) PRIVATE KEY-----/
+        ssh_key = Tempfile.open('msf_ssh_key') do |f|
+          f.write(key)
+          f.flush
+
+          begin
+            Net::SSH::KeyFactory.load_private_key(f.path)
+          rescue StandardError
+            nil
+          end
+        end
+
+        if ssh_key
+          public_key = "#{ssh_key.ssh_type} #{[ssh_key.to_blob].pack('m0')}"
+          keepers << { public: public_key, private: key }
+        end
       else
         # Use the mighty SSHKey library from James Miller to convert them on the fly.
         # This is where a PRIVATE key has been provided
@@ -143,7 +164,7 @@ class MetasploitModule < Msf::Auxiliary
     if keepers.empty?
       print_error "#{ip}:#{rport} SSH - No valid keys found"
     end
-    return keepers.uniq
+    keepers.uniq
   end
 
   def pull_cleartext_keys(keys)
