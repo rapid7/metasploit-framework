cf67b7f144008b42058651d10e24976000f0530b
adfoster-r7 <alandavid_foster@rapid7.com>
2026-06-10 11:22:50 +0100

    Add additional import tests

--- lib/metasploit/framework/spec/threads/logger.rb
+++ lib/metasploit/framework/spec/threads/logger.rb
@@ -10,32 +10,38 @@ require 'securerandom'
 
 require 'metasploit/framework/spec/threads/suite'
 
-original_thread_new = Thread.method(:new)
+# Guard against double-patching if loaded via both RUBYOPT and spec_helper
+unless Thread.respond_to?(:_metasploit_thread_logger_enabled)
+  original_thread_new = Thread.method(:new)
 
-# Patches `Thread.new` so that if logs `caller` so thread leaks can be traced
-Thread.define_singleton_method(:new) { |*args, &block|
-  uuid = SecureRandom.uuid
-  # tag caller with uuid so that only leaked threads caller needs to be printed
-  lines = ["BEGIN Thread.new caller (#{uuid})"]
+  # Patches `Thread.new` so that if logs `caller` so thread leaks can be traced
+  Thread.define_singleton_method(:new) { |*args, &block|
+    uuid = SecureRandom.uuid
+    # tag caller with uuid so that only leaked threads caller needs to be printed
+    lines = ["BEGIN Thread.new caller (#{uuid})"]
 
-  caller.each do |frame|
-    lines << "  #{frame}"
-  end
+    caller.each do |frame|
+      lines << "  #{frame}"
+    end
 
-  lines << 'END Thread.new caller'
+    lines << 'END Thread.new caller'
 
-  Metasploit::Framework::Spec::Threads::Suite::LOG_PATHNAME.parent.mkpath
+    Metasploit::Framework::Spec::Threads::Suite::LOG_PATHNAME.parent.mkpath
 
-  Metasploit::Framework::Spec::Threads::Suite::LOG_PATHNAME.open('a') { |f|
-    # single puts so threads can't write in between each other.
-    f.puts lines.join("\n")
+    Metasploit::Framework::Spec::Threads::Suite::LOG_PATHNAME.open('a') { |f|
+      # single puts so threads can't write in between each other.
+      f.puts lines.join("\n")
+    }
+
+    options = {original_args: args, uuid: uuid}
+
+    original_thread_new.call(options) {
+      # record uuid for thread-leak detection can used uuid to correlate log with this thread.
+      Thread.current[Metasploit::Framework::Spec::Threads::Suite::UUID_THREAD_LOCAL_VARIABLE] = options.fetch(:uuid)
+      block.call(*options.fetch(:original_args))
+    }
   }
 
-  options = {original_args: args, uuid: uuid}
-
-  original_thread_new.call(options) {
-    # record uuid for thread-leak detection can used uuid to correlate log with this thread.
-    Thread.current[Metasploit::Framework::Spec::Threads::Suite::UUID_THREAD_LOCAL_VARIABLE] = options.fetch(:uuid)
-    block.call(*options.fetch(:original_args))
-  }
-}
+  # Marker method to indicate the logger patch has been applied
+  Thread.define_singleton_method(:_metasploit_thread_logger_enabled) { true }
+end
