919f41124fffeb85a3b97ca7b1d393bbf8d013ed
Sokratis Tzifkas <stzifkas@gmail.com>
2026-06-10 23:37:07 +0300

    fix LHOST type assertions in opt_spec

--- spec/lib/msf/core/opt_spec.rb
+++ spec/lib/msf/core/opt_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Msf::Opt do
 
     context 'LHOST' do
       subject { described_class::LHOST }
-      it { is_expected.to be_a(Msf::OptAddress) }
+      it { is_expected.to be_a(Msf::OptAddressOrHostname) }
     end
 
     context 'LPORT' do
@@ -73,7 +73,7 @@ RSpec.describe Msf::Opt do
 
     context 'LHOST()' do
       subject { described_class::LHOST(default) }
-      it { is_expected.to be_a(Msf::OptAddress) }
+      it { is_expected.to be_a(Msf::OptAddressOrHostname) }
       specify 'sets default' do
         expect(subject.default).to eq(default)
       end
