f835ace8cdb4fa994fcb2633716278c683b9344e
dledda-r7 <diego_ledda@rapid7.com>
2026-07-17 16:06:52 -0400

    feat(sessions): display async mode indicator in session listing

--- lib/msf/base/serializer/readable_text.rb
+++ lib/msf/base/serializer/readable_text.rb
@@ -864,6 +864,14 @@ class ReadableText
       row[-1] << " #{session.platform}"
     end
 
+    if session.respond_to?(:async_mode_enabled?) && session.async_mode_enabled?
+      row[-1] << " (async)"
+    end
+
+    if session.respond_to?(:async_mode_enabled?) && session.async_mode_enabled?
+      row[-1] << " (async)"
+    end
+
     if show_extended
       if session.respond_to?(:last_checkin) && session.last_checkin
         row << "#{(Time.now.to_i - session.last_checkin.to_i)}s ago"
