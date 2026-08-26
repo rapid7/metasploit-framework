8bd71a1b072e8d55c04319611a21d16021996276
bwatters-r7 <bwatters@rapid7.com>
2026-08-26 10:48:46 -0500

    Fix default assumptions to be cleaner

--- lib/rubocop/cop/lint/module_duplicate_option.rb
+++ lib/rubocop/cop/lint/module_duplicate_option.rb
@@ -3,8 +3,9 @@
 module RuboCop
   module Cop
     module Lint
-      # Detects Opt helper calls that supply only a new default for an option
-      # already registered by a mixin. Use the DefaultOptions metadata instead.
+      # Detects options registered by a mixin that are registered again without
+      # providing a more specific description. Use DefaultOptions to change only
+      # the value of an inherited option.
       #
       # @example
       #   # bad
@@ -19,43 +20,54 @@ module RuboCop
       #     'RPORT' => 4840
       #   }
       class ModuleDuplicateOption < Base
+        extend AutoCorrector
+        include RangeHelp
+
         MIXIN_OPTIONS = {
           'Msf::Auxiliary::Scanner' => {
-            'RHOSTS' => nil
+            'RHOSTS' => { default: nil, description: 'The target host(s), see https://docs.metasploit.com/docs/using-metasploit/basics/using-metasploit.html' },
+            'THREADS' => { default: 1, description: 'The number of concurrent threads (max one per host)' }
           },
           'Msf::Exploit::Remote::HttpClient' => {
-            'RHOST' => nil,
-            'RPORT' => 80,
-            'Proxies' => nil
+            'RHOST' => { default: nil, description: 'The target host(s), see https://docs.metasploit.com/docs/using-metasploit/basics/using-metasploit.html' },
+            'RPORT' => { default: 80, description: 'The target port' },
+            'VHOST' => { default: nil, description: 'HTTP server virtual host' },
+            'SSL' => { default: false, description: 'Negotiate SSL/TLS for outgoing connections' },
+            'Proxies' => { default: nil, description: nil }
           },
           'Msf::Exploit::Remote::Tcp' => {
-            'RHOST' => nil,
-            'RPORT' => nil
+            'RHOST' => { default: nil, description: 'The target host(s), see https://docs.metasploit.com/docs/using-metasploit/basics/using-metasploit.html' },
+            'RPORT' => { default: nil, description: 'The target port' }
           },
           'Msf::Exploit::Remote::Udp' => {
-            'RHOST' => nil,
-            'RPORT' => nil
+            'RHOST' => { default: nil, description: 'The target host(s), see https://docs.metasploit.com/docs/using-metasploit/basics/using-metasploit.html' },
+            'RPORT' => { default: nil, description: 'The target port' }
           }
         }.freeze
 
         MSG = 'Do not register the pre-existing %<option>s option again; set its value in DefaultOptions instead.'
 
-        def on_send(node)
-          return unless node.method?(:register_options)
-
-          class_node = node.each_ancestor(:class).first
-          return unless class_node&.identifier&.const_name == 'MetasploitModule'
+        def on_class(class_node)
+          return unless class_node.identifier&.const_name == 'MetasploitModule'
 
           inherited_options = inherited_options(class_node)
           return if inherited_options.empty?
 
-          option_nodes(node).each do |option_node|
-            option_name = option_name(option_node)
-            next unless inherited_options.key?(option_name)
-            next unless only_default_changed?(option_node, inherited_options[option_name])
-            next if deregistered_before?(class_node, node, option_name)
+          offenses = class_node.each_descendant(:send).select { |node| node.method?(:register_options) }.flat_map do |register_node|
+            option_nodes(register_node).filter_map do |option_node|
+              option_name = option_name(option_node)
+              next unless inherited_options.key?(option_name)
+              next unless description_unchanged?(option_node, inherited_options[option_name][:description])
+              next if deregistered_before?(class_node, register_node, option_name)
 
-            add_offense(option_node, message: format(MSG, option: option_name))
+              [option_node, option_name, inherited_options[option_name]]
+            end
+          end
+
+          offenses.each_with_index do |(option_node, option_name, _option), index|
+            add_offense(option_node, message: format(MSG, option: option_name)) do |corrector|
+              autocorrect(corrector, class_node, offenses) if index.zero?
+            end
           end
         end
 
@@ -88,11 +100,155 @@ module RuboCop
           end
         end
 
-        def only_default_changed?(node, inherited_default)
-          return false unless node.receiver&.const_name == 'Opt' && node.arguments.one?
+        def description_unchanged?(node, inherited_description)
+          description = if node.receiver&.const_name == 'Opt'
+                          node.arguments[2]
+                        elsif node.method?(:new)
+                          node.arguments[1]&.values&.[](1)
+                        end
 
-          argument = node.first_argument
-          !argument.respond_to?(:value) || argument.value != inherited_default
+          description.nil? || description.nil_type? || (description.str_type? && description.value == inherited_description)
+        end
+
+        def autocorrect(corrector, class_node, offenses)
+          defaults = offenses.filter_map do |option_node, option_name, inherited_option|
+            default_node = option_default(option_node)
+            next if same_default?(default_node, inherited_option[:default])
+
+            [option_name, { source: default_node&.source || 'nil', comment: trailing_comment(option_node) }]
+          end.to_h
+
+          info_hash = info_hash(class_node)
+          return if defaults.any? && info_hash.nil?
+
+          offenses.map(&:first).group_by { |option_node| option_node.each_ancestor(:send).find { |node| node.method?(:register_options) } }.each do |register_node, option_nodes|
+            remove_options(corrector, register_node, option_nodes)
+          end
+          add_defaults(corrector, info_hash, defaults) if defaults.any?
+        end
+
+        def trailing_comment(node)
+          source = node.source_range.source_buffer.source
+          line_end = source.index("\n", node.source_range.end_pos) || source.length
+          source[node.source_range.end_pos...line_end][/#.*$/]
+        end
+
+        def option_default(node)
+          if node.receiver&.const_name == 'Opt'
+            node.first_argument
+          elsif node.method?(:new)
+            node.arguments[1]&.values&.[](2)
+          end
+        end
+
+        def same_default?(node, inherited_default)
+          return inherited_default.nil? if node.nil? || node.nil_type?
+
+          node.respond_to?(:value) && node.value == inherited_default
+        end
+
+        def info_hash(class_node)
+          update_info = class_node.each_descendant(:send).find { |node| node.method?(:update_info) || node.method?(:merge_info) }
+          hash = update_info&.arguments&.find(&:hash_type?)
+          return hash if hash
+
+          initialize_node = class_node.each_descendant(:def).find { |node| node.method_name == :initialize }
+          super_node = initialize_node&.each_descendant(:super)&.first
+          super_node&.arguments&.find(&:hash_type?)
+        end
+
+        def remove_options(corrector, register_node, removed_nodes)
+          array = register_node.first_argument
+          remaining_nodes = array&.array_type? ? array.values - removed_nodes : []
+          if remaining_nodes.empty?
+            remove_expression(corrector, register_node)
+          else
+            removed_nodes.each { |node| remove_array_element(corrector, node) }
+          end
+        end
+
+        def remove_expression(corrector, node)
+          range = node.source_range
+          source = range.source_buffer.source
+          begin_pos = source.rindex("\n", range.begin_pos - 1)&.+(1) || range.begin_pos
+          previous_line_begin = source.rindex("\n", begin_pos - 2)&.+(1) || 0
+          begin_pos = previous_line_begin if source[previous_line_begin...begin_pos].strip.empty?
+          end_pos = source.index("\n", range.end_pos)&.+(1) || range.end_pos
+          corrector.remove(range_between(begin_pos, end_pos))
+        end
+
+        def remove_array_element(corrector, node)
+          siblings = node.parent.values
+          next_node = siblings[siblings.index(node) + 1]
+          if next_node
+            corrector.remove(range_between(node.source_range.begin_pos, next_node.source_range.begin_pos))
+          else
+            previous_node = siblings[siblings.index(node) - 1]
+            corrector.remove(range_between(previous_node.source_range.end_pos, node.source_range.end_pos))
+          end
+        end
+
+        def add_defaults(corrector, hash, defaults)
+          default_pair = hash.pairs.find { |pair| pair.key.str_type? && pair.key.value == 'DefaultOptions' }
+          if default_pair&.value&.hash_type?
+            merge_defaults(corrector, default_pair.value, defaults)
+          else
+            first_pair = hash.pairs.first
+            unless first_pair
+              indent = hash.source_range.column + 2
+              entries = default_entries(defaults, indent + 2)
+              corrector.insert_after(hash.loc.begin, "\n#{' ' * indent}'DefaultOptions' => {\n#{entries}\n#{' ' * indent}}\n#{' ' * (indent - 2)}")
+              return
+            end
+
+            indent = first_pair.source_range.column
+            entries = default_entries(defaults, indent + 2)
+            corrector.insert_before(first_pair, "'DefaultOptions' => {\n#{entries}\n#{' ' * indent}},\n#{' ' * indent}")
+          end
+        end
+
+        def merge_defaults(corrector, hash, defaults)
+          existing = hash.pairs.filter_map { |pair| [pair.key.value, pair] if pair.key.str_type? }.to_h
+          missing = []
+          defaults.each do |name, value|
+            if existing[name]
+              corrector.replace(existing[name].value, value[:source])
+              corrector.insert_before(existing[name], "#{value[:comment]}\n#{' ' * existing[name].source_range.column}") if value[:comment]
+            else
+              missing << [name, value]
+            end
+          end
+          return if missing.empty?
+
+          last_pair = hash.pairs.last
+          if last_pair
+            if hash.single_line?
+              comments = missing.filter_map { |_name, value| value[:comment] }
+              if comments.any?
+                default_pair = hash.parent
+                indent = default_pair.source_range.column
+                corrector.insert_before(default_pair, "#{comments.join("\n#{' ' * indent}")}\n#{' ' * indent}")
+              end
+              entries = missing.map { |name, value| "'#{name}' => #{value[:source]}" }.join(', ')
+              corrector.insert_after(last_pair, ", #{entries}")
+              return
+            end
+
+            indent = last_pair.source_range.column
+            entries = default_entries(missing.to_h, indent)
+            corrector.insert_after(last_pair, ",\n#{entries}")
+          else
+            indent = hash.source_range.column + 2
+            entries = default_entries(missing.to_h, indent)
+            corrector.insert_after(hash.loc.begin, "\n#{entries}\n#{' ' * (indent - 2)}")
+          end
+        end
+
+        def default_entries(defaults, indent)
+          defaults.map do |name, value|
+            comment = value[:comment] ? "#{' ' * indent}#{value[:comment]}\n" : ''
+            "#{comment}#{' ' * indent}'#{name}' => #{value[:source]}"
+          end.join(",\n")
         end
 
         def deregistered_before?(class_node, register_node, option_name)
--- spec/rubocop/cop/lint/module_duplicate_option_spec.rb
+++ spec/rubocop/cop/lint/module_duplicate_option_spec.rb
@@ -3,6 +3,7 @@
 require 'spec_helper'
 require 'rubocop/cop/lint/module_duplicate_option'
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe RuboCop::Cop::Lint::ModuleDuplicateOption do
   subject(:cop) { described_class.new(config) }
 
@@ -22,27 +23,29 @@ RSpec.describe RuboCop::Cop::Lint::ModuleDuplicateOption do
     RUBY
   end
 
-  it 'does not flag an inherited option registered with an option class' do
-    expect_no_offenses(<<~RUBY)
+  it 'flags an inherited option registered with the same description' do
+    expect_offense(<<~RUBY)
       class MetasploitModule < Msf::Auxiliary
         include Msf::Exploit::Remote::Tcp
 
         def initialize(info = {})
           super
           register_options([OptPort.new('RPORT', [true, 'The target port', 4840])])
+                            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Lint/ModuleDuplicateOption: Do not register the pre-existing RPORT option again; set its value in DefaultOptions instead.
         end
       end
     RUBY
   end
 
-  it 'does not flag an option class already registered by the HTTP client mixin' do
-    expect_no_offenses(<<~RUBY)
+  it 'flags an option class with the inherited description' do
+    expect_offense(<<~RUBY)
       class MetasploitModule < Msf::Auxiliary
         include Msf::Exploit::Remote::HttpClient
 
         def initialize(info = {})
           super
           register_options([OptBool.new('SSL', [false, 'Negotiate SSL/TLS for outgoing connections', true])])
+                            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Lint/ModuleDuplicateOption: Do not register the pre-existing SSL option again; set its value in DefaultOptions instead.
         end
       end
     RUBY
@@ -90,14 +93,15 @@ RSpec.describe RuboCop::Cop::Lint::ModuleDuplicateOption do
     RUBY
   end
 
-  it 'does not flag an option class already registered by the scanner mixin' do
-    expect_no_offenses(<<~RUBY)
+  it 'flags an option class already registered by the scanner mixin' do
+    expect_offense(<<~RUBY)
       class MetasploitModule < Msf::Auxiliary
         include Msf::Auxiliary::Scanner
 
         def initialize(info = {})
           super
           register_options([OptInt.new('THREADS', [true, 'The number of concurrent threads (max one per host)', 25])])
+                            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Lint/ModuleDuplicateOption: Do not register the pre-existing THREADS option again; set its value in DefaultOptions instead.
         end
       end
     RUBY
@@ -116,40 +120,43 @@ RSpec.describe RuboCop::Cop::Lint::ModuleDuplicateOption do
     RUBY
   end
 
-  it 'does not flag an inherited option when its required value also changes' do
-    expect_no_offenses(<<~RUBY)
+  it 'flags an inherited option when its required value changes but its description does not' do
+    expect_offense(<<~RUBY)
       class MetasploitModule < Msf::Auxiliary
         include Msf::Exploit::Remote::Tcp
 
         def initialize(info = {})
           super
           register_options([OptPort.new('RPORT', [false, 'The target port', 4840])])
+                            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Lint/ModuleDuplicateOption: Do not register the pre-existing RPORT option again; set its value in DefaultOptions instead.
         end
       end
     RUBY
   end
 
-  it 'does not flag an inherited option when its type also changes' do
-    expect_no_offenses(<<~RUBY)
+  it 'flags an inherited option when its type changes but its description does not' do
+    expect_offense(<<~RUBY)
       class MetasploitModule < Msf::Auxiliary
         include Msf::Exploit::Remote::Tcp
 
         def initialize(info = {})
           super
           register_options([OptString.new('RPORT', [true, 'The target port', '4840'])])
+                            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Lint/ModuleDuplicateOption: Do not register the pre-existing RPORT option again; set its value in DefaultOptions instead.
         end
       end
     RUBY
   end
 
-  it 'does not flag an identical inherited option' do
-    expect_no_offenses(<<~RUBY)
+  it 'flags an identical inherited option' do
+    expect_offense(<<~RUBY)
       class MetasploitModule < Msf::Auxiliary
         include Msf::Exploit::Remote::HttpClient
 
         def initialize(info = {})
           super
           register_options([Opt::RPORT(80)])
+                            ^^^^^^^^^^^^^^ Lint/ModuleDuplicateOption: Do not register the pre-existing RPORT option again; set its value in DefaultOptions instead.
         end
       end
     RUBY
@@ -162,7 +169,7 @@ RSpec.describe RuboCop::Cop::Lint::ModuleDuplicateOption do
 
         def initialize(info = {})
           super
-          register_options([Opt::RPORT(4840, false)])
+          register_options([Opt::RPORT(4840, false, 'The application port')])
         end
       end
     RUBY
@@ -217,4 +224,173 @@ RSpec.describe RuboCop::Cop::Lint::ModuleDuplicateOption do
       end
     RUBY
   end
+
+  it 'moves a changed default to DefaultOptions' do
+    expect_offense(<<~RUBY)
+      class MetasploitModule < Msf::Auxiliary
+        include Msf::Exploit::Remote::Tcp
+
+        def initialize(info = {})
+          super(update_info(
+            info,
+            'Name' => 'Example'
+          ))
+          register_options([Opt::RPORT(4840)])
+                            ^^^^^^^^^^^^^^^^ Lint/ModuleDuplicateOption: Do not register the pre-existing RPORT option again; set its value in DefaultOptions instead.
+        end
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      class MetasploitModule < Msf::Auxiliary
+        include Msf::Exploit::Remote::Tcp
+
+        def initialize(info = {})
+          super(update_info(
+            info,
+            'DefaultOptions' => {
+              'RPORT' => 4840
+            },
+            'Name' => 'Example'
+          ))
+        end
+      end
+    RUBY
+  end
+
+  it 'removes an identical option without adding a default' do
+    expect_offense(<<~RUBY)
+      class MetasploitModule < Msf::Auxiliary
+        include Msf::Exploit::Remote::HttpClient
+
+        def initialize(info = {})
+          super(update_info(info, 'Name' => 'Example'))
+          register_options([OptString.new('VHOST', [false, 'HTTP server virtual host'])])
+                            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Lint/ModuleDuplicateOption: Do not register the pre-existing VHOST option again; set its value in DefaultOptions instead.
+        end
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      class MetasploitModule < Msf::Auxiliary
+        include Msf::Exploit::Remote::HttpClient
+
+        def initialize(info = {})
+          super(update_info(info, 'Name' => 'Example'))
+        end
+      end
+    RUBY
+  end
+
+  it 'merges multiple changed defaults into existing DefaultOptions' do
+    expect_offense(<<~RUBY)
+      class MetasploitModule < Msf::Auxiliary
+        include Msf::Exploit::Remote::Tcp
+
+        def initialize(info = {})
+          super(update_info(
+            info,
+            'DefaultOptions' => {
+              'RPORT' => 1234,
+              'VERBOSE' => true
+            }
+          ))
+          register_options([
+            Opt::RPORT(8080),
+            ^^^^^^^^^^^^^^^^ Lint/ModuleDuplicateOption: Do not register the pre-existing RPORT option again; set its value in DefaultOptions instead.
+            Opt::RHOST('127.0.0.1')
+            ^^^^^^^^^^^^^^^^^^^^^^^ Lint/ModuleDuplicateOption: Do not register the pre-existing RHOST option again; set its value in DefaultOptions instead.
+          ])
+        end
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      class MetasploitModule < Msf::Auxiliary
+        include Msf::Exploit::Remote::Tcp
+
+        def initialize(info = {})
+          super(update_info(
+            info,
+            'DefaultOptions' => {
+              'RPORT' => 8080,
+              'VERBOSE' => true,
+              'RHOST' => '127.0.0.1'
+            }
+          ))
+        end
+      end
+    RUBY
+  end
+
+  it 'keeps unrelated options in the registration array' do
+    expect_offense(<<~RUBY)
+      class MetasploitModule < Msf::Auxiliary
+        include Msf::Exploit::Remote::Tcp
+
+        def initialize(info = {})
+          super(update_info(
+            info,
+            'Name' => 'Example'
+          ))
+          register_options([
+            Opt::RPORT(4840),
+            ^^^^^^^^^^^^^^^^ Lint/ModuleDuplicateOption: Do not register the pre-existing RPORT option again; set its value in DefaultOptions instead.
+            OptString.new('PATH', [true, 'A path'])
+          ])
+        end
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      class MetasploitModule < Msf::Auxiliary
+        include Msf::Exploit::Remote::Tcp
+
+        def initialize(info = {})
+          super(update_info(
+            info,
+            'DefaultOptions' => {
+              'RPORT' => 4840
+            },
+            'Name' => 'Example'
+          ))
+          register_options([
+            OptString.new('PATH', [true, 'A path'])
+          ])
+        end
+      end
+    RUBY
+  end
+
+  it 'merges a default into legacy super metadata' do
+    expect_offense(<<~RUBY)
+      class MetasploitModule < Msf::Auxiliary
+        include Msf::Exploit::Remote::HttpClient
+
+        def initialize
+          super(
+            'Name' => 'Example',
+            'DefaultOptions' => { 'SSL' => true }
+          )
+          register_options([Opt::RPORT(443)]) # HTTPS port
+                            ^^^^^^^^^^^^^^^ Lint/ModuleDuplicateOption: Do not register the pre-existing RPORT option again; set its value in DefaultOptions instead.
+        end
+      end
+    RUBY
+
+    expect_correction(<<~RUBY)
+      class MetasploitModule < Msf::Auxiliary
+        include Msf::Exploit::Remote::HttpClient
+
+        def initialize
+          super(
+            'Name' => 'Example',
+            # HTTPS port
+            'DefaultOptions' => { 'SSL' => true, 'RPORT' => 443 }
+          )
+        end
+      end
+    RUBY
+  end
 end
+# rubocop:enable Metrics/BlockLength
