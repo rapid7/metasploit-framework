4f41a8daddc5dd23ca8d6a0cacc89ef71e72a029
anirbala98 <anirbala98@gmail.com>
2026-09-13 01:59:26 -0400

    Remove unnecessary includes

--- modules/auxiliary/gather/4gaboards_users_info_disclosure_cve_2026_53959.rb
+++ modules/auxiliary/gather/4gaboards_users_info_disclosure_cve_2026_53959.rb
@@ -6,7 +6,6 @@
 class MetasploitModule < Msf::Auxiliary
   include Msf::Auxiliary::Report
   include Msf::Exploit::Remote::HttpClient
-  include Msf::Exploit::Remote::HTTP::Wordpress
   require 'json'
 
   def initialize(info = {})
